afd963459e0778c295e1d26d79ae74cfecaccfbb
oyhoyhk <oyhoyhk@naver.com>
2022-11-03 00:54:48 +0900

    기능: 사용자 입력 받기
    Console.readLine을 이용해 입력을 받는 기능 구현

--- src/App.js
+++ src/App.js
@@ -7,13 +7,18 @@ class App {
 	play() {
 		this.printStartMessage();
 		this.createAnswerNumbers();
+		this.getUserInput();
 	}
 	printStartMessage() {
 		Console.print('숫자 야구 게임을 시작합니다.');
-		Console.close();
 	}
 	createAnswerNumbers() {
 		this._answerNumbers = Random.pickUniqueNumbersInRange(1, 9, 3);
+	}
+	getUserInput() {
+		Console.readLine('숫자를 입력해주세요 : ', input => {
+			// 입력 받은 값을 토대로 다른 작업 진행
+		});
 	}
 }
 
