44c31bd3fcbc3e719ccfe0ee0e27d0163f142abf
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2022-11-03 06:41:24 +0900

    feat: 시작/종료 input기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  play() {
+    const user = new User();
+    selectPlayOption(user);
+  }
 }
+
+class User {
+  constructor() {
+    this.playOption;
+    this.number = [];
+  }
+}
+
+const selectPlayOption = (user) => {
+  MissionUtils.Console.readLine(
+    "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
+    (select) => {
+      console.log(`${select}`);
+      user.playOption = select;
+    }
+  );
+};
 
 module.exports = App;
