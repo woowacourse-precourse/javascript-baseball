d963dd95707d0deda25bc86cc5e4483ac29a020d
cys4585 <choizerowater@gmail.com>
2022-11-02 22:07:08 +0900

    test: Mission Utils 사용 테스트

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  play() {
+    console.log("1~9 중 랜덤: ", MissionUtils.Random.pickNumberInRange(1, 9));
+    console.log("1~9 중 랜덤: ", MissionUtils.Random.pickNumberInRange(1, 9));
+    console.log("1~9 중 랜덤: ", MissionUtils.Random.pickNumberInRange(1, 9));
+    console.log("1~9 중 랜덤: ", MissionUtils.Random.pickNumberInRange(1, 9));
+    console.log("1~9 중 랜덤: ", MissionUtils.Random.pickNumberInRange(1, 9));
+    console.log("1~9 중 랜덤: ", MissionUtils.Random.pickNumberInRange(1, 9));
+    console.log("1~9 중 랜덤: ", MissionUtils.Random.pickNumberInRange(1, 9));
+    console.log("1~9 중 랜덤: ", MissionUtils.Random.pickNumberInRange(1, 9));
+    console.log("1~9 중 랜덤: ", MissionUtils.Random.pickNumberInRange(1, 9));
+    console.log("1~9 중 랜덤: ", MissionUtils.Random.pickNumberInRange(1, 9));
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (answer) => {
+      console.log(`${answer}`);
+      MissionUtils.Console.close();
+    });
+  }
 }
 
 module.exports = App;
+
+const app = new App();
+app.play();
