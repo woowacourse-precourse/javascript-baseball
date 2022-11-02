c843d8e289d053afcb1d81f6793279180fb470c8
Sumin Kim <ksumin0730@hufs.ac.kr>
2022-11-02 19:38:26 +0900

    Feat: random 하게 정답 수 생성 Random.js : 서로다른 세 수(1~9)를 골라 합친 수를 반환

---
+++ src/model/Random.js
@@ -0,0 +1,8 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
+function randomNum(){
+  const numeach = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
+  return parseInt(numeach.join(''))
+}
+
+module.exports = randomNum;
