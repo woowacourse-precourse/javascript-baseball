eb31ecce1d01ce5fc4753424c1cdb84951fe59cf
jangsumin <jsm0726@naver.com>
2022-11-02 18:20:57 +0900

    test: read a line in console

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ const MissionUtils = require("@woowacourse/mission-utils");
 class App {
   play() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    readInputNumber();
   }
+}
+
+function readInputNumber() {
+  MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (input) => {
+    console.log(`입력된 3개의 숫자: ${input}`)
+  });
 }
 
 module.exports = App;
