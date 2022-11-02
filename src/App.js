042cea557fac706ea5a32223bcb7577c2ebcb6d2
nangkyeonglim <nangkyeonglim@gmail.com>
2022-11-02 22:10:24 +0900

    feat: 컴퓨터의 랜덤 숫자를 만드는 기능 구현

--- src/App.js
+++ src/App.js
@@ -2,6 +2,17 @@ const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
   play() {}
+
+  makeRandomNumber() {
+    const resultNumber = [];
+    while (resultNumber.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!resultNumber.includes(number)) {
+        resultNumber.push(number);
+      }
+    }
+    return resultNumber;
+  }
 }
 
 module.exports = App;
