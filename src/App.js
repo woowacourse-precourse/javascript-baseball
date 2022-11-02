da135916b8377f561863edd417b28ae7ea31ba56
HBSPS <wiyeong79@naver.com>
2022-11-02 23:55:02 +0900

    feat: add getComputerNumber method

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
   play() {}
+}
+
+function getComputerNumber() {
+  const computerNumber = [];
+  while (computerNumber.length < 3) {
+    const number = MissionUtils.Random.pickNumberInRange(1, 9);
+    if (!computerNumber.includes(number)) {
+      computerNumber.push(number);
+    }
+  }
+
+  return computerNumber;
 }
 
 module.exports = App;
