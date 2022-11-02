8c290157af3b1d2220582e90f1600260d97e7239
bassyu <yrkdgus@gmail.com>
2022-11-02 22:09:40 +0900

    feat:  난수 생성 함수 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  play() {
+    console.log(getRandomArray());
+  }
+}
+
+const getRandomArray = () => {
+  const randomArray = [];
+  while (randomArray.length < 3) {
+    const random = MissionUtils.Random.pickNumberInRange(1, 9);
+    if (!randomArray.includes(random)) {
+      randomArray.push(random);
+    }
+  }
+
+  return randomArray;
 }
 
 module.exports = App;
+
+const app = new App();
+app.play();
