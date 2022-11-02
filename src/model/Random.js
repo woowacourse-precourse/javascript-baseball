a3f141ad9b6600b11006e51bdceba7bc1345b69f
Sumin Kim <ksumin0730@hufs.ac.kr>
2022-11-02 22:17:21 +0900

    Style: 세미콜론 누락

--- src/model/Random.js
+++ src/model/Random.js
@@ -2,7 +2,7 @@ const MissionUtils = require("@woowacourse/mission-utils");
 
 function randomNum(){
   const numeach = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
-  return parseInt(numeach.join(''))
+  return parseInt(numeach.join(''));
 }
 
 module.exports = randomNum;
