4fff4e0318cff7a7ad94752063c8c47d78bd4c7a
saejinpark <saejinpark951210@gmail.com>
2022-11-02 22:00:32 +0900

    setBaseLine 기능구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
 class App {
-  play() {}
+  play() {
+    let baseLine = null;
+    const numList = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    function setBaseLine() {
+      baseLine = new Set();
+      while (baseLine.size < 3) {
+        baseLine.add(wRandom.pickNumberInList(numList));
+      }
+      baseLine = Array.from(baseLine);
+    }
+    setBaseLine();
+    
+    try {
+      progress();
+    } catch (error) {
+      throw new Error();
+    }
+  }
 }
 
 module.exports = App;
