a3e42bd2a28112fd801a0af48c8b4c4a8274f105
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2022-11-02 22:46:40 +0900

    feat: user의 입력값 검증과 중복검사 함수 구현

--- src/App.js
+++ src/App.js
@@ -16,4 +16,33 @@ const makeRandomNumber = () => {
   return NOT_DUPLICATED_NUMBER;
 };
 
+const findDuplicate = (numberArr) => {
+  const UNIQUE_ARRAY = new Set(numberArr);
+
+  if (numberArr.length !== UNIQUE_ARRAY.size) {
+    return true;
+  }
+
+  return false;
+};
+
+const condition = (input) => {
+  const USER_INPUT = input.split('');
+
+  for (let index = 0; index < USER_INPUT.length; index += 1) {
+    if (USER_INPUT[index] < '1' || USER_INPUT[index] > '9') {
+      throw '1~9 사이의 숫자를 입력해 주세요!';
+    }
+  }
+
+  if (USER_INPUT.length !== 3) {
+    throw '3자리 숫자를 입력해주세요!';
+  }
+  if (findDuplicate(USER_INPUT)) {
+    throw '중복되지 않은 숫자를 입력해 주세요!';
+  }
+
+  return USER_INPUT;
+};
+
 module.exports = App;
