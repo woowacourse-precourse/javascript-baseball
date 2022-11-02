59baf50d78f7d2e3486d683329d8ad808807fe7f
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2022-11-02 23:47:47 +0900

    refactor: 주어진 문자열을 콘솔에 출력하는 메소드 따로 구현

--- src/App.js
+++ src/App.js
@@ -4,8 +4,13 @@ class App {
   constructor(input) {
     this.input = input;
   }
+
+  print(message) {
+    MissionUtils.Console.print(message);
+  }
+
   play() {
-    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    this.print("숫자 야구 게임을 시작합니다.");
   }
 }
 
