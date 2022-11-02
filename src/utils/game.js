d110aa28f10c196f66805a16197a5b81db19115b
Jin1won <wlsdnjs16521@gmail.com>
2022-11-03 03:30:24 +0900

    refactor: game 관련 화살표 함수 축약하여 표현

--- src/utils/game.js
+++ src/utils/game.js
@@ -3,21 +3,16 @@ const { getComputerNumber } = require("./numberMaker");
 const { isValid } = require("../utils/validate");
 const { GAME_MESSAGE } = require("../constants/index");
 
-const startGame = () => {
-  MissionUtils.Console.print(GAME_MESSAGE.START);
-};
+const startGame = () => MissionUtils.Console.print(GAME_MESSAGE.START);
 
-const inputNumber = () => {
+const inputNumber = () =>
   MissionUtils.Console.readLine(GAME_MESSAGE.INPUT_NUMBER, (number) => {
     if (isValid(number)) {
       compareNumbers(number, getComputerNumber());
     }
   });
-};
 
-const closeGame = () => {
-  MissionUtils.Console.close();
-};
+const closeGame = () => MissionUtils.Console.close();
 
 const compareNumbers = (userNumber, computerNumber) => {
   console.log(userNumber, computerNumber);
