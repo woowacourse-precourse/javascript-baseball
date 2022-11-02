ca857aa0d0e1c9958dd2471849244926a8b46f35
dragonfruitlemonade <tpghks9245@gmail.com>
2022-11-02 22:59:01 +0900

    feat: 출력하기(숫자야구 게임을 시작합니다.)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+const MISSIONUTIL = require("@woowacourse/mission-utils");
+const CONSOLE = MISSIONUTIL.Console;
+
 class App {
-  play() {}
+  play() {
+    gameStartMessage();
+  }
 }
+
+function gameStartMessage() {
+  CONSOLE.print("숫자 야구 게임을 시작합니다.");
+}
+
+const app = new App();
+app.play();
+
 
 module.exports = App;
