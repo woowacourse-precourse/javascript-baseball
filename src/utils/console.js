10e9124e63e587011f9e897bc277ad47cc300c5a
박준서 <oponize@naver.com>
2022-11-03 01:15:44 +0900

    feat: 콘솔 유틸 함수 추가

---
+++ src/utils/console.js
@@ -0,0 +1,7 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
+exports.print = MissionUtils.Console.print;
+
+exports.readLine = MissionUtils.Console.readLine;
+
+exports.close = MissionUtils.Console.close;
