0a4aa26f989acff3764d2c9828b969b86d0a2db5
yu-retel <dbdudals100@naver.com>
2022-11-02 21:29:42 +0900

    feat: 컴퓨터 랜덤번호 받아오는기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+const MissionUtils = require("@woowacourse/mission-utils");
 class App {
   play() {}
 }
 
 module.exports = App;
+
+function computerInput(){
+  const computerInputArr=[];
+  while (computerInputArr.length < 3) {
+    const number = MissionUtils.Random.pickNumberInRange(1,9);
+    if (!computerInputArr.includes(number)) {
+      computerInputArr.push(number);
+    }
+  }
+  return computerInputArr;
+}
