20e5824c38784e7cc87c8d5ad7fef9279dc74f6f
minsoo <als494@naver.com>
2022-11-03 01:51:19 +0900

    feat: 랜덤 숫자 3개 뽑는 함수

--- src/App.js
+++ src/App.js
@@ -1,3 +1,15 @@
+// 1. 컴퓨터가 뽑은 랜덤 숫자 3개 배열로 만드는 함수
+function randomNums() {
+  const computer = [];
+  while (computer.length < 3) {
+    const number = MissionUtils.Random.pickNumberInRange(1, 9);
+    if (!computer.includes(number)) {
+      computer.push(number);
+    }
+  }
+  return computer;
+}
+
 class App {
   play() {}
 }
