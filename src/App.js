bf623fac501a27421c0e6d94344951acfec56ac2
0dytpq0 <rkfnahs12e@gmail.com>
2022-11-02 16:59:14 +0900

    컴퓨터의 수 3자리 구하기.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
-class App {
-  play() {}
+// 컴퓨터의 수 3자리 구하기.
+function getMachineNum() {
+  let value = [];
+  let inputNumber;
+  while (1) {
+    inputNumber = Math.floor(Math.random() * 10);
+    if (value.indexOf(inputNumber) !== -1 || inputNumber === 0) {
+      continue;
+    }
+    value.push(inputNumber);
+    if (value.length === 3) {
+      break;
+    }
+  }
+  return value.join("");
 }
 
-module.exports = App;
+
+// class App {
+//   play() { }
+
+
+
+// }
+
+// module.exports = App;
