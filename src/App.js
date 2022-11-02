a0e17253636d21360c3b2fa775969045f25f89be
0dytpq0 <rkfnahs12e@gmail.com>
2022-11-02 19:01:55 +0900

    get machine's random number

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+// 컴퓨터의 수 3자리 구하기.
+
+const machinNum = [];
+while (machinNum.length < 3) {
+  const number = MissionUtils.Random.pickNumberInRange(1, 9);
+  if (!machinNum.includes(number)) {
+    machinNum.push(number);
+  }
+}
+console.log(machinNum.join(""));
+
+
+
 class App {
-  play() {}
+  play() { }
+
+
+
 }
 
-module.exports = App;
+
+// module.exports = App;
