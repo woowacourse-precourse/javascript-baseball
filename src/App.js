a1e612cd08cd5deddcf25a3fe843b9a9b2e8d5f2
oyhoyhk <oyhoyhk@naver.com>
2022-11-02 21:06:19 +0900

    기능: 시작 메시지 출력
    mission-utils의 Console을 이용해 게임 시작 메시지 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+const { Console, Random } = require('@woowacourse/mission-utils');
+
 class App {
-  play() {}
+	constructor() {
+		this._answerNumbers = null;
+	}
+	play() {
+		this.printStartMessage();
+		this.createAnswerNumbers();
+	}
+	printStartMessage() {
+		Console.print('숫자 야구 게임을 시작합니다.');
+		Console.close();
+	}
 }
+
+const app = new App();
+
+app.play();
 
 module.exports = App;
