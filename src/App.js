58fe1b8b15933863eb1a3e21c2d2fdc2ae8bcfb2
guridaek <iue16@naver.com>
2022-11-02 20:03:59 +0900

    feat: 랜덤 숫자 생성 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  constructor() {
+    this.targetNumber = [];
+  }
+
+  resetTargetNumber() {
+    this.targetNumber = [];
+    while (this.targetNumber.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!this.targetNumber.includes(number)) {
+        this.targetNumber.push(number);
+      }
+    }
+  }
+
+  play() {
+    this.resetTargetNumber();
+  }
 }
 
 module.exports = App;
