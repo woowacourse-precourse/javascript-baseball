dda900010cbc29cd07067dd3baf5c2b8dfd6e418
An Sol <apdlvmf486@gmail.com>
2022-11-02 22:06:34 +0900

    feat: 정답 생성 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 class App {
   play() {
     const MissionUtils = require("@woowacourse/mission-utils");
-    console.log(MissionUtils.Random.pickNumberInList([1, 2, 3]));
+
+    function makeAnswer() {
+      const THREE_NUMBERS = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
+      const ANSWER = THREE_NUMBERS[0] * 100 + THREE_NUMBERS[1] * 10 + THREE_NUMBERS[2] * 1;
+      return ANSWER;
+    }
+
   }
 }
 const app = new App();
