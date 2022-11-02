62b86b6b149a362b46c1c6fd4c8d6be43d7ba7f3
Ryong-E <think8867@gmail.com>
2022-11-02 21:15:27 +0900

    feat: getRandomNumbers 매서드 생성 컴퓨터 숫자 생성 기능
    - 중복없이 3자리 숫자
    - 1~9 사이의 숫자만 가능

--- src/App.js
+++ src/App.js
@@ -1,4 +1,21 @@
+const MissionUtils = require("@woowacourse/mission-utils")
 class App {
+  constructor(){
+    this.computer = this.getRandomNumbers();
+  }
+
+  getRandomNumbers(){
+    const Numbers = [];
+    while(Numbers.length < 3){
+      const number = MissionUtils.Random.pickNumberInRange(1,9);
+      if(!Numbers.includes(number)){
+        Numbers.push(number)
+      }
+    }
+
+    return Numbers
+  }
+
   play() {}
 }
 
