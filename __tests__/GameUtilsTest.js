fc80565c7e29d5070e8aa04b746237b54fa916fe
Suyoung <jsy8492@gmail.com>
2022-11-02 19:05:58 +0900

    test: 컴퓨터가 생각중인 숫자 생성 기능 테스트 수정

--- __tests__/GameUtilsTest.js
+++ __tests__/GameUtilsTest.js
@@ -6,13 +6,9 @@ const app = new App();
 describe('게임 기능 테스트', () => {
   describe('컴퓨터가 생각중인 숫자를 무작위로 생성', () => {
     test('서로 다른 임의의 수 3개로 이루어진 배열 생성', () => {
-      const numberSet = new Set();
+      const result = app.generateRandomNumbers();
 
-      while (numberSet.size !== 3) {
-        numberSet.add(Random.pickNumberInRange(1, 9));
-      }
-
-      expect([...numberSet]).toHaveLength(3);
+      expect(result).toHaveLength(3);
     });
   });
 
