7028f57e7a7de557fa98a0e26e50547594dbcc4b
eunsung <esk114477@gmail.com>
2022-11-02 17:37:50 +0900

    feat : computer 랜덤 숫자 생성

--- src/App.js
+++ src/App.js
@@ -2,4 +2,15 @@ class App {
   play() {}
 }
 
+function setComputerNumber() {
+  const computerNumber = [];
+  while (computerNumber.length < 3) {
+    const randomNumber = MissionUtils.Random.pickNumberInRange(1,9);
+    if(!computerNumber.includes(randomNumber)){
+      computerNumber.push(randomNumber);
+    }
+  }
+  return computerNumber;
+}
+
 module.exports = App;
