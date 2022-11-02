7bbba84dbe7bcc607ba73eb64188826f1c71091a
박준서 <oponize@naver.com>
2022-11-03 01:15:21 +0900

    feat: 숫자를 입력받는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 const { EXCEPTION } = require("./constants/index.js");
-const { generateRandomNumber } = require("./utils/number.js");
+const { isValidUserInput, generateRandomNumber } = require("./utils/number.js");
+const Console = require("./utils/console.js");
 
 class App {
   constructor() {
     this.randomNumber;
+    this.userInputNumber;
+    this.showMessage("숫자 야구 게임을 시작합니다.");
   }
 
   play() {}
@@ -13,6 +16,18 @@ class App {
       start: EXCEPTION.MIN_NUMBER,
       end: EXCEPTION.MAX_NUMBER,
       count: EXCEPTION.VALID_NUMBER_LENGTH,
+    });
+  }
+
+  showMessage(message) {
+    Console.print(message);
+  }
+
+  getUserInputNumber() {
+    Console.readLine("숫자를 입력해주세요 : ", (userInputNumber) => {
+      if (!isValidUserInput(userInputNumber))
+        throw new Error("인풋 값이 유효하지 않습니다.");
+      this.userInputNumber = userInputNumber;
     });
   }
 }
