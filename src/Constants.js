0c762b6bacbb982aeeb0fce26dee43e8d6a63a73
Sumin Kim <ksumin0730@hufs.ac.kr>
2022-11-02 23:16:59 +0900

    Fix: 상수 수정 ANSWER 에서 3스트라이크 같이 출력

--- src/Constants.js
+++ src/Constants.js
@@ -15,7 +15,7 @@ const GAME = Object.freeze({
     INPUT : '숫자를 입력해주세요 : ',
     RESTART : '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.',
     START : '숫자 야구 게임을 시작합니다.',
-    ANSWER : '3개의 숫자를 모두 맞히셨습니다! 게임 종료',
+    ANSWER : '3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료',
 })
 
 module.exports = {
