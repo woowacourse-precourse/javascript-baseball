af3472387db28c1c1a02898b12709c21c87485a4
박정규 <srk99279@naver.com>
2022-11-02 23:42:10 +0900

    feat : 볼카운트 체크하는 함수 구현. 사용자 입력 받는 함수 내부의 게임 리셋 관련 코드는 외부로 분리 예정.

--- src/App.js
+++ src/App.js
@@ -8,26 +8,70 @@ const makeRandomNumber = () => {
       computer.push(number);
     }
   }
-  console.log(computer);
   return computer;
 };
 
-const inputNumber = () => {
-  let userInputNumber;
+const inputNumber = (targetNumber) => {
+  let isCorrect = false;
+  console.log(targetNumber);
 
   MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (answer) => {
-    if (answer == 1) makeRandomNumber();
-    if (answer == 2) MissionUtils.Console.close();
-  });
+    let guessNumber;
 
-  return userInputNumber;
+    if (answer.length === 3) {
+      guessNumber = answer.split("").map((v) => +v);
+      let [ball, strike] = checkBallCount(targetNumber, guessNumber);
+      if (strike < 3) inputNumber(targetNumber);
+      if (strike == 3) {
+        MissionUtils.Console.print(
+          "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
+        );
+        /**
+         * reset game 함수 분리
+         */
+      }
+    }
+
+    // 재경기 & 종료
+    if (answer == 1) {
+      app.play();
+    }
+    if (answer == 2) {
+      MissionUtils.Console.close();
+    }
+  });
+  return isCorrect;
+};
+
+const checkBallCount = (targetNumber, guessNumber) => {
+  let ball = 0;
+  let strike = 0;
+
+  for (let number = 0; number < guessNumber.length; number++) {
+    if (
+      targetNumber.includes(guessNumber[number]) &&
+      targetNumber[number] !== guessNumber[number]
+    ) {
+      ball++;
+    }
+    if (
+      targetNumber.includes(guessNumber[number]) &&
+      targetNumber[number] === guessNumber[number]
+    ) {
+      strike++;
+    }
+  }
+
+  return [ball, strike];
 };
 
 class App {
   play() {
-    makeRandomNumber();
+    let target = makeRandomNumber();
 
-    inputNumber();
+    inputNumber(target);
+
+    // checkBallCount([5, 8, 7], [1, 2, 3]);
 
     return;
   }
