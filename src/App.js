10e701c832db858c95714a48cc573485b516be8c
jangsumin <jsm0726@naver.com>
2022-11-02 18:43:18 +0900

    feat: create random computer number

--- src/App.js
+++ src/App.js
@@ -3,8 +3,15 @@ const MissionUtils = require("@woowacourse/mission-utils");
 class App {
   play() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    
+    let computerNumber = createComputerNumber();
+    
     readInputNumber();
   }
+}
+
+function createComputerNumber() {
+  return MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3).join("");
 }
 
 function readInputNumber() {
