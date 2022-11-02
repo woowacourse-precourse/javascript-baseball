96cffcc73732a1f60bfb6295c8cbac92aba7a16a
Jin1won <wlsdnjs16521@gmail.com>
2022-11-03 00:55:20 +0900

    feat: 컴퓨터와 사용자가 입력한 값을 비교한다.

--- src/utils/game.js
+++ src/utils/game.js
@@ -1,4 +1,5 @@
 const MissionUtils = require("@woowacourse/mission-utils");
+const { getComputerNumber } = require("./numberMaker");
 const { GAME_MESSAGE } = require("../constants/index");
 
 const startGame = () => {
@@ -7,8 +8,12 @@ const startGame = () => {
 
 const inputNumber = () => {
   MissionUtils.Console.readLine(GAME_MESSAGE.INPUT_NUMBER, (number) => {
-    console.log(number);
+    compareNumbers(number, getComputerNumber());
   });
+};
+
+const compareNumbers = (userNumber, computerNumber) => {
+  console.log(userNumber === computerNumber);
 };
 
 module.exports = { startGame, inputNumber };
