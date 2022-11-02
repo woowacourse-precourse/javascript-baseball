6230636db534faaf23643bac9d9b7b33c8d1c194
An Sol <apdlvmf486@gmail.com>
2022-11-03 08:50:31 +0900

    fix: 입력 값 체크 기능 수정
    CHECK_INPUT_DURING_GAME 함수는 정수와 자연수를 구분하지 못했고, 1번째 자리 숫자와 3번째 자리 숫자가 같을 때를 고려하지 않았음
    CHECK_INPUT_AFTER_GAME 함수는 일관성을 주기 위해 리턴 값 수정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 class App {
   play() {
     const MissionUtils = require("@woowacourse/mission-utils");
-    
+
     const MAKEANSWER = function makeAnswerWithThreeUniqueNumbers() {
       const THREE_NUMBERS = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
       const ANSWER = THREE_NUMBERS[0] * 100 + THREE_NUMBERS[1] * 10 + THREE_NUMBERS[2] * 1;
@@ -11,14 +11,22 @@ class App {
     const CHECK_INPUT_DURING_GAME = function checkInputDuringGame(userInput) {
       if (userInput.length !== 3) return false;
       if (isNaN(userInput)) return false;
-      if (userInput[0] === userInput[1] || userInput[1] === userInput[2]) return false;
+      if (userInput[0] === userInput[1] 
+        || userInput[1] === userInput[2] 
+        || userInput[0] === userInput[2]) return false;
+      if (!(userInput.match(/[123456789]{3}/))) return false;
       return true;
     }
 
     const CHECK_INPUT_AFTER_GAME = function checkInputAfterGame(userInput) {
-      if (+userInput === 1 || +userInput === 2) return false;
-      return true;
+      if (+userInput === 1 || +userInput === 2) return true;
+      return false;
     }
+
+
+    MissionUtils.Console.readLine('닉네임을 입력해주세요.', (answer) => {
+      console.log(CHECK_INPUT_AFTER_GAME(answer));
+    });
 
   }
 }
