1befc40edcf917e3a5d76ea7dd57190be13e28d9
kyw0716 <kyw0716@naver.com>
2022-11-03 01:40:18 +0900

    feat: 입력값 유효성 판별 기능 추가

--- src/App.js
+++ src/App.js
@@ -11,9 +11,35 @@ class App {
       console.log(this.inputNumber);
     });
   }
+
+  getIsInputValueValid(inputValue) {
+    let isInputValueValid = true;
+    if (
+      getIsLengthInvalid(inputValue) ||
+      getIsRepeatExist(inputValue) ||
+      getIsInvalidWordExist(inputValue)
+    )
+      isInputValueValid = false;
+    return isInputValueValid;
+  }
 }
+
+const getIsLengthInvalid = (inputValue) => {
+  return inputValue.length !== 3;
+};
+
+const getIsRepeatExist = (inputValue) => {
+  for (let i = 1; i < inputValue.length; i++) {
+    if (inputValue[i - 1] === inputValue[i]) return true;
+  }
+  return false;
+};
+
+const getIsInvalidWordExist = (inputValue) => {
+  let inputValueWithoutNumber = inputValue.replace(/[1-9]/g, "");
+  return inputValueWithoutNumber.length !== 0;
+};
 
 module.exports = App;
 
 const app = new App();
-app.play();
