4722418b4afe9a870576179978e4bdd7375cb77b
박준서 <oponize@naver.com>
2022-11-03 01:13:07 +0900

    feat: 올바른 입력 값인지 검증하는 기능 구현

--- src/utils/number.js
+++ src/utils/number.js
@@ -1,6 +1,15 @@
 const MissionUtils = require("@woowacourse/mission-utils");
+const { EXCEPTION } = require("../constants/index.js");
 
 const generateRandomNumber = ({ start, end, count }) =>
   MissionUtils.Random.pickUniqueNumbersInRange(start, end, count).join("");
 
+const isValidUserInput = (input) => {
+  if (input.length !== EXCEPTION.VALID_NUMBER_LENGTH) return false;
+  if (isNaN(input)) return false;
+  if (input.includes(0)) return false;
+  return true;
+};
+
 exports.generateRandomNumber = generateRandomNumber;
+exports.isValidUserInput = isValidUserInput;
