6e4733243534971b90f25c6470d96a1521a26e0d
DESKTOP-PDMTIQO\hansu <fathlic5@gmail.com>
2022-11-03 03:35:22 +0900

    기능1 컴퓨터 숫자 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 class App {
-  play() {}
+  play() {
+    const computerNumber = isComputerNumber();
+  }
 }
-
+function isComputerNumber(){
+  const MissionUtils = require("@woowacourse/mission-utils");
+  const computerNumbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
+  return computerNumbers;
+}
 module.exports = App;
