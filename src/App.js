a81788188c59ff955c1e9ff1cd41065bc688416d
hyunjin <leehj0110@kakao.com>
2022-11-03 01:11:54 +0900

    feat: 게임이 실행되면 출력되는 문구 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+const { Console, Random } = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  play() {
+    Console.print("숫자 야구 게임을 시작합니다.");
+    Console.close();
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
