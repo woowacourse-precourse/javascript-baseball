e47227ef6c5525497353a708262d5fef49f02ab9
kangju2000 <kangju2000@naver.com>
2022-11-02 20:00:01 +0900

    feat: isAvailableValue 메서드 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 class App {
-  play() {}
+  #isPlaying = false;
+  #computerValue = null;
+  #userValue = null;
+
+  isAvailableValue(value) {
+    let stringValue = value + '';
+    return stringValue.length === 3 && /^[1-9]{3}$/.test([...new Set(stringValue)].join(''));
+  }
 }
 
 module.exports = App;
