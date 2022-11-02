2969815e467809851b6930d20ff59756b56c05e3
Subin <80036437+hansb1023@users.noreply.github.com>
2022-11-03 02:33:48 +0900

    전체 구조 잡아두고 숫자 입력부분 구현 중

--- src/App.js
+++ src/App.js
@@ -1,5 +1,38 @@
 class App {
-  play() {}
+  play() {
+    let random = []; //랜덤 숫자가 담길 빈 배열 생성
+    let numbers = 0;
+    
+    for(let i=0; i<3; i++){  //random 배열에 1~9 수 중에 3개의 랜덤한 숫자를 넣음
+      numbers = Math.floor(Math.random() * 9 + 1);
+      if(random.indexOf(numbers) === -1){  //중복되는 숫자 없는지 확인
+        random.push(numbers);
+      }
+      else{  //중복되는 숫자가 있을 경우 다시 반복문 돌아야 함
+        i--;
+      }
+    }
+
+    //사용자에게 숫자 3개를 받음
+    const a = 0;
+    const b = 0;
+    const c = 0;
+    const input = [a, b, c];
+    prompt("숫자 야구 게임을 시작합니다.");
+    input = prompt("숫자를 입력해주세요 : ").split("");
+
+    // if(){  //같은 수가 같은 자리에 - 스트라이크
+
+    // } 
+    // else if(){  //같은 수가 다른 자리에 - 볼
+
+    // }
+    // else if(){  //같은 수가 없을 경우 - 낫싱
+
+    // }
+
+    //잘못된 값을 입력한 경우 throw문을 사용하여 예외 발생 -> 종료
+  }
 }
 
 module.exports = App;
