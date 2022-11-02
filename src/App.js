b20cd70ad96683b223d5ec51a214cbd854e2a472
nangkyeonglim <nangkyeonglim@gmail.com>
2022-11-03 00:13:47 +0900

    feat: 입력한 수에 대한 결과를 나타내는 기능 구현

--- src/App.js
+++ src/App.js
@@ -36,6 +36,12 @@ class App {
     });
     return result;
   }
+  
+  printResult(strike, ball) {
+    if (strike === 0 && ball === 0) MissionUtils.Console.print('낫싱');
+    if (ball > 0) MissionUtils.Console.print(ball + '볼');
+    if (strike > 0) MissionUtils.Console.print(strike + '스트라이크');
+  }
 }
 
 module.exports = App;
