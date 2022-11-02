43775d4ab6af869081add436d7208ca05b53c583
박준서 <oponize@naver.com>
2022-11-02 18:08:23 +0900

    feat: 숫자야구게임에 사용될 상수 생성

---
+++ src/constants/index.js
@@ -0,0 +1,14 @@
+const constants = {
+  CASE: {
+    BALL: "볼",
+    STRIKE: "스트라이크",
+    NOTING: "낫싱",
+  },
+  EXCEPTION: {
+    MIN_NUMBER: 1,
+    MAX_NUMBER: 9,
+    VALID_NUMBER_LENGTH: 3,
+  },
+};
+
+module.exports = constants;
