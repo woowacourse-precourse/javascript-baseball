39fc694db0f87563773cdc3dd12ea76b06fec638
Chris.c <cjs120dd@gmail.com>
2022-11-02 16:34:59 +0900

    feat:게임 시작 문구를 출력한다

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+
+const START_MESSAGE = `숫자 야구 게임을 시작합니다.`;
+
 class App {
-  play() {}
+	play() {
+		this.print(START_MESSAGE);
+	}
+
+	print(message) {
+		MissionUtils.Console.print(message);
+	}
 }
 
 module.exports = App;
---
+++ src/App.test.js
@@ -0,0 +1,24 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+const App = require('./App');
+
+const getLogSpy = () => {
+	const logSpy = jest.spyOn(MissionUtils.Console, 'print');
+	logSpy.mockClear();
+	return logSpy;
+};
+
+describe('숫자 야구 게임', () => {
+	test('print 메소드로 받은값을 출력', () => {
+		const logSpy = getLogSpy();
+		const app = new App();
+		const input = '테스트용 메세지를 출력합니다';
+		app.print(input);
+
+		expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(input));
+	});
+
+	test('진행상황 확인용 테스트', () => {
+		const app = new App();
+		app.play();
+	});
+});
