e9db3d586a77806dda8e7f79b3cab978660273f0
박정규 <srk99279@naver.com>
2022-11-02 17:16:36 +0900

    feat : 랜덤 숫자 생성 함수 구현.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+const makeRandomNumber = () => {
+  const targetRandomNumber = [];
+  for (let randomNumberIdx = 0; randomNumberIdx < 3; randomNumberIdx++) {
+    targetRandomNumber.push(Math.floor(Math.random() * 9 + 1));
+  }
+  return targetRandomNumber;
+};
+
 class App {
-  play() {}
+  play() {
+    let target = [];
+    target = makeRandomNumber();
+
+    console.log(target);
+  }
 }
+
+/** test용 코드 */
+const app = new App();
+app.play();
 
 module.exports = App;
