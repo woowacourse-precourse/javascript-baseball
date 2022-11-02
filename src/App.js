9f32a3fcb97625643e23e0e299fbd96a61089c88
An Sol <apdlvmf486@gmail.com>
2022-11-02 22:11:08 +0900

    refactor: 코드 컨벤션에 맞추어 정답 생성 함수 리펙토링

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ class App {
   play() {
     const MissionUtils = require("@woowacourse/mission-utils");
 
-    function makeAnswer() {
+    const MAKEANSWER = function makeAnswerWithThreeUniqueNumbers() {
       const THREE_NUMBERS = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
       const ANSWER = THREE_NUMBERS[0] * 100 + THREE_NUMBERS[1] * 10 + THREE_NUMBERS[2] * 1;
       return ANSWER;
