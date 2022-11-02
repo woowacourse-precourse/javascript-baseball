14554ead481175be73c84cfd2e3677873f7ab356
Suyoung <jsy8492@gmail.com>
2022-11-02 17:45:50 +0900

    feat: 사용자의 잘못된 입력값에 대한 예외처리
    - 잘못된 값을 입력한 경우 throw문을 사용해 예외를 발생시키고, 게임을 종료

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 const { Console, Random } = require('@woowacourse/mission-utils');
+const VALID_INPUT_REGEX = /^[1-9]{3}$/;
 
 class App {
   play() {
     Console.print('숫자 야구 게임을 시작합니다.');
+    try {
+      this.inputAnswer();
+    } catch (err) {
+      this.exit();
+      throw err;
+    }
+  }
+
+  inputAnswer() {
     Console.readLine('숫자를 입력해주세요 : ', answer => {
-      console.log(answer);
+      if (this.isInvalidInput(answer)) {
+        throw new Error('잘못된 입력입니다. 게임 종료');
+      }
+
+      this.inputAnswer();
     });
+  }
+
+  exit() {
+    Console.close();
+  }
+
+  isInvalidInput(inputValue) {
+    return !VALID_INPUT_REGEX.test(inputValue);
   }
 }
 
