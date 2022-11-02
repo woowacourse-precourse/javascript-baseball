9fb4cdba64538af8986c367590e5330748ece5a4
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2022-11-02 23:45:54 +0900

    feat: 게임 시작을 안내하는 문장 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  constructor(input) {
+    this.input = input;
+  }
+  play() {
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+  }
 }
+
+let input = "hi";
+const app = new App();
+app.play();
 
 module.exports = App;
