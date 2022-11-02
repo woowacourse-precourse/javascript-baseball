101c5efd2db946da6b6f5e2b9c1136191f1ba3a2
Sumin Kim <ksumin0730@hufs.ac.kr>
2022-11-02 21:45:30 +0900

    Feat: 입력값을 전달받음

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const GameControl = require('./Controller.js')
+const constant = require('./Constants');
+const randomNum = require('./model/Random');
+
 class App {
-  play() {}
+  constructor(){
+    this.gamecount = 0;
+  }
+  
+  async play() {
+    const answer = randomNum();
+    this.gamecount += 1;
+    if (this.gamecount === 1){
+      MissionUtils.Console.print(constant.GAME.START);
+    }
+    
+    const game = new GameControl(answer);
+    while(1){
+      const input = await game.userInput();
+      //console.log(game.userOutput(input));
+    }
+    
+  }
+
 }
 
 module.exports = App;
+
+const app = new App();
+app.play();
