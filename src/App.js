311b1764b93228dace9b555072beffa8d810e72a
An Sol <apdlvmf486@gmail.com>
2022-11-02 16:16:15 +0900

    feat: mission-utils 모듈 설치

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 class App {
-  play() {}
+  play() {
+    const MissionUtils = require("@woowacourse/mission-utils");
+    console.log(MissionUtils.Random.pickNumberInList([1, 2, 3]));
+  }
 }
-
+const app = new App();
+app.play();
 module.exports = App;
