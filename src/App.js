f4b1c5c8c5b8b9a50e5646b453484351dc16397e
jangsumin <jsm0726@naver.com>
2022-11-02 17:50:14 +0900

    feat: display game start comment with mission-utils

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
   play() {
-    console.log("숫자 야구 게임을 시작합니다.");
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
   }
 }
 
