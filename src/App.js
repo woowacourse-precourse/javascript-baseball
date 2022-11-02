1eaec41b1221eb35222c9826de928edb5e56d4bb
Rki0 <pky00823@gmail.com>
2022-11-02 16:01:21 +0900

    feat: 게임 시작 메세지 출력 함수
    숫자 야구 게임이 시작될 경우 메세지를 출력하는 함수를 구현하였습니다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  play() {
+    this.showStartMessage();
+  }
+
+  showStartMessage() {
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    MissionUtils.Console.close();
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
