8d68528654dd5d49729f1f649a27bf6c5c6215a9
jisu-kim <kss2460@naver.com>
2022-11-02 20:16:41 +0900

    docs: 명세 기능 추가 중

---
+++ src/test.js
@@ -0,0 +1,10 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
+const computer = [];
+while (computer.length < 3) {
+  const number = MissionUtils.Random.pickNumberInRange(1, 9);
+  if (!computer.includes(number)) {
+    computer.push(number);
+  }
+}
+console.log(computer);
