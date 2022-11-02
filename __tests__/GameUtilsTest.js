79c68a3329229b817f4aaa1185f93d4944724e51
Suyoung <jsy8492@gmail.com>
2022-11-02 19:08:17 +0900

    test: 게임 기능 테스트 코드 수정

--- __tests__/GameUtilsTest.js
+++ __tests__/GameUtilsTest.js
@@ -4,15 +4,13 @@ const { Random } = require('@woowacourse/mission-utils');
 const app = new App();
 
 describe('게임 기능 테스트', () => {
-  describe('컴퓨터가 생각중인 숫자를 무작위로 생성', () => {
-    test('서로 다른 임의의 수 3개로 이루어진 배열 생성', () => {
-      const result = app.generateRandomNumbers();
+  test('컴퓨터가 생각중인 숫자를 무작위로 생성', () => {
+    const result = app.generateRandomNumbers();
 
-      expect(result).toHaveLength(3);
-    });
+    expect(result).toHaveLength(3);
   });
 
-  describe('스트라이크, 볼의 개수 구하기', () => {
+  test('스트라이크, 볼의 개수 구하기', () => {
     const inputValues = ['123', '456', '312', '132'];
     const computerNumbers = [1, 2, 3];
 
