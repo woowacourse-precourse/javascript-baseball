bb0fe5627730fe6e88d0d027fc94a9f2f4cc2396
Suyoung <jsy8492@gmail.com>
2022-11-02 16:24:29 +0900

    feat: 사용자로부터 입력받는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+const { Console, Random } = require('@woowacourse/mission-utils');
+
 class App {
-  play() {}
+  play() {
+    Console.print('숫자 야구 게임을 시작합니다.');
+    Console.readLine('숫자를 입력해주세요 : ', answer => {
+      console.log(answer);
+    });
+  }
 }
+
+new App().play();
 
 module.exports = App;
