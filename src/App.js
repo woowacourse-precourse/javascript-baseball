5e16ea9241caf35965cf578ddf303831e187b25f
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2022-11-02 20:58:04 +0900

    feat: 랜덤한, 중복되지않은 3자리 숫자를 생성하는 함수 구현

--- src/App.js
+++ src/App.js
@@ -2,4 +2,18 @@ class App {
   play() {}
 }
 
+const makeRandomNumber = () => {
+  const RANDOM_NUMBER = [];
+
+  while (RANDOM_NUMBER.length < 3) {
+    const NUMBER = MissionUtils.Random.pickNumberInRange(1, 9);
+
+    if (!RANDOM_NUMBER.includes(NUMBER)) {
+      RANDOM_NUMBER.push(NUMBER);
+    }
+  }
+
+  return RANDOM_NUMBER;
+};
+
 module.exports = App;
