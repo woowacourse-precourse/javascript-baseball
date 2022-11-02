19bdfe999d606287e9e9ab852bf5c82bef1aeb2b
kyw0716 <kyw0716@naver.com>
2022-11-03 01:19:01 +0900

    feat: 숫자 입력 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const Computer = require("./Computer");
+
 class App {
-  play() {}
+  inputNumber;
+  computer = new Computer();
+
+  play() {
+    MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (input) => {
+      this.inputNumber = input;
+      console.log(this.inputNumber);
+    });
+  }
 }
 
 module.exports = App;
+
+const app = new App();
+app.play();
