f5f8761320f80fdb09634057fc96bd7d7b345b92
kangju2000 <kangju2000@naver.com>
2022-11-02 20:19:38 +0900

    feat: setRandomComputerValue 메서드 구현 - 무작위 수를 받아와 컴퓨터의 수를 결정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import { Console, Random } from '@woowacourse/mission-utils';
+
 class App {
   #isPlaying = false;
   #computerValue = null;
   #userValue = null;
+
+  setRandomComputerValue() {
+    let randomValue = Random.pickUniqueNumbersInRange(1, 9, 3).join();
+    this.#computerValue = randomValue;
+  }
 
   isAvailableValue(value) {
     let stringValue = value + '';
