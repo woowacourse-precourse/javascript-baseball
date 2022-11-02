e7fe230939911065130421cdb772f641d29a8118
jangsumin <jsm0726@naver.com>
2022-11-02 20:37:30 +0900

    feat: restart the game

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ const MissionUtils = require("@woowacourse/mission-utils");
 class App {
   play() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
-    
     let computerNumber = createComputerNumber();
-    
     readInputNumber(computerNumber);
   }
 }
@@ -47,6 +45,15 @@ function readInputNumber(computerNumber) {
   MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (myNumber) => {
     console.log(compare(myNumber, computerNumber));
     if (compare(myNumber, computerNumber) !== '3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료') readInputNumber(computerNumber);
+    else restartGame();
+  });
+}
+
+function restartGame() {
+  let computerNumber = createComputerNumber();
+  MissionUtils.Console.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n', (input) => {
+    if (input === "1") readInputNumber(computerNumber);
+    if (input === "2") MissionUtils.Console.close();
   });
 }
 
