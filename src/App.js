022b96577091b76074a68a1894ca7470bce30d8b
An Sol <apdlvmf486@gmail.com>
2022-11-02 23:32:20 +0900

    feat: 사용자의 입력 값을 체크하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 class App {
   play() {
     const MissionUtils = require("@woowacourse/mission-utils");
-
+    
     const MAKEANSWER = function makeAnswerWithThreeUniqueNumbers() {
       const THREE_NUMBERS = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
       const ANSWER = THREE_NUMBERS[0] * 100 + THREE_NUMBERS[1] * 10 + THREE_NUMBERS[2] * 1;
       return ANSWER;
+    }
+
+    const CHECK_INPUT_DURING_GAME = function checkInputDuringGame(userInput) {
+      if (userInput.length !== 3) return false;
+      if (isNaN(userInput)) return false;
+      if (userInput[0] === userInput[1] || userInput[1] === userInput[2]) return false;
+      return true;
+    }
+
+    const CHECK_INPUT_AFTER_GAME = function checkInputAfterGame(userInput) {
+      if (+userInput === 1 || +userInput === 2) return false;
+      return true;
     }
 
   }
