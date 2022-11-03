dc83fa034da25b5981eb3bf798499c835d8ddb6b
jaewoogwak <jaewoo010207@gmail.com>
2022-11-04 02:31:23 +0900

    feat: 플레이어의 수 입력 받는 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
+  constructor() {
+    this.exit = false;
+    this.code = null;
+    this.user = "";
+    this.computer = "";
+  }
+  play() {
+    for (let i = 0; i < 7; i++) {
+      this.start();
+      this.input();
+    }
+  }
+
   start() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+  }
+
+  input() {
+    MissionUtils.Console.readLine("숫자를 입력해주세요.", (answer) => {
+      this.user = answer.split("").map((num) => parseInt(num));
+      console.log("user", this.user);
+    });
   }
 }
 
