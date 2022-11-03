e65f858ac6c8623fa6ee3a41e03820337c4b20c8
박정규 <srk99279@naver.com>
2022-11-03 12:31:26 +0900

    feat : 볼카운트 출력 함수 구현 완료.

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const makeRandomNumber = () => {
 
 const inputNumber = (targetNumber) => {
   let isCorrect = false;
-  console.log(targetNumber);
+  console.log(targetNumber); // 테스트 중 보이게 하기 위함.
 
   MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (answer) => {
     let guessNumber;
@@ -21,7 +21,10 @@ const inputNumber = (targetNumber) => {
     if (answer.length === 3) {
       guessNumber = answer.split("").map((v) => +v);
       let [ball, strike] = checkBallCount(targetNumber, guessNumber);
-      if (strike < 3) inputNumber(targetNumber);
+      if (strike < 3) {
+        printBallCount(ball, strike);
+        inputNumber(targetNumber);
+      }
       if (strike == 3) {
         MissionUtils.Console.print(
           "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
@@ -37,6 +40,7 @@ const inputNumber = (targetNumber) => {
       app.play();
     }
     if (answer == 2) {
+      MissionUtils.Console.print("게임을 종료합니다.");
       MissionUtils.Console.close();
     }
   });
@@ -65,6 +69,17 @@ const checkBallCount = (targetNumber, guessNumber) => {
   return [ball, strike];
 };
 
+const printBallCount = (ball, strike) => {
+  if (ball === 0 && strike > 0)
+    MissionUtils.Console.print(`${strike}스트라이크`);
+  if (strike === 0 && ball > 0) MissionUtils.Console.print(`${ball}볼`);
+  if (ball === 0 && strike === 0) MissionUtils.Console.print("낫싱");
+};
+
+const resetGame = () => {};
+
+const exitGame = () => {};
+
 class App {
   play() {
     let target = makeRandomNumber();
@@ -81,4 +96,4 @@ class App {
 const app = new App();
 app.play();
 
-module.exports = App;
+// module.exports = App;
