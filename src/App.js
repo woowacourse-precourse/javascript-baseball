95831863d47cfd9f5d773250b0ab075c9bc8e883
AhnGeunTaek <dksrmsxor@gmail.com>
2022-11-03 22:10:27 +0900

    Feat: 게임 시작 시 게임 시작 문구 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+
 class App {
-  play() {}
+  constructor(computerInputNumber, UserInputNumber) {
+    this.play = () => {
+      return MissionUtils.Console.print('숫자 야구 게임을 시작합니다');
+    };
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
