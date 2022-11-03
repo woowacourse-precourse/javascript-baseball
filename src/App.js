1c5bb312db2d3b89462791cea6827c6446e29530
jsh3418 <albert5428@gmail.com>
2022-11-03 22:54:26 +0900

    refactor: 사용자에게 답변 요청하는 코드 requestAnswer 메서드로 분리

--- src/App.js
+++ src/App.js
@@ -19,13 +19,16 @@ class App {
     Console.print(message);
   }
 
+  requestAnswer(message) {
+    Console.readLine(message, (answer) => {
+      this.userAnswer = answer;
+    });
+  }
+
   play() {
     this.sendMessage("숫자 야구 게임을 시작합니다.");
     this.initAnswer();
-
-    Console.readLine("숫자를 입력해주세요 : ", (answer) => {
-      this.userAnswer = answer;
-    });
+    this.requestAnswer("숫자를 입력해주세요. : ");
   }
 }
 
