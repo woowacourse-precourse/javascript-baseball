0da986650453b1a39c3d182a692fa932128d2aa2
Suyeon <msjgr2801@gmail.com>
2022-11-03 14:00:13 +0900

    Feat: [Function1] make random numbers of computer
    - 컴퓨터 랜덤 값 생성 함수 구현

--- src/App.js
+++ src/App.js
@@ -41,7 +41,18 @@ class App {
     );
   }
 
-  makeRandomNumber() {}
+  makeRandomNumber() {
+    const computer = [];
+
+    while(computer.length !== 3) {
+      const number = MissionUtils.Random. pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+
+    this.computer = [...computer];
+  }
 
   getUserValue() {}
 
