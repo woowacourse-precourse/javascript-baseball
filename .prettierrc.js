1858f789a9d02c1b2776da3ca181e30590ab498d
jlee0505 <hj960322@gmail.com>
2022-11-03 14:54:03 +0900

    chore : eslint, prettier 설정

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+	trailingComma: 'es5', // 배열, 오브젝트 프로퍼티 등에서 마지막 항목에도 콤마를 붙일지를 설정
+	tabWidth: 4, // tab 1 = space 4
+	useTabs: true, // tab과 space 중 무엇을 사용할 것인가
+	semi: true, // semicolon(;) 자동완성
+	singleQuote: true, // '' 와 "" => '' 로 통일
+	arrowParens: 'avoid', // 화살표함수 인자가 하나일 때 parenthesis() 생략
+};
