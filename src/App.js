7056513f3fc3231fa6c2b268d5cf167f89971146
Dahye <06robin11@gmail.com>
2022-11-03 16:33:07 +0900

    feat:create random three numbers without repeat

--- src/App.js
+++ src/App.js
@@ -1,4 +1,23 @@
 class App {
+  constructor() {
+    this.computerNumber = createComputerNumber();
+  }
+
+  static createComputerNumber = () => {
+    const computerNumber = [];
+    while (computerNumber.length < 3) {
+      const randomNumber = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (isEveryNumberUnique(computerNumber)) {
+        computerNumber.push(randomNumber);
+      }
+    }
+    return computerNumber;
+  };
+
+  static isEveryNumberUnique = (nums) => {
+    return nums.length === new Set(nums).size;
+  };
+
   play() {}
 }
 
