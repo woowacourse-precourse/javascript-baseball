32514311ce4462218619acf0b9545f03e0f00128
0dytpq0 <rkfnahs12e@gmail.com>
2022-11-03 21:41:01 +0900

    <feat> : <strike,ball 값 구하기>
    strike값을 구하는 함수를 구현.
    ball값을 구하는 함수를 구현.
    
    새로운 배열을 만들어 strike값의 조건에 해당하지 않는 사용자 값은 새 배열에 넣고 해당한다면 strike 값을 추가한다.
    새 배열이 사용자 값에 들어있는 가를 판단하고 있다면 그것이 ball 값에 추가가 된다.

--- src/App.js
+++ src/App.js
@@ -3,28 +3,55 @@ const MissionUtils = require("@woowacourse/mission-utils");
 // 1. 컴퓨터의 수 3자리 구하기.
 const machineNum = [];
 while (machineNum.length < 3) {
-    const number = MissionUtils.Random.pickNumberInRange(1, 9);
-    if (!machineNum.includes(number)) {
-        machineNum.push(number);
+  const number = MissionUtils.Random.pickNumberInRange(1, 9);
+  if (!machineNum.includes(number)) {
+    machineNum.push(number);
+  }
+}
+//3, 입력한 숫자에 대한 스트라이크,볼 결과 구하기.
+let checkBallArray = [];
+
+function checkStrike(arr) {
+  let strike = 0;
+  for (let j = 0; j < arr.length; j++) {
+    if (arr[j] === inputNum[j]) {
+      strike += 1;
+    } else {
+      checkBallArray.push(inputNum[j]);
     }
+  }
+  return strike
+}
+
+function checkball(arr) {
+  let ball = 0;
+  for (let i = 0; i < checkBallArray.length; i++) {
+    if (arr.includes(checkBallArray[i])) {
+      ball += 1
+    }
+  }
+  return ball
 }
 
 // 2. 게임 플레이어는 컴퓨터가 생각하고 있는 서로 다른 3개의 숫자를 입력
 const inputNum = [];
 
 MissionUtils.Console.readLine('서로 다른 숫자를 3개 입력하시오.', (answer) => {
-    const num = answer;
-    for (let i = 0; inputNum.length < 3; i++) {
-        inputNum.push(parseInt(num[i]));
-    }
-    MissionUtils.Console.close();
+  const num = answer;
+  for (let i = 0; inputNum.length < 3; i++) {
+    inputNum.push(parseInt(num[i]));
+  }
+  console.log(checkStrike(machineNum));
+  console.log(checkball(machineNum));
+  console.log(checkBallArray);
+  MissionUtils.Console.close();
 });
 
 
 
 
 class App {
-    play() { }
+  play() { }
 
 
 
---
+++ src/tempCodeRunnerFile.js
@@ -0,0 +1,1 @@
+checkStrike(machineNum)
