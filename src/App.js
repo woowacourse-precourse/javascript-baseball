9c14066012d7db4729b05ef9ab08602577ab2d27
moonelysian <dandyblack@naver.com>
2022-11-03 09:56:01 +0900

    feat: 랜덤 숫자 세자리수 생성
    1-9사이에 랜덤한 숫자 3개 고른 후 랜덤하게 섞어서 배열로 결과 반환

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+const { Random, Console } = require("@woowacourse/mission-utils");
 class App {
-  play() {}
+  generateRandomNums() {
+    const randoms = Random.pickUniqueNumbersInRange(1, 9, 3);
+    return Random.shuffle(randoms);
+  }
+  
+  play() {
+  }
 }
 
 module.exports = App;
