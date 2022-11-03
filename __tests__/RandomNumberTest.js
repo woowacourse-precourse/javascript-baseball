90df242f2957dbb62587fad0d748d937b2dd3887
ReturnReason <reasonz@daum.net>
2022-11-04 07:10:24 +0900

    test: 컴퓨터의 숫자가 1~9까지의 숫자로만 이루어져 있는지 테스트 통과 확인

--- __tests__/RandomNumberTest.js
+++ __tests__/RandomNumberTest.js
@@ -1,11 +1,20 @@
 const App = require('../src/App');
 
 describe('기능 구현 테스트 : computer', () => {
+  const app = new App();
+  const numbers = app.makeRandomNumbers();
+
   test('✨ 서로 다른 3자리 숫자로 이루어져 있습니다.', () => {
-    const app = new App();
-    const numbers = app.makeRandomNumbers();
     const numbersLength = [...new Set(numbers)].length;
 
     expect(numbersLength).toBe(3);
+  });
+
+  test('✨ 1~9까지의 숫자로만 이루어져 있습니다.', () => {
+    const reg = /[1-9]{1}/;
+    const fromOneToNine = (number) => reg.test(number);
+    const isOneToNine = numbers.map(String).filter(fromOneToNine).length === 3;
+
+    expect(isOneToNine).toBeTruthy();
   });
 });
