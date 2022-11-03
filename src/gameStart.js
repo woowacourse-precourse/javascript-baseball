42b3e73c2977f1e15fd5c1806a9bc87d06ea3492
2jaebbang <72256238+2jaebbang@users.noreply.github.com>
2022-11-03 19:43:30 +0900

    feat: 중복되지 않는 랜덤의 3자리 수 생성 기능 추가

--- src/gameStart.js
+++ src/gameStart.js
@@ -1,7 +1,13 @@
-const MissionUtils = require('@woowacourse/mission-utils');
+const { Console, Random } = require('@woowacourse/mission-utils');
+
+function generateRandomNumbers() {
+  const generatedRandomNumbers = Random.pickUniqueNumbersInRange(1, 9, 3);
+  return generatedRandomNumbers;
+}
 
 function gameStart() {
-  MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+  Console.print('숫자 야구 게임을 시작합니다.');
+  const computerRandomNumbers = generateRandomNumbers();
 }
 
 exports.gameStart = gameStart;
