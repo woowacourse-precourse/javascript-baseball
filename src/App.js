92a7ee10715633d9c39d54974db14327b552909e
김혜진 <khj000217@naver.com>
2022-11-04 03:24:25 +0900

    feat:install api

--- src/App.js
+++ src/App.js
@@ -1,3 +1,7 @@
+// const MissionUtils = require('@woowacourse/mission-utils');
+// console.log(MissionUtils.Random.pickNumberInList([1, 2, 3]));
+// console.log(MissionUtils.Random.pickNumberInList([1, 2, 3]));
+
 class App {
   play() {}
 }
