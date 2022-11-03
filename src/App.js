d18b168acc02111d92ce14770df07aaa20080166
sangpyokim <rlatkdvy12@naver.com>
2022-11-03 16:33:11 +0900

    feat: write outputString
    feat: outputString
    
    write output string feature

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 class App {
+  userInput = ''
+
   play() {}
+
+  outputString(str) {
+    MissionUtils.Console.print(str)
+  }
+  // 사용자에게 보여줄 str과 입력을 받고 저장함.
+  setUserInput(showStr) {
+    return MissionUtils.Console.readLine(showStr, (answer) => {
+      this.outputString(`${showStr} ${answer}`)
+      return this.userInput = answer
+    })
+  }
 }
 
 module.exports = App;
