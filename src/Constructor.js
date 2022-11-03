097519378bc39db5a238d2eadb94cdf207ce8b0b
moonelysian <dandyblack@naver.com>
2022-11-03 10:08:29 +0900

    fix: export 문법 오류 수정
    export사용 시 오류나서 module.exports로 수정

--- src/Constructor.js
+++ src/Constructor.js
@@ -1,10 +1,10 @@
-export const RESULT = {
+const RESULT = {
   NOTHING: '낫싱',
   BALL: '볼',
   STRIKE: '스트라이크',
 }
 
-export const MESSAGES = {
+const MESSAGES = {
   START: '숫자 야구 게임을 시작합니다.',
   INPUT_NUMBER: '숫자를 입력해주세요 : ',
   SUCCESS: '3개의 숫자를 모두 맞히셨습니다! 게임 종료',
@@ -12,10 +12,16 @@ export const MESSAGES = {
   END: '게임 종료',
 }
 
-export const NUMBERS = {
+const NUMBERS = {
   RANDOM_MIN: 1,
   RANDOM_MAX: 9,
   REQUIRED_LENGHT: 3,
   END_GAME: 1,
   RESTART_GAME: 2,
+}
+
+module.exports = {
+  RESULT,
+  MESSAGES,
+  NUMBERS,
 }
