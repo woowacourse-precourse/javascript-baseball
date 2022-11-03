d712215e5c1ddd881b8eca551779b6e27096b50c
leeuihyun <meta212121@naver.com>
2022-11-04 00:47:24 +0900

    feat : 컴퓨터의 중복 없는 3개의 난수 반환 메서드

--- src/Computer.js
+++ src/Computer.js
@@ -7,6 +7,13 @@ class Computer extends Mission {
   getRandomNumber() {
     return this.mission.Random.pickNumberInRange(1, 9);
   }
+  getComputerNumbers() {
+    const computerNumbers = new Set();
+    while (computerNumbers.size < 3) {
+      computerNumbers.add(this.getRandomNumber());
+    }
+    return Array.from(computerNumbers);
+  }
 }
 
 module.exports = Computer;
