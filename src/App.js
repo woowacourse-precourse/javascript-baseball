747082960a8d387a3ea5a823da5ef96297ae07d2
kinoasm <kinoasm@gmail.com>
2022-11-03 20:16:38 +0900

    Feat: 시작 안내 메시지 생성
    MissionUtils 모듈 가져오기
    시작 시 메시지 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+    play() {
+        this.start();
+    }
+    start() {
+        MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    }
 }
 
 module.exports = App;
