f48f010e6ffedc0efcb2be7a06d7a3142374d07f
KES <aprilsmaple@gmail.com>
2022-11-03 18:53:03 +0900

    feat: created function for making answer number

--- src/App.js
+++ src/App.js
@@ -6,21 +6,42 @@ class App {
       input: process.stdin,
       output: process.stdout,
     });
+    this.answer = {};
   }
+
   play() {
     console.log("숫자 야구 게임을 시작합니다.");
+    this.selectAnswerNumber();
     this.getUserGuess();
+  }
+
+  selectAnswerNumber() {
+    const oneToNine = new Array(9).fill().map((_, idx) => idx + 1);
+    this.answer = this.shuffle(oneToNine)
+      .slice(0, 3)
+      .reduce((acc, cur, idx) => {
+        acc[cur] = idx;
+        return acc;
+      }, {});
+  }
+
+  shuffle(array) {
+    return array.sort(() => Math.random() - 0.5).slice(0, 3);
   }
 
   getUserGuess() {
     this.rl.on("line", (guess) => {
       try {
-        checkVaildation(guess);
+        this.checkGuessVaildation(guess);
       } catch (err) {
         console.log(err.message);
       }
     });
     return;
+  }
+
+  checkGuessVaildation(guess) {
+    if (guess.length !== 3) throw new Error("");
   }
 }
 
