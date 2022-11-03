2c4c79ce0fcd4aec07a8268c057b26fe7b3888d3
ramda <91455804+gnfnfnr@users.noreply.github.com>
2022-11-03 17:31:21 +0900

    feat: enter a value for the user

--- src/App.js
+++ src/App.js
@@ -5,8 +5,15 @@ class App {
     this.randomAnswer = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
   }
 
+  userInput() {
+    MissionUtils.Console.readLine("숫자를 입력해주세요.", (inputNum) => {
+      console.log(`숫자를 입력해주세요. ${inputNum}`);
+    });
+  }
+
   play() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    this.userInput();
   }
 }
 
