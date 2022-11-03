4035a1f5e137b083790c3d912f7c505070fda095
ukkodeveloper <ukkodeveloper@gmail.com>
2022-11-03 13:10:01 +0900

    feat: 숫자야구게임 시작 메세지 출력

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import Computer from './Computer';
 import Interaction from './Interaction';
+import { Console, Random } from '@woowacourse/mission-utils';
 
 class App {
   play() {
+    Interaction.printPlayMessage();
+    const computer = new Computer();
+    const interaction = new Interaction();
+  }
+
+  start() {
     const computer = new Computer();
     const interaction = new Interaction();
   }
 }
+const app = new App();
+app.play();
 
 module.exports = App;
