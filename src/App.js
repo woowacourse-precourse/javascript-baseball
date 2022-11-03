ea51bb2c272db5b8505370db1998180ba163ac96
BB-choi <choibobae0809@gmail.com>
2022-11-03 12:48:11 +0900

    feat: 게임 시작 문구 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const { Console } = MissionUtils;
+
 class App {
-  play() {}
+  MESSAGES = {
+    START: "숫자 야구 게임을 시작합니다.",
+    END: "게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
+    INSERT_NUMBER: "숫자를 입력해주세요",
+    CORRECT(count) {
+      return `${count}개의 숫자를 모두 맞히셨습니다!`;
+    },
+    BALL(count) {
+      return `${count}볼`;
+    },
+    STRIKE(count) {
+      return `${count}스트라이크`;
+    },
+  };
+
+  printMessage(msg) {
+    return Console.print(msg);
+  }
+
+  play() {
+    this.printMessage(this.MESSAGES.START);
+  }
 }
 
 module.exports = App;
