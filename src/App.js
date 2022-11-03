7594e83c8b07815476f1d42ba50409a1d9f59c1c
gilpop8663 <wolfye@naver.com>
2022-11-03 21:07:51 +0900

    feat:게임 플레이어에게 숫자를 물어보는 문구를 출력하는 기능 구현
    사용자의 값을 입력받기 위해서는 MissionUtils 라이브러리에서 제공하는 Console.readLine을 활용한다.
    
    플레이어에게 '숫자를 입력해주세요 : '를 출력한다.

--- src/App.js
+++ src/App.js
@@ -4,9 +4,16 @@ function gameStart() {
   MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
 }
 
+function userInput() {
+  MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (number) => {
+    console.log(number);
+  });
+}
+
 class App {
   play() {
     gameStart();
+    userInput();
   }
 }
 
