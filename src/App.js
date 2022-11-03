3b296b1880d799b57aae9b13405243c612c1cffb
sery <yunsel2@gmail.com>
2022-11-03 19:34:56 +0900

    feat: 랜덤한 세 개의 숫자 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+const { MESSAGE } = require('./constants');
+const Game = require('./game');
+
+const game = new Game();
+
 class App {
+  init() {
+    game.start(MESSAGE.START);
+    game.generateRandomNumber(1, 9, 3);
+  }
+
   play() {}
 }
+
+const app = new App();
+app.init();
 
 module.exports = App;
--- src/game.js
+++ src/game.js
@@ -4,6 +4,18 @@ class Game {
   start(startMessage) {
     MissionUtils.Console.print(startMessage);
   }
+
+  generateRandomNumber(min, max, length) {
+    const randomNumber = [];
+    while (randomNumber.length < length) {
+      const number = MissionUtils.Random.pickNumberInRange(min, max);
+      if (!randomNumber.includes(number)) {
+        randomNumber.push(number);
+      }
+    }
+    // console.log(randomNumber);
+    return randomNumber;
+  }
 }
 
 module.exports = Game;
