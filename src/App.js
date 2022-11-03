711ff91b7fedb596de7c6128be98470945dba419
Dohyeon Lee <dlehgus1110@naver.com>
2022-11-03 15:56:09 +0900

    feat: 컴퓨터 랜덤 숫자 생성 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 const message = require("./constants/message.js");
+const condition = require("./constants/condition.js");
 
 class App {
   constructor() {
     MissionUtils.Console.print(message.GAME_START);
+    this.computerInput = this.generateComputerInput();
   }
 
   play() {}
+
+  generateComputerInput() {
+    let randomNum = new Set();
+    while (randomNum.size !== condition.MAX_NUMBER_LENGTH) {
+      randomNum.add(
+        MissionUtils.Random.pickNumberInRange(
+          condition.MIN_NUMBER_RANGE,
+          condition.MAX_NUMBER_RANGE
+        )
+      );
+    }
+
+    return [...randomNum].join("");
+  }
 }
 
 const app = new App();
