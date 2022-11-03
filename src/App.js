a18f22bffb13517cc1b405689e7f7da1bf4634d2
insu12021202 <insu12021202@gmail.com>
2022-11-03 17:33:05 +0900

    feat:implement printResult function

--- src/App.js
+++ src/App.js
@@ -2,10 +2,24 @@ const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
   play() {
-    let answers = init();
-    console.log(checkInput("123"));
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    startGame();
   }
 }
+
+const startGame = () => {
+  let answers = init();
+  let play = true;
+  checkResult(input, answers);
+  while (play) {
+    MissionUtils.Console.readLine("숫자를 입력하세요.", (input) => {
+      if (checkInput(input)) {
+        //인풋과 정답 비교하기
+        if (checkResult(answers, input));
+      }
+    });
+  }
+};
 
 const init = () => {
   const computer = [];
@@ -31,6 +45,37 @@ const checkInput = (input) => {
     }
   }
   return true;
+};
+
+const checkResult = (input, answer) => {
+  console.log(`답은 ${answer} 입력은 ${input}`);
+  let strike = 0;
+  let ball = 0;
+  answer.map((item, index) => {
+    console.log(item);
+    if (String(item) === String(input)[index]) {
+      strike++;
+    } else if (String(input).includes(item)) {
+      ball++;
+    }
+  });
+
+  printResult(ball, strike);
+  if (strike === 3) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const printResult = (ball, strike) => {
+  if (ball === 0) {
+    MissionUtils.Console.print(`${strike}스트라이크`);
+  } else if (strike === 0) {
+    MissionUtils.Console.print(`${ball}볼`);
+  } else {
+    MissionUtils.Console.print(`${ball}볼 ${strike}스트라이크`);
+  }
 };
 
 const app = new App();
