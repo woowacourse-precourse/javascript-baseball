b8c1f1bdb0445c71aa0288d2212ef200a14239b6
AnJaeSeong <dktmzh6@gmail.com>
2022-11-04 02:51:55 +0900

    feat: set interface

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  play() {
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+    MissionUtils.Console.readLine('숫자를 입력해주세요 :', (i) => {
+      MissionUtils.Console.print(i);
+    })
+  }
 }
+
+const app = new App;
+app.play();
 
 module.exports = App;
