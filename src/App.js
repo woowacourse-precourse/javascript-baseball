c8bb261a16526d9e3b3120a7f9f6fa86993f959b
h2002y <h2002y@naver.com>
2022-11-03 21:13:49 +0900

    fix: Delete not necessary function call

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ class App {
   play() {
     const generateComputerNum = require("./GenerateRandomNumber.js");
     const computerNum = generateComputerNum();
-    generateComputerNum(computerNum);
     for(let i = 0; i < 3; i++){
       console.log(computerNum[i]);
     }
