bc5a35549e7f47ebf0e149554fb5371df112c925
jsh3418 <albert5428@gmail.com>
2022-11-03 22:45:08 +0900

    refactor: 컴퓨터가 정답을 정하는 코드 initAnswer 메서드로 분리

--- src/App.js
+++ src/App.js
@@ -6,15 +6,19 @@ class App {
     this.userAnswer;
   }
 
-  play() {
-    Console.print("숫자 야구 게임을 시작합니다.");
-
+  initAnswer() {
     while (this.computerAnswer.length < 3) {
       const number = Random.pickNumberInRange(1, 9);
       if (!this.computerAnswer.includes(number)) {
         this.computerAnswer.push(number);
       }
     }
+  }
+
+  play() {
+    Console.print("숫자 야구 게임을 시작합니다.");
+
+    this.initAnswer();
 
     Console.readLine("숫자를 입력해주세요 : ", (answer) => {
       this.userAnswer = answer;
