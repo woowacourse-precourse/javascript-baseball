0d61d9c9e1ae904d37b1d6a3f7f37f3c8668f5da
Buyoung Chung <sunarol@naver.com>
2022-11-03 17:05:03 +0900

    feat: Add function inputUserAnswer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
-  constructor(computerRandomNumber = null) {
+  constructor(computerRandomNumber = null, userAnswer = null) {
     this.computerRandomNumber = computerRandomNumber;
+    this.userAnswer = userAnswer;
   }
   play() {
     greeting();
+    this.computerRandomNumber = makeRandomNumberArray();
+    this.userAnswer = this.inputUserAnswer();
+  }
+  inputUserAnswer() {
+    MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (answer) => {
+      this.userAnswer = answer;
+    });
   }
 }
 
@@ -16,5 +24,7 @@ function makeRandomNumberArray() {
 function greeting() {
   console.log("숫자 야구 게임을 시작합니다.");
 }
-
+const app = new App();
+app.play();
+console.log(app.userAnswer);
 module.exports = App;
