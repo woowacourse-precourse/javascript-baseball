c2905a96d02f3dd944a5be90ac7191b46f919520
BBIYAC <kiy7833@gmail.com>
2022-11-03 18:07:55 +0900

    test(getThreeRandom): test getThreeRandom function in UnitTest.js

---
+++ __tests__/UnitTest.js
@@ -0,0 +1,28 @@
+const App = require("../src/App");
+
+expect.extend({
+  toBeDistinct(received) {
+    const pass =
+      received && new Set(received).size === received.length;
+      if (pass) {
+        return {
+          message: () => `expected ${received} string is unique`,
+          pass: true,
+        };
+      } else {
+        return {
+          message: () => `expected ${received} string is not to unique`,
+          pass: false,
+      };
+    }
+  },
+});
+
+describe("숫자 야구 게임", () => {
+  test("숫자 랜덤 생성 테스트", () => {
+    const app = new App();
+    
+    expect(app.getThreeRandom()).toMatch(/[1-9]{3}/g); // 1-9 범위의 세 자리 숫자인지
+    expect(app.getThreeRandom()).toBeDistinct();
+  });
+});
