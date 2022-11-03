b13329464e9bfb447b9db8234c1effacb21cca9c
MAGHC <windowsapple95@gmail.com>
2022-11-03 18:36:53 +0900

    feat: 컴퓨터의 수 생성하는 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
   play() {}
+
+  generateComputerNumberArray() {
+    return MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
+  }
 }
 
 module.exports = App;
