24896c0c908d131baf74340552d33eac0ae9ca46
kangyeongmin <kym6050@hanyang.ac.kr>
2022-11-03 14:59:09 +0900

    feat: 입력에 따른 출력값을 반환하는 기능

--- src/getResult.js
+++ src/getResult.js
@@ -1,3 +1,20 @@
+function getResult(solution, answer) {
+  const numberOfStrike = checkStrike(solution, answer);
+  const numberOfBall = checkBall(solution, answer);
+
+  if (numberOfStrike == 0 && numberOfBall != 0) return numberOfBall + "볼";
+  if (numberOfStrike != 0 && numberOfBall == 0) {
+    if (numberOfStrike === 3)
+      return (
+        numberOfStrike + "스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료"
+      );
+    return numberOfStrike + "스트라이크";
+  }
+  if (numberOfStrike != 0 && numberOfBall != 0)
+    return numberOfBall + "볼 " + numberOfStrike + "스트라이크";
+  if (numberOfStrike == 0 && numberOfBall == 0) return "낫싱";
+}
+
 function checkStrike(solution, answer) {
   let numberOfStrike = 0;
 
@@ -21,3 +38,5 @@ function checkBall(solution, answer) {
 
   return numberOfBall;
 }
+
+module.exports = getResult;
