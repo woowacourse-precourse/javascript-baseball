0e2939f85cedbe4ac4d2c710c5bda99415aeb0ec
nuheajiohc <jaehun341012@gmail.com>
2022-11-03 15:28:24 +0900

    feat: 숫자 입력을 받는 기능 추가

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ const MissionUtils = require("@woowacourse/mission-utils");
 class App {
     play() {
         startGuidePrint();
+        numberInput()
     }
 }
 
@@ -11,4 +12,10 @@ app.play();
 
 function startGuidePrint() {
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+}
+
+function numberInput() {
+    MissionUtils.Console.readLine("세자리 숫자를 입력해주세요 : ", (numberInput) => {
+        console.log(numberInput);
+    });
 }
