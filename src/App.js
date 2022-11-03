636b742c5e01c1dfeb8bcac35ea26a17f38dd833
MJ <mj632900@gmail.com>
2022-11-04 01:48:09 +0900

    refactor : 함수 선언식에서 함수 표현식으로 수정

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
-function createAnswer() {
-    return MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
-}
-
-let isFirstStart = true;
-function printStartMassage() {
-    isFirstStart && MissionUtils.Console.print(`숫자 야구 게임을 시작합니다.`);
-}
-
-function getUserInput() {
-    MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (number) => {
-        console.log("결과");
-        getUserInput();
-    });
-}
-
 class App {
     play() {
-        const COMPUTER_NUMBER = createAnswer();
+        const createAnswer = () => {
+            return MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
+        };
+
+        let isFirstStart = true;
+        const printStartMassage = () => {
+            isFirstStart && MissionUtils.Console.print(`숫자 야구 게임을 시작합니다.`);
+        };
+
+        const getUserInput = () => {
+            MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (number) => {
+                if (number === ANSWER) {
+                    console.log("성공");
+                }
+                getUserInput();
+            });
+        };
+
+        const ANSWER = createAnswer().join("");
+
         printStartMassage();
         getUserInput();
     }
