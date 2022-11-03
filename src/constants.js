3e08dc42f6e0e307f6d1cc17f2ba864710b228b5
sery <yunsel2@gmail.com>
2022-11-03 19:20:43 +0900

    fix: constants에서 module.exports 수정

--- src/constants.js
+++ src/constants.js
@@ -1,24 +1,31 @@
-export const MESSAGE = Object.freeze({
+const MESSAGE = Object.freeze({
   START: '숫자 야구 게임을 시작합니다.',
   ENTER_NUMBER: '숫자를 입력해주세요',
   SUCCESS: '3개의 숫자를 모두 맞히셨습니다! 게임 종료',
   RESTART: '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.',
 });
 
-export const RESULT = Object.freeze({
+const RESULT = Object.freeze({
   BALL: '볼',
   STRIKE: '스트라이크',
   NOTHING: '낫싱',
 });
 
-export const RANDOM_NUMBER = Object.freeze({
+const RANDOM_NUMBER = Object.freeze({
   MIN: 1,
   MAX: 9,
   LENGTH: 3,
   RANGE: /^[1-9]*$/,
 });
 
-export const GAME = Object.freeze({
+const GAME = Object.freeze({
   START: 1,
   END: 2,
 });
+
+module.exports = {
+  MESSAGE,
+  RESULT,
+  RANDOM_NUMBER,
+  GAME,
+};
