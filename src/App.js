9d34e4a7b5da0d0ab5fcd3a15c1e0bcb64b07a79
AnJaeSeong <dktmzh6@gmail.com>
2022-11-04 03:07:53 +0900

    feat: step 1

--- src/App.js
+++ src/App.js
@@ -2,10 +2,30 @@ const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
   play() {
+    let input = [];
+    let computer = []; 
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
-    MissionUtils.Console.readLine('숫자를 입력해주세요 :', (i) => {
-      MissionUtils.Console.print(i);
-    })
+
+  }
+
+  setComputerNum() {
+
+  }
+
+  countStrike() {
+
+  }
+
+  countBall() {
+
+  }
+
+  result() {
+
+  }
+
+  resultString() {
+
   }
 }
 
