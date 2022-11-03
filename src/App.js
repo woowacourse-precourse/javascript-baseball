1adf9f882c2cf8fb72b6a5aeeabb6d7e9788c24e
confidential-nt <youyoy689@gmail.com>
2022-11-03 15:12:32 +0900

    feat(요구사항): #2. 컴퓨터의 숫자를 정한다.
    컴퓨터의 숫자를 정함

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
+  computerNumber;
+
+  setComputerNumber() {
+    const computerNumber = [];
+    while (computerNumber.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computerNumber.includes(number)) {
+        computerNumber.push(number);
+      }
+    }
+
+    this.computerNumber = computerNumber;
+  }
+
   play() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    this.setComputerNumber();
   }
 }
 
