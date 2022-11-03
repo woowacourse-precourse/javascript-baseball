df9039adcf7335da88697353c14086d9eb90bf19
wang-yurin <king_1994@naver.com>
2022-11-04 01:37:22 +0900

    Feat: 컴퓨터가 임의의 숫자 3개 선택하는 함수 구현 중

--- src/App.js
+++ src/App.js
@@ -2,8 +2,29 @@ const MissionUtils = require("@woowacourse/mission-utils");
 class App {
   play() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    playerInputNumber();
   }
 }
+
+// 플레이어 숫자 입력
+function playerInputNumbers() {
+  MissionUtils.Console.readLine("숫자를 입력해주세요: ", (answer) => {
+    const playerNumber = answer;
+    MissionUtils.Console.print(`${playerNumber}를 입력했다`);
+  });
+}
+
+// 컴퓨터 숫자 생산
+function computerNumbers() {
+  const computer = [];
+  while (computer.length < 3) {
+    const number = MissionUtils.Random.pickNumberInRange(1, 9);
+    if (!computer.includes(number)) {
+      computer.push(number);
+    }
+  }
+}
+computerNumbers();
 
 const app = new App();
 app.play();
