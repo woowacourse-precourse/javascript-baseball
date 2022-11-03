deacb5012baefeeaa02c37c732e36d7fd7294944
d0422 <rlfehd2013@naver.com>
2022-11-03 21:24:29 +0900

    Style:GameInputValidation 메소드별 공백추가

--- src/GameInputValidation.js
+++ src/GameInputValidation.js
@@ -2,15 +2,18 @@ module.exports = class GameInputValidation {
   constructor(gameInput) {
     this.gameInput = gameInput;
   }
+
   validation() {
     this.checkLength();
     this.checkNumber();
   }
+
   checkLength() {
     if (this.gameInput.length != 3) {
       throw new Error("입력은 3자리만 가능합니다.");
     }
   }
+
   checkNumber() {
     if (this.gameInput != Number(this.gameInput)) {
       throw new Error("입력은 숫자만 가능합니다.");
