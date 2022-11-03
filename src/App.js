384e07d14c2e002c9b57058d9f62f7429a18d42c
c0c0pang <coc1789@naver.com>
2022-11-04 03:42:41 +0900

    feat: 게임 시작 알림과 유저의 숫자를 문자로 변환 후 저장하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -2,21 +2,29 @@ const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
   play() {
-    ComputerNumber()
-  }
-}
+    GameStart();
+    ComputerNumber();
+  };
+};
 
 class User {
   constructor() {
     this.UserNumber;
-  }
+  };
   GetNum() {
     return this.UserNumber;
-  }
-}
-
+  };
+};
 const ComputerNumber = () => {
   const NUMBER = String(MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3));
   return NUMBER.split(",");
-}
+};
+
+const GameStart = () => {
+  const USER = new User();
+  MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+  MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (answer) => {
+    USER.UserNumber = String(answer).split("");
+  });
+};
 module.exports = App;
