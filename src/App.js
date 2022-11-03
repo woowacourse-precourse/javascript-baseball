a1fb2096fc3b2018d43d1222dd3364ea8cf3ad04
Yoo Seung Guk <dbtmd878@naver.com>
2022-11-03 14:42:48 +0900

    feat(randomNumberGenerator):랜덤한 숫자 생성 함수 추가 및 숫자 입력 받는 함수 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const Console = MissionUtils.Console;
+const pickUniqueNumbersInRange = MissionUtils.Random.pickUniqueNumbersInRange;
 class App {
-  play() {}
+  constructor() {
+    this.RANDOM_NUMBER_ARRAY;
+    this.INPUT_USER_NUMBER = 0;
+    this.gameState = true;
+  }
+  randomNumberGenerator() {
+    this.gameState = false;
+    this.RANDOM_NUMBER_ARRAY = pickUniqueNumbersInRange(1, 9, 3);
+    return this;
+  }
+  play() {
+    this.gameState && this.randomNumberGenerator();
+    Console.readLine("숫자를 입력하세요. : ", (inputNumber) => {
+      this.INPUT_USER_NUMBER = inputNumber;
+      Console.close();
+    });
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
