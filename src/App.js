c4c691c06fc1de383ab6b2c574dd76ce360e7a78
River <swimmingr@gmail.com>
2022-11-03 22:08:43 +0900

    random으로 컴퓨터의 숫자 설정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  play() {
+    const DEF_NUM = MissionUtils.Random.pickUniqueNumbersInRange(1,9,3);
+    console.log(DEF_NUM);
+  }
 }
+const app = new App();
+app.play();
 
 module.exports = App;
