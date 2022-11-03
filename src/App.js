8d050b9d1f272fb60886f92c2c8b1cf3fbaac1ea
woong <inwoong100@gmail.com>
2022-11-04 00:23:15 +0900

    feat(askRestartApp): 재시작하는 기능의 App 내 메소드 구현
    재시작시에 시작하는 메소드를 재사용 하기위해 restart의 input을 받은 start 메소드는 restart 기능으로 동작, 재 시작시 computer Number가 재설정
    되어야 하므로 재설정을 위한 메소드 resetComputerNumber 구현

--- src/App.js
+++ src/App.js
@@ -4,12 +4,7 @@ class App {
   constructor() {
     this.answerMap = new Map();
     this.computerNumberArray = [];
-    while (this.computerNumberArray.length < 3) {
-      const number = MissionUtils.Random.pickNumberInRange(1, 9);
-      if (!this.computerNumberArray.includes(number)) {
-        this.computerNumberArray.push(number);
-      }
-    }
+
     this.userNumberArray = [];
   }
 
@@ -21,8 +16,17 @@ class App {
     this.answerMap = map;
   }
 
+  resetComputerNumberArray() {
+    this.computerNumberArray = [];
+    while (this.computerNumberArray.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!this.computerNumberArray.includes(number)) {
+        this.computerNumberArray.push(number);
+      }
+    }
+  }
+
   compareUserAndComputerNumber() {
-    console.log(this.userNumberArray, this.computerNumberArray);
     this.userNumberArray.forEach((userNumber, userNumberArrayIndex) => {
       const index = this.computerNumberArray.indexOf(userNumber);
       if (index === userNumberArrayIndex) {
@@ -39,14 +43,7 @@ class App {
       MissionUtils.Console.print(`${strike} 스트라이크`);
       if (strike === 3) {
         MissionUtils.Console.print('3개의 숫자를 모두 맞히셨습니다! 게임종료');
-        MissionUtils.Console.readLine(
-          '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요',
-          input => {
-            if (input === 1) {
-              return;
-            }
-          },
-        );
+        return this.askRestartApp();
       }
     } else if (strike === 0) {
       MissionUtils.Console.print(`${ball} 볼`);
@@ -54,6 +51,22 @@ class App {
       MissionUtils.Console.print(`${ball} 볼 ${strike} 스트라이크`);
     }
     this.startApp();
+  }
+
+  askRestartApp() {
+    MissionUtils.Console.readLine(
+      '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.',
+      input => {
+        if (input === '1') {
+          console.log(this);
+          this.startApp('restart');
+        } else if (input === '2') {
+          console.log('종료하기');
+        } else {
+          console.log('error');
+        }
+      },
+    );
   }
 
   getUserNumberFromInput() {
@@ -75,7 +88,9 @@ class App {
     });
   }
 
-  async startApp() {
+  async startApp(start) {
+    console.log(start, 'start');
+    if (start === 'restart') this.resetComputerNumberArray();
     this.initAnswerMap();
     try {
       await this.getUserNumberFromInput();
@@ -87,6 +102,7 @@ class App {
 
   play() {
     MissionUtils.Console.print('숫자 야구게임을 시작합니다.');
+    this.resetComputerNumberArray();
     this.startApp();
   }
 }
