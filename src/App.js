a3c40b54cdcf021693addc09ff2e92acbd3fcb69
gilpop8663 <wolfye@naver.com>
2022-11-04 02:16:46 +0900

    feat:게임이 종료된 후 사용자가 1을 입력하면 게임을 다시 시작하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -8,7 +8,10 @@ function gameRestartQuestion() {
   MissionUtils.Console.readLine(
     "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
     (number) => {
-      console.log(number);
+      if (number === "1") {
+        let answer = randomNumberSetting();
+        userInput(answer);
+      }
     }
   );
 }
