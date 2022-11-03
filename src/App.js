c0a4c74d26131ad184d067d2e51ad4b1459b94a4
Chris.c <cjs120dd@gmail.com>
2022-11-03 11:51:57 +0900

    feat:유저의 입력값을 받아온다

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 
 const START_MESSAGE = `숫자 야구 게임을 시작합니다.`;
-const RANDOM_NUM_RANGE = {
+const INPUT_USER_NUM_MESSAGE = '숫자를 입력하세요';
+const ERROR_MESSAGE = '알맞은 숫자를 입력하지않아 프로그램을 종료합니다';
+const USER_INPUT_FEEDBACK_MESSAGE = answer => `숫자를 입력하세요 : ${answer}`;
+const NUMBER_RANGE = {
 	MIN: 1,
 	MAX: 9,
 };
-const RANDOM_NUM_LENGTH = 3;
+const NUMBER_LENGTH = 3;
+const SHOULD_NOT_INCLUDE_NUMBER = '0';
 
 class App {
 	constructor() {
 		this.computerNumber = '';
+		this.userNumber = '';
 	}
 
 	play() {
 		this.printMessage(START_MESSAGE);
 		this.computerNumber = this.getComputerNumber();
+		this.userNumber = this.getUserNumber();
 	}
 
 	printMessage(message) {
@@ -23,12 +29,34 @@ class App {
 
 	getComputerNumber() {
 		const numberSet = new Set();
-		while (numberSet.size !== RANDOM_NUM_LENGTH) {
-			numberSet.add(
-				MissionUtils.Random.pickNumberInRange(RANDOM_NUM_RANGE.MIN, RANDOM_NUM_RANGE.MAX),
-			);
+		while (numberSet.size !== NUMBER_LENGTH) {
+			numberSet.add(MissionUtils.Random.pickNumberInRange(NUMBER_RANGE.MIN, NUMBER_RANGE.MAX));
 		}
 		return [...numberSet].join('');
+	}
+
+	getUserNumber() {
+		let userAnswer;
+		MissionUtils.Console.readLine(INPUT_USER_NUM_MESSAGE, answer => {
+			this.printMessage(USER_INPUT_FEEDBACK_MESSAGE(answer));
+			if (!this.validateUserNumber(answer)) {
+				throw new Error(ERROR_MESSAGE);
+			}
+			userAnswer = answer;
+		});
+		return userAnswer;
+	}
+
+	validateUserNumber(answer) {
+		const userNumberSet = new Set();
+		[...answer].forEach(string => userNumberSet.add(string));
+		if (userNumberSet.size !== NUMBER_LENGTH) {
+			return false;
+		}
+		if (userNumberSet.has(SHOULD_NOT_INCLUDE_NUMBER)) {
+			return false;
+		}
+		return true;
 	}
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -7,6 +7,17 @@ const getLogSpy = () => {
 	return logSpy;
 };
 
+const mockQuestions = answers => {
+	MissionUtils.Console.readLine = jest.fn();
+	answers.reduce(
+		(acc, input) =>
+			acc.mockImplementationOnce((question, callback) => {
+				callback(input);
+			}),
+		MissionUtils.Console.readLine,
+	);
+};
+
 const mockRandoms = numbers => {
 	MissionUtils.Random.pickNumberInRange = jest.fn();
 	numbers.reduce(
@@ -36,7 +47,49 @@ describe('숫자 야구 게임', () => {
 		expect(result).toHaveLength(3);
 	});
 
+	test('getUserNumber 메서드로 받아온 userNumber를 확인', () => {
+		const correctAnswer = ['123'];
+
+		mockQuestions(correctAnswer);
+
+		const app = new App();
+		app.play();
+		const correctResult = app.userNumber;
+
+		expect(correctResult).toEqual('123');
+		expect([...correctResult]).not.toContain('0');
+		expect(correctResult).toHaveLength(3);
+	});
+
+	test('getUserNumber 메서드로 받아온 userNumber 가 0을 포함할 경우 에러를 발생시킨다', () => {
+		const answer = ['012'];
+
+		mockQuestions(answer);
+
+		expect(() => {
+			const app = new App();
+			app.play();
+		}).toThrow();
+	});
+
+	test('getUserNumber 메서드로 받아온 userNumber 의 숫자가 중복될 경우 에러를 발생시킨다', () => {
+		const answer = ['112'];
+
+		mockQuestions(answer);
+
+		expect(() => {
+			const app = new App();
+			app.play();
+		}).toThrow();
+	});
+
 	test('진행상황 확인용 테스트', () => {
+		const randoms = [1, 3, 5];
+		const answers = ['123'];
+
+		mockRandoms(randoms);
+		mockQuestions(answers);
+
 		const app = new App();
 		app.play();
 	});
