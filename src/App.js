bcc6a80c5229b541cc0dcb6e7ad8248816b001cc
ShimSungBo <shimdb96@gmail.com>
2022-11-03 16:24:51 +0900

    feat: 정답 생성기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
-class App {
-  play() {}
-}
+const MissionUtils = require("@woowacourse/mission-utils");
+const Console = MissionUtils.Console;
+const Random = MissionUtils.Random;
 
+class App {
+  play() {
+    let computerAnswer = [];
+    function makeComputerAnswer() {
+      computerAnswer = new Set();
+      while (computerAnswer.size < 3) {
+        computerAnswer.add(Random.pickNumberInRange(1, 9));
+      }
+      computerAnswer = Array.from(computerAnswer);
+    }
+    makeComputerAnswer();
+    console.log(computerAnswer);
+  }
+}
+const a = new App();
+console.log(a.play());
 module.exports = App;
