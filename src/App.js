4a283084bfef469fe4c8db824daf8d5e1b0e1fbe
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2022-11-04 06:20:42 +0900

    feat: 게임 플레이 옵션 예외처리

--- src/App.js
+++ src/App.js
@@ -1,27 +1,38 @@
-const MissionUtils = require("@woowacourse/mission-utils");
+const MissionUtils = require('@woowacourse/mission-utils');
 
 class App {
-  play() {
-    const user = new User();
-    selectPlayOption(user);
-  }
-}
-
-class User {
   constructor() {
-    this.playOption;
-    this.number = [];
+    this.playOption = 0;
+  }
+
+  play() {
+    this.playOption = selectPlayOption();
+    const validValue = isValidOption(this.playOption);
+    if (!validValue) {
+      throw new Error('Play옵션에 없는 값입니다.');
+    }
   }
 }
 
-const selectPlayOption = (user) => {
+const selectPlayOption = () => {
+  let selectValue;
   MissionUtils.Console.readLine(
-    "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
+    '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.',
     (select) => {
       console.log(`${select}`);
-      user.playOption = select;
-    }
+      selectValue = 3; //
+    },
   );
+
+  return selectValue;
+};
+
+const isValidOption = (option) => {
+  if (option === 1 || option === 2) {
+    return true;
+  }
+
+  return false;
 };
 
 module.exports = App;
