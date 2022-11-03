08f589ba0de1ba1164e40386c702febdc57a6cd7
Tory <88531407+1two13@users.noreply.github.com>
2022-11-03 20:50:02 +0900

    refactor(game): async 함수로 변경, enterNumber 함수 실행

--- src/game.js
+++ src/game.js
@@ -1,10 +1,13 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 const pickedNumberByComputer = require("./pickedNumberByComputer");
+const enterNumber = require("./enterNumber");
 
-function game() {
+async function game() {
   const COMPUTER = pickedNumberByComputer();
   let threeStrike = false;
-  while (!threeStrike) {}
+  while (!threeStrike) {
+    let pickedNumberByUser = await enterNumber("숫자를 입력해주세요 : ");
+  }
   MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
 }
 
