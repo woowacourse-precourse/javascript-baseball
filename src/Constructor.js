f8b394a879a4aeade8f72aa8b5f099d653d094a3
moonelysian <dandyblack@naver.com>
2022-11-03 13:54:09 +0900

    fix: constructors 수정
    - END_GAME과 RESTART_GAME 반대로 넣은거 수정
    - MESSAGE.SUCCESS에 있던 게임 종료를 MESSAGE.END로 대체하기로

--- src/Constructor.js
+++ src/Constructor.js
@@ -7,8 +7,8 @@ const RESULT = {
 const MESSAGES = {
   START: '숫자 야구 게임을 시작합니다.',
   INPUT_NUMBER: '숫자를 입력해주세요 : ',
-  SUCCESS: '3개의 숫자를 모두 맞히셨습니다! 게임 종료',
-  RESTART: '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.',
+  SUCCESS: '3개의 숫자를 모두 맞히셨습니다!',
+  RESTART: '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n',
   END: '게임 종료',
 }
 
@@ -16,8 +16,9 @@ const NUMBERS = {
   RANDOM_MIN: 1,
   RANDOM_MAX: 9,
   REQUIRED_LENGHT: 3,
-  END_GAME: 1,
-  RESTART_GAME: 2,
+  RESTART_GAME: 1,
+  END_GAME: 2,
+  SUCCESS_COUNT: 3,
 }
 
 module.exports = {
