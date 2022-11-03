a6845c9956b949afafc8d17d432b75bc1859305a
Chris.c <cjs120dd@gmail.com>
2022-11-03 10:48:10 +0900

    feat:컴퓨터의 랜덤값을 생성한다

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 
 const START_MESSAGE = `숫자 야구 게임을 시작합니다.`;
+const RANDOM_NUM_RANGE = {
+	MIN: 1,
+	MAX: 9,
+};
+const RANDOM_NUM_LENGTH = 3;
 
 class App {
+	constructor() {
+		this.computerNumber = '';
+	}
+
 	play() {
 		this.printMessage(START_MESSAGE);
+		this.computerNumber = this.getComputerNumber();
 	}
 
 	printMessage(message) {
 		MissionUtils.Console.print(message);
+	}
+
+	getComputerNumber() {
+		const numberSet = new Set();
+		while (numberSet.size !== RANDOM_NUM_LENGTH) {
+			numberSet.add(
+				MissionUtils.Random.pickNumberInRange(RANDOM_NUM_RANGE.MIN, RANDOM_NUM_RANGE.MAX),
+			);
+		}
+		return [...numberSet].join('');
 	}
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -7,6 +7,14 @@ const getLogSpy = () => {
 	return logSpy;
 };
 
+const mockRandoms = numbers => {
+	MissionUtils.Random.pickNumberInRange = jest.fn();
+	numbers.reduce(
+		(acc, number) => acc.mockReturnValueOnce(number),
+		MissionUtils.Random.pickNumberInRange,
+	);
+};
+
 describe('숫자 야구 게임', () => {
 	test('print 메소드로 받은값을 출력', () => {
 		const logSpy = getLogSpy();
@@ -15,6 +23,17 @@ describe('숫자 야구 게임', () => {
 		app.printMessage(input);
 
 		expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(input));
+	});
+
+	test('getComputerNumber 메서드로 0을 포함하지 않는 3자리 랜덤값을 반환', () => {
+		const randoms = [1, 3, 5, 5, 8, 9];
+		mockRandoms(randoms);
+		const app = new App();
+		const result = app.getComputerNumber();
+
+		expect(result).toEqual('135');
+		expect([...result]).not.toContain('0');
+		expect(result).toHaveLength(3);
 	});
 
 	test('진행상황 확인용 테스트', () => {
