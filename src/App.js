3a7ed92ccbea35aebbed1f85ad064b9658191b92
ksg2388 <ksg2388@naver.com>
2022-11-04 01:26:20 +0900

    feat(App): 임의의 수 3개를 컴퓨터에 저장한 후 시작 메시지 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+
 class App {
-  play() {}
+  play() {
+    const computer = [];
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
