a41d12431acbaacda7c3bf3895d547b76b829aec
Jiwoo Park <jiwoo9446@naver.com>
2022-11-03 15:19:40 +0900

    feat: 사용자 입력값 받기 메소드 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  constructor() {}
+  play() {
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    this.startGame();
+  }
+
+  startGame() {
+    MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (userInput) => {
+      MissionUtils.Console.print(userInput);
+    });
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
