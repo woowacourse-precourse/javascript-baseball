83d1e5ad6a3e5ecebd88d8d1654caed96250a891
Suyeon <msjgr2801@gmail.com>
2022-11-03 13:00:30 +0900

    Feat: Initialize constuctor
    -생성자에 필요한 변수 선언
    -사용할 메소드 선언

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
+import * as MissionUtils from "@woowacourse/mission-utils";
+
 class App {
-  play() {}
+  constructor() {
+    this.computer = null;
+    this.user = null;
+  }
+
+  play() {
+    // 1. computer값을 생성한다
+    // 2. user값을 받아와 검사한다
+    // 3. computer & user 값을 비교한다
+  }
+
+  makeRandomNumber() {
+
+  }
+
+  getUserValue() {
+
+  }
+
+  checkUserValue(value) {
+
+  }
+
+  compareWithUser(computer, user) {
+
+  }
+
+  exitProgram() {
+
+  }
 }
 
 module.exports = App;
