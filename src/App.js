c5c12fde0ea995544254844804a60d38dcdecbc8
cys4585 <choizerowater@gmail.com>
2022-11-03 09:52:18 +0900

    feat: 기능1. 라이브러리 호출 기능(입출력, 랜덤 값 생성)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
-  play() {
-    console.log("1~9 중 랜덤: ", MissionUtils.Random.pickNumberInRange(1, 9));
-    console.log("1~9 중 랜덤: ", MissionUtils.Random.pickNumberInRange(1, 9));
-    console.log("1~9 중 랜덤: ", MissionUtils.Random.pickNumberInRange(1, 9));
-    console.log("1~9 중 랜덤: ", MissionUtils.Random.pickNumberInRange(1, 9));
-    console.log("1~9 중 랜덤: ", MissionUtils.Random.pickNumberInRange(1, 9));
-    console.log("1~9 중 랜덤: ", MissionUtils.Random.pickNumberInRange(1, 9));
-    console.log("1~9 중 랜덤: ", MissionUtils.Random.pickNumberInRange(1, 9));
-    console.log("1~9 중 랜덤: ", MissionUtils.Random.pickNumberInRange(1, 9));
-    console.log("1~9 중 랜덤: ", MissionUtils.Random.pickNumberInRange(1, 9));
-    console.log("1~9 중 랜덤: ", MissionUtils.Random.pickNumberInRange(1, 9));
-    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
-    MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (answer) => {
-      console.log(`${answer}`);
-      MissionUtils.Console.close();
-    });
+  async play() {
+    print(`1~9 중 랜덤: ${pickNumberInRange(1, 9)}`);
+    print(`1~9 중 랜덤: ${pickNumberInRange(1, 9)}`);
+    print(`1~9 중 랜덤: ${pickNumberInRange(1, 9)}`);
+
+    print("숫자 야구 게임을 시작합니다.");
+
+    let nbr = await readLine("숫자를 입력해주세요 : ");
+    print(nbr);
+    nbr = await readLine("숫자를 입력해주세요 : ");
+    print(nbr);
+    nbr = await readLine("숫자를 입력해주세요 : ");
+    print(nbr);
+
+    print("숫자 야구 게임을 종료합니다.");
   }
+}
+
+function print(str) {
+  MissionUtils.Console.print(str);
+}
+
+function readLine(question) {
+  return new Promise((resolve, reject) => {
+    MissionUtils.Console.readLine(question, (answer) => {
+      resolve(answer);
+    });
+  });
+}
+
+function pickNumberInRange(startInclusive, endInclusive) {
+  return MissionUtils.Random.pickNumberInRange(startInclusive, endInclusive);
 }
 
 module.exports = App;
