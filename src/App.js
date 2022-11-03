039e0192a4199fc4e32a1b4c670f8dc81626e8af
River <swimmingr@gmail.com>
2022-11-03 23:25:47 +0900

    사용자 숫자 입력

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
   play() {
+    console.log("숫자 야구 게임을 시작합니다.");
+    const ATK_NUM = MissionUtils.Console.readLine('숫자를 입력하세요 :',(answer)=>{return answer});
     const DEF_NUM = MissionUtils.Random.pickUniqueNumbersInRange(1,9,3);
-    console.log(DEF_NUM);
   }
 }
 const app = new App();
