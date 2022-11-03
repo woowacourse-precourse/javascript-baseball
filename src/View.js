adb48efc263b1042f43391dc37e5f7aa8f2bfd4c
Sumin Kim <ksumin0730@hufs.ac.kr>
2022-11-03 23:46:15 +0900

    Refactor: printAnswer 메서드 분리 기존의 Controller.js 에 있던 메서드를 따로 분리

---
+++ src/View.js
@@ -0,0 +1,16 @@
+const constant = require('./Constants');
+
+function printAnswer(resultList){
+    if (resultList[0] === 0 && resultList[1] === 0){
+      return constant.RESULTS.NOTHING;
+    }
+    else if (resultList[0] === 0 && resultList[1] !== 0){
+      return String(resultList[1])+constant.RESULTS.BALL;
+    }
+    else if (resultList[0] !== 0 && resultList[1] === 0){
+      return String(resultList[0])+constant.RESULTS.STRIKE;
+    }
+    return String(String(resultList[1])+constant.RESULTS.BALL+' '+String(resultList[0])+constant.RESULTS.STRIKE);
+}
+
+module.exports = printAnswer;
