b19e988cf61e72c4767d78aff09e2a3930476115
Sumin Kim <ksumin0730@hufs.ac.kr>
2022-11-03 22:30:36 +0900

    Refactor: App.js 는 컨트롤러 호출 역할, 게임 첫 실행 역할만 하도록 설정 기존의 App.js에 있던 입출력부분을 컨트롤러로 임시 이동, 추후에 다시 구분 예정

--- src/App.js
+++ src/App.js
@@ -1,61 +1,17 @@
-const MissionUtils = require("@woowacourse/mission-utils");
 const GameControl = require('./Controller.js');
-const constant = require('./Constants');
-const randomNum = require('./model/Random');
-const Exception = require("./model/Exception.js");
+
 
 class App {
-  constructor(){
-    this.gamecount = 0;
-    this.error = new Exception();
-  }
-  
-  async play() {
-    const answer = randomNum();
-    this.gamecount += 1;
-    if (this.gamecount === 1){
-      MissionUtils.Console.print(constant.GAME.START);
-    }
-    const game = new GameControl(answer);
 
-    while(1){
-      const input = await game.userInput();
-      if(this.error.inputError(input) != false){
-        throw this.error.inputError(input);
-      }
-      if (this.checkSuccess(game.userOutput(input))){
-        this.restartGame(game);
-      }
-      else{
-        MissionUtils.Console.print(game.printAnswer(game.userOutput(input)));
-      }
-    }
-    
-  }
-
-  checkSuccess(resultarray){
-    if (resultarray[0] === 3){
-      MissionUtils.Console.print(constant.GAME.ANSWER);
-      return true;
-    }
-  }
-
-  async restartGame(game){
-    const input =  await game.restartCheck();
-    if (this.error.restartError(input) != false){
-      throw this.error.restartError(input);
-    }
-    if (input === '1'){
-      this.play();
-    }
-    else if (input === '2'){
-      MissionUtils.Console.close();
-    }
+  play() {
+    let game = new GameControl();
+    game.startGame();
   }
 
 }
 
 module.exports = App;
 
-const app = new App();
-app.play();
+// const app = new App();
+// app.play();
+
