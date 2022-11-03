7fc8ae152a858cd475c59bb2e1b17c7766d45d29
kinoasm <kinoasm@gmail.com>
2022-11-03 20:23:58 +0900

    Feat: 입력 검증
    입력 조건
    -3글자일 것
    -숫자일 것
    -3개의 입력이 서로 다를 것
    을 충족하지 못할 시 throw로 에러처리

--- src/App.js
+++ src/App.js
@@ -18,7 +18,21 @@ class App {
         return picks;
     }
     getInput() {
-        MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (input) => {});
+        MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (input) => {
+            this.checkInput(input, 3);
+        });
+    }
+    checkInput(input, len) {
+        if (input.length !== len) throw `입력은 ${len}개여야 합니다.`;
+        if (isNaN(input)) throw "입력은 숫자여야 합니다.";
+        if (
+            len === 3 &&
+            (input[0] === input[1] ||
+                input[1] === input[2] ||
+                input[0] === input[2])
+        )
+            throw "입력은 서로 달라야 합니다.";
+        if (len === 3) this.countInput(input.split("").map(Number));
     }
 }
 
