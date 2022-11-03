d411f8e87b1f8ca6fcd56d77a971d82282927508
JangHoonSeok <orirori512@naver.com>
2022-11-03 23:18:32 +0900

    💡 refactor: 주석 추가

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ class App {
     const ANSWER_NUMBER = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
     // 미션 유틸 라이브러리에 있는 함수 pickUniqueNumbersInRange를 활용하여 1부터 9까지의 숫자 중 겹치지 않는 3개의 숫자를 반환.
     // 출력값 예시)[ 3, 2, 8 ] or [ 2, 3, 4 ] 같이 배열로 반환됨.
-    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다."); // 게임 시작 문구 출력
     let inputNum;
     while (isEnd) {
       // Todo: 사용자가 정답을 맞출때 까지 숫자 입력 받아야함.
