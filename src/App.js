3c2025b84e04a3f441ce730b9cbc0848e5af63ae
An Sol <apdlvmf486@gmail.com>
2022-11-03 10:05:22 +0900

    style: 들여쓰기 조건을 맞추기 위해 기능 구현 함수를 분리

--- src/App.js
+++ src/App.js
@@ -1,36 +1,35 @@
-class App {
-  play() {
-    const MissionUtils = require("@woowacourse/mission-utils");
+const MissionUtils = require("@woowacourse/mission-utils");
 
-    const MAKEANSWER = function makeAnswerWithThreeUniqueNumbers() {
-      const THREE_NUMBERS = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
-      const ANSWER = THREE_NUMBERS[0] * 100 + THREE_NUMBERS[1] * 10 + THREE_NUMBERS[2] * 1;
-      return ANSWER;
-    }
+const CHECK_INPUT_DURING_GAME = function checkInputDuringGame(userInput) {
+  if (userInput.length !== 3) return false;
+  if (isNaN(userInput)) return false;
+  if (userInput[0] === userInput[1]
+    || userInput[1] === userInput[2]
+    || userInput[0] === userInput[2]) return false;
+  if (!(userInput.match(/[123456789]{3}/))) return false;
+  return true;
+}
+const CHECK_INPUT_AFTER_GAME = function checkInputAfterGame(userInput) {
+  if (+userInput === 1 || +userInput === 2) return true;
+  return false;
+}
 
-    const CHECK_INPUT_DURING_GAME = function checkInputDuringGame(userInput) {
-      if (userInput.length !== 3) return false;
-      if (isNaN(userInput)) return false;
-      if (userInput[0] === userInput[1]
-        || userInput[1] === userInput[2]
-        || userInput[0] === userInput[2]) return false;
-      if (!(userInput.match(/[123456789]{3}/))) return false;
-      return true;
-    }
+const MAKEANSWER = function makeAnswerWithThreeUniqueNumbers() {
+  const THREE_NUMBERS = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
+  const ANSWER = THREE_NUMBERS[0] * 100 + THREE_NUMBERS[1] * 10 + THREE_NUMBERS[2] * 1;
+  return ANSWER;
+}
 
-    const CHECK_INPUT_AFTER_GAME = function checkInputAfterGame(userInput) {
-      if (+userInput === 1 || +userInput === 2) return true;
-      return false;
-    }
-
-    const CHECK_STRIKE = function numberOfStrikes(userInput, answer) {
-      let count = 0;
-      for (let index = 0; index < 3; index++){
-        if (userInput[index] === answer.toString()[index]) count++;
-      }
-      return count;
-    }
+const CHECK_STRIKE = function numberOfStrikes(userInput, answer) {
+  let count = 0;
+  for (let index = 0; index < 3; index++){
+    if (userInput[index] === answer.toString()[index]) count++;
   }
+  return count;
+}
+
+class App {
+  play() {}
 }
 const app = new App();
 app.play();
