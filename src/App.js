d3daf078e1e9734ae19b4eac291a27facc1027b8
confidential-nt <youyoy689@gmail.com>
2022-11-03 15:46:17 +0900

    feat(요구사항): #3. 사용자로부터 숫자 입력을 받고 그 숫자와 컴퓨터의 숫자를 비교해 볼,스트라이크 개수로 힌트를 표시한다.
    사용자로부터 숫자 입력을 받고 그 숫자와 컴퓨터의 숫자를 비교해 볼,스트라이크 개수로 힌트를 표시함.

--- src/App.js
+++ src/App.js
@@ -11,13 +11,51 @@ class App {
         computerNumber.push(number);
       }
     }
-
     this.computerNumber = computerNumber;
   }
 
+  splitNumber(number) {
+    return (number + "").split("").map((element) => parseInt(element, 10));
+  }
+
+  getHint(userNumber) {
+    const splitedUserNumber = this.splitNumber(userNumber);
+
+    let ball = 0;
+    let strike = 0;
+
+    for (let i = 0; i < this.computerNumber.length; i++) {
+      for (let j = 0; j < splitedUserNumber.length; j++) {
+        if (this.computerNumber[i] === splitedUserNumber[j] && i === j) {
+          strike++;
+          continue;
+        }
+        if (this.computerNumber[i] === splitedUserNumber[j]) {
+          ball++;
+        }
+      }
+    }
+
+    return ball > 0 && strike === 0
+      ? `${ball}볼`
+      : ball === 0 && strike > 0
+      ? `${strike}스트라이크`
+      : ball > 0 && strike > 0
+      ? `${ball}볼 ${strike}스트라이크`
+      : "낫싱";
+  }
+
   play() {
+    let userNumber;
+
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
     this.setComputerNumber();
+
+    MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (number) => {
+      userNumber = parseInt(number, 10);
+      const hint = this.getHint(userNumber);
+      MissionUtils.Console.print(hint);
+    });
   }
 }
 
