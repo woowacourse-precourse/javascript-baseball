9239133017b9043820ff485647d05d9c44b896da
2wndrhs <2wndrhs@gmail.com>
2022-11-03 07:41:28 -0700

    feat: 컴퓨터가 사용할 랜덤한 숫자를 구하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+
 class App {
-  play() {}
+  #RANDOM_NUMBER = 0;
+
+  // 컴퓨터가 사용할 랜덤한 숫자를 구하는 기능
+  static getRandomNumber() {
+    const RANDOM_SET = new Set();
+    while (RANDOM_SET.size < 3) {
+      RANDOM_SET.add(MissionUtils.Random.pickNumberInRange(1, 9));
+    }
+    const RANDOM_NUMBER = Number([...RANDOM_SET].join(''));
+    return RANDOM_NUMBER;
+  }
+
+  play() {
+    this.#RANDOM_NUMBER = App.getRandomNumber();
+    console.log(this.#RANDOM_NUMBER);
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
