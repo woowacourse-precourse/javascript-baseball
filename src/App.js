45d1720b00a619c09fe754a2e2bbeede26717cb2
evencoding <evencoding@gmail.com>
2022-11-03 15:09:33 +0900

    feat: 컴퓨터에게 유니크한 3자리 랜덤 숫자 부여

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+const { Random, Console } = require('@woowacourse/mission-utils');
+
 class App {
+  constructor() {
+    this.offerComputerRandomNumbers();
+    this.userInputNumbers;
+  }
+
+  offerComputerRandomNumbers = () => {
+    this.computerNumbers = Random.pickUniqueNumbersInRange(1, 9, 3);
+  };
+
   play() {}
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
