b581cebe1ca428f852caf8f2f1f0326a63a38839
vinivin153 <vinivin153@naver.com>
2022-11-04 02:08:29 +0900

    refactor: (START_MESSAGE) 문자열을 상수로 대체

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { START_MESSAGE } from './constant';
 const MissionUtils = require('@woowacourse/mission-utils');
 
 class App {
@@ -8,7 +9,7 @@ class App {
 }
 
 function printGameStartMsg() {
-  MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+  MissionUtils.Console.print(START_MESSAGE);
 }
 
 function createComputerNumber() {
