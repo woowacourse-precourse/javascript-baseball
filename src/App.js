15356a8db09a51bad8d4703998708adfe0b76b6b
jeonjeunghoon <jeonjeunghoon@gmail.com>
2022-11-03 22:04:43 +0900

    feat: 애플리케이션 시작 알림 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import MissionUtils from '@woowacourse/mission-utils';
+
+const MESSAGE = {
+  hello: '숫자 야구 게임을 시작합니다.',
+  bye: '숫자 야구 게임을 종료합니다.',
+};
+
 class App {
+  constructor() {
+    MissionUtils.Console.print(MESSAGE.hello);
+  }
+
   play() {}
 }
 
-module.exports = App;
+const app = new App();
+app.play();
+
+// module.exports = App;
