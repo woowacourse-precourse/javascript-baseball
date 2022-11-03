8bed7d2e03d9823b05f1fc003a60f1d1d5f3d465
SeungWoo Kim <osabero@naver.com>
2022-11-03 16:46:47 +0900

    컴퓨터가 숫자 3개를 임의로 정하는 기능을 구현하였습니다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import * as MissionUtils from "@woowacourse/mission-utils";
 class App {
   play() {}
+  computer()
+  
+  
 }
 
+function computer() {
+  const COM_NUMBER = MissionUtils.Random.pickNumberInRange(1, 9, 3);
+  console.log(COM_NUMBER)
+}
 module.exports = App;
