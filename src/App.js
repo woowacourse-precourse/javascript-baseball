bf2d4e385b8747702dfd02c0bd0c4b50465344eb
kangju2000 <kangju2000@naver.com>
2022-11-04 00:01:58 +0900

    feat: play 메서드 구현

--- src/App.js
+++ src/App.js
@@ -28,6 +28,16 @@ class App {
 
     return stringValue.length === 3 && /^[1-9]{3}$/.test([...new Set(stringValue)].join(''));
   }
+
+  #readUserValue() {}
+
+  play() {
+    if (!this.#isPlaying) MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+    this.#isPlaying = true;
+
+    this.#setRandomComputerValue();
+    this.#readUserValue();
+  }
 }
 
 module.exports = App;
