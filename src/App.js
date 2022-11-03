5f0921b2d3a9633898d543f8da218454f6698b21
js43o <js43o@naver.com>
2022-11-03 20:25:06 +0900

    feat: add inputUserNumber() and checkDistinct()

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
   constructor() {
     this.isContinued = true;
@@ -5,16 +7,34 @@ class App {
     this.user = [];
   }
 
-  main() {}
+  checkDistinct(input) {
+    const arr = typeof input === "string" ? input.split("") : input;
+    const set = new Set(arr);
+
+    return arr.length === set.size;
+  }
+
+  inputUserNumber() {
+    MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (input) => {
+      const inputString = input + "";
+      if (inputString.length !== 3) {
+        throw "3자리 숫자를 입력해야 합니다";
+      }
+      if (!checkDistinct(input)) {
+        throw "각 자릿수는 서로 달라야 합니다";
+      }
+      this.user = inputString.split("");
+    });
+    MissionUtils.Console.close();
+  }
+
+  main() {
+    this.inputUserNumber();
+  }
 
   play() {
-    while (isContinued) {
-      main();
-    }
+    this.main();
   }
 }
-
-let app = new App();
-app.play();
 
 module.exports = App;
