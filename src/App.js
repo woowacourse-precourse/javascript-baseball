e48fd09e9c41c881e536a01a8dac685665a7b5a7
chocoro99 <76997927+chocoro99@users.noreply.github.com>
2022-11-03 18:46:19 +0900

    feat : 컴퓨터의 숫자 정하기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
-class App {
-  play() {}
-}
+const MissionUtils = require("@woowacourse/mission-utils");
+let computer = [];
 
+class App {
+  play() {
+    this.computerNumber();
+  }
+
+  computerNumber() {
+    computer = [];
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+  }
+}
 module.exports = App;
