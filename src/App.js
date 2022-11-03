24e3a4c992f6852ee959af67f1079edf09ab17df
ReturnReason <reasonz@daum.net>
2022-11-04 04:42:10 +0900

    feat: 게임 시작 문구를 출력하는 함수 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+const { Random, Console } = require('./common/common');
+const {
+  START_MESSAGE,
+  END_MESSAGE,
+  INPUT_MESSGAE,
+  MAX_LENGTH,
+  START_NUM,
+  END_NUM,
+} = require('./common/constants');
+
 class App {
-  play() {}
+  constructor() {}
+
+  startGame() {
+    Console.print(START_MESSAGE);
+  }
+
+  play() {
+    this.startGame();
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
