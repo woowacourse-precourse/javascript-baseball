d3f42b2b005d948c0d9fd5d18e54c988dfab6e76
MJ <mj632900@gmail.com>
2022-11-04 02:23:08 +0900

    feat : 정답을 입력했을 때 승리문구를 출력하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -11,17 +11,35 @@ class App {
             isFirstStart && MissionUtils.Console.print(`숫자 야구 게임을 시작합니다.`);
         };
 
+        const isCorrect = (userInput, answer) => {
+            return userInput === answer;
+        };
+
+        const printEndMessage = () => {
+            setTimeout(() => {
+                MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            }, 800);
+            setTimeout(() => {
+                MissionUtils.Console.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            }, 1600);
+        };
+
         const getUserInput = () => {
-            MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (number) => {
-                if (number === ANSWER) {
+            MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (input) => {
+                if (isCorrect(input, ANSWER)) {
                     console.log("성공");
+                    return printEndMessage();
                 }
                 getUserInput();
             });
         };
 
-        const ANSWER = createAnswer().join("");
+        const getBall = (userInput, answer) => {};
 
+        const getStrike = (userInput, answer) => {};
+
+        const ANSWER = createAnswer().join("");
+        console.log(ANSWER);
         printStartMassage();
         getUserInput();
     }
