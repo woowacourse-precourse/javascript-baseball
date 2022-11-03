c5c0b5df48d17f6bbe4c00835b7a1dbf1cf3d006
lee-il-woo <empty001430@gmail.com>
2022-11-03 15:32:35 +0900

    Feat: 추가 getUserNumbers 함수
    user의 입력된 숫자를 배열로 가져오는 함수

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 const MissionUtils = require("@woowacourse/mission-utils");
-function computerNumbers(){
+
+function getComputerNumbers(){
   const COMPUTER_NUMBERS = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
   return COMPUTER_NUMBERS
 }
+
+function getUserNumbers(){
+  let user_numbers;
+  MissionUtils.Console.readLine('숫자를 입력해주세요.\n', (user_number) => {
+    user_numbers = String(user_number).split('').map((number)=> Number(number));
+    return user_numbers
+  });
+}
+
+function play(){
+  const COMPUTER_NUMBERS = getComputerNumbers();
+  const USER_NUMBERS = getUserNumbers();
+  console.log(USER_NUMBERS,COMPUTER_NUMBERS)
+}
+
+console.log(play())
 
 class App {
   play() {}
