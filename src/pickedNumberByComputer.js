4d6034edf6b99c11246a88bd5a709dadb8a0161f
Tory <88531407+1two13@users.noreply.github.com>
2022-11-03 13:48:52 +0900

    feat: 컴퓨터가 정한 랜덤숫자를 출력하는 함수 추가

---
+++ src/pickedNumberByComputer.js
@@ -0,0 +1,12 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
+function pickedNumberByComputer() {
+  let pickedNumber = [];
+  while (pickedNumber.length < 3) {
+    let number = MissionUtils.Random.pickNumberInRange(1, 9);
+    if (!pickedNumber.includes(number)) pickedNumber.push(number);
+  }
+  return pickedNumber.join("");
+}
+
+module.exports = pickedNumberByComputer;
