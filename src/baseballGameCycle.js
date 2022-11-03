c7fe541d30389a92c1e3276750e76b58f027941b
kangyeongmin <kym6050@hanyang.ac.kr>
2022-11-03 17:04:44 +0900

    fix: 조건문 수정

--- src/baseballGameCycle.js
+++ src/baseballGameCycle.js
@@ -6,13 +6,15 @@ const checkInputAvailable = require("./checkInputAvailable");
 function baseballGameCycle(randomNumbers) {
   MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (input) => {
     const answer = input.split("");
+
     if (checkInputAvailable(answer)) {
       const result = getResult(randomNumbers, answer);
       MissionUtils.Console.print(result);
       result.length > 10
         ? getRestartOption()
         : baseballGameCycle(randomNumbers);
-    } else {
+    }
+    if (!checkInputAvailable(answer)) {
       throw new Error("잘못된 값을 입력했습니다.");
     }
   });
