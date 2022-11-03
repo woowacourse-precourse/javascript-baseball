c8176c4d44214bb0c164c6da8ffc35a22036179c
Cobocho <rlaalsrb1111@naver.com>
2022-11-04 05:46:05 +0900

    feat: 출력 메세지 상수화

---
+++ src/data/constants.js
@@ -0,0 +1,11 @@
+const MESSAGE = {
+  START: '숫자 야구 게임을 시작합니다.',
+  INPUT: '숫자를 입력해주세요 : ',
+  
+  IS_BLANK: '공백이 아닌 값을 입력해주세요!',
+  NOT_THREE_LENGTH: '3글자인 값을 입력해주세요!',
+  NOT_NUMBER_RANGE: '1부터 9 사이의 값을 입력해주세요!',
+  IS_DUPLICATED: '중복되지 않은 값을 입력하세요!',
+};
+
+exports.MESSAGE = MESSAGE;
