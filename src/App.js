a37f5434b1999dd1a164d8e7db8a59e425585ff2
hailey-hy <lillyine.hy@gmail.com>
2022-11-03 17:40:25 +0900

    feat: 3. 사용자로부터 입력값 얻기

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
+
 class App {
   play() {
     // 정답 숫자 선정
     const computer = [];
+    const MissionUtils = require("@woowacourse/mission-utils");
     while (computer.length < 3) {
       const number = MissionUtils.Random.pickNumberInRange(1, 9);
       if (!computer.includes(number)) {
         computer.push(number);
       }
     }
+
+    // 사용자로부터 입력값 얻기
+    MissionUtils.Console.readLine('숫자를 입력해주세요.', (answer) => {
+      console.log(`숫자를 입력해주세요 : ${answer}`);
+    });
   }
 }
 
