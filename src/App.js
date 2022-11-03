93e6e7963adb1bc3d52880089ce17761ed7a910c
0dytpq0 <rkfnahs12e@gmail.com>
2022-11-03 19:01:01 +0900

    -<feat> : <사용자가 생각한 3가지 수 입력>
    사용자가 입력한 3가지 수를 inputNum 배열에 넣어주었다.
    
    inputNum 추가

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
-// 컴퓨터의 수 3자리 구하기.
-const machinNum = [];
-while (machinNum.length < 3) {
-  const number = MissionUtils.Random.pickNumberInRange(1, 9);
-  if (!machinNum.includes(number)) {
-    machinNum.push(number);
-  }
+const MissionUtils = require("@woowacourse/mission-utils");
+
+// 1. 컴퓨터의 수 3자리 구하기.
+const machineNum = [];
+while (machineNum.length < 3) {
+    const number = MissionUtils.Random.pickNumberInRange(1, 9);
+    if (!machineNum.includes(number)) {
+        machineNum.push(number);
+    }
 }
-console.log(machinNum.join(""));
+
+// 2. 게임 플레이어는 컴퓨터가 생각하고 있는 서로 다른 3개의 숫자를 입력
+const inputNum = [];
+
+MissionUtils.Console.readLine('서로 다른 숫자를 3개 입력하시오.', (answer) => {
+    const num = answer;
+    for (let i = 0; inputNum.length < 3; i++) {
+        inputNum.push(parseInt(num[i]));
+    }
+    MissionUtils.Console.close();
+});
+
 
 
 
 class App {
-  play() { }
+    play() { }
 
 
 
 }
 
 
-// module.exports = App;
-
+module.exports = App;
