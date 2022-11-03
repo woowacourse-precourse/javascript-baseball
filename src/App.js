fd264339827b2525cf1aa06a65be24b5e3897db7
Cobocho <rlaalsrb1111@naver.com>
2022-11-04 05:47:43 +0900

    feat: 사용자 입력값 배열화 구현

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 const print = require("./print.js");
 const game = require("./game.js");
+const validator = require("./validator.js");
+const constants = require("./data/constants.js");
 
 class App {
   play() {
     print.start();
     this.answer = game.getAnswer();
+    this.getInput();
+  }
+  getInput() {
+    let userInput;
+    MissionUtils.Console.readLine(constants.MESSAGE.INPUT, (input) => {
+      userInput = input.trim().split('').map(number => +number);
+    });
   }
 }
 
