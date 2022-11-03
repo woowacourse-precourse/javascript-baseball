1008f340ee4bd45cfab8e3908f057fd7adbadaac
d0422 <rlfehd2013@naver.com>
2022-11-03 21:17:42 +0900

    Feat: GameInput이 숫자가 아닌 경우 체크하는 기능 추가

--- src/GameInputValidation.js
+++ src/GameInputValidation.js
@@ -4,10 +4,16 @@ module.exports = class GameInputValidation {
   }
   validation() {
     this.checkLength();
+    this.checkNumber();
   }
   checkLength() {
     if (this.gameInput.length != 3) {
       throw new Error("입력은 3자리만 가능합니다.");
+    }
+  }
+  checkNumber() {
+    if (this.gameInput != Number(this.gameInput)) {
+      throw new Error("입력은 숫자만 가능합니다.");
     }
   }
 };
