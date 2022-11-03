1ceed57cf75151540ae681232b1e3876c8ed0110
c0c0pang <coc1789@naver.com>
2022-11-04 03:32:38 +0900

    feat: 유저의 숫자를 받고 불러오는 class 구현

--- src/App.js
+++ src/App.js
@@ -6,9 +6,17 @@ class App {
   }
 }
 
+class User {
+  constructor() {
+    this.UserNumber;
+  }
+  GetNum() {
+    return this.UserNumber;
+  }
+}
 
-const ComputerNumber = ()=>{
-  const NUMBER = String(MissionUtils.Random.pickUniqueNumbersInRange(1,9,3));
+const ComputerNumber = () => {
+  const NUMBER = String(MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3));
   return NUMBER.split(",");
 }
 module.exports = App;
