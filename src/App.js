6540a64d95b8f5b5ea9bc71211a110ba2705eb95
하송희 <99heesong@naver.com>
2022-11-03 19:42:18 +0900

    기능추가: 컴퓨터가 랜덤으로 숫자 3개 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
   play() {}
+
+  getRandomNumber() {
+    MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
+  }
 }
+
+const numberBaseball = new App();
 
 module.exports = App;
