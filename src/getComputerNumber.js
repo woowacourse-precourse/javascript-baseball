4075b6c2c2af75a3384c4b02ace80a5171dea54d
AhnGeunTaek <dksrmsxor@gmail.com>
2022-11-04 00:57:34 +0900

    Feat: 컴퓨터(상대방)의 랜덤 숫자를 생성하는 기능 추가

--- src/getComputerNumber.js
+++ src/getComputerNumber.js
@@ -3,3 +3,15 @@ const MissionUtils = require('@woowacourse/mission-utils');
 const getRandomNumber = (minNum, maxNum) => {
   return MissionUtils.Random.pickNumberInRange(minNum, maxNum);
 };
+
+const getComputerNumber = () => {
+  const computerNumber = new Set();
+
+  while (computerNumber.size < 3) {
+    computerNumber.add(getRandomNumber(1, 9));
+  }
+
+  return [...computerNumber].join('');
+};
+
+module.exports = getComputerNumber;
