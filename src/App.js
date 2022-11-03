dc05c1ba27acd79558c8189b451474b04ed53500
vinivin153 <vinivin153@naver.com>
2022-11-04 01:20:24 +0900

    feat: (createComputerNumber)컴퓨터의 수 랜덤 생성하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -3,11 +3,26 @@ const MissionUtils = require('@woowacourse/mission-utils');
 class App {
   play() {
     printGameStartMsg();
+    const computerNumber = createComputerNumber();
   }
 }
 
 function printGameStartMsg() {
   MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+}
+
+function createComputerNumber() {
+  const computerNumber = [];
+
+  while (computerNumber.length < 3) {
+    const randomNumber = MissionUtils.Random.pickNumberInRange(1, 9);
+
+    if (!computerNumber.includes(randomNumber)) {
+      computerNumber.push(randomNumber);
+    }
+  }
+
+  return computerNumber;
 }
 
 module.exports = App;
