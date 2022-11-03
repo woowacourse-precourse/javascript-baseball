6b3de7e65d67b8f138934ebdb9bbb519811f3e9d
chiman <rlaclghks123@naver.com>
2022-11-03 10:22:17 +0900

    feat: 유저에게 숫자를 입력받는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+const MissionUtils = require("@woowacourse/mission-utils");
 class App {
-  play() {}
+  play() {
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    this.getUserNumber();
+  }
+  getUserNumber() {
+    MissionUtils.Console.readLine("닉네임을 입력해주세요.", (answer) => {
+      console.log(`닉네임: ${answer}`);
+      MissionUtils.Console.close();
+    });
+  }
 }
 
+const app = new App();
+app.play();
 module.exports = App;
