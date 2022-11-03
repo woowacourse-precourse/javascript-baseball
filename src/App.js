b34b18e28d8925a12cff5f3736e28bdd01b0c413
bassyu <yrkdgus@gmail.com>
2022-11-03 14:05:40 +0900

    feat: 입력을 받아 게임을 진행하는 메인 기능 구현
    - play 함수에서 입력을 받고 상황에 따라 진행하는 기능 구현
    - 다른 함수를 클래스 안으로 포함

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
+  constructor() {
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+    const ramdomArray = this.getRandomArray();
+  }
+
   play() {
-    console.log(getRandomArray());
-  }
-}
+    MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (input) => {
+      MissionUtils.Console.print('2스트라이크');
 
-const getRandomArray = () => {
-  const randomArray = [];
-  while (randomArray.length < 3) {
-    const random = MissionUtils.Random.pickNumberInRange(1, 9);
-    if (!randomArray.includes(random)) {
-      randomArray.push(random);
+      if (input === '2') {
+        MissionUtils.Console.close();
+      } else {
+        this.play();
+      }
+    });
+  }
+
+  getRandomArray() {
+    const randomArray = [];
+    while (randomArray.length < 3) {
+      const random = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!randomArray.includes(random)) {
+        randomArray.push(random);
+      }
     }
+  
+    return randomArray;
   }
-
-  return randomArray;
 }
 
 module.exports = App;
