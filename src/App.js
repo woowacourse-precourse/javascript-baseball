6d70f5c4b552fc90c9b4cda5ba773e980b15ebf8
moonelysian <dandyblack@naver.com>
2022-11-03 10:48:53 +0900

    feat: 사용자 입력 validation
    사용자가 입력한 값을 검증한다
    - 1~9사이 수를 사용한 3자리 숫자인지
    - 중복으로 입력된 숫자가 없는지

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 const { Random, Console } = require("@woowacourse/mission-utils");
 const { NUMBERS, MESSAGES, RESULT } = require('../src/Constructor')
 class App {
+  result = {
+    ball: 0,
+    strike: 0
+  }
+
   generateRandomNums(min, max, length) {
     const randoms = Random.pickUniqueNumbersInRange(min, max, length);
     return Random.shuffle(randoms);
   }
+
+  isValidNumberInput(userInput) {
+    const regex = new RegExp(`[${NUMBERS.RANDOM_MIN}-${NUMBERS.RANDOM_MAX}]*${NUMBERS.REQUIRED_LENGHT}`);
+
+    if (!regex.test(userInput)) {
+      return false
+    }
+
+    const set = new Set([...userInput])
+    if (set.size !== userInput.length) {
+      return false;
+    }
+    
+    return true;
+  }
   
   play() {
     const computer =  this.generateRandomNums(NUMBERS.RANDOM_MIN, NUMBERS.RANDOM_MAX, NUMBERS.REQUIRED_LENGHT);
+    Console.print(MESSAGES.START);
+    Console.readLine(MESSAGES.INPUT_NUMBER, (input) => {
+    })
   }
 }
 
