42990b923832f2624974a854dbb494ad824b8d54
moonelysian <dandyblack@naver.com>
2022-11-03 09:53:56 +0900

    feat: 상수로 쓰일 문장 및 숫자들 생성
    - RESULT: 야구 게임 결과에 대한 상수 object
    - MESSAGE: 콘솔에 출력될 메세지 object
    - NUMBERS: 해당 게임을 진행하는데 필요한 고정된 숫자들

---
+++ src/Constructor.js
@@ -0,0 +1,21 @@
+export const RESULT = {
+  NOTHING: '낫싱',
+  BALL: '볼',
+  STRIKE: '스트라이크',
+}
+
+export const MESSAGES = {
+  START: '숫자 야구 게임을 시작합니다.',
+  INPUT_NUMBER: '숫자를 입력해주세요 : ',
+  SUCCESS: '3개의 숫자를 모두 맞히셨습니다! 게임 종료',
+  RESTART: '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.',
+  END: '게임 종료',
+}
+
+export const NUMBERS = {
+  RANDOM_MIN: 1,
+  RANDOM_MAX: 9,
+  REQUIRED_LENGHT: 3,
+  END_GAME: 1,
+  RESTART_GAME: 2,
+}
