5fb4520f2aaab1365dbdb72d9a18f5bd01daa363
chiman <rlaclghks123@naver.com>
2022-11-03 18:36:26 +0900

    refactor: 함수명 수정
    getUserNumber() => getUserNumberFromReadLine()

--- src/App.js
+++ src/App.js
@@ -3,17 +3,44 @@ class App {
   constructor() {
     this.computerNumberArr = this.getRandomNumberFromComputer();
   }
+
   play() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
-    this.getUserNumber();
+    this.getUserNumberFromReadLine();
   }
-  getUserNumber() {
+
+  getUserNumberFromReadLine() {
     MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (userNumberStr) => {
       this.isValid(userNumberStr);
       this.playBaseBall(userNumberStr);
-      console.log(this.computerNumberArr);
     });
   }
+
+  isValid(userNumberStr) {
+    this.isValidSingleDigitNaturalNumber(userNumberStr);
+    this.isValidNumberWithoutDuplicate(userNumberStr);
+  }
+
+  isValidSingleDigitNaturalNumber(userNumberStr) {
+    const regexp = new RegExp("^[1-9]+$");
+    if (!regexp.test(userNumberStr)) {
+      throw "1에서 9까지의 자연수를 입력해주세요";
+    }
+  }
+
+  isValidNumberWithoutDuplicate(userNumberStr) {
+    let isValid = false;
+    const wordLengthWidhoutDuplicate = new Set([...userNumberStr]).size;
+
+    [...userNumberStr].map((i) => {
+      i === "" || i === " " ? (isValid = true) : (isValid = false);
+    });
+
+    if (wordLengthWidhoutDuplicate !== 3 || isValid) {
+      throw "서로 다른 3개의 숫자를 입력해주세요";
+    }
+  }
+
   playBaseBall(userNumberStr) {
     const totalCountStrike = this.countStrike(
       userNumberStr,
@@ -38,7 +65,7 @@ class App {
         `${totalCountBall}볼 ${totalCountStrike}스트라이크`
       );
     }
-    this.getUserNumber();
+    this.getUserNumberFromReadLine();
   }
 
   confirmExit() {
@@ -47,38 +74,13 @@ class App {
       (userNumberStr) => {
         if (userNumberStr === "1") {
           this.computerNumberArr = this.getRandomNumberFromComputer();
-          return this.getUserNumber();
+          return this.getUserNumberFromReadLine();
         }
         return MissionUtils.Console.close();
-        // this.isValid(userNumberStr);
-        // this.playBaseBall(userNumberStr);
       }
     );
   }
 
-  isValid(answer) {
-    this.isValidSingleDigitNaturalNumber(answer);
-    this.isValidNumberWithoutDuplicate(answer);
-  }
-
-  isValidSingleDigitNaturalNumber(answer) {
-    const regexp = new RegExp("^[1-9]+$");
-    if (!regexp.test(answer)) {
-      throw "1에서 9까지의 자연수를 입력해주세요";
-    }
-  }
-  isValidNumberWithoutDuplicate(answer) {
-    let isValid = false;
-    const wordLengthWidhoutDuplicate = new Set([...answer]).size;
-
-    [...answer].map((i) => {
-      i === "" || i === " " ? (isValid = true) : (isValid = false);
-    });
-
-    if (wordLengthWidhoutDuplicate !== 3 || isValid) {
-      throw "서로 다른 3개의 숫자를 입력해주세요";
-    }
-  }
   getRandomNumberFromComputer() {
     const randomArr = [];
 
@@ -101,6 +103,7 @@ class App {
     }, 0);
     return totalCount;
   }
+
   countBall(userNumberStr, computerNumberArr) {
     let totalCount = 0;
     totalCount = computerNumberArr.reduce((count, comCurNum, index) => {
