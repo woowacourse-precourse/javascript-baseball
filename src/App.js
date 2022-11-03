ba86f74ad54a48b2c6fa71d67ffb645ff9139d4a
chocoro99 <76997927+chocoro99@users.noreply.github.com>
2022-11-03 18:51:13 +0900

    feat : 볼과 스트라이크 개수에 따른 메시지 출력과 게임 종료 시 메시지 출력

--- src/App.js
+++ src/App.js
@@ -26,6 +26,23 @@ class App {
         if (i == computer.indexOf(parseInt(answer[i]))) strike++;
         else if (computer.includes(parseInt(answer[i]))) ball++;
       }
+
+      if (ball > 0 && strike > 0) {
+        MissionUtils.Console.print(`${ball}볼 ${strike}스트라이크`);
+      } else if (ball > 0 && strike == 0) {
+        MissionUtils.Console.print(`${ball}볼`);
+      } else if (ball == 0 && strike > 0 && strike != 3) {
+        MissionUtils.Console.print(`${strike}스트라이크`);
+      } else if (ball == 0 && strike == 0) {
+        MissionUtils.Console.print("낫싱");
+      } else if (strike == 3) {
+        MissionUtils.Console.print("3스트라이크");
+        MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        MissionUtils.Console.print(
+          "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
+        );
+      }
+      this.start();
     });
   }
 }
