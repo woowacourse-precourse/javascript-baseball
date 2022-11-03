45e6567e3a83b9ec8f28235015be85d9c5263a8b
dladncks1217 <dlaxodud1217@gmail.com>
2022-11-03 16:36:43 +0900

    feat: 조건에 맞는 정답 숫자 생성 함수 추가

---
+++ src/utils/makeRandomNumber.js
@@ -0,0 +1,17 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
+const makeRandomNumber = () => {
+  const answer = [];
+
+  answer.push(MissionUtils.Random.pickNumberInRange(1, 9)); // 첫 자릿수에 0이 나오지 않도록
+  while (answer.length < 3) {
+    const tempNumber = MissionUtils.Random.pickNumberInRange(0, 9);
+    if (answer.indexOf(tempNumber) === -1) {
+      answer.push(tempNumber);
+    }
+  }
+
+  return +answer.join(""); // 3자리 수를 만들어 number형태로 리턴
+};
+
+module.exports = makeRandomNumber;
