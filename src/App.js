27b63d07159f3bcd566234f658c1f73c5a2f192d
kangyeongmin <kym6050@hanyang.ac.kr>
2022-11-03 14:52:07 +0900

    fix: 오타 수정

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 const MissionUtils = require("@woowacourse/mission-utils");
-const baseballGameCycle = require("./");
-const getResult = require("./getResult");
+const baseballGameCycle = require("./baseballGameCycle");
 const createRandomNumbers = require("./createRandomNumbers");
 
 class App {
   play() {
-    let answer;
     const randomNumbers = createRandomNumbers();
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
 
