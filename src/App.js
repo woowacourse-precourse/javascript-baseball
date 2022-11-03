ecc72d39cb955674912e8448a00cd24720c5f6c4
park junwoo <always1997@kakao.com>
2022-11-03 16:09:58 +0900

    feat: 사용자로부터 숫자 입력 받기 완료

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import * as MissionUtils from '@woowacourse/mission-utils';
 class App {
   play() {
     const computer = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
+    let userAnswer;
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    MissionUtils.Console.readLine("숫자를 입력해주세요 : ",(answer)=> {
+      userAnswer = answer.split('').map((num)=>{return Number(num)});
+    });
   }
 }
 
