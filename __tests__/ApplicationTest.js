97fc75e9bb9139eb9aead86370175cc328a3a1a0
Rki0 <pky00823@gmail.com>
2022-11-03 10:34:43 +0900

    feat: 게임 시작 메세지 출력 함수 테스트 코드
    게임 시작 메세지를 출력하는 함수가 정상 작동하는지 단일 기능에 대하여 테스트하는 코드를 작성했습니다.

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -24,6 +24,15 @@ const getLogSpy = () => {
 };
 
 describe("숫자 야구 게임", () => {
+  test("게임 시작 메세지 출력", () => {
+    const logSpy = getLogSpy();
+
+    const app = new App();
+    app.showStartMessage();
+
+    expect(logSpy).toHaveBeenCalledWith("숫자 야구 게임을 시작합니다.");
+  });
+
   test("게임 종료 후 재시작", () => {
     const randoms = [1, 3, 5, 5, 8, 9];
     const answers = ["246", "135", "1", "597", "589", "2"];
