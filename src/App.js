04da3534191a78548978d97a5c108e21697a6bca
oseunghyun <ohsh1477@gmail.com>
2022-11-03 18:51:23 +0900

    feat: implement generateDifferRandomNum method

--- src/App.js
+++ src/App.js
@@ -2,6 +2,18 @@ const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
   play() {}
+
+  generateDifferRandomNumArr(numOfDigits) {
+    const DIFFER_RANDOM_NUM_ARR = [];
+    let randomNum;
+    while (DIFFER_RANDOM_NUM_ARR.length < numOfDigits) {
+      randomNum = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!DIFFER_RANDOM_NUM_ARR.includes(randomNum)) {
+        DIFFER_RANDOM_NUM_ARR.push(randomNum);
+      }
+    }
+    return DIFFER_RANDOM_NUM_ARR;
+  }
 }
 
 module.exports = App;
