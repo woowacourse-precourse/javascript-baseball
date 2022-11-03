253591f8708f3eae00071aea0bfcd9bd6c21f2b3
wang-yurin <king_1994@naver.com>
2022-11-03 20:11:43 +0900

    Feat: 게임 시작 문구 출력 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+const MissionUtils = require("@woowacourse/mission-utils");
 class App {
-  play() {}
+  play() {
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
