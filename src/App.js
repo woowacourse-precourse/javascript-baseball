bb2f810187aacfc35bc953d0675cbe8d2e67defc
Yoo Seung Guk <dbtmd878@naver.com>
2022-11-03 15:35:26 +0900

    feat(Functions): 숫자 입력받는 함수 분리, 랜덤 숫자와 입력받은 숫자 비교 함수 추가, 결과를 알려주는 함수 추가

--- src/App.js
+++ src/App.js
@@ -4,8 +4,15 @@ const pickUniqueNumbersInRange = MissionUtils.Random.pickUniqueNumbersInRange;
 class App {
   constructor() {
     this.RANDOM_NUMBER_ARRAY;
-    this.INPUT_USER_NUMBER = 0;
+    this.INPUT_USER_NUMBER;
+    this.strikeCount = 0;
+    this.ballCount = 0;
     this.gameState = true;
+  }
+  setting() {
+    this.gameState && this.randomNumberGenerator();
+    this.strikeCount = 0;
+    this.ballCount = 0;
   }
   randomNumberGenerator() {
     this.gameState = false;
@@ -13,11 +20,54 @@ class App {
     return this;
   }
   play() {
-    this.gameState && this.randomNumberGenerator();
+    this.setting();
+    Console.print(this.RANDOM_NUMBER_ARRAY);
+    return this.initNumber();
+  }
+
+  initNumber() {
     Console.readLine("숫자를 입력하세요. : ", (inputNumber) => {
       this.INPUT_USER_NUMBER = inputNumber;
-      Console.close();
+      this.strikeAndBallCheck();
     });
+  }
+
+  strikeAndBallCheck() {
+    const stringNumberArray = String(this.INPUT_USER_NUMBER).split("");
+    for (let index = 0; index < 3; index++) {
+      if (
+        Number(stringNumberArray[index]) === this.RANDOM_NUMBER_ARRAY[index]
+      ) {
+        this.strikeCount++;
+        continue;
+      }
+      this.RANDOM_NUMBER_ARRAY.includes(Number(stringNumberArray[index])) &&
+        this.ballCount++;
+    }
+
+    return this.printResult();
+  }
+
+  printResult() {
+    if (this.strikeCount === 3) {
+      Console.print("3스트라이크!");
+      Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+      return Console.close();
+    }
+    if (this.ballCount === 0 && this.strikeCount === 0) {
+      Console.print("낫싱");
+      return this.initNumber();
+    }
+    if (this.ballCount < 4 && this.strikeCount === 0) {
+      Console.print(`${this.ballCount}볼`);
+      return this.initNumber();
+    }
+    if (this.ballCount === 0 && this.strikeCount < 3) {
+      Console.print(`${this.strikeCount}스트라이크`);
+      return this.initNumber();
+    }
+    Console.print(`${this.ballCount}볼 ${this.strikeCount}스트라이크`);
+    return this.initNumber();
   }
 }
 
