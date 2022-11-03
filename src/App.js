cc94f53973263d7e85fb8c7c4bb7a9ffd144d37a
jsh3418 <albert5428@gmail.com>
2022-11-03 22:41:28 +0900

    feat: 컴퓨터가 정답을 랜덤으로 만드는 기능 구현
    1. 1~9까지의 랜덤 수를 뽑는다.
    2. 중복되지 않도록 검사하여 push한다.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-const { Console } = require("@woowacourse/mission-utils");
+const { Console, Random } = require("@woowacourse/mission-utils");
 
 class App {
   constructor() {
-    this.answer;
+    this.computerAnswer = [];
+    this.userAnswer;
   }
 
   play() {
     Console.print("숫자 야구 게임을 시작합니다.");
 
+    while (this.computerAnswer.length < 3) {
+      const number = Random.pickNumberInRange(1, 9);
+      if (!this.computerAnswer.includes(number)) {
+        this.computerAnswer.push(number);
+      }
+    }
+
     Console.readLine("숫자를 입력해주세요 : ", (answer) => {
-      this.answer = answer;
+      this.userAnswer = answer;
     });
   }
 }
