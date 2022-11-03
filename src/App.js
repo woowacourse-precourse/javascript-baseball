5e95bbffe9c4405afdc60afcb44e9f830ec74d44
Dahye <06robin11@gmail.com>
2022-11-03 20:06:20 +0900

    feat:get Usernumber from user and check its validity.

--- src/App.js
+++ src/App.js
@@ -20,7 +20,35 @@ class App {
     return nums.length === new Set(nums).size;
   };
 
-  play() {}
+  getUserNumber = () => {
+    let userNumber;
+    MissionUtils.Console.readLine("숫자를 입력해주세요 :", (inputNumber) => {
+      try {
+        const valideNumbers = this.checkValidity(inputNumber);
+        userNumber = valideNumbers;
+      } catch (e) {
+        MissionUtils.Console.print(e);
+        MissionUtils.Console.close();
+      }
+    });
+    return userNumber;
+  };
+
+  checkValidity = (userNumber) => {
+    const numsArray = Array.from(String(userNumber), Number);
+    if (numsArray.length !== 3) {
+      throw "숫자 3개를 입력해야 합니다.";
+    } else if (this.isEveryNumberUnique(numsArray)) {
+      throw "중복되지 않는 숫자 3개를 입력해야 합니다.";
+    } else {
+      return numsArray;
+    }
+  };
+
+  play() {
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    const userNumber = this.getUserNumber();
+  }
 }
 
 module.exports = App;
