05107a9318a3f482e5af5810119697ebcef50289
Buyoung Chung <sunarol@naver.com>
2022-11-03 17:06:18 +0900

    refactor: change functions to class App methods

--- src/App.js
+++ src/App.js
@@ -6,25 +6,31 @@ class App {
     this.userAnswer = userAnswer;
   }
   play() {
-    greeting();
-    this.computerRandomNumber = makeRandomNumberArray();
+    this.greeting();
+    this.computerRandomNumber = this.makeRandomNumberArray();
     this.userAnswer = this.inputUserAnswer();
   }
+
+  greeting() {
+    console.log("숫자 야구 게임을 시작합니다.");
+  }
+
   inputUserAnswer() {
     MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (answer) => {
       this.userAnswer = answer;
     });
   }
+
+  makeRandomNumberArray() {
+    this.computerRandomNumber = MissionUtils.Random.pickUniqueNumbersInRange(
+      1,
+      9,
+      3
+    );
+  }
 }
 
-function makeRandomNumberArray() {
-  return MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
-}
-
-function greeting() {
-  console.log("숫자 야구 게임을 시작합니다.");
-}
 const app = new App();
 app.play();
-console.log(app.userAnswer);
+
 module.exports = App;
