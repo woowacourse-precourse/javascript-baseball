53fba8c3585798370aa25900582836db3378383b
loopy-lim <loopy@jnu.ac.kr>
2022-11-03 13:47:44 +0900

    feat: 게임을 시작하면 '숫자 야구 게임을 시작합니다.\n'를 출력한다

---
+++ src/Baseball/Baseball.output.js
@@ -0,0 +1,9 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
+class BaseBasllOutput {
+  start() {
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.\n");
+  }
+}
+
+export default BaseBasllOutput;
