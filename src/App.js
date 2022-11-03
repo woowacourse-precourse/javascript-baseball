276e53fec104face22f7dbedf87436745447955a
kitez <duswl339@ajou.ac.kr>
2022-11-03 17:02:11 +0900

    refactor: function들 class의 method로 수정
    ApplicationTest.js 수정 없이 동작시키기 위해서는 App.js에서는 module.exports = App;으로 해두어야한다.
    
    App class 외부에 선언한 match, validationCheck 함수를 App class의 method로 수정하였다.
    
    이렇게 수정하면 match/validationCheck 함수 각각긔 test code를 돌리기 위해서 각 함수를 export하지 않고 App instance를 생성하여 App의 method로 import하여 사용가능하다.

--- src/App.js
+++ src/App.js
@@ -1,77 +1,77 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 
-/**
- * @param computer : computer가제시한숫자
- * @param user : user가입력한숫자
- *
- * @return result : Array
- * result[0] : ball number
- * result[1] : strike number
- */
-function match(computer, user) {
-  const userArr = user.toString().split('');
-  // result[0] : ball
-  // result[1] : strike
-  const result = [0, 0];
+class App {
+  /**
+   * @param computer : computer가제시한숫자
+   * @param user : user가입력한숫자
+   *
+   * @return result : Array
+   * result[0] : ball number
+   * result[1] : strike number
+   */
+  match(computer, user) {
+    const userArr = user.toString().split('');
+    // result[0] : ball
+    // result[1] : strike
+    const result = [0, 0];
 
-  computer.toString().split('').forEach((c, index) => {
-    let ball = 0;
-    let strike = 0;
-    if (userArr.includes(c)) {
-      ball += 1;
-    }
+    computer.toString().split('').forEach((c, index) => {
+      let ball = 0;
+      let strike = 0;
+      if (userArr.includes(c)) {
+        ball += 1;
+      }
 
-    if (c === userArr[index]) {
-      strike += 1;
-      ball -= 1;
-    }
-    result[0] += ball;
-    result[1] += strike;
-  });
+      if (c === userArr[index]) {
+        strike += 1;
+        ball -= 1;
+      }
+      result[0] += ball;
+      result[1] += strike;
+    });
 
-  return result;
-}
-
-/**
- * computer또는 user가 입력한 숫자 값의 validation check
- * @param number : validationCheckNumber
- *
- * @return
- * valid return true ( length === 3 && 1~9 )
- * invalid return false
- */
-function validationCheck(number) {
-  let result = true;
-
-  const numberString = number.toString();
-  // 3자리가 아니면 return false
-  if (numberString.length !== 3) {
-    return false;
+    return result;
   }
 
-  // 1 ~ 9 가 아니면 return false
-  numberString.split('').forEach((n) => {
-    if (!(n.charCodeAt(0) >= 49 && n.charCodeAt(0) <= 57)) {
-      result = false;
+  /**
+   * computer또는 user가 입력한 숫자 값의 validation check
+   * @param number : validationCheckNumber
+   *
+   * @return
+   * valid return true ( length === 3 && 1~9 )
+   * invalid return false
+   */
+  validationCheck(number) {
+    let result = true;
+
+    const numberString = number.toString();
+    // 3자리가 아니면 return false
+    if (numberString.length !== 3) {
+      return false;
     }
-  });
 
-  return result;
-}
+    // 1 ~ 9 가 아니면 return false
+    numberString.split('').forEach((n) => {
+      if (!(n.charCodeAt(0) >= 49 && n.charCodeAt(0) <= 57)) {
+        result = false;
+      }
+    });
 
-class App {
+    return result;
+  }
+
   play() {
-    // start
+    // start;
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
-    let flag = 1;
-    const input = null;
+    const flag = 1;
+    let user = null;
 
     while (flag === 1) {
-      MissionUtils.Console.readLine('숫자를 입력해주세요 : ', input);
-      console.log('input : ', input);
-      flag = 2;
+      MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (input) => {
+        user = input;
+      });
     }
   }
 }
 
-module.exports = { App, validationCheck, match };
+module.exports = App;
