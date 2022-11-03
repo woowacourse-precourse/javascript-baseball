2dd0ece667f334487c9ed5581059ce684b8373ec
kangju2000 <kangju2000@naver.com>
2022-11-04 00:08:04 +0900

    feat: setUserValue 메서드 구현

--- src/App.js
+++ src/App.js
@@ -29,7 +29,14 @@ class App {
     return stringValue.length === 3 && /^[1-9]{3}$/.test([...new Set(stringValue)].join(''));
   }
 
-  #setUserValue(value) {}
+  #getResult() {}
+
+  #setUserValue(value) {
+    if (this.#isAvailableValue(value)) {
+      this.#userValue = value;
+      this.#getResult();
+    } else throw Error('잘못된 값을 입력했습니다. 게임을 종료합니다.');
+  }
 
   #readUserValue() {
     MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (answer) => this.#setUserValue(answer));
