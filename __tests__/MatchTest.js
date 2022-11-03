0793e3a787d20cb9644464d157fd5dfdcfbd4c84
kitez <duswl339@ajou.ac.kr>
2022-11-03 17:06:57 +0900

    refactor: App.js function import
    App.js의 exports부분과 함수를 App class의 method로 수정하면서 테스트하고 하는 함수를 import하는 로직 수정

--- __tests__/MatchTest.js
+++ __tests__/MatchTest.js
@@ -1,11 +1,13 @@
-const fn = require('../src/App');
+const App = require('../src/App');
 
-describe('match test', () => {
+const app = new App();
+
+describe('App.match test', () => {
   test('1볼 1스트라이크', () => {
     const computer = '123';
     const user = '324';
 
-    const result = fn.match(computer, user);
+    const result = app.match(computer, user);
 
     expect(result).toEqual([1, 1]);
   });
@@ -14,7 +16,7 @@ describe('match test', () => {
     const computer = '123';
     const user = '321';
 
-    const result = fn.match(computer, user);
+    const result = app.match(computer, user);
 
     expect(result).toEqual([2, 1]);
   });
@@ -23,7 +25,7 @@ describe('match test', () => {
     const computer = '123';
     const user = '456';
 
-    const result = fn.match(computer, user);
+    const result = app.match(computer, user);
 
     expect(result).toEqual([0, 0]);
   });
@@ -32,7 +34,7 @@ describe('match test', () => {
     const computer = '123';
     const user = '123';
 
-    const result = fn.match(computer, user);
+    const result = app.match(computer, user);
 
     expect(result).toEqual([0, 3]);
   });
--- __tests__/ValidateTest.js
+++ __tests__/ValidateTest.js
@@ -1,30 +1,32 @@
-const fn = require('../src/App');
+const App = require('../src/App');
+
+const app = new App();
 
 describe('validation test', () => {
   test('correct', () => {
     const input = '123';
-    const result = fn.validationCheck(input);
+    const result = app.validationCheck(input);
 
     expect(result).toEqual(true);
   });
 
   test('incorrect1 : length > 3', () => {
     const input = '1234';
-    const result = fn.validationCheck(input);
+    const result = app.validationCheck(input);
 
     expect(result).toEqual(false);
   });
 
   test('incorrect2 : contain not number', () => {
     const input = '12a';
-    const result = fn.validationCheck(input);
+    const result = app.validationCheck(input);
 
     expect(result).toEqual(false);
   });
 
   test('incorrect2 : contain 0', () => {
     const input = '120';
-    const result = fn.validationCheck(input);
+    const result = app.validationCheck(input);
 
     expect(result).toEqual(false);
   });
