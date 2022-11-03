ec30462c77092f69994c0da426457c54401a641b
vinivin153 <vinivin153@naver.com>
2022-11-04 02:06:54 +0900

    feat: 상수 모듈 추가

---
+++ src/constant.js
@@ -0,0 +1,8 @@
+export const START_MESSAGE = '숫자 야구 게임을 시작합니다.';
+export const INPUT_NUMBER_MESSAGE = '숫자를 입력해주세요 : ';
+export const BALL = '볼';
+export const STRIKE = '스트라이크';
+export const NOTHING = '낫싱';
+export const WIN_MESSAGE = '3개의 숫자를 모두 맞히셨습니다! 게임 종료';
+export const END_MESSAGE =
+  '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.';
