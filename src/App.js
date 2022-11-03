0ad873c2adb4d679ebacf2e7330a9a5096ac9e57
HBSPS <wiyeong79@naver.com>
2022-11-03 14:50:39 +0900

    feat(add getResult method):
    getRsult
    - count Ball & Strike
    - input: comNumberArr, userNumberArr
    - return: [ Ball count, Strike count ]

--- src/App.js
+++ src/App.js
@@ -18,17 +18,32 @@ function getComputerNumber() {
 
 function getUserInput() {
   MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (input) => {
-    MissionUtils.Console.close()
+    MissionUtils.Console.close();
   })
 }
 
 function strToIntArr(str) {
-  const strArr = [...str]
+  const strArr = [...str];
   const intArr = [];
 
   strArr.forEach((item) => intArr.push(parseInt(item)));
 
   return intArr;
+}
+
+function getResult(comArr, userArr) {
+  let resultArr = [0, 0] // resultArr[0] = Ball, resultArr[1] = Strike
+  for (let i = 0; i < comArr.length; i++) {
+    if (comArr[i] == userArr[i]) {
+      resultArr[1] += 1;
+    }
+    
+    if (comArr.includes(userArr[i]) && comArr[i] != userArr[i]) {
+      resultArr[0] += 1;
+    }
+  }
+
+  return resultArr;
 }
 
 module.exports = App;
