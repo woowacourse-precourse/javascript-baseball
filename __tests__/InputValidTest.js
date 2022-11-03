20c70262cdc68fbb5e83b6c88491d29ae3ce1ce0
kyw0716 <kyw0716@naver.com>
2022-11-03 16:49:47 +0900

    comment: TODO 추가 (게임 종료 후 입력값 유효성 테스트 추가하기)

--- __tests__/InputValidTest.js
+++ __tests__/InputValidTest.js
@@ -1,6 +1,7 @@
 const App = require("../src/App");
 
 describe("입력값 유효 체크 테스트", () => {
+  // TODO: 게임 종료 후 입력값 유효 테스트 추가하기
   test("입력값이 유효한 값인지", () => {
     const app = new App();
     const testWordsForInvalid = ["1234", "122", "12d"];
