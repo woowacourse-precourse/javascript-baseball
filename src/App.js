f4822364c3d901220ccfb96ce9358696785ea669
evencoding <evencoding@gmail.com>
2022-11-03 15:46:17 +0900

    feat: 게임 시작 메시지 출력

--- src/App.js
+++ src/App.js
@@ -6,11 +6,15 @@ class App {
     this.userInputNumbers;
   }
 
-  offerComputerRandomNumbers = () => {
+  offerComputerRandomNumbers() {
     this.computerNumbers = Random.pickUniqueNumbersInRange(1, 9, 3);
-  };
-
-  play() {}
+  }
+  play() {
+    this.gameStart();
+  }
+  gameStart() {
+    Console.print('숫자 야구 게임을 시작합니다.');
+  }
 }
 
 const app = new App();
