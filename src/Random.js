4fa33235985ed291c8e5578b45efb6ebc715c915
khj0426 <706shin1728@naver.com>
2022-11-03 15:38:10 +0900

    refactor:랜덤 숫자 3자리 만드는 기능

--- src/Random.js
+++ src/Random.js
@@ -1,38 +1,26 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 const FirstEnd = 1;
 const LastEnd = 9;
+const Range = 3;
 
 /*
-랜덤한 숫자 3개가 들어가는 리스트
+1부터 9까지 3개를 겹치지 않게 리턴(배열 형태)
 */
-const RandomNumberList = [];
+const Number = MissionUtils.Random.pickUniqueNumbersInRange(
+  FirstEnd,
+  LastEnd,
+  Range
+);
 
 /*
-랜덤한 숫자3개까지만 허용
+배열 -> 문자열로 바꿔서 리턴
 */
-
-function OutRange(List) {
-  if (List.length < 3) {
-    return true;
-  }
-  return false;
+function ArrayToString(number) {
+  let Random = '';
+  number.forEach((EachNumber) => {
+    Random += EachNumber.toString();
+  });
+  return Random;
 }
 
-/*
-독립적인 3개의 숫자리턴
-*/
-
-function CreateRandomNumber() {
-  while (OutRange(RandomNumberList)) {
-    const RandomNumber = MissionUtils.Random.pickNumberInRange(
-      FirstEnd,
-      LastEnd
-    );
-    if (!RandomNumberList.includes(RandomNumber)) {
-      RandomNumberList.push(RandomNumber);
-    }
-  }
-
-  return RandomNumberList;
-}
-module.exports = CreateRandomNumber();
+module.exports = ArrayToString(Number);
