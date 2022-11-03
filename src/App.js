49f19e303f2127d443b30b289d28ddbee6457454
dev-redo <mis05041@naver.com>
2022-11-04 02:06:41 +0900

    Feat: 유저에게서 입력값을 받는 로직 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 const { Console, Random } = require('@woowacourse/mission-utils');
 const Computer = require('./Computer');
+const User = require('./User');
 
 const NUMBER_LIMIT = 3;
 const INIT_STATE = {
@@ -14,7 +15,7 @@ class App {
     this.state = INIT_STATE;
 
     this.computer = new Computer(NUMBER_LIMIT);
-    this.user;
+    this.user = new User();
     this.checkValid;
   }
 
@@ -24,10 +25,19 @@ class App {
     this.computerNum = this.computer.makeNumbers();
     Console.print(this.computerNum);
 
-    // 2. 현재 컴퓨터 숫자와 유저의 숫자가 동일한지 판별하기
-    // 3. 동일하지 않다면 -> match
-
     // TODO: 유저의 입력값과 컴퓨터의 숫자가 동일 할 때까지 MATCH
+    // 2. 현재 컴퓨터 숫자와 유저의 숫자가 동일한지 판별하기
+    let isComputerNumAndUserNumEqual = this.computerNum === this.userInput;
+    // 3. 동일해질 때까지 Match -> match
+    while (isComputerNumAndUserNumEqual === false) {
+      // Match flow
+      // 1. 유저가 숫자를 입력한다.
+      // 2. 해당 숫자가 유효한지 판단한다.
+      // 3. 만약 유효하다면 해당 숫자에 대한 문구(ex. 1볼)를 띄운다
+      // 4. 유저의 입력값과 컴퓨터 숫자와 동일한지 여부를 업데이트한다.
+      const userInput = this.user.getInputValue();
+    }
+    // 반복문 탈출 후(= Match 종료) 게임을 다시 할 것 인지 여부를 묻는다.
   }
 
   play() {
---
+++ src/User.js
@@ -0,0 +1,20 @@
+const { Console } = require('@woowacourse/mission-utils');
+
+class User {
+  constructor() {
+    this.inputValue = '';
+  }
+
+  getInputValue() {
+    this.setInputValue();
+    return this.inputValue;
+  }
+
+  setInputValue() {
+    Console.readLine('숫자를 입력해주세요 : ', inputValue => {
+      this.inputValue = inputValue;
+    });
+  }
+}
+
+module.exports = User;
