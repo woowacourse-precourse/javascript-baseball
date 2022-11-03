fd0a3ea6d7e5c35cec6057b2767a0583b0f40f0b
woong <inwoong100@gmail.com>
2022-11-03 22:17:21 +0900

    feat(compareUserAndComputerNumber): startApp 내의 user number와 computer number를 비교하는 기능 분리

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ const MissionUtils = require('@woowacourse/mission-utils');
 class App {
   constructor() {
     this.answerMap = new Map();
-
     this.computerNumberArray = [];
     while (this.computerNumberArray.length < 3) {
       const number = MissionUtils.Random.pickNumberInRange(1, 9);
@@ -13,14 +12,50 @@ class App {
     }
     this.userNumberArray = [];
   }
+
   initAnswerMap() {
     const map = new Map();
-
     map.set('strike', 0);
     map.set('ball', 0);
 
     this.answerMap = map;
   }
+
+  compareUserAndComputerNumber() {
+    console.log(this.userNumberArray, this.computerNumberArray);
+    this.userNumberArray.forEach((userNumber, userNumberArrayIndex) => {
+      const index = this.computerNumberArray.indexOf(userNumber);
+      if (index === userNumberArrayIndex) {
+        this.answerMap.set('strike', this.answerMap.get('strike') + 1);
+      } else if (index > 0) {
+        this.answerMap.set('ball', this.answerMap.get('ball') + 1);
+      }
+    });
+    const strike = this.answerMap.get('strike');
+    const ball = this.answerMap.get('ball');
+    if (strike === 0 && ball === 0) {
+      MissionUtils.Console.print('낫싱');
+    } else if (ball === 0) {
+      MissionUtils.Console.print(`${strike} 스트라이크`);
+      if (strike === 3) {
+        MissionUtils.Console.print('3개의 숫자를 모두 맞히셨습니다! 게임종료');
+        MissionUtils.Console.readLine(
+          '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요',
+          input => {
+            if (input === 1) {
+              return;
+            }
+          },
+        );
+      }
+    } else if (strike === 0) {
+      MissionUtils.Console.print(`${ball} 볼`);
+    } else {
+      MissionUtils.Console.print(`${ball} 볼 ${strike} 스트라이크`);
+    }
+    this.startApp();
+  }
+
   startApp() {
     this.initAnswerMap();
     MissionUtils.Console.readLine('숫자를 입력해주세요 : ', input => {
@@ -30,41 +65,8 @@ class App {
           .split('')
           .map(i => +i),
       ];
-      console.log(this.userNumberArray, this.computerNumberArray);
-      this.userNumberArray.forEach((userNumber, userNumberArrayIndex) => {
-        const index = this.computerNumberArray.indexOf(userNumber);
-        if (index === userNumberArrayIndex) {
-          this.answerMap.set('strike', this.answerMap.get('strike') + 1);
-        } else if (index > 0) {
-          this.answerMap.set('ball', this.answerMap.get('ball') + 1);
-        }
-      });
-      const strike = this.answerMap.get('strike');
-      const ball = this.answerMap.get('ball');
-      if (strike === 0 && ball === 0) {
-        MissionUtils.Console.print('낫싱');
-      } else if (ball === 0) {
-        MissionUtils.Console.print(`${strike} 스트라이크`);
-        if (strike === 3) {
-          MissionUtils.Console.print(
-            '3개의 숫자를 모두 맞히셨습니다! 게임종료',
-          );
-          MissionUtils.Console.readLine(
-            '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요',
-            input => {
-              if (input === 1) {
-                return;
-              }
-            },
-          );
-        }
-      } else if (strike === 0) {
-        MissionUtils.Console.print(`${ball} 볼`);
-      } else {
-        MissionUtils.Console.print(`${ball} 볼 ${strike} 스트라이크`);
-      }
-      this.initAnswerMap();
-      this.startApp();
+
+      this.compareUserAndComputerNumber();
     });
   }
 
