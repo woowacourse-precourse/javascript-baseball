4cde11257f1e0a578e0297c630c26ff3f801d51d
jeonjeunghoon <jeonjeunghoon@gmail.com>
2022-11-03 23:45:09 +0900

    feat: 정답 값 관리 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import MissionUtils from '@woowacourse/mission-utils';
+import Computer from './Computer.js';
 
-const MESSAGE = {
+const APP_MESSAGE = {
   hello: '숫자 야구 게임을 시작합니다.',
   bye: '숫자 야구 게임을 종료합니다.',
 };
 
 class App {
   constructor() {
-    MissionUtils.Console.print(MESSAGE.hello);
+    MissionUtils.Console.print(APP_MESSAGE.hello);
   }
 
-  play() {}
+  play() {
+    this.Computer = new Computer();
+  }
 }
 
 const app = new App();
