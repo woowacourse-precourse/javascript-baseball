6b3a8901bf123d846e662cae29badac5b9b2bedb
jun <ggprgrkjh@naver.com>
2022-11-04 02:26:38 +0900

    feat: 시작 메세지 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
-class App {
-  play() {}
+// 기능요구사항
+
+// - [] 시작 메세지 출력.
+// - [] 랜덤수를 생성한다.
+// - [] 숫자를 입력해주세요 메세지 출력.
+// - [] 숫자를 입력받는다.
+// - [] 스트라이크인지 판별한다.
+// - [] 볼인지 판별한다.
+// - [] 낫싱인지 판별한다.
+// - [] 스트라이크 볼 낫싱을 출력한다.
+// - [] 세게의 숫자가 모두 맞으면 종료한다.
+// - [] 반복한다.
+import MissionUtils from "@woowacourse/mission-utils";
+
+function App() {
+  this.play = () => {
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+  };
+
+  this.init = () => {
+    this.play();
+  };
 }
 
-module.exports = App;
+const app = new App();
+
+app.init();
+
+//module.exports = App;
