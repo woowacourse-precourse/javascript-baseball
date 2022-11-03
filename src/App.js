761356d93a7a90244230c2bb09b6097db6926b2b
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2022-11-03 20:51:07 +0900

    refactor: 입력값과 정답값의 값 동일한지 확인하는 불필요한 메소드 삭제 및 이름 수정

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ class App {
     if (!this.isBadInput(input)) {
       this.input = input;
     }
-    this.compareInputToAnswer();
+    this.setHint();
   }
 
   setAnswer() {
@@ -71,20 +71,16 @@ class App {
   }
 
   success() {
-    this.print("3스트라이크");
     this.print(
       "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
     );
   }
 
-  compareInputToAnswer() {
-    if (JSON.stringify(this.input) === JSON.stringify(this.answer)) {
+  printHint() {
+    if (this.hint["strike"] === 3) {
+      this.print("3스트라이크");
       return this.success();
     }
-    this.makeHint();
-  }
-
-  printHint() {
     if (this.hint["nothing"] === 3) return this.print("낫싱");
     if (this.hint["strike"] === 0) return this.print(`${this.hint["ball"]}볼`);
     if (this.hint["ball"] === 0)
