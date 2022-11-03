bbe0d94fec46be7bfbf97dac1a267691940ef24f
Quartz <skyu.dev@gmail.com>
2022-11-04 00:19:34 +0900

    feat: 입력기능 A: 사용자가 입력한 3자리 숫자 가져오기

--- src/App.js
+++ src/App.js
@@ -6,9 +6,19 @@ class App {
   pickComputerNum() {
     return MissionUtils.Random.pickNumberInRange(1, 9);
   }
+  getUserNum() {
+    let userNum;
+    MissionUtils.Console.readLine("숫자를 입력해주세요 :", (answer) => {
+      userNum = answer;
+      this.print(`숫자를 입력해주세요 : ${answer}`);
+    });
+    return userNum;
+  }
   play() {
     this.print("숫자 야구 게임을 시작합니다.");
     const computerNum = this.pickComputerNum();
+    const userNum = this.getUserNum();
+    console.log(userNum);
   }
 }
 
