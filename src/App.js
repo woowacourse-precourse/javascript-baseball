9d9e7dc85e8deb9c6568b41a37260653d7714a7b
dragonfruitlemonade <tpghks9245@gmail.com>
2022-11-03 22:50:35 +0900

    feat: 컴퓨터의 임의의 수 정하기

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 const MissionUtils = require("@woowacourse/mission-utils");
-const CONSOLE = MissionUtils.Console;
+const Console = MissionUtils.Console;
+const Random = MissionUtils.Random;
 
 class App {
   play() {
     gameStartMessage();
+    computersNumber();
+
+    // playerNumber();
   }
 }
 
 function gameStartMessage() {
-  CONSOLE.print("숫자 야구 게임을 시작합니다.");
+  Console.print("숫자 야구 게임을 시작합니다.");
 }
+
+function computersNumber() {
+  let correctAnswer = new Set();
+  while (correctAnswer.size < 3) {
+    correctAnswer.add(Random.pickNumberInRange(1, 9));
+  }
+  correctAnswer = Array.from(correctAnswer);
+  // Console.print(correctAnswer);
+  return correctAnswer;
+}
+
+// function playerNumber() {
+//   Console.readLine('숫자를 입력해주세요 : ', (number) => {
+//     // console.log(`닉네임: ${answer}`);
+//   });
+// }
 
 const app = new App();
 app.play();
