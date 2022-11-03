6cc58bfaec201faa163e72db549132e0b5b92797
jungsmins1 <jungsmins00@gmail.com>
2022-11-03 18:07:10 +0900

    feat: 상대방(컴퓨터)이 3자리의 수(정답)를 만드는 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
+
+  constructor() {
+    this.answer = []
+  }
+
   play() {}
+
+  createAnswer() {
+    while (this.answer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!this.answer.includes(number)) {
+        this.answer.push(number);
+      }
+    }
+  }
 }
+
+const baseBallGame = new App;
+baseBallGame.play();
 
 module.exports = App;
