4c94981072d59daa242114a694f63cfcf25228b2
BBIYAC <kiy7833@gmail.com>
2022-11-03 23:12:20 +0900

    test(UnitTest): fix two test
    - 숫자 랜덤 생성 테스트 리턴값 변수에 담아서 사용
    - 시작 문구 출력 테스트 - jest.spyOn 사용

--- __tests__/UnitTest.js
+++ __tests__/UnitTest.js
@@ -1,4 +1,5 @@
 const App = require("../src/App");
+const MissionUtils = require("@woowacourse/mission-utils");
 
 expect.extend({
   toBeDistinct(received) {
@@ -17,18 +18,30 @@ expect.extend({
     }
   },
 });
-
-describe("숫자 야구 게임", () => {
-  test("숫자 랜덤 생성 테스트", () => {
-    const app = new App();
-    
-    expect(app.getThreeRandom()).toMatch(/[1-9]{3}/g); // 1-9 범위의 세 자리 숫자인지
-    expect(app.getThreeRandom()).toBeDistinct();
-  });
   
-  test("시작 문구 출력 테스트", () => {
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(MissionUtils.Console, "print");
+  logSpy.mockClear();
+  return logSpy;
+};
+
+describe("숫자 랜덤 생성 테스트", () => {
+  test("case1", () => {
     const app = new App();
-    
-    expect(app.printStart()).toBeUndefined();
+    const randoms = app.getThreeRandom()
+
+    expect(randoms).toMatch(/[1-9]{3}/g); // 1-9 범위의 세 자리 숫자인지
+    expect(randoms).toBeDistinct();
+  });
+});
+
+describe("시작 문구 출력 테스트", () => {
+  test("case1", () => {
+    const logSpy = getLogSpy();
+
+    const app = new App();
+    app.printStart();
+      
+    expect(logSpy).toHaveBeenCalled();
   });
 });
