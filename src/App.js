00abb1235bd4f240b17f12b6e8e73d447e33b99c
hyunjin <leehj0110@kakao.com>
2022-11-03 21:25:59 +0900

    refactor: 숫자 입력문구 리팩터링

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-const { Console, Random } = require("@woowacourse/mission-utils");
-
+const { Console } = require("@woowacourse/mission-utils");
 class App {
   START = "숫자 야구 게임을 시작합니다.";
   REQUEST_NUMBER = "숫자를 입력해주세요 : ";
   END = "게임 종료";
 
+  constructor() {}
+
   print(message, close = false) {
     Console.print(message);
-
-    if (close) Console.close();
   }
 
   play() {
     this.print(this.START, true);
-    this.print(this.REQUEST_NUMBER, false);
+
+    Console.readLine(this.REQUEST_NUMBER, (input) => {
+      console.log(input, "?");
+      Console.close();
+    });
   }
 }
 
-const app = new App();
-app.play();
+function startApp() {
+  const app = new App();
+  app.play();
+}
+
+startApp();
 
 module.exports = App;
