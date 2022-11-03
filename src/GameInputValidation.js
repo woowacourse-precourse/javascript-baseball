589704510c65be163fe29108963282ce6893b1d4
d0422 <rlfehd2013@naver.com>
2022-11-03 21:03:08 +0900

    Style:변수명 camelCase로 변경

--- src/GameInputValidation.js
+++ src/GameInputValidation.js
@@ -1,12 +1,12 @@
 module.exports = class GameInputValidation {
-  constructor(GameInput) {
-    this.GameInput = GameInput;
+  constructor(gameInput) {
+    this.gameInput = gameInput;
   }
   validation() {
     this.checkLength();
   }
   checkLength() {
-    if (this.GameInput.length != 3) {
+    if (this.gameInput.length != 3) {
       throw new Error("입력은 3자리만 가능합니다.");
     }
   }
