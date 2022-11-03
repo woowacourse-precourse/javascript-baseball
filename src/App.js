666d40eb6c021b118c39c29ca105bc9ac1074f5c
BB-choi <choibobae0809@gmail.com>
2022-11-03 21:14:33 +0900

    refactor: 게임시작 문구 출력 메서드 리팩토링

--- src/App.js
+++ src/App.js
@@ -6,14 +6,15 @@ class App {
     START: "숫자 야구 게임을 시작합니다.",
     END: "게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
     INSERT_NUMBER: "숫자를 입력해주세요",
-    CORRECT(count) {
-      return `${count}개의 숫자를 모두 맞히셨습니다!`;
+    NOTHING: "낫싱",
+    CORRECT(num) {
+      return `${num}개의 숫자를 모두 맞히셨습니다!`;
     },
-    BALL(count) {
-      return `${count}볼`;
+    BALL(num) {
+      return `${num}볼`;
     },
-    STRIKE(count) {
-      return `${count}스트라이크`;
+    STRIKE(num) {
+      return `${num}스트라이크`;
     },
   };
 
@@ -21,9 +22,16 @@ class App {
     return Console.print(msg);
   }
 
-  play() {
+  startGame() {
     this.printMessage(this.MESSAGES.START);
   }
+
+  play() {
+    this.startGame();
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
