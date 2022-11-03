9b1b9fe3f806592667aded9cb8d65cc37c844d14
leeuihyun <meta212121@naver.com>
2022-11-04 00:43:45 +0900

    feat : 게임 시작 멘트 출력 메서드

---
+++ src/GameInitMessage.js
@@ -0,0 +1,12 @@
+const Mission = require('./Mission');
+
+class GameInitMessage extends Mission {
+  constructor() {
+    super();
+  }
+  initPrint() {
+    this.mission.Console.print('숫자 야구 게임을 시작합니다.');
+  }
+}
+
+module.exports = GameInitMessage;
