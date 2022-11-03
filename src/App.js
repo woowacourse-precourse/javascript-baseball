28f29f0903b24684ad2348a94dd84b670114486b
nsyeon <lovepny@gmail.com>
2022-11-03 13:16:54 +0900

    feat: 랜덤 함수를 통해 각 자리수를 1 ~ 9로 세팅

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+//indent(인덴트, 들여쓰기) depth를 3이 넘지 않도록 구현한다. 2까지만 허용
+
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  play() {
+    computer = computer_randomnumber();
+
+  }
+}
+
+function computer_randomnumber(){
+  const COMPUTER_NUMBER = [];
+  while(COMPUTER_NUMBER.length < 3) {
+    //컴퓨터 숫자 범위 지정 (1 ~ 9)
+    //MissionUtils.Random.pickNumberInRange 호출 할 때마다 ApplicationTest.js에서 randoms 배열의 값 하나씩 가져오기
+    const COMPUTER_RANDOMNUMBER = MissionUtils.Random.pickNumberInRange(1, 9);
+    COMPUTER_NUMBER.push(COMPUTER_RANDOMNUMBER);
+  }
 }
 
 module.exports = App;
