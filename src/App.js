3cd01b845a9b264ba344bed35980a2306c5d097f
insu12021202 <insu12021202@gmail.com>
2022-11-03 15:21:20 +0900

    feat:init function

--- src/App.js
+++ src/App.js
@@ -2,9 +2,30 @@ const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
   play() {
-    console.log(MissionUtils.Random.pickNumberInList([1, 2, 3]));
+    console.log(init());
   }
 }
+
+const init = () => {
+  let answers = [];
+  let numberList = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  for (let i = 0; i < 3; i++) {
+    let pickNumber = MissionUtils.Random.pickNumberInList(numberList);
+    answers.push(pickNumber);
+    numberList = deleteItemInArr(numberList, pickNumber);
+  }
+  return answers;
+};
+
+const deleteItemInArr = (arr, item) => {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === item) {
+      arr.splice(i, 1);
+      i--;
+    }
+  }
+  return arr;
+};
 
 const app = new App();
 app.play();
