156b8daef971e3e64a07583a195e47ff85e6a07a
Dean <yjj052800@gmail.com>
2022-11-03 23:50:09 +0900

    feat: 상대방 숫자 배열 만들기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  makeComputerArr() {
+    this.computer = [];
+    while (this.computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!this.computer.includes(number)) {
+        this.computer.push(number);
+      }
+    }
+    return this.computer;
+  }
+
+  play() {
+    return this.makeComputerArr();
+  }
 }
 
+const app = new App();
+console.log(app.play());
 module.exports = App;
