ba10060d3b26cab0d43dd4ade3c0c9ab39e4fce7
Tory <88531407+1two13@users.noreply.github.com>
2022-11-03 20:55:31 +0900

    refactor(game): countStrikeBallNothing 함수 실행

--- src/game.js
+++ src/game.js
@@ -1,12 +1,17 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 const pickedNumberByComputer = require("./pickedNumberByComputer");
 const enterNumber = require("./enterNumber");
+const countStrikeBallNothing = require("./countStrikeBallNothing");
 
 async function game() {
   const COMPUTER = pickedNumberByComputer();
   let threeStrike = false;
   while (!threeStrike) {
     let pickedNumberByUser = await enterNumber("숫자를 입력해주세요 : ");
+    let [strike, ball, nothing] = countStrikeBallNothing(
+      pickedNumberByUser,
+      COMPUTER
+    );
   }
   MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
 }
