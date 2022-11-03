394797012c9b430d5ded3d50efe82f6f1ec7ca45
cho seonghoon <chsida655@gmail.com>
2022-11-03 13:26:16 +0900

    feat: 랜덤한 세 자리 배열로 생성 함수

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
+// MissionUtils.Console.print(MissionUtils.Random.pickNumberInList([1, 2, 3]));
+// MissionUtils.Console.close();
 class App {
-  play() {}
+  createRandomNumber() {
+    this.randomNumber = [...Array(3)].map((v) =>
+      MissionUtils.Random.pickNumberInRange(1, 9)
+    );
+  }
+  play() {
+    MissionUtils.Console.print(this.randomNumber);
+  }
 }
+
+let app1 = new App();
+app1.createRandomNumber();
+app1.play();
+
+MissionUtils.Console.close();
 
 module.exports = App;
