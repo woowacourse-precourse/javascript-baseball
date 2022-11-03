3c1bca3d3f4425e94bd46d02670e012bd5fcfe3f
woong <inwoong100@gmail.com>
2022-11-03 22:10:47 +0900

    feat(startApp): App Class 내 startApp method 구현
    App 이 시작해서 askRestartApp의 메소드가 실행되어 App이 시작 혹은 끝이 날 때까지 startApp이 담당 하도록 구현
    
    StartApp내 많은 기능들을 각각 하나의 메소드로 분리 필요

--- src/App.js
+++ src/App.js
@@ -1,5 +1,81 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+
 class App {
-  play() {}
+  constructor() {
+    this.answerMap = new Map();
+
+    this.computerNumberArray = [];
+    while (this.computerNumberArray.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!this.computerNumberArray.includes(number)) {
+        this.computerNumberArray.push(number);
+      }
+    }
+    this.userNumberArray = [];
+  }
+  initAnswerMap() {
+    const map = new Map();
+
+    map.set('strike', 0);
+    map.set('ball', 0);
+
+    this.answerMap = map;
+  }
+  startApp() {
+    this.initAnswerMap();
+    MissionUtils.Console.readLine('숫자를 입력해주세요 : ', input => {
+      this.userNumberArray = [
+        ...input
+          .toString()
+          .split('')
+          .map(i => +i),
+      ];
+      console.log(this.userNumberArray, this.computerNumberArray);
+      this.userNumberArray.forEach((userNumber, userNumberArrayIndex) => {
+        const index = this.computerNumberArray.indexOf(userNumber);
+        if (index === userNumberArrayIndex) {
+          this.answerMap.set('strike', this.answerMap.get('strike') + 1);
+        } else if (index > 0) {
+          this.answerMap.set('ball', this.answerMap.get('ball') + 1);
+        }
+      });
+      const strike = this.answerMap.get('strike');
+      const ball = this.answerMap.get('ball');
+      if (strike === 0 && ball === 0) {
+        MissionUtils.Console.print('낫싱');
+      } else if (ball === 0) {
+        MissionUtils.Console.print(`${strike} 스트라이크`);
+        if (strike === 3) {
+          MissionUtils.Console.print(
+            '3개의 숫자를 모두 맞히셨습니다! 게임종료',
+          );
+          MissionUtils.Console.readLine(
+            '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요',
+            input => {
+              if (input === 1) {
+                return;
+              }
+            },
+          );
+        }
+      } else if (strike === 0) {
+        MissionUtils.Console.print(`${ball} 볼`);
+      } else {
+        MissionUtils.Console.print(`${ball} 볼 ${strike} 스트라이크`);
+      }
+      this.initAnswerMap();
+      this.startApp();
+    });
+  }
+
+  play() {
+    MissionUtils.Console.print('숫자 야구게임을 시작합니다.');
+    this.startApp();
+  }
 }
+
+const app = new App();
+
+app.play();
 
 module.exports = App;
