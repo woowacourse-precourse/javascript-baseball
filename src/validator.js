7137a75acf1c708cde1cd931fef8e851e61fc63a
Cobocho <rlaalsrb1111@naver.com>
2022-11-04 05:49:28 +0900

    feat: 사용자 입력값 3글자 판단 구현

--- src/validator.js
+++ src/validator.js
@@ -1,9 +1,15 @@
 const constants = require("./data/constants.js");
 
+function isNotThreeLength(value) {
+  if(value.length !== 3) return true;
+  return false;
+}
+
 
 function isVaild(value) {
   console.log(value);
   if(value.length === 0) throw new Error(constants.MESSAGE.IS_BLANK);
+  if(isNotThreeLength(value)) throw new Error(constants.MESSAGE.NOT_THREE_LENGTH);
 }
 
 exports.isVaild = isVaild;
