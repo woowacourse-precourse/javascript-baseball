f095efbcab4548e291c8e7a73c9082dfbee1ef05
lsy20140 <lsy20140@naver.com>
2022-11-04 06:19:49 +0900

    feat: 컴퓨터가 3자리 숫자 랜덤으로 생성하는 computerInput 메소드 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
-class App {
-  play() {}
+class BaseballGame {
+  computerInput(){
+    const MissionUtils = require("@woowacourse/mission-utils");
+
+    let computerInput = [];
+    computerInput = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
+
+    return computerInput;
+  }
+
+
+  play(){
+    
+  }
 }
 
-module.exports = App;
+module.exports = BaseballGame;
