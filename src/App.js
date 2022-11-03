63029e0605e2e7b0413958fd79920e35a3acfd4d
BBIYAC <kiy7833@gmail.com>
2022-11-03 22:22:17 +0900

    feat(inputUniqueThreeNumber): input unique three numbers function in App.js

--- src/App.js
+++ src/App.js
@@ -17,6 +17,12 @@ class App {
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
   };
 
+  inputUniqueThreeNumber() {
+    MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (numbers) => {
+      // 입력한 숫자와 정답의 비교 결과 출력
+    });
+  }
+
   play() {}
 }
 
