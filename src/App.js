a3104ecdb3c555344937a0330927191419ecb038
BullWooStar <ccatal66@naver.com>
2022-11-03 21:48:27 +0900

    feat: 숫자입력 완성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
-class App {
-  play() {}
+const MissionUtils = require("@woowacourse/mission-utils");
+
+let CORRECT_NUMBER = [];
+
+function makeCorrectNumber() {
+  CORRECT_NUMBER = MissionUtils.Random.pickUniqueNumbersInRange(1, 10, 3);
 }
 
+let ANSWER_NUMBER = [];
+function putNumber() {
+  MissionUtils.Console.readLine("숫자를 입력해주세요 :", (answer) => {
+    ANSWER_NUMBER = answer.split("").map(Number);
+    console.log(ANSWER_NUMBER);
+  });
+}
+
+class App {
+  play() {
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    makeCorrectNumber();
+    putNumber();
+  }
+}
+
+const app = new App();
+app.play();
+
 module.exports = App;
+
+// node src/App.js
