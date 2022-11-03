594ec749d61e002c53408a3d214fd5af97013436
eunsung <esk114477@gmail.com>
2022-11-03 12:22:40 +0900

    feat : strike, ball 개수 확인

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 const {Random , Console} = require("@woowacourse/mission-utils");
-
-class App {
-  play() {}
-}
-
+// class App {
+//   play() {}
+// }
 console.log('숫자 야구 게임을 시작합니다.');
 
 function setComputerNumber() {
@@ -24,12 +22,39 @@ function setUserNumber(computer) {
   let ball = 0;
   Console.readLine('숫자 3자리를 입력해주세요 : ', (num) => {
     input = num.toString().split("").map((str) => Number(str));
-    Console.close();
+    for(let i = 0; i < input.length; i++){
+      if(input[i] == computer[i]){
+        strike++;
+      }
+      else if(input.includes(computer[i])){
+        ball++;
+      }
+    }
+    if(strike > 0 && ball > 0){
+      console.log(`${strike} 스트라이크 ${ball} 볼`);
+      Console.close();
+      return false;
+    } else if(strike == 0 && ball > 0){
+      console.log(`${ball} 볼`);
+      Console.close();
+      return false;
+    } else if(strike > 0 && ball == 0){
+      console.log(`${strike} 스트라이크`);
+      Console.close();
+      return false;
+    }  else if(strike == 3) {
+      console.log('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+      Console.close();
+      return true;
+    } else {
+      console.log('낫싱');
+      Console.close();
+      return false;
+    }
   })
-  return input;
 }
 
 let computer = setComputerNumber();
 let user = setUserNumber(computer);
 
-module.exports = App;
+// module.exports = App;
