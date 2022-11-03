c92338586c32e55f2caac1f41af80cbd28c75968
kangyeongmin <kym6050@hanyang.ac.kr>
2022-11-03 14:36:27 +0900

    feat: 게임을 시작하는 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const baseballGameCycle = require("./");
+const getResult = require("./getResult");
+const createRandomNumbers = require("./createRandomNumbers");
+
 class App {
-  play() {}
+  play() {
+    let answer;
+    const randomNumbers = createRandomNumbers();
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+
+    baseballGameCycle(randomNumbers);
+  }
 }
 
 module.exports = App;
