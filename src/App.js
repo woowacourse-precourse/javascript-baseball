e0fde1f466581a8e8458670d99a165f609c3ef01
하송희 <99heesong@naver.com>
2022-11-03 21:34:56 +0900

    기능추가: 사용자에게 숫자 3개 입력받기

--- src/App.js
+++ src/App.js
@@ -4,13 +4,22 @@ const Console = MissionUtils.Console;
 class App {
   play() {
     Console.print("숫자 야구 게임을 시작합니다");
+    this.input();
   }
 
   getRandomNumber() {
     MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
   }
+
+  input() {
+    Console.readLine("숫자를 입력해주세요: ", (inputNum) => {
+      console.log(`입력 받은 숫자 : ${inputNum}`);
+    });
+  }
 }
 
 const numberBaseball = new App();
+
+numberBaseball.play();
 
 module.exports = App;
