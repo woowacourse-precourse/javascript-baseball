88ea1bd466774d453638c56afbf28f03530ed83b
이종현 <ijonghyeon@ijonghyeon-ui-MacBookAir.local>
2022-11-03 19:45:16 +0900

    feat : get numbers

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
   play() {
-
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.')
+    MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (number) => {
+      console.log(`numbers : ${number}`)
+    });
   }
 }
 
