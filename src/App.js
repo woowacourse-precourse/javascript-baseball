51dd2dcc874f659be06de2ed16ad9ad3bc2cd566
moonelysian <dandyblack@naver.com>
2022-11-03 10:11:25 +0900

    fix: generateRandomNums 함수 수정
    generateRandomNums에 상수로 랜덤이 고정이었지만 인자로 받아오게 변경
    만약 해당 함수를 다른 조건에 랜덤을 생성해야할 때 재사용 가능

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 const { Random, Console } = require("@woowacourse/mission-utils");
+const { NUMBERS, MESSAGES, RESULT } = require('../src/Constructor')
 class App {
-  generateRandomNums() {
-    const randoms = Random.pickUniqueNumbersInRange(1, 9, 3);
+  generateRandomNums(min, max, length) {
+    const randoms = Random.pickUniqueNumbersInRange(min, max, length);
     return Random.shuffle(randoms);
   }
   
   play() {
+    const computer =  this.generateRandomNums(NUMBERS.RANDOM_MIN, NUMBERS.RANDOM_MAX, NUMBERS.REQUIRED_LENGHT);
   }
 }
 
