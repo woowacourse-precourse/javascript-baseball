672e1d864f26d66a7cc5a766045e9d9371980e0d
SWARVY <swarvy0826@naver.com>
2022-11-03 14:51:12 +0900

    feat : 초안 기능목록에 따른 입력을 받아오는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,38 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  play() {
+    const computer = this.computerInput();
+    
+    let user = this.input();
+    MissionUtils.Console.print(computer);
+    MissionUtils.Console.print(input);
+  }
+
+  computerInput() {
+    const computer = [];
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number))
+        computer.push(number);
+    }
+
+    return computer;
+  }
+
+  playerInput() {
+    let value;
+    MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (number) => {
+      value = number;
+    });
+    MissionUtils.Console.close();
+
+    return value;
+  }
+
+  playBall() {
+
+  }
 }
 
 module.exports = App;
