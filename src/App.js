ed792a62e86860f95e51011d9343cfa9f0148dab
c0c0pang <coc1789@naver.com>
2022-11-03 21:07:07 +0900

    feat: 상대방의 랜덤한 숫자 받아오기 구현.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  play() {
+    ComputerNumber();
+  }
 }
 
+const ComputerNumber = ()=>{
+  const NUMBER = MissionUtils.Random.pickUniqueNumbersInRange(1,9,3);
+  return NUMBER;
+}
 module.exports = App;
