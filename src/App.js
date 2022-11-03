96b6287528aaad6e38b71b5f818f1b904ab416d8
loco9939 <kls9939@naver.com>
2022-11-03 21:52:19 +0900

    feat: create getRandomNums func

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
 class App {
-  play() {}
+  play() {
+    const nums = [1,2,3,4,5,6,7,8,9];
+    console.log(this.getRandomNums(nums,3).length)
+  }
+
+  getRandomNums(nums, count) {
+    let result = [];
+    if (count === 1) return nums.map(num => [num]);
+    
+    nums.forEach((fixed,index,self) => {
+      const rest = [...self.slice(0, index), ...self.slice(index + 1)];
+  
+      const randomNums = this.getRandomNums(rest, count - 1);
+  
+      const attached = randomNums.map(randomNum => [fixed, ...randomNum]);
+  
+      result = [...result, ...attached]
+    });
+    return result;
+  }
 }
 
-module.exports = App;
+const app = new App();
+
+app.play();
+
+// module.exports = App;
