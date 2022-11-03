768b8142e480fb376c4013e20c9e5aa0e3dd038f
BBIYAC <kiy7833@gmail.com>
2022-11-03 18:00:24 +0900

    feat(getThreeRandom): get three random numbers from 1 to 9

--- src/App.js
+++ src/App.js
@@ -1,4 +1,17 @@
 class App {
+  getThreeRandom() {
+    const MissionUtils = require("@woowacourse/mission-utils");
+    const randoms = [];
+
+    while (randoms.length < 3) {
+      const randomNumber = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!randoms.includes(randomNumber)) {
+        randoms.push(randomNumber);
+      }
+    }
+    return randoms.join('');
+  }
+
   play() {}
 }
 
