60e0228bd9e6d35111207af0a94ebbd14a4618a2
Leejin-Yang <ejin1996@gmail.com>
2022-11-03 17:10:44 +0900

    feat: 컴퓨터 숫자 생성 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+const { Random, Console } = require('@woowacourse/mission-utils');
+const { rule } = require('./constants');
+
 class App {
-  play() {}
+  constructor() {
+    this.computerNumbers = [];
+  }
+
+  play() {
+    this.computerNumbers = this.generateComputerNumbers();
+    Console.close();
+  }
+
+  generateComputerNumbers() {
+    const computerNumbers = new Set();
+
+    while (computerNumbers.size < rule.LENGTH) {
+      const number = Random.pickNumberInRange(rule.RANGE_START, rule.RANGE_END);
+      computerNumbers.add(String(number));
+    }
+
+    return [...computerNumbers];
+  }
 }
 
 module.exports = App;
