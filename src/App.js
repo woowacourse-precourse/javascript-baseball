0effdbd075e297bf2ba155ea84630db4a285403e
kyw0716 <kyw0716@naver.com>
2022-11-03 15:59:04 +0900

    feat: 재시작 여부 판별 기능 추가

--- src/App.js
+++ src/App.js
@@ -17,7 +17,18 @@ class App {
       MissionUtils.Console.print(this.hint);
 
       if (this.hint !== "3스트라이크") this.play();
+      else this.recommendRestart();
     });
+  }
+
+  recommendRestart() {
+    MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    MissionUtils.Console.readLine(
+      "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n",
+      (input) => {
+        if (input === "1") this.play();
+      }
+    );
   }
 
   getIsInputValueValid(inputValue) {
