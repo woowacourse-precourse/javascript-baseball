8a72a8907208b808045eddc52f9801f78c615ef2
h2002y <h2002y@naver.com>
2022-11-03 22:18:32 +0900

    feat: Add printGameStart func in PrintMessage.js

---
+++ src/PrintMessage.js
@@ -0,0 +1,7 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
+function printGameStart(){
+    const GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    MissionUtils.Console.print(GAME_START_MESSAGE);
+}
+
