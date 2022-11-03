b0d9d58abdfb79ba373fbfdf1045cf7713f00987
jsh3418 <albert5428@gmail.com>
2022-11-03 16:28:46 +0900

    feat: 숫자를 입력하도록 멘트를 띄우고 답을 받아 변수에 할당

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 const { Console } = require("@woowacourse/mission-utils");
 
 class App {
+  constructor() {
+    this.answer;
+  }
+
   play() {
     Console.print("숫자 야구 게임을 시작합니다.");
+
+    Console.readLine("숫자를 입력해주세요 : ", (answer) => {
+      this.answer = answer;
+    });
   }
 }
 
