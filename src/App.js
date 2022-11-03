fa900679b5704c082825d85ce4bc8bb4a6a45815
cho seonghoon <chsida655@gmail.com>
2022-11-03 13:40:14 +0900

    feat: 사용자의 숫자 입력 받아 number array로 저장

--- src/App.js
+++ src/App.js
@@ -8,15 +8,22 @@ class App {
       MissionUtils.Random.pickNumberInRange(1, 9)
     );
   }
+  getUserInput() {
+    MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (ans) => {
+      this.userInput = ans.split("").map((v) => +v);
+      this.play();
+    });
+  }
   play() {
     MissionUtils.Console.print(this.randomNumber);
+    MissionUtils.Console.print(this.userInput);
   }
 }
 
 let app1 = new App();
 app1.createRandomNumber();
-app1.play();
+app1.getUserInput();
 
-MissionUtils.Console.close();
+// MissionUtils.Console.close();
 
 module.exports = App;
