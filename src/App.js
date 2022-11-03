952a5746dfa48fe0c20cb5e76337a72c21e32b95
Dohyeon Lee <dlehgus1110@naver.com>
2022-11-03 15:33:37 +0900

    feat: 게임 시작 메세지 출력 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const message = require("./constants/message.js");
+
 class App {
+  constructor() {
+    MissionUtils.Console.print(message.GAME_START);
+  }
+
   play() {}
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
