5e1457ad57abc170dc88910f294b66d8546d57a0
Gaeun-Jang <110662057+Gaeun-Jang@users.noreply.github.com>
2022-11-03 15:36:52 +0900

    feat : 서로 다른 임의의 수 3개를 선택 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
+    constructor() {}
+
+    getRandomNumber() {
+        const computer = [];
+        while (computer.length < 3) {
+            const number = MissionUtils.Random.pickNumberInRange(1, 9);
+            if (!computer.includes(number)) {
+                computer.push(number);
+            }
+        }
+    }
+
     play() {}
 }
+
+const app = new App();
 
 module.exports = App;
