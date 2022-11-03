cb313ed7e7c85874db4d6dd75971da980d5a9149
Rki0 <pky00823@gmail.com>
2022-11-03 13:59:57 +0900

    feat: 랜덤 숫자 생성 함수
    컴퓨터가 3개의 랜덤 숫자를 생성하게하는 함수입니다.
    3개의 숫자는 모두 다른 값으로 이루어져있으며, 1 ~ 9 사이의 자연수입니다.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,24 @@ const MissionUtils = require("@woowacourse/mission-utils");
 class App {
   play() {
     this.showStartMessage();
+    this.makeRandomNumber();
   }
 
-  // 게임 시작 시 메세지 출력
   showStartMessage() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
     MissionUtils.Console.close();
+  }
+
+  makeRandomNumber() {
+    this.COMPUTER = [];
+
+    while (this.COMPUTER.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+
+      if (!this.COMPUTER.includes(number)) {
+        this.COMPUTER.push(number);
+      }
+    }
   }
 }
 
