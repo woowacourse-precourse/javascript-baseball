63df24ca2ca107ae9a31c11fc2019932ff89e012
gilpop8663 <wolfye@naver.com>
2022-11-03 21:04:22 +0900

    feat:게임 시작 문구를 출력하는 기능 구현
    MissionUtils 라이브러리에서 제공하는 Console.print를 활용한다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
-class App {
-  play() {}
+const MissionUtils = require("@woowacourse/mission-utils");
+
+function gameStart() {
+  MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
 }
+
+class App {
+  play() {
+    gameStart();
+  }
+}
+
+const app = new App();
+
+app.play();
 
 module.exports = App;
