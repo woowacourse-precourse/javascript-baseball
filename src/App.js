f7fb13a1f86f0eb319481eafa6a810d3e90fc86d
js43o <js43o@naver.com>
2022-11-03 22:05:00 +0900

    feat: add setComputerNumber() and score.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ class App {
     this.isContinued = true;
     this.computer = [];
     this.user = [];
+    this.score = {
+      ball: 0,
+      strike: 0,
+    };
   }
 
   checkDistinct(input) {
@@ -28,13 +32,26 @@ class App {
     MissionUtils.Console.close();
   }
 
+  setComputerNumber() {
+    while (this.computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!this.computer.includes(number)) {
+        this.computer.push(number);
+      }
+    }
+  }
+
   main() {
     this.inputUserNumber();
+    this.setComputerNumber();
   }
 
   play() {
     this.main();
   }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
