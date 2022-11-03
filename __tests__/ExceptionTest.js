acf7c0683bdb6478da2af770975c361f849aab52
turtle601 <turtle601@naver.com>
2022-11-03 14:15:26 +0900

    test(exceptionTest) 세 개의 다른 숫자인지 확인하는 테스트 코드 작성
    Add:
    - case1 : 정상 입력값 '134'
    - case2 : 중복이 있는 입력값 '133'
    - case3 : 중복은 없지만 길이가 3이 아닌 입력값 '1345'

--- __tests__/ExceptionTest.js
+++ __tests__/ExceptionTest.js
@@ -36,3 +36,26 @@ describe("예외 처리 로직 Exception클래스 - 숫자 확인 테스트", ()
     expect(exception.isNumber()).toBeFalsy();
   });
 });
+
+describe("예외 처리 로직 Exception클래스 - 세 개의 다른 숫자들로 이루어져있나요?", () => {
+  test("(정상)입력값 '134'", () => {
+    const input = "134";
+    const exception = new Exception(input);
+
+    expect(exception.is3DifferNumber()).toBeTruthy();
+  });
+
+  test("(중복o)입력값 '133'", () => {
+    const input = "133";
+    const exception = new Exception(input);
+
+    expect(exception.is3DifferNumber()).toBeFalsy();
+  });
+
+  test("(중복x, 길이_3x)입력값 '1345'", () => {
+    const input = "1345";
+    const exception = new Exception(input);
+
+    expect(exception.is3DifferNumber()).toBeFalsy();
+  });
+});
