a33bf030825acffec1721435c82c93e3b9dff49a
eunsung <esk114477@gmail.com>
2022-11-03 12:03:23 +0900

    refactor : setUserNumber 수정

--- src/App.js
+++ src/App.js
@@ -1,31 +1,35 @@
+const {Random , Console} = require("@woowacourse/mission-utils");
+
 class App {
   play() {}
 }
 
+console.log('숫자 야구 게임을 시작합니다.');
+
 function setComputerNumber() {
   let computerNumber = [];
   while (computerNumber.length < 3) {
-    const randomNumber = MissionUtils.Random.pickNumberInRange(1,9);
+    const randomNumber = Random.pickNumberInRange(1,9);
     if(!computerNumber.includes(randomNumber)){
       computerNumber.push(randomNumber);
     }
   }
+  console.log(computerNumber);
   return computerNumber;
 }
 
-function setUserNumber() {
-  let userNumber;
-  const readline = require('readline');
-  const userInput = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+function setUserNumber(computer) {
+  let input;
+  let strike = 0;
+  let ball = 0;
+  Console.readLine('숫자 3자리를 입력해주세요 : ', (num) => {
+    input = num.toString().split("").map((str) => Number(str));
+    Console.close();
   })
-  userInput.question('숫자 3자리를 입력해주세요 : ', num => {
-    userNumber = num;
-    userInput.close();
-  })
-
-  return userNumber;
+  return input;
 }
+
+let computer = setComputerNumber();
+let user = setUserNumber(computer);
 
 module.exports = App;
