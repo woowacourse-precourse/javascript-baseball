b054a91425be660a243ffd3ecb3f24bd1dc5eb19
Suyeon <msjgr2801@gmail.com>
2022-11-03 13:53:35 +0900

    Feat: Implement basic main logic
    - play 함수 실행 시 gameorder (반복)호출
    - gameorder는 메인 로직을 실행하는 함수
    - 컴퓨터 값 생성 ~ 게임 재시작까지의 함수를 호출함으로써 로직 구성

--- src/App.js
+++ src/App.js
@@ -2,35 +2,54 @@ import * as MissionUtils from "@woowacourse/mission-utils";
 
 class App {
   constructor() {
+    this.flag = null;
     this.computer = null;
     this.user = null;
+    this.strike = 0;
+    this.ball = 0;
   }
 
   play() {
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    while (this.flag !== 2) {
+      this.gameOrder();
+    }
+  }
+
+  gameOrder() {
     // 1. computer값을 생성한다
-    // 2. user값을 받아와 검사한다
-    // 3. computer & user 값을 비교한다
+    this.makeRandomNumber();
+    // 정답이 아닌 경우 값을 받아오는 작업을 반복한다.
+    while (this.strike !== 3) {
+      // 2. user값을 받아와 검사한다
+      this.getUserValue();
+      // 3. computer & user 값을 비교한다
+      this.checkUserValue(this.user);
+    }
+    // 4. 게임 재시작
+    this.askRestart();
   }
 
-  makeRandomNumber() {
-
+  askRestart() {
+    MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    MissionUtils.Console.readLine(
+      "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
+      (answer) => {
+        console.log(`숫자입력: ${answer}`);
+        this.flag = answer;
+      }
+    );
   }
 
-  getUserValue() {
+  makeRandomNumber() {}
 
-  }
+  getUserValue() {}
 
-  checkUserValue(value) {
+  checkUserValue(value) {}
 
-  }
+  compareWithUser(computer, user) {}
 
-  compareWithUser(computer, user) {
-
-  }
-
-  exitProgram() {
-
-  }
+  exitProgram() {}
 }
 
 module.exports = App;
