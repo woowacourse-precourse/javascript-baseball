bee008b08f5fd81d4e0c8da692aac28e7fbcf07d
lee-il-woo <empty001430@gmail.com>
2022-11-03 14:00:43 +0900

    Feat: 추가 computerNumbers함수
    컴퓨터의 숫자 배열을 받아오는 함수

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+function computerNumbers(){
+  const COMPUTER_NUMBERS = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
+  return COMPUTER_NUMBERS
+}
+
 class App {
   play() {}
 }
