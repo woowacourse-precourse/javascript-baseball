d05f2980e4e94fb25dc99af59c26d390cb87f39d
Dean <yjj052800@gmail.com>
2022-11-03 23:37:59 +0900

    chore: eslint, prettier 적용

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: ["airbnb-base", "plugin:prettier/recommended"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ],
+    semi: ["error", "always"],
+  },
+  plugins: ["prettier"],
+};
