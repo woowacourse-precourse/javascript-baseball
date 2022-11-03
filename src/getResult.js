0230735a8dd9beff544d2484eafb4ef6ffd5c970
kangyeongmin <kym6050@hanyang.ac.kr>
2022-11-03 14:56:05 +0900

    feat: 입력에서 '볼' 개수를 반환하는 기능

--- src/getResult.js
+++ src/getResult.js
@@ -9,3 +9,15 @@ function checkStrike(solution, answer) {
 
   return numberOfStrike;
 }
+
+function checkBall(solution, answer) {
+  let numberOfBall = 0;
+
+  solution.forEach((number, index) => {
+    if (answer.includes(number) && index != answer.indexOf(number)) {
+      numberOfBall++;
+    }
+  });
+
+  return numberOfBall;
+}
