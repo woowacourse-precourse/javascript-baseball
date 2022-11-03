f61d3b5d2d469296fc726c5f223d4802de37a405
kinoasm <kinoasm@gmail.com>
2022-11-03 20:19:49 +0900

    Feat: 랜덤 숫자 생성
    MissionUtils 내의 pickNumberInRange 이용
    class 내부 변수에 PICKED_NUMBERS 선언

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
+    PICKED_NUMBERS = [];
     play() {
         this.start();
     }
     start() {
         MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+        this.PICKED_NUMBERS = this.getRandomNumber();
+    }
+    getRandomNumber() {
+        const picks = [];
+        for (let i = 0; i < 3; i++) {
+            let now = MissionUtils.Random.pickNumberInRange(1, 9);
+            picks.push(now);
+        }
+        return picks;
     }
 }
 
