2d8a220264584f504277ca3661641d45c6c12053
ctd <qpflapffhs76@gmail.com>
2022-11-03 20:20:29 +0900

    feat: 유저의 입출력을 받는 initUserInputInterface 메서드 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+const { Console } = require('@woowacourse/mission-utils');
+
 class App {
   play() {
     Console.print('숫자 야구 게임을 시작합니다.');
     this.init();
   }
 
-  init() {}
+  initUserInputInterface() {
+    Console.readLine('숫자를 입력해주세요 : ', (input) => {
+      Console.print(input);
+    });
+  }
+
+  init() {
+    this.initUserInputInterface();
+  }
 }
 
 const app = new App();
