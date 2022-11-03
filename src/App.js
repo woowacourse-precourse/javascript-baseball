432db1cacb4d59fb852a61dbecb9118d4ceca4c2
hyunjin <leehj0110@kakao.com>
2022-11-03 22:38:06 +0900

    feat: 사용자 input 받는 부분 promise화

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-const { Console } = require("@woowacourse/mission-utils");
+const { Console, Random } = require("@woowacourse/mission-utils");
 class App {
   START = "숫자 야구 게임을 시작합니다.";
   REQUEST_NUMBER = "숫자를 입력해주세요 : ";
   END = "게임 종료";
   ANSWER = "";
 
-  play() {
+  async play() {
     this.print(this.START, true);
     this.ANSWER = this.makeBaseballGameAnswer();
 
-    Console.readLine(this.REQUEST_NUMBER, (input) => {
-      Console.close();
-    });
+    const input = await this.getPlayerInput();
+    console.log(input);
+    Console.close();
   }
 
   print(message, close = false) {
@@ -19,17 +19,28 @@ class App {
   }
 
   makeBaseballGameAnswer() {
-    return Array.from({ length: 3 }, () => this.getRandomNumber(10)).join("");
+    return Array.from({ length: 3 }, () =>
+      Random.pickNumberInRange(1, 9).toString()
+    ).join("");
   }
 
-  getRandomNumber() {
-    return Math.floor(Math.random() * 9 + 1);
+  // getRandomNumber() {
+  //   const maxNumber = 9;
+  //   return Math.floor(Math.random() * maxNumber + 1);
+  // }
+
+  async getPlayerInput() {
+    return new Promise((resolve) => {
+      Console.readLine(this.REQUEST_NUMBER, (input) => {
+        resolve(input);
+      });
+    });
   }
 }
 
-function startApp() {
+async function startApp() {
   const app = new App();
-  app.play();
+  await app.play();
 }
 
 startApp();
