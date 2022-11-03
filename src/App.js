07cb6a600816a3724fb85ddba164c54a7eb29f80
min <hororonoodle@gmail.com>
2022-11-04 02:24:48 +0900

    feat : [T1-2] 게임 시작 시 문구 출력 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+const GameMessage = require('./Constants/gameMessage');
 class App {
-  play() {}
+  play() {
+    MissionUtils.Console.print(GameMessage.WELCOME_MESSAGE);
+    MissionUtils.Console.readLine(GameMessage.QUESTION_MESSAGE, (userInput) => {
+      return userInput;
+    });
+  }
 }
 
 module.exports = App;
