dfef2955fb51a61b680f5a0f51f626e51f35496e
turtle601 <turtle601@naver.com>
2022-11-03 14:26:41 +0900

    feat(exception) 서로 다른 3개의 숫자인지 확인하는 함수 구현
    Add:
    <Exception클래스>
    - is3DifferNumber() : 서로 다른 3개의 숫자인지 확인

--- src/Exception.js
+++ src/Exception.js
@@ -1,5 +1,8 @@
 class Exception {
   #regexNum = /[1-9]/g;
+  #CONSTANT = Object.freeze({
+    LENGTH: 3,
+  });
 
   constructor(input) {
     this._input = input;
@@ -10,6 +13,10 @@ class Exception {
       !!this._input.match(this.#regexNum) &&
       this._input.match(this.#regexNum).length === this._input.length
     );
+  }
+
+  is3DifferNumber() {
+    return [...new Set(this._input.split(""))].length === this.#CONSTANT.LENGTH;
   }
 }
 
