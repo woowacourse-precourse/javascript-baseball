cdece92ae429591799fccd453a48aa4b7c60468f
olo90632951 <olo90632951@gmail.com>
2022-11-03 13:28:29 +0900

    feat: 사용자가 1을 입력할 때 게임을 재시작하는 기능 추가

--- src/controller/BaseballController.js
+++ src/controller/BaseballController.js
@@ -53,12 +53,17 @@ class BaseballController {
       '3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n',
       (number) => {
         if (number === '1') {
-          console.log(1);
+          this.restartGame();
         } else if (number === '2') {
           closeConsole();
         }
       },
     );
+  }
+
+  restartGame() {
+    this.baseballModel.setComputerValue(this.baseballModel.getRandomNumbers());
+    this.triggerUserInput();
   }
 }
 
