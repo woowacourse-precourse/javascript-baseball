a52172985f6ea86434ce1a63f28f2c8e942193e6
hailey-hy <lillyine.hy@gmail.com>
2022-11-03 17:18:29 +0900

    feat: 2. 정답 숫자 선정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 class App {
-  play() {}
+  play() {
+    // 정답 숫자 선정
+    const computer = [];
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+  }
 }
 
 const app = new App();
