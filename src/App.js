6c99cb5db5ebb351e4af3bf4834af509682e9b1e
AnJaeSeong <dktmzh6@gmail.com>
2022-11-04 03:12:33 +0900

    feat: setComputerNum

--- src/App.js
+++ src/App.js
@@ -2,14 +2,30 @@ const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
   play() {
-    let input = [];
-    let computer = []; 
+    // step1
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+    
+    // step2
+
+    // step3
+
+    // step4
+
+    // step5
+
+    // step6
 
   }
 
   setComputerNum() {
-
+    const computer = [];
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+    return computer;
   }
 
   countStrike() {
