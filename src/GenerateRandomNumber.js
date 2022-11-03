dae8ce79b3c5e4ac4f9d9d375627d4e58a4262e7
h2002y <h2002y@naver.com>
2022-11-03 21:29:55 +0900

    fix: Add const RANDOM_NUM_ARR in GenerateRandomNumber.js

--- src/GenerateRandomNumber.js
+++ src/GenerateRandomNumber.js
@@ -1,7 +1,8 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
 function generateComputerNum() {
-    return MissionUtils.Random.pickUniqueNumbersInRange(1, 10, 3);
+    const RANDOM_NUM_ARR = MissionUtils.Random.pickUniqueNumbersInRange(1, 10, 3);
+    return RANDOM_NUM_ARR;
 }
 
 module.exports = generateComputerNum;
