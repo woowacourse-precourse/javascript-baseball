b6e61fc6170e5860f8346f9e710cc146a59edbea
loopy-lim <loopy@jnu.ac.kr>
2022-11-03 14:03:26 +0900

    feat: 게임이 끝나면 를 출력한다

--- src/Baseball/Baseball.output.js
+++ src/Baseball/Baseball.output.js
@@ -4,6 +4,9 @@ class BaseBasllOutput {
   start() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.\n");
   }
+  end() {
+    MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n");
+  }
 }
 
 export default BaseBasllOutput;
