6bc97446f6ba5a4ce7ff966f6788f1946afb2075
jun <ggprgrkjh@naver.com>
2022-11-04 21:43:50 +0900

    feat: 랜덤 숫자 생성 함수 추가

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 // 기능요구사항
 
 // - [x] 시작 메세지 출력.
-// - [] 랜덤수를 생성한다.
-// - [] 숫자를 입력해주세요 메세지 출력.
-// - [] 숫자를 입력받는다.
+// - [x] 랜덤수를 생성한다.
+// - [x] 숫자를 입력해주세요 메세지 출력.
+// - [x] 숫자를 입력받는다.
 // - [] 스트라이크인지 판별한다.
 // - [] 볼인지 판별한다.
 // - [] 낫싱인지 판별한다.
 // - [] 스트라이크 볼 낫싱을 출력한다.
 // - [] 세게의 숫자가 모두 맞으면 종료한다.
-// - [] 반복한다.
+// - [x] 반복한다.
 import MissionUtils from "@woowacourse/mission-utils";
 
 function App() {
@@ -17,6 +17,7 @@ function App() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
 
     recursiveAsyncReadLine();
+    this.randomNumber = Number(createRandomNumber());
   };
 
   this.init = () => {
@@ -28,6 +29,16 @@ function App() {
       MissionUtils.Console.print(number);
       recursiveAsyncReadLine();
     });
+  };
+
+  const createRandomNumber = () => {
+    let RandomNumber = "";
+    for (let index = 0; index < 3; index++) {
+      RandomNumber += MissionUtils.Random.pickNumberInList([
+        1, 2, 3, 4, 5, 6, 7, 8, 9,
+      ]).toString();
+    }
+    return RandomNumber;
   };
 }
 
