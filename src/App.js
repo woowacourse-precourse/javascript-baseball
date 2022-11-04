d6ebf32e56b0d5845f741e5310fe60adc3bbb2be
jjongs2 <jjongs@hanyang.ac.kr>
2022-11-05 00:26:54 +0900

    feat: 플레이어가 입력한 숫자와 정답을 비교하여 힌트 출력하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ const MissionUtils = require('@woowacourse/mission-utils');
 class App {
   constructor() {
     this.answer = this.getAnswer();
+    this.userInput = this.readInput();
+    this.count = this.getCount(this.answer, this.userInput);
+    MissionUtils.Console.print(this.getHint(this.count));
   }
 
   getAnswer() {
@@ -17,6 +20,51 @@ class App {
     }
 
     return answer;
+  }
+
+  readInput() {
+    let userInput;
+
+    MissionUtils.Console.readLine('숫자를 입력해 주세요 : ', (input) => {
+      userInput = Array.from(input, Number);
+    });
+
+    return userInput;
+  }
+
+  getCount(answer, userInput) {
+    let count = {
+      ball: 0,
+      strike: 0,
+    };
+
+    userInput.forEach((number, index) => {
+      const findIndex = answer.findIndex((element) => element === number);
+
+      if (findIndex === index) {
+        count.strike += 1;
+      } else if (findIndex !== -1) {
+        count.ball += 1;
+      }
+    });
+
+    return count;
+  }
+
+  getHint(count) {
+    let hint = '';
+
+    if (count.ball > 0) {
+      hint = `${count.ball}볼`;
+    }
+    if (count.strike > 0) {
+      hint += ` ${count.strike}스트라이크`;
+    }
+    if (hint === '') {
+      hint = '낫싱';
+    }
+
+    return hint.trimStart();
   }
 
   play() {}
