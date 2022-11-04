8107a25b94409ae57b1faaa68d86ba1a8bfda96b
Sim <zh025700@naver.com>
2022-11-04 17:56:48 +0900

    feat(#App): make answer 3 value

--- src/App.js
+++ src/App.js
@@ -5,23 +5,38 @@ class App {
   userAnswer = 0;
   isRight = false;
   err = false;
+  strike = 0;
+  ball = 0;
+
   constructor() {
-    this.answer = MissionUtils.Random.pickNumberInRange(1, 9);
+    this.answer = this.createAnswer();
     this.isRight = false;
     this.err = false;
+    this.strike = 0;
+    this.ball = 0;
   }
+
+  createAnswer = () => {
+    return [1, 2, 3].map(() => MissionUtils.Random.pickNumberInRange(1, 9));
+  };
+
+  initializer = () => {
+    this.isRight = false;
+    this.err = false;
+    this.strike = 0;
+    this.ball = 0;
+  };
 
   async Game() {
     while (true) {
+      this.initializer();
       await this.doBaseBall();
+
       if (this.err) {
-        console.log("ERR");
-      } else {
-        if (this.isRight) {
-          break;
-        } else {
-          console.log(false);
-        }
+        console.log("err");
+      }
+      if (this.isRight) {
+        break;
       }
     }
   }
@@ -29,15 +44,25 @@ class App {
   async doBaseBall() {
     this.userAnswer = await this.input();
     this.checkBallValidity();
-    if (this.err) {
-      return;
-    } else {
-      [...this.userAnswer].map((e) => {
-        console.log(e);
-      });
+    if (!this.err) {
+      this.judgeResult();
     }
   }
 
+  judgeResult = () => {
+    [...this.userAnswer].map((e, ballIdx) => {
+      this.countStrike(e, ballIdx);
+      this.countBall();
+    });
+  };
+
+  countStrike = (userAnswerBall, ballIdx) => {
+    if ([...this.answer][ballIdx] === userAnswerBall) {
+      console.log("same");
+    }
+  };
+
+  countBall = () => {};
   checkBallValidity = () => {
     if (isNaN(parseInt(this.userAnswer))) {
       this.err = true;
@@ -57,7 +82,7 @@ class App {
 
   play() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
-    this.Game(this);
+    this.Game();
   }
 }
 
@@ -67,5 +92,6 @@ const endGame = () => {
 
 const app = new App();
 // app.play();
-app.checkBallValidity("c13");
+app.createAnswer();
+console.log(app.answer);
 module.exports = App;
