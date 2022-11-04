eb324f3915e86fee5f11c7575299aaa3c607b5d4
YeonsuBaek <soothbugger@gmail.com>
2022-11-04 13:58:51 +0900

    test: 컴퓨터 숫자 랜덤으로 정하기 테스트 완료

--- __tests__/functionsTest.js
+++ __tests__/functionsTest.js
@@ -1,4 +1,12 @@
 const App = require("../src/App");
+const MissionUtils = require("@woowacourse/mission-utils");
+
+const mockRandoms = (numbers) => {
+  MissionUtils.Random.pickNumberInRange = jest.fn();
+  numbers.reduce((acc, number) => {
+    return acc.mockReturnValueOnce(number);
+  }, MissionUtils.Random.pickNumberInRange);
+};
 
 describe("야구 게임 테스트", () => {
   test("시작 메세지 출력", () => {
@@ -9,6 +17,17 @@ describe("야구 게임 테스트", () => {
     app.startGame();
 
     expect(logSpy).toHaveBeenCalledWith('숫자 야구 게임을 시작합니다.');
+  });
+
+  test("컴퓨터 숫자 랜덤", () => {
+    const number = [1, 2, 3];
+    
+    mockRandoms(number);
+
+    const app = new App();
+    const result = app.selectComputer(); 
+
+    expect(result).toEqual("123");
   });
 });
 
