fd6f913724ee04e6f95eecaeafd09cf1c3638e6a
kimeuns <dmstn2006@gmail.com>
2022-11-05 01:35:14 +0900

    feat: 게임 시작 후 숫자 입력

--- src/App.js
+++ src/App.js
@@ -24,7 +24,13 @@ class App {
     this.resultNumbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
   }
 
-  play() {}
+  play() {
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+    MissionUtils.Console.readLine("숫자를 입력해주세요 :", (answer) => {
+      this.checkNumbers(answer);
+    });
+
+  }
 }
 
 module.exports = App;
