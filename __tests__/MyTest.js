461f7f5668d6bee59ce3e6e38022ace86580c1f2
DongjoonShin <ehdwns1516@ajou.ac.kr>
2022-11-04 13:19:18 +0900

    test: 상대방의 숫자와 사용자가 입력한 숫자 비교 기능 테스트 추가

--- __tests__/MyTest.js
+++ __tests__/MyTest.js
@@ -20,4 +20,23 @@ describe('숫자 야구 게임 My Test', () => {
     expect(rand_num2).toHaveLength(3);
     expect(result).toBeFalsy();
   });
+
+  test('상대방의 숫자와 사용자가 입력한 숫자를 비교', () => {
+    const app = new App();
+    const computer = '369';
+    const user_inputs = ['000', '146', '321', '936', '369'];
+    const result = [];
+
+    user_inputs.forEach((input) => {
+      result.push(app.compareNumbers(computer, input));
+    });
+
+    expect(result).toEqual([
+      [0, 0],
+      [1, 0],
+      [0, 1],
+      [3, 0],
+      [0, 3],
+    ]);
+  });
 });
--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 class App {
   play() {}
   makeRandomNumber(len) {}
+  compareNumbers(computer, user) {}
 }
 
 module.exports = App;
