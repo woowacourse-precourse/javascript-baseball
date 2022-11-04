86016114562c14d89c411357943affa614cc5780
Gyeongm1n <affirming_km@naver.com>
2022-11-04 18:58:32 +0900

    feat: 사용자 입력값 예외처리 기능 구현

--- src/App.js
+++ src/App.js
@@ -19,9 +19,33 @@ class App {
     Console.print('숫자 야구 게임을 시작합니다.');
   }
 
+  handleUserInputException(userInput) {
+    const removeNaN = userInput.replace(/[^0-9]/g, '');
+    const removeDuplicates = [...new Set(removeNaN.split(''))];
+
+    if (removeNaN.length !== 3) {
+      throw new Error('3자리 숫자를 입력해주세요');
+    }
+
+    for (const num of removeNaN) {
+      if (Number(num) <= 0) {
+        throw new Error('1에서 9 사이의 숫자를 입력해주세요');
+      }
+    }
+
+    if (removeDuplicates.length !== 3) {
+      throw new Error('각 자리에 중복되지 않은 숫자를 입력해주세요');
+    }
+  }
+
   getUserInput() {
     Console.readLine('숫자를 입력해주세요 : ', (expectedAnswer) => {
-      Console.print(expectedAnswer);
+      try {
+        this.handleUserInputException(expectedAnswer);
+      } catch (e) {
+        Console.print(e.message);
+        Console.close();
+      }
     });
   }
 
