b76037c14a100a34cefb7efe86f5ed4b89128365
loopy-lim <loopy@jnu.ac.kr>
2022-11-04 17:03:42 +0900

    feat: 입력에 대해 ball의 상태를 변경한다

--- src/Baseball/Baseball.computer.js
+++ src/Baseball/Baseball.computer.js
@@ -3,19 +3,29 @@ const BaseballOutput = require("./Baseball.output");
 const BaseballValidator = require("./BaseBall.validator");
 
 class BaseballComputer {
-  baseballNumber = [];
+  baseballNumbers = [];
   baseballOutput = new BaseballOutput();
   baseballValidator = new BaseballValidator();
 
   constructor() {
-    this.baseballNumber = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
+    this.baseballNumbers = MissionUtils.Random.pickUniqueNumbersInRange(
+      1,
+      9,
+      3
+    );
   }
-  getNumber() {
+  getUserNumbers() {
     const userNumbers = this.baseballOutput.getNumber();
     this.baseballValidator.checkNumericNumbers(...userNumbers);
     this.baseballValidator.checkNumbersLength(...userNumbers);
     this.baseballValidator.checkOtherNumbers(...userNumbers);
     return userNumbers;
+  }
+  getBallState(userNumbers) {
+    return this.baseballValidator.checkBallState(
+      this.baseballNumbers,
+      userNumbers
+    );
   }
 }
 
