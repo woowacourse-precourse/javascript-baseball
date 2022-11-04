cb9a35f3927e5c400a873aa2b62038c124c5e5ff
woo-jk <201702042@o.cnu.ac.kr>
2022-11-04 17:55:50 +0900

    test: 뽑은 숫자 중복 테스트 코드 작성

--- __tests__/DrawNumbersTest.js
+++ __tests__/DrawNumbersTest.js
@@ -18,5 +18,11 @@ describe.only("숫자 뽑기 테스트", () => {
     });
   });
 
-  test("각 숫자가 중복되어 있는지 확인", () => {});
+  test("각 숫자가 중복되어 있는지 확인", () => {
+    const app = new App();
+    const threeRandomNumber = app.drawThreeRandomNumbers();
+    const nonduplicatedNumber = [...new Set(threeRandomNumber.split(""))];
+
+    expect(nonduplicatedNumber.length).toEqual(3);
+  });
 });
