1a99edb19eb6e893413a7c71ae022c45aa845c2a
jeonjeunghoon <jeonjeunghoon@gmail.com>
2022-11-04 15:32:20 +0900

    feat: 사용자 입력 값 관리 기능 추가

--- src/Computer.js
+++ src/Computer.js
@@ -2,7 +2,11 @@ import MissionUtils from '@woowacourse/mission-utils';
 
 class Computer {
   constructor() {
-    this._answer = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
+    this.getAnswer();
+  }
+
+  getAnswer() {
+    this.answer = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
   }
 }
 
--- src/User.js
+++ src/User.js
@@ -1,10 +1,44 @@
 import MissionUtils from '@woowacourse/mission-utils';
 
 class User {
-  constructor() {
-    MissionUtils.Console.readLine('숫자를 입력해주세요 : ', number => {
-      this.number = number;
+  waitEnter() {
+    return new Promise(resolve => {
+      MissionUtils.Console.readLine('숫자를 입력해주세요 : ', num => {
+        resolve(num);
+      });
     });
+  }
+
+  async getUserInput() {
+    this.num = await this.waitEnter();
+  }
+
+  convertStringToNum() {
+    this.num = parseInt(this.num, 10);
+  }
+
+  convertNumToArray() {
+    this.numArr = Array.from(String(this.num), Number);
+  }
+
+  checkTruthy() {
+    return Boolean(this.num);
+  }
+
+  checkType() {
+    return typeof this.num === 'number';
+  }
+
+  checkRange() {
+    return this.num >= 123 && this.num <= 987;
+  }
+
+  checkDuplication() {
+    return this.numArr.every((num, i, numArr) => numArr.indexOf(num) === i);
+  }
+
+  checkUserInput() {
+    return this.checkTruthy() && this.checkType() && this.checkRange() && this.checkDuplication();
   }
 }
 
