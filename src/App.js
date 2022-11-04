58622779860dbc218b7326af8b023a66dd260e81
Gaeun-Jang <110662057+Gaeun-Jang@users.noreply.github.com>
2022-11-04 17:57:04 +0900

    feat : 숫자 입력받아서 배열로 바꾸는 기능 추가

--- src/App.js
+++ src/App.js
@@ -15,6 +15,12 @@ class App {
         MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
     }
 
+    getInputNumber() {
+        MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (number) => {
+            this.inputNumber = String(number).split("");
+        });
+    }
+
     play() {}
 }
 
