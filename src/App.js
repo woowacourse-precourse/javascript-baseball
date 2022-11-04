42b6259fc19d095e046b1604b95747f420fbcdeb
khkh0109 <ist200120@gmail.com>
2022-11-04 17:05:50 +0900

    feat: 비교 결과 판단하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -22,6 +22,25 @@ function comparingNumbers(inputNum, randomNum) {
   return result;
 }
 
+function returnResult(result) {
+  const ball = result[0];
+  const strike = result[1];
+  if(ball === 0 && strike === 0) {
+    return '낫싱';
+  } else if(ball !== 0 && strike === 0) {
+    return `${ball}볼`;
+  } else if(ball === 0 && strike !== 0) {
+    if(strike < 3) {
+      return `${strike}스트라이크`;
+    } else {
+      return `3스트라이크 
+3개의 숫자를 모두 맞히셨습니다! 게임 종료`;
+    }
+  } else if(ball !== 0 && strike !== 0) {
+    return `${ball}볼 ${strike}스트라이크`;
+  } 
+}
+
 const app = new App();
 app.play();
 module.exports = App;
