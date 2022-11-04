a68289dd8b65b326699b662bf8468bc620eb0963
wonkyum-kim <dodo12120@naver.com>
2022-11-04 20:22:14 +0900

    feat(src/App.js): initial setup
    1. Add MissionUtils library
    2. Execute app.play() with try catch statement
    
    BREAKING CHANGE: App.js can use MissionUtils and catch an error

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+
 class App {
   play() {}
+}
+
+try {
+  const app = new App();
+  app.play();
+} catch {
+  MissionUtils.Console.close();
 }
 
 module.exports = App;
