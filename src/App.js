480ae92b7f620b5663663e55464f323d7ca30b8c
ramda <91455804+gnfnfnr@users.noreply.github.com>
2022-11-04 20:11:44 +0900

    feat: show ball and strike result

--- src/App.js
+++ src/App.js
@@ -3,14 +3,13 @@ const MissionUtils = require("@woowacourse/mission-utils");
 class App {
   constructor() {
     this.randomAnswer = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
-    this.userInputNum;
   }
 
   userInput() {
     MissionUtils.Console.readLine("숫자를 입력해주세요.", (inputNum) => {
       if (this.userInputvalidation(inputNum)) {
         console.log(`숫자를 입력해주세요. ${inputNum}`);
-        this.userInputNum = inputNum;
+        this.showResult(inputNum);
       } else {
         const error = new Error("유효한 값이 아닙니다");
         MissionUtils.Console.close();
@@ -29,10 +28,46 @@ class App {
       : false;
   }
 
+  showResult(userInputNum) {
+    const result = {
+      ballNum: this.countBalls(userInputNum),
+      strikeNum: this.countStrikes(userInputNum),
+    };
+
+    MissionUtils.Console.print(userInputNum, this.randomAnswer.join(""));
+    if (userInputNum === this.randomAnswer.join("")) {
+      MissionUtils.Console.print("3스트라이크");
+      MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    } else if (!result.strikeNum && !result.ballNum) {
+      MissionUtils.Console.print("낫싱");
+    } else if (!result.strikeNum) {
+      MissionUtils.Console.print(result.ballNum + "볼");
+    } else if (!result.ballNum) {
+      MissionUtils.Console.print(result.strikeNum + "스트라이크");
+    } else {
+      MissionUtils.Console.print(
+        `${result.ballNum} 볼 ${result.strikeNum} 스트라이크`
+      );
+    }
+  }
+
+  countBalls(userInputNum) {
+    return this.randomAnswer.reduce((acc, cur, idx) => {
+      return userInputNum.includes(cur) && userInputNum.indexOf(cur) !== idx
+        ? acc + 1
+        : acc;
+    }, 0);
+  }
+
+  countStrikes(userInputNum) {
+    return this.randomAnswer.reduce((acc, cur, idx) => {
+      return userInputNum.indexOf(cur) === idx ? acc + 1 : acc;
+    }, 0);
+  }
+
   play() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
     this.userInput();
-    MissionUtils.Console.print(this.userInputNum);
   }
 }
 
