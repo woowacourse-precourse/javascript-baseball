aa5f886b394bde7b6dc738e94dcc8eabf706b819
loopy-lim <loopy@jnu.ac.kr>
2022-11-04 16:33:39 +0900

    feat: 사용자의 입력을 받는다

--- src/Baseball/Baseball.computer.js
+++ src/Baseball/Baseball.computer.js
@@ -1,10 +1,21 @@
 const MissionUtils = require("@woowacourse/mission-utils");
+const BaseballOutput = require("./Baseball.output");
+const BaseballValidator = require("./BaseBall.validator");
 
 class BaseballComputer {
   baseballNumber = [];
   baseballOutput = new BaseballOutput();
+  baseballValidator = new BaseballValidator();
+
   constructor() {
     this.baseballNumber = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
+  }
+  getNumber() {
+    const userNumbers = this.baseballOutput.getNumber();
+    this.baseballValidator.checkNumericNumbers(...userNumbers);
+    this.baseballValidator.checkNumbersLength(...userNumbers);
+    this.baseballValidator.checkOtherNumbers(...userNumbers);
+    return userNumbers;
   }
 }
 
--- src/Baseball/Baseball.output.js
+++ src/Baseball/Baseball.output.js
@@ -7,6 +7,11 @@ class BaseBasllOutput {
   end() {
     MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n");
   }
+  getNumber() {
+    MissionUtils.Console.readLine("숫자를 입력해주세요: \n", (answer) => {
+      return answer;
+    });
+  }
   restart() {
     Console.readLine(
       "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n",
