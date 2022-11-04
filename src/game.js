da68239f61c6a451a97bbcb2a6c83f6cf4078518
jongwonp <jw032876@gmail.com>
2022-11-05 00:12:45 +0900

    게임 기능 수정

--- src/game.js
+++ src/game.js
@@ -1,5 +1,20 @@
-const main = (num) => {
-  console.log(`${num}은 올바른 값입니다!`);
+const MissionUtils = require('@woowacourse/mission-utils');
+const exitOrRestart = require('./exitOrRestart');
+
+const game = (userNum, computerNum) => {
+  let ballCount = 0;
+  let strikeCount = 0;
 };
 
-module.exports = main;
+const guessedCorrectly = () => {
+  MissionUtils.Console.print('3스트라이크');
+  MissionUtils.Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+  MissionUtils.Console.readLine(
+    '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.',
+    (num) => {
+      exitOrRestart(num);
+    }
+  );
+};
+
+module.exports = game;
