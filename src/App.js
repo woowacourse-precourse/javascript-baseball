5e67641751776db9ca629ed653d355997e74eb67
c0c0pang <coc1789@naver.com>
2022-11-04 20:04:25 +0900

    feat: 볼,스트라이크,낫싱 중 만족하는 조건을 출력하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -43,4 +43,34 @@ const GameStart = (USER) => {
   }
 };
 
+const NumberCompare = (computer, user) => {
+  let BALL_SCORE = 0;
+  let STRIKE_SCORE = 0;
+  for (let index = 0; index < computer.length; index++) {
+    if (computer[index] === user[index]) {
+      STRIKE_SCORE += 1;
+    }
+    if (computer.indexOf(user[index]) >= 0) {
+      BALL_SCORE += 1;
+    }
+  }
+  if (STRIKE_SCORE === 0 && BALL_SCORE === 0) {
+    MissionUtils.Console.print('낫싱');
+    return false;
+  };
+  if (STRIKE_SCORE === 3) {
+    MissionUtils.Console.print(`${STRIKE_SCORE}스트라이크`);
+    return true;
+  };
+  if (STRIKE_SCORE === 0 && BALL_SCORE > 0) {
+    MissionUtils.Console.print(`${BALL_SCORE}볼`);
+    return false;
+  };
+  if (STRIKE_SCORE > 0 && BALL_SCORE - STRIKE_SCORE == 0) {
+    MissionUtils.Console.print(`${STRIKE_SCORE}스트라이크`);
+    return false
+  };
+  MissionUtils.Console.print(`${BALL_SCORE - STRIKE_SCORE}볼 ${STRIKE_SCORE}스트라이크`);
+  return false;
+}
 module.exports = App;
