121929aa68e493d138f83b24c1effc1c57cc543c
curomame <curomame@naver.com>
2022-11-04 22:11:53 +0900

    ➕ feat - read user input value
    유저가 입력한 값을 읽는 메소드 추가

--- src/App.js
+++ src/App.js
@@ -12,6 +12,15 @@ class App {
     return Number(number.join(''))
   }
 
+  readUserInputValue(){
+    MissionUtils.Console.readLine('숫자를 입력해주세요 : ',(value)=>{
+      //TODO - 입력한 값이 유효한지 확인하는 기능
+      return value;
+    })
+  }
+
+  
+
 }
 
 module.exports = App;
