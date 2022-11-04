30a22e843cf28a4f96a0a0f6981d3472e60c2546
froggy1014 <clcl6084@gmail.com>
2022-11-05 03:59:30 +0900

    feat: 게임 처음 시작 초기화 및 안내 출력 기능 구현
    1. init(): 처음 게임을 시작하여 객체가 생성될때, 안내멘트와 세자리수를 생성 메소드를 호출하는 메소드입니다.
    2. generateRandomNumbers(): 중복없는 세자리수를 생성해 반환하는 메소드입니다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  constructor() {
+    this.randoms;
+    this.init();
+  }
+
+  init() {
+    this.randoms = this.generateRandomNumbers()
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+  }
+
+  generateRandomNumbers() {
+    const uniqueRandomNumbers = [];
+    while(uniqueRandomNumbers.length < 3) {
+      let number = MissionUtils.Random.pickNumberInRange(1, 9)
+      if(!uniqueRandomNumbers.includes(number)) {
+        uniqueRandomNumbers.push(number);
+      }
+    }
+    return uniqueRandomNumbers;
+  }
+
+  play() {
+    console.log(this.randoms);
+  }
+
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
