57052a402a7113ee8ecf309d14e8af61afc5e23e
hxezin <leehxejin@gmail.com>
2022-11-04 19:29:51 +0900

    feat: 1-9 사이 숫자 중 서로 다른 임의의 수 3개 선택

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  constructor() {
+    this.computerNumber = [];
+  }
+
+  play() {
+    const numberList = new Set();
+    while (numberList.size < 3) {
+      const randomNumber = MissionUtils.Random.pickNumberInRange(1, 9);
+      numberList.add(randomNumber);
+    }
+    this.computerNumber.push(...numberList);
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+  }
 }
 
 module.exports = App;
