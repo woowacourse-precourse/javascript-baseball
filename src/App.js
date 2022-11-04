40b4fe8a35f4a4e29fc90a34cd8568ec2265b18c
Hoon <si932174@gmail.com>
2022-11-04 16:30:07 +0900

    feat : MissionUtils.Console 라이브러리를 사용한 콘솔출력 함수 작성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
+const printer = (message) => {
+  MissionUtils.Console.print(message);
+};
+
 class App {
-  play() {}
+  play() {
+    printer("숫자 야구 게임을 시작합니다.");
+  }
 }
 
-module.exports = App;
+const app = new App();
+app.play();
+
+module.exports = { App, printer };
