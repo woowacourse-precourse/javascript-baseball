4f297e3d8f1f4af32aa2ca825c277cd9d923dd0d
yong <tkddbs1644@gmail.com>
2022-11-04 21:35:25 +0900

    feat: 서로 다른 3자리의 랜덤숫자를 생성하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
   play() {}
+
+  generateRandomNumber() {
+    const computerNumbers = [];
+    while (computerNumbers.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computerNumbers.includes(number)) {
+        computerNumbers.push(number);
+      }
+    }
+    return computerNumbers.map((number) => String(number)).join("");
+  }
 }
 
 module.exports = App;
