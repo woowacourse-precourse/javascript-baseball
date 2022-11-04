fa4a48fcdd55a1fc718ff53eca9eeff2025ca888
pydevjeong <njh3421@gmail.com>
2022-11-04 23:26:54 +0900

    feat(App.js) : computerRandomNumber function

--- src/App.js
+++ src/App.js
@@ -2,16 +2,25 @@ const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
   play() {
-    this.showMessage()
-    this.userInput()
+    this.showMessage();
+    this.userInput();
   }
-  showMessage(){
-    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.')
+  showMessage() {
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
   }
-  userInput(){
-    MissionUtils.Console.readLine('숫자를 입력해주세요 : ',(userNumber)=>{
-      console.log(`사용자 : ${userNumber}`)
-    })
+  userInput() {
+    MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (userNumber) => {
+      console.log(`사용자 : ${userNumber}`);
+    });
+  }
+  computerRandomNumber() {
+    const COMPUTER = [];
+    while (COMPUTER.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!COMPUTER.includes(number)) {
+        COMPUTER.push(number);
+      }
+    }
   }
 }
 
