537a2136f09c60e50df0ca0dfd1bdf4f7880b33e
gunyongbok <qhrrjsdyd123@gmail.com>
2022-11-04 17:27:35 +0900

    feat: 서로 다른 3개의 수 뽑는 함수 만들기
    MissionUtils 라이브러리를 이용해서 1부터 9까지 서로 다른 수를 뽑는 함수 만들기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+
 class App {
-    play() {}
+    constructor() {
+        this.computerNumberArray = [];
+    }
+    play() {
+        this.pickRandomThreeNumbers();
+    }
+
+    // 랜덤한 3개의 숫자를 뽑는 함수
+    pickRandomThreeNumbers() {
+        const numberSet = new Set();
+        while (numberSet.size !== 3) {
+            numberSet.add(MissionUtils.Random.pickNumberInRange(1, 9));
+        }
+        console.log(Array.from(numberSet));
+    }
 }
+let app = new App();
+app.play();
 
 module.exports = App;
