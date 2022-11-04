8992fe64d3b1ed302a8873c964c3c44896907fe3
leeuihyun <meta212121@naver.com>
2022-11-04 23:19:27 +0900

    refactor : 하나의 기능만을 수행하기 위해 비교한 값을 출력만을 처리

--- src/Compare.js
+++ src/Compare.js
@@ -23,27 +23,14 @@ class Compare extends Mission {
   }
 
   returnConsole(strike, ball) {
-    if (strike === 3) {
+    if (strike === 3)
       this.mission.Console.print(
         '3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료'
       );
-      return 'clear';
-    }
-    if (!strike && !ball) {
-      this.mission.Console.print('낫싱');
-      return 'nothing';
-    }
-    if (strike && !ball) {
-      this.mission.Console.print(`${strike}스트라이크`);
-      return 'onlyStrike';
-    }
-
-    if (!strike && ball) {
-      this.mission.Console.print(`${ball}볼`);
-      return 'onlyBall';
-    }
+    if (!strike && !ball) this.mission.Console.print('낫싱');
+    if (strike && !ball) this.mission.Console.print(`${strike}스트라이크`);
+    if (!strike && ball) this.mission.Console.print(`${ball}볼`);
     this.mission.Console.print(`${ball}볼 ${strike}스트라이크`);
-    return 'strikeBall';
   }
 
   getResult() {
