b80a5638607e26d2bf23625e17d0345cb26da3b0
gilpop8663 <wolfye@naver.com>
2022-11-04 18:45:34 +0900

    refactor:함수 분리 및 for 문 제거, 함수명 변경
    함수 분리한 이유는 export module을 통해 테스트를 용이하게 하기 위해 함수 분리를 하였습니다
    
    for 문을 제거하고 자바스크립트 내장 메서드를 이용하여 코드 가독성을 높이기 위해 코드를 변경하였습니다
    
    함수명이 작동하는 동작과 어울리지 않는다고 생각하여 변경하였습니다

--- src/App.js
+++ src/App.js
@@ -1,120 +1,18 @@
-const MissionUtils = require("@woowacourse/mission-utils");
-
-const gameStartPhrase = () => {
-  MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
-};
-
-const gameRestartQuestion = () => {
-  MissionUtils.Console.readLine(
-    "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
-    (number) => {
-      number = Number(number);
-      if (number === 1) {
-        let answer = randomNumberSetting();
-        userInput(answer);
-      }
-
-      if (number === 2) {
-        MissionUtils.Console.close();
-      }
-
-      if (number !== 1 && number !== 2) {
-        throw "잘못된 값을 입력하였습니다.";
-      }
-    }
-  );
-};
-
-const isNotNumber = (number) => {
-  return isNaN(Number(number));
-};
-
-const isContainsNumberZero = (number) => {
-  return number.includes("0");
-};
-
-const isDuplicate = (number) => {
-  const set = new Set();
-  set.add(number[0]);
-  set.add(number[1]);
-  set.add(number[2]);
-
-  if (set.size !== number.length) return true;
-  return false;
-};
-
-const exceptionDetection = (number) => {
-  if (number.length !== 3) return true;
-  if (isNotNumber(number)) return true;
-  if (isContainsNumberZero(number)) return true;
-  if (isDuplicate(number)) return true;
-  return false;
-};
-
-const userInput = (answer) => {
-  MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (number) => {
-    if (exceptionDetection(number)) {
-      throw "잘못된 값을 입력하였습니다.";
-    }
-
-    let userNumberArray = number.split("").map((num) => Number(num));
-    let strikeNum = strikeCalculation(answer, userNumberArray);
-    let ballNum = ballCalculation(answer, userNumberArray);
-
-    if (strikeNum === 0 && ballNum === 0) {
-      MissionUtils.Console.print("낫싱");
-      userInput(answer);
-    }
-
-    if (strikeNum !== 3) {
-      let message = "";
-      message += ballNum ? `${ballNum}볼 ` : "";
-      message += strikeNum ? `${strikeNum}스트라이크 ` : "";
-      MissionUtils.Console.print(message);
-      userInput(answer);
-    }
-
-    if (strikeNum === 3) {
-      MissionUtils.Console.print(
-        `3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료`
-      );
-      gameRestartQuestion();
-    }
-  });
-};
-
-const strikeCalculation = (answer, userNumber) => {
-  let count = 0;
-  for (let i = 0; i < 3; i++) {
-    if (answer[i] === userNumber[i]) count++;
-  }
-  return count;
-};
-
-const ballCalculation = (answer, userNumber) => {
-  let count = 0;
-  for (let i = 0; i < 3; i++) {
-    if (answer[i] !== userNumber[i] && userNumber.includes(answer[i])) count++;
-  }
-  return count;
-};
-
-const randomNumberSetting = () => {
-  let randomNumberArray = [];
-  while (randomNumberArray.length < 3) {
-    let randomNumber = MissionUtils.Random.pickNumberInRange(1, 9);
-    if (randomNumberArray.includes(randomNumber)) continue;
-    randomNumberArray.push(randomNumber);
-  }
-  return randomNumberArray;
-};
+const {
+  randomNumberSetting,
+  gameStartPhrase,
+  gameProgressQuestion,
+} = require("./gameFunctions");
 
 class App {
   play() {
-    let answer = randomNumberSetting();
+    const answer = randomNumberSetting();
     gameStartPhrase();
-    userInput(answer);
+    gameProgressQuestion(answer);
   }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
---
+++ src/gameFunctions.js
@@ -0,0 +1,108 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const { isExceptionDetection } = require("./exceptionFunctions");
+
+const strikeCalculation = (answer, userNumber) => {
+  let count = 0;
+  answer.forEach((number, index) => {
+    if (number === userNumber[index]) count++;
+  });
+  return count;
+};
+
+const ballCalculation = (answer, userNumber) => {
+  let count = 0;
+  answer.forEach((number, index) => {
+    if (number !== userNumber[index] && userNumber.includes(number)) count++;
+  });
+  return count;
+};
+
+const gameResultOutput = (strikeNum, ballNum) => {
+  if (strikeNum === 0 && ballNum === 0) {
+    MissionUtils.Console.print("낫싱");
+    return 0;
+  }
+
+  if (strikeNum !== 3) {
+    let message = "";
+    message += ballNum ? `${ballNum}볼 ` : "";
+    message += strikeNum ? `${strikeNum}스트라이크 ` : "";
+    MissionUtils.Console.print(message);
+    return 0;
+  }
+
+  if (strikeNum === 3) {
+    MissionUtils.Console.print(
+      `3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료`
+    );
+    return 1;
+  }
+};
+
+const gameRestartResult = (number) => {
+  if (number === 1) {
+    let answer = randomNumberSetting();
+    gameProgressQuestion(answer);
+  }
+
+  if (number === 2) {
+    MissionUtils.Console.close();
+  }
+};
+
+const randomNumberSetting = () => {
+  let randomNumberArray = [];
+  while (randomNumberArray.length < 3) {
+    let randomNumber = MissionUtils.Random.pickNumberInRange(1, 9);
+    if (randomNumberArray.includes(randomNumber)) continue;
+    randomNumberArray.push(randomNumber);
+  }
+  return randomNumberArray;
+};
+
+const gameStartPhrase = () => {
+  MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+};
+
+const gameRestartQuestion = () => {
+  MissionUtils.Console.readLine(
+    "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
+    (number) => {
+      number = Number(number);
+
+      if (number !== 1 && number !== 2) {
+        throw "잘못된 값을 입력하였습니다.";
+      }
+
+      gameRestartResult(number);
+    }
+  );
+};
+
+const gameProgressQuestion = (answer) => {
+  MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (number) => {
+    if (isExceptionDetection(number)) {
+      throw "잘못된 값을 입력하였습니다.";
+    }
+
+    let userNumberArray = number.split("").map((num) => Number(num));
+    let strikeNum = strikeCalculation(answer, userNumberArray);
+    let ballNum = ballCalculation(answer, userNumberArray);
+
+    const gameResult = gameResultOutput(strikeNum, ballNum, answer);
+
+    if (gameResult === 0) gameProgressQuestion(answer);
+    if (gameResult === 1) gameRestartQuestion();
+  });
+};
+
+module.exports = {
+  strikeCalculation,
+  ballCalculation,
+  gameResultOutput,
+  gameRestartResult,
+  randomNumberSetting,
+  gameProgressQuestion,
+  gameRestartQuestion,
+  gameStartPhrase,
+};
