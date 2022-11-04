6df89b5282250342fc895e351b409598130ed717
dlwl98 <ff981113@hanyang.ac.kr>
2022-11-04 21:01:19 +0900

    feat: App 클래스 Dispatcher 인스턴스 주입

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+const Action = require('./Action');
+const Dispatcher = require('./Dispatcher');
+
 class App {
-  play() {}
+  #action;
+
+  #dispatcher;
+
+  constructor() {
+    this.#action = {};
+    this.#dispatcher = {};
+  }
+
+  play() {
+    this.#action = new Action();
+    this.#dispatcher = new Dispatcher();
+
+    this.#action.injection(this.#dispatcher);
+    this.#dispatcher.register(() => console.log('a'));
+    this.#dispatcher.register((payload) => console.log(payload));
+    this.#action.sendToDispatcher('asd');
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
