b087f21cbc4109c07f2493413ef7ff21f0beba28
BuildTools <unconfigured@null.spigotmc.org>
2022-11-04 21:41:19 +0900

    메세지 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+
 class App {
-  play() {}
+  play() {
+    console.log('숫자 야구 게임을 시작합니다.');
+  }
 }
 
 module.exports = App;
+
+const app = new App();
+app.play();
