156f37425d8ed0eee941845ac10dc3c657e922fc
jhyns <tockiib@gmail.com>
2022-11-04 22:53:13 +0900

    test: Add target number generation test
    generateTargetNumber 함수가 서로 다른 세 random number를 담은 배열을 반환하는지 확인하는 테스트

---
+++ __tests__/TargetNumberTest.js
@@ -0,0 +1,24 @@
+const App = require("../src/App");
+const MissionUtils = require("@woowacourse/mission-utils");
+
+describe('Target number test', () => {
+  test('target number 배열의 길이는 3', () => {
+    const target = App.generateTargetNumber();
+
+    expect(target.length).toEqual(3);
+  });
+
+  test('target number 배열의 값은 1~9 사이의 숫자', () => {
+    const targetNumber = App.generateTargetNumber();
+    const result = targetNumber.every((number) => number >= 1 && number <= 9);
+
+    expect(result).toBe(true);
+  });
+
+  test('target number 배열의 값은 중복되지 않음', () => {
+    const targetNumber = App.generateTargetNumber();
+    const result = targetNumber.every((number, index) => targetNumber.indexOf(number) === index);
+
+    expect(result).toBe(true);
+  });
+});
