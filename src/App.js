f782c3c020a1245aa3e06df03094ec448ce9b7e9
SSY1203 <tlstjsdud566@naver.com>
2022-11-04 12:52:58 +0900

    feat : App.js 컴퓨터의 랜덤 수 받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+const MissionUtils = require('@woowacourse/mission-utils');
 class App {
   play() {}
+
+  getComputerNumber() {
+    const computerNumbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
+    return computerNumbers;
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
