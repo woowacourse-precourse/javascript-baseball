c76c4144277d50c8b7ba386e2e51436f136806a0
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2022-11-04 16:19:10 +0900

    refactor: 함수의 순서 변경 및 위치 이동, 함수 선언식으로 변경

--- src/App.js
+++ src/App.js
@@ -1,114 +1,128 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+
 class App {
-  play() {}
+  play() {
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+    const COMPUTER = makeRandomNumber();
+    result();
+
+    function makeRandomNumber() {
+      const COMPUTER_NUMBER = [];
+
+      while (COMPUTER_NUMBER.length < 3) {
+        const RANDOM_NUMBER = MissionUtils.Random.pickNumberInRange(1, 9);
+
+        if (!COMPUTER_NUMBER.includes(String(RANDOM_NUMBER))) {
+          COMPUTER_NUMBER.push(String(RANDOM_NUMBER));
+        }
+      }
+
+      return COMPUTER_NUMBER;
+    }
+
+    function result() {
+      MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (userInput) => {
+        const USER = condition(userInput);
+        const BALL = countBall(COMPUTER, USER);
+        const STRIKE = countStrike(COMPUTER, USER);
+
+        if (BALL === 0 && STRIKE === 0) {
+          MissionUtils.Console.print('낫싱');
+          result();
+        }
+        if (BALL > 0 && STRIKE === 0) {
+          MissionUtils.Console.print(`${BALL}볼`);
+          result();
+        }
+        if (BALL === 0 && STRIKE > 0 && STRIKE < 3) {
+          MissionUtils.Console.print(`${STRIKE}스트라이크`);
+          result();
+        }
+        if (BALL > 0 && STRIKE > 0) {
+          MissionUtils.Console.print(`${BALL}볼 ${STRIKE}스트라이크`);
+          result();
+        }
+        if (STRIKE === 3) {
+          MissionUtils.Console.print(`${STRIKE}스트라이크`);
+          restartOrEnd();
+        }
+      });
+    }
+
+    function condition(input) {
+      const USER_NUMBER = input.split('');
+
+      for (let index = 0; index < USER_NUMBER.length; index += 1) {
+        if (USER_NUMBER[index] < '1' || USER_NUMBER[index] > '9') {
+          throw '1~9 사이의 숫자를 입력해 주세요!';
+        }
+      }
+
+      if (USER_NUMBER.length !== 3) {
+        throw '3자리 숫자를 입력해주세요!';
+      }
+      if (findDuplicate(USER_NUMBER)) {
+        throw '중복되지 않은 숫자를 입력해 주세요!';
+      }
+
+      return USER_NUMBER;
+    }
+
+    function findDuplicate(numberArr) {
+      const UNIQUE_ARRAY = new Set(numberArr);
+
+      if (numberArr.length !== UNIQUE_ARRAY.size) {
+        return true;
+      }
+
+      return false;
+    }
+
+    function countBall(computerNumber, userNumber) {
+      let ballNumber = 0;
+
+      for (let index = 0; index < computerNumber.length; index += 1) {
+        if (
+          computerNumber[index] !== userNumber[index] &&
+          computerNumber.includes(userNumber[index])
+        ) {
+          ballNumber += 1;
+        }
+      }
+
+      return ballNumber;
+    }
+
+    function countStrike(computerNumber, userNumber) {
+      let strikeNumber = 0;
+
+      for (let index = 0; index < computerNumber.length; index += 1) {
+        if (computerNumber[index] === userNumber[index]) {
+          strikeNumber += 1;
+        }
+      }
+
+      return strikeNumber;
+    }
+
+    function restartOrEnd() {
+      MissionUtils.Console.print('🎉🥳 정답입니다!🥳🎉 게임 종료.');
+      MissionUtils.Console.readLine(
+        '게임을 다시 시작하려면 1, 종료하려면 2를 입력하세요.',
+        (number) => {
+          if (number === '1') {
+            app.play();
+          }
+          if (number === '2') {
+            MissionUtils.Console.close();
+          }
+        }
+      );
+    }
+  }
 }
 
-const makeRandomNumber = () => {
-  const COMPUTER_NUMBER = [];
+const app = new App();
+app.play();
 
-  while (COMPUTER_NUMBER.length < 3) {
-    const RANDOM_NUMBER = MissionUtils.Random.pickNumberInRange(1, 9);
-
-    if (!COMPUTER_NUMBER.includes(String(RANDOM_NUMBER))) {
-      COMPUTER_NUMBER.push(String(RANDOM_NUMBER));
-    }
-  }
-
-  return COMPUTER_NUMBER;
-};
-
-const findDuplicate = (numberArr) => {
-  const UNIQUE_ARRAY = new Set(numberArr);
-
-  if (numberArr.length !== UNIQUE_ARRAY.size) {
-    return true;
-  }
-
-  return false;
-};
-
-const condition = (input) => {
-  const USER_NUMBER = input.split('');
-
-  for (let index = 0; index < USER_NUMBER.length; index += 1) {
-    if (USER_NUMBER[index] < '1' || USER_NUMBER[index] > '9') {
-      throw '1~9 사이의 숫자를 입력해 주세요!';
-    }
-  }
-
-  if (USER_NUMBER.length !== 3) {
-    throw '3자리 숫자를 입력해주세요!';
-  }
-  if (findDuplicate(USER_NUMBER)) {
-    throw '중복되지 않은 숫자를 입력해 주세요!';
-  }
-
-  return USER_NUMBER;
-};
-
-const countBall = (computerNumber, userNumber) => {
-  let ballNumber = 0;
-
-  for (let index = 0; index < computerNumber.length; index += 1) {
-    if (computerNumber[index] !== userNumber[index] && computerNumber.includes(userNumber[index])) {
-      ballNumber += 1;
-    }
-  }
-
-  return ballNumber;
-};
-
-const countStrike = (computerNumber, userNumber) => {
-  let strikeNumber = 0;
-
-  for (let index = 0; index < computerNumber.length; index += 1) {
-    if (computerNumber[index] === userNumber[index]) {
-      strikeNumber += 1;
-    }
-  }
-
-  return strikeNumber;
-};
-
-const result = () => {
-  MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (userInput) => {
-    const USER = condition(userInput);
-    ball = countBall(COMPUTER, USER);
-    strike = countStrike(COMPUTER, USER);
-
-    if (ball === 0 && strike === 0) {
-      MissionUtils.Console.print('낫싱');
-      result();
-    }
-    if (ball > 0 && strike === 0) {
-      MissionUtils.Console.print(`${ball} 볼`);
-      result();
-    }
-    if (ball === 0 && strike > 0 && strike < 3) {
-      MissionUtils.Console.print(`${strike} 스트라이크`);
-      result();
-    }
-    if (ball > 0 && strike > 0) {
-      MissionUtils.Console.print(`${ball} 볼 ${strike} 스트라이크`);
-      result();
-    }
-    if (strike === 3) {
-      MissionUtils.Console.print(`${strike} 스트라이크`);
-      restartOrEnd();
-    }
-  });
-};
-
-const restartOrEnd = () => {
-  return MissionUtils.Console.readLine(
-    '🎉🥳 정답입니다! 🥳🎉 게임을 종료합니다.\n게임을 다시 시작하러면 1, 종료하려면 2를 입력하세요.',
-    (number) => {
-      if (number === '1') {
-        app.play();
-      }
-      if (number === '2') {
-        MissionUtils.Console.close();
-      }
-    }
-  );
-};
 module.exports = App;
