eecf062635a419fac75a7cf80a294f89e1fe4f82
somin <smhs12@naver.com>
2022-11-04 22:42:02 +0900

    feat: strike 갯수 카운트 함수

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ class App {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
     MissionUtils.Console.readLine("숫자를 입력해주세요:", (userInput) => {
       if (this.checkInputValidation(userInput)) {
+        this.compareInputWithAnswer(computerNumber, userInput);
       } else {
         MissionUtils.Console.close();
       }
@@ -27,6 +28,21 @@ class App {
     else if (new Set(userInput).size !== 3) return;
     else if (userInput.includes("0")) return;
     return true;
+  }
+
+  compareInputWithAnswer(computerNumber, userInput) {
+    const strike = this.countStrike(computerNumber, userInput);
+  }
+
+  countStrike(computerNumber, userInput) {
+    let strike = 0;
+    for (let i = 0; i < computerNumber.length; i++) {
+      const userIndex = userInput.indexOf(computerNumber[i]);
+      if (userIndex !== -1 && userIndex === i) {
+        strike++;
+      }
+    }
+    return strike;
   }
 }
 
