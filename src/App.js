6aeb3ca8346bf5d748f7826dbb05186bcf506075
somin <smhs12@naver.com>
2022-11-04 20:06:30 +0900

    feat: 상대(컴퓨터) 숫자 생성 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+const MissionUtils = require("@woowacourse/mission-utils");
 class App {
+  createComputerNumber() {
+    const eachNumberArray = [];
+    while (eachNumberArray.length < 3) {
+      let eachNumber = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!eachNumberArray.includes(eachNumber)) {
+        eachNumberArray.push(eachNumber);
+      }
+    }
+    return eachNumberArray.join("");
+  }
   play() {}
 }
+
+new App();
 
 module.exports = App;
