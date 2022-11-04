7f36430ccd931073649f5b13e7d67afaf1d6c663
coolIlive <galilee155@naver.com>
2022-11-04 12:01:28 +0900

    Feat: 추가_ 정답 검사 기능
    - 사용자의 숫자를 입력받은 후 isValidValue()가 app.checkcheck를 싱행함
    - app.checkchec는 checkAnswer()를 실행
    - const countBall를 통해 볼 개수를 센다.
    - const countScrike를 통해 스트라이크 개수를 센다.
    - checkAnswer()에서 결과를 출력함
    (+) 정답일 경우 실행할 함수는 앞으로 만들것임.

--- src/App.js
+++ src/App.js
@@ -2,25 +2,22 @@ const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
   constructor() {
-    this.randomValue=''
+    this.randomValue=makeRandomValue()
     this.userValue=''
   }
 
   play() {
-    makeRandomValue()
-    // inputUserValue()
-    console.log(this.randomValue)
-
-  }
-
-  setRandomValue(number) {
-    this.randomValue=number
+    MissionUtils.Console.print(this.randomValue)
+    inputUserValue()
   }
 
   setUserValue(number) {
     this.userValue=number
   }
-
+  
+  checkcheck() {
+    checkAnswer(this.randomValue,this.userValue)
+  }
 }
 
 const app=new App()
@@ -32,7 +29,7 @@ function makeRandomValue() {
   while (randomValue.size<3) {
     randomValue.add(MissionUtils.Random.pickNumberInRange(1, 9));
   }
-  app.setRandomValue([...randomValue].join(''))
+  return [...randomValue].join('')
 }
 
 // 사용자의 숫자 입력받기
@@ -48,7 +45,50 @@ function isValidValue(number) {
   if (!number.match(/[1-9]{3}/) || isUnique!==3) {
     throw ("잘못된 형식을 입력하였습니다. 서로 다른 숫자 3가지를 입력하세요")
   }
+  // return answer
   app.setUserValue(number)
+  app.checkcheck()
+}
+
+function here() {
+  console.log('55555555555')
+}
+
+// 정답 검사
+function checkAnswer(computer,user) {
+  const ball=countBall(computer,user)
+  const strike=countScrike(computer,user)
+  if (strike===3) {
+    // 재시작 여부 묻는 함수 실행하기
+    here()
+  }
+  if (ball===0 && strike===0) {
+    MissionUtils.Console.print(`낫싱`);
+    // inputUserValue()
+  }
+  if (ball===0 && strike!==0) {
+    MissionUtils.Console.print(`${strike}스트라이크`);
+    // inputUserValue()
+  }
+  if (ball!==0 && strike===0) {
+    MissionUtils.Console.print(`${ball}볼`);
+    // inputUserValue()
+  }
+  if (ball!==0 && strike!==0) {
+    MissionUtils.Console.print(`${ball}볼 ${strike}스트라이크`);
+    // inputUserValue()
+  }
+  
+}
+
+//볼 개수
+const countBall = (computer,user) => {
+  return [...computer].filter(x => user.includes(x)).length
+}
+
+//스트라이크 개수
+const countScrike = (computer,user) => {
+  return [...computer].filter((x,idx) => user[idx]===x).length
 }
 
 // MissionUtils.Console.close()
