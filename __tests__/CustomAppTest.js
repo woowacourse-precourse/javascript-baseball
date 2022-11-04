41a6a3e94de4c5c4a0ad4b6bfc522733d252bca1
turtle601 <turtle601@naver.com>
2022-11-04 20:20:17 +0900

    test(customApp) isStrikeOut() 함수 테스트 코드 작성

--- __tests__/CustomAppTest.js
+++ __tests__/CustomAppTest.js
@@ -147,3 +147,32 @@ describe('App 클래스 - getResultToString()', () => {
     expect(app.getResultToString(random, input)).toEqual('2볼');
   });
 });
+
+describe('App 클래스 - isStrikeOut()', () => {
+  test('3볼일 때', () => {
+    const random = [2, 1, 3];
+    const input = [3, 2, 1];
+
+    const app = new App();
+
+    expect(app.isStrikeOut(random, input)).toBeFalsy();
+  });
+
+  test('2스트라이크 노볼 상황', () => {
+    const random = [1, 2, 3];
+    const input = [1, 4, 3];
+
+    const app = new App();
+
+    expect(app.isStrikeOut(random, input)).toBeFalsy();
+  });
+
+  test('3스트라이크일 때', () => {
+    const random = [1, 2, 3];
+    const input = [1, 2, 3];
+
+    const app = new App();
+
+    expect(app.isStrikeOut(random, input)).toBeTruthy();
+  });
+});
