33f43ba940eefef1d532c0a4f49ea7a5bd7c0108
woo-jk <201702042@o.cnu.ac.kr>
2022-11-04 17:07:01 +0900

    fix: 뽑은 숫자 범위 테스트명 변경

--- __tests__/DrawNumbersTest.js
+++ __tests__/DrawNumbersTest.js
@@ -1,14 +1,14 @@
 const App = require("../src/App");
 
 describe.only("숫자 뽑기 테스트", () => {
-  test("숫자 범위가 1부터 9까지인지 확인", () => {
+  test("숫자 개수가 3개인지 확인", () => {
     const app = new App();
     const threeRandomNumber = app.drawThreeRandomNumbers();
 
     expect(threeRandomNumber.length).toHaveLength(3);
   });
 
-  test("숫자 개수가 3개인지 확인", () => {});
+  test("숫자 범위가 1부터 9까지인지 확인", () => {});
 
   test("각 숫자가 중복되어 있는지 확인", () => {});
 });
