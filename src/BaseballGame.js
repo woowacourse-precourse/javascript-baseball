05f3948712794759ec185b06502c3258a4096885
TaeeunKim <xodms0309@naver.com>
2022-11-05 02:11:09 +0900

    feat: user input을 받아오는 기능 구현

--- src/BaseballGame.js
+++ src/BaseballGame.js
@@ -3,11 +3,20 @@ const { Console, Random } = require('@woowacourse/mission-utils');
 class BaseballGame {
   constructor() {
     this.computerNumber = [];
+    this.guess = [];
   }
 
   start() {
     Console.print('숫자 야구 게임을 시작합니다.');
     this.computerNumber = this.createComputerNumber();
+    this.getUserInput();
+  }
+
+  getUserInput() {
+    Console.readLine('숫자를 입력해주세요 : ', (guess) => {
+      const userGuessToArray = guess.split('').map(Number);
+      this.guess = userGuessToArray;
+    });
   }
 
   createComputerNumber() {
