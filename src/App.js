96db27699429771a6f0511cd6012486381973e72
yaena1223 <yaena1223@dgu.ac.kr>
2022-11-05 06:45:45 +0900

    feat: add second function - compare and give hint

--- src/App.js
+++ src/App.js
@@ -1,21 +1,47 @@
-const MissionUtils = require("@woowacourse/mission-utils");
+const MissionUtils = require('@woowacourse/mission-utils');
 class App {
+  constructor() {
+    this.end = false;
+    this.error = false;
+  }
   play() {
-    console.log(rand_num());
+    // this.play_number_baseball_game();
+    /**while(True),
+     * 다 맞히면 게임 종료 후 1,2 선택 (1이면 재시작, 2면 애플리케이션을 종료)
+     * 입력값 잘못되면 throw 이용한 예외처리 => 얘는 애플리케이션을 종료
+     * **/
+  }
+
+  // play_number_baseball_game() {
+
+  // }
+
+  computer_random_number() {
+    const number_list = [];
+    while (number_list.length < 3) {
+      const single_digit = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!number_list.includes(single_digit)) {
+        number_list.push(single_digit);
+      }
+    }
+    const final_random_num = number_list.join('');
+    return final_random_num;
+  }
+
+  compare_and_give_hint(input, answer) {
+    let strike = 0;
+    let ball = 0;
+    for (let i = 0; i < 3; i++) {
+      if (input[i] === answer[i]) strike += 1;
+      else if (answer.indexOf(input[i]) > -1 && answer.indexOf(input[i]) < 3) {
+        ball += 1;
+      }
+    }
+    if (ball === 0 && strike === 0) return '낫싱';
+    else if (strike === 3) return true;
+    return `${ball}볼 ${strike}스트라이크`;
   }
 }
-
-const rand_num = function computer_random_number() {
-  const number_list = [];
-  while (number_list.length < 3) {
-    const single_digit = MissionUtils.Random.pickNumberInRange(1, 9);
-    if (!number_list.includes(single_digit)) {
-      number_list.push(single_digit);
-    }
-  }
-  const final_random_num = number_list.map((number) => number).join("");
-  return final_random_num;
-};
 
 module.exports = App;
 const app = new App();
