db95562a6502293f8c267a00503f850e7d05b20b
oio337a <oio337a@gmail.com>
2022-11-04 21:27:32 +0900

    feat : pick random numbers

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
+  pickNumbers() {
+    MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
+  }
+
   play() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
-    MissionUtils.Console.close();
+    const randoms = this.pickNumbers();
+    MissionUtils.Console.print(randoms);
+    return MissionUtils.Console.close();
   }
 }
 
 const app = new App();
 app.play();
+
 module.exports = App;
