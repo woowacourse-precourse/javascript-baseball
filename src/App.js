0cdbe910905538287d39e74eafb989f761308cc1
Gyeongm1n <affirming_km@naver.com>
2022-11-04 15:13:54 +0900

    feat: 1에서 9까지 서로 다른 임의의 수 3개 선택하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+const { Random, Console } = require('@woowacourse/mission-utils');
+
 class App {
-  play() {}
+  constructor() {
+    this.threeDigitsAnswer = [];
+  }
+
+  getThreeDigitsAnswer() {
+    while (this.threeDigitsAnswer.length < 3) {
+      const randomNumber = Random.pickNumberInRange(1, 9);
+      if (!this.threeDigitsAnswer.includes(randomNumber)) {
+        this.threeDigitsAnswer.push(randomNumber);
+      }
+    }
+  }
+
+  init() {
+    this.getThreeDigitsAnswer();
+    Console.print('숫자 야구 게임을 시작합니다.');
+  }
+
+  play() {
+    this.init();
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
