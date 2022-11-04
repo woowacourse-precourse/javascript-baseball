37432012d5f15d1d682ba79d80893947051b18a6
2wndrhs <2wndrhs@gmail.com>
2022-11-03 18:36:40 -0700

    feat: 사용자의 입력을 받는 기능 구현

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ const MissionUtils = require('@woowacourse/mission-utils');
 class App {
   #RANDOM_NUMBER = 0;
 
+  #userInput = 0;
+
   // 컴퓨터가 사용할 랜덤한 숫자를 구하는 기능
   static getRandomNumber() {
     const RANDOM_SET = new Set();
@@ -13,9 +15,16 @@ class App {
     return RANDOM_NUMBER;
   }
 
+  // 사용자의 입력을 받는 기능
+  setUserInput() {
+    MissionUtils.Console.readLine('숫자를 입력해주세요 : ', answer => {
+      this.#userInput = answer;
+    });
+  }
+
   play() {
     this.#RANDOM_NUMBER = App.getRandomNumber();
-    console.log(this.#RANDOM_NUMBER);
+    this.getUserInput();
   }
 }
 
