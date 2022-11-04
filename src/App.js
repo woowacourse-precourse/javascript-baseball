894465c82ca3f8aa9e5040559981d7f96df791ff
Raehyeon Jeong <raehy19@gmail.com>
2022-11-04 23:12:51 +0900

    feat(src/App.js) : 게임 시작 문구 출력

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 
+const GAME_START_MSG = '숫자 야구 게임을 시작합니다.';
+
 class App {
-  play() {}
+  play() {
+    MissionUtils.Console.print(GAME_START_MSG);
+  }
 
   generateAnswer() {
     return MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
