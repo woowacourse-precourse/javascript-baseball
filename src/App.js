831b6e85b03a72eeabb887904a255a25c2477ec6
doheeMac <thsehgml37@korea.ac.kr>
2022-11-04 15:19:58 +0900

    Feat: 사용자 입력 기능 추가

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ class App {
   play() {
     const givenNumber = makeGivenNumber();
     console.log("숫자 야구 게임을 시작합니다.");
+    const inputNumber = getInput();
   }
 }
 
@@ -16,6 +17,12 @@ const makeGivenNumber = () => {
     }
   }
   return givenNumber;
+};
+
+const getInput = () => {
+  MissionUtils.Console.readline("숫자를 입력해주세요 : ", (inputNumber) => {
+    return inputNumber;
+  });
 };
 
 module.exports = App;
