3aad76cdf4123362a56f9719d19d81ccb8600bf1
khj0426 <706shin1728@naver.com>
2022-11-04 20:05:50 +0900

    refactor:MissionUtils로 입력받고, 입력받은 값 리턴(콘솔닫아주는 기능 추가)

--- src/Input.js
+++ src/Input.js
@@ -1,15 +1,14 @@
 const MissionUtils = require('@woowacourse/mission-utils');
-
 /*
 MissionUtils의 Console로 숫자 입력받아서 입력받은 값 리턴
 */
 const Inputobj = {};
-Inputobj.GetInputNumber = function () {
-  let InputNumber;
-  MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (Number) => {
-    InputNumber = Number;
+Inputobj.InputStr = '';
+Inputobj.GetInput = function () {
+  MissionUtils.readLine('숫자를 입력해주세요 : ', (InputNumber) => {
+    Inputobj.InputStr = InputNumber;
   });
-  return InputNumber;
+  MissionUtils.Console.close();
 };
 
-module.exports = Inputobj;
+module.exports = Inputobj.InputStr;
