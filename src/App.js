ac4a21cf646cf948a744d3f45fc4d030a46f31f2
Quartz <skyu.dev@gmail.com>
2022-11-04 13:30:19 +0900

    feat: 출력기능 - 컴퓨너 숫자와 사용자 숫자 비교하여 결과 판단과 재시작 여부 로직

--- src/App.js
+++ src/App.js
@@ -14,11 +14,58 @@ class App {
     });
     return userNum;
   }
+  getResult(computer, user) {
+    let resultFlag = 1;
+    const { strike, ball } = this.compareNums(computer, user);
+    const resultSring = this.printResult(strike, ball);
+    if (resultSring === "3스트라이크") {
+      this.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+      this.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+      MissionUtils.Console.readLine(
+        "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
+        (answer) => {
+          resultFlag = answer;
+          this.print(`${answer}`);
+        }
+      );
+    }
+    return resultFlag;
+  }
+  compareNums(computer, user) {
+    let strike = 0;
+    let ball = 0;
+    for (let i = 0; i < user.length; i++) {
+      if (user[i] === computer[i]) {
+        strike++;
+      } else if (computer.includes(user[i])) {
+        ball++;
+      }
+    }
+    return { strike, ball };
+  }
+  printResult(strike, ball) {
+    let resultSring = "";
+    if (ball !== 0) {
+      resultSring += `${ball}볼 `;
+    }
+    if (strike !== 0) {
+      resultSring += `${strike}스트라이크`;
+    }
+    if (ball === 0 && strike === 0) {
+      resultSring = "낫싱";
+    }
+    this.print(resultSring);
+    return resultSring;
+  }
   play() {
     this.print("숫자 야구 게임을 시작합니다.");
-    const computerNum = this.pickComputerNum();
-    const userNum = this.getUserNum();
-    console.log(userNum);
+    let restartFlag = 1; // 1: restart, 2: end
+    while (restartFlag === 1) {
+      const computerNum = this.pickComputerNum();
+      const userNum = this.getUserNum();
+      console.log(computerNum, userNum);
+      restartFlag = this.getResult(computerNum.toString(), userNum);
+    }
   }
 }
 
