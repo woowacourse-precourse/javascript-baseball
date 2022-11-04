a383609999e6ecc2b38487995c6c1c2de4b9f724
jinseo <ghkdwlstj99@gmail.com>
2022-11-05 02:38:53 +0900

    feat: 입력값이 스트라이크일 경우 구현

---
+++ src/NumberBaseballReferee.js
@@ -0,0 +1,14 @@
+const { MissionUtils } = require("@woowacourse/mission-utils");
+
+function numberBaseballReferee(strike, ball){
+    allStrike(strike,ball);
+}
+
+function allStrike(strike, ball){
+    if(strike > 0 && ball == 0){
+        MissionUtils.Console.print(`${strike}` +스트라이크);
+    }
+    if(strike == 3){
+        MissionUtils.console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+    }
+}
