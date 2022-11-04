8e0ad6030355939c80124760d2028eb661b76a12
cys4585 <choizerowater@gmail.com>
2022-11-04 19:04:14 +0900

    fix: 기능5. 게임 재시작, 완전 종료 선택 기능

--- src/Game.js
+++ src/Game.js
@@ -43,9 +43,9 @@ const gameEnd = () => {
 
 const askRestartOrQuit = async () => {
   const answer = await readLine(
-    "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
+    "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n",
   );
-  return answer === 1 ? "restart" : "quit";
+  return answer === "1" ? "restart" : "quit";
 };
 
 const playGame = async () => {
