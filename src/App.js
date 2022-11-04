8edc7e7af06ca65f94ec7c7908d59f55fcd87a4f
ghenmaru <rbgks1937@naver.com>
2022-11-04 16:32:17 +0900

    feat: 입력에 따른 게임 재시작 및 종료

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
-
-
-function gameStart(){
+function startGame(){
   MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (answer) => {
-    console.log(getHint(answer));
+    // console.log(getHint(answer)); 힌트 출력
+    console.log('숫자 :', answer);
+    pickComputerNumber();
     MissionUtils.Console.close();
   });
 }
 
 function getHint(){
+  let hint = '';
+  return hint;
 }
+
+function endGame(){
+  console.log()
+  MissionUtils.Console.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. : ', (answer) => {
+    if (answer === '1'){
+      startGame();
+    } else if (answer === '2'){
+      MissionUtils.Console.close();
+      return;
+    } else{
+      MissionUtils.Console.print('다시 입력해주세요 !');
+      endGame();
+    }
+  });
+}
+
 
 function pickComputerNumber() {
   const computer = [];
@@ -23,7 +41,7 @@ function pickComputerNumber() {
 class App {
   play() {
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
-    gameStart();
+    startGame();
     // console.log('start game !');
     // const MissionUtils = require("@woowacourse/mission-utils");
     // console.log(MissionUtils.Random.pickNumberInList([1, 2, 3]));
@@ -37,5 +55,6 @@ class App {
 const MissionUtils = require("@woowacourse/mission-utils");
 
 const app = new App();
-app.play();
+// app.play();
+endGame();
 module.exports = App;
