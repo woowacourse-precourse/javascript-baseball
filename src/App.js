be328d44af286602b789e9870829218ceadb3811
devMingu <rooster100@naver.com>
2022-11-04 16:54:40 +0900

    feat: 기능구현 1번: 컴퓨터가 생각한 숫자를 가져오는 함수

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
+const getComputerInputOneByOne = () => {
+  let computerInput;
+  computerInput = MissionUtils.Random.pickNumberInRange();
+  return computerInput;
+}
+
+const getComputerInput = () => {
+  let inputString = "";
+  for (let i=0 ; i < 3 ; i++){
+    inputString += getComputerInputOneByOne();
+  }
+  return inputString;
+}
+
 class App {
-  play() {}
+  play() {
+  }
 }
 
 module.exports = App;
