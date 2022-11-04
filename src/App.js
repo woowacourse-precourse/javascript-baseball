c1e383fa98867a8eea5634ca0c61780082019c97
pione3r <jhyong0626@gmail.com>
2022-11-04 15:06:59 +0900

    feat: compareNumbers function
    added compareNumbers function
    - compare computerNumber with userNumber
    - return compare result which is nothing, strike, ball

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ const MissionUtils = require("@woowacourse/mission-utils");
 class App {
   play() {
     const computerNumber = setComputerNumber();
+    const userNumber = setUserNumber();
+    let compareResult = compareNumbers(computerNumber, userNumber);
   }
 }
 
@@ -28,6 +30,24 @@ function setUserNumber() {
   })
 
   return userNumber;
+}
+
+function compareNumbers(computerNumber, userNumber) {
+  let ball = 0;
+  let strike = 0;
+  let result = "";
+  for (let i = 0; i < computerNumber.length; i++) {
+    let num = Number(userNumber[i]);
+    if (computerNumber[i] == num) strike++;
+    if ((computerNumber[i] != num) && computerNumber.includes(num)) ball++;
+  }
+
+  if (ball === 0 && strike === 0) result = "낫싱";
+  if (ball === 0 && strike !== 0) result = `${strike}스트라이크`;
+  if (ball !== 0 && strike === 0) result = `${ball}볼`;
+  if (ball !== 0 && strike !== 0) result = `${ball}볼 ${strike}스트라이크`;
+
+  return result;
 }
 
 module.exports = App;
