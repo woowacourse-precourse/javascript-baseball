855c0d09ae042f23e98d2bfb1b9a8c22a7f21cfe
sujungYu <su_moment@naver.com>
2022-11-04 15:09:24 +0900

    feat(getRandomNumber) : 3자리 랜덤 값 생성

--- src/App.js
+++ src/App.js
@@ -1,4 +1,15 @@
 class App {
+  getRandomNumber() {
+    const MissionUtils = require('@woowacourse/mission-utils');
+    const computer = [];
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+    return computer;
+  }
   play() {}
 }
 
