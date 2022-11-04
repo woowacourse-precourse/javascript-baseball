b694e2178436b4df3ec24f8331413fdab997871b
turtle601 <turtle601@naver.com>
2022-11-04 12:39:34 +0900

    feat(app) 임의의 서로 다른 3가지 랜덤값을 가져오는 함수 구현
    Add:
    <App클래스>
    - get3RandomNumbers()

--- src/App.js
+++ src/App.js
@@ -1,4 +1,23 @@
+const { Random } = require('@woowacourse/mission-utils');
+
+const RANDOMLIST = Object.freeze({
+  STARTPOINT: 1,
+  ENDPOINT: 9,
+  COUNT: 3,
+});
+
 class App {
+  #random;
+
+  constructor() {
+    const { STARTPOINT, ENDPOINT, COUNT } = RANDOMLIST;
+    this.#random = Random.pickUniqueNumbersInRange(STARTPOINT, ENDPOINT, COUNT);
+  }
+
+  get3RandomNumbers() {
+    return this.#random;
+  }
+
   play() {}
 }
 
