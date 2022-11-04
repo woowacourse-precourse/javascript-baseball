3f8ff18c55e8ad944602ee6cc7ead468e660496c
Lee-Young-Jae <yjlee1937@naver.com>
2022-11-05 00:33:33 +0900

    feat: 컴퓨터가 임의의 수(정답)를 발생시키는 메서드 생성

--- src/App.js
+++ src/App.js
@@ -1,4 +1,18 @@
 class App {
+  constructor() {
+    this.answer = this.makeAnswer();
+  }
+
+  makeAnswer() {
+    const answer = [];
+    while (computer.length < 3) {
+      const number = Random.pickNumberInRange(1, 9);
+      if (!answer.includes(number)) {
+        answer.push(number);
+      }
+    }
+    return answer;
+  }
   play() {}
 }
 
