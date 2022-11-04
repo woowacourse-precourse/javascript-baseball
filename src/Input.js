0b2071f1d3afddf7ff4fa919cb6a337f86d7d329
yan9vvoojin <yan9vvoojin@naver.com>
2022-11-05 00:10:56 +0900

    feat: [Input.js] 주어진 배열의 모든 요소가 1과 9사이인지 확인하는 기능인 isBetweenOneAndNine 추가

--- src/Input.js
+++ src/Input.js
@@ -6,6 +6,10 @@ class Input {
   static isLengthThree(numbers) {
     return numbers.length === 3;
   }
+
+  static isBetweenOneAndNine(numbers) {
+    return numbers.every((number) => number >= 1 && number <= 9);
+  }
 }
 
 module.exports = Input;
