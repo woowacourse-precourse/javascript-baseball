cc43e1848b3f0130a553bfed21d103b187cca783
Tory <88531407+1two13@users.noreply.github.com>
2022-11-04 21:57:01 +0900

    wrench: GameManager 파일, src/util 폴더 삭제

--- src/util/input.js
+++
@@ -1,9 +0,0 @@
-const { Console } = require("@woowacourse/mission-utils");
-
-function input(comment) {
-  return new Promise((resolve) => {
-    Console.readLine(comment, resolve);
-  });
-}
-
-module.exports = input;
