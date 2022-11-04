e46a711bfa061a62097f5bd41f09dfb2cabf9e7d
jhyns <tockiib@gmail.com>
2022-11-04 22:54:42 +0900

    feat: Add target number generation
    게임의 정답으로 쓰일 세 숫자를 담은 배열을 반환하는 함수 추가
    숫자는 배열에 담겨있으며 각각 중복되지 않음

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+
 class App {
-  play() {}
+  static generateTargetNumber() {
+    const target = [];
+
+    while (target.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!target.includes(number)) target.push(number);
+    }
+    return target;
+  }
 }
 
 module.exports = App;
