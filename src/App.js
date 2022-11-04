30ebdb8de8b19fe438d37cefd148e599959251a1
kimeuns <dmstn2006@gmail.com>
2022-11-05 01:32:39 +0900

    feat: 사용자 숫자 입력 기능 생성
    입력값을 확인하여 예외 처리하는 함수를 추가함
    숫자인지, 개수가 맞는지, 중복되는 숫자가 없는지

--- src/App.js
+++ src/App.js
@@ -2,13 +2,28 @@ const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
   resultNumbers;
-  constructor(resultNumbers) {
+  constructor() {
     this.setNumbers();
   }
 
+  // 숫자인지, 겹치는게 없는지, 3개만 입력했는지, 
+  checkNumbers(inputNumber){
+    if (isNaN(inputNumber)) {
+      throw "숫자가 아닙니다.";
+    }
+    if (inputNumber.length > 3) {
+      throw new Error("숫자가 초과했습니다.(3개만 입력)");
+    }
+    if ([...new Set(inputNumber.split(""))].length !== 3) {
+      console.log([...new Set(inputNumber.split[""])]);
+      throw "중복되는 숫자가 입력되었습니다.";
+    }
+  }
+  
   setNumbers() {
     this.resultNumbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
   }
+
   play() {}
 }
 
