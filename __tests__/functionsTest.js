8c7846844170a8eaf7a0dac34a0e028636b80298
YeonsuBaek <soothbugger@gmail.com>
2022-11-04 14:22:38 +0900

    test: 사용자 숫자 입력 받기 테스트 완료

--- __tests__/functionsTest.js
+++ __tests__/functionsTest.js
@@ -8,6 +8,15 @@ const mockRandoms = (numbers) => {
   }, MissionUtils.Random.pickNumberInRange);
 };
 
+const mockQuestions = (answers) => {
+  MissionUtils.Console.readLine = jest.fn();
+  answers.reduce((acc, input) => {
+    return acc.mockImplementationOnce((question, callback) => {
+      callback(input);
+    });
+  }, MissionUtils.Console.readLine);
+};
+
 describe("야구 게임 테스트", () => {
   test("시작 메세지 출력", () => {
     const logSpy = jest.spyOn(console, 'log');
@@ -28,6 +37,17 @@ describe("야구 게임 테스트", () => {
     const result = app.selectComputer(); 
 
     expect(result).toEqual("123");
+  });
+
+  test("사용자 숫자 입력 받기", () => {
+    const userNumber = ["456"];
+
+    mockQuestions(userNumber);
+
+    const app = new App();
+    const result = app.selectUser(); 
+
+    expect(result).toEqual("456");
   });
 });
 
