484f2d5ed42aa6accd322bdf05858b2f36ce032f
Tory <88531407+1two13@users.noreply.github.com>
2022-11-04 10:41:23 +0900

    refactor(game): class 함수로 변경, COMPUTER =>  pickedNumberByComputer로 변수명 변경

--- src/game.js
+++ src/game.js
@@ -1,26 +1,29 @@
 const { Console } = require("@woowacourse/mission-utils");
 const pickedNumberByComputer = require("./pickedNumberByComputer");
-const enterNumber = require("./enterNumber");
+const input = require("./util/input");
 const countStrikeBallNothing = require("./countStrikeBallNothing");
 const resultsForCount = require("./resultsForCount");
 const GameManager = require("./GameManager");
 
-async function game() {
-  const COMPUTER = pickedNumberByComputer();
-  console.log(COMPUTER);
-  let threeStrike = false;
-  while (!threeStrike) {
-    let pickedNumberByUser = await enterNumber("숫자를 입력해주세요 : ");
-    let [strike, ball, nothing] = countStrikeBallNothing(
-      pickedNumberByUser,
-      COMPUTER
-    );
-    resultsForCount(strike, ball, nothing);
-
-    if (strike === 3) threeStrike = true;
+class Game {
+  constructor() {
+    this.pickedNumberByComputer = pickedNumberByComputer();
+    this.threeStrike = false;
   }
-  Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-  new GameManager().restartOrEnd();
+
+  async isThreeStrike() {
+    while (!this.threeStrike) {
+      let pickedNumberByUser = await input("숫자를 입력해주세요 : ");
+      let [strike, ball, nothing] = countStrikeBallNothing(
+        pickedNumberByUser,
+        this.pickedNumberByComputer
+      );
+      resultsForCount(strike, ball, nothing);
+      if (strike === 3) this.threeStrike = true;
+    }
+    Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    new GameManager().restartOrEnd();
+  }
 }
 
-module.exports = game;
+module.exports = Game;
