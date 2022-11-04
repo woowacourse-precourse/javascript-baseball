3c8ef1885b2e61326957428bdc80f16cb85dcdc3
khkh0109 <ist200120@gmail.com>
2022-11-04 15:57:18 +0900

    feat: 숫자 비교하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
 class App {
-  play() {}
+  play() {
+
+  }
 }
 
+
+function comparingNumbers(inputNum, randomNum) {
+  const result = [0, 0];
+  const inputNumbers = inputNum.split('').map(item => +item);
+  const randomNumbers = String(randomNum).split('').map(item => +item);
+  randomNumbers.forEach((number, index) => {
+    const inputNumIndex = inputNumbers.indexOf(number);
+    if(inputNumIndex === -1) {
+      result[0] += 0;
+    } else if(inputNumIndex === index) {
+      result[1]++;
+    } else {
+      result[0]++;
+    }
+  });
+  return result;
+}
+
+const app = new App();
+app.play();
 module.exports = App;
+
+
+
