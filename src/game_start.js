145b8860cbf92e0e5d25ca06d6879edac43858ab
jinseo <ghkdwlstj99@gmail.com>
2022-11-05 02:20:06 +0900

    feat: 사용자 입력 시  숫자가 3자리수가 아닌 경우 구현

--- src/game_start.js
+++ src/game_start.js
@@ -1,5 +1,12 @@
 const { MissionUtils } = require("@woowacourse/mission-utils");
 
-function game_start(answer){
+function gameStart(answer){
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+    three_digits_error(answer);
+}
+
+function threeDigitsError(answer){
+    if(answer.length !==3){
+        throw "Error" ;
+    }
 }
