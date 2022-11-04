1effe47aa49d09e03cc8ade8e7082de1aa560d27
Jaehyeon <jaehyeonp74@gmail.com>
2022-11-04 22:31:39 +0900

    feat: 숫자 야구게임 시작, 종료 문구 알리는 메서드 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+
 class App {
-  play() {}
+  play() {
+    this.start();
+  }
+
+  start() {
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+  }
+
+  close() {
+    MissionUtils.Console.close();
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
