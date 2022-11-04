0618240a9a07655ec1bbbf58090b28538c5c60e0
2jaebbang <72256238+2jaebbang@users.noreply.github.com>
2022-11-05 06:14:44 +0900

    comment: 주석 제거

--- src/validation.js
+++ src/validation.js
@@ -1,16 +1,13 @@
-// 3자리 숫자인지 판별
 function isThreeDigitsNumber(inputNumbers) {
   return inputNumbers.length === 3;
 }
 
-// 입력값이 모두 정수 형태의 숫자인지 판별
 function isIntegerNumber(inputNumbers) {
   return inputNumbers.every((inputNumber) =>
     Number.isInteger(parseInt(inputNumber, 10))
   );
 }
 
-// 중복되는 숫자가 없는지 판별
 function isExistDuplicatedNumber(inputNumbers) {
   const inputNumbersSet = new Set(inputNumbers);
   return inputNumbers.length === inputNumbersSet.size;
