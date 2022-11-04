4f79a39d0bbd06169bda0803ab78b48830cdd15d
jinseo <ghkdwlstj99@gmail.com>
2022-11-05 02:40:11 +0900

    feat: 입력값이 볼일 경우 구현

--- src/NumberBaseballReferee.js
+++ src/NumberBaseballReferee.js
@@ -2,6 +2,7 @@ const { MissionUtils } = require("@woowacourse/mission-utils");
 
 function numberBaseballReferee(strike, ball){
     allStrike(strike,ball);
+    allBall(strike, ball);
 }
 
 function allStrike(strike, ball){
@@ -10,5 +11,11 @@ function allStrike(strike, ball){
     }
     if(strike == 3){
         MissionUtils.console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+    }
+}
+
+function allBall(strike, ball){
+    if(strike == 0 && ball > 0){
+        MissionUtils.Console.print(`${ball}` + 볼);
     }
 }
