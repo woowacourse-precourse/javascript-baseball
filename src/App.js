38578d1f5b0c9b7e1eef670b0e3a348ecf5b0f73
LEEJW1953 <jiwonlee1953@naver.com>
2022-11-04 17:07:58 +0900

    feat: 컴퓨터가 3개의 숫자를 선택하는 메서드 추가
    컴퓨터가 1부터 9 사이의 서로다른 3개의 숫자를 정하는 메서드를
    추가했습니다.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
+  computerPick() { //컴퓨터가 1부터 9 사이의 서로다른 3개의 숫자를 정하는 메서드
+    const COMPUTER=MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
+    MissionUtils.Console.close();
+    return COMPUTER;
+  }
   play() {}
 }
 
