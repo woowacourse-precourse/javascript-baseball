bdb76cbee245437f1133a6d17238d33d01438eeb
jiseung-kang <ill0o0lli@ajou.ac.kr>
2022-11-04 21:30:32 +0900

    feat(App): 게임시작 시 초기 세팅 구현
    App 인스턴스를 생성하고 play() 메서드를 실행하면 다음을 실행하도록 구현
    - computer, user, strike, ball 초기화
    - computer값을 랜덤한 3자리 수의 문자열로 초기화
    
    constructor에서 초기화된 computer, user, strike, ball을 사용해 로직을 구현해야 한다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+const MissionUtils = require('@woowacourse/mission-utils');
 class App {
-  play() {}
+  constructor() {
+    this.computer = '';
+    this.user = '';
+    this.strike = 0;
+    this.ball = 0;
+  }
+
+  play() {
+    this.startGame();
+  }
+
+  startGame() {
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+    this.setComputer();
+    MissionUtils.Console.close();
+  }
+
+  setComputer() {
+    while (this.computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9) + '';
+      if (!this.computer.includes(number)) {
+        this.computer += number;
+      }
+    }
+  }
 }
 
 module.exports = App;
