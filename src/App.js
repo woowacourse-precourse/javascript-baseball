ebcc4800ff9efa22c81ea4e124678d1989e4b207
ksg2388 <ksg2388@naver.com>
2022-11-04 12:54:10 +0900

    feat(App): checkResult 함수 추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 
+const checkResult = (computer, user) => {
+  let count = 0;
+  const test = user.split('').map((a) => Number(a));
+  for (let i = 0; i < 3; i++) {
+    if (computer[i] !== test[i]) break;
+    count += 1;
+  }
+  if (count === 3) {
+    MissionUtils.Console.close();
+  } else {
+    MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (user) => checkResult(computer, user));
+  }
+};
+
 class App {
   play() {
     const computer = [];
+
     while (computer.length < 3) {
       const number = MissionUtils.Random.pickNumberInRange(1, 9);
       if (!computer.includes(number)) {
@@ -11,6 +26,7 @@ class App {
     }
 
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+    MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (user) => checkResult(computer, user));
   }
 }
 
