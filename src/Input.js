0fed1c507821be0f40181a8e9b98d295af208934
yan9vvoojin <yan9vvoojin@naver.com>
2022-11-05 00:09:10 +0900

    feat: [Input.js] 주어진 배열의 길이가 3인지 확인하는 기능인 isLengthThree 추가

--- src/Input.js
+++ src/Input.js
@@ -2,6 +2,10 @@ class Input {
   static toNumbers(input) {
     return input.split('').map((char) => Number(char));
   }
+
+  static isLengthThree(numbers) {
+    return numbers.length === 3;
+  }
 }
 
 module.exports = Input;
