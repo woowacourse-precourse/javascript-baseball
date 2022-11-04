901c2d22838b4db00039ff532394e15ad968fbf2
kyw0716 <kyw0716@naver.com>
2022-11-04 21:45:32 +0900

    test: 힌트 산출, 입력값 유효성 체크 테스트 코드 수정
    BaseballGame의 메서드 => 각 모듈의 함수

--- __tests__/InputValidTest.js
+++ __tests__/InputValidTest.js
@@ -1,13 +1,12 @@
-const BaseballGame = require("../src/BaseballGame");
+const getIsInputValueValid = require("../src/ValidCheck");
 
 describe("입력값 유효 체크 테스트", () => {
   // TODO: 게임 종료 후 입력값 유효 테스트 추가하기
   test("입력값이 유효한 값인지", () => {
-    const game = new BaseballGame();
     const testWordsForInvalid = ["1234", "122", "12d"];
     testWordsForInvalid.forEach((testWord) => {
-      expect(game.getIsInputValueValid(testWord)).toEqual(false);
+      expect(getIsInputValueValid(testWord)).toEqual(false);
     });
-    expect(game.getIsInputValueValid("123")).toEqual(true);
+    expect(getIsInputValueValid("123")).toEqual(true);
   });
 });
