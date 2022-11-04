f571cd22ab49486db2ca45da3b21acf06ec0ca01
khj0426 <706shin1728@naver.com>
2022-11-04 17:17:21 +0900

    feat:입력 받은 문자열이 3글자이고, 모두 서로 다른지 확인하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,45 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+const Random = require('./Random');
+const IsInputIsValid = require('./IsValidInput');
+
 class App {
-  play() {}
+  constructor() {
+    this.Number = Random;
+    this.strike = 0;
+    this.ball = 0;
+  }
+  UserInputNumber = '';
+
+  play() {
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+    this.input();
+  }
+  input() {
+    MissionUtils.Console.readLine('숫자를 입력하세요 : ', (InputNumber) => {
+      this.UserInputNumber = InputNumber;
+    });
+    MissionUtils.Console.print(this.UserInputNumber);
+    MissionUtils.Console.close();
+  }
+  CheckInputIsValid() {
+    /*
+    Compare Number and UserInputNumber
+    */
+    /*
+    if UserInputNumber`s length is not equal three
+    */
+    const CheckSet = new Set();
+    const UserInputArray = [...this.UserInputNumber];
+    UserInputArray.forEach((EachChar) => {
+      CheckSet.add(EachChar);
+    });
+    if (CheckSet.size !== 3) {
+      throw new Error('Input must be 3 different charater');
+    }
+    return true;
+  }
 }
 
+const app = new App();
+app.play();
 module.exports = App;
