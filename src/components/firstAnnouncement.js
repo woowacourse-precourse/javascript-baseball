0f61af9f6b4f82ab4e574785729b3284ebf46f07
Ryan-Dia <cjfdnjs158@naver.com>
2022-11-04 23:35:49 +0900

    feat(첫멘트):숫자 야구 게임을 시작합니다  멘트 출력하는 함수 추가

---
+++ src/components/firstAnnouncement.js
@@ -0,0 +1,7 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
+const firstAnnouncement = () => {
+  MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+};
+
+module.exports = firstAnnouncement;
