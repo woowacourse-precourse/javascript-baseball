62196ae10be3ce32837ae76c05348658247545e7
LEEJW1953 <jiwonlee1953@naver.com>
2022-11-05 04:01:52 +0900

    feat: 플레이어의 숫자가 잘못된 값인지 확인하는 메서드 추가
    플레이어가 입력한 숫자가 서로 다른 3개의 숫자인지 확인하는 메서드를
    추가했습니다.

--- src/App.js
+++ src/App.js
@@ -7,14 +7,21 @@ class App {
     return COMPUTER;
   }
   playerPick() { // 게임 플레이어의 입력을 받는 메서드
-    const PLAYER = MissionUtils.Console.readLine('숫자를 입력해주세요 :', (input) => {
+    const PLAYER = MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (input) => {
       console.log(`숫자 : ${input}`);
     });
     MissionUtils.Console.close();
     return PLAYER;
   }
+  checkNumber(num) { // 플레이어가 입력한 숫자가 서로 다른 3개의 숫자인지 확인하는 메서드
+    if (num.length!==3) {
+      return false;
+    } else if ((num[0]===num[1]) || (num[0]===num[2]) || (num[1]===num[2])) {
+      return false;
+    }
+    return true;
+  }
   play() {
-    console.log('숫자 야구 게임을 시작합니다.');
   }
 }
 
