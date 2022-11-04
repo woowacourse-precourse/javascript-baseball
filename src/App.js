6ab2dce16bc353f8efc9ab3fee1ca961d41b04ca
Subin Jo <sbjo1202@naver.com>
2022-11-05 02:12:31 +0900

    feat: 기능 2 3자리 수 생성하기 구현

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ class App {
   play() {
     PrintGameStartPhrase();
 
+    const computerNumber = makeComputerNumber();
+
+
   }
 }
 
@@ -12,6 +15,20 @@ function PrintGameStartPhrase() {
   MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
 }
 
+// 기능 2
+function makeComputerNumber() {
+  const computerNumberList = [];
+
+  while (computerNumberList.length < 3) {
+    const randomNumber = MissionUtils.Random.pickNumberInRange(1, 9);
+
+    if (!(computerNumberList.includes(randomNumber))) {
+      computerNumberList.push(randomNumber);
+    }
+  }
+
+  return computerNumberList.join("");
+}
 
 
 const baseballGame = new App();
