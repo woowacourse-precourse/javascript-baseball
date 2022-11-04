518f16576249c878d1605de2455ed1a8182e7d2a
Gaeun-Jang <110662057+Gaeun-Jang@users.noreply.github.com>
2022-11-04 13:41:00 +0900

    refactor : computer 배열 위치 수정

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
-    constructor() {}
+    constructor() {
+        this.computer = [];
+    }
 
     getRandomNumber() {
-        const computer = [];
-        while (computer.length < 3) {
+        while (this.computer.length < 3) {
             const number = MissionUtils.Random.pickNumberInRange(1, 9);
-            if (!computer.includes(number)) {
-                computer.push(number);
+            if (!this.computer.includes(number)) {
+                this.computer.push(number);
             }
         }
         MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
