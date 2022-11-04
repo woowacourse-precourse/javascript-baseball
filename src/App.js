907ce2ce83b878754b8f2656ddeac79fe25b1350
SNUPI <joohaem@daum.net>
2022-11-04 22:38:55 +0900

    [ docs ] 실행 FLow 및 구현 사항 작성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+// CHECK :: 레포지터리 :: https://github.com/joohaem/javascript-baseball
+// CHECK :: 1주차 피드백 체크
+// CHECK :: 커밋 컨벤션 :: https://gist.github.com/stephenparish/9941e89d80e2bc58a153#format-of-the-commit-message
+// CHECK :: Random 값 추출 / Console 활용 -> MissionUtils 라이브러리
+
 class App {
   play() {}
 }
 
 module.exports = App;
+
+const app = new App();
+app.play();
