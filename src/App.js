cd1b2667550a2de863a5eeeed7d44282b700988b
gunyongbok <qhrrjsdyd123@gmail.com>
2022-11-04 17:31:43 +0900

    feat: 플레어이의 3개의 숫자를 입력받고 출력해주기
    readLine 을 사용하여 숫자를 입력받고 reuturn해준다.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ class App {
     }
     play() {
         this.pickRandomThreeNumbers();
+        this.pickRandomNumberUser();
     }
 
     // 랜덤한 3개의 숫자를 뽑는 함수
@@ -15,6 +16,14 @@ class App {
             numberSet.add(MissionUtils.Random.pickNumberInRange(1, 9));
         }
         console.log(Array.from(numberSet));
+        return Array.from(numberSet);
+    }
+
+    // 유저의 숫자를 입력받는 함수
+    pickRandomNumberUser() {
+        MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (answer) => {
+            console.log(answer);
+        });
     }
 }
 let app = new App();
