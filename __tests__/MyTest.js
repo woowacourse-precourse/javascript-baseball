034d09c69d1526aac902bdc538c54571d8fac8dc
Dean <yjj052800@gmail.com>
2022-11-04 20:40:35 +0900

    test: 사용자, 상대방 숫자 비교 기능 테스트

--- __tests__/MyTest.js
+++ __tests__/MyTest.js
@@ -15,3 +15,9 @@ test("입력값 제한 사항 체크", () => {
   const startText = app.inputCheck("123");
   expect(startText).toEqual([1, 2, 3]);
 });
+
+test("사용자 숫자 상대방 숫자 비교", () => {
+  const app = new App();
+  const resultText = app.compare([1, 2, 3], [5, 6, 4]);
+  expect(resultText).toEqual("낫싱");
+});
