b38343db32943febcaa71904f34fe3f8720020b1
Tory <88531407+1two13@users.noreply.github.com>
2022-11-04 15:53:05 +0900

    refactor(GameManager): restartOrEnd => inputRestartOrEnd로 함수명 변경 및 코드 추가

--- src/gameManager.js
+++ src/gameManager.js
@@ -12,11 +12,20 @@ class GameManager {
     Console.print("숫자 야구 게임을 시작합니다.");
     while (this.restart) {
       await this.game.playGame();
+      await this.inputRestartOrEnd();
     }
   }
 
-  async restartOrEnd() {
-    await input("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n");
+  async inputRestartOrEnd() {
+    const SELECTEDNUMBER = Number(
+      await input("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n")
+    );
+
+    if (SELECTEDNUMBER === 1) this.game = new BaseballGame();
+    else if (SELECTEDNUMBER === 2) {
+      this.restart = false;
+      Console.close();
+    } else throw new Error("1 또는 2만 입력해주세요.");
   }
 }
 
