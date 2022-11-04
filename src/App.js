e2662ca977a1c26573b1f1aec0b8cca465f165dd
jiheon788 <jiheon788@ajou.ac.kr>
2022-11-04 18:15:56 +0900

    feat: add func to create random number of computer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
-class App {
-  play() {}
+const MissionUtils = require("@woowacourse/mission-utils");
+
+const getRandomNumber = (count) => {
+  const computer = [];
+  while (computer.length < count) {
+    const number = MissionUtils.Random.pickNumberInRange(1, 9);
+    if (!computer.includes(number)) {
+      computer.push(number);
+    }
+  }
+
+  return computer;
 }
 
+class App {
+  play() {
+    const computer = getRandomNumber(3);
+
+    console.log(computer);
+
+    MissionUtils.Console.close();
+  }
+}
+
+const app = new App();
+app.play();
+
 module.exports = App;
+
