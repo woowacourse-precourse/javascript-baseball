ad620063416cdd131a0d7b4e11b4ea06657afc74
ReturnReason <reasonz@daum.net>
2022-11-04 19:30:04 +0900

    refactor: 문자열 백틱에서 작은 따옴표로 변경

--- src/common/constants.js
+++ src/common/constants.js
@@ -8,8 +8,8 @@ const GAME_RESULT = {
   nothing: '낫싱',
 };
 
-const START_MESSAGE = `숫자 야구 게임을 시작합니다.`;
-const INPUT_MESSGAE = `숫자를 입력해주세요 : `;
+const START_MESSAGE = '숫자 야구 게임을 시작합니다.';
+const INPUT_MESSGAE = '숫자를 입력해주세요 : ';
 const END_MESSAGE = `${MAX_LENGTH}개의 숫자를 모두 맞히셨습니다! 게임 종료
 게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.`;
 
