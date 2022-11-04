9d668c99e329352f0705eb7d668ac04cabc0e8cc
Chanwoong1 <jcw7468@gmail.com>
2022-11-04 20:39:10 +0900

    feat: 1-1 구현완료 (게임 시작 문구 출력)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+const { Console } = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+
+  play() {
+    this.startGame();
+  }
+
+  async startGame() {
+    Console.print("숫자 야구 게임을 시작합니다.");
+    await this.baseballGameStart();
+    Console.close();
+  }
+
+
 }
 
 module.exports = App;
+
+function check() {
+  const app = new App();
+
+  app.play();
+  return 0;
+}
+
+check();
