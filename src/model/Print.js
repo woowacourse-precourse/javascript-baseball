8a3c25a4e841254ae23bcb1f527a52cdd5050a36
Sumin Kim <ksumin0730@hufs.ac.kr>
2022-11-04 13:09:15 +0900

    Refactor: 변수명 변경, 필요없는 형변환 제거

--- src/model/Print.js
+++ src/model/Print.js
@@ -1,16 +1,18 @@
 const constant = require('../Constants');
 
 function printAnswer(resultList){
-    if (resultList[0] === 0 && resultList[1] === 0){
+    const [strike, ball] = resultList;
+    
+    if (strike === 0 && ball === 0){
       return constant.RESULTS.NOTHING;
     }
-    else if (resultList[0] === 0 && resultList[1] !== 0){
-      return String(resultList[1])+constant.RESULTS.BALL;
+    else if (strike === 0 && ball !== 0){
+      return String(ball)+constant.RESULTS.BALL;
     }
-    else if (resultList[0] !== 0 && resultList[1] === 0){
-      return String(resultList[0])+constant.RESULTS.STRIKE;
+    else if (strike !== 0 && ball === 0){
+      return String(strike)+constant.RESULTS.STRIKE;
     }
-    return String(String(resultList[1])+constant.RESULTS.BALL+' '+String(resultList[0])+constant.RESULTS.STRIKE);
+    return String(ball)+constant.RESULTS.BALL+' '+String(strike)+constant.RESULTS.STRIKE;
 }
 
 module.exports = printAnswer;
