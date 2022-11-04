1d8f04e686c5a7378ca4152ad7bbca483bd34199
peacemaker474 <81507322+peacemaker474@users.noreply.github.com>
2022-11-04 20:42:27 +0900

    Feat: 0과 실수에 대한 예외처리 추가

--- src/error.js
+++ src/error.js
@@ -12,24 +12,36 @@ const checkOverlapNumbers = (userInput) => {
     return checkError;
 }
 
-const checkOnlyNumbers = (userInput) => userInput.every(v => parseInt(v));
+const checkOnlyNumbers = (userInput) => userInput.every(v => +v || v === '0');
 const checkNumbersLength = (userInput) => userInput.length > 3;
+const checkZeroNumber = (userInput) => userInput.some(v => v === '0');
+const checkFloatNumbers = (userInput) => userInput.some(v => !Number.isInteger(+v));
 
 function validateNumbers(userInput) {
 
-    if (checkNumbersLength(userInput)) {
-        Console.print("3개 이상의 숫자를 입력하셨습니다. 게임을 다시 시작해주세요.");
-        throw Error("over Length");
-    }
-
     if (checkOverlapNumbers(userInput)) {
         Console.print("중복되는 숫자를 입력하셨습니다. 게임을 다시 시작해주세요.");
-        throw Error("overlap");
+        throw Error("Overlap Number");
     }
 
     if (!checkOnlyNumbers(userInput)) {
         Console.print("숫자가 아닌 값을 입력하셨습니다. 게임을 다시 시작해주세요.");
-        throw Error("not number");
+        throw Error("Not Number");
+    }
+
+    if (checkZeroNumber(userInput)) {
+        Console.print("0을 입력하셨습니다. 게임을 다시 시작해주세요.");
+        throw Error("Zero Number");
+    }
+
+    if (checkFloatNumbers(userInput)) {
+        Console.print("실수를 입력하셨습니다. 게임을 다시 시작해주세요.");
+        throw Error("Float Number")
+    }
+
+    if (checkNumbersLength(userInput)) {
+        Console.print("3개 이상의 숫자를 입력하셨습니다. 게임을 다시 시작해주세요.");
+        throw Error("Over Length");
     }
 }
 
