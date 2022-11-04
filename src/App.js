85379c76797e3ab778c8759f34fc93f06959cf1b
pangkyu <baesense@naver.com>
2022-11-04 21:15:14 +0900

    refactor : mission-utils 라이브러리 사용으로 인한 일부 코드 리팩토링
    mission-utils라이브러리 Console.print활용을 하기 위해 일부코드를 변경했음

--- src/App.js
+++ src/App.js
@@ -1,30 +1,28 @@
-const MissionUtils = require("@woowacourse/mission-utils");
+const { Console, Random } = require("@woowacourse/mission-utils");
 
 function checkAnswer(input) {}
 
 function getAnswer() {
   const computer = [];
-  while (computer.length < 3) {
-    const number = MissionUtils.Random.pickNumberInRange(1, 9);
-    if (!computer.includes(number)) {
-      computer.push(number);
-    }
-  }
+  // while (computer.length < 3) {
+  //   const number = MissionUtils.Random.pickNumberInRange(1, 9);
+  //   if (!computer.includes(number)) {
+  //     computer.push(number);
+  //   }
+  // }
 
   return computer;
 }
 
-function play() {
-  const answerNumber = getAnswer();
-  console.log("숫자 야구 게임을 시작합니다.");
-  console.log(answerNumber);
-  df;
+class App {
+  play() {
+    const answerNumber = getAnswer();
+    Console.print("숫자 야구게임을 시작합니다.");
+    console.log(answerNumber);
+  }
 }
 
-play();
-getAnswer();
-// class App {
-//   play() {}
-// }
+const app = new App();
+app.play();
 
-// module.exports = App;
+module.exports = App;
