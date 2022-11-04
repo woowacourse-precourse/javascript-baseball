41e2dc7bc9f4b56519b6c8aa009181c6ac4221ba
YejinHwang-D <hyj3463@naver.com>
2022-11-04 17:58:28 +0900

    feat: 기능 목록 2번 구현

--- src/App.js
+++ src/App.js
@@ -14,13 +14,37 @@ class App {
 function createRandom() {
   let randomNumber = [];
   let tempForRandom;
+  let userInput;
 
   while (randomNumber.length < 3) {
     tempForRandom = MissionUtils.Random.pickNumberInRange(1,9);
     randomNumber.push(tempForRandom);
-  }
+  } 
 
   return randomNumber;
+}
+
+function checkUserInput(userInput) {
+  let userInput;
+
+  userInput = input('숫자를 입력해주세요 : ');
+  if (userInput.length !== 3) {
+    throw new Error();
+  }
+  let userInputStr = userInput.split("");
+  userInput = userInputStr.map((val) => Number(val));
+
+  return userInput;
+}
+
+function input(text) {
+  let result = 0;
+  MissionUtils.Console.readLine('입력', (answer) => {
+    MissionUtils.Console.print(`${text}${answer}`);
+    result = answer;
+  });
+
+  return result;
 }
 
 module.exports = App;
