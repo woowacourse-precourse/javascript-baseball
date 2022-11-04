8fc836dbb40bb66f1d8753cce929b7f3aa401d93
Jeong seulho <wjdtmfgh@yu.ac.kr>
2022-11-04 18:04:09 +0900

    Feat(feature No.2): get number from player
    define startGame() function and pass number to checkNum() function

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ const MissionUtils = require("@woowacourse/mission-utils");
 class App {
   play() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
-    this.inputNum();
+    this.startGame();
+  }
+
+  startGame() {
+    MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (num) => {
+      this.checkNum(num);
+    });
   }
 }
 
