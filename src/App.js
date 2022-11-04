4babe9e2b4c7d771f7962b1dee5976d5a02a1f55
Hoon <si932174@gmail.com>
2022-11-04 17:55:04 +0900

    feat : 입력받은 길이3의 배열 2개로 판정하기

--- src/App.js
+++ src/App.js
@@ -31,13 +31,37 @@ const stringToArrConverter = (numbersString) => {
   return numbersNumberArray;
 };
 
+const discriminator = (userNumbers, refNumbers) => {
+  let discrimination = "";
+
+  let strikeCount = 0;
+  let ballCount = 0;
+
+  for (let i = 0; i < 3; i++) {
+    if (refNumbers.indexOf(userNumbers[i]) === i) {
+      strikeCount = strikeCount + 1;
+    } else if (refNumbers.includes(userNumbers[i])) {
+      ballCount = ballCount + 1;
+    }
+  }
+
+  discrimination =
+    (ballCount ? `${ballCount}볼` : "") +
+      (strikeCount ? `${strikeCount}스트라이크` : "") || "낫싱";
+
+  return discrimination;
+};
+
 class App {
   async play() {
     printer("숫자 야구 게임을 시작합니다.");
     const refNumbersArr = refNumbersGetter();
+    printer(refNumbersArr);
     const usersInput = await userNumbersGetter();
     const userNumbersArr = stringToArrConverter(usersInput);
+    const discrimination = discriminator(userNumbersArr, refNumbersArr);
     printer(userNumbersArr);
+    printer(discrimination);
   }
 }
 
