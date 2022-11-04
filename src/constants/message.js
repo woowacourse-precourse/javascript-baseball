0d0b4d8a574a7fdc07fb3719def8ca3232f221cf
Dohyeon Lee <dlehgus1110@naver.com>
2022-11-04 21:30:04 +0900

    feat: 에러 메세지 객체 및 상수 생성

--- src/constants/message.js
+++ src/constants/message.js
@@ -1,7 +1,19 @@
 const GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
 const USER_NUMBER_INPUT_REQUEST_MESSAGE = "숫자를 입력해 주세요 : ";
 
+const INVALID_INPUT_TYPE = "입력에 숫자 이외의 문자가 있습니다. 게임 종료!";
+const INVALID_INPUT_LENGTH = "숫자의 길이가 올바르지 않습니다. 게임 종료!";
+const DUPLICATED_NUMBER = "입력에 중복된 숫자가 있습니다. 게임 종료!";
+const INVALID_INPUT_RANGE = "입력 가능한 숫자 범위를 벗어났습니다. 게임 종료!";
+const ERROR_MESSAGE = {
+  INVALID_INPUT_TYPE,
+  INVALID_INPUT_LENGTH,
+  DUPLICATED_NUMBER,
+  INVALID_INPUT_RANGE,
+};
+
 module.exports = {
   GAME_START_MESSAGE,
   USER_NUMBER_INPUT_REQUEST_MESSAGE,
+  ERROR_MESSAGE,
 };
