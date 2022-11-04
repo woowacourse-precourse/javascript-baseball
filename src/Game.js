b10097d43e6806627e21b4c799097e2ae9b43605
cys4585 <choizerowater@gmail.com>
2022-11-04 19:03:39 +0900

    feat: 기능5. 게임 재시작, 완전 종료 선택 기능

--- src/Game.js
+++ src/Game.js
@@ -49,11 +49,16 @@ const askRestartOrQuit = async () => {
 };
 
 const playGame = async () => {
+  let isRestart = true;
+
   print("숫자 야구 게임을 시작합니다.");
-  await gameStart();
-  gameEnd();
-  if ((await askRestartOrQuit()) === "quit") {
-    closeIO();
+  while (isRestart) {
+    await gameStart();
+    gameEnd();
+    if ((await askRestartOrQuit()) === "quit") {
+      closeIO();
+      isRestart = false;
+    }
   }
 };
 
