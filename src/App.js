41044388af18aa9bfa1cfa5b5c758231429222d2
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2022-11-04 22:02:31 +0900

    refactor: 불필요한 return 제거

--- src/App.js
+++ src/App.js
@@ -86,7 +86,7 @@ class App {
     MissionUtils.Console.readLine(
       "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n",
       (selectedNumByUser) => {
-        return this.selectReplayOrClose(selectedNumByUser);
+        this.selectReplayOrClose(selectedNumByUser);
       }
     );
   }
