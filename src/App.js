ae5174dd6b2cdc12712b87974d9d86b5e45664d1
Raehyeon Jeong <raehy19@gmail.com>
2022-11-04 21:28:12 +0900

    feat(src/App.js) : 중복없는 랜덤 3자리 숫자 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+
 class App {
   play() {}
+
+  generateAnswer() {
+    return MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
+  }
+
+  constructor() {
+    this.answer = this.generateAnswer();
+  }
 }
 
 module.exports = App;
