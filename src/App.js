873e9eec3ed0de10adab6bad0bbc35bbede5851c
Raehyeon Jeong <raehy19@gmail.com>
2022-11-05 00:17:01 +0900

    feat(src/App.js) : 사용자에게 숫자 입력 받는 기능

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 
 const GAME_START_MSG = '숫자 야구 게임을 시작합니다.';
+const NUM_INPUT_MSG = '숫자를 입력해주세요 : ';
 
 class App {
   play() {
     MissionUtils.Console.print(GAME_START_MSG);
+    this.getPlayerInput();
+  }
+
+  getPlayerInput() {
+    MissionUtils.Console.readLine(NUM_INPUT_MSG, (line) => {
+      return line;
+    });
   }
 
   generateAnswer() {
@@ -17,3 +25,6 @@ class App {
 }
 
 module.exports = App;
+
+const app = new App();
+app.play();
