6ffc8925b62322df51ad873f3e603ebde9d0c20c
somin <smhs12@naver.com>
2022-11-04 20:11:43 +0900

    feat: 게임 시작 문구 출력 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 class App {
+  constructor() {
+    const computerNumber = this.createComputerNumber();
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+  }
   createComputerNumber() {
     const eachNumberArray = [];
     while (eachNumberArray.length < 3) {
