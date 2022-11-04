a3c0bc1006b5185304181b62bd81074d77740d10
jinseo <ghkdwlstj99@gmail.com>
2022-11-05 02:22:29 +0900

    feat: 사용자 입력 시 숫자에 0이 들어있는 경우 구현

--- src/game_start.js
+++ src/game_start.js
@@ -2,11 +2,18 @@ const { MissionUtils } = require("@woowacourse/mission-utils");
 
 function gameStart(answer){
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
-    three_digits_error(answer);
+    threeDigitsError(answer);
+    includeZeroError(answer);
 }
 
 function threeDigitsError(answer){
     if(answer.length !==3){
         throw "Error" ;
+    }
+}
+
+function includeZeroError(answer){
+    if(answer.includes(0)){
+        throw "Error"
     }
 }
