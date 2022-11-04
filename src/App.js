6ca13c8ccd05f3e718371f8f4037f2161d71b736
DongWooKim97 <nanaukim@naver.com>
2022-11-04 22:55:00 +0900

    refactor: 자릿수 비교 기능을 GetScore클래스로 이동 및 로직 수정

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ class App {
   constructor() {
     this.missionRandom = require("@woowacourse/mission-utils").Random;
     this.missionConsole = require("@woowacourse/mission-utils").Console;
+    this.score = require("./play/GetScore");
   }
   play() {
     return this.getUserInput();
@@ -20,28 +21,9 @@ class App {
 
   getUserInput() {
     this.missionConsole.readLine("숫자를 입력해주세요 : ", (userInput) => {
-      if (this.compareEachNumber(this.makeRandomNumbers(), userInput)) {
-        this.missionConsole.print(this.compareEachNumber(this.makeRandomNumbers(), userInput));
-      }
+      this.missionConsole.print(this.compareEachNumber(this.makeRandomNumbers(), userInput));
       this.missionConsole.close();
     });
-  }
-
-  compareEachNumber(pcInputNumbers, userInput) {
-    const computerInputNumbers = pcInputNumbers.split("").map((i) => +i);
-    const userInputNumbers = userInput.split("").map((i) => +i);
-
-    return userInputNumbers.reduce((acc, currentValue, index) => {
-      if (computerInputNumbers[index] > currentValue) {
-        // return acc.push;
-        acc.push(`${index}번째 인덱스 자릿수: pc`);
-      } else if (computerInputNumbers[index] === currentValue) {
-        acc.push(`${index}번째 인덱스 자릿수: 무승부`);
-      } else {
-        acc.push(`${index}번째 인덱스 자릿수: user`);
-      }
-      return acc;
-    }, []);
   }
 }
 const app = new App();
