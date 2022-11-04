60acd209accdd31309a7ec47d5f2a7282f165acf
leeuihyun <meta212121@naver.com>
2022-11-04 15:51:23 +0900

    feat : 사용자의 입력값 검사 조건 통과 못할 시 에러 반환

--- src/User.js
+++ src/User.js
@@ -8,12 +8,25 @@ class User extends Mission {
   userInputStart() {
     this.mission.Console.readLine('숫자를 입력해주세요 : ', (answer) => {
       const userNumbers = this.makeNumberArray(answer);
+      this.checkUserNumber(userNumbers);
     });
   }
   makeNumberArray(answer) {
     const userNumbers = answer.split('').map((item) => Number(item));
     console.log(userNumbers);
     return userNumbers;
+  }
+  checkUserNumber(userArr) {
+    if (userArr.length !== 3)
+      throw '입력할 수 있는 길이는 3입니다. 종료합니다.';
+
+    userArr.forEach((item) => {
+      if (isNaN(item)) throw '숫자만 입력 가능합니다. 종료합니다.';
+      if (item < 1 || item > 9) throw '1-9 범위만 입력 가능합니다. 종료합니다.';
+    });
+
+    if (userArr.length !== new Set(userArr).size)
+      throw '중복되었습니다. 종료합니다.';
   }
 }
 
