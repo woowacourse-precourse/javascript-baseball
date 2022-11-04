98d62c2c1d0f4fe908f9bbc484abb06092ead397
BBIYAC <kiy7833@gmail.com>
2022-11-04 18:27:32 +0900

    feat(printResult): print ball strike result
    -  볼과 스트라이크 개수 계산 함수(getBallStrike) 추가
    -  게임 결과 출력(printResult) 함수 추가

--- src/App.js
+++ src/App.js
@@ -21,10 +21,34 @@ class App {
     return (numbers.length === 3) && (numbers.length === new Set(numbers).size);
   };
 
-  inputNumbers() {
+  getBallStrike(numbers, answers) {
+    let ball = 0;
+    let strike = 0;
+
+    Array.from(numbers).forEach((number, idx) => {
+      if (number === answers[idx]) strike++;
+      else if (answers.includes(number)) ball++;
+    })
+    return [ball, strike];
+  };
+
+  printResult(numbers, answers) {
+    const [ball, strike] = this.getBallStrike(numbers, answers);
+    let result;
+
+    if (ball && strike) result = `${ball}볼 ${strike}스트라이크`;
+    else if (ball) result = `${ball}볼`;
+    else if (strike) result = `${strike}스트라이크`;
+    else result = `낫싱`;
+
+    MissionUtils.Console.print(result);
+    if (ball && strike) this.inputRestartOrEnd();
+  };
+
+  inputNumbers(answers) {
     MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (numbers) => {
       if (this.isUniqueThreeNumber(numbers)) {
-        console.log(`${numbers}을 입력하셨습니다.`);
+        this.printResult(numbers, answers);
       } else {
         throw new Error('입력 형식에 맞지 않습니다.')
       }
