f69e88cdc9f57a7d25808146412ed54a0a261a16
Hoon <si932174@gmail.com>
2022-11-04 16:34:27 +0900

    feat : 1부터 9사이의 서로 다른 임의의 수 3개 배열로 저장

--- src/App.js
+++ src/App.js
@@ -4,9 +4,24 @@ const printer = (message) => {
   MissionUtils.Console.print(message);
 };
 
+const refNumbersGetter = () => {
+  const refNumbers = [];
+
+  while (refNumbers.length < 3) {
+    const targetNumber = MissionUtils.Random.pickNumberInRange(1, 9);
+    if (!refNumbers.includes(targetNumber)) {
+      refNumbers.push(targetNumber);
+    }
+  }
+
+  return refNumbers;
+};
+
 class App {
   play() {
     printer("숫자 야구 게임을 시작합니다.");
+    const refNumbersArr = refNumbersGetter();
+    printer(refNumbersArr);
   }
 }
 
