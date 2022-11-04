aae66e7969e116a853791db7d57e5fe88d74b617
River <swimmingr@gmail.com>
2022-11-04 23:31:57 +0900

    기능 구현 수정

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
+const BALL_COUNT = [1,2,3]; // 3자리 숫자 비교를 위한 횟수를 담은 배열 
+
+function inputNumber(){
+ let inputNum =  MissionUtils.Console.readLine('숫자를 입력하세요 :',(answer)=>{return answer});
+  return inputNum;
+}
+
+function playBall(atkNum,defNum){
+  if(atkNum !== undefined){
+  if(defNum.indexOf(inputToList(atkNum)[BALL_COUNT.map(function Count(item){item})])===-1 ){
+    console.log("낫싱");
+  }
+}
+}
 class App {
   play() {
-    console.log("숫자 야구 게임을 시작합니다.");
-    const ATK_NUM = MissionUtils.Console.readLine('숫자를 입력하세요 :',(answer)=>{return answer});
     const DEF_NUM = MissionUtils.Random.pickUniqueNumbersInRange(1,9,3);
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    MissionUtils.Console.print(DEF_NUM);
+    let atkNum =[];
+    atkNum.push(inputNumber());
+    playBall(atkNum,DEF_NUM);
   }
 }
 const app = new App();
