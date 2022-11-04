c913f35042fb417802e48b4ff8ac2ee755d03b40
doheeMac <thsehgml37@korea.ac.kr>
2022-11-04 17:55:52 +0900

    Feat: 사용자의 잘못된 입력값 throw 처리 추가

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
-import * as MissionUtils from "@woowacourse/mission-utils";
+const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
   play() {
-    const givenNumber = makeGivenNumber();
+    const computerNumber = makeComputerNumber();
     console.log("숫자 야구 게임을 시작합니다.");
-    const inputNumber = getInput();
+
+    for (let playCount = 0; playCount < 3; playCount++) {
+      const userInput = getUserInput();
+    }
   }
 }
 
-const makeGivenNumber = () => {
-  const givenNumber = MissionUtils.pickUniqueNumbersInRange(1, 9, 3);
-  return givenNumber;
+const makeComputerNumber = () => {
+  const computerNumber = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
+  return computerNumber;
 };
 
-const getInput = () => {
-  MissionUtils.Console.readline("숫자를 입력해주세요 : ", (inputNumber) => {
-    return inputNumber;
+const getUserInput = () => {
+  let inputArray = [];
+
+  MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (inputNumber) => {
+    if (isNaN(inputNumber) || inputNumber.length !== 3) {
+      throw new Error(
+        "Input values should be given in three natural numbers under 10."
+      );
+    }
+
+    inputArray = inputNumber.split("");
+    const set = new Set(inputArray);
+
+    if (inputArray.length !== set.size) {
+      throw new Error("Duplication is not allowed.");
+    }
   });
+  return inputArray;
 };
 
 module.exports = App;
