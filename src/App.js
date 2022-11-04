56ce18807d52bd8bbedc0278eaeb4f2192a8ce24
Sim <zh025700@naver.com>
2022-11-04 15:35:12 +0900

    feat(#App): create random number

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
+// 클래스내에서는 안되는구나...
+// 클래스에서 this로 자신 객체 전달 가능
 class App {
-  play() {}
+  answer = 0;
+  userAnswer = 0;
+  constructor() {
+    this.answer = MissionUtils.Random.pickNumberInRange(1, 9);
+  }
+
+  play() {
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    Game(this);
+  }
 }
+
+function Game(user) {
+  input(user.userAnswer);
+}
+
+const input = (userAnswer) => {
+  MissionUtils.Console.readLine("숫자를 입력해주세요 :", (answer) => {
+    userAnswer = answer;
+  });
+};
+const endGame = () => {
+  process.exit(1);
+};
+
+const app = new App();
+app.play();
 
 module.exports = App;
