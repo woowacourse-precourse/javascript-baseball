9fd1cfae9001fc2b119e39baaf655b6e98013514
wonkyum-kim <dodo12120@naver.com>
2022-11-04 20:39:05 +0900

    feat(src/App.js): make baseball numbers
    Set the answer numbers
    - `setBaseballAnswer` delete the old answer
    - `setBaseballAnswer` pick numbers by calling `findNewBaseballNumber`
    - `findNewBaseballNumber` neglect the dulpicated numbers
    
    BREAKING CHANGE: answer numbers can be derived by `setBaseballAnswer`

--- src/App.js
+++ src/App.js
@@ -3,11 +3,31 @@ const MissionUtils = require('@woowacourse/mission-utils');
 class App {
   firstPlay = true;
 
+  baseballAnswer = [];
+
+  findNewBaseballNumber(CHECK_SAME_NUMBER) {
+    let newBaseballNumber = 0;
+    do {
+      newBaseballNumber = MissionUtils.Random.pickNumberInRange(1, 9);
+    } while (CHECK_SAME_NUMBER.has(newBaseballNumber));
+    CHECK_SAME_NUMBER.add(newBaseballNumber);
+    this.baseballAnswer.push(newBaseballNumber);
+  }
+
+  setBaseballAnswer() {
+    this.baseballAnswer = []; // Clear the answer berfore starting a game.
+    const CHECK_SAME_NUMBER = new Set();
+    for (let count = 0; count < 3; count += 1) {
+      this.findNewBaseballNumber(CHECK_SAME_NUMBER);
+    }
+  }
+
   play() {
     if (this.firstPlay) {
       MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
     }
     this.firstPlay = false;
+    this.setBaseballAnswer();
   }
 }
 
