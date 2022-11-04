91ac0edee0220cbcea7e29abe1526fcb54fe3165
MAGHC <windowsapple95@gmail.com>
2022-11-04 15:37:15 +0900

    refactor : lint 규칙에 따라 정적 method 처리

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
-  constructor() {
-    this.getUserInputNumber = MissionUtils.Console.readLine("숫자를입력해주세요: ", (input) => {
+  play() {}
+
+  static getUserInputNumber() {
+    return MissionUtils.Console.readLine("숫자를입력해주세요: ", (input) => {
       MissionUtils.Console.print(`입력하신 숫자는 ${input} 입니다.`);
       MissionUtils.Console.close();
     });
   }
 
-  play() {
-    this.getUserInputNumber;
-  }
-
-  generateComputerNumberArray() {
+  static generateComputerNumberArray() {
     return MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
   }
 }
 
 module.exports = App;
+
+const app = new App();
+
+console.log(app.play());
