53fc38c7fefed59418adc93afec29e3e83c7329b
seongjin <77876601+ysjkof@users.noreply.github.com>
2022-11-04 10:44:11 +0900

    chore: 기능구현 목록, 동작 과정 작성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,42 @@
 class App {
-  play() {}
+  generateGoalNumber() {}
+
+  receiveNumberFromUser() {
+    // 서로 다른 3자리의 수 입력 받고 반환
+    // 잘못된 값일 경우 throw 하고 앱 종료하기
+  }
+
+  compareGoalAndUserAnswer() {
+    // 스트라이크 아웃 비교하고 결과 반환
+  }
+
+  printResult() {
+    // 볼 스트라이크 낫싱 등 결과 출력;
+  }
+
+  confirmRestart() {}
+
+  restartGame() {
+    this.play();
+  }
+
+  exitGame() {}
+
+  play() {
+    const goal = generateGoalNumber();
+    let userAnswer = receiveNumberFromUser();
+    let score = compareGoalAndUserAnswer(goal, userAnswer);
+
+    while (score !== 스트라이크3) {
+      printResult(score);
+      userAnswer = receiveNumberFromUser();
+      score = compareGoalAndUserAnswer(goal, userAnswer);
+    }
+
+    printResult(score);
+    if (confirmRestart()) return restartGame();
+    exitGame();
+  }
 }
 
 module.exports = App;
