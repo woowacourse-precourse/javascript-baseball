7ecb0e86f66cee7025f1bb90d648bf9dcd73186c
Deemou <4106243@naver.com>
2022-11-05 07:37:46 +0900

    feat: generateRandomNumber() 추가
    서로 다른 세 자리 숫자를 랜덤으로 생성

--- src/App.js
+++ src/App.js
@@ -2,6 +2,21 @@ const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
   play() {}
+  generateRandomNumber(digits) {
+    const START_INCLUSIVE = 1,
+      END_INCLUSIVE = 9;
+    const randomNumberSet = new Set();
+
+    while (randomNumberSet.size < digits) {
+      const num = MissionUtils.Random.pickNumberInRange(
+        START_INCLUSIVE,
+        END_INCLUSIVE
+      );
+      randomNumberSet.add(num);
+    }
+
+    return [...randomNumberSet];
+  }
   print(message) {
     MissionUtils.Console.print(message);
   }
