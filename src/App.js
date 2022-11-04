c440faa573efc8a027a19a76a782052605564558
areumsheep <areumsheep@gmail.com>
2022-11-04 23:03:19 +0900

    add: 컴퓨터가 1~9까지의 랜덤 숫자 3개 저장

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+const Messages = require('./constants/Messages');
+
 class App {
-  play() {}
+  play() {
+    const computer = [];
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+  }
 }
+const app = new App();
+app.play();
 
 module.exports = App;
