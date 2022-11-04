ab0e81db39a8e00d5e93d4aa83e7c646a683cee7
YejinHwang-D <hyj3463@naver.com>
2022-11-04 18:08:50 +0900

    feat: 기능 목록 5번 구현

--- src/App.js
+++ src/App.js
@@ -20,7 +20,6 @@ function createRandom() {
     tempForRandom = MissionUtils.Random.pickNumberInRange(1,9);
     randomNumber.push(tempForRandom);
   } 
-
   return randomNumber;
 }
 
@@ -33,7 +32,6 @@ function checkUserInput(userInput) {
   }
   let userInputStr = userInput.split("");
   userInput = userInputStr.map((val) => Number(val));
-
   return userInput;
 }
 
@@ -43,14 +41,13 @@ function input(text) {
     MissionUtils.Console.print(`${text}${answer}`);
     result = answer;
   });
-
   return result;
 }
 
 function baseballGame(randomNumber, userInput) {
   let strike = 0;
   let ball = 0;
-  
+
   for (let val of userInput) {
     if (randomNumber.indexOf(val) === -1)
       continue;
@@ -59,8 +56,41 @@ function baseballGame(randomNumber, userInput) {
     else if (randomNumber.indexOf(val) !== userInput.indexOf(val) && randomNumber.indexOf(val) !== -1)
       ball += 1;
   }
-
   return [strike, ball];
+}
+
+function check3Strike(strike, ball) {
+  if (strike === 3) {
+    MissionUtils.Console.print(`${strike}스트라이크`);
+    MissionUtils.Console.print(`3개의 숫자를 모두 맞히셨습니다! 게임 종료`);
+    userInput = input('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n');
+    userInput = Number(userInput);
+    if (userInput === 1) {
+      let randomNumber = [];
+      randomNumber = createRandom();
+      return randomNumber;
+    }
+    else if (userInput === 2) {
+      MissionUtils.Console.print('게임 종료');
+      return 'break';
+    }
+  }
+  else if (strike > 0 && ball > 0) {
+    MissionUtils.Console.print(`${ball}볼 ${strike}스트라이크`);
+    return 'not a 3 strike';
+  }
+  else if (strike > 0 && ball <= 0) {
+    MissionUtils.Console.print(`${strike}스트라이크`);
+    return 'not a 3 strike';
+  }
+  else if (strike <= 0 && ball > 0) {
+    MissionUtils.Console.print(`${ball}볼`);
+    return 'not a 3 strike';
+  }
+  else {
+    MissionUtils.Console.print(`낫싱`);
+    return 'not a 3 strike';
+  }
 }
 
 module.exports = App;
