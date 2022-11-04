8a8593eda922a867ee9fa41d42d3c9717d07f43e
woo-jk <201702042@o.cnu.ac.kr>
2022-11-04 18:30:11 +0900

    feat: 숫자 뽑기 기능 구현

--- src/App.js
+++ src/App.js
@@ -3,7 +3,16 @@ const MissionUtils = require("@woowacourse/mission-utils");
 class App {
   play() {}
 
-  drawThreeRandomNumbers() {}
+  drawThreeRandomNumbers() {
+    const threeRandomNumber = new Set();
+
+    while (threeRandomNumber.size < 3) {
+      const newNumber = MissionUtils.Random.pickNumberInRange(1, 9);
+      threeRandomNumber.add(newNumber);
+    }
+
+    return [...threeRandomNumber].join("");
+  }
 }
 
 const app = new App();
