c969202386462f1f208aaf6665105d4600d6da54
Azamman327 <wjsdbdud327@naver.com>
2022-11-05 06:02:41 +0900

    Feat: 사용자에게 입력받기

--- __tests__/baseballTest.js
+++ __tests__/baseballTest.js
@@ -1,15 +1,13 @@
 const MissionUtils = require("@woowacourse/mission-utils");
-let computer;
 
-class App {
-  play() {
+test("컴퓨터 랜덤값 확인", () => {
     computer = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
+    console.log(computer);
+});
 
+test("MissionUtils를 통한 출력 확인", () => {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.")
     MissionUtils.Console.readLine('숫자를 입력해주세요.', (answers) => {
       console.log(`숫자: ${answers}`);
     });
-  }
-}
-
-module.exports = App;
+});
