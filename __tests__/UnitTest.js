94ff3a42e28c9d0dea9a7db8463e68c46e5a0181
moonelysian <dandyblack@naver.com>
2022-11-04 14:16:47 +0900

    test: 0을 포함한 숫자 입력 시 예외 처리 테스트

--- __tests__/UnitTest.js
+++ __tests__/UnitTest.js
@@ -23,14 +23,20 @@ const getLogSpy = () => {
   return logSpy;
 };
 
+const getReadlineSpy = () => {
+  const logSpy = jest.spyOn(MissionUtils.Console, "readLine");
+  logSpy.mockClear();
+  return logSpy;
+};
+
 describe("숫자 야구 게임", () => {
   test("1-9까지 서로 다른 임의의 수 3개 생성", () => {
     const app = new App();
     const randoms = app.generateRandomNums(1, 9, 3);
-    const set = new Set(randoms);
+    const unduplicatedArray = [...new Set(randoms)];
 
-    expect(randoms.length).toEqual(3);
-    expect(set.size).toEqual(3);
+    expect(randoms).toHaveLength(3);
+    expect(unduplicatedArray).toHaveLength(3);
     expect(randoms.includes(0)).toEqual(false);
   });
 
@@ -110,4 +116,17 @@ describe("숫자 야구 게임", () => {
       expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
     });
   });
+  
+  test("예외처리-0을 포함한 숫자입력", () => {
+    const randoms = [1, 3, 5];
+    const answers = ["013"];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow();
+  })
 });
