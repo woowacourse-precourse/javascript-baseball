514352546d2f469d834bdd1264411636656546f7
2wndrhs <2wndrhs@gmail.com>
2022-11-04 07:25:53 -0700

    style: 메서드명 가독성 좋게 수정

--- __tests__/ValidationTest.js
+++ __tests__/ValidationTest.js
@@ -9,18 +9,18 @@ const mockQuestion = answer => {
 };
 
 describe('입력값 유효성 검증 테스트', () => {
-  test('validateUserInput 메서드로 입력값이 적절한지 검증(case1)', () => {
-    const RESULT = App.validateUserInput('123');
+  test('isValidUserInput 메서드로 입력값이 적절한지 검증(case1)', () => {
+    const RESULT = App.isValidUserInput('123');
     expect(RESULT).toBe(true);
   });
 
-  test('validateUserInput 메서드로 입력값이 적절한지 검증(case2)', () => {
-    const RESULT = App.validateUserInput('111');
+  test('isValidUserInput 메서드로 입력값이 적절한지 검증(case2)', () => {
+    const RESULT = App.isValidUserInput('111');
     expect(RESULT).toBe(false);
   });
 
-  test('validateUserInput 메서드로 입력값이 적절한지 검증(case3)', () => {
-    const RESULT = App.validateUserInput('abc');
+  test('isValidUserInput 메서드로 입력값이 적절한지 검증(case3)', () => {
+    const RESULT = App.isValidUserInput('abc');
     expect(RESULT).toBe(false);
   });
 
@@ -28,7 +28,7 @@ describe('입력값 유효성 검증 테스트', () => {
     mockQuestion('1234');
     expect(() => {
       const app = new App();
-      app.setUserInput();
+      app.getUserInput();
     }).toThrow();
   });
 });
--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 
 class App {
-  #RANDOM_NUMBER = 0;
+  #RANDOM_NUMBER = [];
 
   #userInput = 0;
 
@@ -11,21 +11,21 @@ class App {
     while (RANDOM_SET.size < 3) {
       RANDOM_SET.add(MissionUtils.Random.pickNumberInRange(1, 9));
     }
-    const RANDOM_NUMBER = Number([...RANDOM_SET].join(''));
+    const RANDOM_NUMBER = [...RANDOM_SET];
     return RANDOM_NUMBER;
   }
 
   // 사용자의 입력을 받는 기능
-  setUserInput() {
+  getUserInput() {
     MissionUtils.Console.readLine('숫자를 입력해주세요 : ', answer => {
-      if (!App.validateUserInput(answer)) {
+      if (!App.isValidUserInput(answer)) {
         throw new Error('잘못된 값을 입력하였습니다!');
       }
     });
   }
 
   // 사용자가 입력한 값이 적절한지 검증하는 기능
-  static validateUserInput(input) {
+  static isValidUserInput(input) {
     const REGEX = /^[1-9]{3}$/;
     const INPUT_SET = new Set(input);
     if (REGEX.test(input) && INPUT_SET.size === 3) {
@@ -36,7 +36,7 @@ class App {
 
   play() {
     this.#RANDOM_NUMBER = App.getRandomNumber();
-    this.setUserInput();
+    this.getUserInput();
   }
 }
 
