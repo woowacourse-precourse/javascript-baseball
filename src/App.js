88337eafeef24537e9fd8cde18f56f8e00f4eda2
pydevjeong <njh3421@gmail.com>
2022-11-04 20:55:34 +0900

    MissionUtils Testing, Temporary wrote Readme

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  play() {
+    this.showMessage()
+  }
+  showMessage(){
+    MissionUtils.Console.print('안녕하세요.')
+    MissionUtils.Console.print(MissionUtils.Random.pickNumberInList([1, 2, 3]))
+    MissionUtils.Console.close()
+  }
+
 }
 
+const app = new App();
+app.play();
 module.exports = App;
