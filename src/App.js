e2a4c9411ccb4d25c6db0aadaa8f6fda99f76aba
yaena1223 <yaena1223@dgu.ac.kr>
2022-11-05 04:51:34 +0900

    feat: add first function - computer random number

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+const MissionUtils = require("@woowacourse/mission-utils");
 class App {
-  play() {}
+  play() {
+    console.log(rand_num());
+  }
 }
 
+const rand_num = function computer_random_number() {
+  const number_list = [];
+  while (number_list.length < 3) {
+    const single_digit = MissionUtils.Random.pickNumberInRange(1, 9);
+    if (!number_list.includes(single_digit)) {
+      number_list.push(single_digit);
+    }
+  }
+  const final_random_num = number_list.map((number) => number).join("");
+  return final_random_num;
+};
+
 module.exports = App;
+const app = new App();
+app.play();
