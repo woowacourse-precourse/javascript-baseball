b5480e89ca5e1c33bb5f80d4b2b0b59fb27bf878
jiho1996 <jhp9270@naver.com>
2022-11-04 16:44:23 +0900

    feat : 상수모음 객체 추가

---
+++ src/constants/message.js
@@ -0,0 +1,7 @@
+const GAME_MESSAGE = Object.freeze({
+  NOTIFY_START_MESSAGE: "숫자 야구 게임을 시작합니다.",
+  ASK_INPUT_MESSAGE: "숫자를 입력하세요",
+  FINSH_GAME_MESSAGE: "숫자 야구 게임을 종료합니다.",
+});
+
+module.exports = GAME_MESSAGE;
