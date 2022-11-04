7393b6c0982b794083f7a3d8e5dccabdc4caedaa
seongminn <berry9901@gmail.com>
2022-11-04 17:53:43 +0900

    feat(BaseballGame): 게임 시작 메세지 출력하는 기능 구현

--- src/game/BaseballGame.js
+++ src/game/BaseballGame.js
@@ -1,8 +1,18 @@
+const { Console } = require('@woowacourse/mission-utils');
 const { createRandomNumber } = require('../computer/computer');
 
 class BaseballGame {
   constructor() {
     this.randomNumber = createRandomNumber();
+    this.init();
+  }
+
+  init() {
+    this.printStartMessage();
+  }
+
+  printStartMessage() {
+    Console.print('숫자 야구 게임을 시작합니다.');
   }
 }
 
