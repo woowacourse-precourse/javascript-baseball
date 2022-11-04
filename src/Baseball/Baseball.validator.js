92aded18e35a792e77f394625cd00fe6d2bb2735
loopy-lim <loopy@jnu.ac.kr>
2022-11-04 16:27:29 +0900

    feat: 사용자는 숫자만을 입력한다

--- src/Baseball/Baseball.validator.js
+++ src/Baseball/Baseball.validator.js
@@ -1,4 +1,10 @@
 class BaseBallValidator {
+  checkNumericNumbers([...numbers]) {
+    nonNuemrics = numbers.filter((number) => "1" > number && number > "9");
+    if (nonNuemrics > 0) {
+      throw "숫자가 아닌 값이 있습니다.";
+    }
+  }
   checkOtherNumbers([...numbers]) {
     const removedDuplicateNumbers = Array.from(new Set(...numbers));
     if (
