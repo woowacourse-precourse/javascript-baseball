0d721ec7db2f469d5dc32287ceb2914feda4ff5a
turtle601 <turtle601@naver.com>
2022-11-04 14:03:28 +0900

    test(customApp) isStrike 함수 테스트 코드 작성
    - 에러 로직 구현과 랜덤함수 내에서 1 - 9 이외의 수는 없다고 판단, 테스트 코드를 구현
    - case 1: random-[1,2,3] input-[1,4,5] 각 원소에 대해 strike 판단

--- __tests__/CustomAppTest.js
+++ __tests__/CustomAppTest.js
@@ -1,14 +1,29 @@
 const App = require('../src/App');
 const MissionUtils = require('@woowacourse/mission-utils');
 
-describe('App 클래스 - get3RandomNumbers', () => {
+describe('App 클래스 - get3RandomNumbers()', () => {
   test('MissionUtils.Random.pickUniqueNumbersInRange 함수 동작 테스트', () => {
     const app = new App();
     const isNumber = (item) => typeof item === 'number';
 
-    expect(app.get3RandomNumbers().every((item) => isNumber(item))).toBeTruthy();
+    expect(
+      app.get3RandomNumbers().every((item) => isNumber(item)),
+    ).toBeTruthy();
     expect([...new Set(app.get3RandomNumbers())]).toHaveLength(3);
 
     MissionUtils.Console.close();
+  });
+});
+
+describe('App 클래스 - isStrike()', () => {
+  test('주어진 입력값이 random값과 비교 스트라이크인지 확인하는 함수', () => {
+    const random = [1, 2, 3];
+    const input = [1, 4, 5];
+
+    const app = new App();
+
+    expect(app.isStrike(random[0], input[0])).toBeTruthy();
+    expect(app.isStrike(random[1], input[1])).toBeFalsy();
+    expect(app.isStrike(random[2], input[2])).toBeFalsy();
   });
 });
