1803a8e6e24faa1b7381a59330b2108a6d6755b0
BuildTools <unconfigured@null.spigotmc.org>
2022-11-04 22:21:40 +0900

    정답 선언

--- src/App.js
+++ src/App.js
@@ -2,11 +2,23 @@ const MissionUtils = require('@woowacourse/mission-utils');
 
 class App {
   play() {
+    const answer = makeComputerAnswer();
     console.log('숫자 야구 게임을 시작합니다.');
     MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (message) =>
-      MissionUtils.Console.print(message)
+      console.log(message)
     );
   }
+}
+
+function makeComputerAnswer() {
+  const computer = [];
+  while (computer.length < 3) {
+    const number = MissionUtils.Random.pickNumberInRange(1, 9);
+    if (!computer.includes(number)) {
+      computer.push(number);
+    }
+  }
+  return computer;
 }
 
 module.exports = App;
