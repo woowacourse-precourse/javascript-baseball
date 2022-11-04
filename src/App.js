066992104a26d853340a5ffd1037f3e4c143e915
lee-il-woo <empty001430@gmail.com>
2022-11-04 14:35:18 +0900

    Feat: 추가 getStrike, getBall 함수
    스트라이크 개수와 볼 개수를 리턴하는 함수

--- src/App.js
+++ src/App.js
@@ -8,15 +8,38 @@ function getComputerNumbers(){
 function getUserNumbers(){
   let user_numbers;
   MissionUtils.Console.readLine('숫자를 입력해주세요.\n', (user_number) => {
+    if(user_number.toString().length != 3) throw new Error("에러")
     user_numbers = String(user_number).split('').map((number)=> Number(number));
     return user_numbers
   });
 }
 
+function getStrike(COMPUTER_NUMBERS,USER_NUMBERS,score){
+  COMPUTER_NUMBERS.map((element,index)=>{
+    if(USER_NUMBERS[index] == element) score.strike = score.strike + 1 
+  })
+  return score.strike
+}
+function getBall(COMPUTER_NUMBERS,USER_NUMBERS,score){
+  COMPUTER_NUMBERS.map((element,index)=>{
+    if(USER_NUMBERS.indexOf(element) != -1 && USER_NUMBERS.indexOf(element) != index) score.ball = score.ball + 1
+  })
+  return score.ball
+}
+
 function play(){
   const COMPUTER_NUMBERS = getComputerNumbers();
-  const USER_NUMBERS = getUserNumbers();
-  console.log(USER_NUMBERS,COMPUTER_NUMBERS)
+  console.log(COMPUTER_NUMBERS)
+  let strike = 0;
+  while (strike != 3){
+    const USER_NUMBERS = getUserNumbers();
+    console.log(USER_NUMBERS)
+    let score = {strike: 0, ball: 0};
+    strike = getStrike(COMPUTER_NUMBERS,USER_NUMBERS,score)
+    let ball = getBall(COMPUTER_NUMBERS,USER_NUMBERS,score)
+    MissionUtils.Console.print(strike+"스트라이크"+ball+"볼")
+  }
+  MissionUtils.Console.print("게임 종료")
 }
 
 console.log(play())
