ef24dad076e47f8797b44dab32150a0ab55e57a4
kyw0716 <kyw0716@naver.com>
2022-11-04 21:20:27 +0900

    test: 입력 유효성 검사 테스트 코드 객체 App => BaseballGame으로 수정

--- __tests__/InputValidTest.js
+++ __tests__/InputValidTest.js
@@ -1,13 +1,13 @@
-const App = require("../src/App");
+const BaseballGame = require("../src/BaseballGame");
 
 describe("입력값 유효 체크 테스트", () => {
   // TODO: 게임 종료 후 입력값 유효 테스트 추가하기
   test("입력값이 유효한 값인지", () => {
-    const app = new App();
+    const game = new BaseballGame();
     const testWordsForInvalid = ["1234", "122", "12d"];
     testWordsForInvalid.forEach((testWord) => {
-      expect(app.getIsInputValueValid(testWord)).toEqual(false);
+      expect(game.getIsInputValueValid(testWord)).toEqual(false);
     });
-    expect(app.getIsInputValueValid("123")).toEqual(true);
+    expect(game.getIsInputValueValid("123")).toEqual(true);
   });
 });
