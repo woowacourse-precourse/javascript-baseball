88431f1174b43d981387039494c064a049416131
sery <yunsel2@gmail.com>
2022-11-04 21:54:43 +0900

    feat: 입력된 값의 볼, 스트라이크 점수 계산 함수 추가

--- src/game.js
+++ src/game.js
@@ -5,19 +5,13 @@ const { MESSAGE } = require('./constants');
 class Game {
   constructor() {
     this.started = false;
-    this.randomNumber;
-    this.score = {
-      ball: 0,
-      strike: 0,
-    };
+    this.randomNumbers;
   }
 
   init() {
     this.start(MESSAGE.START);
     this.generateRandomNumber(1, 9, 3);
     this.started = true;
-    this.score.ball = 0;
-    this.score.strike = 0;
   }
 
   start(startMessage) {
@@ -25,21 +19,28 @@ class Game {
   }
 
   generateRandomNumber(min, max, length) {
-    this.randomNumber = [];
-    while (this.randomNumber.length < length) {
+    this.randomNumbers = [];
+    while (this.randomNumbers.length < length) {
       const number = MissionUtils.Random.pickNumberInRange(min, max);
-      if (!this.randomNumber.includes(number)) {
-        this.randomNumber.push(number);
+      if (!this.randomNumbers.includes(number)) {
+        this.randomNumbers.push(number);
       }
     }
+    console.log(this.randomNumbers);
 
-    return this.randomNumber;
+    return this.randomNumbers;
   }
 
   play() {
-    MissionUtils.Console.readLine(MESSAGE.ENTER_NUMBER, (inputNumbers) => {
-      this.isValidInputNumber(inputNumbers, RANDOM_NUMBER.RANGE);
-      console.log(this.randomNumber);
+    MissionUtils.Console.readLine(MESSAGE.ENTER_NUMBER, (input) => {
+      this.isValidInputNumber(input, RANDOM_NUMBER.RANGE);
+      const inputNumbers = [...input].map(Number);
+      const { ball, strike } = this.countScore(
+        inputNumbers,
+        this.randomNumbers
+      );
+      console.log(`볼${ball}`);
+      console.log(`스트라이크${strike}`);
     });
   }
 
@@ -53,6 +54,21 @@ class Game {
     ) {
       throw new Error('1부터 9까지 서로 다른 숫자 3개를 입력해주세요');
     }
+  }
+
+  countScore(inputNumbers, randomNumbers) {
+    let ball = 0;
+    let strike = 0;
+
+    inputNumbers.forEach((inputNum, inputIdx) => {
+      if (inputNum === randomNumbers[inputIdx]) {
+        strike++;
+      } else if (randomNumbers.includes(inputNum)) {
+        ball++;
+      }
+    });
+
+    return { ball, strike };
   }
 }
 
