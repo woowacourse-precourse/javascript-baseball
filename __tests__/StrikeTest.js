df71e91bf001ffe4446f01b143ac1742c5ea9b1d
SWARVY <swarvy0826@naver.com>
2022-11-04 17:03:57 +0900

    Jest : 테스트 케이스 추가

--- __tests__/StrikeTest.js
+++ __tests__/StrikeTest.js
@@ -16,4 +16,34 @@ describe("스트라이크 개수 체크",() => {
         expect(checkStrike).toBe(strikeCount);
         expect(checkBall).toBe(ballCount);
     });
+
+    test('컴퓨터가 랜덤하게 뽑은 값은 4 3 2 입니다', () => {
+        //조건
+        const computer = [4, 3, 2];
+        const player = [5, 2, 3];
+        const strikeCount = 0;
+        const ballCount = 2;
+    
+        //실행
+        const [checkStrike, checkBall] = app.checkBallCounts(computer, player);
+    
+        //평가
+        expect(checkStrike).toBe(strikeCount);
+        expect(checkBall).toBe(ballCount);
+    });
+
+    test('컴퓨터가 랜덤하게 뽑은 값은 6 1 5 입니다', () => {
+        //조건
+        const computer = [6, 1, 5];
+        const player = [4, 2, 3];
+        const strikeCount = 0;
+        const ballCount = 0;
+    
+        //실행
+        const [checkStrike, checkBall] = app.checkBallCounts(computer, player);
+    
+        //평가
+        expect(checkStrike).toBe(strikeCount);
+        expect(checkBall).toBe(ballCount);
+    });
 });
