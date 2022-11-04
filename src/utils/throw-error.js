44c2ad43cfcf7cb37f816925cac86e01b59df8a6
Sumin Kim <ksumin0730@hufs.ac.kr>
2022-11-04 13:29:32 +0900

    Style: 들여쓰기 맞춤

--- src/utils/throw-error.js
+++ src/utils/throw-error.js
@@ -3,12 +3,12 @@ const error = new Exception();
 
 exports.inputError = (input) => {
   if(error.inputError(input) != false){
-        throw new Error(error.inputError(input));
+    throw new Error(error.inputError(input));
   }
 }
 
 exports.restartError = (input) => {
   if (error.restartError(String(input)) != false){
-        throw new Error(error.restartError(String(input)));
+    throw new Error(error.restartError(String(input)));
   }
 }
