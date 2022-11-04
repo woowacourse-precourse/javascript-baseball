34d18b94d75f0f15c81e77579f43a4cab7baff25
YejinHwang-D <hyj3463@naver.com>
2022-11-04 17:23:48 +0900

    feat: 기능 목록 1번 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  constructor() {
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+  };
+
+  play() {
+    let randomNumber = [];
+    randomNumber = createRandom();
+  }
+}
+
+function createRandom() {
+  let randomNumber = [];
+  let tempForRandom;
+
+  while (randomNumber.length < 3) {
+    tempForRandom = MissionUtils.Random.pickNumberInRange(1,9);
+    randomNumber.push(tempForRandom);
+  }
+
+  return randomNumber;
 }
 
 module.exports = App;
