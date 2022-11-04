8c5bff460b599b324ac554c429209520dc9deadb
2jaebbang <72256238+2jaebbang@users.noreply.github.com>
2022-11-05 05:38:47 +0900

    refactor: gameStart 메서드 실행 전 시작 알림 출력

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+const { Console } = require('@woowacourse/mission-utils');
 const { gameStart, generateRandomNumbers } = require('./gameStart');
 const GameData = require('./GameData');
 
 class App {
   play() {
     const gameData = new GameData();
+    Console.print('숫자 야구 게임을 시작합니다.');
     gameStart(generateRandomNumbers(gameData));
   }
 }
