a116479e5c9ce1376b8a0073284d2fb5a5cc9a7c
woo-jk <201702042@o.cnu.ac.kr>
2022-11-05 04:27:01 +0900

    feat: 플레이어 입력 함수 Promise로 구현

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
-  play() {}
+  play() {
+    this.temp();
+  }
 
   drawThreeRandomNumbers() {
     const threeRandomNumber = new Set();
-
     while (threeRandomNumber.size < 3) {
       const newNumber = MissionUtils.Random.pickNumberInRange(1, 9);
       threeRandomNumber.add(newNumber);
@@ -16,6 +17,19 @@ class App {
 
   printGameStartPhrase() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+  }
+
+  async temp() {
+    const value = await this.readPlayerInput();
+    console.log(value);
+  }
+
+  readPlayerInput() {
+    return new Promise((resolve) => {
+      MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (input) => {
+        resolve(input);
+      });
+    });
   }
 }
 
