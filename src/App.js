eeaef35f18e4335333a992c2bc4fc335ef0fceb9
Minhye Kang <alsgp9898@naver.com>
2022-11-04 16:44:41 +0900

    feat: 이전 테스트 코드를 성공시키는 함수 구현 및 출력 메세지 객체 생성
    - 게임 메세지들을 상수로 관리하기 위해 GAME_MESSAGE, GAME_ERROR_MESSAGE 객체 생성 후 동결
    - isInputValid 함수에 중복하는 숫자가 있는 경우 예외를 발생시키는 코드 추가

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
-const GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
-const INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+const GAME_MESSAGE = Object.freeze({
+  START: "숫자 야구 게임을 시작합니다.",
+  INPUT: "숫자를 입력해주세요 : ",
+});
+
+const GAME_ERROR_MESSAGE = Object.freeze({
+  BLANK: "아무것도 입력되지 않았습니다.",
+  DUPLICATE: "중복된 숫자가 있습니다.",
+});
 
 class App {
   constructor() {
     this.number = "";
   }
   getUserInput() {
-    MissionUtils.Console.readLine(INPUT_MESSAGE, (number) => {
+    MissionUtils.Console.readLine(GAME_MESSAGE.INPUT, (number) => {
       this.number = number;
       this.isInputValid();
       this.getUserInput();
@@ -16,12 +23,13 @@ class App {
   }
 
   isInputValid() {
-    if (this.number === "")
-      throw new Error("잘못된 값을 입력하여 게임을 종료합니다.");
+    if (this.number === "") throw new Error(GAME_ERROR_MESSAGE.BLANK);
+    if ([...new Set(this.number.split(""))].length !== 3)
+      throw new Error(GAME_ERROR_MESSAGE.DUPLICATE);
   }
 
   printGameStartMessage() {
-    MissionUtils.Console.print(GAME_START_MESSAGE);
+    MissionUtils.Console.print(GAME_MESSAGE.START);
     this.getUserInput();
   }
 
