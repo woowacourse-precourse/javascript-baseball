3209409f7cd06ad87158f34320d455910276ffdd
ashleysyheo <ashleysyheo@gmail.com>
2022-11-05 01:11:08 +0900

    feat: count balls in user input number

--- src/components/CalculateGame.js
+++ src/components/CalculateGame.js
@@ -1,15 +1,23 @@
 class CalculateGame {
   constructor(computerNumber, userInputNumber) {
-    this.computerNumber = computerNumber.split(''); 
+    this.computerNumber = computerNumber.split('');
     this.userInputNumber = userInputNumber.split('');
-    this.strike = 0; 
-    this.ball = 0; 
+    this.strike = 0;
+    this.ball = 0;
   }
 
   countStrike() {
     this.userInputNumber.forEach((number, index) => {
       if (number === this.computerNumber[index]) {
-        this.strike += 1; 
+        this.strike += 1;
+      }
+    });
+  }
+
+  countBall() {
+    this.userInputNumber.forEach((number, index) => {
+      if (number !== this.computerNumber[index] && this.computerNumber.includes(number)) {
+        this.ball += 1;
       }
     });
   }
