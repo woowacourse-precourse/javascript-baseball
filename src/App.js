5a49994411db5022f017174bea4da79dc8d1722b
SWARVY <swarvy0826@naver.com>
2022-11-04 17:01:14 +0900

    feat : 프로그램 흐름 변경 및 예외처리 추가

--- src/App.js
+++ src/App.js
@@ -1,66 +1,46 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
-  async play() {
-    let CLEAR = false;
-
+  play() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
     const computer = this.computerInput();
     console.log(computer);
-
-    while(CLEAR === false) {
-      try{
-        let player = await this.playerInput();
-        let [strikeCount, ballCount] = this.checkBallCounts(computer, player.split("").map(Number));
-        this.printBallCounts(strikeCount, ballCount);
-        CLEAR = this.isClear(strikeCount);
-      }catch(e) {
-        console.error(e);
-      }
-    }
-
-    let retry = await this.isRetry();
-    try{
-      if(retry === "1")
-        this.play();
-      else
-        return;
-    }catch(e) {
-      console.error(e);
-    }
+    this.sendValue(computer);
   }
 
-  playerInput() {
-    return new Promise(resolve => {
-      MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (number) => {
-        this.isNumber(number);
-        this.isCorrectNumber(number);
-        this.isNumberRepeat(number);
-        resolve(number);
-      });
+  sendValue(computer) {
+    MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (player) => {
+      this.playerCheck(player);
+      let [strikeCount, ballCount] = this.checkBallCounts(computer, player.split("").map(Number));
+      this.printBallCounts(strikeCount, ballCount);
+      
+      if(!this.isClear(strikeCount))
+        this.sendValue(computer);
+      else
+        this.sendRetry();
     });
   }
 
-  isNumber(value) {
-    const RegExp = /[0-9]/g;
-    if(RegExp.test(value) === false)
-      throw new Error("Error!");
+  sendRetry() {
+    MissionUtils.Console.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.', (number) => {
+      this.retryCheck(number);
+      if(number === "1")
+        this.play();
+      else
+        MissionUtils.Console.close();
+        return 0;
+    })
   }
 
-  isCorrectNumber(value) {
-    if(value.length !== 3)
-      throw new Error("Error!");
+  retryCheck(number) {
+    this.isNumber(number);
+    this.isOneOrTwo(number);
   }
 
-  isNumberRepeat(value) {
-    let valueSet = new Set(value);
-    if(value.length !== valueSet.size)
-      throw new Error("Error!");
-  }
-
-  isOneOrTwo(value) {
-    if(value !== "1" || value !== "2")
-      throw new Error("Error!");
+  playerCheck(number) {
+    this.isNumber(number);
+    this.isCorrectNumber(number);
+    this.isNumberRepeat(number);
   }
 
   computerInput() {
@@ -73,65 +53,77 @@ class App {
     return computer;
   }
 
+  isNumber(value) {
+    const RegExp = /[1-9]/g;
+    if(RegExp.test(value) === false)
+      throw Error();
+  }
+
+  isCorrectNumber(value) {
+    if(value.length !== 3)
+      throw Error();
+  }
+
+  isNumberRepeat(value) {
+    let valueSet = new Set(value);
+    if(value.length !== valueSet.size)
+      throw Error();
+  }
+
+  isOneOrTwo(value) {
+    if(!(value === "1" || value === "2"))
+      throw Error();
+  }
+
   isClear(strikeCount) {
-    if(strikeCount === 3)
+    if(strikeCount === 3) {
+      MissionUtils.Console.print(`3개의 숫자를 모두 맞히셨습니다! 게임 종료`);
       return true;
+    }
     return false;
   }
 
-  isRetry() {
-    return new Promise(resolve => {
-      MissionUtils.Console.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.', (number) => {
-        this.isNumber(number);
-        this.isOneOrTwo(number);
-        resolve(number);
-      })
-    });
-  }
+  
 
   printBallCounts(strikeCount, ballCount) {
-    if(strikeCount === 3) {
-      MissionUtils.Console.print(`${strikeCount}스트라이크`);
-      MissionUtils.Console.print(`3개의 숫자를 모두 맞히셨습니다! 게임 종료`);
-    }
+    if(strikeCount >= 1 && ballCount >= 1)
+      MissionUtils.Console.print(`${ballCount}볼 ${strikeCount}스트라이크`);
+    else if(ballCount >= 1)
+      MissionUtils.Console.print(`${ballCount}볼`);
     else if(strikeCount >= 1)
       MissionUtils.Console.print(`${strikeCount}스트라이크`);
-    if(ballCount >= 1)
-      MissionUtils.Console.print(`${ballCount}볼`);
-    if(strikeCount === 0 && ballCount === 0)
-      MissionUtils.Console.print(`낫싱`);
+    else
+      MissionUtils.Console.print("낫싱");
   }
 
   checkBallCounts(computer, player) {
+    let strikeArray;
     let strikeCount;
     let ballCount;
-
-    let temp_computer = computer.slice();
-    let temp_player = player.slice();
  
-    [strikeCount, temp_computer, temp_player] = this.checkStrikeCount(temp_computer, temp_player);
-    ballCount = this.checkBallCount(temp_computer, temp_player);
+    [strikeCount, strikeArray] = this.checkStrikeCount(computer, player);
+    ballCount = this.checkBallCount(computer, player, strikeArray);
 
     return [strikeCount, ballCount];
   }
 
   checkStrikeCount(computer, player) {
     let strikeCount = 0;
+    let alreadyStrike = [];
     for(let i = 0; i < computer.length; i++) {
       if(computer[i] === player[i]) {
         strikeCount += 1;
-        computer[i] = undefined;
-        player[i] = undefined;
+        alreadyStrike.push(computer[i]);
       }
     }
 
-    return [strikeCount, computer, player];
+    return [strikeCount, alreadyStrike];
   }
 
-  checkBallCount(computer, player) {
+  checkBallCount(computer, player, strikeArray) {
     let ballCount = 0;
     for(let i = 0; i < player.length; i++) {
-      if(computer.includes(player[i]) && player[i] !== undefined)
+      if(computer.includes(player[i]) && !strikeArray.includes(player[i]))
         ballCount += 1;
     }
 
@@ -139,6 +131,4 @@ class App {
   }
 }
 
-const app = new App();
-app.play();
 module.exports = App;
