825d951879efd79e8aa51453e9d10ecef9a320a5
doheeMac <thsehgml37@korea.ac.kr>
2022-11-04 15:14:01 +0900

    Feat: 컴퓨터(상대방)의 숫자 생성 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import * as MissionUtils from "@woowacourse/mission-utils";
+
 class App {
-  play() {}
+  play() {
+    const givenNumber = makeGivenNumber();
+    console.log("숫자 야구 게임을 시작합니다.");
+  }
 }
+
+const makeGivenNumber = () => {
+  const givenNumber = [];
+  while (givenNumber.length < 3) {
+    const number = MissionUtils.Random.pickNumberInRange(1, 9);
+    if (!givenNumber.includes(number)) {
+      givenNumber.push(number);
+    }
+  }
+  return givenNumber;
+};
 
 module.exports = App;
