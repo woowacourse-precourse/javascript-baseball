ccd10348301efb3e35d1a03516e7a7957a7cd846
jjongs2 <jjongs@hanyang.ac.kr>
2022-11-04 23:18:00 +0900

    feat: 1부터 9까지 서로 다른 임의의 숫자 3개를 정답으로 선정하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,4 +1,26 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+
 class App {
+  constructor() {
+    this.answer = this.getAnswer();
+  }
+
+  getAnswer() {
+    let answer = {};
+    let index = 1;
+
+    while (index <= 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+
+      if (answer[number] === undefined) {
+        answer[number] = index;
+        index += 1;
+      }
+    }
+
+    return answer;
+  }
+
   play() {}
 }
 
