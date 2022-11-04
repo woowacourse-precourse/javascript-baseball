983110ec025cde0ccb3a6dcb132a2ed9d096bc43
somin <smhs12@naver.com>
2022-11-04 22:28:03 +0900

    feat: 입력 값 유효성 검사 기능

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ class App {
   play() {
     const computerNumber = this.createComputerNumber();
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
-    MissionUtils.Console.readLine("숫자를 입력해주세요:", (userInput) =>
-      this.checkInputValidation(userInput)
-    );
+    MissionUtils.Console.readLine("숫자를 입력해주세요:", (userInput) => {
+      if (this.checkInputValidation(userInput)) {
+      } else {
+        MissionUtils.Console.close();
+      }
+    });
   }
 
   createComputerNumber() {
@@ -17,6 +20,13 @@ class App {
       }
     }
     return eachNumberArray.join("");
+  }
+
+  checkInputValidation(userInput) {
+    if (userInput.length < 3) return;
+    else if (new Set(userInput).size !== 3) return;
+    else if (userInput.includes("0")) return;
+    return true;
   }
 }
 
