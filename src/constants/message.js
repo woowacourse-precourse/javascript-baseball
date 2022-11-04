4eabeca85942260aae61747bd14836f7ae6c0531
Dohyeon Lee <dlehgus1110@naver.com>
2022-11-04 17:34:37 +0900

    feat: 사용자 숫자입력 요청 메시지 상수 추가

--- src/constants/message.js
+++ src/constants/message.js
@@ -1,5 +1,7 @@
 const GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+const USER_NUMBER_INPUT_REQUEST_MESSAGE = "숫자를 입력해 주세요 : ";
 
 module.exports = {
   GAME_START_MESSAGE,
+  USER_NUMBER_INPUT_REQUEST_MESSAGE,
 };
