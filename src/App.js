3d7f2d3d52bbacbba668bf634969afbadab91fb1
Dahye <06robin11@gmail.com>
2022-11-04 23:40:31 +0900

    refactor: capitalize every constant, change arrow function into method
    I learned that arrow function is considered as property not method.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,35 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
 const REPLY = {
-  replay: "1",
-  gameEnd: "2",
+  REPLAY: "1",
+  GAMEEND: "2",
 };
 
 const MESSAGE = {
-  gameStart: "숫자 야구 게임을 시작합니다.",
-  askReplay: "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
-  gameEnd: "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
-  askNumber: "숫자를 입력해주세요 :",
+  GAMESTART: "숫자 야구 게임을 시작합니다.",
+  ASKREPLAY: "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
+  GAMEEND: "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
+  ASKNUMBER: "숫자를 입력해주세요 :",
 };
 
-const ERRORMESSAGE = {
-  repeat: "중복되지 않는 숫자 3개를 입력해야 합니다.",
-  quantity: "숫자 3개를 입력해야 합니다.",
-  notNumber: "숫자만 입력해야 합니다.",
+const ERROR_MESSAGE = {
+  REPEAT: "서로 다른 숫자 3개를 입력해야 합니다.",
+  QUANTITY: "숫자 3개를 입력해야 합니다.",
+  NOTNUMBER: "숫자만 입력해야 합니다.",
 };
 
-const STRIKE = "스트라이크";
-const BALL = "볼";
-const NOTHING = "낫싱";
+const BASEBALLTERM = {
+  STRIKE: "스트라이크",
+  BALL: "볼",
+  NOTHING: "낫싱",
+};
 
 class App {
   constructor() {
     this.computerNumber = null;
   }
 
-  generateComputerNumber = () => {
+  generateComputerNumber() {
     const computerNumber = [];
     while (computerNumber.length < 3) {
       const randomNumber = MissionUtils.Random.pickNumberInRange(1, 9);
@@ -36,16 +38,16 @@ class App {
       }
     }
     this.computerNumber = computerNumber;
-  };
+  }
 
-  isEveryNumberUnique = (nums) => {
+  isEveryNumberUnique(nums) {
     return nums.length === new Set(nums).size;
-  };
+  }
 
-  getUserNumber = () => {
+  getUserNumber() {
     let userNumber;
 
-    MissionUtils.Console.readLine(MESSAGE.askNumber, (inputNumber) => {
+    MissionUtils.Console.readLine(MESSAGE.ASKNUMBER, (inputNumber) => {
       userNumber = Array.from(inputNumber, Number);
     });
 
@@ -57,23 +59,23 @@ class App {
     }
 
     return userNumber;
-  };
+  }
 
-  checkValidity = (userNumber) => {
+  checkValidity(userNumber) {
     if (userNumber.length !== 3) {
-      throw new Error(ERRORMESSAGE.quantity);
+      throw new Error(ERROR_MESSAGE.QUANTITY);
     }
 
     if (!this.isEveryNumberUnique(userNumber)) {
-      throw new Error(ERRORMESSAGE.repeat);
+      throw new Error(ERROR_MESSAGE.REPEAT);
     }
 
     if (!userNumber.every((num) => Number.isInteger(num))) {
-      throw new Error(ERRORMESSAGE.notNumber);
+      throw new Error(ERROR_MESSAGE.NOTNUMBER);
     }
-  };
+  }
 
-  getBallAndStrikeNumber = (computer, user) => {
+  getBallAndStrikeNumber(computer, user) {
     const ballNum = user
       .filter((item, ind) => item !== computer[ind])
       .filter((item) => computer.includes(item)).length;
@@ -81,44 +83,44 @@ class App {
     const strikeNum = user.filter((item, ind) => item === computer[ind]).length;
 
     return [ballNum, strikeNum];
-  };
+  }
 
-  convertNumberToMessage = (matchNum) => {
+  convertNumberToMessage(matchNum) {
     const [ballNum, strikeNum] = matchNum;
 
-    let message = `${ballNum === 0 ? "" : ballNum + BALL} ${
-      strikeNum === 0 ? "" : strikeNum + STRIKE
+    let message = `${ballNum === 0 ? "" : ballNum + BASEBALLTERM.BALL} ${
+      strikeNum === 0 ? "" : strikeNum + BASEBALLTERM.STRIKE
     }`;
 
     if (matchNum.every((item) => item === 0)) {
-      message = NOTHING;
+      message = BASEBALLTERM.NOTHING;
     }
 
     return message.trim();
-  };
+  }
 
-  showMessage = (message) => {
+  showMessage(message) {
     MissionUtils.Console.print(message);
 
     if (message === "3스트라이크") {
-      MissionUtils.Console.print(MESSAGE.gameEnd);
+      MissionUtils.Console.print(MESSAGE.GAMEEND);
       this.askToPlayAgain();
     } else {
       this.compareNumbers();
     }
-  };
+  }
 
-  askToPlayAgain = () => {
-    MissionUtils.Console.readLine(MESSAGE.askReplay, (answer) => {
-      if (answer === REPLY.replay) {
+  askToPlayAgain() {
+    MissionUtils.Console.readLine(MESSAGE.ASKREPLAY, (answer) => {
+      if (answer === REPLY.REPLAY) {
         this.playNewGame();
-      } else if (answer === REPLY.gameEnd) {
+      } else if (answer === REPLY.GAMEEND) {
         MissionUtils.Console.close();
       }
     });
-  };
+  }
 
-  compareNumbers = () => {
+  compareNumbers() {
     const userNumber = this.getUserNumber();
     const ballAndStrikeNumber = this.getBallAndStrikeNumber(
       this.computerNumber,
@@ -126,15 +128,15 @@ class App {
     );
     const result = this.convertNumberToMessage(ballAndStrikeNumber);
     this.showMessage(result);
-  };
+  }
 
-  playNewGame = () => {
+  playNewGame() {
     this.generateComputerNumber();
     this.compareNumbers();
-  };
+  }
 
   play() {
-    MissionUtils.Console.print(MESSAGE.gameStart);
+    MissionUtils.Console.print(MESSAGE.GAMESTART);
     this.playNewGame();
   }
 }
