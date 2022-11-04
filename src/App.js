05847af9d45994c1410efe0b4b98be680fa3927a
sujungYu <su_moment@naver.com>
2022-11-04 22:18:48 +0900

    feat(getUserAnswer) : 숫자 입력

--- src/App.js
+++ src/App.js
@@ -15,6 +15,12 @@ class App {
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
   }
 
+  getUserAnswer() {
+    MissionUtils.Console.readLine('숫자를 입력해주세요 : ', answer => {
+      console.log(answer);
+    });
+  }
+
   play() {}
 }
 
