629a94dd3565f16844431309c8fc71f2fd3fdb78
Tory <88531407+1two13@users.noreply.github.com>
2022-11-04 15:10:04 +0900

    refactor(BaseballGame): 삭제한 countStrikeBallNothing, resultsForCount 파일의 코드 추가

--- src/BaseballGame.js
+++ src/BaseballGame.js
@@ -1,14 +1,35 @@
 const { Console } = require("@woowacourse/mission-utils");
 const pickedNumberByComputer = require("./pickedNumberByComputer");
 const input = require("./util/input");
-const countStrikeBallNothing = require("./countStrikeBallNothing");
-const resultsForCount = require("./resultsForCount");
-const GameManager = require("./GameManager");
 
 class BaseballGame {
   constructor() {
     this.pickedNumberByComputer = pickedNumberByComputer();
-    this.threeStrike = false;
+  }
+
+  countStrikeBallNothing(pickedNumberByUser, pickedNumberByComputer) {
+    let strike = 0;
+    let ball = 0;
+    let nothing = 0;
+    pickedNumberByComputer.forEach((num, idx) => {
+      // 같은 수이고,
+      if (pickedNumberByComputer.includes(Number(pickedNumberByUser[idx]))) {
+        // 같은 자리일 때
+        if (num === Number(pickedNumberByUser[idx])) strike += 1;
+        // 다른 자리일 때
+        else ball += 1;
+        // 같은 수가 없을 때
+      } else nothing += 1;
+    });
+
+    return [strike, ball, nothing];
+  }
+
+  printResultsForCount(strike, ball, nothing) {
+    if (nothing === 3) Console.print(`낫싱`);
+    else if (strike > 0 && ball === 0) Console.print(`${strike}스트라이크`);
+    else if (ball > 0 && strike === 0) Console.print(`${ball}볼`);
+    else Console.print(`${ball}볼 ${strike}스트라이크 `);
   }
 
   async isThreeStrike() {
