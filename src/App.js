24b9baf58c841f6a3b9a9aa90aff705f04f922c3
ksg2388 <ksg2388@naver.com>
2022-11-04 13:25:56 +0900

    feat(App): getBall 함수 추가

--- src/App.js
+++ src/App.js
@@ -12,15 +12,28 @@ const getStrike = (computer, user) => {
   return strike;
 };
 
+const getBall = (computer, user) => {
+  let ball = 0;
+
+  for (let i = 0; i < 3; i++) {
+    if (computer.includes(user[i]) && computer[i] !== user[i]) {
+      ball += 1;
+    }
+  }
+
+  return ball;
+};
+
 const checkResult = (computer, userNumber) => {
   const user = userNumber.split('').map((a) => Number(a));
   const strike = getStrike(computer, user);
+  const ball = getBall(computer, user);
 
   if (strike === 3) {
     MissionUtils.Console.print(`${strike}스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료`);
     MissionUtils.Console.close();
   } else {
-    MissionUtils.Console.print(`${strike}스트라이크`);
+    MissionUtils.Console.print(`${ball}볼 ${strike}스트라이크`);
     MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (userNumber) => checkResult(computer, userNumber));
   }
 };
