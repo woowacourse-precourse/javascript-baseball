1a1170613f943a2b707d143e6f8374b41a244a67
woo-jk <201702042@o.cnu.ac.kr>
2022-11-04 19:57:24 +0900

    feat: 게임 시작 문구 출력 함수 구현

--- src/App.js
+++ src/App.js
@@ -13,6 +13,10 @@ class App {
 
     return [...threeRandomNumber].join("");
   }
+
+  printGameStartPhrase() {
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+  }
 }
 
 const app = new App();
