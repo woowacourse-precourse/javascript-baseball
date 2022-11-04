a3aa257e3a04ce8f3d94d30a44b3fdabe51b7e4e
BuildTools <unconfigured@null.spigotmc.org>
2022-11-04 21:53:12 +0900

    입력 받기

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ const MissionUtils = require('@woowacourse/mission-utils');
 class App {
   play() {
     console.log('숫자 야구 게임을 시작합니다.');
+    MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (message) =>
+      MissionUtils.Console.print(message)
+    );
   }
 }
 
