6c14f4d16ce48755da3f1dfb5edb3b0a43d13164
yuna Kim <yuna000804@gmail.com>
2022-11-04 14:36:16 +0900

    컴퓨터가 랜덤으로 숫자 3개 선택

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
   play() {}
 }
 
+const letComputerInput = function() {
+  const computer = [];
+  while (computer.length < 3) {
+    const number = MissionUtils.Random.pickNumberInRange(1, 9);
+    if (!computer.includes(number)) {
+      computer.push(number);
+    }
+  }
+}
+
 module.exports = App;
+
