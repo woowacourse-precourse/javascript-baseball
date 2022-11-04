39ec970897194bc2fb2b87b6c464c8e2b93ae4a4
leeuihyun <meta212121@naver.com>
2022-11-04 16:34:15 +0900

    feat : 사용자의 값, 컴퓨터의 값을 비교하여 스트라이크 개수 반환 메서드

---
+++ src/Compare.js
@@ -0,0 +1,18 @@
+const Mission = require('./Mission');
+
+class Compare extends Mission {
+  constructor(computerNumbers, userNumbers) {
+    super();
+    this.computerNumbers = String(computerNumbers).split(',');
+    this.userNumbers = String(userNumbers).split(',');
+  }
+
+  getStrikeCount() {
+    return this.computerNumbers.reduce((acc, value, index) => {
+      if (value === this.userNumbers[index]) return acc + 1;
+      return acc;
+    }, 0);
+  }
+}
+
+module.exports = Compare;
