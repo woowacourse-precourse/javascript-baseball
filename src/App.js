41c1d9b28191b39bae63cb0a6761629251c1a3ee
ShimSungBo <shimdb96@gmail.com>
2022-11-04 17:27:47 +0900

    feat USER의 입력 값 유효 테스트 구현

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 const Console = MissionUtils.Console;
 const Random = MissionUtils.Random;
+const test = require("./test");
 
 class App {
   play() {
@@ -14,8 +15,33 @@ class App {
     }
     makeComputerAnswer();
     console.log(computerAnswer);
+    Console.print("숫자 야구 게임을 시작합니다.");
+
+    function isVaildData(input) {
+      const vaildLength = input.length === 3;
+      const vaildNumber = [...input].map((num) => {
+        if (isNaN(num)) return false;
+        return +num;
+      });
+      const isUniqueNumber = new Set(input.split("")).size === 3;
+
+      if (!vaildLength || !vaildNumber || !isUniqueNumber) {
+        throw new Error();
+      }
+
+      return vaildNumber;
+    }
   }
 }
-const a = new App();
-console.log(a.play());
+
+[...a].map((num) => {
+  if (isNaN(num)) return false;
+  return +num;
+});
+
+// const a = new App();
+// console.log(a.play());
+Console.readLine("숫자를 입력해주세요 : ", (answer) => {
+  console.log(answer);
+});
 module.exports = App;
