436f3fb8182b222610c5750be7d364143d02bb64
Subin Jo <sbjo1202@naver.com>
2022-11-05 01:53:55 +0900

    feat: 기능 1 게임 시작 문구 출력하기 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  play() {
+    PrintGameStartPhrase();
+
+  }
 }
+
+// 기능 1
+function PrintGameStartPhrase() {
+  MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+}
+
+
+
+const baseballGame = new App();
+baseballGame.play();
 
 module.exports = App;
