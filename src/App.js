7487e1f63556a93e64b65ee8458649ae52fc930f
Deemou <4106243@naver.com>
2022-11-05 07:31:51 +0900

    feat: print() 추가
    메시지를 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
   play() {}
+  print(message) {
+    MissionUtils.Console.print(message);
+  }
 }
+const app = new App();
+app.play();
 
 module.exports = App;
