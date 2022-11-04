3adcb0ecd550b7d666030109089f8ac5eb3e042f
yong <tkddbs1644@gmail.com>
2022-11-04 22:58:48 +0900

    test: isValidNumber 메서드로 입력 받은 숫자가 서로 다른 3자리의 숫자인지 확인

--- __tests__/BaseballGameUnitTest.js
+++ __tests__/BaseballGameUnitTest.js
@@ -20,4 +20,17 @@ describe("숫자 야구 게임 단위 테스트", () => {
       expect(removeDuplicatedNumber(randomNumber).length).toEqual(3);
     });
   });
+
+  test("isValidNumber 메서드로 입력 받은 숫자가 서로 다른 3자리의 숫자인지 확인", () => {
+    const app = new App();
+    expect(app.isValidNumber("")).toBe(false);
+    expect(app.isValidNumber(" ")).toBe(false);
+    expect(app.isValidNumber("\n")).toBe(false);
+    expect(app.isValidNumber("1")).toBe(false);
+    expect(app.isValidNumber("12")).toBe(false);
+    expect(app.isValidNumber("123")).toBe(true);
+    expect(app.isValidNumber("111")).toBe(false);
+    expect(app.isValidNumber("1234")).toBe(false);
+    expect(app.isValidNumber("abc")).toBe(false);
+  });
 });
