9c731374d0fe50bc6aeeb0363787391e941dec34
Buyoung Chung <sunarol@naver.com>
2022-11-04 22:41:22 +0900

    fix: now functions execute after readline

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
-  constructor(computerRandomNumber = null, userAnswer = null) {
+  constructor(computerRandomNumber = [], userAnswer = null) {
     this.computerRandomNumber = computerRandomNumber;
     this.userAnswer = userAnswer;
   }
+
   play() {
     this.greeting();
-    this.computerRandomNumber = this.makeRandomNumberArray();
-    this.userAnswer = this.inputUserAnswer();
-    this.parseUserAnswertoString();
-    this.printBallStrike(this.checkBallStrike(this.userAnswer));
+    this.makeRandomNumberArray();
+    this.inputUserAnswer();
   }
 
   greeting() {
@@ -19,39 +18,31 @@ class App {
 
   inputUserAnswer() {
     MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (answer) => {
-      this.userAnswer = answer;
+      this.parseUserAnswertoString(answer);
+      this.printBallStrike(this.checkBallStrike(this.computerRandomNumber, this.userAnswer));
     });
   }
 
   makeRandomNumberArray() {
-    this.computerRandomNumber = MissionUtils.Random.pickUniqueNumbersInRange(
-      1,
-      9,
-      3
-    );
+    this.computerRandomNumber = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
   }
 
-  parseUserAnswertoString() {
-    this.userAnswer = String(this.userAnswer);
+  parseUserAnswertoString(answer) {
+    this.userAnswer = String(answer);
   }
 
-  checkBallStrike(computerRandomNumber, userAnswer) {
+  checkBallStrike(rn, input) {
     let ball = 0;
     let strike = 0;
 
     for (let i = 0; i < input.length; i++) {
-      if (
-        computerRandomNumber.includes(parseInt(userAnswer[i])) &&
-        computerRandomNumber[i] == userAnswer[i]
-      ) {
+      if (rn.includes(parseInt(input[i])) && rn[i] == input[i]) {
         strike++;
-      } else if (
-        computerRandomNumber.includes(parseInt(userAnswer[i])) &&
-        computerRandomNumber[i] != userAnswer[i]
-      ) {
+      } else if (rn.includes(parseInt(input[i])) && rn[i] != input[i]) {
         ball++;
       }
     }
+
     return [ball, strike];
   }
 
@@ -60,7 +51,6 @@ class App {
     let strike = ballStrike[1];
 
     if (ball === 0 && strike !== 0) {
-      MissionUtils;
       console.log(`${strike}스트라이크`);
     } else if (strike === 0 && ball !== 0) {
       console.log(`${ball}볼`);
