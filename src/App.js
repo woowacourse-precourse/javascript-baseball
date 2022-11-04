ffdf6ee7e9a21273ed1486def2ccae834b814e72
dudgp128 <20181048@sungshin.ac.kr>
2022-11-04 14:40:50 +0900

    FEAT : 입력값을 검증한다

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+const MissionUtils = require("@woowacourse/mission-utils");
 class App {
-  play() {}
+  answer = "";
+  isValid = null;
+  constructor() {
+    this.answer = MissionUtils.Random.pickNumberInRange(123, 987);
+  }
+  isValid = (number) => {
+    let regex = new RegExp(/^[1-9]{3}&/);
+    let overlap = new RegExp(/([1-9])\1/);
+
+    return regex.test(String(number))
+      ? overlap.test(String(number))
+        ? true
+        : false
+      : false;
+  };
+  play() {
+    MissionUtils.Console.readLine("닉네임을 입력해주세요.", (inputNum) => {
+      if (this.isValid(inputNum)) {
+      } else {
+        throw "유효하지 않는 값";
+      }
+    });
+  }
 }
 
 module.exports = App;
