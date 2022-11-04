bf37c4fdfd749574cbbc05c3da32b7c3553df240
ashleysyheo <ashleysyheo@gmail.com>
2022-11-05 00:35:48 +0900

    feat: validate user input number

--- src/components/Game.js
+++ src/components/Game.js
@@ -3,8 +3,18 @@ const MissionUtils = require('@woowacourse/mission-utils');
 class Game {
   getUserInputNumber() {
     MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (input) => {
-      MissionUtils.Console.print(input);
+      this.validateUserInputNumber(input);
     });
+  }
+
+  validateUserInputNumber(input) {
+    const INPUT_SET = new Set(input.split(''));
+
+    if (INPUT_SET.size !== 3 || !/^[1-9]+$/g.test(input) || input.length !== 3) {
+      throw new Error('조건에 맞는 숫자를 다시 입력해주세요!');
+    }
+
+    return true;
   }
 }
 
