e3a4a414653e8c1e8eb02abca6710c3d2fa78adb
woo-jk <201702042@o.cnu.ac.kr>
2022-11-04 17:22:13 +0900

    test: 뽑은 숫자의 길이 테스트 코드 작성

--- __tests__/DrawNumbersTest.js
+++ __tests__/DrawNumbersTest.js
@@ -8,7 +8,15 @@ describe.only("숫자 뽑기 테스트", () => {
     expect(threeRandomNumber.length).toHaveLength(3);
   });
 
-  test("숫자 범위가 1부터 9까지인지 확인", () => {});
+  test("숫자 범위가 1부터 9까지인지 확인", () => {
+    const app = new App();
+    const threeRandomNumber = app.drawThreeRandomNumbers();
+
+    threeRandomNumber.split("").forEach((number) => {
+      expect(number).toBeGreaterThanOrEqual(1);
+      expect(number).toBeLessThanOrEqual(9);
+    });
+  });
 
   test("각 숫자가 중복되어 있는지 확인", () => {});
 });
