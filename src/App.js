5fec765c5a3c94a226bdc5434e8ae8b9f8c82bd9
MAGHC <windowsapple95@gmail.com>
2022-11-04 23:30:02 +0900

    feat: 컴퓨터/유저 숫자비교 기능 추가

--- src/App.js
+++ src/App.js
@@ -4,25 +4,41 @@ class App {
   constructor() {
     this.GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
     this.isValidUserNumber = false;
-    this.userNumber = 0;
+    this.computerNumberArray = App.generateComputerNumberArray();
   }
 
   play() {
     MissionUtils.Console.print(this.GAME_START_MESSAGE);
-    App.getUserInputNumber();
+    this.getUserInputNumber();
   }
 
-  static convertUserNumberToArray() {
-    if (this.isValidUserNumber) return this.userNumber.toString().split("");
+  compareNumber() {
+    const userNumberArray = this.convertUserNumberToArray().map((number) => +number);
+    let strikeCount = 0;
+    let ballCount = 0;
+    console.log(userNumberArray, this.computerNumberArray);
+
+    userNumberArray.forEach((userNumber, index) => {
+      if (this.computerNumberArray.includes(userNumber)) {
+        if (this.computerNumberArray[index] === userNumber) strikeCount += 1;
+        else ballCount += 1;
+      }
+    });
+  }
+
+  convertUserNumberToArray() {
+    if (this.isValidUserNumber) {
+      return this.userNumber.toString().split("");
+    }
     return false;
   }
 
-  static getUserInputNumber() {
+  getUserInputNumber() {
     return MissionUtils.Console.readLine("숫자를입력해주세요: ", (input) => {
       MissionUtils.Console.print(`입력하신 숫자는 ${input} 입니다.`);
       this.isValidUserNumber = true;
       this.userNumber = input;
-      MissionUtils.Console.close();
+      this.compareNumber();
     });
   }
 
