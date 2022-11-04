66a2e8062b2e54f5fb0eaec7e0db3618fc4bf9ff
Hannah Ahn <ahnlook90@gmail.com>
2022-11-04 16:07:11 +0900

    feat: answer result function
    And exception handling in play functin

--- src/App.js
+++ src/App.js
@@ -1,41 +1,43 @@
 import * as MissionUtils from "@woowacourse/mission-utils"
 
-function announcement (status) {
-  switch (status) {
-    case 'start':
-      Console.print('숫자 야구 게임을 시작합니다.')
-      break
-    case 'requestAndAnswer':
-      // 숫자 3개 인풋을 받아서 결과 출력
-      Console.readLine('숫자를 입력해주세요', (answer) => {
-        console.log(`닉네임: ${answer}`);
-      })
-      break
-    case 'success':
-      Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료')
-      break
-    case 'requestRestart':
-      Console.print('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.')
-      break
-  }
-}
-
 function initCount () {
   return [0, 0]
 }
 
 function getRandomNumber () {
-  let randomNumber = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3)
+  const randomNumber = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3)
   return randomNumber.join('')
 }
 
+function answerResult (strike, ball) {
+  if (strike === 3) {
+    return MissionUtils.Console.print(`${strike}스트라이크 3개의 숫자를 모두 맞히셨습니다! 게임 종료
+    게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.`)
+  }
+
+  if (!strike+ball) {
+    return MissionUtils.Console.print("낫싱")
+  }
+
+  if (strike > 0 && !ball) {
+    return MissionUtils.Console.print(`${strike}스트라이크`)
+  }
+
+  if (!strike && ball > 0) {
+    return MissionUtils.Console.print(`${ball}볼`)
+  }
+
+  if (strike > 0 && ball > 0) {
+    return MissionUtils.Console.print(`${ball}볼 ${strike}스트라이크`)
+  }
+}
+
 // 사용자에게 number 입력 받아 실행하는 함수
-function game (number) {
-  if (!isNaN(number)) {
+function palyBaseballGame (number) {
+  if (isNaN(number)) {
     throw new Error('숫자를 입력하세요.')
   }
 
-  const correctNum = getRandomNumber() // 랜덤 숫자 부분을 class constructor로 빼야할 것 같음
   const num = String(number)
   
   let [strike, ball] = initCount()
@@ -50,45 +52,12 @@ function game (number) {
       }
   }
 
-  if (strike === 3) {
-    MissionUtils.Console.print(`${strike}스트라이크
-    3개의 숫자를 모두 맞히셨습니다! 게임 종료
-    게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.`)
-
-    [strike, ball] = initCount()
-    return 
-  }
-
-  if (!strike+ball) {
-    MissionUtils.Console.print("낫싱")
-
-    [strike, ball] = initCount()
-    return
-  }
-
-  if (strike > 0 && !ball) {
-    MissionUtils.Console.print(`${strike}스트라이크`)
-
-    [strike, ball] = initCount()
-    return 
-  }
-
-  if (!strike && ball > 0) {
-    MissionUtils.Console.print(`${ball}볼`)
-
-    [strike, ball] = initCount()
-    return 
-  }
-
-  if (strike > 0 && ball > 0) {
-    MissionUtils.Console.print(`${ball}볼 ${strike}스트라이크`)
-
-    [strike, ball] = initCount()
-    return 
-  }
+  answerResult(strike, ball)
+  [strike, ball] = initCount()
+  
 }
 class App {
-  play() {}
+  play()
 }
 
 module.exports = App;
