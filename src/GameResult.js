cd65895ae1089cfcd044ac08b5ad498fc66f3e9d
Rki0 <pky00823@gmail.com>
2022-11-04 15:24:26 +0900

    refactor: modularzie scoring function
    Modularized scoring function.
    To comply with indent rule, scoring function has been modularized.

--- src/GameResult.js
+++ src/GameResult.js
@@ -13,20 +13,24 @@ class GameResult {
 
     const USER_INPUT = userInput.split("").map((item) => Number(item));
 
-    for (let index = 0; index < this.COMPUTER.length; index++) {
-      if (this.COMPUTER[index] === USER_INPUT[index]) {
-        this.strike++;
-        continue;
-      }
-
-      if (USER_INPUT.includes(this.COMPUTER[index])) {
-        this.ball++;
-      }
-    }
+    this.checkBallOrStrike(USER_INPUT);
 
     MissionUtils.Console.print(this.strike);
     MissionUtils.Console.print(this.ball);
     MissionUtils.Console.close();
+  }
+
+  checkBallOrStrike(userInputArray) {
+    for (let index = 0; index < this.COMPUTER.length; index++) {
+      if (this.COMPUTER[index] === userInputArray[index]) {
+        this.strike++;
+        continue;
+      }
+
+      if (userInputArray.includes(this.COMPUTER[index])) {
+        this.ball++;
+      }
+    }
   }
 }
 
