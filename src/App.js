c7203a60a1b29ea1e7829482b01bd30d4ca1dcb2
oio337a <oio337a@gmail.com>
2022-11-04 21:36:08 +0900

    retactor : print start message and pick random numbers in startGame

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
-  pickNumbers() {
-    MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
+  print(message) {
+    return MissionUtils.Console.print(message);
   }
 
+  pickNumbers() {
+    return MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
+  }
+
+  startGame() {
+    this.print("숫자 야구 게임을 시작합니다.");
+    return this.pickNumbers();
+  }
   play() {
-    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
-    const randoms = this.pickNumbers();
-    MissionUtils.Console.print(randoms);
+    this.startGame();
     return MissionUtils.Console.close();
   }
 }
