a6332da9ece99e872144b0e3096e797071b20d4f
pangkyu <baesense@naver.com>
2022-11-05 01:12:00 +0900

    feat : 유저 데이터 유효성검사 함수
    유저가 입력한 데이터가 1-9의 값을 가지고 있는지, 3자리를 입력했는지 체크하는 기능

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
 const { Console, Random } = require("@woowacourse/mission-utils");
 
-function checkAnswer(input) {}
+/**
+ * @description 유저가 입력한 값이 정상 데이터인지 검사
+ */
+/**
+ * @description 유저입력값의 유효성을 확인하는 기능
+ */
+function checkVaildUserInputValue(userInput) {
+  let check = /^[1-9]+$/;
+  if (!userInput.length === 3) {
+    throw new Error("3자리의 숫자를 입력해야합니다.");
+  }
+  for (let i = 0; i < userInput.length; i++) {
+    if (!check.test(userInput[i])) {
+      throw new Error("1~9까지의 숫자만 입력 가능합니다.");
+    }
+  }
+}
 
+/**
+ * @description 유저가 입력한 값이 답과 일치하는지 비교
+ */
+function checkAnswer(answerNumber, userInput) {
+  let splitAnswerNumber = [];
+  for (let i = 0; i < answerNumber.length; i++) {}
+  console.log(splitAnswerNumber);
+}
+
+/**
+ * @description 컴퓨터가 랜덤으로 만든 정답값
+ */
 function cpuMakeAnswer() {
+  // 입력받는 방법몰라서 임의값넣느라 미사용처리
   const computer = [];
   while (computer.length < 3) {
     const number = Random.pickNumberInRange(1, 9);
@@ -18,27 +47,12 @@ class App {
   constructor() {}
   play() {
     const userInput = [1, 2, 3];
-    const answerNumber = cpuMakeAnswer();
-    Console.print(answerNumber);
+    // const answerNumber = cpuMakeAnswer();
+    const answerNumber = [3, 2, 9];
 
     Console.print("숫자 야구게임을 시작합니다.");
-    // console.log(answerNumber);
-    /**
-     * todo : readLine이 안먹히는데 어떻게 사용하는지 알아야함
-     */
-    // inputNumber() {
-    //   Console.readLine("숫자를 입력해주세요.", (inputNumber) => {
-    //     this.inputNumber = inputNumber;
-    //   });
-    // }
-    // const inputNum = [];
-
-    // Console.readLine("서로 다른 숫자를 3개 입력하시오.", (answer) => {
-    //   Console.print(answer);
-
-    //   Console.close();
-    // });
-    //한 개의 입력(띄어쓰기x)
+    checkVaildUserInputValue(userInput);
+    checkAnswer(answerNumber, userInput);
   }
 
   callResult() {}
