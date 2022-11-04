36b1da8c4577387be73c3b01947b53fb16cc9601
Deemou <4106243@naver.com>
2022-11-05 08:04:59 +0900

    test: generateRandomNumber() 테스트 케이스 추가

--- __tests__/BaseballGameTest.js
+++ __tests__/BaseballGameTest.js
@@ -10,4 +10,17 @@ describe("숫자 야구 게임", () => {
 
     expect(logSpy).toHaveBeenCalledWith(message);
   });
+  test("서로 다른 세 자리 수를 랜덤으로 생성", () => {
+    const DIGITS = 3;
+    const set = new Set();
+
+    const app = new App();
+    const randomNumber = app.generateRandomNumber(DIGITS);
+
+    randomNumber.forEach((num) => {
+      expect(num.toString()).toMatch(/[1-9]/);
+      set.add(num);
+    });
+    expect(set.size).toBe(DIGITS);
+  });
 });
