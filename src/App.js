c0024c48840c22edcb4c5f67df0a5223006c381f
DongWooKim97 <nanaukim@naver.com>
2022-11-04 21:03:43 +0900

    feat: 컴퓨터가 게임을 진행할 숫자 배열 생성 기능 작성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
 class App {
-  play() {}
+  constructor() {
+    this.missionRandom = require("@woowacourse/mission-utils").Random;
+  }
+  play() {
+    return this.makeRandomNumbers();
+  }
+
+  makeRandomNumbers() {
+    const computerInputNumbersArray = [];
+    while (computerInputNumbersArray.length < 3) {
+      const eachNumber = this.missionRandom.pickNumberInRange(1, 9);
+      if (!computerInputNumbersArray.includes(eachNumber)) {
+        computerInputNumbersArray.push(eachNumber);
+      }
+    }
+    return computerInputNumbersArray.join("");
+  }
 }
+const app = new App();
+app.play();
 
 module.exports = App;
