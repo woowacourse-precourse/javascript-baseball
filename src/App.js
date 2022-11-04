9b32f182ee10ae704ebe9d0f0b8e3a71f7e0f3ba
Jeong seulho <wjdtmfgh@yu.ac.kr>
2022-11-04 19:13:22 +0900

    Feat(feature No.3): check valid input
    define checkValid function and NUMBER_LENGTH_MODE = 3 (on constructor)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
+  constructor() {
+    // 3자리 숫자 모드
+    this.NUMBER_LENGTH_MODE = 3;
+  }
+
   play() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
     this.startGame();
@@ -10,6 +15,24 @@ class App {
     MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (num) => {
       this.checkNum(num);
     });
+  }
+
+  checkNum(num) {
+    this.checkValid(num);
+  }
+
+  checkValid(num) {
+    const IS_VALID_LENGTH = num.length === this.NUMBER_LENGTH_MODE;
+    const IS_TYPE_NUMBER = !Number.isNaN(num);
+
+    const numSet = new Set(num);
+    const IS_NO_DUPLICATE = num.length === numSet.size;
+
+    const IS_VALID = IS_VALID_LENGTH && IS_TYPE_NUMBER && IS_NO_DUPLICATE;
+
+    if (!IS_VALID) {
+      throw new Error("올바르지 않은 입력");
+    }
   }
 }
 
