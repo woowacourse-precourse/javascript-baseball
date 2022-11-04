da9eab64cc5b6ed1d1f04d1b7d064e537c81da54
Minhye Kang <alsgp9898@naver.com>
2022-11-04 13:15:00 +0900

    feat: 게임 시작 문구 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
+const GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+
 class App {
-  play() {}
+  printGameStartMessage() {
+    MissionUtils.Console.print(GAME_START_MESSAGE);
+  }
+
+  play() {
+    this.printGameStartMessage();
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
