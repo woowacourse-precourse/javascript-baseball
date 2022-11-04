56e83da5a830115e75688c51391a156a96987dfa
Suyeon <msjgr2801@gmail.com>
2022-11-04 12:59:58 +0900

    Feat: Edit Console Log
    - 게임이 끝난 후 '3개의 숫자를 모두 맞히셨습니다! 게임 종료' 글자 추가

--- src/App.js
+++ src/App.js
@@ -108,7 +108,8 @@ class App {
     let isRestart;
     
     MissionUtils.Console.readLine(
-      "게임을 재시작하려면 1, 종료하려면 2를 입력해주세요.",
+      `3개의 숫자를 모두 맞히셨습니다! 게임 종료₩n
+      게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.`,
       (answer) => {
         answer = Number(answer);
         isRestart = answer;
