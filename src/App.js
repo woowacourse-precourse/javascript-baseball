022c5000db8b10ee6c505c579aab833c603d928c
sungjihyun <jhsung23@gmail.com>
2022-11-04 16:43:54 +0900

    feat: 컴퓨터의 숫자를 서로 다른 세 자리의 수로 생성하기
    MissionUtils 라이브러리를 활용해 랜덤 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
+  #computer = [];
+
+  constructor() {
+    while (this.#computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!this.#computer.includes(number)) {
+        this.#computer.push(number);
+      }
+    }
+  }
+
   play() {}
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
