8dbe4b3df0f3d52e2260460f0e4fbc41c601d799
pione3r <jhyong0626@gmail.com>
2022-11-04 15:43:04 +0900

    feat: printResult function
    added printResult function
    - print result about comparing

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
   play() {
-    const computerNumber = setComputerNumber();
-    const userNumber = setUserNumber();
-    let compareResult = compareNumbers(computerNumber, userNumber);
+    let computerNumber = setComputerNumber();
+
+    while (true) {
+      let userNumber = setUserNumber();
+      let compareResult = compareNumbers(computerNumber, userNumber);
+      if (compareResult == "3스트라이크") break;
+      else break;
+    }
   }
 }
-
-const app = new App();
-app.play();
 
 function setComputerNumber() {
   const computer = [];
@@ -27,7 +29,7 @@ function setUserNumber() {
   let userNumber = "";
   MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (answer) => {
     userNumber = answer;
-  })
+  });
 
   return userNumber;
 }
@@ -39,7 +41,7 @@ function compareNumbers(computerNumber, userNumber) {
   for (let i = 0; i < computerNumber.length; i++) {
     let num = Number(userNumber[i]);
     if (computerNumber[i] == num) strike++;
-    if ((computerNumber[i] != num) && computerNumber.includes(num)) ball++;
+    if (computerNumber[i] != num && computerNumber.includes(num)) ball++;
   }
 
   if (ball === 0 && strike === 0) result = "낫싱";
@@ -47,7 +49,17 @@ function compareNumbers(computerNumber, userNumber) {
   if (ball !== 0 && strike === 0) result = `${ball}볼`;
   if (ball !== 0 && strike !== 0) result = `${ball}볼 ${strike}스트라이크`;
 
+  printResult(result);
+
   return result;
+}
+
+function printResult(result) {
+  MissionUtils.Console.print(result);
+
+  if (result == "3스트라이크") {
+    MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+  }
 }
 
 module.exports = App;
