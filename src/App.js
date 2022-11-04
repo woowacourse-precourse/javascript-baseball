3067515ce3595a116f7004b3ed744b8527205e6b
LEEJW1953 <jiwonlee1953@naver.com>
2022-11-05 02:43:30 +0900

    feat: 게임 플레이어의 입력을 받는 메서드 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
-  computerPick() { //컴퓨터가 1부터 9 사이의 서로다른 3개의 숫자를 정하는 메서드
-    const COMPUTER=MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
+  computerPick() { // 컴퓨터가 1부터 9 사이의 서로다른 3개의 숫자를 정하는 메서드
+    const COMPUTER = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
     MissionUtils.Console.close();
     return COMPUTER;
   }
-  play() {}
+  playerPick() { // 게임 플레이어의 입력을 받는 메서드
+    const PLAYER = MissionUtils.Console.readLine('숫자를 입력해주세요 :', (input) => {
+      console.log(`숫자 : ${input}`);
+    });
+    MissionUtils.Console.close();
+    return PLAYER;
+  }
+  play() {
+  }
 }
 
 module.exports = App;
