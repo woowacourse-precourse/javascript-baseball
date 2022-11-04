fd7591dfb0acbfb420a69594de0cde5d32846878
dev-redo <mis05041@naver.com>
2022-11-04 23:12:30 +0900

    Fix: 게임 다시 시작할 시 state 재설정 이슈 해결

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-const { Console, Random } = require('@woowacourse/mission-utils');
+const { Console } = require('@woowacourse/mission-utils');
 const CheckValid = require('./CheckValid');
 const Computer = require('./Computer');
 const User = require('./User');
@@ -20,14 +20,13 @@ class App {
     this.checkValid = new CheckValid(NUMBER_LIMIT);
   }
 
-  // TODO: play 로직들 함수로 분리하기
   play() {
     this.computerNum = this.computer.makeNumbers();
     Console.print('숫자 야구 게임을 시작합니다.');
 
-    while (this.isGameEnd === false) {
-      const userInput = this.user.getInputValue();
-      const isUserInputValid = this.checkValid.validateInput(userInput);
+    while (this.isGameEnd) {
+      this.userInput = this.user.getInputValue();
+      this.isUserInputValid = this.checkValid.validateInput(userInput);
 
       if (isUserInputValid === false) {
         throw new Error('유저의 입력값이 유효하지 않습니다!');
@@ -44,14 +43,8 @@ class App {
     }
 
     this.askUserToRestart();
-    // 7. 반복문 탈출 후(= Match 종료) 게임을 다시 할 것 인지 여부를 묻는다. - askUserToRestart
-    //  - 유저가 말한 숫자들과 컴퓨터의 숫자들이 동일할 시 맞추었다는 문구와 함께 게임 종료한다.
-    //    >> 문구:  "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
-    //  - 사용자의 입력값 받은 후, 입력값에 따라 게임을 다시 시작하거나 종료시킨다.
-    //    >>
   }
 
-  // TODO: countBall과 countStrike에서 splittedComputerNum, splittedUserInput 중복되는 것을 어디로 빼줄지 고민하기
   countBall(computerNum, userInput) {
     const splittedComputerNum = [...computerNum];
     const splittedUserInput = [...userInput];
@@ -98,19 +91,25 @@ class App {
   }
 
   askUserToRestart() {
-    const userChoice = Console.readLine('3개의 숫자를 모두 맞히셨습니다! 게임 종료', userChoice => {
-      console.log(`게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n${userChoice}`);
+    Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+    Console.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.', userChoice => {
+      console.log('userChoice :>> ', userChoice);
+
+      if (userChoice === '1') return this.restart();
+      if (userChoice === '2') return this.exit();
+
+      throw new Error('유저의 입력값이 유효하지 않습니다!');
     });
-
-    if (userChoice === 1) this.restart();
-    if (userChoice === 2) return;
-
-    throw new Error('1, 2가 아닌 값을 입력했습니다!');
   }
 
   restart() {
     this.state = INIT_STATE;
     this.play();
+  }
+
+  exit() {
+    Console.print('게임 종료');
+    Console.close();
   }
 }
 
