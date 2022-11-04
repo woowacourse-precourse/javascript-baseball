c9c8973644ff22ddea7efa436afa2c71d1f58bea
Lee-Young-Jae <yjlee1937@naver.com>
2022-11-05 00:38:49 +0900

    feat: 사용자 입력 예외처리 기능 추가

--- src/App.js
+++ src/App.js
@@ -13,7 +13,24 @@ class App {
     }
     return answer;
   }
-  play() {}
+
+  checkUserInput(userInput) {
+    if (userInput.match(/[^1-9]/)) {
+      throw new Error("입력이 숫자가 아닙니다.");
+    }
+    if (userInput.length !== 3) {
+      throw new Error("입력한 숫자의 개수가 3개가 아닙니다.");
+    }
+    if (new Set(userInput.split("")).size !== 3) {
+      throw new Error("입력한 숫자가 중복되었습니다.");
+    }
+  }
+
+  play() {
+    Console.readLine("숫자를 입력해주세요 : ", (userInput) => {
+      this.checkUserInput(userInput);
+    });
+  }
 }
 
 module.exports = App;
