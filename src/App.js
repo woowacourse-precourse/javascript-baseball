4d34d1463ea7f15e6515f9f41dbef482db56b33a
BB-choi <choibobae0809@gmail.com>
2022-11-04 13:53:24 +0900

    feat: 게임에 사용할 3자리 수 생성

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 const MissionUtils = require("@woowacourse/mission-utils");
-const { Console } = MissionUtils;
+const { Console, Random } = MissionUtils;
 
 class App {
+  constructor(count = 3, minNum = 1, maxNum = 9) {
+    this.count = count;
+    this.minNum = minNum;
+    this.maxNum = maxNum;
+  }
+
+  set gameNumber(number) {
+    this._gameNumber = number;
+  }
+
+  get gameNumber() {
+    return this._gameNumber;
+  }
+
   MESSAGES = {
     START: "숫자 야구 게임을 시작합니다.",
     END: "게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
@@ -22,8 +36,29 @@ class App {
     return Console.print(msg);
   }
 
+  createUniqueNumberInList(array) {
+    let pickedNumber = Random.pickNumberInRange(this.minNum, this.maxNum);
+
+    while (array.includes(pickedNumber)) {
+      pickedNumber = Random.pickNumberInRange(this.minNum, this.maxNum);
+    }
+
+    return pickedNumber;
+  }
+
+  createGameNumbers() {
+    const gameNumbers = Array.from({ length: this.count }).reduce(
+      (prev) => [...prev, this.createUniqueNumberInList(prev)],
+      []
+    );
+
+    this.gameNumber = gameNumbers;
+  }
+
   startGame() {
     this.printMessage(this.MESSAGES.START);
+    this.createGameNumbers();
+    this.printMessage(this.gameNumber);
   }
 
   play() {
