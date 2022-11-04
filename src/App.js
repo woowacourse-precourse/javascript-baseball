19ace2005850b1a41de5877da0978b142890d20f
ramda <91455804+gnfnfnr@users.noreply.github.com>
2022-11-04 21:08:21 +0900

    feat: add gameover and gamerestart

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
   constructor() {
-    this.randomAnswer = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
+    this.randomAnswer;
   }
 
   userInput() {
@@ -35,10 +35,7 @@ class App {
     };
 
     MissionUtils.Console.print(userInputNum, this.randomAnswer.join(""));
-    if (userInputNum === this.randomAnswer.join("")) {
-      MissionUtils.Console.print("3스트라이크");
-      MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-    } else if (!result.strikeNum && !result.ballNum) {
+    if (!result.strikeNum && !result.ballNum) {
       MissionUtils.Console.print("낫싱");
     } else if (!result.strikeNum) {
       MissionUtils.Console.print(result.ballNum + "볼");
@@ -49,6 +46,7 @@ class App {
         `${result.ballNum} 볼 ${result.strikeNum} 스트라이크`
       );
     }
+    return result.strikeNum === 3 ? this.gameEnd() : this.userInput();
   }
 
   countBalls(userInputNum) {
@@ -65,8 +63,39 @@ class App {
     }, 0);
   }
 
+  gameEnd() {
+    MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    MissionUtils.Console.readLine(
+      "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
+      (endNum) => {
+        console.log(
+          `게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. ${endNum}`
+        );
+        this.gameEndvalidation(endNum);
+      }
+    );
+  }
+
+  gameEndvalidation(endNum) {
+    switch (endNum) {
+      case 1:
+        MissionUtils.Console.close();
+        break;
+
+      case 2:
+        this.play();
+        break;
+
+      default:
+        MissionUtils.Console.print(
+          "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
+        );
+    }
+  }
+
   play() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    this.randomAnswer = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
     this.userInput();
   }
 }
