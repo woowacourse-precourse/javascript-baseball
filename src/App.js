774d1dc265d60a42a50cb08e4ace1c5fb44f82be
c0c0pang <coc1789@naver.com>
2022-11-04 20:00:52 +0900

    feat: 잘못된 입력에 대한 예외처리 기능 구현

--- src/App.js
+++ src/App.js
@@ -4,37 +4,43 @@ class App {
   play() {
     const USER = new User();
     GameStart(USER);
-    if (USER.GetNum() !== 3) {
-      throw new '3자리 숫자가 아닙니다.\n애플리케이션을 종료하겠습니다.';
-    }
   };
 };
 
 class User {
   constructor() {
     this.Number;
+    this.Select_Number;
   };
-  GetNum() {
+  GetNumber() {
     return this.Number;
+  };
+  GetSelect() {
+    return this.Select_Number;
   };
 };
 const ComputerNumber = () => {
-  const NUMBER = String(MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3));
-  return NUMBER.split(",");
+  const NUMBER = [];
+  while(NUMBER.length !==3){
+    NUMBER.push(String(MissionUtils.Random.pickNumberInRange(1, 9)));
+  }
+  return NUMBER;
 };
 
 const GameStart = (USER) => {
-  let ANSWER = 1;
   MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
   let COMPUTER_NUMBER = ComputerNumber();
   let USER_NUMBER;
-  MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (number) => {
-    USER.Number = String(number).split("");
-    USER_NUMBER = USER.Number;
-  });
+  let USER_SELECT = "1";
+  while (USER_SELECT !== "2") {
+    MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (number) => {
+      USER.Number = String(number).split("");
+      USER_NUMBER = USER.Number;
+    });
+    if (USER_NUMBER.length !== 3) {
+      throw '3자리 숫자가 아닙니다.\n애플리케이션을 종료하겠습니다.';
+    }
+  }
 };
 
-const NumberCompare = (computer, user) => {
-
-}
 module.exports = App;
