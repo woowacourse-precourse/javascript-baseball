6313823ede867da962f63dd851c3299647e5a53d
Minhye Kang <alsgp9898@naver.com>
2022-11-04 13:42:20 +0900

    feat: 사용자 입력값을 받는 함수 생성

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
 const GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+const INPUT_MESSAGE = "숫자를 입력해주세요 : ";
 
 class App {
+  constructor() {
+    this.userInput = "";
+  }
+  getUserInput() {
+    MissionUtils.Console.readLine(INPUT_MESSAGE, (number) => {
+      this.userInput = number;
+      this.getUserInput();
+    });
+  }
+
   printGameStartMessage() {
     MissionUtils.Console.print(GAME_START_MESSAGE);
+    this.getUserInput();
   }
 
   play() {
