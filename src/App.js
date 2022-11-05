5e96c777fbf3a806cac4ee9e5a1da789d62b6e60
jin-Pro <dnjun2@ajou.ac.kr>
2022-11-05 21:21:21 +0900

    feat: baseball game App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
+const { Console } = require("@woowacourse/mission-utils");
+const {
+  makeAnswer,
+  inputReply,
+  inputReplay,
+  closePlay,
+  replyCheckAnswer,
+  makeReplyToReply,
+  replyValidation,
+} = require("./util.js");
+
 class App {
-  play() {}
+  play() {
+    const answer = makeAnswer();
+    const playing = (n) => {
+      if (replyValidation(n)) {
+        const replyInfo = replyCheckAnswer(n, answer);
+        const { message, done } = makeReplyToReply(replyInfo);
+        Console.print(message);
+        if (done) {
+          inputReplay((input) => {
+            if (input === "2") closePlay();
+            else this.play();
+          });
+        } else {
+          inputReply(playing);
+        }
+      }
+    };
+    inputReply(playing);
+  }
 }
 
 module.exports = App;
