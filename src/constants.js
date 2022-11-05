042d7f4458d25f449c08eec02c8de68f3bfe1e86
pakxe <pigkill40@naver.com>
2022-11-05 17:25:31 +0900

    fix: export가 안되는 오류 수정

--- src/constants.js
+++ src/constants.js
@@ -1,31 +1,29 @@
-const constant = {
-  ANSWER: {
-    LENGTH: 3,
-    MIN: 1,
-    MAX: 9,
-  },
-
-  GAME: {
-    STRIKE: '스트라이크',
-    BALL: '볼',
-    NOTHING: '낫싱',
-    RESTART: 1,
-    EXIT: 2,
-  },
-
-  GAME_SENTENCE: {
-    OPENING: '숫자 야구 게임을 시작합니다.',
-    INPUT: '숫자를 입력해주세요 : ',
-    CORRECT: `${ANSWER.LENGTH}개의 숫자를 모두 맞히셨습니다! 게임 종료`,
-    ASK_MORE: `게임을 새로 시작하려면 ${GAME.RESTART}, 종료하려면 ${GAME.EXIT}를 입력하세요.`,
-  },
-
-  ERROR: {
-    LENGTH: `${ANSWER.LENGTH}개로 입력해주세요.`,
-    NAN: '숫자만 입력해주세요.',
-    INVALID: `${ANSWER.MIN} ~ ${ANSWER.MAX}사이의 숫자만 입력해주세요.`,
-    DUPLICATION: '중복된 숫자는 입력할 수 없습니다.',
-  },
+const ANSWER = {
+  LENGTH: 3,
+  MIN: 1,
+  MAX: 9,
 };
 
-module.exports = constant;
+const GAME = {
+  STRIKE: '스트라이크',
+  BALL: '볼',
+  NOTHING: '낫싱',
+  RESTART: 1,
+  EXIT: 2,
+};
+
+const GAME_SENTENCE = {
+  OPENING: '숫자 야구 게임을 시작합니다.',
+  INPUT: '숫자를 입력해주세요 : ',
+  CORRECT: `${ANSWER.LENGTH}개의 숫자를 모두 맞히셨습니다! 게임 종료`,
+  ASK_MORE: `게임을 새로 시작하려면 ${GAME.RESTART}, 종료하려면 ${GAME.EXIT}를 입력하세요.`,
+};
+
+const ERROR = {
+  LENGTH: `${ANSWER.LENGTH}개로 입력해주세요.`,
+  NAN: '숫자만 입력해주세요.',
+  INVALID: `${ANSWER.MIN} ~ ${ANSWER.MAX}사이의 숫자만 입력해주세요.`,
+  DUPLICATION: '중복된 숫자는 입력할 수 없습니다.',
+};
+
+module.exports = { ANSWER, GAME, GAME_SENTENCE, ERROR };
