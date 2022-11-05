d99578dc8dee11833cc9fb27307324b993de55c7
김희진 <choha941108@gmail.com>
2022-11-05 17:42:51 +0900

    test(MyTestCase):각 함수의 테스트 케이스 정의
    * 기능목록에 포함된 함수에 대한 테스트 케이스 정의
    * 테스트 케이스 파일의 실행 여부 테스트
        * 실제 테스트 케이스 구현 없이, 정의 및 실행 여부만 확인

--- __tests__/MyTestCase.js
+++ __tests__/MyTestCase.js
@@ -14,5 +14,105 @@ describe("build_answer 테스트", () => {
         const set_result = [...new Set(result)]
         expect(set_result).toHaveLength(3);
     });
-
+    test("각 개체가 한자리 숫자인지 확인", () => {
+        const result = build_answer();
+        const set_result = [...new Set(result)]
+        expect(set_result).toHaveLength(3);
+    });
 })
+describe("choice_answer 테스트", () => {
+    test("3자리 배열 인지 확인", () => {
+        const result = build_answer();
+        expect(result).toHaveLength(3);
+    });
+    test("배열 객체의 타입이 숫자인지 확인", () => {
+        const result = build_answer();
+        expect(result[0]).any(Number);
+        expect(result[1]).any(Number);
+        expect(result[2]).any(Number);
+    });
+    test("입력값이 중복있을떄 예외확인", () => {
+        const result = build_answer();
+        const set_result = [...new Set(result)]
+        expect(set_result).toHaveLength(3);
+    });
+    test("각 개체가 한자리 숫자인지 확인", () => {
+        const result = build_answer();
+        const set_result = [...new Set(result)]
+        expect(set_result).toHaveLength(3);
+    });
+    test("입력 받은 숫자가 정상 출력 되는지 확인", () => {
+        const result = build_answer();
+        const set_result = [...new Set(result)]
+        expect(set_result).toHaveLength(3);
+    });
+})
+describe("count_strike 테스트", () => {
+    test("도출값 3이하 인지 확인", () => {
+        const result = count_strike([1,5,6],[8,9,3]);
+        expect(result).toBeLessThanOrEqual(3);
+    });
+    test("스트라이크가 0인 경우", () => {
+        const result = count_strike([1,5,6],[0,5,8]);
+        expect(result).toEqual(1);
+    });
+    test("스트라이크가 1인 경우", () => {
+        const result = count_strike([1,5,6],[0,5,8]);
+        expect(result).toEqual(1);
+    });
+    test("스트라이크가 2인 경우", () => {
+        const result = count_strike([1,5,6],[0,5,6]);
+        expect(result).toEqual(2);
+    });
+    test("스트라이크가 3인 경우", () => {
+        const result = count_strike([1,5,6],[1,5,6]);
+        expect(result).toEqual(3);
+    });
+    test("스트라이크가 0이면서 ball이 있는 경우", () => {
+        const result = count_strike([1,5,6],[0,5,8]);
+        expect(result).toEqual(1);
+    });
+    test("스트라이크가 1이면서 ball이 있는 경우", () => {
+        const result = count_strike([1,5,6],[0,5,8]);
+        expect(result).toEqual(1);
+    });
+    test("스트라이크가 2이면서 ball이 있는 경우", () => {
+        const result = count_strike([1,5,6],[0,5,8]);
+        expect(result).toEqual(1);
+    });
+})
+describe("count_ball 테스트", () => {
+    test("도출값 3이하 인지 확인", () => {
+        const result = count_strike([1,5,6],[8,9,3]);
+        expect(result).toBeLessThanOrEqual(3);
+    });
+    test("볼이 0인 경우", () => {
+        const result = count_strike([1,5,6],[0,5,8]);
+        expect(result).toEqual(1);
+    });
+    test("볼이 1인 경우", () => {
+        const result = count_strike([1,5,6],[0,5,8]);
+        expect(result).toEqual(1);
+    });
+    test("볼이 2인 경우", () => {
+        const result = count_strike([1,5,6],[0,5,6]);
+        expect(result).toEqual(2);
+    });
+    test("볼이 3인 경우", () => {
+        const result = count_strike([1,5,6],[1,5,6]);
+        expect(result).toEqual(3);
+    });
+    test("볼이 0이면서 strike이 있는 경우", () => {
+        const result = count_strike([1,5,6],[0,5,8]);
+        expect(result).toEqual(1);
+    });
+    test("볼이 1이면서 strike이 있는 경우", () => {
+        const result = count_strike([1,5,6],[0,5,8]);
+        expect(result).toEqual(1);
+    });
+    test("볼이 2이면서 strike이 있는 경우", () => {
+        const result = count_strike([1,5,6],[0,5,8]);
+        expect(result).toEqual(1);
+    });
+})
+//print hint는 제공된 테스트 케이스 신뢰
