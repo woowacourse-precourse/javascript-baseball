894339430590e67a1a87cc17c9ba7c48faab7be3
murpin <qw04011@gmail.com>
2022-11-05 21:19:07 +0900

    feat(App/setRandomNumber): 랜덤한 3자리 수 반환하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -10,6 +10,15 @@ class App {
     Console.print("숫자 야구 게임을 시작합니다.");
   }
 
-}
-
+  setRandomNumber() {
+    const computer = [];
+    while (computer.length < 3) {
+      const number = Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+          computer.push(number);
+        }
+    }
+    this.randomNumber = computer;
+  } 
+  
 module.exports = App;
