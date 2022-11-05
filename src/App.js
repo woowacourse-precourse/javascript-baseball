0dfc8d956843f093ff84d1864d19c553f6ab0c84
chee9835 <huisu121@gmail.com>
2022-11-05 18:08:08 +0900

    feat: askNumInput 숫자 입력받는 함수

--- src/App.js
+++ src/App.js
@@ -4,9 +4,20 @@ function printMsg(message) {
   MissionUtils.Console.print(message);
 }
 
+function askNumInput() {
+  MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (number) => {
+    printMsg(`입력하신 숫자는... ${number} 입니다.`);
+  });
+}
+
 class App {
   play() {
     printMsg("숫자 야구 게임을 시작합니다.");
+    try {
+      askNumInput();
+    } catch (e) {
+      printMsg(e);
+    }
   }
 }
 
