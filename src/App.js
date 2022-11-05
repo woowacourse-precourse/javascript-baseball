3c3bbc5ecd9cf328b7eee3fa9be369a3206dc6ca
DongWooKim97 <nanaukim@naver.com>
2022-11-05 13:54:26 +0900

    feat: 유효성 검사를 통과한 입력값만이 게임 진행하는 코드 작성

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ class App {
     this.missionRandom = require("@woowacourse/mission-utils").Random;
     this.missionConsole = require("@woowacourse/mission-utils").Console;
     this.score = require("./play/GetScore");
+    this.user = require("./input/userInputValid");
   }
+
   play() {
     return this.getUserInput();
   }
@@ -23,9 +25,11 @@ class App {
     const computerInputNumbers = this.makeRandomNumbers();
 
     this.missionConsole.readLine("숫자를 입력해주세요 : ", (userInputNumbers) => {
-      console.log(computerInputNumbers, userInputNumbers);
-      const scoreCount = new this.score(computerInputNumbers, userInputNumbers);
-      this.missionConsole.print(scoreCount.compare());
+      if (new this.user(userInputNumbers).checkValid()) {
+        console.log(computerInputNumbers, userInputNumbers);
+        const scoreCount = new this.score(computerInputNumbers, userInputNumbers);
+        this.missionConsole.print(scoreCount.compare());
+      }
       this.missionConsole.close();
     });
   }
