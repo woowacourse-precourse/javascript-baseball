e653b54f0f3aad168e44e327467330748225f696
BullWooStar <ccatal66@naver.com>
2022-11-06 00:47:39 +0900

    feat: 게임종료 기능 추가

--- src/App.js
+++ src/App.js
@@ -15,15 +15,35 @@ function putNumber() {
   });
 }
 
-let STRIKE_NUMBER = [];
-let BALL_NUMBER = [];
-
 function comparingNumber() {
   if (JSON.stringify(ANSWER_NUMBER) === JSON.stringify(CORRECT_NUMBER)) {
     MissionUtils.Console.print("3스트라이크");
     MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-    return;
+    gameWin();
+  } else {
+    notWin();
   }
+}
+
+function gameWin() {
+  MissionUtils.Console.readLine(
+    "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
+    (answer) => {
+      if (answer === "1") {
+        makeCorrectNumber();
+        console.log(CORRECT_NUMBER);
+        putNumber();
+      } else {
+        return;
+      }
+    }
+  );
+}
+
+function notWin() {
+  let STRIKE_NUMBER = [];
+  let BALL_NUMBER = [];
+
   ANSWER_NUMBER.map((el, index) => {
     if (ANSWER_NUMBER[index] === CORRECT_NUMBER[index]) {
       STRIKE_NUMBER.push(el);
@@ -33,6 +53,7 @@ function comparingNumber() {
       BALL_NUMBER.push(el);
     }
   });
+
   if (STRIKE_NUMBER.length === 0 && BALL_NUMBER.length === 0) {
     MissionUtils.Console.print("낫싱");
   } else if (STRIKE_NUMBER.length !== 0 && BALL_NUMBER.length === 0) {
