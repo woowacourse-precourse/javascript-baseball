91cdf604a714d38be8047bce22da55115905f93f
jiyujin <yujin1679@naver.com>
2022-11-05 20:07:08 +0900

    feat: 숫자를 입력받는 userInput 함수 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  async play() {
+    let userNumber = await this.userInput('숫자입력: ');
+    console.log(userNumber);
+  }
+  async userInput(prompt) {
+    return await new Promise((resolve) => {
+      MissionUtils.Console.readLine(prompt, resolve);
+    });
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
