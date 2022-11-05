be4133497cff10c94dbfd505f2a231d7274e6255
DESKTOP-PDMTIQO\hansu <fathlic5@gmail.com>
2022-11-06 00:51:36 +0900

    :sparkles: feat: 기능2 사용자의 입력값 검증 구현 우아한테크코스 라이브러리 API를 이용해, 사용자의 입력값을 받도록 구현, 입력값을 검증하여 잘못된 값 입력 시 throw문으로 예외 발생 구현

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ class App {
     play() {
         const MISSION_UTILS = require("@woowacourse/mission-utils");
         MISSION_UTILS.Console.print('숫자 야구 게임을 시작합니다.');
+        this.isUserNumbers(this.isComputerNumbers());
     }
 
     isComputerNumbers() {
@@ -11,7 +12,18 @@ class App {
             const RANDOM_NUMBER = MISSION_UTILS.Random.pickNumberInRange(1, 9);
             if (!COMPUTER_NUMBERS.includes(RANDOM_NUMBER)) COMPUTER_NUMBERS.push(RANDOM_NUMBER);
         }
-        return parseInt(COMPUTER_NUMBERS.join(''))
+        return parseInt(COMPUTER_NUMBERS.join(''));
+    }
+
+    isUserNumbers(computerNumbers) {
+        const MISSION_UTILS = require("@woowacourse/mission-utils");
+        return MISSION_UTILS.Console.readLine('숫자를 입력해주세요 : ',(userNumbers) => {
+            if(isNaN(userNumbers)) throw new Error('숫자를 입력해주세요.');
+            if(userNumbers.toString().length !== 3) throw new Error('3자리 숫자를 입력해주세요.');
+            if(new Set(userNumbers).size !== 3) throw new Error('중복되지 않는 숫자를 입력해주세요.');
+            if(String(userNumbers).indexOf(0) !== -1) throw new Error('1부터 9의 숫자만 입력해주세요.');
+            parseInt(userNumbers);
+        });
     }
 }
 module.exports = App;
