0cc95c8777846b8917f5b45515ad03e2f753a2e3
yhnb3 <60080270+yhnb3@users.noreply.github.com>
2022-11-05 19:47:21 +0900

    Feat: make code to pass the testcode

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
 const { game, statusValidation } = require("./libs");
 const { Console } = require("@woowacourse/mission-utils");
+const makeBallCountString = require("./libs/makeBallCountString");
+const getRandomThreeNumber = require("./libs/getRandomThreeNumber");
 
 class App {
-  async play() {
-    console.log("숫자 야구 게임을 시작합니다.");
-    let isOver = false;
-    while (!isOver) {
-      for await (const { status, num } of game()) {
-        isOver = !statusValidation({ status, num });
+  constructor() {
+    this.iterator = this.iterator.bind(this);
+    this.play = this.play.bind(this);
+    this.computer = getRandomThreeNumber();
+  }
+  askRestart() {
+    Console.readLine("다시 시작 1 종료 2 :  ", (res) => {
+      if (res === "2") return Console.close();
+      this.computer = getRandomThreeNumber();
+      this.iterator();
+    });
+  }
+  iterator() {
+    Console.readLine("되나?   ", (res) => {
+      if (res.length > 3) throw new Error();
+      const ballCount = makeBallCountString(this.computer, res);
+      Console.print(ballCount);
+      if (ballCount == "3스트라이크") {
+        Console.print("게임 종료");
+        this.askRestart();
       }
-    }
-    Console.close();
+      this.iterator();
+    });
+  }
+  play() {
+    this.iterator();
   }
 }
+
 const app = new App();
 app.play();
 
