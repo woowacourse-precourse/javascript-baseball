e58edc9ff03687a0fa23e82a42796ee74fe476b5
BuildTools <unconfigured@null.spigotmc.org>
2022-11-06 05:22:46 +0900

    게임 시작 메세지 출력

--- src/App.js
+++ src/App.js
@@ -3,34 +3,6 @@ const MissionUtils = require('@woowacourse/mission-utils');
 class App {
   play() {
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
-    const answer = this.makeComputerAnswer();
-    this.playBaseball(answer);
-  }
-
-  makeComputerAnswer() {
-    const computer = [];
-    while (computer.length < 3) {
-      const number = MissionUtils.Random.pickNumberInRange(1, 9);
-      if (!computer.includes(number)) {
-        computer.push(number);
-      }
-    }
-    return computer;
-  }
-
-  playBaseball(answer) {
-    let result = [];
-    MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (number) => {
-      for (let i = 0; i < 3; i++) {
-        result.push(number[i]);
-      }
-      console.log(result);
-    });
-  }
-
-  checkBall(message, answer) {
-    const result = message.filter((message) => answer.inclueds(answer));
-    return result;
   }
 }
 
@@ -38,3 +10,5 @@ module.exports = App;
 
 const app = new App();
 app.play();
+
+// 콜백함수는 다른 함수에 입력값으로 넣어야 한다.
