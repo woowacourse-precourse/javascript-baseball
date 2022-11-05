37c904fd1f0e453f864ac17f97eb1f7d2e2b6cd2
Jaehyeon <jaehyeonp74@gmail.com>
2022-11-06 03:14:47 +0900

    feat: try catch 문을 이용해 세자리 수 입력에 대한 예외처리

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 const generateNumber = require('./generateNumber');
+const validateInputValue = require('./validator');
 
 class App {
   play() {
     this.start();
     this.computerNumArr = generateNumber();
+    this.getInputAndCompare();
   }
 
   start() {
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
   }
+
+  getInputAndCompare() {
+    MissionUtils.Console.readLine('숫자를 입력해주세요 : ', input => {
+      try {
+        const { computerNumArr } = this;
+        const isInputValidate = validateInputValue(input);
+      } catch (error) {
+        this.wrongInput(error);
+      }
+    });
+  }
+
+  wrongInput(err) {}
 
   close() {
     MissionUtils.Console.close();
