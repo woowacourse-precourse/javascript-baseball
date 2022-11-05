20a89c1bcfa29ab02d741494284d4f5aa7d4c3d0
deli-ght <minjju0660@gmail.com>
2022-11-06 01:14:32 +0900

    feat(App.js): choose random answer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 class App {
-  constructor() {}
+  answer;
+  constructor() {
+    this.answer = [];
+  }
 
   play() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    this.getRandomNum();
+
+    console.log(this.answer);
+  }
+
+  getRandomNum() {
+    while (this.answer.length < 3) {
+      const NUMBER = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!this.answer.includes(NUMBER)) {
+        this.answer.push(NUMBER);
+      }
+    }
   }
 }
 
