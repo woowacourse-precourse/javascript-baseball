681eda5b3043b286b1d850d14d315465d67bc562
cham-min <hymn9613@gmail.com>
2022-11-05 23:43:57 +0900

    feat: 숫자야구 게임 실행시 사용자에게 보여질 문구 출력.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+const { Console, Random } = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  play() {
+    this.start();
+  }
+
+  start() {
+    Console.print("숫자 야구 게임을 시작합니다.");
+  }
 }
 
 module.exports = App;
