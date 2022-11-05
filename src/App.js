f9f8b253da2f81edca1900accf3d8f16ed5931b2
wonkyung <mwk0725@gmail.com>
2022-11-05 16:26:39 +0900

    feat: 게임 처음 시작 시 나오는 문구 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+
 class App {
-  play() {}
+  constructor() {
+    this.playFirstTime = true;
+  }
+  play() {
+    if (this.playFirstTime) {
+      MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+    }
+  }
 }
 
 module.exports = App;
