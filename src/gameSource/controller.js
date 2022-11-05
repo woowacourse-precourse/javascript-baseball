964a8f75373b8f939f565f1b129d732eee45df70
An Sol <apdlvmf486@gmail.com>
2022-11-05 13:59:42 +0900

    style: 들여쓰기 스타일 통일
    space 4칸 -> space 2칸으로 변경

--- src/gameSource/controller.js
+++ src/gameSource/controller.js
@@ -4,44 +4,43 @@ const GAMELOGICS = require('./gameLogic');
 const VALIDATIONCHECK = require('./inputCheck');
 
 const MAKEANSWER = function makeAnswerWithThreeUniqueNumbers() {
-    let threeUniqueNumbers = []
-    while (threeUniqueNumbers.length < 3) {
-        const RANDOM_NUMBER = MissionUtils.Random.pickNumberInRange(1, 9);
-        threeUniqueNumbers = CHECK_DUPLICATE(threeUniqueNumbers, RANDOM_NUMBER);
-    }
-    return threeUniqueNumbers[0] * 100 + threeUniqueNumbers[1] * 10 + threeUniqueNumbers[2] * 1;
+	let threeUniqueNumbers = []
+	while (threeUniqueNumbers.length < 3) {
+		const RANDOM_NUMBER = MissionUtils.Random.pickNumberInRange(1, 9);
+		threeUniqueNumbers = CHECK_DUPLICATE(threeUniqueNumbers, RANDOM_NUMBER);
+	}
+	return threeUniqueNumbers[0] * 100 + threeUniqueNumbers[1] * 10 + threeUniqueNumbers[2] * 1;
 }
 
 const CHECK_DUPLICATE = function checkRepeatedElementOfArray(array, target) {
-    const ARRAY_COPIED = [...array];
-    if (!array.includes(target)) ARRAY_COPIED.push(target)
-    return ARRAY_COPIED;
+	const ARRAY_COPIED = [...array];
+	if (!array.includes(target)) ARRAY_COPIED.push(target)
+	return ARRAY_COPIED;
 }
 
 class GameStartOrRestart {
-    launchNewGame() {
-        const ANSWER = MAKEANSWER();
-        console.log(ANSWER);
-        this.gamePlayApplication(ANSWER);
-    }
+	launchNewGame() {
+		const ANSWER = MAKEANSWER();
+		this.gamePlayApplication(ANSWER);
+	}
 
-    gamePlayApplication(answer) {
-        MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (userInput) => {
-            GAMELOGICS.getHintFromInput(userInput, answer);
-        });
-    }
+	gamePlayApplication(answer) {
+		MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (userInput) => {
+			GAMELOGICS.getHintFromInput(userInput, answer);
+		});
+	}
 
-    restartGame(userInput) {
-        if (!VALIDATIONCHECK.checkUserInputAfterGameOver(userInput)) throw new Error("잘못된 숫자를 입력하였습니다.");
-        if (+userInput === 1) this.launchNewGame();
-        if (+userInput === 2) MissionUtils.Console.close();
-    }
+	restartGame(userInput) {
+		if (!VALIDATIONCHECK.checkUserInputAfterGameOver(userInput)) throw new Error("잘못된 숫자를 입력하였습니다.");
+		if (+userInput === 1) this.launchNewGame();
+		if (+userInput === 2) MissionUtils.Console.close();
+	}
 
-    askQuestionToUserWhenGameEnds() {
-        MissionUtils.Console.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n', (userInput) => {
-            this.restartGame(userInput);
-        });
-    }
+	askQuestionToUserWhenGameEnds() {
+		MissionUtils.Console.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n', (userInput) => {
+			this.restartGame(userInput);
+		});
+	}
 }
 const GAMESTART_OR_RESTART = new GameStartOrRestart();
 module.exports = GAMESTART_OR_RESTART;
--- src/gameSource/inputCheck.js
+++ src/gameSource/inputCheck.js
@@ -1,20 +1,20 @@
 //사용자 인풋 체크
 class ValidationCheck {
 
-    checkUserInputDuringGamePlay(userInput) {
-        if (userInput.length !== 3) return false;
-        if (isNaN(userInput)) return false;
-        if (userInput[0] === userInput[1]
-            || userInput[1] === userInput[2]
-            || userInput[2] === userInput[0]) return false;
-        if (!(userInput.match(/[123456789]{3}/))) return false;
-        return true;
-    }
+  checkUserInputDuringGamePlay(userInput) {
+    if (userInput.length !== 3) return false;
+    if (isNaN(userInput)) return false;
+    if (userInput[0] === userInput[1]
+      || userInput[1] === userInput[2]
+      || userInput[2] === userInput[0]) return false;
+    if (!(userInput.match(/[123456789]{3}/))) return false;
+    return true;
+  }
 
-    checkUserInputAfterGameOver(userInput) {
-        if (+userInput === 1 || +userInput === 2) return true;
-        return false;
-    }
+  checkUserInputAfterGameOver(userInput) {
+    if (+userInput === 1 || +userInput === 2) return true;
+    return false;
+  }
 
 }
 const VALIDATIONCHECK = new ValidationCheck();
