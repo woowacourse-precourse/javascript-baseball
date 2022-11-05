962bae710cb9afb33da3d789ceac618fb62fcf58
jeonbyeongmin <qudals7613@gmail.com>
2022-11-05 11:45:18 +0900

    feat: 사용자 입력을 받아 숫자 배열로 변환하는 메서드 작성

--- src/App.js
+++ src/App.js
@@ -5,10 +5,32 @@ const MissionUtils = require("@woowacourse/mission-utils");
 class App {
   constructor() {
     this.computer = [];
+    this.user = [];
   }
 
   play() {
     this.setRandomNumbers();
+    this.print("숫자 야구 게임을 시작합니다.");
+    this.readLine("숫자를 입력해주세요 : ", (answer) =>
+      this.setUserNumbers(answer)
+    );
+  }
+
+  /**
+   *
+   * @param {string} str
+   */
+  print(str) {
+    MissionUtils.Console.print(str);
+  }
+
+  /**
+   *
+   * @param {string} query
+   * @param {function} callback
+   */
+  readLine(query, callback) {
+    MissionUtils.Console.readLine(query, callback);
   }
 
   setRandomNumbers() {
@@ -16,6 +38,24 @@ class App {
       const number = this.getRandomNumber();
       this.pushNumberToComputer(number);
     }
+  }
+
+  /**
+   *
+   * @param {string} str
+   */
+  setUserNumbers(str) {
+    const numbers = this.separateNumbers(str);
+    this.user = [...numbers];
+  }
+
+  /**
+   *
+   * @param {string} str
+   */
+  separateNumbers(str) {
+    const numbers = [...str];
+    return numbers.map((number) => Number(number));
   }
 
   /**
