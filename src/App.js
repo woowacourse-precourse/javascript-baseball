2b9833bb1cf8ebba535a8e863243602c2b34caa7
insu12021202 <insu12021202@gmail.com>
2022-11-05 18:45:41 +0900

    feat:all passed test case

--- src/App.js
+++ src/App.js
@@ -4,37 +4,37 @@ class App {
   play() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
     startGame();
+    askRetry();
   }
 }
 
 const startGame = () => {
   let answers = init();
   let play = true;
-  checkResult(input, answers);
+
   while (play) {
     MissionUtils.Console.readLine("숫자를 입력하세요.", (input) => {
       if (checkInput(input)) {
         //인풋과 정답 비교하기
-        if (checkResult(answers, input)) {
-          play = false;
-        }
+        play = checkResult(input, answers);
+        console.log("play: ", play);
+      } else {
+        throw "입력되는 숫자는 서로 다른 3자리이어야 합니다.";
       }
     });
   }
-  //게임 종료
-  askRetry();
 };
 
 const askRetry = () => {
-  Console.readLine(
+  MissionUtils.Console.readLine(
     "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
     (answer) => {
-      if (answer === 1) {
+      if (answer === "1") {
         startGame();
-      } else if (answer === 2) {
+      } else if (answer === "2") {
         return;
       } else {
-        throw "입력은 1과 2로만 가능합니다.";
+        throw "게임을 시작하거나 종료하려면 1 또는 2를 입력해야 합니다.";
       }
     }
   );
@@ -55,8 +55,6 @@ const checkInput = (input) => {
   let str = String(input);
   if (str.length !== 3) {
     return false;
-  } else if (isNaN(input)) {
-    return false;
   }
   for (let i = 0; i < str.length; i++) {
     if (i !== str.indexOf(str[i])) {
@@ -67,7 +65,6 @@ const checkInput = (input) => {
 };
 
 const checkResult = (input, answer) => {
-  console.log(`답은 ${answer} 입력은 ${input}`);
   let strike = 0;
   let ball = 0;
   answer.map((item, index) => {
@@ -80,23 +77,22 @@ const checkResult = (input, answer) => {
   printResult(ball, strike);
   if (strike === 3) {
     MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-    return true;
-  } else {
     return false;
+  } else {
+    return true;
   }
 };
 
 const printResult = (ball, strike) => {
-  if (ball === 0) {
+  if (ball === 0 && strike !== 0) {
     MissionUtils.Console.print(`${strike}스트라이크`);
-  } else if (strike === 0) {
+  } else if (strike === 0 && ball !== 0) {
     MissionUtils.Console.print(`${ball}볼`);
+  } else if (ball === 0 && strike === 0) {
+    MissionUtils.Console.print("낫싱");
   } else {
     MissionUtils.Console.print(`${ball}볼 ${strike}스트라이크`);
   }
 };
-
-const app = new App();
-app.play();
 
 module.exports = App;
