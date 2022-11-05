a1323182a9abdd67e0f7a5f1466d033d41721018
JayPark0110 <90092440+JayPark0110@users.noreply.github.com>
2022-11-05 23:06:57 +0900

    refactor: 예외 처리 문구 변수화하여 재사용 가능하도록 변경

--- src/components/error/CheckError.js
+++ src/components/error/CheckError.js
@@ -1,16 +1,7 @@
-const errorMessage =
+const guideMessage =
   "\n1 ~ 9로 구성되고, 3자리가 모두 다른 숫자 3개를 입력해주세요.\n게임 종료";
-
-const CheckError = (answer) => {
-  let splitAnswer = answer.split("");
-
-  hasZero(answer);
-  if (checkDuplicated(splitAnswer)) {
-    throw "숫자끼리 중복되어서는 안됩니다.\n세 자리 모두 다른 수를 입력해주세요.";
-  }
-  containNotANumber(splitAnswer, answer);
-  notRightLength(answer.length);
-};
+const inputMore = "글자 더 입력하셨습니다.";
+const inputLess = "글자 덜 입력하셨습니다.";
 
 const checkDuplicated = (splitAnswer) => {
   return splitAnswer[0] == splitAnswer[1] ||
@@ -20,22 +11,33 @@ const checkDuplicated = (splitAnswer) => {
     : false;
 };
 
+const CheckError = (answer) => {
+  let splitAnswer = answer.split("");
+
+  hasZero(answer);
+  if (checkDuplicated(splitAnswer) && splitAnswer.length > 1) {
+    throw "숫자끼리 중복되어서는 안됩니다.\n세 자리 모두 다른 수를 입력해주세요.";
+  }
+  containNotANumber(splitAnswer, answer);
+  notRightLength(answer.length);
+};
+
 const hasZero = (answer) => {
   let isZero = false;
   const zeroRegex = /[0]/;
   zeroRegex.test(answer) ? (isZero = true) : (isZero = false);
   if (answer.length === 3 && isZero) {
-    throw `숫자 0이 포함되었습니다.${errorMessage}`;
+    throw `숫자 0이 포함되었습니다.${guideMessage}`;
   }
   if (answer.length > 3 && isZero) {
     throw `숫자 0이 포함되었으며, ${
-      answer.length - 3
-    }글자 초과되었습니다.${errorMessage}`;
+      answer.length - 3 + inputMore + guideMessage
+    }`;
   }
   if (answer.length < 3 && isZero) {
     throw `숫자 0이 포함되었으며, ${
-      3 - answer.length
-    }글자 미달되었습니다.${errorMessage}`;
+      3 - answer.length + inputLess + guideMessage
+    }`;
   }
 };
 
@@ -53,32 +55,30 @@ const makeNotANumberList = (splitAnswer) => {
 
 const containNotANumber = (splitAnswer, answer) => {
   let notANumberList = makeNotANumberList(splitAnswer);
-  console.log(notANumberList);
   let hasNotANumber = notANumberList.length !== 0;
+  let isNotANumber = `${notANumberList.join(",")}번째 문자`;
 
   if (hasNotANumber && answer.length === 3) {
-    throw `${notANumberList.join(
-      ","
-    )}번째 문자는 숫자가 아닙니다.${errorMessage}`;
+    throw `${isNotANumber}는 숫자가 아닙니다.${guideMessage}`;
   }
   if (hasNotANumber && answer.length > 3) {
-    throw `${notANumberList.join(",")}번째 문자는 숫자가 아니며, ${
-      answer.length - 3
-    }개의 문자를 더 입력하셨습니다.${errorMessage}`;
+    throw `${isNotANumber}는 숫자가 아니며, ${
+      answer.length - 3 + inputMore + guideMessage
+    }`;
   }
   if (hasNotANumber && answer.length < 3) {
-    throw `${notANumberList.join(",")}번째 문자는 숫자가 아니며, ${
-      3 - answer.length
-    }개의 문자를 덜 입력하셨습니다.${errorMessage}`;
+    throw `${isNotANumber}는 숫자가 아니며, ${
+      3 - answer.length + inputLess + guideMessage
+    }`;
   }
 };
 
 const notRightLength = (length) => {
   if (length > 3) {
-    throw `${length - 3}개의 숫자를 더 입력하셨습니다.${errorMessage}`;
+    throw `${length - 3 + inputMore + guideMessage}`;
   }
   if (length < 3) {
-    throw `${3 - length}개의 숫자를 덜 입력하셨습니다.${errorMessage}`;
+    throw `${3 - length + inputLess + guideMessage}`;
   }
 };
 
