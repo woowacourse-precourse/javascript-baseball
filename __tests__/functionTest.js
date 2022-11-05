243c14b0703b8462df221fb79312f69c6034deb0
freemoon99 <soloist132@gmail.com>
2022-11-05 18:56:27 +0900

    feat: 유효성 검사 예외 테스트
    유효성 검사 테스트 케이스

--- __tests__/functionTest.js
+++ __tests__/functionTest.js
@@ -1,10 +1,69 @@
 const App = require("../src/App");
+const MissionUtils = require("@woowacourse/mission-utils");
 
-describe('상대방(컴퓨터)의 랜덤 숫자 테스트', () => {
+const mockQuestions = (answers) => {
+  MissionUtils.Console.readLine = jest.fn();
+  answers.reduce((acc, input) => {
+    return acc.mockImplementationOnce((question, callback) => {
+      callback(input);
+    });
+  }, MissionUtils.Console.readLine);
+};
+
+describe('함수 기능 테스트', () => {
   test('숫자가 세자리가 맞는지 확인', () => {
     const app = new App();
     const result = app.computerInput().length;
 
     expect(result).toEqual(3);
+  });
+  test("입력되지 않았을 때", () => {
+    const answers = [""];
+
+    mockQuestions(answers);
+
+    expect(() => {
+      app.play();
+    }).toThrow();
+  });
+
+  test("숫자가 아닐 때", () => {
+    const answers = ["안녕", "테스트", "코드"];
+
+    mockQuestions(answers);
+
+    expect(() => {
+      app.play();
+    }).toThrow();
+  });
+
+  test("중복된 숫자 있을 때", () => {
+    const answers = ["111"];
+
+    mockQuestions(answers);
+
+    expect(() => {
+      app.play();
+    }).toThrow();
+  });
+
+  test("입력된 수가 3자리가 아닐 때", () => {
+    const answers = ["1", "12", "1234"];
+
+    mockQuestions(answers);
+
+    expect(() => {
+      app.play();
+    }).toThrow();
+  });
+
+  test("1~9사이의 숫자가 아닐때", () => {
+    const answers = ["000", "012"];
+
+    mockQuestions(answers);
+
+    expect(() => {
+      app.play();
+    }).toThrow();
   });
 });
