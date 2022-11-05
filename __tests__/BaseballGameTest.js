6d912ed18f49e3775381737f52dc8e4e21cc0f6f
Deemou <4106243@naver.com>
2022-11-05 20:31:41 +0900

    test: isEqual() 테스트 케이스 추가

--- __tests__/BaseballGameTest.js
+++ __tests__/BaseballGameTest.js
@@ -21,6 +21,22 @@ describe("숫자 야구 게임", () => {
       expect(num.toString()).toMatch(/[1-9]/);
       set.add(num);
     });
+
     expect(set.size).toBe(DIGITS);
+  });
+  test("두 인자가 같은지 확인", () => {
+    const char1 = "1",
+      char2 = "1",
+      num1 = 1,
+      num2 = 2;
+
+    const app = new App();
+    const result1 = app.isEqual(char1, char2),
+      result2 = app.isEqual(char1, num1),
+      result3 = app.isEqual(num1, num2);
+
+    expect(result1).toBe(true);
+    expect(result2).toBe(false);
+    expect(result3).toBe(false);
   });
 });
