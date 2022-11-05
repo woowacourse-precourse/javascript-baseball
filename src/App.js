fd15d050e54696acd6b010a1392adace6debf9e1
kyeongwxx <kyeongwxx@gmail.com>
2022-11-05 16:40:46 +0900

    feat(gameStart()): 게임 시작 문구를 출력하는 함수 구현
    게임 시작 시 "숫자 야구 게임을 시작합니다." 문자열 출력
    
    출력 값은 [MissionUtils 라이브러리]에서 제공하는 Console.print() 메서드를 사용

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
+const gameStart = () => {
+  MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+};
+
 class App {
-  play() {}
+  play() {
+    gameStart();
+  }
 }
 
 module.exports = App;
+
+const app = new App();
+app.play();
