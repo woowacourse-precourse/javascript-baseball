e7a90b2348c581ffb9c61549a7b1daf1eb68396f
DongWooKim97 <nanaukim@naver.com>
2022-11-05 15:35:55 +0900

    style: 파일 위치 변경에 따른 주소명 변경

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 class App {
   constructor() {
     this.missionConsole = require("@woowacourse/mission-utils").Console.print("숫자 야구 게임을 시작합니다");
-    this.playGame = require("./PlayGame");
-    this.pcNumber = require("./GetComputerInput");
+    this.playGame = require("./play/PlayGame");
+    this.pcNumber = require("./input/GetComputerInput");
   }
   play() {
     const pcInputNumber = new this.pcNumber().makeRandomNumbers();
