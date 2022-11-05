a7e77c8ced22a28c9dbcc412c68a5f0333f08544
han0224 <dbn0224@gmail.com>
2022-11-05 18:23:25 +0900

    Feat: 랜덤 숫자 추출 기능 추가
    getRandomBaseballNumber 함수 추가하여 중복되지 않는 3개의 숫자를 추출해 리턴하는 함수

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  constructor() {}
+  getRandomBaseballNumber() {
+    const computer = new Set();
+    while (computer.size < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      computer.add(number);
+    }
+    return Number(Array.from(computer).join(""));
+  }
+  play() {
+    this.getRandomBaseballNumber();
+  }
 }
 
 module.exports = App;
