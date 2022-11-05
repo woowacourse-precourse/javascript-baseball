18551a25c4a899d232a38c1d19e6335fbee80032
murpin <qw04011@gmail.com>
2022-11-05 21:27:43 +0900

    feat(App/isValidateInputNumber): 숫자 입력 시 3자리 숫자가 아닌 경우 예외처리 기능 추가

--- src/App.js
+++ src/App.js
@@ -81,4 +81,11 @@ class App {
       }
     })
   }
+
+  isValidateInputNumber(number) {
+    if (number.length !== 3){
+      throw new Error('3자리 수를 입력해야합니다');
+    }
+  }
+
 module.exports = App;
