a456ee0f17d3cb9dd9e918878b5fc034f2c9f147
freemoon99 <soloist132@gmail.com>
2022-11-05 17:32:52 +0900

    feat: 컴퓨터의 랜덤 숫자 출력
    컴퓨터가 랜덤으로 임의의 수 3개를 선택한다

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  computerInput(){
+    const computer = [];
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) computer.push(number);
+    }
+    return computer
+  }
+
+  play() {
+
+  }
 }
 
 module.exports = App;
