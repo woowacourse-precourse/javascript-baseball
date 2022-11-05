5e635457bea55b0adc0cbf55a86f806a68e46ff9
kyeongwxx <kyeongwxx@gmail.com>
2022-11-05 17:03:26 +0900

    feat(pickRandomNumber()): 서로 다른 임의의 수를 선택하는 함수 구현
    컴퓨터는 1에서 9까지 서로 다른 임의의 수 3개를 선택
    
    임의의 수는 [MissionUtils 라이브러리]에서 제공하는 Random.pickUniquekNumberInRange() 메서드를 사용

--- src/App.js
+++ src/App.js
@@ -4,9 +4,15 @@ const gameStart = () => {
   MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
 };
 
+const pickRandomNumber = () => {
+  return MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3).join("");
+};
+
 class App {
   play() {
     gameStart();
+
+    let answer = pickRandomNumber();
   }
 }
 
