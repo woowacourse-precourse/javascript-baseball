042e7c018811413c1577178badaded613fe115bd
bj lee <maplestory1419@naver.com>
2022-11-05 17:47:12 +0900

    2번 기능구현

--- src/App.js
+++ src/App.js
@@ -3,14 +3,23 @@ const MissionUtils = require("@woowacourse/mission-utils");
 class App {
   constructor() {
     this.Computer_Number=[]
+    this.Input_Number;
   }
   GenerageNumber(){
     for(let i=0;i<3;i++){
       this.Computer_Number.push(MissionUtils.Random.pickNumberInRange(0, 9))
     }
+  } 
+  Input(){
+    //let result = str.split(' ').join('');
+    MissionUtils.Console.readLine('번호를 입력해주세요.', (answer) => {
+      this.Input_Number=answer.split(' ').join('').split('');
+    });
   }
+
   play() {
     this.GenerageNumber()
+    this.Input()
   }
 
 }
