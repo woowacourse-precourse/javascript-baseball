82c51a65c826a6575f9a1ea588438303ecda6bc2
pakxe <pigkill40@naver.com>
2022-11-05 19:42:29 +0900

    test: 중복 검사 테스트 코드 작성

--- __tests__/ValidationTest.js
+++ __tests__/ValidationTest.js
@@ -1,4 +1,4 @@
-const { validLength, validRange } = require('../src/Validation');
+const { validLength, validRange, duplication } = require('../src/Validation');
 
 describe('길이 검사', () => {
   test('case1', () => {
@@ -27,5 +27,17 @@ describe('범위 검사', () => {
   });
   test('case4', () => {
     expect(validRange([0, 0, 0])).toEqual(false);
+  });
+});
+
+describe('중복 검사', () => {
+  test('case1', () => {
+    expect(duplication([1, 2, 3])).toEqual(false);
+  });
+  test('case2', () => {
+    expect(duplication([1, 2, 2])).toEqual(true);
+  });
+  test('case3', () => {
+    expect(duplication([1, 1, 1])).toEqual(true);
   });
 });
