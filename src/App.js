96f84284bf4808de90425ccfb8320547cae1c4f3
chee9835 <huisu121@gmail.com>
2022-11-05 18:05:14 +0900

    feat: printMsg 메세지 출력 함수

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
+function printMsg(message) {
+  MissionUtils.Console.print(message);
+}
+
 class App {
-  play() {}
+  play() {
+    printMsg("숫자 야구 게임을 시작합니다.");
+  }
 }
 
 module.exports = App;
