9142a478b49e177ea96eb8d913a7c2a4453788a6
froggy1014 <clcl6084@gmail.com>
2022-11-06 04:09:15 +0900

    test: 입력값의 공백에 대한 테스트 케이스 추가

--- __tests__/ValidationTest.js
+++ __tests__/ValidationTest.js
@@ -3,26 +3,38 @@ const { Console } = require('@woowacourse/mission-utils');
 
 const inputValidationTest = (palyerInput) => {
   Console.readLine = jest.fn((question, callback) => callback(palyerInput))
-  
-  const app = new App();
-  app.play();
 };
 
 describe('유효성 검사 (예외처리)', () => {
   test('입력값 길이 검사', () => {
-    expect(() => 
+    expect(() => {
       inputValidationTest('12')
-    ).toThrow(new Error('입력값은 세자리 수를 입력해주세요.'));
+      const app = new App();
+      app.play();
+    }).toThrow(new Error('입력값은 세자리 수를 입력해주세요.'));
   });
 
   test('입력값 중첩값 검사', () => {
-    expect(() => inputValidationTest('122')
-    ).toThrow(new Error('중첩되지 않는 세자리 수를 입력해주세요.'))
+    expect(() => {
+      inputValidationTest('122')
+      const app = new App();
+      app.play();
+    }).toThrow(new Error('중첩되지 않는 세자리 수를 입력해주세요.'))
   });
 
   test('입력값 넘버타입 검사', () => {
-    expect(() => 
+    expect(() => {
       inputValidationTest('12f')
-    ).toThrow(new Error('숫자만 입력해주세요.'))    
+      const app = new App();
+      app.play();
+    }).toThrow(new Error('숫자만 입력해주세요.'))    
+  });
+
+  test('입력값 공백 검사', () => {
+    expect(() => {
+      inputValidationTest(' 12')
+      const app = new App();
+      app.play();
+    }).toThrow(new Error('공백은 넣지 말아주세요.'))    
   });
 });
