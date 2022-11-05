58cf943ffea3f5fb75a900931dc9f629b302b80e
Gaeun-Jang <110662057+Gaeun-Jang@users.noreply.github.com>
2022-11-05 13:37:57 +0900

    feat : 입력값 확인 후 3스트라이크라면 게임 종료, 아니라면 다시 입력 요청 기능 추가

--- src/App.js
+++ src/App.js
@@ -47,23 +47,29 @@ class App {
         return strike;
     }
 
+    threeStrikes() {
+        MissionUtils.Console.print("3스트라이크");
+        MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
+
     getHintMessage(inputNumber) {
-        let hintMessage = "";
         const ball = this.getBalls(inputNumber);
         const strike = this.getStrikes(inputNumber);
-        if (ball > 0) {
-            hintMessage += `${ball}볼`;
+        if (strike === 3) {
+            this.threeStrikes();
+        } else if (ball > 0 && strike === 0) {
+            MissionUtils.Console.print(`${ball}볼`);
+            this.getInputNumber();
+        } else if (ball > 0 && strike > 0) {
+            MissionUtils.Console.print(`${ball}볼 ${strike}스트라이크`);
+            this.getInputNumber();
+        } else if (ball === 0 && strike > 0) {
+            MissionUtils.Console.print(`${strike}스트라이크`);
+            this.getInputNumber();
+        } else if (ball === 0 && strike === 0) {
+            MissionUtils.Console.print("낫싱");
+            this.getInputNumber();
         }
-        if (ball > 0 && strike > 0) {
-            hintMessage += " ";
-        }
-        if (strike > 0) {
-            hintMessage += `${strike}스트라이크`;
-        }
-        if (ball === 0 && strike === 0) {
-            hintMessage += "낫싱";
-        }
-        return hintMessage;
     }
 
     checkInputNumber(inputNumber) {
@@ -77,11 +83,14 @@ class App {
         ) {
             throw "입력값이 잘못되었습니다.";
         } else {
-            MissionUtils.Console.print(this.getHintMessage(inputNumber));
+            this.getHintMessage(inputNumber);
         }
     }
 
-    play() {}
+    play() {
+        this.getRandomNumber();
+        this.getInputNumber();
+    }
 }
 
 module.exports = App;
