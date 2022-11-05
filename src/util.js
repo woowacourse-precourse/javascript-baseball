a924121b24cb91a32d2153b372985b716d2384ae
jin-Pro <dnjun2@ajou.ac.kr>
2022-11-05 20:26:28 +0900

    feat: makeReplyToReply Func

--- src/util.js
+++ src/util.js
@@ -26,7 +26,15 @@ const replyCheckAnswer = (input, answer) => {
   return { ball, strike };
 };
 
-const makeReplyToReply = ({ ball, strike }) => {};
+const makeReplyToReply = ({ ball, strike }) => {
+  if (ball === 0 && strike === 0) return "낫싱";
+  if (strike === 3)
+    return "3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+  let answer = "";
+  if (ball) answer += `${ball}볼 `;
+  if (strike) answer += `${strike}스트라이크`;
+  return answer;
+};
 
 const inputReply = () => {};
 
