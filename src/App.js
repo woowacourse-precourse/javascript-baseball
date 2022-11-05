9e8f8a30ef9ca69d273a380859cad80eae0808b3
KingJiwon <wldnjs0401@naver.com>
2022-11-05 17:22:06 +0900

    서로다른 임의의 컴퓨터숫자 3개 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
+  pushUniqueNumber(arr, num) {
+    if (!arr.includes(num)) {
+      return arr.push(num);
+    }
+  }
+  getRandomComputerArray() {
+    let randomComputerArray = [];
+    while (randomComputerArray.length < 3) {
+      let randomNumber = MissionUtils.Random.pickNumberInRange(1, 9);
+      this.pushUniqueNumber(randomComputerArray, randomNumber);
+    }
+  }
   play() {}
 }
+// const app = new App();
+// app.play();
 
 module.exports = App;
