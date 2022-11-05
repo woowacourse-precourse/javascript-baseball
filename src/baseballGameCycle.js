ceb3019aff24dfb8d21f6ecb9148b0dfcc0fc823
kangyeongmin <kym6050@hanyang.ac.kr>
2022-11-05 21:43:32 +0900

    fix: 공백 입력 버그 수정

--- src/baseballGameCycle.js
+++ src/baseballGameCycle.js
@@ -7,16 +7,12 @@ function baseballGameCycle(randomNumbers) {
   MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (input) => {
     const answer = input.split("");
 
-    if (checkInputAvailable(answer)) {
-      const result = getResult(randomNumbers, answer);
-      MissionUtils.Console.print(result);
-      result.length > 10
-        ? getRestartOption()
-        : baseballGameCycle(randomNumbers);
-    }
-    if (!checkInputAvailable(answer)) {
+    if (!input || !checkInputAvailable(answer)) {
       throw new Error("잘못된 값을 입력했습니다.");
     }
+    const result = getResult(randomNumbers, answer);
+    MissionUtils.Console.print(result);
+    result.length > 10 ? getRestartOption() : baseballGameCycle(randomNumbers);
   });
 }
 
