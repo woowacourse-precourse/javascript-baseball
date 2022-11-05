2af1410d2ccdfd1b1c20669ab3bc3a755c091720
yuna Kim <yuna000804@gmail.com>
2022-11-05 23:19:57 +0900

    refactor : App class 내에 함수 정의

--- src/App.js
+++ src/App.js
@@ -3,24 +3,26 @@ const { Random, Console } = require("@woowacourse/mission-utils");
 
 class App {
   play() {}
-}
-
-const letComputerInput = function() {
-  const computer = [];
-  while (computer.length < 3) {
-    const number = Random.pickNumberInRange(1, 9);
-    if (!computer.includes(number)) {
-      computer.push(number);
+  
+  letComputerInput() {
+    const computer = [];
+    while (computer.length < 3) {
+      const number = Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
     }
   }
+  
+  letUserInput() {
+    Console.print("숫자 야구 게임을 시작합니다.");
+    Console.readLine('숫자를 입력해주세요 : ', (answer) => {
+      //answer의 예외사항 체크
+    })
+  }
+
 }
 
-const letUserInput = function() {
-  Console.print("숫자 야구 게임을 시작합니다.");
-  Console.readLine('숫자를 입력해주세요 : ', (answer) => {
-    //answer의 예외사항 체크
-  })
-}
 
 module.exports = App;
 
