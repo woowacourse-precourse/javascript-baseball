b589c35ca7399ef6fa133a7f6307a38d30477d16
AhnGeunTaek <dksrmsxor@gmail.com>
2022-11-05 21:40:45 +0900

    Feat: 컴퓨터(상대방)의 랜덤 숫자 생성하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -3,10 +3,24 @@ const MissionUtils = require('@woowacourse/mission-utils');
 class App {
   play() {
     this.startGame();
+    this.getComputerNumber();
   }
 
   startGame() {
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다');
+  }
+
+  getComputerNumber() {
+    const computerNumbers = [];
+
+    while (computerNumbers.length < 3) {
+      const randomNumber = MissionUtils.Random.pickNumberInRange(1, 9);
+
+      if (!computerNumbers.includes(randomNumber)) {
+        computerNumbers.push(randomNumber);
+      }
+    }
+    return computerNumbers.join('');
   }
 }
 
