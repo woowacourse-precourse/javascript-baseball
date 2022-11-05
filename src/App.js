1b5a0b33b3fff76ae1a5a46db62e254f180919a6
Sungwk <73461032+Sungwk@users.noreply.github.com>
2022-11-05 16:03:25 +0900

    feat : 3. 사용자의 3자리 수 입력받기

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ class App {
   play() {
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
     this.computerNumber = this.getComputerNumber();
+    this.inputNumber = this.getInputNumber();
   }
   getComputerNumber() {
     const COMPUTER = [];
@@ -14,6 +15,14 @@ class App {
     }
     return COMPUTER;
   }
+  getInputNumber() {
+    MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (inputNumber) => {
+      return inputNumber;
+    });
+  }
 }
+
+let app = new App();
+app.play();
 
 module.exports = App;
