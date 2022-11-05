91f8f952663f824962b7ce6f4ad466fc346f4d23
2yunseong <dbsdltjd3701@naver.com>
2022-11-05 16:11:00 +0900

    feat: 컴퓨터의 카운트를 생성하는 기능

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
+  constructor() {
+    this.count = this.generateCount();
+  }
   play() {}
 
   gameStart() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+  }
+
+  generateCount() {
+    return MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3).reduce(
+      (acc, cur) => acc + cur,
+      ""
+    );
   }
 }
 
