8afabb8984f1a56e56310fe3a89713ce6bd4fe77
ReturnReason <reasonz@daum.net>
2022-11-05 12:05:39 +0900

    refactor: 각 테스트가 독립적인 변수를 갖도록 수정

--- __tests__/RandomNumberTest.js
+++ __tests__/RandomNumberTest.js
@@ -1,16 +1,17 @@
 const App = require('../src/App');
 
 describe('기능 구현 테스트(게임 시작 시 3자리 랜덤 숫자 생성 함수) : computer 💻', () => {
-  const app = new App();
-  const numbers = app.makeRandomNumbers();
-
   test('✨ 서로 다른 3자리 숫자로 이루어져 있습니다.', () => {
+    const app = new App();
+    const numbers = app.makeRandomNumbers();
     const numbersLength = [...new Set(numbers)].length;
 
     expect(numbersLength).toBe(3);
   });
 
   test('✨ 1~9까지의 숫자로만 이루어져 있습니다.', () => {
+    const app = new App();
+    const numbers = app.makeRandomNumbers();
     const reg = /[1-9]{1}/;
     const fromOneToNine = (number) => reg.test(number);
     const isOneToNine = numbers.map(String).filter(fromOneToNine).length === 3;
