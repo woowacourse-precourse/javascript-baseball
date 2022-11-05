1eff8ec0b571d20c06c21a9738a1f04c9b73f391
jiyujin <yujin1679@naver.com>
2022-11-06 01:12:14 +0900

    볼, 스트라이크 개수를 반환하는 countBalls, countStrikes 함수 추가

--- src/App.js
+++ src/App.js
@@ -5,17 +5,19 @@ class App {
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
     let userInput = await this.userInput('숫자를 입력해주세요: ');
     let checkUserNumber = this.checkUserInput(userInput);
-    if (checkUserNumber) {
-      console.log(checkUserNumber);
-    }
     let randomNumber = this.setRandomNumber();
     console.log(randomNumber);
+    if (checkUserNumber) {
+      console.log(this.gameResult(checkUserNumber, randomNumber));
+    }
   }
+
   async userInput(prompt) {
     return await new Promise((resolve) => {
       MissionUtils.Console.readLine(prompt, resolve);
     });
   }
+
   checkUserInput(userInput) {
     const userInputToArr = [...new Set(userInput)];
     if (userInputToArr.length !== 3) {
@@ -25,8 +27,37 @@ class App {
     const userInputToNumberArr = userInputToArr.map((item) => Number(item));
     return userInputToNumberArr;
   }
+
   setRandomNumber() {
     return MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
+  }
+
+  countStrickes(userNumbers, randomNumbers) {
+    let strikeCount = 0;
+    userNumbers.forEach((userNumber, index) => {
+      let randomNumber = randomNumbers[index];
+      if (userNumber == randomNumber) {
+        strikeCount += 1;
+      }
+    });
+    return strikeCount;
+  }
+
+  countBalls(userNumbers, randomNumbers) {
+    let ballCount = 0;
+    userNumbers.forEach((userNumber, index) => {
+      let randomNumber = randomNumbers[index];
+      if (userNumber != randomNumber && randomNumbers.includes(userNumber)) {
+        ballCount += 1;
+      }
+    });
+    return ballCount;
+  }
+
+  gameResult(userNumbers, randomNumbers) {
+    let strikes = this.countStrickes(userNumbers, randomNumbers);
+    let balls = this.countBalls(userNumbers, randomNumbers);
+    console.log(`${strikes} strike ${balls} ball`);
   }
 }
 
