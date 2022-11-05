c576ace98e4f6c68f20dd0161886a5b2c37ba047
jsh3418 <albert5428@gmail.com>
2022-11-05 16:14:53 +0900

    feat: 사용자 입력한 수와 비교하여 결과 메시지를 띄움
    1. 3스트라이크 일 때 '3스트라이크'를 띄움.
    2. 스트라이크, 볼 중 하나라도 있다면 몇볼 몇스트라이크인지 띄움.
    3. 스트라이크, 볼 둘 다 0이라면 '낫싱'을 띄움.

--- src/App.js
+++ src/App.js
@@ -22,8 +22,39 @@ class App {
   requestAnswer(message) {
     Console.readLine(message, (answer) => {
       if (answer.length !== 3) throw new Error("입력 에러! 숫자 3자리를 입력해주세요!");
-      this.userAnswer = answer.split("");
+      this.userAnswer = answer.split("").map(Number);
+
+      this.compareAnswer(this.answer, this.userAnswer);
     });
+  }
+
+  compareAnswer(answer, userAnswer) {
+    let strike = 0;
+    let ball = 0;
+
+    userAnswer.forEach((number, index) => {
+      if (number === answer[index]) {
+        strike++;
+        return;
+      }
+
+      if (answer.includes(number)) {
+        ball++;
+        return;
+      }
+    });
+
+    if (strike === 3) {
+      this.sendMessage("3스트라이크");
+      return;
+    }
+
+    if (strike !== 0 || ball !== 0) {
+      this.sendMessage(`${ball ? `${ball}볼 ` : ""}${strike ? `${strike}스트라이크` : ""}`);
+      return;
+    }
+
+    this.sendMessage("낫싱");
   }
 
   play() {
