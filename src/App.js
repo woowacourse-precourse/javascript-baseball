23267878bfad35ea87c0e8010e9d1d8f33ad836c
RosaDamascena <hsj990604@gmail.com>
2022-11-06 01:26:37 +0900

    feat : 숫자 야구 게임 시작 문구 출력 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-function randomNum(numbers) {
+function randomNum() {
     let numbers = [];
     for (let i = 0; i < 9; i++) {
       numbers.push(i + 1);
@@ -10,11 +10,13 @@ function randomNum(numbers) {
       answer.push(numbers[index]);
       numbers.splice(index, 1);
     }
-    console.log(answer);
+  
   }
 
 class App {
-  play() {}
+  play() {
+    console.print("숫자 야구 게임을 시작합니다.");
+  }
 }
 
 module.exports = App;
