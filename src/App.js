f5d700dd706802db221708cf81c8ddbc59c0435d
hanbeulYou <hanbeul.you@gmail.com>
2022-11-06 01:07:23 +0900

    feat: 사용자 input값 유효성 검증

--- src/App.js
+++ src/App.js
@@ -5,23 +5,55 @@ function makeComputerRandNums() {
   return randNumsArr;
 }
 
-function isValidInput(input) {
-  console.log(input);
+function isLengthThree(userInputArray) {
+  return userInputArray.length === 3;
+}
+
+function isValidNumber(userInputArray) {
+  const returnBool = userInputArray.every((element) => {
+    return element >= '1' && element <= '9';
+  });
+  return returnBool;
+}
+
+
+function isSameNumber(userInputArray) {
+  const userInputSet = new Set(userInputArray);
+  return userInputSet.length === userInputArray.length;
+}
+
+function isValidInput(userInputArray) {
+  let returnIsValid = [true, true, true];
+  returnIsValid[0] = isLengthThree(userInputArray);
+  returnIsValid[1] = isValidNumber(userInputArray);
+  returnIsValid[2] = isSameNumber(userInputArray);
+  
+  const returnBool = returnIsValid.every((element) => {
+    return element;
+  });
+  return returnBool;
+}
+
+function numToArr(userInputNums) {
+  const userInputArray = userInputNums.split('');
+  return userInputArray;
 }
 
 function getUserInput() {
-  let userInput;
-  MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (input) => {
-    isValidInput(input)
+  let userInputArray = [];
+  let isValidFlag = true;
+  MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (userInputNums) => {
+    userInputArray = numToArr(userInputNums);
+    isValidFlag = isValidInput(userInputArray);
   })
-  return userInput;
+  return userInputArray;
 }
 
 function playGame() {
   while (true) {
-    const computerRandNumsArr = makeComputerRandNums();
-    getUserInput();
-    break;
+    const computerRandNumsArray = makeComputerRandNums();
+    const userInputArray = getUserInput();
+    const gameResult = getGameResult(computerRandNumsArray, userInputArray);
   }
 }
 
