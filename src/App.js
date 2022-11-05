d2a6ea230bd21d22d70978f5597c347e2d3b540b
s0ojin <onepst@hanyang.ac.kr>
2022-11-05 22:47:31 +0900

    feat : makeRandomNumber 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 class App {
   constructor() {
     this.printGreeting();
+    this.makeRandomNumber();
   }
 
   printGreeting() {
     Console.print('숫자 야구 게임을 시작합니다.');
+  }
+
+  makeRandomNumber() {
+    let randomNumSet = new Set();
+    while (3) {
+      randomNumSet.add(Random.pickNumbersInRange(1, 9));
+    }
+    return Array.from(randomNumSet);
   }
 
   play() {}
