da7966498f0394cd229b88d143c76113ce047c59
oio337a <oio337a@gmail.com>
2022-11-05 17:08:03 +0900

    feat : userinput

--- src/App.js
+++ src/App.js
@@ -13,9 +13,42 @@ class App {
     this.print("숫자 야구 게임을 시작합니다.");
     return this.pickNumbers();
   }
+
+  input() {
+    return new Promise((resolve) => {
+      MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (answer) => {
+        this.userInput = [...answer];
+        resolve();
+      });
+    });
+  }
+
+  async inGame() {
+    await this.input();
+    if (this.userInput.length !== 3) {
+      throw new Error("3자리 수를 입력해주세요.");
+    }
+    if (this.userInput.map()) this.print(this.userInput);
+  }
+
   play() {
     this.startGame();
-    return MissionUtils.Console.close();
+    this.inGame();
+    // await this.input();
+
+    // while (1) {
+    //   switch (coin) {
+    //     case "1":
+    //       break;
+    //     case "2":
+    //       break;
+    //     default:
+    //       this.print("잘못입력하셨습니다. 새로시작 : 1 , 종료 : 2");
+    //       break;
+    //   }
+    //   console.log(123);
+    // }
+    return;
   }
 }
 
