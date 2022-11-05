d75eb393a807e4f08c527d16fd317e6d21a84f37
pakxe <pigkill40@naver.com>
2022-11-05 17:28:43 +0900

    feat: 문장을 출력하는 모듈 삽입

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+const { Console } = require('@woowacourse/mission-utils');
+const { GAME_SENTENCE } = require('./constants');
+
 class App {
+  constructor() {}
+
   play() {}
 }
+
+const app = new App();
+
+// 문장 출력
+Console.print(GAME_SENTENCE.OPENING);
 
 module.exports = App;
