70bafd3a13a77a8375323474ec9e41a67fa2af0e
ReturnReason <reasonz@daum.net>
2022-11-05 10:20:20 +0900

    refactor: 상수 파일 분리

--- src/common/constants.js
+++ src/common/constants.js
@@ -8,24 +8,9 @@ const GAME_RESULT = {
   nothing: '낫싱',
 };
 
-const START_MESSAGE = '숫자 야구 게임을 시작합니다.';
-const INPUT_MESSGAE = '숫자를 입력해주세요 : ';
-const END_MESSAGE = `${MAX_LENGTH}개의 숫자를 모두 맞히셨습니다! 게임 종료
-게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.`;
-
-const INVALID_ERROR_MESSAGE = `숫자가 아닌 값이 포함되어 있습니다. 게임 종료`;
-const DUPLICATE_ERROR_MESSAGE = `중복된 숫자가 포함되어 있습니다. 게임 종료`;
-const RANGE_ERROR_MESSAGE = `입력 값의 길이가 올바르지 않습니다. (3자리 숫자가 아님) 게임 종료`;
-
 module.exports = {
   MAX_LENGTH,
   START_NUM,
   END_NUM,
   GAME_RESULT,
-  START_MESSAGE,
-  END_MESSAGE,
-  INPUT_MESSGAE,
-  INVALID_ERROR_MESSAGE,
-  DUPLICATE_ERROR_MESSAGE,
-  RANGE_ERROR_MESSAGE,
 };
---
+++ src/common/messages.js
@@ -0,0 +1,19 @@
+const START_MESSAGE = '숫자 야구 게임을 시작합니다.';
+const INPUT_MESSGAE = '숫자를 입력해주세요 : ';
+const END_MESSAGE = `3개의 숫자를 모두 맞히셨습니다! 게임 종료
+게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.`;
+
+const INVALID_ERROR_MESSAGE =
+  '올바르지 않은 값이 포함되어 있습니다. (0 또는 숫자가 아님) 게임 종료';
+const DUPLICATE_ERROR_MESSAGE = '중복된 숫자가 포함되어 있습니다. 게임 종료';
+const RANGE_ERROR_MESSAGE =
+  '입력 값의 길이가 올바르지 않습니다. (3자리 숫자가 아님)게임 종료';
+
+module.exports = {
+  START_MESSAGE,
+  END_MESSAGE,
+  INPUT_MESSGAE,
+  INVALID_ERROR_MESSAGE,
+  DUPLICATE_ERROR_MESSAGE,
+  RANGE_ERROR_MESSAGE,
+};
