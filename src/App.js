0da12c49231a604449c740aae6a3f641befcda6c
minjaeKim <smallkdb@gmail.com>
2022-11-05 21:35:04 +0900

    feat(App): add runTurn method
    게임 한 턴을 실행하는 runTurn 메서드를 App 클래스에 추가하였습니다.
    - 이 메서드는 다음 과정을 수행합니다.
      - 숫자 입력받기
      - 입력받은 숫자로 볼 카운트 계산 후 출력
      - 볼 카운트에 따라 게임을 종료하거나 한턴 재실행
    추가적으로 숫자 야구의 입력 길이를 생성자로 추가하였습니다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+const { Console } = require('@woowacourse/mission-utils');
+const { countBall, countStrike, printBallCount } = require('./utils/BallCount');
+
 class App {
-  play() {}
+	constructor() {
+		this.NUMBER_LENGTH = 3;
+	}
+
+	play() {}
+
+	runTurn(computerNumbers) {
+		Console.readLine('숫자를 입력해주세요 : ', (input) => {
+			const userNumbers = input.split('');
+			const ball = countBall(userNumbers, computerNumbers);
+			const strike = countStrike(userNumbers, computerNumbers);
+			printBallCount(ball, strike);
+			return strike === this.NUMBER_LENGTH ? this.quitGame() : this.runGame(computerNumbers);
+		});
+	}
+
+	quitGame() {}
 }
 
 module.exports = App;
