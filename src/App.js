4a86e1cfbe0fd760bd9f9095d67462275ed951a5
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2022-11-06 06:19:38 +0900

    refactor: Console의 지속적 오류 해결

--- src/App.js
+++ src/App.js
@@ -1,42 +1,48 @@
-const MissionUtils = require('@woowacourse/mission-utils');
+const { Console } = require('@woowacourse/mission-utils');
 
 class App {
-  constructor() {
-    this.playOption = '';
+  play() {
+    try {
+      // 게임 시작 콘솔
+      Console.print('숫자 야구 게임을 시작합니다.');
+
+      // 게임 진행 콘솔
+      Console.readLine('숫자를 입력해주세요 : ', (userInput) => {
+        Console.print(`input : ${userInput}`);
+
+        // 게임끝 재시작 콘솔
+        this.selectContinue();
+      });
+    } catch (err) {}
   }
 
-  play() {
-    this.playOption = selectPlayOption();
-    const validValue = isValidOption(this.playOption);
-    checkValidPlayOption(validValue);
+  end() {
+    Console.print('게임을 종료합니다.');
+    return Console.close();
+  }
+
+  selectContinue() {
+    Console.readLine(
+      '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.',
+      (userInput) => {
+        Console.print(`input : ${userInput}`);
+
+        const playContinue = isValidContinueOption(userInput);
+        if (playContinue) return this.play();
+
+        return this.end();
+      },
+    );
   }
 }
 
-const checkValidPlayOption = (validValue) => {
-  if (!validValue) {
-    throw new Error('Play옵션에 없는 값입니다.');
-  }
+const isValidContinueOption = (option) => {
+  if (option === '1') return true;
+  if (option === '2') return false;
+
+  throw new Error('1,2 이외의값');
 };
 
-const selectPlayOption = () => {
-  let selectValue;
-  MissionUtils.Console.readLine(
-    '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.',
-    (select) => {
-      console.log(`${select}`);
-      selectValue = select;
-    },
-  );
-
-  return selectValue;
-};
-
-const isValidOption = (option) => {
-  if (option === '1' || option === '2') {
-    return true;
-  }
-
-  return false;
-};
-
+const app = new App();
+app.play();
 module.exports = App;
