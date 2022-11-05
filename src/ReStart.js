c1f718425c5c24a11d8b4a79414701cbcedaba97
Rki0 <pky00823@gmail.com>
2022-11-05 12:09:06 +0900

    feat: added checking player input function
    Added checking player input function.
    This function checks whether the player's input for restart  meets the constraints.

--- src/ReStart.js
+++ src/ReStart.js
@@ -5,9 +5,18 @@ class ReStart {
     MissionUtils.Console.readLine(
       "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n",
       (userInput) => {
+        this.checkReStartInput(userInput);
         MissionUtils.Console.close();
       }
     );
+  }
+
+  checkReStartInput(userInput) {
+    if (userInput === "1" || userInput === "2") {
+      return true;
+    }
+
+    throw new Error("잘못된 입력입니다. 게임을 종료합니다.");
   }
 }
 
