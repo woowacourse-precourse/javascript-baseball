1b127ac720d364c260ba788c4034e76d9ba0eeac
이석준 <sukjune96@gmail.com>
2022-11-05 21:49:51 +0900

    refactor: 사용자 입력 받는 함수 gmae 메서드에 포함시킴

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 const MissionUtils = require("@woowacourse/mission-utils");
+const getScore = require("./getScore");
+const getUserExecption = require("./getUserExecption");
 const makeAnswer = require("./makeNumber");
 
 class App {
   play() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    const answer = makeAnswer();
+    this.game(answer);
+  }
+  game(answer) {
+    console.log(answer); //정답 미리보는 콘솔 로그
+    MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (input) => {
+      const inputArr = getUserExecption(input);
+      const score = getScore(inputArr, answer);
+    });
   }
 }
 const app = new App();
