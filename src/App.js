6c99afd9d0ff9bc4e21686ce862247b7db92bee3
semnil5202 <semnil5202@gmail.com>
2022-11-06 00:51:13 +0900

    feat: 세 자리의 숫자를 랜덤으로 생성
    MissionUtils 라이브러리의 pickNumberInRange 함수를 이용하여 랜덤 숫자를 만든다.
    1에서 9사이로 이루어진 세 자리 숫자를 만들며 각 자릿수는 중복되지 않는다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
   play() {}
+
+  makeRandomNumber() {
+    const results = [];
+    while (results.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!results.includes(number)) results.push(number);
+    }
+    return results;
+  }
 }
 
 module.exports = App;
