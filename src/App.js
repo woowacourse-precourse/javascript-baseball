41169ae859aa6254f7b95df517d48c0eefb22e50
Yoonkyoung Kim <100656920+Yoonkyoungme@users.noreply.github.com>
2022-11-05 21:43:40 +0900

    feat: 사용자가 입력한 숫자와 컴퓨터 숫자를 비교하여 ball, strike 개수 세기

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ class App {
   play() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
     const computerNumber = this.createComputerNumber();
-    const userNumber = this.inputUserNumber();
-    console.log(computerNumber, userNumber);
+    const userNumber = this.inputUserNumber(computerNumber);
+    // console.log(computerNumber, userNumber);
   }
 
   // 컴퓨터의 숫자 랜덤으로 생성한다. (1부터 9까지 서로 다른 수로 이루어진 3자리의 수)
@@ -22,16 +22,16 @@ class App {
   }
 
   // 사용자에게 숫자를 입력 받는다.
-  inputUserNumber() {
-    let userNumber;
+  inputUserNumber(computerNumber) {
+    let user;
 
     MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (num) => {
-      console.log(num, typeof num);
       const checkNumber = this.isNumberVaild(num);
-      console.log("vaild check", checkNumber);
-      userNumber = checkNumber;
+      user = checkNumber;
+
+      const compareResults = this.compareNumbers(user, computerNumber);
     });
-    return userNumber;
+    return user;
   }
 
   // 사용자가 입력한 숫자 유효성 검사
@@ -41,29 +41,43 @@ class App {
 
     //  입력한 수가 3자리 수인가
     if (len !== 3) {
-      console.log("3자리 수 아님");
       throw "3자리 수를 입력해주세요.";
-      isVaild = true;
     }
 
+    // 서로 다른 숫자인가
     if (len !== set.size) {
-      console.log("서로 다른 숫자임");
       throw "서로 다른 수를 입력하세요.";
     }
+
     // 1 ~ 9 범위에 해당하는 수를 입력했는가
     this.isNumberInRange(number);
+
     return number;
   }
 
   // 유효성 검사: 1 ~ 9 범위에 해당하는 수를 입력했는가
   isNumberInRange(number) {
     for (let i = 0; i < number.length; i++) {
-      console.log("각 자리 수 ", number[i]);
       if (!(parseInt(number[i]) >= 1 && parseInt(number[i]) <= 9)) {
-        console.log("숫자 범위에 맞지 않음");
         throw "1 ~ 9 범위에 해당하는 숫자를 입력하세요.";
       }
     }
+  }
+
+  // 사용자가 입력한 숫자와 컴퓨터 숫자를 비교하여 ball, strike 개수 세기
+  countBallAndStrike(user, computer) {
+    let score = {
+      ball: 0,
+      strike: 0,
+    };
+    let compare = [...user].map((num, index) => {
+      if (num === computer[index]) {
+        score.strike++;
+      } else if (computer.includes(num)) {
+        score.ball++;
+      }
+    });
+    return score;
   }
 }
 
