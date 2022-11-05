2e8cbca8abb99d8013e7c03beb6c9cc39a7b12d8
jiyujin <yujin1679@naver.com>
2022-11-05 23:36:39 +0900

    feat: 서로 다른 3자리 숫자를 입력하는지 확인하는 checkUserInput 함수 추가

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@ const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
   async play() {
-    let userNumber = await this.userInput('숫자입력: ');
-    console.log(userNumber);
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+    let userInput = await this.userInput('숫자를 입력해주세요: ');
+    let checkUserNumber = this.checkUserInput(userInput);
+    if (checkUserNumber) {
+      console.log(checkUserNumber);
+    }
   }
   async userInput(prompt) {
     return await new Promise((resolve) => {
       MissionUtils.Console.readLine(prompt, resolve);
     });
+  }
+  checkUserInput(userInput) {
+    const userInputToArr = [...new Set(userInput)];
+    if (userInputToArr.length !== 3) {
+      MissionUtils.Console.print('조건에 맞게 숫자를 입력해주세요!');
+      return 0;
+    }
+    return userInputToArr;
   }
 }
 
