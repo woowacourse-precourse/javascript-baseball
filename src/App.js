7ee86ef38bcf050d3cecb6ef63e5f82e43ee93c6
BuildTools <unconfigured@null.spigotmc.org>
2022-11-06 03:37:06 +0900

    받아들이는 형식 문자열->배열

--- src/App.js
+++ src/App.js
@@ -2,23 +2,36 @@ const MissionUtils = require('@woowacourse/mission-utils');
 
 class App {
   play() {
-    const answer = makeComputerAnswer();
-    console.log('숫자 야구 게임을 시작합니다.');
-    MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (message) =>
-      console.log(message)
-    );
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+    const answer = this.makeComputerAnswer();
+    this.playBaseball(answer);
   }
-}
 
-function makeComputerAnswer() {
-  const computer = [];
-  while (computer.length < 3) {
-    const number = MissionUtils.Random.pickNumberInRange(1, 9);
-    if (!computer.includes(number)) {
-      computer.push(number);
+  makeComputerAnswer() {
+    const computer = [];
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
     }
+    return computer;
   }
-  return computer;
+
+  playBaseball(answer) {
+    let result = [];
+    MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (number) => {
+      for (let i = 0; i < 3; i++) {
+        result.push(number[i]);
+      }
+      console.log(result);
+    });
+  }
+
+  checkBall(message, answer) {
+    const result = message.filter((message) => answer.inclueds(answer));
+    return result;
+  }
 }
 
 module.exports = App;
