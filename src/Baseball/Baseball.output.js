e0038cc40ac4f00bc31f138d3d66e437dc22d428
loopy-lim <loopy@jnu.ac.kr>
2022-11-05 19:57:22 +0900

    docs: 게임을 시작하는 문구를 수정한다

--- src/Baseball/Baseball.output.js
+++ src/Baseball/Baseball.output.js
@@ -2,7 +2,7 @@ const MissionUtils = require("@woowacourse/mission-utils");
 
 class BaseBasllOutput {
   start() {
-    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.\n");
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
   }
   end() {
     MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n");
