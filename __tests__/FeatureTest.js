ce738e9208cb296a7dc6419147de45d852f84a3b
froggy1014 <clcl6084@gmail.com>
2022-11-06 02:22:27 +0900

    test: 기능 목록 테스트 케이스 추가
    1. 정답과 입력값 비교 및 힌트 출력 테스트 케이스 추가
    2. 재시작 및 종료 기능 테스트 케이스 추가

--- __tests__/FeatureTest.js
+++ __tests__/FeatureTest.js
@@ -57,4 +57,54 @@ describe('기능 목록 검사', () => {
       expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(message));
     });
   });
+
+  test('정답과 입력값 비교 검사 및 힌트 출력', () => {
+    const randoms = [1, 2, 3];
+    const playerInput = ['456', '415', '134', '153', '123']
+    const logSpy = getLogSpy();
+
+    const messages = [
+      '숫자 야구 게임을 시작합니다.',
+      '낫싱',
+      '1볼',
+      '1볼 1스트라이크',
+      '2스트라이크',
+      '3스트라이크',
+    ];
+
+    mockRandoms(randoms);
+    mockQuestions(playerInput);
+
+    const app = new App();
+    app.play();
+
+    messages.forEach((message) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(message));
+    });
+  });
+
+  test('재시작 및 종료 검사', () => {
+    const randoms = [1, 2, 3, 4, 5, 6];
+    const playerInput = ['123', '1', '456', '2']
+    const logSpy = getLogSpy();
+
+    const messages = [
+      '숫자 야구 게임을 시작합니다.',
+      '3스트라이크',
+      '3개의 숫자를 모두 맞히셨습니다! 게임 종료',
+      '3스트라이크',
+      '3개의 숫자를 모두 맞히셨습니다! 게임 종료',
+      '게임 종료',
+    ]
+
+    mockRandoms(randoms);
+    mockQuestions(playerInput);
+
+    const app = new App();
+    app.play();
+
+    messages.forEach((message) => {
+      expect(logSpy).toHaveBeenCalledWith(message);
+    });
+  });
 });
