3287a9e6b518e106a3dff416184a51cc330ada48
xilucks <siun0331@gmail.com>
2022-11-05 16:47:31 +0900

    feat: Computer 클래스 생성

---
+++ src/Computer.js
@@ -0,0 +1,26 @@
+export default class Computer {
+  constructor() {
+    this.numbers = this.settingComputerNums();
+  }
+
+  settingComputerNums() {
+    let computerNumbers = [];
+    while (computerNumbers.length !== 3) {
+      computerNumbers.push(this.createNewNum(computerNumbers));
+    }
+
+    return computerNumbers;
+  }
+
+  createNewNum(computerNumbers) {
+    const newNumber = MissionUtils.Random.pickNumberInRange(1, 9);
+
+    return this.numberDuplicationCheck(computerNumbers, newNumber)
+      ? this.createNewNum(computerNumbers)
+      : newNumber;
+  }
+
+  numberDuplicationCheck(computerNumbers, newNumber) {
+    return computerNumbers.includes(newNumber);
+  }
+}
