2baf16fd3836dea5474eaf008b7b482a6345f7ef
jongwonp <jw032876@gmail.com>
2022-11-05 18:29:29 +0900

    종료 기능 완료, 재시작 기능 필요

--- src/exitOrRestart.js
+++ src/exitOrRestart.js
@@ -1,14 +1,22 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 
-const exitOrRestart = (num) => {
-  if (num === '1') restart();
-  if (num === '2') exit();
+const guessedCorrectly = () => {
+  MissionUtils.Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+  MissionUtils.Console.readLine(
+    '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.',
+    (num) => {
+      exitOrRestart(num);
+    }
+  );
 };
 
-const restart = () => {};
+const exitOrRestart = (num) => {
+  if (num === '1') return true;
+  if (num === '2') exit();
+};
 
 const exit = () => {
   MissionUtils.Console.close();
 };
 
-module.exports = exitOrRestart;
+module.exports = guessedCorrectly;
