54d8d61053c4669f79c26c605eea22fa3e8949e6
hanbeulYou <hanbeul.you@gmail.com>
2022-11-05 21:25:13 +0900

    feat: computer random numbers 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
+function makeComputerRandNums() {
+  const randNumsArr = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3)
+  return randNumsArr;
+}
+
+function playGame() {
+  while (true) {
+    const computerRandNumsArr = makeComputerRandNums();
+    console.log(computerRandNumsArr);
+    break;
+  }
+}
+
 class App {
-  play() {}
+  constructor() {
+  }
+
+  play() {
+    playGame();
+  }
 }
 
 module.exports = App;
+const a = new App()
+a.play();
