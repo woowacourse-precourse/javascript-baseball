280abf73f2df04934582e11f93e5974bd7b6e04e
sunny <huiseon37@gmail.com>
2022-11-05 21:47:03 +0900

    feat: BaseBallGame 클래스의 랜덤 숫자 생성 함수 구현

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ class User {
   inputValue;
   constructor(){
     this.input();
+    const game = new BaseBallGame();
   }
   input(){
     MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (answer) => {
@@ -10,9 +11,21 @@ class User {
     })
   }
 }
+
+class BaseBallGame {
+  answer;
+  constructor(){
+    this.init();
+  }
+  init(){
+    answer = Array.from(Array(3),() => MissionUtils.Random.pickNumberInRange(1, 9));
+  }
+}
+
 class App {
   play() {
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+    new User();
   }
 }
 
