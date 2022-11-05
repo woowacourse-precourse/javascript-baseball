1a12e72ace0cc405d8f8ae91deb86cfdaf339839
Sungwk <73461032+Sungwk@users.noreply.github.com>
2022-11-05 15:58:45 +0900

    feat : 1부터 9까지 서로 다른 수로 이루어진 임의의 3자리의 수 생성 후 할당

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 const MissionUtils = require('@woowacourse/mission-utils');
-const utils = {
-  print(message) {
-    MissionUtils.Console.print(message);
-  },
-};
 class App {
   play() {
-    utils.print('숫자 야구 게임을 시작합니다.');
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+    this.computerNumber = this.getComputerNumber();
+  }
+  getComputerNumber() {
+    const COMPUTER = [];
+    while (COMPUTER.length < 3) {
+      const NUMBER = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!COMPUTER.includes(NUMBER)) {
+        COMPUTER.push(NUMBER);
+      }
+    }
+    return COMPUTER;
   }
 }
 
