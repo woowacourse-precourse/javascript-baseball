c0a28cbbaf09f0fd6a89cb3342633ea5095441da
pakxe <pigkill40@naver.com>
2022-11-05 18:55:23 +0900

    feat: 범위 검사 테스트 코드 작성

--- __tests__/ValidationTest.js
+++ __tests__/ValidationTest.js
@@ -1,4 +1,4 @@
-const { validLength } = require('../src/Validation');
+const { validLength, validRange } = require('../src/Validation');
 
 describe('길이 검사', () => {
   test('case1', () => {
@@ -12,5 +12,20 @@ describe('길이 검사', () => {
   });
   test('case4', () => {
     expect(validLength([])).toEqual(false);
+  });
+});
+
+describe('범위 검사', () => {
+  test('case1', () => {
+    expect(validRange([1, 2, 0])).toEqual(false);
+  });
+  test('case2', () => {
+    expect(validRange([0, 3, 4])).toEqual(false);
+  });
+  test('case3', () => {
+    expect(validRange([1, 2, 3])).toEqual(true);
+  });
+  test('case4', () => {
+    expect(validRange([0, 0, 0])).toEqual(false);
   });
 });
