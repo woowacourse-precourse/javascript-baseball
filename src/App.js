3fa15e34dffa2715777a249307c28a26d9a40ecd
froggy1014 <clcl6084@gmail.com>
2022-11-06 03:09:38 +0900

    refactor: AirBnB Coding Convention 적용
    1. AirBnB 코딩 컨벤션 적용
    2. 구조분해 할당을 통한 불필요 코드 제거

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-const MissionUtils = require("@woowacourse/mission-utils");
+const { Console, Random } = require('@woowacourse/mission-utils');
 
 class App {
   constructor() {
-    this.randoms;
     this.init();
-  }
+  };
 
   init() {
-    this.randoms = this.generateRandomNumbers()
-    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+    this.randoms = this.generateRandomNumbers();
+    Console.print('숫자 야구 게임을 시작합니다.');
   }
 
   generateRandomNumbers() {
     const uniqueRandomNumbers = [];
-    while(uniqueRandomNumbers.length < 3) {
-      let number = MissionUtils.Random.pickNumberInRange(1, 9)
-      if(!uniqueRandomNumbers.includes(number)) {
+    while (uniqueRandomNumbers.length < 3) {
+      let number = Random.pickNumberInRange(1, 9)
+      if (!uniqueRandomNumbers.includes(number)) {
         uniqueRandomNumbers.push(number);
       }
     }
@@ -23,44 +22,44 @@ class App {
   }
 
   play() {
-    MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (playerInput) => {
-      this.inputValidation(playerInput)
-    })
-  }
+    Console.readLine('숫자를 입력해주세요 : ', (playerInput) => {
+      this.inputValidation(playerInput);
+    });
+  };
 
   inputValidation(playerInput) {
-    const inputToSet = new Set(playerInput.split('').map(Number))
+    const inputToSet = new Set(playerInput.split('').map(Number));
     
-    if(playerInput.length !== 3) {
-      throw new Error('입력값은 세자리 수를 입력해주세요.')
-    } else if([...inputToSet].length !== 3) {
-      throw new Error('중첩되지 않는 세자리 수를 입력해주세요.')
+    if (playerInput.length !== 3) {
+      throw new Error('입력값은 세자리 수를 입력해주세요.');
+    } else if ([...inputToSet].length !== 3) {
+      throw new Error('중첩되지 않는 세자리 수를 입력해주세요.');
     }
 
-    inputToSet.forEach((v) => { if(isNaN(v)) throw new Error('숫자만 입력해주세요.') })
+    inputToSet.forEach((v) => { if (isNaN(v)) throw new Error('숫자만 입력해주세요.') });
 
-    return this.checkAnswer(playerInput);
+    this.checkAnswer(playerInput);
   }
 
   checkAnswer(playerInput) {   
     const ball = this.countBall(playerInput);
     const strike = this.countStrike(playerInput);
 
-    return this.printHint(ball, strike);
+    this.printHint(ball, strike);
   }
 
   countStrike(playerInput) {
     let strikes = 0;
-    for(let digit = 0; digit < 3; digit++)
-      if(this.randoms[digit] === Number(playerInput[digit])) strikes++;
+    for (let digit = 0; digit < 3; digit++)
+      if (this.randoms[digit] === Number(playerInput[digit])) strikes++;
     
     return strikes;
   }
 
   countBall(playerInput) {
     let balls = 0;
-    for(let digit = 0; digit < 3; digit++) {
-      if(this.randoms[digit] !== Number(playerInput[digit]) && this.randoms.includes(Number(playerInput[digit]))){
+    for (let digit = 0; digit < 3; digit++) {
+      if (this.randoms[digit] !== Number(playerInput[digit]) && this.randoms.includes(Number(playerInput[digit]))) {
         balls++;
       } 
     }
@@ -68,37 +67,36 @@ class App {
   }
 
   printHint(ball, strike) {
-    if(strike === 3) {
-      MissionUtils.Console.print(`3스트라이크`);
-      MissionUtils.Console.print(`3개의 숫자를 모두 맞히셨습니다! 게임 종료`)
+    if (strike === 3) {
+      Console.print('3스트라이크');
+      Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
       return this.correctAnswer();
-    } 
+    };
     
-    if( ball !== 0 && strike !== 0) MissionUtils.Console.print(`${ball}볼 ${strike}스트라이크`)
-    else if(ball !== 0 && strike === 0) MissionUtils.Console.print(`${ball}볼`)
-    else if(ball === 0 && strike !== 0) MissionUtils.Console.print(`${strike}스트라이크`)
-    else if (ball === 0 && strike === 0) MissionUtils.Console.print('낫싱')
+    if ( ball !== 0 && strike !== 0) Console.print(`${ball}볼 ${strike}스트라이크`);
+    else if (ball !== 0 && strike === 0) Console.print(`${ball}볼`);
+    else if (ball === 0 && strike !== 0) Console.print(`${strike}스트라이크`);
+    else if (ball === 0 && strike === 0) Console.print('낫싱');
     
-    return this.play();
-  }
+    this.play();
+  };
 
   correctAnswer() { 
-    MissionUtils.Console.readLine(`게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n`, (decision) => {
-      if(Number(decision) === 1) {
-        this.randoms = this.generateRandomNumbers()
-        return this.play();
+    Console.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n', (decision) => {
+      if (Number(decision) === 1) {
+        this.randoms = this.generateRandomNumbers();
+        this.play();
       }
-      else if(Number(decision) === 2) {
-        MissionUtils.Console.print("게임 종료")
-        return MissionUtils.Console.close();
+      else if (Number(decision) === 2) {
+        Console.print('게임 종료');
+        Console.close();
       }
       else this.correctAnswer();
-    })
-  }
+    });
+  };
+};
 
-}
-
-const app = new App();
-app.play();
+const baseballGame = new App();
+baseballGame.play();
 
 module.exports = App;
