f9106224d25eea52f0807543a5c5c982c5c927fe
evencoding <evencoding@gmail.com>
2022-11-05 19:33:06 +0900

    refactor: 상수를 담은 오브젝트들을 constants 파일로 분리

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
 const { Console, Random } = require('@woowacourse/mission-utils');
-
-const GAME_VALUE = {
-  restart: '1',
-};
-
-const GAME_MESSAGE = {
-  startNotification: '숫자 야구 게임을 시작합니다.',
-  input: '숫자를 입력해주세요 : ',
-  gameOver:
-    '3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요',
-  exit: '게임을 종료합니다.',
-};
-
-const ERROR_MESSAGE = {
-  duplicate: '중복 된 숫자를 입력할 수 없습니다',
-  invalidValue: '1~9까지의 숫자 중 세개의 숫자를 입력해주세요',
-  invalidRestartValue: '1과 2 중 하나의 숫자를 선택해야 합니다.',
-};
+const {
+  GAME_VALUE,
+  GAME_MESSAGE,
+  ERROR_MESSAGE,
+} = require('./assets/constants');
 
 const isValidRestartInputValue = (inputValue) => {
   if (!['1', '2'].includes(inputValue)) {
