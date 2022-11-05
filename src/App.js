555eeb2e03753cb73580e1f0a2833344102c9450
oing-yong <74910908+oing-yong@users.noreply.github.com>
2022-11-05 13:46:45 +0900

    feat: class App에 printBallCount(ballCount)함수 추가
    + console.print로 화면에 볼카운트(ballCount 객체) 출력
    + !출력 형식: 숫자+스트라이크, 숫자+볼, 낫싱 예)1볼 1스트라이크 (출력 후 개행)

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import * as MissionUtils from "@woowacourse/mission-utils";
 class App {
   computerNum() {
     const computerTheeDifferNum = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
-    console.log(computerTheeDifferNum);
+    return computerTheeDifferNum;
   }
 
   userNum() {
@@ -32,11 +32,11 @@ class App {
     };
     const userNum = userNum.toString();
     const computerNum = computerNum.toString();
-    for(let index=0; index<3; index++){
-      if(userNum[index] === computerNum[index]){
+    for (let index = 0; index < 3; index++) {
+      if (userNum[index] === computerNum[index]) {
         ballCount.strike++;
         continue;
-      }else if(computerNum.match(userNum[index]) === userNum[index]){
+      } else if (computerNum.match(userNum[index]) === userNum[index]) {
         ballCount.ball++;
         continue;
       }
@@ -45,9 +45,25 @@ class App {
 
     return ballCount;
   }
-  
+
+  printBallCount(ballCount) {
+    if (ballCount.strike > 0) {
+      console.print('${ballCount.strike}스트라이크');
+    }
+    if (ballCount.ball > 0) {
+      console.print('${ballCount.ball}볼');
+    }
+    if (ballCount.nothing === 3){
+      console.print('낫싱');
+    }
+    console.print('\n');
+  }
+
   play() {
-    this.computerNum();
+    const computerNum = this.computerNum();
+    let userNum = this.userNum();
+    let ballCount = this.compareNum(userNum, computerNum);
+
   }
 }
 
