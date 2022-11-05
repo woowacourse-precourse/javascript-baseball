282c043c845887189248bae4410fe950d2773074
yhnb3 <60080270+yhnb3@users.noreply.github.com>
2022-11-05 20:18:18 +0900

    Feat: make checkBallInputValidation fnc

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -49,7 +49,7 @@ describe("숫자 야구 게임", () => {
 
   test("예외 테스트", () => {
     const randoms = [1, 3, 5];
-    const answers = ["1234"];
+    const answers = ["aa"];
 
     mockRandoms(randoms);
     mockQuestions(answers);
--- src/App.js
+++ src/App.js
@@ -4,43 +4,57 @@ const makeBallCountString = require("./libs/makeBallCountString");
 const getRandomThreeNumber = require("./libs/getRandomThreeNumber");
 
 class App {
-  RESTART = "1";
-  GAMEOVER = "2";
   constructor() {
-    this.iterator = this.iterator.bind(this);
+    this.inning = this.inning.bind(this);
     this.play = this.play.bind(this);
     this.computer;
   }
   computerNumber() {
     return getRandomThreeNumber();
   }
-  checkRestartInputValidation(number) {
-    if (!number === RESTART || !number === GAMEOVER)
-      throw new Error("잘못 입력");
+  checkRestartInputValidation(input) {
+    const RESTART = "1";
+    const GAMEOVER = "2";
+    if (!input === RESTART || !input === GAMEOVER)
+      throw new Error("1 또는 2를 입력해야합니다.");
+  }
+  isNotNumber(number) {
+    return !(number >= 1 && number <= 9);
+  }
+  checkBallInputValidation(input) {
+    const inputArr = input.split("");
+    if (input.length !== 3) throw new Error("3자리 수를 입력해야합니다.");
+    if (inputArr.some(this.isNotNumber))
+      throw new Error("숫자만 입력해야합니다.");
   }
   askRestart() {
-    Console.readLine("다시 시작 1 종료 2 :  ", (res) => {
-      this.checkRestartInputValidation(number);
-      if (res === "2") return Console.close();
+    const GAMEOVER = "2";
+    Console.readLine("다시 시작 1 종료 2 :  ", (input) => {
+      this.checkRestartInputValidation(input);
+      if (input === GAMEOVER) return Console.close();
       this.computer = getRandomThreeNumber();
-      this.iterator();
+      this.inning();
     });
   }
-  iterator() {
-    Console.readLine("되나?   ", (res) => {
-      if (res.length > 3) throw new Error();
-      const ballCount = makeBallCountString(this.computer, res);
-      Console.print(ballCount);
-      if (ballCount == "3스트라이크") {
-        Console.print("게임 종료");
-        this.askRestart();
-      }
-      this.iterator();
+  checkBallCount(number) {
+    this.checkBallInputValidation(number);
+    const ballCount = makeBallCountString(this.computer, number);
+    Console.print(ballCount);
+    if (ballCount == "3스트라이크") {
+      Console.print("게임 종료");
+      this.askRestart();
+    }
+  }
+  inning() {
+    Console.readLine("되나?   ", (input) => {
+      if (input.length > 3) throw new Error();
+      this.checkBallCount(input);
+      this.inning();
     });
   }
   play() {
     this.computer = this.computerNumber();
-    this.iterator();
+    this.inning();
   }
 }
 
