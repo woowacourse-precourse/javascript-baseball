889fdbcbfdaf5c43e7d8b1d1b603e7e3150b0725
gsong <rmsdlf1206@gmail.com>
2022-11-05 15:44:51 +0900

    feat: 전역 MESSAGES data  작성

--- src/App.js
+++ src/App.js
@@ -1,3 +1,11 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+const MESSAGES = {
+  start: "숫자 야구 게임을 시작합니다.",
+  end: "숫자 야구 게임을 종료합니다.",
+  input: "숫자를 입력해주세요 : ",
+  correct: "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
+  restart: "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
+};
 class App {
   play() {}
 }
