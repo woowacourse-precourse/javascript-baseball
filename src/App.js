2eb72349e9ce4039a56ae2cc78b216e29be2e398
Yoonkyoung Kim <100656920+Yoonkyoungme@users.noreply.github.com>
2022-11-05 13:55:20 +0900

    feat: 컴퓨터 숫자 랜덤으로 생성

--- src/App.js
+++ src/App.js
@@ -4,6 +4,18 @@ class App {
   play() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
   }
+
+  // 컴퓨터의 숫자 랜덤으로 생성한다. (1부터 9까지 서로 다른 수로 이루어진 3자리의 수)
+  createComputerNumber() {
+    const computer = [];
+
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+  }
 }
 
 module.exports = App;
