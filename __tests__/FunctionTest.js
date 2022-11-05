8c00dbdfd260a647e84a70b209845f45c78c3e6b
2yunseong <dbsdltjd3701@naver.com>
2022-11-05 22:25:49 +0900

    test: 모두 숫자인지 확인하는 기능 테스트 코드 작성

--- __tests__/FunctionTest.js
+++ __tests__/FunctionTest.js
@@ -26,4 +26,25 @@ describe("기능 테스트", () => {
     const result = app.isVaildLength("1213");
     expect(result).toEqual(false);
   });
+
+  test("입력이 1부터 9까지로 이루어진 유효하면 true 반환", () => {
+    const app = new App();
+    const result = app.isVaildNumberFormat("123");
+    expect(result).toEqual(true);
+  });
+
+  test("입력이 1부터 9까지로 이루어진 유효하지 않으면 false 반환", () => {
+    const app = new App();
+    const case1 = app.isVaildNumberFormat("a123");
+    const case2 = app.isVaildNumberFormat("123a");
+    const case3 = app.isVaildNumberFormat("12");
+    const case4 = app.isVaildNumberFormat("1234");
+    const case5 = app.isVaildNumberFormat(" 123 ");
+
+    expect(case1).toEqual(false);
+    expect(case2).toEqual(false);
+    expect(case3).toEqual(false);
+    expect(case4).toEqual(false);
+    expect(case5).toEqual(false);
+  });
 });
