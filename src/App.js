fe564e1f034825346b07db958ec89d8251b74803
Cobocho <rlaalsrb1111@naver.com>
2022-11-05 22:26:22 +0900

    refactor: 클리어 조건 상수화

--- src/App.js
+++ src/App.js
@@ -5,24 +5,24 @@ const constants = require("./data/constants.js");
 
 class App {
   play() {
-    print.start();
     this.answer = GameUtils.System.answer;
     console.log(this.answer);
+    print.start();
     this.submitInput();
   }
   submitInput() {
     let userInput;
-    MissionUtils.Console.readLine(constants.MESSAGE.INPUT, (input) => {
+    MissionUtils.Console.readLine(constants.GAME_MESSAGE.INPUT, (input) => {
       userInput = input.trim().split('').map(number => +number);
       GameUtils.Validator.isVaildAnswer(userInput);
       const result = GameUtils.System.getResult(userInput, this.answer);
       print.result(result);
-      if(result.strike !== 3) return this.submitInput();
+      if(result.strike !== constants.CLEAR_CONDITION) return this.submitInput();
       this.clearGame();
     });
   }
   clearGame() {   
-    MissionUtils.Console.readLine(constants.MESSAGE.CLEAR, (submit) => {
+    MissionUtils.Console.readLine(constants.GAME_MESSAGE.CLEAR, (submit) => {
       GameUtils.Validator.isVaildRestartSubmit(submit);
       if(+submit === 1) app.play();
       if(+submit === 2) {
--- src/data/constants.js
+++ src/data/constants.js
@@ -1,17 +1,18 @@
-const MESSAGE = {
-  START: '숫자 야구 게임을 시작합니다.',
-  INPUT: '숫자를 입력해주세요 : ',
-  
+const ERROR_MESSAGE = {  
   IS_BLANK: '공백이 아닌 값을 입력해주세요!',
   NOT_THREE_LENGTH: '3글자인 값을 입력해주세요!',
   NOT_NUMBER_RANGE: '1부터 9 사이의 값을 입력해주세요!',
   IS_DUPLICATED: '중복되지 않은 값을 입력하세요!',
 
   IS_INVALID_RESTART_SUBMIT: '1과 2중에서 입력해주세요!',
+};
 
+const GAME_MESSAGE = {  
+  START: '숫자 야구 게임을 시작합니다.',
+  INPUT: '숫자를 입력해주세요 : ',
   CLEAR: `3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.`,
   GAMEOVER: `게임 종료`,
-};
+}
 
 const RESULT_COUNT = {
   STRIKE: '스트라이크',
@@ -19,5 +20,9 @@ const RESULT_COUNT = {
   NOTHING: '낫싱',
 };
 
-exports.MESSAGE = MESSAGE;
+const CLEAR_CONDITION = 3;
+
+exports.ERROR_MESSAGE = ERROR_MESSAGE;
+exports.GAME_MESSAGE = GAME_MESSAGE;
 exports.RESULT_COUNT = RESULT_COUNT;
+exports.CLEAR_CONDITION = CLEAR_CONDITION;
