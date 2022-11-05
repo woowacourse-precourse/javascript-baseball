dcf87280388e4dffe3742ba1a5f554b4a721400e
saram <yujun1018@naver.com>
2022-11-05 22:07:49 +0900

    feat: 1에서 9까지 서로 다른 임의의 수 3개를 만드는 기능

--- src/App.js
+++ src/App.js
@@ -1,4 +1,14 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+
 class App {
+  createNumberList() {
+    const numberList = [];
+    while (numberList.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!numberList.includes(number)) numberList.push(number);
+    }
+    return numberList;
+  }
   play() {}
 }
 
