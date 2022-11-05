62854b69ffe519b52e41e82854a40382c42f7369
seongminn <berry9901@gmail.com>
2022-11-05 13:36:38 +0900

    feat(BaseballGame): 반복해서 사용자의 값을 입력 받는 기능 구현

--- src/game/BaseballGame.js
+++ src/game/BaseballGame.js
@@ -1,18 +1,27 @@
 const { Console } = require('@woowacourse/mission-utils');
 const { createRandomNumber } = require('../computer/computer');
+const inputValidator = require('../input/validator');
 
 class BaseballGame {
   constructor() {
     this.randomNumber = createRandomNumber();
     this.init();
+    this.isCorrect = false;
   }
 
   init() {
     this.printStartMessage();
+    this.startGame();
   }
 
   printStartMessage() {
     Console.print('숫자 야구 게임을 시작합니다.');
+  }
+
+  startGame() {
+    while (!this.isCorrect) {
+      Console.readLine('숫자를 입력해주세요: ', (input) => {});
+    }
   }
 }
 
