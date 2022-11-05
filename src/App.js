1f9c23ec832ad45c4df1c46806426ff061af8f45
Ryong-E <think8867@gmail.com>
2022-11-05 13:49:49 +0900

    feat: outputResult 매서드 생성 숫자 비교 기능
    - 컴퓨터의 숫자와 사용자의 입력값을 비교하여 ball,strike를 계산
    - 리턴값은 scoreConversion함수에 ball,strike를 넣어 ball,strike수에 맞는 결과문을 반환

--- src/App.js
+++ src/App.js
@@ -1,19 +1,46 @@
-const MissionUtils = require("@woowacourse/mission-utils")
+const MissionUtils = require('@woowacourse/mission-utils');
 class App {
-  constructor(){
+  constructor() {
     this.computer = this.getRandomNumbers();
   }
 
-  getRandomNumbers(){
+  getRandomNumbers() {
     const Numbers = [];
-    while(Numbers.length < 3){
-      const number = MissionUtils.Random.pickNumberInRange(1,9);
-      if(!Numbers.includes(number)){
-        Numbers.push(number)
+    while (Numbers.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!Numbers.includes(number)) {
+        Numbers.push(number);
       }
     }
 
-    return Numbers
+    return Numbers;
+  }
+
+  outputResult(answer) {
+    let strike = 0;
+    let ball = 0;
+
+    for (let number = 0; number < answer.length; number++) {
+      if (String(this.computer[number]) === answer[number]) {
+        strike += 1;
+      }
+      if (String(this.computer[number]) !== answer[number] && String(this.computer).includes(answer[number])) {
+        ball += 1;
+      }
+    }
+
+    return this.scoreConversion(strike, ball);
+  }
+
+  scoreConversion(strike, ball) {
+    if (strike == 0 && ball == 0) {
+      return '낫싱';
+    }
+    if (strike === 3) {
+      return '3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료';
+    }
+
+    return `${ball}볼 ${strike}스트라이크`;
   }
 
   play() {}
