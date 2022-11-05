36cb27888835f93e9e60b0e889f7a64ded6ce75b
pakxe <pigkill40@naver.com>
2022-11-05 18:54:50 +0900

    feat: 숫자 배열이 유효한 범위 내의 숫자로 구성돼 있는지 확인하는 함수

--- src/Validation.js
+++ src/Validation.js
@@ -2,3 +2,14 @@ const { ERROR, ANSWER } = require('./Constants');
 const { generateNumArr } = require('./Utils');
 
 const validLength = (num) => num.length === ANSWER.LENGTH;
+
+const validRange = (num) => {
+  const rangeArr = generateNumArr(ANSWER.MIN, ANSWER.MAX);
+
+  return num.reduce((result, number) => {
+    if (!rangeArr.includes(number)) result = false;
+    return result;
+  }, true);
+};
+
+module.exports = { validLength, validRange };
