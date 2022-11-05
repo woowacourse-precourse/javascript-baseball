98c5470ec5775ca78547648e5915fd0455c10089
hae-on <solbi2004@naver.com>
2022-11-05 23:55:27 +0900

    feat: 초기 환경 설정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+const { Console, Random } = MissionUtils;
+
 class App {
   play() {}
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
