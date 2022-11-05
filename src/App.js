dc830cd6e9e970a86a1823002b60fd4a700ba369
GC-Park <kuyt3122@naver.com>
2022-11-05 17:16:25 +0900

    feat:게임 종료 후 다시 시작 및 종료하기 기능 추가

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ class App {
     play() {
         this.computerInput = this.randomMakeNumber()
         this.userInputNumber()
+        return;
     }
 
     randomMakeNumber() {
@@ -49,7 +50,8 @@ class App {
         MissionUtils.Console.print("낫싱");
         this.userInputNumber();
       } else if(strikes===3){
-        MissionUtils.Console.print("3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        MissionUtils.Console.print("3스트라이크");
+        this.finishAndRestart()
       }else if(strikes===0){
         MissionUtils.Console.print(balls+"볼");
         this.userInputNumber();
@@ -66,6 +68,16 @@ class App {
         MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (answer) => {
             this.compareNumber(answer.split(''))
         })
+    }
+
+    finishAndRestart(){
+      MissionUtils.Console.readLine('3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n', (answer) => {
+        if(Number(answer)===1) this.play();
+        else if(Number(answer)===2) {
+          MissionUtils.Console.print("게임 종료");
+          MissionUtils.Console.close();
+        }
+      })
     }
 }
 
