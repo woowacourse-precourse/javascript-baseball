0a852c38cf44c2239f872afbf6291a575a102d84
HWI <dlwognl99@naver.com>
2022-11-05 23:58:22 +0900

    feat: 컴퓨터 랜덤 값 생성 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  play() {
+    // 컴퓨터 랜덤 숫자 생성
+    const computerNumber = RandomChoice();
+    MissionUtils.Console.print(computerNumber);
+    MissionUtils.Console.close();
+  }
 }
+
+const RandomChoice = () => {
+  const randomNumber = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
+  return randomNumber;
+};
 
 module.exports = App;
