e9018dc23d1b4b0c072311ac41c9ac0009faf3d9
Ryong-E <think8867@gmail.com>
2022-11-05 19:02:33 +0900

    refactor: outputResult 매서드 for문 forEach로 수정

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App {
   }
 
   guessAnswer() {
-    MissionUtils.Console.readLine('숫자를 입력하세요 : ', answer => {
+    MissionUtils.Console.readLine('숫자를 입력하세요 : ', (answer) => {
       this.guessAnswerValidate(answer);
       if (this.outputResult(answer) === '3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료') {
         MissionUtils.Console.print(this.outputResult(answer));
@@ -37,21 +37,26 @@ class App {
     if (answer.length !== duplicate.size) {
       throw '서로다른 3자리 숫자를 입력해주세요';
     }
-    if (!answer.split('').every(number => number.charCodeAt() >= 49 && number.charCodeAt() <= 57)) {
+    if (
+      !answer.split('').every((number) => number.charCodeAt() >= 49 && number.charCodeAt() <= 57)
+    ) {
       throw '범위에 벗어난 숫자입니다';
     }
   }
 
   restartEndGameAnswer() {
-    MissionUtils.Console.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요\n', answer => {
-      this.restartEndGameAnswerValidator(answer);
-      if (answer === '1') {
-        this.play();
+    MissionUtils.Console.readLine(
+      '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요\n',
+      (answer) => {
+        this.restartEndGameAnswerValidator(answer);
+        if (answer === '1') {
+          this.play();
+          return;
+        }
+        MissionUtils.Console.close();
         return;
       }
-      MissionUtils.Console.close();
-      return;
-    });
+    );
   }
 
   restartEndGameAnswerValidator(answer) {
@@ -67,14 +72,15 @@ class App {
     let strike = 0;
     let ball = 0;
 
-    for (let number = 0; number < answer.length; number++) {
-      if (String(this.computer[number]) === answer[number]) {
+    answer.split('').forEach((number, index) => {
+      const computer = String(this.computer[index]);
+      if (computer === number) {
         strike += 1;
       }
-      if (String(this.computer[number]) !== answer[number] && String(this.computer).includes(answer[number])) {
+      if (computer !== number && String(this.computer).includes(number)) {
         ball += 1;
       }
-    }
+    });
 
     return this.scoreConversion(strike, ball);
   }
