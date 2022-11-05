d37326560ddac865705e2c094e2f496b4174402e
Dohyeon Lee <dlehgus1110@naver.com>
2022-11-05 21:55:04 +0900

    feat: 게임 재시작 입력 에러 메세지 상수 추가

--- src/constants/message.js
+++ src/constants/message.js
@@ -15,11 +15,13 @@ const INVALID_INPUT_TYPE = "입력에 숫자 이외의 문자가 있습니다. �
 const INVALID_INPUT_LENGTH = "숫자의 길이가 올바르지 않습니다. 게임 종료!";
 const DUPLICATED_NUMBER = "입력에 중복된 숫자가 있습니다. 게임 종료!";
 const INVALID_INPUT_RANGE = "입력 가능한 숫자 범위를 벗어났습니다. 게임 종료!";
+const INVALID_TRIGGER = "유효하지 않은 입력입니다. 게임 종료!";
 const ERROR_MESSAGE = {
   INVALID_INPUT_TYPE,
   INVALID_INPUT_LENGTH,
   DUPLICATED_NUMBER,
   INVALID_INPUT_RANGE,
+  INVALID_TRIGGER,
 };
 
 module.exports = {
