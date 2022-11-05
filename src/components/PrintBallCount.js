8193b8827cb65b08312c685d83e3db65a633aa99
JayPark0110 <90092440+JayPark0110@users.noreply.github.com>
2022-11-05 23:05:35 +0900

    refactor: if문 사용 최소화 리팩토링

--- src/components/PrintBallCount.js
+++ src/components/PrintBallCount.js
@@ -4,15 +4,12 @@ const PrintBallCount = (ball, strike) => {
   if (ball === 0 && strike > 0)
     MissionUtils.Console.print(`${strike}스트라이크`);
   if (strike === 0 && ball > 0) MissionUtils.Console.print(`${ball}볼`);
-  if (strike > 0 && ball > 0)
-    MissionUtils.Console.print(`${ball}볼 ${strike}스트라이크`);
-  if (ball === 0 && strike === 0) MissionUtils.Console.print("낫싱");
 
-  // ball === 0 && strike === 0
-  //   ? MissionUtils.Console.print("낫싱")
-  //   : ball !== 0 && strike !== 0
-  //   ? MissionUtils.Console.print(`${ball}볼 ${strike}스트라이크`)
-  //   : "";
+  ball === 0 && strike === 0
+    ? MissionUtils.Console.print("낫싱")
+    : ball !== 0 && strike !== 0
+    ? MissionUtils.Console.print(`${ball}볼 ${strike}스트라이크`)
+    : null;
 };
 
 module.exports = PrintBallCount;
