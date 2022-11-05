66d8e6873476b8f6c8a87907b28f03a8971b901b
imhynni <omelas329@gmail.com>
2022-11-05 21:42:14 +0900

    feat: 사용자로부터 숫자 3개 입력 받기

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
+  #userInput;
   #answer;
 
   pickNumber() {
@@ -14,10 +15,22 @@ class App {
     return computer;
   }
 
+  checkInput(input) {}
+
+  getUserInput() {
+    MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (input) => {
+      this.#userInput = input;
+    });
+  }
+
   play() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
-    this.answer = this.pickNumber();
+    this.#answer = this.pickNumber();
+    this.getUserInput();
   }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
