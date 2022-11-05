f37bb6a71cfb037caa0c6c123fdcb1eadbc501e7
violetwhenisawu <anashape710@gmail.com>
2022-11-05 15:31:27 +0900

    Feat(temporary): 게임 종료 시, 게임 재시작 / 종료를 입력받는 함수 추가

--- src/temporaryCode.js
+++ src/temporaryCode.js
@@ -53,6 +53,21 @@ class temporaryCode {
     return strike;
   }
 
+  restartGame(userSelect) {
+    const userSelectNum = Number(userSelect);
+    if (userSelectNum === 1) return; // 1이면, 새 게임 시작
+    if (userSelectNum === 2) Console.close(); // 2면, 입력 받는것을 종료
+  }
+
+  isGameOver() {
+    Console.readLine(
+      '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n',
+      (userSelect) => {
+        this.restartGame(userSelect);
+      }
+    );
+  }
+
   getGameHint(userInputNum, computerNum) {
     const [ball, strike] = [
       this.BallCounter(userInputNum, computerNum),
@@ -67,6 +82,7 @@ class temporaryCode {
     if (strike === 3) {
       Console.print(`${strike}스트라이크`);
       Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+      this.isGameOver();
     }
     return;
   }
