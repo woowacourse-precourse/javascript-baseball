fca2680b03db3511e8c53b36370a88f556d29d6d
userHWSEO <gmldnjseocnd@gmail.com>
2022-11-05 15:26:13 +0900

    feat: 기능 요구 사항 추가
    - 스트라이크와 볼의 카운트를 세어주는 checkCount 함수 생성.
    
    - 사용자가 3자리 숫자가 아니거나 숫자가 아닌 다른 값을 넣을 때 에러 처리 적용.

--- src/App.js
+++ src/App.js
@@ -9,6 +9,30 @@ class App {
       }
       return random_number;
     }
+
+    function checkCount(random_number, user_number) {
+      let strike_count = 0;
+      let ball_count = 0;
+      const str_user_num = user_number.toString();
+      const str_random_num = random_number.toString();
+      if (str_user_num.length !== 3) {
+        throw new Error("세 자리 숫자를 입력해주세요.");
+      }
+      if (typeof user_number !== "number") {
+        throw new Error("숫자를 입력해주세요.");
+      }
+      for (let i = 0; i < str_random_num.length; i++) {
+        if (str_random_num[i] === str_user_num[i]) {
+          strike_count++;
+        } else if (str_user_num.indexOf(str_random_num[i]) !== -1) {
+          ball_count++;
+        }
+      }
+      if (strike_count === 0 && ball_count === 0) {
+        return "낫싱";
+      }
+      return [strike_count, ball_count];
+    }
   }
 }
 
