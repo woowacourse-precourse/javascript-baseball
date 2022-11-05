baf3fbfb6424e86a2d961b1575e57a683722937b
jinseo <ghkdwlstj99@gmail.com>
2022-11-05 12:41:55 +0900

    refactor: CreateNumber class 함수 구현

--- src/CreateNumber.js
+++ src/CreateNumber.js
@@ -1,13 +1,16 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
-function pickedComputerNumber() {
+class CreateNumber {
+ pickedComputerNumber() {
     let pickNumber = [];
     while (pickNumber.length < 3) {
-      const computerNumber = MissionUtils.Random.pickNumberInRange(1, 9);
+      let computerNumber = MissionUtils.Random.pickNumberInRange(1, 9);
       if (!pickNumber.includes(computerNumber)){
         pickNumber.push(computerNumber);
     }
   }
   return pickNumber.join("");
+  }
 }
-module.exports = pickedComputerNumber;
+
+module.exports = CreateNumber;
--- src/GameStart.js
+++ src/GameStart.js
@@ -1,5 +1,5 @@
 const { MissionUtils } = require("@woowacourse/mission-utils");
-
+ 
 function gameStart(answer){
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
     threeDigitsError(answer);
