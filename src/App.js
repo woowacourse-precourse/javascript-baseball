f214c19f6d1a956dfe6c9d3e63390aa27e318675
chex <chex417@naver.com>
2022-11-05 22:16:40 +0900

    feat: 게임시작 문구를 출력하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  static printGameStart(){
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+  }
+
+  play() {
+    App.printGameStart();
+  }
 }
 
 module.exports = App;
