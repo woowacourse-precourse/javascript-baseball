984fa02c7a617ded95001e1afb3edc4d3fcc1380
oing-yong <74910908+oing-yong@users.noreply.github.com>
2022-11-05 10:04:59 +0900

    feat: class App에 userNum(), isThreeDifferNum(number) 객체 추가
    + MissionUtils 라이브러리의 Console API를 이용해 숫자를 입력 받아 변수에 저장
    + !예외처리1 호출 후 true가 반환되면 플레이어 입력값을 MissionUtils 라이브러리의 Console API를 이용해 화면 출력(출력 후 개행), false 반환되면 게임 종료
    + !예외처리1 : 입력받은 값이 서로 다른 세 자리 숫자라면 true 반환, 아니라면 false 반환

--- src/App.js
+++ src/App.js
@@ -6,7 +6,25 @@ class App {
   }
 
   computerNum() {
-    const computerTheeDifferNum = MissionUtils.pickUniqueNumbersInRange(1, 9, 3);
+    const computerTheeDifferNum = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
+  }
+
+  userNum() {
+    MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (answer) => {
+      answer = Number(answer);
+      if (isThreeDifferNum(answer)) {
+        console.log('${answer}\n');
+        return answer;
+      }
+      gameEixt();
+    })
+  }
+
+  isThreeDifferNum(number) {
+    const numArr = Array.from(number);
+    const set = new Set(numArr);
+
+    return (number != NaN) && (number.length === 3) && (number.lenth === set.size) ? true : false;
   }
 
   play() {}
