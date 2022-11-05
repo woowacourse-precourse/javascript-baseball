979a3c51b2e38266497c32e4fb4fbfd94a595024
chex <chex417@naver.com>
2022-11-06 08:13:54 +0900

    feat: 숫자인지 판별하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
-  static printGameStart(){
+  static printGameStart() {
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
   }
 
+  static isNumber(str) {
+    const NUMBER_REGEXP = /^[0-9]+$/;
+    const PREFIX_ZERO_REGEXP = /^[0]+[0-9]+$/;
+    if (PREFIX_ZERO_REGEXP.test(str) || !NUMBER_REGEXP.test(str)) {
+      return false;
+    }
+    return true;
+  }
+  
   play() {
     App.printGameStart();
   }
