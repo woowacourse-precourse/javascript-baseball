d1cd82f235a51089bda7ec6f17ea5cb920847990
hwanfront <kim015jh@gmail.com>
2022-11-05 19:42:13 +0900

    fix(createRandomNumbers): use pickNumberInRange method
    Changed method for ApplicationTest:
    - before use Random.pickUniqueNumbersInRange
    - now use Random.pickNumberInRange

--- src/utils/createRandomNumbers.js
+++ src/utils/createRandomNumbers.js
@@ -1,10 +1,15 @@
 const { Random } = require('@woowacourse/mission-utils');
 const { NUMBERS_RULES } = require('../static/constants');
 
-const createRandomNumbers = () => Random.pickUniqueNumbersInRange(
-  NUMBERS_RULES.minOfRange,
-  NUMBERS_RULES.maxOfRange,
-  NUMBERS_RULES.digit,
-);
+const createRandomNumbers = () => {
+  const numbers = new Set();
+
+  while (numbers.size < NUMBERS_RULES.digit) {
+    const number = Random.pickNumberInRange(NUMBERS_RULES.minOfRange, NUMBERS_RULES.maxOfRange);
+    numbers.add(number);
+  }
+
+  return [...numbers];
+};
 
 module.exports = createRandomNumbers;
