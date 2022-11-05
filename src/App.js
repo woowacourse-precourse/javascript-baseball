8c477f9befe1db12bd4fa7e4529a63a81fe642ab
wontae chang <cwt629@naver.com>
2022-11-05 13:49:23 +0900

    feat: generate random answer

--- src/App.js
+++ src/App.js
@@ -4,10 +4,21 @@ class App {
   play() {
 
   }
+
+  // 정답이 될 무작위 난수를 배열로서 생성하는 함수
+  generateRandomAnswer() {
+    const ANSWER = [];
+
+    while (ANSWER.length < 3) {
+      const RANDOM_NUM = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!ANSWER.includes(RANDOM_NUM))
+        ANSWER.push(RANDOM_NUM);
+    }
+
+    return ANSWER;
+  }
 }
 
 const app = new App();
-app.play();
-
 
 module.exports = App;
