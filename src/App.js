c653b964b281546939a1b5d30a57930936a919e1
ilgon0110 <moses011010@gmail.com>
2022-11-05 17:31:45 +0900

    feat: 상대방(Computer)의 랜덤 숫자 생성 기능 구현 (#1)
    상대방(Computer)의 Random한 숫자를 생성하는 메서드 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+
+const { Random } = MissionUtils;
+
 class App {
+  constructor() {
+    this.computerNum = [];
+  }
   play() {}
+
+  createComputerNum() {
+    const randomNumberSet = new Set();
+    while (randomNumberSet.size < 3) {
+      randomNumberSet.add(Random.pickNumberInRange(1, 9));
+    }
+    this.computerNum = [...randomNumberSet];
+    return this.computerNum.join('');
+  }
 }
 
 module.exports = App;
