131d223c613ed98ce0e600032625d3c3af6877bb
Sungwk <73461032+Sungwk@users.noreply.github.com>
2022-11-05 16:11:28 +0900

    feat : 4. 입력받은 값이 적절한지 검사 후 부적절하면 throw

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ class App {
   play() {
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
     this.computerNumber = this.getComputerNumber();
-    this.inputNumber = this.getInputNumber();
+    this.getInputNumber();
   }
   getComputerNumber() {
     const COMPUTER = [];
@@ -17,8 +17,38 @@ class App {
   }
   getInputNumber() {
     MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (inputNumber) => {
-      return inputNumber;
+      this.checkInputNumber(inputNumber);
     });
+  }
+  isInputLengthRight(input) {
+    if (input.length !== 3) throw '세 자리를 입력하세요.';
+  }
+  //입력한 숫자가 모두 숫자인지 확인하는 함수
+  isInputNumber(input) {
+    if (isNaN(parseInt(input))) {
+      throw '숫자를 입력하세요.';
+    }
+  }
+  //입력한 숫자에 0이 포함되는지 확인하는 함수
+  isInputIncludeZero(input) {
+    for (let i = 0; i < input.length; i++) {
+      if (parseInt(input[i]) === 0) {
+        throw '0을 포함할 수 없습니다.';
+      }
+    }
+  }
+  //입력한 숫자가 모두 다른 숫자인지 확인하는 함수
+  isInputDifferentNumber(input) {
+    const check = new Set(input);
+    if (check.size !== input.length) {
+      throw '서로 다른 숫자를 입력하세요.';
+    }
+  }
+  checkInputNumber(input) {
+    this.isInputNumber(input);
+    this.isInputLengthRight(input);
+    this.isInputIncludeZero(input);
+    this.isInputDifferentNumber(input);
   }
 }
 
