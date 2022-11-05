d5bb9baef08837adc20be894b96488425def6ecd
Jaehyeon <jaehyeonp74@gmail.com>
2022-11-05 17:41:18 +0900

    feat: 야구게임 결과를 반환하는 함수 구현

--- src/compare.js
+++ src/compare.js
@@ -19,6 +19,14 @@ function compareArrResult(comArr, userArr) {
     let strikeOrBall = compareDigit(elem, index, userArr);
     result[strikeOrBall] += 1;
   });
+
+  let strikeCount = result['스트라이크'];
+  let ballCount = result['볼'];
+
+  if (ballCount > 0 && strikeCount > 0) return `${ballCount}볼 ${strikeCount}스트라이크`;
+  if (ballCount === 0 && strikeCount > 0) return `${strikeCount}스트라이크`;
+  if (ballCount > 0 && strikeCount === 0) return `${ballCount}볼`;
+  return '낫싱';
 }
 
 module.exports = compareArrResult;
