a33b9053f58bc72b95f3b03dd239405485c6e2cf
Choejeonghun <saa00123@naver.com>
2022-11-05 18:17:08 +0900

    feat: 2번 기능 구현

--- src/App.js
+++ src/App.js
@@ -8,6 +8,15 @@ class App {
         computer.push(number);
       }
     }
+
+    var strike = 0;
+    var ball = 0;
+
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+    MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (question) => {
+      MissionUtils.Console.print(question);
+      MissionUtils.Console.close();
+    });
   }
 }
 
