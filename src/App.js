28559bf6b43c148fc462142338c7f707f8fcd993
imb96 <kimminje7267@naver.com>
2022-11-05 14:58:53 +0900

    feat : 게임이 시작되고 플레이어에게 입력을 받는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const Computer = require("./Computer");
+const IsValid = require("./IsValid");
+const Compare = require("./Compare");
 class App {
-  play() {}
+  constructor() {
+    this.CORRECT = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    this.computer = new Computer().computerNumber();
+    this.isValid = new IsValid();
+    this.compare = new Compare();
+  }
+  play() {
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    this.start();
+  }
+  start() {
+    const computer = this.computer;
+    let user = [];
+    let result = "";
+    MissionUtils.Console.readLine("숫자를 입력해주세요: ", (answer) => {
+      if (this.isValid.isValid(answer) === true) {
+        answer.split("").forEach((x) => user.push(+x));
+        result = this.compare.compare(computer, user);
+        MissionUtils.Console.print(result);
+      }
+    });
+  }
 }
-
 module.exports = App;
