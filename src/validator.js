e069c5ba1436084beff43d346809b942524d6c2c
Jaehyeon <jaehyeonp74@gmail.com>
2022-11-06 02:35:13 +0900

    refactor: 세 자리 수 입력값 예외처리 메세지 수정

--- src/validator.js
+++ src/validator.js
@@ -14,9 +14,22 @@ function isAllDifferent(str) {
 }
 
 function isInputValidate(str) {
-  if (!isLengthEqualsThree(str) || !isAllDifferent(str) || !isNumInRange(str)) {
-    throw new Error('1부터 9까지 서로 다른 수로 이루어진 3자리 수를 입력해주세요!');
+  if (!isLengthEqualsThree(str)) {
+    throw new Error(
+      '입력한 값이 3자리가 아니에요! 1부터 9까지 서로 다른 수로 이루어진 3자리 수를 입력해주세요!',
+    );
   }
+  if (!isAllDifferent(str)) {
+    throw new Error(
+      '서로 다른 수가 아니에요! 1부터 9까지 서로 다른 수로 이루어진 3자리 수를 입력해주세요!',
+    );
+  }
+  if (!isNumInRange(str)) {
+    throw new Error(
+      '각 자리 수 중 1부터 9로 이루어지지 않은 수가 있어요! 1부터 9까지 서로 다른 수로 이루어진 3자리 수를 입력해주세요!',
+    );
+  }
+
   return true;
 }
 
