5b3a125d0bb09f72db99b7466ee22a570a498d63
ilgon0110 <moses011010@gmail.com>
2022-11-05 19:12:19 +0900

    test: 예외처리 1-2 테스트 완료 (#1)
    각 숫자의 범위가 1~9인지 아닌 지 확인하는 테스트

--- __tests__/featureTest.js
+++ __tests__/featureTest.js
@@ -1,11 +1,15 @@
 const App = require('../src/App');
+const app = new App();
 
 describe('상대방(Computer)의 숫자 테스트', () => {
   test('1-1. 숫자가 아닌 경우', () => {
-    const app = new App();
     app
       .createComputerNum()
-      .split('')
       .forEach((num) => expect(Number.isNaN(num)).toBe(false));
+  });
+  test('1-2. 각 숫자가 1~9 범위가 아닌 경우', () => {
+    app
+      .createComputerNum()
+      .forEach((num) => expect(num >= 1 && num <= 9).toBe(true));
   });
 });
