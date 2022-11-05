319fd354e15692e5feb8dd47d5124577c20e704d
cjm0611 <81799492+cjm0611@users.noreply.github.com>
2022-11-05 09:23:29 +0900

    feat: 볼, 스트라이크 개수, 낫싱 표시 (기능 3번)

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ const MissionUtils = require("@woowacourse/mission-utils");
 class App {
   play() {
     const computer = this.getComputerRandomNumberString();
+
     this.numberBaseballGame(computer);
   }
 
   getComputerRandomNumberString() {
     let randomNumberList = [];
+
     while (randomNumberList.length < 3) {
       const number = MissionUtils.Random.pickNumberInRange(1, 9);
       if (!randomNumberList.includes(number)) {
         randomNumberList.push(number);
       }
     }
+
     return randomNumberList.join('');
   }
 
@@ -23,9 +26,40 @@ class App {
         MissionUtils.Console.close();
       }
       else{
+        this.showBallStrike(computer, user);
         this.numberBaseballGame(computer);
       }
     });
+  }
+
+  showBallStrike(computer, user){
+    let ball = 0;
+    let strike = 0;
+    [...computer].forEach((computerNumber, computerIndex)=>{
+      [...user].forEach((userNumber, userIndex)=>{
+        if(computerNumber === userNumber){
+          if(computerIndex === userIndex){
+            strike++;
+          }
+          else{
+            ball++;
+          }
+        }
+      });
+    });
+
+    if(!ball && !strike){
+      MissionUtils.Console.print("낫싱");
+    }
+    else if(!strike){
+      MissionUtils.Console.print(ball + "볼");
+    }
+    else if(!ball){
+      MissionUtils.Console.print(strike + "스트라이크");
+    }
+    else{
+      MissionUtils.Console.print(ball + "볼 " + strike + "스트라이크");
+    }
   }
 }
 
