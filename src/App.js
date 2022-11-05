2b9f7fa54b353be1898205c1292c573ad2ca6e5b
Hyun Su <tlsgustn93@naver.com>
2022-11-05 12:50:38 +0900

    feat:threePitching require

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+const { threePitching } = require('./modules/generateNumbers')
+
 class App {
-  play() {}
+  play() {
+    threePitching();
+  }
 }
 
+const app = new App()
+app.play()
+console.log(threePitching())
 module.exports = App;
