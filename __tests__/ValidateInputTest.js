52d492ef36a11bafbdd7d877668c1abd9210f9df
woo-jk <201702042@o.cnu.ac.kr>
2022-11-06 00:04:16 +0900

    test: throw 예외 처리 테스트 코드 작성

--- __tests__/ValidateInputTest.js
+++ __tests__/ValidateInputTest.js
@@ -1,8 +1,17 @@
 const App = require("../src/App");
 const MissionUtils = require("@woowacourse/mission-utils");
 
+const mockQuestions = (answers) => {
+  MissionUtils.Console.readLine = jest.fn();
+  answers.reduce((acc, input) => {
+    return acc.mockImplementationOnce((question, callback) => {
+      callback(input);
+    });
+  }, MissionUtils.Console.readLine);
+};
+
 describe.only("플레이어 입력 검증 테스트", () => {
-  test("정상 입력 확인", () => {
+  test("입력 검증 함수 정상 입력", () => {
     const inputs = ["123", "231", "451", "643"];
 
     const app = new App();
@@ -13,7 +22,7 @@ describe.only("플레이어 입력 검증 테스트", () => {
     });
   });
 
-  test("잘못된 입력 확인", () => {
+  test("입력 검증 함수 잘못된 입력", () => {
     const inputs = [
       ["123", true],
       ["445", false],
@@ -28,5 +37,16 @@ describe.only("플레이어 입력 검증 테스트", () => {
       const result = app.validateInput(input);
       expect(result).toEqual(answer);
     });
+  });
+
+  test("잘못된 입력시 throw 예외 처리", () => {
+    const answers = ["1234"];
+
+    mockQuestions(answers);
+
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow();
   });
 });
