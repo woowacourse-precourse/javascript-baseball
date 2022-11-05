08f653ebc9db13086d960a94c3c187451a33e0a5
“somm12” <sominan8744@gmail.com>
2022-11-05 11:02:49 +0900

    fix: makeRandomNumber 함수 요구사항에 맞게 재정의

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 const MissionUtils = require("@woowacourse/mission-utils");
+const readLine = MissionUtils.Console.readLine;
+const print = MissionUtils.Console.print;
+const closeConsole = MissionUtils.Console.close;
 class App {
   makeRandomNumber() {
-    return MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
+    let arr = new Set();
+    while (arr.size < 3) {
+      arr.add(MissionUtils.Random.pickNumberInRange(1, 9));
+    }
+    return Array.from(arr);
   }
 
   play() {
-    let randomNumber = this.makeRandomNumber();
+    let randomNumbers = this.makeRandomNumber();
   }
 }
 const app = new App();
