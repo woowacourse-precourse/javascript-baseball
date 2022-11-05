88a2b8fa117461daa367e8210589e84f41ed3c00
bj lee <maplestory1419@naver.com>
2022-11-05 20:40:13 +0900

    3번 기능구현

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
+
   constructor() {
     this.Computer_Number=[]
     this.Input_Number;
+    this.check=0;
   }
   GenerageNumber(){
     for(let i=0;i<3;i++){
       this.Computer_Number.push(MissionUtils.Random.pickNumberInRange(0, 9))
     }
   } 
+ 
   Input(){
-    //let result = str.split(' ').join('');
-    MissionUtils.Console.readLine('번호를 입력해주세요.', (answer) => {
-      this.Input_Number=answer.split(' ').join('').split('');
-    });
+      this.check=new Promise(function(resolve, reject){
+      MissionUtils.Console.readLine('번호를 입력해주세요.', (answer) => {
+        if (answer) {
+          return resolve(answer.split(' ').join('').split(''));
+        }
+      })
+    })
   }
 
   play() {
     this.GenerageNumber()
     this.Input()
+    this.check.then(x=>this.Input_Number=x)
+    
+
   }
+
 
 }
 let N=new App()
 N.play()
+
+
 module.exports = App;
