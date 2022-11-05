1d22e7ee93d1ee82a4a46fc42a14dedc0853416f
jeonbyeongmin <qudals7613@gmail.com>
2022-11-05 11:11:41 +0900

    feat: 컴퓨터가 랜덤 숫자 배열을 얻는 메서드 작성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,49 @@
+// @ts-check
+
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  constructor() {
+    this.computer = [];
+  }
+
+  play() {
+    this.setRandomNumbers();
+  }
+
+  setRandomNumbers() {
+    while (this.computer.length < 3) {
+      const number = this.getRandomNumber();
+      this.pushNumberToComputer(number);
+    }
+  }
+
+  /**
+   *
+   * @returns {number}
+   */
+  getRandomNumber() {
+    return MissionUtils.Random.pickNumberInRange(1, 9);
+  }
+
+  /**
+   *
+   * @param {number} number
+   */
+  pushNumberToComputer(number) {
+    if (this.isValidNumber(number)) {
+      this.computer.push(number);
+    }
+  }
+
+  /**
+   *
+   * @param {number} number
+   * @returns {boolean}
+   */
+  isValidNumber(number) {
+    return !this.computer.includes(number);
+  }
 }
 
 module.exports = App;
