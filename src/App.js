dc44fe7a3e2582dd874f5b44a4147518985c24d1
BullWooStar <ccatal66@naver.com>
2022-11-05 22:56:37 +0900

    feat: 결과 분석 및 알려주기

--- src/App.js
+++ src/App.js
@@ -3,21 +3,57 @@ const MissionUtils = require("@woowacourse/mission-utils");
 let CORRECT_NUMBER = [];
 
 function makeCorrectNumber() {
-  CORRECT_NUMBER = MissionUtils.Random.pickUniqueNumbersInRange(1, 10, 3);
+  CORRECT_NUMBER = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
 }
 
 let ANSWER_NUMBER = [];
+
 function putNumber() {
   MissionUtils.Console.readLine("숫자를 입력해주세요 :", (answer) => {
     ANSWER_NUMBER = answer.split("").map(Number);
-    console.log(ANSWER_NUMBER);
+    comparingNumber();
   });
+}
+
+let STRIKE_NUMBER = [];
+let BALL_NUMBER = [];
+
+function comparingNumber() {
+  if (JSON.stringify(ANSWER_NUMBER) === JSON.stringify(CORRECT_NUMBER)) {
+    MissionUtils.Console.print("3스트라이크");
+    MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    return;
+  }
+  ANSWER_NUMBER.map((el, index) => {
+    if (ANSWER_NUMBER[index] === CORRECT_NUMBER[index]) {
+      STRIKE_NUMBER.push(el);
+    } else if (
+      CORRECT_NUMBER.filter((el) => el !== CORRECT_NUMBER[index]).includes(el)
+    ) {
+      BALL_NUMBER.push(el);
+    }
+  });
+  if (STRIKE_NUMBER.length === 0 && BALL_NUMBER.length === 0) {
+    MissionUtils.Console.print("낫싱");
+  } else if (STRIKE_NUMBER.length !== 0 && BALL_NUMBER.length === 0) {
+    MissionUtils.Console.print(`${STRIKE_NUMBER.length} 스트라이트`);
+  } else if (STRIKE_NUMBER.length == 0 && BALL_NUMBER.length !== 0) {
+    MissionUtils.Console.print(`${BALL_NUMBER.length} 볼`);
+  } else {
+    MissionUtils.Console.print(
+      `${BALL_NUMBER.length} 볼 ${STRIKE_NUMBER.length} 스트라이트`
+    );
+  }
+  STRIKE_NUMBER = [];
+  BALL_NUMBER = [];
+  putNumber();
 }
 
 class App {
   play() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
     makeCorrectNumber();
+    console.log(CORRECT_NUMBER);
     putNumber();
   }
 }
