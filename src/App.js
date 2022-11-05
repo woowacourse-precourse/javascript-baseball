829f06ece6b589c3a213bbc45a6f4c8584024481
somin <smhs12@naver.com>
2022-11-05 16:49:57 +0900

    feat: 게임 재시작 혹은 종료 선택 기능 1. 1을 입력하면 재시작 2. 2를 입력하면 종료 3. 1, 2가 아닌 다른 숫자 입력시 종료

--- src/App.js
+++ src/App.js
@@ -43,6 +43,7 @@ class App {
     const ball = this.countBall(userInput);
     if (strike === 3) {
       this.printEndMessage();
+      this.askRestart();
     } else {
       this.printCompareResult(strike, ball);
       this.printInputMessage();
@@ -53,6 +54,20 @@ class App {
     MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
   }
 
+  askRestart() {
+    MissionUtils.Console.readLine(
+      "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n",
+      (userInput) => {
+        if (userInput === "1") {
+          this.play();
+        } else if (userInput === "2") {
+          MissionUtils.Console.close();
+        } else {
+          MissionUtils.Console.close();
+        }
+      }
+    );
+  }
   countStrike(userInput) {
     let strike = 0;
     for (let i = 0; i < this.computerNumber.length; i++) {
