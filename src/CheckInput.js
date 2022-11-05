dda99577e6352844cb873094f3d52e6e6e93b17d
Dongkeun <hongdongk@naver.com>
2022-11-05 22:53:14 +0900

    예외2: 1~9이외의값 입력하는경우

--- src/CheckInput.js
+++ src/CheckInput.js
@@ -4,10 +4,20 @@ const CheckInput = (input) => {
   if(CheckSize(inputarray)){
     throw "세자리의 수를 입력해주세요!!"
   }
+  else if(CheckValidate(inputarray)){
+    throw "1~9사이의 숫자를입력해주세요!!";
+  }
 }
 
 const CheckSize = (input) => {
   return (input.length !== 3) ? true : false
+};
+
+const CheckValidate = (input) => {
+  const validnumber = [1,2,3,4,5,6,7,8,9];
+    for( let i of input){
+      return (!validnumber.includes(i)) ? true : false
+    }
 };
 
 module.exports = CheckInput;
