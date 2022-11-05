314413c8130f86e816b770bee1c77bed4bed731f
jiyujin <yujin1679@naver.com>
2022-11-06 01:42:13 +0900

    feat: 볼, 스트라이크 개수 결과를 출력하는 gameResult 함수 추가

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ const MissionUtils = require("@woowacourse/mission-utils");
 class App {
   async play() {
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
-    let userInput = await this.userInput('숫자를 입력해주세요: ');
-    let checkUserNumber = this.checkUserInput(userInput);
     let randomNumber = this.setRandomNumber();
     console.log(randomNumber);
-    if (checkUserNumber) {
-      console.log(this.gameResult(checkUserNumber, randomNumber));
+    MissionUtils.Console.print(GAMEOVER);
+    while (true) {
+      let userInput = await this.userInput('숫자를 입력해주세요: ');
+      let checkUserNumber = this.checkUserInput(userInput);
+      MissionUtils.Console.print(userInput);
+      if (checkUserNumber) {
+        this.gameResult(checkUserNumber, randomNumber);
+      }
     }
   }
 
@@ -57,7 +61,21 @@ class App {
   gameResult(userNumbers, randomNumbers) {
     let strikes = this.countStrickes(userNumbers, randomNumbers);
     let balls = this.countBalls(userNumbers, randomNumbers);
-    console.log(`${strikes} strike ${balls} ball`);
+
+    if (strikes == 0 && balls == 0) {
+      MissionUtils.Console.print('낫싱');
+    } else if (strikes == 3) {
+      MissionUtils.Console.print('3스트라이크');
+      MissionUtils.Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+      MissionUtils.Console.print('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.');
+      this.continueOrEnd();
+    } else if (strikes == 0) {
+      MissionUtils.Console.print(`${balls}볼`);
+    } else if (balls == 0) {
+      MissionUtils.Console.print(`${balls}스트라이크`);
+    } else {
+      MissionUtils.Console.print(`${balls}볼 ${strikes}스트라이크`);
+    }
   }
 }
 
