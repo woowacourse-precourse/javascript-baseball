50829bcd08b983f655144c0c6024ee01753014bc
jiho1996 <jhp9270@naver.com>
2022-11-05 17:19:56 +0900

    feat : 사용자의 입력을 통한 사용자 값 생성 구현

--- src/game.js
+++ src/game.js
@@ -4,8 +4,7 @@ const GAME_MESSAGE = require("./constants/message");
 class NumericBaseballGame {
   constructor() {}
 
-  start() {
-    MissionUtils.Console.print(GAME_MESSAGE.NOTIFY_START_MESSAGE);
+  async creat() {
     const getRandomNumber = () => MissionUtils.Random.pickNumberInRange(1, 9);
     const isNumberNotInSpace = (number, array) => !array.includes(number);
     const isNotFull = (computerSpace) => computerSpace.length < 3;
@@ -23,7 +22,23 @@ class NumericBaseballGame {
       return computerNumberSpace;
     };
 
-    this.numberFromComputer = getNumberFromComputer();
+    const getNumberFromUser = async () => {
+      const convertArgsStringToInt = (arg) => Number(arg);
+      const inputStringNumberFromUser = await new Promise((resolve) => {
+        MissionUtils.Console.readLine("숫자를 입력하시오. : ", (answer) => {
+          resolve(answer);
+        });
+      });
+
+      return Array.from([...inputStringNumberFromUser], convertArgsStringToInt);
+    };
+
+    return [getNumberFromComputer(), await getNumberFromUser()];
+  }
+
+  async start() {
+    MissionUtils.Console.print(GAME_MESSAGE.NOTIFY_START_MESSAGE);
+    const [numberFromComputer, numberFromUser] = await this.creat();
   }
 }
 module.exports = NumericBaseballGame;
