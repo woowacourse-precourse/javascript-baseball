541cb91fa250a3c37cfab01cd13301bc34072ce4
Dongkeun <hongdongk@naver.com>
2022-11-05 22:49:34 +0900

    기능3: 세자리수입력받는 함수

---
+++ src/UserInput.js
@@ -0,0 +1,8 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
+const UserInput = () => {
+  MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (input) => {
+  });
+}
+ 
+module.exports = UserInput;
