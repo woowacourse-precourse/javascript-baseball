34bf65ca67557e70e9a41a8003f39b0a19c61370
chiman <rlaclghks123@naver.com>
2022-11-05 21:26:23 +0900

    refactor: 변수명 수정
    1.regexp => regExp
    2. answer => inputNumber

--- src/Validation.js
+++ src/Validation.js
@@ -1,13 +1,13 @@
 class Validation {
-  isValidationSingleDigitNaturalNumber(answer) {
-    const regexp = new RegExp("^[1-9]+$");
-    if (!regexp.test(answer)) {
+  isValidationSingleDigitNaturalNumber(inputNumber) {
+    const regExp = new RegExp("^[1-9]+$");
+    if (!regExp.test(inputNumber)) {
       throw "1에서 9까지의 자연수를 입력해주세요";
     }
   }
 
-  isValidationNumberWithoutDuplicate(answer) {
-    const wordLengthWidhoutDuplicate = new Set([...answer]).size;
+  isValidationNumberWithoutDuplicate(inputNumber) {
+    const wordLengthWidhoutDuplicate = new Set([...inputNumber]).size;
     if (wordLengthWidhoutDuplicate !== 3) {
       throw "서로 다른 3개의 숫자를 입력해주세요";
     }
