351260cf4c8193985d79c790344a359e443d2e5b
yuna Kim <yuna000804@gmail.com>
2022-11-05 23:17:03 +0900

    feat : 사용자가 answer 제출

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 const MissionUtils = require("@woowacourse/mission-utils");
+const { Random, Console } = require("@woowacourse/mission-utils");
 
 class App {
   play() {}
@@ -7,11 +8,18 @@ class App {
 const letComputerInput = function() {
   const computer = [];
   while (computer.length < 3) {
-    const number = MissionUtils.Random.pickNumberInRange(1, 9);
+    const number = Random.pickNumberInRange(1, 9);
     if (!computer.includes(number)) {
       computer.push(number);
     }
   }
+}
+
+const letUserInput = function() {
+  Console.print("숫자 야구 게임을 시작합니다.");
+  Console.readLine('숫자를 입력해주세요 : ', (answer) => {
+    //answer의 예외사항 체크
+  })
 }
 
 module.exports = App;
