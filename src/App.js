c416fed529c3b3a780867626e8eb48105602581d
SeungMiL <tmskqj@naver.com>
2022-11-05 21:18:11 +0900

    feat: MissionUtils 라이브러리를 통해 1~9까지 서로 다른 수 3개 랜덤값 추출 기능 완료

--- src/App.js
+++ src/App.js
@@ -8,6 +8,18 @@ class App {
   }
 }
 
+class RandomNumber {
+  getNumber() {
+    const computer = [];
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      };
+    };
+  };
+};
+
 class BaseBallPlay {
   start() {
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
