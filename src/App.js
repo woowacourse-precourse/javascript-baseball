130265d19e358f75183f6efd63ca31b370c6b2d9
violetwhenisawu <anashape710@gmail.com>
2022-11-06 01:21:00 +0900

    Feat: 사용자 입력 값 유효성 검사 기능 추가 (App.js)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
+const { Console } = require('@woowacourse/mission-utils');
+const inputCheck = require('./Baseball/inputCheck');
+
 class App {
-  play() {}
+  constructor() {
+    this.userInputNum = '';
+  }
+
+  play() {
+    Console.print('숫자 야구 게임을 시작합니다.');
+    this.startGame();
+  }
+
+  startGame() {
+    Console.readLine('숫자를 입력해주세요 : ', (answer) => {
+      this.userInputNum = answer;
+      const IS_VALID_INPUT = inputCheck.checkInputValidation(this.userInputNum);
+
+      if (IS_VALID_INPUT === false) {
+        throw new Error('유효하지 않은 숫자를 입력했습니다.');
+      }
+
+      if (IS_VALID_INPUT) {
+        this.startGame();
+      }
+    });
+  }
 }
+
+const NUM_BASEBALL = new App();
+NUM_BASEBALL.play();
 
 module.exports = App;
---
+++ src/Baseball/inputCheck.js
@@ -0,0 +1,18 @@
+class inputValidation {
+  checkInputValidation(userInputNum) {
+    const SET = new Set(userInputNum);
+    const UNIQUE_ELEMENTS = [...SET];
+
+    const CHECK_NUM_LENGTH = userInputNum.length === 3;
+    const CHECK_IS_NUMBER = !isNaN(userInputNum);
+    const CHECK_NUM_OVERLAP = UNIQUE_ELEMENTS.length === 3;
+
+    if (CHECK_NUM_LENGTH && CHECK_IS_NUMBER && CHECK_NUM_OVERLAP) {
+      return true;
+    }
+    return false;
+  }
+}
+
+const inputCheck = new inputValidation();
+module.exports = inputCheck;
