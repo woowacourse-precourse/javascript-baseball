360783722b62b7a2e0981deaaf984eaf7ca51c07
yaena1223 <yaena1223@dgu.ac.kr>
2022-11-05 23:56:22 +0900

    feat: add fifth function : game restart/end function

--- src/App.js
+++ src/App.js
@@ -4,28 +4,43 @@ class App {
     this.answer = '';
   }
   play() {
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
     this.play_number_baseball_game();
   }
+
+  receive_restart_input() {
+    MissionUtils.Console.readLine(
+      '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. : ',
+      input_num => {
+        if (input_num == 1) {
+          const app = new App();
+          app.play_number_baseball_game();
+        } else if (input_num == 2) {
+          MissionUtils.Console.close();
+        } else throw '잘못된 형식입니다';
+      },
+    );
+  }
+
   play_number_baseball_game() {
-    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
     this.answer = this.computer_random_number();
-    console.log(this.answer);
+    // console.log(this.answer);
     this.receive_input();
   }
 
   check_continue(input, answer) {
     const compare_result = this.compare_and_give_hint(input, this.answer);
+    MissionUtils.Console.print(compare_result);
     if (compare_result != '3스트라이크') {
-      MissionUtils.Console.print(compare_result);
       this.receive_input();
     } else {
       MissionUtils.Console.print("'3개의 숫자를 모두 맞히셨습니다! 게임 종료'");
+      this.receive_restart_input();
       // MissionUtils.Console.close();
     }
   }
 
   receive_input() {
-    // console.log(this.answer);
     MissionUtils.Console.readLine('숫자를 입력해주세요 : ', input_num => {
       const validation = this.check_input_validation(input_num);
       if (!validation) {
