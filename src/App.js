cb63e47d54e495d82c9b0bdce33837cb63ce8db3
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-11-06 00:30:45 +0900

    feat($play): 초기 프로그램 시작 시 문구 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+
 class App {
-  play() {}
+  constructor() {
+    this.message = '숫자 야구 게임을 시작합니다.';
+  }
+
+  play() {
+    MissionUtils.Console.print(this.message);
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
