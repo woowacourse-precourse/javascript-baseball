9d8bce6169a5620a459e1f514300ad668e0b8c15
bassyu <yrkdgus@gmail.com>
2022-11-05 12:10:42 +0900

    feat: 함수 테스트 추가
    - 난수 생성 테스트 추가
    - 볼 스트라이크 계산 테스트 추가
    - 출력 문자열 생성 테스트 추가

---
+++ __tests__/FunctionTest.js
@@ -0,0 +1,29 @@
+const App = require('../src/App');
+
+describe('함수 테스트', () => {
+  const app = new App();
+
+  test('난수 생성 중복 체크', () => {
+    const randomList = app.createRandomList();
+    const set = new Set(randomList);
+
+    expect(set.size).toEqual(3);
+  });
+
+  test('볼 스트라이크 계산', () => {
+    const randomArray = [1, 3, 5];
+    const inputArray = [3, 1, 5];
+    const { ball, strike } = app.calculateCount(randomArray, inputArray);
+
+    expect(ball).toEqual(2);
+    expect(strike).toEqual(1);
+  });
+
+  test('출력 문자열 생성', () => {
+    const ball = 2;
+    const strike = 1;
+    const result = app.createResultString(ball, strike);
+
+    expect(result).toEqual('2볼 1스트라이크');
+  });
+});
