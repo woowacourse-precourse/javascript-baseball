7d1ae75298aa71a8f8069bd12bbaf5d152a696f7
sewonjun <93499071+sewonjun@users.noreply.github.com>
2022-11-05 18:46:22 +0900

    docs: README.md에 기능목록 작성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
-class App {
-  play() {}
-}
+const MissionUtils = require("@woowacourse/mission-utils");
 
-module.exports = App;
+console.log(MissionUtils.Random.pickNumberInList([1, 2, 3]));
+MissionUtils.Console.print("안녕하세요.");
+MissionUtils.Console.close();
+// class App {
+//   // play() {}
+// }
+
+// module.exports = App;
