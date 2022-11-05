0a9c07329b19e71b3fd8fa796cc9b055e832d1c5
BuildTools <unconfigured@null.spigotmc.org>
2022-11-06 06:23:37 +0900

    숫자 입력 받기

--- src/App.js
+++ src/App.js
@@ -2,19 +2,23 @@ const MissionUtils = require('@woowacourse/mission-utils');
 
 class App {
   play() {
+    const computerAnswer = makeComputerAnswer();
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+    MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (userMessage) => {
+      MissionUtils.Console.print('숫자는 ' + userMessage);
+    });
   }
+}
 
-  makeComputerAnswer() {
-    const computer = [];
-    while (computer.length < 3) {
-      const number = MissionUtils.Random.pickNumberInRange(1, 9);
-      if (!computer.includes(number)) {
-        computer.push(number);
-      }
+function makeComputerAnswer() {
+  const computer = [];
+  while (computer.length < 3) {
+    const number = MissionUtils.Random.pickNumberInRange(1, 9);
+    if (!computer.includes(number)) {
+      computer.push(number);
     }
-    return computer;
   }
+  return computer;
 }
 
 module.exports = App;
