08f3e8911769ed53f36417ef4b0d6ccd3259a30c
unknown <sjjoseph106@gmail.com>
2022-11-05 19:30:54 +0900

    feat: 게임 시작 문구 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+const { Console } = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  start() {
+    Console.print("숫자 야구 게임을 시작합니다.");
+  }
+
+  play() {
+    this.start();
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
