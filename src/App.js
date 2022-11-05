5bdad0fe7ceef2163607daeb858af9821bbb9540
freemoon99 <soloist132@gmail.com>
2022-11-05 18:55:59 +0900

    feat: 숫자를 입력받아 유효성 검사
    숫자를 입력받는 기능, 유효성 검사 기능

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
-  computerInput(){
+  computerInput() {
     const computer = [];
     while (computer.length < 3) {
       const number = MissionUtils.Random.pickNumberInRange(1, 9);
@@ -10,8 +10,25 @@ class App {
     return computer
   }
 
+  checkIsvaild(num) {
+    if(num === '') throw new Error('입력해주세요.');
+    if((/[^0-9]/g).match === null) throw new Error('숫자를 입력해주세요.');
+    if(num.length !== 3) throw new Error('숫자 3개가 입력되지 않았습니다.');
+    if([...new Set(num.split(""))].length !== 3) throw new Error('중복된 숫자가 있습니다.');
+    if((/[^1-9]/g).test(num)) throw new Error('1~9 사이의 숫자만 입력할 수 있습니다.');
+  }
+
+  userInput() {
+    MissionUtils.Console.readLine('숫자를 입력해주세요: ', (number) => {
+      this.checkIsvaild(number);
+      this.userInput();
+    });
+  }
+  
   play() {
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+    this.computerInput();
+    this.userInput();
   }
 }
 
