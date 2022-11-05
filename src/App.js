a5fb1e3a89d2f7c6814c494756d4d9d24a7158a8
sunny <huiseon37@gmail.com>
2022-11-05 20:46:47 +0900

    feat: 게임 시작 play 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+class User {
+  constructor(){
+    console.log('유저 클래스 생성');
+  }
+}
 class App {
-  play() {}
+  play() {
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+    new User();
+  }
 }
 
 module.exports = App;
