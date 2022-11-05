0007bfd8ee760b651934953533d0df9dfa243228
Jaehyeon <jaehyeonp74@gmail.com>
2022-11-05 17:21:45 +0900

    refactor: 임의의 세 자리 수 만드는 함수의 리턴 값 배열로 수정
    - 사용하는 모듈이 중복을 제거한 배열을 리턴해서 Set을 이용할 필요 없다고 판단

--- src/generateNumber.js
+++ src/generateNumber.js
@@ -2,9 +2,7 @@ const MissionUtils = require('@woowacourse/mission-utils');
 
 function generateNumber() {
   const uniqueNumArr = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
-  const numSet = new Set(uniqueNumArr);
-
-  return numSet;
+  return uniqueNumArr;
 }
 
 module.exports = generateNumber;
