ea2f715618ed81355a024cf5ace4ecf165822a1f
yuu <22yuu@naver.com>
2022-11-05 18:36:37 +0900

    refactor: 유효성검사 기능 코드 리팩토링

--- src/App.js
+++ src/App.js
@@ -1,37 +1,48 @@
 const MissionUtils = require("@woowacourse/mission-utils");
-/*
-MissionUtils.Console.readLine("닉네임을 입력해주세요 ->", (answer) => {
-  console.log(`닉네임 : ${answer}`);
-  MissionUtils.Console.print("안녕하세요.");
-  MissionUtils.Console.close();
-  console.log(MissionUtils.Random.pickNumberInRange(1, 10));
-  console.log(MissionUtils.Random.pickNumberInList([1, 2, 3, 4, 5]));
-  console.log(MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3));
-});*/
+
 const LENGTH = 3;
 
+const QuestionText = Object.freeze({
+  startText: "숫자 야구 게임을 시작합니다.",
+  inputText: "숫자를 입력해주세요 : ",
+  endText: "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
+  redoText: "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
+});
 class App {
   constructor() {
-    this.target = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
     this.io = MissionUtils.Console;
-    this.io.print("숫자 야구 게임을 시작합니다.");
+    this.io.print(QuestionText.startText);
     this.userInput = undefined;
+    this.userInputArray = [];
+    this.isFinish = false;
+    this.target = this.setTarget();
   }
 
   play() {
-    // 사용자의 입력 값을 받음
-    this.io.readLine("숫자를 입력해주세요 : ", (answer) => {
-      this.userInput = answer;
-      // console.log(this.userInput);
-      this.validation(parseInt(this.userInput));
-    });
+    this.start();
+  }
+
+  onInput(text, callback) {
+    this.io.readLine(text, callback.bind(this));
+  }
+
+  setTarget() {
+    const computer = [];
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) computer.push(number);
+    }
+    console.log(computer);
+    return computer;
+  }
+
+  start() {
+    this.onInput(QuestionText.inputText, this.onGame);
   }
 
   // 유효성 검사
   validation(input) {
-    if (isNaN(input)) {
-      throw new Error("정수 값을 입력해주세요!");
-    }
+    if (isNaN(input)) throw new Error("정수 값을 입력해주세요!");
 
     const len = Math.ceil(Math.log10(input + 1));
     if (len !== 3) throw new Error("서로 다른 3자리의 수를 입력해주세요!");
@@ -43,10 +54,10 @@ class App {
 
   isDuplicate(input) {
     const checkArray = Array(10).fill(false);
-    const userInputArray = this.inputToArray(input);
+    this.userInputArray = this.inputToArray(input);
 
     for (let i = 0; i < LENGTH; i++) {
-      let idx = userInputArray[i];
+      let idx = this.userInputArray[i];
       if (checkArray[idx]) throw new Error("중복이 있습니다!");
 
       checkArray[idx] = true;
@@ -54,17 +65,23 @@ class App {
   }
 
   inputToArray(input) {
-    const userInputArray = [];
+    const array = [];
 
     while (input > 0) {
-      userInputArray.push(input % 10);
+      array.unshift(input % 10);
       input = Math.floor(input / 10);
     }
 
-    return userInputArray;
+    return array;
+  }
+
+  // Game
+  onGame(input) {
+    this.io.print(input);
   }
 }
 
 const app = new App();
 app.play();
+
 module.exports = App;
