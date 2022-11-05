24e99243e151661802fb6591e97e64a3ccef2f9f
BuildTools <unconfigured@null.spigotmc.org>
2022-11-06 05:23:56 +0900

    정답 생성 메서드 생성

--- src/App.js
+++ src/App.js
@@ -4,11 +4,20 @@ class App {
   play() {
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
   }
+
+  makeComputerAnswer() {
+    const computer = [];
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+    return computer;
+  }
 }
 
 module.exports = App;
 
 const app = new App();
 app.play();
-
-// 콜백함수는 다른 함수에 입력값으로 넣어야 한다.
