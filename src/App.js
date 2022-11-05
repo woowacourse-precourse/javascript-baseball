a4db91cc04d9e5fdf857858d606219b158509bf4
chasj0326@naver.com <62418379+chasj0326@users.noreply.github.com>
2022-11-05 17:52:44 +0900

    feat: App.js - 문제 생성 기능 구현
    게임 시작 부분 함수로 재작성 startGame()
    문제 생성 함수 구현 makeProblem(NUMBER_LENGTH)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
+function startGame(){
+  // 1. 게임 시작
+  MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+}
+
+function makeProblem(NUMBER_LENGTH){
+  // 2. 문제 생성
+  let problemNumbers = [];
+  let problem = 0;
+  let tempNumber;
+  while (problemNumbers.length < NUMBER_LENGTH){
+    tempNumber = MissionUtils.Random.pickNumberInRange(1, 9);
+    if(!problemNumbers.includes(tempNumber)){
+      problemNumbers.push(tempNumber);
+      problem += 10**(problemNumbers.length-1)*tempNumber;
+    }
+  }
+  return problem;
+}
+
 class App {
   play() {
-    // START GAME
-    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    //1. 게임 시작
+    startGame();
+
+    // 2. 문제 생성
+    const NUMBER_LENGTH = 3;
+    const problem = makeProblem(NUMBER_LENGTH);
+    
   }
 }
 
