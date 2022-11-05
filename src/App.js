2626a01c19f5b9a9f9f5030df136b33effc22432
daal2quf <daal2quf@gmail.com>
2022-11-05 21:10:26 +0900

    feat: 3자리 랜덤 숫자 생성 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
-
   play() {
     this.gamaStartAlram()
+    this.createRandomNum()
   }
 
   gamaStartAlram() {
     MissionUtils.Console.print("게임을 시작합니다")
   }
 
+  createRandomNum() {
+    let random_nums = new Set()
+    while (random_nums.size < 3) {
+      random_nums.add(MissionUtils.Random.pickNumberInRange(1, 9))
+    }
+    return this.correct_answer = [...random_nums].join('')
+  }
 }
 
 const app = new App();
