8d6feb61b0392fae0c3ff70dac3da86654159d91
DongWooKim97 <nanaukim@naver.com>
2022-11-05 15:20:12 +0900

    feat: 게임을 통제할 PlayGame 클래스 생성 및 기능(메서드) 작성

---
+++ src/PlayGame.js
@@ -0,0 +1,49 @@
+class PlayGame {
+  constructor() {
+    this.missionRandom = require("@woowacourse/mission-utils").Random;
+    this.missionConsole = require("@woowacourse/mission-utils").Console;
+    this.valid = require("./input/userInputValid");
+    this.score = require("./play/GetScore");
+    this.pcNumber = require("./GetComputerInput");
+  }
+
+  getOtherPcNumber() {
+    return new this.pcNumber().makeRandomNumbers();
+  }
+
+  gameStart(computerInputNumbers) {
+    console.log(computerInputNumbers);
+    this.missionConsole.readLine("숫자를 입력해주세요 : ", (userInputNumbers) => {
+      if (new this.valid(userInputNumbers).checkValid()) {
+        if (this.result(computerInputNumbers, userInputNumbers) !== "3스트라이크") {
+          this.missionConsole.print(this.result(computerInputNumbers, userInputNumbers));
+          return this.gameStart(computerInputNumbers);
+        }
+        if (this.result(computerInputNumbers, userInputNumbers) === "3스트라이크") {
+          this.missionConsole.print(this.result(computerInputNumbers, userInputNumbers));
+          this.missionConsole.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+          return this.askRestart();
+        }
+      }
+    });
+  }
+
+  askRestart() {
+    this.missionConsole.readLine("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n", (userInputNumbers) => {
+      if (userInputNumbers === "1") {
+        return this.gameStart(this.getOtherPcNumber());
+      } else if (userInputNumbers === "2") {
+        return this.missionConsole.close();
+      } else {
+        throw "잘못 된 입력입니다. 게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+      }
+    });
+  }
+
+  result(computerInputNumbers, userInputNumbers) {
+    const scoreCount = new this.score(computerInputNumbers, userInputNumbers);
+    return scoreCount.compare();
+  }
+}
+
+module.exports = PlayGame;
