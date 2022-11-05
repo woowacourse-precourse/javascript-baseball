a30d1aed77171601e41437165898a4c71b83a6f4
Hyun Su <tlsgustn93@naver.com>
2022-11-06 00:11:38 +0900

    refactor: App.js 정리

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-const { threePitchingBall } = require('./modules/generateNumbers')
+const { generateNumbers } = require("./modules/generateNumbers");
+const { baseBall } = require("./modules/gameInformation");
+const { gameStart } = require("./modules/gameStart")
 
 class App {
   play() {
-    threePitchingBall();
+    gameStart();
+    baseBall(generateNumbers());
   }
 }
 
-const app = new App()
-app.play()
-console.log(threePitchingBall())
+const app = new App();
+app.play();
+
 module.exports = App;
