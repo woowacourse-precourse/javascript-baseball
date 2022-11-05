a6195a4d20249942e6b277df01ca4b94a2b1678a
jiho1996 <jhp9270@naver.com>
2022-11-05 15:21:27 +0900

    feat : 시작 문구 출력 기능 추가

--- src/game.js
+++ src/game.js
@@ -1,9 +1,11 @@
 const MissionUtils = require("@woowacourse/mission-utils");
+const GAME_MESSAGE = require("./constants/message");
 
 class NumericBaseballGame {
   constructor() {}
 
   start() {
+    MissionUtils.Console.print(GAME_MESSAGE.NOTIFY_START_MESSAGE);
     const getRandomNumber = () => MissionUtils.Random.pickNumberInRange(1, 9);
     const isNumberNotInSpace = (number, array) => !array.includes(number);
     const isNotFull = (computerSpace) => computerSpace.length < 3;
