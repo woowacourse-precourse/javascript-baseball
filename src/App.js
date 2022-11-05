d1ab83ba5169d23b1379c5b22de3a20c8ce0bebb
Sungwk <73461032+Sungwk@users.noreply.github.com>
2022-11-05 16:45:27 +0900

    feat : 5. 입력받은값 3지라수와 비교 후 strike,ball갯수 카운트

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ const MissionUtils = require('@woowacourse/mission-utils');
 class App {
   play() {
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+    this.result = {
+      ball: 0,
+      strike: 0,
+    };
     this.computerNumber = this.getComputerNumber();
     this.getInputNumber();
   }
+  //컴퓨터의 랜덤 숫자 3개를 리턴하는 함수
   getComputerNumber() {
     const COMPUTER = [];
     while (COMPUTER.length < 3) {
@@ -15,11 +20,13 @@ class App {
     }
     return COMPUTER;
   }
+  //사용자의 숫자를 받는 함수
   getInputNumber() {
     MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (inputNumber) => {
       this.checkInputNumber(inputNumber);
     });
   }
+  //입력한 숫자의 자릿수가 세 자리 인지 확인하는 함수
   isInputLengthRight(input) {
     if (input.length !== 3) throw '세 자리를 입력하세요.';
   }
@@ -44,11 +51,24 @@ class App {
       throw '서로 다른 숫자를 입력하세요.';
     }
   }
+  //입력한 숫자가 올바른 숫자인지 확인하는 함수
   checkInputNumber(input) {
     this.isInputNumber(input);
     this.isInputLengthRight(input);
     this.isInputIncludeZero(input);
     this.isInputDifferentNumber(input);
+    this.getResult(input);
+  }
+  //볼, 스트라이크 갯수 카운트하는 함수
+  getResult(input) {
+    for (let i = 0; i < 3; i++) {
+      if (parseInt(input[i]) === this.computerNumber[i]) {
+        this.result.strike += 1;
+      } else if (this.computerNumber.includes(parseInt(input[i]))) {
+        this.result.ball += 1;
+      }
+    }
+    console.log(this.result);
   }
 }
 
