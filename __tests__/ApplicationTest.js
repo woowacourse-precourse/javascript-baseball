70c2be772be8549c9113353cf4194e82460c8d00
chiman <rlaclghks123@naver.com>
2022-11-05 21:19:33 +0900

    test: validation test파일 추가

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -28,13 +28,7 @@ describe("숫자 야구 게임", () => {
     const randoms = [1, 3, 5, 5, 8, 9];
     const answers = ["246", "135", "1", "597", "589", "2"];
     const logSpy = getLogSpy();
-    const messages = [
-      "낫싱",
-      "3스트라이크",
-      "1볼 1스트라이크",
-      "3스트라이크",
-      "게임 종료",
-    ];
+    const messages = ["낫싱", "3스트라이크", "1볼 1스트라이크", "3스트라이크", "게임 종료"];
 
     mockRandoms(randoms);
     mockQuestions(answers);
@@ -58,41 +52,5 @@ describe("숫자 야구 게임", () => {
       const app = new App();
       app.play();
     }).toThrow();
-  });
-  test("예외테스트2 : 0이 포함될 경우", () => {
-    const randoms = [1, 3, 5];
-    const answers = ["120"];
-
-    mockRandoms(randoms);
-    mockQuestions(answers);
-
-    expect(() => {
-      const app = new App();
-      app.play();
-    }).toThrow("1에서 9까지의 자연수를 입력해주세요");
-  });
-  test("예외테스트3 : 빈문자열이 포함된 경우", () => {
-    const randoms = [1, 3, 5];
-    const answers = ["12 "];
-
-    mockRandoms(randoms);
-    mockQuestions(answers);
-
-    expect(() => {
-      const app = new App();
-      app.play();
-    }).toThrow("1에서 9까지의 자연수를 입력해주세요");
-  });
-  test("예외테스트4 : 중복되는 숫자가 있는 경우 ", () => {
-    const randoms = [1, 3, 5];
-    const answers = ["122"];
-
-    mockRandoms(randoms);
-    mockQuestions(answers);
-
-    expect(() => {
-      const app = new App();
-      app.play();
-    }).toThrow("서로 다른 3개의 숫자를 입력해주세요");
   });
 });
