8f480ccc65c8cb1cf6e928b3af375b2915e3d019
Yeom-JinHo <pwlsghq@naver.com>
2022-11-05 19:31:17 +0900

    test : 사용자 입력 예외처리

--- __tests__/SelfTest.js
+++ __tests__/SelfTest.js
@@ -1,6 +1,25 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 const App = require('../src/App');
 
+const mockQuestions = answers => {
+  MissionUtils.Console.readLine = jest.fn();
+  answers.reduce(
+    (acc, input) =>
+      acc.mockImplementationOnce((question, callback) => {
+        callback(input);
+      }),
+    MissionUtils.Console.readLine
+  );
+};
+
+const mockRandoms = numbers => {
+  MissionUtils.Random.pickNumberInRange = jest.fn();
+  numbers.reduce(
+    (acc, number) => acc.mockReturnValueOnce(number),
+    MissionUtils.Random.pickNumberInRange
+  );
+};
+
 const getLogSpy = () => {
   const logSpy = jest.spyOn(MissionUtils.Console, 'print');
   logSpy.mockClear();
@@ -13,5 +32,31 @@ describe('숫자 야구 게임 (셀프테스트)', () => {
     const app = new App();
     app.play();
     expect(logSpy).toHaveBeenCalledWith('숫자 야구 게임을 시작합니다.');
+  });
+
+  test('사용자 입력 예외처리', () => {
+    const answers = [
+      '333',
+      '0',
+      '1,',
+      '4561',
+      '1789',
+      '염진호',
+      'a12',
+      '!@#',
+      '',
+      '😂😂😂',
+      '012'
+    ];
+    mockQuestions(answers);
+
+    answers.forEach(() => {
+      expect(() => {
+        const app = new App();
+        app.play();
+      }).toThrow(
+        '입력형식이 잘못되었습니다. 서로 다른 숫자 3개를 입력해주세요.🙏'
+      );
+    });
   });
 });
