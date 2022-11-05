a011b875717fbacf396542161de45966cae36f01
han0224 <dbn0224@gmail.com>
2022-11-05 19:22:08 +0900

    Feat: 야구게임 숫자 사용자 입력 기능 추가
    예외처리 완료
    - 사용자가 숫자가 아닌 다른 문자를 입력했을 경우
    - 사용자가 3자리가 아닌 숫자를 입력했을 경우
    - 사용자가 중복되는 숫자를 입력했을 경우
    
    useInputNumber함수의 경우 사용자의 입력을 받아 예외처리해주는 함수를 호출 정상적인 숫자면 해당 숫자를 반환
    isBaseballNumber함수의 경우 매개변수로 들어온 숫자가 야구게임에 적합한 게임인지 확인하는 함수
    NumberToArray함수의 경우 매개변수로 받은 숫자를 한 자리수씩 배열에 할당해주어 해당 배열을 반환하는 함수
    isNumber함수의 경우 매개변수로 받은 숫자가 숫자일 경우 true, 숫자가 아닐 경우 false를 반환하는 함수

--- src/App.js
+++ src/App.js
@@ -10,9 +10,50 @@ class App {
     }
     return Number(Array.from(computer).join(""));
   }
+
+  isNumber(number) {
+    return !Number.isNaN(number) && typeof number === "number";
+  }
+
+  NumberToArray(number) {
+    return Array.from(String(number), (num) => Number(num));
+  }
+
+  isBaseballNumber(number) {
+    number = Number(number);
+    if (!this.isNumber(number)) {
+      throw "숫자만 입력해주세요";
+    }
+    if (number >= 1000 || number <= 99) {
+      throw "3개의 숫자만 입력해주세요";
+    }
+    if (new Set(this.NumberToArray(number)).size !== 3) {
+      throw "중복되지 않는 숫자로 입력해주세요";
+    }
+  }
+
+  userInputNumber() {
+    let userInputNumber = 0;
+    MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (number) => {
+      this.isBaseballNumber(number);
+      userInputNumber = number;
+      MissionUtils.Console.close();
+    });
+    return userInputNumber;
+  }
+
   play() {
-    this.getRandomBaseballNumber();
+    try {
+      this.getRandomBaseballNumber();
+      this.userInputNumber();
+    } catch (err) {
+      console.log(err);
+      return;
+    }
   }
 }
 
 module.exports = App;
+
+const app = new App();
+app.play();
