63319fd6be6939b6f8a9919560a706e332c826d4
froggy1014 <clcl6084@gmail.com>
2022-11-06 02:17:33 +0900

    test: 기능 목록 테스트 케이스 추가
    1. 게임 시작 안내 출력 테스트 케이스
    2. 입력값 받는 기능 테스트 케이스

---
+++ __tests__/FeatureTest.js
@@ -0,0 +1,60 @@
+const App = require('../src/App');
+const { Console, Random } = require('@woowacourse/mission-utils');
+
+const mockQuestions = (answers) => {
+  Console.readLine = jest.fn();
+  answers.reduce((acc, input) => {
+    return acc.mockImplementationOnce((question, callback) => {
+      callback(input);
+    });
+  }, Console.readLine);
+};
+
+const mockRandoms = (numbers) => {
+  Random.pickNumberInRange = jest.fn();
+
+  numbers.reduce((acc, number) => {
+    return acc.mockReturnValueOnce(number);
+  }, Random.pickNumberInRange);
+};
+
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(Console, 'print');
+  logSpy.mockClear();
+  
+  return logSpy;
+};
+
+describe('기능 목록 검사', () => {
+  test('게임 처음 시작 초기화 및 안내 출력 기능', () => {
+    const logSpy = getLogSpy();
+
+    const app = new App();
+    app.play();
+
+    expect(logSpy).toHaveBeenCalledWith('숫자 야구 게임을 시작합니다.');
+  });
+
+  test('플레이어 입력값 받는 기능', () => {
+    const randoms = [1, 2, 3];
+    const playerInput = ['142','123'];
+    const logSpy = getLogSpy();
+
+    const messages = [
+      '숫자 야구 게임을 시작합니다.',
+      '1볼 1스트라이크',
+      '3스트라이크',
+      '3개의 숫자를 모두 맞히셨습니다! 게임 종료',
+    ];
+
+    mockRandoms(randoms);
+    mockQuestions(playerInput);
+
+    const app = new App();
+    app.play();
+
+    messages.forEach((message) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(message));
+    });
+  });
+});
