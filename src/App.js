61e00a311634e28c9920c3d53316326e8ed08408
unknown <sjjoseph106@gmail.com>
2022-11-05 21:50:51 +0900

    feat: 숫자 입력 문구를 띄우고 사용자 입력 값 받기

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 const { Console } = require("@woowacourse/mission-utils");
 
 const Computer = require("./Computer");
+const User = require("./User");
 
-const INITIAL_STATE = {
+const initialState = {
   answer: "",
+  inputValue: "",
 };
 
 class App {
   constructor() {
-    this.state = INITIAL_STATE;
+    this.state = initialState;
 
     this.computer = new Computer();
+    this.user = new User();
   }
 
   start() {
     Console.print("숫자 야구 게임을 시작합니다.");
     this.state.answer = this.computer.getThreeUniqueNumbers();
+
+    this.state.inputValue = this.user.getInputValue();
+    Console.print(this.state.inputValue);
   }
 
   play() {
