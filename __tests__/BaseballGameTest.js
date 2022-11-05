ef277652d6b915d4dc826d4ca0f64d3ed47df157
olo90632951 <olo90632951@gmail.com>
2022-11-05 20:24:30 +0900

    test: 사용자 입력값의 형식이 잘못되었을 때 예외 발생

--- __tests__/BaseballGameTest.js
+++ __tests__/BaseballGameTest.js
@@ -9,6 +9,17 @@ const getSpy = (object, methodName) => {
   return spy;
 };
 
+const mockUserValue = (numbers) => {
+  MissionUtils.Console.readLine = jest.fn();
+  numbers.reduce(
+    (acc, input) =>
+      acc.mockImplementationOnce((question, callback) => {
+        callback(input);
+      }),
+    MissionUtils.Console.readLine,
+  );
+};
+
 describe('숫자 야구 게임', () => {
   test('게임 시작 문구 출력', () => {
     const logSpy = getSpy(MissionUtils.Console, 'print');
@@ -29,5 +40,18 @@ describe('숫자 야구 게임', () => {
     expect(result).toHaveLength(3);
     expect(result).toEqual(expect.not.stringContaining('0'));
     expect(result.length).toEqual(removeDuplicatedNumber.size);
+  });
+
+  test('사용자의 입력값이 1에서 9까지의 서로 다른 3자리 수가 아닐 때 예외 발생', () => {
+    const userValue = ['000', '120', '102', '0', '12', '3', '121', 'a', 'AAA'];
+
+    mockUserValue(userValue);
+
+    for (let i = 0; i < userValue.length; i += 1) {
+      expect(() => {
+        const app = new App();
+        app.play();
+      }).toThrow();
+    }
   });
 });
