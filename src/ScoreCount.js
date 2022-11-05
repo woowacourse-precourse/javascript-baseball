3aaefb4361ef0eb63e1e6b3c0d03b17e1fe7dd3a
Dongkeun <hongdongk@naver.com>
2022-11-05 22:56:21 +0900

    기능4: 입력받은수 점수계산하는 함수

---
+++ src/ScoreCount.js
@@ -0,0 +1,18 @@
+const ScoreCount = (input, Randomnumber) => {
+  let inputarray = input.split('').map((a) => Number(a));
+  let strike = 0,
+      ball = 0;
+      none = 0;
+      
+  for(let i=0; i<3; i++){
+    if(Randomnumber.includes(inputarray[i])){
+      if(inputarray[i] == Randomnumber[i]) strike += 1; 
+      else ball += 1;   
+    }
+    else none += 1;
+  };
+  
+  return [strike, ball, none];
+}
+  
+module.exports = ScoreCount;
