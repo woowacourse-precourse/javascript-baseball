896d67bd79b65553fd41d344c9b9139dd5a94066
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-11-06 00:33:52 +0900

    feat($makeComputerAnswer): 컴퓨터가 랜덤으로 세자리수 정답 생성

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 
 class App {
+  #computerAnswer = '';
+
   constructor() {
     this.message = '숫자 야구 게임을 시작합니다.';
+  }
+
+  #makeComputerAnswer() {
+    const answer = [];
+    while (answer.length < 3) {
+      const num = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!answer.includes(num)) {
+        answer.push(num);
+      }
+    }
+    this.#computerAnswer = answer;
   }
 
   play() {
