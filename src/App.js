7ab4bb75d050b173c7fd6de69cd7f5dd5cba0ada
“somm12” <sominan8744@gmail.com>
2022-11-05 10:51:14 +0900

    feat: 랜덤 숫자 만들기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+const MissionUtils = require("@woowacourse/mission-utils");
 class App {
-  play() {}
-}
+  makeRandomNumber() {
+    return MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
+  }
 
+  play() {
+    let randomNumber = this.makeRandomNumber();
+  }
+}
+const app = new App();
+app.play();
 module.exports = App;
