1a0fc69ef5ee8b4242af39b96bea9360b8bcce46
ReturnReason <reasonz@daum.net>
2022-11-05 09:55:42 +0900

    feat: 에러 메시지 상수 추가

--- src/common/constants.js
+++ src/common/constants.js
@@ -12,7 +12,10 @@ const START_MESSAGE = '숫자 야구 게임을 시작합니다.';
 const INPUT_MESSGAE = '숫자를 입력해주세요 : ';
 const END_MESSAGE = `${MAX_LENGTH}개의 숫자를 모두 맞히셨습니다! 게임 종료
 게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.`;
-const ERROR_MESSAGE = `잘못된 입력값입니다.`;
+
+const INVALID_ERROR_MESSAGE = `숫자가 아닌 값이 포함되어 있습니다. 게임 종료`;
+const DUPLICATE_ERROR_MESSAGE = `중복된 숫자가 포함되어 있습니다. 게임 종료`;
+const RANGE_ERROR_MESSAGE = `입력 값의 길이가 올바르지 않습니다. (3자리 숫자가 아님) 게임 종료`;
 
 module.exports = {
   MAX_LENGTH,
@@ -22,5 +25,7 @@ module.exports = {
   START_MESSAGE,
   END_MESSAGE,
   INPUT_MESSGAE,
-  ERROR_MESSAGE,
+  INVALID_ERROR_MESSAGE,
+  DUPLICATE_ERROR_MESSAGE,
+  RANGE_ERROR_MESSAGE,
 };
