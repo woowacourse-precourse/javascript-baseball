aea0bf56156562ea063ebc2587d11b73a846e2dc
bj lee <maplestory1419@naver.com>
2022-11-05 16:49:31 +0900

    1번 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
-class App {
-  play() {}
-}
+const MissionUtils = require("@woowacourse/mission-utils");
 
+class App {
+  constructor() {
+    this.Computer_Number=[]
+  }
+  GenerageNumber(){
+    for(let i=0;i<3;i++){
+      this.Computer_Number.push(MissionUtils.Random.pickNumberInRange(0, 9))
+    }
+  }
+  play() {
+    this.GenerageNumber()
+  }
+
+}
+let N=new App()
+N.play()
 module.exports = App;
