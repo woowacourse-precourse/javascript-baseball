236900ee62e27d117e47c76527ad8c2abb5d8c33
SaeWooKKang <87258182+SaeWooKKang@users.noreply.github.com>
2022-11-05 20:32:58 +0900

    feat(기능목록2번): 정답 초기화하는 resetGameAnswer 메서드 생성

--- src/BaseBallGame.js
+++ src/BaseBallGame.js
@@ -1,5 +1,18 @@
 class BaseBallGame{
+  constructor() {
+    this.gameAnswer = [];
+  }
+  
+  resetGameAnswer() {
+    const answer = [];
 
+    while (answer.length < 3) {
+      const number = Random.pickNumberInRange(1, 9);
+      if (!answer.includes(number)) answer.push(number);
+    }
+
+    this.gameAnswer = answer;
+  }
 }
 
 export default BaseBallGame;
