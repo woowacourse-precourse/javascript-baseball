5fd4a057b004571b2bb821f8760f05bd8069bea4
jaewoogwak <jaewoo010207@gmail.com>
2022-11-06 01:40:44 +0900

    refactor: 함수명 직관적으로 변경 및 로직 개선

--- src/App.js
+++ src/App.js
@@ -1,73 +1,80 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 
-const VALIDATION_CODE = ['SUCCESS', 'RESTART', 'EXIT'];
-
 class App {
   constructor() {
-    this.exit = false;
-    this.code = null;
+    this.state = '';
     this.user = '';
     this.computer = '';
   }
 
   play() {
+    this.start();
     this.pickRandomNumber();
-    while (!this.exit) {
-      this.start();
-      this.input();
-      this.command();
+    while (this.state !== 'EXIT') {
+      this.input('숫자를 입력해주세요.');
+      this.result();
+      this.checkState();
     }
+  }
+
+  checkState() {
+    switch (this.state) {
+      case 'EXIT':
+        this.print('게임 종료');
+        MissionUtils.Console.close();
+        break;
+      case 'RESTART':
+        this.pickRandomNumber();
+        break;
+      default:
+        break;
+    }
+  }
+
+  setState(nextState) {
+    this.state = nextState;
   }
 
   start() {
     this.print('숫자 야구 게임을 시작합니다.');
   }
 
-  input() {
-    MissionUtils.Console.readLine('숫자를 입력해주세요.', (answer) => {
-      this.code = this.checkValidation(answer);
+  input(message) {
+    MissionUtils.Console.readLine(message, (answer) => {
       this.user = answer.split('').map((num) => parseInt(num, 10));
+      this.checkInputValidation(answer);
     });
-  }
-
-  command() {
-    switch (this.code) {
-      case 'SUCCESS':
-        this.output();
-        break;
-      case 'EXIT':
-        this.exit = true;
-        this.print('게임 종료');
-        MissionUtils.Console.close();
-        break;
-      default:
-        break;
-    }
   }
 
   pickRandomNumber() {
     const pick = [];
     while (pick.length < 3) {
       const number = MissionUtils.Random.pickNumberInRange(1, 9);
-      pick.push(number);
+      if (!pick.includes(number)) pick.push(number);
     }
     this.computer = pick;
   }
 
-  checkValidation(answer) {
-    if (answer.split('').length > 3) throw Error('유효하지 않은 입력입니다.');
+  checkInputValidation(answer) {
+    const { length } = answer.split('');
+    // 길이가 1이면 게임 종료 후 재시작/종료(1 or 2)인지 체크
+    if (length === 1) this.setState(this.checkRestart(answer));
+    else if (length === 3) this.setState('SUCCESS');
+    else throw Error('에러');
+  }
 
+  checkRestart(answer) {
     switch (answer) {
       case '1':
-        return VALIDATION_CODE[1];
+        return 'RESTART';
       case '2':
-        return VALIDATION_CODE[2];
+        return 'EXIT';
       default:
-        return VALIDATION_CODE[0];
+        throw Error('잘못된 입력입니다.');
     }
   }
 
-  checkStrike() {
+  countStrike() {
     let strike = 0;
     this.user.forEach((number, i) => {
       if (number === this.computer[i]) strike += 1;
@@ -75,7 +82,7 @@ class App {
     return strike;
   }
 
-  checkBall() {
+  countBall() {
     let ball = 0;
     this.user.forEach((number, i) => {
       if (this.computer.includes(number) && this.computer[i] !== number)
@@ -85,26 +92,29 @@ class App {
   }
 
   compare() {
-    const strike = this.checkStrike();
-    const ball = this.checkBall();
+    const strike = this.countStrike();
+    const ball = this.countBall();
     return { strike, ball };
   }
 
   victory() {
     this.print('3스트라이크');
     this.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
-    this.print('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.');
+    this.state = 'WIN';
   }
 
-  output() {
+  result() {
     const { strike, ball } = this.compare();
-
-    if (strike === 0 && ball === 0) this.print('낫싱');
-    else if (strike === 3) {
+    if (strike === 3) {
       this.victory();
-      this.pickRandomNumber();
-    } else if (ball === 0) this.print(`${strike}스트라이크`);
+      this.input('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.');
+    } else this.output(strike, ball);
+  }
+
+  output(strike, ball) {
+    if (strike === 0 && ball === 0) this.print('낫싱');
     else if (strike === 0) this.print(`${ball}볼`);
+    else if (ball === 0) this.print(`${strike}스트라이크`);
     else this.print(`${ball}볼 ${strike}스트라이크`);
   }
 
@@ -112,5 +122,8 @@ class App {
     MissionUtils.Console.print(string);
   }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
