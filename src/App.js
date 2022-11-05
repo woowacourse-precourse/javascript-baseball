01f25d5abd2f80a31966fb71333ca51456cc01de
“somm12” <sominan8744@gmail.com>
2022-11-05 12:51:28 +0900

    feat: 예외처리 userInputNumberException 함수 구현

--- src/App.js
+++ src/App.js
@@ -2,27 +2,59 @@ const MissionUtils = require("@woowacourse/mission-utils");
 const readLine = MissionUtils.Console.readLine;
 const print = MissionUtils.Console.print;
 const closeConsole = MissionUtils.Console.close;
-class App {
-  makeRandomNumber() {
-    let arr = new Set();
-    while (arr.size < 3) {
-      arr.add(MissionUtils.Random.pickNumberInRange(1, 9));
-    }
-    return Array.from(arr);
-  }
 
-  makeUserInputNumber() {
-    let userInputNumbers;
-    readLine("숫자를 입력해주세요 : ", (inputNumber) => {
-      userInputNumbers = [...inputNumber].map(Number);
+function makeRandomNumber() {
+  let arr = new Set();
+  while (arr.size < 3) {
+    arr.add(MissionUtils.Random.pickNumberInRange(1, 9));
+  }
+  return Array.from(arr);
+}
+
+function userInputNumberException(inputArray) {
+  let checkCondition = 0;
+  if (inputArray.length === 3) {
+    checkCondition += 1;
+  }
+  // 배열 안에 수들이 모두 숫자인지.
+  if (!inputArray.includes(NaN)) {
+    checkCondition += 1;
+  }
+  //1 ~ 9의 범위인지
+  if (!inputArray.includes(0)) {
+    checkCondition += 1;
+  }
+  //다 다른 수인가. set 사용
+  if (new Set(inputArray).size === 3) {
+    checkCondition += 1;
+  }
+  if (checkCondition === 4) {
+    return true;
+  }
+  return false;
+}
+
+function makeUserInputNumber() {
+  let userInputNumbers;
+  let exceptionSign;
+  readLine("숫자를 입력해주세요 : ", (inputNumber) => {
+    userInputNumbers = [...inputNumber].map(Number);
+    exceptionSign = userInputNumberException(userInputNumbers);
+
+    if (exceptionSign) {
       return userInputNumbers;
-    });
-  }
+    } else {
+      print("게임 종료");
+      closeConsole();
+    }
+  });
+}
 
+class App {
   play() {
     print("숫자 야구 게임을 시작합니다.");
-    let randomNumbers = this.makeRandomNumber();
-    let userInputNumbers = this.makeUserInputNumber();
+    let randomNumbers = makeRandomNumber();
+    let userInputNumbers = makeUserInputNumber();
   }
 }
 const app = new App();
