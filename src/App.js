3917bc7ebef1dccc20d182a1993cf3069737c293
imhynni <omelas329@gmail.com>
2022-11-05 20:17:54 +0900

    feat: 1부터 9까지의 숫자 중 서로 다른 3개를 골라 정답 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  #answer;
+
+  pickNumber() {
+    const computer = [];
+    while (computer.length < 3) {
+      const num = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(num)) {
+        computer.push(num);
+      }
+    }
+    return computer;
+  }
+
+  play() {
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    this.answer = this.pickNumber();
+  }
 }
 
 module.exports = App;
