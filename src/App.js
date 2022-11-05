1e731b15ca45027a38bf200c76d10c8525df9349
curomame <curomame@naver.com>
2022-11-05 23:57:37 +0900

    ➕ feat - print game start message
    게임 시작을 알리는 메세지를 출력하는 메소드 구현

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ const MissionUtils = require("@woowacourse/mission-utils");
 class App {
   play() {}
 
+  printGameStartMessgae(){
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+  }
+
   getRandomThreeDigitsNumber() {
     const number = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
     return Number(number.join(""));
