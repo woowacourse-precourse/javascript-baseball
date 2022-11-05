66469c7060cec1bceaa0bc97c4e7969b655423b9
jjongs2 <jjongs@hanyang.ac.kr>
2022-11-05 14:07:05 +0900

    refactor: 클래스 내에서 getter 역할을 하는 메서드들을 setter 역할로 변경

--- src/App.js
+++ src/App.js
@@ -2,69 +2,57 @@ const MissionUtils = require('@woowacourse/mission-utils');
 
 class App {
   constructor() {
-    this.answer = this.getAnswer();
-    this.userInput = this.readInput();
-    this.count = this.getCount(this.answer, this.userInput);
-    MissionUtils.Console.print(this.getHint(this.count));
+    this.setAnswer();
+    this.readInput();
+    this.setCount(this.answer, this.userInput);
+    this.setHint(this.count);
   }
 
-  getAnswer() {
-    let answer = [];
-
-    while (answer.length < 3) {
+  setAnswer() {
+    this.answer = [];
+    while (this.answer.length < 3) {
       const number = MissionUtils.Random.pickNumberInRange(1, 9);
 
-      if (!answer.includes(number)) {
-        answer.push(number);
+      if (!this.answer.includes(number)) {
+        this.answer.push(number);
       }
     }
-
-    return answer;
   }
 
   readInput() {
-    let userInput;
-
     MissionUtils.Console.readLine('숫자를 입력해 주세요 : ', (input) => {
-      userInput = Array.from(input, Number);
+      this.userInput = Array.from(input, Number);
     });
-
-    return userInput;
   }
 
-  getCount(answer, userInput) {
-    let count = {
+  setCount(answer, userInput) {
+    this.count = {
       ball: 0,
       strike: 0,
     };
-
     userInput.forEach((number, index) => {
       const findIndex = answer.findIndex((element) => element === number);
 
       if (findIndex === index) {
-        count.strike += 1;
+        this.count.strike += 1;
       } else if (findIndex !== -1) {
-        count.ball += 1;
+        this.count.ball += 1;
       }
     });
-
-    return count;
   }
 
-  getHint(count) {
-    let hint = '';
-
+  setHint(count) {
+    this.hint = '';
     if (count.ball > 0) {
-      hint = `${count.ball}볼`;
+      this.hint = `${count.ball}볼`;
     }
     if (count.strike > 0) {
-      hint += ` ${count.strike}스트라이크`;
+      this.hint += ` ${count.strike}스트라이크`;
     }
     if (hint === '') {
-      hint = '낫싱';
+      this.hint = '낫싱';
     }
-
-    return hint.trimStart();
+    this.hint.trimStart();
   }
 
   play() {}
