ee40e7084658b5355698021135fa12d9410d94d1
millifail <hwangbird99@gmail.com>
2022-11-06 08:34:08 +0900

    feat(number-generator):generate random numbers
    [게임시작] 1~9의 랜덤한 숫자 3개를 지정하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import * as MissionUtils from "@woowacourse/mission-utils";
+
 class App {
-  play() {}
+  play() {
+    const RANDOM_NUMBER = this.GET_RANDOM_NUMBER();
+  }
+
+  GET_RANDOM_NUMBER() {
+    return MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3); //ex - [2,5,6]
+  }
 }
 
 module.exports = App;
