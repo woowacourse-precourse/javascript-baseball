e5b9ecaf24f5f5bda674cea7a04d753f239f283b
wonkyung <mwk0725@gmail.com>
2022-11-05 16:56:24 +0900

    feat: 게임 랜덤 정답 생성 기능 구현

--- src/App.js
+++ src/App.js
@@ -2,11 +2,24 @@ const MissionUtils = require('@woowacourse/mission-utils');
 
 class App {
   constructor() {
+    this.ANSWER_NUMBER = [];
     this.playFirstTime = true;
   }
+
   play() {
     if (this.playFirstTime) {
       MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+    }
+    this.createAnswerNumber();
+  }
+
+  createAnswerNumber() {
+    this.ANSWER_NUMBER = [];
+    while (this.ANSWER_NUMBER.length < 3) {
+      const randomNumber = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!this.ANSWER_NUMBER.includes(randomNumber)) {
+        this.ANSWER_NUMBER.push(randomNumber);
+      }
     }
   }
 }
