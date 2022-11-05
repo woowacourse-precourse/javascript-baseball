096bec85793897b877548b72224c7422ba2bc435
pakxe <pigkill40@naver.com>
2022-11-05 22:48:23 +0900

    feat: 볼 개수 세는 함수

--- src/checkAnswer.js
+++ src/checkAnswer.js
@@ -1,3 +1,12 @@
 const strikeCount = (numArr, answer) => {
   return numArr.filter((number, i) => number === answer[i]).length;
 };
+
+const ballCount = (numArr, answer) => {
+  return numArr.filter(
+    (number, i) => answer.includes(number) && number !== answer[i]
+  ).length;
+};
+
+console.log(ballCount([1, 2, 3], [2, 3, 1]));
+console.log(ballCount([1, 2, 3], [1, 2, 4]));
