2894b7d4eaa4a75acbd4cce9ccea0eff842f29cc
2wndrhs <2wndrhs@gmail.com>
2022-11-04 23:49:37 -0700

    test: 랜덤 숫자 테스트 수정 getRandomNumber 메서드가 배열을 반환하기 때문에 다시 숫자로 변환하는 코드 추가

--- __tests__/RandomNumberTest.js
+++ __tests__/RandomNumberTest.js
@@ -2,7 +2,7 @@ const App = require('../src/App');
 
 describe('랜덤 숫자 테스트', () => {
   test('getRandonNumber 메서드는 111에서 999사이의 숫자를 반환', () => {
-    const RANDOM_NUMBER = App.getRandomNumber();
+    const RANDOM_NUMBER = Number(App.getRandomNumber().join(''));
 
     expect(RANDOM_NUMBER).toBeGreaterThanOrEqual(111);
     expect(RANDOM_NUMBER).toBeLessThanOrEqual(999);
