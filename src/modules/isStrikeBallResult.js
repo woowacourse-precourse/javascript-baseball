60e2b9c1de56b98a84e5a048c0fb7c5c314a3bb3
Hyun Su <tlsgustn93@naver.com>
2022-11-06 00:14:14 +0900

    feat: 볼, 스트라이크 판단 로직

---
+++ src/modules/isStrikeBallResult.js
@@ -0,0 +1,24 @@
+function isStrikeBall(computerInput, userInput) {
+    const userInputArr = `${userInput}`.split('')
+    const computerInputArr = `${computerInput}`.split('')
+    let strikeCount = 0
+    let ballCount = 0
+
+    computerInputArr.forEach((ele , idx) => {
+        console.log(ele, userInput[idx])
+        if(ele === userInputArr[idx]) strikeCount++
+        else if(userInputArr.includes(ele)) ballCount++
+    })
+    return { strikeCount, ballCount } 
+}
+
+function isStrikeBallResult(computerInput, userInput) {
+    const { strikeCount, ballCount } = isStrikeBall(computerInput, userInput)
+
+    if(strikeCount === 0 && ballCount === 0) return '낫싱';
+    if(ballCount === 0) return `${strikeCount}스트라이크`;
+    if(strikeCount === 0) return `${ballCount}볼`;
+    return `${ballCount}볼 ${strikeCount}스트라이크`;
+}
+
+exports.isStrikeBallResult = isStrikeBallResult
