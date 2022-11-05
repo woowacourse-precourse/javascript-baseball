c43ff1e41f512e609226b840cc78e4f9f5760b60
이석준 <sukjune96@gmail.com>
2022-11-05 22:56:07 +0900

    refactor: 입력 받는 함수 game 메서드와 분리시킴

--- src/App.js
+++ src/App.js
@@ -6,14 +6,18 @@ const makeAnswer = require("./makeNumber");
 class App {
   play() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
-    const answer = makeAnswer();
-    this.game(answer);
+    this.game();
   }
-  game(answer) {
+  game() {
+    const answer = makeAnswer();
     console.log(answer); //정답 미리보는 콘솔 로그
+    this.userInput(answer);
+  }
+  userInput(answer) {
     MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (input) => {
       const inputArr = getUserExecption(input);
       const score = getScore(inputArr, answer);
+      console.log(score);
     });
   }
 }
