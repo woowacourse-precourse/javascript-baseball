072f58eed49603c82897172360b478bb8dbb31f0
Siby1lA <psb4644@gmail.com>
2022-11-05 20:49:33 +0900

    feat: 유저의 입력을 받는 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
-  computerRandomNumber = () => {
+  getComputerNumber = () => {
     const computer = [];
     while (computer.length < 3) {
       const number = MissionUtils.Random.pickNumberInRange(1, 9);
@@ -11,8 +11,22 @@ class App {
     }
     return computer;
   };
+  getUserNumber = () => {
+    let userNumber;
+    MissionUtils.Console.readLine("숫자를 입력해주세요", (inputNumber) => {
+      const userNumber = [...inputNumber];
+      try {
+        // 예외처리 throw 할 곳
+      } catch (error) {
+        MissionUtils.Console.print(error);
+        MissionUtils.Console.close();
+      }
+    });
+    return userNumber;
+  };
   gameStart = () => {
-    this.computerNumber = this.computerRandomNumber();
+    this.computerNumber = this.getComputerNumber();
+    this.userNumber = this.getUserNumber();
   };
   play() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
