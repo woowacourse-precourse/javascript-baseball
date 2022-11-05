cdd9837de8732f950dbe01d53d8945440662ec2f
xilucks <siun0331@gmail.com>
2022-11-05 21:51:53 +0900

    feat: 숫자 범위에 따른 예외처리 작성
    4자리수 입력시, 예외처리가 발생하지 않던 케이스 추가

--- src/User.js
+++ src/User.js
@@ -9,12 +9,19 @@ class User {
   }
 
   numbersValidation(numbers) {
+    if (numbers.length <= 3) this.rangeErrorValidation(numbers);
+    else {
+      throw "올바르지 못한 숫자 입력입니다1.";
+    }
+  }
+
+  rangeErrorValidation(numbers) {
     numbers.reduce((acc, cur) => {
       if (!acc.includes(cur) && cur >= 1 && cur <= 9) {
         acc.push(cur);
         return acc;
       }
-      throw "올바르지 못한 숫자 입력입니다.";
+      throw "올바르지 못한 숫자 입력입니다2.";
     }, []);
   }
 }
