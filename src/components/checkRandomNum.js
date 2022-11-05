aa2d1b31aeed8ac28ac5da9b16a93bc2c7c310d8
Ryan-Dia <cjfdnjs158@naver.com>
2022-11-05 16:14:14 +0900

    refactor(랜덤숫자검토):  split함수 빠졌던거 추가

--- src/components/checkRandomNum.js
+++ src/components/checkRandomNum.js
@@ -1,5 +1,5 @@
 const checkRandomNum = (randomNum) => {
-  if (randomNum.length !== 3) throw "입력하신 값이 3자리가 아닙니다";
+  if (randomNum.split("").length !== 3) throw "입력하신 값이 3자리가 아닙니다";
   if (!Number(randomNum)) throw "입력하신 값이 숫자가 아닙니다";
   if (Number(randomNum) < 101) throw "-, + 부호등 불필요한 문자가 존재합니다.";
   let isSameNum = randomNum.split("").reduce((acc, num, i, arr) => {
@@ -10,3 +10,5 @@ const checkRandomNum = (randomNum) => {
   if (isSameNum) throw "입력하신 값에 중복되는 숫자가 존재합니다.";
   return randomNum;
 };
+
+module.exports = checkRandomNum;
