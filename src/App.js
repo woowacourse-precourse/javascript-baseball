8955156ad51e9bcfe34dd0c6117de179d993a803
Sungwk <73461032+Sungwk@users.noreply.github.com>
2022-11-05 15:56:12 +0900

    feat : 1. 게임 시작 문구 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+const utils = {
+  print(message) {
+    MissionUtils.Console.print(message);
+  },
+};
 class App {
-  play() {}
+  play() {
+    utils.print('숫자 야구 게임을 시작합니다.');
+  }
 }
 
 module.exports = App;
