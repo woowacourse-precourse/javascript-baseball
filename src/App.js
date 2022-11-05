de9f6d2c7510339fd68a73f6b6568bbb36628763
cjm0611 <81799492+cjm0611@users.noreply.github.com>
2022-11-05 10:04:08 +0900

    feat: 게임 재시작/종료 기능 구현 (기능 5번)

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ const MissionUtils = require("@woowacourse/mission-utils");
 class App {
   play() {
     const computer = this.getComputerRandomNumberString();
-
+    
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
     this.numberBaseballGame(computer);
   }
@@ -68,7 +68,13 @@ class App {
   selectRestartOrExitGame(){
     MissionUtils.Console.print('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.');
     MissionUtils.Console.readLine('', (choice) => {
-      MissionUtils.Console.close();
+      if(choice==='1'){
+        const computer = this.getComputerRandomNumberString();
+        this.numberBaseballGame(computer);
+      }
+      else{
+        MissionUtils.Console.close();
+      }
     });
   }
 }
