69d0fd85bb91b9810156b9fb5328dfe9d9ca9438
jiseung-kang <ill0o0lli@ajou.ac.kr>
2022-11-06 00:22:14 +0900

    feat(App): 게임 진행 전반 흐름 구현
    App 인스턴스의 play() 메서드를 실행하면 정답을 맞출 때까지 게임을 반복하도록 playGame() 구현
    - 사용자 입력을 받고 유효한 입력인지 검증
    - 사용자 입력 값을 저장하고 결과 출력 후 사용자 값 초기화
    - 결과가 정답이면 게임 종료 함수를 실행하고, 그렇지 않으면 playGame()을 반복하도록 구현
    
    전체적인 흐름과 흐름에 필요한 함수들을 만들었다. setScore(), printResult(), endGame()을 구현해야 한다.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+const LENGTH = 3;
 const MissionUtils = require('@woowacourse/mission-utils');
 class App {
   constructor() {
@@ -9,21 +10,67 @@ class App {
 
   play() {
     this.startGame();
+    while (this.strike !== LENGTH) this.playGame();
   }
 
   startGame() {
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
     this.setComputer();
-    MissionUtils.Console.close();
+  }
+
+  playGame() {
+    MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (answer) => {
+      MissionUtils.Console.close();
+      this.user = answer;
+
+      if (!this.isValidAnswer) {
+        this.user = '';
+        throw new TypeError();
+      }
+
+      this.setScore();
+      this.printResult();
+      this.resetScore();
+
+      if (this.strike === LENGTH) {
+        MissionUtils.Console.print(
+          `${LENGTH}개의 숫자를 모두 맞히셨습니다! 게임 종료`
+        );
+        this.endGame();
+      } else {
+        this.playGame();
+      }
+    });
   }
 
   setComputer() {
-    while (this.computer.length < 3) {
+    while (this.computer.length < LENGTH) {
       const number = MissionUtils.Random.pickNumberInRange(1, 9) + '';
       if (!this.computer.includes(number)) {
         this.computer += number;
       }
     }
+  }
+
+  setScore() {
+    // to be implemented
+  }
+
+  printResult() {
+    // to be implemented
+  }
+
+  resetScore() {
+    this.strike = 0;
+    this.ball = 0;
+  }
+
+  endGame() {
+    // to be implemented
+  }
+
+  get isValidAnswer() {
+    return this.user.match(/[1-9]/g) && new Set([...this.user]).size === 3;
   }
 }
 
