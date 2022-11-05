6b259f8d27e72a0dc094775439dabe35ecaa0df3
Dongkeun <hongdongk@naver.com>
2022-11-05 22:48:07 +0900

    기능2: 게임시작문구 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  play() {
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    let Randomnumber = ComputerRandomnumber();
+  }
 }
 
 module.exports = App;
