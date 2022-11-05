1cadc4724af564da004b6729cac6b245736e5678
su-lim <sujin.0xlim@gmail.com>
2022-11-05 14:10:05 +0900

    feat(generateNumberList): 서로다른 n개의 숫자 리스트를  생성하는 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,3 +1,22 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
+// game setting
+const START_GAME_NUM = 1;
+const END_GAME_NUM = 9;
+const GAME_NUM_SIZE = 3;
+
+function generateNumberList(size) {
+  if (END_GAME_NUM - START_GAME_NUM < size) {
+    return -1;
+  }
+  const result = MissionUtils.Random.pickUniqueNumbersInRange(
+    START_GAME_NUM,
+    END_GAME_NUM,
+    size
+  );
+  return result;
+}
+
 class App {
   play() {}
 }
