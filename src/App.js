df158936afc4a6f2a4ac333fedf678e288bd3efb
Dongkeun <hongdongk@naver.com>
2022-11-05 23:24:36 +0900

    기능함수들 서로연결하기

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 const MissionUtils = require("@woowacourse/mission-utils");
+const ComputerRandomnumber = require("./ComputerRandomNumber");
+const UserInput = require("./UserInput");
 
 class App {
   play() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
     let Randomnumber = ComputerRandomnumber();
+    UserInput(Randomnumber);
   }
 }
 
@@ -11,6 +14,7 @@ const RegameorEnd = () => {
   MissionUtils.Console.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.', (input) => {
     if(input == 1){
       let Randomnumber = ComputerRandomnumber();
+      UserInput(Randomnumber);
     }
     else if(input == 2){
       MissionUtils.Console.print("게임 종료");
--- src/UserInput.js
+++ src/UserInput.js
@@ -1,8 +1,27 @@
 const MissionUtils = require("@woowacourse/mission-utils");
+const CheckInput = require("./CheckInput");
+const PrintScore = require("./PrintScore");
+const ScoreCount = require("./ScoreCount");
+const App = require("./App");
 
-const UserInput = () => {
+const UserInput = (RandomNumber) => {
   MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (input) => {
+    CheckInput(input);
+    GameSystem(input, RandomNumber);
   });
 }
- 
+
+const GameSystem = (input, ComputerNumber) => {
+  let [strike, ball, none] = ScoreCount(input, ComputerNumber);
+  PrintScore(strike, ball, none);
+  
+  if (strike === 3) {
+    MissionUtils.Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+    App.RegameorEnd();
+  }
+  else{
+    UserInput(ComputerNumber);
+  }
+}
+
 module.exports = UserInput;
