f9f05ae19f99e2728abf2a6dae0182b1ae4248ee
ReturnReason <reasonz@daum.net>
2022-11-06 05:02:05 +0900

    refactor: 필요없는 상수 제거

--- src/common/constants.js
+++ src/common/constants.js
@@ -2,12 +2,6 @@ const MAX_LENGTH = 3;
 const BEGIN_NUM = 1;
 const END_NUM = 9;
 
-const GAME_RESULT = {
-  strike: '스트라이크',
-  ball: '볼',
-  nothing: '낫싱',
-};
-
 module.exports = {
   MAX_LENGTH,
   BEGIN_NUM,
