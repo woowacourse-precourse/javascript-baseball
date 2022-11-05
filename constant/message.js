15ffc739816223aa4297fd7b4e5290b47e951610
oio337a <oio337a@gmail.com>
2022-11-05 19:19:27 +0900

    refactor : put together constant values

---
+++ constant/message.js
@@ -0,0 +1,14 @@
+const GAME_START = "숫자 야구 게임을 시작합니다.";
+const USER_INPUT_REQUEST = "숫자를 입력해 주세요 : ";
+
+const INVALID_LENGTH = "3자리 숫자를 입력해 주세요.";
+const INVALID_RANGE = "1부터 9까지의 숫자를 입력해 주세요.";
+const INVALID_DUPLICATE = "중복된 숫자가 있습니다.";
+
+module.exports = {
+  GAME_START,
+  USER_INPUT_REQUEST,
+  INVALID_DUPLICATE,
+  INVALID_LENGTH,
+  INVALID_RANGE,
+};
