3e3522eeba299bbf71f4cb02fd4f27e36daecb32
userHWSEO <gmldnjseocnd@gmail.com>
2022-11-05 16:09:41 +0900

    feat: 게임 다시하기 기능 추가
    - replay 함수를 추가하여 게임을 다시하거나 혹은 게임을 종료하는 기능 추가.
    
    - gamePlay 함수를 생성하여 게임을 다시 시작하게 된다면 해당 함수를 리턴.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,47 @@ const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
   play() {
-    function makeRandomNumber() {
-      let random_number = "";
-      for (let i = 0; i < 3; i++) {
-        random_number += MissionUtils.Random.pickNumberInRange(1, 9);
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+
+    function playGame() {
+      MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (user_number) => {
+        function makeRandomNumber() {
+          let random_number = "";
+          for (let i = 0; i < 3; i++) {
+            random_number += MissionUtils.Random.pickNumberInRange(1, 9);
+          }
+          return random_number;
+        }
+        const random_number = makeRandomNumber();
+        checkCount(random_number, user_number);
+      });
+    }
+
+    function replay(strike_count, ball_count, random_number) {
+      if (strike_count !== 0 && ball_count !== 0) {
+        MissionUtils.Console.print(`${ball_count}볼 ${strike_count}스트라이크`);
       }
-      return random_number;
+      if (strike_count === 0 && ball_count !== 0) {
+        MissionUtils.Console.print(`${ball_count}볼`);
+      }
+      if (strike_count !== 0 && ball_count === 0) {
+        MissionUtils.Console.print(`${strike_count}스트라이크`);
+      }
+      if (strike_count === 0 && ball_count === 0) {
+        MissionUtils.Console.print("낫싱");
+      }
+      if (strike_count === 3) {
+        return MissionUtils.Console.readLine(
+          "3개의 숫자를 모두 맞히셨습니다! 게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
+          (answer) => {
+            if (answer === "1") return playGame();
+            return MissionUtils.Console.print("게임 종료");
+          }
+        );
+      }
+      MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (user_number) => {
+        checkCount(random_number, user_number);
+      });
     }
 
     function checkCount(random_number, user_number) {
@@ -18,7 +53,7 @@ class App {
       if (str_user_num.length !== 3) {
         throw new Error("세 자리 숫자를 입력해주세요.");
       }
-      if (typeof user_number !== "number") {
+      if (typeof Number(user_number) !== "number") {
         throw new Error("숫자를 입력해주세요.");
       }
       for (let i = 0; i < str_random_num.length; i++) {
@@ -28,12 +63,10 @@ class App {
           ball_count++;
         }
       }
-      if (strike_count === 0 && ball_count === 0) {
-        return "낫싱";
-      }
-      return [strike_count, ball_count];
+      replay(strike_count, ball_count, random_number);
     }
+
+    playGame();
   }
 }
-
 module.exports = App;
