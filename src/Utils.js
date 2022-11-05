736c585b8e5a4afe555567205d8066c4b25471d8
pakxe <pigkill40@naver.com>
2022-11-05 17:44:01 +0900

    feat: 문자열을 숫자 배열로 만들어서 반환하는 함수 작성

--- src/Utils.js
+++ src/Utils.js
@@ -2,3 +2,7 @@ const generateNumArr = ([min, max]) =>
   Array(max - min + 1)
     .fill()
     .map((number, index) => min + index);
+
+const stringToNumArr = (str) => str.split('').map(Number);
+
+module.exports = { generateNumArr, stringToNumArr };
