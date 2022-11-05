7ea04c8a3360b8a45f7dfa77300f6eaeaa6da190
SeungMiL <tmskqj@naver.com>
2022-11-05 21:06:59 +0900

    feat: 게임 시작 문구 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  play() {
+    const baseBallGame = new BaseBallPlay();
+    baseBallGame.start();
+
+  }
 }
+
+class BaseBallPlay {
+  start() {
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+  };
+};
 
 module.exports = App;
