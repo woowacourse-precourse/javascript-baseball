44341eb64de6058c05431eb56bf6aaff723fc10e
confidential-nt <youyoy689@gmail.com>
2022-11-06 02:12:53 +0900

    feat(요구사항): #4. 이 과정을 사용자가 입력한 숫자와 컴퓨터의 숫자가 일치할 때까지 반복한다.
    #3의 과정을 사용자가 입력한 숫자와 컴퓨터의 숫자가 일치할 때까지 반복한다.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,17 @@ const MissionUtils = require("@woowacourse/mission-utils");
 class App {
   computerNumber;
 
+  play() {
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    this.setGame();
+  }
+
+  setGame() {
+    this.setComputerNumber();
+    // console.log("computerNumber", this.computerNumber);
+    this.compareWithUserNumber();
+  }
+
   setComputerNumber() {
     const computerNumber = [];
     while (computerNumber.length < 3) {
@@ -14,34 +25,67 @@ class App {
     this.computerNumber = computerNumber;
   }
 
+  compareWithUserNumber() {
+    MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (number) => {
+      if (number.length > 3 || number.length < 3)
+        throw new Error("3자리의 숫자를 입력해주세요.");
+
+      const userNumber = parseInt(number, 10);
+
+      // if (!Number.isNaN(userNumber)) throw new RangeError();
+
+      const scoreObject = this.getScoreObject(userNumber);
+      const hint = this.getHint(scoreObject);
+
+      MissionUtils.Console.print(hint);
+
+      if (scoreObject.strike === 3) {
+        MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        this.afterGameEnded();
+        return;
+      }
+      this.compareWithUserNumber();
+    });
+  }
+
+  getScoreObject(userNumber) {
+    const scoreObject = { strike: 0, ball: 0 };
+
+    const splitedUserNumber = this.splitNumber(userNumber);
+
+    for (let i = 0; i < this.computerNumber.length; i++) {
+      this.getScore(splitedUserNumber, i, scoreObject, "strike", "ball");
+    }
+
+    return scoreObject;
+  }
+
   splitNumber(number) {
     return (number + "").split("").map((element) => parseInt(element, 10));
   }
 
-  getHint(userNumber) {
-    const splitedUserNumber = this.splitNumber(userNumber);
-
-    let ball = 0;
-    let strike = 0;
-
-    for (let i = 0; i < this.computerNumber.length; i++) {
-      for (let j = 0; j < splitedUserNumber.length; j++) {
-        if (this.computerNumber[i] === splitedUserNumber[j] && i === j) {
-          strike++;
-          continue;
-        }
-        if (this.computerNumber[i] === splitedUserNumber[j]) {
-          ball++;
-        }
+  getScore(userNumberArray, computerNumberIndex, scoreObject, ...scoreName) {
+    for (let j = 0; j < userNumberArray.length; j++) {
+      if (
+        this.computerNumber[computerNumberIndex] === userNumberArray[j] &&
+        computerNumberIndex === j
+      ) {
+        scoreObject[scoreName[0]]++;
+        continue;
+      }
+      if (this.computerNumber[computerNumberIndex] === userNumberArray[j]) {
+        scoreObject[scoreName[1]]++;
       }
     }
+  }
 
-    return ball > 0 && strike === 0
-      ? `${ball}볼`
-      : ball === 0 && strike > 0
-      ? `${strike}스트라이크`
-      : ball > 0 && strike > 0
-      ? `${ball}볼 ${strike}스트라이크`
+  getHint(scoreObject) {
+    return scoreObject.ball > 0 && scoreObject.strike === 0
+      ? `${scoreObject.ball}볼`
+      : scoreObject.ball === 0 && scoreObject.strike > 0
+      ? `${scoreObject.strike}스트라이크`
+      : scoreObject.ball > 0 && scoreObject.strike > 0
+      ? `${scoreObject.ball}볼 ${scoreObject.strike}스트라이크`
       : "낫싱";
   }
 
