d7c354848baab6252b9855731bbdb69dd1f0573d
pp449 <mma7710@naver.com>
2022-11-05 23:54:54 +0900

    feat: 게임을 시작하면 시작 text를 출력하고 랜덤 숫자를 프로퍼티에 저장하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+
 class App {
-  play() {}
+  gameStart() {
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+    this.getRandomNumbers();
+    this.printAnswer();
+  }
+
+  getRandomNumbers() {
+    this.answer = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
+  }
+
+  printAnswer() {
+    MissionUtils.Console.print(this.answer[0]);
+  }
+
+  play() {
+    this.gameStart();
+  }
 }
+
+const baseball = new App();
+baseball.play();
 
 module.exports = App;
