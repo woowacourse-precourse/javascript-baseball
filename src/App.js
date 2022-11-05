ff39bc4e43f46edf7a4408a7f23f13504fbc7ebc
Chanwoong1 <jcw7468@gmail.com>
2022-11-05 17:58:36 +0900

    refactor: 기능 구현 목록 1번 수정

--- src/App.js
+++ src/App.js
@@ -1,18 +1,77 @@
-const { Console } = require("@woowacourse/mission-utils");
+const { Console, Random } = require("@woowacourse/mission-utils");
 
+class RandomSelectNumbersByComputer {
+  randomSelectComputerNumbers() {
+    const selectedNumber = Random.pickUniqueNumbersInRange(1, 9, 3);
 
-class App {
+    return selectedNumber;
+  }
+}
 
-  play() {
-    this.startGame();
+module.exports = RandomSelectNumbersByComputer;
+
+class ValidateUserInput {
+  inputNumbers() {
+    Console.readLine("숫자를 입력해주세요 : ", this.validateUserInput);
   }
 
-  async startGame() {
-    Console.print("숫자 야구 게임을 시작합니다.");
-    await this.baseballGameStart();
+  validateUserInput(userInput) {
+    Console.print(userInput);
+  }
+}
+
+class BaseballGame {
+  constructor() {
+    this.computerNumbers =
+      new RandomSelectNumbersByComputer().randomSelectComputerNumbers();
+    this.validateUserInput = new ValidateUserInput();
+  }
+
+  playGame() {
+    Console.print('숫자 야구 게임을 시작합니다.');
+    Console.print(this.computerNumbers);
+    this.validateUserInput.inputNumbers();
     Console.close();
   }
 
+}
+
+module.exports = BaseballGame;
+
+class App {
+  constructor() {
+    this.baseballGame = new BaseballGame();
+  }
+
+  play() {
+    this.baseballGame.playGame();
+  }
+
+  // async startGame() {
+  //   Console.print("숫자 야구 게임을 시작합니다.");
+  //   await this.baseballGameStart();
+  //   const computerNumbers = await this.randomSelectComputerNumbers();
+
+  //   Console.close();
+  // }
+
+  // async baseballGameStart() {
+  //   const beforeValidNumbers = await inputNumbers("숫자를 입력해주세요 : ");
+  //   if (!validateNumbers(beforeValidNumbers)) Console.close();
+  //   Console.print('Valid Input');
+  // }
+
+  // randomSelectComputerNumbers() {
+  //   const computerNumbers = Random.pickUniqueNumbersInRange(1, 9, 3);
+  //   return computerNumbers;
+  // }
+
+  // validateNumbers(beforeValidNumbers) {
+  //   return (
+  //     typeof beforeValidNumbers !== 'string' ||
+  //     beforeValidNumbers.length !== 3
+  //   ) ? false : true;
+  // }
 }
 
 module.exports = App;
