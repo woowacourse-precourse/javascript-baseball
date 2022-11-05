fb98b78d2c35d1240973db9a2325016eac459200
jjongs2 <jjongs@hanyang.ac.kr>
2022-11-05 20:09:55 +0900

    feat: 게임이 종료된 경우 안내 메시지를 출력한 뒤 입력 대기하는 기능 구현
      - 1이 입력된 경우 게임 새로 시작
      - 2가 입력된 경우 게임 완전히 종료

--- src/App.js
+++ src/App.js
@@ -64,7 +64,26 @@ class App {
     } while (this.count.strike !== 3);
   }
 
-  play() {
+  readState() {
+    return new Promise((resolve) => {
+      MissionUtils.Console.readLine(
+        '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n',
+        (input) => {
+          this.state = Number(input);
+          resolve();
+        }
+      );
+    });
+  }
+
+  async play() {
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+    do {
+      await this.startGame();
+      MissionUtils.Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+      await this.readState();
+    } while (this.state === 1);
+    MissionUtils.Console.close();
   }
 }
 
