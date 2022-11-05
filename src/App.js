c53cb4a265868cb01967b85bd9e4d656ed839287
murpin <qw04011@gmail.com>
2022-11-06 00:22:51 +0900

    refactor(App/isValidateInputNumber): 정규표현식에 대한 에러시 재입력 기능으로 변경

--- src/App.js
+++ src/App.js
@@ -91,16 +91,19 @@ class App {
 
   isValidateInputNumber(number) {
     if (NUMBER_REGEXP.test(number)){
-      throw new Error('숫자를 입력하시길 바랍니다.');
+      Console.print('숫자를 입력하시길 바랍니다.');
+      this.inputUserNumber('숫자를 입력하세요 :');
+    }
+    if (DUPLICATION_NUMBER_REGEXP.test(number)) {
+      Console.print('중복되지 않으며, 0이 아닌 숫자를 입력해야합니다.');
+      this.inputUserNumber('숫자를 입력하세요 :');
+    }
+    if (IS_ZERO_REGEXP.test(number)) {
+      Console.print('0이 포함된 수는 입력할 수 없습니다');
+      this.inputUserNumber('숫자를 입력하세요 :');
     }
     if (number.length !== 3){
       throw new Error('3자리 수를 입력해야합니다');
-    }
-    if (DUPLICATION_NUMBER_REGEXP.test(number)) {
-      throw new Error('중복되지 않으며, 0이 아닌 숫자를 입력해야합니다.');
-    }
-    if (IS_ZERO_REGEXP.test(number)) {
-      throw new Error('0이 포함된 수는 입력할 수 없습니다');
     }
   }
 
