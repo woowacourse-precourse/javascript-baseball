1471f7043cc2e192d6fd2d5d99ab9a0e2562c7dc
KingJiwon <wldnjs0401@naver.com>
2022-11-05 23:47:11 +0900

    user입력을 배열에 저장

--- docs/README.md
+++ docs/README.md
@@ -2,4 +2,11 @@
 
 ---
 
-1. 컴퓨터가 1~9까지 중 임의의 수 3개 선택하기
+1. 컴퓨터가 1~9까지 중 서로 다른 임의의 수 3개 선택하기
+
+- getRandomComputerArray()로 컴퓨터의 random array만들기
+- pushUniqueNumber()을 통해 난수를 중복되지 않게 string으로 push하기
+
+2. 유저의 input 받아서 저장하기
+
+- pushUserInput()으로 숫자 하나씩 userInputArray에 push하기
--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
   pushUniqueNumber(arr, num) {
+    num = String(num);
     if (!arr.includes(num)) {
       return arr.push(num);
     }
@@ -12,7 +13,22 @@ class App {
       let randomNumber = MissionUtils.Random.pickNumberInRange(1, 9);
       this.pushUniqueNumber(randomComputerArray, randomNumber);
     }
+    return randomComputerArray;
   }
+  pushUserInput(arr, num) {
+    for (let i = 0; i < 3; i++) {
+      arr.push(num[i]);
+    }
+  }
+  getUserInputArray() {
+    let userInputArray = [];
+    MissionUtils.Console.readLine("숫자를 입력하세요:", (answer) => {
+      this.pushUserInput(userInputArray, answer);
+    });
+
+    return userInputArray;
+  }
+
   play() {}
 }
 // const app = new App();
