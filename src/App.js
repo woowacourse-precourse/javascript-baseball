379010f7f525f3e73e1b169271ba3a82a5fb10ca
Cobocho <rlaalsrb1111@naver.com>
2022-11-05 23:10:43 +0900

    refactor: 파일명 변경 및 출력 기능 `class`화

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 const MissionUtils = require("@woowacourse/mission-utils");
-const GameUtils = require("./gameUtils.js");
-const print = require("./print.js");
+const GameUtils = require("./GameUtils.js");
+const Print = require("./Print.js");
 const constants = require("./data/constants.js");
 
 class App {
   play() {
     this.answer = GameUtils.System.answer;
     console.log(this.answer);
-    print.start();
+    Print.GameMessage.start();
     this.submitInput();
   }
   submitInput() {
-    let userInput;
     MissionUtils.Console.readLine(constants.GAME_MESSAGE.INPUT, (input) => {
       const noSpaceInput = input.replace(/\s/g,'');
       const userInput = noSpaceInput.split('').map(number => +number);
-      console.log(userInput);
       GameUtils.Validator.isVaildAnswer(userInput);
       const result = GameUtils.System.getResult(userInput, this.answer);
-      print.result(result);
+      Print.GameMessage.result(result);
       this.isClear(result.strike);
     });
   }
@@ -35,7 +33,7 @@ class App {
   isRestart(submit) {    
     if(+submit === 1) this.play();
     if(+submit === 2) {
-      print.gameover();
+      Print.GameMessage.gameover();
       MissionUtils.Console.close();
     }
   }
--- src/print.js
+++ src/print.js
@@ -1,22 +1,20 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 const constants = require("./data/constants.js");
 
-function start() {
-  MissionUtils.Console.print(constants.GAME_MESSAGE.START);
+class GameMessage {
+  static start() {
+    MissionUtils.Console.print(constants.GAME_MESSAGE.START);
+  }  
+  static gameover() {
+    MissionUtils.Console.print(constants.GAME_MESSAGE.GAMEOVER);
+  }
+  static result(resultValue) {
+    let resultMessage = '';
+    if(resultValue.ball) resultMessage += `${resultValue.ball}${constants.RESULT_COUNT.BALL} `;
+    if(resultValue.strike) resultMessage += `${resultValue.strike}${constants.RESULT_COUNT.STRIKE}`;
+    if(resultMessage === '') resultMessage = constants.RESULT_COUNT.NOTHING;
+    MissionUtils.Console.print(resultMessage);
+  }
 }
 
-function gameover() {
-  MissionUtils.Console.print(constants.GAME_MESSAGE.GAMEOVER);
-}
-
-function result(resultValue) {
-  let resultMessage = '';
-  if(resultValue.ball) resultMessage += `${resultValue.ball}${constants.RESULT_COUNT.BALL} `;
-  if(resultValue.strike) resultMessage += `${resultValue.strike}${constants.RESULT_COUNT.STRIKE}`;
-  if(resultMessage === '') resultMessage = constants.RESULT_COUNT.NOTHING;
-  MissionUtils.Console.print(resultMessage);
-}
-
-exports.start = start;
-exports.result = result;
-exports.gameover = gameover;
+exports.GameMessage = GameMessage;
