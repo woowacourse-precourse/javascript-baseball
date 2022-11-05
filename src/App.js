11c88632839106754994bf450218c0451e3b46ef
Siby1lA <psb4644@gmail.com>
2022-11-05 20:22:49 +0900

    feat: 컴퓨터의 수인 랜덤값 3개를 생성하는 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  computerRandomNumber = () => {
+    const computer = [];
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+    return computer;
+  };
+  gameStart = () => {
+    this.computerNumber = this.computerRandomNumber();
+  };
+  play() {
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    this.gameStart();
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
