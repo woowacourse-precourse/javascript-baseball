b88c148a7e123b89647b8bce322fd78e724f2145
userHWSEO <gmldnjseocnd@gmail.com>
2022-11-05 15:04:19 +0900

    feat: 랜덤 숫자 출력 기능
    - MissionUtils 라이브러리를 활용한 랜덤 숫자 출력
    - 출력 된 숫자들로 세 자리 숫자 (ComputerNumber)를 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  play() {
+    function makeRandomNumber() {
+      let random_number = "";
+      for (let i = 0; i < 3; i++) {
+        random_number += MissionUtils.Random.pickNumberInRange(1, 9);
+      }
+      return random_number;
+    }
+  }
 }
 
 module.exports = App;
