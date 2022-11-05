9fab8742aec844cd331cda87366fffc2ebf89ec1
RosaDamascena <hsj990604@gmail.com>
2022-11-06 00:04:29 +0900

    feat : 3개의 랜덤 숫자를 뽑는 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,3 +1,18 @@
+function randomNum(numbers) {
+    let numbers = [];
+    for (let i = 0; i < 9; i++) {
+      numbers.push(i + 1);
+    }
+
+    let answer = [];
+    for (let i = 0; i < 3; i++) {
+      const index = Math.floor(Math.random() * (numbers.length - i));
+      answer.push(numbers[index]);
+      numbers.splice(index, 1);
+    }
+    console.log(answer);
+  }
+
 class App {
   play() {}
 }
