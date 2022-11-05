af614d2c62bb662a44d2eb2b745d6f7291ea1c9b
2wndrhs <2wndrhs@gmail.com>
2022-11-04 21:48:18 -0700

    feat: 사용자가 입력한 값을 볼, 스트라이크로 판단하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ const MissionUtils = require('@woowacourse/mission-utils');
 class App {
   #RANDOM_NUMBER = [];
 
-  #userInput = 0;
+  #USER_INPUT = [];
 
   // 컴퓨터가 사용할 랜덤한 숫자를 구하는 기능
   static getRandomNumber() {
@@ -21,6 +21,8 @@ class App {
       if (!App.isValidUserInput(answer)) {
         throw new Error('잘못된 값을 입력하였습니다!');
       }
+      this.#USER_INPUT = [...answer].map(number => +number);
+      this.getComputerOutput();
     });
   }
 
@@ -32,6 +34,27 @@ class App {
       return true;
     }
     return false;
+  }
+
+  // 사용자가 입력한 값을 판단하여 결과를 출력하는 기능
+  getComputerOutput() {
+    const BALL_STRIKE_MAP = this.getStrikeAndBallMap();
+  }
+
+  // 사용자가 입력한 값을 볼, 스트라이크로 판단하는 기능
+  getBallAndStrikeMap() {
+    const BALL_STRIKE_MAP = this.#USER_INPUT.reduce((acc, cur, idx) => {
+      const IS_INCLUDED = this.#RANDOM_NUMBER.includes(cur);
+      const RANDOM_INDEX = this.#RANDOM_NUMBER.indexOf(cur);
+      if (IS_INCLUDED && idx !== RANDOM_INDEX) {
+        acc.set('BALL_COUNT', (acc.get('BALL_COUNT') || 0) + 1);
+      }
+      if (IS_INCLUDED && idx === RANDOM_INDEX) {
+        acc.set('STRIKE_COUNT', (acc.get('STRIKE_COUNT') || 0) + 1);
+      }
+      return acc;
+    }, new Map());
+    return BALL_STRIKE_MAP;
   }
 
   play() {
