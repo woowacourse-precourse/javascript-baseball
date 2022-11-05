fe2442fb95289e349c31a4372014f133f8479225
loopy-lim <loopy@jnu.ac.kr>
2022-11-05 22:57:30 +0900

    docs: 온점 추가한다

--- src/Baseball/Baseball.validator.js
+++ src/Baseball/Baseball.validator.js
@@ -11,7 +11,7 @@ class BaseballValidator {
   }
   static checkNumbersLength(numbers) {
     if (numbers === undefined) {
-      throw new Error("알 수 없는 입력값입니다");
+      throw new Error("알 수 없는 입력값입니다.");
     }
     if (numbers.length !== 3) {
       throw new Error("글자가 3개가 아닙니다.");
@@ -58,7 +58,7 @@ class BaseballValidator {
   }
   static checkRestartValue(restartValue) {
     if (!(+restartValue === 1 || +restartValue === 2)) {
-      throw new Error("잘못된 입력값입니다");
+      throw new Error("잘못된 입력값입니다.");
     }
     return +restartValue === 1;
   }
