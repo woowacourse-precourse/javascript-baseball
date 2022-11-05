21ce22e0b0be2502fcd92dd65412db08e3e585c0
DESKTOP-PDMTIQO\hansu <fathlic5@gmail.com>
2022-11-06 00:38:09 +0900

    :sparkles: feat: 기능1 컴퓨터의 무작위 숫자 3개 뽑기 구현 우아한테크코스 라이브러리 API를 사용해, 상대방(컴퓨터)의 무작위 숫자 3개 뽑기 구현, 게임 시작 문구 출력 구현

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 class App {
-  play() {
-    const computerNumber = isComputerNumber();
-  }
-}
-function isComputerNumber(){
-  const MissionUtils = require("@woowacourse/mission-utils");
-  const computerNumbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
-  return computerNumbers;
+    play() {
+        const MISSION_UTILS = require("@woowacourse/mission-utils");
+        MISSION_UTILS.Console.print('숫자 야구 게임을 시작합니다.');
+    }
+
+    isComputerNumbers() {
+        const MISSION_UTILS = require("@woowacourse/mission-utils");
+        const COMPUTER_NUMBERS = [];
+        while (COMPUTER_NUMBERS.length < 3) {
+            const RANDOM_NUMBER = MISSION_UTILS.Random.pickNumberInRange(1, 9);
+            if (!COMPUTER_NUMBERS.includes(RANDOM_NUMBER)) COMPUTER_NUMBERS.push(RANDOM_NUMBER);
+        }
+        return parseInt(COMPUTER_NUMBERS.join(''))
+    }
 }
 module.exports = App;
