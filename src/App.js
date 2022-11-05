15a6a67d9649c4312db075d8a0ff9e73ef485c14
haesa <xdhaesa@gmail.com>
2022-11-05 23:15:31 +0900

    feat(App.js): 랜덤 숫자 3개 뽑기

--- src/App.js
+++ src/App.js
@@ -1,4 +1,21 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+const { Random } = MissionUtils;
+
 class App {
+  #baseballNumber = [];
+
+  constructor() {}
+
+  pickBaseballNumber() {
+    while (this.#baseballNumber.length < 3) {
+      const number = Random.pickNumberInRange(1, 9);
+
+      if (!this.#baseballNumber.includes(number)) {
+        this.#baseballNumber.push(number);
+      }
+    }
+  }
+
   play() {}
 }
 
