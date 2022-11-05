2da4a525be4a21f7631fd89c55cb6b527de275eb
daal2quf <daal2quf@gmail.com>
2022-11-05 21:06:41 +0900

    feat: 게임 시작 문구 출력 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+
+  play() {
+    this.gamaStartAlram()
+  }
+
+  gamaStartAlram() {
+    MissionUtils.Console.print("게임을 시작합니다")
+  }
+
 }
 
+const app = new App();
+app.play();
 module.exports = App;
