81d7350514386d45d2b4991d1a4a3bf154d0e102
leeuihyun <meta212121@naver.com>
2022-11-05 14:30:30 +0900

    feat : 기능별로 분류한 클래스들을 App.js에서 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+const User = require('./User');
+const GameInitMessage = require('./GameInitMessage');
+const Computer = require('./Computer');
+
 class App {
-  play() {}
+  play() {
+    const gameInitMessage = new GameInitMessage();
+    gameInitMessage.initPrint();
+
+    const computer = new Computer();
+    const computerNumbers = computer.getComputerNumbers();
+
+    const user = new User(computerNumbers);
+    user.userInputStart();
+  }
 }
 
 module.exports = App;
