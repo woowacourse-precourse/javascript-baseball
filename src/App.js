db3e81470b0d442a29793092e67ace8e012e2c13
BaeDongWoo <ehddn5410@naver.com>
2022-11-05 13:41:54 +0900

    기능 1) 상대방(컴퓨터)의 서로 다른 임의의 수 3개를 선택

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+/*const MissionUtils = require("@woowacourse/mission-utils");
 class App {
-  play() {}
+  play(){
+  }
+}*/
+class Computer {
+  constructor(computer_number){
+    this.computer_number=computer_number;
+  }
+  getcomputer_number(){
+    this.computer_number=[];
+    while (this.computer_number.length<3){
+      const number=MissionUtils.Random.pickNumberInRange(1,9);
+      if(!this.computer_number.includes(number)){
+        this.computer_number.push(number);
+      }
+    }
+    return this.computer_number;
+  }
 }
+computer=new Computer()
+console.log(computer.getcomputer_number());
 
-module.exports = App;
+//module.exports = App;*/
