f08f05862bcc4ddd8b3de5cab2e525b080d02e77
dudgp128 <20181048@sungshin.ac.kr>
2022-11-05 19:37:34 +0900

    FEAT : 입력값과 컴퓨터 숫자 비교하기

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App {
     while (computer.size < 3) {
       computer.add(MissionUtils.Random.pickNumberInRange(1, 9));
     }
-    return String([...computer].join(""));
+    return [...computer];
   }
 
   isValidInput(input) {
@@ -15,14 +15,45 @@ class App {
     );
   }
 
+  NumberToArray(number) {
+    let numArray = [];
+    while (number >= 1) {
+      numArray.unshift(number % 10);
+      number = parseInt(number / 10);
+    }
+    return numArray;
+  }
+
+  CompareInputWithComputer(input, computer) {
+    let inputArray = this.NumberToArray(input);
+
+    let result = new Map();
+
+    result.set("stright", null);
+    result.set("ball", null);
+    result.set("nothing", null);
+
+    inputArray.forEach((e, idx) => {
+      computer.indexOf(e) != -1
+        ? computer.indexOf(e) == idx
+          ? result.set("stright", result.get("stright") + 1 ?? 1)
+          : result.set("ball", result.get("ball") + 1 ?? 1)
+        : result.set("nothing", result.get("nothing") + 1 ?? 1);
+    });
+    return result;
+  }
+
+  executeError() {}
+
   play() {
     MissionUtils.Console.print("숫자게임을 시작합니다.");
     let computerNum = this.selectNum();
     while (true) {
-      MissionUtils.Console.readLine("숫자를 입력해주세요.", (number) => {
-        isValidInput(number);
+      MissionUtils.Console.readLine("숫자를 입력해주세요.", (input) => {
+        this.isValidInput(input)
+          ? this.CompareInputWithComputer(input.computerNum)
+          : this.executeError;
       });
-      break;
     }
   }
 }
