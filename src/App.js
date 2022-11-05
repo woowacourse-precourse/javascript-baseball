081c628bb56346d1e8a8bf448c492fad69141de8
sujungYu <su_moment@naver.com>
2022-11-05 15:14:47 +0900

    feat(endGameSystem) : 게임 새로 시작, 게임 종료 구분

--- src/App.js
+++ src/App.js
@@ -11,17 +11,13 @@ class App {
     return computer;
   }
 
-  postStartMessage() {
-    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
-  }
-
   postEndMessage() {
     MissionUtils.Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
     MissionUtils.Console.readLine(
       '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요',
       answer => {
         if (this.testEndAnswer(answer)) {
-          return answer;
+          this.endGameSystem(answer);
         }
       },
     );
@@ -31,6 +27,11 @@ class App {
     const testType = /[1-2]/;
     if (!testType.test(answer)) throw new Error('1, 2 중 입력하세요.');
     return answer;
+  }
+
+  endGameSystem(answer) {
+    if (answer == 1) this.play();
+    else MissionUtils.Console.close();
   }
 
   getUserAnswer(computer) {
