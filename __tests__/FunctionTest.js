15533ae8b461da6f8fcd792c396948e0c5a334f8
2yunseong <dbsdltjd3701@naver.com>
2022-11-05 21:51:17 +0900

    test: 사용자의 입력이 3자리인지 확인하는 기능 테스트 코드 작성

--- __tests__/FunctionTest.js
+++ __tests__/FunctionTest.js
@@ -14,4 +14,16 @@ describe("기능 테스트", () => {
     const result = app.generateCount([1, 3, 5]);
     expect(result).toEqual("135");
   });
+
+  test("입력 길이가 3으로 유효하면 true 반환", () => {
+    const app = new App();
+    const result = app.isVaildLength("123");
+    expect(result).toEqual(true);
+  });
+
+  test("입력 길이가 3이 아니면 false 반환", () => {
+    const app = new App();
+    const result = app.isVaildLength("1213");
+    expect(result).toEqual(false);
+  });
 });
