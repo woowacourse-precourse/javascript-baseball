570d9667eb1d0a7425cce8e6f2cdb8237a6438ad
sunny <huiseon37@gmail.com>
2022-11-05 21:03:28 +0900

    feat: user 클래스의 숫자 입력받는 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 class User {
+  inputValue;
   constructor(){
-    console.log('유저 클래스 생성');
+    this.input();
+  }
+  input(){
+    MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (answer) => {
+      this.inputValue = answer;
+    })
   }
 }
 class App {
   play() {
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
-    new User();
   }
 }
 
