4f86e883f51ca7794204841653e3021146d2f972
BB-choi <choibobae0809@gmail.com>
2022-11-05 20:25:35 +0900

    feat: 사용자 입력 올바르지 않은 경우 에러 문구 표시

--- src/App.js
+++ src/App.js
@@ -6,6 +6,31 @@ class App {
     this.count = count;
     this.minNum = minNum;
     this.maxNum = maxNum;
+    this.MESSAGES = {
+      START: "숫자 야구 게임을 시작합니다.",
+      END: "게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
+      INSERT_NUMBER: `${this.count}자리 숫자(각 자리 수: ${this.minNum}~${this.maxNum})를 입력해주세요 : `,
+      ERROR: {
+        INSERT: "올바르지 않은 입력입니다.\n",
+        RANGE: `각 자리의 수는 ${this.minNum}부터 ${this.maxNum}까지 입력할 수 있습니다.`,
+        TYPE: "숫자만 입력할 수 있습니다.",
+        DIGIT: `${this.count}자리 수가 입력되어야 합니다.`,
+        DUPLICATE: "각 자리의 수는 중복되지 않아야 합니다.",
+        END: "\n프로그램을 종료합니다.",
+      },
+      RESULT: {
+        NOTHING: "낫싱",
+        CORRECT(num) {
+          return `${num}개의 숫자를 모두 맞히셨습니다!`;
+        },
+        BALL(num) {
+          return `${num}볼`;
+        },
+        STRIKE(num) {
+          return `${num}스트라이크`;
+        },
+      },
+    };
   }
 
   set gameNumber(number) {
@@ -23,22 +48,6 @@ class App {
   get userNumber() {
     return this._userNumber;
   }
-
-  MESSAGES = {
-    START: "숫자 야구 게임을 시작합니다.",
-    END: "게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
-    INSERT_NUMBER: "숫자를 입력해주세요 : ",
-    NOTHING: "낫싱",
-    CORRECT(num) {
-      return `${num}개의 숫자를 모두 맞히셨습니다!`;
-    },
-    BALL(num) {
-      return `${num}볼`;
-    },
-    STRIKE(num) {
-      return `${num}스트라이크`;
-    },
-  };
 
   printMessage(msg) {
     return Console.print(msg);
@@ -87,19 +96,27 @@ class App {
     const numbers = input.split("").map(Number);
 
     if (!this.isValidDigit(numbers)) {
-      throw new Error("isValidDigit");
+      throw new Error(
+        `${this.MESSAGES.ERROR.INSERT}${this.MESSAGES.ERROR.DIGIT}${this.MESSAGES.ERROR.END}`
+      );
     }
 
     if (!numbers.every(this.isNumber)) {
-      throw new Error("isNumber");
+      throw new TypeError(
+        `${this.MESSAGES.ERROR.INSERT}${this.MESSAGES.ERROR.TYPE}${this.MESSAGES.ERROR.END}`
+      );
     }
 
     if (!numbers.every(this.isValidNumber.bind(this))) {
-      throw new Error("isValidNumber");
+      throw new RangeError(
+        `${this.MESSAGES.ERROR.INSERT}${this.MESSAGES.ERROR.RANGE}${this.MESSAGES.ERROR.END}`
+      );
     }
 
     if (this.hasDuplicateElement(numbers)) {
-      throw new Error("hasDuplicateElement");
+      throw new Error(
+        `${this.MESSAGES.ERROR.INSERT}${this.MESSAGES.ERROR.DUPLICATE}${this.MESSAGES.ERROR.END}`
+      );
     }
 
     return true;
