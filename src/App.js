ba26a04fe819b071dcebc0ea1d13db0a7928d1b6
TaeHyung KIM <th1122kim@gmail.com>
2022-11-05 15:51:25 +0900

    feat: 랜덤으로 컴퓨터의 3자리 수 만드는 기능 구현

--- src/App.js
+++ src/App.js
@@ -3,7 +3,21 @@ const Console = MissionUtils.Console;
 const Random = MissionUtils.Random;
 
 class App {
-  play() {}
+  play() {
+    // 랜덤으로 컴퓨터의 3자리 수 만드는 기능
+    let RandomNumber = "";
+
+    const make_Random = () => {
+      let arr = [];
+      while (arr.length < 3) {
+        let num = Random.pickNumberInRange(1, 9);
+        if (!arr.includes(num)) arr.push(num);
+      }
+      RandomNumber = arr.join("");
+    };
+
+    make_Random();
+  }
 }
 
 module.exports = App;
