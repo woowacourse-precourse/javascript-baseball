255342f1fc8aaf8bcc8ae7e3123a6bfac46d205c
Jaehyeon <jaehyeonp74@gmail.com>
2022-11-06 04:16:56 +0900

    feat: endOrRetry 메소드 구현
    - try catch문 이용해 예외처리
    - 입력 값에 따라 재시도 또는 종료

--- src/App.js
+++ src/App.js
@@ -36,7 +36,23 @@ class App {
     return this.endOrRetry();
   }
 
-  endOrRetry() {}
+  endOrRetry() {
+    MissionUtils.Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+    MissionUtils.Console.print('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.');
+    MissionUtils.Console.readLine('', input => {
+      try {
+        const endOrRetryInput = this.isValidEndOrRetryInput(input);
+        if (endOrRetryInput === 1) {
+          this.play();
+        }
+        if (endOrRetryInput === 2) {
+          MissionUtils.Console.close();
+        }
+      } catch (error) {
+        this.wrongInput(error);
+      }
+    });
+  }
 
   isValidEndOrRetryInput(answer) {
     if (answer === '1' || answer === '2') {
