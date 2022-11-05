c1e949dcdf62528a80b2287be7ddf0f428274c20
GC-Park <kuyt3122@naver.com>
2022-11-05 20:35:13 +0900

    fix:예외 처리를 위한 코드들로 인한 에러 수정

--- src/App.js
+++ src/App.js
@@ -74,16 +74,13 @@ class App {
     }
 
     finishAndRestart() {
-        MissionUtils.Console.readLine(
-            '3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n',
-            (answer) => {
-                if (Number(answer) === 1) this.play()
-                else if (Number(answer) === 2) {
-                    MissionUtils.Console.print('게임 종료')
-                    MissionUtils.Console.close()
-                }
+        MissionUtils.Console.readLine('3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n',(answer) => {
+            if (Number(answer) === 1) this.play()
+            else if (Number(answer) === 2) {
+                MissionUtils.Console.print('게임 종료')
+                MissionUtils.Console.close()
             }
-        )
+        })
     }
 
     exceptionHandler(userInput) {
