ddb80c1877571aa17f5239f2633e4485f4a2b667
sery <yunsel2@gmail.com>
2022-11-05 12:43:36 +0900

    test: 유효하지 않은 숫자 입력시 에러처리 테스트 수정

--- __tests__/featureListTest.js
+++ __tests__/featureListTest.js
@@ -43,15 +43,12 @@ describe('기능 구현 목록 테스트', () => {
     expect([...new Set(random)].length).toEqual(3);
   });
 
-  test('사용자가 입력한 값이 유효하지 않을 때 에러처리', () => {
-    const answers = ['012'];
-    // 사용자 입력값 계속 받게 함수 수정 후 answers에 '2346', 'ab3', '123' 추가해서 테스트
+  test('유효하지 않은 값이 들어 왔을 때 에러처리', () => {
+    const numbers = '1230';
 
-    mockQuestions(answers);
-
-    expect(() => game.play()).toThrow(
-      '1부터 9까지 서로 다른 숫자 3개를 입력해주세요'
-    );
+    expect(() => {
+      game.isValidInputNumber(numbers, RANDOM_NUMBER.RANGE);
+    }).toThrow('1부터 9까지 서로 다른 숫자 3개를 입력해주세요');
   });
 
   test('랜덤 값과 사용자 입력한 값 비교', () => {
