89778176dac353913d6d57f5a858e0b9a5454027
Yoonkyoung Kim <100656920+Yoonkyoungme@users.noreply.github.com>
2022-11-05 16:11:40 +0900

    feat: 사용자가 입력한 숫자 유효성 검사

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
     const computerNumber = this.createComputerNumber();
     const userNumber = this.inputUserNumber();
-    // console.log(computerNumber, userNumber);
+    console.log(computerNumber, userNumber);
   }
 
   // 컴퓨터의 숫자 랜덤으로 생성한다. (1부터 9까지 서로 다른 수로 이루어진 3자리의 수)
@@ -26,9 +26,31 @@ class App {
     let userNumber;
 
     MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (num) => {
-      userNumber = num;
+      const number = this.vaildCheck(num);
+      number = userNumber;
     });
     return userNumber;
+  }
+
+  // 사용자가 입력한 숫자 유효성 검사
+  vaildCheck(number) {
+    const set = new Set(number);
+    const len = number.length;
+    //  입력한 수가 3자리 수인가
+    if (len !== 3) {
+      throw "숫자를 입력하세요.";
+    }
+    // 서로 다른 숫자인가
+    if (len === set.size) {
+      throw "서로 다른 수를 입력하세요.";
+    }
+    // 1 ~ 9 범위에 해당하는 수를 입력했는가
+    [...number].map((num, index) => {
+      if (!(parseInt(num) <= 1 && parseInt(num) <= 9)) {
+        throw "1 ~ 9 범위에 해당하는 숫자를 입력하세요.";
+      }
+    });
+    return number;
   }
 }
 
