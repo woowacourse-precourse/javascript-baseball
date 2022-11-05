86aba271e15b51ae6a29dd675c40c8044e2ef135
MyungAe <ladiescheong@naver.com>
2022-11-05 23:47:28 +0900

    feat(System): 기능 7, 8번 합쳐서 구현

--- src/System.js
+++ src/System.js
@@ -49,12 +49,22 @@ class System {
       MissionUtils.Console.print(`${scoreboard[0]}스트라이크`);
       if (scoreboard[0] === 3) {
         MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        this.isStartOverGame();
       }
     } else {
       MissionUtils.Console.print(
         `${scoreboard[1]}볼 ${scoreboard[0]}스트라이크`
       );
     }
+  }
+
+  isStartOverGame() {
+    MissionUtils.Console.print(
+      "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
+    );
+    MissionUtils.Console.readLine("", (answer) => {
+      // do something
+    });
   }
 }
 
