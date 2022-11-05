952058a3ef1f6bdec676682065e02d676005783c
Dongkeun <hongdongk@naver.com>
2022-11-05 23:16:41 +0900

    기능6: 게임종료하거나 다시하는함수

--- src/App.js
+++ src/App.js
@@ -7,4 +7,21 @@ class App {
   }
 }
 
+const RegameorEnd = () => {
+  MissionUtils.Console.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.', (input) => {
+    if(input == 1){
+      let Randomnumber = ComputerRandomnumber();
+    }
+    else if(input == 2){
+      MissionUtils.Console.print("게임 종료");
+      MissionUtils.Console.close();
+    }
+    else {
+      MissionUtils.Console.print("1,2 둘중에 입력하세요!!");
+    }
+  });
+};
+
 module.exports = App;
+exports.RegameorEnd = RegameorEnd;
+
