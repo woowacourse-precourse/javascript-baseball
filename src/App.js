bb852413746cefe83280c60d88d8c8c150f6d6c1
김희진 <choha941108@gmail.com>
2022-11-05 21:13:49 +0900

    feat(build_answer): 3자리 임의의 수 배열 생성
    * 미션 라이브러리 통해 1부터 10까지의 임의의 수 3개 반환하는 함수사용
    * 3개 숫자 반환 자료형 및 미중복 숫자 확인

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
   play() {}
+
+  //build_answer
+  build_answer() {
+    const answer = MissionUtils.Random.pickUniqueNumbersInRange(1, 10, 3);
+    return answer;
+  }
 }
 
 module.exports = App;
