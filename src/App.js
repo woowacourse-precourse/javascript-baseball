d36755141dbf6415dd9652699b44a8ff70f4ac8a
hae-on <solbi2004@naver.com>
2022-11-06 00:19:26 +0900

    feat: 1~9 사이의 랜덤한 수 3개 생성

--- src/App.js
+++ src/App.js
@@ -2,7 +2,14 @@ const MissionUtils = require('@woowacourse/mission-utils');
 const { Console, Random } = MissionUtils;
 
 class App {
-  play() {}
+  makeRandomNumbers() {
+    const computerNumber = Random.pickUniqueNumbersInRange(1, 9, 3);
+    this.computerNumberArray = computerNumber;
+  }
+
+  play() {
+    this.makeRandomNumbers();
+  }
 }
 
 const app = new App();
