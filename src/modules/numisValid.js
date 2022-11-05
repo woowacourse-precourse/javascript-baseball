e33cc6c7ec3af19c7ae7ad237de64cd321f6df5c
Hyun Su <tlsgustn93@naver.com>
2022-11-05 15:49:19 +0900

    refactor: 가독성 수정

--- src/modules/numisValid.js
+++ src/modules/numisValid.js
@@ -1,11 +1,7 @@
 function isValid(input) {
-    if(input.length != 3) throw new Error();
-    let number = input.map((num) => {
-        if(isNaN(num)) throw new Error();
-        return num;
-    })
-    if(new Set(number).size != 3) throw new Error();
-    return number;
+    if(new Set(input).size !== 3) return false;
+    const checkNumber = input.every(v => !isNaN(v) ? true : false)
+    return checkNumber ? input : false;
 }
 
 exports.isValid = isValid
