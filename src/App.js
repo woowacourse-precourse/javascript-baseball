0a69f241b477f759bab5be9eb3291fde3772c9db
dladncks1217 <dlaxodud1217@gmail.com>
2022-11-06 03:15:31 +0900

    feat: 게임 시작 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+const { Console } = require("@woowacourse/mission-utils");
+const makeRandomNumber = require("./utils/makeRandomNumber");
+
 class App {
-  play() {}
+  play() {
+    Console.print("숫자 야구 게임을 시작합니다.");
+    const randomNum = makeRandomNumber();
+    this.gameStart(randomNum);
+  }
+
+  gameStart(answer) {}
 }
 
 module.exports = App;
