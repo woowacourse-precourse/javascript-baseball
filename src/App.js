2c3488cea144a07155f4bcbe75d29b19c11b8eba
GC-Park <kuyt3122@naver.com>
2022-11-05 16:13:41 +0900

    feat:볼 수 체크 함수 추가

--- src/App.js
+++ src/App.js
@@ -1,47 +1,56 @@
-const MissionUtils = require("@woowacourse/mission-utils");
+const MissionUtils = require('@woowacourse/mission-utils')
 
 class App {
-
-  constructor(){
-    this.computerInput=[]
-    this.userInput=[]
-  }
-
-  play() {
-    this.computerInput = this.randomMakeNumber();
-    this.userInputNumber();
-  }
-
-  randomMakeNumber(){
-    const computer = [];
-    while (computer.length < 3) {
-      const number = MissionUtils.Random.pickNumberInRange(1, 9);
-      if (!computer.includes(number)) {
-        computer.push(number);
-      }
+    constructor() {
+        this.computerInput = []
+        this.userInput = []
     }
-    return computer;
-  }
 
-  strikeCount(computerInput, userInput){
-    let strikeTotal=0;
-    computerInput.map((computer, index)=>{
-      if(computer===Number(userInput[index])) strikeTotal++;
-    })
-    return strikeTotal;
-  }
+    play() {
+        this.computerInput = this.randomMakeNumber()
+        this.userInputNumber()
+    }
 
-  compareNumber(userInput){
+    randomMakeNumber() {
+        const computer = []
+        while (computer.length < 3) {
+            const number = MissionUtils.Random.pickNumberInRange(1, 9)
+            if (!computer.includes(number)) {
+                computer.push(number)
+            }
+        }
+        return computer
+    }
 
-  }
+    strikeCount(computerInput, userInput) {
+        let strikeTotal = 0
+        computerInput.map((computer, index) => {
+            if (computer === Number(userInput[index])) strikeTotal++
+        })
+        return strikeTotal
+    }
 
-  userInputNumber(){
-    MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (answer) => {
-      this.compareNumber(answer.split(''));
-    });
-  }
-};
+    ballCount(computerInput, userInput) {
+        let ballTotal = 0
+        computerInput.map((computer, index) => {
+            if (
+                computer !== Number(userInput[index]) &&
+                computerInput.includes(Number(userInput[index]))
+            )
+                ballTotal++
+        })
+        return ballTotal
+    }
 
-const app = new App();
-app.play();
-module.exports = App;
+    compareNumber(userInput) {}
+
+    userInputNumber() {
+        MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (answer) => {
+            this.compareNumber(answer.split(''))
+        })
+    }
+}
+
+const app = new App()
+app.play()
+module.exports = App
