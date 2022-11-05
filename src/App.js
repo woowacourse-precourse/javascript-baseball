41b6b0bfa0f52dbe69735ace3e9c9d22cafdc31e
hozzijeong <ghwls0905@naver.com>
2022-11-06 00:25:10 +0900

    Feat: 스트라이크, 볼 계산하는 함수 구현

--- src/App.js
+++ src/App.js
@@ -9,12 +9,12 @@ function App() {
   };
   this.play = () => {
     Console.print("숫자 야구 게임을 시작합니다.");
-    Console.readLine("숫자를 입력해주세요: ", (number) => {
-      Console.print(`입력 숫자: ${number} ${this.state.computer}`);
+    Console.readLine("숫자를 입력해주세요: ", (input) => {
+      compareInputWithComputer(input);
     });
   };
 
-  this.init = () => {
+  const setComputerNumber = () => {
     const computer = [];
     while (computer.length < 3) {
       const number = Random.pickNumberInRange(1, 9);
@@ -22,6 +22,39 @@ function App() {
         computer.push(number);
       }
     }
+    return computer;
+  };
+
+  const checkUserInputValidation = (userInput) => {
+    /**
+     * 오류 입력 처리
+     * 숫자가 1~9가 아닌 경우
+     * 같은 숫자가 2개이상 포함된 경우
+     * 입력값이 3자리가 아닌 경우
+     *
+     */
+  };
+
+  const getStrikeCount = (user, computer) => {
+    return computer.filter((number, index) => number === user[index]).length;
+  };
+
+  const getBallCount = (user, computer) => {
+    return user.reduce((acc, cur, index) => {
+      if (cur === computer[index]) return acc;
+      if (computer.includes(cur)) return acc + 1;
+      else return acc;
+    }, 0);
+  };
+
+  const compareInputWithComputer = (input) => {
+    const userInput = input.split("");
+    const strikeCount = getStrikeCount(userInput, this.state.computer);
+    const ballCount = getBallCount(userInput, this.state.computer);
+  };
+
+  this.init = () => {
+    const computer = setComputerNumber();
     this.setState({ computer });
   };
 
@@ -37,4 +70,4 @@ function App() {
 const app = new App();
 app.play();
 
-module.exports = App;
+// module.exports = App;
