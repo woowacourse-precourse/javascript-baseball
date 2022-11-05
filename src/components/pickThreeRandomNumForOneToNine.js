80a2f4a6a1521dd576eb865966afdcadbd8db65d
Ryan-Dia <cjfdnjs158@naver.com>
2022-11-05 14:11:19 +0900

    refactor(랜덤숫자뽑기): 프로그램 요구 사항에 맞게 Random.pickNumberInRange()사용

--- src/components/pickThreeRandomNumForOneToNine.js
+++ src/components/pickThreeRandomNumForOneToNine.js
@@ -4,12 +4,11 @@ const pickThreeRandomNumForOneToNine = (RandomNumData = []) => {
   const NUM_REQUIRED = 3;
   if (RandomNumData.length === NUM_REQUIRED) return RandomNumData;
   const numberPickUp = [...RandomNumData];
-  const numArray = Array.from({ length: 9 }, (num, i) => i + 1);
-  const randomNum = MissionUtils.Random.pickNumberInList(numArray);
-  if (!numberPickUp.length && !randomNum)
-    return pickThreeRandomNumForOneToNine();
+  const randomNum = MissionUtils.Random.pickNumberInRange(1, 9);
   if (numberPickUp.includes(randomNum))
     return pickThreeRandomNumForOneToNine(numberPickUp);
   numberPickUp.push(randomNum);
   return pickThreeRandomNumForOneToNine(numberPickUp);
 };
+
+module.exports = pickThreeRandomNumForOneToNine;
