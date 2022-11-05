e3937095b78887cdf7d52c8c445cd88e7b7899e6
dlwl98 <ff981113@hanyang.ac.kr>
2022-11-05 13:44:49 +0900

    feat: GameStatusUI 클래스에 Action 멤버 추가

--- src/GameStatusUI.js
+++ src/GameStatusUI.js
@@ -1,12 +1,23 @@
 const { Console } = require('@woowacourse/mission-utils');
 
 class GameStatusUI {
+  #action;
+
   constructor() {
-    this.update = (newGameStatus) => {
-      if (newGameStatus === 'START') {
-        Console.print('숫자 야구 게임을 시작합니다.');
-      }
-    };
+    this.#action = {};
+  }
+
+  injection(newAction) {
+    this.#action = newAction;
+  }
+
+  update(newGameStatus) {
+    if (newGameStatus === 'START') {
+      Console.print('숫자 야구 게임을 시작합니다.');
+    }
+    this.#action.sendToDispatcher({
+      type: 'game-start',
+    });
   }
 }
 
