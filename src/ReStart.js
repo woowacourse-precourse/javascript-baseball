7d655c83fcad48cabbe74b5379b3a1eec7eb8d21
Rki0 <pky00823@gmail.com>
2022-11-05 12:54:08 +0900

    refactor: rename function to be clearer
    Renamed decideReStart().
    In order to modularize the function that better fits the name,
    the function name used in the past has been modified.
    
    BREAKING CHANGE: decideReStart() was renamed.
    
    Before:
    decideReStart() - get player input whether to restart
    
    After:
    getRestartInput() - same function. Just changed name.

--- src/ReStart.js
+++ src/ReStart.js
@@ -1,7 +1,7 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
 class ReStart {
-  decideReStart() {
+  getRestartInput() {
     MissionUtils.Console.readLine(
       "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n",
       (userInput) => {
