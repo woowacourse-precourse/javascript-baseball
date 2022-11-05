989070ac690bef590bf9880ef0488c351fbefce4
jinseo <ghkdwlstj99@gmail.com>
2022-11-05 16:11:02 +0900

    refactor: userInput class 로직 수정

---
+++ src/userInput.js
@@ -0,0 +1,42 @@
+const { MissionUtils } = require("@woowacourse/mission-utils");
+ 
+class userInput{
+    constructor(){
+        MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+        MissionUtils.Console.readLine('숫자를 입력해주세요 :' , (answer) => {
+        this.threeDigitsError(answer);
+        this.includeZeroError(answer);
+        this.isNotNumberError(answer);
+        this.overlapNumberError(answer);
+        });
+    }
+
+    threeDigitsError(answer){
+    if(answer.length !==3){
+        throw "Error (숫자의 길이가 3이 아닙니다.)" ;
+    }
+}
+
+    includeZeroError(answer){
+    if(answer.includes(0)){
+        throw "Error (숫자가 0을 포함하고 있습니다)" ;
+    } 
+}
+
+    isNotNumberError(answer){
+    if(!isNan(answer)){
+        throw "Error (숫자 형식이 아닙니다.)" ;
+    }
+}
+
+    overlapNumberError(answer){
+    let userInput = Array.from(answer);
+    for(let i = 0 ; i < userInput.length; i++){
+        if(userInput[i] == userInput[i+1]){
+            throw "Error (중복된 숫자가 있습니다.)" ;
+        }
+    }
+}
+}
+
+module.exports = userInput;
