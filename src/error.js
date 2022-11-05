3ea1fe3c81092eac4f41d949ea9fbe49d1f69798
Yoo Seung Guk <dbtmd878@naver.com>
2022-11-05 15:10:47 +0900

    refactor:함수형에서 클래스로 변경

--- src/error.js
+++ src/error.js
@@ -1,46 +1,59 @@
-function inputNumberSizeCheck(INPUT_USER_NUMBER) {
-  if (INPUT_USER_NUMBER.length !== 3) {
-    return true;
+module.exports = class ErrorClass {
+  throwError() {
+    throw new Error("잘못된 값을 입력하셨습니다.");
   }
-  return false;
-}
 
-function overlapNumberCheck(INPUT_USER_NUMBER) {
-  const overLapNumberSet = new Set(INPUT_USER_NUMBER);
-  if (overLapNumberSet.size !== 3) {
-    return true;
-  }
-  return false;
-}
-function numberTypeCheck(INPUT_USER_NUMBER) {
-  const numberTypeString = INPUT_USER_NUMBER;
-  for (let letter of numberTypeString) {
-    if (typeof parseInt(letter, 10) !== "number") {
+  inputNumberSizeCheck(INPUT_USER_NUMBER) {
+    if (INPUT_USER_NUMBER.length !== 3) {
       return true;
     }
+    return false;
   }
-  return false;
-}
-function zeroCheck(INPUT_USER_NUMBER) {
-  for (let letter of INPUT_USER_NUMBER) {
-    if (parseInt(letter, 10) === 0) {
+
+  overlapNumberCheck(INPUT_USER_NUMBER) {
+    const overLapNumberSet = new Set(INPUT_USER_NUMBER);
+    if (overLapNumberSet.size !== 3) {
       return true;
     }
+    return false;
   }
-  return false;
-}
-
-function spaceCheck(INPUT_USER_NUMBER) {
-  for (let letter of INPUT_USER_NUMBER) {
-    if (isNaN(parseInt(letter, 10))) {
-      return true;
+  numberTypeCheck(INPUT_USER_NUMBER) {
+    const numberTypeString = INPUT_USER_NUMBER;
+    for (let letter of numberTypeString) {
+      if (typeof parseInt(letter, 10) !== "number") {
+        return true;
+      }
     }
+    return false;
   }
-  return false;
-}
+  zeroCheck(INPUT_USER_NUMBER) {
+    for (let letter of INPUT_USER_NUMBER) {
+      if (parseInt(letter, 10) === 0) {
+        return true;
+      }
+    }
+    return false;
+  }
 
-exports.zeroCheck = zeroCheck;
-exports.inputNumberSizeCheck = inputNumberSizeCheck;
-exports.overlapNumberCheck = overlapNumberCheck;
-exports.numberTypeCheck = numberTypeCheck;
-exports.spaceCheck = spaceCheck;
+  spaceCheck(INPUT_USER_NUMBER) {
+    for (let letter of INPUT_USER_NUMBER) {
+      if (isNaN(parseInt(letter, 10))) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  errorCheck(number) {
+    if (
+      this.inputNumberSizeCheck(number) ||
+      this.overlapNumberCheck(number) ||
+      this.numberTypeCheck(number) ||
+      this.zeroCheck(number) ||
+      this.spaceCheck(number)
+    ) {
+      return this.throwError();
+    }
+    return false;
+  }
+};
