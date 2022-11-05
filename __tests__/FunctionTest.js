5bc4317d073b70d5e95f45b8e5e2652b655b5b46
2yunseong <dbsdltjd3701@naver.com>
2022-11-05 21:45:20 +0900

    test: 컴퓨터 카운트 생성 테스트 코드 작성

--- __tests__/FunctionTest.js
+++ __tests__/FunctionTest.js
@@ -1,14 +1,17 @@
 const App = require("../src/App");
 const MissionUtils = require("@woowacourse/mission-utils");
 
-const mockGenerate = () => {
-  MissionUtils.Random.pickUnique;
-};
 describe("기능 테스트", () => {
   test("게임 스타트 출력", () => {
     const app = new App();
     const logSpy = jest.spyOn(MissionUtils.Console, "print");
     app.gameStart();
     expect(logSpy).toHaveBeenCalledWith("숫자 야구 게임을 시작합니다.");
+  });
+
+  test("컴퓨터 카운트 생성", () => {
+    const app = new App();
+    const result = app.generateCount([1, 3, 5]);
+    expect(result).toEqual("135");
   });
 });
