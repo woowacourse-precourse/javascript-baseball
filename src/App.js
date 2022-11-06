2ebb4d8fd7c3fcf53fd21e09aab7b8559cff4f7a
cruelladevil <dev.timetravel@gmail.com>
2022-11-06 23:45:46 +0900

    feat: create computer numbers

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-const { Console } = require('@woowacourse/mission-utils');
+const { Console, Random } = require('@woowacourse/mission-utils');
 
 class App {
   play() {
     Console.print('숫자 야구 게임을 시작합니다.');
+    const computer = this.createUniqueNumbers(1, 9, 3);
+  }
+
+  createUniqueNumbers(start, end, count) {
+    const numberSet = new Set();
+
+    while (numberSet.size !== count) {
+      const number = Random.pickNumberInRange(start, end);
+      if (!numberSet.has(number)) numberSet.add(number);
+    }
+
+    return [...numberSet];
   }
 }
 
