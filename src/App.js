2fe8afa7cbf0daf8aff5d802e4a106d53b4410d1
sojinjang <jsojin6119@gmail.com>
2022-11-07 01:28:17 +0900

    :sparkles: feat(input): 입력값 유효성 검사 통과 못하면 error throw

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ class App {
   play() {
     const computerNum = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
-    MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (userInput) => {
-      const validator = new Validator(userInput.split(""));
-      console.log(validator.isValidInput());
-      MissionUtils.Console.close();
-    });
+    MissionUtils.Console.readLine(
+      "각 자리 숫자가 1에서 9 사이인 서로 다른 세자리 숫자를 입력해주세요 : ",
+      (userInput) => {
+        const validator = new Validator(userInput.split(""));
+        if (!validator.isValidInput()) throw new Error("입력값을 확인하세요.");
+      }
+    );
   }
 }
 
