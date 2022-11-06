8b51d790f699c535265cf28145ef7421a5e4cab5
ksone02 <eksovus@gmail.com>
2022-11-06 17:25:56 +0900

    feat: 입력 값 컴퓨터 숫자와 비교해 볼, 스트라이크 개수 구하기

--- src/App.js
+++ src/App.js
@@ -10,8 +10,26 @@ class App {
   recursiveInput() {
     Console.readLine(MESSAGE.INPUT, inputNum => {
       CheckException(inputNum, 3);
-      this.restart();
+      const { ball, strike } = this.check(inputNum);
+      if (strike === 3) {
+        Console.print('정답');
+        this.restart();
+      }
     });
+  }
+
+  check(inputNum) {
+    const count = {
+      ball: 0,
+      strike: 0,
+    };
+    const inputNumArr = inputNum.split('');
+
+    inputNumArr.forEach((num, index) => {
+      if (parseInt(num) === this.computerNum[index]) count.strike += 1;
+      else if (this.computerNum.includes(parseInt(num))) count.ball += 1;
+    });
+    return count;
   }
 
   restart() {
@@ -22,8 +40,12 @@ class App {
   play() {
     Console.print(MESSAGE.START);
     this.computerNum = this.randomArr();
+
     this.recursiveInput();
   }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
