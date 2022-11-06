0d0aefa54d3a1a868c1ed67bcb90f3f8f2d2984c
Leemainsw <s2018s15@e-mirim.hs.kr>
2022-11-07 00:57:23 +0900

    feat : 1부터 9까지 중복되지 않은 임의의 수 3개 저장

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 const { Random, Console } = MissionUtils;
 class App {
+  randomNumbers;
+
   play() {
     Console.print("숫자 야구 게임을 시작합니다.");
+  }
+
+  saveRandomNumbers() {
+    this.randomNumbers = [];
+    while (this.randomNumbers.length < 3) {
+      const number = Random.pickNumberInRange(1, 9);
+      if (!this.randomNumbers.includes(number)) {
+        this.randomNumbers.push(number);
+      }
+    }
   }
 }
 
