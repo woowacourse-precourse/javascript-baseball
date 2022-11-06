67e0859ec18b98e0fd13e6059248001e1004b8e8
pp449 <mma7710@naver.com>
2022-11-06 16:40:01 +0900

    feat: 사용자에게 3개 숫자를 입력받는 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 
 class App {
-  gameStart() {
-    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
-    this.getRandomNumbers();
-    this.printAnswer();
+  constructor() {
+    this.answer = [];
   }
 
   getRandomNumbers() {
     this.answer = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
   }
 
-  printAnswer() {
-    MissionUtils.Console.print(this.answer[0]);
+  userInputNumber() {
+    MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (numbers) => {
+      console.log(numbers);
+    });
+  }
+
+  gameStart() {
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+    this.getRandomNumbers();
+    this.userInputNumber();
   }
 
   play() {
