0889fbb31be65f1857fd680ce3edd29c13f41bdb
su2333066 <su2333066@naver.com>
2022-11-07 06:46:30 +0900

    2번 기능 구현

--- src/App.js
+++ src/App.js
@@ -3,7 +3,23 @@ const MissionUtils = require("@woowacourse/mission-utils");
 class App {
   constructor() {
     this.selectComputerNumber = [];
-    this.inputNumber = "";
+    this.inputNumber = [];
+  }
+
+  inputNum() {
+    // 사용자의 숫자 입력
+    MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (number) => {
+      if (number.length !== 3) {
+        throw "3자리수를 입력하세요.";
+      }
+      for (let i = 0; i < number.length; i++) {
+        if (!this.inputNumber.includes(Number(number[i]))) {
+          this.inputNumber.push(Number(number[i]));
+        } else {
+          throw "서로 다른 3자리수를 입력하세요.";
+        }
+      }
+    });
   }
 
   selectRandomNum() {
@@ -16,32 +32,16 @@ class App {
     }
   }
 
-  inputNum() {
-    // 사용자의 숫자 입력
-    MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (number) => {
-      // if (typeof number !== "number") {
-      //   throw "숫자를 입력하세요";
-      // }
-      // if (String(number).length !== 3) {
-      //   throw "서로다른 3자리수를 입력하세요.";
-      // }
-      this.inputNumber = String(number);
-    });
-  }
-
   play() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
 
     this.selectRandomNum();
-    console.log(this.selectComputerNumber);
-    // 정답을 맞출때까지 반복
-    // while (this.inputNumber !== this.computerNumber) {
-    // }
+    // console.log(this.selectComputerNumber);
     this.inputNum();
   }
 }
 
-//module.exports = App;
+// module.exports = App;
 
 const app = new App();
 app.play();
