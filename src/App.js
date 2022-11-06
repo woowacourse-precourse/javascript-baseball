b9d222ab3d823f5ef0b4b51debdca0139ceef167
이건우 <lifeflavor@igeon-uui-MacBookPro.local>
2022-11-06 22:47:55 +0900

    1. 컴퓨터 숫자 설정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  play() {
+    const computer = [];
+    let strikeCount;
+    let ballCount;
+    let nothing;
+
+    // 컴퓨터의 랜덤한 숫자 3개
+    function computerNumbers() {
+      while (computer.length < 3) {
+        let number = MissionUtils.Random.pickNumberInRange(1, 9);
+        if (!computer.includes(number)) {
+          computer.push(number);
+        }
+      }
+    }
+
+    computerNumbers();
+  }
 }
 
 module.exports = App;
