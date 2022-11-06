5bdc7ead37a7240aaae99d31de234c71944ce6b9
min <hororonoodle@gmail.com>
2022-11-07 00:41:47 +0900

    feat : [T3-1] 사용자 오류 입력 항목에 대한 분기 처리 및 게임 시작 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 const GameMessage = require('./Constants/gameMessage');
 class App {
-  #answer = '';
+  answer = '';
   userInput = '';
+  result = '';
 
   constructor() {
-    this.answer = '';
-    this.result = '';
+    this.inputAssert = {
+      assertLength: (input) => input.length === 3,
+      assertInteger: (input) => Number.isInteger(+input),
+      assertPositive: (input) => Math.sign(input) === 1,
+    };
   }
 
   printWelcomeMessage() {
@@ -27,18 +31,26 @@ class App {
 
   getUserInput() {
     MissionUtils.Console.readLine(GameMessage.QUESTION_MESSAGE, (input) => {
+      const { assertLength, assertInteger, assertPositive } = this.inputAssert;
+
+      if (!assertLength(input))
+        throw Error(GameMessage.WRONG_INPUT_ERROR_MESSAGE);
+      if (!assertInteger(input))
+        throw Error(GameMessage.WRONG_INPUT_ERROR_MESSAGE);
+      if (!assertPositive(input))
+        throw Error(GameMessage.WRONG_INPUT_ERROR_MESSAGE);
+
       this.userInput = input;
     });
   }
 
-  get userInput() {
-    return this.userInput;
+  start() {
+    this.answerGenerator();
+    this.getUserInput();
   }
 
   play() {
     this.printWelcomeMessage();
-    this.answerGenerator();
-    this.getUserInput();
   }
 }
 
