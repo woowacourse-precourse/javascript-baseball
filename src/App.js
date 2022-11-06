cfe67835dd9936cdaff67483ea3c04bee81794fe
WaiNaat <qoli0703@gmail.com>
2022-11-06 13:38:27 +0900

    feat(App.js): add function for generating radom answer
    MissionUtils.Random.pickUniqueNumbersInRange 를 사용하면 편하게 정답을 만들 수 있지만
    프로그래밍 요구 사항에서 Random.pickNumberInRange 를 사용하라고 했으므로 코드에서 사용하지 않음.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+// import * as MissionUtils from '@woowacourse/mission-utils';
+const MissionUtils = require('@woowacourse/mission-utils');
+
+const GAME_ANSWER_LENGTH = 3;
+const MAX_VALID_NUMBER = 9;
+const MIN_VALID_NUMBER = 1;
+
 class App {
   play() {}
+
+  static generateAnswer() {
+    const answer = [];
+    while (answer.length < GAME_ANSWER_LENGTH) {
+      App.addUniqueRandomNumber(answer);
+    }
+    return answer;
+  }
+
+  static addUniqueRandomNumber(array) {
+    const number = MissionUtils.Random.pickNumberInRange(MIN_VALID_NUMBER, MAX_VALID_NUMBER);
+    if (!array.includes(number)) {
+      array.push(number);
+    }
+  }
 }
 
 module.exports = App;
