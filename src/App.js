bf62e0b518f1968fc6d87c14d09f828665438437
cybersyn2 <quandonestpetit@outlook.com>
2022-11-06 19:54:47 +0900

    draft computer.judge

--- src/App.js
+++ src/App.js
@@ -19,6 +19,24 @@ class Computer {
       }
     }
   }
+
+  judge(message) {
+    let ball = 0,
+      strike = 0;
+
+    for (let i = 0; i < 3; i++) {
+      if (!this._digits.includes(message[i])) continue;
+      ball += 1;
+    }
+
+    for (let i = 0; i < 3; i++) {
+      if (message[i] != this._digits[i]) continue;
+      strike += 1;
+      ball -= 1;
+    }
+
+    return [strike, ball];
+  }
 }
 
 module.exports = App;
