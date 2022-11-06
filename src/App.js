7d617c4f6be70e6bdcded34ae6aeb60321e8018a
myung-j-p <jmpack97@gmail.com>
2022-11-07 04:46:57 +0900

    feat: 사용자에게 숫자 입력받는 기능 추가

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
   play() {
+    console.log('숫자 야구 게임을 시작합니다.')
     // 1.컴퓨터 리스트 생성
     const computerLs = new Computer();
     computerLs.makeComNumLs();
@@ -19,7 +20,21 @@ class Computer extends App {
 }
 
 class User extends App {
-  getUserLs(){}
+  getUserLs () {
+    MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (answer) => {
+      MissionUtils.Console.close();
+      return answer
+    });
+  }
 }
 
 module.exports = App;
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> test <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<??
+const app = new App();
+const computerLs = new Computer();
+const userLs = new User();
+
+userLs.getUserLs()
+
+// console.log(computerLs.makeComNumLs());
