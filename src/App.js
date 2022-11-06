3989955f8aaec35f7e695eafdab44ab79db3926a
SSY1203 <tlstjsdud566@naver.com>
2022-11-06 21:03:57 +0900

    feat : App.js에서 비교한 결과를 출력하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -47,7 +47,33 @@ class App {
         ball++;
       }
     }
-    // TODO result display function
+    this.displayResult(strike, ball);
+  }
+
+  displayResult(strike, ball) {
+    let result = '';
+
+    if (strike === 0 && ball === 0) {
+      result = '낫싱';
+    } else {
+      if (ball > 0) {
+        result += `${ball}볼 `;
+      }
+      if (strike > 0) {
+        result += `${strike}스트라이크`;
+      }
+      if (strike === 3) {
+        result = '3스트라이크';
+      }
+    }
+    Console.print(result);
+
+    if (result === '3스트라이크') {
+      Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+      // TODO game restart check function
+    } else {
+      this.getUserNumber();
+    }
   }
 
   play() {}
