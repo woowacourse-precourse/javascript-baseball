7b7d2549271c399053bef76310b439d73b33ac0b
ksone02 <eksovus@gmail.com>
2022-11-06 16:46:07 +0900

    feat: 컴퓨터 3개 랜덤 숫자 뽑기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+const { Console, Random } = require('@woowacourse/mission-utils');
+const { MESSAGE } = require('./data/constants');
+
 class App {
-  play() {}
+  randomArr() {
+    return Random.pickUniqueNumbersInRange(1, 9, 3);
+  }
+  play() {
+    Console.print(MESSAGE.START);
+  }
 }
 
 module.exports = App;
