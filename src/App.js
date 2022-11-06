a9830d55b304ebb4686af3e9cd846bc3e54f8245
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2022-11-07 00:47:08 +0900

    feat(Controller):  게임 결과를 App.js에서 받아 this에 저장 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 const Render = require("./Render");
 const CheckInputValid = require("./CheckValid");
+const GameJudgment = require("./GameJudgment");
 function numToArr(num) {
   return [...String(num)];
 }
@@ -13,12 +14,21 @@ class App {
       const checkInputValid = new CheckInputValid({
         userNum: this.userNum,
       });
+
       try {
         checkInputValid.checkValidation();
       } catch (error) {
         throw new Error(error);
       }
     });
+
+    const gameJudgment = new GameJudgment({
+      user: this.userNum,
+      computer: this.computer,
+    });
+    const [ballCount, strikeCount] = gameJudgment.judgement();
+    this.ball = ballCount;
+    this.strikeCount = strikeCount;
   }
 }
 
