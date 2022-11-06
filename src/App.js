db9b9b0d4a81fbe02e4a6e386cce29fe632d787a
chasj0326@naver.com <62418379+chasj0326@users.noreply.github.com>
2022-11-07 02:27:49 +0900

    feat: App.js - 게임 종료 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
 function startGame(){
-  // 1. 게임 시작
   MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
 }
 
 function makeProblem(NUMBER_LENGTH){
-  // 2. 문제 생성
   const problemNumberList = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, NUMBER_LENGTH);
   return problemNumberList;
 }
@@ -46,36 +44,44 @@ function isSuccess(strike_count){
   return strike_count===3;
 }
 
-function getResults(problem){
-  // 3. 숫자 입력 받기
-  MissionUtils.Console.readLine("숫자를 입력해 주세요. :", (input)=>{
+function playOneRound(problem){
+  MissionUtils.Console.readLine("숫자를 입력해 주세요. : ", (input)=>{
     const answer = input;
-    // 4. 결과 계산
     const result = makeResult(answer, problem);
     const [ball_count, strike_count] = result;
-    // 5. 결과 출력
     showResult(ball_count, strike_count);
-    // 6. 결과 반영
     if(!isSuccess(strike_count)){
-      return getResults(problem);
+      return playOneRound(problem);
     }
-    MissionUtils.Console.close(); // 결과 반영 제대로 되었는지 테스트
-    // 3 스트라이크 -> 콘솔 close
+    MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    restartGame();
+  })
+}
+
+function playGame(){
+  const NUMBER_LENGTH = 3;
+  const problem = makeProblem(NUMBER_LENGTH);
+
+  playOneRound(problem);
+}
+
+function restartGame(){
+  MissionUtils.Console.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+  MissionUtils.Console.readLine("",(input)=>{
+    if(input==='1'){
+      playGame();
+    }
+    if(input==='2'){
+      MissionUtils.Console.close();
+    }
   })
 }
 
 
-
 class App {
   play() {
-    //1. 게임 시작
     startGame();
-
-    // 2. 문제 생성
-    const NUMBER_LENGTH = 3;
-    let problem = makeProblem(NUMBER_LENGTH);
-
-    getResults(problem);
+    playGame();
   }
 }
 
--- src/Message.js
+++
@@ -1,9 +0,0 @@
-const MissionUtils = require("@woowacourse/mission-utils");
-
-class Message {
-  startGame(){
-    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
-  }
-}
-
-module.exports = Message;
