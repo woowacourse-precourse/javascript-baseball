e8a1cc51fc39beed103a3f1f8f04179a630a9e6d
nerdchanii <nerdchanii@gmail.com>
2022-11-07 02:14:10 +0900

    feat(game): 게임 클래스를 커맨드로 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+const Game = require('./Game');
 const Io = require('./Io');
 class App {
   constructor() {
+    this.Game = new Game();
     this.Io = Io;
   }
 
   play() {
     this.Io.output('숫자 야구 게임을 시작합니다.');
+    this.Game.playCommand();
   }
 }
 
---
+++ src/Game.js
@@ -0,0 +1,16 @@
+const Io = require('./Io');
+class Game {
+  constructor() {
+  }
+
+  /**
+   * 커맨드를 실행한다.
+   * @public 
+   * @method
+   * @return {} void
+   * @description 게임을 시작한다. 클래스 외부에서 호출한다. 
+   */
+  playCommand () {}
+}
+
+module.exports = Game;
