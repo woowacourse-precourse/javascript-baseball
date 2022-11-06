fea798c527efce2ca1b02e33cb635fe6580c1cdd
myung-j-p <jmpack97@gmail.com>
2022-11-07 04:15:52 +0900

    feat: 컴퓨터 중복없는 랜덤 3자리수 생성기능 추가

--- src/App.js
+++ src/App.js
@@ -5,15 +5,17 @@ class App {
   play() {
     // 1.컴퓨터 리스트 생성
     const computerLs = new Computer();
-
+    computerLs.makeComNumLs();
     // 2.유저 리스트 입력
     const userLs = new User();
-
+    userLs.getUserLs();
   }
 }
 
 class Computer extends App {
-  makeComNumLs(){}
+  makeComNumLs(){
+    return MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
+  }
 }
 
 class User extends App {
