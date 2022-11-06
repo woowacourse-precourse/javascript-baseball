0e4df045d486f3c49bdea676155bad8aafe915f2
wnw <fkaus65@naver.com>
2022-11-06 21:39:16 +0900

    feat(getStrikeBallCount): add getStrikeBallCount
    getStrikeBallCount: Count the number of strikes and balls

--- src/Baseball.js
+++ src/Baseball.js
@@ -11,6 +11,8 @@ class Baseball {
   playGame() {
     Console.print(SYSTEM_MESSAGE.START);
     this.randomNumber = Computer.getRandomNumber();
+
+    this.getUserNumber();
   }
 
   getUserNumber() {
@@ -19,6 +21,20 @@ class Baseball {
 
       this.getStrikeBallCount(this.randomNumber, userInput);
     });
+  }
+
+  getStrikeBallCount(randomNumber, userInput) {
+    const userNumber = userInput.toString();
+    let [STRIKE, BALL] = [BASIC_NUMBER.INIT, BASIC_NUMBER.INIT];
+
+    randomNumber
+      .map((num) => `${num}`)
+      .forEach((num, index) => {
+        if (num === userNumber[index]) STRIKE++;
+        else if (userNumber.includes(num)) BALL++;
+      });
+
+    this.printStrikeBallCountMessage(STRIKE, BALL);
   }
 }
 
