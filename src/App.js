77b3973ebd368d3a5f42109de75d38a4a5549508
unsnruu <unsnruu@gmail.com>
2022-11-06 22:24:50 +0900

    feat : 사용자의 입력을 검증하는 메서드 생성

--- src/App.js
+++ src/App.js
@@ -25,6 +25,21 @@ class App {
 
     return results;
   }
+  isRepeated(inputs) {
+    const visited = Array.from({ length: 10 }, (_) => false);
+    for (let i = 0; i < inputs.length; i++) {
+      const curNum = inputs[i];
+      if (visited[curNum]) return true;
+      visited[curNum] = true;
+    }
+    return false;
+  }
+  validateInput(inputs) {
+    //음수나, 9초과의 수가 입력된다면 숫자 길이에서 걸러지기 때문에 별도의 처리를 하지 않았습니다.
+    if (inputs.length !== 3) throw new Error("세자리의 수를 입력해주세요.");
+    if (isNaN(inputs.join(""))) throw new Error("숫자가 아닙니다.");
+    if (this.isRepeated(inputs)) throw new Error("반복되는 숫자가 있습니다.");
+  }
   play() {}
 }
 
