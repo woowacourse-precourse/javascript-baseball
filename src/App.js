6cef8446efbd60c1e729f1a8771f47f161edfcad
HoberMin <102784200+HoberMin@users.noreply.github.com>
2022-11-07 02:42:10 +0900

    feat : 랜덤숫자 생성 함수

--- src/App.js
+++ src/App.js
@@ -9,8 +9,17 @@ function play() {
   proceedGame(computerNum);
 }
 
+function MakeNum() {
+  const randomNumber = [];
+  while (randomNumber.length < 3) {
+    const number = Random.pickNumberInRange(1, 9);
+    if (!randomNumber.includes(number.toString())) {
+      randomNumber.push(number.toString());
     }
   }
+  return randomNumber;
+}
+
       }
       }
   }
