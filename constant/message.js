bef54f4916b65a733477fbf96eaeeb1cfb678f21
oio337a <oio337a@gmail.com>
2022-11-06 19:40:05 +0900

    refactor : chage constants

--- constant/message.js
+++ constant/message.js
@@ -1,14 +1,12 @@
 const GAME_START = "숫자 야구 게임을 시작합니다.";
 const USER_INPUT_REQUEST = "숫자를 입력해 주세요 : ";
-
-const INVALID_LENGTH = "3자리 숫자를 입력해 주세요.";
-const INVALID_RANGE = "1부터 9까지의 숫자를 입력해 주세요.";
-const INVALID_DUPLICATE = "중복된 숫자가 있습니다.";
+const ERROR = "유효하지 않은 입력값입니다.";
+const GAME_END =
+  "3개의 숫자를 모두 맞히셨습니다! 게임종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
 module.exports = {
   GAME_START,
   USER_INPUT_REQUEST,
-  INVALID_DUPLICATE,
-  INVALID_LENGTH,
-  INVALID_RANGE,
+  ERROR,
+  GAME_END,
 };
