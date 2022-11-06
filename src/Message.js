5e736433520b7633ad44fc74f8a638a03951ede2
yan9vvoojin <yan9vvoojin@naver.com>
2022-11-06 17:21:09 +0900

    feat: [Message.js] 출력 메시지 추가

---
+++ src/Message.js
@@ -0,0 +1,9 @@
+const START_MESSAGE = '숫자 야구 게임을 시작합니다.';
+const END_MESSAGE = '3개의 숫자를 모두 맞히셨습니다! 게임 종료';
+const ERROR_MESSAGE = '1부터 9까지 서로 다른 3개의 숫자를 입력해주세요.';
+const REQUEST_MESSAGE = '숫자를 입력해주세요 : ';
+const RESTART_MESSAGE = '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n';
+
+const NOTHING = '낫싱';
+const BALL = '볼';
+const STRIKE = '스트라이크';
