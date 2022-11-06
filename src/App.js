6b9ab66178a2beb543e2b22661c2abe4e223ffcf
unsnruu <unsnruu@gmail.com>
2022-11-06 22:21:39 +0900

    feat : generateAnswer 메서드 구현
    겹치지 않는 세자리의 숫자를 생성하는 generateAnswer 메서드를 구현하였습니다.

--- src/App.js
+++ src/App.js
@@ -9,6 +9,22 @@ class App {
   generateNumber() {
     return this.utils.Random.pickNumberInRange(1, 9);
   }
+  generateAnswer() {
+    const visited = Array.from({ length: 10 }, (_) => false);
+    const results = [];
+
+    while (results.length < 3) {
+      let newNumber = this.generateNumber();
+
+      while (visited[newNumber]) {
+        newNumber = this.generateNumber();
+      }
+      visited[newNumber] = true;
+      results.push(newNumber);
+    }
+
+    return results;
+  }
   play() {}
 }
 
