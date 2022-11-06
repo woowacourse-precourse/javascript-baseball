c87bc309ccbe1ca4ac7b968e472b7fa6a8f80860
Chaeduk <kkj2551@naver.com>
2022-11-06 18:13:16 +0900

    feat(숫자 입력 받기) : 1~9까지 입력 테스트 케이스 작성

--- __tests__/InputTest.js
+++ __tests__/InputTest.js
@@ -9,19 +9,43 @@ describe('1~9까지 서로 다른 3자리 숫자 입력 받기', () => {
   });
   test('3자리 숫자가 정상적으로 입력되면 true를 반환한다.', () => {
     const numberBaseball = new NumberBaseball();
-    const input = '123';
+    const input = ['1', '2', '3'];
     const result = numberBaseball.inputValidCheck(input);
     expect(result).toEqual(true);
   });
   test('3자리 이하의 숫자가 입력되면 false를 반환한다.', () => {
     const numberBaseball = new NumberBaseball();
-    const input = '12';
+    const input = ['1', '2'];
     const result = numberBaseball.inputValidCheck(input);
     expect(result).toEqual(false);
   });
   test('3자리 이상의 숫자가 입력되면 false를 반환한다.', () => {
     const numberBaseball = new NumberBaseball();
-    const input = '1234';
+    const input = ['1', '2', '3', '4'];
+    const result = numberBaseball.inputValidCheck(input);
+    expect(result).toEqual(false);
+  });
+  test('서로 다른 수 3자리 수를 입력받으면 true를 반환한다.', () => {
+    const numberBaseball = new NumberBaseball();
+    const input = ['1', '2', '3'];
+    const result = numberBaseball.inputValidCheck(input);
+    expect(result).toEqual(true);
+  });
+  test('세자리 수 중 중복된 수가 있다면 false를 반환한다.', () => {
+    const numberBaseball = new NumberBaseball();
+    const input = ['1', '2', '1'];
+    const result = numberBaseball.inputValidCheck(input);
+    expect(result).toEqual(false);
+  });
+  test('1~9까지 수를 입력받았다면 true를 반환한다.', () => {
+    const numberBaseball = new NumberBaseball();
+    const input = ['1', '2', '3'];
+    const result = numberBaseball.inputValidCheck(input);
+    expect(result).toEqual(true);
+  });
+  test('1~9가 아닌 값이 입력받았다면 false를 반환한다', () => {
+    const numberBaseball = new NumberBaseball();
+    const input = ['가', '나', '다'];
     const result = numberBaseball.inputValidCheck(input);
     expect(result).toEqual(false);
   });
