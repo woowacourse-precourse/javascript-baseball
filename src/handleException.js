33dbb2d69c1f12d972ec02b9a730c303b02d0c90
semnil5202 <semnil5202@gmail.com>
2022-11-07 05:28:31 +0900

    feat: 0 입력 시 예외 처리 기능
    주어진 요구사항에는 1에서 9사이의 숫자만 받을 수 있으므로 0은 예외 처리 대상이다.

--- src/handleException.js
+++ src/handleException.js
@@ -2,6 +2,7 @@ function handleException(inputs) {
   if (exceptionCase(inputs) === 1) throw "숫자만 입력할 수 있습니다.";
   if (exceptionCase(inputs) === 2) throw "중복된 수는 입력할 수 없습니다.";
   if (exceptionCase(inputs) === 3) throw "세 자리 수만 입력할 수 있습니다.";
+  if (exceptionCase(inputs) === 4) throw "0은 입력할 수 없습니다.";
 }
 
 function exceptionCase(inputs) {
@@ -9,6 +10,7 @@ function exceptionCase(inputs) {
   if (isNaN(inputs.join(""))) return 1;
   if (inputs.length !== uniqueInputs.length) return 2;
   if (inputs.length !== 3) return 3;
+  if (inputs.includes(0)) return 4;
 }
 
 module.exports = handleException;
