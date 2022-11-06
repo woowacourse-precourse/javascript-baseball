a3519e72bd5455a4e2d396722b40aacad0f37f65
ksone02 <eksovus@gmail.com>
2022-11-06 16:57:27 +0900

    feat: 플레이어 숫자 반복 입력

--- src/App.js
+++ src/App.js
@@ -5,8 +5,22 @@ class App {
   randomArr() {
     return Random.pickUniqueNumbersInRange(1, 9, 3);
   }
+
+  recursiveInput() {
+    Console.readLine(MESSAGE.INPUT, inputNum => {
+      this.restart();
+    });
+  }
+
+  restart() {
+    this.computerNum = this.randomArr();
+    this.recursiveInput();
+  }
+
   play() {
     Console.print(MESSAGE.START);
+    this.computerNum = this.randomArr();
+    this.recursiveInput();
   }
 }
 
