59baa4e39019394ba77defc1c16e16e5612a6ece
2je0 <bulljy@naver.com>
2022-11-07 03:06:34 +0900

    ✨ feat: randomGenerator 구현
    랜덤으로 중복되지 않는 세개의 숫자를 만들어 배열로 반환하는 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+
+const { Random } = MissionUtils;
 class App {
   play() {}
+
+  randomGenerator() {
+    const ret = [];
+    while (ret.length < 3) {
+      const num = Random.pickNumberInRange(1, 9);
+      if (ret.indexOf(num) === -1) ret.push(num);
+    }
+    return ret;
+  }
 }
 
 module.exports = App;
