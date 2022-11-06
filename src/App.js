8b9cbfe41f4ed358b5ec06d1ec35ff8a2df60db8
minsoo <als494@naver.com>
2022-11-06 22:42:28 +0900

    refactor: class에 맞게 리팩토링

--- src/App.js
+++ src/App.js
@@ -1,86 +1,87 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
-// 1. 컴퓨터가 뽑은 랜덤 숫자 3개 배열로 만드는 함수
-function randomNums() {
-  let nums = [];
-  while (nums.length < 3) {
-    let number = MissionUtils.Random.pickNumberInRange(1, 9);
-    if (!nums.includes(number)) {
-      nums.push(number);
-    }
-  }
-  return nums;
-}
+class App {
+  constructor() {}
 
-// 2. 사용자에게 숫자 3개 받는 함수
-function readNums() {
-  let user;
-  MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (answer) => {
-    user = answer.split("").map((string) => Number(string));
-    user.map((num) => {
-      if (isNaN(num)) {
-        throw "Not a number";
+  // 1. 컴퓨터가 뽑은 랜덤 숫자 3개 배열로 만드는 함수
+  randomNums() {
+    let nums = [];
+    while (nums.length < 3) {
+      let number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!nums.includes(number)) {
+        nums.push(number);
+      }
+    }
+    return nums;
+  }
+
+  // 2. 사용자에게 숫자 3개 받는 함수
+  readNums() {
+    let user = "";
+    MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (answer) => {
+      user = answer.split("").map((string) => Number(string));
+      user.map((num) => {
+        if (isNaN(num)) {
+          throw "Not a number";
+        }
+      });
+      if (user.includes(0)) {
+        throw "0 exists";
+      } else if (user.length !== 3) {
+        throw "Not three digits";
       }
     });
-    if (user.includes(0)) {
-      throw "0 exists";
-    } else if (user.length !== 3) {
-      throw "Not three digits";
-    }
-  });
-  return user;
-}
-
-// 3. 컴퓨터 숫자와 사용자 숫자 비교하는 함수
-function compareNums(computer, user) {
-  let ball = 0;
-  let strike = 0;
-
-  for (let i = 0; i < user.length; i++) {
-    if (user[i] === computer[i]) {
-      strike++;
-    } else if (computer.includes(user[i])) {
-      ball++;
-    }
+    return user;
   }
-  return [ball, strike];
-}
 
-// 4. 3번에 비교한 결과를 바탕으로 출력해주는 함수
-function printResult(score) {
-  let ball = score[0];
-  let strike = score[1];
-  if (strike === 3) {
-    MissionUtils.Console.print("3스트라이크");
-    MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-    return true;
-  } else if (ball === 0 && strike === 0) {
-    MissionUtils.Console.print("낫싱");
-  } else if (strike === 0) {
-    MissionUtils.Console.print(`${ball}볼`);
-  } else if (ball === 0) {
-    MissionUtils.Console.print(`${strike}스트라이크`);
-  } else {
-    MissionUtils.Console.print(`${ball}볼 ${strike}스트라이크`);
+  // 3. 컴퓨터 숫자와 사용자 숫자 비교하는 함수
+  compareNums(computer, user) {
+    let ball = 0;
+    let strike = 0;
+
+    for (let i = 0; i < user.length; i++) {
+      if (user[i] === computer[i]) {
+        strike++;
+      } else if (computer.includes(user[i])) {
+        ball++;
+      }
+    }
+    return [ball, strike];
   }
-  return false;
-}
 
-class App {
+  // 4. 3번에 비교한 결과를 바탕으로 출력해주는 함수
+  printResult(score) {
+    let ball = score[0];
+    let strike = score[1];
+    
+    if (strike === 3) {
+      MissionUtils.Console.print("3스트라이크");
+      MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+      return true;
+    } 
+    
+    if (ball === 0 && strike === 0) {
+      MissionUtils.Console.print("낫싱");
+    } else if (strike === 0) {
+      MissionUtils.Console.print(`${ball}볼`);
+    } else if (ball === 0) {
+      MissionUtils.Console.print(`${strike}스트라이크`);
+    } else {
+      MissionUtils.Console.print(`${ball}볼 ${strike}스트라이크`);
+    }
+    return false;
+  }
+
   play() {
     let check = 1;
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
-    // let computer = [];
     while (check === 1) {
-      let computer = randomNums();
-      let user;
-      let score;
-      let result;
+      let computer = this.randomNums();
 
       while (1) {
-        user = readNums();
-        score = compareNums(computer, user);
-        result = printResult(score);
+        let user = this.readNums();
+        let score = this.compareNums(computer, user);
+        let result = this.printResult(score);
         if (result) break;
       }
 
@@ -95,4 +96,7 @@ class App {
   }
 }
 
-module.exports = App;
+const baseballGame = new App();
+baseballGame.play();
+
+export default App;
