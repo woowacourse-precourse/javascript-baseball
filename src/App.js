eb28b2105aed812fefb37dafde285bf9cdf7989f
SeungMiL <tmskqj@naver.com>
2022-11-06 11:53:56 +0900

    feat: 유저 인풋기능 추가

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ class App {
   play() {
     const baseBallGame = new BaseBallPlay();
     baseBallGame.start();
-
+    baseBallGame.repeatContext();
   }
 }
 
@@ -15,16 +15,29 @@ class RandomNumber {
       const number = MissionUtils.Random.pickNumberInRange(1, 9);
       if (!computer.includes(number)) {
         computer.push(number);
-      };
-    };
-  };
-};
+      }
+    }
+
+    return computer;
+  }
+}
 
 class BaseBallPlay {
   start() {
-    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
-  };
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+  }
 
-};
+  userInputHandler(computer) {
+    MissionUtils.Console.readLine("숫자를 입력해주세요 :", (input) => {
+      MissionUtils.Console.print(`테스트 : ${computer} ,${input}`);
+    });
+  }
+
+  repeatContext() {
+    const computerAnswer = new RandomNumber();
+    const computer = computerAnswer.getNumber();
+    this.userInputHandler(computer);
+  }
+}
 
 module.exports = App;
