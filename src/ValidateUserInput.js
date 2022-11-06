3b776c19dc1ef1c85001f848da4e2a7f17256324
Tory <88531407+1two13@users.noreply.github.com>
2022-11-06 15:26:11 +0900

    refactor(ValidateUserInput): ERROR_COMMENT 값 및 위치 변경

--- src/ValidateUserInput.js
+++ src/ValidateUserInput.js
@@ -2,13 +2,11 @@ class ValidateUserInput {
   constructor() {
     this.numberInRange = /^[1-9]+$/;
     this.pickedNumber = [];
+    this.ERROR_COMMENT =
+      "1부터 9까지 서로 다른 수로 이루어진 3자리 숫자를 입력해주세요.";
   }
 
   validate = (pickedNumberByUser) => {
-    const ERROR_COMMENT = new Error(
-      "1부터 9까지 서로 다른 수로 이루어진 3자리 숫자를 입력해주세요."
-    );
-
     // 서로 다른 3자리 수를 입력하지 않은 경우
     pickedNumberByUser
       .split("")
@@ -16,7 +14,7 @@ class ValidateUserInput {
         !this.pickedNumber.includes(el) ? this.pickedNumber.push(el) : ""
       );
     if (this.pickedNumber.length !== 3) {
-      throw ERROR_COMMENT;
+      throw new Error(this.ERROR_COMMENT);
     }
 
     if (
@@ -24,7 +22,7 @@ class ValidateUserInput {
       // 1 ~ 9가 아닌 다른 값을 입력한 경우
       !this.numberInRange.test(pickedNumberByUser)
     ) {
-      throw ERROR_COMMENT;
+      throw new Error(this.ERROR_COMMENT);
     }
   };
 }
