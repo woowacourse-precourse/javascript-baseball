2dbc6b598d2c3087d30c26c6a4107387244573a7
HamZik <b1y9mn@gmail.com>
2022-11-06 15:58:17 +0900

    feat: sayStart 함수 추가
    게임 시작 문구를 출력하는 함수 sayStart를 추가하였습니다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+const MissionUtils = require("@woowacourse/mission-utils");
 class App {
-  play() {}
+  play() {
+    
+    function sayStart() {
+      console.log('숫자 야구 게임을 시작합니다.');
+    };
+
+    sayStart();
+  }
 }
+
+let app = new App();
+app.play();
 
 module.exports = App;
