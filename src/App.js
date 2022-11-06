dec3c2673a7589d9871b2330062692281a931580
energy1-914 <dlawldnjs5@daum.net>
2022-11-07 04:20:33 +0900

    fix: split메소드 삭제(타입에러)

--- src/App.js
+++ src/App.js
@@ -13,18 +13,14 @@ class App {
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
     MissionUtils.Console.readLine('숫자를 입력해주세요 :', (userInputNumber) => {
       this.userInputNumber = userInputNumber;
-      //this.countMethod(this.userInputNumber, this.computerNumber)
+      this.countMethod(this.userInputNumber, this.computerNumber)
     });
     // MissionUtils.Console.close();
   }
 
-  splitMethod(input) {
-    return input.toString().split('');
-  }
-
   countMethod(userNumber, computerNumber) {
-    const userString = this.splitMethod(userNumber);
-    const computerString = this.splitMethod(computerNumber);
+    const userString = this.userInputNumber.split('');
+    const computerString = this.computerNumber.split('');
     
     for ( let i=0; i<3; i++ ) {
       if ( userString[i] === computerString[i] ) {
@@ -57,3 +53,5 @@ module.exports = App;
 
 const test = new App;
 test.play();
+// test.countMethod();
+// test.printMessageMethod();
