96ae9e97eef82f9e2890f57613a301486c8f1029
woo-jk <201702042@o.cnu.ac.kr>
2022-11-06 15:35:17 +0900

    test: 게임 종료 테스트 코드 작성

---
+++ __tests__/EndGameTest.js
@@ -0,0 +1,45 @@
+const App = require("../src/App");
+const MissionUtils = require("@woowacourse/mission-utils");
+
+const mockQuestion = (input) => {
+  MissionUtils.Console.readLine = jest.fn();
+  MissionUtils.Console.readLine.mockImplementationOnce((question, callback) => {
+    callback(input);
+  });
+};
+
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(MissionUtils.Console, "print");
+  logSpy.mockClear();
+  return logSpy;
+};
+
+describe.only("게임 종료 테스트", () => {
+  test("게임 종료 문구 출력 확인", () => {
+    const logSpy = getLogSpy();
+    const app = new App();
+    app.endPlayerTurn();
+
+    expect(logSpy).toHaveBeenCalledWith("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+  });
+
+  test("재시작 테스트", () => {
+    const app = new App();
+    const playSpy = jest.spyOn(app, "play");
+
+    app.endPlayerTurn();
+    mockQuestion(1);
+
+    expect(playSpy).toHaveBeenCalled();
+  });
+
+  test("프로그램 종료 테스트", () => {
+    const app = new App();
+    const closeSpy = jest.spyOn(MissionUtils.Console, "close");
+
+    app.endPlayerTurn();
+    mockQuestion(2);
+
+    expect(closeSpy).toHaveBeenCalled();
+  });
+});
--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ class App {
       MissionUtils.Console.print(resultOutput);
 
       if (strikeCount === 3) {
-        MissionUtils.Console.print("게임 종료");
+        MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
         MissionUtils.Console.close();
       }
       if (strikeCount !== 3) {
@@ -48,6 +48,7 @@ class App {
       }
     });
   }
+  endPlayerTurn() {}
 }
 
 const app = new App();
