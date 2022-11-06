08401abd1fff4c86a871b271a1a7cfcefdfc177d
cloudedpanther <rickeykoh@ajou.ac.kr>
2022-11-07 00:54:14 +0900

    fix(bug fix): Output class
    Modify requrie statement of MissionUtils
    Add MissionUtils.Console.close() in the end of printToUser method

--- src/Output.js
+++ src/Output.js
@@ -1,4 +1,4 @@
-const { MissionUtils } = require("@woowacourse/mission-utils");
+const MissionUtils = require("@woowacourse/mission-utils");
 
 const WELCOME_MESSAGE = "숫자 야구 게임을 시작합니다.";
 const END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
@@ -6,6 +6,7 @@ const END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료"
 class Output {
   static printToUser(message) {
     MissionUtils.Console.print(message);
+    MissionUtils.Console.close();
   }
 }
 
