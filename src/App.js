1b417c807fb9f5f44c9af01f4fd04f314c302dc0
Suji Cha <suji0509@naver.com>
2022-11-06 23:01:01 +0900

    feat: 컴퓨터가 1~9까지 서로 다른 수로 이루어진 3자리 수 뽑기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
 class App {
-  play() {}
+  play() {
+    const MissionUtils = require("@woowacourse/mission-utils");
+
+    console.log("숫자 야구 게임을 시작합니다.");
+
+    // 1. 컴퓨터 : 1부터 9까지 서로 다른 수로 이루어진 3자리의 수를 뽑기
+    let answer = 0;
+    selectNumber();
+    console.log("answer:", answer);
+
+    function selectNumber() {
+      let num_candidate = MissionUtils.Random.pickNumberInRange(1, 999);
+      num_candidate = num_candidate.toString();
+      if (
+        num_candidate[0] !== num_candidate[1] &&
+        num_candidate[1] !== num_candidate[2] &&
+        num_candidate[2] !== num_candidate[0]
+      ) {
+        answer = num_candidate;
+        return answer;
+      } else {
+        selectNumber();
+      }
+    }
+  }
 }
 
 module.exports = App;
+
+let game = new App();
+
+game.play();
