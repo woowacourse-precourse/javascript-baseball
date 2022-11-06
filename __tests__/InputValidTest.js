a16f789b8170c116ccc5778e17210242e1ed50c5
kyw0716 <kyw0716@naver.com>
2022-11-06 10:22:47 +0900

    test: 입력값 유효성 검사  테스트 세분화
    게임중 => 중복되는 값 입력, 숫자가 아닌 값 입력
    게임후 => 1, 2 이외의 값 입력

--- __tests__/InputValidTest.js
+++ __tests__/InputValidTest.js
@@ -1,14 +1,59 @@
-const getIsInputValueValid = require("../src/ValidCheck");
+const MissionUtils = require("@woowacourse/mission-utils");
+const App = require("../src/App");
+
+const mockQuestions = (answers) => {
+  MissionUtils.Console.readLine = jest.fn();
+  answers.reduce((acc, input) => {
+    return acc.mockImplementationOnce((question, callback) => {
+      callback(input);
+    });
+  }, MissionUtils.Console.readLine);
+};
+
+const mockRandoms = (numbers) => {
+  MissionUtils.Random.pickNumberInRange = jest.fn();
+  numbers.reduce((acc, number) => {
+    return acc.mockReturnValueOnce(number);
+  }, MissionUtils.Random.pickNumberInRange);
+};
 
 describe("입력값 유효 체크 테스트", () => {
-  // TODO: 게임 종료 후 입력값 유효 테스트 추가하기
-  test("입력값이 유효한 값이 아니라면 false가 리턴된다.", () => {
-    const testWordsForInvalid = ["1234", "122", "12d"];
-    testWordsForInvalid.forEach((testWord) => {
-      expect(getIsInputValueValid(testWord)).toEqual(false);
-    });
-    expect(getIsInputValueValid("123")).toEqual(true);
+  test("입력값에 중복이 포함되어 있다면 에러를 발생시킨다.", () => {
+    const randoms = [1, 2, 3];
+    const answer = ["122"];
+
+    mockRandoms(randoms);
+    mockQuestions(answer);
+
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow();
   });
 
-  test;
+  test("입력값에 숫자가 아닌 값이 포함되어 있다면 에러를 발생시킨다.", () => {
+    const randoms = [1, 2, 3];
+    const answer = ["12d"];
+
+    mockRandoms(randoms);
+    mockQuestions(answer);
+
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow();
+  });
+
+  test("게임 종료 후 1, 2 이외의 값이 입력되면 에러를 발생시킨다.", () => {
+    const randoms = [1, 3, 5, 5, 8, 9];
+    const answers = ["246", "135", "1", "597", "589", "23"];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow();
+  });
 });
