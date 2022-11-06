27ac5c548f06525db2940eff7c45b330440d1303
JUNU <106523012+bokjunwoo@users.noreply.github.com>
2022-11-06 23:36:08 +0900

    feat : 컴퓨터 랜덤 숫자 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
+  answer;
+
   play() {}
+
+  computerRandomNumber() {
+    const nonDuplicateNumbers = [];
+    while (nonDuplicateNumbers.length < 3) {
+      const randomNumber = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!nonDuplicateNumbers.includes(randomNumber)) {
+        nonDuplicateNumbers.push(randomNumber);
+      }
+    }
+    
+    this.answer = Number(nonDuplicateNumbers.join(""));
+    return this.answer;
+  }
 }
+
+const app = new App();
+app.play();
+
+console.log(app.computerRandomNumber());
 
 module.exports = App;
