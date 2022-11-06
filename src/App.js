371f9bcd457853c8bfd6805a045625dee5f39902
cybersyn2 <quandonestpetit@outlook.com>
2022-11-06 20:19:17 +0900

    draft GameLoop start

--- src/App.js
+++ src/App.js
@@ -39,4 +39,14 @@ class Computer {
   }
 }
 
+class GameLoop {
+  constructor() {
+    this.Opponent = new Computer();
+  }
+  start() {
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    this._continue();
+  }
+}
+
 module.exports = App;
