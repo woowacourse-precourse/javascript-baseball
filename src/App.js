963b7e5a3e4b227389c0de4f120ad4dcf73138c5
solo5star <solo_5@naver.com>
2022-11-07 04:06:20 +0900

    feat(App): `play()` 호출 시 게임 시작 메세지 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+const Messages = require('./messages');
+const {print} = require('./input');
+
 class App {
-  play() {}
+  // eslint-disable-next-line class-methods-use-this
+  play() {
+    while (true) {
+      print(Messages.GAME_START);
+      // TODO: 숫자 야구 게임 구현
+
+      break;
+    }
+  }
 }
 
 module.exports = App;
