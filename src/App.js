5ac38cd5fc6cdef27f8ba49f0b17d077d0324cee
sohyun <park.sohyun0430@gmail.com>
2022-11-06 15:18:28 +0900

    feat: inputPlayerNumber 함수 구현
    사용자가 생각하는 정답 입력받기(MissionUtils 라이브러리의 Console.readLine 사용)
    getHintOrThrowError 함수 - inputNumber에 따라서 예외처리 후 종료 또는 hint를 얻는 getHint 함수 호출
    getHintOrThrowError 함수 호출 후, 힌트가 '3스트라이크' 이면 정답으로 판정 후, restart 할지 종료할지 선택하는 checkRestart 함수 호출
    정답이 아니라면 inputPlayerNumber 함수 다시 호출하여 사용자가 다른 숫자를 입력하도록 함

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 class App {
+
+  static answer;
+  static hintString;
+
   play() {}
 
   generateComputerAnswer(){
@@ -11,6 +15,18 @@ class App {
       }
     }
     return COMPUTER_RANDOM_NUMBER;
+  }
+
+  inputPlayerNumber(){
+    MissionUtils.Console.readLine('숫자를 입력해주세요:', (inputNumber)=>{
+      this.getHintOrThrowError(inputNumber);
+      MissionUtils.Console.print(this.hintString);
+      if(this.hintString=='3스트라이크'){
+        MissionUtils.Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+        this.checkRestart();
+      }
+      else this.inputPlayerNumber();
+    });
   }
 }
 
