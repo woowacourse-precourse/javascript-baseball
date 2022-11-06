0a95c4f34aebdf15430687b17b8371ad935a6d7b
sungjihyun <jhsung23@gmail.com>
2022-11-06 12:37:01 +0900

    feat: 게임을 재시작할지 종료할지 물어보는 기능 추가

--- src/App.js
+++ src/App.js
@@ -90,6 +90,13 @@ class App {
     MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
   }
 
+  askRestartOrTerminate() {
+    MissionUtils.Console.readLine(
+      "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
+      (number) => number
+    );
+  }
+
   play() {}
 }
 
