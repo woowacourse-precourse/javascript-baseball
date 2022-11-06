1c0f85dbe31168ec58835ed8acfad8c2d8858f7b
cloudedpanther <rickeykoh@ajou.ac.kr>
2022-11-07 01:42:37 +0900

    feat(method): getReplayRequest
    Add to Input class
    - getReplayRequest method
    - ASK_REPLAY constant
    - requestMap object

--- src/Input.js
+++ src/Input.js
@@ -2,6 +2,12 @@ const MissionUtils = require("@woowacourse/mission-utils");
 const Parse = require("./Parse");
 
 const ASK_ANSWER = "숫자를 입력해주세요 : ";
+const ASK_REPLAY = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+const requestMap = {
+  1: true,
+  2: false,
+};
 
 class Input {
   static getUserAnswer() {
@@ -14,7 +20,16 @@ class Input {
     return Parse.numberToArray(answer);
   }
 
-  static getReplayRequest() {}
+  static getReplayRequest() {
+    let replay = false;
+    MissionUtils.Console.print(ASK_REPLAY);
+    MissionUtils.Console.readLine("", (request) => {
+      replay = request;
+    });
+    MissionUtils.Console.close();
+
+    return requestMap[replay];
+  }
 }
 
 module.exports = Input;
