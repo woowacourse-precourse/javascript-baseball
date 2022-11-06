cf358d71b25f708c6657809fdc0a009d9e6d6a9d
min <hororonoodle@gmail.com>
2022-11-07 01:31:21 +0900

    feat : [T3-2]에 대한 게임 종료 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 const GameMessage = require('./Constants/gameMessage');
+
+const firstRange = 1;
+const lastRange = 9;
+const length = 3;
+
 class App {
-  answer = '';
-  userInput = '';
-  result = '';
+  #answer;
+  #userInput;
 
   constructor() {
+    this.#userInput = '';
+    this.#answer = '';
+
     this.inputAssert = {
-      assertLength: (input) => input.length === 3,
+      assertLength: (input) => input.length === length,
       assertInteger: (input) => Number.isInteger(+input),
       assertPositive: (input) => Math.sign(input) === 1,
     };
+  }
+
+  get userInput() {
+    return this.#userInput;
+  }
+
+  get answer() {
+    return this.#answer;
   }
 
   printWelcomeMessage() {
@@ -18,15 +33,12 @@ class App {
   }
 
   answerGenerator() {
-    const firstRange = 1;
-    const lastRange = 9;
-    const length = 3;
     const randomNumber = MissionUtils.Random.pickUniqueNumbersInRange(
       firstRange,
       lastRange,
       length
     );
-    this.answer = randomNumber.join('');
+    this.#answer = randomNumber.join('');
   }
 
   getUserInput() {
@@ -40,7 +52,25 @@ class App {
       if (!assertPositive(input))
         throw Error(GameMessage.WRONG_INPUT_ERROR_MESSAGE);
 
-      this.userInput = input;
+      this.#userInput = input;
+
+      this.end();
+    });
+  }
+
+  end() {
+    MissionUtils.Console.readLine(GameMessage.END_GAME_MESSAGE, (input) => {
+      switch (input) {
+        case GameMessage.RESTART_INPUT:
+          this.#answer = '';
+          this.start();
+          break;
+        case GameMessage.GAMEOVER_INPUT:
+          MissionUtils.Console.print(GameMessage.GAMEOVER_MESSAGE);
+          break;
+        default:
+          throw Error(GameMessage.WRONG_INPUT_ERROR_MESSAGE);
+      }
     });
   }
 
@@ -51,6 +81,7 @@ class App {
 
   play() {
     this.printWelcomeMessage();
+    this.start();
   }
 }
 
