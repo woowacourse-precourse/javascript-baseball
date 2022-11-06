5646e8616c405e8dcd82c41f902f7c0013235bd7
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2022-11-07 00:50:45 +0900

    feat(Render): ball, strike count의 값을 받아 값에 따라 다르게 출력하는 기능 추가

--- src/Render.js
+++ src/Render.js
@@ -10,5 +10,17 @@ class Render {
       });
     });
   }
+
+  result({ ballCount, strikeCount }) {
+    MissionUtils.Console.print(`${ballCount}볼 ${strikeCount}스트라이크`);
+    if (strikeCount === 3) {
+      MissionUtils.Console.print(`3스트라이크
+3개의 숫자를 모두 맞히셨습니다! 게임 종료`);
+    }
+
+    if (ballCount === 0 && strikeCount === 0) {
+      MissionUtils.Console.print(`낫싱`);
+    }
+  }
 }
 module.exports = Render;
