6467064758c7458930c8e9a9f51ad965605a4ccc
rabBit64 <gojh47@gmail.com>
2022-11-06 10:51:04 +0900

    feat: 기능목록1 - 컴퓨터가 임의의 수 3개 선택

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
-class App {
-  play() {}
+function initRandom(MissionUtils){
+  const computer = [];
+  while(computer.length < 3){
+    const number = MissionUtils.Random.pickNumberInRange(1,9);
+    if(!computer.includes(number)){
+      computer.push(number);
+    }
+  }
+  return computer;
 }
 
+class App {
+  play() {
+    const MissionUtils = require("@woowacourse/mission-utils");
+    start: while(true){
+      console.log("숫자 야구 게임을 시작합니다.");
+      //1. 컴퓨터가 1에서 9까지 서로 다른 임의의 수 3개를 선택한다.
+      const computer = initRandom(MissionUtils);
+      
+    }
+  }
+}
 module.exports = App;
