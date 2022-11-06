a366c7431c7003cf7d4a66877f055a4ce121d7f6
knh6269 <knh6269@gmail.com>
2022-11-07 01:35:59 +0900

    feat(getComputerAndUser): 컴퓨터의 랜덤 숫자를 받는 함수
    기능 목록(#2) : 컴퓨터의 3자리 랜덤 숫자를 생성하는 함수

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 class App {
   play() {}
+  getComputerAndUser() {
+    const computerNumber = this.recieveComputer();
+    this.checkCorrectNumber(computerNumber);
+    this.userInput(computerNumber)
+  }
+
   play() {
     STD.print(`숫자 야구 게임을 시작합니다.`);
     this.getComputerAndUser();
