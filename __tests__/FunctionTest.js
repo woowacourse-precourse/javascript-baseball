815013f19e39b92b6d785981365cf1898f01b157
cloudedpanther <rickeykoh@ajou.ac.kr>
2022-11-07 01:25:51 +0900

    test(parse): numberToArray method
    Add test of numberToArray method of Parse class

--- __tests__/FunctionTest.js
+++ __tests__/FunctionTest.js
@@ -1,13 +1,26 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 const { Output } = require("../src/Output");
+const Parse = require("../src/Parse");
+
+const getPrintLogSpy = () => {
+  const logSpy = jest.spyOn(MissionUtils.Console, "print");
+  logSpy.mockClear();
+  return logSpy;
+};
 
 describe("기능 테스트", () => {
   test("화면에 문구 출력", () => {
-    const outputSpy = jest.spyOn(MissionUtils.Console, "print");
+    const logSpy = getPrintLogSpy();
 
     Output.printToUser("Hello");
 
-    expect(outputSpy).toBeCalledTimes(1);
-    expect(outputSpy).toBeCalledWith("Hello");
+    expect(logSpy).toBeCalledTimes(1);
+    expect(logSpy).toBeCalledWith("Hello");
+  });
+
+  test("숫자를 배열로 바꾸기", () => {
+    const number = 123;
+    const parsed = Parse.numberToArray(number);
+    expect(parsed).toEqual([1, 2, 3]);
   });
 });
