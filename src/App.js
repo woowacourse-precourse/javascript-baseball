415144658cb6965a6d2db270d04ed47c8b753735
unqeye <salyuan@naver.com>
2022-11-06 14:42:30 +0900

    feat: 볼 카운트 출력

--- src/App.js
+++ src/App.js
@@ -46,6 +46,20 @@ class App {
 
     this.ballCount = ballCount;
   }
+
+  printBallCount() {
+    const { ball, strike } = this.ballCount;
+
+    if (ball === 0 && strike === 0) {
+      MissionUtils.Console.print('낫싱');
+    } else if (ball === 0) {
+      MissionUtils.Console.print(`${strike}스트라이크`);
+    } else if (strike === 0) {
+      MissionUtils.Console.print(`${ball}볼`);
+    } else {
+      MissionUtils.Console.print(`${ball}볼 ${strike}스트라이크`);
+    }
+  }
 }
 
 module.exports = App;
--- src/App.test.js
+++ src/App.test.js
@@ -141,3 +141,65 @@ describe('볼 카운트 계산', () => {
     expect(app.ballCount).toStrictEqual({ ball: 3, strike: 0 });
   });
 });
+
+describe('볼 카운트 출력', () => {
+  test('낫싱', () => {
+    const spy = jest.spyOn(MissionUtils.Console, 'print');
+
+    const app = new App();
+    app.ballCount = { ball: 0, strike: 0 };
+    app.printBallCount();
+
+    expect(spy).toHaveBeenCalledWith('낫싱');
+  });
+
+  test('1스트라이크', () => {
+    const spy = jest.spyOn(MissionUtils.Console, 'print');
+
+    const app = new App();
+    app.ballCount = { ball: 0, strike: 1 };
+    app.printBallCount();
+
+    expect(spy).toHaveBeenCalledWith('1스트라이크');
+  });
+
+  test('1볼', () => {
+    const spy = jest.spyOn(MissionUtils.Console, 'print');
+
+    const app = new App();
+    app.ballCount = { ball: 1, strike: 0 };
+    app.printBallCount();
+
+    expect(spy).toHaveBeenCalledWith('1볼');
+  });
+
+  test('1볼 1스트라이크', () => {
+    const spy = jest.spyOn(MissionUtils.Console, 'print');
+
+    const app = new App();
+    app.ballCount = { ball: 1, strike: 1 };
+    app.printBallCount();
+
+    expect(spy).toHaveBeenCalledWith('1볼 1스트라이크');
+  });
+
+  test('3스트라이크', () => {
+    const spy = jest.spyOn(MissionUtils.Console, 'print');
+
+    const app = new App();
+    app.ballCount = { ball: 0, strike: 3 };
+    app.printBallCount();
+
+    expect(spy).toHaveBeenCalledWith('3스트라이크');
+  });
+
+  test('3볼', () => {
+    const spy = jest.spyOn(MissionUtils.Console, 'print');
+
+    const app = new App();
+    app.ballCount = { ball: 3, strike: 0 };
+    app.printBallCount();
+
+    expect(spy).toHaveBeenCalledWith('3볼');
+  });
+});
