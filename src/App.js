bca929d65a088c7bcbe17bd140fede840d053aa9
ksone02 <eksovus@gmail.com>
2022-11-06 17:38:32 +0900

    feat: 볼, 스트라이크 개수를 토대로 개수 또는 낫싱 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 const { Console, Random } = require('@woowacourse/mission-utils');
-const { MESSAGE } = require('./data/constants');
+const { MESSAGE, RESULT } = require('./data/constants');
 const CheckException = require('./utils');
 
 class App {
@@ -7,14 +7,26 @@ class App {
     return Random.pickUniqueNumbersInRange(1, 9, 3);
   }
 
+  isThreeStrike(strike) {
+    if (strike === 3) {
+      Console.print(MESSAGE.SUCCESS);
+
+      this.restart();
+      return;
+    }
+  }
+
   recursiveInput() {
     Console.readLine(MESSAGE.INPUT, inputNum => {
       CheckException(inputNum, 3);
+
       const { ball, strike } = this.check(inputNum);
-      if (strike === 3) {
-        Console.print('정답');
-        this.restart();
-      }
+      Console.print(this.result(ball, strike));
+
+      this.isThreeStrike(strike);
+
+      this.recursiveInput();
+      return;
     });
   }
 
@@ -30,6 +42,15 @@ class App {
       else if (this.computerNum.includes(parseInt(num))) count.ball += 1;
     });
     return count;
+  }
+
+  result(ball, strike) {
+    if (ball == 0 && strike == 0) return RESULT.NOTHING;
+
+    if (ball > 0 && strike > 0)
+      return `${ball}${RESULT.BALL} ${strike}${RESULT.STRIKE}`;
+    if (ball > 0 && strike == 0) return `${ball}${RESULT.BALL}`;
+    if (ball == 0 && strike > 0) return `${strike}${RESULT.STRIKE}`;
   }
 
   restart() {
