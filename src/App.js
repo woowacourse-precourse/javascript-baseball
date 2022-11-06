f58424caa4b6de635aecce2b003ab10c794c9d35
b-hyoung <youqlrqod@naver.com>
2022-11-06 15:23:21 +0900

    add random Number(3)

--- src/App.js
+++ src/App.js
@@ -6,26 +6,32 @@
  */
 
 class App {
-  constructor() {
-    
-  };
   play() {
-    
+    console.log("숫자 야구 게임을 시작합니다.")
   }
   //유저 입력 받기
-  userNumber(){
+  userNumber() {
     var readline = require('readline');
-    
+
     var r = readline.createInterface({
       input: process.stdin,
-      output:process.stdout
+      output: process.stdout
     });
     r.question("숫자를 입력해주세요 : ", function (numbers) {
       r.close() // 반드시 close()를 해줘야 합니다.ß
       return numbers
     });
   }
- 
+  computer() {
+    const computer = [];
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+  }
+
 
 }
 const app = new App()
