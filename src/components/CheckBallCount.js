0fa92b3f1fd8d664e24b56d16321d24679bfe6b9
JayPark0110 <90092440+JayPark0110@users.noreply.github.com>
2022-11-06 19:20:03 +0900

    refactor: CheckBallCount 내 for문 forEach문으로 변경

--- src/components/CheckBallCount.js
+++ src/components/CheckBallCount.js
@@ -1,11 +1,12 @@
 const CheckBallCount = (targetNumber, userGuessedNumber) => {
   let ball = 0;
   let strike = 0;
-  for (let idx = 0; idx < userGuessedNumber.length; idx++) {
-    let isIncludes = targetNumber.includes(userGuessedNumber[idx]);
-    let isBallOrStrike = targetNumber[idx] === userGuessedNumber[idx];
-    isIncludes ? (isBallOrStrike ? strike++ : ball++) : null;
-  }
+  userGuessedNumber.forEach((el, idx) => {
+    let isIncludes = targetNumber.includes(el);
+    let isBallOrStrike = targetNumber[idx] === el;
+    isIncludes ? (isBallOrStrike ? strike++ : ball++) : "";
+  });
+
   return [ball, strike];
 };
 
