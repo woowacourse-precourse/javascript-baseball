bd732a32d1b79f35bf05dce4ccd9290edc0549d5
GunHoi <lgh509@kyonggi.ac.kr>
2022-11-06 16:36:01 +0900

    Feat 6.1 - 3스트라이크 이면 (7번)  게임 종료 멘트 출력

--- src/App.js
+++ src/App.js
@@ -59,6 +59,16 @@ class App {
       return ball+'볼 '+strike+'스트라이크';
     }
   }
+  isCorrect(strike){
+    if(strike === 3){
+      this.gameClearMessage();
+    }else{
+      
+    }
+  }
+  gameClearMessage(){
+    return MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+  }
   play() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
     this.makeRandomNumber();
