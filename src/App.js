e58f15b02f15dc364a503df0b12427aea4d242c5
“somm12” <sominan8744@gmail.com>
2022-11-06 13:03:26 +0900

    feat: 숫자 입력받기 - makeUserInputNumber 함수 정의

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 class App {
   randomNumber = [];
+  userInputNumber = [];
 
   constructor() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
   }
   play() {
     this.makeRandomNumber();
+    this.makeUserInputNumber();
   }
+
   makeRandomNumber() {
     let arr = new Set();
+
     while (arr.size < 3) {
       arr.add(MissionUtils.Random.pickNumberInRange(1, 9));
     }
     this.randomNumber = Array.from(arr);
+  }
+
+  makeUserInputNumber() {
+    MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (inputNumber) => {
+      this.userInputNumber = [...inputNumber].map(Number);
+    });
   }
 }
 
