d2d9d49c5aa546425f7a417031fd4beca3d6e33e
jaesung4231 <jaesung4231@naver.com>
2022-11-07 00:22:49 +0900

    비교한 값을 통해 스트라이크, 볼, 낫싱을 계산하고 출력하기

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ const MissionUtils = require("@woowacourse/mission-utils");
 class App {
   comInput=[]
   PlayerInput=[]
+  strike=0
+  Ball=0
   setComInput(){
     for(let i=0;i<3;i++){
       this.comInput.push(MissionUtils.Random.pickNumberInRange(1, 9))
@@ -13,13 +15,42 @@ class App {
     this.comInput=[]
   }
 
+
   setPlayerInput(ans){
     this.PlayerInput=[]
     for(let i=0;i<3;i++){
       this.PlayerInput.push(Number(ans[i]))
     }
     return this.PlayerInput
+  }
 
+  getStrike(Player, comInput){
+   this.strike=0
+   for(let i=0;i<3;i++){
+    if(Player[i]==comInput[i]){
+      this.strike++;
+    }
+  }
+  return this.strike
+  }
+
+  getBall(Player, comInput){
+    this.Ball=0
+    for(let i=0;i<3;i++){
+      if((Player[i]!==comInput[i]) && comInput.includes(Player[i])){
+        this.Ball++;
+      }
+    }
+    return this.Ball
+  }
+
+  printResult(){
+    if(this.Ball==0 && this.strike==0){
+      console.log("낫싱")
+    }
+    else{
+      console.log(`${this.strike}스트라이크 ${this.Ball}볼`)
+    }
   }
 
   start(){
@@ -28,13 +59,16 @@ class App {
     this.resetComInput()
     this.setComInput()
     this.setPlayerInput(answer)
+    console.log(this.PlayerInput)
+    console.log(this.comInput)
+    this.getStrike(this.PlayerInput, this.comInput)
+    this.getBall(this.PlayerInput, this.comInput)
+    console.log(this.Ball, this.strike)
+    this.printResult()
     });
   }
-
-
-
-  
 }
 const app = new App
 app.start()
+
 // module.exports = App;
