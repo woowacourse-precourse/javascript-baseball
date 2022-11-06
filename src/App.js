013d62c63e51937a34a57d44bcc44c78ec92c81e
millifail <hwangbird99@gmail.com>
2022-11-06 17:44:58 +0900

    feat(input-number):scan a number and validate
    - 숫자 입력 구현
    - 사용자가 잘못된 값을 입력한 경우 예외 발생 구현

--- src/App.js
+++ src/App.js
@@ -3,11 +3,37 @@ import * as MissionUtils from "@woowacourse/mission-utils";
 class App {
   play() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+
     const RANDOM_NUMBER = this.GET_RANDOM_NUMBER();
   }
 
   GET_RANDOM_NUMBER() {
     return MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3); //ex - [2,5,6]
+  }
+
+  INPUT_NUMBER(correct_number) {
+    MissionUtils.Console.readLine("숫자를 입력해주세요.", (answer) => {
+      const NUMBER_LIST = String(answer)
+        .split("")
+        .map((e) => Number(e));
+
+      if (NUMBER_LIST.includes(NaN)) {
+        //문자열 입력
+        throw "입력값은 문자열이 될 수 없습니다!";
+      }
+      if (NUMBER_LIST.includes(0)) {
+        //0입력
+        throw "입력값은 1~9사이의 숫자여야 합니다!";
+      }
+      if (NUMBER_LIST.length !== 3) {
+        //3자리가 아닌 숫자 입력
+        throw "입력값은 세 자리 숫자여야 합니다!";
+      }
+      if (NUMBER_LIST.length !== new Set(NUMBER_LIST).length) {
+        //동일한 숫자 입력
+        throw "입력값은 서로 다른 숫자여야 합니다!";
+      }
+    });
   }
 }
 
