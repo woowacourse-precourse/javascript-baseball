29cd90d55cc90301ef4a37ca89911328e66c2876
Chaeduk <kkj2551@naver.com>
2022-11-06 18:42:06 +0900

    feat(3자리 난수 생성) : 구현

--- src/model/number-baseball.model.js
+++ src/model/number-baseball.model.js
@@ -1,6 +1,12 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+
 const CONSTANTS = require('../constant');
 
-class NumberBaseball {
+class NumberBaseballModel {
+  constructor() {
+    this.computer = [];
+  }
+
   splitNumber(num) {
     return num.split('');
   }
@@ -27,6 +33,20 @@ class NumberBaseball {
   inputNumCheck(strArray) {
     return strArray.every((num) => CONSTANTS.NUMBERS.includes(num));
   }
+
+  async generateNum() {
+    // 리팩토링 필요!!
+    while (this.computer.length < CONSTANTS.INPUT_LENGTH) {
+      const number = await MissionUtils.Random.pickNumberInRange(
+        CONSTANTS.MIN_NUMBER,
+        CONSTANTS.MAX_NUMBER,
+      );
+      if (!this.computer.includes(String(number))) {
+        this.computer.push(String(number));
+      }
+    }
+    return this;
+  }
 }
 
-module.exports = NumberBaseball;
+module.exports = NumberBaseballModel;
