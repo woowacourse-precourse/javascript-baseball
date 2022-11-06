778e7bd2af35a89ca94fd4d647c742b724557ea3
marugy <gyulife7301@naver.com>
2022-11-06 21:36:19 +0900

    숫자 입력 받고 숫자인지 타입  확인하기

--- src/App.js
+++ src/App.js
@@ -12,7 +12,36 @@ class App {
     return computer;
   }
 
-  play() {}
+  checkType(num) {
+    if (typeof num != "Number") throw -1;
+  }
+
+  checkDigit(num) {
+    if (num < 123 || num > 987) throw -1;
+  }
+
+  checkOverlap(num) {
+    const number = String(num).split("");
+    if (
+      number[0] == number[1] ||
+      number[0] == number[2] ||
+      number[1] == number[2]
+    )
+      throw -1;
+  }
+
+  checkError(num) {
+    this.checkType(num);
+    this.checkDigit(num);
+    this.checkOverlap(num);
+  }
+
+  play() {
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.\n");
+    MissionUtils.Console.readLine("숫자를 입력해주세요:", (num) => {
+      this.checkError(num);
+    });
+  }
 }
 
 module.exports = App;
