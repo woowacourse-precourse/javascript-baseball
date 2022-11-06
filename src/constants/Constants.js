0d163ea9a0cb889d0f8ab4b6298acc90943613ed
JuhyunLim <87933367+userJu@users.noreply.github.com>
2022-11-06 10:05:08 +0900

    Feat: Add Constants

---
+++ src/constants/Constants.js
@@ -0,0 +1,19 @@
+const START_GAME = "숫자 야구 게임을 시작합니다";
+const GAME_END = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+const RETRY_OR_END = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요";
+
+const REQUIRE_NUMBER = "숫자를 입력해주세요 : ";
+
+const NOTTHING = "낫싱";
+const BALL = "볼";
+const STRIKE = "스트라이크";
+
+module.exports = {
+  START_GAME,
+  GAME_END,
+  RETRY_OR_END,
+  REQUIRE_NUMBER,
+  NOTTHING,
+  BALL,
+  STRIKE,
+};
