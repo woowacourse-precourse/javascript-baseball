6ebea00f57ba5acbb103b060defab2922232cbe7
Tory <88531407+1two13@users.noreply.github.com>
2022-11-06 15:28:52 +0900

    refactor(ValidateUserInput): pickedNumber 변수를 constructor 메소드로 위치 변경

--- src/ValidateUserInput.js
+++ src/ValidateUserInput.js
@@ -1,19 +1,20 @@
 class ValidateUserInput {
   constructor() {
     this.numberInRange = /^[1-9]+$/;
-    this.pickedNumber = [];
     this.ERROR_COMMENT =
       "1부터 9까지 서로 다른 수로 이루어진 3자리 숫자를 입력해주세요.";
   }
 
   validate = (pickedNumberByUser) => {
+    const pickedNumber = [];
+
     // 서로 다른 3자리 수를 입력하지 않은 경우
     pickedNumberByUser
       .split("")
       .forEach((el) =>
-        !this.pickedNumber.includes(el) ? this.pickedNumber.push(el) : ""
+        !pickedNumber.includes(el) ? pickedNumber.push(el) : ""
       );
-    if (this.pickedNumber.length !== 3) {
+    if (pickedNumber.length !== 3) {
       throw new Error(this.ERROR_COMMENT);
     }
 
