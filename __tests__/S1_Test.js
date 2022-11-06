ff1a8de0ca2152af9221214927fd0280ea26249e
min <hororonoodle@gmail.com>
2022-11-06 23:23:13 +0900

    test (fix) :[T-1] 사용자 입력 질문 항목 matcher 내용 수정

--- __tests__/S1_Test.js
+++ __tests__/S1_Test.js
@@ -10,7 +10,7 @@ const getLogSpy = () => {
 describe('[S1] 유저는 게임이 진행되었는지 한 눈에 딱 알 수 있도록 게임을 시작하면 숫자 야구 게임을 시작합니다. 라는 문구를 보기를 원한다.', () => {
   test('[T1-1] 숫자 야구 게임을 시작합니다. 와 같은 게임 안내 문구를 상수화 하여 정리', () => {
     expect(GameMessage.WELCOME_MESSAGE).toEqual('숫자 야구 게임을 시작합니다.');
-    expect(GameMessage.QUESTION_MESSAGE).toEqual('숫자를 입력해주세요. ');
+    expect(GameMessage.QUESTION_MESSAGE).toEqual('숫자를 입력해주세요. : ');
   });
   test('[T1-2] 게임 시작 시 문구 출력 기능', () => {
     const answers = ['1234'];
