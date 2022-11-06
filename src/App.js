c5f506918158f13673f5476ecd445ddf5d46ee52
chchaeun <kce3615@naver.com>
2022-11-07 00:02:07 +0900

    feat: 랜덤 숫자 발생

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  constructor() {
+    this.answer = null;
+  }
+  play() {
+    this.getAnswerNumber();
+  }
+  getAnswerNumber() {
+    const RANGE_START_NUMBER = 1;
+    const RANGE_END_NUMBER = 9;
+    const NUMBER_OF_DIGITS = 3;
+
+    const ANSWER = MissionUtils.Random.pickUniqueNumbersInRange(
+      RANGE_START_NUMBER,
+      RANGE_END_NUMBER,
+      NUMBER_OF_DIGITS
+    );
+
+    this.answer = ANSWER;
+  }
 }
 
 module.exports = App;
