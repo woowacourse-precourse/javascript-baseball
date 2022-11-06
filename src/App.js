a947f6633ce537b8e88988e627f1ba65db112187
nerdchanii <nerdchanii@gmail.com>
2022-11-07 02:06:17 +0900

    feat(io): 입출력 클래스 추가
    변경에 유연하기 위해 입출력인터페이스를 형성한다

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+const Io = require('./Io');
 class App {
-  play() {}
+  constructor() {
+    this.Io = Io;
+  }
+
+  play() {
+    this.Io.output('숫자 야구 게임을 시작합니다.');
+  }
 }
 
 module.exports = App;
---
+++ src/Io.js
@@ -0,0 +1,26 @@
+const { Console } = require('@woowacourse/mission-utils');
+
+class Io {
+  /**
+   * 메시지를 출력하고, 사용자의 입력을 받아 콜백 함수를 실행한다.
+   * @param {string} message 
+   * @param {(input:string)=>void} callback 
+   */
+  static input(message, callback) {
+    Console.readLine(message, callback);
+  }
+
+  /**
+   * 메시지를 출력한다.
+   * @param {string} message 
+   */
+  static output(message) {
+    Console.print(message);
+  }
+
+  static close() {
+    Console.close();
+  }
+}
+
+module.exports = Io;
