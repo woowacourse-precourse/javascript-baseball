fcaf10585d76a4774415832e9de3746a4b4f6fc8
gabrielyoon7 <gabrielyoon7@gmail.com>
2022-11-07 02:59:38 +0900

    style: semi-colons

--- src/game/ball.js
+++ src/game/ball.js
@@ -33,15 +33,15 @@ const countBallStrike = (computerNumbersArray, userNumbersArray) => {
 const printBallStrike = (ball, strike) => {
     let textArray = [];
     if (ball !== 0) {
-        textArray.push(`${ball}볼`)
+        textArray.push(`${ball}볼`);
     }
     if (strike !== 0) {
-        textArray.push(`${strike}스트라이크`)
+        textArray.push(`${strike}스트라이크`);
     }
 
     let text = '낫싱';
     if (textArray.length > 0) {
-        text = textArray.join(' ')
+        text = textArray.join(' ');
     }
     MissionUtils.Console.print(text);
 }
--- src/game/validation.js
+++ src/game/validation.js
@@ -1,27 +1,26 @@
 
 const isValidBallNumber = (answer) => {
-
     if (answer.length !== 3) {
-        return false
+        return false;
     }
 
     if (answer[0] === answer[1]) {
-        return false
+        return false;
     }
 
     if (answer[1] === answer[2]) {
-        return false
+        return false;
     }
 
     if (answer[2] === answer[0]) {
-        return false
+        return false;
     }
 
     if (isNaN(Number(answer))) {
-        return false
+        return false;
     }
 
-    return true
+    return true;
 }
 
 module.exports = { isValidBallNumber };
