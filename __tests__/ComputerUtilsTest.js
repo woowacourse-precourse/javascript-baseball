df57bb5dfc9c88d5c5306b959a70dc16f7955b6a
Circlewee <pnj1118@naver.com>
2022-11-06 15:43:24 +0900

    test(computer): 힌트 제공 테스트 코드 작성 (#5)
    1. 아무 것도 일치하지 않는 경우
    2. 1볼 1스트라이크
    3. 3볼
    4. 3스트라이크

--- __tests__/ComputerUtilsTest.js
+++ __tests__/ComputerUtilsTest.js
@@ -15,3 +15,27 @@ describe('서로 다른 임의의 수 3개 생성 테스트', () => {
     expect(sortedRandomNumbers[2]).toBeLessThanOrEqual(COMPUTER.END_NUMBER);
   });
 });
+
+describe('유저의 입력에 따른 힌트 테스트', () => {
+  const answer = [1, 2, 3];
+
+  test('아무 것도 일치하지 않는 경우', () => {
+    const userInput = [4, 5, 6];
+    expect(computerUtils.generateHint(userInput, answer)).toBe('낫싱');
+  });
+
+  test('1볼 1스트라이크', () => {
+    const userInput = [2, 7, 3];
+    expect(computerUtils.generateHint(userInput, answer)).toBe('1볼 1스트라이크');
+  });
+
+  test('3볼', () => {
+    const userInput = [3, 1, 2];
+    expect(computerUtils.generateHint(userInput, answer)).toBe('3볼');
+  });
+
+  test('3스트라이크', () => {
+    const userInput = [1, 2, 3];
+    expect(computerUtils.generateHint(userInput, answer)).toBe('3스트라이크');
+  });
+});
