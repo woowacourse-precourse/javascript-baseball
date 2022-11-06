644425068fc6cab2648ef2f84917515b6827110c
JSH99 <tjgus9966@naver.com>
2022-11-06 22:42:53 +0900

    feat(restartOrQuitGame): 다시 시작 및 완전 종료 기능 추가

--- src/restartOrQuitGame.js
+++ src/restartOrQuitGame.js
@@ -1,11 +1,20 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const createRandomNumbers = require("./createRandomNumbers");
+
 const restartOrQuitGame = () => {
     MissionUtils.Console.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.', (endOption) => {
         if (!isVaildEndOption(endOption)) { 
             throw new Error('잘못된 값입니다. 1 또는 2를 입력해주세요.');
         } 
-        return endOption;
     });
-    MissionUtils.Console.close();
+
+     // 다시 시작
+    if(endOption == 1) {
+        const computerNum = createRandomNumbers();
+        playGame(computerNum);
+    }
+    // 완전 종료
+    MissionUtils.Console.close(); 
 }
 
 module.exports = restartOrQuitGame;
