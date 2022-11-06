013532a3e787b6735ace78f314b799be2ce1af9d
GC-Park <kuyt3122@naver.com>
2022-11-06 17:00:13 +0900

    docs:구현할 기능 목록 번호 수정

--- __tests__/myselfTest.js
+++ __tests__/myselfTest.js
@@ -2,24 +2,26 @@ const App = require("../src/App");
 const MissionUtils = require("@woowacourse/mission-utils");
 
 describe("숫자 야구 게임", () => {
-  test("사용자 입력 확인 테스트 코드 1", () => {
-    const input = "123";
-    const result = input.split("");
+  // test("사용자 입력 확인 테스트 코드 1", () => {
+  //   const input = "123";
+  //   const result = input.split("");
 
-    const app = new App();
-    app.play();
+  //   const app = new App();
+  //   app.play();
 
-    expect(result).toContain("2", "1", "3");
-    expect(result).toContainEqual("1", "2", "3");
-  });
-  test("사용자 입력 확인 테스트 코드 2", () => {
-    const input = "876";
-    const result = input.split("");
+  //   expect(result).toContain("2", "1", "3");
+  //   expect(result).toContainEqual("1", "2", "3");
+  // });
+  // test("사용자 입력 확인 테스트 코드 2", () => {
+  //   const input = "876";
+  //   const result = input.split("");
 
-    const app = new App();
-    app.play();
+  //   const app = new App();
+  //   app.play();
 
-    expect(result).toContain("7", "6", "8");
-    expect(result).toContainEqual("8", "7", "6");
-  });
+  //   expect(result).toContain("7", "6", "8");
+  //   expect(result).toContainEqual("8", "7", "6");
+  // });
+
+  
 });
