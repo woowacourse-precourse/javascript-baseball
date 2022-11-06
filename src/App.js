3a46a96de1eb019d15386855272e0c0aedbc9739
Woong <jiyko7@kyonggi.ac.kr>
2022-11-06 23:16:34 +0900

    1. 시작문구 출력
    1. 시작문구 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 class App {
-  play() {}
+  play() {
+    const MissionUtils = require("@woowacourse/mission-utils");
+
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+  
+  
+  }
 }
+
+
+var a = new App();
+a.play();
+
 
 module.exports = App;
