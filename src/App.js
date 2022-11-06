38688bc807eab5e7a2aacbabc395d98c0640660a
nuheajiohc <jaehun341012@gmail.com>
2022-11-06 13:23:43 +0900

    refactor: 변수명 리팩토링

--- src/App.js
+++ src/App.js
@@ -5,20 +5,17 @@ const RETURN_COUNT = 3;
 
 class App {
     play() {
-        startGuidePrint();
-        const randomNumber = generateRandomNumber();
-        userInputNumber(randomNumber);
+        gameStartGiude();
+        userInputNumber(generateRandomNumber());
     }
     restart() {
-        const randomNumber = generateRandomNumber();
-        userInputNumber(randomNumber);
+        userInputNumber(generateRandomNumber());
     }
 }
 
-function startGuidePrint() {
+function gameStartGiude() {
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
 }
-
 
 function generateRandomNumber() {
     const computer = [];
@@ -30,27 +27,29 @@ function generateRandomNumber() {
     }
     return computer;
 }
+const a = new App();
+a.play();
 
-function userInputNumber(randomNumber) {
-    MissionUtils.Console.readLine("세자리 숫자를 입력해주세요 : ", number => {
-        return gameStart(number, randomNumber)
+function userInputNumber(computerNumber) {
+    MissionUtils.Console.readLine('세자리 숫자를 입력해주세요 : ', userNumber => {
+        const toArrUserNumber = String(userNumber).split("").map(Number);
+        return gameStart(toArrUserNumber, computerNumber)
     });
 }
 
-function gameStart(number, randomNumber) {
-    const numberSplit = String(number).split("").map(Number);
-    inputValidation(numberSplit);
-    const [strikeCount, ballCount] = strikeBallCount(numberSplit, randomNumber);
-    resultPrint(strikeCount, ballCount);
-    if (strikeCount !== 3) {
-        userInputNumber(randomNumber);
+function gameStart(userNumber, computerNumber) {
+    inputValidation(userNumber);
+    const [countBall, countStrike] = strikeBallCount(userNumber, computerNumber);
+    getHint(countBall, countStrike);
+    if (countStrike !== 3) {
+        userInputNumber(computerNumber);
     } else {
         MissionUtils.Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
-        MissionUtils.Console.readLine("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n", wantNewstart);
+        MissionUtils.Console.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n', selectNewGame);
     }
 }
 
-function wantNewstart(number) {
+function selectNewGame(number) {
     const num = Number(number);
     if (num === 1) {
         const app = new App();
@@ -76,14 +75,14 @@ function threeDigitValidation(number) {
 }
 
 function numberRangeValidation(number) {
-    const isNumberBetween = number.every(eachDigit => eachDigit > 0 && eachDigit < 10);
+    const isNumberBetween = number.every(eachDigit => (eachDigit >= MIN_NUMBER && eachDigit <= MAX_NUMBER));
     if (!isNumberBetween) {
         throw "잘못된 값을 입력하였습니다.";
     }
 }
 
 function reduplicationValidation(number) {
-    const isReduplication = new Set(number).size === 3;
+    const isReduplication = (new Set(number).size === 3);
     if (!isReduplication) {
         throw "잘못된 값을 입력하였습니다.";
     }
@@ -92,26 +91,34 @@ function reduplicationValidation(number) {
 function strikeBallCount(userNumber, computerNumber) {
     let strikeCount = 0;
     let ballCount = 0;
+    145
     userNumber.forEach((eachDigit, index) => {
         if (eachDigit === computerNumber[index]) {
-            strikeCount++;
-        } else if (computerNumber.includes(eachDigit)) {
-            ballCount++;
+            strikeCount += 1;
+            return 0;
+        }
+        if (computerNumber.includes(eachDigit)) {
+            ballCount += 1;
+            return 0;
         }
     })
-    return [strikeCount, ballCount];
+    return [ballCount, strikeCount];
 }
 
-function resultPrint(strikeCount, ballCount) {
-    if (strikeCount > 0 && ballCount > 0) {
-        MissionUtils.Console.print(`${ballCount}볼 ${strikeCount}스트라이크`);
-    } else if (strikeCount > 0 && ballCount === 0) {
-        MissionUtils.Console.print(`${strikeCount}스트라이크`);
-    } else if (strikeCount === 0 && ballCount > 0) {
-        MissionUtils.Console.print(`${ballCount}볼`);
-    } else {
-        MissionUtils.Console.print(`낫싱`);
+
+function getHint(ballCount, strikeCount) {
+    let hint = '';
+    if (ballCount) {
+        hint += `${ballCount}볼`;
     }
+    if (ballCount && strikeCount) {
+        hint += " ";
+    }
+    if (strikeCount) {
+        hint += `${strikeCount}스트라이크`;
+    }
+    hint = hint || "낫싱";
+    MissionUtils.Console.print(hint);
 }
 
 module.exports = App;
