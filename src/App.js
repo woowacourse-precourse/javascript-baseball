e979360e58700c3aadccb927bcca667d8746c942
chsua <csj1919@naver.com>
2022-11-06 18:07:36 +0900

    feat: 1. 랜덤숫자생성함수 작성

--- src/App.js
+++ src/App.js
@@ -1,4 +1,19 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
+  makeComNum() {
+    // 1. 컴퓨터 랜덤숫자 생성
+    // return: [0,0,0]
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    let set = new Set();
+
+    while (set.size != 3) {
+      let num = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (num !== 0) set.add(num);
+    }
+    return [...set];
+  }
+
   play() {}
 }
 
