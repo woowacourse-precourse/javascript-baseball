9215cde0e85d4a84c37c669a1bc4c61273e2a90f
feb-dain <feb.dain@gmail.com>
2022-11-07 02:18:34 +0900

    feat: accept number from user

--- src/Play.js
+++ src/Play.js
@@ -12,6 +12,14 @@ const makeRandomValue = () => {
   return computerValue;
 };
 
+const userValue = async () => {
+  const userInput = await consoleUtil.readLine("숫자를 입력해주세요 : ");
+  if (userInput < 1 || isNaN(userInput) || userInput.length !== 3) {
+    throw new Error("3자리 양의 정수를 입력해주세요.");
+  }
+  return Array.from(userInput, (num) => Number(num));
+};
+
 const playing = () => {};
 
 module.exports = playing;
