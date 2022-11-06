fc1867050dd1ed339098f1e6b2a2b929a85e6d3c
froggy1014 <clcl6084@gmail.com>
2022-11-06 21:38:17 +0900

    refactor: 프로세스간 필요없는 메소드 제거 및 병합
    before - 유효성 검사 -> 볼과 스트라이크 카운팅 -> 정답 비교 및 힌트 출력
    after - 유효성 검사 -> 볼과 스트라이크 카운팅 후 바로 정답 비교 및 힌트 출력

--- src/App.js
+++ src/App.js
@@ -20,30 +20,24 @@ class App extends GameUtil{
 
   validateInput(playerInput) {
     const result = super.validateInput(playerInput);
-    if (result === 'PASS') return this.rightInput(playerInput);
+    if (result === 'PASS') return this.printHint(playerInput);
 
     throw new Error(result);  
   }
 
-  rightInput(playerInput) {   
+  printHint(playerInput) {
     const ball = super.countBall(this.randoms, playerInput);
     const strike = super.countStrike(this.randoms, playerInput);
-
-    this.printHint(ball, strike);
-  }
-
-  printHint(ball, strike) {
-    if (strike === 3) {
-      Console.print('3스트라이크');
-      Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
-      return this.correctAnswer();
-    }
-
+    
+    if (strike === 3) return this.correctAnswer();
+    
     Console.print(super.showHint(ball, strike));
     this.play();
   }
-
+  
   correctAnswer() { 
+    Console.print('3스트라이크');
+    Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
     Console.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n', (decision) => {
       switch (Number(decision)) {
         case 1:
