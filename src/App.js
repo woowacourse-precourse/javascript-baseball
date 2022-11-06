bde28867e9eb3f3c0290126f771795f2f5ffad4e
JuhyunLim <87933367+userJu@users.noreply.github.com>
2022-11-06 14:18:14 +0900

    Feat: 컴퓨터의 임의의 수 선택 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 const mode = require("./a.js");
-const Constants = require("./utils/Constants");
+const { START_NUMBER, END_NUMBER, START_GAME } = require("./utils/Constants");
+const { getComputerNumber } = require("./utils/MissionUtils");
 
 class App {
   play() {
-    MissionUtils.Console.print(Constants.START_GAME);
+    MissionUtils.Console.print(START_GAME);
+    const computerNumbers = getComputerNumber(START_NUMBER, END_NUMBER);
   }
 }
 
 module.exports = App;
+
+const app = new App();
+app.play();
