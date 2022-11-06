ec5167e764cf39a71f5ed5fa02d9b1db147a7e32
WaiNaat <qoli0703@gmail.com>
2022-11-06 17:53:54 +0900

    test(CountTest): change output format to follow mission requirements
    미션 입출력 요구 사항에 맞는 출력을 하는지 검사하도록 코드를 수정.
    예시: 1볼 1스트라이크일 때
    기존 출력: 1 1
    현재 출력: 1볼 1스트라이크

--- __tests__/CountTest.js
+++ __tests__/CountTest.js
@@ -1,123 +1,150 @@
+const MissionUtils = require('@woowacourse/mission-utils');
 const App = require('../src/App');
+
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(MissionUtils.Console, 'print');
+  logSpy.mockClear();
+  return logSpy;
+};
 
 describe('볼-스트라이크 결과 표시', () => {
   test('경우 1', () => {
     const input = [1, 2, 3];
     const answer = [1, 2, 3];
     const app = new App();
+    const logSpy = getLogSpy();
 
     app.input = input;
     app.answer = answer;
     app.countResult();
+    app.printResult();
 
-    expect(`${app.ballCount} ${app.strikeCount}`).toEqual('0 3');
+    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining('3스트라이크'));
   });
 
   test('경우 2', () => {
     const input = [1, 2, 3];
     const answer = [2, 3, 1];
     const app = new App();
+    const logSpy = getLogSpy();
 
     app.input = input;
     app.answer = answer;
     app.countResult();
+    app.printResult();
 
-    expect(`${app.ballCount} ${app.strikeCount}`).toEqual('3 0');
+    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining('3볼'));
   });
 
   test('경우 3', () => {
     const input = [7, 4, 8];
     const answer = [1, 2, 3];
     const app = new App();
+    const logSpy = getLogSpy();
 
     app.input = input;
     app.answer = answer;
     app.countResult();
+    app.printResult();
 
-    expect(`${app.ballCount} ${app.strikeCount}`).toEqual('0 0');
+    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining('낫싱'));
   });
 
   test('경우 4', () => {
     const input = [7, 4, 8];
     const answer = [2, 1, 8];
     const app = new App();
+    const logSpy = getLogSpy();
 
     app.input = input;
     app.answer = answer;
     app.countResult();
+    app.printResult();
 
-    expect(`${app.ballCount} ${app.strikeCount}`).toEqual('0 1');
+    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining('1스트라이크'));
   });
 
   test('경우 5', () => {
     const input = [7, 4, 8];
     const answer = [7, 4, 6];
     const app = new App();
+    const logSpy = getLogSpy();
 
     app.input = input;
     app.answer = answer;
     app.countResult();
+    app.printResult();
 
-    expect(`${app.ballCount} ${app.strikeCount}`).toEqual('0 2');
+    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining('2스트라이크'));
   });
 
   test('경우 6', () => {
     const input = [7, 4, 8];
     const answer = [7, 8, 4];
     const app = new App();
+    const logSpy = getLogSpy();
 
     app.input = input;
     app.answer = answer;
     app.countResult();
+    app.printResult();
 
-    expect(`${app.ballCount} ${app.strikeCount}`).toEqual('2 1');
+    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining('2볼 1스트라이크'));
   });
 
   test('경우 7', () => {
     const input = [7, 4, 8];
     const answer = [1, 2, 8];
     const app = new App();
+    const logSpy = getLogSpy();
 
     app.input = input;
     app.answer = answer;
     app.countResult();
+    app.printResult();
 
-    expect(`${app.ballCount} ${app.strikeCount}`).toEqual('0 1');
+    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining('1스트라이크'));
   });
 
   test('경우 8', () => {
     const input = [7, 4, 8];
     const answer = [8, 2, 4];
     const app = new App();
+    const logSpy = getLogSpy();
 
     app.input = input;
     app.answer = answer;
     app.countResult();
+    app.printResult();
 
-    expect(`${app.ballCount} ${app.strikeCount}`).toEqual('2 0');
+    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining('2볼'));
   });
 
   test('경우 9', () => {
     const input = [7, 4, 8];
     const answer = [4, 6, 8];
     const app = new App();
+    const logSpy = getLogSpy();
 
     app.input = input;
     app.answer = answer;
     app.countResult();
+    app.printResult();
 
-    expect(`${app.ballCount} ${app.strikeCount}`).toEqual('1 1');
+    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining('1볼 1스트라이크'));
   });
 
   test('경우 10', () => {
     const input = [7, 4, 8];
     const answer = [3, 7, 6];
     const app = new App();
+    const logSpy = getLogSpy();
 
     app.input = input;
     app.answer = answer;
     app.countResult();
+    app.printResult();
 
-    expect(`${app.ballCount} ${app.strikeCount}`).toEqual('1 0');
+    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining('1볼'));
   });
 });
