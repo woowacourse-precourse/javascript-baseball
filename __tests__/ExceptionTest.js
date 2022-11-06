5071fcabf409078990c56eecad733d0f181e689c
turtle601 <turtle601@naver.com>
2022-11-06 12:02:18 +0900

    refactor(exceptionTest) 재시작/종료 에러 관련 클래스 네이밍 변경
    Changed:
    RestartException -> NextException

--- __tests__/ExceptionTest.js
+++ __tests__/ExceptionTest.js
@@ -1,4 +1,4 @@
-const { Exception, BaseBallException, RestartException } = require('../src/Exception');
+const { Exception, BaseBallException, NextException } = require('../src/Exception');
 
 describe('Exception클래스 - 숫자야구 예외 처리 로직', () => {
   test('(입력값 "1345"', () => {
@@ -54,7 +54,7 @@ describe('예외 처리 로직 Exception클래스 - (재시작/종료) 입력값
   test('(0과 1을 제외한 숫자)입력값 "3"', () => {
     const input = '3';
     const exception = new Exception();
-    const throwErrorFn = () => exception.occurError(new RestartException(input));
+    const throwErrorFn = () => exception.occurError(new NextException(input));
 
     expect(throwErrorFn).toThrow();
   });
@@ -62,7 +62,7 @@ describe('예외 처리 로직 Exception클래스 - (재시작/종료) 입력값
   test('(0과 1을 제외한 문자)입력값 "a"', () => {
     const input = 'a';
     const exception = new Exception();
-    const throwErrorFn = () => exception.occurError(new RestartException(input));
+    const throwErrorFn = () => exception.occurError(new NextException(input));
 
     expect(throwErrorFn).toThrow();
   });
