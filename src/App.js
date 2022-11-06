cc948cd07fdc4c346eb1dbdbb5eef26c2e496d8b
Narcoker <jykim9868@gmail.com>
2022-11-07 02:15:21 +0900

    Feat: Add makeAnswer()

--- src/App.js
+++ src/App.js
@@ -5,9 +5,21 @@ class App {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
     MissionUtils.Console.close();
   }
+  makeAnswer() {
+    const answer = [];
+    while (answer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!answer.includes(number)) {
+        answer.push(number);
+      }
+    }
+    return answer;
+  }
 
   play() {
     this.printStart();
+    let answer = this.makeAnswer();
+
   }
 }
 
