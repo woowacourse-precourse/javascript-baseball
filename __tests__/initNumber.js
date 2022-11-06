54d72559956b7e4d854d9cfcf70a5580f3dcfea0
jisu-kim <kss2460@naver.com>
2022-11-06 17:53:49 +0900

    test: initNumber 길이 체크 test 작성

--- __tests__/initNumber.js
+++ __tests__/initNumber.js
@@ -1,7 +1,9 @@
-const setNumber = require("../src/setNumber");
+const initNumber = require("../src/initNumber");
 
 describe("초기 3자리 숫자 세팅", () => {
-  test("중복 체크", () => {
-    expect(setNumber()).toEqual([1, 2, 3]);
+  const init = initNumber();
+  const range = 3;
+  test("길이가 3인지 확인", () => {
+    expect(init.length).toBe(3);
   });
 });
