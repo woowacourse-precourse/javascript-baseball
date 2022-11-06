72793e8593acb6daff8df6264a87ee2d96b30b58
dladncks1217 <dlaxodud1217@gmail.com>
2022-11-07 01:54:42 +0900

    test: 랜덤 변수(정답) 생성 기능 테스트 추가

--- __tests__/utilTest.js
+++ __tests__/utilTest.js
@@ -1,16 +1,9 @@
 const countBallAndStrike = require("../src/utils/countBallAndStrike");
+const makeRandomNumber = require("../src/utils/makeRandomNumber");
 const printGameMessage = require("../src/utils/printGameMessage");
 const verifyInputNumber = require("../src/utils/verifyInputNumber");
 
 describe("유틸함수 동작 테스트", () => {
-  test("볼 스트라이크 개수 확인 동작 테스트", () => {
-    expect(countBallAndStrike("123", "321")).toEqual({ strike: 1, ball: 2 });
-    expect(countBallAndStrike("123", "456")).toEqual({ strike: 0, ball: 0 });
-    expect(countBallAndStrike("789", "786")).toEqual({ strike: 2, ball: 0 });
-    expect(countBallAndStrike("467", "746")).toEqual({ strike: 0, ball: 3 });
-    expect(countBallAndStrike("641", "361")).toEqual({ strike: 1, ball: 1 });
-    expect(countBallAndStrike("361", "361")).toEqual({ strike: 3, ball: 0 });
-  });
 
   test("입력값 검증 함수 테스트", () => {
     expect(verifyInputNumber("123")).toBe(true);
@@ -18,6 +11,23 @@ describe("유틸함수 동작 테스트", () => {
     expect(verifyInputNumber("1523")).toBe(false);
     expect(verifyInputNumber("a13")).toBe(false);
     expect(verifyInputNumber("952")).toBe(true);
+  });
+
+  test("랜덤 변수(정답) 생성 확인 테스트", () => {
+    expect(verifyInputNumber(makeRandomNumber())).toBe(true);
+    expect(verifyInputNumber(makeRandomNumber())).toBe(true);
+    expect(verifyInputNumber(makeRandomNumber())).toBe(true);
+    expect(verifyInputNumber(makeRandomNumber())).toBe(true);
+    expect(verifyInputNumber(makeRandomNumber())).toBe(true);
+  });
+
+  test("볼 스트라이크 개수 확인 동작 테스트", () => {
+    expect(countBallAndStrike("123", "321")).toEqual({ strike: 1, ball: 2 });
+    expect(countBallAndStrike("123", "456")).toEqual({ strike: 0, ball: 0 });
+    expect(countBallAndStrike("789", "786")).toEqual({ strike: 2, ball: 0 });
+    expect(countBallAndStrike("467", "746")).toEqual({ strike: 0, ball: 3 });
+    expect(countBallAndStrike("641", "361")).toEqual({ strike: 1, ball: 1 });
+    expect(countBallAndStrike("361", "361")).toEqual({ strike: 3, ball: 0 });
   });
 
   test("볼 스트라이크 출력 함수 테스트", () => {
