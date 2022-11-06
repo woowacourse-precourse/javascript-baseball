baac6fa06b9560ba921fca77353ef3c01b010397
“somm12” <sominan8744@gmail.com>
2022-11-06 13:01:17 +0900

    feat: 랜덤 숫자 만들기 - makeRandomNumber함수 정의

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+const MissionUtils = require("@woowacourse/mission-utils");
 class App {
-  play() {}
+  randomNumber = [];
+
+  constructor() {
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+  }
+  play() {
+    this.makeRandomNumber();
+  }
+  makeRandomNumber() {
+    let arr = new Set();
+    while (arr.size < 3) {
+      arr.add(MissionUtils.Random.pickNumberInRange(1, 9));
+    }
+    this.randomNumber = Array.from(arr);
+  }
 }
 
 module.exports = App;
