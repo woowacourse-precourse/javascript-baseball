38ed4ab6f10fb70d7b8ca4fc86954dae7db85052
DESKTOP-PDMTIQO\hansu <fathlic5@gmail.com>
2022-11-06 22:58:24 +0900

    :recycle: refactor: 상수명 네이밍 컨벤션 리팩토링 자바스크립트 코드 컨벤션 중 상수들의 네이밍을  SNAKE_CASE로 작성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 const { Random, Console } = require("@woowacourse/mission-utils");
 
+const MAX_SIZE = 3;
+const START_NUMBER = 1;
+const END_NUMBER = 9;
+const REPLAY_GAME_KEY = '1';
+const END_GAME_KEY = '2';
+
 class App {
     play() {
         Console.print('숫자 야구 게임을 시작합니다.');
@@ -8,8 +14,8 @@ class App {
 
     isComputerNumbers() {
         const COMPUTER_NUMBERS = [];
-        while (COMPUTER_NUMBERS.length < 3) {
-            const RANDOM_NUMBER = Random.pickNumberInRange(1, 9);
+        while (COMPUTER_NUMBERS.length < MAX_SIZE) {
+            const RANDOM_NUMBER = Random.pickNumberInRange(START_NUMBER, END_NUMBER);
             if (!COMPUTER_NUMBERS.includes(RANDOM_NUMBER)) COMPUTER_NUMBERS.push(RANDOM_NUMBER);
         }
         return parseInt(COMPUTER_NUMBERS.join(''));
@@ -18,8 +24,8 @@ class App {
     isUserNumbers(computerNumbers) {
         return Console.readLine('숫자를 입력해주세요 : ',(userNumbers) => {
             if(isNaN(userNumbers)) throw new Error('숫자를 입력해주세요.');
-            if(userNumbers.toString().length !== 3) throw new Error('3자리 숫자를 입력해주세요.');
-            if(new Set(userNumbers).size !== 3) throw new Error('중복되지 않는 숫자를 입력해주세요.');
+            if(userNumbers.toString().length !== MAX_SIZE) throw new Error('3자리 숫자를 입력해주세요.');
+            if(new Set(userNumbers).size !== MAX_SIZE) throw new Error('중복되지 않는 숫자를 입력해주세요.');
             if(String(userNumbers).indexOf(0) !== -1) throw new Error('1부터 9의 숫자만 입력해주세요.');
             parseInt(userNumbers);
             this.isMatchNumbers(computerNumbers,userNumbers);
@@ -41,9 +47,9 @@ class App {
     replayGame() {
         Console.print('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.');
         Console.readLine('', (answer) => {
-            if(answer == 1) return this.isUserNumbers(this.isComputerNumbers());
-            if(answer == 2) return Console.print('게임 종료');
-            if(answer !== 1 && answer !== 2) throw new Error('1 또는 2를 입력하세요.');
+            if(answer == REPLAY_GAME_KEY) return this.isUserNumbers(this.isComputerNumbers());
+            if(answer == END_GAME_KEY) return Console.print('게임 종료');
+            if(answer !== REPLAY_GAME_KEY && answer !== END_GAME_KEY) throw new Error('1 또는 2를 입력하세요.');
         })
     }
 
