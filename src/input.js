8de3981aa445156bdb7e7b98e5cec7a3dea5a1b7
peacemaker474 <81507322+peacemaker474@users.noreply.github.com>
2022-11-06 15:19:01 +0900

    Feat: 게임이 끝나고 재시작 및 종료 여부 물어보기 추가

--- src/input.js
+++ src/input.js
@@ -14,12 +14,25 @@ function extractComputerNumber() {
     }
 }
 
+function resetAllInputNumbers(isRestart) {
+    if (isRestart === '1') {
+        baseballGameStart();
+    } else {
+        Console.close();
+    }
+}
+
+function baseballGameReset() {
+    Console.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n', resetAllInputNumbers)
+}
+
 function inputUserNumber(userNumber) {
     const input = userNumber.split('').map(Number);
     validateNumbers(input);
 
     if (compareNumbers(input, computerState())) {
         Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+        baseballGameReset();
     } else {
         return baseballGameStart();
     }
