d186ea81729f5f86dd20430c16ae92e3e6918581
Serin-Kim <serin9864@gmail.com>
2022-11-06 21:45:51 +0900

    feat: getResultMessage 함수 구현
    사용자의 입력 숫자에 대한 힌트와 결과(스트라이크, 볼, 낫싱) 출력함
    이 과정을 반복해 컴퓨터가 선택한 3개의 숫자를 모두 맞히면 게임을 종료함

--- src/App.js
+++ src/App.js
@@ -24,10 +24,42 @@ class App {
     return true;
   };
 
+  getResultMessage = (strike, ball, nothing) => {
+    let message = "";
+    if (nothing) message = "낫싱";
+    if (ball) message += `${ball}볼 `;
+    if (strike) message += `${strike}스트라이크`;
+    return message;
+  };
+
+  printResult = (ANSWER, input) => {
+    if (ANSWER === input) {
+      MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+      MissionUtils.Console.close();
+      return;
+    }
+
+    let strike = 0;
+    let ball = 0;
+    let nothing = true;
+
+    for (let i = 0; i < ANSWER.length; i++) {
+      const LETTER = input[i];
+      if (ANSWER.includes(LETTER)) {
+        ANSWER[i] === LETTER ? strike++ : ball++;
+        nothing = false;
+      }
+    }
+
+    const resultMessage = this.getResultMessage(strike, ball, nothing);
+    MissionUtils.Console.print(resultMessage);
+    this.getInputAndCompare(ANSWER);
+  };
+
   getInputAndCompare = (ANSWER) => {
     MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (input) => {
-      MissionUtils.Console.close();
       if (!this.isValid(input)) throw "Invalid input!"; // TODO: or this.checkIsValid() 하고 함수 안에서 throw?
+      this.printResult(ANSWER, input);
     });
   };
 
