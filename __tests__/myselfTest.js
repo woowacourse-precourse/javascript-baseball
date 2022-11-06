5f57e62708e6e3358b123a38c1720f599c1634ff
GC-Park <kuyt3122@naver.com>
2022-11-06 17:20:18 +0900

    feat:볼과 스트라이크 수 테스트 코드 작성 후 적용

--- __tests__/myselfTest.js
+++ __tests__/myselfTest.js
@@ -1,27 +1,91 @@
-const App = require("../src/App");
-const MissionUtils = require("@woowacourse/mission-utils");
+const App = require('../src/App')
+const MissionUtils = require('@woowacourse/mission-utils')
 
-describe("숫자 야구 게임", () => {
-  // test("사용자 입력 확인 테스트 코드 1", () => {
-  //   const input = "123";
-  //   const result = input.split("");
+describe('숫자 야구 게임', () => {
+    // test("사용자 입력 확인 테스트 코드 1", () => {
+    //   const input = "123";
+    //   const result = input.split("");
 
-  //   const app = new App();
-  //   app.play();
+    //   const app = new App();
+    //   app.play();
 
-  //   expect(result).toContain("2", "1", "3");
-  //   expect(result).toContainEqual("1", "2", "3");
-  // });
-  // test("사용자 입력 확인 테스트 코드 2", () => {
-  //   const input = "876";
-  //   const result = input.split("");
+    //   expect(result).toContain("2", "1", "3");
+    //   expect(result).toContainEqual("1", "2", "3");
+    // });
+    // test("사용자 입력 확인 테스트 코드 2", () => {
+    //   const input = "876";
+    //   const result = input.split("");
 
-  //   const app = new App();
-  //   app.play();
+    //   const app = new App();
+    //   app.play();
 
-  //   expect(result).toContain("7", "6", "8");
-  //   expect(result).toContainEqual("8", "7", "6");
-  // });
+    //   expect(result).toContain("7", "6", "8");
+    //   expect(result).toContainEqual("8", "7", "6");
+    // });
 
+    const mockQuestions = (answers) => {
+      MissionUtils.Console.readLine = jest.fn();
+      answers.reduce((acc, input) => {
+        return acc.mockImplementationOnce((question, callback) => {
+          callback(input);
+        });
+      }, MissionUtils.Console.readLine);
+    };
+    
+    const mockRandoms = (numbers) => {
+      MissionUtils.Random.pickNumberInRange = jest.fn();
+      numbers.reduce((acc, number) => {
+        return acc.mockReturnValueOnce(number);
+      }, MissionUtils.Random.pickNumberInRange);
+    };
+    
+    const getLogSpy = () => {
+      const logSpy = jest.spyOn(MissionUtils.Console, "print");
+      logSpy.mockClear();
+      return logSpy;
+    };
+
+    // test("볼과 스트라이크 체크", () => {
+    //   const randoms = [3, 6, 9];
+    //   const answers = ["246", "872", "386", "369"];
+    //   const logSpy = getLogSpy();
+    //   const messages = [
+    //     "1볼",
+    //     "낫싱",
+    //     "1볼 1스트라이크",
+    //     "3스트라이크",
+    //   ];
   
-});
+    //   mockRandoms(randoms);
+    //   mockQuestions(answers);
+  
+    //   const app = new App();
+    //   app.play();
+  
+    //   messages.forEach((output) => {
+    //     expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    //   });
+    // });
+
+    // test("볼과 스트라이크 체크", () => {
+    //   const randoms = [7, 9, 8];
+    //   const answers = ["246", "135", "879", "798"];
+    //   const logSpy = getLogSpy();
+    //   const messages = [
+    //     "낫싱",
+    //     "낫싱",
+    //     "3볼",
+    //     "3스트라이크",
+    //   ];
+  
+    //   mockRandoms(randoms);
+    //   mockQuestions(answers);
+  
+    //   const app = new App();
+    //   app.play();
+  
+    //   messages.forEach((output) => {
+    //     expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    //   });
+    // });
+})
--- src/App.js
+++ src/App.js
@@ -23,12 +23,51 @@ class App {
         return computer
     }
 
-    
+    strikeCount(computerInput, userInput) {
+        let strikeTotal = 0
+        computerInput.map((computer, index) => {
+            if (computer === Number(userInput[index])) strikeTotal++
+        })
+        return strikeTotal
+    }
+
+    ballCount(computerInput, userInput) {
+        let ballTotal = 0
+        computerInput.map((computer, index) => {
+            if (
+                computer !== Number(userInput[index]) &&
+                computerInput.includes(Number(userInput[index]))
+            )
+                ballTotal++
+        })
+        return ballTotal
+    }
+
+    compareNumber(userInput) {
+        const strikes = this.strikeCount(this.computerInput, userInput)
+        const balls = this.ballCount(this.computerInput, userInput)
+        if (strikes === 0 && balls === 0) {
+            MissionUtils.Console.print('낫싱')
+            this.userInputNumber()
+        } else if (strikes === 3) {
+            MissionUtils.Console.print('3스트라이크')
+            MissionUtils.Console.close()
+        } else if (strikes === 0) {
+            MissionUtils.Console.print(balls + '볼')
+            this.userInputNumber()
+        } else if (balls === 0) {
+            MissionUtils.Console.print(strikes + '스트라이크')
+            this.userInputNumber()
+        } else {
+            MissionUtils.Console.print(balls + '볼 ' + strikes + '스트라이크')
+            this.userInputNumber()
+        }
+    }
 
     userInputNumber() {
         MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (answer) => {
             this.userInput = answer.split('')
-            MissionUtils.Console.print(this.userInput);
+            this.compareNumber(this.userInput)
         })
     }
 }
