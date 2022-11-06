fac4dd16a820e1de5ddf0a63ad7044f975c0c183
cham-min <hymn9613@gmail.com>
2022-11-06 22:34:46 +0900

    fix(baseballGame): add this reference

--- src/App.js
+++ src/App.js
@@ -10,12 +10,14 @@ class App {
 
   start() {
     Console.print("숫자 야구 게임을 시작합니다.");
+
+    this.COMPUTER_NUMBER = this.randomNum();
   }
 
   randomNum() {
-    COMPUTER_NUMBER = Random.pickUniqueNumbersInRange(1, 9, 3);
+    this.COMPUTER_NUMBER = Random.pickUniqueNumbersInRange(1, 9, 3);
 
-    return COMPUTER_NUMBER;
+    return this.COMPUTER_NUMBER;
   }
 
   playerInput() {
