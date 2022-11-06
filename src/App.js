c638225982d8b8af377ca36ae25847bd77871c95
Yoonkyoung Kim <100656920+Yoonkyoungme@users.noreply.github.com>
2022-11-06 18:30:45 +0900

    feat: 비교 결과 출력

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
     const computerNumber = this.createComputerNumber();
     const userNumber = this.inputUserNumber(computerNumber);
-    console.log(computerNumber, userNumber);
+    // console.log(computerNumber, userNumber);
   }
 
   // 컴퓨터의 숫자 랜덤으로 생성한다. (1부터 9까지 서로 다른 수로 이루어진 3자리의 수)
@@ -24,13 +24,10 @@ class App {
   // 사용자에게 숫자를 입력 받는다.
   inputUserNumber(computerNumber) {
     let userNumber;
-    MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (num) => {
-      this.isNumberVaild(num);
-      userNumber = num;
-      const compareResults = this.countBallAndStrike(
-        userNumber,
-        computerNumber
-      );
+    MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (number) => {
+      this.isNumberVaild(number);
+      userNumber = number;
+      this.countScore(number, computerNumber);
     });
     return userNumber;
   }
@@ -67,6 +64,12 @@ class App {
     }
   }
 
+  countScore(user, computer) {
+    const score = this.countBallAndStrike(user, computer);
+    const result = this.printScore(score);
+    console.log(result);
+  }
+
   // 사용자가 입력한 숫자와 컴퓨터 숫자를 비교하여 ball, strike 개수 세기
   countBallAndStrike(user, computer) {
     let score = {
@@ -81,6 +84,27 @@ class App {
       }
     });
     return score;
+  }
+
+  // 비교 결과 출력
+  printScore(score) {
+    let result = "";
+    const ball = score.ball;
+    const strike = score.strike;
+
+    if (score.ball > 0 && score.strike > 0) {
+      result += `${score.ball}볼 ${score.strike}스트라이크`;
+    } else if (strike === 0 && ball > 0) {
+      result += `${score.ball}볼`;
+    } else if (ball === 0 && strike > 0) {
+      result += `${score.strike}스트라이크`;
+    }
+
+    if (!result) {
+      result = "낫싱";
+    }
+
+    MissionUtils.Console.print(result);
   }
 }
 
