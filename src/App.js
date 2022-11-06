e46b353ec1f8eabe635259e0fbd1cb4922a42b83
HoberMin <102784200+HoberMin@users.noreply.github.com>
2022-11-07 02:41:47 +0900

    feat : play함수에서 랜덤숫자를 생성 & 게임시작

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ const { Random } = require("@woowacourse/mission-utils");
 
 let computerNum = [];
 
+function play() {
+  Console.print("숫자 야구 게임을 시작합니다.");
+  computerNum = MakeNum();
+  proceedGame(computerNum);
+}
 
     }
   }
