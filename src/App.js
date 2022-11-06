29ac5071387b7168208b04eb7de17923baafa49f
AhnGeunTaek <dksrmsxor@gmail.com>
2022-11-06 21:04:22 +0900

    Feat: 사용자 입력값이 1 ~ 9 사이의 숫자인지 체크하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ const MissionUtils = require('@woowacourse/mission-utils');
 class App {
   play() {
     this.startGame();
-    this.getComputerNumber();
+    this.playBaseballGame();
   }
 
   startGame() {
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다');
+  }
+
+  playBaseballGame() {
+    const computerInputNumbers = this.getComputerNumber();
+    this.getUserNumbers();
   }
 
   getComputerNumber() {
@@ -24,15 +29,27 @@ class App {
   }
 
   getUserNumbers() {
-    let userAnswer;
-
-    MissionUtils.Console.readLine('숫자를 입력해주세요 :', (answer) => {
-      userAnswer = answer;
+    MissionUtils.Console.readLine('숫자를 입력해주세요 :', (userAnswer) => {
+      this.isValidUserNumbers(userAnswer);
     });
-    return userAnswer;
+  }
+
+  isValidUserNumbers(userAnswer) {
+    const userNumbers = userAnswer.split('').map((number) => parseInt(number, 10));
+    isScopeUserNumber(userNumbers);
+    isScopeLength(userNumbers);
+  }
+
+  isScopeUserNumber(userNumbers) {
+    const isScope = userNumbers.every((number) => (number >= 1 && number <= 9 ? true : false));
+
+    if (!isScope) {
+      throw new Error('1 ~ 9까지의 숫자만 입력하세요');
+    }
   }
 }
 
 const app = new App();
 app.play();
+
 module.exports = App;
