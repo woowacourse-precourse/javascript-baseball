14b1b9375a2e08636813a96b1a193916d322a719
GC-Park <kuyt3122@naver.com>
2022-11-06 16:55:50 +0900

    feat:사용자 입력 테스트 코드 추가 후 적용

--- src/App.js
+++ src/App.js
@@ -23,79 +23,16 @@ class App {
         return computer
     }
 
-    strikeCount(computerInput, userInput) {
-        let strikeTotal = 0
-        computerInput.map((computer, index) => {
-            if (computer === Number(userInput[index])) strikeTotal++
-        })
-        return strikeTotal
-    }
-
-    ballCount(computerInput, userInput) {
-        let ballTotal = 0
-        computerInput.map((computer, index) => {
-            if (
-                computer !== Number(userInput[index]) &&
-                computerInput.includes(Number(userInput[index]))
-            )
-                ballTotal++
-        })
-        return ballTotal
-    }
-
-    compareNumber(userInput) {
-        const strikes = this.strikeCount(this.computerInput, userInput)
-        const balls = this.ballCount(this.computerInput, userInput)
-        if (strikes === 0 && balls === 0) {
-            MissionUtils.Console.print('낫싱')
-            this.userInputNumber()
-        } else if (strikes === 3) {
-            MissionUtils.Console.print('3스트라이크')
-            this.finishAndRestart()
-        } else if (strikes === 0) {
-            MissionUtils.Console.print(balls + '볼')
-            this.userInputNumber()
-        } else if (balls === 0) {
-            MissionUtils.Console.print(strikes + '스트라이크')
-            this.userInputNumber()
-        } else {
-            MissionUtils.Console.print(balls + '볼 ' + strikes + '스트라이크')
-            this.userInputNumber()
-        }
-    }
+    
 
     userInputNumber() {
         MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (answer) => {
             this.userInput = answer.split('')
-            const validation = this.exceptionHandler(this.userInput)
-            if (validation) throw new Error()
-            this.compareNumber(this.userInput)
+            MissionUtils.Console.print(this.userInput);
         })
-    }
-
-    finishAndRestart() {
-        MissionUtils.Console.readLine('3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n',(answer) => {
-            if (Number(answer) === 1) this.play()
-            else if (Number(answer) === 2) {
-                MissionUtils.Console.print('게임 종료')
-                MissionUtils.Console.close()
-            }
-        })
-    }
-
-    exceptionHandler(userInput) {
-        if (userInput.length !== 3) return 1
-
-        let isNumber = 0
-        userInput.map((user) => {
-            if (isNaN(Number(user))) return 1
-        })
-
-        if (userInput.includes('0')) return 1
-
-        const repeatCheck = new Set(userInput)
-        if (repeatCheck.size !== 3) return 1
     }
 }
 
+const app = new App();
+app.play();
 module.exports = App
