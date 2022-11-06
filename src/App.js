a8b03803ece5781d17a8427ef1a6924d9b88f404
seunghoria <102285042+seunghoria@users.noreply.github.com>
2022-11-06 17:54:00 +0900

    feat: 게임 시작 문구 출력 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
+const mConsole = MissionUtils.Console;
+const mRandom = MissionUtils.Random;
+
 class App {
-  play() {}
+  play() {
+    this.startGame();
+  }
+
+  startGame() {
+    mConsole.print("숫자 야구 게임을 시작합니다.");
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
