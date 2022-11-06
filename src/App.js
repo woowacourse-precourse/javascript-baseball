db02bd3f7743dbfeacf0f448115d82eeacfe91ab
millifail <hwangbird99@gmail.com>
2022-11-06 16:47:18 +0900

    feat(number-generator):update code
    - [게임시작] 게임시작 시 콘솔에 문구 출력

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import * as MissionUtils from "@woowacourse/mission-utils";
 
 class App {
   play() {
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
     const RANDOM_NUMBER = this.GET_RANDOM_NUMBER();
   }
 
