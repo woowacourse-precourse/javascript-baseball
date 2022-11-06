40e6b614429948b59ecabe0df7194558ad5ce631
leeseooo <leeseoooo@naver.com>
2022-11-06 17:16:35 +0900

    feat: 게임의 정답 숫자를 구하는 함수 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  play() {
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+  }
+  getComputerNumber() {
+    while (this.computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+
+      if (!this.computer.includes(number)) {
+        this.computer.push(number);
+      }
+    }
+    MissionUtils.Console.print(this.computer);
+  }
 }
 
 module.exports = App;
