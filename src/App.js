8ae2e24bed72e153f1176355a199356ea1607de5
oio337a <oio337a@gmail.com>
2022-11-06 20:34:54 +0900

    feat : get strike and ball count

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-const { Random, Console } = require("@woowacourse/mission-utils");
+const { Console } = require("@woowacourse/mission-utils");
 const MESSAGE = require("../constant/message");
 const UserInput = require("./UserInput");
 const Solution = require("./Solution");
@@ -13,12 +13,29 @@ class App {
     return Console.print(message);
   }
 
+  getStrikeBallCount(solution, userinput) {
+    let strike = 0;
+    let ball = 0;
+
+    for (let index = 0; index < 3; index++) {
+      strike += solution[index] === userinput[index];
+    }
+
+    for (let inputValue of userinput) {
+      ball += solution.includes(inputValue);
+    }
+    ball -= strike;
+
+    return [strike, ball];
+  }
+
   readInput(solution) {
     Console.readLine(MESSAGE.USER_INPUT_REQUEST, (userinput) => {
       const inputError = new UserInput(userinput);
       if (!inputError.checkAllUserInput) {
         return this.throwError();
       }
+
       Console.close();
     });
   }
