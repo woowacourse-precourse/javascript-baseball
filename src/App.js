e077d93fa4e66bb50cce52bc67fbec8f3064654d
Leemainsw <s2018s15@e-mirim.hs.kr>
2022-11-07 00:52:30 +0900

    feat : 게임 시작 문구 출력

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 const MissionUtils = require('@woowacourse/mission-utils');
-
 const { Random, Console } = MissionUtils;
 class App {
-  play() {}
+  play() {
+    Console.print("숫자 야구 게임을 시작합니다.");
+  }
 }
 
 module.exports = App;
