762da12cf36a7c0a4cc49b7ef8ef2525875ffae5
rabBit64 <gojh47@gmail.com>
2022-11-06 10:55:50 +0900

    feat: 기능목록3 - 힌트 제공(스트라이크,볼,낫싱)

--- src/App.js
+++ src/App.js
@@ -26,6 +26,53 @@ function guessNumber(MissionUtils){
   });
   return number;
 }
+function countHint(computer,guess){
+  let strikeCnt = 0;
+  let ballCnt = 0;
+  let strikeIdx = new Array(computer.length);
+  for(let idx=0; idx<computer.length; idx++){
+    strikeIdx[idx]=false;
+  }
+  for(let idx=0; idx<3; idx++){
+    // 3-1. 같은 수가 같은 자리에 있으면 1스트라이크
+    if(computer[idx]==guess[idx]){
+      strikeCnt+=1;
+      strikeIdx[idx]=true;
+    }
+  }
+  // 3-2. 같은 수가 다른 자리에 있으면 1볼
+  for(let comIdx=0; comIdx<computer.length; comIdx++){
+    let computerNum = computer[comIdx];
+    for(let guessIdx=0; guessIdx<guess.length; guessIdx++){
+      let guessNum = guess[guessIdx];
+      if(computerNum == guessNum && comIdx != guessIdx && strikeIdx[comIdx]==false){
+        ballCnt+=1;
+      }
+    }
+  }
+  return [strikeCnt, ballCnt];
+}
+function provideHint(computer, guess, MissionUtils){
+  let count = countHint(computer,guess);
+  let strikeCnt = count[0];
+  let ballCnt = count[1];
+  
+  let strikeAns = strikeCnt + "스트라이크";
+  let ballAns = ballCnt + "볼";
+  let hint;
+  if(strikeCnt!=0 && ballCnt==0){
+    hint = strikeAns;
+  }else if(strikeCnt==0 && ballCnt!=0){
+    hint = ballAns;
+  }else if(strikeCnt!=0 && ballCnt!=0){
+    hint = strikeAns + " "+ballAns;
+  }else if(strikeCnt==0 && ballCnt==0){
+    // 3-3. 같은 수가 전혀 없으면 낫싱
+    hint = "낫싱";
+  }
+  MissionUtils.Console.print(hint);
+  
+}
 class App {
   play() {
     const MissionUtils = require("@woowacourse/mission-utils");
@@ -36,9 +83,9 @@ class App {
       while(true){
         //2. 게임 플레이어가 서로 다른 3개의 숫자를 입력한다.
         const guess = guessNumber(MissionUtils);
-    
+        // 3. 힌트를 제공한다.
+        let ret = provideHint(computer,guess, MissionUtils);
       }
-      
     }
   }
 }
