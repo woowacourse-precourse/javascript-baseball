49211fed52bcf031b1d4912ff7973d9764205b64
yan9vvoojin <yan9vvoojin@naver.com>
2022-11-07 01:32:16 +0900

    feat: [App.js] 사용자의 입력값을 확인하고 결과를 처리하는 기능인 handleInput 추가

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 const Message = require('./Message');
+const Input = require('./Input');
+const Count = require('./Count');
 const createRandomNumbers = require('./createRandomNumbers');
 
 class App {
@@ -13,6 +15,21 @@ class App {
 
   static start() {
     App.computerNumbers = createRandomNumbers();
+    Message.requestInput(App.handleInput);
+  }
+
+  static handleInput(input) {
+    const userNumbers = Input.toNumbers(input);
+    if (!Input.isValid(userNumbers)) {
+      return Message.throwError();
+    }
+    const ballCount = Count.ball(App.computerNumbers, userNumbers);
+    const strikeCount = Count.strike(App.computerNumbers, userNumbers);
+    Message.printResult(ballCount, strikeCount);
+    if (strikeCount === 3) {
+      Message.printEnd();
+      return Message.requestRestart(App.handleRestart);
+    }
     Message.requestInput(App.handleInput);
   }
 }
