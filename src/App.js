b3541ab5907d4e72a5e51cd6772a68d79bf4442a
chee9835 <huisu121@gmail.com>
2022-11-06 20:41:22 +0900

    refactor: 구현한 함수를 class App 메서드로 추가

--- src/App.js
+++ src/App.js
@@ -1,99 +1,109 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
-function printMsg(message) {
-  MissionUtils.Console.print(message);
-}
-
-function setAnswer() {
-  return MissionUtils.Random.pickUniqueNumbersInRange(1, 10, 3);
-}
-
-function askNumInput() {
-  MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (input) => {
-    isValidInput(input);
-  });
-}
-
-function getHint(answer, input) {
-  var { ballCount, strikeCount } = isBallOrStrike(answer, input);
-
-  if (ballCount === 0 && strikeCount === 0) {
-    printMsg("낫싱");
-    askNumInput();
-  } else if (strikeCount === 3) {
-    printMsg("3스트라이크");
-    askRematchOrExit();
-  } else {
-    printMsg(`${ballCount}볼 ${strikeCount}스트라이크`);
-    askNumInput();
-  }
-}
-
-function isBallOrStrike(answer, input) {
-  var ballCount = 0;
-  var strikeCount = 0;
-
-  var answerSet = new Set(answer);
-  for (var idx in input) {
-    if (answer[idx] === input[idx]) {
-      strikeCount += 1;
-      continue;
-    }
-    if (answerSet.has(input[idx])) ballCount += 1;
-  }
-  return { ballCount, strikeCount };
-}
-
-function askRematchOrExit() {
-  MissionUtils.Console.readLine(
-    "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n",
-    (input) => {
-      switch (input) {
-        case "1":
-          printMsg("재경기를 진행합니다.");
-          askNumInput();
-          break;
-        case "2":
-          printMsg("게임을 종료합니다.");
-          MissionUtils.Console.close();
-          break;
-        default:
-          printMsg("1 또는 2를 입력하지 않았습니다.");
-          askRematchOrExit();
-      }
-    }
-  );
-}
-
-function isValidInput(input) {
-  if (!isNumber(input) || !isVaildLength(input) || !isAllDiffNum(input)) {
-    throw new Error("유효한 값이 아니므로 게임을 종료합니다.");
-  }
-}
-
-function isNumber(input) {
-  const NUM_REG = /[1-9]/g;
-  var remainNotNum = input.replace(NUM_REG, "");
-  if (remainNotNum) return false;
-  return true;
-}
-
-function isVaildLength(input) {
-  if (input.length !== 3) return false;
-  return true;
-}
-
-function isAllDiffNum(input) {
-  const setInput = new Set(input.split(""));
-  if (setInput.size !== 3) return false;
-  return true;
-}
-
 class App {
+  constructor() {
+    this.ANSWER;
+    this.input;
+  }
+
   play() {
-    printMsg("숫자 야구 게임을 시작합니다.");
-    const ANSWER = setAnswer();
-    askNumInput();
+    this.printMsg("숫자 야구 게임을 시작합니다.");
+    this.ANSWER = this.setAnswer();
+    this.askNumInput();
+  }
+
+  printMsg(message) {
+    MissionUtils.Console.print(message);
+  }
+
+  setAnswer() {
+    return MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
+  }
+
+  askNumInput() {
+    MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (input) => {
+      this.isValidInput(input);
+      this.getHint(input);
+    });
+  }
+
+  isValidInput(input) {
+    if (
+      !this.isNumber(input) ||
+      !this.isVaildLength(input) ||
+      !this.isAllDiffNum(input)
+    ) {
+      throw new Error("유효한 값이 아니므로 게임을 종료합니다.");
+    }
+  }
+
+  getHint(input) {
+    var { ballCount, strikeCount } = this.isBallOrStrike(input);
+
+    if (ballCount === 0 && strikeCount === 0) {
+      this.printMsg("낫싱");
+      this.askNumInput();
+    } else if (strikeCount === 3) {
+      this.printMsg("3스트라이크");
+      this.askRematchOrExit();
+    } else {
+      this.printMsg(`${ballCount}볼 ${strikeCount}스트라이크`);
+      this.askNumInput();
+    }
+  }
+
+  isBallOrStrike(input) {
+    var ballCount = 0;
+    var strikeCount = 0;
+
+    var answerSet = new Set(this.ANSWER);
+    for (var idx in input) {
+      if (this.ANSWER[idx] === input[idx]) {
+        strikeCount += 1;
+        continue;
+      }
+      if (answerSet.has(input[idx])) ballCount += 1;
+    }
+    return { ballCount, strikeCount };
+  }
+
+  askRematchOrExit() {
+    MissionUtils.Console.readLine(
+      "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n",
+      (input) => {
+        switch (input) {
+          case "1":
+            this.printMsg("재경기를 진행합니다.");
+            this.askNumInput();
+            break;
+          case "2":
+            this.printMsg("게임을 종료합니다.");
+            MissionUtils.Console.close();
+            break;
+          default:
+            this.printMsg("1 또는 2를 입력하지 않았습니다.");
+            this.askRematchOrExit();
+        }
+      }
+    );
+  }
+
+  isNumber(input) {
+    const NUM_REG = /[1-9]/g;
+    var remainNotNum = input.replace(NUM_REG, "");
+    if (remainNotNum) return false;
+    return true;
+  }
+
+  isVaildLength(input) {
+    if (input.length !== 3) return false;
+    return true;
+  }
+
+  isAllDiffNum(input) {
+    const setInput = new Set(input.split(""));
+    if (setInput.size !== 3) return false;
+    return true;
   }
 }
 
