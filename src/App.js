f580e6df865c4557a5c395cdcabce25b52b6ad09
seolyeongbae <peixueying@gmail.com>
2022-11-07 00:48:57 +0900

    feat: 게임 시작 함수를  구현한다

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  play() {
+    startGame();
+    MissionUtils.Console.close();
+  }
 }
+
+const startGame = () => {
+  MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+  const answer = generateAnswer();
+  inputNumber(answer);
+
+  return;
+};
 
 module.exports = App;
