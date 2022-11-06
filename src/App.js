7d51fd8ee644d2bed59c66a9f1fe811442edb08e
unqeye <salyuan@naver.com>
2022-11-06 13:56:15 +0900

    feat: 정답 생성

--- src/App.js
+++ src/App.js
@@ -6,6 +6,16 @@ class App {
   printGameStartMessage() {
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
   }
+
+  generateAnswer() {
+    this.answer = '';
+    while (this.answer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!this.answer.includes(number)) {
+        this.answer += number;
+      }
+    }
+  }
 }
 
 module.exports = App;
--- src/App.test.js
+++ src/App.test.js
@@ -11,3 +11,14 @@ describe('게임 시작 문구 출력', () => {
     expect(spy).toHaveBeenCalledWith('숫자 야구 게임을 시작합니다.');
   });
 });
+
+describe('정답 생성', () => {
+  test('정답 생성', () => {
+    const app = new App();
+    app.generateAnswer();
+
+    expect(app.answer.length).toBe(3);
+    expect(new Set(app.answer).size).toBe(3);
+    expect([...app.answer].every((number) => number >= 1 && number <= 9)).toBe(true);
+  });
+});
