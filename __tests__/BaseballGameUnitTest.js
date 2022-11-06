fd27ed24f35714738a7194119a799871c0f9a471
yong <tkddbs1644@gmail.com>
2022-11-06 23:51:15 +0900

    test: printMessage 메서드로 스트라이크, 볼, 낫싱을 출력

--- __tests__/BaseballGameUnitTest.js
+++ __tests__/BaseballGameUnitTest.js
@@ -99,4 +99,36 @@ describe("숫자 야구 게임 단위 테스트", () => {
       expect(ball).toBe(ballCount);
     });
   });
+
+  test("printMessage 메서드로 스트라이크, 볼, 낫싱을 출력", () => {
+    const app = new App();
+
+    const TEST_CASE = [
+      {
+        strike: 3,
+        ball: 0,
+        result: "3스트라이크",
+      },
+      {
+        strike: 0,
+        ball: 0,
+        result: "낫싱",
+      },
+      {
+        strike: 2,
+        ball: 1,
+        result: "1볼 2스트라이크",
+      },
+      {
+        strike: 0,
+        ball: 2,
+        result: "2볼",
+      },
+    ];
+
+    TEST_CASE.forEach(({ strike, ball, result }) => {
+      const message = app.printMessage({ strike, ball });
+      expect(message).toBe(result);
+    });
+  });
 });
