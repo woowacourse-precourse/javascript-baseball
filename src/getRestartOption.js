19bf4b6ee0ee03474503239b91e5f8426529dc8d
kangyeongmin <kym6050@hanyang.ac.kr>
2022-11-06 15:26:13 +0900

    style: 가독성 개선

--- src/getRestartOption.js
+++ src/getRestartOption.js
@@ -1,18 +1,17 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
 function getRestartOption() {
-  MissionUtils.Console.readLine(
-    "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n",
-    (input) => {
-      if (input == 1) {
-        const playBaseballGame = require("./playBaseballGame");
-        playBaseballGame();
-      }
-      if (input == 2) {
-        MissionUtils.Console.close();
-      }
+  const COMMENT = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n";
+
+  MissionUtils.Console.readLine(COMMENT, (input) => {
+    if (input == 1) {
+      const playBaseballGame = require("./playBaseballGame");
+      playBaseballGame();
     }
-  );
+    if (input == 2) {
+      MissionUtils.Console.close();
+    }
+  });
 }
 
 module.exports = getRestartOption;
--- src/getResult.js
+++ src/getResult.js
@@ -4,15 +4,11 @@ const checkBall = require("./checkBall");
 function getResult(solution, answer) {
   const numberOfStrike = checkStrike(solution, answer);
   const numberOfBall = checkBall(solution, answer);
+  const END_COMMENT = "3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료";
 
   if (numberOfStrike == 0 && numberOfBall != 0) return numberOfBall + "볼";
-  if (numberOfStrike != 0 && numberOfBall == 0) {
-    if (numberOfStrike === 3)
-      return (
-        numberOfStrike + "스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료"
-      );
-    return numberOfStrike + "스트라이크";
-  }
+  if (numberOfStrike != 0 && numberOfBall == 0)
+    return numberOfStrike === 3 ? END_COMMENT : numberOfStrike + "스트라이크";
   if (numberOfStrike != 0 && numberOfBall != 0)
     return numberOfBall + "볼 " + numberOfStrike + "스트라이크";
   if (numberOfStrike == 0 && numberOfBall == 0) return "낫싱";
