3ba760fcb4b136b0f88136d7b36453686f35ab57
sohyun <park.sohyun0430@gmail.com>
2022-11-06 14:53:37 +0900

    feat: generateComputerAnswer 함수 구현
    컴퓨터의 임의의 수 생성하기(게임에서 정답에 해당)
    MissionUtils 라이브러리에서 제공하는 Random 함수를 사용하여 구현함
    가능한 수: 123~987 사이의 수이면서, 0을 포함하지 않고 중복되는 수가 없는 세자리 수

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+const MissionUtils = require("@woowacourse/mission-utils");
 class App {
   play() {}
+
+  generateComputerAnswer(){
+    const COMPUTER_RANDOM_NUMBER = [];
+    while (COMPUTER_RANDOM_NUMBER.length < 3) {
+      const RANDOM_NUMBER = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!COMPUTER_RANDOM_NUMBER.includes(RANDOM_NUMBER)) {
+        COMPUTER_RANDOM_NUMBER.push(RANDOM_NUMBER);
+      }
+    }
+    return COMPUTER_RANDOM_NUMBER;
+  }
 }
 
 module.exports = App;
