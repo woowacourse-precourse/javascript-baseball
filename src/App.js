950f263d2961c91c71a2b47aedcf83194a7fe8a8
JuhyunLim <87933367+userJu@users.noreply.github.com>
2022-11-06 13:36:47 +0900

    Feat: 게임 시작 문구 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const mode = require("./a.js");
+const Constants = require("./constants/Constants");
+
 class App {
-  play() {}
+  play() {
+    MissionUtils.Console.print(Constants.START_GAME);
+  }
 }
 
 module.exports = App;
