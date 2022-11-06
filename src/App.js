c7bfe80bc9d3700e311d63c5c2f19c14dab1cfeb
energy1-914 <dlawldnjs5@daum.net>
2022-11-07 01:29:45 +0900

    feat: 스트라이크/볼 갯수 세는 메소드 구현

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ class App {
   constructor() {
     this.computerNumber = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
     this.userInputNumber;
+    this.countStrike = 0;
+    this.countBall = 0;
   }
 
   play() {
     //MissionUtils.Console.print(this.computerNumber);
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
-    MissionUtils.Console.readLine('숫자를 입력해주세요 :', (answer) => {
-      this.userInputNumber = answer;
+    MissionUtils.Console.readLine('숫자를 입력해주세요 :', (userInputNumber) => {
+      this.userInputNumber = userInputNumber;
+      //this.countMethod(this.userInputNumber, this.computerNumber)
     });
     // MissionUtils.Console.close();
   }
@@ -19,6 +22,19 @@ class App {
     return input.toString().split('');
   }
 
+  countMethod(userNumber, computerNumber) {
+    const userString = this.splitMethod(userNumber);
+    const computerString = this.splitMethod(computerNumber);
+    
+    for ( let i=0; i<3; i++ ) {
+      if ( userString[i] === computerString[i] ) {
+        this.countStrike += 1;
+      }
+      else if (userString.includes(computerString[i])) {
+        this.countBall += 1;
+      }
+    }
+  }
 }
 
 module.exports = App;
