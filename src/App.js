f4ba7ae18b94731fcacbb0939851252a2e01f685
Jungkyu Yang <67031524+jungkyuYang@users.noreply.github.com>
2022-11-07 04:24:01 +0900

    feat: add 랜덤한 숫자를 생성하는 기능
    MissionUtils 라이브러리에서 제공하는 Random.pickNumberInRange() 활용

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
-const { Console } = MissionUtils;
+const { Console, Random } = MissionUtils;
 
 class App {
   play() {
     Console.print("숫자 야구 게임을 시작합니다.");
+
     return;
+  }
+
+  createComputerNumber() {
+    const computer = [];
+
+    while (computer.length < 3) {
+      const number = Random.pickNumberInRange(1, 9);
+
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+
+    return computer;
   }
 }
 
