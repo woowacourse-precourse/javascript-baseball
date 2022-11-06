6eb435daf4a2d44cadf56d9a32777c848c0b1edc
min <hororonoodle@gmail.com>
2022-11-07 01:30:02 +0900

    feat : 게임 종료 관련 메시지 상수 추가

--- src/Constants/gameMessage.js
+++ src/Constants/gameMessage.js
@@ -2,6 +2,11 @@ class GameMessage {
   static WELCOME_MESSAGE = '숫자 야구 게임을 시작합니다.';
   static QUESTION_MESSAGE = '숫자를 입력해주세요. : ';
   static WRONG_INPUT_ERROR_MESSAGE = '잘못된 값을 입력하셨습니다.';
+  static END_GAME_MESSAGE =
+    '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n';
+  static RESTART_INPUT = '1';
+  static GAMEOVER_INPUT = '2';
+  static GAMEOVER_MESSAGE = '게임 종료';
 }
 
 module.exports = GameMessage;
