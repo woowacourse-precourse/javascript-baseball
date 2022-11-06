0369745219cc9f63e162436b6eeb0fc63cde4e7f
“somm12” <sominan8744@gmail.com>
2022-11-06 13:06:23 +0900

    feat: 숫자 입력 예외처리 함수 checkInputNumber 정의 및 App 종료 함수 gameTerminate 정의

--- src/App.js
+++ src/App.js
@@ -8,7 +8,35 @@ class App {
   }
   play() {
     this.makeRandomNumber();
-    this.makeUserInputNumber();
+    try {
+      this.makeUserInputNumber();
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  gameTerminate() {
+    MissionUtils.Console.print("게임을 종료합니다.");
+    MissionUtils.Console.close();
+  }
+
+  checkInputNumber() {
+    // 배열 안에 수들이 모두 숫자인지.
+    if (this.userInputNumber.includes(NaN)) {
+      throw "숫자가 아닌 입력입니다.";
+    }
+
+    if (this.userInputNumber.length !== 3) {
+      throw "세자리 숫자가 아닙니다!";
+    }
+    //1 ~ 9의 범위인지
+    if (this.userInputNumber.includes(0)) {
+      throw "1 ~ 9 범위내어야 합니다";
+    }
+    //다 다른 수인가. set 사용
+    if (new Set(this.userInputNumber).size !== 3) {
+      throw "같은 수가 중복입니다";
+    }
   }
 
   makeRandomNumber() {
@@ -23,6 +51,12 @@ class App {
   makeUserInputNumber() {
     MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (inputNumber) => {
       this.userInputNumber = [...inputNumber].map(Number);
+      try {
+        this.checkInputNumber();
+      } catch (e) {
+        this.gameTerminate();
+        throw e;
+      }
     });
   }
 }
