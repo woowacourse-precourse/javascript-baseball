665434f0d2a55d4349e06f66d1e57e09e2fa2bed
knh6269 <knh6269@gmail.com>
2022-11-07 01:39:05 +0900

    feat(userInput): 유저 숫자 입력 함수
    기능 목록(#3) : 유저의 숫자를 입력받는 함수

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 class App {
   play() {}
+  userInput(computerNumber) {
+    STD.readLine('숫자를 입력해주세요 : ', userNumber => {
+      this.checkUserNumberAndCompare(userNumber, computerNumber);
+    })
+  }
+
   getComputerAndUser() {
     const computerNumber = this.recieveComputer();
     this.checkCorrectNumber(computerNumber);
