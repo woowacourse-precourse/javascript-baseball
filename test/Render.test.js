652f76c9d9bc279157a76e1d1311e237a6e6b013
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2022-11-06 13:51:15 +0900

    feat(test): 게임 결과에 따라 유저에게 입력받는 기능 및 숫자를 배열로 변환시키는 기능 테스트

---
+++ test/Render.test.js
@@ -0,0 +1,18 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+describe("게임 결과에 따라 유저에게 입력받기", () => {
+  test("User로 부터 1과 2중 하나의 입력을 받는 함수 테스트", (done) => {
+    function callback() {
+      MissionUtils.Console.readLine("1은 재시작 2는 종료입니다", (number) => {
+        expect(number).toBe(number);
+        done();
+      });
+    }
+    callback();
+  });
+  test("number를 각 자릿수마다 string으로 변경 후 배열로 묶기 ", () => {
+    function numToArr(num) {
+      return [...String(num)];
+    }
+    expect(numToArr(123)).toEqual(["1", "2", "3"]);
+  });
+});
