e531af9280fc03026e258e8464427a626b7cf44d
cham-min <hymn9613@gmail.com>
2022-11-06 20:48:31 +0900

    feat: 컴퓨터 랜덤 세 자리 숫자 생성

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 const { Console, Random } = require("@woowacourse/mission-utils");
 
 class App {
+  let NUMBER = [];
+
   play() {
     this.start();
   }
 
   start() {
     Console.print("숫자 야구 게임을 시작합니다.");
+  }
+
+  randomNum() {
+    NUMBER = pickUniqueNumbersInRange(1, 9, 3);
+
+    return NUMBER;
   }
 }
 
