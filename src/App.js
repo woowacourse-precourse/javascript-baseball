cf4ba3319002db1583fb911ad13364cd23beb349
TeusLee <xodn1311@gmail.com>
2022-11-06 16:46:43 +0900

    Add ball_counting function

--- src/App.js
+++ src/App.js
@@ -1,3 +1,17 @@
+const MissionUtils = require("@woowacourse/mission-utils");    
+
+const ball_cnt = (input_num, computer_num) => {
+  let input_num_str = String(input_num)
+  let input_computer_str = String(computer_num)
+  result_cnt = 0
+  for (let i = 0; i < input_computer_str.length; i++){
+      if (input_num_str[i]!==input_computer_str[i] && input_computer_str.includes(input_num_str[i])){
+          result_cnt += 1
+      }
+  }
+  return result_cnt
+}
+
 class App {
   play() {}
 }
