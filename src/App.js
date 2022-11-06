aced87875b63c4a3515fa121b22d0125dc275456
cham-min <hymn9613@gmail.com>
2022-11-06 21:43:26 +0900

    style(variable): change variable name NUMBER to COMPUTER_NUMBER

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 const { Console, Random } = require("@woowacourse/mission-utils");
 
 class App {
-  NUMBER = [];
+  COMPUTER_NUMBER = [];
 
   play() {
     this.start();
@@ -12,9 +12,9 @@ class App {
   }
 
   randomNum() {
-    NUMBER = pickUniqueNumbersInRange(1, 9, 3);
+    COMPUTER_NUMBER = pickUniqueNumbersInRange(1, 9, 3);
 
-    return NUMBER;
+    return COMPUTER_NUMBER;
   }
 
   playerInput() {
@@ -27,9 +27,9 @@ class App {
     let BALL = 0;
     let STRIKE = 0;
 
-    for (let i = 0; i < this.NUMBER; i++) {
-      if (this.NUMBER[i] === input[i]) STRIKE++;
-      if (this.NUMBER.indexOf(input[i]) >= 0) BALL++;
+    for (let i = 0; i < this.COMPUTER_NUMBER; i++) {
+      if (this.COMPUTER_NUMBER[i] === input[i]) STRIKE++;
+      if (this.COMPUTER_NUMBER.indexOf(input[i]) >= 0) BALL++;
     }
   }
 }
