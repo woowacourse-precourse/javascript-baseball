3fa2a7f4cdc2ac5a3074c76c9a089fdd6d8679a9
jhyns <tockiib@gmail.com>
2022-11-06 22:18:50 +0900

    test: Fix test not working
    test함수가 제대로 동작하도록 수정

--- __tests__/TargetNumberTest.js
+++ __tests__/TargetNumberTest.js
@@ -2,20 +2,23 @@ const App = require('../src/App');
 
 describe('Target number test', () => {
   test('target number 배열의 길이는 3', () => {
-    const target = App.generateTargetNumber();
+    const app = new App();
+    const target = app.generateTargetNumber();
 
     expect(target.length).toEqual(3);
   });
 
   test('target number 배열의 값은 1~9 사이의 숫자', () => {
-    const targetNumber = App.generateTargetNumber();
+    const app = new App();
+    const targetNumber = app.generateTargetNumber();
     const result = targetNumber.every((number) => number >= 1 && number <= 9);
 
     expect(result).toBe(true);
   });
 
   test('target number 배열의 값은 중복되지 않음', () => {
-    const targetNumber = App.generateTargetNumber();
+    const app = new App();
+    const targetNumber = app.generateTargetNumber();
     const result = targetNumber.every(
       (number, index) => targetNumber.indexOf(number) === index
     );
--- __tests__/ValidateGuessedNumberTest.js
+++ __tests__/ValidateGuessedNumberTest.js
@@ -2,20 +2,23 @@ const App = require('../src/App');
 
 describe('Validate guessed number test', () => {
   test('input string의 길이가 3이 아니면 오류 throw', () => {
+    const app = new App();
     const TEST_STRING = '1234';
 
-    expect(() => App.validateGuessedNumber(TEST_STRING)).toThrow();
+    expect(() => app.validateGuessedNumber(TEST_STRING)).toThrow();
   });
 
   test('input string에 숫자가 아닌 문자가 있으면 오류 throw', () => {
+    const app = new App();
     const TEST_STRING = '1a4';
 
-    expect(() => App.validateGuessedNumber(TEST_STRING)).toThrow();
+    expect(() => app.validateGuessedNumber(TEST_STRING)).toThrow();
   });
 
   test('input string이 정상적이면 진행', () => {
+    const app = new App();
     const TEST_STRING = '123';
 
-    expect(() => App.validateGuessedNumber(TEST_STRING)).not.toThrow();
+    expect(() => app.validateGuessedNumber(TEST_STRING)).not.toThrow();
   });
 });
