d3790d743ed230de3d4242cfcfa59ce302840a8b
Chanwoong1 <jcw7468@gmail.com>
2022-11-06 16:44:08 +0900

    refactor: 시작과 재시작이 동일한 메서드를 사용하도록 변경

--- src/App.js
+++ src/App.js
@@ -18,8 +18,12 @@ class BaseballGame {
     else Console.print(`${ballCount}볼 ${strikeCount}스트라이크`);
   };
 
-  playGame() {
+  play() {
     Console.print('숫자 야구 게임을 시작합니다.');
+    this.playGame();
+  }
+
+  playGame() {
     const computerNumbers = this.randomSelectComputerNumbers();
     this.inputUserNumbers(computerNumbers);
   }
@@ -50,12 +54,10 @@ class BaseballGame {
     Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     Console.readLine("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n", (number) => {
       if (number === '1') {
-        const computerNumbers = this.randomSelectComputerNumbers();
-        this.inputUserNumbers(computerNumbers);
+        this.playGame();
       } else if (number === '2') {
         Console.close();
       } else {
-        Console.close();
         throw ERROR_TEXT;
       }
     });
@@ -90,7 +92,7 @@ class App {
   }
 
   play() {
-    this.baseballGame.playGame();
+    this.baseballGame.play();
   }
 }
 
