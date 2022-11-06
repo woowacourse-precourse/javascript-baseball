6aacb8c12a8c432941d5d0e48d04cdf2fd00961f
unknown <chn0202@gmail.com>
2022-11-07 03:39:07 +0900

    Feat: 야구게임 시작 메서드 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+const MissionUtils = require("@woowacourse/mission-utils");
 class App {
-  play() {}
+  constructor() {
+    this.repit = true;
+    this.game = true;
+  }
+  play() {
+    if (this.repit) console.log("숫자 야구 게임을 시작합니다.");
+    this.repit = false;
+
+    const computer = this.computerNumber();
+
+    while (this.game) {
+      this.compare(computer);
+    }
+  }
 }
 
 module.exports = App;
