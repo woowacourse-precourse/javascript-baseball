d04c2e283b6ce4c355e466942c7b42c9468f5870
ramda <91455804+gnfnfnr@users.noreply.github.com>
2022-11-06 21:04:01 +0900

    style: reflect printWidth 80 to 100

--- src/App.js
+++ src/App.js
@@ -36,26 +36,18 @@ class App {
   }
 
   showResult(userInputNum) {
-    const result = {
-      ballNum: this.countBalls(userInputNum),
-      strikeNum: this.countStrikes(userInputNum),
-    };
-    if (!result.strikeNum && !result.ballNum) Console.print('낫싱');
-    if (!result.strikeNum && result.ballNum)
-      Console.print(`${result.ballNum} 볼`);
-    if (!result.ballNum && result.strikeNum)
-      Console.print(`${result.strikeNum} 스트라이크`);
-    if (result.ballNum && result.strikeNum) {
-      Console.print(`${result.ballNum}볼 ${result.strikeNum}스트라이크`);
-    }
-    return result.strikeNum === 3 ? this.gameEnd() : this.userInputProcess();
+    const ballNum = this.countBalls(userInputNum);
+    const strikeNum = this.countStrikes(userInputNum);
+    if (!strikeNum && !ballNum) Console.print('낫싱');
+    if (!strikeNum && ballNum) Console.print(`${ballNum}볼`);
+    if (!ballNum && strikeNum) Console.print(`${strikeNum}스트라이크`);
+    if (ballNum && strikeNum) Console.print(`${ballNum}볼 ${strikeNum}스트라이크`);
+    return strikeNum === 3 ? this.gameEnd() : this.userInputProcess();
   }
 
   countBalls(userInputNum) {
     return this.randomAnswer.reduce((acc, cur, idx) => {
-      return userInputNum.includes(cur) && userInputNum.indexOf(cur) !== idx
-        ? acc + 1
-        : acc;
+      return userInputNum.includes(cur) && userInputNum.indexOf(cur) !== idx ? acc + 1 : acc;
     }, 0);
   }
 
@@ -67,15 +59,10 @@ class App {
 
   gameEnd() {
     Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
-    Console.readLine(
-      '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.',
-      (endNum) => {
-        console.log(
-          `게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. ${endNum}`
-        );
-        this.gameEndvalidation(endNum);
-      }
-    );
+    Console.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.', (endNum) => {
+      console.log(`게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. ${endNum}`);
+      this.gameEndvalidation(endNum);
+    });
   }
 
   gameEndvalidation(endNumber) {
