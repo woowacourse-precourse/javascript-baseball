6bc5f38c6d5e2164c6c1a5098fd57531c2cb124e
Woong <jiyko7@kyonggi.ac.kr>
2022-11-07 00:30:28 +0900

    5. 사용자 입력값 처리하기
    5. 사용자 입력값 처리하기

--- src/App.js
+++ src/App.js
@@ -13,18 +13,72 @@ class App {
       }
     }
     MissionUtils.Console.print(computer);
-      
-    MissionUtils.Console.readLine("숫자를 입력하세요 : ", function(input) {
-      if (isNaN(input) || input.length != 3){
-        throw new Error("3자리 숫자를 입력하세요");
-      }
-      
-      MissionUtils.Console.print("성공");
-    });
-
+    
+    game(MissionUtils, computer);
   }
 }
 
+function game(MissionUtils, computer) {
+  var ballcount = 0;
+  var strikecount = 0;
+  var check = true;
+  MissionUtils.Console.readLine("숫자를 입력하세요 : ", function(input) { 
+    if (isNaN(input) || input.length != 3){
+      throw new Error("3자리 숫자를 입력하세요");
+    }
+
+    if (input.charAt(0) == input.charAt(1) || input.charAt(0) == input.charAt(2) || input.charAt(1) == input.charAt(2)){
+      throw new Error("3자리 숫자 모두 다르게 입력하세요");
+    }
+    
+    for (var i = 0; i<3; i++){
+      if (computer[0] == input.charAt(i)){
+        if (i==0){
+          strikecount++;
+          break;
+        }
+        ballcount++;
+      }
+    }
+    for (var i = 0; i<3; i++){
+      if (computer[1] == input.charAt(i)){
+        if (i==1){
+          strikecount++;
+          break;
+        }
+        ballcount++;
+      }
+    }
+    for (var i = 0; i<3; i++){
+      if (computer[2] == input.charAt(i)){
+        if (i==2){
+          strikecount++;
+          break;
+        }
+        ballcount++;
+      }
+    }
+
+    if (ballcount == 0 && strikecount == 0){
+      MissionUtils.Console.print("낫싱");
+    }
+    else if (ballcount>0 && strikecount == 0){
+      MissionUtils.Console.print(ballcount + "볼");
+    }
+    else if (strikecount>0 && ballcount == 0){
+      MissionUtils.Console.print(strikecount + "스트라이크");
+      if (strikecount == 3){
+        MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        MissionUtils.Console.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+      }
+    }
+    else{
+      MissionUtils.Console.print(ballcount + "볼 " + strikecount + "스트라이크");
+    }
+    game(MissionUtils, computer);
+  });
+
+}
 
 var a = new App();
 a.play();
