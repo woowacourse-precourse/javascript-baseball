5ff184fd6755e8de9b5ad82344c31bc89cf2b25d
dladncks1217 <dlaxodud1217@gmail.com>
2022-11-06 20:48:56 +0900

    refactor: MissionUtils.Random import 코드 수정

--- src/utils/makeRandomNumber.js
+++ src/utils/makeRandomNumber.js
@@ -1,11 +1,11 @@
-const MissionUtils = require("@woowacourse/mission-utils");
+const { Random } = require("@woowacourse/mission-utils");
 
 const makeRandomNumber = () => {
   const answer = [];
 
-  answer.push(MissionUtils.Random.pickNumberInRange(1, 9)); // 첫 자릿수에 0이 나오지 않도록
+  answer.push(Random.pickNumberInRange(1, 9)); // 첫 자릿수에 0이 나오지 않도록
   while (answer.length < 3) {
-    const tempNumber = MissionUtils.Random.pickNumberInRange(0, 9);
+    const tempNumber = Random.pickNumberInRange(0, 9);
     if (answer.indexOf(tempNumber) === -1) {
       answer.push(tempNumber);
     }
