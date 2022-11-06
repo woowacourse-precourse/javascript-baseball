5d0accdd4fd8ee6ae1f4c5fcbb979575eac5567e
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2022-11-06 17:20:18 +0900

    FEAT: input 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  play() {
+    this.userInput();
+  }
+
+  userInput() {
+    MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (input) => {
+      MissionUtils.Console.print(input);
+      MissionUtils.Console.close();
+    });
+  }
+
+  
 }
+const test = new App();
+test.play();
 
 module.exports = App;
