35abaf7733d82debfa48fad2afd7bb16f7d048df
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-11-06 12:46:02 +0900

    feat($validate): 사용자가 모두 다른 숫자를 입력했는지 판별

--- src/validateInput.js
+++ src/validateInput.js
@@ -1,7 +1,14 @@
 const onlyNumRegex = /^[1-9]+$/;
 
+const isAllDifferent = (userInput) => {
+  const [first, second, third] = userInput.split('');
+  return first !== second && second !== third && third !== first;
+};
+
 const validateInput = (userInput) => {
   if (userInput.length !== 3 || !onlyNumRegex.test(userInput)) {
+    throw Error('잘못된 입력입니다. 프로그램을 종료합니다.');
+  } else if (!isAllDifferent(userInput)) {
     throw Error('잘못된 입력입니다. 프로그램을 종료합니다.');
   }
 };
