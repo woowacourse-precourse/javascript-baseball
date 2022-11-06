24195e1777bf8edf69504964be8f460e8857840e
dragonfruitlemonade <tpghks9245@gmail.com>
2022-11-06 17:20:59 +0900

    feat(Game): getStrikeAndBall 기능 구현, 스트라이크 볼 한번에 구하기

--- src/Game.js
+++ src/Game.js
@@ -10,42 +10,43 @@ class Game {
   
 
     playGame() {    
-        MissionUtils.Console.readLine("숫자를 입력해주세요 : ", userNumber => {
-            const howManyStrike = this.getStrike(this.computersNumber, userNumber);
-            const howManyBall = this.getBall(this.computersNumber, userNumber);
-            const compareResult = this.compareBallStrike(howManyStrike, howManyBall);
-
-            console.log(compareResult);
-            return compareResult;
+        MissionUtils.Console.readLine("숫자를 입력해주세요 : ", usersNumber => {
+            console.log(this.computersNumber);
+            
+            usersNumber = [...usersNumber].map(idx => parseInt(idx));
+            console.log(usersNumber);
+            const [ howManyStrike, howManyBall ] = this.getStrikeAndBall(this.computersNumber, usersNumber);
+            
+            // const compareResult = this.compareBallStrike(howManyStrike, howManyBall);    
+            // console.log(compareResult);
+            // return compareResult;
+            // const [howManyStrike, howManyBall] = this.getStrikeBallCount(this.computersNumber, userNumber);
+            console.log(howManyStrike);
+            console.log(howManyBall);
+            
+            return howManyStrike;
         });
+
+        return;
     }
 
-    // 유저와 컴퓨터 대조해서 스트라이크 갯수 리턴
-    getStrike(computersNumber, userNumber) {
+    // 유저와 컴퓨터 대조해서 스트라이크 볼 갯수 리턴
+    getStrikeAndBall(computersNumber, usersNumber) {
         let howManyStrike = 0;
-
-        for (let i = 0; i < 3; i++) {
-            if (computersNumber[i] === userNumber[i]) howManyStrike = howManyStrike + 1;
-        }
-
-        return howManyStrike;
-    }
-    
-    // 유저와 컴퓨터 대조해서 볼 갯수 리턴
-    getBall(computersNumber, userNumber) {
         let howManyBall = 0;
 
-        for (let i = 0; i < 3; i++) {
-            if (computersNumber[i] != userNumber[i] && computersNumber.include(userNumber[i])) howManyBall = howManyBall + 1;
-        }
-        
-        return howManyBall;
+        usersNumber.forEach((number, idx) => {
+            if (number === computersNumber[idx]) howManyStrike++;
+            if (number !== computersNumber[idx] && computersNumber.includes(number)) howManyBall++;
+        });
+        return [ howManyStrike, howManyBall ];
     }
+    
 
     // 입력된 숫자 대조 결과 리턴
-    compareBallStrike() {
-
-    }
+    //compareBallStrike() {
+//
+    //}
 }
 
 const game = new Game;
