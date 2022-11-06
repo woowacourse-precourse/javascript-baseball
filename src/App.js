47a8971eb54c3a8aa0d2b77f0619351d7aaad98c
DESKTOP-PDMTIQO\hansu <fathlic5@gmail.com>
2022-11-06 22:39:57 +0900

    :recycle: refactor: 반복 사용되는 API 최소화 리팩토링 반복적으로 사용되었던 우아한테크코스 라이브러리 API를 최소화, 간략화 시킴

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
+const { Random, Console } = require("@woowacourse/mission-utils");
+
 class App {
     play() {
-        const MISSION_UTILS = require("@woowacourse/mission-utils");
-        MISSION_UTILS.Console.print('숫자 야구 게임을 시작합니다.');
+        Console.print('숫자 야구 게임을 시작합니다.');
         this.isUserNumbers(this.isComputerNumbers());
     }
 
     isComputerNumbers() {
-        const MISSION_UTILS = require("@woowacourse/mission-utils");
         const COMPUTER_NUMBERS = [];
         while (COMPUTER_NUMBERS.length < 3) {
-            const RANDOM_NUMBER = MISSION_UTILS.Random.pickNumberInRange(1, 9);
+            const RANDOM_NUMBER = Random.pickNumberInRange(1, 9);
             if (!COMPUTER_NUMBERS.includes(RANDOM_NUMBER)) COMPUTER_NUMBERS.push(RANDOM_NUMBER);
         }
         return parseInt(COMPUTER_NUMBERS.join(''));
     }
 
     isUserNumbers(computerNumbers) {
-        const MISSION_UTILS = require("@woowacourse/mission-utils");
-        return MISSION_UTILS.Console.readLine('숫자를 입력해주세요 : ',(userNumbers) => {
+        return Console.readLine('숫자를 입력해주세요 : ',(userNumbers) => {
             if(isNaN(userNumbers)) throw new Error('숫자를 입력해주세요.');
             if(userNumbers.toString().length !== 3) throw new Error('3자리 숫자를 입력해주세요.');
             if(new Set(userNumbers).size !== 3) throw new Error('중복되지 않는 숫자를 입력해주세요.');
@@ -29,9 +28,8 @@ class App {
 
     isMatchNumbers(computerNumbers,userNumbers) {
         if(computerNumbers == userNumbers) {
-            const MISSION_UTILS = require("@woowacourse/mission-utils");
-            MISSION_UTILS.Console.print('3스트라이크');
-            MISSION_UTILS.Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+            Console.print('3스트라이크');
+            Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
             return this.replayGame();
         }
         if(computerNumbers !== userNumbers) {
@@ -40,12 +38,11 @@ class App {
         }
     }
 
-    replayGame(){
-        const MISSION_UTILS = require("@woowacourse/mission-utils");
-        MISSION_UTILS.Console.print('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.');
-        MISSION_UTILS.Console.readLine('', (answer) => {
+    replayGame() {
+        Console.print('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.');
+        Console.readLine('', (answer) => {
             if(answer == 1) return this.isUserNumbers(this.isComputerNumbers());
-            if(answer == 2) return MISSION_UTILS.Console.print('게임 종료');
+            if(answer == 2) return Console.print('게임 종료');
             if(answer !== 1 && answer !== 2) throw new Error('1 또는 2를 입력하세요.');
         })
     }
@@ -62,11 +59,10 @@ class App {
     }
 
     isProvideHints(ball,strike) {
-        const MISSION_UTILS = require("@woowacourse/mission-utils");
-        if(ball == 0 && strike == 0) return MISSION_UTILS.Console.print(`낫싱`);
-        if(ball == 0 && strike > 0)  return MISSION_UTILS.Console.print(`${strike}스트라이크`);
-        if(ball > 0 && strike == 0)  return MISSION_UTILS.Console.print(`${ball}볼`);
-        if(ball > 0 && strike > 0)  return MISSION_UTILS.Console.print(`${ball}볼 ${strike}스트라이크`);
+        if(ball == 0 && strike == 0) return Console.print(`낫싱`);
+        if(ball == 0 && strike > 0)  return Console.print(`${strike}스트라이크`);
+        if(ball > 0 && strike == 0)  return Console.print(`${ball}볼`);
+        if(ball > 0 && strike > 0)  return Console.print(`${ball}볼 ${strike}스트라이크`);
     }
 }
 module.exports = App;
