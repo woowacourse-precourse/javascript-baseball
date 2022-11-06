e65193147abc9c9dd77e8dbb79de8f36bbf8e689
semnil5202 <semnil5202@gmail.com>
2022-11-07 05:20:43 +0900

    fix(handleException): 예외 처리 버그 수정
    이 함수는 사용자가 입력한 값을 숫자형 배열로 전달받는다.
    사용자가 문자를 입력할 경우 배열의 요소들은 NaN이 되며 함수의 로직상 길이가 1인 배열로 인식된다.
    따라서 중복 문자가 입력된 예외로 인식되는 버그가 발생하였고 이를 수정한다.

--- src/handleException.js
+++ src/handleException.js
@@ -1,9 +1,8 @@
 function handleException(inputs) {
   const uniqueInputs = Array.from(new Set(inputs));
-
+  if (isNaN(inputs.join(""))) return false;
   if (inputs.length !== uniqueInputs.length) return false;
   if (inputs.length !== 3) return false;
-  if (isNaN(inputs.join(""))) return false;
   return true;
 }
 
