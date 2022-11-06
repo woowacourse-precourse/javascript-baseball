1aac91c19356841d5879ad2670916155dd8c493b
feb-dain <feb.dain@gmail.com>
2022-11-07 02:12:43 +0900

    feat: make computer's random number

--- src/Play.js
+++ src/Play.js
@@ -1,1 +1,17 @@
 const { consoleUtil, randomUtil } = require("./Utils");
+
+const makeRandomValue = () => {
+  const computerValue = [];
+
+  while (computerValue.length < 3) {
+    const randomNumber = randomUtil.pickNumberInRange(1, 9);
+    if (!computerValue.includes(randomNumber)) {
+      computerValue.push(randomNumber);
+    }
+  }
+  return computerValue;
+};
+
+const playing = () => {};
+
+module.exports = playing;
