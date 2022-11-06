159147436cc6b31c71296ed0642f8453c58410c5
cruelladevil <dev.timetravel@gmail.com>
2022-11-07 01:05:20 +0900

    feat: ask play game again

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ class App {
         this.guess(computer);
       } else {
         Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
-        Console.close();
+        this.askPlayAgain();
       }
     });
   }
@@ -75,6 +75,20 @@ class App {
     }
 
     return `${ball}볼 ${strike}스트라이크`;
+  }
+
+  askPlayAgain() {
+    Console.print('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.');
+    Console.readLine('', (input) => {
+      switch (input) {
+        case '1':
+          return this.play();
+        case '2':
+          return Console.close();
+        default:
+          throw new Error('게임을 종료합니다.');
+      }
+    });
   }
 }
 
