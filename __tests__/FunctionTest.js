2a4f9914b3b2a175b1b55d33ef8b9557ad168e00
cloudedpanther <rickeykoh@ajou.ac.kr>
2022-11-07 01:45:45 +0900

    test(input): getReplayRequest method
    Add tests of getReplayRequest method of Input class
    - test when user wants to replay
    - test when user wants to terminate

--- __tests__/FunctionTest.js
+++ __tests__/FunctionTest.js
@@ -36,4 +36,32 @@ describe("기능 테스트", () => {
 
     expect(userAnswer).toEqual([3, 5, 1]);
   });
+
+  test("재시작 여부 받아오기: 재시작", () => {
+    const request = 1;
+    const logSpy = getPrintLogSpy();
+
+    MissionUtils.Console.readLine = jest.fn((ask, callback) => {
+      callback(request);
+    });
+
+    const userWantsReplay = Input.getReplayRequest();
+
+    expect(logSpy).toBeCalledWith("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    expect(userWantsReplay).toBe(true);
+  });
+
+  test("재시작 여부 받아오기: 종료", () => {
+    const request = 2;
+    const logSpy = getPrintLogSpy();
+
+    MissionUtils.Console.readLine = jest.fn((ask, callback) => {
+      callback(request);
+    });
+
+    const userWantsReplay = Input.getReplayRequest();
+
+    expect(logSpy).toBeCalledWith("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    expect(userWantsReplay).toBe(false);
+  });
 });
