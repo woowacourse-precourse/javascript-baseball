10d8a041b868cc62b0a74eeabe15b5d61869e8ff
sojinjang <jsojin6119@gmail.com>
2022-11-07 00:52:41 +0900

    :sparkles: feat(input): 입력값 유효성 검사 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+const Validator = require("./validator.js");
 const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
@@ -5,6 +6,8 @@ class App {
     const computerNum = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
     MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (userInput) => {
+      const validator = new Validator(userInput.split(""));
+      console.log(validator.isValidInput());
       MissionUtils.Console.close();
     });
   }
