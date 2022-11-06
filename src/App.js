713ded387b4310be96ce77d3e1a6bc992042d9ed
jiheon788 <jiheon788@ajou.ac.kr>
2022-11-06 20:56:34 +0900

    feat(Exceptions): implement each digit of input range exception

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ const { Referee } = require("./Referee");
 
 const isRangeError = (inputs) => inputs.length !== 3
 const isDuplicate = (inputs) => new Set(inputs).size !== inputs.length
+const isDigit = (inputs) => inputs.every(input => input >= 1 && input <= 9)
 
 class App {
   user = new User();
@@ -35,11 +36,14 @@ class App {
     Console.readLine("숫자를 입력해주세요 : ", (input) => {
       this.user.numbers = input.split("")
 
-      if (isRangeError(input)) {
+      if (isRangeError(this.user.numbers)) {
         throw new Error("Input must be length of 3")
       }
-      if (isDuplicate(input)) {
+      if (isDuplicate(this.user.numbers)) {
         throw new Error("Each digit of input must be different")
+      }
+      if (!isDigit(this.user.numbers)) {
+        throw new Error("Each digit of input should be between 1 and 9")
       }
 
       console.log("정답: ", this.computer.numbers);
@@ -61,13 +65,15 @@ class App {
 
   isRegame () {
     Console.readLine("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n", (input) => {
+      
       if (input === "1") {
         this.play();
       } else if (input === "2") {
         Console.close();
         return;
-      } 
-      throw new Error("input error");
+      } else {
+        throw new Error("input should be 1 or 2");
+      }
     })
   }
 }
