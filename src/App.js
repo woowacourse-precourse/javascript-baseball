9873e9efabcc92dc7461e8b98d6130ad4d2b1853
KimJerry <k90966277@gmail.com>
2022-11-07 00:08:31 +0900

    feat(random): add function to generate three-digit random number
    feat(random): add function to generate three-digit random number
    
    Add new method to App class:
    - create an array contains three random number.
    - generate a string by joining three numbers in array.
    - return generated string.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+const MissionUtils = require('@woowacourse/mission-utils'); // module import.
+
 class App {
-  play() {}
+    // 랜덤한 3자리 숫자를 생성하고 리턴.
+    generateRandomNumber() {
+        const randomNumberArray = MissionUtils.Random.pickUniqueNumbersInRange(
+            1,
+            9,
+            3
+        );
+        return randomNumberArray.join('');
+    }
+
+    play() {
+        const RANDOM_NUMBER = this.generateRandomNumber();
+    }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
