2b0a79a4ea1830544f6a38f4d7652b500f90e24d
woo-jk <201702042@o.cnu.ac.kr>
2022-11-06 10:57:28 +0900

    test: 플레이 과정 테스트 코드 작성

--- __tests__/StrikeAndBallCountTest.js
+++ __tests__/StrikeAndBallCountTest.js
@@ -1,6 +1,28 @@
 const App = require("../src/App");
 const StrikeAndBall = require("../src/StrikeAndBall");
 
+const mockQuestions = (answers) => {
+  MissionUtils.Console.readLine = jest.fn();
+  answers.reduce((acc, input) => {
+    return acc.mockImplementationOnce((question, callback) => {
+      callback(input);
+    });
+  }, MissionUtils.Console.readLine);
+};
+
+const mockRandoms = (numbers) => {
+  MissionUtils.Random.pickNumberInRange = jest.fn();
+  numbers.reduce((acc, number) => {
+    return acc.mockReturnValueOnce(number);
+  }, MissionUtils.Random.pickNumberInRange);
+};
+
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(MissionUtils.Console, "print");
+  logSpy.mockClear();
+  return logSpy;
+};
+
 describe.only("스트라이크 볼 카운트 테스트", () => {
   test("스트라이크 볼 카운트 결과 가져오는 함수", () => {
     const answerNumber = "123";
@@ -18,7 +40,7 @@ describe.only("스트라이크 볼 카운트 테스트", () => {
     });
   });
 
-  test("스트라이크 볼 카운트 결과를 문자열로 바꿔주는 함수", () => {
+  test("스트라이크 볼 카운트 결과를 문자열로 바꿔주는 함수 테스트", () => {
     const inputs = [
       [3, 0, "3스트라이크"],
       [1, 1, "1볼 1스트라이크"],
@@ -30,6 +52,23 @@ describe.only("스트라이크 볼 카운트 테스트", () => {
     inputs.forEach(([strike, ball, resultString]) => {
       const result = StrikeAndBall.getString(strike, ball);
       expect(result).toEqual(resultString);
+    });
+  });
+
+  test("플레이 과정 테스트", () => {
+    const randoms = [1, 2, 3];
+    const answers = ["231", "135", "789", "123"];
+    const logSpy = getLogSpy();
+    const messages = ["3스트라이크", "1볼 1스트라이크", "낫싱", "3스트라이크"];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    const app = new App();
+    app.play();
+
+    messages.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
     });
   });
 });
