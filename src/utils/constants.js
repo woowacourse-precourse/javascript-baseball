df330db2688a0298c8095fbe1480aced6598a28c
turtle601 <turtle601@naver.com>
2022-11-06 09:59:32 +0900

    refactor(constant.js) COMMAND(app입출력) 관련 상수 추가

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -9,7 +9,18 @@ const BASEBALL = Object.freeze({
   NOTHING: '낫싱',
 });
 
+const COMMAND = Object.freeze({
+  START_MESSAGE: '숫자 야구 게임을 시작합니다.',
+  QUESTION: '숫자를 입력해주세요 : ',
+  NEXT_QUESTION: '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요',
+  STRIKEOUT: '3개의 숫자를 모두 맞히셨습니다! 게임 종료',
+  CLOSE: '게임 종료',
+  RESTART: '1',
+  EXIT: '2',
+});
+
 module.exports = {
   RANDOMLIST,
   BASEBALL,
+  COMMAND,
 };
