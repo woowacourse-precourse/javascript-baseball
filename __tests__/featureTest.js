aa35a0411eebe69ec6895a2d479d7fec3418b0e7
ilgon0110 <moses011010@gmail.com>
2022-11-06 16:16:07 +0900

    test: User의 입력 값 받기 테스트 완료 (#2)
    사용자의 입력 값 확인하는 테스트 진행

--- __tests__/featureTest.js
+++ __tests__/featureTest.js
@@ -1,5 +1,23 @@
+const MissionUtils = require('@woowacourse/mission-utils');
 const App = require('../src/App');
 const app = new App();
+
+const mockQuestions = (answers) => {
+  MissionUtils.Console.readLine = jest.fn();
+  answers.reduce(
+    (acc, input) =>
+      acc.mockImplementationOnce((question, callback) => {
+        callback(input);
+      }),
+    MissionUtils.Console.readLine,
+  );
+};
+
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(MissionUtils.Console, 'print');
+  logSpy.mockClear();
+  return logSpy;
+};
 
 describe('상대방(Computer)의 숫자 테스트', () => {
   test('1-1. 숫자가 아닌 경우', () => {
@@ -18,5 +36,19 @@ describe('상대방(Computer)의 숫자 테스트', () => {
   });
   test('1-4. 길이가 3이 아닌 경우', () => {
     expect(app.createComputerNum().length).toEqual(3);
+  });
+});
+
+describe('User의 입력값 받기', () => {
+  test('사용자의 값 입력 확인', () => {
+    const answers = ['345'];
+    const logSpy = getLogSpy();
+
+    mockQuestions(answers);
+
+    app.getUserInput();
+    answers.forEach((input) =>
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(input)),
+    );
   });
 });
