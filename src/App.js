909b85d43db553be8212dcccfa1bd0fbc74442c5
khkh0109 <ist200120@gmail.com>
2022-11-06 16:10:25 +0900

    feat(play): 랜덤 숫자 만들기

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 class App {
   play() {
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+    const computer = pickRandomNumbers();
+    
+    
   }
 }
 
@@ -14,7 +18,6 @@ function pickRandomNumbers() {
     }
   return computer;
 }
-
 
 function comparingNumbers(inputNum, randomNum) {
   const result = [0, 0];
