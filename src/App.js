5670048457d3353c826d3796995825df1d00d3dc
daal2quf <daal2quf@gmail.com>
2022-11-07 06:34:19 +0900

    feat: 스트라이크 볼 개수 알려주는 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,64 +1,76 @@
 const MissionUtils = require("@woowacourse/mission-utils");
-const ExceptionCheck = require("./ExceptionCheck")
+const ExceptionCheck = require("./ExceptionCheck");
 
 class App {
   constructor() {
+    //게임 정보
     this.correctAnswer;
     this.userInput;
-    this.ball = 0
-    this.strike = 0
+    this.ball = 0;
+    this.strike = 0;
   }
+
   play() {
-    this.gamaStartAlram()
-    this.createRandomNum()
-    this.getAnswer()
+    this.gamaStartAlram();
+    this.createRandomNum();
+    this.getAnswer();
   }
 
   gamaStartAlram() {
-    MissionUtils.Console.print("게임을 시작합니다")
+    MissionUtils.Console.print("게임을 시작합니다");
   }
 
   createRandomNum() {
-    let computerRandomNums = new Set()
+    let computerRandomNums = new Set();
     while (computerRandomNums.size < 3) {
-      computerRandomNums.add(MissionUtils.Random.pickNumberInRange(1, 9))
+      computerRandomNums.add(MissionUtils.Random.pickNumberInRange(1, 9));
     }
-    MissionUtils.Console.print(computerRandomNums)
-    return this.correctAnswer = [...computerRandomNums].join('')
+    MissionUtils.Console.print(computerRandomNums);
+    return this.correctAnswer = [...computerRandomNums].join('');
   }
 
   getAnswer() {
     MissionUtils.Console.readLine('숫자를 입력해주세요 :', (input) => {
-      const exceptionCheck = new ExceptionCheck()
-      exceptionCheck.UserInputCheck(input)
-      MissionUtils.Console.print(`입력한 숫자는 ${input} 입니다`)
-      this.userInput = input
+      const exceptionCheck = new ExceptionCheck();
+      exceptionCheck.UserInputCheck(input);
+      MissionUtils.Console.print(`입력한 숫자는 ${input} 입니다`);
+      this.userInput = input;
       this.strikeCount();
       this.ballCount();
-      return this.userInput
-    })
+      this.strikeBallCountAlram();
+      return this.userInput;
+    });
   }
 
   strikeCount() {
-    const correctAnswerArr = this.correctAnswer.split('')
+    const correctAnswerArr = this.correctAnswer.split('');
     correctAnswerArr.map((number, index) => {
       if (number === this.userInput[index]) {
         this.strike += 1;
       }
-    })
-    return this.strike
+    });
+    return this.strike;
   }
 
   ballCount() {
-    const correctAnswerArr = this.correctAnswer.split('')
+    const correctAnswerArr = this.correctAnswer.split('');
     correctAnswerArr.map((number, index) => {
       if (number !== this.userInput[index] && this.userInput.includes(number)) {
         this.ball += 1;
       }
-    })
-    return this.ball
+    });
+    return this.ball;
   }
 
+  strikeBallCountAlram() {
+    if (this.ball === 0 && this.strike === 0) {
+      MissionUtils.Console.print('낫싱');
+    } else {
+      MissionUtils.Console.print(`입력한 수: ${this.userInput}, ${this.ball}볼 ${this.strike}스트라이크`);
+    }
+    this.strikeBallChecking();
+
+  }
 }
 
 const app = new App();
