9c9d064fe5a68f59315f0f41fe05057305293239
Hungeun2 <bepoohun@gmail.com>
2022-11-06 23:48:07 +0900

    Feat: 1. 게임 시작 구현 게임을 시작합니다 메시지  출력 (mission-utils) util/Constant File 생성 ( 게임 메시지  )

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
-class App {
-  play() {}
-}
+const { Console } = require("@woowacourse/mission-utils");
+const GAME_MESSAGE = require("./util/Constant");
 
+class App {
+  play() {
+    Console.print(GAME_MESSAGE.START_MESSAGE);
+  }
+}
+const baseball = new App();
+baseball.play();
 module.exports = App;
