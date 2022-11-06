86602cf6c58828d94643e7f0185d1a44f2406b6c
wonkyung <mwk0725@gmail.com>
2022-11-06 21:09:35 +0900

    refactor: userinput -> userInput 이름 수정

--- src/App.js
+++ src/App.js
@@ -116,12 +116,12 @@ class App {
     MissionUtils.Console.print(
       '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.',
     );
-    MissionUtils.Console.readLine('', userinput => {
-      if (userinput === '1') {
+    MissionUtils.Console.readLine('', userInput => {
+      if (userInput === '1') {
         this.playFirstTime = false;
         return this.play();
       }
-      if (userinput === '2') {
+      if (userInput === '2') {
         return MissionUtils.Console.close();
       }
       return MissionUtils.Console.close();
