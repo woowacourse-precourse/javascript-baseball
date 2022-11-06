6c4cdfab5d6b90440378ba682ba34ab308d3c0cc
jihong <rlawyd9@kookmin.ac.kr>
2022-11-07 01:33:38 +0900

    feat: select computer number

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
+
 class App {
-  play() {}
+  selectComputerNum(){
+    const computer = [];
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+    return computer;
+  }
+
+  play() {
+
+    const computer_num = this.selectComputerNum();
+
+  }
 }
 
 module.exports = App;
