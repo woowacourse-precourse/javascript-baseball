8dcd8fc10e35ebdfd52a667f4e03f7f6cd4144ed
minsoo <als494@naver.com>
2022-11-06 22:41:51 +0900

    feat: 게임 시작 부분 fix: 결과 출력 부분

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ const MissionUtils = require("@woowacourse/mission-utils");
 
 // 1. 컴퓨터가 뽑은 랜덤 숫자 3개 배열로 만드는 함수
 function randomNums() {
-  const computer = [];
-  while (computer.length < 3) {
-    const number = MissionUtils.Random.pickNumberInRange(1, 9);
-    if (!computer.includes(number)) {
-      computer.push(number);
+  let nums = [];
+  while (nums.length < 3) {
+    let number = MissionUtils.Random.pickNumberInRange(1, 9);
+    if (!nums.includes(number)) {
+      nums.push(number);
     }
   }
-  return computer;
+  return nums;
 }
 
 // 2. 사용자에게 숫자 3개 받는 함수
@@ -17,11 +17,11 @@ function readNums() {
   let user;
   MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (answer) => {
     user = answer.split("").map((string) => Number(string));
-    user.map(num => {
+    user.map((num) => {
       if (isNaN(num)) {
         throw "Not a number";
       }
-    })
+    });
     if (user.includes(0)) {
       throw "0 exists";
     } else if (user.length !== 3) {
@@ -50,11 +50,12 @@ function compareNums(computer, user) {
 function printResult(score) {
   let ball = score[0];
   let strike = score[1];
-  if (ball === 0 && strike === 0) {
-    MissionUtils.Console.print("낫싱");
-  } else if (strike === 3) {
+  if (strike === 3) {
     MissionUtils.Console.print("3스트라이크");
     MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    return true;
+  } else if (ball === 0 && strike === 0) {
+    MissionUtils.Console.print("낫싱");
   } else if (strike === 0) {
     MissionUtils.Console.print(`${ball}볼`);
   } else if (ball === 0) {
@@ -62,10 +63,36 @@ function printResult(score) {
   } else {
     MissionUtils.Console.print(`${ball}볼 ${strike}스트라이크`);
   }
+  return false;
 }
 
 class App {
-  play() {}
+  play() {
+    let check = 1;
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    // let computer = [];
+    while (check === 1) {
+      let computer = randomNums();
+      let user;
+      let score;
+      let result;
+
+      while (1) {
+        user = readNums();
+        score = compareNums(computer, user);
+        result = printResult(score);
+        if (result) break;
+      }
+
+      MissionUtils.Console.readLine(
+        "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. ",
+        (answer) => {
+          if (answer === 2) check = 2;
+        }
+      );
+      if (check === 2) break;
+    }
+  }
 }
 
 module.exports = App;
