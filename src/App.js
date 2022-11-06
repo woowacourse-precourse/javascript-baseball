8ba84b943a90c36a67047e38b78e891d0d50b087
dongyeopca <ckehdduq95@gmail.com>
2022-11-06 16:40:44 +0900

    "feat: 숫자 생성 함수 구현"  MissionUtils 라이브러리의 Random.pickNumberInRange()를 활용하여 서로 다른 세자리 수를 리턴

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+const { Random, Console } = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  play() {
+    const computer = this.createNumber().join("");
+    this.userInput(computer);
+  }
+
+  createNumber() {
+    const computer = [];
+    while (computer.length < 3) {
+      const number = Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+    return computer;
+  }
 }
+
+const app = new App();
+Console.print("숫자 야구 게임을 시작합니다.");
+app.play();
 
 module.exports = App;
