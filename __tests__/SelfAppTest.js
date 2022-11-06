79a65dd2822f3ae38dfbb1a3bdf3fe9a0076fd5e
DESKTOP-PDMTIQO\hansu <fathlic5@gmail.com>
2022-11-07 05:08:26 +0900

    :white_check_mark: test: 기능4 힌트 출력 테스트 추가 기능4 힌트가 정상 출력되는지 테스트 추가 기능3 재시작과 종료가 정상 작동되는지 테스트 추가

--- __tests__/SelfAppTest.js
+++ __tests__/SelfAppTest.js
@@ -17,6 +17,12 @@ const mockRandoms = (numbers) => {
   }, MissionUtils.Random.pickNumberInRange);
 }
 
+const getLogSpy = () => {
+    const logSpy = jest.spyOn(MissionUtils.Console, "print");
+    logSpy.mockClear();
+    return logSpy;
+}
+
 describe("기능3과 기능4 테스트", () => {
   test("예외 테스트", () => {
     const randoms = [1, 2, 3, 4, 5, 6];
@@ -29,5 +35,27 @@ describe("기능3과 기능4 테스트", () => {
       const app = new App();
       app.play();
     }).toThrow(RangeError);
+  })
+
+  test("3스트라이크 일때 검사/재시작/종료 출력 테스트", () => {
+    const randoms = [1, 2, 3, 4, 5, 6];
+    const answers = ["123", "1", "546", "456", "2"];
+    const logSpy = getLogSpy();
+    const messages = [
+      "3스트라이크",
+      "2볼 1스트라이크",
+      "3스트라이크",
+      "게임 종료",
+    ];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    const app = new App();
+    app.play();
+
+    messages.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    })
   })
 })
