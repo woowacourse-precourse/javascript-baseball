5903cde8e8eeb12bba70d2a38ba356418cb1fbcf
Circlewee <pnj1118@naver.com>
2022-11-06 15:41:08 +0900

    feat(computer): 유저 입력에 따라 힌트 제공 (#5)
    1. 스트라이크 여부를 판단하는 함수 구현
    2. 볼 여부를 판단하는 함수 구현
    3. 점수에 따라 힌트를 제공하는 함수 구현

--- src/utils/computerUtils.js
+++ src/utils/computerUtils.js
@@ -1,6 +1,24 @@
 const { Random } = require('@woowacourse/mission-utils');
 const COMPUTER = require('../constants/COMPUTER');
 
+const countStrike = (inputNumber, answer) => {
+  if (inputNumber === answer) {
+    return 1;
+  }
+
+  return 0;
+};
+
+const countBall = (inputNumber, answers, index) => {
+  const findBallCallback = (number, answerIndex) => number === inputNumber && answerIndex !== index;
+
+  if (answers.find(findBallCallback)) {
+    return 1;
+  }
+
+  return 0;
+};
+
 const getRandomNumber = () => {
   return Random.pickUniqueNumbersInRange(
     COMPUTER.START_NUMBER,
@@ -9,6 +27,35 @@ const getRandomNumber = () => {
   );
 };
 
+const generateHint = (inputNumbers, answers) => {
+  const score = {
+    strike: 0,
+    ball: 0,
+  };
+
+  for (let index = 0; index < COMPUTER.COUNT; index++) {
+    const inputNumber = inputNumbers[index];
+
+    score.strike += countStrike(inputNumber, answers[index]);
+    score.ball += countBall(inputNumber, answers, index);
+  }
+
+  if (score.strike === 0 && score.ball === 0) {
+    return '낫싱';
+  }
+
+  if (score.strike === 0) {
+    return `${score.ball}볼`;
+  }
+
+  if (score.ball === 0) {
+    return `${score.strike}스트라이크`;
+  }
+
+  return `${score.ball}볼 ${score.strike}스트라이크`;
+};
+
 module.exports = {
   getRandomNumber,
+  generateHint,
 };
