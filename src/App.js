b915f6d2e38d9d02265c2737b17d3e8f4a996ce4
seunghoria <102285042+seunghoria@users.noreply.github.com>
2022-11-06 17:59:08 +0900

    feat: 사용자 숫자 입력 기능 구현

--- src/App.js
+++ src/App.js
@@ -6,10 +6,17 @@ const mRandom = MissionUtils.Random;
 class App {
   play() {
     this.startGame();
+    this.getInput();
   }
 
   startGame() {
     mConsole.print("숫자 야구 게임을 시작합니다.");
+  }
+
+  getInput() {
+    mConsole.readLine("숫자를 입력해주세요 : ", (input) => {
+      input;
+    });
   }
 }
 
