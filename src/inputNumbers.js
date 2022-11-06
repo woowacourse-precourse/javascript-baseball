c38b7b576f3e2880f932b36300cfa4de3018fc7d
JSH99 <tjgus9966@naver.com>
2022-11-06 18:46:14 +0900

    feat(inputNumbers): 숫자 입력 기능 구현

---
+++ src/inputNumbers.js
@@ -0,0 +1,14 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const isVaildNumber = require("./isVaildNumber");
+
+const inputNumbers = () => {
+    MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (inputNum) => {
+        if (isVaildNumber(inputNum)) { 
+            throw new Error('잘못된 값입니다. 1부터 9까지 서로 다른 3자리 수를 입력하세요.');
+        } else { 
+            return String(inputNum).split('');
+        }
+    });
+}
+
+module.exports = inputNumbers;
