f0aa7ed346891ce73ed5423b2f291d889db462b3
Gabriel Ju Hyun, Yoon <69189073+gabrielyoon7@users.noreply.github.com>
2022-11-06 19:41:45 +0900

    feat: 자릿수 검사 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+
+
+  play() {
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (answer) => {
+      if(!this.isValidBallNumber(answer)){
+        throw new Error();
+      }
+    });
+  }
+
+  isValidBallNumber(answer) {
+    if (answer.length !== 3) {
+      return false
+    }
+    if (answer[0] === answer[1] || answer[1] === answer[2] || answer[2] === answer[0]) {
+      return false
+    }
+    if (isNaN(Number(answer))) {
+      return false
+    }
+    return true
+  }
 }
+
+const init = new App();
+init.play();
 
 module.exports = App;
