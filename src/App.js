ca22e84fedd28f793152f86584cf68ee62e9dc28
pp449 <mma7710@naver.com>
2022-11-07 00:12:37 +0900

    refactor: 보기좋게 메소드 순서 변경

--- src/App.js
+++ src/App.js
@@ -6,23 +6,41 @@ class App {
     this.userNumbers = [];
   }
 
+  play() {
+    this.gameStart();
+  }
+
+  gameStart() {
+    Console.print('숫자 야구 게임을 시작합니다.');
+    return this.getRandomNumbers();
+  }
+
   getRandomNumbers() {
     this.computerNumbers = Random.pickUniqueNumbersInRange(1, 9, 3);
+    return this.userInputNumber();
+  }
+
+  userInputNumber() {
+    Console.readLine('숫자를 입력해주세요 : ', (numbers) => {
+      return this.checkInputError(numbers);
+    });
   }
 
   checkInputError(numbers) {
     const newNumbers = new Set(numbers); // 중복된 숫자를 찾기위해 사용
     if (isNaN(numbers)) {
-      throw '숫자 이외의 입력';
+      throw new Error('숫자 이외의 입력');
     }
 
     if (numbers.length !== 3) {
-      throw '3자리 숫자 이외의 입력';
+      throw new Error();
     }
 
     if (newNumbers.size !== 3) {
-      throw '중복된 숫자 입력';
+      throw new Error();
     }
+
+    return this.setUserNumber(numbers);
   }
 
   setUserNumber(numbers) {
@@ -33,29 +51,23 @@ class App {
     }
 
     this.userNumbers.reverse();
+    return this.discriminationStrikeOrBall();
   }
 
-  printGameResult(gameResult) {
-    if (gameResult[2] === 3) {
-      Console.print('낫싱');
-      return;
-    }
-    let text = '';
-
-    for (let i = 0; i < 2; i++) {
-      if (gameResult[i] === 0) continue;
-
-      text += gameResult[i];
-      switch (i) {
-        case 0:
-          text += '볼 ';
-          break;
-        case 1:
-          text += '스트라이크';
+  discriminationStrikeOrBall() {
+    const result = this.userNumbers.reduce((acc, cur, i) => {
+      if (this.computerNumbers.includes(cur)) {
+        if (this.computerNumbers[i] === cur) return acc + 10;
+        else return acc + 100;
       }
-    }
 
-    Console.print(text);
+      return acc + 1;
+    }, 0);
+
+    const isGameSet = this.calculateGameResult(result);
+    if (isGameSet) return this.gameSet();
+
+    return this.userInputNumber();
   }
 
   calculateGameResult(result) {
@@ -72,56 +84,41 @@ class App {
   gameSet() {
     Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
     Console.readLine(
-      '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.',
+      '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n',
       (num) => {
         if (num === '1') {
           return this.play();
         }
 
         if (num === '2') {
-          process.exit();
-        }
-
-        throw '잘못된 숫자 입력';
+          Console.close();
+        } else throw new Error('잘못된 숫자 입력');
       },
     );
   }
 
-  discriminationStrikeOrBall() {
-    const result = this.userNumbers.reduce((acc, cur, i) => {
-      if (this.computerNumbers.includes(cur)) {
-        if (this.computerNumbers[i] === cur) return acc + 10;
-        else return acc + 100;
-      }
-
-      return acc + 1;
-    }, 0);
-
-    const isGameSet = this.calculateGameResult(result);
-    if (isGameSet) return this.gameSet();
-    return this.userInputNumber();
-  }
-
-  userInputNumber() {
-    try {
-      Console.readLine('숫자를 입력해주세요 : ', (numbers) => {
-        this.checkInputError(numbers);
-        this.setUserNumber(numbers);
-        return this.discriminationStrikeOrBall();
-      });
-    } catch (e) {
-      console.error(e);
+  printGameResult(gameResult) {
+    if (gameResult[2] === 3) {
+      Console.print('낫싱');
+      return;
     }
-  }
 
-  gameStart() {
-    Console.print('숫자 야구 게임을 시작합니다.');
-    this.getRandomNumbers();
-    this.userInputNumber();
-  }
+    let text = '';
 
-  play() {
-    this.gameStart();
+    for (let i = 0; i < 2; i++) {
+      if (gameResult[i] === 0) continue;
+
+      text += gameResult[i];
+      switch (i) {
+        case 0:
+          text += '볼 ';
+          break;
+        case 1:
+          text += '스트라이크';
+      }
+    }
+
+    Console.print(text);
   }
 }
 
