97bb2d257ff04f871d7c144edaf10dd1c6104150
semnil5202 <semnil5202@gmail.com>
2022-11-06 21:43:28 +0900

    feat: 게임을 재시작하거나 완전 종료하는 기능
    사용자가 3 strike를 수행했을 때 게임을 새로 시작할건지, 종료할건지 선택한다.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ const calculateScore = require("./calculateScore");
 class App {
   constructor() {
     this.answer = this.makeRandomNumber();
+    this.isContinue = true;
   }
 
   play() {
@@ -28,14 +29,37 @@ class App {
       const score = calculateScore(this.answer, inputs);
       const print = this.printScore(score);
       MissionUtils.Console.print(print);
+      if (this.isContinue) this.enterAnswer();
+      else this.isContinueGame();
     });
   }
 
   printScore({ strike, ball }) {
+    if (strike === 3) {
+      this.isContinue = false;
+      return `${strike}스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료`;
+    }
     if (strike !== 0 && ball === 0) return `${strike}스트라이크`;
     if (strike === 0 && ball !== 0) return `${ball}볼`;
     if (strike === 0 && ball === 0) return "낫싱";
     return `${ball}볼 ${strike}스트라이크`;
+  }
+
+  isContinueGame() {
+    MissionUtils.Console.readLine(
+      "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. : \n",
+      (input) => {
+        if (Number(input) === 1) {
+          this.isContinue = true;
+          this.makeRandomNumber();
+          this.enterAnswer();
+        }
+        if (Number(input) === 2) {
+          MissionUtils.Console.print("게임을 종료합니다.");
+          MissionUtils.Console.close();
+        }
+      }
+    );
   }
 }
 
