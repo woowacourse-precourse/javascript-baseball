9873b82169c296401175067bcb65083debe674f5
seoyoung-dev <seoyoung.dev@gmail.com>
2022-11-07 06:35:43 +0900

    feat: print game starting message and get user input

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 
+let RANDOM_NUMBER;
+
 function generateRandomNumber() {
-    const RANDOM_NUMBER = parseInt(
+    RANDOM_NUMBER = parseInt(
         MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3).join('')
     );
     return RANDOM_NUMBER;
 }
 
+function startGame() {
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+    MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (response) => {});
+}
+
 class App {
-    play() {}
+    play() {
+        generateRandomNumber();
+        startGame();
+    }
 }
 
 module.exports = App;
