d81dc43044887bf5b28bd6d5c8c12d5fbfa1adb6
JSH99 <tjgus9966@naver.com>
2022-11-07 04:38:37 +0900

    fix(compareWithInput): userNum 자료형 수정

--- src/compareWithInput.js
+++ src/compareWithInput.js
@@ -1,12 +1,14 @@
-const compareWithInput = (computerNum, userNum) => {
+const compareWithInput = (computerNumArr, inputNum) => {
     let ballCount = 0;
     let strikeCount = 0;
+    const computerNum = computerNumArr.join('');
+    const userNum = toString(inputNum);
 
     for(let i = 0; i < computerNum.length; i++) {
         if (userNum.includes(computerNum[i])) {
           ballCount++;
         }
-        if (userNum[i] == computerNum[i]) {
+        if (userNum[i] === computerNum[i]) {
           strikeCount++;
       }
     }
