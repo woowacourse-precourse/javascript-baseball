38af1fdb190bc82b1fcf67663566e51cfb5e06db
khkh0109 <ist200120@gmail.com>
2022-11-06 16:37:35 +0900

    fix(play): 버그 수정

--- src/App.js
+++ src/App.js
@@ -2,43 +2,43 @@ const MissionUtils = require("@woowacourse/mission-utils");
 class App {
   play() {
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
-    const computer = pickRandomNumbers();
-
-    const game = function() {
-      MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (answer) => {
-        try{
-          judgeInputNumber(answer);
-        } catch(error) {
-          MissionUtils.Console.print(error);
-          MissionUtils.Console.print('게임 종료');
-          MissionUtils.Console.close();
-          return;
-        }
-
-        const compareNumberArr = comparingNumbers(answer, computer);
-        const result = returnResult(compareNumberArr);
-        MissionUtils.Console.print(result);
-        
-        if(compareNumberArr[1] == 3) {
-          MissionUtils.Console.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.', (answer) => {
-            if(answer == 1) {
-              MissionUtils.Console.print(1);
-              app.play();
-            } else if(answer == 2) {
-              MissionUtils.Console.print(2);
-              MissionUtils.Console.print('게임 종료');
-              return MissionUtils.Console.close();
-            } else {
-              MissionUtils.Console.print('잘못된 값 입력. 게임 종료');
-              return MissionUtils.Console.close();
-            }
-          });
-        } 
-        game();
-      });
+    const play = function() {
+      const computer = pickRandomNumbers();
+      
+      const game = function() {
+        MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (answer) => {
+          try{
+            judgeInputNumber(answer);
+          } catch(error) {
+            MissionUtils.Console.print(error);
+            MissionUtils.Console.print('게임 종료');
+            MissionUtils.Console.close();
+            return;
+          }
+  
+          const compareNumberArr = comparingNumbers(answer, computer);
+          const result = returnResult(compareNumberArr);
+          MissionUtils.Console.print(result);
+          
+          if(compareNumberArr[1] == 3) {
+            MissionUtils.Console.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.' + '\n', (answer) => {
+              if(answer == 1) {
+                play();
+              } else if(answer == 2) {
+                MissionUtils.Console.print('게임 종료');
+                return MissionUtils.Console.close();
+              } else {
+                MissionUtils.Console.print('잘못된 값 입력. 게임 종료');
+                return MissionUtils.Console.close();
+              }
+            });
+          } 
+          game();
+        });
+      };
+      game();
     };
-
-    game();
+    play();
   }
 }
 
