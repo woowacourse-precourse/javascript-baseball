6cd24846ebb3cafadac925c373dda9764a910528
saejinpark <saejinpark951210@gmail.com>
2022-11-06 23:26:07 +0900

    continueAnswer 테스트

--- __tests__/funcTest.js
+++ __tests__/funcTest.js
@@ -1,4 +1,27 @@
 const App = require("../src/App");
+const MissionUtils = require("@woowacourse/mission-utils");
+
+const mockQuestions = (answers) => {
+  MissionUtils.Console.readLine = jest.fn();
+  answers.reduce((acc, input) => {
+    return acc.mockImplementationOnce((question, callback) => {
+      callback(input);
+    });
+  }, MissionUtils.Console.readLine);
+};
+
+const mockRandoms = (numbers) => {
+  MissionUtils.Random.pickNumberInRange = jest.fn();
+  numbers.reduce((acc, number) => {
+    return acc.mockReturnValueOnce(number);
+  }, MissionUtils.Random.pickNumberInRange);
+};
+
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(MissionUtils.Console, "print");
+  logSpy.mockClear();
+  return logSpy;
+};
 
 describe("기능테스트", () => {
   test("setBaseLine", () => {
@@ -37,7 +60,40 @@ describe("기능테스트", () => {
   test("compare", () => {
     const app = new App();
     app.setBaseLine();
-    const answer = app.compare([1, 2, 3])
+    const answer = app.compare([1, 2, 3]);
     expect(typeof answer).toEqual("boolean");
   });
+
+  test("continueAnswer1", () => {
+    const app = new App();
+    const spyFn = jest.spyOn(app, "setBaseLine");
+    app.continueAnswer("1");
+    expect(spyFn).toBeCalledTimes(1);
+  });
+  
+  test("continueAnswer1", () => {
+    const app = new App();
+    const spySetBaseLine = jest.spyOn(app, "setBaseLine");
+    const spyProgressFn = jest.spyOn(app, "progress");
+    app.continueAnswer("1");
+    expect(spySetBaseLine).toBeCalledTimes(1);
+    expect(spyProgressFn).toBeCalledTimes(1);
+  });
+  
+  test("continueAnswer2", () => {
+    const app = new App();
+    const spySetBaseLine = jest.spyOn(app, "setBaseLine");
+    const spyProgressFn = jest.spyOn(app, "progress");
+    app.continueAnswer("2");
+    expect(spySetBaseLine).not.toBeCalledTimes(1);
+    expect(spyProgressFn).not.toBeCalledTimes(1);
+  }); 
+  
+  test("continueAnswerDefault", () => {
+    expect(() => {
+      const app = new App();
+      app.continueAnswer("3")
+    }).toThrow();
+  }); 
+  
 });
--- src/App.js
+++ src/App.js
@@ -68,7 +68,7 @@ class App {
       }
     );
   }
-  
+
   nextProgress(end) {
     if (end) {
       wConsole.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
