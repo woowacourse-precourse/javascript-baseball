1af12d719b8d6c71df184ce703606e59425a3b0c
min <hororonoodle@gmail.com>
2022-11-07 00:40:40 +0900

    test : [T3-1]에 대한 테스트 케이스 추가

--- __tests__/S3_Test.js
+++ __tests__/S3_Test.js
@@ -1,4 +1,36 @@
+const App = require('../src/App');
+const MissionUtils = require('@woowacourse/mission-utils');
+
+const mockQuestions = (answers) => {
+  MissionUtils.Console.readLine = jest.fn();
+  answers.reduce((acc, input) => {
+    return acc.mockImplementationOnce((question, callback) => {
+      callback(input);
+    });
+  }, MissionUtils.Console.readLine);
+};
+
+const mockRandoms = (numbers) => {
+  MissionUtils.Random.pickNumberInRange = jest.fn();
+  numbers.reduce((acc, number) => {
+    return acc.mockReturnValueOnce(number);
+  }, MissionUtils.Random.pickNumberInRange);
+};
+
 describe('[S3]유저는 잘못된 정보를 입력할 경우 버그로 인해 올바르지 못한 게임을 하는 것을 원치 않는다.', () => {
-  test('[T3-1] 유저가 1부터 9까지 서로 다른 세자리수 외에 수를 입력한걸 검증하는 기능', () => {});
+  test('[T3-1] 유저가 1부터 9까지 서로 다른 세자리수 외에 수를 입력한걸 검증하는 기능', () => {
+    const testAnswer = [1, 3, 5];
+    const testErrorInput = [['12'], ['12.123'], ['-500']];
+
+    testErrorInput.map((errorInput) =>
+      expect(() => {
+        mockRandoms(testAnswer);
+        mockQuestions(errorInput);
+        const app = new App();
+        app.play();
+      }).toThrow()
+    );
+  });
+
   test('[T3-2] 게임 종료 기능', () => {});
 });
