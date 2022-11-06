627acb0b64cfee1abc1bf06d1aa46f5e0c868220
unqeye <salyuan@naver.com>
2022-11-06 19:44:21 +0900

    fix: 게임 재시작 여부 입력 이후 로직을 콜백함수 내부로 이동

--- src/App.js
+++ src/App.js
@@ -83,11 +83,13 @@ class App {
     MissionUtils.Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
   }
 
-  getNewGame() {
+  processNewGame() {
     MissionUtils.Console.readLine(
       '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.',
       (input) => {
         this.newGame = input;
+        this.checkNewGame();
+        this.handleNewGame();
       },
     );
   }
