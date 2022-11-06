4bb1093b3e2fef7467a00c1c488257f3eb077177
cys4585 <choizerowater@gmail.com>
2022-11-06 16:41:25 +0900

    test: 어플리케이션 테스트 추가

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -81,4 +81,74 @@ describe("숫자 야구 게임", () => {
       app.play();
     }).toThrow();
   });
+
+  test("나의 테스트", () => {
+    const randoms = [1, 3, 5, 5, 8, 9, 3, 6, 1];
+    const answers = [
+      "246",
+      "135",
+      "1",
+      "597",
+      "589",
+      "1",
+      "123",
+      "456",
+      "789",
+      "136",
+      "361",
+      "2",
+    ];
+    const logSpy = getLogSpy();
+    const messages = [
+      "숫자 야구 게임을 시작합니다.",
+      "낫싱",
+      "3스트라이크",
+      "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
+      "1볼 1스트라이크",
+      "3스트라이크",
+      "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
+      "2볼",
+      "1볼",
+      "낫싱",
+      "3볼",
+      "3스트라이크",
+      "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
+    ];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    const app = new App();
+    app.play();
+
+    messages.forEach((output, index) => {
+      expect(logSpy).toHaveBeenNthCalledWith(index + 1, output);
+    });
+  });
+
+  test("나의 예외 테스트1", () => {
+    const randoms = [1, 3, 5];
+    const answers = ["12"];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow();
+  });
+
+  test("나의 예외 테스트2", () => {
+    const randoms = [1, 3, 5];
+    const answers = ["12a"];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow();
+  });
 });
