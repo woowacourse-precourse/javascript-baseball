fa2e10236b400cba2e5b6a315ae3ca8957778571
jihong <rlawyd9@kookmin.ac.kr>
2022-11-07 02:11:59 +0900

    feat: input game num

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ const MissionUtils = require("@woowacourse/mission-utils");
 
 
 class App {
+
   selectComputerNum(){
     const computer = [];
     while (computer.length < 3) {
@@ -13,11 +14,32 @@ class App {
     return computer;
   }
 
+
+  getGameResult(){
+
+  }
+
+
   play() {
 
     const computer_num = this.selectComputerNum();
 
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+
+    MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (input) => {
+      console.log(input);
+      MissionUtils.Console.close();
+    })
+    
+    // while(getGameResult(input)){}
+
   }
+
 }
 
 module.exports = App;
+
+
+const app = new App();
+app.play();
+
