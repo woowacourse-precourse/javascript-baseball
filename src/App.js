ba751e5fb93ac8a6a35e34c34135b7e18ec6f92a
su2333066 <su2333066@naver.com>
2022-11-07 00:37:34 +0900

    랜덤 3자리수 생성(서로 다른 수)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,47 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  constructor() {
+    this.selectComputerNumber = [];
+    this.inputNumber = "";
+  }
+
+  selectRandomNum() {
+    // 컴퓨터의 1~9까지 서로다른 3자리의 숫자 선택
+    while (this.selectComputerNumber.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!this.selectComputerNumber.includes(number)) {
+        this.selectComputerNumber.push(number);
+      }
+    }
+  }
+
+  inputNum() {
+    // 사용자의 숫자 입력
+    MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (number) => {
+      // if (typeof number !== "number") {
+      //   throw "숫자를 입력하세요";
+      // }
+      // if (String(number).length !== 3) {
+      //   throw "서로다른 3자리수를 입력하세요.";
+      // }
+      this.inputNumber = String(number);
+    });
+  }
+
+  play() {
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+
+    this.selectRandomNum();
+    console.log(this.selectComputerNumber);
+    // 정답을 맞출때까지 반복
+    // while (this.inputNumber !== this.computerNumber) {
+    // }
+    this.inputNum();
+  }
 }
 
-module.exports = App;
+//module.exports = App;
+
+const app = new App();
+app.play();
