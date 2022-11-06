fdbab9d619720fbae5e85331a01e87b82faa44a9
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2022-11-07 00:20:34 +0900

    FEAT: 각 자리가 중복되지 않는 1~9의 수를 가지는 세자리수 랜덤 생성

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ const inputValidation = require("./inputValidation");
 const outputValidation = require("./outputValidation");
 
 class App {
-  constructor(inputNum) {
+  constructor(inputNum, randomNum) {
     this.inputNum = inputNum;
+    this.randomNum = randomNum;
   }
 
   play() {
+    this.randomNumber();
     this.userInput().then(() => {
       this.checkInputValidation();
     });
@@ -17,8 +19,8 @@ class App {
     return new Promise((resolve) => {
       MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (input) => {
         this.inputNum = input;
-        if(!this.checkInputValidation()){
-          throw "잘못된 값을 입력하셨습니다."
+        if (!this.checkInputValidation()) {
+          throw "잘못된 값을 입력하셨습니다.";
         }
         MissionUtils.Console.print(input);
         MissionUtils.Console.close();
@@ -31,6 +33,19 @@ class App {
     inputValidation.checkNoOverlap(this.inputNum);
     inputValidation.checkOnlyNum(this.inputNum);
     inputValidation.checkThreeNum(this.inputNum);
+  }
+
+  randomNumber() {
+    this.randomNum = [];
+    for (let rotate = 0; rotate < 3; rotate++) {
+      const random = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (this.randomNum.indexOf(random) === -1) {
+        this.randomNum.push(random);
+      } else {
+        rotate--;
+      }
+    }
+    MissionUtils.Console.print(this.randomNum);
   }
 }
 const startGame = new App();
