0899dad346d0f3c59ff9682470fb8e1663275b1c
JUNU <106523012+bokjunwoo@users.noreply.github.com>
2022-11-07 00:06:28 +0900

    feat : 게임 시작 문구 추가

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ const MissionUtils = require("@woowacourse/mission-utils");
 class App {
   answer;
 
-  play() {}
+  play() {
+    this.gameStartMessage()
+  }
+
+  gameStartMessage() {
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.')
+  };
+  
 
   computerRandomNumber() {
     const nonDuplicateNumbers = [];
