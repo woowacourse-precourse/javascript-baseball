53a3599319a9da4c3e6a0ed2f1b96c978f3a1dcb
KINHYEONJI <hyeonji0718@gmail.com>
2022-11-07 02:58:34 +0900

    fix(Judge) : 1-9 사이의 숫자만으로 야구게임을 진행하므로 정규표현식 범위 수정

--- src/Judge.js
+++ src/Judge.js
@@ -1,8 +1,8 @@
-const { LENGTH } = require("./Constant.js");
+const { LENGTH, RANGE } = require("./Constant.js");
 
 class Judge {
   isPlayerInputValid(playerInput) {
-    const numReg = /[\d]+/;
+    const numReg = /[1-9]+/;
     if (playerInput.length !== LENGTH) return false;
     if (playerInput.match(numReg)[0].length !== LENGTH) return false;
     if (this.isDuplicateNum(playerInput)) return false;
