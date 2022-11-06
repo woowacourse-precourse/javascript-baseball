7b606f1ccee7fd45edc7fb821a8b5466131422d0
turtle601 <turtle601@naver.com>
2022-11-06 09:38:10 +0900

    refactor(constant.js) BASEBALL 관련 상수 추가

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,6 +3,13 @@ const RANDOMLIST = Object.freeze({
   ENDPOINT: 9,
 });
 
+const BASEBALL = Object.freeze({
+  STRIKE: '스트라이크',
+  BALL: '볼',
+  NOTHING: '낫싱',
+});
+
 module.exports = {
   RANDOMLIST,
+  BASEBALL,
 };
