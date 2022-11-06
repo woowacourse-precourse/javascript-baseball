06096c93d03c26715ac62c8e77ef4705ec001d40
NYeonK <skdus728@gmail.com>
2022-11-06 18:07:33 +0900

    [ feat ] 컴퓨터가 입력한 숫자에 대한 결과를 출력

--- src/gameProcess.js
+++ src/gameProcess.js
@@ -1,25 +1,46 @@
 const MissionUtils = require("@woowacourse/mission-utils");
+const computer = [];
 
 function getComputerNumbers() {
-    const computer = [];
     while (computer.length < 3) {
         const number = MissionUtils.Random.pickNumberInRange(1, 9);
         if (!computer.includes(number)) computer.push(number);
     }
-    console.log(computer.join(''))
-    return computer.join('');
+    console.log(computer)
+    return computer;
 }
 
 function gameProcess() {
     getComputerNumbers();
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
     MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (answer) => {
-        gameHint(answer);
+        countResult(answer);
     });
 }
 
-function gameHint(number) {
-    console.log(number);
+function countResult(answer) {
+    let ball = 0, strike = 0;
+    answer = answer.split('');
+
+    for (let i = 0; i < 3; i++){
+        if (Number(answer[i]) === computer[i]) strike++;
+        else if(computer.includes(Number(answer[i]))) ball++;
+    }
+    
+    return getHint(ball,strike);
+}
+
+function getHint(ball,strike) {
+    let hintMessage = '';
+
+    (ball&&strike) ? hintMessage += `${ball}볼 ${strike}스트라이크`
+    : ball ? hintMessage += `${ball}볼`
+    : strike ? hintMessage += `${strike}스트라이크`
+    : hintMessage += `낫싱`
+
+    console.log(hintMessage);
+    
+    return hintMessage;
 }
 
 gameProcess();
