b20313230bd004698ea8fe3613fac58c04ee9c10
mjmj0217 <mjmj0217@naver.com>
2022-11-06 20:58:49 +0900

    refactor(Game): 의미없는 코드 삭제

--- src/Game.js
+++ src/Game.js
@@ -25,7 +25,6 @@ class Game {
     Console.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     Console.readLine("", (cmd) => {
       if (isInvalidCmd(cmd)) {
-        console.log(cmd);
         throw new Error("Invalid Command!");
       }
       if (cmd === "1") {
