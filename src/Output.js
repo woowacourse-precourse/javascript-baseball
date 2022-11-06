ceb8b1f142c4decfe36474486ff5f8ba2df45063
cloudedpanther <rickeykoh@ajou.ac.kr>
2022-11-06 20:02:45 +0900

    feat(method): add printToUser method
    Add to Output class
    - printToUser method
    - WELCOME_MESSAGE constant
    - END_MESSAGE constant

--- src/Output.js
+++ src/Output.js
@@ -1,5 +1,16 @@
+const { MissionUtils } = require("@woowacourse/mission-utils");
+
+const WELCOME_MESSAGE = "숫자 야구 게임을 시작합니다.";
+const END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+
 class Output {
-  static printToUser(message) {}
+  static printToUser(message) {
+    MissionUtils.Console.print(message);
+  }
 }
 
-module.exports = Output;
+module.exports = {
+  WELCOME_MESSAGE,
+  END_MESSAGE,
+  Output,
+};
