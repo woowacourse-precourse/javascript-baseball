2814d0961c90469f006ff81d6d88637e1c7e71c4
2jaebbang <72256238+2jaebbang@users.noreply.github.com>
2022-11-06 22:03:10 +0900

    refactor: 투구 분석 및 결과 출력 기능 파일 분리

--- src/gameStart.js
+++ src/gameStart.js
@@ -1,43 +1,7 @@
 const { Console } = require('@woowacourse/mission-utils');
 const { validationNumbers } = require('./validation');
 const { generateRandomNumbers } = require('./generateRandomNumbers');
-
-function pitchAnalysis(gameData, userNumbers, computerNumbers) {
-  const notStrikeNumbers = userNumbers.filter(
-    (userNumber, index) => computerNumbers[index] !== userNumber
-  );
-  const ballNumbers = notStrikeNumbers.filter((notStrikeNumber) =>
-    computerNumbers.includes(notStrikeNumber)
-  );
-  const strike = 3 - notStrikeNumbers.length;
-  const ball = ballNumbers.length;
-
-  gameData.setBall(ball);
-  gameData.setStrike(strike);
-}
-
-function printPitchResult(gameData) {
-  const ball = gameData.getBall();
-  const strike = gameData.getStrike();
-
-  if (ball === 0 && strike === 0) {
-    Console.print('낫싱');
-    return;
-  }
-  if (ball === 0) {
-    Console.print(`${strike}스트라이크`);
-    if (strike === 3) {
-      Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
-      gameData.setThreeStrikeState(true);
-    }
-    return;
-  }
-  if (strike === 0) {
-    Console.print(`${ball}볼`);
-    return;
-  }
-  Console.print(`${ball}볼 ${strike}스트라이크`);
-}
+const { pitchAnalysis } = require('./pitchAnalysis');
 
 function gameStart(gameData) {
   const BUTTON = { restart: '1', exit: '2' };
@@ -52,7 +16,7 @@ function gameStart(gameData) {
     }
 
     pitchAnalysis(gameData, userRandomNumbers, computerRandomNumbers);
-    printPitchResult(gameData);
+
     if (!gameData.getThreeStrikeState()) {
       return gameStart(gameData);
     }
