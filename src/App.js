af5d41494a5d067a081b4eb98b5e8d7f07501e46
seoyoung-dev <seoyoung.dev@gmail.com>
2022-11-07 03:50:57 +0900

    feat: generate three digits random number

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+
+function generateRandomNumber() {
+    const RANDOM_NUMBER = parseInt(
+        MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3).join('')
+    );
+    return RANDOM_NUMBER;
+}
+
 class App {
-  play() {}
+    play() {}
 }
 
 module.exports = App;
