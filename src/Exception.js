66ca0fd17d45ec6003459b069cb3cebadac2e822
bkdragon0228 <rlaqjarb2067@naver.com>
2022-11-07 00:20:02 +0900

    refactor : 에러명을 통일
    1204와 같은 1~9 이외의 숫자, 세자리수를 동시에 만족하지 않는 경우를 위해
    오류명을 통일했습니다.

--- src/Exception.js
+++ src/Exception.js
@@ -14,23 +14,20 @@ class Exception {
         this.isOneOrTwo(this.input);
     }
     isThreeLength(input) {
-        if (input.length !== 3) throw new Error('3자리 숫자를 입력해주세요.');
+        if (input.length !== 3) throw new Error();
     }
     isDuplication(input) {
         const inputSet = [...new Set(input.split(''))];
 
-        if (inputSet.length !== 3)
-            throw new Error('서로 다른 수를 입력해주세요.');
+        if (inputSet.length !== 3) throw new Error();
     }
     isHaveZero(input) {
         for (let number of input) {
-            if (number === '0')
-                throw new Error('1 ~ 9 사이에 숫자를 입력해주세요.');
+            if (number === '0') throw new Error();
         }
     }
     isOneOrTwo(input) {
-        if (input !== '1' && input !== '2')
-            throw new Error('1 또는 2를 입력해주세요.');
+        if (input !== '1' && input !== '2') throw new Error();
     }
 }
 
