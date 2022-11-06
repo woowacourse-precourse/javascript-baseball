930da681f5fd4f6d82e720a336bae8000bd7559a
DESKTOP-PDMTIQO\hansu <fathlic5@gmail.com>
2022-11-07 04:33:02 +0900

    :white_check_mark: test: 기능1과 기능2의 리팩토링 테스트 코드 기능1과 기능2에서 입력값에 따른 테스트 추가 수정

--- __tests__/SelfTest.js
+++ __tests__/SelfTest.js
@@ -1,64 +1,66 @@
 const App = require("../src/App");
 const MissionUtils = require("@woowacourse/mission-utils");
 
-test("상대방 결과에 중복값 있는지 테스트", () => {
-  expect(() => {
-    const app = new App();
-    const computerNumber = app.isComputerNumbers();
-      
-    expect(new Set(computerNumber).size).toEqual(3);
-  })
-})
-
 const mockQuestions = (answers) => {
   MissionUtils.Console.readLine = jest.fn();
   answers.reduce((acc, input) => {
     return acc.mockImplementationOnce((question, callback) => {
       callback(input);
-    })
+    });
   }, MissionUtils.Console.readLine);
 }
 
-test("사용자가 0을 입력했을때 RangeError 나는지 테스트", () => {
-  const answers = ["103"];
+describe("기능1과 기능2 테스트", () => {
+ test("상대방 결과에 중복값 있는지 테스트", () => {
+    expect(() => {
+      const app = new App();
+      const computerNumber = app.isComputerNumbers();
+      
+      expect(new Set(computerNumber).size).toEqual(3);
+    })
+  })
 
-  mockQuestions(answers);
+  test("사용자가 0을 입력했을때 RangeError 나는지 테스트", () => {
+    const answers = ["103"];
 
-  expect(() => {
-    const app = new App();
-    app.play();
-  }).toThrow(RangeError);
-})
+    mockQuestions(answers);
 
-test("사용자가 글자수를 지키지 않을시 RangeError 나는지 테스트", () => {
-  const answers = ["1234"];
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow(RangeError);
+  })
 
-  mockQuestions(answers);
+  test("사용자가 글자수를 지키지 않을시 RangeError 나는지 테스트", () => {
+    const answers = ["1234"];
 
-  expect(() => {
-    const app = new App();
-    app.play();
-  }).toThrow(RangeError);
-})
+    mockQuestions(answers);
 
-test("사용자가 중복값을 입력시 RangeError 나는지 테스트", () => {
-  const answers = ["1234"];
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow(RangeError);
+  })
 
-  mockQuestions(answers);
+  test("사용자가 중복값을 입력시 RangeError 나는지 테스트", () => {
+    const answers = ["1234"];
 
-  expect(() => {
-    const app = new App();
-    app.play();
-  }).toThrow(RangeError);
-})
+    mockQuestions(answers);
 
-test("사용자가 숫자외의 값을 입력시 RangeError 나는지 테스트", () => {
-  const answers = ["1a@"];
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow(RangeError);
+  })
 
-  mockQuestions(answers);
+  test("사용자가 숫자외의 값을 입력시 RangeError 나는지 테스트", () => {
+    const answers = ["1a@"];
 
-  expect(() => {
-    const app = new App();
-    app.play();
-  }).toThrow(RangeError);
+    mockQuestions(answers);
+
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow(RangeError);
+  })
 })
