8d8256e890904c0881fc373b125a6f71a5b3efd9
energy1-914 <dlawldnjs5@daum.net>
2022-11-07 00:55:59 +0900

    feat: computerNumber에 컴퓨터의 3자리 랜덤값 지정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
+  constructor() {
+    this.computerNumber = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
+    this.userInputNumber;
+  }
+
   play() {}
 }
 
 module.exports = App;
+
+const test = new App;
+test.play();
