8198abebfc8bbb947c96a3bade7420290f22f373
Serin-Kim <serin9864@gmail.com>
2022-11-06 17:23:44 +0900

    feat: getInputAndCompare 함수 구현
    사용자로부터 서로 다른 3개의 숫자를 입력받는 함수 작성

--- src/App.js
+++ src/App.js
@@ -5,9 +5,16 @@ class App {
     return MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3).join("");
   };
 
+  getInputAndCompare = (ANSWER) => {
+    MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (input) => {
+      MissionUtils.Console.close();
+    });
+  };
+
   play = () => {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
     const ANSWER = this.makeAnswer();
+    this.getInputAndCompare(ANSWER);
   };
 }
 
