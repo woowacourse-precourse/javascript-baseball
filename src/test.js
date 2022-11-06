bc26e9c9a630a2ef07c6e02d845d3c02caafc406
chsua <csj1919@naver.com>
2022-11-06 18:22:10 +0900

    test: 4. print문구 만드는 함수(answer)

--- src/test.js
+++ src/test.js
@@ -20,6 +20,22 @@ describe("게임함수확인용", () => {
     expect(app.checkNum("861", [8, 6, 1])).toEqual([0, 3]);
   });
 
+  test("answer_1", () => {
+    expect(app.answer([0, 0])).toEqual("낫싱");
+  });
+
+  test("answer_2", () => {
+    expect(app.answer([0, 1])).toEqual("1스트라이크");
+  });
+
+  test("answer_3", () => {
+    expect(app.answer([2, 0])).toEqual("2볼");
+  });
+
+  test("answer_4", () => {
+    expect(app.answer([2, 1])).toEqual("2볼 1스트라이크");
+  });
+
   // 아래 close 를 하지않을 시 test가 끝나지 않음
   MissionUtils.Console.close();
 });
