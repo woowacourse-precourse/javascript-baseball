bd4a4f919c3ba511d27d9323a6d6e9a4be85db9f
Buyoung Chung <sunarol@naver.com>
2022-11-06 22:37:27 +0900

    test: Add computer random array test

--- __tests__/MyTest.js
+++ __tests__/MyTest.js
@@ -25,4 +25,11 @@ describe("기능 테스트", () => {
 
     expect(logSpy).toHaveBeenCalledWith(expect.stringContaining("숫자 야구 게임을 시작합니다."));
   });
+
+  test("컴퓨터 난수 배열 테스트", () => {
+    baseballGame.computer.makeRandomNumberArray();
+    const testArray = baseballGame.computer.computerRandomNumberArray;
+
+    expect(checkComputerRandomArray(testArray)).toBe(true);
+  });
 });
