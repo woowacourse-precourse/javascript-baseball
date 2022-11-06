fe78fb516721640f2950b48798233e6c5262593b
SeungWoo Kim <osabero@naver.com>
2022-11-06 16:24:27 +0900

    임의의 3개의 숫자를 뽑는것을 구현했습니다.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import * as MissionUtils from "@woowacourse/mission-utils";
+const MissionUtils = require("@woowacourse/mission-utils");
 class App {
-  play() {}
-  computer()
+  play() {
+    computer()
+    input()
+  }
+}
   
-  
+function computer() {
+  const COM_NUMBER = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
+  console.log(COM_NUMBER)
 }
 
-function computer() {
-  if(CHECK_ANSWER){break}
-  const COM_NUMBER = MissionUtils.Random.pickNumberInRange(1, 9, 3);
-  console.log(COM_NUMBER)
+function input() {
+  while(CHECK_ANSWER == false){
+    MissionUtils.Console.readLine('숫자를 입력해 주세요', (answer) => {
+      MissionUtils.Console.print('ㅠㅠ')
+      // inputcheck(answer);
+    })
+  };
+}
+
+function inputcheck(answer) {
+  if (answer.regExp == /(?:d{3})$/) {
+    throw exception()
+  }
+}
+function exception() {
+  MissionUtils.Console.print('잘못된 숫자를 입력하여 게임을 종료합니다.')
 }
 module.exports = App;
