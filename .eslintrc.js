9db17e487a61930bd7949e90486c47a830a1c0a5
Leemainsw <s2018s15@e-mirim.hs.kr>
2022-11-07 00:46:51 +0900

    feat : eslint 및 prettier 설정

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: "eslint:recommended",
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {
+    indent: ["error", 2],
+    semi: ["error", "always"],
+    "no-trailing-spaces": 0,
+    "keyword-spacing": 0,
+    "no-unused-vars": 1,
+    "no-multiple-empty-lines": 0,
+    "space-before-function-paren": 0,
+    "eol-last": 0,
+  },
+};
