dc0a9693baa393bd6a0d20b64ce327790747aa4b
HoberMin <102784200+HoberMin@users.noreply.github.com>
2022-11-06 17:26:48 +0900

    feat : 랜덤숫자 생성

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 const { Console } = require("@woowacourse/mission-utils");
 const { Random } = require("@woowacourse/mission-utils");
+
 class App {
   play() {}
+
+  MakeNum() {
+    const randomNumber = [];
+    while (randomNumber.length < 3) {
+      const number = Random.pickNumberInRange(1, 9);
+      if (!randomNumber.includes(number.toString())) {
+        randomNumber.push(number.toString());
+      }
+    }
+    return randomNumber;
+  }
 }
 
 module.exports = App;
