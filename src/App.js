416940865de8d5ad53ba8e586996d45515b528b2
bae-sh <hyeon1293@gmail.com>
2022-11-07 03:38:30 +0900

    feat : setAnswer 구현

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 class App {
   constructor() {
-    this.number = [];
+    this.computer = [];
   }
-  play() {}
+  play() {
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+    this.setAnswer();
+    this.input();
+  }
+  setAnswer() {
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+    return computer;
+  }
+  input() {}
+  checkAnswer() {}
 }
 
 const app = new App();
