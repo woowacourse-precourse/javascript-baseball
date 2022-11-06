4e11b98e81043b65ac36ebfb6c0329ddc56fa600
2je0 <bulljy@naver.com>
2022-11-07 03:10:28 +0900

    ✨ feat: 사용자 입력 검증하는 함수 checkError 구현
    헬퍼함수 hasError와 함께 4자리 이상 입력, 숫자 아닌값 입력, 1~9아닌 값 입력, 중복된 숫자를 입력했을 때 에러를 냄

--- src/App.js
+++ src/App.js
@@ -12,6 +12,21 @@ class App {
     }
     return ret;
   }
+
+  checkError(query) {
+    if (this.hasError(query)) throw new Error('사용자 인풋 에러');
+  }
+
+  hasError(query) {
+    const queryArr = this.getQueryArrFromQuery(query);
+    if (queryArr.length > 3) return true;
+    queryArr.forEach((num) => {
+      if (Number(num) < 1 || Number(num) > 9) return true;
+    });
+    const querySet = new Set(queryArr);
+    if (querySet.size !== 3) return true;
+    return false;
+  }
 }
 
 module.exports = App;
