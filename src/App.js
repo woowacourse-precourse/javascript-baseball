f9f20e0f40e1afa464d1e39fab65ad864f7923a7
chchaeun <kce3615@naver.com>
2022-11-07 00:39:54 +0900

    refactor: 게임 초기 세팅값 멤버변수로 변경

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
   constructor() {
+    this.RANGE_START_NUMBER = 1;
+    this.RANGE_END_NUMBER = 9;
+    this.NUMBER_OF_DIGITS = 3;
+
     this.answer = null;
     this.inputNumber = null;
   }
@@ -11,14 +15,10 @@ class App {
     this.getInputNumber();
   }
   getAnswerNumber() {
-    const RANGE_START_NUMBER = 1;
-    const RANGE_END_NUMBER = 9;
-    const NUMBER_OF_DIGITS = 3;
-
     const ANSWER = MissionUtils.Random.pickUniqueNumbersInRange(
-      RANGE_START_NUMBER,
-      RANGE_END_NUMBER,
-      NUMBER_OF_DIGITS
+      this.RANGE_START_NUMBER,
+      this.RANGE_END_NUMBER,
+      this.NUMBER_OF_DIGITS
     );
 
     this.answer = ANSWER;
@@ -37,7 +37,7 @@ class App {
   handleGameException(inputNumber) {
     const NOT_A_NUMBER_EXCEPTION = "입력값이 숫자가 아닙니다.";
     const NOT_INTEGER_EXCEPTION = "입력값이 정수가 아닙니다.";
-    const NUMBER_OF_DIGITS_EXCEPTION = "입력값이 3자리의 수가 아닙니다.";
+    const NUMBER_OF_DIGITS_EXCEPTION = `입력값이 ${this.NUMBER_OF_DIGITS}자리의 수가 아닙니다.`;
     const SAME_NUMBER_EXCEPTION = "입력한 3자리의 수 중 같은 수가 존재합니다.";
 
     let errorMessage = null;
