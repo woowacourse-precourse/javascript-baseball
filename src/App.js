151f8094d1ad3828d500601883edae87d6b6fe1c
seunghoria <102285042+seunghoria@users.noreply.github.com>
2022-11-06 23:00:28 +0900

    feat: 사용자 입력값 유효성 검증 기능 구현

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ const MissionUtils = require("@woowacourse/mission-utils");
 
 const mConsole = MissionUtils.Console;
 const mRandom = MissionUtils.Random;
+const GAME_NUMBER_LENGTH = 3;
 
 class App {
   play() {
@@ -14,9 +15,38 @@ class App {
   }
 
   getInput() {
-    mConsole.readLine("숫자를 입력해주세요 : ", (input) => {
-      input;
+    mConsole.readLine("숫자를 입력해주세요 : ", (pickedNumber) => {
+      if (this.checkInputNumber(pickedNumber)) {
+        pickedNumber;
+      }
     });
+  }
+
+  checkScope(input) {
+    if (input.includes(0)) throw "0이 아닌 1~9 사이 숫자를 입력해주세요.";
+  }
+
+  checkType(input) {
+    if (isNaN(input)) throw "숫자를 입력해주세요.";
+  }
+
+  checkLength(input) {
+    if (input.length !== GAME_NUMBER_LENGTH)
+      throw `${GAME_NUMBER_LENGTH}개의 숫자를 입력해주세요.`;
+  }
+
+  checkDuplicatedInput(input) {
+    if (input.length !== new Set(input).size)
+      throw "서로 다른 숫자를 입력해주세요.";
+  }
+
+  checkInputNumber(input) {
+    this.checkScope(input);
+    this.checkType(input);
+    this.checkLength(input);
+    this.checkDuplicatedInput(input);
+
+    return true;
   }
 }
 
