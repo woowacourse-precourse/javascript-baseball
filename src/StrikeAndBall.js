b3566e61bc6abcfd37286ce22c9cba3670f028ab
woo-jk <201702042@o.cnu.ac.kr>
2022-11-06 11:50:15 +0900

    feat: 볼 개수 구하는 함수 구현

--- src/StrikeAndBall.js
+++ src/StrikeAndBall.js
@@ -1,6 +1,15 @@
 function getResult(answer, input) {}
-function getBallCount(input) {}
-function getStrikeCount(input) {}
+function getBallCount(answer, input) {
+  let ballCount = 0;
+  input.split("").forEach((eachNumber, index) => {
+    if (answer.includes(eachNumber) && answer.indexOf(eachNumber) !== index) {
+      ballCount++;
+    }
+  });
+
+  return ballCount;
+}
+function getStrikeCount(answer, input) {}
 function getString(strike, ball) {}
 
 exports.getResult = getResult;
