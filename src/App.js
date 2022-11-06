e7c81ef75588f7b46720ed5bcd0707de6ae572a4
millifail <hwangbird99@gmail.com>
2022-11-07 06:51:39 +0900

    feat(game-control): control the game
    - [게임 종료] 사용자의 3자리 숫자가 같은 경우 게임이 종료되는 기능 구현

--- src/App.js
+++ src/App.js
@@ -35,6 +35,13 @@ class App {
         this.HINT(user_input_number, correct_number);
       }
     }
+    MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    Console.readLine(
+      "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
+      (num) => {
+        num = Number(num);
+      }
+    );
   }
 
   HINT(user, correct) {
