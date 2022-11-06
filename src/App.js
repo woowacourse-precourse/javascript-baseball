59a7c9a93abe815999c41892af2bfe794c33f0a1
subin <newdeas100@gmail.com>
2022-11-06 17:39:19 +0900

    feat(baseball) : initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
-class App {
-  play() {}
+const MissionUtils = require("@woowacourse/mission-utils");
+
+
+function getInput(){
+  MissionUtils.Console.print('숫자 야구게임을 시작합니다.');
+  MissionUtils.Console.close();
 }
+
+
+
+
+class App {
+  play() {
+    getInput();
+  }
+}
+
+
+const app = new App();
+app.play();
+
+
 
 module.exports = App;
