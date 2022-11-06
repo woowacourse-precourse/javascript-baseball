d1bdb09d5c257e94a612c4f3ae4877cf017cef3b
Sallyvely <tpdus4159@naver.com>
2022-11-06 22:11:16 +0900

    기능 구현 : 점수 계산하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -23,6 +23,30 @@ class App {
     })
       return userInput;
     }
+      /*스트라이크인지 점수를 계산하는 기능*/
+  strike(computerRandomNum, userInputNum){
+    let strikeScore=0;
+    console.log(computerRandomNum+"스트 "+userInputNum);
+    for(let index=0;index<3;index++){
+      if(String(computerRandomNum).charAt(index)==String(userInputNum).charAt(index)){
+        strikeScore+=1;
+        
+      }
+    }  
+    return strikeScore;
+  }
+  /*볼인지 점수 계산하는 기능*/
+  ball(computerRandomNum, userInputNum){
+    console.log(computerRandomNum+"볼 "+userInputNum);
+    let ballScore=0;
+    for(let index=0; index<3;index++){
+      if((computerRandomNum).includes(String(userInputNum).charAt(index))&&!(String(computerRandomNum).charAt(index)==String(userInputNum).charAt(index))){
+        ballScore+=1;
+        
+      }
+    }
+    return ballScore;
+  }
 }
 
 module.exports = App;
