93d0d088d9627fc6399230312235ad845e5fe382
wonkyung <mwk0725@gmail.com>
2022-11-06 15:28:59 +0900

    feat: 정답 맞춘 후 다시 시작할지 종료할지 물어보는 문구 출력

--- src/App.js
+++ src/App.js
@@ -55,6 +55,7 @@ class App {
     if (this.strikeCount === 3) {
       MissionUtils.Console.print('3스트라이크');
       MissionUtils.Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+      return this.askRestartOrExit();
     }
     return false;
   }
@@ -107,6 +108,12 @@ class App {
     this.strikeCheck();
     this.ballCheck();
     this.strikeAndBallCheck();
+  }
+
+  askRestartOrExit() {
+    MissionUtils.Console.print(
+      '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.',
+    );
   }
 }
 
