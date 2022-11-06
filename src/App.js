3e7bc36e7bb832fa180248f3d934279003293e9e
SeungMiL <tmskqj@naver.com>
2022-11-06 15:59:59 +0900

    feat: 랜덤값과 각 자리수 비교 후 스트라이크, 볼, 낫싱 출력 기능 완료

--- src/App.js
+++ src/App.js
@@ -27,9 +27,45 @@ class BaseBallPlay {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
   }
 
+  strike(computer, user) {
+    let count = 0;
+    computer.forEach((num, i) => {
+      if (num === +user[i]) {
+        count += 1;
+      }
+    });
+
+    return count;
+  }
+
+  ball(computer, user) {
+    let count = 0;
+    computer.forEach((num, i) => {
+      if (num === +user[i]) {
+        count += 1;
+      }
+    });
+
+    return count;
+  }
+
+  judgement(computer, user) {
+    let strike = this.strike(computer, user);
+    let ball = this.ball(computer, user);
+
+    if (strike === 3) {
+      return `3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료`;
+    }
+    if (strike === 0 && ball === 0) return `낫싱`;
+    if (strike === 0) return `${strike}스트라이크`;
+    if (ball === 0) return `${ball}볼`;
+
+    return `${ball}볼 ${strike}스트라이크`;
+  }
+
   userInputHandler(computer) {
     MissionUtils.Console.readLine("숫자를 입력해주세요 :", (input) => {
-      MissionUtils.Console.print(`테스트 : ${computer} ,${input}`);
+      MissionUtils.Console.print(this.judgement(computer, input));
     });
   }
 
