5d941de9f458bc6d5d867003eb2448fdf2561c4e
2pandi <coder.2pandi@gmail.com>
2022-11-06 22:14:07 +0900

    feat: add function to make random unique numbers

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-const { Console } = require("@woowacourse/mission-utils");
+const { Console, Random } = require("@woowacourse/mission-utils");
 
 class App {
   play() {
     Console.print("숫자 야구 게임을 시작합니다.");
+    this.makeRandomNumber();
+  }
+
+  makeRandomNumber() {
+    const correctNumbers = new Set();
+    while (correctNumbers.size < 3) {
+      correctNumbers.add(Random.pickNumberInRange(1, 9));
+    }
   }
 }
 
