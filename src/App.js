91dd45a641f99c0c208d039358bf0eefd5671592
김아현 <dhflxhdxhd@gmail.com>
2022-11-06 17:28:18 +0900

    Feat : strike, ball 개수 세기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 const MissionUtils = require("@woowacourse/mission-utils");
-// USER 수 입력 
-const userNum = "123" 
+// USER 수 입력
 
 function makeRandom() {
   let answer = new Array();
- 
+
   while (answer.length < 3) {
     let number = MissionUtils.Random.pickNumberInRange(1, 9);
 
@@ -12,7 +11,27 @@ function makeRandom() {
       answer.push(number);
     }
   }
+
+  console.log(`answer ${answer}`);
   return answer;
 }
 
+function countStrikeAndBall() {
+  let userNum = "123";
+  let randomNumber = makeRandom();
+  let strike = 0;
+  let ball = 0;
 
+  randomNumber.forEach((item, index) => {
+    if (item === userNum[index]) {
+      strike++;
+    } else if (userNum.includes(item)) {
+      ball++;
+    }
+    console.log(item);
+  });
+
+  return `${strike} and ${ball}`;
+}
+
+console.log(countStrikeAndBall());
