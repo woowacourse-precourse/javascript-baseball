593a1c878f01aa78e1e6381ea5014ba8999e2b37
cloudedpanther <rickeykoh@ajou.ac.kr>
2022-11-07 01:24:04 +0900

    feat(method): numberToArray
    Add numberToArray method to Parse class

--- src/Parse.js
+++ src/Parse.js
@@ -1,5 +1,10 @@
 class Parse {
-  static numberToArray(number) {}
+  static numberToArray(number) {
+    return number
+      .toString()
+      .split("")
+      .map((digit) => parseInt(digit));
+  }
 }
 
 module.exports = Parse;
