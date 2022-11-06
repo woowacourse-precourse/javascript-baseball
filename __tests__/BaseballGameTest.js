96832de2e3c62a147d73b54ef445dae2f4dd7957
Deemou <4106243@naver.com>
2022-11-06 22:05:19 +0900

    test: initGame() 테스트 케이스 추가

--- __tests__/BaseballGameTest.js
+++ __tests__/BaseballGameTest.js
@@ -195,4 +195,27 @@ describe("숫자 야구 게임", () => {
       expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
     });
   });
+
+  test("게임 종료 후 재시작", () => {
+    const randoms = [1, 3, 5, 5, 8, 9];
+    const answers = ["246", "135", "1", "597", "589", "2"];
+    const logSpy = getLogSpy();
+    const messages = [
+      "낫싱",
+      "3스트라이크",
+      "1볼 1스트라이크",
+      "3스트라이크",
+      "게임 종료",
+    ];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    const app = new App();
+    app.initGame();
+
+    messages.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
 });
