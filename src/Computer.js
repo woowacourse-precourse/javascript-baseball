2ded6cae92f51b1698a943e69611304d5cbb903f
dragonfruitlemonade <tpghks9245@gmail.com>
2022-11-06 17:49:42 +0900

    feat(Computer): 게임 성공 메세지, 성공 후 재시작 종료 메세지 출력 함수

--- src/Computer.js
+++ src/Computer.js
@@ -1,19 +1,27 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
 class Computer {
-  startMessage() {
-      MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
-   }
+    startMessage() {
+        MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    }
   
-  pickedNum() {
-    let correctAnswer = new Set();
-      
-    while (correctAnswer.size < 3) {
-      correctAnswer.add(MissionUtils.Random.pickNumberInRange(1, 9));
+    successMessage() {
+        MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 
-    return Array.from(correctAnswer);
-  }
+    chooseMessage() {
+        MissionUtils.Console.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
+    }
+
+    pickedNum() {
+        let correctAnswer = new Set();
+      
+    while (correctAnswer.size < 3) {
+        correctAnswer.add(MissionUtils.Random.pickNumberInRange(1, 9));
+        }
+
+        return Array.from(correctAnswer);
+    }
 }
 
 module.exports = Computer;
