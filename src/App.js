a9d582620237944d89ec10c5c6447c37653a5b1e
bae-sh <hyeon1293@gmail.com>
2022-11-07 04:28:31 +0900

    chore : this.replay() 수정

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ class App {
 
       if (IS_ANSWER) {
         this.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
-        replay();
+        this.replay();
       } else {
         const BALL_STRIKTE_CONTENT = `${ball === 0 ? '' : `${ball}볼 `}${
           strike === 0 ? '' : `${strike}스트라이크`
@@ -58,13 +58,14 @@ class App {
       if (findIndex === idx) strike += 1;
       else if (findIndex !== -1) ball += 1;
     });
+
     return { strike, ball };
   }
 
   replay() {
-    this.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.', number => {
-      if (number === 1) this.start();
-      else if (number === 2) MissionUtils.Console.close();
+    this.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n', number => {
+      if (number === '1') this.start();
+      else if (number === '2') MissionUtils.Console.close();
       else throw Error('잘못된 입력입니다.');
     });
   }
