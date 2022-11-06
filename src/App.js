05659dae74b408f23f6fbff5b0116c7078d99348
jaesung4231 <jaesung4231@naver.com>
2022-11-06 22:47:38 +0900

    README 작성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+const MissionUtils = require("@woowacourse/mission-utils");
 class App {
-  play() {}
-}
 
-module.exports = App;
+  start(){
+    MissionUtils.Console.readLine('숫자 야구 게임을 시작합니다', (answer) => {
+    console.log(`닉네임: ${answer}`);
+    });
+  }
+
+
+
+  
+}
+const app = new App
+app.play()
+// module.exports = App;
