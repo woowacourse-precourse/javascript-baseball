8d257c5b7de1f7ad14f17cbe48e1748a3821bed6
seongminn <berry9901@gmail.com>
2022-11-07 01:28:20 +0900

    fix(BaseballGame): getResult 함수에서 불리언 값을 반환하여 askRestart 함수 실행을 결정하도록 수정

--- src/game/BaseballGame.js
+++ src/game/BaseballGame.js
@@ -28,24 +28,30 @@ class BaseballGame {
 
     const { ball, strike } = this.calculator.calcScore(input, randomNumber);
 
-    this.printResult(ball, strike);
+    return this.printResult(ball, strike);
   }
 
   printResult(ball, strike) {
-    if (!ball && !strike) return Console.print(GAME_MESSAGE.nothing);
+    if (!ball && !strike) {
+      Console.print(GAME_MESSAGE.nothing);
 
-    if (strike === NUMBER_LENGTH) {
-      this.isStop = true;
-      return Console.print(`${strike}스트라이크`);
+      return false;
     }
 
-    return Console.print(`${ball}볼 ${strike}스트라이크`);
+    if (strike === NUMBER_LENGTH) {
+      Console.print(`${strike}스트라이크`);
+
+      return true;
+    }
+
+    Console.print(`${ball}볼 ${strike}스트라이크`);
+
+    return false;
   }
 
   askRestart() {
     Console.readLine(GAME_MESSAGE.restart, (answer) => {
       if (answer === '1') {
-        this.isStop = false;
         this.startGame();
       }
     });
@@ -56,11 +62,13 @@ class BaseballGame {
 
     while (!this.isStop) {
       Console.readLine(GAME_MESSAGE.input, (input) => {
-        this.getResult(input, randomNumber);
+        const isCorrect = this.getResult(input, randomNumber);
+
+        if (isCorrect) {
+          this.askRestart();
+        }
       });
     }
-
-    this.askRestart();
   }
 }
 
