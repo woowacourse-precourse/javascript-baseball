8ce7fe9c712637c15994bd182d728de76e1197a9
lsy20140 <lsy20140@naver.com>
2022-11-06 16:08:02 +0900

    feat: 사용자로부터 값 입력 받는 userInput 메소드 생성

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 class BaseballGamee {
+  
   computerInput(){
     const MissionUtils = require("@woowacourse/mission-utils");
 
@@ -7,21 +8,37 @@ class BaseballGamee {
 
     return computerInput;
   }
-  
 
-  play(){
+  checkUserInput(userInput){
+    const isValidArr = userInput.map(e => e=Number.isNaN(e));
+    if(userInput.length!==3) return false;
+    if(isValidArr.includes(true)) return false;
+    if(new Set(userInput).size!==3) return false;
+    if(userInput.includes(0)) return false;
+  }
+
+
+  userInput(){
     const MissionUtils = require("@woowacourse/mission-utils");
     let userInput = [];
-    console.log("숫자 야구 게임을 시작합니다.");
     MissionUtils.Console.readLine ('line : ', (answer) => {
       userInput = answer.toString().split('').map(e => e = parseInt(e));
       const isValid = this.checkUserInput(userInput);
-      MissionUtils.Console.close();
-
+      
+      if(isValid==false){
+        throw "잘못된 값을 입력했습니다.";
+      }
     });
+    return userInput;
+  }
 
+  
+
+  play(){
     
-
+    console.log("숫자 야구 게임을 시작합니다.");
+    
+    const userInput = this.userInput();
   }
 }
 
