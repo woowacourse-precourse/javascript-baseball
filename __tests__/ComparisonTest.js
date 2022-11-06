f4f218d2107c67835bd25869ca6ba372a2f88d8f
unsnruu <unsnruu@gmail.com>
2022-11-06 23:07:08 +0900

    test : 예외처리에 대한 테스트 작성

--- __tests__/ComparisonTest.js
+++ __tests__/ComparisonTest.js
@@ -31,7 +31,31 @@ describe("숫자 값을 비교하기", () => {
 });
 
 describe("예외 처리 테스트", () => {
-  test("사용자가 숫자가 아닌 값을 입력한 경우", () => {});
-  test("사용자가 범위를 초과한 입력을 한 경우", () => {});
-  test("사용자가 같은 값을 가진 수를 입력했다면", () => {});
+  test("사용자가 숫자가 아닌 값을 입력한 경우", () => {
+    const answer = ["abc"];
+    mockQuestions(answer);
+
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow("숫자가 아닙니다.");
+  });
+  test("사용자가 범위를 초과한 입력을 한 경우", () => {
+    const answer = ["12345"];
+    mockQuestions(answer);
+
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow("세자리의 수를 입력해주세요.");
+  });
+  test("사용자가 같은 값을 가진 수를 입력했다면", () => {
+    const answer = ["111"];
+    mockQuestions(answer);
+
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow("반복되는 숫자가 있습니다.");
+  });
 });
