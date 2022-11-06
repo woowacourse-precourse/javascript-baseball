db8618196bd3b5c995c6da10ad4d71d4ac67dfae
Jungkyu Yang <67031524+jungkyuYang@users.noreply.github.com>
2022-11-07 04:41:59 +0900

    feat: add 사용자의 값을 입력받는 기능
    MissionUtils 라이브러리에서 제공하는 Console.readLine() 활용

--- src/App.js
+++ src/App.js
@@ -6,8 +6,16 @@ class App {
   play() {
     Console.print("숫자 야구 게임을 시작합니다.");
 
-    return;
+    return this.getUserInput();
   }
+
+  getUserInput() {
+    Console.readLine("숫자를 입력해주세요 :", (answer) => {
+      return this.checkAnswer(answer);
+    });
+  }
+
+  checkAnswer(userNumber) {}
 
   createComputerNumber() {
     const computer = [];
