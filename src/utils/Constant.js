fa23060804ad9e4d3dbd8a5edef5e800fa5f3f81
KUD <kimud6003@gmail.com>
2022-11-06 10:45:11 +0900

    ✨ Feature: Constant 정의 - 문자열 및 정적 콘텐츠 지정

---
+++ src/utils/Constant.js
@@ -0,0 +1,21 @@
+const RESTART = '1';
+const MSG = {
+  START: '숫자 야구 게임을 시작합니다.',
+  INPUT: '숫자를 입력해주세요 : ',
+  END: '3개의 숫자를 모두 맞히셨습니다! 게임 종료',
+  SELECT: '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n',
+};
+
+const BALL_STATUS = {
+  BALL: '볼',
+  STRIKE: '스트라이크',
+};
+
+const NOTHING = '낫싱';
+
+module.exports = {
+  RESTART,
+  MSG,
+  BALL_STATUS,
+  NOTHING,
+};
