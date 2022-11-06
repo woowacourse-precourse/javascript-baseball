67c2e5223080ce1dd3d562a47960fdf379a36861
seunghoria <102285042+seunghoria@users.noreply.github.com>
2022-11-06 23:22:29 +0900

    test: 스트라이크와 볼 개수 카운트 기능에 대한 테스트 코드 작성

---
+++ __tests__/CountInputTest.js
@@ -0,0 +1,36 @@
+const App = require("../src/App.js");
+
+describe("스트라이크와 볼 갯수 카운트 기능 테스트", () => {
+  test(`countInput 볼 카운트 확인`, () => {
+    const app = new App();
+    const computerInput = [7, 8, 9];
+    const userInput = [9, 7, 8];
+    const result = [3, 0];
+
+    const methodResult = app.countInput(computerInput, userInput);
+
+    expect(methodResult).toEqual(result);
+  });
+
+  test(`countInput 스트라이크 카운트 확인`, () => {
+    const app = new App();
+    const computerInput = [7, 8, 9];
+    const userInput = [7, 1, 2];
+    const result = [0, 1];
+
+    const methodResult = app.countInput(computerInput, userInput);
+
+    expect(methodResult).toEqual(result);
+  });
+
+  test(`countInput 스트라이크,볼 카운트 확인`, () => {
+    const app = new App();
+    const computerInput = [7, 8, 9];
+    const userInput = [7, 2, 8];
+    const result = [1, 1];
+
+    const methodResult = app.countInput(computerInput, userInput);
+
+    expect(methodResult).toEqual(result);
+  });
+});
