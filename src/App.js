fdad2bc539d00c0d6f7f55541fe52b5441ca5556
이석준 <sukjune96@gmail.com>
2022-11-06 18:50:12 +0900

    fix: 정답 시 메세지 출력 수정

--- src/App.js
+++ src/App.js
@@ -23,6 +23,7 @@ class App {
   }
   checkGame(score, answer) {
     if (score.strike === 3) {
+      MissionUtils.Console.print("3스트라이크");
       MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
       this.restart();
     } else {
