f6d3b376bec5f32004ab3dc3a6118a9072add815
min <hororonoodle@gmail.com>
2022-11-07 00:26:18 +0900

    test  : 사용자 입력값 변수명을 answer 에서 testUserInput으로 변경 및 입력 오류 방지를 위한 mockQuestions 추가

--- __tests__/S2_Test.js
+++ __tests__/S2_Test.js
@@ -18,6 +18,9 @@ describe('[S2] 유저는 올바른 게임 진행을 통해 재미를 느끼기 �
     const lastRange = 9;
     const length = 3;
 
+    const testUserInput = ['123'];
+    mockQuestions(testUserInput);
+
     const app = new App();
     app.play();
 
@@ -34,8 +37,8 @@ describe('[S2] 유저는 올바른 게임 진행을 통해 재미를 느끼기 �
     });
   });
   test('[T2-2] 사용자로 부터 1 부터 9 까지 서로 다른 세자리 수를 입력 받는 기능', () => {
-    const answer = ['123'];
-    mockQuestions(answer);
+    const testUserInput = ['123'];
+    mockQuestions(testUserInput);
     const app = new App();
     app.play();
     expect(app.userInput).toBe('123');
