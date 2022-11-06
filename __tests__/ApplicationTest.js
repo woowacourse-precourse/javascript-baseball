097beb36669a37b349862d53c8414e78090f0267
minjaeKim <smallkdb@gmail.com>
2022-11-06 17:10:05 +0900

    style(test): format code to convention
    요구 사항에 맞는 컨벤션으로 코드 형식을 변경하였습니다.
    - ApplicationTest.js 파일의 코드 형식을 변경하였습니다.

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -1,62 +1,56 @@
-const App = require("../src/App");
-const MissionUtils = require("@woowacourse/mission-utils");
+const App = require('../src/App');
+const MissionUtils = require('@woowacourse/mission-utils');
 
 const mockQuestions = (answers) => {
-  MissionUtils.Console.readLine = jest.fn();
-  answers.reduce((acc, input) => {
-    return acc.mockImplementationOnce((question, callback) => {
-      callback(input);
-    });
-  }, MissionUtils.Console.readLine);
+	MissionUtils.Console.readLine = jest.fn();
+	answers.reduce((acc, input) => {
+		return acc.mockImplementationOnce((question, callback) => {
+			callback(input);
+		});
+	}, MissionUtils.Console.readLine);
 };
 
 const mockRandoms = (numbers) => {
-  MissionUtils.Random.pickNumberInRange = jest.fn();
-  numbers.reduce((acc, number) => {
-    return acc.mockReturnValueOnce(number);
-  }, MissionUtils.Random.pickNumberInRange);
+	MissionUtils.Random.pickNumberInRange = jest.fn();
+	numbers.reduce((acc, number) => {
+		return acc.mockReturnValueOnce(number);
+	}, MissionUtils.Random.pickNumberInRange);
 };
 
 const getLogSpy = () => {
-  const logSpy = jest.spyOn(MissionUtils.Console, "print");
-  logSpy.mockClear();
-  return logSpy;
+	const logSpy = jest.spyOn(MissionUtils.Console, 'print');
+	logSpy.mockClear();
+	return logSpy;
 };
 
-describe("숫자 야구 게임", () => {
-  test("게임 종료 후 재시작", () => {
-    const randoms = [1, 3, 5, 5, 8, 9];
-    const answers = ["246", "135", "1", "597", "589", "2"];
-    const logSpy = getLogSpy();
-    const messages = [
-      "낫싱",
-      "3스트라이크",
-      "1볼 1스트라이크",
-      "3스트라이크",
-      "게임 종료",
-    ];
+describe('숫자 야구 게임', () => {
+	test('게임 종료 후 재시작', () => {
+		const randoms = [1, 3, 5, 5, 8, 9];
+		const answers = ['246', '135', '1', '597', '589', '2'];
+		const logSpy = getLogSpy();
+		const messages = ['낫싱', '3스트라이크', '1볼 1스트라이크', '3스트라이크', '게임 종료'];
 
-    mockRandoms(randoms);
-    mockQuestions(answers);
+		mockRandoms(randoms);
+		mockQuestions(answers);
 
-    const app = new App();
-    app.play();
+		const app = new App();
+		app.play();
 
-    messages.forEach((output) => {
-      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
-    });
-  });
+		messages.forEach((output) => {
+			expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+		});
+	});
 
-  test("예외 테스트", () => {
-    const randoms = [1, 3, 5];
-    const answers = ["1234"];
+	test('예외 테스트', () => {
+		const randoms = [1, 3, 5];
+		const answers = ['1234'];
 
-    mockRandoms(randoms);
-    mockQuestions(answers);
+		mockRandoms(randoms);
+		mockQuestions(answers);
 
-    expect(() => {
-      const app = new App();
-      app.play();
-    }).toThrow();
-  });
+		expect(() => {
+			const app = new App();
+			app.play();
+		}).toThrow();
+	});
 });
