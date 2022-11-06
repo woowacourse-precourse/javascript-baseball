f8ac2e3911418bc5b50fa74c391c8255ce2ede78
ryuna <anottrx@gmail.com>
2022-11-07 00:32:26 +0900

    refactor: Airbnb 자바스크립트 스타일 가이드 적용

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-const MissionUtils = require("@woowacourse/mission-utils");
+const MissionUtils = require('@woowacourse/mission-utils');
+
+const getRandomNumber = () => {
+  const computer = [];
+  while (computer.length < 3) {
+    const number = MissionUtils.Random.pickNumberInRange(1, 9);
+    if (!computer.includes(number)) computer.push(number);
+  }
+  return computer;
+};
 
 class App {
   play() {
     // MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
     let computerNumber = getRandomNumber();
-    let guessNumber;
-    while (true) {
+    const IS_PLAYING = true;
+    while (IS_PLAYING) {
+      let guessNumber;
       // MissionUtils.Console.print("숫자를 입력해주세요 : ");
-      MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (x) => {
+      MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (x) => {
         guessNumber = x;
       });
-      guessNumber = guessNumber.split("");
+      this.guessNumber = guessNumber.split('');
       // MissionUtils.Console.print(
       //   `guessNumber는 ${guessNumber}, computerNumber는 ${computerNumber}`
       // );
       MissionUtils.Console.close();
       try {
         if (guessNumber.length !== 3) {
-          throw new Error("예외");
+          throw new Error('예외');
         }
       } catch (e) {
         // console.log();
@@ -30,7 +40,7 @@ class App {
       // ).length;
       let strike = 0;
       computerNumber.forEach((num, index) => {
-        if (num.toString() === guessNumber[index]) strike++;
+        if (num.toString() === guessNumber[index]) strike += 1;
         // MissionUtils.Console.print(
         //   `num ${num}, guessNumber[index] ${guessNumber[index]}`
         // );
@@ -39,17 +49,16 @@ class App {
       let ball = 0;
       computerNumber.forEach((computerNum) => {
         if (guessNumber.includes(computerNum.toString())) {
-          ball++;
+          ball += 1;
         }
       });
       ball -= strike;
 
       // MissionUtils.Console.print(`strike ${strike}, ball ${ball}`);
-      if (strike > 0 && ball > 0)
-        MissionUtils.Console.print(`${ball}볼 ${strike}스트라이크`);
+      if (strike > 0 && ball > 0) MissionUtils.Console.print(`${ball}볼 ${strike}스트라이크`);
       else if (strike > 0) MissionUtils.Console.print(`${strike}스트라이크`);
       else if (ball > 0) MissionUtils.Console.print(`${ball}볼`);
-      else MissionUtils.Console.print(`낫싱`);
+      else MissionUtils.Console.print('낫싱');
 
       if (strike === 3) {
         // MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
@@ -57,14 +66,14 @@ class App {
         // "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
         // );
         let input;
-        MissionUtils.Console.readLine("", (x) => {
+        MissionUtils.Console.readLine('', (x) => {
           input = x;
         });
         MissionUtils.Console.close();
         // MissionUtils.Console.print(`input ${input}`);
 
-        if (input.toString() === "2") {
-          MissionUtils.Console.print(`게임 종료`);
+        if (input.toString() === '2') {
+          MissionUtils.Console.print('게임 종료');
           break;
         } else {
           computerNumber = getRandomNumber();
@@ -75,14 +84,5 @@ class App {
     }
   }
 }
-
-const getRandomNumber = () => {
-  const computer = [];
-  while (computer.length < 3) {
-    const number = MissionUtils.Random.pickNumberInRange(1, 9);
-    if (!computer.includes(number)) computer.push(number);
-  }
-  return computer;
-};
 
 module.exports = App;
