b94a420dd5ccc73592869f31e55a21e508f16e58
TeusLee <xodn1311@gmail.com>
2022-11-06 16:47:12 +0900

    add counting strike no function

--- src/App.js
+++ src/App.js
@@ -12,6 +12,18 @@ const ball_cnt = (input_num, computer_num) => {
   return result_cnt
 }
 
+const strike_cnt = (input_num, computer_num) => {
+  let input_num_str = String(input_num)
+  let input_computer_str = String(computer_num)
+  result_cnt = 0
+  for (let i = 0; i < input_computer_str.length; i++){
+      if (input_num_str[i]===input_computer_str[i]){
+          result_cnt += 1
+      }
+  }
+  return result_cnt
+}
+
 class App {
   play() {}
 }
