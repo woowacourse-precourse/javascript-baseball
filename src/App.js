8906d5ab629819cdac64baa2ad25a268dbb9c4be
leeseooo <leeseoooo@naver.com>
2022-11-06 17:18:24 +0900

    feat: 게임 초기화 함수 추가

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ const MissionUtils = require("@woowacourse/mission-utils");
 class App {
   play() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    this.init();
+  }
+  init() {
+    this.computer = [];
+    this.user = [];
+    this.result = "";
+    this.isCorrect = false;
+    this.getComputerNumber();
   }
   getComputerNumber() {
     while (this.computer.length < 3) {
@@ -15,5 +23,7 @@ class App {
     MissionUtils.Console.print(this.computer);
   }
 }
+const app = new App();
+app.play();
 
 module.exports = App;
