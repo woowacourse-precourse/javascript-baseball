401abbe17078fc5b463b39745637043e67ac3be3
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2022-11-07 01:33:51 +0900

    feat(Render): 게임을 시작 할 때 사용자에게 출력할 멘트 함수 추가

--- src/Render.js
+++ src/Render.js
@@ -3,6 +3,10 @@ const MissionUtils = require("@woowacourse/mission-utils");
 class Render {
   constructor() {}
 
+  startment() {
+    MissionUtils.Console.print("게임을 시작합니다!⚾️");
+  }
+
   getUser() {
     return new Promise((resolve, reject) => {
       MissionUtils.Console.readLine("숫자를 입력해주세요", (number) => {
