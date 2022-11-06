55d4493ab7a3a6510d5ec3b04d72da7d8a60691a
unqeye <salyuan@naver.com>
2022-11-06 19:53:03 +0900

    fix: 게임 종료 메시지 출력 후 개행

--- src/App.js
+++ src/App.js
@@ -88,7 +88,7 @@ class App {
 
   processNewGame() {
     MissionUtils.Console.readLine(
-      '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.',
+      '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n',
       (input) => {
         this.newGame = input;
         this.checkNewGame();
