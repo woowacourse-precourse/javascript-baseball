3c99bcb8c11dcb6cd0f067ceeb235b9489a629f2
2je0 <bulljy@naver.com>
2022-11-07 03:54:23 +0900

    ✨ feat: play 함수 구현

--- src/App.js
+++ src/App.js
@@ -6,7 +6,11 @@ const BALL = 0;
 const END = '2';
 
 class App {
-  play() {}
+  play() {
+    Console.print('숫자 야구 게임을 시작합니다.');
+    const answer = this.randomGenerator();
+    this.startQuery(answer);
+  }
 
   startQuery(answer) {
     Console.readLine('숫자를 입력해주세요 : ', (query) => {
