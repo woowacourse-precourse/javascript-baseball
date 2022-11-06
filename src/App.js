340d780478af2d27b98ef43cbb8ad45d426e6708
min <hororonoodle@gmail.com>
2022-11-06 23:09:00 +0900

    feat : [T2-1] 게임 정답 생성 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 const GameMessage = require('./Constants/gameMessage');
 class App {
-  play() {
+  constructor() {
+    this.userInput = '';
+    this.answer = '';
+    this.result = '';
+  }
+
+  printWelcomeMessage() {
     MissionUtils.Console.print(GameMessage.WELCOME_MESSAGE);
-    MissionUtils.Console.readLine(GameMessage.QUESTION_MESSAGE, (userInput) => {
-      return userInput;
-    });
+  }
+
+  answerGenerator() {
+    const firstRange = 1;
+    const lastRange = 9;
+    const length = 3;
+    const randomNumber = MissionUtils.Random.pickUniqueNumbersInRange(
+      firstRange,
+      lastRange,
+      length
+    );
+    this.answer = randomNumber.join('');
+  }
+
+  play() {
+    this.printWelcomeMessage();
+    this.answerGenerator();
   }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
