e08ce06f6e3e87e52661398eed71ce293bbb855f
dladncks1217 <dlaxodud1217@gmail.com>
2022-11-06 15:42:38 +0900

    refac: features 목록 제거

--- __tests__/featureTest.js
+++
@@ -1,8 +0,0 @@
-const startGameAndMakeRandomNumber = require("../src/features/startGameAndMakeRandomNumber");
-
-describe("게임 동작 기능 테스트", () => {
-  test("게임 시작 기능 테스트", () => {
-    expect(startGameAndMakeRandomNumber()).toBeGreaterThan(100);
-    expect(startGameAndMakeRandomNumber()).toBeLessThan(1000);
-  });
-});
