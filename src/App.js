8832beb4611a30b1344a2dd96821de807b2774a2
yuu <22yuu@naver.com>
2022-11-06 18:10:43 +0900

    feat: 입력에 대한 결과 출력 및 게임 종료 후 재시작 / 종료 기능 구현

--- src/App.js
+++ src/App.js
@@ -8,12 +8,12 @@ const QuestionText = Object.freeze({
   endText: "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
   redoText: "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
 });
+
 class App {
   constructor() {
     this.io = MissionUtils.Console;
     this.io.print(QuestionText.startText);
     this.userInputArray = [];
-    this.isFinish = false;
     this.target = this.setTarget();
   }
 
@@ -25,23 +25,56 @@ class App {
     this.io.readLine(text, callback.bind(this));
   }
 
-  setTarget() {
-    const computer = [];
-    while (computer.length < 3) {
-      const number = MissionUtils.Random.pickNumberInRange(1, 9);
-      if (!computer.includes(number)) computer.push(number);
-    }
-    console.log(computer);
-    return computer;
-  }
-
   start() {
     this.onInput(QuestionText.inputText, this.onGame);
   }
 
+  finish() {
+    try {
+      this.io.print(QuestionText.endText);
+      this.onInput(QuestionText.redoText, this.isRestart);
+      // this.io.close();
+    } catch {
+      new Error("경고:게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
+  }
+
+  onGame(input) {
+    this.validation(parseInt(input));
+    // console.log(this.userInputArray);
+
+    let ball = this.countBall();
+    let strike = this.countStrike();
+
+    if (strike === LENGTH) {
+      this.io.print(this.showResult(ball, strike));
+      this.finish();
+    } else {
+      this.io.print(this.showResult(ball, strike));
+      this.onInput(QuestionText.inputText, this.onGame);
+    }
+  }
+
+  isRestart(input) {
+    let value = parseInt(input);
+    if (isNaN(value)) throw Error();
+
+    switch (value) {
+      case 1:
+        this.target = this.setTarget();
+        this.onInput(QuestionText.inputText, this.onGame);
+        break;
+      case 2:
+        this.io.close();
+        break;
+      default:
+        throw new Error("Not valid value");
+    }
+  }
+
   // 유효성 검사
   validation(input) {
-    if (isNaN(input)) throw new Error("정수 값을 입력해주세요!");
+    if (isNaN(input)) new Error("정수 값을 입력해주세요!");
 
     const len = Math.ceil(Math.log10(input + 1));
     if (len !== 3) throw new Error("서로 다른 3자리의 수를 입력해주세요!");
@@ -57,7 +90,7 @@ class App {
 
     for (let i = 0; i < LENGTH; i++) {
       let idx = this.userInputArray[i];
-      if (checkArray[idx]) throw new Error("중복이 있습니다!");
+      if (checkArray[idx]) throw new Error("중복이 있습니다.");
 
       checkArray[idx] = true;
     }
@@ -74,22 +107,17 @@ class App {
     return array;
   }
 
-  // Game
-  onGame(input) {
-    // this.io.print(input);
-    this.validation(parseInt(input));
-
-    let ball = this.countBall();
-    let strike = this.countStrike();
-
-    if (strike === LENGTH) {
-      this.finish();
-    } else {
-      this.io.print(this.showResult(ball, strike));
-      this.onInput(QuestionText.inputText, this.onGame);
+  setTarget() {
+    const computer = [];
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) computer.push(number);
     }
+    // console.log(computer);
+    return computer;
   }
 
+  // game 관련
   countStrike() {
     let count = 0;
 
@@ -104,17 +132,16 @@ class App {
 
     for (let i = 0; i < LENGTH; i++) {
       if (
-        this.target[i] !== this.userInputArray[i] &&
-        this.target.includes(this.userInputArray[i])
+        this.target.includes(this.userInputArray[i]) &&
+        this.target[i] !== this.userInputArray[i]
       )
         count++;
-
-      return count;
     }
+    return count;
   }
 
   showResult(ball, strike) {
-    if (ball === 0 && strike === 0) return "낫싱";
+    if (ball == 0 && strike == 0) return "낫싱";
     else if (ball > 0 && strike == 0) return `${ball}볼`;
     else if (ball == 0 && strike > 0) return `${strike}스트라이크`;
     else return `${ball}볼 ${strike}스트라이크`;
