66296a332c3e78857f07371c677a3e737ad9e715
hyukzz <ri_chard@naver.com>
2022-11-06 16:54:08 +0900

    feat: 상황별 결과 값 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import useBall from "./utils/useBall";
+import useStrike from "./utils/useStrike";
+
 class App {
-  play() {}
+	static play(cpuInput, userInput) {
+		if (cpuInput === userInput) return "성공";
+
+		const ball = useBall(cpuInput, userInput);
+		const strike = useStrike(cpuInput, userInput);
+
+		if (ball === 0 && strike === 0) return "낫싱";
+
+		let result = "";
+
+		if (ball) result += `${ball}}볼`;
+		if (strike) result += `${strike}스트라이크`;
+
+		return result;
+	}
 }
 
 module.exports = App;
