42045903e81032d185c7fa70a5d9686f25eadd63
devMingu <rooster100@naver.com>
2022-11-06 15:31:26 +0900

    refactor: 사용자 값 받아오는 함수

--- src/App.js
+++ src/App.js
@@ -1,44 +1,99 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
-
 class App {
-
-  constructor () {}
+  constructor() {}
 
   play() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
 
-    let user = this.getUserInput();
     let computer = this.getComputerInput();
+    this.getUserInput(computer);
+  }
+
+  getUserInput(computer) {
+    MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (userValue) => {
+      let isUserInputValid = this.detectError(userValue);
+
+      if (isUserInputValid) { this.compareUserAndComputer(userValue, computer); }
+    });
+  }
+
+  compareUserAndComputer(userValue, computer) {
+    let [strike, ball] = this.countStrikeAndBall(userValue, computer);
+    if (this.isStrikeOut(strike, ball)) {
+      this.reGame();
+    } else {
+      this.getUserInput(computer);
+    }
+  }
+
+  countStrikeAndBall(user, computer) {
+    user = [...user];
+    computer = [...computer];
+
+    let idx = 0;
+    let strike = 0;
+    let ball = 0;
+    user.forEach((element) => {
+      if (computer.includes(element)) {
+        if (element === computer[idx]) {
+          strike += 1;
+        } else {
+          ball += 1;
+        }
+      }
+      idx += 1;
+    });
+    return [strike, ball];
+  }
+
+  reGame() {
+    MissionUtils.Console.readLine(
+      "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. \n",
+      (userValue) => {
+        if (userValue === "1") {
+          let computer = this.getComputerInput();
+          this.getUserInput(computer);
+        } else if (userValue === "2") {
+          MissionUtils.Console.close();
+        } else {
+          throw new Error("유효하지 않은 값이 입력되었습니다.");
+        }
+      }
+    );
   }
 
   detectError(user) {
-    if(this.detectStringError(user)) {
+    user = this.removeRepeated(user);
+
+    if (this.detectStringError(user)) {
       if (user.length != 3) {
         throw new Error("유효하지 않은 값이 입력되었습니다.");
       }
     }
+    return 1;
   }
+
   detectStringError(user) {
     user = [...user];
-    user.forEach(element => {
-      if (!(element >= '1' && element <= '9')) { 
-        throw new Error("유효하지 않은 값이 입력되었습니다."); 
+    user.forEach((element) => {
+      if (!(element >= "1" && element <= "9")) {
+        throw new Error("유효하지 않은 값이 입력되었습니다.");
       }
     });
     return 1;
   }
 
-  detectReStartError(user) {
-    if (!(user === '1' || user === '2')) {
-      throw new Error("유효하지 않은 값이 입력되었습니다. 종료오류");
-    }
+  removeRepeated(user) {
+    const set = new Set([...user]);
+    user = [...set];
+    return user;
   }
 
   getComputerInput() {
     let computerInput = [];
     while (computerInput.length < 3) {
-      const randomNumber = MissionUtils.Random.pickNumberInRange(1,9);
+      const randomNumber = MissionUtils.Random.pickNumberInRange(1, 9);
       if (!computerInput.includes(randomNumber)) {
         computerInput.push(randomNumber);
       }
@@ -46,59 +101,37 @@ class App {
     return computerInput.join("");
   }
 
-  compareUserAndComputer(computer) {
-    MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (userValue) => {
-        let isUserInputValid = this.detectError(userValue);
-
-        if (isUserInputValid) {
-            let [strike, ball] = this.countStrikeAndBall(userValue, computer);
-            if (this.isStrikeOut(strike, ball)){
-                this.reGame();
-            }
-            else{
-                this.compareUserAndComputer(computer);
-            }
-        }
-
-    }); 
-  }
-
-  countStrikeAndBall(user, computer) {
-    user = [...user];
-    computer = [...computer];
-
-    let i = 0;
-    let strike = 0;
-    let ball = 0;
-    user.forEach(element => {
-      if (computer.includes(element)){
-        if (element === computer[i]) { strike += 1 }
-        else { ball += 1}
-      }
-      i += 1;
-    });
-    return [strike, ball];
-  }
-  
-  isStrikeOut(user, computer) {  
-    if (strike === 0 && ball === 0) { MissionUtils.Console.print("낫싱")};
-    if (strike === 1 && ball === 0) { MissionUtils.Console.print("1스트라이크")};
-    if (strike === 2 && ball === 0) { MissionUtils.Console.print("2스트라이크")};
-    if (strike === 3 && ball === 0) { 
+  isStrikeOut(strike, ball) {
+    if (strike === 0 && ball === 0) {
+      MissionUtils.Console.print("낫싱");
+    }
+    if (strike === 1 && ball === 0) {
+      MissionUtils.Console.print("1스트라이크");
+    }
+    if (strike === 2 && ball === 0) {
+      MissionUtils.Console.print("2스트라이크");
+    }
+    if (strike === 3 && ball === 0) {
       MissionUtils.Console.print("3스트라이크");
       MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
       return 1;
-    };
-    if (strike === 0 && ball === 1) { MissionUtils.Console.print("1볼")};
-    if (strike === 0 && ball === 2) { MissionUtils.Console.print("2볼")};
-    if (strike === 1 && ball === 1) { MissionUtils.Console.print("1볼 1스트라이크")};
-    if (strike === 1 && ball === 2) { MissionUtils.Console.print("2볼 1스트라이크")};
-  
-  
+    }
+    if (strike === 0 && ball === 1) {
+      MissionUtils.Console.print("1볼");
+    }
+    if (strike === 0 && ball === 2) {
+      MissionUtils.Console.print("2볼");
+    }
+    if (strike === 1 && ball === 1) {
+      MissionUtils.Console.print("1볼 1스트라이크");
+    }
+    if (strike === 1 && ball === 2) {
+      MissionUtils.Console.print("2볼 1스트라이크");
+    }
+
     return 0;
   }
 }
-
 
 const game = new App();
 game.play();
