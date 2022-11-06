7cceee9eee9acf661e97f406374a284da08c48a5
Serin-Kim <serin9864@gmail.com>
2022-11-06 20:14:40 +0900

    feat: isValid 함수 구현
    사용자 입력 값이 유효한지 판단하는 함수를 작성함
    세 자리 숫자인지, 숫자로만 이루어져 있는지, 중복 값이 있는지 판별함

--- src/App.js
+++ src/App.js
@@ -5,9 +5,29 @@ class App {
     return MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3).join("");
   };
 
+  isDuplicated = (input) => {
+    const inputArray = input.split("");
+    const inputSet = new Set(inputArray);
+    return inputArray.length !== inputSet.size;
+  };
+
+  containsThreeNumbers = (input) => {
+    return input.length === 3;
+  };
+
+  containsOnlyNumbers = (input) => {
+    return input >= "102" && input <= "987";
+  };
+
+  isValid = (input) => {
+    if (!this.containsThreeNumbers(input) || !this.containsOnlyNumbers(input) || this.isDuplicated(input)) return false;
+    return true;
+  };
+
   getInputAndCompare = (ANSWER) => {
     MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (input) => {
       MissionUtils.Console.close();
+      if (!this.isValid(input)) throw "Invalid input!"; // TODO: or this.checkIsValid() 하고 함수 안에서 throw?
     });
   };
 
