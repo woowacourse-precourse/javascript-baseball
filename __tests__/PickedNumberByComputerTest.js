4b4a71421588fe6d793029952492fbb75f64b139
Tory <88531407+1two13@users.noreply.github.com>
2022-11-06 17:19:33 +0900

    refactor(PickedNumberByComputerTest): 중복나오도록 mocking 후 테스트

--- __tests__/PickedNumberByComputerTest.js
+++ __tests__/PickedNumberByComputerTest.js
@@ -1,9 +1,20 @@
-const { Console } = require("@woowacourse/mission-utils");
+const { Console, Random } = require("@woowacourse/mission-utils");
 const PickedNumberByComputer = require("../src/baseball/PickedNumberByComputer");
+
+const mockRandoms = (numbers) => {
+  Random.pickNumberInRange = jest.fn();
+  numbers.reduce((acc, number) => {
+    return acc.mockReturnValueOnce(number);
+  }, Random.pickNumberInRange);
+};
 
 describe("랜덤 숫자 테스트", () => {
   test("랜덤으로 뽑은 숫자 3개 반환", () => {
     // given
+    const randoms = [1, 5, 9];
+    mockRandoms(randoms);
+
+    // when
     const randomNumbers = PickedNumberByComputer.randomNumInRange();
 
     // then
@@ -13,11 +24,16 @@ describe("랜덤 숫자 테스트", () => {
 
   test("랜덤으로 뽑은 숫자 3개 중복 안됨", () => {
     // given
+    const randoms = [1, 2, 2, 2, 9];
+    mockRandoms(randoms);
+
+    // when
     const randomNumbers = PickedNumberByComputer.randomNumInRange();
     const randomNumbersSet = new Set(randomNumbers);
 
     // then
     expect(randomNumbers.length === randomNumbersSet.size).toEqual(true);
+    expect(randomNumbers).toEqual([1, 2, 9]);
   });
 });
 
