8962a69def28ba9002c56bf2dbb3a54ecbf07a82
HamZik <b1y9mn@gmail.com>
2022-11-06 16:17:06 +0900

    feat: ComputerPicksNumber 함수 추가
    컴퓨터가 서로 다른 3자리의 수를 뽑는 함수 ComputerPicksNumber를 추가하였습니다.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 class App {
   play() {
-    
+
+    function ComputerPicksNumber() {
+      let targetNumber = MissionUtils.Random.pickNumberInRange(100, 999);
+      let [a, b, c] = targetNumber.toString().split("");
+      do {
+        let targetNumber = MissionUtils.Random.pickNumberInRange(100, 999);
+        [a, b, c] = targetNumber.toString().split("");
+      } while (a === b || b === c || a === c)
+      targetNumber = a + b + c;
+      return targetNumber;
+    };
+
     function sayStart() {
       console.log('숫자 야구 게임을 시작합니다.');
     };
 
     sayStart();
+
+    let NumberPickedByComputer = ComputerPicksNumber();
+    console.log("컴퓨터 픽 넘버는?: " + NumberPickedByComputer);
   }
 }
 
