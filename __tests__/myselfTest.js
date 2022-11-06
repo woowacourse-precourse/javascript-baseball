e0a2a849b44e8cce028ec9e8530d0d575c5a87b8
GC-Park <kuyt3122@naver.com>
2022-11-06 16:57:41 +0900

    feat:사용자 입력 테스트 코드 하나 더 추가 후 적용

--- __tests__/myselfTest.js
+++ __tests__/myselfTest.js
@@ -2,7 +2,7 @@ const App = require("../src/App");
 const MissionUtils = require("@woowacourse/mission-utils");
 
 describe("숫자 야구 게임", () => {
-  test("게임 종료 후 재시작", () => {
+  test("사용자 입력 확인 테스트 코드 1", () => {
     const input = "123";
     const result = input.split("");
 
@@ -11,5 +11,15 @@ describe("숫자 야구 게임", () => {
 
     expect(result).toContain("2", "1", "3");
     expect(result).toContainEqual("1", "2", "3");
+  });
+  test("사용자 입력 확인 테스트 코드 2", () => {
+    const input = "876";
+    const result = input.split("");
+
+    const app = new App();
+    app.play();
+
+    expect(result).toContain("7", "6", "8");
+    expect(result).toContainEqual("8", "7", "6");
   });
 });
