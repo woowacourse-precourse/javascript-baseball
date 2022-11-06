8e9704bc8e5a2117cf37e22794d6a591d394c002
dladncks1217 <dlaxodud1217@gmail.com>
2022-11-06 15:54:34 +0900

    feat: 볼 스트라이크 출력 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 const { Console } = require("@woowacourse/mission-utils");
+const countBallAndStrike = require("./utils/countBallAndStrike");
 const makeRandomNumber = require("./utils/makeRandomNumber");
+const printGameMessage = require("./utils/printGameMessage");
+const verifyInputNumber = require("./utils/verifyInputNumber");
 
 class App {
   play() {
     Console.print("숫자 야구 게임을 시작합니다.");
     const randomNum = makeRandomNumber();
-    this.gameStart(randomNum);
+    this.gameStart(randomNum.toString());
   }
 
-  gameStart(answer) {}
+  gameStart(answer) {
+    Console.readLine("숫자를 입력해주세요 : ", (userNumber) => {
+      // 사용자 입력값 유효 여부 확인
+      if (!verifyInputNumber(userNumber)) {
+        this.inputErrorException();
+      }
+
+      const userResult = countBallAndStrike(userNumber, answer); // 볼 스트라이크 개수 세기
+
+      printGameMessage(userResult.strike, userResult.ball); // 볼 스트라이크 출력
+    });
+  }
+
+  inputErrorException() {
+    throw new Error("잘못된 값을 입력하셨습니다! 게임이 종료됩니다.");
+  }
 }
+
+const check = new App();
+check.play();
 
 module.exports = App;
---
+++ src/utils/printGameMessage.js
@@ -0,0 +1,23 @@
+const { Console } = require("@woowacourse/mission-utils");
+
+const printGameMessage = (strike, ball) => {
+  let resultMessage = ""; // 출력될 최종 메시지
+
+  const message = {
+    strikePrint: ["", "1스트라이크", "2스트라이크", "3스트라이크"],
+    ballPrint: ["", "1볼", "2볼", "3볼"],
+  };
+
+  if (strike === 0 && ball === 0) {
+    resultMessage = "낫싱";
+  } else {
+    resultMessage =
+      `${message.strikePrint[strike]} ${message.ballPrint[ball]}`.trim();
+  }
+
+  Console.print(resultMessage);
+
+  return resultMessage;
+};
+
+module.exports = printGameMessage;
