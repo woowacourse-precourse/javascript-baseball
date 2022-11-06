c4d11111e58db54fa0062ed633d4b0b6734ef78a
bkdragon0228 <rlaqjarb2067@naver.com>
2022-11-06 23:32:07 +0900

    style : 실행 결과 예시에 맞게 멘트 수정

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ const Exception = require('../src/Exception');
 const GAME_START = '숫자 야구 게임을 시작합니다.';
 const THREE_STRIKE = '3스트라이크';
 const GAME_OVER = '3개의 숫자를 모두 맞히셨습니다! 게임 종료';
-const REPLAY = '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.';
+const REPLAY = '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n';
 
 class App {
     constructor() {
@@ -22,7 +22,7 @@ class App {
         this.process();
     }
     process() {
-        MissionUtils.Console.readLine('숫자를 입력하세요.', (number) => {
+        MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (number) => {
             this.verification(number, 1);
             const comment = this.match(number);
             MissionUtils.Console.print(comment);
