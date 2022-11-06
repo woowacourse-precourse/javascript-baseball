88234f0753a2a26c43dd446f77c643976544ae42
Circlewee <pnj1118@naver.com>
2022-11-06 17:32:55 +0900

    feat(app): 게임 첫 실행 설정 (#6)
    1. getRandomNumber()를 사용한 정답 설정
    2. 게임 시작 문구 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+const { Console } = require('@woowacourse/mission-utils');
+const computerUtils = require('./utils/computerUtils');
+
 class App {
-  play() {}
+  answer = [];
+
+  play() {
+    this.answer = computerUtils.getRandomNumber();
+    Console.print('숫자 야구 게임을 시작합니다.');
+  }
 }
 
 module.exports = App;
