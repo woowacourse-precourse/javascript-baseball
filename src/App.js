592cf1cac08a78599f742536746197f374457693
energy1-914 <dlawldnjs5@daum.net>
2022-11-07 06:47:12 +0900

    fix: - computerNumberArray 배열 원소 문자로 변경 - Console.print 앞에 MissionUtils 붙여서 수정

--- src/App.js
+++ src/App.js
@@ -13,20 +13,21 @@ class App {
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
     MissionUtils.Console.readLine('숫자를 입력해주세요 :', (userInputNumber) => {
       this.userInputNumber = userInputNumber;
-      this.countMethod(this.userInputNumber, this.computerNumber)
+      this.countMethod();
+      this.printMessageMethod();
     });
     // MissionUtils.Console.close();
   }
 
-  countMethod(userNumber, computerNumber) {
-    const userString = this.userInputNumber.split('');
-    const computerString = this.computerNumber.split('');
+  countMethod() {
+    const userNumberArray = this.userInputNumber.split('');  
+    const computerNumberArray = this.computerNumber.map(num=>num.toString());
     
     for ( let i=0; i<3; i++ ) {
-      if ( userString[i] === computerString[i] ) {
+      if ( userNumberArray[i] === computerNumberArray[i] ) {
         this.countStrike += 1;
       }
-      else if (userString.includes(computerString[i])) {
+      else if (userNumberArray.includes(computerNumberArray[i])) {
         this.countBall += 1;
       }
     }
@@ -34,16 +35,16 @@ class App {
 
   printMessageMethod() {
     if ( this.countStrike > 0 && this.countBall === 0 ) {
-      Console.print(`${this.countStrike}스트라이크`)
+      MissionUtils.Console.print(`${this.countStrike}스트라이크`)
     }
     else if ( this.countStrike === 0 && this.countBall > 0 ) {
-      Console.print(`${this.countBall}볼`)
+      MissionUtils.Console.print(`${this.countBall}볼`)
     }
     else if (this.countStrike > 0 && this.countBall > 0) {
-      Console.print(`${this.countBall}볼 ${this.countStrike}스트라이크`)
+      MissionUtils.Console.print(`${this.countBall}볼 ${this.countStrike}스트라이크`)
     }
     else if ( this.countStrike + this.countBall === 0) {
-      Console.print('낫싱');
+      MissionUtils.Console.print('낫싱');
     }
  }   
 
@@ -53,5 +54,3 @@ module.exports = App;
 
 const test = new App;
 test.play();
-// test.countMethod();
-// test.printMessageMethod();
