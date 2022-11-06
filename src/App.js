65a222d38717436516f21901c84aaca413b7c2e2
cho seonghoon <chsida655@gmail.com>
2022-11-06 18:43:28 +0900

    feat: 사용자가 입력한 숫자 strike 또는 ball로 체크

--- src/App.js
+++ src/App.js
@@ -4,15 +4,23 @@ const MissionUtils = require("@woowacourse/mission-utils");
 // MissionUtils.Console.close();
 class App {
   createRandomNumber() {
-    this.randomNumber = [...Array(3)].map((v) =>
+    this.randomNumber = [...Array(3)].map(() =>
       MissionUtils.Random.pickNumberInRange(1, 9)
     );
   }
   getUserInput() {
     MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (ans) => {
       this.userInput = ans.split("").map((v) => +v);
-      this.play();
     });
+  }
+  chekUserInput() {
+    this.strikeCount = 0;
+    this.ballCount = 0;
+    for (let i = 0; i < 3; i++) {
+      if (this.randomNumber[i] === this.userInput[i]) this.strikeCount++;
+      if (this.randomNumber.includes(this.userInput[i])) this.ballCount++;
+    }
+    this.ballCount -= this.strikeCount;
   }
   play() {
     MissionUtils.Console.print(this.randomNumber);
