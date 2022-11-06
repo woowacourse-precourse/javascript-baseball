39d05b685f5eaf596796bc64829f33f2604d88ea
sojinjang <jsojin6119@gmail.com>
2022-11-06 23:54:06 +0900

    :sparkles: feat(input): 컴퓨터 수 생성, 입력값 받기 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  play() {
+    const computerNum = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (userInput) => {
+      MissionUtils.Console.close();
+    });
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
