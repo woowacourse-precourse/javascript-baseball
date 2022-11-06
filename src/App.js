7b9eb16308f8cf9e5c7b4232135acc709726e0f1
semnil5202 <semnil5202@gmail.com>
2022-11-07 06:52:55 +0900

    fix: 게임 재시작 출력문 수정

--- src/App.js
+++ src/App.js
@@ -56,7 +56,7 @@ class App {
 
   isContinueGame() {
     MissionUtils.Console.readLine(
-      "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. : \n",
+      "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. \n",
       (input) => this.continueOrEnd(Number(input))
     );
   }
