15d931c50d62868a7531f43b8f3e45c641316c15
jiwon903 <50747295+jiwon903@users.noreply.github.com>
2022-11-07 06:19:03 +0900

    ball 유무 및 개수 check함수 구현

--- src/App.js
+++ src/App.js
@@ -5,10 +5,33 @@ class App {
 
 function checkStrike(randomNum,userNumber) {
   var strike=0;
+  var randomNum =String(randomNum);
+  var userNumber =String(userNumber);
   for(let i=0; i<3; i++){
-    if(randomNum[i]==userNumber[i]){
+      if(randomNum[i]==userNumber[i]){
       strike=strike+1;
     }
   }
   return strike;
+}
+
+function checkball(randomNum,userNumber){
+  var randomNumList=[];
+  var userNumberList=[];
+  var randomNum =String(randomNum);
+  var userNumber =String(userNumber);
+  var ball=0;
+
+  //리스트로 값 이동
+  for(let i=0; i<3; i++){
+      randomNumList.push(randomNum[i]);
+      userNumberList.push(userNumber[i]);
+  }
+  
+  for(let i=0; i<3; i++){
+      if(userNumberList.includes(randomNumList[i]) && userNumberList[i]!=randomNumList[i]){
+          ball=ball+1;
+      }
+  }
+  return ball;
 }
