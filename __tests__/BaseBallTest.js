954e979cfd51ad46a42ba29165a5c0523c56cf14
turtle601 <turtle601@naver.com>
2022-11-06 09:29:08 +0900

    refactor(BaseBallTest) 테스트 코드 내 require 구문 App -> BaseBall 변경

--- __tests__/BaseBallTest.js
+++ __tests__/BaseBallTest.js
@@ -1,163 +1,163 @@
-const App = require('../src/App');
+const BaseBall = require('../src/BaseBall');
 
 describe('App 클래스 - isStrike()', () => {
   test('주어진 입력값이 random값과 비교 스트라이크인지 확인하는 함수', () => {
     const random = [1, 2, 3];
     const input = [1, 4, 5];
 
-    const app = new App();
+    const baseball = new BaseBall();
 
-    expect(app.isStrike(random[0], input[0])).toBeTruthy();
-    expect(app.isStrike(random[1], input[1])).toBeFalsy();
-    expect(app.isStrike(random[2], input[2])).toBeFalsy();
+    expect(baseball.isStrike(random[0], input[0])).toBeTruthy();
+    expect(baseball.isStrike(random[1], input[1])).toBeFalsy();
+    expect(baseball.isStrike(random[2], input[2])).toBeFalsy();
   });
 });
 
-describe('App 클래스 - isBall()', () => {
+describe('baseball 클래스 - isBall()', () => {
   test('주어진 입력값이 random값과 비교 볼인지 확인하는 예제', () => {
     const random = [2, 1, 3];
     const input = [1, 4, 5];
 
-    const app = new App();
+    const baseball = new BaseBall();
 
-    expect(app.isBall(random, input, 0)).toBeTruthy();
-    expect(app.isBall(random, input, 1)).toBeFalsy();
-    expect(app.isBall(random, input, 2)).toBeFalsy();
+    expect(baseball.isBall(random, input, 0)).toBeTruthy();
+    expect(baseball.isBall(random, input, 1)).toBeFalsy();
+    expect(baseball.isBall(random, input, 2)).toBeFalsy();
   });
 
   test('스트라이크일때 볼이라고 하지 않는지 확인하는 예제', () => {
     const random = [1, 2, 3];
     const input = [1, 3, 2];
 
-    const app = new App();
+    const baseball = new BaseBall();
 
-    expect(app.isBall(random, input, 0)).toBeFalsy();
-    expect(app.isBall(random, input, 1)).toBeTruthy();
-    expect(app.isBall(random, input, 2)).toBeTruthy();
+    expect(baseball.isBall(random, input, 0)).toBeFalsy();
+    expect(baseball.isBall(random, input, 1)).toBeTruthy();
+    expect(baseball.isBall(random, input, 2)).toBeTruthy();
   });
 });
 
-describe('App 클래스 - countStrike()', () => {
+describe('baseball 클래스 - countStrike()', () => {
   test('2볼 1스트라이크 상황', () => {
     const random = [2, 1, 3];
     const input = [1, 2, 3];
 
-    const app = new App();
+    const baseball = new BaseBall();
 
-    expect(app.countStrike(random, input)).toEqual(1);
+    expect(baseball.countStrike(random, input)).toEqual(1);
   });
 
   test('노볼 노스트라이크 상황', () => {
     const random = [1, 2, 3];
     const input = [4, 5, 6];
 
-    const app = new App();
+    const baseball = new BaseBall();
 
-    expect(app.countStrike(random, input)).toEqual(0);
+    expect(baseball.countStrike(random, input)).toEqual(0);
   });
 
   test('3스트라이크 상황', () => {
     const random = [1, 2, 3];
     const input = [1, 2, 3];
 
-    const app = new App();
+    const baseball = new BaseBall();
 
-    expect(app.countStrike(random, input)).toEqual(3);
+    expect(baseball.countStrike(random, input)).toEqual(3);
   });
 });
 
-describe('App 클래스 - countBall()', () => {
+describe('baseball 클래스 - countBall()', () => {
   test('2볼 1스트라이크 상황', () => {
     const random = [2, 1, 3];
     const input = [1, 2, 3];
 
-    const app = new App();
+    const baseball = new BaseBall();
 
-    expect(app.countBall(random, input)).toEqual(2);
+    expect(baseball.countBall(random, input)).toEqual(2);
   });
 
   test('노볼 노스트라이크 상황', () => {
     const random = [1, 2, 3];
     const input = [4, 5, 6];
 
-    const app = new App();
+    const baseball = new BaseBall();
 
-    expect(app.countBall(random, input)).toEqual(0);
+    expect(baseball.countBall(random, input)).toEqual(0);
   });
 
   test('3스트라이크 상황', () => {
     const random = [1, 2, 3];
     const input = [1, 2, 3];
 
-    const app = new App();
+    const baseball = new BaseBall();
 
-    expect(app.countBall(random, input)).toEqual(0);
+    expect(baseball.countBall(random, input)).toEqual(0);
   });
 });
 
-describe('App 클래스 - getResultToString()', () => {
+describe('baseball 클래스 - getResultToString()', () => {
   test('2볼 1스트라이크 상황', () => {
     const random = [2, 1, 3];
     const input = [1, 2, 3];
 
-    const app = new App();
+    const baseball = new BaseBall();
 
-    expect(app.getResultToString(random, input)).toEqual('2볼 1스트라이크');
+    expect(baseball.getResultToString(random, input)).toEqual('2볼 1스트라이크');
   });
 
   test('노볼 노스트라이크 상황', () => {
     const random = [1, 2, 3];
     const input = [4, 5, 6];
 
-    const app = new App();
+    const baseball = new BaseBall();
 
-    expect(app.getResultToString(random, input)).toEqual('낫싱');
+    expect(baseball.getResultToString(random, input)).toEqual('낫싱');
   });
 
   test('3스트라이크 상황', () => {
     const random = [1, 2, 3];
     const input = [1, 2, 3];
 
-    const app = new App();
+    const baseball = new BaseBall();
 
-    expect(app.getResultToString(random, input)).toEqual('3스트라이크');
+    expect(baseball.getResultToString(random, input)).toEqual('3스트라이크');
   });
 
   test('2볼 상황', () => {
     const random = [1, 2, 3];
     const input = [2, 1, 4];
 
-    const app = new App();
+    const baseball = new BaseBall();
 
-    expect(app.getResultToString(random, input)).toEqual('2볼');
+    expect(baseball.getResultToString(random, input)).toEqual('2볼');
   });
 });
 
-describe('App 클래스 - isStrikeOut()', () => {
+describe('baseball 클래스 - isStrikeOut()', () => {
   test('3볼일 때', () => {
     const random = [2, 1, 3];
     const input = [3, 2, 1];
 
-    const app = new App();
+    const baseball = new BaseBall();
 
-    expect(app.isStrikeOut(random, input)).toBeFalsy();
+    expect(baseball.isStrikeOut(random, input)).toBeFalsy();
   });
 
   test('2스트라이크 노볼 상황', () => {
     const random = [1, 2, 3];
     const input = [1, 4, 3];
 
-    const app = new App();
+    const baseball = new BaseBall();
 
-    expect(app.isStrikeOut(random, input)).toBeFalsy();
+    expect(baseball.isStrikeOut(random, input)).toBeFalsy();
   });
 
   test('3스트라이크일 때', () => {
     const random = [1, 2, 3];
     const input = [1, 2, 3];
 
-    const app = new App();
+    const baseball = new BaseBall();
 
-    expect(app.isStrikeOut(random, input)).toBeTruthy();
+    expect(baseball.isStrikeOut(random, input)).toBeTruthy();
   });
 });
