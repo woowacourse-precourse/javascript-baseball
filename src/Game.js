caf192eb1ef7c48719754466be5035fcaa112205
mjmj0217 <mjmj0217@naver.com>
2022-11-06 18:31:57 +0900

    feat(Game): user 입력 받는 기능 추가

--- src/Game.js
+++ src/Game.js
@@ -1,6 +1,24 @@
+const { Console } = require("@woowacourse/mission-utils");
+const getResultMessage = require("./functions/getResultMessage");
+const makeRandomNumber = require("./functions/makeRandomNumber");
+
 class Game {
+  random;
+  getUserAnswer() {
+    Console.readLine("숫자를 입력해주세요 :", (input) => {
+      let result = getResultMessage(this.random, input);
+      Console.print(result);
+      if (input === this.random) {
+        //게임 종료
+      } else {
+        this.getUserAnswer();
+      }
+    });
+  }
+
   start() {
     this.random = makeRandomNumber();
+    this.getUserAnswer();
   }
 }
 
