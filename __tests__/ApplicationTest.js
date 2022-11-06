2afd41c03e2a48e7b1c14b33df6f21bdf7ae9cb5
TaeeunKim <xodms0309@naver.com>
2022-11-06 23:13:13 +0900

    feat: 사용자가 입력하는 값에 따른 예외 테스트 케이스 추가

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -1,5 +1,5 @@
-const App = require("../src/App");
-const MissionUtils = require("@woowacourse/mission-utils");
+const App = require('../src/App');
+const MissionUtils = require('@woowacourse/mission-utils');
 
 const mockQuestions = (answers) => {
   MissionUtils.Console.readLine = jest.fn();
@@ -18,23 +18,17 @@ const mockRandoms = (numbers) => {
 };
 
 const getLogSpy = () => {
-  const logSpy = jest.spyOn(MissionUtils.Console, "print");
+  const logSpy = jest.spyOn(MissionUtils.Console, 'print');
   logSpy.mockClear();
   return logSpy;
 };
 
-describe("숫자 야구 게임", () => {
-  test("게임 종료 후 재시작", () => {
+describe('숫자 야구 게임', () => {
+  test('게임 종료 후 재시작', () => {
     const randoms = [1, 3, 5, 5, 8, 9];
-    const answers = ["246", "135", "1", "597", "589", "2"];
+    const answers = ['246', '135', '1', '597', '589', '2'];
     const logSpy = getLogSpy();
-    const messages = [
-      "낫싱",
-      "3스트라이크",
-      "1볼 1스트라이크",
-      "3스트라이크",
-      "게임 종료",
-    ];
+    const messages = ['낫싱', '3스트라이크', '1볼 1스트라이크', '3스트라이크', '게임 종료'];
 
     mockRandoms(randoms);
     mockQuestions(answers);
@@ -47,9 +41,48 @@ describe("숫자 야구 게임", () => {
     });
   });
 
-  test("예외 테스트", () => {
+  test('자리 수에 대한 예외 테스트', () => {
     const randoms = [1, 3, 5];
-    const answers = ["1234"];
+    const answers = ['1234'];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow();
+  });
+
+  test('범위 안에 있는 숫자인지에 대한 예외 테스트', () => {
+    const randoms = [1, 3, 5];
+    const answers = ['012'];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow();
+  });
+
+  test('숫자가 아닌 값이 들어올 때 예외 테스트', () => {
+    const randoms = [1, 3, 5];
+    const answers = ['a12'];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow();
+  });
+
+  test('서로 다른 숫자가 들어오지 않을 때 예외 테스트', () => {
+    const randoms = [1, 3, 5];
+    const answers = ['122'];
 
     mockRandoms(randoms);
     mockQuestions(answers);
