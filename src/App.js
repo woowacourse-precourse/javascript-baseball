8f3b26047e4a4d7ea2bee5285d9330a520a5b9f7
Serin-Kim <serin9864@gmail.com>
2022-11-06 17:20:12 +0900

    feat: makeAnswer 함수 구현
    1부터 9까지 서로 다른 수로 이루어진 3자리 랜덤넘버를 생성하는 함수 작성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  makeAnswer = () => {
+    return MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3).join("");
+  };
+
+  play = () => {
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    const ANSWER = this.makeAnswer();
+  };
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
