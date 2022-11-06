5768a6b7291a9917fb91bf07afff16c1077b2bc5
chex <chex417@naver.com>
2022-11-06 16:46:22 +0900

    refactor: 확인용 console.log삭제

--- src/App.js
+++ src/App.js
@@ -98,7 +98,7 @@ class App {
         if (!App.isCorrectInput(userInput)) throw new Error('Input is invalid')
         
         score = App.calculateScore(computerNum, userInput);
-        console.log(computerNum);
+        
         if (App.isThreeStrike(score)) {
           MissionUtils.Console.readLine('3스트라이크 3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.',
             (selectInput) => {
