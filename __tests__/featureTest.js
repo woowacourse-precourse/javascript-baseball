a926c809e3b5154aaca9e5c7371d3ad817d98fa8
ilgon0110 <moses011010@gmail.com>
2022-11-06 18:36:49 +0900

    fix: Computer 숫자 타입 변경에 따른 test code 수정 (#1)

--- __tests__/featureTest.js
+++ __tests__/featureTest.js
@@ -29,21 +29,14 @@ const getLogSpy = () => {
 
 describe('상대방(Computer)의 숫자 테스트', () => {
   test('1-1. 숫자가 아닌 경우', () => {
-    app
-      .createComputerNum()
-      .forEach((num) => expect(Number.isNaN(+num)).toBe(false));
+    expect(Number.isNaN(Number(app.createComputerNum()))).toBe(false);
   });
-  test('1-2. 각 숫자가 1~9 범위가 아닌 경우', () => {
-    app
-      .createComputerNum()
-      .forEach((num) => expect(num >= 1 && num <= 9).toBe(true));
+  test('1-2, 1-4. 각 숫자가 1~9 범위가 아닌 경우', () => {
+    expect(app.createComputerNum().length).toEqual(3);
   });
   test('1-3. 값에 중복된 숫자가 있을 경우', () => {
-    const testComputerSet = new Set(app.createComputerNum());
+    const testComputerSet = new Set(app.createComputerNum().split(''));
     expect(testComputerSet.size).toEqual(3);
-  });
-  test('1-4. 길이가 3이 아닌 경우', () => {
-    expect(app.createComputerNum().length).toEqual(3);
   });
 });
 
