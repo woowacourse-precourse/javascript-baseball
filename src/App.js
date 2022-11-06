8fbb46adc3043dec522c82b0ca9890ddf31098df
ryuna <anottrx@gmail.com>
2022-11-06 23:09:00 +0900

    feat: 사용자에게 숫자를 매번 입력받게 한다

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
-  play() {}
+  play() {
+    // MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    let computerNumber = getRandomNumber();
+    let guessNumber;
+    while (true) {
+      // MissionUtils.Console.print("숫자를 입력해주세요 : ");
+      MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (x) => {
+        guessNumber = x;
+      });
+      guessNumber = guessNumber.split("");
+      // MissionUtils.Console.print(
+      //   `guessNumber는 ${guessNumber}, computerNumber는 ${computerNumber}`
+      // );
+      MissionUtils.Console.close();
+    }
+  }
 }
 
 const getRandomNumber = () => {
