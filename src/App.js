1b8dce20683c3affa1f18d7cd6c0de33822bfa14
Sallyvely <tpdus4159@naver.com>
2022-11-06 22:09:39 +0900

    기능 구현: 컴퓨터 숫자를 랜덤하게 생성하는 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 class App {
   play() {}
+
+  createRandomNumber() {
+    let computerNumber="";
+    let count=0;
+    while(count<3){
+      let charNum=MissionUtils.Random.pickNumberInRange(1, 9);
+      if(!computerNumber.includes(String(charNum))){
+        computerNumber+=String(charNum);
+        count++;
+      }
+      }
+      return computerNumber;
+    }
+
 }
 
 module.exports = App;
