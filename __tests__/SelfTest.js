218871477b15518b44e642f329a6bc8724ca4af3
DESKTOP-PDMTIQO\hansu <fathlic5@gmail.com>
2022-11-07 03:56:57 +0900

    :white_check_mark: test: 기능2 사용자가 잘못된 값을 입력시 오류 체크 테스트 추가 사용자의 입력값에 범위에서 벗어난 값이 있는지 테스트 추가 사용자의 입력값이 글자수를 어길시 테스트 추가 사용자의 입력값에 중복된 값이 있는지 테스트 추가 사용자의 입력값이 숫자가 아닐시 테스트 추가

--- __tests__/SelfTest.js
+++ __tests__/SelfTest.js
@@ -1,3 +1,6 @@
+const App = require("../src/App");
+const MissionUtils = require("@woowacourse/mission-utils");
+
 test("상대방 결과에 중복값 있는지 테스트", () => {
   expect(() => {
     const app = new App();
@@ -5,4 +8,57 @@ test("상대방 결과에 중복값 있는지 테스트", () => {
       
     expect(new Set(computerNumber).size).toEqual(3);
   })
+})
+
+const mockQuestions = (answers) => {
+  MissionUtils.Console.readLine = jest.fn();
+  answers.reduce((acc, input) => {
+    return acc.mockImplementationOnce((question, callback) => {
+      callback(input);
+    })
+  }, MissionUtils.Console.readLine);
+}
+
+test("사용자가 0을 입력했을때 RangeError 나는지 테스트", () => {
+  const answers = ["103"];
+
+  mockQuestions(answers);
+
+  expect(() => {
+    const app = new App();
+    app.play();
+  }).toThrow(RangeError);
+})
+
+test("사용자가 글자수를 지키지 않을시 RangeError 나는지 테스트", () => {
+  const answers = ["1234"];
+
+  mockQuestions(answers);
+
+  expect(() => {
+    const app = new App();
+    app.play();
+  }).toThrow(RangeError);
+})
+
+test("사용자가 중복값을 입력시 RangeError 나는지 테스트", () => {
+  const answers = ["1234"];
+
+  mockQuestions(answers);
+
+  expect(() => {
+    const app = new App();
+    app.play();
+  }).toThrow(RangeError);
+})
+
+test("사용자가 숫자외의 값을 입력시 RangeError 나는지 테스트", () => {
+  const answers = ["1a@"];
+
+  mockQuestions(answers);
+
+  expect(() => {
+    const app = new App();
+    app.play();
+  }).toThrow(RangeError);
 })
