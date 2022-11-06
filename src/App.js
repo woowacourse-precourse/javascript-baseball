257d8e5e482e8c9643da5b6af8636bbec6a40cc5
하송희 <99heesong@naver.com>
2022-11-06 19:57:12 +0900

    기능수정: 기능별 함수 세분화

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 const Console = MissionUtils.Console;
 const validator = require("./Validate");
+let randomArr = [0, 0, 0];
 
 class App {
   play() {
     Console.print("숫자 야구 게임을 시작합니다");
-    this.input();
+    randomArr = this.getRandomNumber();
+    this.runGame();
+  }
+
+  runGame() {
+    Console.readLine("숫자를 입력해주세요: ", (inputStr) => {
+      const inputArr = this.validateInput(inputStr);
+    });
   }
 
   getRandomNumber() {
-    MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
+    return MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
   }
 
   validateInput(inputStr) {
@@ -22,10 +30,7 @@ class App {
       throw Error("입력 형식이 맞지 않습니다.");
 
     const inputArr = inputStr.split("").map(Number);
-  }
-
-  input() {
-    Console.readLine("숫자를 입력해주세요: ", this.validateInput);
+    return inputArr;
   }
 }
 
