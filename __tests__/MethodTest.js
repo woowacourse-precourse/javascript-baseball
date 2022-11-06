012a7d25b6b330910e9f2a029599b839d4ca43af
Adult96 <djxor007@gmail.com>
2022-11-06 21:29:47 +0900

    test(__tests__): Add compare strike method test case
    사용자 입력값과 컴퓨터 랜덤값의 각 자릿수를 비교하는 compareStrike 메서드 테스트 케이스 추가

--- __tests__/MethodTest.js
+++ __tests__/MethodTest.js
@@ -13,4 +13,13 @@ describe('App 클래스 메서드 테스트', () => {
     expect(result).toHaveLength(3);
     expect([1, 2, 3, 4, 5, 6, 7, 8, 9]).toEqual(expect.arrayContaining(result));
   });
+
+  test('strike 메서드', () => {
+    const input = [1, 2, 3];
+    const computer = [3, 2, 1];
+    const strike = app.compareStrike(input, computer);
+    expect(strike).toBe(1);
+    expect(strike).not.toBe(2);
+    expect(strike).not.toBe(3);
+  });
 });
