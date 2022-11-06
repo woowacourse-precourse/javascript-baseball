5add1465e3db380d1f25a476773536d50ab04ecc
han0224 <dbn0224@gmail.com>
2022-11-06 16:39:21 +0900

    Feat: 게임 시작 문구 출력
    game함수를 만들어 게임 시작 문구를 제외한 기능을 포함
    재시작할 경우 게임 시작 문구가 필요없기에 game함수를 호출

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App {
   userInputNumber() {
     MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (number) => {
       this.NumberUtil.isBaseballNumber(number);
+      console.log(this.computerNumber);
       const result = this.compareNumber(number);
       MissionUtils.Console.print(result);
       if (result !== "3스트라이크") {
@@ -49,15 +50,20 @@ class App {
       "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n",
       (number) => {
         number = Number(number);
-        if (number === 1) this.play();
+        if (number === 1) this.game();
         else if (number === 2) MissionUtils.Console.close();
         else throw "1또는 2만 입력해주세요.";
       }
     );
   }
-  play() {
+
+  game() {
     this.computerNumber = this.NumberUtil.getRandomBaseballNumber();
     this.userInputNumber();
+  }
+  play() {
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    this.game();
   }
 }
 
