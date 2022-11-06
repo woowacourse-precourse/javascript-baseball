9dbda476ebc781b0b762f2106ef4382b8240fe83
dragonfruitlemonade <tpghks9245@gmail.com>
2022-11-06 20:48:32 +0900

    feat(Game): exception(예외사항) input 유효값 확인 기능

--- src/Game.js
+++ src/Game.js
@@ -28,23 +28,46 @@ class Game {
         return MissionUtils.Console.print("낫싱");
     }
 
+    exception(usersNumber) {
+        // 길이가 3자리인지 확인, 3자리 전부 일의 자리 숫자인지 확인 가능.
+        if (usersNumber.length !== 3) return true;
+        // 서로 다른 수인지 확인
+        if (Array.from(usersNumber).length !== new Set(usersNumber.split("")).length) return true;
+        if (isNaN(usersNumber)) return true;
+        return false;
+    }
+
     playGame() {    
         MissionUtils.Console.readLine("숫자를 입력해주세요 : ", usersNumber => {
+            if (this.exception(usersNumber)) {
+                MissionUtils.Console.close();
+            }
+            
             console.log(this.computersNumber);
             usersNumber = [...usersNumber].map(idx => parseInt(idx));
             console.log(usersNumber);
-
+  
             // 볼 스트라이크 갯수 구하기, resultMessage함수 결과 출력
             const [howManyStrike, howManyBall] = this.getStrikeAndBall(this.computersNumber, usersNumber);
             this.resultMessage(howManyStrike, howManyBall);
             
             if (howManyStrike !== 3) this.playGame();
+            if (howManyStrike === 3) new Computer().successMessage();
             
-            
-            return ;
-        });
+            MissionUtils.Console.readLine(new Computer().chooseMessage, (choice) => {
+                if (!(choice === 1 || choice === 2)) {
+                    MissionUtils.Console.close();
+                }
+                if (choice === 1) {
+                    this.computersNumber = new Computer().pickedNum();
+                    this.playGame();
+                }
+                if (choice === 2) {
+                    MissionUtils.Console.close();
+                }
+            })
 
-        return;
+        });
     }
 }
 
