b431920c8863d88ffdd00db18530c1092947b82c
yeongseokim <1903020@ewhain.net>
2022-11-07 00:30:15 +0900

    feat(createRandomNumber): 1~9 사이의 랜덤 수 생성
    랜덤한 수 3개를 생성하여 App의 constructor에 저장
    
    컴퓨터가 서로 다른 수 3개 선택 필요

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
+  constructor() {
+    this.computerRandomNumber = this.createRandomNumber();
+  }
+
+  createRandomNumber() {
+    const computerRandomNumber = [];
+    while (computerRandomNumber.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computerRandomNumber.includes(number)) {
+        computerRandomNumber.push(number);
+      }
+    }
+    return computerRandomNumber;
+  }
+
   play() {
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+    console.log(this.computerRandomNumber);
   }
 }
-
+const app = new App();
+app.play();
 module.exports = App;
