dbd6543cb96d66a1c359ce14289af618ea27c47b
Woong <jiyko7@kyonggi.ac.kr>
2022-11-07 00:45:53 +0900

    6. throw 함수 분리 및 재시작 종료 기능 추가
    6. throw 함수 분리 및 재시작 종료 기능 추가

--- src/App.js
+++ src/App.js
@@ -28,14 +28,9 @@ function game(MissionUtils, computer) {
   var strikecount = 0;
   var check = true;
   MissionUtils.Console.readLine("숫자를 입력하세요 : ", function(input) { 
-    if (isNaN(input) || input.length != 3){
-      throw new Error("3자리 숫자를 입력하세요");
-    }
-
-    if (input.charAt(0) == input.charAt(1) || input.charAt(0) == input.charAt(2) || input.charAt(1) == input.charAt(2)){
-      throw new Error("3자리 숫자 모두 다르게 입력하세요");
-    }
-    
+    check3num(input);
+    checkdiffnum(input);
+  
     for (var i = 0; i<3; i++){
       if (computer[0] == input.charAt(i)){
         if (i==0){
@@ -74,7 +69,20 @@ function game(MissionUtils, computer) {
       MissionUtils.Console.print(strikecount + "스트라이크");
       if (strikecount == 3){
         MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-        MissionUtils.Console.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        MissionUtils.Console.readLine("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n", function(restart) {
+          check1num(restart);
+          if (restart == 1){
+            game(MissionUtils, init(MissionUtils));
+          }
+          else if (restart == 2){
+            MissionUtils.Console.close();
+            return;
+          }
+          else{
+            throw new Error("1과 2만 입력 가능합니다");
+          }
+
+        });
       }
     }
     else{
@@ -83,6 +91,23 @@ function game(MissionUtils, computer) {
     game(MissionUtils, computer);
   });
 
+}
+function check1num(input) {
+  if (isNaN(input) || input.length != 1){
+    throw new Error("1자리 숫자를 입력하세요");
+  }
+}
+
+function check3num(input) {
+  if (isNaN(input) || input.length != 3){
+    throw new Error("3자리 숫자를 입력하세요");
+  }
+}
+
+function checkdiffnum(input) {
+  if (input.charAt(0) == input.charAt(1) || input.charAt(0) == input.charAt(2) || input.charAt(1) == input.charAt(2)){
+    throw new Error("3자리 숫자 모두 다르게 입력하세요");
+  }
 }
 
 var a = new App();
