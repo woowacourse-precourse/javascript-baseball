e09c9138ad6e538d1413799943683c87306b6498
mjmj0217 <mjmj0217@naver.com>
2022-11-06 20:41:59 +0900

    feat(Game): 사용자의 잘못된 값 입력에 대한 예외처리(예외발생 후 종료)

--- src/Game.js
+++ src/Game.js
@@ -1,13 +1,17 @@
 const { Console } = require("@woowacourse/mission-utils");
 const getResultMessage = require("./functions/getResultMessage");
 const makeRandomNumber = require("./functions/makeRandomNumber");
+const isInvalidCmd = require("./functions/isInvalidCmd");
+const isInvalidNumber = require("./functions/isInvalidNumber");
 
 class Game {
   random;
   getUserAnswer() {
     Console.readLine("숫자를 입력해주세요 :", (input) => {
-      let result = getResultMessage(this.random, input);
-      Console.print(result);
+      if (isInvalidNumber(input)) throw new Error("Invalid Input!!");
+
+      Console.print(getResultMessage(this.random, input));
+
       if (input === this.random) {
         this.finishGame();
       } else {
@@ -20,6 +24,10 @@ class Game {
     Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     Console.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     Console.readLine("", (cmd) => {
+      if (isInvalidCmd(cmd)) {
+        console.log(cmd);
+        throw new Error("Invalid Command!");
+      }
       if (cmd === "1") {
         this.start();
       } else {
