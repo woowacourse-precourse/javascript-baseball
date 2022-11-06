74b94dc93f341c1c2f8590e8ed57803d6036849d
min <hororonoodle@gmail.com>
2022-11-07 00:26:52 +0900

    feat : 사용자 오류 메시지 상수 추가

--- src/Constants/gameMessage.js
+++ src/Constants/gameMessage.js
@@ -1,6 +1,7 @@
 class GameMessage {
   static WELCOME_MESSAGE = '숫자 야구 게임을 시작합니다.';
   static QUESTION_MESSAGE = '숫자를 입력해주세요. : ';
+  static WRONG_INPUT_ERROR_MESSAGE = '잘못된 값을 입력하셨습니다.';
 }
 
 module.exports = GameMessage;
