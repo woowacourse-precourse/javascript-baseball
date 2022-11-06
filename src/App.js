dc637bc0513ce7cfdae68e38bc2829969dd25c87
myung-j-p <jmpack97@gmail.com>
2022-11-07 04:14:31 +0900

    feat : 컴퓨터, 사용자 클래스 분화

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
+
 class App {
-  play() {}
+  play() {
+    // 1.컴퓨터 리스트 생성
+    const computerLs = new Computer();
+
+    // 2.유저 리스트 입력
+    const userLs = new User();
+
+  }
+}
+
+class Computer extends App {
+  makeComNumLs(){}
+}
+
+class User extends App {
+  getUserLs(){}
 }
 
 module.exports = App;
