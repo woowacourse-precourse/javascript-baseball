f2cbd18f3783cd45ac001baf3dcb75d6e5cad510
seunghoria <102285042+seunghoria@users.noreply.github.com>
2022-11-06 23:39:55 +0900

    feat: 스트라이크와 볼 개수 카운트에 따른 결과 출력 기능 구현

--- src/App.js
+++ src/App.js
@@ -25,6 +25,7 @@ class App {
           this.computerInputNumber,
           checkedUser
         );
+        this.printResult(countResultArr);
       }
     });
   }
@@ -77,6 +78,22 @@ class App {
 
     let countArr = [BALLS, STRIKES];
     return countArr;
+  }
+
+  printResult(count) {
+    if (count[1] === GAME_NUMBER_LENGTH) {
+      mConsole.print(
+        `${GAME_NUMBER_LENGTH}스트라이크\n${GAME_NUMBER_LENGTH}개의 숫자를 모두 맞히셨습니다! 게임 종료`
+      );
+    } else {
+      if (count[0] === 0 && count[1] === 0) mConsole.print("낫싱");
+      else
+        mConsole.print(
+          `${count[0] > 0 ? count[0] + "볼 " : ""}${
+            count[1] > 0 ? count[1] + "스트라이크" : ""
+          }`.trim()
+        );
+    }
   }
 }
 
