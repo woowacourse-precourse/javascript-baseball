8b37083c5e3d077841cf0e18e7c2d6c386e5e523
Rki0 <pky00823@gmail.com>
2022-11-06 17:24:15 +0900

    refactor: merge separated modules
    Merged several previously separated modules.
    This is to solve the problem with circular(cyclic) dependencies(references).
    
    BREAKING CHANGE: Merged several modules to solve circular dependencies.
    
    Before:
    
    Functions has been divided into the following files.
    App.js, Player.js, ReStart.js
    
    After:
    
    Functions have been merged again to solve circular dependencies.
    As a result, the remaining file is 'App.js'.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 const RandomNumber = require("./RandomNumber");
-const Player = require("../src/Player");
+const Constraints = require("./Constraints");
 
 class App {
   constructor() {
@@ -9,13 +9,111 @@ class App {
 
   play() {
     this.COMPUTER = RandomNumber.makeRandomNumber();
-
-    const player = new Player(this.COMPUTER);
-    player.getPlayerInput();
+    this.getPlayerInput();
   }
 
   showStartMessage() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+  }
+
+  getPlayerInput() {
+    console.log("computer", this.COMPUTER);
+
+    MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (userInput) => {
+      const constraints = new Constraints();
+      constraints.checkConstraints(userInput);
+
+      this.comparePlayerInputWithRandomNumber(userInput);
+    });
+  }
+
+  comparePlayerInputWithRandomNumber(userInput) {
+    this.ball = 0;
+    this.strike = 0;
+
+    const USER_INPUT = userInput.split("").map((item) => Number(item));
+
+    this.checkBallOrStrike(USER_INPUT);
+
+    this.showPlayerResult();
+  }
+
+  checkBallOrStrike(userInputArray) {
+    for (let index = 0; index < this.COMPUTER.length; index++) {
+      if (this.COMPUTER[index] === userInputArray[index]) {
+        this.strike++;
+        continue;
+      }
+
+      if (userInputArray.includes(this.COMPUTER[index])) {
+        this.ball++;
+      }
+    }
+  }
+
+  showPlayerResult() {
+    if (this.strike === 3) {
+      MissionUtils.Console.print("3스트라이크");
+      MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+
+      this.getPlayerRePlayInput();
+
+      return;
+    }
+
+    if (this.ball === 0 && this.strike === 0) {
+      MissionUtils.Console.print("낫싱");
+
+      this.getPlayerInput();
+
+      return;
+    }
+
+    if (this.ball !== 0 && this.strike !== 0) {
+      MissionUtils.Console.print(`${this.ball}볼 ${this.strike}스트라이크`);
+
+      this.getPlayerInput();
+
+      return;
+    }
+
+    if (this.ball !== 0 && this.strike === 0) {
+      MissionUtils.Console.print(`${this.ball}볼`);
+
+      this.getPlayerInput();
+
+      return;
+    }
+
+    if (this.ball === 0 && this.strike !== 0 && this.strike !== 3) {
+      MissionUtils.Console.print(`${this.strike}스트라이크`);
+
+      this.getPlayerInput();
+    }
+  }
+
+  getPlayerRePlayInput() {
+    MissionUtils.Console.readLine(
+      "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n",
+      (userInput) => {
+        const constraints = new Constraints();
+        constraints.checkRePlayInput(userInput);
+
+        this.decideReStart(userInput);
+      }
+    );
+  }
+
+  decideReStart(userInput) {
+    if (userInput === "1") {
+      this.play();
+
+      return;
+    }
+
+    if (userInput === "2") {
+      MissionUtils.Console.close();
+    }
   }
 }
 
