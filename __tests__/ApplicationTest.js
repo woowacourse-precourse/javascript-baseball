560989651d01186e93d751811b0710093148eaa3
ksone02 <eksovus@gmail.com>
2022-11-06 20:04:15 +0900

    test: 숫자 야구 게임 메인 테스트

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -25,28 +25,28 @@ const getLogSpy = () => {
 };
 
 describe('숫자 야구 게임', () => {
-  //test('게임 종료 후 재시작', () => {
-  //  const randoms = [1, 3, 5, 5, 8, 9];
-  //  const answers = ['246', '135', '1', '597', '589', '2'];
-  //  const logSpy = getLogSpy();
-  //  const messages = [
-  //    '낫싱',
-  //    '3스트라이크',
-  //    '1볼 1스트라이크',
-  //    '3스트라이크',
-  //    '게임 종료',
-  //  ];
-  //
-  //  mockRandoms(randoms);
-  //  mockQuestions(answers);
-  //
-  //  const app = new App();
-  //  app.play();
-  //
-  //  messages.forEach(output => {
-  //    expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
-  //  });
-  //});
+  test('게임 종료 후 재시작', () => {
+    const randoms = [1, 3, 5, 5, 8, 9];
+    const answers = ['246', '135', '1', '597', '589', '2'];
+    const logSpy = getLogSpy();
+    const messages = [
+      '낫싱',
+      '3스트라이크',
+      '1볼 1스트라이크',
+      '3스트라이크',
+      '게임 종료',
+    ];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    const app = new App();
+    app.play();
+
+    messages.forEach(output => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
 
   test('예외 테스트', () => {
     const randoms = [1, 3, 5];
