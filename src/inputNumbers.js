415cffce6db8e2bcbfc317c26ba8b9569c4eca82
JSH99 <tjgus9966@naver.com>
2022-11-06 21:38:53 +0900

    fix(inputNumber): return 오류 해결

--- src/inputNumbers.js
+++ src/inputNumbers.js
@@ -2,13 +2,16 @@ const MissionUtils = require("@woowacourse/mission-utils");
 const isVaildNumber = require("./isVaildNumber");
 
 const inputNumbers = () => {
+    let userNum;
+
     MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (inputNum) => {
         if (!isVaildNumber(inputNum)) { 
             throw new Error('잘못된 값입니다. 1부터 9까지 서로 다른 3자리 수를 입력하세요.');
         } 
-        return inputNum;
+        userNum = inputNum;
     });
     MissionUtils.Console.close();
+    return userNum;
 }
 
 module.exports = inputNumbers;
