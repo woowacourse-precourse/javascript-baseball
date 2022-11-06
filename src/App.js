abbd02eb09ae20b17b67bb0f486981515c2cb3ca
hae-on <solbi2004@naver.com>
2022-11-06 23:18:23 +0900

    feat: 숫자 야구 시작 멘트 출력

--- src/App.js
+++ src/App.js
@@ -91,7 +91,7 @@ class App {
       '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n',
       (answer) => {
         if (answer === '1') {
-          this.play();
+          this.start();
           return;
         }
         if (answer === '2') {
@@ -103,9 +103,14 @@ class App {
     );
   }
 
-  play() {
+  start() {
     this.makeRandomNumbers();
     this.getUserNumbers();
+  }
+
+  play() {
+    Console.print('숫자 야구 게임을 시작합니다.');
+    this.start();
   }
 }
 
