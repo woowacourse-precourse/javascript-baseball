8a200aa269603972ba4103e6820d4794fdff9228
Jungkyu Yang <67031524+jungkyuYang@users.noreply.github.com>
2022-11-07 04:17:00 +0900

    feat: add 게임시작 기능"

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
+const { Console } = MissionUtils;
+
 class App {
-  play() {}
+  play() {
+    Console.print("숫자 야구 게임을 시작합니다.");
+    return;
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
