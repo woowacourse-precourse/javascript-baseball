337ba056feb4943f4293211521b0fb75a9355501
bae-sh <hyeon1293@gmail.com>
2022-11-07 03:58:12 +0900

    feat : checkAnswer 구현

--- src/App.js
+++ src/App.js
@@ -5,10 +5,13 @@ class App {
   }
   play() {
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
-    this.setAnswer();
+    this.computer = this.setAnswer();
     this.input();
   }
+
   setAnswer() {
+    const computer = [];
+
     while (computer.length < 3) {
       const number = MissionUtils.Random.pickNumberInRange(1, 9);
       if (!computer.includes(number)) {
@@ -17,8 +20,24 @@ class App {
     }
     return computer;
   }
-  input() {}
-  checkAnswer() {}
+
+  input() {
+    MissionUtils.Console.readLine('숫자를 입력해주세요 : ', userNumber => {
+      const { strike, ball } = this.checkAnswer(userNumber);
+      console.log(strike, ball);
+    });
+  }
+  checkAnswer(userNumber) {
+    const userNumberArr = userNumber.split('').map(Number);
+    let [strike, ball] = [0, 0];
+
+    userNumberArr.forEach((number, idx) => {
+      const findIndex = this.computer.indexOf(number);
+      if (findIndex === idx) strike += 1;
+      else if (findIndex !== -1) ball += 1;
+    });
+    return { strike, ball };
+  }
 }
 
 const app = new App();
