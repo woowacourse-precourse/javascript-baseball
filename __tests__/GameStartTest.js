56f08b4db8c1f4ccb3d40b98b3eaae8270916d07
위성노 <dev.satellite.no92@gmail.com>
2022-11-06 19:34:06 +0900

    test(GameStartTest): add test case and afterAll

--- __tests__/GameStartTest.js
+++ __tests__/GameStartTest.js
@@ -7,12 +7,25 @@ const getLogSpy = () => {
   return logSpy;
 };
 
+const app = new App();
+
+afterAll(()=>{
+  MissionUtils.Console.close();
+})
+
 describe("게임 시작 테스트", () => {
   test("게임 시작 문구 체크", () => {
-    const app = new App();
     const testStr = "숫자 야구 게임을 시작합니다.";
     const logSpy = getLogSpy();
     app.gameStart();
     expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(testStr));
+  });
+
+  test("게임 초기 조건 세팅", () => {
+    app.gameStart();
+    const result = app.gameSet;
+    const gameSet = app.gameSetting();
+
+    expect(result).toEqual(gameSet);
   });
 });
