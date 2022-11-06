d4417c996aba9c1fe801392f67b170d9c0b66120
NYeonK <skdus728@gmail.com>
2022-11-06 19:50:49 +0900

    [ feat ] 재시작 혹은 완전히 종료

--- src/gameProcess.js
+++ src/gameProcess.js
@@ -1,20 +1,20 @@
 const MissionUtils = require("@woowacourse/mission-utils");
-const computer = [];
+let computerNumber;
 
 function getComputerNumbers() {
+    const computer = [];
     while (computer.length < 3) {
         const number = MissionUtils.Random.pickNumberInRange(1, 9);
         if (!computer.includes(number)) computer.push(number);
     }
-    console.log(computer)
-    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
     return computer;
 }
 
 function gameProcess() {
-        MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (answer) => {
-            countResult(answer);
-        });
+    console.log(computerNumber)
+    MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (answer) => {
+        countResult(answer);
+    });
 }
 
 function countResult(answer) {
@@ -22,8 +22,8 @@ function countResult(answer) {
     answer = answer.split('');
 
     for (let i = 0; i < 3; i++){
-        if (Number(answer[i]) === computer[i]) strike++;
-        else if(computer.includes(Number(answer[i]))) ball++;
+        if (Number(answer[i]) === computerNumber[i]) strike++;
+        else if(computerNumber.includes(Number(answer[i]))) ball++;
     }
     
     return getHint(ball,strike);
@@ -39,8 +39,17 @@ function getHint(ball,strike) {
     
     MissionUtils.Console.print(hintMessage);
 
-    return (strike !== 3) ? gameProcess() : hintMessage;
+    return (strike !== 3) ? gameProcess() : askRetry();
 }
 
-getComputerNumbers();
+function askRetry() {
+    MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    MissionUtils.Console.readLine('', (answer) => {
+        computerNumber = getComputerNumbers()
+        return answer === "1" ? gameProcess() : MissionUtils.Console.close();
+    });
+}
+
+computerNumber = getComputerNumbers();
+MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
 gameProcess();
