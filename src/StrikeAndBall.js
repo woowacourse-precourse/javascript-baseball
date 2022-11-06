d3c1b01b05d8d5124e5d39ed46182d8c3ef84050
woo-jk <201702042@o.cnu.ac.kr>
2022-11-06 13:41:48 +0900

    feat: 스트라이크 볼 개수를 문자열로 바꿔주는 함수 구현

--- src/StrikeAndBall.js
+++ src/StrikeAndBall.js
@@ -21,7 +21,13 @@ function getStrikeCount(answer, input) {
 
   return strikeCount;
 }
-function getString(strike, ball) {}
+function getString(strike, ball) {
+  if (strike === 0 && ball === 0) return "낫싱";
+  const output =
+    (ball ? `${ball}볼 ` : ``) + (strike ? `${strike}스트라이크` : ``);
+
+  return output.trim();
+}
 
 exports.getResult = getResult;
 exports.getString = getString;
