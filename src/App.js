e9033daa55d6659a9b14ac136c6b101b02cdbb57
DESKTOP-PDMTIQO\hansu <fathlic5@gmail.com>
2022-11-06 23:14:19 +0900

    :recycle: refactor: 반복 사용되는 변수 멤버변수로 리팩토링 반복해서 사용하는 컴퓨터와 사용자의 숫자를 생성자 안의 변수로 빼서 class에서 모든 함수가 사용할 수 있도록 만듬

--- src/App.js
+++ src/App.js
@@ -7,6 +7,11 @@ const REPLAY_GAME_KEY = '1';
 const END_GAME_KEY = '2';
 
 class App {
+    constructor() {
+        this.computerNumbers = 0;
+        this.userNumbers = 0;
+    }
+
     play() {
         Console.print('숫자 야구 게임을 시작합니다.');
         this.isUserNumbers(this.isComputerNumbers());
