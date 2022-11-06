778815d71c67fa039960ebaf0c4ef2aa02d85bd4
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2022-11-07 00:30:49 +0900

    feat(CheckValid): 유저에게 받은 입력값의 타당성 검사 기능 추가

---
+++ src/CheckValid.js
@@ -0,0 +1,49 @@
+function checkLength(userNum) {
+  if (userNum.length !== 3) {
+    return false;
+  }
+}
+function checkDuplicates(userNum) {
+  let checkArr = [];
+  for (let i = 0; i < userNum.length; i++) {
+    if (checkArr.includes(userNum[i]) === true) {
+      return false;
+    }
+    checkArr.push(userNum[i]);
+  }
+}
+function checkZero(userNum) {
+  if (/^[1-9]*$/g.test(userNum.join("")) === false) {
+    return false;
+  }
+}
+
+function checkBlank(userNum) {
+  if (userNum.length === 0) {
+    return false;
+  }
+}
+
+class CheckUserInput {
+  constructor({ userNum }) {
+    this.userNum = userNum;
+  }
+
+  checkValidation() {
+    if (checkLength(this.userNum) === false) {
+      throw new Error("숫자를 3개 입력해주세요");
+    }
+
+    if (checkDuplicates(this.userNum) === false) {
+      throw new Error("숫자에 중복이 있습니다");
+    }
+
+    if (checkZero(this.userNum) === false) {
+      throw new Error("숫자 1~9까지만 입력이 가능합니다");
+    }
+    if (checkBlank(this.userNum) === false) {
+      throw new Error("공백은 입력할 수 없습니다");
+    }
+  }
+}
+module.exports = CheckUserInput;
