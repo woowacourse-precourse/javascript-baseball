87954a7e92fbae04ebbeb001af1a6e9bf4fe971c
Quartz <skyu.dev@gmail.com>
2022-11-06 23:09:13 +0900

    refactor: 테스트 코드의 앱 인스턴스 중복 제거

--- __tests__/ExceptioinFnTest.js
+++ __tests__/ExceptioinFnTest.js
@@ -1,12 +1,11 @@
 const App = require("../src/App");
 
 describe("입력 A: 예외 처리 테스트", () => {
+  const app = new App();
   test("공백만 입력되면 오류가 발생합니다", () => {
-    const app = new App();
     expect(() => app.validateUserNum("  ")).toThrow("숫자를 입력해주세요");
   });
   test("문자를 입력하면 오류가 발생합니다", () => {
-    const app = new App();
     expect(() => app.validateUserNum("가나다")).toThrow(
       "문자를 제외한 숫자만을 입력해주세요"
     );
@@ -15,7 +14,6 @@ describe("입력 A: 예외 처리 테스트", () => {
     );
   });
   test("3자리가 아닌 숫자를 입력하면 오류가 발생합니다", () => {
-    const app = new App();
     expect(() => app.validateUserNum("01")).toThrow(
       "입력한 숫자가 3 자리가 아닙니다"
     );
@@ -27,7 +25,6 @@ describe("입력 A: 예외 처리 테스트", () => {
     );
   });
   test("중복된 숫자를 입력하면 오류가 발생합니다.", () => {
-    const app = new App();
     expect(() => app.validateUserNum("888")).toThrow(
       "입력한 숫자에 중복된 숫자가 존재합니다"
     );
@@ -36,13 +33,12 @@ describe("입력 A: 예외 처리 테스트", () => {
     );
   });
   test("공백은 제거하여 3 자리 숫자로 교체합니다", () => {
-    const app = new App();
     expect(app.validateUserNum("1 3 9")).toBe("139");
   });
 });
 describe("입력 B: 예외 처리 테스트", () => {
+  const app = new App();
   test("1이나 2 이외의 값을 입력하면 오류가 발생합니다", () => {
-    const app = new App();
     expect(() => app.validateRestartNum("재시작")).toThrow(
       "1 과 2 중 하나를 입력해주세요"
     );
--- __tests__/IntegrationTest.js
+++ __tests__/IntegrationTest.js
@@ -21,14 +21,14 @@ const mockQuestions = (answers) => {
 };
 
 describe("구현 기능 목록 테스트", () => {
+  const app = new App();
   test("게임 시작 메시지 출력", () => {
     const logSpy = getLogSpy();
-    const app = new App();
+
     app.printStartMsg();
     expect(logSpy).toHaveBeenCalledWith("숫자 야구 게임을 시작합니다.");
   });
   test("컴퓨터의 세자리 수 만들기", () => {
-    const app = new App();
     const computerNum = app.pickComputerNum();
     let answerArr = computerNum.split("");
     let duplicates = answerArr.filter((value, index) => {
@@ -39,21 +39,20 @@ describe("구현 기능 목록 테스트", () => {
   });
   test("입력 기능 A: 사용자에게 3 자리 숫자 받기", () => {
     const answers = ["246"];
-    const app = new App();
+
     mockQuestions(answers);
 
     expect(app.getUserNum()).toMatch(/^[1-9]{3}$/);
   });
   test("입력 기능 B: 게임 재시작/종료 구분 숫자 받기(1 or 2)", () => {
     const answers = ["1", "2"];
-    const app = new App();
+
     mockQuestions(answers);
 
     expect(app.getUserGameFlag()).toBe(1);
     expect(app.getUserGameFlag()).toBe(2);
   });
   test("출력 기능: 입력 기능 A의 숫자에 대한 결과 판단 로직", () => {
-    const app = new App();
     // 스트라이크 / 볼  판단
     expect(app.compareNums("123", "123")).toEqual({ strike: 3, ball: 0 });
     expect(app.compareNums("485", "821")).toEqual({ strike: 0, ball: 1 });
