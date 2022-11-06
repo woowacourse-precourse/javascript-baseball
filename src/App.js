4efc636f775ec27f8f7d62639248d303f98cd4fb
youngentry <90388461+youngentry@users.noreply.github.com>
2022-11-06 17:31:31 +0900

    feat: SF_1 게임 시작 시 메시지 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const { MESSAGE, PLAYING } = require("./Message");
+
 class App {
-  play() {}
+    play() {
+        MissionUtils.Console.print(MESSAGE.START);
+    }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
---
+++ src/Message.js
@@ -0,0 +1,15 @@
+const MESSAGE = {
+    START: "숫자 야구 게임을 시작합니다.",
+    FINISH: "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
+    INPUT: "숫자를 입력해주세요 : 000",
+    CLEAR: "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
+    ERROR: "잘못된 값을 입력하셨습니다. 게임을 종료합니다.",
+};
+
+const PLAYING = {
+    NOTHING: "낫싱",
+    BALL: "볼",
+    STRIKE: "스트라이크",
+};
+
+module.exports = { MESSAGE, PLAYING };
