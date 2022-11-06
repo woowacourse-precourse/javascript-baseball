086f13bb5773605f0461779e5d39a3b7defec154
JayPark0110 <90092440+JayPark0110@users.noreply.github.com>
2022-11-06 19:19:07 +0900

    refactor: PrinBallCount 내부 if문을 삼항연산자로 대체 완료.

--- src/components/PrintBallCount.js
+++ src/components/PrintBallCount.js
@@ -1,14 +1,13 @@
 const Console = require("@woowacourse/mission-utils").Console;
 
 const PrintBallCount = (ball, strike) => {
-  if (ball === 0 && strike > 0) Console.print(`${strike}스트라이크`);
-  if (strike === 0 && ball > 0) Console.print(`${ball}볼`);
-
-  ball === 0 && strike === 0
-    ? Console.print("낫싱")
-    : ball !== 0 && strike !== 0
-    ? Console.print(`${ball}볼 ${strike}스트라이크`)
-    : null;
+  ball == 0
+    ? strike == 0
+      ? Console.print("낫싱")
+      : Console.print(`${strike}스트라이크`)
+    : strike == 0
+    ? Console.print(`${ball}볼`)
+    : Console.print(`${ball}볼 ${strike}스트라이크`);
 };
 
 module.exports = PrintBallCount;
