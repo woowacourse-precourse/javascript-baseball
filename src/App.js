8de8febefbd36b76efbf446831d3f7b29b6dfd6b
Sungjae.Kwak <sungjae8970@gmail.com>
2022-11-06 17:14:16 +0900

    feat : 게임시작 문구 출력 구현
    게임시작 문구 출력을 메소드로 만들어 호출한다

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+const { Console, Random } = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  play() {
+    this.showStartMessage();
+  }
+
+  showStartMessage() {
+    Console.print("숫자 야구 게임을 시작합니다.");
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
