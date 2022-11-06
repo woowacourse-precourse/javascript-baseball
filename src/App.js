466272b8d6c43f03ab30fb1edb0f8062aa85b9a9
김아현 <dhflxhdxhd@gmail.com>
2022-11-06 15:22:44 +0900

    Feat : 랜덤 숫자 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,43 @@
-class App {
-  play() {}
+console.log("hi");
+
+const MissionUtils = require("@woowacourse/mission-utils");
+
+function makeRandom() {
+  let answer = new Array();
+
+  while (true) {
+    while (answer.length < 3) {
+      let number = MissionUtils.Random.pickNumberInRange(1, 9);
+
+      if (!answer.includes(number)) {
+        answer.push(number);
+      }
+    }
+    return answer;
+  }
 }
 
-module.exports = App;
+console.log(makeRandom());
+
+// class App {
+//   play() {
+//     this.randomNumber = this.makeRandom();
+//   }
+
+//   makeRandom() {
+//     let answer = new Array();
+
+//     while (ades(number)) {
+//         answer.push(numnswer.length < 3) {
+//       const number = MissionUtils.Random.pickNumberInRange(1, 9);
+//       if (!answer.incluber);
+//       }
+//     }
+
+//     return answer;
+//   }
+// }
+
+// const app = new App();
+// console.log(app.makeRandom());
+// module.exports = App;
