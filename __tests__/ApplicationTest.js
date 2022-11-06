555f70ce0e217da439d4527a18c460d944326b64
chiman <rlaclghks123@naver.com>
2022-11-06 14:02:52 +0900

    test: 테스트케이스 추가
    게임 재시작 또는 종료시 1,2를 제외한 숫자를 누른 경우 예외처리

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -53,4 +53,16 @@ describe("숫자 야구 게임", () => {
       app.play();
     }).toThrow();
   });
+  test("예외 테스트 : 게임 재시작 또는 종료시 1,2를 제외한 숫자를 누른 경우", () => {
+    const randoms = [1, 3, 5];
+    const answers = ["123"];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    expect(() => {
+      const app = new App();
+      app.checkExitOrRestart("3");
+    }).toThrow("새로 시작할려면 1, 종료하려면 2를 입력해주세요.");
+  });
 });
