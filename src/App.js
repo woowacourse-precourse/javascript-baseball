947de9b1ec4dee80f1f2547770f9702ec3caac4e
jaesung4231 <jaesung4231@naver.com>
2022-11-06 23:31:45 +0900

    컴퓨터한테 랜덤한 숫자 입력받기

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 class App {
+  comInput=[]
+  PlayerInput=[]
+  setComInput(){
+    for(let i=0;i<3;i++){
+      this.comInput.push(MissionUtils.Random.pickNumberInRange(1, 9))
+    }
+    return this.comInput
+  }
+
+  resetComInput(){
+    this.comInput=[]
+  }
+
+  setPlayerInput(ans){
+    this.PlayerInput=[]
+    for(let i=0;i<3;i++){
+      this.PlayerInput.push(Number(ans[i]))
+    }
+    return this.PlayerInput
+  }
 
   start(){
-    MissionUtils.Console.readLine('숫자 야구 게임을 시작합니다', (answer) => {
-    console.log(`닉네임: ${answer}`);
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.')
+    MissionUtils.Console.readLine('숫자를 입력해주세요:', (answer) => {
+    this.resetComInput()
+    this.setComInput()
+    console.log(this.setPlayerInput(answer))
     });
   }
 
@@ -12,5 +35,5 @@ class App {
   
 }
 const app = new App
-app.play()
+app.start()
 // module.exports = App;
