fd6abd8a0010896cff8f3d0bd74669bff6f9e2b3
Adult96 <djxor007@gmail.com>
2022-11-06 21:16:51 +0900

    test(random): Add random number method test case
    배열 안에 1~9까지의 숫자가 3개 들어있는 randomNumber 메서드 테스트 케이스 추가

--- __tests__/MethodTest.js
+++ __tests__/MethodTest.js
@@ -7,4 +7,10 @@ describe('App 클래스 메서드 테스트', () => {
     app.print('hello');
     expect(console.log).toHaveBeenCalledWith('hello');
   });
+
+  test('랜덤 숫자 생성 메서드', () => {
+    const result = app.creatRandomNumber();
+    expect(result).toHaveLength(3);
+    expect([1, 2, 3, 4, 5, 6, 7, 8, 9]).toEqual(expect.arrayContaining(result));
+  });
 });
