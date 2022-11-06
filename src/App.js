854bce310d4486918e7f4da719311af0fb59f99f
Sungjae.Kwak <sungjae8970@gmail.com>
2022-11-06 17:16:47 +0900

    feat : 임의의 숫자 3개 생성
    임의의 숫자 3개를 만들고 콘솔로 확인합니다
    확인용 출력하는 과정은 추후에 삭제합니다

--- src/App.js
+++ src/App.js
@@ -3,10 +3,22 @@ const { Console, Random } = require("@woowacourse/mission-utils");
 class App {
   play() {
     this.showStartMessage();
+    console.log(this.makeRandomNumber());
   }
 
   showStartMessage() {
     Console.print("숫자 야구 게임을 시작합니다.");
+  }
+
+  makeRandomNumber() {
+    const randomNumber = [];
+    while (randomNumber.length < 3) {
+      const number = Random.pickNumberInRange(1,9);
+      if (!randomNumber.includes(number)) {
+        randomNumber.push(number);
+      }
+    }
+    return randomNumber;
   }
 }
 
