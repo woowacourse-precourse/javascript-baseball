4c9f0499afbc58e296e2a24c06dc7bbd8b04f824
2wndrhs <2wndrhs@gmail.com>
2022-11-06 03:06:19 -0800

    style: 스네이크 케이스로 작성한 변수명 카멜 케이스로 수정

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 
 class App {
-  #RANDOM_NUMBER = [];
+  #randomNumber = [];
 
-  #USER_INPUT = [];
+  #userInput = [];
+
+  play() {
+    this.#randomNumber = App.getRandomNumber();
+    this.getUserInput();
+  }
 
   // 컴퓨터가 사용할 랜덤한 숫자를 구하는 기능
   static getRandomNumber() {
-    const RANDOM_SET = new Set();
-    while (RANDOM_SET.size < 3) {
-      RANDOM_SET.add(MissionUtils.Random.pickNumberInRange(1, 9));
+    const randomSet = new Set();
+    while (randomSet.size < 3) {
+      randomSet.add(MissionUtils.Random.pickNumberInRange(1, 9));
     }
-    const RANDOM_NUMBER = [...RANDOM_SET];
-    return RANDOM_NUMBER;
+    return [...randomSet];
   }
 
   // 사용자의 입력을 받는 기능
@@ -21,7 +25,7 @@ class App {
       if (!App.isValidUserInput(answer)) {
         throw new Error('잘못된 값을 입력하였습니다!');
       }
-      this.#USER_INPUT = [...answer].map(number => +number);
+      this.#userInput = [...answer].map(number => +number);
       this.getComputerOutput();
     });
   }
@@ -29,8 +33,7 @@ class App {
   // 사용자가 입력한 값이 적절한지 검증하는 기능
   static isValidUserInput(input) {
     const REGEX = /^[1-9]{3}$/;
-    const INPUT_SET = new Set(input);
-    if (REGEX.test(input) && INPUT_SET.size === 3) {
+    if (REGEX.test(input) && new Set(input).size === 3) {
       return true;
     }
     return false;
@@ -38,46 +41,41 @@ class App {
 
   // 사용자가 입력한 값을 판단하여 결과를 출력하는 기능
   getComputerOutput() {
-    const BALL_STRIKE_MAP = this.getBallAndStrikeMap();
-    const BALL_COUNT = BALL_STRIKE_MAP.get('BALL_COUNT');
-    const STRIKE_COUNT = BALL_STRIKE_MAP.get('STRIKE_COUNT');
+    const ballStrikeMap = this.getBallAndStrikeMap();
+    const ball = ballStrikeMap.get('BALL');
+    const strike = ballStrikeMap.get('STRIKE');
     let result = '';
-    if (!BALL_STRIKE_MAP.size) {
+    if (!ballStrikeMap.size) {
       result = '낫싱';
     }
-    if (BALL_STRIKE_MAP.size === 1) {
-      if (STRIKE_COUNT) {
-        result = `${STRIKE_COUNT}스트라이크`;
+    if (ballStrikeMap.size === 1) {
+      if (strike) {
+        result = `${strike}스트라이크`;
       }
-      if (BALL_COUNT) {
-        result = `${BALL_COUNT}볼`;
+      if (ball) {
+        result = `${ball}볼`;
       }
     }
-    if (BALL_STRIKE_MAP.size === 2) {
-      result = `${BALL_COUNT}볼 ${STRIKE_COUNT}스트라이크`;
+    if (ballStrikeMap.size === 2) {
+      result = `${ball}볼 ${strike}스트라이크`;
     }
     MissionUtils.Console.print(result);
   }
 
   // 사용자가 입력한 값을 볼, 스트라이크로 판단하는 기능
   getBallAndStrikeMap() {
-    const BALL_STRIKE_MAP = this.#USER_INPUT.reduce((acc, number, idx) => {
-      const IS_INCLUDED = this.#RANDOM_NUMBER.includes(number);
-      const RANDOM_INDEX = this.#RANDOM_NUMBER.indexOf(number);
-      if (IS_INCLUDED && idx !== RANDOM_INDEX) {
+    const ballStrikeMap = this.#userInput.reduce((acc, number, idx) => {
+      const isIncluded = this.#randomNumber.includes(number);
+      const targetIndex = this.#randomNumber.indexOf(number);
+      if (isIncluded && idx !== targetIndex) {
         acc.set('BALL_COUNT', (acc.get('BALL_COUNT') || 0) + 1);
       }
-      if (IS_INCLUDED && idx === RANDOM_INDEX) {
+      if (isIncluded && idx === targetIndex) {
         acc.set('STRIKE_COUNT', (acc.get('STRIKE_COUNT') || 0) + 1);
       }
       return acc;
     }, new Map());
-    return BALL_STRIKE_MAP;
-  }
-
-  play() {
-    this.#RANDOM_NUMBER = App.getRandomNumber();
-    this.getUserInput();
+    return ballStrikeMap;
   }
 }
 
