e476918a9cbfeded0ac896a54754022a7d35f10a
MI_MO <43203911+yujeonghyeop@users.noreply.github.com>
2022-11-07 00:43:45 +0900

    feat: 랜덤 숫자 생성 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const Notice = require("../const/Printexplain.js")
 class App {
   play() {}
+  generateRandomnumber(){
+    const Randomnumber = [];
+    while (Randomnumber.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!Randomnumber.includes(number)) {
+        Randomnumber.push(number);
+        }
+      }
+      return [...Randomnumber].join("");
+  }
 }
 
 module.exports = App;
