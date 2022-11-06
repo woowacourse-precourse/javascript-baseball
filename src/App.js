95978ae2d161f15d28ca37e49fc6294b1b188cf1
Gabriel Ju Hyun, Yoon <69189073+gabrielyoon7@users.noreply.github.com>
2022-11-06 19:56:03 +0900

    feat: 문제 출제 (컴퓨터의 숫자 생성)

--- src/App.js
+++ src/App.js
@@ -4,12 +4,19 @@ class App {
 
 
   play() {
+    const computerAnswer = this.generateRandomBallNumber();
+    // MissionUtils.Console.print(computerAnswer)
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
-    MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (answer) => {
-      if(!this.isValidBallNumber(answer)){
+    MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (userAnswer) => {
+      if(!this.isValidBallNumber(userAnswer)){
         throw new Error();
       }
     });
+  }
+
+  generateRandomBallNumber(){
+    const computerAnswer = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
+    return computerAnswer.join('');
   }
 
   isValidBallNumber(answer) {
@@ -24,6 +31,9 @@ class App {
     }
     return true
   }
+
+  
+
 }
 
 const init = new App();
