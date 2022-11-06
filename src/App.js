fffdd7513c2082d3bd69d4d15c318b5d2b29fd4e
cybersyn2 <quandonestpetit@outlook.com>
2022-11-06 19:17:48 +0900

    draft computer.initialize

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
   play() {}
+}
+
+class Computer {
+  constructor() {
+    this._digits = [];
+    this._initialize();
+  }
+
+  _initialize() {
+    this._digits.length = 0;
+    while (this._digits.length <= 3) {
+      const digit = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!this._digits.includes(digit)) {
+        this._digits.push(digit);
+      }
+    }
+  }
 }
 
 module.exports = App;
