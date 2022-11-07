bfceabc2b883e17d119f6a99f95a5c746715a46e
동동이 <drdragon777@naver.com>
2022-11-08 00:15:57 +0900

    feat - 플레이어의 입력의 유효성을 체크한다. 숫자를 입력하지 않았을 때, 예외를 발생시킨다.

--- src/Input.js
+++ src/Input.js
@@ -1,6 +1,10 @@
 const { END_NOTICE } = require("./Console");
 
 class Input {
+  checkNum(number) {
+    if (isNaN(number)) throw new Error("숫자를 입력해주세요.");
+  }
+
   checkOneOrTwo(input) {
     if (input !== "1" && input !== "2") throw new Error(END_NOTICE);
   }
