c1ad29c7f9d54f8b617a8e635f7449f4c8cd897d
kim-limit <rlawogks2468@naver.com>
2022-11-07 16:24:20 +0900

    feat: 'add select game start logic'

--- src/App.js
+++ src/App.js
@@ -18,6 +18,11 @@ function baseBallGame() {
     MissionUtils.Console.print(
       "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
     );
+
+    MissionUtils.Console.readLine("", (answer) => {
+      playGame = answer;
+      MissionUtils.Console.print(answer);
+    });
   }
 }
 
