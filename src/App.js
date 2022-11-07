16852338478c648ab50071eeb189313b4d2a98c4
hwnim5324 <hwnim5324@naver.com>
2022-11-07 15:34:22 +0900

    feat: 4-1 기능 구현.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+const User = require("./User");
+const Computer = require("./Computer");
+const BaseBall = require("./BaseBall");
+
 class App {
-  play() {}
+  
+  play(){
+    const ui = new User();
+    const computer = new Computer();
+    const baseball = new BaseBall();
+
+    while(true){
+      ui.setState('INGAME');
+      
+      do{
+        baseball.reset();
+        ui.scan();
+        baseball.referee(ui.getInput(), computer.getBalls());
+        ui.print(baseball.getResult());
+      }while(!baseball.isEnded());
+      
+      ui.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+      ui.setState('OUTGAME');
+      ui.scan();
+
+      if(Number(ui.getInput())===2) break;
+      computer.reset();
+    }
+  }
 }
 
 module.exports = App;
