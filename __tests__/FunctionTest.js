7011ec385e3da26350155c57a2b28a532e11fabe
Subin Jo <sbjo1202@naver.com>
2022-11-07 15:37:14 +0900

    test: 기능 1 게임 시작 문구 출력하기 테스트

---
+++ __tests__/FunctionTest.js
@@ -0,0 +1,22 @@
+const App = require("../src/App");
+const MissionUtils = require("@woowacourse/mission-utils");
+
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(MissionUtils.Console, "print");
+  logSpy.mockClear();
+  return logSpy;
+};
+
+describe("기능 테스트", () => {
+  test("기능 1, 게임 시작 문구 출력하기", () => {
+    const logSpy = getLogSpy();
+
+    const app = new App();
+    app.PrintGameStartPhrase();
+
+    expect(logSpy).toHaveBeenCalledWith(
+      expect.stringContaining('숫자 야구 게임을 시작합니다.')
+    );
+  });
+
+});
