11a3bc1e00bd5db747d1c5abf0ce15dd5a6ec5d3
seunghoria <102285042+seunghoria@users.noreply.github.com>
2022-11-07 16:26:23 +0900

    refactor: 결과 출력 기능 함수 파일 분리

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ const MissionUtils = require("@woowacourse/mission-utils");
 const isValidUser = require("./isValidUser.js");
 const getComputerNumber = require("./getComputerNumber.js");
 const countInput = require("./countInput.js");
+const printResult = require("./printResult.js");
 
 const mConsole = MissionUtils.Console;
 const GAME_NUMBER_LENGTH = 3;
@@ -27,27 +28,11 @@ class App {
           this.computerInputNumber,
           checkedUser
         );
-        this.printResult(countResultArr);
+        printResult(countResultArr);
         if (countResultArr[1] === GAME_NUMBER_LENGTH) this.checkRestart();
         else this.getInput();
       }
     });
-  }
-
-  printResult(count) {
-    if (count[1] === GAME_NUMBER_LENGTH) {
-      mConsole.print(
-        `${GAME_NUMBER_LENGTH}스트라이크\n${GAME_NUMBER_LENGTH}개의 숫자를 모두 맞히셨습니다! 게임 종료`
-      );
-    } else {
-      if (count[0] === 0 && count[1] === 0) mConsole.print("낫싱");
-      else
-        mConsole.print(
-          `${count[0] > 0 ? count[0] + "볼 " : ""}${
-            count[1] > 0 ? count[1] + "스트라이크" : ""
-          }`.trim()
-        );
-    }
   }
 
   checkRestart() {
