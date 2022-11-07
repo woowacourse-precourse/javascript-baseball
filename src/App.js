c88dd1882968d5ce7089637fa75dae7b5e053258
useon <yoosk4986@naver.com>
2022-11-08 05:36:48 +0900

    feat: 게임을 시작한다

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  play() {
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+    this.startGame();
+  }
+
+  startGame() {
+    this.inputComputerAnswer();
+    this.inputUserAnswer();
+  }
 }
 
 module.exports = App;
