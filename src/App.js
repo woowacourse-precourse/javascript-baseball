678a535a3ff7a09f9660b7c70c1eb901047979c9
saetakki <lime.lemonade03@gmail.com>
2022-11-08 00:49:12 +0900

    feat : get input value from user

--- src/App.js
+++ src/App.js
@@ -1,3 +1,15 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+const Console = MissionUtils.Console;
+
+const getUserInput = (f, num) => {
+  if (f(num) === false) {
+    throw '유효한 입력값이 아닙니다!';
+  }
+  return num;
+};
+
+
+
 class App {
   play() {}
 }
