99846868dd07a74ac4b02bff642024d6358a33ae
solo5star <solo_5@naver.com>
2022-11-07 16:14:57 +0900

    style: .eslintrc.js에 `ecmaVersion: 2022` 추가
    eslint의 자체 파서가 javascript 최신 문법을 인식하지 못하는
    오류가 있어 옵션을 추가하였음

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,8 @@ module.exports = {
   env: {
     node: true,
   },
+  parserOptions: {
+    ecmaVersion: 2022,
+  },
   extends: ['airbnb', 'plugin:jest/recommended', 'plugin:prettier/recommended'],
 };
