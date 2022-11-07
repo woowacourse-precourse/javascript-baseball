e4f935ec23ef8030d9153ae377c5ac097b8eefe2
MI_MO <43203911+yujeonghyeop@users.noreply.github.com>
2022-11-07 18:47:51 +0900

    test: caculateStrike 메소드 테스트

--- __tests__/MethodTest.js
+++ __tests__/MethodTest.js
@@ -8,6 +8,22 @@ const getLogSpy = () => {
 };
 
 describe("구현 기능 목록 테스트", () => {
-    
+
+    test("모두 같은자리 같은숫자일 때", () => {
+      const app = new App();
+      expect(app.caculateStrike("123", "123")).toEqual(3);
+    });
+    test("두개의 자리가 같은숫자일 때", () => {
+      const app = new App();
+      expect(app.caculateStrike("467", "457")).toEqual(2);
+    });
+    test("하나의 자리가 같은숫자일 때", () => {
+        const app = new App();
+        expect(app.caculateStrike("196", "124")).toEqual(1);
+    });
+    test("모두 자리가 다른 숫자일 때", () => {
+      const app = new App();
+      expect(app.caculateStrike("123", "678")).toEqual(0);
+    });  
     
   });
