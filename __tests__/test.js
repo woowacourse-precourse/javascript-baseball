00befccd8cf0dfba3cb5458f97922a0b0c941e59
yesolyo <ahnyelim77@gmail.com>
2022-11-07 16:43:34 +0900

    test: 게임 종료 후 완전 종료를 선택하였을때 테스트 코드

---
+++ __tests__/test.js
@@ -0,0 +1,49 @@
+const App = require("../src/App");
+const MissionUtils = require("@woowacourse/mission-utils");
+
+const mockQuestions = (answers) => {
+  MissionUtils.Console.readLine = jest.fn();
+  answers.reduce((acc, input) => {
+    return acc.mockImplementationOnce((question, callback) => {
+      callback(input);
+    });
+  }, MissionUtils.Console.readLine);
+};
+
+const mockRandoms = (numbers) => {
+  MissionUtils.Random.pickNumberInRange = jest.fn();
+  numbers.reduce((acc, number) => {
+    return acc.mockReturnValueOnce(number);
+  }, MissionUtils.Random.pickNumberInRange);
+};
+
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(MissionUtils.Console, "print");
+  logSpy.mockClear();
+  return logSpy;
+};
+
+describe("숫자 야구 게임", () => {
+  test("게임 종료 후 완전 종료하기", () => {
+    const randoms = [2, 3, 6];
+    const answers = ["246", "185", "632", "678", "236", "2"];
+    const logSpy = getLogSpy();
+    const messages = [
+      "2스트라이크",
+      "낫싱",
+      "2볼 1스트라이크",
+      "1볼",
+      "게임 종료",
+    ];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    const app = new App();
+    app.play();
+
+    messages.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+});
