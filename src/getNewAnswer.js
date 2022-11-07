03b0805dc65ffeca652895ccf1c02ef9138cc88b
Yesong <to06109@naver.com>
2022-11-07 17:25:22 +0900

    feat: 랜덤값 서로 다른 값인지 확인

--- src/getNewAnswer.js
+++ src/getNewAnswer.js
@@ -1,5 +1,9 @@
 const { Random } = require("@woowacourse/mission-utils");
 
+function checkDifferentValue(num) {
+  return !answer.includes(num);
+}
+
 function getRandomValue() {
   return Random.pickNumberInRange(1, 9);
 }
