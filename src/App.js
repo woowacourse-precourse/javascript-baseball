3409305a8feaafeecd3c091cf1a0db3e3aa07515
wlthdud <thdudzi@naver.com>
2022-11-07 18:48:41 +0900

    feat: 컴퓨터 랜덤숫자 정하기 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
-class App {
-  play() {}
+const MissionUtils = require("@woowacourse/mission-utils");
+
+class App{
+  play() { 
+
+    //컴퓨터 랜덤 숫자 정하기
+    const comNum = [];
+    while (comNum.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!comNum.includes(number)) {
+        comNum.push(number);
+      }
+    }
+    MissionUtils.Console.print(comNum);
+    
+ 
+  }
 }
+
 
 module.exports = App;
