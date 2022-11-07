f1633d508d2eacc30c99e5aecdd7d514513b969d
Narcoker <jykim9868@gmail.com>
2022-11-07 19:08:33 +0900

    Test: Add case 입력값에 0이 포함된 경우

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -205,6 +205,46 @@ describe("숫자 야구 게임", () => {
     }).toThrow(ERROR.NOT_DIFFERENCE_NUMBER);
   });
 
+  test("예외 테스트: 입력값에 0이 포함된 경우 1", () => {
+    const randoms = [1, 3, 5];
+    const answers = ["012"];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow(ERROR.NOT_THREE_NUMBER);
+  });
+
+  test("예외 테스트: 입력값에 0이 포함된 경우 2", () => {
+    const randoms = [1, 3, 5];
+    const answers = ["102"];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow(ERROR.NOT_THREE_NUMBER);
+  });
+
+  test("예외 테스트: 입력값에 0이 포함된 경우 3", () => {
+    const randoms = [1, 3, 5];
+    const answers = ["102"];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow(ERROR.NOT_THREE_NUMBER);
+  });
+
+
 
 
 });
