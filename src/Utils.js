40297144c38f150fee6ebd606d57902ba3f60bf8
BB-choi <choibobae0809@gmail.com>
2022-11-08 00:29:12 +0900

    fix: Utils 최소 숫자, 최대숫자 오류 해결

--- src/Utils.js
+++ src/Utils.js
@@ -1,19 +1,25 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const { Random } = MissionUtils;
+
 class Utils {
   constructor() {}
 
-  static #createUniqueNumberInList(array) {
-    let pickedNumber = Random.pickNumberInRange(this.minNum, this.maxNum);
+  static #createUniqueNumberInList({ array, minNumber, maxNumber }) {
+    let pickedNumber = Random.pickNumberInRange(minNumber, maxNumber);
 
     while (array.includes(pickedNumber)) {
-      pickedNumber = Random.pickNumberInRange(this.minNum, this.maxNum);
+      pickedNumber = Random.pickNumberInRange(minNumber, maxNumber);
     }
 
     return pickedNumber;
   }
 
-  static createUniqueNumbers(count) {
+  static createUniqueNumbers({ count, minNumber, maxNumber }) {
     const numbers = Array.from({ length: count }).reduce(
-      (prev) => [...prev, Utils.#createUniqueNumberInList(prev)],
+      (prev) => [
+        ...prev,
+        Utils.#createUniqueNumberInList({ array: prev, minNumber, maxNumber }),
+      ],
       []
     );
 
