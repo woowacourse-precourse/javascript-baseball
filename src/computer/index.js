f999b5d6cf42a407001834fd7bd2828732410d63
SNUPI <joohaem@daum.net>
2022-11-07 14:49:11 +0900

    style: change variable name (-computer)

--- src/computer/index.js
+++ src/computer/index.js
@@ -3,34 +3,35 @@ const { checkIncorrectNumber } = require("../util/computer/check");
 const { getPrint } = require("../util/computer/print");
 
 function Computer() {
-  const computerNumberArr = [];
-  while (computerNumberArr.length < 3) {
+  const answerNumberArr = [];
+
+  while (answerNumberArr.length < 3) {
     const number = MissionUtils.Random.pickNumberInRange(1, 9);
-    if (!computerNumberArr.includes(number)) {
-      computerNumberArr.push(number);
+
+    if (!answerNumberArr.includes(number)) {
+      answerNumberArr.push(number);
     }
   }
-  const computerNumberStr = computerNumberArr.join("");
+  const answerNumberStr = answerNumberArr.join("");
 
   function checkGameResult(numberStr) {
-    if (checkIncorrectNumber(numberStr))
-      return new Error("올바르지 않은 입력값입니다.");
+    if (checkIncorrectNumber(numberStr)) throw "올바르지 않은 입력값입니다.";
 
     let ball = 0;
     let strike = 0;
 
-    if (numberStr === computerNumberStr)
+    if (numberStr === answerNumberStr)
       return { isEnd: true, print: "3스트라이크" };
 
     [...numberStr].forEach((digit, i) => {
-      if (+digit === computerNumberArr[i]) return strike++;
-      if (computerNumberArr.includes(+digit)) return ball++;
+      if (+digit === answerNumberArr[i]) return strike++;
+      if (answerNumberArr.includes(+digit)) return ball++;
     });
 
     return { isEnd: false, print: getPrint(ball, strike) };
   }
 
-  return { computerNumberStr, checkGameResult };
+  return { answerNumberStr, checkGameResult };
 }
 
 module.exports = Computer;
