a0b00675ec0acb882f3ff22218c247cc833919a5
SeungWoo Kim <osabero@naver.com>
2022-11-07 19:42:39 +0900

    함수의 변수를 추가하고 컴퓨터 숫자 세개를 랜덤으로 주는 기능 코드를 수정했습니다.

--- src/App.js
+++ src/App.js
@@ -1,31 +1,71 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 class App {
   play() {
-    computer()
-    input()
+    baseBall()
+
   }
 }
-  
-function computer() {
-  const COM_NUMBER = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
-  console.log(COM_NUMBER)
-}
 
-function input() {
-  while(CHECK_ANSWER == false){
-    MissionUtils.Console.readLine('숫자를 입력해 주세요', (answer) => {
-      MissionUtils.Console.print('ㅠㅠ')
-      // inputcheck(answer);
-    })
-  };
-}
-
-function inputcheck(answer) {
-  if (answer.regExp == /(?:d{3})$/) {
-    throw exception()
+function baseBall() {
+  const COM_NUMBER = []
+  while (COM_NUMBER.length < 3) {
+    const number = MissionUtils.Random.pickNumberInRange(1, 9);
+    if (!COM_NUMBER.includes(number)) {
+      COM_NUMBER.push(number)
+    }
   }
+  inputAnswer(COM_NUMBER)
+}
+
+function inputAnswer(COM_NUMBER) {
+  MissionUtils.Console.readLine('숫자를 입력해 주세요', (input) => {
+    inputcheck(input, COM_NUMBER);
+  })
+
+}
+
+function inputcheck(input, COM_NUMBER) {
+  var input_array = []
+  for (let x of input) {
+    input_array.push(x)
+  }
+  var input_set = new Set(input_array);
+  // if (input !== /[1-9]{3}/ || input_set.length !== 3) {
+  //   throw exception()
+  // }
+  answerCheck(input_array, COM_NUMBER)
 }
 function exception() {
   MissionUtils.Console.print('잘못된 숫자를 입력하여 게임을 종료합니다.')
 }
+function answerCheck (input_array, COM_NUMBER) {
+  var strike = 0
+  var ball = 0
+  for (let i = 0; i<3 ; i++) {
+    if(input_array[i] == COM_NUMBER[i]) {
+      strike++;
+      continue}
+    if(COM_NUMBER.includes(input_array[i])) {
+      ball++
+      continue
+    }
+  }
+  resultprint(strike, ball)
+}
+function resultprint(strike, ball) {
+  var result = ''
+  if(strike==3){
+    result += '정답입니다!';
+    return newGame
+    }
+  if(strike==0 && ball==0){result += '낫싱'}
+  if(ball>0){result += `${ball}볼`}
+  if(strike>0){result = result + " " + `${strike}스트라이크`}
+  MissionUtils.Console.print(result)
+  return inputAnswer
+}
+function newGame (){
+
+}
+
 module.exports = App;
