bfffc169c493533cecc6868b632676eef8d3d398
minjun06 <35772631+minjun06@users.noreply.github.com>
2022-11-08 06:24:05 +0900

    Generate Random Number

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
-  play() {
-    
-  }
 
   constructor() {
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
   }
-}
+
+  play() {
+    this.setRandNum();
+    this.userInput();
+  }
+
+
+  setRandNum() {
+  const randNum = [];
+  while (randNum.length < 3) {
+    const number = MissionUtils.Random.pickNumberInRange(1, 9);
+    if (!randNum.includes(number)) {
+      randNum.push(number);
+    }
+  }
+  MissionUtils.Console.print('숫자 '+ randNum[0]+ randNum[1]+ randNum[2]);
+  };
+
+  userInput() {
+
+  };
+};
 
 const app = new App();
-app.play
+app.play();
+
 
 module.exports = App;
