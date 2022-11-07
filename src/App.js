84191b9f97c87be69cc95b2cd432d3bb12eaa322
MINJUN PARK <mj9457@naer,com>
2022-11-07 21:58:41 +0900

    feat. 사용자로부터 숫자 입력

--- src/App.js
+++ src/App.js
@@ -25,6 +25,18 @@ class App {
         MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
     }
 
+    inputNumber() {
+        MissionUtils.Console.readLine("숫자를 입력해주세요 : ",(NUMBER_ENTERED) => {
+            if(NUMBER_ENTERED) {
+                const number_converted_letter = toString(NUMBER_ENTERED);
+                const letter_converted_number = number_converted_letter.split('').map((letter) => Number(letter))
+                this.gamePlayerNumber.push(letter_converted_number);
+            } else {
+                throw "3개의 중복되지않는 숫자를 입력해주세요. 게임 종료";
+            }
+        })
+    }
+
 }
 
 module.exports = App;
