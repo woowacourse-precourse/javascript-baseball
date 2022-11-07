b1a873547a5dcc87f06a409603d77a87a36f725b
GyuriKimm <gyuri.kim.2@stonybrook.edu>
2022-11-08 04:16:52 +0900

    implement random function

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+const MISSION_Utils = require('@woowacourse/mission-utils');
 class App {
-  play() {}
+  play() {
+    
+  }
+  random(){
+    let arr = [];
+    while (arr.length < 3) {
+      const number = MISSION_Utils.Random.pickNumberInRange(1, 9);
+      if (!arr.includes(number)) {
+        arr.push(number);
+      } 
+    }
+  return arr;
+  }
 }
 
 module.exports = App;
