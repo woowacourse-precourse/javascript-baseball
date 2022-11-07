d7904d4563ea59aa4e52dfbdd05d4bdaa861b8d2
c0c0pang <coc1789@naver.com>
2022-11-07 20:52:12 +0900

    style: 함수 이름 수정

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ const MissionUtils = require("@woowacourse/mission-utils");
 class App {
   play() {
     const USER = new User();
-    GameStart(USER);
+    gameStart(USER);
   };
 };
 
@@ -12,14 +12,14 @@ class User {
     this.Number;
     this.Select_Number;
   };
-  GetNumber() {
+  getNumber() {
     return this.Number;
   };
-  GetSelect() {
+  getSelect() {
     return this.Select_Number;
   };
 };
-const ComputerNumber = () => {
+const computerNumber = () => {
   const NUMBER = [];
   while(NUMBER.length !==3){
     NUMBER.push(String(MissionUtils.Random.pickNumberInRange(1, 9)));
@@ -27,9 +27,9 @@ const ComputerNumber = () => {
   return NUMBER;
 };
 
-const GameStart = (USER) => {
+const gameStart = (USER) => {
   MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
-  let COMPUTER_NUMBER = ComputerNumber();
+  let COMPUTER_NUMBER = computerNumber();
   let USER_NUMBER;
   let USER_SELECT = "1";
   while (USER_SELECT !== "2") {
@@ -40,18 +40,18 @@ const GameStart = (USER) => {
     if (USER_NUMBER.length !== 3) {
       throw MissionUtils.Console.close();
     }
-    if (NumberCompare(COMPUTER_NUMBER, USER_NUMBER)) {
+    if (numberCompare(COMPUTER_NUMBER, USER_NUMBER)) {
       MissionUtils.Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
       MissionUtils.Console.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n', (select) => {
         USER.Select_Number = select;
         USER_SELECT = USER.Select_Number;
       });
-      COMPUTER_NUMBER = ComputerNumber();
+      COMPUTER_NUMBER = computerNumber();
     }
   }
 };
 
-const NumberCompare = (computer, user) => {
+const numberCompare = (computer, user) => {
   let BALL_SCORE = 0;
   let STRIKE_SCORE = 0;
   for (let index = 0; index < computer.length; index++) {
