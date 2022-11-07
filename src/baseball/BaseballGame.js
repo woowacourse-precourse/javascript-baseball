292007f782d29ba409f008df5b0f8f85d3a9d18b
Y2IN <dpdls0228@daum.net>
2022-11-08 00:43:53 +0900

    feat : 게임 시작 종료 구현

--- src/baseball/BaseballGame.js
+++ src/baseball/BaseballGame.js
@@ -15,6 +15,25 @@ class BaseballGame {
     if (this.FirstGame) {
       Console.print("숫자 야구 게임을 시작합니다.");
     }
-    Console.readline("숫자를 입력해주세요 : ", this.playerNumber);
+    Console.readline("숫자를 입력해주세요 : ", this.playing);
+  };
+
+  playing = (userInput) => {
+    ValidUserNumbers.isThreeNumberInRange(userInput);
+
+    let [strike, ball, nothing] = this.StrikeCount(
+      userInput,
+      this.computerNumbers
+    );
+    //프린트 출력
+    // console.log(typeof strike);
+    if (strike == 3) {
+      Console.readline(
+        "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n",
+        this.RestartEndDistinction
+      );
+      return;
+    }
+    this.playGame();
   };
 }
