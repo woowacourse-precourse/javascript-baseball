4b57757a5f3fc81aee0ab3b0a0c5a110df29bb18
JSH99 <tjgus9966@naver.com>
2022-11-07 21:18:22 +0900

    refactor(playGame): 매개변수명, 변수명 수정

--- src/App.js
+++ src/App.js
@@ -77,21 +77,21 @@ const printCompareResult = (ballCount, strikeCount) => {
     return false;
 }
 
-const playGame = (computerNum) => {
+const playGame = (computerNumArr) => {
     // 1. 숫자 입력
     const userNum = inputNumbers(); 
 
     // 2. 비교 
-    const comparedResult = compareWithInput(computerNum, userNum);
+    const comparedResult = compareWithInput(computerNumArr, userNum);
 
     // 3. 결과 출력
-    const resultMSG = printCompareResult(...comparedResult);
+    const isAnswer = printCompareResult(...comparedResult);
     
-    if(resultMSG) { // 정답일 때
+    if(isAnswer) { // 정답일 때
         MissionUtils.Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
         restartOrQuitGame();
     } else { // 정답이 아닐 때
-        playGame(computerNum);
+        playGame(computerNumArr);
     }
 }
 
