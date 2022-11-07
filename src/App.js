44e21d27f66ed0c45340c6809a2edf371089a0d8
yuu <22yuu@naver.com>
2022-11-07 16:59:44 +0900

    refactor: Game Class 내부함수 리팩토링 및 Builder Pattern 적용

--- src/App.js
+++ src/App.js
@@ -1,119 +1,14 @@
-const MissionUtils = require("@woowacourse/mission-utils");
-const Validation = require("./Validation.js");
+const GameBuilder = require("./Game.js");
 
 const LENGTH = 3;
 
-const QuestionText = Object.freeze({
-  startText: "숫자 야구 게임을 시작합니다.",
-  inputText: "숫자를 입력해주세요 : ",
-  endText: "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
-  redoText: "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
-});
-
 class App {
   constructor() {
-    this.io = MissionUtils.Console;
-    this.io.print(QuestionText.startText);
-    this.userInputArray = [];
-    this.target = this.setTarget();
-    this.validation = new Validation(LENGTH);
+    this.game = new GameBuilder().setTargetLength(LENGTH).build();
   }
 
   play() {
-    this.start();
-  }
-
-  onInput(text, callback) {
-    this.io.readLine(text, callback.bind(this));
-  }
-
-  start() {
-    this.onInput(QuestionText.inputText, this.onGame);
-  }
-
-  finish() {
-    try {
-      this.io.print(QuestionText.endText);
-      this.onInput(QuestionText.redoText, this.isRestart);
-      // this.io.close();
-    } catch {
-      new Error("경고:게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-    }
-  }
-
-  onGame(input) {
-    this.userInputArray = this.validation.validation(parseInt(input));
-
-    let ball = this.countBall();
-    let strike = this.countStrike();
-
-    this.io.print(this.showResult(ball, strike));
-
-    if (strike === LENGTH) {
-      this.finish();
-    }
-
-    this.onInput(QuestionText.inputText, this.onGame);
-  }
-
-  isRestart(input) {
-    let value = parseInt(input);
-    if (isNaN(value))
-      throw new Error(
-        "경고:게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
-      );
-
-    switch (value) {
-      case 1:
-        this.target = this.setTarget();
-        this.onInput(QuestionText.inputText, this.onGame);
-        break;
-      case 2:
-        this.io.close();
-        break;
-      default:
-        throw new Error("Not valid value");
-    }
-  }
-
-  setTarget() {
-    const computer = [];
-    while (computer.length < 3) {
-      const number = MissionUtils.Random.pickNumberInRange(1, 9);
-      if (!computer.includes(number)) computer.push(number);
-    }
-    console.log(computer);
-    return computer;
-  }
-
-  // game 관련
-  countStrike() {
-    let count = 0;
-
-    for (let i = 0; i < LENGTH; i++) {
-      if (this.target[i] === this.userInputArray[i]) count++;
-    }
-    return count;
-  }
-
-  countBall() {
-    let count = 0;
-
-    for (let i = 0; i < LENGTH; i++) {
-      if (
-        this.target.includes(this.userInputArray[i]) &&
-        this.target[i] !== this.userInputArray[i]
-      )
-        count++;
-    }
-    return count;
-  }
-
-  showResult(ball, strike) {
-    if (ball == 0 && strike == 0) return "낫싱";
-    else if (ball > 0 && strike == 0) return `${ball}볼`;
-    else if (ball == 0 && strike > 0) return `${strike}스트라이크`;
-    else return `${ball}볼 ${strike}스트라이크`;
+    this.game.play();
   }
 }
 
---
+++ src/Game.js
@@ -0,0 +1,139 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const Validation = require("./Validation.js");
+
+const QuestionText = Object.freeze({
+  startText: "숫자 야구 게임을 시작합니다.",
+  inputText: "숫자를 입력해주세요 : ",
+  endText: "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
+  redoText: "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
+});
+
+class GameBuilder {
+  setTargetLength(length) {
+    this.setTargetLength = length;
+    return this;
+  }
+
+  build() {
+    return new Game(this.setTargetLength);
+  }
+}
+
+class Game {
+  constructor(targetLength) {
+    this.targetLength = targetLength;
+
+    this.target = undefined;
+
+    this.io = MissionUtils.Console;
+    this.io.print(QuestionText.startText);
+
+    this.userInputArray = [];
+    this.validation = new Validation(this.targetLength);
+  }
+
+  onInput(text, callback) {
+    this.io.readLine(text, callback.bind(this));
+  }
+
+  init() {
+    this.target = this.setTarget();
+  }
+
+  start(text, callback) {
+    this.onInput(text, callback);
+  }
+
+  play() {
+    this.init();
+    this.start(QuestionText.inputText, this.onGame);
+  }
+
+  finish() {
+    try {
+      this.io.print(QuestionText.endText);
+      this.start(QuestionText.redoText, this.isRestart);
+    } catch {
+      new Error("경고:게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
+  }
+
+  onGame(input) {
+    this.userInputArray = this.validation.validation(parseInt(input));
+
+    const ball = this.countBall();
+    const strike = this.countStrike();
+
+    this.io.print(this.showResult(ball, strike));
+
+    if (this.isCorrect(strike)) this.finish();
+
+    this.start(QuestionText.inputText, this.onGame);
+  }
+
+  isCorrect(strike) {
+    return strike === this.targetLength;
+  }
+
+  isRestart(input) {
+    let value = parseInt(input);
+    if (isNaN(value))
+      throw new Error(
+        "경고:게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
+      );
+
+    switch (value) {
+      case 1:
+        this.play();
+        break;
+      case 2:
+        this.io.close();
+        break;
+      default:
+        throw new Error("Not valid value");
+    }
+  }
+
+  // game 관련
+  countStrike() {
+    let strike = 0;
+
+    this.target.map((digit, index) => {
+      if (digit === this.userInputArray[index]) strike++;
+    });
+
+    return strike;
+  }
+
+  countBall() {
+    let ball = 0;
+
+    this.userInputArray.map((digit, index) => {
+      let targetIdx = this.target.indexOf(digit);
+
+      if (targetIdx !== -1 && targetIdx !== index) ball++;
+    });
+
+    return ball;
+  }
+
+  showResult(ball, strike) {
+    if (ball == 0 && strike == 0) return "낫싱";
+    else if (ball > 0 && strike == 0) return `${ball}볼`;
+    else if (ball == 0 && strike > 0) return `${strike}스트라이크`;
+
+    return `${ball}볼 ${strike}스트라이크`;
+  }
+
+  setTarget() {
+    const computer = [];
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) computer.push(number);
+    }
+    console.log(computer);
+    return computer;
+  }
+}
+
+module.exports = GameBuilder;
