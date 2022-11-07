af06dd79adee26124b9817ee0958f2a4bff3809b
ilgon0110 <moses011010@gmail.com>
2022-11-07 22:04:12 +0900

    refactor: findAnswer Class 생성
    strike와 ball을 연산하는 기능을 따로 Class Component로 리팩토링

---
+++ src/FindAnswer.js
@@ -0,0 +1,46 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+
+const { Console } = MissionUtils;
+
+class FindAnswer {
+  constructor() {
+    this.strike = 0;
+    this.ball = 0;
+  }
+
+  getAnswer(computerNum, userInput) {
+    this.strike = 0;
+    this.ball = 0;
+    this.isStrike(computerNum, userInput);
+    this.isBall(computerNum, userInput);
+    const strikeText = this.strike ? `${this.strike}스트라이크` : '';
+    const ballText = this.ball ? `${this.ball}볼` : '';
+    if (this.strike || this.ball) {
+      Console.print(`${ballText} ${strikeText}`.trim());
+    } else {
+      Console.print('낫싱');
+    }
+    return { strike: this.strike, ball: this.ball };
+  }
+
+  isStrike(computerNum, userInput) {
+    computerNum.split('').forEach((num, idx) => {
+      if (computerNum[idx] === userInput[idx]) {
+        this.strike += 1;
+      }
+    });
+  }
+
+  isBall(computerNum, userInput) {
+    computerNum.split('').forEach((num, idx) => {
+      if (
+        computerNum[idx] !== userInput[idx] &&
+        userInput.includes(computerNum[idx])
+      ) {
+        this.ball += 1;
+      }
+    });
+  }
+}
+
+module.exports = FindAnswer;
