cd5d40d16b5bb784ec5dde87351e6c58f81c6169
Sungjae.Kwak <sungjae8970@gmail.com>
2022-11-07 16:02:56 +0900

    refactor : 모듈화된 파일 App에 적용
    - 게임 실행 메소드를 적용함
    - 완전 분리를 시키면서 App에서 필요없던 모듈은
    불러오지 않도록 코드를 수정(line1~line2)

--- src/App.js
+++ src/App.js
@@ -1,82 +1,12 @@
-const { Console, Random } = require("@woowacourse/mission-utils");
-const { validateThreeFigures, validateNextAction } = require("./modules/validation");
-const { showStartMessage, showCountMessage, showCorrectMessage, showNothingMessage } = require("./modules/showMessage");
+const { showStartMessage } = require("./modules/showMessage");
+const { makeRandomNumber ,getUsersPrediction } = require("./modules/playGameLoop");
 
 class App {
   play() {
     showStartMessage();
-    const randomNumber = this.makeRandomNumber();
+    const randomNumber = makeRandomNumber();
     console.log(randomNumber);
-    this.getUsersPrediction(randomNumber);
-  }
-
-  makeRandomNumber() {
-    const randomNumber = [];
-    while (randomNumber.length < 3) {
-      const number = Random.pickNumberInRange(1,9);
-      if (!randomNumber.includes(number)) {
-        randomNumber.push(number);
-      }
-    }
-    return randomNumber;
-  }
-
-  getUsersPrediction(randomNumber) {
-    Console.readLine('숫자를 입력해주세요 : ', (prediction) => {
-      console.log(prediction);
-      if (!validateThreeFigures(prediction)) {
-        throw '잘못된 값을 입력했습니다!';
-      }
-      const convertedNumber = prediction.split('').map(Number);
-      console.log(convertedNumber);
-      if (this.isRightAnswer(randomNumber, convertedNumber)) {
-        showCorrectMessage();
-        this.getUsersNextAction();
-      } else if (this.isNothing(randomNumber, convertedNumber)) {
-        showNothingMessage();
-        this.getUsersPrediction(randomNumber);
-      } else {
-        const [ballCount, strikeCount] = this.calculateCount(randomNumber, convertedNumber);
-        showCountMessage(ballCount, strikeCount);
-        this.getUsersPrediction(randomNumber);
-      }
-    })
-  }
-
-  isRightAnswer(randomNumber, userInput) {
-    return randomNumber.join('') === userInput.join('');
-  }
-
-  isNothing(randomNumber, userInput) {
-    const union = new Set([...randomNumber, ...userInput]);
-    return union.size === 6;
-  }
-
-  calculateCount(randomNumber, userInput) {
-    let ballCount = 0;
-    let strikeCount = 0;
-    randomNumber.forEach((number, index) => {
-      if (userInput.includes(number) && index !== userInput.indexOf(number)) {
-        ballCount++;
-      } else if(userInput[index] === number) {
-        strikeCount++;
-      }
-    });
-    return [ballCount, strikeCount];
-  }
-
-  getUsersNextAction() {
-    Console.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.', (userInput) => {
-      if (!validateNextAction(userInput)) {
-        throw '잘못된 값을 입력했습니다!'
-      } else if (userInput === '1') {
-        const randomNumber = this.makeRandomNumber();
-        console.log(randomNumber)
-        this.getUsersPrediction(randomNumber);
-      } else if (userInput === '2') {
-        Console.close();
-      }
-    })
+    getUsersPrediction(randomNumber);
   }
 }
 
