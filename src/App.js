df14f9c83f08fc1baf5fbda52076fced346ff802
Monte <monte6198@gmail.com>
2022-11-07 19:02:16 +0900

    feat: 상대방 수 생성 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  constructor() {
+    this.opponent = new Opponent(); //상대방 등장
+    this.opponent.setRandomNumber(); //상대방이 숫자를 랜덤으로 지정
+  }
+  play() {
+    MissionUtils.Console.print(`상대방의 숫자는 ${this.opponent.number} 입니다`);
+  }
+}
+
+class Opponent {
+  //상대방 관련 클래스
+  constructor() {
+    this.number = []; //빈 숫자 배열 생성
+  }
+  setRandomNumber() {
+    //랜덤으로 숫자 3개를 추출하여 배열에 저장(중복 없음)
+    while (this.number.length < 3) {
+      const randomNumber = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!this.number.includes(randomNumber)) {
+        // 중복 숫자 없어야 함
+        this.number.push(randomNumber);
+      }
+    }
+  }
 }
 
 module.exports = App;
