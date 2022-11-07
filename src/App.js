7a225a73ee488472e0fa2cb5f925ab2cfbc1b864
pione3r <jhyong0626@gmail.com>
2022-11-07 23:24:16 +0900

    refactor: play game function
    refactor play game function to recursive

--- src/App.js
+++ src/App.js
@@ -2,18 +2,14 @@ const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
   play() {
-    let computerNumber = this.setComputerNumber();
-
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    this.playNewGame();
+  }
 
-    while (true) {
-      let userNumber = this.setUserNumber();
-      let compareResult = this.compareNumbers(computerNumber, userNumber);
-      let restart = this.restartGame(compareResult);
-      if (restart == "1") computerNumber = this.setComputerNumber();
-      else if (restart == "2") break;
-      else continue;
-    }
+  playNewGame() {
+    let computerNumber = this.setComputerNumber();
+    let userNumber = this.setUserNumber();
+    this.compareNumbers(computerNumber, userNumber);
   }
 
   setComputerNumber() {
@@ -34,9 +30,10 @@ class App {
       userNumber = answer;
     });
 
-    if (userNumber.length !== 3) throw "숫자 입력은 3자리만 가능합니다";
-    else if (isNaN(userNumber)) throw "숫자가 아닙니다";
-    else if (Number(userNumber) < 0) throw "음수를 입력했습니다";
+    if (userNumber.length !== 3)
+      throw new Error("숫자 입력은 3자리만 가능합니다");
+    else if (isNaN(userNumber)) throw new Error("숫자가 아닙니다");
+    else if (Number(userNumber) < 0) throw new Error("음수를 입력했습니다");
     else return userNumber;
   }
 
@@ -57,7 +54,9 @@ class App {
 
     this.printResult(result);
 
-    return result;
+    if (result != "3스트라이크")
+      this.compareNumbers(computerNumber, this.setUserNumber());
+    else return result;
   }
 
   printResult(result) {
@@ -65,24 +64,19 @@ class App {
 
     if (result == "3스트라이크") {
       MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+      this.restartGame();
     }
   }
 
-  restartGame(compareResult) {
-    if (compareResult == "3스트라이크") {
-      MissionUtils.Console.print(
-        "게임을 시작하려면 1, 종료하려면 2를 입력하세요."
-      );
-
-      let restart = "";
-      MissionUtils.Console.readLine("", (answer) => {
-        if (answer != "1" && answer != "2") throw "잘못 입력하셨습니다.";
-
-        restart = answer;
-      });
-
-      return restart;
-    } else return;
+  restartGame() {
+    MissionUtils.Console.print(
+      "게임을 시작하려면 1, 종료하려면 2를 입력하세요."
+    );
+    MissionUtils.Console.readLine("", (answer) => {
+      if (answer == "1") this.playNewGame();
+      else if (answer == "2") MissionUtils.Console.close();
+      else throw new Error("잘못 입력하셨습니다.");
+    });
   }
 }
 
