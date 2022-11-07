e69cda1095317cefd8e43b33ea25f40327dc8cbd
Yoonkyoung Kim <100656920+Yoonkyoungme@users.noreply.github.com>
2022-11-07 16:58:10 +0900

    refactor: 게임 시작 기능 분리

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ const MissionUtils = require("@woowacourse/mission-utils");
 class App {
   play() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    this.playGame();
+  }
+
+  // 게임 시작
+  playGame() {
     const computerNumber = this.createComputerNumber();
-    const userNumber = this.inputUserNumber(computerNumber);
-    // console.log(computerNumber, userNumber);
+    this.inputUserNumber(computerNumber);
   }
 
   // 컴퓨터의 숫자 랜덤으로 생성한다. (1부터 9까지 서로 다른 수로 이루어진 3자리의 수)
@@ -105,6 +109,7 @@ class App {
     }
 
     MissionUtils.Console.print(result);
+    return result;
   }
 }
 
