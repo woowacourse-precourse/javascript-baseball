fbc68cbfffd2ef36724c3d9a509696278842b718
JSH99 <tjgus9966@naver.com>
2022-11-07 21:32:02 +0900

    feat(restartOrQuitGame): 재시작 선택 안내 문구 출력

--- src/App.js
+++ src/App.js
@@ -99,7 +99,8 @@ const playGame = (computerNumArr) => {
 
 const restartOrQuitGame = () => {
   MissionUtils.Console.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.', (endOption) => {
-    MissionUtils.Console.print(endOption);
+    MissionUtils.Console.print(`게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.${endOption}`);
+
     if (!isVaildEndOption(endOption)) {
       throw new Error('잘못된 값입니다. 1 또는 2를 입력해주세요.');
     }
