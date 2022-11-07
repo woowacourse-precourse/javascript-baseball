ccd0ff2d91640488db9949c67816b380797f2628
Chanwoong1 <jcw7468@gmail.com>
2022-11-07 22:54:00 +0900

    test: ComputerNumbers에 대한 테스트 케이스 추가

---
+++ __tests__/ComputerNumberTest.js
@@ -0,0 +1,63 @@
+const { Console } = require('@woowacourse/mission-utils');
+
+const {
+  randomSelectComputerNumbers
+} = require('../src/game-utils/ComputerNumbers');
+
+afterAll(() => {
+  Console.close();
+});
+
+describe('string 타입으로 반환되는지 확인', () => {
+  test('case1', () => {
+    expect(typeof randomSelectComputerNumbers()).toBe('string');
+  });
+
+  test('case2', () => {
+    expect(typeof randomSelectComputerNumbers()).toBe('string');
+  });
+
+  test('case3', () => {
+    expect(typeof randomSelectComputerNumbers()).toBe('string');
+  });
+
+  test('case4', () => {
+    expect(typeof randomSelectComputerNumbers()).toBe('string');
+  });
+});
+
+describe('길이가 3으로 반환되는지 확인', () => {
+  test('case1', () => {
+    expect(randomSelectComputerNumbers().length).toBe(3);
+  });
+
+  test('case2', () => {
+    expect(randomSelectComputerNumbers().length).toBe(3);
+  });
+
+  test('case3', () => {
+    expect(randomSelectComputerNumbers().length).toBe(3);
+  });
+
+  test('case4', () => {
+    expect(randomSelectComputerNumbers().length).toBe(3);
+  });
+});
+
+describe('반환된 문자가 1부터 9까지의 숫자인지 확인', () => {
+  test('case1', () => {
+    expect([...randomSelectComputerNumbers()].every((element) => (element >= '1' && element <= '9'))).toBe(true);
+  });
+
+  test('case2', () => {
+    expect([...randomSelectComputerNumbers()].every((element) => (element >= '1' && element <= '9'))).toBe(true);
+  });
+
+  test('case3', () => {
+    expect([...randomSelectComputerNumbers()].every((element) => (element >= '1' && element <= '9'))).toBe(true);
+  });
+
+  test('case4', () => {
+    expect([...randomSelectComputerNumbers()].every((element) => (element >= '1' && element <= '9'))).toBe(true);
+  });
+});
