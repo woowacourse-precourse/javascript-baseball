c44bab14149db9981b40c55429f1bbc65742cd06
chchaeun <kce3615@naver.com>
2022-11-07 15:51:42 +0900

    feat: 입력 예외 테스트 케이스 작성

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -47,9 +47,87 @@ describe("숫자 야구 게임", () => {
     });
   });
 
-  test("예외 테스트", () => {
+  test("숫자가 아닌 입력 확인", () => {
+    const randoms = [1, 3, 5];
+    const answers = ["abc"];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow();
+  });
+
+  test("정수가 아닌 입력 확인", () => {
+    const randoms = [1, 3, 5];
+    const answers = ["12.3"];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow();
+  });
+
+  test("세자리 수보다 큰 입력 확인", () => {
     const randoms = [1, 3, 5];
     const answers = ["1234"];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow();
+  });
+
+  test("세자리 수보다 작은 입력 확인", () => {
+    const randoms = [1, 3, 5];
+    const answers = ["1"];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow();
+  });
+
+  test("세자리 수 중 같은 수가 있는 입력 확인", () => {
+    const randoms = [1, 3, 5];
+    const answers = ["113"];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow();
+  });
+
+  test("재시작 입력이 정수인지 확인", () => {
+    const randoms = [1, 3, 5];
+    const answers = ["135", "$%^"];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow();
+  });
+
+  test("재시작 입력이 1 또는 2인지 확인", () => {
+    const randoms = [1, 3, 5];
+    const answers = ["135", "4"];
 
     mockRandoms(randoms);
     mockQuestions(answers);
