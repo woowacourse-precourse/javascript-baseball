3b339d53c28a75f9fff2cdb09370dde49c5f3a1b
leesoomin <soomin@leesoominui-MacBookPro.local>
2022-11-08 05:46:33 +0900

    feat: 사용자 입력 예외 처리 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
 class App {
-  play() {}
+  play() {
+
+  }
+
+  // 사용자 입력 받기 1 예외처리
+  checkExceptionOne(num){
+    if(isNan(num)){
+      throw new Error("숫자를 입력하세요.");
+    }else if(num.charAt(0) === 0 || num.charAt(1) === 0 || num.charAt(2) === 0){
+      throw new Error("1-9 사이의 숫자를 3번 입력하세요.");
+    } else if(num.charAt(0) === " " || num.charAt(1) === " " || num.charAt(2) === " "){
+      throw new Error("공백을 포함하지 않는 숫자를 입력하세요.");
+    } else if(num.length !== 3){
+      throw new Error("세자리 숫자를 입력하세요.");
+    } else if(num.charAt(0) === num.charAt(1)|| num.charAt(0) === num.charAt(2) || num.charAt(1) === num.charAt(2)){
+      throw new Error("중복되지 않는 숫자를 입력하세요.");
+    } else {
+      return 1;
+    }
+  }
+  // 사용자 입력 받기 2 예외처리
+  checkExceptionTwo(input){
+    if(input === "1" || input === "2"){
+      return 1;
+    } else {
+      throw new Error("1 또는 2를 입력하세요.");
+    }
+  }
 }
 
 module.exports = App;
