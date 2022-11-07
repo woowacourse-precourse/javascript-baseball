1c9ababf4e134b268cc3fcf54cf727c92ad33c75
sh981013s <sh981013s@gmail.com>
2022-11-07 20:14:44 +0900

    feat: Controller 유저가 제시한 수 결과 도출 메서드 구현

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -20,6 +20,24 @@ class Controller {
     this.userGivenNumber.setState(userGivenNumber);
   }
 
+  // 유저가 제시한 수에 따라 결과를 도출한다.
+  getSingleTryResult() {
+    const userGivenNumber = this.userGivenNumber.getState();
+    const computerGivenNumber = this.computerNumber.getState();
+
+    const strikeBallCount = [0, 0];
+
+    for (let i = 0; i < userGivenNumber.length; i++) {
+      if (userGivenNumber[i] === computerGivenNumber[i]) {
+        strikeBallCount[0]++;
+      } else if (computerGivenNumber.includes(userGivenNumber[i])) {
+        strikeBallCount[1]++;
+      }
+    }
+
+    this.view.printSingleTryResult(strikeBallCount);
+  }
+
   // 유저가 제시한 수에 문제가 없는지 확인한다.
   checkIsUserInputValid() {
     const isUserInputValid = this.validation.getIsUserGuessInputValid(
@@ -30,6 +48,9 @@ class Controller {
     if (!isUserInputValid) {
       throw new Error(this.view.WRONG_COMMENT);
     }
+
+    // 문제가 없다면 다음 단계로
+    this.getSingleTryResult();
   }
 }
 
