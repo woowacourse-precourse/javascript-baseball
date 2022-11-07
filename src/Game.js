78f31d4843baead44cdb5aba5f2fd6deeb6ca8d4
DAIN KIM <dnkim419@gmail.com>
2022-11-08 01:06:21 +0900

    feat(기능4): 입력한 숫자에 대한 결과 출력

--- src/Game.js
+++ src/Game.js
@@ -27,6 +27,7 @@ class Game {
     Console.readLine('숫자를 입력해주세요 : ', (inputStr) => {
       const guess = this.isValidInput(inputStr);
       const { ball, strike } = this.judgeBallStrike(guess, this.answer);
+      this.printHint(ball, strike);
     });
   }
 
@@ -62,6 +63,13 @@ class Game {
     });
 
     return { ball, strike };
+  }
+
+  printHint(ball, strike) {
+    if (ball === 0 && strike === 0) Console.print('낫싱');
+    if (ball > 0 && strike === 0) Console.print(`${ball}볼`);
+    if (ball === 0 && strike > 0) Console.print(`${strike}스트라이크`);
+    if (ball > 0 && strike > 0) Console.print(`${ball}볼 ${strike}스트라이크`);
   }
 }
 
