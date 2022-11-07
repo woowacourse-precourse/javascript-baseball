a4c5d74fcb97566bc266b70d1f26a045f69b0609
0dytpq0 <rkfnahs12e@gmail.com>
2022-11-08 03:58:13 +0900

    <fix> : <리팩토링>
    출력 값 (백틱)사용
    reset함수 변경
    각종 불필요한 것들 삭제
    
    좀 더 보기편하게 만들고자 (백틱)들을 사용함
    reset함수를 variableReset으로 좀더 눈에 보이게 이름을 바꿈

--- src/App.js
+++ src/App.js
@@ -1,15 +1,6 @@
 
 const MissionUtils = require("@woowacourse/mission-utils");
 
-// // 1. 컴퓨터의 수 3자리 구하기.
-// let machineNum = [];
-// while (machineNum.length < 3) {
-//   let number = MissionUtils.Random.pickNumberInRange(1, 9);
-//   if (!machineNum.includes(number)) {
-//     machineNum.push(number);
-//   }
-// }
-
 class App {
   constructor() {
 
@@ -29,7 +20,7 @@ class App {
 
   }
 
-  // 3. 3. 컴퓨터와 플레이어의 값을 비교해 스트라이크,볼의 값을 구함.
+  // 3. 컴퓨터와 플레이어의 값을 비교해 스트라이크,볼의 값을 구함.
   // strike 갯수 확인 함수
   checkStrike(arr) {
     for (let j = 0; j < arr.length; j++) {
@@ -51,25 +42,29 @@ class App {
     return this.ball
   }
   // 새 게임을 시작할 때 변수 값들 초기화 함수.
-  reSet() {
+  variableReSet() {
     this.ball = 0;
     this.strike = 0;
     this.checkBallArray.length = 0;
     this.inputNum.length = 0;
   }
 
+  machineNumReset() {
+    this.arr.length = 0;
+    while (this.arr.length < 3) {
+      let number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!this.arr.includes(number)) {
+        this.arr.push(number);
+      }
+    }
+  }
   // 게임 재시작 함수(컴퓨터 입력값 초기화)
   //  4. 게임을 종료한 후 게임을 다시 시작하거나 완전히 종료할 수 있다.
   rePlay() {
     MissionUtils.Console.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.', (answer) => {
       if (answer === '1') {
-        this.arr.length = 0;
-        while (this.arr.length < 3) {
-          let number = MissionUtils.Random.pickNumberInRange(1, 9);
-          if (!this.arr.includes(number)) {
-            this.arr.push(number);
-          }
-        }
+        this.variableReSet();
+        this.machineNumReset();
         this.newplay();
       } else {
         MissionUtils.Console.close();
@@ -102,42 +97,36 @@ class App {
       this.exception(this.inputNum);
       const strike = this.checkStrike(this.arr);
       const ball = this.checkBall(this.arr);
-      if (strike === 3) {
-        MissionUtils.Console.print(strike + "스트라이크");
-        MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
 
-        this.reSet();
+      // 결과 값을 위한 조건식들
+      if (strike === 3) {
+        MissionUtils.Console.print(`${strike}스트라이크`);
+        MissionUtils.Console.print(`3개의 숫자를 모두 맞히셨습니다! 게임 종료`);
         this.rePlay();
       } else if (strike > 0 && ball === 0) {
-        MissionUtils.Console.print(strike + '스트라이크');
-
+        MissionUtils.Console.print(`${strike}스트라이크`);
         this.play();
       } else if (ball > 0 && strike === 0) {
-        MissionUtils.Console.print(ball + '볼');
-
+        MissionUtils.Console.print(`${ball}볼`);
         this.play();
       } else if (strike === 0 && ball === 0) {
-        MissionUtils.Console.print('낫싱');
-
+        MissionUtils.Console.print(`낫싱`);
         this.play();
       } else if (strike > 0 && ball > 0) {
-        MissionUtils.Console.print(ball + '볼 ' + strike + '스트라이크');
-
+        MissionUtils.Console.print(`${ball}볼 ${strike}스트라이크`);
         this.play();
       }
     });
   }
 
   play() {
-    console.log(this.arr);
     // 2. 게임 플레이어는 컴퓨터가 생각하고 있는 서로 다른 3개의 숫자를 입력
     if (this.inputNum.length === 0) {
       MissionUtils.Console.print("숫자 야구 게임을 시작합니다.")
-      this.reSet();
       this.newplay();
     }
     else {
-      this.reSet();
+      this.variableReSet();
       this.newplay();
     }
   }
