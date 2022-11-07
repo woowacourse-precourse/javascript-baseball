01415f1ea72faa76ada82fae2339db29a262d20a
hafnium1923 <hafnium1923@gmail.com>
2022-11-08 00:49:13 +0900

    feat reGame
    재시작 함수 구현

--- src/App.js
+++ src/App.js
@@ -6,24 +6,28 @@ class App {
 
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
     const RANDOM = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
-    console.log(RANDOM); // 콘솔
-    let continueGame = this.startGame(RANDOM);
-    // if(continueGame != 0) {
-    //    MissionUtils.Console.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.', (reStart) => {
-    //     console.log('12312');
-    //     continueGame = reStart;
-    //     MissionUtils.Console.close();
-    // })
-    //}
+    this.startGame(RANDOM);
   }
   
-  startGame(RANDOM){ 
+  reGame() {
+
+    MissionUtils.Console.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.', (wantGame) => {
+      if(wantGame === '1'){
+        const RANDOM = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
+        this.startGame(RANDOM);
+      }else if(wantGame === '2'){
+      return MissionUtils.Console.close();
+      }
+      
+    })
+  }
+  startGame(RANDOM) { 
         
     MissionUtils.Console.readLine('숫자를 입력해주세요', (user_num) => {
       let flag = this.checkNum(RANDOM,user_num);
       if(flag){
         MissionUtils.Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
-        MissionUtils.Console.close();
+        this.reGame();
       }
     })
   }
@@ -36,13 +40,11 @@ class App {
     }
     let strike = [];
     let j = -1;
-    console.log(bucketNum); // 콘솔
     for(let i = 0; i < bucketNum.length; i++){
       if(bucketNum[i] === RANDOM[i]){
         strike[++j] = bucketNum[i];
       }
     }
-    console.log(strike); // 콘솔
     let ball = RANDOM.filter(x => bucketNum.includes(x)).filter(x => !strike.includes(x));
     this.print(strike.length, ball.length);
 
@@ -52,7 +54,7 @@ class App {
       this.startGame(RANDOM);
   }
 
-  print(strike, ball){
+  print(strike, ball) {
     if(ball != 0 && strike === 0){
       MissionUtils.Console.print(`${ball}볼`);
     }else if(strike != 0 && ball === 0){
