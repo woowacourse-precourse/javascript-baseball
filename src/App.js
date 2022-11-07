6ced3678d609af6eaa637d5242f534f1e9f57d95
lee-il-woo <empty001430@gmail.com>
2022-11-07 17:45:07 +0900

    Refactor: 변경 변수명 및 함수의 순서
    중복되거나 알아보기 힘든 변수명 변경, 코드의 흐름을 알 수 있게 함수의 순서 변경

--- src/App.js
+++ src/App.js
@@ -1,79 +1,23 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
 function checkUserNumber(user_number) {
-  let user_numbers = String(user_number)
-    .split("")
+  console.log(Number(user_number))
+  if(isNaN(user_number)) throw new Error("숫자만 입력해 주세요");
+  let user_numbers = user_number.split("")
     .map((number) => Number(number));
-  if (user_numbers.length != 3) throw new Error("에러남");
-  if (new Set(user_numbers).size != 3) throw new Error("에러남2");
+  if (user_numbers.length != 3) throw new Error("입력값이 세자리가 아닙니다");
+  if (new Set(user_numbers).size != 3) throw new Error("중복된 값이 있습니다");
   return user_numbers;
 }
 
-function getComputerNumbers() {
-  let computer_numbers = [];
-  while(computer_numbers.length != 3){
-    let computer_number = MissionUtils.Random.pickNumberInRange(1,9);
-    if(!computer_numbers.includes(computer_number)){
-      computer_numbers.push(computer_number)
-    }
-  }
-  return computer_numbers;
-}
-
-function game(computer_numbers) {
-  let score = { strike: 0, ball: 0 };
-  MissionUtils.Console.readLine("숫자를 입력해주세요.", (user_number) => {
-    let user_numbers = checkUserNumber(user_number);
-    let strike = getStrike(computer_numbers, user_numbers, score);
-    let ball = getBall(computer_numbers, user_numbers, score);
-    gameResult(strike,ball);
-    if (strike == 3) return winGame();
-    return game(computer_numbers);
-  });
-}
-
-function gameResult(strike,ball){
-  if (strike == 0 && ball == 0) return MissionUtils.Console.print("낫싱");
-  if(strike == 0) return MissionUtils.Console.print(`${ball}볼`);
-  if(ball == 0) return MissionUtils.Console.print(`${strike}스트라이크`);
-  if(strike != 0 && ball != 0) return MissionUtils.Console.print(`${ball}볼 ${strike}스트라이크`);
-}
-
-function setgame() {
-  let computer_numbers = getComputerNumbers();
-  game(computer_numbers);
-}
-
-function winGame() {
-  MissionUtils.Console.readLine(
-    "재시작 1 종료 2",
-    (renumber) => {
-      restart(renumber);
-    }
-  );
-}
-
-function restart(renumber) {
-  if (renumber == 1) {
-    return setgame();
-  }
-  if (renumber == 2) {
-    MissionUtils.Console.print("게임 종료");
-    return MissionUtils.Console.close();
-  }
-  if (renumber != 1 || renumber != 2) {
-    throw new Error("잘못된 번호");
-  }
-}
-
-function getStrike(computer_numbers, user_numbers, score) {
+function getStrikeScore(computer_numbers, user_numbers, score) {
   computer_numbers.map((element, index) => {
     if (user_numbers[index] == element) score.strike = score.strike + 1;
   });
   return score.strike;
 }
 
-function getBall(computer_numbers, user_numbers, score) {
+function getBallScore(computer_numbers, user_numbers, score) {
   computer_numbers.map((element, index) => {
     if (
       user_numbers.indexOf(element) != -1 &&
@@ -84,9 +28,71 @@ function getBall(computer_numbers, user_numbers, score) {
   return score.ball;
 }
 
+function gameResult(strike_score, ball_score) {
+  if (strike_score == 0 && ball_score == 0)
+    return MissionUtils.Console.print("낫싱");
+  if (strike_score == 0) return MissionUtils.Console.print(`${ball_score}볼`);
+  if (ball_score == 0)
+    return MissionUtils.Console.print(`${strike_score}스트라이크`);
+  if (strike_score != 0 && ball_score != 0)
+    return MissionUtils.Console.print(
+      `${ball_score}볼 ${strike_score}스트라이크`
+    );
+}
+
+function game(computer_numbers) {
+  let score = { strike: 0, ball: 0 };
+  MissionUtils.Console.readLine("숫자를 입력해주세요", (user_number) => {
+    let user_numbers = checkUserNumber(user_number);
+    let strike_score = getStrikeScore(computer_numbers, user_numbers, score);
+    let ball_score = getBallScore(computer_numbers, user_numbers, score);
+    gameResult(strike_score, ball_score);
+    if (strike_score == 3) return gameResultAfter();
+    return game(computer_numbers);
+  });
+}
+
+function getComputerNumbers() {
+  let computer_numbers = [];
+  while (computer_numbers.length != 3) {
+    let computer_number = MissionUtils.Random.pickNumberInRange(1, 9);
+    if (!computer_numbers.includes(computer_number)) {
+      computer_numbers.push(computer_number);
+    }
+  }
+  return computer_numbers;
+}
+
+function setGame() {
+  let computer_numbers = getComputerNumbers();
+  game(computer_numbers);
+}
+
+function restart(result_number) {
+  if (result_number == 1) {
+    return setGame();
+  }
+  if (result_number == 2) {
+    MissionUtils.Console.print("게임 종료");
+    return MissionUtils.Console.close();
+  }
+  if (result_number != 1 || result_number != 2) {
+    throw new Error("잘못된 번호 입니다");
+  }
+}
+
+function gameResultAfter() {
+  MissionUtils.Console.readLine(
+    "재시작은 1 종료는 2를 입력해주세요",
+    (result_number) => {
+      restart(result_number);
+    }
+  );
+}
+console.log(setGame())
 class App {
   play() {
-    setgame();
+    setGame();
   }
 }
 module.exports = App;
