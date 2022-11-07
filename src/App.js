cd7b41bc0d7f66d9e6ac56ffc1e14013a50101a9
SorinKwon <ksl020700@gmail.com>
2022-11-07 18:36:53 +0900

    feat(App): selectComputerNumber 함수 추가
    새로운 함수 추가:
    - 랜덤 숫자 선택 기능

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
     initGame() {
         MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
         this.selectComputerNumber();
     }
+    
+    selectComputerNumber() {
+      const random_number = [];
+      while (random_number.length < 3) {
+          const selected_number = MissionUtils.Random.pickNumberInRange(1, 9);
+          if (!random_number.includes(selected_number)) {
+              random_number.push(selected_number);
+          }
+      }
+      this.computer_number = random_number;
+  }
+
 }
 const app = new App;
 app.play();
