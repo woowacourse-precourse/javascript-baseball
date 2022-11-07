93e341649eebf562bf92548f0e68aee450d9da7e
rayark1 <max980203@snu.ac.kr>
2022-11-07 19:13:16 +0900

    feat(/src/App.js): 컴퓨터가 1에서 9까지 서로 다른 임의의 수 3개를 생성

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
 const startMessage = function printGameStartMessage() {
-  MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+  MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+};
+
+const generateAnswer = function generateRandomThreeNumbers() {
+  const answer = [];
+  while (answer.length < 3) {
+    const number = MissionUtils.Random.pickNumberInRange(1, 9);
+    if (!answer.includes(number)) {
+      answer.push(number);
+    }
+  }
 };
 
 class App {
