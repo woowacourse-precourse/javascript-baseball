ca5921d6df78ae42ed84f02acc16f8d51b515570
emma <jhj191058@gmail.com>
2022-11-07 22:20:22 +0900

    feat : 컴퓨터의 랜덤한 세자리 숫자 만들기

--- src/App.js
+++ src/App.js
@@ -1,3 +1,10 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
+function randomComputerNum(){
+  const number = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
+  return number;
+}
+
 class App {
   play() {}
 }
