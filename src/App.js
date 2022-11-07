c7f9165a4bff8fff03e9333e6d2d0f649c3e20ce
ywoo12321 <ywoo12321@gmail.com>
2022-11-07 14:53:02 +0900

    숫자 야구 해결방안 구상

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
+const { Console } = require("@woowacourse/mission-utils");
+const NewGame = require("./NewGame");
 class App {
-  play() {}
+  play() {
+    Console.print("숫자 야구 게임을 시작합니다.");
+    new NewGame();
+  }
 }
+const app = new App();
+app.play();
+module.exports = App;
+// 게임 시작 함수
+// 랜덤으로 서로 다른 숫자 3개 골라 놓기 함수
+// 사용자 입력 함수
+// 사용자와 랜덤 비교 함수
+//
 
 module.exports = App;
+
+/*
+숫자 야구 게임을 시작합니다.
+숫자를 입력해주세요 : 123
+1볼 1스트라이크
+숫자를 입력해주세요 : 145
+1볼
+숫자를 입력해주세요 : 671
+2볼
+숫자를 입력해주세요 : 216
+1스트라이크
+숫자를 입력해주세요 : 713
+3스트라이크
+3개의 숫자를 모두 맞히셨습니다! 게임 종료
+게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.
+*/
---
+++ src/NewGame.js
@@ -0,0 +1,12 @@
+const { Console, Random } = require("@woowacourse/mission-utils");
+class NewGame {
+  constructor() {
+    this.gameStart();
+  }
+  gameStart() {
+    //무작위 숫자 받아오기
+    //사용자 숫자 받아오기
+  }
+}
+
+module.exports = NewGame;
