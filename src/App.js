9c2b4be8ad3a52d3532d7d67d047da48cdbac524
feb-dain <feb.dain@gmail.com>
2022-11-07 09:18:31 +0900

    feat: accept user's number

--- src/App.js
+++ src/App.js
@@ -14,6 +14,17 @@ class App {
     }
   }
 
+  acceptUserNumber() {
+    CONSOLE_UTIL.readLine("숫자를 입력해 주세요 : ", (userInput) => {
+      userInput = String(userInput);
+      const USER_VALUE_ARRAY = Array.from(userInput, Number);
+      if (userInput < 1 || isNaN(userInput) || userInput.length !== 3) {
+        throw new Error("3자리 양의 정수를 입력해 주세요.");
+      }
+      return this.compare(USER_VALUE_ARRAY, this.computerValue);
+    });
+  }
+
   play() {}
 }
 
