354da3246e927667c1b6332a29d92b8142e92636
신선영 <sunyeong0412@naver.com>
2022-11-07 22:56:14 +0900

    feat: 게임 시작 시 문구 출력

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
+// 컴퓨터가 가지고 있을 세 개의 수 생성
+function createAnswer() {
+  const answer = [];
+  while (answer.length < 3) {
+    const num = MissionUtils.Random.pickNumberInRange(1, 9);
+    if (!answer.includes(num)) {
+      answer.push(num);
+    }
+  }
+  return answer;
+}
+
 class App {
   play() {
-    const answer = [];
-    while (answer.length < 3) {
-      const num = MissionUtils.Random.pickNumberInRange(1, 9);
-      if (!answer.includes(num)) {
-        answer.push(num);
-      }
-    }
+    const answer = createAnswer();
 
-    MissionUtils.Console.print(answer);
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
   }
 }
 
