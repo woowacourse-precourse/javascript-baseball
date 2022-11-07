51dd8527d3d7a6b337e8f2c24070f50604925f2b
jetty8013 <jetty8013@gmail.com>
2022-11-08 01:33:07 +0900

    feat: 컴퓨터 랜덤 숫자 생성 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const game = require("./game")
+
 class App {
-  play() {}
+  play() {
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.")
+    const computerNumber = game.computerNumber()
+  }
 }
 
 module.exports = App;
---
+++ src/game.js
@@ -0,0 +1,17 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
+const game = {
+  computerNumber(){
+    const computer = [];
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+    return computer
+    },
+}
+
+
+module.exports = game;
