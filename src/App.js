9644670aba5b96df8144b3e10ef95176df8be4b8
HeoJiye <heoze1031@gmail.com>
2022-11-07 21:47:23 +0900

    feat(match): 입력한 숫자에 대해 결과를 출력하는 기능 추가
    입력한 숫자에 대해 strike와 ball을 카운트해서 결과를 출력하는 함수 match() 추가

--- src/App.js
+++ src/App.js
@@ -17,6 +17,8 @@ class App {
         return; 
       }
 
+      this.match(computer_nums, player_nums);
+
       MissionUtils.Console.close();
     });
   }
@@ -40,6 +42,24 @@ class App {
     if (filtered.length !== 3) {
       throw 'Invaild Value: there are duplicate values in answer';
     }
+  }
+
+  match(computer_nums, player_nums) {
+    let strike = 0;
+    let ball = 0;
+
+    player_nums.forEach((item, idx) => {
+      if (computer_nums.indexOf(item) == idx) strike += 1;
+      else if (computer_nums.indexOf(item) > -1) ball += 1;
+    });
+
+    let result = '';
+    if(ball > 0) result += `${ball}볼 `;
+    if(strike > 0) result += `${strike}스트라이크`;
+
+    if(ball <= 0 && strike <= 0) result = '낫싱';
+
+    MissionUtils.Console.print(result);
   }
 }
 
