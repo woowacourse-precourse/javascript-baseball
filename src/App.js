3a05c3440afb7f39d42e487b2df8220364885c96
ming0830 <minsun0830@naver.com>
2022-11-07 23:23:08 +0900

    feat : 임의의 숫자 3자리를 생성하는 매서드 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
-class App {
-  play() {}
+const MissionUtils = require("@woowacourse/mission-utils");
+
+class App{
+  constructor(){
+    this.uniqueNumberList = [];
+  }
+
+  play(){}
+    
+
+  getRandumNumber(){
+    const randomNumber = new Set();
+    while(randomNumber.size !==3){
+      randomNumber.add(MissionUtils.Random.pickNumberInRange(1,9));
+    }
+    const uniqueNumberList = [...randomNumber]
+    return uniqueNumberList
+  }
 }
+const app= new App();
+console.log(app.getRandumNumber())
 
 module.exports = App;
