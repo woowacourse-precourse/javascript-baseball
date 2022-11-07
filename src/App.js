3ea8114a8738ea9cea31d331230fb7e5a71e9bd5
BaeDongWoo <ehddn5410@naver.com>
2022-11-07 21:15:29 +0900

    기능1) 상대방(컴퓨터)의 서로 다른 임의의 수 3개를 선택(수정)

--- src/App.js
+++ src/App.js
@@ -1,45 +1,25 @@
 const MissionUtils = require("@woowacourse/mission-utils");
-const readLine=require('readline');
-class App {
-  play(){
-  }
-}
 
-class Computer {
-  constructor(computer_number){
-    this.computer_number=computer_number;
+class App {
+  play() {
+    MissionUtils.Console.print(START_MESSAGE);
+    this.getComputerNumber();
   }
-  getcomputer_number(){
-    this.computer_number=[];
-    while (this.computer_number.length<3){
-      const number=MissionUtils.Random.pickNumberInRange(1,9);
-      if(!this.computer_number.includes(number)){
-        this.computer_number.push(number);
+  getComputerNumber() {
+    const computer = [];
+    while (computer.length < 3) {
+      let number = MissionUtils.Random.pickNumberInRange(1, 9);
+      number = number.toString();
+      if (!computer.includes(number)) {
+        computer.push(number);
       }
     }
-    return this.computer_number;
+    MissionUtils.Console.print(computer);
+    this.compare(computer);
   }
 }
-computer=new Computer()
-console.log(computer.getcomputer_number());
 
-class Player{
-  constructor(player_number){
-    this.player_number=player_number;
-  }
-  getplayer_number(){
-    Console.readLine('숫자를 입력하세요.',(number)=>{
-      this.player_number=number;
-      Console.close();
-    })
-  }
-  player_numbercheck(player_number){
-    if(player_number.length!=3||player_number[1]===player_number[2]||player_number[1]===player_number[3]||
-      player_number[2]===player_number[3]||typeof player!='number'){
-        throw new Error ("잘못된 입력입니다.")
-      }
-  }
-}
-player=new Player();
-player.getplayer_number();
-//module.exports = App;*/
+const app = new App();
+app.play();
+
+module.exports = App;
