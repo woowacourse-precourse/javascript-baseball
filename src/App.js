263d30726627cae8d9ac0bfd700b0163426d0263
DongjoonShin <ehdwns1516@ajou.ac.kr>
2022-11-07 15:45:52 +0900

    feat: 3자리 숫자 랜덤 숫자 생성 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+
 class App {
-  play() {}
-  makeRandomNumber(len) {}
+  constructor(numberSize = 3) {
+    this.numberSize = numberSize;
+  }
+
+  play() {
+    const computer = this.makeRandomNumber();
+    MissionUtils.Console.close();
+  }
+
+  makeRandomNumber() {
+    const computer = [];
+    while (computer.length < this.numberSize) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+    return computer;
+  }
+
   compareNumbers(computer, user) {}
+
   printCompareResult(result) {}
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
