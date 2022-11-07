733e81387e0e9941e977e6c3019f29d1b8e99ead
Dae-une <ko301201@naver.com>
2022-11-07 16:04:39 +0900

    feat: 랜덤한 숫자 3개 뽑기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  play() {
+    const Console = MissionUtils.Console;
+    const Random = MissionUtils.Random;
+
+    const tries = [];
+    let targetNums;
+
+    const randomPick = () => {
+      let result = [];
+      while (result.length < 3) {
+        const number = Random.pickNumberInRange(1, 9);
+        if (!result.includes(number)) {
+          result.push(number);
+        }
+      }
+      return result.join("");
+    };
+
+    const startGame = () => {
+      targetNums = randomPick();
+      enterNumber();
+    };
+
+    Console.print("숫자야구 게임을 시작합니다.");
+    startGame();
+  }
 }
 
 module.exports = App;
