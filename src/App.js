cca3a144c788aafe78a1420cf40ef00199edc583
Minyoung Lee <mminy62@gmail.com>
2022-11-07 16:28:33 +0900

    feat: computer인 random 3자리수 만들기
    1. computer : random 수로 3자리 수 만들기(서로 다른 임의의 수)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
+
 class App {
-  play() {}
+  play() {
+    const computer = [];
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+    console.log(computer); 
+  }
+
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
