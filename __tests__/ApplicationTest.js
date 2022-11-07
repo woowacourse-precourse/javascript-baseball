ff1fc6052cf818a72ba8519e1698d5d3a1b02ba8
hyunjin <leehj0110@kakao.com>
2022-11-07 11:01:27 +0900

    test: Input class의 각 메서드 추가

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -1,6 +1,8 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 const App = require("../src/App");
 const Console = require("../src/Console");
-const MissionUtils = require("@woowacourse/mission-utils");
+const Input = require("../src/Input");
 
 const mockQuestions = (answers) => {
   MissionUtils.Console.readLine = jest.fn();
@@ -62,6 +64,49 @@ describe("숫자 야구 게임", () => {
   });
 });
 
+describe("Input", () => {
+  test("isValidGuess는 적절한 입력에 대하여 true를 반환한다.", () => {
+    const inputs = ["123", "789", "987", "135"];
+    inputs.forEach((input) => {
+      expect(Input.isValidGuess(input)).toBe(true);
+    });
+  });
+
+  test("isValidGuess는 적절하지 않은 입력에 대하여 에러를 발생시켜야한다.", () => {
+    const inputs = ["abc", "1234", "ab2", "12", "12345", "333", "112"];
+
+    inputs.forEach((input) => {
+      expect(() => Input.isValidGuess(input)).toThrow(Error);
+    });
+  });
+
+  test("checkNumber", () => {
+    const number = 1;
+    const notNumber = "a";
+
+    expect(Input.checkNumber(number)).toBe(true);
+    expect(() => Input.checkNumber(notNumber)).toThrow(Error);
+  });
+
+  test("checkZero", () => {
+    const zero = 0;
+    const notZero = 1;
+
+    expect(() => Input.checkZero(zero)).toThrow(Error);
+    expect(Input.checkZero(notZero)).toBe(true);
+  });
+
+  test("checkIsOneOrTwo", () => {
+    const one = "1";
+    const two = "2";
+    const notOneOrTwo = "3";
+
+    expect(Input.checkIsOneOrTwo(one)).toBe(true);
+    expect(Input.checkIsOneOrTwo(two)).toBe(true);
+    expect(() => Input.checkIsOneOrTwo(notOneOrTwo)).toThrow(Error);
+  });
+});
+
 describe("Console", () => {
   test("readLine", () => {
     const readLineSpy = jest.spyOn(MissionUtils.Console, "readLine");
@@ -88,124 +133,3 @@ describe("Console", () => {
     expect(closeSpy).toHaveBeenCalled();
   });
 });
-
-// test("게임이 실행되면 숫자 야구 게임의 정답을 생성한다.", () => {
-//   const app = new App();
-//   const answer = app.ANSWER;
-//   expect(answer).toHaveLength(3);
-// });
-// test("게임이 실행되면 '숫자를 입력해주세요 : '라는 문구를 출력한다.", () => {
-//   const logSpy = getLogSpy();
-//   const app = new App();
-//   app.play();
-//   expect(logSpy).toHaveBeenCalledWith("숫자를 입력해주세요 : ");
-// });
-
-// describe("App", () => {
-//   test("print 메소드를 통해 원하는 메시지를 출력한다.", () => {
-//     const logSpy = getLogSpy();
-
-//     const MESSAGE = "메시지";
-//     const app = new App();
-
-//     app.print(MESSAGE);
-//     MissionUtils.Console.close();
-
-//     expect(logSpy).toHaveBeenCalledWith(MESSAGE);
-//   });
-
-//   test("generateAnswer 메소드를 통해 정답을 생성한다.", () => {
-//     const app = new App();
-//     const answer = app.generateAnswer();
-
-//     const checkNumber = (number) => !isNaN(number);
-//     const checkRange = (number) => number >= 1 && number <= 9;
-
-//     expect(Array.from(new Set(answer))).toHaveLength(3);
-//     expect(
-//       Array.from(answer)
-//         .map((char) => Number(char))
-//         .every(checkRange)
-//     ).toBeTruthy();
-//     expect(
-//       Array.from(answer)
-//         .map((char) => Number(char))
-//         .every(checkNumber)
-//     ).toBeTruthy();
-//   });
-
-//   test("isValidInput 메소드를 통해 사용자의 입력이 유효한 입력인지 확인한다.", () => {
-//     const app = new App();
-
-//     const LONGER_THEN_THREE = "1234";
-//     const SHORTER_THEN_THREE = "12";
-//     const NOT_NUMBER = "a12";
-//     const HAS_ZERO = "012";
-
-//     const VALID_NUMBER = "123";
-
-//     expect(() => app.isValidInput(LONGER_THEN_THREE)).toThrow(Error);
-//     expect(() => app.isValidInput(SHORTER_THEN_THREE)).toThrow(Error);
-//     expect(() => app.isValidInput(NOT_NUMBER)).toThrow(Error);
-//     expect(() => app.isValidInput(HAS_ZERO)).toThrow(Error);
-
-//     expect(app.isValidInput(VALID_NUMBER)).toBeTruthy();
-//   });
-
-//   test("isGameEnd 메소드를 통해 게임이 종료되었는지 확인한다.", () => {
-//     const app = new App();
-//     app.answer = app.generateAnswer();
-//     app.userInput = app.answer;
-
-//     expect(app.isGameEnd()).toBeTruthy();
-
-//     let wrongUserInput = "124";
-//     if (app.answer === wrongUserInput) wrongUserInput = "123";
-//     app.userInput = wrongUserInput;
-
-//     expect(app.isGameEnd()).toBeFalsy();
-//   });
-// });
-
-// test("getRandomNumber 메소드를 통해 랜덤한 숫자(1~9)를 생성한다.", () => {
-//   const app = new App();
-
-//   const randomNumber = app.getRandomNumber();
-
-//   expect(randomNumber).toBeGreaterThanOrEqual(1);
-//   expect(randomNumber).toBeLessThanOrEqual(9);
-// });
-
-// test("숫자 야구 게임의 정답을 만든다.", () => {
-//   const app = new App();
-
-//   const answer = app.makeBaseballGameAnswer();
-
-//   expect(answer).toHaveLength(3);
-//   expect(typeof answer).toBe("string");
-//   expect(
-//     Array.from(answer).every((number) => !isNaN(Number(number)))
-//   ).toBeTruthy();
-// });
-
-// test("유저의 input이 올바르지 않을 경우 에러를 발생시킨다.", () => {
-//   const app = new App();
-//   mocking 어떻게해?
-//   app.getPlayerInput = jest.fn(() => "1A3");
-
-//   const input = app.getPlayerInput();
-
-//   expect(result).toThrow(RangeError);
-// });
-
-// test("유저의 input과 정답을 비교해 결과를 출력한다.", () => {
-//   const app = new App();
-//   const logSpy = getLogSpy();
-
-//   const input = "123";
-//   app.ANSWER = "456";
-
-//   app.print(app.compareUserInputWithAnswer(input));
-
-//   expect(logSpy).toHaveBeenCalledWith("낫싱");
-// });
