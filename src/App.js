a2a7b0a0a4c2b9a6f3d266a528c0f7e2530dcde0
Minyoung Lee <mminy62@gmail.com>
2022-11-07 21:34:02 +0900

    feat: 사용자의 입력값 받기

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
+let user = [];
+let computer = [];
 
 class App {
+
   play() {
-    const computer = [];
     while (computer.length < 3) {
       const number = MissionUtils.Random.pickNumberInRange(1, 9);
       if (!computer.includes(number)) {
         computer.push(number);
       }
     }
-    console.log(computer); 
+    //console.log(computer);
+    this.input();
+  }
+
+  input(){
+    MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (answer) => {
+      user = answer.split('').map((el) => parseInt(el));
+      //console.log(user);
+      MissionUtils.Console.close();
+    });
   }
 
 }
