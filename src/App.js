2f898acf77bb796e0e4be7cd9688f0d3d3c35778
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2022-11-08 00:56:24 +0900

    feat: 게임 플레이어가 입력한 값에 대한 예외 처리

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
+const COMPUTER_NUMBER_LENGTH = 3;
+
 const INPUT_NUMBER_MESSAGE = "숫자를 입력해주세요 :";
+
+const LENGTH_ERROR_MESSAGE = "3자리의 수를 입력해주세요.";
+const RANGE_ERROR_MESSAGE = "1부터 9까지의 수만 입력해주세요.";
+const DUPLICATED_ERROR_MESSAGE = "서로 다른 3자리를 입력해주세요.";
+const NOT_A_NUMBER_ERROR_MESSAGE = "숫자로만 입력해주세요.";
 
 class App {
   play() {}
@@ -32,10 +39,44 @@ class App {
     let playerNum;
 
     MissionUtils.Console.readLine(INPUT_NUMBER_MESSAGE, (input) => {
-      playerNum = input;
+      if (this.isValidPlayerInput(input)) playerNum = input;
     });
 
     return playerNum;
+  }
+
+  isValidPlayerInput(playerNum) {
+    if (!this.isValidLength(playerNum)) {
+      throw new Error(LENGTH_ERROR_MESSAGE);
+    }
+
+    if (!this.isValidRange(playerNum)) {
+      throw new Error(RANGE_ERROR_MESSAGE);
+    }
+
+    if (!this.isValidDuplicated(playerNum)) {
+      throw new Error(DUPLICATED_ERROR_MESSAGE);
+    }
+
+    if (isNaN(parseInt(playerNum))) {
+      throw new Error(NOT_A_NUMBER_ERROR_MESSAGE);
+    }
+
+    return true;
+  }
+
+  isValidLength(playerNum) {
+    return playerNum.length === COMPUTER_NUMBER_LENGTH;
+  }
+
+  isValidRange(playerNum) {
+    return !playerNum.includes("0");
+  }
+
+  isValidDuplicated(playerNum) {
+    const set = new Set(playerNum);
+
+    return set.size === COMPUTER_NUMBER_LENGTH;
   }
 }
 
