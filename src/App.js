2ff96c5c559837963971f59a2b494fee11b5ef31
hailey-hy <lillyine.hy@gmail.com>
2022-11-08 02:22:26 +0900

    refactor: 자바스크립트 컨벤션 준수

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
-const MissionUtils = require("@woowacourse/mission-utils");
+const MissionUtils = require('@woowacourse/mission-utils');
 
 class App {
   // 프로그램 시작
-  play() {
+  play () {
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
     const computer = this.pickComputerNumber;
     this.gameStart(computer)
   }
 
-  gameStart(computer) {
+  gameStart (computer) {
     MissionUtils.Console.readLine('숫자를 입력해주세요.', (user) => {
       MissionUtils.Console.print(`숫자를 입력해주세요 : ${user}`);
-      let userSplited = user.split("");
+      let userSplited = user.split('');
       this.checkUser(userSplited);
       const result = this.scoreCounter(userSplited, computer);
       const answer = this.printScore(result);
       MissionUtils.Console.print(answer);
-      if(answer === '3스트라이크'){
+      if (answer === '3스트라이크') {
         MissionUtils.Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
         this.restartOrQuit();
       }
-      else{
+      else {
         this.gameStart(computer);
       }
     });
   }
 
   // 정답 숫자 선정
-  get pickComputerNumber() {
+  get pickComputerNumber () {
     let computer = [];
     while (computer.length < 3) {
       const number = MissionUtils.Random.pickNumberInRange(1, 9);
@@ -39,7 +39,7 @@ class App {
   }
 
   // 사용자로부터 입력값 얻기
-  get pickUserNumber() {
+  get pickUserNumber () {
     let user = '';
     MissionUtils.Console.readLine('숫자를 입력해주세요.', (answer) => {
       user = answer;
@@ -48,42 +48,41 @@ class App {
   }
 
   // 입력값 예외 처리
-  checkUser(user) {
+  checkUser (user) {
     this.checkIfNumber(user);
     this.checkIfThreeDigit(user);
     this.checkIfDiff(user);
     this.checkIfnotZero(user);
-    return true;
   }
 
   // 숫자인지 체크
-  checkIfNumber(user) {
-    for (let i = 0; i < 3; i++){
-      if(isNaN(user[i])){ 
+  checkIfNumber (user) {
+    for (let i = 0; i < 3; i++) {
+      if (isNaN(user[i])) { 
         throw new Error ('숫자가 아님');
       }
     }
   }
   
   // 3자리인지 체크
-  checkIfThreeDigit(user) {
-    if(user.length != 3){
+  checkIfThreeDigit (user) {
+    if (user.length != 3) {
       
       throw new Error ('3자리가 아님');
     }
   }
   
   // 서로 다른 숫자인지 체크
-  checkIfDiff(user) {
+  checkIfDiff (user) {
     let numberCheck = new Set(user);
-    if(numberCheck.size != user.length){
+    if (numberCheck.size != user.length) {
       
       throw new Error ('서로 다른 숫자가 아님');
     }
   }
   
   // 1 ~ 9사이의 숫자인지 체크
-  checkIfnotZero(user) {
+  checkIfnotZero (user) {
     for (let i = 0; i < 3; i++){
       if(1 > Number(user[i]) || Number(user[i])>9){
                
@@ -93,7 +92,7 @@ class App {
   }
 
   // 정답 숫자와 사용자의 입력값 체크
-  scoreCounter(user, computer) {
+  scoreCounter (user, computer) {
     let result = [0, 0]
     for(let i = 0; i < 3; i++){
       if(user[i] == computer[i]){
@@ -107,10 +106,10 @@ class App {
   }
   
   // 결과 출력
-  printScore(result) {
+  printScore (result) {
     let answer = '';
-    if(result[0] === 0 && result[1] == 0){
-      answer = "낫싱";
+    if (result[0] === 0 && result[1] == 0) {
+      answer = '낫싱';
     }
     else if (result[0] === 0 && result[1] > 0){
       answer = `${result[1]}볼`;
@@ -125,13 +124,13 @@ class App {
   }
 
   // 프로그램 종료 및 재시작
-  restartOrQuit() {
+  restartOrQuit () {
     MissionUtils.Console.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.', (answer) => {
       MissionUtils.Console.print(`게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n${answer}`);
-      if(answer == '1'){
+      if (answer == '1') {
         this.play();
       }
-      else{
+      else {
         MissionUtils.Console.close();
       }
     });
