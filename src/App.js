b364ffafd46381b4e549bdace8368a02faf0bea0
jinseo <ghkdwlstj99@gmail.com>
2022-11-08 01:56:06 +0900

    refactor: 불필요한 변수 삭제

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 const GameProgress = require("./GameProgress");
-const UserNumber = require("./UserNumber");
-const CreateNumber = require("./CreateNumber");
 const { Console } = require("@woowacourse/mission-utils");
 
 
 class App {
   constructor(){
     this.gameProgress = new GameProgress();
-    this.userNumber = new UserNumber();
-    this.createNumber = new CreateNumber();
     
 
   }
   play() {
-    Console.print("숫자 야구 게임을 시작합니다.")
+    Console.print('숫자 야구 게임을 시작합니다.');
     this.gameProgress.gameStart();
   }
 }
