ec17c7d984ccf17d86b552ad562bf10b698a1b6c
Subin Jo <sbjo1202@naver.com>
2022-11-07 16:32:32 +0900

    test: 기능 6 결과 출력하기(x볼 y스트라이크) 테스트

--- __tests__/FunctionTest.js
+++ __tests__/FunctionTest.js
@@ -71,4 +71,16 @@ describe("기능 테스트", () => {
       expect.stringContaining('3스트라이크 \n3개의 숫자를 모두 맞히셨습니다! 게임 종료')
     );
   });
+
+  test("기능 6, 결과 출력하기 - x볼 y스트라이크", () => {
+    const logSpy = getLogSpy();
+    const checkResult = [1, 2];
+
+    const app = new App();
+    app.printResult(checkResult);
+
+    expect(logSpy).toHaveBeenCalledWith(
+      expect.stringContaining('1볼 2스트라이크')
+    );
+  });
 });
