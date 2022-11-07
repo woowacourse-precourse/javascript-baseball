e31f09f241cb0ba9387331b431eec17085c9b6b0
SorinKwon <ksl020700@gmail.com>
2022-11-07 18:31:27 +0900

    feat(App): initGame 함수 추가
    새로운 함수 추가:
    - 게임 시작 안내 문구 출력
    - 컴퓨터의 랜덤 숫자 선택 함수 호출

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+const MissionUtils = require("@woowacourse/mission-utils");
 class App {
-  play() {}
+    initGame() {
+        MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+        this.selectComputerNumber();
+    }
 }
-
+const app = new App;
+app.play();
 module.exports = App;
