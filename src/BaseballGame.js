a3ff01633d2490c0513e0db0636e594040a1aaf3
YMS <yms1789@naver.com>
2022-11-07 19:56:50 +0900

    Feat: 랜덤한 3개의 숫자 생성 기능 추가

--- src/BaseballGame.js
+++ src/BaseballGame.js
@@ -11,6 +11,19 @@ class BaseballGame {
     }
   }
 
-  play() {}
+  pickUniqueNumbers() {
+    const computer = [];
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+    return computer;
+  }
+
+  play() {
+    this.init();
+  }
 }
 module.exports = BaseballGame;
