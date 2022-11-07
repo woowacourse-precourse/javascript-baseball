8eda059627e8395591e4994a32d3028bd52fc3d9
emma <jhj191058@gmail.com>
2022-11-07 22:57:23 +0900

    feat : 플레이어에게 서로 다른 3자리 숫자 받기

--- src/App.js
+++ src/App.js
@@ -6,6 +6,14 @@ function randomComputerNum(){
 }
 
 class App {
+
+  start() {
+    MissionUtils.Console.readLine('숫자 야구 게임을 시작합니다.\n숫자를 입력해주세요 : ', (answer) => {
+      console.log(answer);
+      Console.close();
+    });
+  }
+  
   play() {}
 }
 
