997d60446574c97d49fb72868ba0737fd68c6d55
Sangjun-man <molung@icloud.com>
2022-11-07 22:58:05 +0900

    feat : 게임시작, 종료 기능 구현
    게임시작, 종료, 게임종료후 재시작 or 프로그램 종료 기능을 구현했습니다
    - App 클래스의 play, playing, end 메서드를 통해 게임실행 상태를 조정합니다

--- src/App.js
+++ src/App.js
@@ -1,5 +1,45 @@
+const { getInputValue, print, consoleClose } = require("./InputOutput");
+const { getThisTurnResult } = require("./Check");
+const { getRandomThreeNumbers } = require("./Random");
+
 class App {
-  play() {}
+  gameState;
+  randomNumber;
+  constructor() {
+    this.play();
+  }
+  async play() {
+    this.randomNumber = getRandomThreeNumbers();
+    this.gameState = true;
+    print("숫자 야구 게임을 시작합니다.");
+    while (this.gameState) {
+      this.playing();
+    }
+    return this.end();
+  }
+  playing() {
+    print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    this.gameState = false;
+  }
+  async end() {
+    let gameRestartCheck = Number(
+      await getInputValue(
+        "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
+      )
+    );
+    if (!(gameRestartCheck === 1 || gameRestartCheck === 2)) {
+      print("올바른 값이 아닙니다. 다시 입력해주세요");
+      return this.end();
+    }
+
+    if (gameRestartCheck === 1) {
+      return this.play();
+    } else {
+      print("게임을 종료합니다.");
+      consoleClose();
+    }
+  }
 }
 
+const app = new App();
 module.exports = App;
