002689ab04870b08e64e929a7384a09e19306208
daal2quf <daal2quf@gmail.com>
2022-11-07 18:11:56 +0900

    refactor: MissionUtils 라이브러리 파일을 통해 리팩토링

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-const MissionUtils = require("@woowacourse/mission-utils");
+const { print, close, readline, pickNumberInRange } = require("./Utils");
 const ExceptionCheck = require("./ExceptionCheck");
 
 class App {
@@ -17,22 +17,22 @@ class App {
   }
 
   gamaStartAlram() {
-    MissionUtils.Console.print("게임을 시작합니다");
+    print("게임을 시작합니다");
   }
 
   createRandomNum() {
     let computerRandomNums = new Set();
     while (computerRandomNums.size < 3) {
-      computerRandomNums.add(MissionUtils.Random.pickNumberInRange(1, 9));
+      computerRandomNums.add(pickNumberInRange(1, 9));
     }
     return this.correctAnswer = [...computerRandomNums].join('');
   }
 
   getAnswer() {
-    MissionUtils.Console.readLine('숫자를 입력해주세요 :', (input) => {
+    readLine('숫자를 입력해주세요 :', (input) => {
       const exceptionCheck = new ExceptionCheck();
       exceptionCheck.UserInputCheck(input);
-      MissionUtils.Console.print(`입력한 숫자는 ${input} 입니다`);
+      print(`입력한 숫자는 ${input} 입니다`);
       this.userInput = input;
       this.strikeCount();
       this.ballCount();
@@ -62,14 +62,14 @@ class App {
   }
 
   nothing() {
-    MissionUtils.Console.print('낫싱');
+    print('낫싱');
   }
 
   strikeBallCountAlram() {
     if (this.ball === 0 && this.strike === 0) {
       this.nothing();
     } else {
-      MissionUtils.Console.print(`입력한 수: ${this.userInput}, ${this.ball}볼 ${this.strike}스트라이크`);
+      print(`입력한 수: ${this.userInput}, ${this.ball}볼 ${this.strike}스트라이크`);
     }
     this.strikeBallChecking();
   }
@@ -90,19 +90,19 @@ class App {
   }
 
   win() {
-    MissionUtils.Console.print(`3스트라이크! 정답은 : ${this.correctAnswer} 입니다`);
+    print(`3스트라이크! 정답은 : ${this.correctAnswer} 입니다`);
   }
 
   restartOrEnd() {
-    MissionUtils.Console.readLine(`1 을 입력하면 종료, 2 를 입력하면 게임을 재시작 합니다`, (answer) => {
+    readLine(`1 을 입력하면 종료, 2 를 입력하면 게임을 재시작 합니다`, (answer) => {
       if (answer == 2) {
         this.countReset();
         app.play();
       } else if (answer == 1) {
-        MissionUtils.Console.print('게임을 종료합니다');
-        MissionUtils.Console.close();
+        print('게임을 종료합니다');
+        close();
       } else {
-        MissionUtils.Console.print('1 과 2 둘중 하나만 선택하세요');
+        print('1 과 2 둘중 하나만 선택하세요');
         this.restartOrEnd();
       }
     });
