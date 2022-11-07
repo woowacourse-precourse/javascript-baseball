556b0a5298621ecc240b988c33e8355fc9fa470e
semnil5202 <semnil5202@gmail.com>
2022-11-07 22:29:45 +0900

    test(ApplicationTest): 게임 플레이 메시지 테스트 코드 추가
    이전 :
    낫싱, 3스트라이크, 볼 스트라이크, 게임 종료
    
    이후 :
    낫싱, 볼, 3스트라이크, 볼 스트라이크, 3스트라이크 외 스트라이크
    즉, 3스트라이크 외 스트라이크와 볼 메세지를 추가한다.
    
    이유 :
    게임 플레이 시 볼만 있을 경우 또는 스트라이크만 있을 경우가 있다.
    이에 대한 테스트가 기존 코드에선 누락되었으므로 추가한다.
    스트라이크만 있을 경우는 3스트라이크의 경우와 다르다.
    3스트라이크 이후에는 게임이 종료되고 재시작 또는 완전종료에 대한 입력값을 기대하기 때문이다.
    따라서 3스트라이크 외 스트라이크 테스트 메시지를 추가한다.

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -26,12 +26,14 @@ const getLogSpy = () => {
 describe("숫자 야구 게임", () => {
   test("게임 종료 후 재시작", () => {
     const randoms = [1, 3, 5, 5, 8, 9];
-    const answers = ["246", "135", "1", "597", "589", "2"];
+    const answers = ["246", "753", "135", "1", "597", "789", "589", "2"];
     const logSpy = getLogSpy();
     const messages = [
       "낫싱",
+      "2볼",
       "3스트라이크",
       "1볼 1스트라이크",
+      "2스트라이크",
       "3스트라이크",
       "게임 종료",
     ];
