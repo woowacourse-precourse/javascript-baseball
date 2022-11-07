d301d51871344a469b3ccbf60cec54b64c6b6f79
Ryan-Dia <cjfdnjs158@naver.com>
2022-11-07 18:29:58 +0900

    refator : 선 테스트 후 코드 작성을 위해 초기화

--- src/components/firstAnnouncement.js
+++
@@ -1,7 +0,0 @@
-const MissionUtils = require("@woowacourse/mission-utils");
-
-const firstAnnouncement = () => {
-  MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
-};
-
-module.exports = firstAnnouncement;
