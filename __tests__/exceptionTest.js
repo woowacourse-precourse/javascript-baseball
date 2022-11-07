57ecdddb7a9ecee49a699f7e556bda7deaa7901c
chasj0326@naver.com <62418379+chasj0326@users.noreply.github.com>
2022-11-07 17:03:25 +0900

    test: tests/exceptionTest.js - 정답입력 예외처리 테스트 작성

---
+++ __tests__/exceptionTest.js
@@ -0,0 +1,59 @@
+const App = require("../src/App");
+const MissionUtils = require("@woowacourse/mission-utils");
+
+const mockQuestions = (answers) => {
+  MissionUtils.Console.readLine = jest.fn();
+  answers.reduce((acc, input) => {
+    return acc.mockImplementationOnce((question, callback) => {
+      callback(input);
+    });
+  }, MissionUtils.Console.readLine);
+};
+
+const mockRandoms = (numbers) => {
+  MissionUtils.Random.pickNumberInRange = jest.fn();
+  numbers.reduce((acc, number) => {
+    return acc.mockReturnValueOnce(number);
+  }, MissionUtils.Random.pickNumberInRange);
+};
+
+describe("숫자 입력 예외 처리 테스트", () => {
+  test("1. 알파벳 입력", () => {
+    const randoms = [1,2,3];
+    const answers = ["13a"];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow();
+  });
+
+  test("2. 입력 길이 초과", () => {
+    const randoms = [1,2,3];
+    const answers = ["1313"];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow();
+  });
+
+  test("3. 같은 수 여러개", () => {
+    const randoms = [1,2,3];
+    const answers = ["131"];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow();
+  });
+});
