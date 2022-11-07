f5bc0f1506b88cd7d7585a1f9f01427708814402
minjaeKim <smallkdb@gmail.com>
2022-11-07 22:52:14 +0900

    style: set tab to two blank characters
    탭을 공백문자 2개로 변경하였습니다.

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -1,91 +1,91 @@
-const App = require('../src/App');
-const MissionUtils = require('@woowacourse/mission-utils');
+const App = require("../src/App");
+const MissionUtils = require("@woowacourse/mission-utils");
 
 const mockQuestions = (answers) => {
-	MissionUtils.Console.readLine = jest.fn();
-	answers.reduce((acc, input) => {
-		return acc.mockImplementationOnce((question, callback) => {
-			callback(input);
-		});
-	}, MissionUtils.Console.readLine);
+  MissionUtils.Console.readLine = jest.fn();
+  answers.reduce((acc, input) => {
+    return acc.mockImplementationOnce((question, callback) => {
+      callback(input);
+    });
+  }, MissionUtils.Console.readLine);
 };
 
 const mockRandoms = (numbers) => {
-	MissionUtils.Random.pickNumberInRange = jest.fn();
-	numbers.reduce((acc, number) => {
-		return acc.mockReturnValueOnce(number);
-	}, MissionUtils.Random.pickNumberInRange);
+  MissionUtils.Random.pickNumberInRange = jest.fn();
+  numbers.reduce((acc, number) => {
+    return acc.mockReturnValueOnce(number);
+  }, MissionUtils.Random.pickNumberInRange);
 };
 
 const getLogSpy = () => {
-	const logSpy = jest.spyOn(MissionUtils.Console, 'print');
-	logSpy.mockClear();
-	return logSpy;
+  const logSpy = jest.spyOn(MissionUtils.Console, "print");
+  logSpy.mockClear();
+  return logSpy;
 };
 
-describe('숫자 야구 게임', () => {
-	test('게임 종료 후 재시작', () => {
-		const randoms = [1, 3, 5, 5, 8, 9];
-		const answers = ['246', '135', '1', '597', '589', '2'];
-		const logSpy = getLogSpy();
-		const messages = ['낫싱', '3스트라이크', '1볼 1스트라이크', '3스트라이크', '게임 종료'];
+describe("숫자 야구 게임", () => {
+  test("게임 종료 후 재시작", () => {
+    const randoms = [1, 3, 5, 5, 8, 9];
+    const answers = ["246", "135", "1", "597", "589", "2"];
+    const logSpy = getLogSpy();
+    const messages = ["낫싱", "3스트라이크", "1볼 1스트라이크", "3스트라이크", "게임 종료"];
 
-		mockRandoms(randoms);
-		mockQuestions(answers);
+    mockRandoms(randoms);
+    mockQuestions(answers);
 
-		const app = new App();
-		app.play();
+    const app = new App();
+    app.play();
 
-		messages.forEach((output) => {
-			expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
-		});
-	});
+    messages.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
 
-	test('예외 테스트', () => {
-		const randoms = [1, 3, 5];
-		const answers = ['1234'];
+  test("예외 테스트", () => {
+    const randoms = [1, 3, 5];
+    const answers = ["1234"];
 
-		mockRandoms(randoms);
-		mockQuestions(answers);
+    mockRandoms(randoms);
+    mockQuestions(answers);
 
-		expect(() => {
-			const app = new App();
-			app.play();
-		}).toThrow();
-	});
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow();
+  });
 
-	test('게임 종료 후 입력값에 대해 예외 테스트', () => {
-		const randoms = [3, 1, 8];
-		const answers = ['318', '3'];
-		const messages = ['3스트라이크'];
-		const logSpy = getLogSpy();
+  test("게임 종료 후 입력값에 대해 예외 테스트", () => {
+    const randoms = [3, 1, 8];
+    const answers = ["318", "3"];
+    const messages = ["3스트라이크"];
+    const logSpy = getLogSpy();
 
-		mockRandoms(randoms);
-		mockQuestions(answers);
+    mockRandoms(randoms);
+    mockQuestions(answers);
 
-		expect(() => {
-			const app = new App();
-			app.play();
-		}).toThrow();
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow();
 
-		messages.forEach((output) => {
-			expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
-		});
-	});
+    messages.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
 
-	test('게임 종료 후 프로그램 종료', () => {
-		const randoms = [5, 6, 7];
-		const answers = ['543', '321', '756', '576', '567', '2'];
-		const messages = ['1스트라이크', '낫싱', '3볼', '2볼 1스트라이크', '3스트라이크', '게임 종료'];
-		const logSpy = getLogSpy();
+  test("게임 종료 후 프로그램 종료", () => {
+    const randoms = [5, 6, 7];
+    const answers = ["543", "321", "756", "576", "567", "2"];
+    const messages = ["1스트라이크", "낫싱", "3볼", "2볼 1스트라이크", "3스트라이크", "게임 종료"];
+    const logSpy = getLogSpy();
 
-		mockRandoms(randoms);
-		mockQuestions(answers);
+    mockRandoms(randoms);
+    mockQuestions(answers);
 
-		const app = new App();
-		app.play();
-		messages.forEach((output) => {
-			expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
-		});
-	});
+    const app = new App();
+    app.play();
+    messages.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
 });
--- src/utils/RandomNumber.js
+++ src/utils/RandomNumber.js
@@ -1,19 +1,19 @@
-const { Random } = require('@woowacourse/mission-utils');
+const { Random } = require("@woowacourse/mission-utils");
 
 function getRandomNumberExceptList(startInclusive, endInclusive, list) {
-	const randomNumber = String(Random.pickNumberInRange(startInclusive, endInclusive));
-	return list.includes(randomNumber)
-		? getRandomNumberExceptList(startInclusive, endInclusive, list)
-		: randomNumber;
+  const randomNumber = String(Random.pickNumberInRange(startInclusive, endInclusive));
+  return list.includes(randomNumber)
+    ? getRandomNumberExceptList(startInclusive, endInclusive, list)
+    : randomNumber;
 }
 
 function getUniqueNumbersInRange(startInclusive, endInclusive, count) {
-	const list = [];
-	while (list.length < count) {
-		const uniqueNumber = getRandomNumberExceptList(startInclusive, endInclusive, list);
-		list.push(uniqueNumber);
-	}
-	return list;
+  const list = [];
+  while (list.length < count) {
+    const uniqueNumber = getRandomNumberExceptList(startInclusive, endInclusive, list);
+    list.push(uniqueNumber);
+  }
+  return list;
 }
 
 module.exports = { getUniqueNumbersInRange, getRandomNumberExceptList };
