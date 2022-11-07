b72f84d56eae9c2512dc87a4ce51487af803b3a4
piporica <49173614+piporica@users.noreply.github.com>
2022-11-08 00:16:18 +0900

    feat: Make answer random numbers
    make three random numbers in 1~9
    it become game's answer

--- src/NumberBaseball.js
+++ src/NumberBaseball.js
@@ -3,6 +3,16 @@ const MissionUtils = require('@woowacourse/mission-utils');
 class NumberBaseball {
   gameStart() {
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+
+    let answer = this.makeAnswer();
+    console.log(answer);
+
+    MissionUtils.Console.close();
+  }
+
+  makeAnswer() {
+    let pickedNumber = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
+    return pickedNumber;
   }
 }
 module.exports = NumberBaseball;
