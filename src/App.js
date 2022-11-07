801d600a2da3302b33bc6e56bc9f869a5f648d1e
wisdomin121@naver.com <wisdomin121@naver.com>
2022-11-07 11:56:59 +0900

    ✨ [FEAT] : 게임에 사용되는 문장들을 상수로 지정
    GAME_START_SENTENCE = 숫자 야구 게임을 시작합니다.
    GET_USER_NUMBER_SENTENCE = 숫자를 입력해주세요 :
    GAME_END_SENTENCE = 3개의 숫자를 모두 맞히셨습니다! 게임 종료
    GAME_RESTART_SENTENCE = 게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.
    
    총 4개의 상수를 지정했다.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
+const GAME_START_SENTENCE = '숫자 야구 게임을 시작합니다.';
+const GET_USER_NUMBER_SENTENCE = '숫자를 입력해주세요 : ';
+const GAME_END_SENTENCE = '3개의 숫자를 모두 맞히셨습니다! 게임 종료';
+const GAME_RESTART_SENTENCE = '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.';
+
 class App {
   play() {}
 }
