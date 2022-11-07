0fdb3c6c015d29db897e44ff77d9ff5ef2627cc6
Sunghyun627 <kksscc4@gmail.com>
2022-11-07 18:01:40 +0900

    feat: 사용자의 입력값을 받기 위한 문구를 출력하는 기능

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 const messages = require('./Constants.js');
 class App {
+  constructor() {
+    this.userNumber = '';
+    this.computerNumber = '';
+    this.isGameRestart = true;
+  }
+
   play() {
     this.printGameStartMessage();
     this.init();
+    this.GameStart();
   }
 
   init() {
     this.computerNumber = this.createComputerNumber();
+    this.isGameFinished = false;
   }
 
   printGameStartMessage() {
@@ -22,6 +30,14 @@ class App {
     }
     return Number(computerNumber);
   }
+
+  GameStart() {
+    MissionUtils.Console.readLine(messages.ENTER_USER_NUMBER_MESSAGE, (input) => {
+      MissionUtils.Console.close();
+    });
+  }
 }
+const app = new App();
+app.play();
 
 module.exports = App;
--- src/Constants.js
+++ src/Constants.js
@@ -1,3 +1,4 @@
 const GAME_START_MESSAGE = '숫자 야구 게임을 시작합니다.';
+const ENTER_USER_NUMBER_MESSAGE = '숫자를 입력해주세요 : ';
 
-module.exports = { GAME_START_MESSAGE };
+module.exports = { GAME_START_MESSAGE, ENTER_USER_NUMBER_MESSAGE };
