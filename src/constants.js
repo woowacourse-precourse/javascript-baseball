7750433f05ce493123436ecac1fb799612ff6c9b
YoommY <isho48239@gmail.com>
2022-11-07 15:02:46 +0900

    Fear : constants file 로 분리

---
+++ src/constants.js
@@ -0,0 +1,12 @@
+const constants = {
+  BALL: '볼',
+  STRIKE: '스트라이크',
+  NOTING: '낫싱',
+  ALL_STRIKE: '3스트라이크',
+  RESTART: '1',
+  END: '2',
+  START: '숫자를 입력하세요',
+  CORRECT: '3개의 숫자를 모두 맞히셨습니다! 게임 종료'
+};
+
+module.exports = constants;
