4de28bb769fe53c6672b97274828354be30a7755
박승훈 <tmdgns1126@naver.com>
2022-11-07 23:32:04 +0900

    feat: [Com] 사용자가 맞춰야 하는 임의의 3자리 수 생성

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-const { Console } = require("@woowacourse/mission-utils");
+const { Console, Random } = require("@woowacourse/mission-utils");
+
+class Computer {
+  createRandomNum() {
+    const random3 = Random.pickUniqueNumbersInRange(1, 9, 3);
+    return random3.join("");
+  }
+}
 
 class App {
+  constructor() {
+    this.computer = new Computer();
+  }
+
   play() {
     Console.print("숫자 야구 게임을 시작합니다.");
     this.startGame();
   }
 
-  startGame() {}
+  startGame() {
+    const targetNum = this.computer.createRandomNum();
+  }
 }
 
 module.exports = App;
