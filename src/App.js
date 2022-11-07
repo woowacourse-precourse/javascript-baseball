4d9177ea79bda99a60d238c5e1fb6ad4dae8672e
HeoJiye <heoze1031@gmail.com>
2022-11-07 14:25:26 +0900

    feat(play): add 숫자 입력 받기 기능
    Console.readLine()으로 세자리 숫자를 문자열 형태로 받아 숫자 배열로 전환하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -5,7 +5,12 @@ class App {
     const numbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
 
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
-    MissionUtils.Console.close();
+    
+    MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (answer) => {
+      const inputNumber = answer.split('').map(Number);
+
+      MissionUtils.Console.close();
+    });
   }
 }
 
