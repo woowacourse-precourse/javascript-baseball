c75f000a69ae185c7f02117786fd07c90c0f1c0f
MINJUN PARK <mj9457@naer,com>
2022-11-07 18:28:01 +0900

    feat. 1~9까지 3자리의 조건을 가진 임의의 수 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
 class App {
-    play() {}
+
+    constructor() {
+        this.computerNumber = [];
+        this.gamePlayerNumber = [];
+    }
+
+    play() {
+        
+    }
+
+    createRandomNumber() {
+        const randomNumberList = [];
+        while (computer.length < 3) {
+            const randomnumber = MissionUtils.Random.pickNumberInRange(1, 9);
+                if (!randomNumberList.includes(randomnumber)) {
+                    randomNumberList.push(randomnumber);
+            }
+        }
+    }
 }
 
 module.exports = App;
