83ebb5d3564ff03c084730ddbb2379c4b4ae4431
pinkishincoloragain <smb1103@gmail.com>
2022-11-07 15:47:51 +0900

    feat: 사용자 입력 메소드 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+const { Console } = require('@woowacourse/mission-utils');
+const { validateGameInput, validateRestartInput } = require('./validate');
+const config = require('./config');
+
 class App {
-  play() {}
+  constructor() {
+    this.inputPhrase = config.PHRASE.INPUT;
+    this.restartPhrase = config.PHRASE.RESTART;
+  }
+
+  getGuessFromUser() {
+    Console.readLine(this.inputPhrase, (input) => {
+      validateGameInput(input);
+    });
+  }
+
+  getRestartFromUser() {
+    Console.readLine(this.restartPhrase, (input) => {
+      validateRestartInput(input);
+    });
+  }
 }
 
 module.exports = App;
