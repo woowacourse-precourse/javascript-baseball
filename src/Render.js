d726b8950681a59b47ec0314bbaeb40b2bc5b725
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2022-11-07 14:51:39 +0900

    feat(Render): 반복되는 멘트를 상수로 저장 후 불러오기

--- src/Render.js
+++ src/Render.js
@@ -1,15 +1,22 @@
 const MissionUtils = require("@woowacourse/mission-utils");
+const {
+  START_MENTION,
+  START_GETNUMBER,
+  GAME_NOTHING,
+  END_MENTION,
+  END_RETRY_MENTION,
+} = require("./constants");
 
 class Render {
   constructor() {}
 
   startment() {
-    MissionUtils.Console.print("게임을 시작합니다!⚾️");
+    MissionUtils.Console.print(START_MENTION);
   }
 
   getUser() {
     return new Promise((resolve, reject) => {
-      MissionUtils.Console.readLine("숫자를 입력해주세요", (number) => {
+      MissionUtils.Console.readLine(START_GETNUMBER, (number) => {
         resolve(number);
       });
     });
@@ -20,23 +27,19 @@ class Render {
       MissionUtils.Console.print(`${ballCount}볼 ${strikeCount}스트라이크`);
     }
     if (strikeCount === 3) {
-      MissionUtils.Console.print(`3스트라이크
-3개의 숫자를 모두 맞히셨습니다! 게임 종료`);
+      MissionUtils.Console.print(END_MENTION);
     }
 
     if (ballCount === 0 && strikeCount === 0) {
-      MissionUtils.Console.print(`낫싱`);
+      MissionUtils.Console.print(GAME_NOTHING);
     }
   }
 
   replayQnA() {
     return new Promise((resolve, reject) => {
-      MissionUtils.Console.readLine(
-        "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
-        (number) => {
-          resolve(number);
-        }
-      );
+      MissionUtils.Console.readLine(END_RETRY_MENTION, (number) => {
+        resolve(number);
+      });
     });
   }
 }
---
+++ src/constants.js
@@ -0,0 +1,7 @@
+exports.START_MENTION = `숫자 야구 게임을 시작합니다.`;
+exports.START_GETNUMBER = `숫자를 입력해주세요 :`;
+
+exports.GAME_NOTHING = `낫싱`;
+
+exports.END_MENTION = `3스트라이크 3개의 숫자를 모두 맞히셨습니다! 게임 종료`;
+exports.END_RETRY_MENTION = `게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.`;
