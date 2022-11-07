43691897ae45bc071d99ae16ddf174c1fdeb2e1b
saetakki <lime.lemonade03@gmail.com>
2022-11-08 00:50:48 +0900

    feat: add validation func

--- src/App.js
+++ src/App.js
@@ -8,6 +8,23 @@ const getUserInput = (f, num) => {
   return num;
 };
 
+const isNumber = value => {
+  const regNumExp = /[0-9]/g;
+  if (regNumExp.test(value) === false) {
+    throw '숫자가 아닙니다!';
+  }
+  return value;
+};
+
+const isValidNumber = value => {
+  const regValidNumExp = /^\d{3}$/
+  if (regValidNumExp.test(value) === false){
+    throw '유효한 숫자가 아닙니다!'
+  }
+  return 
+}
+
+
 
 
 class App {
