19ee6084674a01811c90df39f952d78aa7edbc68
jetty8013 <jetty8013@gmail.com>
2022-11-08 01:41:15 +0900

    feat: 사용자vs컴퓨터 게임 대결 기능 구현

--- src/game.js
+++ src/game.js
@@ -20,6 +20,27 @@ const game = {
     }
     return computer
     },
+
+    battle(userNumber,computerNumber){
+      userNumber = String(userNumber).split('');
+      const score = {
+        ball: 0,
+        strike: 0
+      };
+  
+      userNumber.forEach((value,index) =>{
+        if(computerNumber[index] === Number(value)){
+          score.strike++
+        }else if(computerNumber.includes(Number(value))){
+          score.ball++
+        }
+      })
+
+      if(score.ball === 0 && score.strike === 0) return "낫싱"
+      else if(score.ball !== 0 && score.strike === 0) return `${score.ball}볼`
+      else if(score.ball === 0 && score.strike !== 0) return `${score.strike}스트라이크`
+      else if(score.ball !== 0 && score.strike !== 0) return `${score.ball}볼 ${score.strike}스트라이크`
+      },
 }
 
 
