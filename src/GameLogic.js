bddff12a704f73f6ef470846cbb53ababb26a872
random-olive <104327907+random-olive@users.noreply.github.com>
2022-11-07 22:23:30 +0900

    Feat: game logic : judgement

--- src/GameLogic.js
+++ src/GameLogic.js
@@ -10,7 +10,20 @@ const {
 } = require("./Constant");
 
 class GameLogic {
-  judge(userInput, computerInput) {}
+  judge(userInput, computerInput) {
+    const strikeBallNothing = [0, 0, 0];
+
+    userInput.forEach((el, index) => {
+      if (el === computerInput[index]) {
+        strikeBallNothing[0] += 1;
+      } else if (computerInput.includes(Number(el))) {
+        strikeBallNothing[1] += 1;
+      }
+    });
+
+    return strikeBallNothing;
+  }
+
   result() {}
 }
 
