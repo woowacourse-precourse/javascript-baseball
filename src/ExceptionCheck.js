744ee801f78077b33741d65f458e73d89f8ec44e
daal2quf <daal2quf@gmail.com>
2022-11-07 23:33:46 +0900

    refactor: user의 input에 대한 예외처리를 error던질 수 있게 변경

--- src/ExceptionCheck.js
+++ src/ExceptionCheck.js
@@ -1,23 +1,24 @@
-
 class ExceptionCheck {
 
   UserInputCheck(userInput) {
     if (userInput.length !== 3) {
-      throw "3개의 숫자를 입력해주세요"
+      throw new Error("3개의 숫자를 입력해주세요");
     }
 
     if (isNaN(userInput)) {
-      throw "숫자만 입력해주세요"
+      throw new Error("숫자만 입력해주세요");
     }
 
     if ([...new Set(userInput)].length !== userInput.length) {
-      throw "중복된 숫자 안돼용"
+      throw new Error("중복된 숫자 안돼용");
     }
 
     if (userInput.includes('0')) {
-      throw "1~9사이의 숫자만 입력해주세요"
+      throw new Error("1~9사이의 숫자만 입력해주세요");
     }
   }
+
+  /// 1, 2 에 대한 예외처리도 여기로
 }
 
-module.exports = ExceptionCheck
+module.exports = ExceptionCheck;
