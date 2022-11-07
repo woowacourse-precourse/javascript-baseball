1ec408dc41b24745f0c30f96630de34585cc6151
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2022-11-07 15:40:55 +0900

    refactor(constants): 각 객체별로 상수를 불러올 수 있게 구조 변환

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-GAME = {
+const GAME = Object.freeze({
   START_MENTION: `숫자 야구 게임을 시작합니다.`,
   START_GETNUMBER: `숫자를 입력해주세요 :`,
 
@@ -8,6 +8,13 @@ GAME = {
   END_RETRY_MENTION: `게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.`,
 
   END_MENTION: `수고하셨습니다.`,
-};
+});
 
-module.exports = GAME;
+const ERROR = Object.freeze({
+  USER_INPUT_LENGTH: `숫자를 3개 입력해주세요.`,
+  USER_INPUT_DUPLICATES: `숫자에 중복이 있습니다.`,
+  USER_INPUT_RANGE: `숫자 1~9까지만 입력이 가능합니다.`,
+  USER_INPUT_BLANK: `공백은 입력할 수 없습니다.`,
+});
+
+module.exports = { GAME, ERROR };
