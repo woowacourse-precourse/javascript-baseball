caa2689c5257694f65ee0182880327bf632a64bc
nalsae <nalsae0207@naver.com>
2022-11-07 10:06:59 +0900

    feat: Implement a function to create random numbers
    MissionUtils 라이브러리의 Random API를 사용하여 1 ~ 9 사이의 난수 3개를 배열로
    반환하는 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 const MissonUtils = require("@woowacourse/mission-utils");
 
 class App {
+  #RANDOM_NUM_LENGTH = 3;
   #randomNum = [];
+
+  createRandomNum() {
+    Array.from({ length: this.#RANDOM_NUM_LENGTH }).forEach(
+      () =>
+        (this.#randomNum = [
+          ...this.#randomNum,
+          MissonUtils.Random.pickNumberInRange(1, 9),
+        ])
+    );
+  }
 
   play() {}
 }
+
+const app = new App();
+
+app.createRandomNum();
 
 module.exports = App;
