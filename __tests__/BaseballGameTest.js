6ab66d5495af0b2a96aa42ebb9cd49f2adbefdf3
sohyun <park.sohyun0430@gmail.com>
2022-11-07 17:01:48 +0900

    feat: getHint return값에 대한 테스트 코드 작성
    정답배열과 사용자 입력값을 인수로 하는 getHint 함수에 대해 결과값 힌트에 관한 테스트 코드를 작성함.

--- __tests__/BaseballGameTest.js
+++ __tests__/BaseballGameTest.js
@@ -1,23 +1,21 @@
-const App = require("../src/App");
+const App = require("../src/App")
 const MissionUtils = require("@woowacourse/mission-utils");
 
 const mockQuestions = (answers) => {
-    //jest.fn() : 가짜 함수 생성
     MissionUtils.Console.readLine = jest.fn();
     answers.reduce((acc, input) => {
-      // mockImplementationsOnce
       return acc.mockImplementationOnce((question, callback) => {
         callback(input);
       });
     }, MissionUtils.Console.readLine);
-  };
+};
   
-  const mockRandoms = (numbers) => {
+const mockRandoms = (numbers) => {
     MissionUtils.Random.pickNumberInRange = jest.fn();
     numbers.reduce((acc, number) => {
       return acc.mockReturnValueOnce(number);
     }, MissionUtils.Random.pickNumberInRange);
-  };
+};
 
 test("사용자 입력받기 예외 테스트 5가지", () => {
     const randoms = [1, 3, 5];
@@ -30,4 +28,32 @@ test("사용자 입력받기 예외 테스트 5가지", () => {
       const app = new App();
       app.play();
     }).toThrow();
-  });
+});
+  
+test("다른 자리 같은 수 일 때, hint 볼이 나오는지 테스트",()=>{
+  const app = new App();
+  const testResult1 = app.getHint([1,2,3],'451');
+  expect(testResult1).toEqual('1볼');
+  const testResult2 = app.getHint([1, 2, 3], '431');
+  expect(testResult2).toEqual('2볼');
+  const testResult3 = app.getHint([1, 2, 3], '312');
+  expect(testResult3).toEqual('3볼');
+});
+
+test('같은 자리 같은 수 일 때, hint strike가 나오는지 테스트', () => {
+  const app = new App();
+  const testResult1 = app.getHint([1, 2, 3], '145');
+  expect(testResult1).toEqual('1스트라이크');
+  const testResult2 = app.getHint([1, 2, 3], '127');
+  expect(testResult2).toEqual('2스트라이크');
+  const testResult3 = app.getHint([1, 2, 3], '123');
+  expect(testResult3).toEqual('3스트라이크');
+});
+
+test('strike,ball 같이 있을 때 hint가 맞는지 테스트', () => {
+  const app = new App();
+  const testResult1 = app.getHint([1, 2, 3], '134');
+  expect(testResult1).toEqual('1볼 1스트라이크');
+  const testResult2 = app.getHint([1, 2, 3], '321');
+  expect(testResult2).toEqual('2볼 1스트라이크');
+});
