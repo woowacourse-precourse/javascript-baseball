75dbc7e7ec12785231c24d1f9715079631766fe0
vinivin153 <vinivin153@naver.com>
2022-11-07 21:46:41 +0900

    refactor: 모든 기능 함수형 -> 클래스형으로 변환

--- src/App.js
+++ src/App.js
@@ -2,152 +2,145 @@ const MissionUtils = require('@woowacourse/mission-utils');
 const Constants = require('./constant');
 
 class App {
-  async play() {
-    printMessage(Constants.START_MESSAGE);
-    const computerNumber = createComputerNumber();
-    const userNumber = await inputNumber(Constants.INPUT_NUMBER_MESSAGE);
-    userNumberException(userNumber);
-    const strike = countStrike(computerNumber, userNumber);
-    const ball = countBall(computerNumber, userNumber);
-    printResult(ball, strike);
+  constructor() {
+    this.computerNumber = null;
+    this.userNumber = null;
+  }
 
-    if (isAnswer(strike)) {
-      printMessage(Constants.WIN_MESSAGE);
-      const selectedNumber = Number(await inputNumber(Constants.END_MESSAGE));
-      selectedNumberException(selectedNumber);
+  printMessage(message) {
+    MissionUtils.Console.print(message);
+  }
+
+  createComputerNumber() {
+    const computerNumber = [];
+
+    while (computerNumber.length < 3) {
+      const randomNumber = MissionUtils.Random.pickNumberInRange(1, 9);
+
+      if (!computerNumber.includes(randomNumber)) {
+        computerNumber.push(randomNumber);
+      }
     }
+
+    this.computerNumber = computerNumber;
   }
-}
 
-function printMessage(message) {
-  MissionUtils.Console.print(message);
-}
+  input(message) {
+    return new Promise((resolve) => {
+      MissionUtils.Console.readLine(message, resolve);
+    });
+  }
 
-function createComputerNumber() {
-  const computerNumber = [];
+  async inputNumber(message) {
+    const number = await input(message);
 
-  while (computerNumber.length < 3) {
-    const randomNumber = MissionUtils.Random.pickNumberInRange(1, 9);
+    return [...number].map((num) => Number(num));
+  }
 
-    if (!computerNumber.includes(randomNumber)) {
-      computerNumber.push(randomNumber);
+  isNumber() {
+    for (let number of this.userNumber) {
+      if (!(1 <= number && number <= 9)) {
+        return false;
+      }
     }
+
+    return true;
   }
 
-  return computerNumber;
-}
-
-function input(message) {
-  return new Promise((resolve) => {
-    MissionUtils.Console.readLine(message, resolve);
-  });
-}
-
-async function inputNumber(message) {
-  const number = await input(message);
-
-  return [...number].map((num) => Number(num));
-}
-
-function isNumber(userNumber) {
-  for (let number of userNumber) {
-    if (!(1 <= number && number <= 9)) {
-      return false;
-    }
+  isLength3() {
+    return this.userNumber.length === 3;
   }
 
-  return true;
-}
+  isDuplicated() {
+    const userNumberSet = new Set(this.userNumber);
 
-function isLength3(userNumber) {
-  return userNumber.length === 3;
-}
-
-function isDuplicated(userNumber) {
-  const userNumberSet = new Set(userNumber);
-
-  return userNumberSet.size === userNumber.length;
-}
-
-function userNumberException(userNumber) {
-  if (
-    !(isNumber(userNumber) && isLength3(userNumber) && isDuplicated(userNumber))
-  ) {
-    throw new Error('Invalid number..!');
+    return userNumberSet.size === this.userNumber.length;
   }
-}
 
-function isAnswer(strike) {
-  return strike === 3;
-}
-
-function countStrike(computerNumber, userNumber) {
-  let strike = 0;
-
-  for (let i = 0; i < 3; i++) {
-    if (computerNumber[i] === userNumber[i]) {
-      strike++;
+  userNumberException() {
+    if (!(this.isNumber() && this.isLength3() && this.isDuplicated())) {
+      throw new Error('Invalid number..!');
     }
   }
 
-  return strike;
-}
+  isAnswer(strike) {
+    return strike === 3;
+  }
 
-function countBall(computerNumber, userNumber) {
-  let ball = 0;
+  countStrike() {
+    let strike = 0;
 
-  for (let i = 0; i < 3; i++) {
-    if (
-      computerNumber.includes(userNumber[i]) &&
-      computerNumber[i] !== userNumber[i]
-    ) {
-      ball++;
+    for (let i = 0; i < 3; i++) {
+      if (this.computerNumber[i] === this.userNumber[i]) {
+        strike++;
+      }
+    }
+
+    return strike;
+  }
+
+  countBall() {
+    let ball = 0;
+
+    for (let i = 0; i < 3; i++) {
+      if (
+        this.computerNumber.includes(this.userNumber[i]) &&
+        this.computerNumber[i] !== this.userNumber[i]
+      ) {
+        ball++;
+      }
+    }
+
+    return ball;
+  }
+
+  printResult(ball, strike) {
+    let result = ``;
+
+    if (ball) result += `${ball}볼`;
+
+    if (strike) result += ` ${strike}스트라이크`;
+
+    if (result === ``) result += `낫싱`;
+
+    MissionUtils.Console.print(result.trim());
+  }
+
+  selectedNumberException(selectedNumber) {
+    if (!(selectedNumber === 1 || selectedNumber === 2)) {
+      throw new Error('Invalid number..!');
     }
   }
 
-  return ball;
-}
+  async tryGetAnswer() {
+    this.userNumber = await this.inputNumber(Constants.INPUT_NUMBER_MESSAGE);
+    this.userNumberException();
 
-function printResult(ball, strike) {
-  let result = ``;
+    const strike = this.countStrike();
+    const ball = this.countBall();
+    this.printResult(ball, strike);
 
-  if (ball) result += `${ball}볼`;
+    if (this.isAnswer(strike)) {
+      this.printMessage(Constants.WIN_MESSAGE);
+      const selectedNumber = Number(
+        await this.inputNumber(Constants.END_MESSAGE)
+      );
+      this.selectedNumberException(selectedNumber);
 
-  if (strike) result += ` ${strike}스트라이크`;
-
-  if (result === ``) result += `낫싱`;
-
-  MissionUtils.Console.print(result.trim());
-}
-
-function selectedNumberException(selectedNumber) {
-  if (!(selectedNumber === 1 || selectedNumber === 2)) {
-    throw new Error('Invalid number..!');
-  }
-}
-
-async function getAnswer(computerNumber) {
-  const userNumber = await inputNumber(Constants.INPUT_NUMBER_MESSAGE);
-  userNumberException(userNumber);
-
-  const strike = countStrike(computerNumber, userNumber);
-  const ball = countBall(computerNumber, userNumber);
-  printResult(ball, strike);
-
-  if (isAnswer(strike)) {
-    printMessage(Constants.WIN_MESSAGE);
-    const selectedNumber = Number(await inputNumber(Constants.END_MESSAGE));
-    selectedNumberException(selectedNumber);
-
-    if (selectedNumber === 1) {
-      // 새로운 게임 시작
+      if (selectedNumber === 1) {
+        this.startNewGame();
+      }
     }
   }
-}
 
-function startNewGame() {
-  const computerNumber = createComputerNumber();
-  getAnswer();
-}
+  startNewGame() {
+    this.createComputerNumber();
+    this.tryGetAnswer();
+  }
 
+  play() {
+    this.printMessage(Constants.START_MESSAGE);
+    this.startNewGame();
+  }
+}
 module.exports = App;
