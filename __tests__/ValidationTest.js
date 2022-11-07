9b973d0d7ef421df5d9eab222ef82797d500025a
2wndrhs <2wndrhs@gmail.com>
2022-11-06 22:03:16 -0800

    test: 유효성 검증 테스트 수정 모듈로 분리한 유효성 검증 로직들만 테스트 할 수 있도록 수정

--- __tests__/ValidationTest.js
+++ __tests__/ValidationTest.js
@@ -1,34 +1,35 @@
-const MissionUtils = require('@woowacourse/mission-utils');
-const App = require('../src/App');
+const Validator = require('../src/Validator');
 
-const mockQuestion = answer => {
-  MissionUtils.Console.readLine = jest.fn();
-  MissionUtils.Console.readLine.mockImplementation((question, callback) => {
-    callback(answer);
-  });
-};
-
-describe('입력값 유효성 검증 테스트', () => {
-  test('isValidUserInput 메서드로 입력값이 적절한지 검증(case1)', () => {
-    const RESULT = App.isValidUserInput('123');
+describe('유효성 검증 테스트', () => {
+  test('numberInputValidator 메서드로 서로 다른 3자리의 숫자인지 검증(case1)', () => {
+    const RESULT = Validator.numberInputValidator('123');
     expect(RESULT).toBe(true);
   });
 
-  test('isValidUserInput 메서드로 입력값이 적절한지 검증(case2)', () => {
-    const RESULT = App.isValidUserInput('111');
+  test('numberInputValidator 메서드로 서로 다른 3자리의 숫자인지 검증(case2)', () => {
+    const RESULT = Validator.numberInputValidator('111');
     expect(RESULT).toBe(false);
   });
 
-  test('isValidUserInput 메서드로 입력값이 적절한지 검증(case3)', () => {
-    const RESULT = App.isValidUserInput('abc');
+  test('menuInputValidator 메서드로 1과 2 중 하나의 숫자인지 검증(case1)', () => {
+    const RESULT = Validator.menuInputValidator('1');
+    expect(RESULT).toBe(true);
+  });
+
+  test('menuInputValidator 메서드로 1과 2 중 하나의 숫자인지 검증(case2)', () => {
+    const RESULT = Validator.menuInputValidator('3');
     expect(RESULT).toBe(false);
   });
 
-  test('입력 값이 적절하지 않을 때 예외 발생 테스트', () => {
-    mockQuestion('1234');
+  test('서로 다른 3자리의 숫자가 아니면 예외 발생', () => {
     expect(() => {
-      const app = new App();
-      app.askUserInput();
+      Validator.validate('112', Validator.numberInputValidator);
+    }).toThrow();
+  });
+
+  test('1과 2중 하나의 숫자가 아니면 예외 발생', () => {
+    expect(() => {
+      Validator.validate('3', Validator.menuInputValidator);
     }).toThrow();
   });
 });
