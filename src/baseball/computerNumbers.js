da18d876999ce0b17c6126fcdfbc890d7f50b443
Y2IN <dpdls0228@daum.net>
2022-11-07 23:49:06 +0900

    feat : ComputerNumber 생성

---
+++ src/baseball/computerNumbers.js
@@ -0,0 +1,17 @@
+const { Random } = require("@woowacourse/mission-utils");
+
+class ComputerNumbers {
+  static randomSelectComputerNumbers() {
+    const randomNumbers = [];
+    while (randomNumbers.length < 3) {
+      const number = Random.pickNumberInRange(1, 9);
+      if (randomNumbers.includes(number) === false) {
+        randomNumbers.push(number);
+      }
+    }
+
+    return randomNumbers;
+  }
+}
+
+module.exports = ComputerNumbers;
