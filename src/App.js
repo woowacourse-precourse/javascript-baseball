bafaac2744b8ca18d0b10cb6b0131477d39dc002
hailey-hy <lillyine.hy@gmail.com>
2022-11-08 00:41:26 +0900

    fix: 컴퓨터 숫자와 사용자 숫자간의 타입이 상이한 문제 해결

--- src/App.js
+++ src/App.js
@@ -9,27 +9,31 @@ class App {
   }
 
   gameStart(computer) {
-    const user = this.pickUserNumber;
-    this.checkUser(user);
-    const result = this.scoreCounter(user, computer);
-    const answer = this.printScore(result);
-    MissionUtils.Console.print(answer);
-    if(answer === '3스트라이크'){
-      MissionUtils.Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
-      this.restartOrQuit();
-    }
-    else{
-      this.gameStart();
-    }
+    MissionUtils.Console.readLine('숫자를 입력해주세요.', (user) => {
+      console.log(computer);
+      let userSplited = user.split("");
+      console.log(userSplited);
+      this.checkUser(userSplited);
+      const result = this.scoreCounter(userSplited, computer);
+      const answer = this.printScore(result);
+      MissionUtils.Console.print(answer);
+      if(answer === '3스트라이크'){
+        MissionUtils.Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+        this.restartOrQuit();
+      }
+      else{
+        this.gameStart(computer);
+      }
+    });
   }
 
   // 정답 숫자 선정
   get pickComputerNumber() {
-    let computer = '';
+    let computer = [];
     while (computer.length < 3) {
       const number = MissionUtils.Random.pickNumberInRange(1, 9);
       if (!computer.includes(number)) {
-        computer += String(number);
+        computer.push(number);
       }
     }
     return computer;
@@ -37,12 +41,11 @@ class App {
 
   // 사용자로부터 입력값 얻기
   get pickUserNumber() {
-    let user = ''
+    let user = '';
     MissionUtils.Console.readLine('숫자를 입력해주세요.', (answer) => {
-      console.log(`숫자를 입력해주세요 : ${answer}`);
-      user = answer
+      user = answer;
     });
-    return user;
+    return user
   }
 
   // 입력값 예외 처리
@@ -56,19 +59,18 @@ class App {
 
   // 숫자인지 체크
   checkIfNumber(user) {
-    if(isNaN(user)){ 
-      MissionUtils.Console.close()
-      throw new Error ('숫자가 아님');
-      
+    for (let i = 0; i < 3; i++){
+      if(isNaN(user[i])){ 
+        throw new Error ('숫자가 아님');
+      }
     }
   }
   
   // 3자리인지 체크
   checkIfThreeDigit(user) {
     if(user.length != 3){
-      MissionUtils.Console.close()
-      throw new Error ('3자리가 아님');
       
+      throw new Error ('3자리가 아님');
     }
   }
   
@@ -76,17 +78,16 @@ class App {
   checkIfDiff(user) {
     let numberCheck = new Set(user);
     if(numberCheck.size != user.length){
-      MissionUtils.Console.close()
-      throw new Error ('서로 다른 숫자가 아님');
       
+      throw new Error ('서로 다른 숫자가 아님');
     }
   }
   
   // 1 ~ 9사이의 숫자인지 체크
   checkIfnotZero(user) {
     for (let i = 0; i < 3; i++){
-      if(1 > Number(input[i]) || Number(input[i])>9){
-        MissionUtils.Console.close()       
+      if(1 > Number(user[i]) || Number(user[i])>9){
+               
         throw new Error ('1 ~ 9 사이의 숫자가 아님');
       }
     }
@@ -96,10 +97,10 @@ class App {
   scoreCounter(user, computer) {
     let result = [0, 0]
     for(let i = 0; i < 3; i++){
-      if(user[i] === computer[i]){
+      if(user[i] == computer[i]){
         result[0] += 1;
       }
-      else if(computer.includes(user[i])){
+      else if(computer.includes(Number(user[i]))){
         result[1] += 1;
       }
     }
