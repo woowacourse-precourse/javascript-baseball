e973b47d20fe51fabee97e709f1d090ff7af9547
HWI <dlwognl99@naver.com>
2022-11-07 13:10:48 +0900

    feat: 게임을 종료하거나 재시작하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -8,10 +8,14 @@ class App {
     const computerNumber = RandomChoice();
     // 사용자 값 입력
     const userNumber = userInput();
+    console.log(userNumber);
     // 게임 결과
     gameResult(computerNumber, userNumber);
   }
 }
+
+const app = new App();
+app.play();
 
 function gameStart() {
   MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
@@ -84,6 +88,19 @@ function countBall(computerNumber, userNumber) {
     .length;
 }
 
-function reStart() {}
+function reStart() {
+  MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+  MissionUtils.Console.readLine(
+    "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. ",
+    (answer) => {
+      if (answer === "1") {
+        app.play();
+      }
+      if (answer === "2") {
+        MissionUtils.Console.close();
+      }
+    }
+  );
+}
 
 module.exports = App;
