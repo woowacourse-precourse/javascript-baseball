d2545b1c6f6b0b5acfb47f65aa275d0075b944a4
김아현 <dhflxhdxhd@gmail.com>
2022-11-07 21:50:10 +0900

    Feat : class 추가

--- src/App.js
+++ src/App.js
@@ -1,55 +1,53 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
-function printResult(strike,ball){
-  if(strike === 0 && ball === 0){
-    console.log('낫싱');
-  }else if(strike>0 && ball > 0){
-    console.log(`${strike}스트라이크 ${ball}볼`);
-  }else if(strike>0){
-    console.log(`${strike}스트라이크`);
-  }else if(ball > 0){
-    console.log(`${ball}볼`);
-  }
-  
-  if(strike === 3){
-      console.log('정답입니다.')
-  }
-}
-
-
-function countStrikeAndBall() {
-  let userNum = "123";
-  let randomNumber = makeRandom();
-  let strike = 0;
-  let ball = 0;
-
-  if (userNum !== randomNumber)
-  randomNumber.forEach((item, index) => {
-    if (item === userNum[index]) {
-      strike++;
-    } else if (userNum.includes(item)) {
-      ball++;
-    }
-  });
-
-  print(strike,ball);
-
-  return `${strike} and ${ball}`;
-}
-
-function makeRandom() {
-  let answer = new Array();
-
-  while (answer.length < 3) {
-    let number = MissionUtils.Random.pickNumberInRange(1, 9);
-
-    if (!answer.includes(number)) {
-      answer.push(number);
+class App {
+  printResult(strike, ball, randomNumber) {
+    if (strike === 0 && ball === 0) {
+      console.log("낫싱");
+    } else if (strike > 0 && ball > 0) {
+      console.log(`${strike}스트라이크 ${ball}볼`);
+    } else if (strike > 0) {
+      console.log(`${strike}스트라이크`);
+    } else if (ball > 0) {
+      console.log(`${ball}볼`);
     }
   }
 
-  console.log(`answer ${answer}`);
-  return answer;
+  countStrikeAndBall(userNumber, randomNumber) {
+    let strike = 0;
+    let ball = 0;
+
+    if (userNumber !== randomNumber) {
+      userNumber.forEach((item, index) => {
+        if (item === randomNumber[index]) {
+          strike++;
+        } else if (randomNumber.includes(item)) {
+          ball++;
+        }
+      });
+
+      this.printResult(strike, ball, randomNumber);
+    }
+  }
+
+  makeRandom() {
+    let randomArray = new Array();
+
+    while (randomArray.length < 3) {
+      let number = MissionUtils.Random.pickNumberInRange(1, 9);
+
+      if (!randomArray.includes(number)) {
+        randomArray.push(number);
+      }
+    }
+
+    const toStringRandomArray = randomArray.map((value) => String(value));
+    return toStringRandomArray;
+  }
+
+  play() {}
 }
 
-console.log(countStrikeAndBall());
+const app = new App();
+app.play();
+// module.exports = App;
