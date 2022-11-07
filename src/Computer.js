d1afb1ba65244647b7147f47945b346e75fb8fe1
Suhyeon Lee <suhyeonlee.dev@gmail.com>
2022-11-07 15:02:28 +0900

    feat: 입력받은 숫자의 유효성을 검사하는 기능 구현

--- src/Computer.js
+++ src/Computer.js
@@ -12,8 +12,20 @@ class Computer {
     return randomNumList.join('');
   }
 
+  validateInput(userInput) {
+    return (
+      userInput.length === 3 &&
+      Boolean(userInput.match(/^[1-9]+$/)) &&
+      new Set(userInput.split('')).size === 3
+    );
+  }
+
   getUserNumber() {
-    Console.readLine('숫자를 입력해주세요 : ', (input) => {});
+    Console.readLine('숫자를 입력해주세요 : ', (userInput) => {
+      if (!this.validateInput(userInput)) {
+        throw new Error('유효하지 않은 값이 입력되어 게임이 종료됩니다.');
+      }
+    });
   }
 }
 
