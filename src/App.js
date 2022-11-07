6bff0b6d4eff612d2b64e6b00126a703c0329e00
yesolyo <ahnyelim77@gmail.com>
2022-11-07 22:23:19 +0900

    feat: 같은 숫자가 다른 자리에 있는 수만큼 볼 표시

--- src/App.js
+++ src/App.js
@@ -62,6 +62,8 @@ class App {
   strikeBallResult(strikeBallCount) {
     if (strikeBallCount[0] > 0 && strikeBallCount[1] == 0) {
       result = strikeBallCount[0] + "스트라이크";
+    } else if (strikeBallCount[1] > 0 && strikeBallCount[0] == 0) {
+      result = strikeBallCount[1] + "볼";
     }
   }
 }
