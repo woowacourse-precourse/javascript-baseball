4efd8943f84cd429b55a35de44d3d6cc4125a278
동동이 <drdragon777@naver.com>
2022-11-08 05:32:50 +0900

    chore - 종료 문구 수정

--- src/Console.js
+++ src/Console.js
@@ -4,7 +4,7 @@ class Console {
   static START = "숫자 야구 게임을 시작합니다.";
   static INPUT = "숫자를 입력해주세요 : ";
   static END = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
-  static END_NOTICE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+  static END_NOTICE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n";
 
   static readLine(message, callback) {
     MissionUtils.Console.readLine(message, callback);
