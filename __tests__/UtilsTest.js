e45ff11d0859d93cee41ddf59beea49e31d2988d
daal2quf <daal2quf@gmail.com>
2022-11-07 20:09:32 +0900

    test: MissionUtils 라이브러리 함수화 파일중 close 함수 테스트
    테스트중 MissionUtils라이브러리 함수화 파일에 close()를 Close로 오타발견

--- __tests__/UtilsTest.js
+++ __tests__/UtilsTest.js
@@ -4,8 +4,14 @@ const MissionUtils = require("@woowacourse/mission-utils");
 
 describe("utils 정상 작동 테스트", () => {
   test("print 출력 테스트", () => {
-    const printTest = jest.spyOn(MissionUtils.Console, "print");
+    const printMocking = jest.spyOn(MissionUtils.Console, "print");
     print("안녕하세여");
-    expect(printTest).toBeCalledWith("안녕하세여");
+    expect(printMocking).toBeCalledWith("안녕하세여");
+  });
+
+  test("close 작동 테스트", () => {
+    const closeMocking = jest.spyOn(MissionUtils.Console, "close");
+    close();
+    expect(closeMocking).toBeCalledWith();
   });
 });
