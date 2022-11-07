9459f3ee34e6f87cedc6d4d532270966c99243dc
c0c0pang <coc1789@naver.com>
2022-11-08 04:04:56 +0900

    feat: 사용자의 숫자와 선택을 관리하는 파일 추가

---
+++ src/UserData.js
@@ -0,0 +1,13 @@
+class User {
+    constructor() {
+      this.number;
+      this.select;
+    };
+    getNumber() {
+      return this.number;
+    };
+    getSelect() {
+      return this.select;
+    };
+  };
+module.exports = User;
