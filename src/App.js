6dc1378957cc37e3e722c153e170d27760bf7982
thewronghand <penfreak77@gmail.com>
2022-11-08 04:25:27 +0900

    feat: getRandomNumber 기능 구현
    특정 범위 내에서 정수인 난수 문자열을 생성하는 getRandomNumber 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
-class App {
-  play() {}
+function App () {
+  this.play = () => {
+    const getRandomNumber = (MIN_RANDOM_NUIMBER, MAX_RANDOM_NUMBER) => {
+      const randomNumberArray = [];
+      while (randomNumberArray.length < VALID_NUMBER_LENGTH) {
+        const randomNumber = MissionUtils.Random.pickNumberInRange(
+          MIN_RANDOM_NUIMBER,
+          MAX_RANDOM_NUMBER
+        );
+        if (!randomNumberArray.includes(randomNumber)) {
+          randomNumberArray.push(randomNumber);
+        }
+      }
+      return randomNumberArray.join('');
+    }
+
+  }
 }
 
 module.exports = App;
+
