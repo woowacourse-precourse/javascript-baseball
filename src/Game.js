4383d659c43c9dc3019781e66d9767d418f6c188
DAIN KIM <dnkim419@gmail.com>
2022-11-08 04:27:59 +0900

    refactor(기능5): 코드 위치 수정
    - "게임 종료" 문구가 Console.readLine을 통해 출력될 경우, 우테코에서 제공한 ApplicationTest의 테스트 중 하나에서 fail 발생
    - Console.print를 통해 출력되도록 위치 수정

--- src/Game.js
+++ src/Game.js
@@ -75,8 +75,9 @@ class Game {
   }
 
   askReplay() {
+    Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
     Console.readLine(
-      '3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n',
+      '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n',
       (inputStr) => {
         const answer = this.isValidAnswer(inputStr);
         if (answer === 1) this.play();
