5013518db216316205e52f8ceadc962625fb4188
daal2quf <daal2quf@gmail.com>
2022-11-07 22:36:42 +0900

    feat: 게임 진행시 필요한 메세지 객체 파일 추가

---
+++ src/Message.js
@@ -0,0 +1,16 @@
+
+const GAME_MSG = {
+  START: "숫자 야구 게임을 시작합니다.",
+  NUM_REQUEST: "숫자를 입력해주세요 : ",
+  END: "게임 종료",
+  RESTART_ASK: "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
+  WIN: "3개의 숫자를 모두 맞히셨습니다! 게임 종료"
+};
+
+const BASEBALL_MSG = {
+  STRIKE: "스트라이크",
+  BALL: "볼",
+  NOTHING: "낫싱"
+};
+
+module.exports = { GAME_MSG, BASEBALL_MSG };
