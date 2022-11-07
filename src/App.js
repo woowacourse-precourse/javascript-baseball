10054e276b21fbec2e033160782848934ebe7c88
ho-ji <wkdd0205@gmail.com>
2022-11-07 21:56:31 +0900

    feat: Add isCorrectInput function
    1~9로 이뤄진 서로 다른 3자리 수인지 판별하는 함수를 구현함

--- src/App.js
+++ src/App.js
@@ -23,5 +23,18 @@ function createComputerNumber() {
   }
   return computer;
 }
+function isCorrectInput(input) {
+  const changeInputToNumber = Number(input);
+  if (changeInputToNumber === NaN) return false;
+  if (changeInputToNumber < 123) return false;
+  if (changeInputToNumber > 987) return false;
+  if (input.length != 3) return false;
+  if (input[0] === input[1]) return false;
+  if (input[1] === input[2]) return false;
+  if (input[0] === input[2]) return false;
+  if (input[1] === "0") return false;
+  if (input[2] === "0") return false;
+  return true;
+}
 
 module.exports = App;
