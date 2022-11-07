eb396a1cb256775650b7dd03fd0761c26468fd49
Minju Park <project.mizzu@gmail.com>
2022-11-08 00:56:17 +0900

    Feat : 게임 시작 안내 문구 출력
    - 애플리케이션 실행 시 "숫자 야구 게임을 시작합니다." 텍스트 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+const { MissionUtils, Console } = require('@woowacourse/mission-utils');
+
 class App {
-  play() {}
+  play() {
+    this.print('숫자 야구 게임을 시작합니다.');
+    this.close();
+  }
+
+  print(msg) {
+    Console.print(msg);
+  }
+
+  close() {
+    Console.close();
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
