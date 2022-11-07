ba545422246799f4d0b458190edd1753ff22f4c0
hafnium1923 <hafnium1923@gmail.com>
2022-11-08 03:57:16 +0900

    feat fuc_makeRandom
    랜덤 값 생성 함수 추가

--- src/App.js
+++ src/App.js
@@ -4,26 +4,32 @@ class App {
   
   play() {
     MISSIONUTILS.Console.print('숫자 야구 게임을 시작합니다.');
-    const RANDOM = MISSIONUTILS.Random.pickUniqueNumbersInRange(1, 9, 3);
-    console.log(RANDOM);//콘솔
-    this.startGame(RANDOM);
+    this.startGame(this.makeRandomNum());
+  }
+  
+  makeRandomNum() {
+    let com_num = [];
+    while(com_num.length < 3){
+      let num =  MISSIONUTILS.Random.pickNumberInRange(1,9);
+      if(!com_num.includes(num))
+        com_num.push(num);
+    }
+    return com_num;
   }
   
   reGame() {
     MISSIONUTILS.Console.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.', (wantGame) => {
       if(wantGame === '1'){
-        const RANDOM = MISSIONUTILS.Random.pickUniqueNumbersInRange(1, 9, 3);
-        this.startGame(RANDOM);
+        this.startGame(this.makeRandomNum());
       }else if(wantGame === '2'){
         return MISSIONUTILS.Console.close();
-      }else throw '다시하기_ 잘못된 입력입니다.';
+      }//else throw '다시하기_ 잘못된 입력입니다.';
       
     })
   }
 
   startGame(RANDOM) {       
     MISSIONUTILS.Console.readLine('숫자를 입력해주세요', (user_num) => {
-      if(user_num.length != 3) throw '숫자입력_ 잘못된 입력입니다.';
       let flag = this.checkNum(RANDOM,user_num);
       if(flag){
         MISSIONUTILS.Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
@@ -33,13 +39,14 @@ class App {
   }
 
   checkNum(RANDOM,num) {
+    console.log(`입력값확인 랜덤: ${RANDOM} 입력: ${num}`);
     let bucketNum = [];
     for(let i = 2; i >= 0; i--) {
-      if(num % 10 === 0) throw '입력범위 아님_ 0 입력';
+     // if(num % 10 === 0) throw '입력범위 아님_ 0 입력';
       bucketNum[i] = num % 10;
       num = parseInt(num /10); 
     }
-    if(this.duplicateCheck(bucketNum)) throw '중복 값 입력';
+    //if(this.duplicateCheck(bucketNum)) throw '중복 값 or 범위 밖 입력';
 
     let strike = [];
     let j = -1;
@@ -57,7 +64,7 @@ class App {
     else
       this.startGame(RANDOM);
   }
-
+  
   duplicateCheck(bucketNum){
     const NUM_SET = new Set(bucketNum);
     if(NUM_SET.size != 3) return 1;
