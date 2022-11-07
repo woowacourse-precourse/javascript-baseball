f7597a14f372ef51a2c593c7901e8d791563418c
Yesong <to06109@naver.com>
2022-11-07 17:27:07 +0900

    feat: 초기 랜덤값 배열에 push

--- src/getNewAnswer.js
+++ src/getNewAnswer.js
@@ -8,6 +8,15 @@ function getRandomValue() {
   return Random.pickNumberInRange(1, 9);
 }
 
-function getNewAnswer() {}
+function getNewAnswer() {
+  answer = [];
+  while (answer.length < 3) {
+    const newRandomValue = getRandomValue();
+    if (checkDifferentValue(newRandomValue)) {
+      answer.push(newRandomValue);
+    }
+  }
+  return answer.join("");
+}
 
 module.exports = getNewAnswer;
