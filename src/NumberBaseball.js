d1c2c13d1e83298995e2c3ed5c8a7746556a1396
piporica <49173614+piporica@users.noreply.github.com>
2022-11-08 01:35:23 +0900

    feat: Make user answer reciver
    make user answer reciver
    it get three number string for user,
    and return number array (length = 3)
    use callback function

--- src/NumberBaseball.js
+++ src/NumberBaseball.js
@@ -1,18 +1,32 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 
 class NumberBaseball {
-  gameStart() {
+  async gameStart() {
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
 
     let answer = this.makeAnswer();
     console.log(answer);
 
-    MissionUtils.Console.close();
+    this.tryCycle();
+    // close는 게임 끝났을 때 넣어야 함 - js 비동기 조심할 것
+  }
+
+  tryCycle() {
+    this.getUserReply((reply) => {
+      console.log(reply);
+    });
   }
 
   makeAnswer() {
-    let pickedNumber = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
+    const pickedNumber = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
     return pickedNumber;
+  }
+
+  getUserReply(callbackFunc) {
+    MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (answer) => {
+      // 오류입력 체크 들어가야 할 부분
+      callbackFunc([...answer].map((x) => Number(x)));
+    });
   }
 }
 module.exports = NumberBaseball;
