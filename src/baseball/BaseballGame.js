ff99a5cef7d0fc938d31f18f672f6fededb8de88
Y2IN <dpdls0228@daum.net>
2022-11-08 01:02:06 +0900

    feat : strike,ball,nothing count해서 출력 구현

--- src/baseball/BaseballGame.js
+++ src/baseball/BaseballGame.js
@@ -53,4 +53,18 @@ class BaseballGame {
     });
     return [strike, ball, nothing];
   };
+
+  printResultCount = (strike, ball, nothing) => {
+    if (nothing == 3) {
+      Console.print("낫싱");
+    } else if (ball > 0 && strike === 0) {
+      Console.print(`${ball}볼`);
+    } else if (ball == 0 && strike > 0) {
+      Console.print(`${strike}strike`);
+    }
+    Console.print(`${ball}볼 ${strike}스트라이크`);
+
+    if (strike === 3)
+      Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+  };
 }
