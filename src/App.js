3f8351fbea2480652dee0c232758b7b54572a7fe
wzrabbit <curious.wzrabbit@gmail.com>
2022-11-07 10:55:30 +0900

    feat(app): 게임 내의 라운드를 진행하는 기능 구현
    - enterGuessMode()
    - 라운드란, 플레이어가 추측한 숫자를 입력으로 받아오고 결과를 출력하는 과정
    - 입력값 검증은 gametools의 errorIfInvalidGuessFormat(playerGuess)를 사용

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+const { Console } = require('@woowacourse/mission-utils');
+const { MESSAGE } = require('./Constant');
 const BaseballGameTools = require('./BaseballGameTools');
 
 class App {
@@ -7,6 +9,22 @@ class App {
 
   shuffleNumber() {
     this.answerArray = BaseballGameTools.getThreeNumber();
+  }
+
+  enterGuessMode() {
+    Console.readLine(MESSAGE.ASK_GUESS, (playerGuess) => {
+      BaseballGameTools.errorIfInvalidGuessFormat(playerGuess);
+      const baseballResult = BaseballGameTools
+        .getBaseballResultMessage(this.answerArray, playerGuess);
+      Console.print(baseballResult);
+
+      if (baseballResult === MESSAGE.THREE_STRIKE) {
+        Console.print(MESSAGE.GAME_CLEAR);
+        this.enterReplayChooseMode();
+      } else {
+        this.enterGuessMode();
+      }
+    });
   }
 }
 
