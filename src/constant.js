a494deee38c8a29ce6a04482945adeee51e80447
nangkyeonglim <nangkyeonglim@gmail.com>
2022-11-07 18:21:05 +0900

    fix: GAME_END 선언 전 사용 수정

--- src/constant.js
+++ src/constant.js
@@ -10,16 +10,16 @@ const RESULT = {
     STRIKE: '스트라이크',
 };
 
+const GAME_END = {
+    RETRY: '1',
+    EXIT: '2',
+};
+
 const TEXT = {
     INITIAL: '숫자 야구 게임을 시작합니다.',
     GET_USER_NUMBER: '숫자를 입력해주세요 : ',
     CORRECT_ANSWER: `${ANSWER.LENGTH}개의 숫자를 모두 맞히셨습니다! 게임 종료`,
     RETRY: `게임을 새로 시작하려면 ${GAME_END.RETRY}, 종료하려면 ${GAME_END.EXIT}를 입력하세요.\n`,
-};
-
-const GAME_END = {
-    RETRY: '1',
-    EXIT: '2',
 };
 
 const ERROR = {
