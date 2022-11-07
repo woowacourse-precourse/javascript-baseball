55cbee103259e61c88e047c63ac9f801efa5febb
Sunghyun627 <kksscc4@gmail.com>
2022-11-07 14:42:58 +0900

    feat: 컴퓨터의 1부터 9까지 서로 다른 수로 이루어진 3자리 수를 생성하는 기능

--- src/App.js
+++ src/App.js
@@ -3,10 +3,25 @@ const messages = require('./Constants.js');
 class App {
   play() {
     this.printGameStartMessage();
+    this.init();
+  }
+
+  init() {
+    this.computerNumber = this.createComputerNumber();
   }
 
   printGameStartMessage() {
     MissionUtils.Console.print(messages.GAME_START_MESSAGE);
   }
+
+  createComputerNumber() {
+    let computerNumber = '';
+    while (computerNumber.length !== 3) {
+      let digit = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computerNumber.includes(digit)) computerNumber += digit;
+    }
+    return Number(computerNumber);
+  }
 }
+
 module.exports = App;
