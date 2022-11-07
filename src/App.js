bce032801acffb84bb69d2353e434f7e6d37ad98
sein <hyuntpdls@gmail.com>
2022-11-08 01:55:10 +0900

    feat: 사용자가 3개의 숫자를 모두 맞힐 경우 게임 종료 문구를 출력

--- src/App.js
+++ src/App.js
@@ -100,6 +100,8 @@ class App {
 
   printWinMessage(result) {
     MissionUtils.Console.print(result);
+    MissionUtils.Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+    MissionUtils.Console.print('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.');  
   }
 
   printMessage(result) {
