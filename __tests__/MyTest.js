1e7c1399a5060647fa47f61dc9ce9a823f41df64
Suyeon <msjgr2801@gmail.com>
2022-11-07 12:50:57 +0900

    Test: [MyTest.js] Add Test Case File
    JEST Test Case 추가
    1. strike, ball 에 대한 판별문이 모두 동작하는지 확인하는 케이스
    2. 재시작이 1,2 외 다른 숫자 입력 시 throw 발생하는 예외 케이스
    3. 사용자 입력 값이 3글자가 아니거나, 중복되는 값이거나, 숫자가 아니면 throw 발생하는 예외 케이스

---
+++ __tests__/MyTest.js
@@ -0,0 +1,116 @@
+const App = require("../src/App");
+const MissionUtils = require("@woowacourse/mission-utils");
+
+const mockQuestions = (answers) => {
+  MissionUtils.Console.readLine = jest.fn();
+  answers.reduce((acc, input) => {
+    return acc.mockImplementationOnce((question, callback) => {
+      callback(input);
+    });
+  }, MissionUtils.Console.readLine);
+};
+
+const mockRandoms = (numbers) => {
+  MissionUtils.Random.pickNumberInRange = jest.fn();
+  numbers.reduce((acc, number) => {
+    return acc.mockReturnValueOnce(number);
+  }, MissionUtils.Random.pickNumberInRange);
+};
+
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(MissionUtils.Console, "print");
+  logSpy.mockClear();
+  return logSpy;
+};
+
+describe("숫자 야구 게임", () => {
+  // 기본 동작
+  test("낫싱, 스트라이크, 볼, 스트라이크&볼, 정답 모두 출력 후 게임 종료", () => {
+    const randoms = [1, 3, 5];
+    const answers = ["789", "124", "389", "173", "135", "2"];
+    const logSpy = getLogSpy();
+    const messages = [
+      "낫싱",
+      "1스트라이크",
+      "1볼",
+      "1볼 1스트라이크",
+      "3스트라이크",
+      "게임 종료",
+    ];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    const app = new App();
+    app.play();
+
+    messages.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
+  test("재시작 시 예외 테스트", () => {
+    const randoms = [1, 3, 5];
+    const answers = ["135", "3"];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow();
+  })
+
+  test("중복 숫자 예외 테스트", () => {
+    const randoms = [1, 3, 5];
+    const answers = ["114"];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow();
+  });
+
+  test("3글자 미만 예외 테스트", () => {
+    const randoms = [1, 3, 5];
+    const answers = ["12"];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow();
+  });
+
+  test("3글자 초과 예외 테스트", () => {
+    const randoms = [1, 3, 5];
+    const answers = ["1234"];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow();
+  });
+
+  test("isNaN 입력 예외 테스트", () => {
+    const randoms = [1, 3, 5];
+    const answers = ["가나다"];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow();
+  });
+});
