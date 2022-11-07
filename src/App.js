cdfab3f65bad19ac43f6424f31028f019356d84c
jiho1996 <jhp9270@naver.com>
2022-11-07 15:19:56 +0900

    refactor : App play로직 분리

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
-const NumericBaseballGame = require("./game");
+const MissionUtils = require("@woowacourse/mission-utils");
+// const GameController = require("./GameController");
+const ComputerModel = require("./ComputerModel");
+const GameController = require("./GameController");
+const UserModel = require("./UserModel");
 
 class App {
   constructor() {
-    this.numericBaseballGame = new NumericBaseballGame();
+    this.GameController = new GameController();
+    this.computerModel = new ComputerModel();
+    this.userModel = new UserModel();
   }
 
   play() {
-    this.numericBaseballGame.start();
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+
+    const numberFromComputer = this.computerModel.getNumberFromComputer();
+    console.log(numberFromComputer);
+    this.try(numberFromComputer);
+  }
+
+  try(numberFromComputer) {
+    MissionUtils.Console.readLine(
+      "숫자를 입력해주세요 : ",
+      (numberFromUser) => {
+        const validNumberFromUser = this.userModel.convertStringToArray(
+          this.userModel.isInputNumbersValid(numberFromUser)
+        );
+        const isClear = this.GameController.start(
+          validNumberFromUser,
+          numberFromComputer
+        );
+        this.try(numberFromComputer);
+      }
+    );
   }
 }
 
