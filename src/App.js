7359b0ad6bab00442c3783358ca281fc629f29e0
YoommY <isho48239@gmail.com>
2022-11-08 08:48:55 +0900

    Feat : 컴퓨터 숫자 랜덤 3개 뽑기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const constants = require('./constants');
+
 class App {
-  play() {}
-}
+  play() {
+    let computer = [];
+    let strike;
+    let ball;
+    let nothing;
+
+    // 컴퓨터의 랜덤한 숫자 3개
+    function computerNumbers() {
+      while (computer.length < 3) {
+        let number = MissionUtils.Random.pickNumberInRange(1, 9);
+        if (!computer.includes(number)) { //중복되지 않는 숫자만 뽑기
+          computer.push(number);
+        }
+      }
+    }
+
+
+    computerNumbers();
+
+  }
+  }
 
 module.exports = App;
