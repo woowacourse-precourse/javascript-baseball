54c5190e147922ae180a086fbb19c029340df0b1
Suyoung <jsy8492@gmail.com>
2022-11-07 15:31:47 +0900

    test: RegexTest - 어떤 정규식에 대한 테스트인지 알아볼 수 있도록 describe문 추가
    - 올바른 입력값에 대해서도 테스트하기 위해 테스트 케이스 추가

--- __tests__/RegexTest.js
+++ __tests__/RegexTest.js
@@ -5,55 +5,63 @@ const {
 } = require('../src/lib/constants/validation');
 
 describe('정규식 테스트', () => {
-  test('중복이 없는 세 자리 숫자인 경우 true를 반환해야 한다.', () => {
-    const inputValues = ['123', '456'];
-    const result = inputValues.map(inputValue =>
-      THREE_DIGIT_NUMBER_REGEX.test(inputValue),
-    );
+  describe('1에서 9까지의 수로 이루어진 세 자리 수에 대한 정규식 테스트', () => {
+    test('세 자리 숫자인 경우 true를 반환해야 한다.', () => {
+      const inputValues = ['123', '456', '1234'];
+      const result = inputValues.map(inputValue =>
+        THREE_DIGIT_NUMBER_REGEX.test(inputValue),
+      );
 
-    expect(result).toEqual([true, true]);
+      expect(result).toEqual([true, true, false]);
+    });
+
+    test('0이 포함된 경우 false를 반환해야 한다.', () => {
+      const inputValues = ['012', '123'];
+      const result = inputValues.map(inputValue =>
+        THREE_DIGIT_NUMBER_REGEX.test(inputValue),
+      );
+
+      expect(result).toEqual([false, true]);
+    });
+
+    test('숫자가 아닌 문자가 포함된 경우 false를 반환해야 한다.', () => {
+      const inputValues = ['asdf', 'asd120', 'a1b2c3', '123'];
+      const result = inputValues.map(inputValue =>
+        THREE_DIGIT_NUMBER_REGEX.test(inputValue),
+      );
+
+      expect(result).toEqual([false, false, false, true]);
+    });
+
+    test('세 자리 숫자가 아닌 경우 false를 반환해야 한다.', () => {
+      const inputValues = ['1234', '12', '3', '123'];
+      const result = inputValues.map(inputValue =>
+        THREE_DIGIT_NUMBER_REGEX.test(inputValue),
+      );
+
+      expect(result).toEqual([false, false, false, true]);
+    });
   });
 
-  test('1 또는 2인 경우 true를 반환해야 한다.', () => {
-    const inputValues = ['1', '2'];
-    const result = inputValues.map(inputValue =>
-      GAME_MENU_CODE_REGEX.test(inputValue),
-    );
+  describe('1 또는 2의 숫자에 대한 정규식 테스트', () => {
+    test('1 또는 2인 경우 true를 반환해야 한다.', () => {
+      const inputValues = ['1', '2', '3'];
+      const result = inputValues.map(inputValue =>
+        GAME_MENU_CODE_REGEX.test(inputValue),
+      );
 
-    expect(result).toEqual([true, true]);
+      expect(result).toEqual([true, true, false]);
+    });
   });
 
-  test('중복된 숫자가 존재하는 경우 true를 반환해야 한다.', () => {
-    const inputValues = ['122', '455', '699', '333'];
-    const result = inputValues.map(inputValue =>
-      DUPLICATE_CHARACTER_REGEX.test(inputValue),
-    );
+  describe('중복된 숫자를 포함하는지에 대한 정규식 테스트', () => {
+    test('중복된 숫자가 존재하는 경우 true를 반환해야 한다.', () => {
+      const inputValues = ['122', '455', '699', '333', '123'];
+      const result = inputValues.map(inputValue =>
+        DUPLICATE_CHARACTER_REGEX.test(inputValue),
+      );
 
-    expect(result).toEqual([true, true, true, true]);
-  });
-
-  test('0이 포함된 경우 false를 반환해야 한다.', () => {
-    const inputValue = '012';
-    const result = THREE_DIGIT_NUMBER_REGEX.test(inputValue);
-
-    expect(result).toEqual(false);
-  });
-
-  test('숫자가 아닌 문자가 포함된 경우 false를 반환해야 한다.', () => {
-    const inputValues = ['asdf', 'asd120', 'a1b2c3'];
-    const result = inputValues.map(inputValue =>
-      THREE_DIGIT_NUMBER_REGEX.test(inputValue),
-    );
-
-    expect(result).toEqual([false, false, false]);
-  });
-
-  test('세 자리 숫자가 아닌 경우 false를 반환해야 한다.', () => {
-    const inputValues = ['1234', '12', '3'];
-    const result = inputValues.map(inputValue =>
-      THREE_DIGIT_NUMBER_REGEX.test(inputValue),
-    );
-
-    expect(result).toEqual([false, false, false]);
+      expect(result).toEqual([true, true, true, true, false]);
+    });
   });
 });
