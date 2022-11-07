6b8eb35da5149a1ddca3c4e9c720d6e39adb32e4
Steadystudy <sgmin206@naver.com>
2022-11-08 01:49:02 +0900

    feat: 스트라이크, 볼 갯수 세기

--- src/App.js
+++ src/App.js
@@ -8,8 +8,11 @@ const ERROR_MESSAGE = {
   LENGTH: '3자리 숫자여야 합니다',
   GAME_END: '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.',
 };
+
 class App {
   #answer;
+  strike;
+  ball;
 
   constructor() {
     MissionUtils.Console.print(GAME_START_COMMENT);
@@ -31,9 +34,24 @@ class App {
     if (value.length !== 3) throw new Error(ERROR_MESSAGE.LENGTH);
   }
 
+  checkStirkeOrBall(value) {
+    const splittedValue = value.toString().split('').map(Number);
+    this.strike = 0;
+    this.ball = 0;
+
+    this.#answer.forEach((num, index) => {
+      if (num === splittedValue[index]) {
+        this.strike += 1;
+      } else if (splittedValue.includes(num)) {
+        this.ball += 1;
+      }
+    });
+  }
+
   play() {
     MissionUtils.Console.readLine(INPUT_NUMBER, (value) => {
       this.isValidNumber(value);
+      this.checkStirkeOrBall(value);
     });
   }
 }
