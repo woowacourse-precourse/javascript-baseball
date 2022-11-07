f3a306899c826df786cfddc7011d5d678b9d473f
HJeong1200 <hjeong1200@gmail.com>
2022-11-08 00:32:44 +0900

    사용자-숫자-입력-기능

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
-  play() {}
+  play() {
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+
+    const randomNumber = this.getRandomNumber();
+
+    this.round(randomNumber);
+  }
 
   getRandomNumber() {
     const computer = [];
@@ -14,6 +20,33 @@ class App {
     }
 
     return computer;
+  }
+
+  round() {
+    return MissionUtils.Console.readLine("숫자를 입력해주세요: ", (answer) => {
+      if (
+        answer.length !== 3 ||
+        isNaN(Number(answer)) ||
+        !this.checkDuplicate(answer)
+      ) {
+        throw new Error("잘못된 값을 입력하여 게임을 종료합니다.");
+      }
+      // 컴퓨터 입력값과 사용자가 입력한 답을 확인하는 함수
+    });
+  }
+
+  checkDuplicate(num) {
+    const set = new Set();
+
+    for (let el of num) {
+      set.add(el);
+    }
+
+    if (set.size === 3) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
 
