0ccb7f4dc7628671e059aa6b2792d815037633c0
김세환 <sehwan@gimsehwan-ui-MacBookPro.local>
2022-11-07 21:34:13 +0900

    chore(Exception): usersNumber -> userNum 변수 이름 변경

--- src/Exception.js
+++ src/Exception.js
@@ -1,23 +1,23 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
-function exception(usersNumber) {
+function exception(userNum) {
     // 길이가 3자리인지 확인, 3자리 전부 일의 자리 숫자인지 확인 가능.
-    if (usersNumber.length !== 3) {
+    if (userNum.length !== 3) {
         MissionUtils.Console.close();
         throw "잘못 입력하셨습니다.";
     }
     // 서로 다른 수인지 확인
-    if (usersNumber[0] === usersNumber[1] || usersNumber[1] === usersNumber[2] || usersNumber[0] === usersNumber[2]) {
+    if (userNum[0] === userNum[1] || userNum[1] === userNum[2] || userNum[0] === userNum[2]) {
         MissionUtils.Console.close();
         throw "중복되지 않는 숫자를 입력해주세요.";
     }
     // 숫자가 아닐경우
-    if (isNaN(usersNumber)) {
+    if (isNaN(userNum)) {
         MissionUtils.Console.close();
         throw "숫자를 입력해주세요."
     }
     // 0을 넣었을 경우
-    if (usersNumber.includes("0")) {
+    if (userNum.includes("0")) {
         MissionUtils.Console.close();
         throw "1 ~ 9 사이의 값을 입력해주세요."
     }
