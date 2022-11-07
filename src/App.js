6c96a33cd687291abfc6b5ec59e804ba91ae2459
박승훈 <tmdgns1126@naver.com>
2022-11-07 23:55:10 +0900

    feat: [App] 목표값을 성공적으로 맞춘 경우 게임 종료 문구 출력

--- src/App.js
+++ src/App.js
@@ -28,6 +28,21 @@ class App {
 
   startGame() {
     const targetNum = this.computer.createRandomNum();
+    while (true) {
+      if (this.checkComplete(targetNum)) break;
+    }
+    this.endGame();
+  }
+
+  endGame() {
+    Console.print("3스트라이크");
+    Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    Console.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+  }
+
+  checkComplete(targetNum) {
+    const userNum = this.user.guessNum();
+    if (targetNum === userNum) return true;
   }
 }
 
