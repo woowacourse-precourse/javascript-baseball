0dc198eb1237a9a1dcb33c802863f46856b1e39a
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2022-11-08 00:46:10 +0900

    feat: 1에서 9까지 서로 다른 랜덤 숫자 3개로 상대방(컴퓨터)의 수를 생성하는 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
   play() {}
+
+  getRandomNum() {
+    return MissionUtils.Random.pickNumberInRange(1, 9);
+  }
+
+  getComputerNum() {
+    let computerNum = [];
+
+    let num;
+
+    while (computerNum.length < 3) {
+      num = this.getRandomNum();
+      if (!computerNum.includes(num)) {
+        computerNum.push(num);
+      }
+    }
+
+    return computerNum;
+  }
 }
 
 module.exports = App;
