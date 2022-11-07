beccf00287b791cb0fd23f139d1e5d81fa5842ac
jisu-kim <kss2460@naver.com>
2022-11-08 02:00:03 +0900

    feat: compareNumber 함수 구현

--- src/compareNumber.js
+++ src/compareNumber.js
@@ -2,6 +2,7 @@ const MissionUtils = require("@woowacourse/mission-utils");
 const isStrike = require("../src/isStrike");
 const isBall = require("../src/isBall");
 const isNothing = require("../src/isNothing");
+const isContinue = require("../src/isContinue");
 
 const compareNumber = (initNumber) => {
   const initPrintNumber = initNumber.join("");
@@ -11,25 +12,36 @@ const compareNumber = (initNumber) => {
     const strikeScore = isStrike(initNumber, userNumber);
     const ballScore = isBall(initNumber, userNumber);
     const checkNothing = isNothing(initNumber, userNumber);
-    console.log("유저 넘버", userNumber);
-    console.log("스트라이크", strikeScore);
-    console.log("볼", ballScore);
-    console.log("낫싱", checkNothing);
+    let saveString = `${ballScore} ${strikeScore}`;
+    let printString = saveString.replace("null", "");
+    // console.log("유저 넘버", userNumber);
+    // console.log("스트라이크", strikeScore);
+    // console.log("볼", ballScore);
+    // console.log("낫싱", checkNothing);
     if (checkNothing === "낫싱") {
       MissionUtils.Console.print(`낫싱`);
       compareNumber(initNumber);
-    } else if (strikeScore === 3) {
-      MissionUtils.Console.print(`${strikeScore}스트라이크`);
-      MissionUtils.Console.print(
-        `${strikeScore}개의 숫자를 모두 맞히셨습니다! 게임종료`
-      );
-    } else {
-      MissionUtils.Console.print(`${ballScore}볼 ${strikeScore}스트라이크`);
+    } else if (
+      ballScore === null &&
+      checkNothing == false &&
+      strikeScore === "3스트라이크"
+    ) {
+      MissionUtils.Console.print(`3스트라이크`);
+      MissionUtils.Console.print(`3개의 숫자를 모두 맞히셨습니다!`);
+      isContinue();
+    } else if (strikeScore !== null && ballScore !== null) {
+      MissionUtils.Console.print(saveString);
+      compareNumber(initNumber);
+    } else if (strikeScore !== null && ballScore === null) {
+      MissionUtils.Console.print(strikeScore);
+      compareNumber(initNumber);
+    } else if (strikeScore === null && ballScore !== null) {
+      MissionUtils.Console.print(ballScore);
       compareNumber(initNumber);
     }
   });
 };
 
-const initNumber = [1, 2, 3];
-compareNumber(initNumber);
-// module.exports = compareNumber;
+// compareNumber([1, 2, 3]);
+
+module.exports = compareNumber;
