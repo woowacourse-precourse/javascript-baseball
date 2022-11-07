4e7cfd8d5af6119079f878ad0b05a2d022a6188f
Jinu <jinu200131@gmail.com>
2022-11-07 18:54:29 +0900

    feat (App) : 사용자 입력 <-> 컴퓨터 숫자 판정 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
+const { Console } = require('@woowacourse/mission-utils');
+const User = require('./User.js')
+const Computer = require('./Computer.js')
+
 class App {
-  play() {}
+  constructor() {
+    this.user = new User();
+    this.computer = new Computer();
+  }
+
+  play() {
+
+  }
+
+  getGuessRst(userGuess, computerNum) {
+    const cNumArray = [...computerNum];
+    const uNumArray = [...userGuess];
+    let ballCnt = 0, strCnt = 0;
+    for(let cnum of cNumArray) {
+        const loc = cNumArray.indexOf(cnum);
+        if(uNumArray.indexOf(cnum) === loc){
+            strCnt++; continue;
+        }
+        if(uNumArray.includes(cnum)){
+            ballCnt++;
+        }
+    }
+    return [ballCnt, strCnt];
+  }
+  
 }
 
 module.exports = App;
