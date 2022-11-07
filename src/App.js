d0e66b00f1e623db535a9ab6cec9be1ec7ba6127
Hchan2 <seongchan_@naver.com>
2022-11-07 21:51:32 +0900

    사용자 입력 예외 체크

--- src/App.js
+++ src/App.js
@@ -3,6 +3,54 @@ const GAME_START = '숫자 야구 게임을 시작합니다.';
 const RESTART = '숫자 야구 게임을 재시작합니다.';
 
 class App {
+  // 사용자 입력 숫자 문제점 체크
+  userInputCheck(userInputNumber) {
+    this.numberLengthCheck(userInputNumber);
+    this.numberCheck(userInputNumber);
+    this.overlapCheck(userInputNumber);
+    this.oneToNineCheck(userInputNumber);
+  }
+
+  // 입력 숫자가 세 자리가 아닐 경우
+  numberLengthCheck(inputNumber) {
+    if (inputNumber.length !== 3) {
+      throw new Error('세 자리를 입력해주세요.');
+    }
+  }
+
+  // 숫자를 입력하지 않은 경우
+  numberCheck(inputNumber) {
+    if (isNaN(Number(inputNumber))) {
+      throw new Error('숫자만 입력해주세요.');
+    }
+  }
+
+  // 중복 숫자가 있는 경우
+  overlapCheck(inputNumber) {
+    const numberSet = new Set(inputNumber);
+    if (numberSet.size !== inputNumber.length) {
+      throw new Error('숫자를 중복으로 입력하지 말아주세요.');
+    }
+  }
+
+  // 1부터 9 안에 입력하지 않은 경우
+  oneToNineCheck(inputNumber) {
+    if (inputNumber < 1 && inputNumber > 9) {
+      throw new Error('숫자 1과 9 사이로 입력해주세요.');
+    }
+  }
+
+  // 사용자 입력
+  userInputNumber() {
+    let userAnswer = "";
+    MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (input) => {
+      this.userInputCheck(input);
+      userAnswer = input;
+    });
+
+    return userAnswer;
+  }
+  
   // 사용자 입력 숫자 생성
   generateNumber(randomNumber) {
     const userNumber = this.userInputNumber();
