7c966613a12c3cb0c27ae0c47c79331407be3c76
jetty8013 <jetty8013@gmail.com>
2022-11-08 01:38:03 +0900

    feat: 사용자 숫자 입력 기능 구현

--- src/game.js
+++ src/game.js
@@ -1,6 +1,15 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
 const game = {
+  userInput(){
+    let input = ""
+    MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (answer) => { 
+      input = answer;
+    });
+    if(input.length !== 3 || input.includes("0")){ throw `오류 : ${input}`};
+    return input;
+    },
+
   computerNumber(){
     const computer = [];
     while (computer.length < 3) {
