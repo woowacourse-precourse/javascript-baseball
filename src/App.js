10420a8d03a44a5d66a1548a5983e07c1f945d8e
Doyoung.Kim <dyflower@nate.com>
2022-11-08 01:43:08 +0900

    refact : 생성 메소드 변경

--- src/App.js
+++ src/App.js
@@ -6,7 +6,12 @@ class App {
 
 function createAnswer() {
   let answer = [];
-  answer = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
+  while (answer.length < 3) {
+    const number = MissionUtils.Random.pickNumberInRange(1, 9);
+    if (!answer.includes(number)) {
+      answer.push(number);
+    }
+  }
   return answer;
 }
 module.exports = App;
