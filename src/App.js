0980d32bfe09862c9fad5877193d25c2b28ea1fc
Hchan2 <seongchan_@naver.com>
2022-11-07 21:49:15 +0900

    랜덤 숫자 발생 함수 생성

--- src/App.js
+++ src/App.js
@@ -3,6 +3,25 @@ const GAME_START = '숫자 야구 게임을 시작합니다.';
 const RESTART = '숫자 야구 게임을 재시작합니다.';
 
 class App {
+  // 랜덤 숫자 발생
+  numberRandom() {
+    const computer = [];
+    while (computer.length < 3) {
+        const number = MissionUtils.Random.pickNumberInRange(1, 9);
+        if (!computer.includes(number)) {
+            computer.push(number);
+        }
+    }
+    return computer.join("");
+  }
+  
+  // 게임 실행 (랜덤 숫자 생성)
+  startGame() {
+    const randomNumber = this.numberRandom();
+
+    this.generateNumber(randomNumber);
+  }
+
   play() {
     MissionUtils.Console.print(GAME_START);
     this.startGame();
