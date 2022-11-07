dee18de6535af0a5442aa476499fa32152fe9387
seoye0ng <101791501+seoye0ng@users.noreply.github.com>
2022-11-07 23:22:31 +0900

    feat : 숫자 비교 볼

--- src/App.js
+++ src/App.js
@@ -35,6 +35,27 @@ function verifyPlayerAnswer(playerAnswer) {
   ) {
     throw new Error("잘못된 값을 입력했습니다.");
   } else {
+    compareAnswer(playerAnswer);
+  }
+}
+
+function ball() {
+  const ballArray = [];
+  for (let i = 0; i < creatComputerAnswer.length; i++) {
+    if (
+      creatComputerAnswer[i] !== playerAnswer[i] &&
+      creatComputerAnswer.includes(playerAnswer[i])
+    ) {
+      ballArray.push(i);
+    }
+  }
+  return ballArr.length > 0 === true;
+}
+
+function compareAnswer(playerAnswer) {
+  //1. 볼 (같은 수 && 다른 자리)
+  if (ball()) {
+    MissionUtils.Console.print(ballArray + "볼");
   }
 }
 
