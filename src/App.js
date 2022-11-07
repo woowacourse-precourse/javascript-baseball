9c981776ad26c10452db75acf86cb147a8001cf5
박승훈 <tmdgns1126@naver.com>
2022-11-08 00:05:00 +0900

    feat: [App] 정답인 경우 입력에 따른 게임 재개 또는 종료 구현

--- src/App.js
+++ src/App.js
@@ -31,18 +31,31 @@ class App {
     while (true) {
       if (this.checkComplete(targetNum)) break;
     }
-    this.endGame();
-  }
-
-  endGame() {
-    Console.print("3스트라이크");
-    Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-    Console.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    this.sayCorrect();
+    this.askRestart();
   }
 
   checkComplete(targetNum) {
     const userNum = this.user.guessNum();
     if (targetNum === userNum) return true;
+  }
+
+  sayCorrect() {
+    Console.print("3스트라이크");
+    Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+  }
+
+  askRestart() {
+    Console.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    Console.readLine("", (isRestart) => {
+      if (isRestart === "1") return this.startGame();
+      if (isRestart === "2") return this.endGame();
+    });
+  }
+
+  endGame() {
+    Console.print("게임을 종료합니다.");
+    Console.close();
   }
 }
 
