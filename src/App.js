28d686a27e9180249e4ed2e5f298cdcac44dab8f
Haram Baek <haram@Haramui-MacBookAir.local>
2022-11-07 22:34:48 +0900

    refactor: 함수형에서 클래스형으로 변경

--- src/App.js
+++ src/App.js
@@ -18,63 +18,102 @@ const MissionUtils = require("@woowacourse/mission-utils");
 //   return randNum
 // }
 
-function checkNothing(randNum, inputNum) {
-  let isNothing = true;
+// function inputUserAnswer(randNum) {
+//   let strike = 0;
+//   let ball = 0;
+//   let inputNum = [];
 
-  inputNum.map((num, idx) => {
-    if (randNum.includes(num)) {
-      isNothing = false;
-    }
-  })
+//   MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (answer) => {
+//     for (a of answer) {
+//       inputNum.push(parseInt(a))
+//     }
+//     console.log(inputNum, '입력된 숫자');
 
-  return isNothing
-}
+//     // 낫싱인지 확인
+//     if (checkNothing(randNum, inputNum)) {
+//       MissionUtils.Console.print('낫싱');
+//     } else {
+//       // 스트라이크 갯수 확인
+//       strike = checkStrike(randNum, inputNum);
+//       // 볼 갯수 확인
+//       ballList = checkBall(randNum, inputNum);
+//     }
+//     if (strike === 3) {
+//       MissionUtils.Console.print('3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료\n')
+//       MissionUtils.Console.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.', (opinion) => {
+//         if (opinion === 1) {
+//           inputUserAnswer();
+//         } else {
+//           MissionUtils.Console.close()
+//         }
+//       })
+//     } else {
+//       MissionUtils.Console.print(`${ball}볼 ${strike}스트라이크`);
+//       inputUserAnswer();
+//     }
+//   })
+// }
 
-function checkBall(randNum, inputNum) {
-  let ball = 0;
+class App {
+  play() {
+    // 1. 컴퓨터가 정답(1-9 사이의 서로 다른 수로 이루어진 3자리 숫자)을 생성한다.
+    const RAND_NUM = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
+    //2. 사용자가 숫자를 입력한다. 
+    const INPUT_NUM = this.inputUserAnswer();
+  }
 
-  inputNum.map((num, idx) => {
-    if (randNum.includes(num) && num !== randNum[idx]) {
-      ball++;
-    }
-  })
+  inputUserAnswer() {
+    let input = [];
+    MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (answer) => {
+      for (let a of answer) {
+        input.push(parseInt(a));
+      }
+    })
+    return input
+  }
 
-  return ball
-}
+  checkNothing() {
+    let isNothing = true;
 
-function checkStrike(randNum, inputNum) {
-  let strike = 0;
+    INPUT_NUM.map((num, idx) => {
+      if (RAND_NUM.includes(num)) {
+        isNothing = false;
+      }
+    })
 
-  inputNum.map((num, idx) => {
-    if (num === randNum[idx]) {
-      strike++;
-    }
-  })
+    return isNothing
+  }
 
-  return strike
-}
+  checkBall() {
+    let ball = 0;
 
-function inputUserAnswer(randNum) {
-  let strike = 0;
-  let ball = 0;
-  let inputNum = [];
+    INPUT_NUM.map((num, idx) => {
+      if (RAND_NUM.includes(num) && num !== RAND_NUM[idx]) {
+        ball++;
+      }
+    })
 
-  MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (answer) => {
-    for (a of answer) {
-      inputNum.push(parseInt(a))
-    }
-    console.log(inputNum, '입력된 숫자');
+    return ball
+  }
 
-    // 낫싱인지 확인
-    if (checkNothing(randNum, inputNum)) {
+  checkStrike() {
+    let strike = 0;
+
+    INPUT_NUM.map((num, idx) => {
+      if (num === RAND_NUM[idx]) {
+        strike++;
+      }
+    })
+
+    return strike
+  }
+
+  printMessages() {
+    let strike = this.checkStrike;
+    let ball = this.checkBall;
+    if (this.checkNothing) {
       MissionUtils.Console.print('낫싱');
-    } else {
-      // 스트라이크 갯수 확인
-      strike = checkStrike(randNum, inputNum);
-      // 볼 갯수 확인
-      ballList = checkBall(randNum, inputNum);
-    }
-    if (strike === 3) {
+    } else if (strike === 3) {
       MissionUtils.Console.print('3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료\n')
       MissionUtils.Console.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.', (opinion) => {
         if (opinion === 1) {
@@ -87,15 +126,7 @@ function inputUserAnswer(randNum) {
       MissionUtils.Console.print(`${ball}볼 ${strike}스트라이크`);
       inputUserAnswer();
     }
-  })
-}
 
-class App {
-  play() {
-    // 1. 컴퓨터가 정답(1-9 사이의 서로 다른 수로 이루어진 3자리 숫자)을 생성한다.
-    const randNum = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
-    // console.log(randNum, "생성된 숫자");
-    inputUserAnswer(randNum);
   }
 }
 
