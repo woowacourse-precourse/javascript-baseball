cbe326bdc1be0dd4adf7cef48543a16cc43512ec
Sunghyun627 <kksscc4@gmail.com>
2022-11-07 13:16:06 +0900

    feat: 게임 시작 문구를 출력하는 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+const messages = require('./Constants.js');
 class App {
-  play() {}
-}
+  play() {
+    this.printGameStartMessage();
+  }
 
+  printGameStartMessage() {
+    MissionUtils.Console.print(messages.GAME_START_MESSAGE);
+  }
+}
 module.exports = App;
---
+++ src/Constants.js
@@ -0,0 +1,3 @@
+const GAME_START_MESSAGE = '숫자 야구 게임을 시작합니다.';
+
+module.exports = { GAME_START_MESSAGE };
