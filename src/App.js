221ab782e16531075a88f22b36954d051148d8e5
RosaDamascena <hsj990604@gmail.com>
2022-11-07 15:29:43 +0900

    feat : 컴퓨터 랜덤값과 입력값 비교 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 const MissionUtils = require("@woowacourse/mission-utils");
+
+function compare(computer_num, user_num) {
+  let i = 0;
+  let strike = 0;
+  let ball = 0;
+
+  //input 값 배열에 저장
+  const user_choice = [];
+  user_choice.push(user_num);
+}
+
 class App {
   play() {
-    const random_num = MissionUtils.Random.pickUniqueNumbersInRange([1, 9, 3]);
+    const computer_num = MissionUtils.Random.pickUniqueNumbersInRange([1, 9, 3]);
 
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
 
     let user_num;
     MissionUtils.Cosole.readLine("숫자를 입력해주세요 :", (number) => {
       user_num = number;
-      console.log(user_num);
+      console.print(user_num);
     });
   }
 }
