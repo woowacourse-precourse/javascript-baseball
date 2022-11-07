c098dfdf19b6609181641c1264939612dccf378d
turtle601 <turtle601@naver.com>
2022-11-07 14:08:22 +0900

    test(RandomNumberTest) 테스트 로직 수정
    - 각각 다른 random함수를 생성하여 숫자인지 체크와 중복 체크를 따로하는 코드 수정

--- __tests__/RandomNumberTest.js
+++ __tests__/RandomNumberTest.js
@@ -3,10 +3,11 @@ const { Console } = require('@woowacourse/mission-utils');
 
 describe('RandomNumber 클래스 - makeNew()', () => {
   test('서로 다른 임의의 세 숫자가 있는지 확인', () => {
+    const random = RandomNumber.makeNew();
     const isNumber = (item) => typeof item === 'number';
 
-    expect(RandomNumber.makeNew().every((item) => isNumber(item))).toBeTruthy();
-    expect([...new Set(RandomNumber.makeNew())]).toHaveLength(3);
+    expect(random.every((item) => isNumber(item))).toBeTruthy();
+    expect([...new Set(random)]).toHaveLength(3);
 
     Console.close();
   });
