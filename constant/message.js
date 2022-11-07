6f4258f9ce9f375c8972e534d55948c52fcad9b9
oio337a <oio337a@gmail.com>
2022-11-07 21:22:58 +0900

    feat : plus invalid message

--- constant/message.js
+++ constant/message.js
@@ -5,6 +5,10 @@ const GAME_END = "3개의 숫자를 모두 맞히셨습니다! 게임종료";
 const RESTART = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n";
 const END = "게임 종료";
 
+const INVALID_LENGTH = "3자리 숫자를 입력해 주세요.";
+const INVALID_RANGE = "1부터 9까지 숫자를 입력해 주세요.";
+const INVALID_DUPLICATE = "중복된 숫자 없이 입력해 주세요.";
+
 module.exports = {
   GAME_START,
   USER_INPUT_REQUEST,
@@ -12,4 +16,7 @@ module.exports = {
   GAME_END,
   RESTART,
   END,
+  INVALID_DUPLICATE,
+  INVALID_LENGTH,
+  INVALID_RANGE,
 };
