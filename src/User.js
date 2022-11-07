78c1fc809fa19028ab959c3cb7d63c62f29e78f6
osdoonhyun <skw1996@naver.com>
2022-11-07 17:40:10 +0900

    Feat: User.js 사용자 입력 유효성 검사 기능 구현

---
+++ src/User.js
@@ -0,0 +1,35 @@
+const Game = require("./Game");
+
+class User {
+  validateInput(userInput) {
+    return (
+      this.checkType(userInput) &&
+      this.checkUnique(userInput) &&
+      this.checkLength(userInput) &&
+      this.checkRange(userInput)
+    );
+  }
+
+  checkType(userInput) {
+    userInput.forEach((number) => {
+      if (isNaN(number)) return false;
+    });
+  }
+
+  checkUnique(userInput) {
+    const unique = new Set(userInput);
+    if (userInput.length !== unique.length) return false;
+  }
+
+  checkLength(userInput) {
+    if (userInput.length !== Game.NUMBER.LENGTH) return false;
+  }
+
+  checkRange(userInput) {
+    userInput.forEach((number) => {
+      if (!number > 0) return false;
+    });
+  }
+}
+
+module.exports = User;
