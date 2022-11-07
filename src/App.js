77d7a1aae26fb8abab372c86bfb025398047f2be
seohee0112 <ghdtj7@naver.com>
2022-11-07 17:47:51 +0900

    init : 기능 구현 함수 셋팅

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const STRIKE = 0;
+const BALL = 1;
 class App {
-  play() {}
+  constructor() {}
+
+  play() {
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+
+    const answer = this.getAnswer();
+    this.getInput(answer);
+  }
+  getAnswer() {}
+  getInput(answer) {}
+  compareAnswerAndInput(answer, userInput) {}
+  getStrikeBall(answer, userInput) {}
+
+  printCompareResult(strike, ball) {}
+
+  isAnswer(strike, ball) {}
+  restartGame() {}
+
+  detectInputError(userInput) {}
+
+  isInputValueError(userInput) {}
+
+  removeRepeatInputValue(userInput) {}
 }
+
+const game = new App();
+game.play();
 
 module.exports = App;
