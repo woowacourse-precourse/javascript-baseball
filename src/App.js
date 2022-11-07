2d6ef0b02b370518838e2d3fd615281358918e82
sh981013s <sh981013s@gmail.com>
2022-11-07 21:01:32 +0900

    feat: App 초기 실행 constructor, play 메서드 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+const Controller = require("./controller/controller");
+
 class App {
-  play() {}
+  constructor() {
+    this.controller = new Controller(true);
+  }
+
+  // 게임 초기 실행
+  play() {
+    this.controller.init();
+  }
 }
 
 module.exports = App;
