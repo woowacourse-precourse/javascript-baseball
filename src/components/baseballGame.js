24f01168186dc8fd7f7b831ba119faab0ca7d847
cocoa <jwv0323@kau.kr>
2022-11-08 03:17:05 +0900

    feat: 야구게임을 시작하는 메서드

--- src/components/baseballGame.js
+++ src/components/baseballGame.js
@@ -3,76 +3,83 @@ const { NUMBER, MESSAGE, GAME } = require('../constant/baseballGame');
 
 class BaseballGame{
   
-  startMessage() {
-    Console.print(MESSAGE.START);
-  }
+    start() {
+        const answer = this.createAnswer();
 
-  resultMessage(answer, inputNumber) {
-    const ball = this.getBall(answer, inputNumber);
-    const strike = this.getStrike(answer, inputNumber);
-
-    let message = ``;
-
-    if(ball){ 
-      message += `${ball}${GAME.BALL} `; 
+        this.startMessage();
+        this.inputNumber(answer);
     }
 
-    if(strike){ 
-      message += `${strike}${GAME.STRIKE}`; 
+    startMessage() {
+        Console.print(MESSAGE.START);
     }
 
-    if(!ball && !strike){ 
-      message += `${GAME.NOTHING}`; 
+    resultMessage(answer, inputNumber) {
+        const ball = this.getBall(answer, inputNumber);
+        const strike = this.getStrike(answer, inputNumber);
+
+        let message = ``;
+
+        if(ball){ 
+        message += `${ball}${GAME.BALL} `; 
+        }
+
+        if(strike){ 
+        message += `${strike}${GAME.STRIKE}`; 
+        }
+
+        if(!ball && !strike){ 
+        message += `${GAME.NOTHING}`; 
+        }
+
+        Console.print(message);
+        this.progress(answer, strike);
     }
 
-    Console.print(message);
-    this.progress(answer, strike);
-  }
+    getBall(answer, inputNumber){
+        let ball = 0;
+        
+        [...inputNumber].forEach((number, index) => {
+        if(number !== answer[index] && answer.includes(number)){
+            ball++;
+        }
+        });
 
-  getBall(answer, inputNumber){
-    let ball = 0;
-    
-    [...inputNumber].forEach((number, index) => {
-      if(number !== answer[index] && answer.includes(number)){
-        ball++;
-      }
-    });
-
-    return ball;
-  }
-
-  getStrike(answer, inputNumber){
-    let strike = 0;
-    
-    [...inputNumber].forEach((number, index) => {
-      if(number === answer[index]) {
-        strike++;
-      }
-    });
-
-    return strike;
-  }
-
-  createAnswer() {
-    const answer = [];
-
-    while (answer.length < NUMBER.DIGIT) {
-      const number = Random.pickNumberInRange(NUMBER.MINIMUM_RANGE, NUMBER.MAXIMUM_RANGE);
-
-      if (!answer.includes(number)) {
-        answer.push(number);
-      }
+        return ball;
     }
 
-    return answer.join('');
-  }
+    getStrike(answer, inputNumber){
+        let strike = 0;
+        
+        [...inputNumber].forEach((number, index) => {
+        if(number === answer[index]) {
+            strike++;
+        }
+        });
 
-  inputNumber(answer) {
-    Console.readLine(MESSAGE.INPUT, (inputNumber) => {
-      this.validateInputNumber(inputNumber);
-      this.resultMessage(answer, inputNumber);
-    });
-  }
+        return strike;
+    }
+
+    createAnswer() {
+        const answer = [];
+
+        while (answer.length < NUMBER.DIGIT) {
+        const number = Random.pickNumberInRange(NUMBER.MINIMUM_RANGE, NUMBER.MAXIMUM_RANGE);
+
+        if (!answer.includes(number)) {
+            answer.push(number);
+        }
+        }
+
+        return answer.join('');
+    }
+
+    inputNumber(answer) {
+        Console.readLine(MESSAGE.INPUT, (inputNumber) => {
+        this.validateInputNumber(inputNumber);
+        this.resultMessage(answer, inputNumber);
+        });
+    }
 
 }
 
