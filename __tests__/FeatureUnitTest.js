6351f106be33b0678cefd4081d28967807a07d51
letsjo <hyunoh.jo@gmail.com>
2022-11-07 23:04:42 +0900

    test: 테스트를 위한 테스트 파일 FeatureUnitTest.js 추가

---
+++ __tests__/FeatureUnitTest.js
@@ -0,0 +1,26 @@
+const App = require('../src/App');
+const MissionUtils = require('@woowacourse/mission-utils');
+
+const mockQuestions = answers => {
+  MissionUtils.Console.readLine = jest.fn();
+  answers.reduce((acc, input) => {
+    return acc.mockImplementationOnce((question, callback) => {
+      callback(input);
+    });
+  }, MissionUtils.Console.readLine);
+};
+
+const mockRandoms = numbers => {
+  MissionUtils.Random.pickNumberInRange = jest.fn();
+  numbers.reduce((acc, number) => {
+    return acc.mockReturnValueOnce(number);
+  }, MissionUtils.Random.pickNumberInRange);
+};
+
+const getPrintSpy = () => {
+  const printSpy = jest.spyOn(MissionUtils.Console, 'print');
+  printSpy.mockClear();
+  return printSpy;
+};
+
+describe('기능 단위 목록별 테스트', () => {});
