03e527eb04b7c98d2a8ee1f8ca8be0b75062277d
yesolyo <ahnyelim77@gmail.com>
2022-11-07 16:17:29 +0900

    feat: 정답을 맞출때까지 숫자를 입력

--- src/App.js
+++ src/App.js
@@ -78,6 +78,8 @@ class App {
     if (result == "3스트라이크") {
       MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
       this.restartEnd();
+    } else {
+      this.userInputNumber(COMPUTER_NUMBER);
     }
   }
 }
