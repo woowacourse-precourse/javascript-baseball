455e752a9a13b38dbc03b880b4dc59178755e440
gsong <rmsdlf1206@gmail.com>
2022-11-07 20:30:03 +0900

    feat: 입력값 관리 , 입력값 받기, 입력 값 유효성 검사, test코드 작성

--- src/App.js
+++ src/App.js
@@ -5,14 +5,17 @@ const MESSAGES = {
   input: "숫자를 입력해주세요 : ",
   correct: "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
   restart: "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
+  error: "잘못된 숫자를 입력하여 프로그램을 종료합니다.",
 };
 class App {
   constructor() {
     this.computerNum = "";
+    this.userNumber = "";
   }
   play() {
     this.print(MESSAGES.start);
     this.computerNum = this.getComputerNum();
+    this.userNumber = this.getUserNumber();
   }
   print(message) {
     MissionUtils.Console.print(message);
@@ -21,7 +24,23 @@ class App {
     let computerNumArr = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
     return computerNumArr.join("");
   }
+  getUserNumber() {
+    let userAnswer;
+    MissionUtils.Console.readLine(MESSAGES.input, (answer) => {
+      this.print(MESSAGES.input + answer);
+      if (!this.checkValidUserNumber(answer)) {
+        throw new Error(MESSAGES.error);
+      }
+      userAnswer = answer;
+    });
+    return userAnswer;
+  }
+  checkValidUserNumber(answer) {
+    const userNumberSet = new Set();
+    [...answer].forEach((string) => userNumberSet.add(string));
+    if (userNumberSet.size !== 3) return false;
+    if (userNumberSet.has("0")) return false;
+    return true;
+  }
 }
-const app = new App();
-app.play();
 module.exports = App;
--- src/App.test.js
+++ src/App.test.js
@@ -7,6 +7,15 @@ const getSpy = () => {
   return logSpy;
 };
 
+const mockQuestions = (answers) => {
+  MissionUtils.Console.readLine = jest.fn();
+  answers.reduce(
+    (acc, input) =>
+      acc.mockImplementationOnce((question, callback) => callback(input)),
+    MissionUtils.Console.readLine
+  );
+};
+
 describe("숫자 야구 게임", () => {
   test("print 메소드로 받은값을 출력", () => {
     const Spy = getSpy();
@@ -15,5 +24,48 @@ describe("숫자 야구 게임", () => {
     app.print(input);
 
     expect(Spy).toHaveBeenCalledWith(expect.stringContaining(input));
+  });
+  test("getUserNumber 메서드로 받아온 userNumber를 확인", () => {
+    const correctAnswer = ["123"];
+
+    mockQuestions(correctAnswer);
+
+    const app = new App();
+    app.play();
+    const correctResult = app.userNumber;
+
+    expect(correctResult).toEqual("123");
+    expect([...correctResult]).not.toContain("0");
+    expect(correctResult).toHaveLength(3);
+  });
+
+  test("getUserNumber 메서드로 받아온 userNumber 가 0을 포함할 경우 에러를 발생시킨다", () => {
+    const answer = ["012"];
+
+    mockQuestions(answer);
+
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow();
+  });
+
+  test("getUserNumber 메서드로 받아온 userNumber 의 숫자가 중복될 경우 에러를 발생시킨다", () => {
+    const answer = ["112"];
+
+    mockQuestions(answer);
+
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow();
+  });
+  test("진행상황 확인용 테스트", () => {
+    const answers = ["123"];
+
+    mockQuestions(answers);
+
+    const app = new App();
+    app.play();
   });
 });
