e49b906ab7d581bafc9c39098d064483d1edcd60
TeusLee <xodn1311@gmail.com>
2022-11-07 23:51:29 +0900

    Change console.log -> MissionUtils.Console.print Randomnumber Generate integer -> ArrayLike

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 const MissionUtils = require("@woowacourse/mission-utils");    
 
 const ball_cnt = (input_num, computer_num) => {
-  let input_num_str = String(input_num)
-  let input_computer_str = String(computer_num)
+  //let input_num_str = String(input_num)
+  //let input_computer_str = String(computer_num)
   result_cnt = 0
-  for (let i = 0; i < input_computer_str.length; i++){
-      if (input_num_str[i]!==input_computer_str[i] && input_computer_str.includes(input_num_str[i])){
+  for (let i = 0; i < computer_num.length; i++){
+      if (parseInt(input_num[i])!==computer_num[i] && computer_num.includes(parseInt(input_num[i]))){
           result_cnt += 1
       }
   }
@@ -13,11 +13,11 @@ const ball_cnt = (input_num, computer_num) => {
 }
 
 const strike_cnt = (input_num, computer_num) => {
-  let input_num_str = String(input_num)
-  let input_computer_str = String(computer_num)
+  //let input_num_str = String(input_num)
+  //let input_computer_str = String(computer_num)
   result_cnt = 0
-  for (let i = 0; i < input_computer_str.length; i++){
-      if (input_num_str[i]===input_computer_str[i]){
+  for (let i = 0; i < input_num.length; i++){
+      if (parseInt(input_num[i])===computer_num[i]){
           result_cnt += 1
       }
   }
@@ -39,25 +39,30 @@ const gm_result = (input_num, computer_num) => {
 }
 
 const gen_rand_num = () => {
-  result = 0
-  for (let i = 0; i < 3; i++){
-    temp = MissionUtils.Random.pickNumberInRange(1, 9)*(10**i)
-    result = result + temp
-  }  
-  return result
+  computer = []
+  while (computer.length < 3) {
+    const number = MissionUtils.Random.pickNumberInRange(1, 9);
+    if (!computer.includes(number)) {
+      computer.push(number);
+    }
+  }
+  return computer
 }
 
 const play_inst = (is_end, solution) => {  
   MissionUtils.Console.readLine("숫자를 입력해 주세요", (answer)=>{    
-    //console.log(answer, solution)
-    if (answer == solution) {
+    console.log(answer, solution)
+    let temp_result = gm_result(answer, solution)
+    if (temp_result === "3스트라이크") {
       //case when answer is exactly same with solution
+      MissionUtils.Console.print(temp_result)
+      MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
       is_end[0] = true
-    } else if (answer.length <= 3) {
+    } else if (answer.length == 3) {
       //case answer is valid but not solution
-      console.log(gm_result(answer, solution))
+      MissionUtils.Console.print(temp_result)
       is_end[0] = false
-    } else if (answer.length > 3) {
+    } else {
       //case answer is invalid, throw error and shut down program
       throw new Error("Invalid Input to inference");
     }
@@ -87,7 +92,8 @@ const play_again_test = (is_end, callback)=> {
       callback(is_end, gen_rand_num())
     } else if (answer == 2) {
       //case when game is end and just print game end to console
-      console.log("game end")
+      MissionUtils.Console.print("game end")
+      MissionUtils.Console.close()
     }
   })
 }
@@ -95,7 +101,7 @@ const play_again_test = (is_end, callback)=> {
 class App {
   play() {
     var is_end = [false]
-    console.log("숫자 야구 게임을 시작합니다.")    
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.")    
     try {
       play_inst(is_end, gen_rand_num())              
     } catch (e) {
