e04a42596305ef5c8590119163e31c4246f81454
김나현 <nahyeon.dev@gmail.com>
2022-11-07 18:35:16 +0900

    feat: add compareBothNumber(user, computer) and numberToArr(), fix constructor()

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
-  constructor(computerNum, userNum){
+  constructor(computerNum, userNum, strike, ball, none, check){
     this.computerNum = computerNum;
     this.userNum = userNum;
+    this.strike = strike;
+    this.ball = ball;
+    this.none = none;
+    this.check = check;
   }
   play() { 
     this.random();
@@ -27,9 +31,43 @@ function setRandomNumberComputer(){
   }
   return computer;
 }
-
+function numberToArr(number){
+  var numArray = [];
+  do{
+    numArray.push(number%10);
+    number = Math.floor(number/10);
+  }while(number > 0);
+  
+  return numArray;
+}
 function compareBothNumber(randomNumber, userNumber){
+  const setApp = new App();
+  let random = numberToArr(randomNumber);
+  let user = numberToArr(userNumber);
 
+  let strike = 0;
+  let ball = 0;
+  let notting = false;
+  let check = false;
+
+  if(random[0] == user[0]) strike++;
+  else{
+    if(random[0] == user[1]) ball++;
+    else if(random[0] == user[2]) ball++;
+  }
+  if(random[1] == user[1]) strike++;
+  else{
+    if(random[1] == user[2]) ball++;
+  }
+  if(random[2] == user[2]) strike++;
+
+  if(strike == 0 && ball == 0) notting = true;
+  if(strike != 3 && strike > 0 && ball > 0) check = true; 
+
+  setApp.strike = strike;
+  setApp.ball = ball;
+  setApp.none = notting;
+  setApp.check = check;
 }
 
 function inputMyNumber(){
