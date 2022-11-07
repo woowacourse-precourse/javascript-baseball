f22f634fd551664466ed21224eb92b993390bd6b
Inae Choi <inaechoi89@gmail.com>
2022-11-07 22:05:49 +0900

    feat: add a function to check duplicate number

--- src/components/HandlingException.js
+++ src/components/HandlingException.js
@@ -1,3 +1,7 @@
 const handleException = (inputArray, digit) => {};
 
+const checkDuplicate = (inputArray, digit) => {
+  return [...new Set(inputArray)].length !== digit;
+};
+
 module.exports = handleException;
