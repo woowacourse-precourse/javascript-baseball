432008d2c47fdc6c725f1c6f74108dd3157f789d
jeno8522 <68889383+jeno8522@users.noreply.github.com>
2022-11-07 20:05:17 +0900

    pickRandomAnswer 메소드
    컴퓨터가 1~19 사이의 숫자 중 3개의 숫자를 뽑아 정답을 정하게 하는 메소드

--- src/App.js
+++ src/App.js
@@ -14,6 +14,13 @@ class App {
   startMent() {
     Console.print("숫자 야구 게임을 시작합니다.");
   }
+  pickRandomAnswer() {
+    while (this.#answer.length < 3) {
+      const number = Random.pickNumberInRange(1, 9);
+      if (!this.#answer.includes(number)) this.#answer += `${number}`;
+    }
+    this.#answer = [...this.#answer];
+  }
 }
 
 const app = new App();
