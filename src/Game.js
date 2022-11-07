bce8d2035885071cbf0f609a603584598da6a728
Suhyeon Lee <suhyeonlee.dev@gmail.com>
2022-11-07 22:31:59 +0900

    feat: 정답이 아닌 경우 사용자에게 숫자를 재입력 받는 기능 구현

--- src/Game.js
+++ src/Game.js
@@ -8,11 +8,14 @@ class Game {
 
   async start() {
     const answer = this.computer.makeAnswer();
-    const userNumber = await this.computer.getUserNumber();
-    const result = this.computer.getResult(answer, userNumber);
-    this.computer.printResult(result);
-    if (result.strikeCnt === 3) {
-      Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+    while (true) {
+      const userNumber = await this.computer.getUserNumber();
+      const result = this.computer.getResult(answer, userNumber);
+      this.computer.printResult(result);
+      if (result.strikeCnt === 3) {
+        Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+        break;
+      }
     }
   }
 }
