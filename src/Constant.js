a7390fcd43478e701ca6d5784970bb0176dc13ba
wzrabbit <curious.wzrabbit@gmail.com>
2022-11-07 17:44:53 +0900

    refactor(constant): 모든 객체에 Object.freeze 적용
    - 모든 객체는 값이 변경되어서는 안 되므로, 변경을 막기 위해 Object.freeze 적용

--- src/Constant.js
+++ src/Constant.js
@@ -1,4 +1,4 @@
-const MESSAGE = {
+const MESSAGE = Object.freeze({
   GAME_START: '숫자 야구 게임을 시작합니다.',
   ASK_GUESS: '숫자를 입력해 주세요 : ',
   GAME_CLEAR: '3개의 숫자를 모두 맞히셨습니다! 게임 종료',
@@ -6,25 +6,25 @@ const MESSAGE = {
   FORMAT_ERROR_GUESS: '입력값은 반드시 1~9로만 구성된, 자릿수의 숫자가 모두 다른 세 자리의 양의 정수여야 합니다.',
   FORMAT_ERROR_CHOICE: '입력값은 반드시 1 또는 2여야 합니다.',
   THREE_STRIKE: '3스트라이크',
-};
+});
 
-const CONSTANT = {
+const CONSTANT = Object.freeze({
   DIGIT_LIMIT: 3,
-};
+});
 
-const REGEX = {
+const REGEX = Object.freeze({
   GUESS: /^[1-9]{3}$/,
   CHOICE: /^[12]$/,
-};
+});
 
-const CHOICE = {
+const CHOICE = Object.freeze({
   PLAY_AGAIN: '1',
   EXIT: '2',
-};
+});
 
-module.exports = {
+module.exports = Object.freeze({
   MESSAGE,
   CONSTANT,
   REGEX,
   CHOICE,
-};
+});
