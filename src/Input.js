32730891a6f65f08039443e2016467f707513308
동동이 <drdragon777@naver.com>
2022-11-08 00:39:55 +0900

    feat - isValidInput에서 사용자 input을 숫자 배열로 만들기

--- src/Input.js
+++ src/Input.js
@@ -4,6 +4,7 @@ class Input {
   isValidInput(input) {
     if (this.isLengthThree(input))
       throw new Error("3자리 숫자를 입력해주세요.");
+    const numArr = input.split("").map(Number);
   }
 
   isLengthThree(input) {
