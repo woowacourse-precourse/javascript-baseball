89327794c377b52daedec0c81072ec0843a859b8
Sumin Kim <ksumin0730@hufs.ac.kr>
2022-11-07 12:28:47 +0900

    Style: 들여쓰기 수정

--- src/utils/AnswerPhase.js
+++ src/utils/AnswerPhase.js
@@ -1,18 +1,18 @@
 const constant = require('../Constants');
 
 function answerPhase(resultList){
-    const [strike, ball] = resultList;
-    
-    if (strike === 0 && ball === 0){
-      return constant.RESULTS.NOTHING;
-    }
-    else if (strike === 0 && ball !== 0){
-      return String(ball)+constant.RESULTS.BALL;
-    }
-    else if (strike !== 0 && ball === 0){
-      return String(strike)+constant.RESULTS.STRIKE;
-    }
-    return String(ball)+constant.RESULTS.BALL+' '+String(strike)+constant.RESULTS.STRIKE;
+  const [strike, ball] = resultList;
+  
+  if (strike === 0 && ball === 0){
+    return constant.RESULTS.NOTHING;
+  }
+  else if (strike === 0 && ball !== 0){
+    return String(ball)+constant.RESULTS.BALL;
+  }
+  else if (strike !== 0 && ball === 0){
+    return String(strike)+constant.RESULTS.STRIKE;
+  }
+  return String(ball)+constant.RESULTS.BALL+' '+String(strike)+constant.RESULTS.STRIKE;
 }
 
 module.exports = answerPhase;
