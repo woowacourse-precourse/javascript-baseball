f1128ba207171c6f9a119774058cd95c398fb8b9
HeoJiye <heoze1031@gmail.com>
2022-11-07 15:53:31 +0900

    refactor(App): 예외처리 throw문 추가
    throw문을 이용한 예외처리로 수정, 에러 발생시 어플리케이션 종료
    기존: 부울 함수 vaildate를 이용해 잘못된 값일 경우 함수를 종료함

--- src/App.js
+++ src/App.js
@@ -9,8 +9,11 @@ class App {
     MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (answer) => {
       const inputNumber = answer.split('').map(Number);
 
-      if(!this.vaildation(inputNumber)) { // 입력값이 유효하지 않으면 종료한다.
+      try {
+        this.vaildation(inputNumber)
+      } catch (e) {
         MissionUtils.Console.close();
+        console.error(e);
         return; 
       }
 
@@ -19,21 +22,17 @@ class App {
   }
 
   vaildation(inputNumber) {
-    if(inputNumber.length != 3) return false;
+    if(inputNumber.length != 3) throw 'Invaild Value: answer of length is not 3!';
 
     for(const item of inputNumber) {
-      if(isNaN(item)) return false;
+      if(isNaN(item)) throw 'Invaild Value: answer includes Not-A-Number value';
     }
 
-    if(inputNumber.includes(0)) return false;
+    if(inputNumber.includes(0)) throw 'Invaild Value: answer includes 0';
 
     const filtered = inputNumber.filter((item, index) => inputNumber.indexOf(item) === index);
-    if(filtered.length != 3) return false;
-
-    return true;
+    if(filtered.length != 3) throw 'Invaild Value: there are duplicate values in the answer';
   }
 }
 
 module.exports = App;
-
-new App().play();
