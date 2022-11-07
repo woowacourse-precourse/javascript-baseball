117a8465cf6c56f4dc002a479096d682629abb43
osdoonhyun <skw1996@naver.com>
2022-11-07 16:50:28 +0900

    Feat: App에서 사용자 숫자 입력 기능 구현

--- src/App.js
+++ src/App.js
@@ -9,6 +9,17 @@ class App {
 
   start() {
     const computerNumber = this.computer.makeRandomNumber();
+
+    //사용자 숫자 입력
+    MissionUtils.Console.readLine(Game.MESSAGE.INPUT, (userInput) => {
+      //입력이 형식에 맞는지 유효성 검사
+    });
+
+    //사용자가 입력한 숫자에 대한 결과 출력
+
+    //컴퓨터가 선택한 숫자 3개 모두 맞출 시 게임 종료 문구 출력 후 게임 종료
+
+    //사용자 재시작/ 종료 선택
   }
 
   restart() {}
