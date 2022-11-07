7f79fc46ecf95bebf81d8d048613e53105f9b2db
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2022-11-07 15:24:53 +0900

    refactor(Render): 상수를 object로 묶어 상수 범위를 제한하여 가독성을 높임

--- src/Render.js
+++ src/Render.js
@@ -1,23 +1,16 @@
 const MissionUtils = require("@woowacourse/mission-utils");
-const {
-  START_MENTION,
-  START_GETNUMBER,
-  GAME_NOTHING,
-  GAME_END_MENTION,
-  END_RETRY_MENTION,
-  END_MENTION,
-} = require("./constants");
+const GAME = require("./constants");
 
 class Render {
   constructor() {}
 
   startment() {
-    MissionUtils.Console.print(START_MENTION);
+    MissionUtils.Console.print(GAME.START_MENTION);
   }
 
   getUser() {
     return new Promise((resolve, reject) => {
-      MissionUtils.Console.readLine(START_GETNUMBER, (number) => {
+      MissionUtils.Console.readLine(GAME.START_GETNUMBER, (number) => {
         resolve(number);
       });
     });
@@ -28,24 +21,24 @@ class Render {
       MissionUtils.Console.print(`${ballCount}볼 ${strikeCount}스트라이크`);
     }
     if (strikeCount === 3) {
-      MissionUtils.Console.print(GAME_END_MENTION);
+      MissionUtils.Console.print(GAME.GAME_END_MENTION);
     }
 
     if (ballCount === 0 && strikeCount === 0) {
-      MissionUtils.Console.print(GAME_NOTHING);
+      MissionUtils.Console.print(GAME.GAME_NOTHING);
     }
   }
 
   replayQnA() {
     return new Promise((resolve, reject) => {
-      MissionUtils.Console.readLine(END_RETRY_MENTION, (number) => {
+      MissionUtils.Console.readLine(GAME.END_RETRY_MENTION, (number) => {
         resolve(number);
       });
     });
   }
 
   end() {
-    MissionUtils.Console.print(END_MENTION);
+    MissionUtils.Console.print(GAME.END_MENTION);
   }
 }
 module.exports = Render;
--- src/constants.js
+++ src/constants.js
@@ -1,9 +1,13 @@
-exports.START_MENTION = `숫자 야구 게임을 시작합니다.`;
-exports.START_GETNUMBER = `숫자를 입력해주세요 :`;
+GAME = {
+  START_MENTION: `숫자 야구 게임을 시작합니다.`,
+  START_GETNUMBER: `숫자를 입력해주세요 :`,
 
-exports.GAME_NOTHING = `낫싱`;
+  GAME_NOTHING: `낫싱`,
 
-exports.GAME_END_MENTION = `3스트라이크 3개의 숫자를 모두 맞히셨습니다! 게임 종료`;
-exports.END_RETRY_MENTION = `게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.`;
+  GAME_END_MENTION: `3스트라이크 3개의 숫자를 모두 맞히셨습니다! 게임 종료`,
+  END_RETRY_MENTION: `게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.`,
 
-exports.END_MENTION = `수고하셨습니다.`;
+  END_MENTION: `수고하셨습니다.`,
+};
+
+module.exports = GAME;
