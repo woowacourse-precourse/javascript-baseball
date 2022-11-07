51b99cc550234db672d0b2400528b18b6dd9114a
ywoo12321 <ywoo12321@gmail.com>
2022-11-07 15:32:11 +0900

    플레이어 숫자, 컴퓨터 숫자 받아오기 완료, 숫자 비교하기 함수 구현 예정

--- src/NewGame.js
+++ src/NewGame.js
@@ -1,11 +1,31 @@
 const { Console, Random } = require("@woowacourse/mission-utils");
 class NewGame {
+  computerAnswer;
+
   constructor() {
     this.gameStart();
   }
   gameStart() {
-    //무작위 숫자 받아오기
-    //사용자 숫자 받아오기
+    this.getComputerAnswer();
+    this.getUserAnswer();
+  }
+  getComputerAnswer() {
+    const computerNumberList = [];
+    while (computerNumberList.length < 3) {
+      const randomNumber = Random.pickNumberInRange(1, 9);
+      if (!computerNumberList.includes(randomNumber)) {
+        computerNumberList.push(randomNumber);
+      }
+    }
+    this.computerAnswer = Number(computerNumberList.join(""));
+  }
+
+  getUserAnswer() {
+    Console.readLine("숫자를 입력해주세요 : ", (userAnswer) => {
+      console.log(userAnswer, "플레이어 입력값");
+      console.log(this.computerAnswer, "컴퓨터 입력값");
+      //숫자 비교하기 함수
+    });
   }
 }
 
