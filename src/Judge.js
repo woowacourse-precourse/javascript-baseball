193bab16487149d0a59055710bae00d4a5bba860
choieastsea <choieastsea@gmail.com>
2022-11-07 18:19:47 +0900

    feat: 볼, 스트라이크 출력 기능
    볼, 스트라이크 수에 따라 적절하게 출력하도록 함

--- src/Judge.js
+++ src/Judge.js
@@ -5,21 +5,38 @@ class Judge {
   checkIsValidInput(player_input) {
     if (!player_input) throw "Player Input Is Invalid!";
   }
-  performOneGame(opponentInput){
+  performOneGame(opponentInput) {
     Console.readLine("숫자를 입력해주세요 : ", (input) => {
       Console.print(opponentInput);
       let [ball, strike] = this.countBallStrike(opponentInput, input);
-      Console.print(`${ball}볼 ${strike}스트라이크`);
+      this.printBS(ball, strike);
+      if (strike === 3) {
+        //종료할지 다시할지 물어보기
+      } else {
+        this.performOneGame(opponentInput);
+      }
     });
   }
-
+  printBS(ball, strike) {
+    if (ball + strike === 0) {
+      Console.print("낫싱");
+    } else if (ball === 0) {
+      Console.print(`${strike}스트라이크`);
+    } else if (strike === 0) {
+      Console.print(`${ball}볼`);
+    } else {
+      Console.print(`${ball}볼 ${strike}스트라이크`);
+    }
+  }
   countBallStrike(opponent_input, player_input) {
     let ball = 0;
     let strike = 0;
     this.checkIsValidInput(player_input);
     for (let idx in player_input) {
       strike += this.countIfStrike(player_input[idx], opponent_input[idx]);
-      ball += this.countIfBall(player_input[idx], opponent_input);
+      ball +=
+        this.countIfBall(player_input[idx], opponent_input) &
+        !this.countIfStrike(player_input[idx], opponent_input[idx]);
     }
     return [ball, strike];
   }
@@ -30,7 +47,9 @@ class Judge {
   }
 
   countIfBall(player_char, opponent_input) {
-    if (opponent_input.indexOf(player_char) > 0) return 1;
+    if (opponent_input.indexOf(player_char) >= 0) {
+      return 1;
+    }
     return 0;
   }
 }
