5d4c4b5f973a32c28453cfeea17e9307a49f3a31
minseok1109 <park485201@naver.com>
2022-11-07 13:56:50 +0900

    refactor: 테스트 코드 수정 give-when-then 패턴 적용 #기능목록 - 1

--- __tests__/Mytest.js
+++ __tests__/Mytest.js
@@ -1,25 +1,51 @@
-const utils = require("../src/utils/utils");
-
-describe("기능목록 테스트", () => {
-  test("숫자를 입력했는지 확인", () => {
-    expect(() => utils.checkNumberisOk("1234")).toThrow(
+const Utils = require("../src/utils/utils.js");
+describe("Utils.checkNumberisOk", () => {
+  test("문자가 섞여있으면 예외가 발생해야 한다", () => {
+    //given
+    const mixNumStr = "1e3";
+    //when
+    //then
+    expect(() => Utils.checkNumberisOk(mixNumStr)).toThrow(
       new Error("숫자를 입력하세요!")
     );
   });
 
-  test("3자리 숫자를 입력했는지 확인", () => {
-    expect(() => utils.checkNumberisOk(1234)).toThrow(
-      new Error("숫자를 3자리 입력하세요!")
+  test("문자형이면서 3자리 숫자가 아니면 예외가 발생해야 한다.", () => {
+    //given
+    const over3Length = "1234";
+    //when
+    //then
+    expect(() => Utils.checkNumberisOk(over3Length)).toThrow(
+      new Error("입력의 길이와 숫자인지 확인하세요!")
     );
   });
 
-  test("중복된 숫자를 입력했는지 확인", () => {
-    expect(() => utils.checkNumberisOk(113)).toThrow(
+  test("입력이 문자이면 예외가 발생해야 한다.", () => {
+    //given
+    const stringNumber = "456";
+    //when
+    //then
+    expect(() => Utils.checkNumberisOk(stringNumber)).toThrow(
+      new Error("숫자를 입력하세요!")
+    );
+  });
+
+  test("중복된 숫자가 있으면 예외가 발생해야 한다.", () => {
+    //given
+    const duplicatedNumber = 113;
+    //when
+    //then
+    expect(() => Utils.checkNumberisOk(duplicatedNumber)).toThrow(
       new Error("중복된 숫자를 입력하였습니다!")
     );
   });
-  test("0을 입력했는지 확인", () => {
-    expect(() => utils.checkNumberisOk(105)).toThrow(
+
+  test("0이 있으면 예외가 발생해야 한다.", () => {
+    //given
+    const containZero = 105;
+    //when
+    //then
+    expect(() => Utils.checkNumberisOk(containZero)).toThrow(
       new Error("0을 입력하였습니다!")
     );
   });
