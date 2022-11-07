254f82b62c7e1a6e0f27346b95ac14b5e969e73e
Hyungeol94 <55428818+Hyungeol94@users.noreply.github.com>
2022-11-07 20:15:16 +0900

    게임 시작 문구 출력, 세 가지 숫자를 랜덤으로 산출하기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 class App {
-  play() {}
+  play() {
+    const MissionUtils = require("@woowacourse/mission-utils");
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    var firstNumber = MissionUtils.Random.pickNumberInRange();
+    var secondNumber = MissionUtils.Random.pickNumberInRange();
+    var thirdNumber = MissionUtils.Random.pickNumberInRange();
+
+  }
 }
 
 module.exports = App;
