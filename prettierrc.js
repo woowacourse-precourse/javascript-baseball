e42591492cf5a393063bf862e53fe12652664f9e
SSY1203 <tlstjsdud566@naver.com>
2022-11-07 13:53:04 +0900

    feat : eslint 및 prettier 규격을 지키기 위한 파일

---
+++ prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  singleQuoto: true,
+  semi: true,
+  useTabs: false,
+  tabWidth: 2,
+  trailingComma: "all",
+  printWidth: 100,
+  bracketSpacing: true,
+  arrowParens: "avoid",
+};
