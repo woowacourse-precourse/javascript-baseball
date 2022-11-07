b6a6b54b0a7fc329826f7b91665a111efc62fdee
Narcoker <jykim9868@gmail.com>
2022-11-07 18:00:50 +0900

    Refactor: Add print message object

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
+const MESSAGE = Object.freeze({
+  START: "숫자 야구 게임을 시작합니다.",
+  INPUT_NUMBER: "숫자를 입력해주세요 : ",
+  THREE_STRIKE: "3스트라이크",
+  STRIKE: "스트라이크",
+  BALL: "볼",
+  NOTHING: "낫싱",
+  CONGRATULATION: "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
+  RESTART: "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
+});
+
+const ERROR = Object.freeze({
+  NOT_THREE_NUMBER: "입력값은 0이 아닌 숫자로 구성된 3자리 수이어야 합니다.",
+  NOT_OVERLAP_NUMBER: "연속된 숫자는 포함될 수 없습니다.",
+  NOT_DIFFERENCE_NUMBER: "각 자리수는 모두 달라야합니다.",
+  INVAID_RESTART_TYPE: "1 또는 2만 입력할 수 있습니다."
+});
+
+
 class App {
   printStart() {
-    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    MissionUtils.Console.print(MESSAGE.START);
   }
+
   makeAnswer() {
     const answer = [];
     while (answer.length < 3) {
@@ -18,22 +38,22 @@ class App {
   isCorrectInput(value) {
     const regExp = /^[1-9]{3}$/;
     if (!regExp.test(value))
-      throw new Error("입력값은 0이 아닌 숫자로 구성된 3자리 수이어야 합니다.");
+      throw new Error(ERROR.NOT_THREE_NUMBER);
 
     const overlapReg = /(.)\1+/;
     if (overlapReg.test(value))
-      throw new Error("연속된 숫자는 포함될 수 없습니다.");
+      throw new Error(NOT_OVERLAP_NUMBER);
 
     const differenceReg = /(.)\d\1/;
     if (differenceReg.test(value)) {
-      throw new Error("세자리 수는 모두 다른 수여야합니다.");
+      throw new Error(NOT_DIFFERENCE_NUMBER);
     }
   }
 
   isCorrectRestartInput(value) {
     const regExp = /^[1-2]{1}$/;
     if (!regExp.test(value))
-      throw new Error("잘못된 재시작 값");
+      throw new Error(INVAID_RESTART_TYPE);
   }
 
   stringToAnswerType(value) {
@@ -63,14 +83,16 @@ class App {
 
   printResult(result) {
     if (result.strike === 3) {
-      MissionUtils.Console.print("3스트라이크");
+      MissionUtils.Console.print(MESSAGE.THREE_STRIKE);
       return;
     }
+
     if (result.nothing === 3) {
-      MissionUtils.Console.print("낫싱");
+      MissionUtils.Console.print(MESSAGE.NOTHING);
       return;
     }
-    MissionUtils.Console.print(`${result.ball}볼 ${result.strike}스트라이크`);
+
+    MissionUtils.Console.print(`${result.ball}${MESSAGE.BALL} ${result.strike}${MESSAGE.STRIKE}`);
   }
 
   isAnswer(result) {
@@ -78,26 +100,28 @@ class App {
   }
 
   askRestart() {
-    MissionUtils.Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
-    MissionUtils.Console.readLine("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.", (input) => {
+    MissionUtils.Console.print(MESSAGE.CONGRATULATION);
+
+    MissionUtils.Console.readLine(MESSAGE.RESTART, (input) => {
       this.isCorrectRestartInput(input);
-      if (input === "1") { // 재시작
+
+      if (input === "1")
         this.newGame();
-      }
-      if (input === "2") { // 종료
+
+      if (input === "2")
         MissionUtils.Console.close();
-      }
     })
   }
 
   guessAnswer(answer) {
-    MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (input) => {
+    MissionUtils.Console.readLine(MESSAGE.INPUT_NUMBER, (input) => {
       this.isCorrectInput(input);
+
       input = this.stringToAnswerType(input);
       let result = this.compare(answer, input);
       this.printResult(result);
-      this.isAnswer(result) ? this.askRestart() : this.guessAnswer(answer);
 
+      this.isAnswer(result) ? this.askRestart() : this.guessAnswer(answer);
     });
   }
 
