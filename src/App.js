33cf67cc6e1410a71632f38fab924aa6f9292c63
HWI <dlwognl99@naver.com>
2022-11-07 12:03:57 +0900

    feat: 게임 결과 출력 기능 구현

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ class App {
     const computerNumber = RandomChoice();
     // 사용자 값 입력
     const userNumber = userInput();
+    // 게임 결과
+    gameResult(computerNumber, userNumber);
   }
 }
 
@@ -35,7 +37,7 @@ function userInput() {
   });
   return userNumber;
 }
-
+// 유저 입력 예외처리
 function checkValidNumber(answer) {
   const checkDeplicated = [...new Set(answer)];
   if (!answer.match(/[1-9]/)) {
@@ -47,6 +49,37 @@ function checkValidNumber(answer) {
   if (checkDeplicated.length !== 3) {
     throw new Error("중복되지 않는 값을 입력해주세요.");
   }
+}
+
+function gameResult(computerNumber, userNumber) {
+  const strike = countStrike(computerNumber, userNumber);
+  const ball = countBall(computerNumber, userNumber);
+  if (strike === 3) {
+    //
+  }
+  if ((strike === 0) & (ball === 0)) {
+    MissionUtils.Console.print("낫싱");
+  }
+  if ((strike !== 0) & (ball === 0)) {
+    MissionUtils.Console.print(`${strike}스트라이크`);
+  }
+  if ((strike === 0) & (ball !== 0)) {
+    MissionUtils.Console.print(`${ball}볼`);
+  }
+  if ((strike !== 0) & (ball !== 0)) {
+    MissionUtils.Console.print(`${strike}스트라이크 ${ball}볼`);
+  }
+}
+
+function countStrike(computerNumber, userNumber) {
+  return [...computerNumber].filter(
+    (element, index) => userNumber[index] === element
+  ).length;
+}
+
+function countBall(computerNumber, userNumber) {
+  return [...computerNumber].filter((element) => userNumber.includes(element))
+    .length;
 }
 
 module.exports = App;
