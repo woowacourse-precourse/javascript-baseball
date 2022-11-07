fd8551ceda42e9e4c0d7e687a60a40d200714882
박승훈 <tmdgns1126@naver.com>
2022-11-07 23:49:43 +0900

    feat: [User] 사용자의 입력을 받는 User 클래스와 함수 구현

--- src/App.js
+++ src/App.js
@@ -7,9 +7,18 @@ class Computer {
   }
 }
 
+class User {
+  guessNum() {
+    Console.readLine("숫자를 입력해주세요 : ", (input) => {
+      return input;
+    });
+  }
+}
+
 class App {
   constructor() {
     this.computer = new Computer();
+    this.user = new User();
   }
 
   play() {
