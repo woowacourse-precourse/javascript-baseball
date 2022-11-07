48fb7d30b78e672726dd496d26c96db91c0485ca
daal2quf <daal2quf@gmail.com>
2022-11-08 03:07:43 +0900

    test: 1, 2, 3스트라이크일때의 경우의 수 테스트

--- __tests__/CounterTest.js
+++ __tests__/CounterTest.js
@@ -27,4 +27,49 @@ describe("볼과 스트라이크 개수를 구하는 class Counter 테스트", (
     const strikeCount = count.strike(userInput, computerNums);
     expect([strikeCount, ballCount]).toEqual([0, 3]);
   });
+
+  test("1스트라이크 0볼", () => {
+    const count = new Counter();
+    const computerNums = '123';
+    const userInput = '156';
+    const ballCount = count.ball(userInput, computerNums);
+    const strikeCount = count.strike(userInput, computerNums);
+    expect([strikeCount, ballCount]).toEqual([1, 0]);
+  });
+
+  test("1스트라이크 1볼", () => {
+    const count = new Counter();
+    const computerNums = '123';
+    const userInput = '136';
+    const ballCount = count.ball(userInput, computerNums);
+    const strikeCount = count.strike(userInput, computerNums);
+    expect([strikeCount, ballCount]).toEqual([1, 1]);
+  });
+
+  test("1스트라이크 2볼", () => {
+    const count = new Counter();
+    const computerNums = '123';
+    const userInput = '132';
+    const ballCount = count.ball(userInput, computerNums);
+    const strikeCount = count.strike(userInput, computerNums);
+    expect([strikeCount, ballCount]).toEqual([1, 2]);
+  });
+
+  test("2스트라이크 0볼", () => {
+    const count = new Counter();
+    const computerNums = '123';
+    const userInput = '124';
+    const ballCount = count.ball(userInput, computerNums);
+    const strikeCount = count.strike(userInput, computerNums);
+    expect([strikeCount, ballCount]).toEqual([2, 0]);
+  });
+
+  test("3스트라이크 0볼", () => {
+    const count = new Counter();
+    const computerNums = '123';
+    const userInput = '123';
+    const ballCount = count.ball(userInput, computerNums);
+    const strikeCount = count.strike(userInput, computerNums);
+    expect([strikeCount, ballCount]).toEqual([3, 0]);
+  });
 });
