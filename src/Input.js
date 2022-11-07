e3ce8dba03630648ec694334fd4e7578816ef0a7
random-olive <104327907+random-olive@users.noreply.github.com>
2022-11-07 19:21:06 +0900

    Feat: input class init

---
+++ src/Input.js
@@ -0,0 +1,15 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const {
+  GAME_CONFIG_NUMBER,
+  ERROR_CHECK,
+  INGAME_MESSAGE,
+  GAME_RESULT,
+  RESPONSE,
+} = require("./Constant");
+
+class Input {
+  computer() {}
+  user() {}
+}
+
+module.exports = Input;
