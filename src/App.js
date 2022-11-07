0776588e35874ee65bd45b04ea476141d40b7b01
dhkham <khamdong@gmail.com>
2022-11-08 00:55:11 +0900

    feat(App.js): add getCompNumArray function
    pick random number from 1-9 using pickNumberInRange function and push it to an array without any duplicates

--- src/App.js
+++ src/App.js
@@ -4,6 +4,17 @@ class App {
   play() {
     const compNumArr = [];
     const userNumArr = [];
+
+    getCompNumArray(compNumArr);
   }
 }
 module.exports = App;
+
+function getCompNumArray(compNumArr) {
+  while (compNumArr.length < 3) {
+    const number = MissionUtils.Random.pickNumberInRange(1, 9);
+    if (!compNumArr.includes(number)) {
+      compNumArr.push(number);
+    }
+  }
+}
