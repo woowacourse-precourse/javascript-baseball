7238f9c9f00c86612305bbb2894819e8d8a02541
huyntohoon <81351526+hyuntohoon@users.noreply.github.com>
2022-11-08 08:13:00 +0900

    feat : 시작 후 메세지 출력 및 print 함수 추가

--- src/game.js
+++ src/game.js
@@ -1,3 +1,21 @@
-class Game {}
+const {
+  MESSAGE,
+  RANDOM_NUMBER,
+  END_INPUT,
+  SCORE,
+  ERROR,
+} = require("./constants");
+const { Console, Random } = require("@woowacourse/mission-utils");
+class Game {
+  play() {
+    this.init();
+  }
+  init() {
+    this.print(MESSAGE.START);
+  }
+  print(message) {
+    return Console.log(print(message));
+  }
+}
 
 module.exports = Game;
