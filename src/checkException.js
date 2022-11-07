516e07314710dae623be2b1b6bbefe6b9b23b659
Yesong <to06109@naver.com>
2022-11-07 19:54:17 +0900

    feat(checkIsDigitalInRange): 모두 1-9 사이의 정수로 이루어졌는지 확인

--- src/checkException.js
+++ src/checkException.js
@@ -4,8 +4,13 @@ function checkInputLength(input) {
   return input.length !== 3;
 }
 
+function checkIsDigitInRange(input) {
+  const regexp = new RegExp("^[1-9]+$");
+  return !regexp.test(input);
+}
+
 function checkException(input) {
-  if (checkInputLength(input)) {
+  if (checkInputLength(input) | checkIsDigitInRange(input)) {
     throw new Error(`${Message.error}`);
   }
 }
