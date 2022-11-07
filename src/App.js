78e708e4f9fc99346f2a26f9ca5d2c10b0aab4df
nailen1 <nietsnie1@gmail.com>
2022-11-07 21:56:16 +0900

    feat: 입력값 검증 후 에러 발생 기능

--- src/App.js
+++ src/App.js
@@ -15,4 +15,21 @@ function generateAnswer() {
   return numsArr.join('')
 }
 
+function throwError(input) {
+  let inputArr = Array.from(String(input));
+  if (inputArr.length !== 3) {
+    throw new Error("3자리 숫자만 입력 가능합니다.")
+  }
+  if (input.includes(0)) {
+    throw new Error("0은 포함될 수 없습니다.")
+  }
+  let inputSet = new Set(inputArr);
+  if (inputSet.size < 3) {
+    throw new Error("중복되지 않은 숫자 3개만 입력 가능합니다.")
+  }
+  if (input.replace(/[0-9]/g, '').length !== 0) {
+    throw new Error("숫자만 입력 가능합니다.")
+  }
+}
+
 module.exports = App;
