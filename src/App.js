ad784324059aa418f6be7c738b6a1526c21ebc62
장경은 <jang_kyung_eun@gyeong-eun-ui-MacBookAir.local>
2022-11-08 02:24:23 +0900

    feat: 랜덤숫자 생성 기능 구현

--- src/App.js
+++ src/App.js
@@ -2,6 +2,21 @@ function starGame() {
   console.log("숫자 야구 게임을 시작합니다.");
 }
 
+function makeRandomNum() {
+  let ranNumArr = [];
+  let ranNum = 0;
+  for (let i = 0; i < 3; i++) {
+    ranNumArr.push(Math.floor(Math.random()*9+1));
+    if (ranNumArr[i] == ranNumArr[i-1] || ranNumArr[i] == ranNumArr[i-2]) {
+      ranNumArr.pop();
+      i--;
+    }
+  }
+  ranNum = ranNumArr.map(Number).join('');
+  return ranNum;
+}
+
 starGame();
+const computerNum = makeRandomNum();
 
 module.exports = App;
