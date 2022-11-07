4389fe7e8fba52b2b8a0dcb277eba27682780cd7
kim-limit <rlawogks2468@naver.com>
2022-11-07 15:21:57 +0900

    feat: 'add check exception func'

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  play() {
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+  }
+}
+
+function checkException(number) {
+  if (number === undefined) {
+    throw new Error("undefined");
+  } else if (number === "") {
+    throw new Error("입력해야 합니다.");
+  } else if (isNaN(number)) {
+    throw new Error("숫자를 입력해야 합니다.");
+  } else if (number.length !== 3) {
+    throw new Error("세자리 숫자를 입력해야 합니다.");
+  } else if (checkSameNumber(number)) {
+    throw new Error("서로 다른 숫자를 입력해야 합니다.");
+  }
 }
 
 module.exports = App;
