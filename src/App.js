05144aeabb1fd037759f5cac8989fb77d2c7efea
rayark1 <max980203@snu.ac.kr>
2022-11-08 00:23:04 +0900

    feat(src/App.js): 숫자 입력 prompt 및 입력값에 따른 flow 구현

--- src/App.js
+++ src/App.js
@@ -14,6 +14,19 @@ const generateAnswer = function generateRandomThreeNumbers() {
   }
 };
 
+const promptInput = function promptUserGuessInput(answer) {
+  MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (input) => {
+    validateInput(input);
+    const result = calculateResult(input, answer);
+    MissionUtils.Console.print(result);
+    if (result === "3개의 숫자를 모두 맞히셨습니다! 게임 종료") {
+      askPlayNewGame();
+      return;
+    }
+    promptInput(answer);
+  });
+}
+
 class App {
   play() {}
 }
