864c11b08dfe8538960aa98be16d39197ed6fffb
leeuihyun <meta212121@naver.com>
2022-11-08 02:42:24 +0900

    test : 숫자 야구 시작시 출력 테스트

---
+++ __tests__/myFunctionTest.js
@@ -0,0 +1,48 @@
+const Game = require('../src/game/Game');
+const MissionUtils = require('@woowacourse/mission-utils');
+
+const mockQuestions = (answers) => {
+  MissionUtils.Console.readLine = jest.fn();
+  answers.reduce((acc, input) => {
+    return acc.mockImplementationOnce((question, callback) => {
+      callback(input);
+    });
+  }, MissionUtils.Console.readLine);
+};
+
+const mockRandoms = (numbers) => {
+  MissionUtils.Random.pickNumberInRange = jest.fn();
+  numbers.reduce((acc, number) => {
+    return acc.mockReturnValueOnce(number);
+  }, MissionUtils.Random.pickNumberInRange);
+};
+
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(MissionUtils.Console, 'print');
+  logSpy.mockClear();
+  return logSpy;
+};
+
+describe('숫자 야구 게임 시작 테스트', () => {
+  test('숫자 야구 게임 시작 출력 테스트', () => {
+    const log = getLogSpy();
+
+    const game = new Game();
+    game.initPrint();
+
+    expect(log).toHaveBeenCalledWith('숫자 야구 게임을 시작합니다.');
+  });
+
+  test('예외 테스트', () => {
+    const randoms = [1, 3, 5];
+    const answers = ['1234'];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow();
+  });
+});
