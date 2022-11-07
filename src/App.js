0cfd3feb97378d50e81d03da414d95e70b8a4074
doka <posmane5@gmail.com>
2022-11-08 00:19:26 +0900

    자잘한 코드 정리

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ class App {
   play() {}
 }
 
-module.exports = App;
-
 /**
  * 숫자, 숫자의 위치 일치여부 가려내는 기능
  * @param {Array} userNums
  * @param {Array} computerNums
  */
- function matchNumber(userNums, computerNums) {
+function matchNumber(userNums, computerNums) {
   let userNumsList = [...userNums];
   let computerNumsList = [...computerNums];
 
@@ -21,7 +19,7 @@ module.exports = App;
 
   /**
    * userNumList를 돌면서 인덱스와 값을 추출한뒤 computerNumList의 원소들과 매칭하기
-   * strike와  ball 여부확인 
+   * strike와  ball 여부확인
    */
   userNumsList.forEach(function (userNum, userIdx) {
     computerNumsList.forEach(function (computerNum, computerIdx) {
@@ -36,16 +34,15 @@ module.exports = App;
   // result를 보고 스트라이크, 볼, 혹은 완전 불일치여부 확인후 결과 문자열 리턴
   if (matchResult.strike === 0 && matchResult.ball === 0) {
     return "낫싱";
-  } else {  
+  } else {
     return `${matchResult.ball ? matchResult.ball + "볼 " : ""}${
       matchResult.strike ? matchResult.strike + "스트라이크" : ""
     }`;
   }
-   
 }
 
- 
-// Test를 위한 코드
+// Test를 위한 exports 코드
 module.exports = {
-  matchNumber
-}
+  matchNumber,
+  App,
+};
