32a3dda86c5725244f3e1572d44ff05444486eb2
JaeyeonHong <39540473+HongJaeyeon@users.noreply.github.com>
2022-11-08 04:39:48 +0900

    feat: 답을 생성하는 makeThreeUniqueRandomNumber 기능 추가

--- src/BaseBallGame.js
+++ src/BaseBallGame.js
@@ -9,8 +9,13 @@ class BaseBallGame{
 
     start = () => {
         MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+        this.setAnswer();
         this.enterUserInput();
     };
+
+    setAnswer = () => {
+        this.answer = this.makeThreeUniqueRandomNumber();
+    }
 
     setUserAnswer = (userInput) => {
         this.userAnswer = userInput;
@@ -37,12 +42,25 @@ class BaseBallGame{
         MissionUtils.Console.readLine("숫자를 입력해주세요", (input) => {
             let userInput = input.split("").map((item) => parseInt(item));
             if (this.isVaildInput(userInput)){
-                return
+                this.setUserAnswer(userInput);
             }else {
                 throw new Error("올바르지 않은 값이 입력됐습니다.");
             }
         });
     };
+
+    makeThreeUniqueRandomNumber = () => {
+        let pickNumber;
+        let pickNumberList = [];
+        while (pickNumberList.length < 3){
+            pickNumber = MissionUtils.Random.pickNumberInRange(1,9);
+            if (!pickNumberList.includes(pickNumber)){
+                pickNumberList.push(pickNumber);
+            }
+        }
+        return pickNumberList;
+    }
+
 }
 
 module.exports = BaseBallGame
