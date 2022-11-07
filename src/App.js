c12c262d60e7a44ddc156d6520be32b791d4fa4f
jaehak26 <jack10829@naver.com>
2022-11-08 01:38:24 +0900

    스트라이크/볼 검사, 카운트 출력, 게임 종료
    스트라이크/볼을 검사하여 카운터를 세고 출력
    이후 스트라이크가 3개면 게임 종료
    아닐 경우 속행
    게임 종료시 입력값을 받고 1이면 재실행
    2면 콘솔 종료

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-// run `node index.js` in the terminal
-//@woowacourse/mission-utils
 
 const MissionUtils = require('@woowacourse/mission-utils');
 
@@ -8,24 +6,17 @@ const GAME_END = 2;
 const ERROR = -1;
 
 class App {
-  #gameState;
   #computerAnswer = [];
   #userInput;
-  #strikeCount=0
-  #ballCount=0
-
-  constructor() {
-    this.#gameState = GAME_START;
-  }
+  #strikeCount = 0;
+  #ballCount = 0;
 
   play() {
     this.#computerAnswer = this.#gameStart();
-    console.log(this.#computerAnswer);
 
     this.#inputFromUser();
   }
 
-  //private 처리
   #gameStart() {
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
 
@@ -40,14 +31,30 @@ class App {
     return computer;
   }
 
+  #gameEnd() {
+    MissionUtils.Console.readLine(
+      '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n',
+      (replay) => {
+        if (Number(replay) === GAME_START) {
+          return this.play();
+        } else if (Number(replay) === GAME_END) {
+        } else {
+          MissionUtils.Console.print('잘못 입력하셨습니다.');
+          return this.#gameEnd();
+        }
+        MissionUtils.Console.close();
+      }
+    );
+    return;
+  }
+
   #inputFromUser() {
     //출력 중간에 입력값이 보이는 에러가 있어 개행문자를 추가했습니다.
     //ex) 숫자를 입력해345요
     let input = 0;
-
+    [this.#strikeCount, this.#ballCount] = [0, 0];
     MissionUtils.Console.readLine('숫자를 입력해주세요 : \n', (answer) => {
       input = answer;
-      console.log(input);
 
       //예외 발생 시 다시 입력 처리
       if (this.#inputExcept(input) === ERROR) {
@@ -55,37 +62,63 @@ class App {
       }
 
       this.#userInput = this.#inputToArray(input);
+      this.#compareTwoArray();
+      this.#printStrikeBall();
+      if (this.#strikeCount !== 3) {
+        return this.#inputFromUser();
+      }
 
-      MissionUtils.Console.close();
+      MissionUtils.Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+      this.#gameEnd();
     });
 
     return;
   }
 
-  #compareTwoArray(){
-    [this.#strikeCount,this.#ballCount]=[0,0]
-    
-    for(let comIdx=0; comIdx<#computerAnswer.length;comIdx++){
-      for(let userIdx=0; userIdx<#userInput.length;userIdx++){
+  #printStrikeBall() {
+    const strikeStr = this.#strikeCount + ' 스트라이크';
+    const ballStr = this.#ballCount + ' 볼';
+    const outStr = '아웃';
 
-        this.#compareTwoNumber(comIdx,userIdx)
-      }
+    if (this.#ballCount === 0 && this.#strikeCount !== 0) {
+      MissionUtils.Console.print(strikeStr);
+    } else if (this.#ballCount !== 0 && this.#strikeCount === 0) {
+      MissionUtils.Console.print(ballStr);
+    } else if (this.#ballCount === 0 && this.#strikeCount === 0) {
+      MissionUtils.Console.print(outStr);
+    } else {
+      MissionUtils.Console.print(strikeStr + ballStr);
     }
+    return;
   }
 
-  #compareTwoNumber(comIdx, userIdx){
-    if(comIdx===userIdx
-      &&this.#computerAnswer[comIdx]===this.#userInput[userIdx]){
-        this.#strikeCount++;
-    }
-    if(comIdx!==userIdx
-      &&this.#computerAnswer[comIdx]===this.#userInput[userIdx]){
-        this.#ballCount++;
+  #compareTwoArray() {
+    for (let comIdx = 0; comIdx < this.#computerAnswer.length; comIdx++) {
+      for (let userIdx = 0; userIdx < this.#userInput.length; userIdx++) {
+        this.#compareTwoNumber(comIdx, userIdx);
       }
+    }
+    return;
+  }
+
+  #compareTwoNumber(comIdx, userIdx) {
+    if (
+      comIdx === userIdx &&
+      this.#computerAnswer[comIdx] === this.#userInput[userIdx]
+    ) {
+      this.#strikeCount++;
+    }
+    if (
+      comIdx !== userIdx &&
+      this.#computerAnswer[comIdx] === this.#userInput[userIdx]
+    ) {
+      this.#ballCount++;
+    }
+    return;
   }
 
   #inputToArray(input) {
-    return input.split('');
+    return input.split('').map((arrData) => Number(arrData));
   }
 
   #inputExcept(input = '') {
