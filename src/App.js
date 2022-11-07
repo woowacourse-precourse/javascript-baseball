afd5f6842994bab76dd12b6952916ae907b21d01
jaehak26 <jack10829@naver.com>
2022-11-07 21:01:11 +0900

    게임 시작 및 랜덤숫자 만들기
    게임 시작멘트를 콘솔로 출력하고 컴퓨터는 무작위 숫자 세 개(배열)를 만든다

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+
 class App {
-  play() {}
+  play() {
+    this.gameStart();
+  }
+
+  gameStart() {
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+
+    const computer = [];
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+    MissionUtils.Console.close();
+    console.log(computer);
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
