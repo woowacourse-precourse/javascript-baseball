c137de87ba96ecfd94dbf83dceab61f2fe95b3ad
kangju2000 <kangju2000@naver.com>
2022-11-08 02:59:52 +0900

    refactor: Referee 클래스의 gameResult 메서드 구현 - App 클래스의 getResult 메서드의 로직을 Referee 클래스의 gameResult 메서드에 적용

--- src/Referee.js
+++ src/Referee.js
@@ -18,6 +18,27 @@ class Referee {
     this.player.setValue();
   }
 
+  gameResult() {
+    const [computerValue, playerValue] = [this.computer.getValue(), this.player.getValue()];
+    let [ball, strike] = [0, 0];
+
+    for (let i = 0; i < 3; i++) {
+      if (computerValue[i] === playerValue[i]) strike++;
+      else if (computerValue.includes(playerValue[i])) ball++;
+    }
+
+    const nothingString = ball === 0 && strike === 0 ? '낫싱' : '';
+    const ballString = ball ? `${ball}볼 ` : '';
+    const strikeString = strike ? `${strike}스트라이크` : '';
+
+    MissionUtils.Console.print(nothingString + ballString + strikeString);
+
+    if (strike === 3) {
+      MissionUtils.Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+      this.gameFinish();
+    } else this.player.setValue();
+  }
+
   gameFinish() {
     MissionUtils.Console.readLine(
       '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n',
