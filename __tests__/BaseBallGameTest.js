c0a726e06b7e31f0083ed75b14f1225642deaf68
SaeWooKKang <87258182+SaeWooKKang@users.noreply.github.com>
2022-11-07 11:37:32 +0900

    test(기능목록10-3): checkUserAnswer

--- __tests__/BaseBallGameTest.js
+++ __tests__/BaseBallGameTest.js
@@ -29,4 +29,41 @@ describe("숫자 야구 게임 메서드 테스트", () => {
     expect(() => bindValidation([1,6,6]))
       .toThrow('입력 숫자가 중복되었습니다.');
   });
+  
+  test('게임 규칙에 의한 정답 체크', () => {
+    const { checkUserAnswer } = baseBallGame;
+    let gameAnswer = [1,2,3];
+
+    let userAnswer = [1,2,3];
+    expect(checkUserAnswer(gameAnswer, userAnswer)).toEqual(
+      {
+        victory: true,
+        count: { 스트라이크: 3, 볼: 0, 낫싱: 0}
+      }
+    );
+
+    userAnswer = [4,5,6];
+    expect(checkUserAnswer(gameAnswer, userAnswer)).toEqual(
+      {
+        victory: false,
+        count: { 스트라이크: 0, 볼: 0, 낫싱: 3}
+      }
+    );
+    
+    userAnswer = [3,1,2];
+    expect(checkUserAnswer(gameAnswer, userAnswer)).toEqual(
+      {
+        victory: false,
+        count: { 스트라이크: 0, 볼: 3, 낫싱: 0}
+      }
+    );
+
+    userAnswer = [1,3,9];
+    expect(checkUserAnswer(gameAnswer, userAnswer)).toEqual(
+      {
+        victory: false,
+        count: { 스트라이크: 1, 볼: 1, 낫싱: 1}
+      }
+    );
+  });
 })
