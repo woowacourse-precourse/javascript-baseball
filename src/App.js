5aca4fc6f0f51ab39123115f32a85118832940d8
juicyorange <rlaxodud9809@khu.ac.kr>
2022-11-07 21:23:38 +0900

    feat: add createRandomNumDigitArray function
    Add createRandomNumDigitArray fuction that return digit array
    - must be not include same number
    - return 3 length array
    - use @woowacourse/mission-utils
    
    Breaks add @woowacourse/mission-utils package

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+
 class App {
   play() {}
+
+  /**
+   * return random num digit array
+   * @returns randomNumDigitArray
+   */
+  createRandomNumDigitArray() {
+    const randomNumDigitArray = [];
+    while (randomNumDigitArray.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!randomNumDigitArray.includes(number)) {
+        randomNumDigitArray.push(number);
+      }
+    }
+
+    return randomNumDigitArray;
+  }
 }
 
 module.exports = App;
