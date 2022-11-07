32d2d698b3316f67af7394e22095562f8a00ffec
Doyoung.Kim <dyflower@nate.com>
2022-11-08 01:32:57 +0900

    feat : 무작위 세자리 수 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+
 class App {
   play() {}
 }
 
+function createAnswer() {
+  let answer = [];
+  answer = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
+  return answer;
+}
 module.exports = App;
