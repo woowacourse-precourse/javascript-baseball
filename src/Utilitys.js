350009fda54818b80cd57296dc6db50dfb18e977
MyungAe <ladiescheong@naver.com>
2022-11-08 01:01:41 +0900

    feat(Utilitys): 남은 MissionUtils 객체 함수 관리에 용이하도록 따로 구현

--- src/Utilitys.js
+++ src/Utilitys.js
@@ -7,3 +7,38 @@ const readLine = async (message) => {
     });
   });
 };
+
+const print = (message) => {
+  MissionUtils.Console.print(message);
+};
+
+const closeConsole = () => {
+  MissionUtils.Console.close();
+};
+
+const Console = { readLine, print, closeConsole };
+
+const pickNumberInRange = (start, end) => {
+  return MissionUtils.Random.pickNumberInRange(start, end);
+};
+
+const pickNumberInList = (array) => {
+  return MissionUtils.Random.pickNumberInList(array);
+};
+
+const pickUniqueNumbersInRange = (start, end, count) => {
+  return MissionUtils.Random.pickUniqueNumbersInRange(start, end, count);
+};
+
+const shuffle = (array) => {
+  return MissionUtils.Random.shuffle(array);
+};
+
+const Random = {
+  pickNumberInRange,
+  pickNumberInList,
+  pickUniqueNumbersInRange,
+  shuffle,
+};
+
+module.exports = { Console, Random };
