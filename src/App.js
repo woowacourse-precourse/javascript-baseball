d4908baaf2bd33f08a4506fbe6a3d78993448d8a
김나현 <nahyeon.dev@gmail.com>
2022-11-07 18:23:32 +0900

    feat: add constructor() and play() and random()/set computer random number function

--- src/App.js
+++ src/App.js
@@ -1,5 +1,37 @@
+
 class App {
-  play() {}
+  constructor(computerNum){
+    this.computerNum = computerNum;
+  }
+  play() { 
+    this.random();
+  }
+  random(){
+    this.computerNum = setRandomNumberComputer();
+  }
+
 }
+
+function setRandomNumberComputer(){
+  const computer = [];
+  while(computer.length < 3) {
+    const number = MissionUtils.Random.pickNumberInRange(1, 9);
+    if(!computer.includes(number)){
+      computer.push(number);
+    }
+  }
+  return computer;
+}
+ 
+
+const app = new App();
+
+MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+while(isFinish == 1){
+  app.play();
+}
+
+//종료
+
 
 module.exports = App;
