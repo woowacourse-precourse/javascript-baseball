9b92fd32ccdbcf3eff2c81f4346790d3bbcd16ac
Deemou <4106243@naver.com>
2022-11-08 04:26:46 +0900

    refactor<App>: 함수 모듈화에 따른 리팩토링

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 const MissionUtils = require("@woowacourse/mission-utils");
+const {
+  print,
+  isEqual,
+  throwException,
+  generateRandomNumber,
+} = require("./Util");
 
 class App {
   play() {
     const GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
-    this.print(GAME_START_MESSAGE);
+    print(GAME_START_MESSAGE);
     this.initGame();
   }
   initGame() {
     const DIGITS = 3;
-    const answer = this.generateRandomNumber(DIGITS);
+    const answer = generateRandomNumber(DIGITS);
     this.playRound(answer);
   }
   playRound(answer) {
@@ -19,14 +25,14 @@ class App {
 
     MissionUtils.Console.readLine(INPUT_MESSAGE, (userNumber) => {
       if (!this.isValidNumber(userNumber, digits))
-        this.throwException(ERROR_MESSAGE);
+        throwException(ERROR_MESSAGE);
       const [ballCount, strikeCount] = this.checkBaseballCount(
         answer,
         userNumber
       );
       this.printResult([ballCount, strikeCount]);
       if (this.isCorrectAnswer(strikeCount, digits)) {
-        this.print(GAME_END_MESSAGE);
+        print(GAME_END_MESSAGE);
         this.selectRestartOrExit();
       }
       this.playRound(answer);
@@ -39,25 +45,10 @@ class App {
     const RESTART = 1,
       EXIT = 2;
     MissionUtils.Console.readLine(FINAL_MESSAGE, (command) => {
-      if (!this.isValidCommand(command)) this.throwException(ERROR_MESSAGE);
+      if (!this.isValidCommand(command)) throwException(ERROR_MESSAGE);
       if (command == RESTART) this.initGame();
       if (command == EXIT) MissionUtils.Console.close();
     });
-  }
-  generateRandomNumber(digits) {
-    const START_INCLUSIVE = 1,
-      END_INCLUSIVE = 9;
-    const randomNumberSet = new Set();
-
-    while (randomNumberSet.size < digits) {
-      const num = MissionUtils.Random.pickNumberInRange(
-        START_INCLUSIVE,
-        END_INCLUSIVE
-      );
-      randomNumberSet.add(num);
-    }
-
-    return [...randomNumberSet];
   }
   checkBaseballCount(answer, userNumber) {
     const userNumberList = userNumber.split("").map((num) => Number(num));
@@ -66,7 +57,7 @@ class App {
     let strikeCount = 0;
 
     userNumberList.forEach((num, index) => {
-      if (this.isEqual(num, answer[index])) strikeCount++;
+      if (isEqual(num, answer[index])) strikeCount++;
       else notStrikeList.push(num);
     });
     notStrikeList.forEach((num) => {
@@ -77,53 +68,43 @@ class App {
   }
   printResult(baseBallCounts) {
     const message = this.getResult(baseBallCounts);
-    this.print(message);
+    print(message);
   }
   getResult([ballCount, strikeCount]) {
     const totalCount = ballCount + strikeCount;
     let message = "";
-    if (this.isEqual(totalCount, 0)) {
+    if (isEqual(totalCount, 0)) {
       message = "낫싱";
       return message;
     }
-    if (!this.isEqual(ballCount, 0)) {
+    if (!isEqual(ballCount, 0)) {
       message = message + `${ballCount}볼 `;
     }
-    if (!this.isEqual(strikeCount, 0)) {
+    if (!isEqual(strikeCount, 0)) {
       message = message + `${strikeCount}스트라이크`;
     }
     return message;
   }
-  print(message) {
-    MissionUtils.Console.print(message);
-  }
-  isEqual(a, b) {
-    return a === b;
-  }
   isCorrectAnswer(strikeCount, digits) {
-    return this.isEqual(strikeCount, digits);
+    return isEqual(strikeCount, digits);
   }
   isValidNumber(num, digits) {
     if (isNaN(num)) return false;
 
     const numList = num.split("");
-    if (!this.isEqual(numList.length, digits)) return false;
+    if (!isEqual(numList.length, digits)) return false;
 
     const numSet = numList.reduce((numSet, num) => {
       return numSet.add(num);
     }, new Set());
     if (numSet.has("0")) return false;
-    if (!this.isEqual(numSet.size, digits)) return false;
+    if (!isEqual(numSet.size, digits)) return false;
 
     return true;
   }
   isValidCommand(command) {
     const commandList = ["1", "2"];
     return commandList.includes(command);
-  }
-  throwException(message) {
-    MissionUtils.Console.close();
-    throw new Error(message);
   }
 }
 const app = new App();
