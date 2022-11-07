1b8620ee4fcf8bca2d91da8c98f593e1bdad8077
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2022-11-07 15:52:53 +0900

    refactor(CheckValid): CheckVaild에서 에러를 발생시키지않고 값을 반환하도록 구조 변환

--- src/CheckValid.js
+++ src/CheckValid.js
@@ -1,3 +1,5 @@
+const { ERROR } = require("./constants");
+
 function checkLength(userNum) {
   if (userNum.length !== 3) {
     return false;
@@ -42,32 +44,30 @@ class CheckInputValid {
 
   checkValidation() {
     if (checkLength(this.userNum) === false) {
-      throw new Error("숫자를 3개 입력해주세요");
+      return ERROR.USER_INPUT_LENGTH;
     }
 
     if (checkDuplicates(this.userNum) === false) {
-      throw new Error("숫자에 중복이 있습니다");
+      return ERROR.USER_INPUT_DUPLICATES;
     }
 
     if (checkRange(this.userNum) === false) {
-      throw new Error("숫자 1~9까지만 입력이 가능합니다");
+      return ERROR.USER_INPUT_RANGE;
     }
     if (checkBlank(this.userNum) === false) {
-      throw new Error("공백은 입력할 수 없습니다");
+      return ERROR.USER_INPUT_BLANK;
     }
   }
 
   checkRetryInput() {
     if (checkRangeOfRetryUserInput(this.retryNum) === false) {
-      throw new Error(
-        "입력은 1또는 2만 가능합니다. 2개 이상 입력할 수 없습니다."
-      );
+      return ERROR.USER_RETRY_INPUT_RANGE;
     }
     if (checkBlank(this.retryNum) === false) {
-      throw new Error("공백은 입력할 수 없습니다");
+      return ERROR.USER_RETRY_INPUT_BLANK;
     }
     if (checkLengthOfRetryUserInput(this.retryNum) === false) {
-      throw new Error("숫자가 2개 이상 입력되었습니다.");
+      return ERROR.USER_RETRY_INPUT_LENGTH;
     }
   }
 }
