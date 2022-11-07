5e39677c2c148a7d947c9389247d3abc0fb986b8
안호진 <asdv94@naver.com>
2022-11-08 04:13:51 +0900

    fix: 콜백 미처리 버그 수정 및 불필요한 메소드 제거
    Console.readLine 입력 콜백 함수 처리 로직 누락으로 인한 프로그램 오류 수정
    주어진 테스트 통과

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App {
   #GAME_MSG = {
     START: "숫자 야구 게임을 시작합니다.",
     PLEASE_INPUT: "숫자를 입력해주세요 : ",
-    ASK_RESTART: "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
+    ASK_RESTART: "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n",
     QUIT: "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
   };
   #GAME_RESULT = { STRIKE: "스트라이크", BALL: "볼", NOTHING: "낫싱" };
@@ -15,14 +15,11 @@ class App {
     DUPLICATE_NUMBERS: "서로 다른 3개의 숫자를 입력해주세요.",
     ONLY_ONE_OR_TWO: "1 또는 2를 입력해주세요.",
   };
+  #CORRECT_ANSWER = "3스트라이크";
+  #EXIT_APP = "게임 종료";
   #computerNumbers;
-  #isUserWon;
-  #isStartGame;
   constructor() {
-    this.userNumbers = null;
     this.#computerNumbers = null;
-    this.#isUserWon = false;
-    this.#isStartGame = true;
   }
   #getComputerNumbers() {
     return this.#computerNumbers;
@@ -30,40 +27,31 @@ class App {
   #setComputerNumbers(arr) {
     this.#computerNumbers = arr;
   }
-  getIsUserWon() {
-    return this.#isUserWon;
-  }
-  #setIsUserWon(bool) {
-    this.#isUserWon = bool;
-  }
-  getIsStartGame() {
-    return this.#isStartGame;
-  }
-  #setIsStartGame(bool) {
-    this.#isStartGame = bool;
-  }
-  #initComputerNumbers(arr = [], randNo = Random.pickNumberInRange(1, 9)) {
+  #initComputerNumbers(arr = [], randNo = 0) {
     arr.length === 3
       ? this.#setComputerNumbers(arr)
-      : (arr.includes(randNo) || arr.push(randNo),
+      : ((randNo = Random.pickNumberInRange(1, 9)),
+        arr.includes(randNo) || arr.push(randNo),
         this.#initComputerNumbers(arr));
   }
   #takeUserNumbersInput() {
     const checkUnique = (arr) => new Set(arr).size === 3;
     const checkUserNumbersInputValidity = (arr) => {
-      if (arr.every((el) => !Number.isInteger(el) || el > 9 || el < 1))
+      if (!arr.every((el) => Number.isInteger(el) && el <= 9 && el >= 1))
         throw this.#ERROR_MSG.ONLY_NUMBERS;
       if (arr.length !== 3) throw this.#ERROR_MSG.INVALID_LENGTH;
       if (!checkUnique(arr)) throw this.#ERROR_MSG.DUPLICATE_NUMBERS;
     };
     Console.readLine(this.#GAME_MSG.PLEASE_INPUT, (input) => {
-      const inputNumberArr = input.trim().split("").map(Number);
-      try {
-        checkUserNumbersInputValidity(inputNumberArr);
-        this.userNumbers = inputNumberArr;
-      } catch (e) {
-        this.#handleException(e);
-      }
+      const userNumbers = input.trim().split("").map(Number);
+      checkUserNumbersInputValidity(userNumbers);
+      const rstArr = this.#compareEachNumbers(
+        this.#getComputerNumbers(),
+        userNumbers
+      );
+      this.#getResult(rstArr) === this.#CORRECT_ANSWER
+        ? this.#askRestart()
+        : this.#takeUserNumbersInput();
     });
   }
   #compareEachNumbers(computerNumbers, userNumbers) {
@@ -78,63 +66,39 @@ class App {
     );
   }
   #getResult([ballCnt, strikeCnt]) {
-    return ballCnt === 0 && strikeCnt === 0
-      ? this.#GAME_RESULT.NOTHING
-      : [
-          [ballCnt, this.#GAME_RESULT.BALL],
-          [strikeCnt, this.#GAME_RESULT.STRIKE],
-        ]
-          .filter(([cnt]) => cnt > 0)
-          .map((line) => line.join(""))
-          .join(" ");
-  }
-  #printResult(str) {
-    Console.print(str);
-  }
-  #handleGameOver(str) {
-    this.#setIsUserWon(str === "3스트라이크");
+    const rstStr =
+      ballCnt === 0 && strikeCnt === 0
+        ? this.#GAME_RESULT.NOTHING
+        : [
+            [ballCnt, this.#GAME_RESULT.BALL],
+            [strikeCnt, this.#GAME_RESULT.STRIKE],
+          ]
+            .filter(([cnt]) => cnt > 0)
+            .map((line) => line.join(""))
+            .join(" ");
+    Console.print(rstStr);
+    return rstStr.trim();
   }
   #askRestart() {
     Console.readLine(this.#GAME_MSG.ASK_RESTART, (input) => {
-      try {
-        const trimmed = +input.trim();
-        if (trimmed !== 1 && trimmed !== 2)
-          throw this.#ERROR_MSG.ONLY_ONE_OR_TWO;
-        this.#setIsStartGame(trimmed === 1);
-      } catch (e) {
-        this.#handleException(e);
-      }
+      const restartNo = +input.trim();
+
+      if (restartNo !== 1 && restartNo !== 2)
+        throw this.#ERROR_MSG.ONLY_ONE_OR_TWO;
+      restartNo === 1 ? this.#startGame() : this.#exitApp();
     });
   }
-  #handleException(error) {
-    Console.print(error);
-    this.#exitApp();
-  }
   #exitApp() {
+    Console.print(this.#EXIT_APP);
     Console.close();
-  }
-  #continueGame() {
-    this.#takeUserNumbersInput();
-    const rstArr = this.#compareEachNumbers(
-      this.#getComputerNumbers(),
-      this.userNumbers
-    );
-    const rstStr = this.#getResult(rstArr);
-    this.#printResult(rstStr);
-    this.#handleGameOver(rstStr);
   }
   #startGame() {
     this.#initComputerNumbers();
-    while (!this.getIsUserWon()) this.#continueGame();
-    Console.print(this.#GAME_MSG.QUIT);
-    this.#askRestart();
+    this.#takeUserNumbersInput();
   }
   play() {
     Console.print(this.#GAME_MSG.START);
-    while (this.getIsStartGame()) {
-      this.#startGame();
-    }
-    this.#exitApp();
+    this.#startGame();
   }
 }
 
