0b7a0233165e1e5fe2ffb10200b5d21a78905163
안호진 <asdv94@naver.com>
2022-11-07 23:10:38 +0900

    feat: 1-1.1 메세지 상수 초기화
    1. 초기화 기능 앱 초기화 기능 구현
    게임 안내(#GAME_MSG) 메세지(START: `숫자 야구 게임을 시작합니다.`, PLEASE_INPUT: `숫자를 입력해주세요 : `, ASK_RESTART: `게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.`, QUIT: `3개의 숫자를 모두 맞히셨습니다! 게임 종료`) 선언, 할당
    게임 결과(#GAME_RESULT) 메세지(STRIKE: `스트라이크`, BALL: `볼`, NOTHING: `낫싱`) 선언, 할당
    (추가구현)에러(#ERROR_MSG) 메세지(ONLY_NUMBERS: `숫자만 입력해주세요.`, INVALID_LENGTH: `반드시 3개의 숫자를 입력해주세요.`, DUPLICATE_NUMBERS: `서로 다른 3개의 숫자를 입력해주세요.`, ONLY_ONE_OR_TWO: `1 또는 2를 입력해주세요.`) 선언, 할당

--- src/App.js
+++ src/App.js
@@ -1,4 +1,17 @@
 class App {
+  #GAME_MSG = {
+    START: "숫자 야구 게임을 시작합니다.",
+    PLEASE_INPUT: "숫자를 입력해주세요 : ",
+    ASK_RESTART: "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
+    QUIT: "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
+  };
+  #GAME_RESULT = { STRIKE: "스트라이크", BALL: "볼", NOTHING: "낫싱" };
+  #ERROR_MSG = {
+    ONLY_NUMBERS: "숫자만 입력해주세요.",
+    INVALID_LENGTH: "반드시 3개의 숫자를 입력해주세요.",
+    DUPLICATE_NUMBERS: "서로 다른 3개의 숫자를 입력해주세요.",
+    ONLY_ONE_OR_TWO: "1 또는 2를 입력해주세요.",
+  };
   play() {}
 }
 
