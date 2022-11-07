b36c573142a222af99292bf4673d59a4e7f01578
Wang HoEun <hoeunwang@gmail.com>
2022-11-07 11:16:53 +0900

    feat: Random 라이브러리를 활용하여 randomNumbers 생성

--- src/component/Computer.js
+++ src/component/Computer.js
@@ -1,5 +1,19 @@
 const { Random } = require("@woowacourse/mission-utils");
 
-class Computer {}
+class Computer {
+  setRandomNumbers() {
+    const randomNumbers = [];
+
+    while (randomNumbers.length < 3) {
+      const number = Random.pickNumberInRange(1, 9);
+
+      if (randomNumbers.includes(number) === false) {
+        randomNumbers.push(number);
+      }
+    }
+
+    return randomNumbers.join("");
+  }
+}
 
 module.exports = Computer;
