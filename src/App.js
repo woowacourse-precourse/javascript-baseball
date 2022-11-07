da45d756666756a00b089b228c4bcd1c929db3b0
hafnium1923 <hafnium1923@gmail.com>
2022-11-07 23:58:35 +0900

    feat update_print
    출력문 수정

--- src/App.js
+++ src/App.js
@@ -21,8 +21,10 @@ class App {
         
     MissionUtils.Console.readLine('숫자를 입력해주세요', (user_num) => {
       let flag = this.checkNum(RANDOM,user_num);
-      if(flag)
+      if(flag){
+        MissionUtils.Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
         MissionUtils.Console.close();
+      }
     })
   }
 
@@ -51,13 +53,13 @@ class App {
   }
 
   print(strike, ball){
-    if(ball != 0){
-      MissionUtils.Console.print(`${ball}볼 `);
-    }
-    if(strike != 0){
+    if(ball != 0 && strike === 0){
+      MissionUtils.Console.print(`${ball}볼`);
+    }else if(strike != 0 && ball === 0){
       MissionUtils.Console.print(`${strike}스트라이크`);
-    }
-    if(ball === 0 && strike === 0){
+    }else if(ball != 0 && strike != 0){
+      MissionUtils.Console.print(`${ball}볼 ${strike}스트라이크`);
+    }else{
       MissionUtils.Console.print('낫싱');
     }
   }
