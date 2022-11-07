75dcc75db268affcb1ef6566de2efe0b028e4891
random-olive <104327907+random-olive@users.noreply.github.com>
2022-11-07 22:58:24 +0900

    Fix: game logic : ball strike nothing result

--- src/GameLogic.js
+++ src/GameLogic.js
@@ -1,12 +1,7 @@
 const MissionUtils = require("@woowacourse/mission-utils");
-const Validation = require("../src/Validation");
+const App = require("../src/App");
 
-const {
-  GAME_CONFIG,
-  ERROR_MESSAGE,
-  INGAME_MESSAGE,
-  RESPONSE,
-} = require("./Constant");
+const { INGAME_MESSAGE, RESPONSE } = require("./Constant");
 
 class GameLogic {
   judge(userInput, computerInput) {
@@ -26,8 +21,25 @@ class GameLogic {
   result(ballStrike) {
     if (ballStrike[1] === 3) {
       MissionUtils.Console.print(INGAME_MESSAGE.END);
+      return true;
     } else if (ballStrike[0] === 0 && ballStrike[1] === 0) {
-      MissionUtils.Console.print();
+      MissionUtils.Console.print(INGAME_MESSAGE.NOTHING);
+      return false;
+    } else if (ballStrike[0] === 0) {
+      MissionUtils.Console.print(ballStrike[1] + INGAME_MESSAGE.STRIKE);
+      return false;
+    } else if (ballStrike[1] === 0) {
+      MissionUtils.Console.print(ballStrike[0] + INGAME_MESSAGE.BALL);
+      return false;
+    } else {
+      MissionUtils.Console.print(
+        ballStrike[0] +
+          INGAME_MESSAGE.BALL +
+          " " +
+          ballStrike[1] +
+          INGAME_MESSAGE.STRIKE
+      );
+      return false;
     }
   }
 }
