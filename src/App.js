306ee03ac6acaec4f9c29ed87f6711d7ee73c8bf
kimyou1102 <yoosk5485@naver.com>
2022-11-08 00:14:10 +0900

    refactor: 함수명을 변경하고 게임종료 함수를 분리한다.

--- src/App.js
+++ src/App.js
@@ -7,28 +7,35 @@ const hint = require("./hint.js");
 class App {
   play() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
-    this.gameStart();
+    this.startGame();
   }
 
-  gameStart() {
+  startGame() {
     const computerNums = pickNumber.pickComputerNum();
 
-    while (true) {
-      const inputNumber = numberInput();
+    let isFinish = false;
+    while (!isFinish) {
+      const inputNumber = numberInput.numberInput();
       const compare_value = compare(inputNumber, computerNums);
       const result = hint(compare_value);
 
-      if (result === "3스트라이크") {
-        MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-        break;
-      }
+      isFinish = this.EndGame(result);
     }
     pickNumber.pickNewOrEnd(this.pickOneOrTwo);
   }
 
+  EndGame(result) {
+    let isTrue = false;
+    if (result === "3스트라이크") {
+      isTrue = true;
+      MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
+    return isTrue;
+  }
+
   pickOneOrTwo = (number) => {
     if (number === "1") {
-      this.gameStart();
+      this.startGame();
     } else {
       MissionUtils.Console.print("게임 종료");
     }
