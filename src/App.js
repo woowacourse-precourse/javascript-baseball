ebb9a014a3bb2f76cf993fda6a32db261f410a99
sujungYu <su_moment@naver.com>
2022-11-07 16:18:22 +0900

    refactor : 변수 네이밍 수정

--- src/App.js
+++ src/App.js
@@ -4,21 +4,19 @@ class App {
     const computer = [];
     while (computer.length < 3) {
       const number = MissionUtils.Random.pickNumberInRange(1, 9);
-      if (!computer.includes(number)) {
-        computer.push(number);
-      }
+      if (!computer.includes(number)) computer.push(number);
     }
     return computer;
   }
 
+  postStartMessage() {
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+  }
+
   postEndMessage() {
     MissionUtils.Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
-    MissionUtils.Console.readLine(
-      '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요',
-      answer => {
-        if (this.testEndAnswer(answer)) {
-          this.endGameSystem(answer);
-        }
+    MissionUtils.Console.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n ', (answer) => {
+        if (this.testEndAnswer(answer)) this.endGameSystem(answer);
       },
     );
   }
@@ -35,11 +33,9 @@ class App {
   }
 
   getUserAnswer(computer) {
-    MissionUtils.Console.readLine('숫자를 입력해주세요 : ', answer => {
+    MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (answer) => {
       const user = answer.split('').map(Number);
-      if (this.testAnswer(user, computer)) {
-        this.gameSystem(computer, user);
-      }
+      if (this.testAnswer(user, computer)) this.gameSystem(computer, user);
     });
   }
 
@@ -47,7 +43,7 @@ class App {
     const testType = /[1-9]/;
     const set = new Set(answer);
     answer.forEach(number => {
-      if (!testType.test(number)) throw new Error('숫자를 입력하세요.');
+      if (!testType.test(number)) throw new Error('1~9사이 숫자만 입력하세요.');
       if (answer.length !== 3) throw new Error('3자리를 입력하세요.');
       if (answer.length !== set.size) throw new Error('중복된 수가 있습니다.');
     });
@@ -55,24 +51,24 @@ class App {
   }
 
   gameSystem(computer, user) {
-    let strike = 0;
-    let ball = 0;
+    let strikeCount = 0;
+    let ballCount = 0;
     user.forEach((number, index) => {
-      if (number === computer[index]) strike++;
-      else if (computer.includes(number)) ball++;
+      if (number === computer[index]) strikeCount++;
+      else if (computer.includes(number)) ballCount++;
     });
-    return this.postGameMessage(computer, strike, ball);
+    return this.postGameMessage(computer, strikeCount, ballCount);
   }
 
-  postGameMessage(computer, strike, ball) {
+  postGameMessage(computer, strikeCount, ballCount) {
     let message;
-    if (strike && ball) message = `${ball}볼 ${strike}스트라이크`;
-    else if (strike) message = `${strike}스트라이크`;
-    else if (ball) message = `${ball}스트라이크`;
+    if (strikeCount > 0 && ballCount > 0) message = `${ballCount}볼 ${strikeCount}스트라이크`;
+    else if (strikeCount > 0) message = `${strikeCount}스트라이크`;
+    else if (ballCount > 0) message = `${ballCount}볼`;
     else message = `낫싱`;
 
     MissionUtils.Console.print(message);
-    if (strike === 3) this.postEndMessage();
+    if (strikeCount === 3) this.postEndMessage();
     else this.getUserAnswer(computer);
   }
 
@@ -82,5 +78,7 @@ class App {
     this.getUserAnswer(computer);
   }
 }
+const app = new App();
+app.play();
 
 module.exports = App;
