83bf05a3431c4687832fd2e8bc4e841a0e0c286d
jinhobyeong <jhb_0921@naver.com>
2022-11-07 15:48:55 +0900

    docs : 기능 구현 목록 작성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  play() {
+    console.log(MissionUtils.Random.pickNumberInList([1, 2, 3]), "ㄴㄹㅁㄴㄹ");
+
+    // 입력
+    Console.readLine("닉네임을 입력해주세요.", (answer) => {
+      console.log(`닉네임: ${answer}`);
+    });
+
+    // 출력
+    Console.print("안녕하세요.");
+
+    // 입출력 제어 ?
+    //Console.close();
+  }
 }
 
 module.exports = App;
