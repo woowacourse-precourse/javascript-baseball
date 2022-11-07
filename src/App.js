9fcf7c726ba30afd1c1be2dc5b3f3c1eeb224bff
Monte <monte6198@gmail.com>
2022-11-07 23:09:10 +0900

    feat: 사용자 입력 유효성 검사 추가

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ class App {
     this.gameStart();
     // MissionUtils.Console.print(`상대방의 숫자는 ${this.opponent.number} 입니다`);
     const userInput = this.user.getInput();
-    MissionUtils.Console.print(`나의 입력은 ${userInput} 입니다`);
+    // MissionUtils.Console.print(`나의 입력은 ${userInput} 입니다`);
+    this.user.checkValidation(userInput);
   }
   gameStart() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
@@ -38,13 +39,17 @@ class User {
   getInput() {
     let userInput;
     MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (userInputNumber) => {
-      userInput = userInputNumber
-        .toString()
-        .split("")
-        .map((letter) => Number(letter));
+      userInput = userInputNumber.toString().split("");
       MissionUtils.Console.close();
     });
     return userInput;
+  }
+  checkValidation(userInput) {
+    userInput.map((number) => {
+      if (isNaN(number)) throw new Error("오직 숫자만 입력이 가능합니다.");
+    });
+    if (userInput.includes("0")) throw new Error("1부터 9 사이의 숫자만 입력 가능합니다.");
+    if (userInput.length !== 3) throw new Error("3자리 수만 입력이 가능합니다.");
   }
 }
 
