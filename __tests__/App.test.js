d07c49a9bb4a617dbd6c0f729e4a539379c3c18c
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2022-11-07 20:22:47 +0900

    test (App.test.js): init() - '숫자 야구 게임을 시작합니다. 문구를 출력한다.'

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -7,6 +7,12 @@ afterEach(() => {
   close();
 });
 
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(console, 'log');
+  logSpy.mockClear();
+  return logSpy;
+};
+
 describe('isStart()', () => {
   test('boolean 타입을 반환한다.', () => {
     const app = new App();
@@ -18,5 +24,16 @@ describe('isFinish()', () => {
   test('boolean 타입을 반환한다.', () => {
     const app = new App();
     expect(typeof app.isFinish()).toBe('boolean');
+  });
+});
+
+describe('init()', () => {
+  test('숫자 야구 게임을 시작합니다. 문구를 출력한다.', () => {
+    const app = new App();
+    const logSpy = getLogSpy();
+
+    app.init();
+
+    expect(logSpy).toHaveBeenCalledWith('숫자 야구 게임을 시작합니다.');
   });
 });
