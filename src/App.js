5888cc7219a9a2dc9d7cd189d04dea41a0dbf481
JUNU <106523012+bokjunwoo@users.noreply.github.com>
2022-11-07 18:53:52 +0900

    feat : 게임 규칙 생성

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ class App {
 
   play() {
     this.gameStartMessage();
+    this.computerRandomNumber();
     this.getUserNumberInput();
   }
 
@@ -20,6 +21,7 @@ class App {
       const randomNumber = MissionUtils.Random.pickNumberInRange(1, 9);
       if (!nonDuplicateNumbers.includes(randomNumber)) {
         nonDuplicateNumbers.push(randomNumber);
+        this.nonDuplicateNumbers = nonDuplicateNumbers;
       }
     }
     return nonDuplicateNumbers
@@ -28,6 +30,7 @@ class App {
   getUserNumberInput() {
     MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (userInput) => {
       this.checkError(userInput);
+      this.checkAnswer(userInput);
     });
   }
 
@@ -38,6 +41,31 @@ class App {
     if(userInput.length !== 3) throw new Error('3개의 글자가 아닙니다.');
 
     if(new Set(userInput).size !== 3) throw new Error('중복된 숫자가 있습니다.');
+  }
+
+  checkAnswer(userInput){
+    let STRIKE = 0;
+    let BALL = 0;
+    let result = '';
+
+    for(let i=0; i<userInput.length; i++){
+      if(userInput[i] === this.nonDuplicateNumbers[i]) STRIKE ++;
+      else if(userInput.includes(this.nonDuplicateNumbers[i])) BALL ++;
+    }
+
+    if(BALL === 0 && STRIKE === 0) {
+      result = '낫싱'
+    } else if(BALL === 0 && STRIKE !== 0) {
+      result =`${STRIKE}스트라이크`;
+    } else if(BALL !== 0 && STRIKE === 0) {
+      result =`${BALL}볼`;
+    } else if(BALL !== 0 && STRIKE !== 0) {
+      result=`${BALL}볼 ${STRIKE}스트라이크`;
+    } else {
+      result = '3스트라이크';
+    }
+
+    MissionUtils.Console.print(`${result}`);
   }
 }
 
