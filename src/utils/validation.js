1ed6267e0c74ef761377143cdb0d328a0a6eae21
sh981013s <sh981013s@gmail.com>
2022-11-07 19:42:45 +0900

    feat: Validation 유저 제시 수 validation 체크 로직 구현

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -5,6 +5,23 @@ class Validation {
     this.isUserGuessInputValid = true;
     this.isUserRestartInputValid = true;
   }
+
+  /**
+   * 유저가 제시한 수가 유효한지 확인 후 유효하지 않다면 상태를 변경한다.
+   * @param {string[]} userInput [유저가 제시한 수]
+   */
+  checkUserGuessInputValid(userInput) {
+    const userInputLength = userInput.length;
+    const userInputSet = new Set(userInput);
+
+    if (
+      userInputLength !== 3 ||
+      userInputSet.size !== 3 ||
+      !this.numberRangeRegex.test(+userInput.join(""))
+    ) {
+      this.isUserGuessInputValid = false;
+    }
+  }
 }
 
 module.exports = Validation;
