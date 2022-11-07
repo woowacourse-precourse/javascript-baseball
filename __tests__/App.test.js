26b24a46d09e1d4a02ab98678273c433c7fd5f66
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2022-11-07 20:11:53 +0900

    test (App.test.js): isStart() - 'boolean 타입을 반환한다.'

---
+++ __tests__/App.test.js
@@ -0,0 +1,15 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+const App = require('../src/App');
+
+const { close } = MissionUtils.Console;
+
+afterEach(() => {
+  close();
+});
+
+describe('isStart()', () => {
+  test('boolean 타입을 반환한다.', () => {
+    const app = new App();
+    expect(typeof app.isStart()).toBe('boolean');
+  });
+});
