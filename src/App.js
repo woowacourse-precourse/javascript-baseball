396ba4fab4253e51eb27275dc9a23394fde7c769
mochang2 <qjsrodksro@gmail.com>
2022-11-07 12:06:03 +0900

    feat: 야구 게임의 결과를 반환, 출력하는 메서드 구현

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ class App {
 
   play() {
     this.answer = this.generateAnswer()
+    console.log(this.answer)
     this.getInput()
   }
 
@@ -31,11 +32,57 @@ class App {
       }
 
       const numbers = Array.from(input).map((digit) => parseInt(digit))
-      const gameResult = {}
+      const { strike, ball, finished } = this.determineGameResult(numbers)
 
-      // 게임 끝나지 않았으면 다시 getInput 호출
-      // 게임 끝났으면 1, 2 선택할 수 있는 메서드 호출
+      this.printGameResult({ strike, ball })
+
+      if (!finished) {
+        this.getInput()
+      } else {
+        // 게임 끝났으면 1, 2 선택할 수 있는 메서드 호출
+      }
     })
+  }
+
+  /**
+   * @typedef {Object} baseballResult
+   * @property {number} strike
+   * @property {number} ball
+   * @property {boolean=} finished
+   */
+
+  /**
+   * @param {number[]} numbers - this.ballCount와 길이가 동일한 입력값
+   * @returns {baseballResult}
+   */
+  determineGameResult(numbers) {
+    const strikes = this.answer.filter(
+      (digit, index) => numbers[index] === digit
+    ).length
+    const balls = this.answer.filter(
+      (digit, index) => numbers[index] !== digit && numbers.includes(digit)
+    ).length
+
+    return {
+      strike: strikes,
+      ball: balls,
+      finished: strikes === this.ballCount,
+    }
+  }
+
+  /**
+   * @param {baseballResult} baseballResult
+   */
+  printGameResult({ strike, ball }) {
+    if (strike === 0 && ball === 0) {
+      Console.print('낫싱')
+    } else {
+      const result = `${ball ? `${ball}볼 ` : ' '}${
+        strike ? `${strike}스트라이크` : ''
+      }`
+
+      Console.print(result.trim())
+    }
   }
 
   /**
