64e1313c6b8f00801498471d6405f437a1b0f7f5
dustmddus <dustmddus5@gmail.com>
2022-11-07 23:29:15 +0900

    feat: 컴퓨터, 사용자 숫자 비교 후 strike, ball 갯수 세기

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ const { Console, Random } = require("@woowacourse/mission-utils");
 const Validation = require("./Validation");
 
 class App {
-  userInput() {
+  getUserInput() {
     Console.readLine("숫자를 입력해주세요 : ", (userInput) => {
       const validation = new Validation(userInput);
       if (validation.isValidInput()) {
@@ -11,11 +11,25 @@ class App {
     });
   }
 
+  doGame(computerRandomNumber) {
+    const userNumber = this.getUserInput();
+    let strikeCount = 0;
+    let ballCount = 0;
+    for (let idx = 0; idx < 3; i++) {
+      if (computerRandomNumber[idx] === userNumber[idx]) strikeCount++;
+      else if (userNumber.includes(computerRandomNumber[idx])) {
+        ballCount += 1;
+      }
+    }
+    return { strikeCount, ballCount };
+  }
+
   start() {
     Console.print("숫자 야구 게임을 시작합니다.");
     const computerRandomNumber = Random.pickUniqueNumbersInRange(1, 9, 3).join(
       ""
     );
+    this.doGame(computerRandomNumber);
   }
 
   play() {
