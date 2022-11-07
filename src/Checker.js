29ca8fdbf75e572c78c9c2f4977db7674828ee67
박승훈 <tmdgns1126@naver.com>
2022-11-08 00:51:51 +0900

    feat: [Checker] 스트라이크가 몇 개인지 판단하는 함수 구현

--- src/Checker.js
+++ src/Checker.js
@@ -7,6 +7,18 @@ class Checker {
       Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
       return true;
     }
+
+    const strike = this.checkStrike(targetNum, userNum);
+  }
+
+  checkStrike(targetNum, userNum) {
+    let strike = 0;
+    for (let i = 0; i < 3; i++) {
+      if (targetNum[i] === userNum[i]) {
+        strike++;
+      }
+    }
+    return strike;
   }
 }
 
