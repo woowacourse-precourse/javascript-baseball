bcb3d517b1600f05ea2085ad6befb0e726c60d8d
NaveOWO <pianowell96@gmail.com>
2022-11-07 19:43:56 +0900

    [ feat ] 서로 다른 3개의 숫자 랜덤추출

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
+  constructor(answer) {
+    this.answer = answer;
+  }
+
   startGame() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
   }
 
+  createRandom() {
+    const numbers = [];
+    while (numbers.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!numbers.includes(number)) {
+        numbers.push(number);
+      }
+    }
+    this.answer = numbers;
+  }
+
   play() {
     this.startGame();
+    this.createRandom();
   }
 }
 
