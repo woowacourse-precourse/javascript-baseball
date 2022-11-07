de8fe4621393209fce8ea5d17761a006ffdc4700
oio337a <oio337a@gmail.com>
2022-11-07 17:22:38 +0900

    style : fix new line

--- constant/message.js
+++ constant/message.js
@@ -2,7 +2,7 @@ const GAME_START = "숫자 야구 게임을 시작합니다.";
 const USER_INPUT_REQUEST = "숫자를 입력해 주세요 : ";
 const ERROR = "유효하지 않은 입력값입니다.";
 const GAME_END = "3개의 숫자를 모두 맞히셨습니다! 게임종료";
-const RESTART = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+const RESTART = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n";
 
 module.exports = {
   GAME_START,
