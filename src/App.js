dd0cd86334d3367808435dbf38f78834b39e68c7
chasj0326@naver.com <62418379+chasj0326@users.noreply.github.com>
2022-11-08 03:09:33 +0900

    refactor: App.js - 전체 코드 리팩토링

--- src/App.js
+++ src/App.js
@@ -1,103 +1,115 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
-  // 예외 처리
-  exceptionForUserNumbers(input){
-    const check = /^[0-9]+$/; 
-    if(input.length>3){
-      return true;
-    }
-    if(!check.test(input)){
-      return true;
-    }
-    const test = [...input];
-    if(test.length !== [...new Set(test)].length){
-      return true;
-    }
-  }
-  
-  exceptionForQuitOption(input){
-    if(!(input==='1'||input==='2')){
-      return true;
-    }
+  constructor(){
+    this.NUMBER_LENGTH = 3;
   }
 
-  initBaseballGame(){
-    const NUMBER_LENGTH = 3;
-    const computerNumbers = this.makeComputerNumbers(NUMBER_LENGTH);
-    this.playOneRound(computerNumbers);
+  isVaildPlayer(player){
+    const test = [...player];
+    return (
+      (player.length <= this.NUMBER_LENGTH) &&     // 입력 길이 초과 확인
+      (/^[0-9]+$/.test(player)) &&                 // 숫자로 이루어져 있는지 확인
+      (test.length === [...new Set(test)].length)  // 중복 숫자 있는지 확인
+    );
   }
 
-  makeComputerNumbers(NUMBER_LENGTH){
-    const computerNumbers = new Set([]);
-    while ([...computerNumbers].length < NUMBER_LENGTH){
-      computerNumbers.add(MissionUtils.Random.pickNumberInRange(1,9));
+  isVaildOption(option){
+    return (
+      (option==='1')||(option==='2')
+    )
+  }
+
+  play(){
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    this.initGame();
+  }
+
+  initGame(){
+    let computer = this.makeComputer();
+    this.startASet(computer);
+  }
+
+  makeComputer(){
+    const computer = new Set([]);
+    let number;
+    while([...computer].length < this.NUMBER_LENGTH){
+      number = MissionUtils.Random.pickNumberInRange(1,9);
+      computer.add(String(number));
     }
-    return [...computerNumbers];
+    return [...computer];
   }
 
-  playOneRound(computerNumbers){
-    MissionUtils.Console.readLine("숫자를 입력해 주세요. : ", (userNumbers)=>{
-      if(this.exceptionForUserNumbers(userNumbers)){
-        throw '입력 오류'
-      }
-      const [ball, strike] = this.countBallAndStrike(userNumbers, computerNumbers);
-      this.showResult(ball, strike);
-      if(strike!==3){
-        return this.playOneRound(computerNumbers);
-      }
-      MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-      this.quitOptions();
+  startASet(computer){
+    MissionUtils.Console.readLine("숫자를 입력해 주세요 : ", (player)=>{
+      if(!this.isVaildPlayer(player)) throw 'player error'
+      this.playASet(computer, player);
     })
   }
 
-  countBallAndStrike(userNumbers, computerNumbers){//결과 계산 함수
-    const strike = computerNumbers.reduce((count,computerNumber,index)=>(
-      count+(String(computerNumber)===userNumbers[index])
-    ), 0)
-    const ball = computerNumbers.reduce((count, computerNumber)=>(
-      count+([...userNumbers].includes(String(computerNumber)))
-    ), 0) - strike;
-    return [ball, strike];
+  playASet(computer, player){
+    const strike = this.countStrike(player, computer);
+    const ball = this.countBall(player, computer, strike);
+    this.showResult(ball, strike);
+
+    if(!this.playerWin(strike)){
+      return this.startASet(computer);
+    }
+    this.selectOptions();
   }
 
-  showResult(ball, strike){//결과 출력 함수
-    if (ball + strike ===0){
+  countStrike(player, computer){
+    const strike = computer.reduce((count, computer_number, index)=>(
+      count + (computer_number === player[index])
+    ), 0)
+    return strike;
+  }
+
+  countBall(player, computer, strike){
+    const ball = computer.reduce((ball, computer_number)=>(
+      ball + ([...player].includes(computer_number))
+    ), 0) - strike;
+    return ball;
+  }
+
+  showResult(ball, strike){
+    if (ball + strike === 0){
       MissionUtils.Console.print('낫싱');
       return;
     }
     let resultPrint = '';
-    if(ball){
-      resultPrint += `${ball}볼 `;
+    if (ball){
+      resultPrint += `${ball}볼 `
     }
-    if(strike){
+    if (strike){
       resultPrint += `${strike}스트라이크`
     }
     MissionUtils.Console.print(resultPrint);
   }
 
-  quitOptions(){
-    MissionUtils.Console.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-    MissionUtils.Console.readLine("",(option)=>{
-      if(this.exceptionForQuitOption(option)){
-        throw '종료 옵션 선택 에러'
-      }
-      if(option === '1'){
-        this.initBaseballGame();
-      }
-      else if(option === '2'){
-        MissionUtils.Console.close();
-      }
-    })
+  playerWin(strike){
+    return strike===this.NUMBER_LENGTH;
   }
 
-  play() {
-    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
-    this.initBaseballGame();
+  quitOrRestart(option){
+    if(option === '1'){
+      this.initGame();
+    }
+    else{
+      MissionUtils.Console.close();
+      return;
+    }
   }
-  
+
+  selectOptions(){
+    MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    MissionUtils.Console.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+
+    MissionUtils.Console.readLine("",(option)=>{
+      if(!this.isVaildOption(option)) throw 'option error'
+      this.quitOrRestart(option);
+    });
+  }
 }
 
 module.exports = App;
-const app = new App();
-app.play(); 
