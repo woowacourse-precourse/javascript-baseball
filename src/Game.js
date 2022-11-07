e2ce4d920ed9a76271ba9f966d37e3144f7088da
DAIN KIM <dnkim419@gmail.com>
2022-11-07 20:17:40 +0900

    feat(기능2): 정답인 3자리 수 생성

--- src/Game.js
+++ src/Game.js
@@ -1,10 +1,25 @@
-const { Console } = require('@woowacourse/mission-utils');
+const { Console, Random } = require('@woowacourse/mission-utils');
 
 class Game {
-  constructor() {}
+  constructor() {
+    this.correctNumber;
+  }
 
   play() {
     Console.print('숫자 야구 게임을 시작합니다.');
+    this.correctNumber = this.generateThreeDigitNumber();
+  }
+
+  generateThreeDigitNumber() {
+    const threeDigitNumber = [];
+    while (threeDigitNumber.length < 3) {
+      const number = Random.pickNumberInRange(1, 9);
+      if (!threeDigitNumber.includes(number)) {
+        threeDigitNumber.push(number);
+      }
+    }
+
+    return threeDigitNumber;
   }
 }
 
