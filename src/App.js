d9d712edaefa7f722dc44a2a142d89aaab37d17e
MINJUN PARK <mj9457@naer,com>
2022-11-07 22:10:29 +0900

    사용자로부터 입력받은 수가 유효한지 판별

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ class App {
 
     inputNumber() {
         MissionUtils.Console.readLine("숫자를 입력해주세요 : ",(NUMBER_ENTERED) => {
-            if(NUMBER_ENTERED) {
+            if(this.inputNumberIsValid(NUMBER_ENTERED)) {
                 const number_converted_letter = toString(NUMBER_ENTERED);
                 const letter_converted_number = number_converted_letter.split('').map((letter) => Number(letter))
                 this.gamePlayerNumber.push(letter_converted_number);
@@ -35,8 +35,14 @@ class App {
                 throw "3개의 중복되지않는 숫자를 입력해주세요. 게임 종료";
             }
         })
-    }
+    }   
 
+    inputNumberIsValid(inputNumber) {
+        if(toString(inputNumber).length !==3 || isNaN(inputNumber) || new Set(inputNumber).size !== 3) {
+            return false;
+        }
+        return true;
+    }
 }
 
 module.exports = App;
