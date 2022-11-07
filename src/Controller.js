8ef1d05ce7d66cdc91ae101a14f7d21e88cfef77
hjy0951 <hjy0951@gmail.com>
2022-11-07 19:55:11 +0900

    feat: Controller 클래스 구현

---
+++ src/Controller.js
@@ -0,0 +1,56 @@
+class Controller {
+  isValidInput(input) {
+    return this.isRightLength(input) && this.hasDuplicateNumbers(input);
+  }
+
+  isRightLength(input) {
+    return input.length === 3;
+  }
+
+  hasDuplicateNumbers(input) {
+    return input.filter(function (number, index) {
+      return input.indexOf(number) === index;
+    }).length === 3;
+  }
+
+  compareAnswer(input, answer) {
+    const strikes = this.getStrikes(input, answer);
+    const balls = this.getBalls(input, answer);
+
+    if (this.isFinish(strikes) === true) {
+      return '';
+    }
+
+    const hint = strikes === 0 && balls === 0
+      ? this.makeHint(strikes, balls) : '낫싱';
+    return hint;
+  }
+
+  getStrikes(input, answer) {
+    let count = 0;
+    input.forEach(function (value, index) {
+      if (answer[index] === value) count++;
+    });
+    return count;
+  }
+
+  getBalls(input, answer) {
+    let count = 0;
+    input.forEach(function (value, index) {
+      if (answer[index] !== value && answer.includes(value)) count++;
+    });
+    return count;
+  }
+
+  isFinish(strikes) {
+    return strikes === 3;
+  }
+
+  makeHint(strikes, balls) {
+    const strikeHint = strikes > 0 ? `${strikes}스트라이크 ` : '';
+    const ballHint = balls > 0 ? `${balls}볼` : '';
+    return (strikeHint + ballHint).trim();
+  }
+}
+
+module.exports = Controller;
