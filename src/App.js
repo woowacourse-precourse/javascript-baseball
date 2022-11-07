4508ac67d6597e8dd2ee3c8a8e74653e63ce2bca
leesoomin <soomin@leesoominui-MacBookPro.local>
2022-11-07 21:15:22 +0900

    feat: 입력 예외처리 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
 class App {
   play() {}
+  
+  checkException(num){
+    if(isNaN(num)){
+      throw new Error("숫자를 입력하세요.");
+    }else if(num<0){
+      throw new Error("양수를 입력하세요.");
+    }else if(num.charAt(0) == 0 || num.charAt(1) == 0 || num.charAt(1)){
+      throw new Error("1-9 사이의 숫자로 이루어진 세자리 숫자를 입력하세요.");
+    }else if(num.charAt(0) == " " ||num.charAt(1) == " " || num.charAt(3) == " "){
+      throw new Error("공백을 포함하지 않는 숫자를 입력하세요.")
+    }else if(num.length !== 3){
+      throw new Error("3자리 숫자를 입력하세요.");
+    }else if(num.charAt(0) == num.charAt(1) || num.charAt(0) == num.charAt(2) || num.charAt(1) == num.charAt(2)){
+      throw new Error("중복되지 않는 숫자를 입력하세요.");
+    }
+  }
+
 }
 
 module.exports = App;
