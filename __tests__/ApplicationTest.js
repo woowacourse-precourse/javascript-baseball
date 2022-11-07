17a773b1c04e99e39ad51abac2ba3f2f38a0f3cf
bkdragon0228 <rlaqjarb2067@naver.com>
2022-11-07 12:12:11 +0900

    test : 작성한 테스트케이스 기반으로 실제 진행 테스트 추가
    실제 진행 테스트를 추가했습니다. 예외사항과 나올 수 있는 멘트(결과)를
    확인했습니다.

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -46,9 +46,91 @@ describe('숫자 야구 게임', () => {
             );
         });
     });
+    test('게임 종료 후 종료', () => {
+        const randoms = [1, 3, 5];
+        const answers = ['246', '513', '135', '2'];
+        const logSpy = getLogSpy();
+        const messages = ['낫싱', '3스트라이크', '3볼', '게임 종료'];
+        mockRandoms(randoms);
+        mockQuestions(answers);
+        const app = new App();
+        app.play();
+        messages.forEach((output) => {
+            expect(logSpy).toHaveBeenCalledWith(
+                expect.stringContaining(output)
+            );
+        });
+    });
+    test('나올 수 있는 모든 경우 확인', () => {
+        const randoms = [1, 3, 5];
+        const answers = [
+            '246',
+            '917',
+            '813',
+            '351',
+            '815',
+            '153',
+            '189',
+            '139',
+            '135',
+            '2',
+        ];
+        const logSpy = getLogSpy();
+        const messages = [
+            '낫싱',
+            '1볼',
+            '2볼',
+            '3볼',
+            '1볼 1스트라이크',
+            '2볼 1스트라이크',
+            '1스트라이크',
+            '2스트라이크',
+            '3스트라이크',
+            '게임 종료',
+        ];
+        mockRandoms(randoms);
+        mockQuestions(answers);
+        const app = new App();
+        app.play();
+        messages.forEach((output) => {
+            expect(logSpy).toHaveBeenCalledWith(
+                expect.stringContaining(output)
+            );
+        });
+    });
     test('예외 테스트', () => {
         const randoms = [1, 3, 5];
         const answers = ['1234'];
+        mockRandoms(randoms);
+        mockQuestions(answers);
+        expect(() => {
+            const app = new App();
+            app.play();
+        }).toThrow();
+    });
+    test('예외 테스트2', () => {
+        const randoms = [1, 3, 5];
+        const answers = ['1123'];
+        mockRandoms(randoms);
+        mockQuestions(answers);
+        expect(() => {
+            const app = new App();
+            app.play();
+        }).toThrow();
+    });
+    test('예외 테스트3', () => {
+        const randoms = [1, 3, 5];
+        const answers = ['240'];
+        mockRandoms(randoms);
+        mockQuestions(answers);
+        expect(() => {
+            const app = new App();
+            app.play();
+        }).toThrow();
+    });
+    test('예외 테스트4', () => {
+        const randoms = [1, 3, 5];
+        const answers = ['abc'];
         mockRandoms(randoms);
         mockQuestions(answers);
         expect(() => {
@@ -61,42 +143,42 @@ describe('숫자 야구 게임', () => {
 describe('Test', () => {
     test('입력 받은 수가 세자리 수가 아닌 경우(2자리)', () => {
         expect(() => {
-            const exception = new Exception('12');
+            const exception = new Exception('12', 'process');
             exception.checkInputException();
         }).toThrow();
     });
 
     test('입력 받은 수가 세자리 수가 아닌 경우(4자리)', () => {
         expect(() => {
-            const exception = new Exception('1234');
+            const exception = new Exception('1234', 'process');
             exception.checkInputException();
         }).toThrow();
     });
 
     test('입력 받은 수가 세자리 수가 아닌 경우(5자리)', () => {
         expect(() => {
-            const exception = new Exception('12345');
+            const exception = new Exception('12345', 'process');
             exception.checkInputException();
         }).toThrow();
     });
 
     test('입력 받은 수가 서로 다른 수가 아닌 경우', () => {
         expect(() => {
-            const exception = new Exception('112');
+            const exception = new Exception('112', 'process');
             exception.checkInputException();
         }).toThrow();
     });
 
     test('입력 받은 수에 0이 포함된 경우', () => {
         expect(() => {
-            const exception = new Exception('120');
+            const exception = new Exception('120', 'process');
             exception.checkInputException();
         }).toThrow();
     });
 
     test('재시작 여부 확인 시 1또는 2이외의 값이 입력될 경우', () => {
         expect(() => {
-            const exception = new Exception('3');
+            const exception = new Exception('3', 'replay');
             exception.checkReplayInputException();
         }).toThrow();
     });
