c824687dde6378a47514c8124e282fc6d744c82e
minjun06 <35772631+minjun06@users.noreply.github.com>
2022-11-08 05:44:40 +0900

    Start Game Message

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-
+const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
-  play() {}
+  play() {
+    
+  }
+
+  constructor() {
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+  }
 }
+
+const app = new App();
+app.play
 
 module.exports = App;
