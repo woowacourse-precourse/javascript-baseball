cd0b6bbb0934070fb3a08134c379cd3a52ca14a8
khj0426 <706shin1728@naver.com>
2022-11-07 17:36:51 +0900

    refactor:볼,스트라이크,낫싱 메시지 상수화

--- src/MESSAGE.js
+++ src/MESSAGE.js
@@ -3,5 +3,8 @@ const MESSAGE = {
   SUCCESS: '3개의 숫자를 모두 맞히셨습니다!',
   END: '게임 종료',
   INPUTERROR: '잘못된 입력값입니다.다시 입력해주세요!',
+  BALL: '볼',
+  STRIKE: '스트라이크',
+  NOTHING: '낫싱',
 };
 module.exports = MESSAGE;
