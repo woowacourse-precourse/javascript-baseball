d76afe759d5f7816287e28eaa8aa65dfe2d01c14
ming0830 <minsun0830@naver.com>
2022-11-08 04:25:35 +0900

    feat : 스트라이크, 볼의 개수를 세주는 기능 구현

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ const MissionUtils = require("@woowacourse/mission-utils");
 
 class App{
   constructor(){
-    this.uniqueNumberList = [];
+    this.uniqueNumberList =[];
   }
 
   
@@ -13,6 +13,7 @@ class App{
       randomNumber.add(MissionUtils.Random.pickNumberInRange(1,9));
     }
     const uniqueNumberList = [...randomNumber]
+    this.uniqueNumberList = uniqueNumberList;
     return uniqueNumberList
   }
   
@@ -20,27 +21,43 @@ class App{
   userInput(){
     MissionUtils.Console.readLine('숫자를 입력해주세요 : ',(userInputValue)=>{
       this.isVaildUserInput(userInputValue);
-      const InputNumber = userInputValue.split('').map((v)=>parseInt(v))
-      
+      const InputNumber = userInputValue.split('').map((v)=>parseInt(v));
+      this.getHintCount(InputNumber);
     });
   }
   
-  
-  isVaildUserInput(userInputValue){
-    if(userInputValue.length <= 0 || userInputValue.length >3){
+  isVaildUserInput(number){
+    if(number.length !== 3){
       throw new Error("숫자 세자리 수를 입력하세요.");
     }
-    const uniqueUserNum = new Set(userInputValue);
-    if(uniqueUserNum.size !== userInputValue.length){
+    const uniqueUserNum = new Set(number);
+    if(uniqueUserNum.size !== number.length){
       throw new Error("중복되지 않는 숫자를 입력하세요.");
     }
   }
 
+  getHintCount(userInput){
+    let COUNT_STRIKE = 0;
+    let COUNT_DUPLICATION= 0;
+    let COUNT_BALL = 0;
 
+    if(this.uniqueNumberList === userInput) COUNT_STRIKE = 3;
+    
+    
+    for(let index = 0; index<this.uniqueNumberList.length;index++){
+      if(this.uniqueNumberList[index] === userInput[index]) COUNT_STRIKE++
+      if(userInput.includes(this.uniqueNumberList[index])) COUNT_DUPLICATION++
+    }
+    COUNT_BALL = COUNT_DUPLICATION - COUNT_STRIKE ;
+    // MissionUtils.Console.print(COUNT_DUPLICATION);
+    // MissionUtils.Console.print(COUNT_STRIKE);
+    // MissionUtils.Console.print(COUNT_BALL);
+  }
 
   play(){
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
     this.getRandumNumber();
+    MissionUtils.Console.print(this.uniqueNumberList);
     this.userInput();
   
   }
