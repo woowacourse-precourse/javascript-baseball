ea99e263f66a0b2ef4f77440c12de5382728281d
piporica <49173614+piporica@users.noreply.github.com>
2022-11-08 02:35:56 +0900

    feat: Make user reply checker
    사용자 입력에 대한 반응을 return하는 함수
    filter을 이용하여 숫자를 센다
    answer을 전역변수로 두지 않게 하기 위해
    cycle 과 check함수 인수에 answer가 추가되었다

--- src/NumberBaseball.js
+++ src/NumberBaseball.js
@@ -1,19 +1,29 @@
-const MissionUtils = require('@woowacourse/mission-utils');
+const MissionUtils = require("@woowacourse/mission-utils");
 
 class NumberBaseball {
   async gameStart() {
-    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
 
-    let answer = this.makeAnswer();
-    console.log(answer);
+    const answer = this.makeAnswer();
+    console.log(`정답${answer}`);
 
-    this.tryCycle();
+    this.tryCycle(answer);
     // close는 게임 끝났을 때 넣어야 함 - js 비동기 조심할 것
   }
 
-  tryCycle() {
+  tryCycle(answer) {
     this.getUserReply((reply) => {
-      console.log(reply);
+      const tryResult = this.checkUserReply(reply, answer);
+      if (tryResult.ball === 0 && tryResult.strike === 0) {
+        MissionUtils.Console.print("낫싱");
+      } else {
+        MissionUtils.Console.print(
+          `${tryResult.ball}볼 ${tryResult.strike}스트라이크`
+        );
+      }
+      if (tryResult.strike === 3) {
+        console.log("정답처리 해야 할 곳");
+      }
     });
   }
 
@@ -23,10 +33,19 @@ class NumberBaseball {
   }
 
   getUserReply(callbackFunc) {
-    MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (answer) => {
+    MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (reply) => {
       // 오류입력 체크 들어가야 할 부분
-      callbackFunc([...answer].map((x) => Number(x)));
+      callbackFunc([...reply].map((x) => Number(x)));
     });
+  }
+
+  checkUserReply(reply, answer) {
+    const BALL_COUNT = reply.filter(
+      (x, index) => answer.includes(x) && answer[index] !== x
+    ).length;
+    const STRIKE_COUNT = reply.filter((x, index) => x === answer[index]).length;
+
+    return { ball: BALL_COUNT, strike: STRIKE_COUNT };
   }
 }
 module.exports = NumberBaseball;
