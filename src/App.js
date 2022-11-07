e6d14610cebdfec888fad4ed22b6409e9cb89637
afds4567 <afds4567@gmail.com>
2022-11-07 17:22:18 +0900

    feat(createAnswer): 컴퓨터 정답 생성 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
-class App {
-  play() {}
+const MissionUtils = require('@woowacourse/mission-utils');
+
+function createAnswer() {
+  const computer = [];
+  while (computer.length < 3) {
+    const number = MissionUtils.Random.pickNumberInRange(1, 9);
+    if (!computer.includes(number)) {
+      computer.push(number);
+    }
+  }
+  return computer.join('');
 }
+class App {
+  play() {
+    this.answer = createAnswer();
+  }
+}
+const app = new App();
+app.play();
 
 module.exports = App;
