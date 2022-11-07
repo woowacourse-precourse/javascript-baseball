8e745eb9c76a9b5c942f6d2a7f4f336759eb90f5
NaveOWO <pianowell96@gmail.com>
2022-11-07 20:07:54 +0900

    [ feat ] 게임재개 여부를 묻는 문구 출력후, 사용자가 값을 입력할 때까지 대기

--- src/App.js
+++ src/App.js
@@ -73,7 +73,14 @@ class App {
 
   printGameOver() {
     MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    this.printAskRetry();
     this.isDone = true;
+  }
+
+  printAskRetry() {
+    MissionUtils.Console.print(
+      "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
+    );
   }
 
   play() {
