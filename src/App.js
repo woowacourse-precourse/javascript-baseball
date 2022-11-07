82d545ba89d1dcd1f24efdfeebadde431b972948
inthhh <glass005533@gmail.com>
2022-11-07 15:40:00 +0900

    feat: gameStart Method

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const Console = MissionUtils.Console;
+const Random = MissionUtils.Random;
+
 class App {
-  play() {}
+  play() {
+    Console.print("숫자 야구 게임을 시작합니다.");
+    gameStart();
+    return 0;
+  }
+}
+
+function gameStart(){
+  
 }
 
 module.exports = App;
