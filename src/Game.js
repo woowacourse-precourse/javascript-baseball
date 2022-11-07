ac4fbb4876782551585a74ac67fa9a5f6a043729
Suhyeon Lee <suhyeonlee.dev@gmail.com>
2022-11-07 23:05:07 +0900

    feat: 게임 재시작 및 종료 여부를 묻는 문구를 출력하는 기능 구현

--- src/Game.js
+++ src/Game.js
@@ -17,6 +17,14 @@ class Game {
         break;
       }
     }
+    this.askRestart();
+  }
+
+  askRestart() {
+    Console.readLine(
+      '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n',
+      (userInput) => {}
+    );
   }
 }
 
