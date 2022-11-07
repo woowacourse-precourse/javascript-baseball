9adab87bbf234a73427b35744bd3bb4a204b63c4
unknown <chn0202@gmail.com>
2022-11-08 01:26:24 +0900

    Test: getComputerNumber 메서드명 변경

--- __tests__/ComputerNumberTest.js
+++ __tests__/ComputerNumberTest.js
@@ -3,7 +3,7 @@ const App = require("../src/App");
 describe("computerNumber() 테스트", () => {
   test("컴퓨터 숫자 조건에 맞는지", () => {
     const app = new App();
-    const computerNumber = app.computerNumber();
+    const computerNumber = app.getComputerNumber();
 
     expect(() => app.checkNumber(computerNumber.join(""))).not.toThrow();
   });
