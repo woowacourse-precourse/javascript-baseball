d826fad88c1ea8ec1ebb696219a6858bc3c63011
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2022-11-07 15:43:21 +0900

    refactor: 문자열에 작은 따옴표 사용하도록 수정

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -1,5 +1,5 @@
-const App = require("../src/App");
-const MissionUtils = require("@woowacourse/mission-utils");
+const App = require('../src/App');
+const MissionUtils = require('@woowacourse/mission-utils');
 
 const mockQuestions = (answers) => {
   MissionUtils.Console.readLine = jest.fn();
@@ -18,22 +18,22 @@ const mockRandoms = (numbers) => {
 };
 
 const getLogSpy = () => {
-  const logSpy = jest.spyOn(MissionUtils.Console, "print");
+  const logSpy = jest.spyOn(MissionUtils.Console, 'print');
   logSpy.mockClear();
   return logSpy;
 };
 
-describe("숫자 야구 게임", () => {
-  test("게임 종료 후 재시작", () => {
+describe('숫자 야구 게임', () => {
+  test('게임 종료 후 재시작', () => {
     const randoms = [1, 3, 5, 5, 8, 9];
-    const answers = ["246", "135", "1", "597", "589", "2"];
+    const answers = ['246', '135', '1', '597', '589', '2'];
     const logSpy = getLogSpy();
     const messages = [
-      "낫싱",
-      "3스트라이크",
-      "1볼 1스트라이크",
-      "3스트라이크",
-      "게임 종료",
+      '낫싱',
+      '3스트라이크',
+      '1볼 1스트라이크',
+      '3스트라이크',
+      '게임 종료',
     ];
 
     mockRandoms(randoms);
@@ -47,18 +47,18 @@ describe("숫자 야구 게임", () => {
     });
   });
 
-  test("게임 종료 후 재시작: 볼과 스트라이크도 잘 출력되는지 확인", () => {
+  test('게임 종료 후 재시작: 볼과 스트라이크도 잘 출력되는지 확인', () => {
     const randoms = [1, 3, 5, 5, 8, 9];
-    const answers = ["246", "132", "135", "1", "597", "892", "589", "2"];
+    const answers = ['246', '132', '135', '1', '597', '892', '589', '2'];
     const logSpy = getLogSpy();
     const messages = [
-      "낫싱",
-      "2스트라이크",
-      "3스트라이크",
-      "1볼 1스트라이크",
-      "2볼",
-      "3스트라이크",
-      "게임 종료",
+      '낫싱',
+      '2스트라이크',
+      '3스트라이크',
+      '1볼 1스트라이크',
+      '2볼',
+      '3스트라이크',
+      '게임 종료',
     ];
 
     mockRandoms(randoms);
@@ -72,16 +72,16 @@ describe("숫자 야구 게임", () => {
     });
   });
 
-  test("첫 게임 시작 후 안내문구와 힌트값이 잘 출력되는지 확인", () => {
+  test('첫 게임 시작 후 안내문구와 힌트값이 잘 출력되는지 확인', () => {
     const randoms = [4, 5, 9];
-    const answers = ["468", "287", "459"];
+    const answers = ['468', '287', '459'];
     const logSpy = getLogSpy();
     const messages = [
-      "숫자 야구 게임을 시작합니다.",
-      "1스트라이크",
-      "낫싱",
-      "3스트라이크",
-      "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
+      '숫자 야구 게임을 시작합니다.',
+      '1스트라이크',
+      '낫싱',
+      '3스트라이크',
+      '3개의 숫자를 모두 맞히셨습니다! 게임 종료',
     ];
 
     mockRandoms(randoms);
@@ -95,9 +95,9 @@ describe("숫자 야구 게임", () => {
     });
   });
 
-  test("예외 테스트: 입력값의 길이가 3이 아닌 경우", () => {
+  test('예외 테스트: 입력값의 길이가 3이 아닌 경우', () => {
     const randoms = [1, 3, 5];
-    const answers = ["1234"];
+    const answers = ['1234'];
 
     mockRandoms(randoms);
     mockQuestions(answers);
@@ -105,12 +105,12 @@ describe("숫자 야구 게임", () => {
     expect(() => {
       const app = new App();
       app.play();
-    }).toThrow("3개의 숫자만 입력하세요.");
+    }).toThrow('3개의 숫자만 입력하세요.');
   });
 
-  test("예외 테스트: 입력값에 서로 동일한 숫자가 포함된 경우", () => {
+  test('예외 테스트: 입력값에 서로 동일한 숫자가 포함된 경우', () => {
     const randoms = [1, 3, 5];
-    const answers = ["122"];
+    const answers = ['122'];
 
     mockRandoms(randoms);
     mockQuestions(answers);
@@ -118,12 +118,12 @@ describe("숫자 야구 게임", () => {
     expect(() => {
       const app = new App();
       app.play();
-    }).toThrow("서로 다른 숫자를 입력하세요.");
+    }).toThrow('서로 다른 숫자를 입력하세요.');
   });
 
-  test("예외 테스트: 게임 종료 후 1,2 이외의 값 입력한 경우", () => {
+  test('예외 테스트: 게임 종료 후 1,2 이외의 값 입력한 경우', () => {
     const randoms = [1, 3, 5];
-    const answers = ["135", "4"];
+    const answers = ['135', '4'];
 
     mockRandoms(randoms);
     mockQuestions(answers);
@@ -131,12 +131,12 @@ describe("숫자 야구 게임", () => {
     expect(() => {
       const app = new App();
       app.play();
-    }).toThrow("1 또는 2만 입력해주세요.");
+    }).toThrow('1 또는 2만 입력해주세요.');
   });
 
-  test("예외 테스트: 입력값에 숫자 이외의 값 포함된 경우", () => {
+  test('예외 테스트: 입력값에 숫자 이외의 값 포함된 경우', () => {
     const randoms = [1, 3, 5];
-    const answers = ["안12"];
+    const answers = ['안12'];
 
     mockRandoms(randoms);
     mockQuestions(answers);
@@ -144,12 +144,12 @@ describe("숫자 야구 게임", () => {
     expect(() => {
       const app = new App();
       app.play();
-    }).toThrow("문자를 제외한 숫자만 입력하세요.");
+    }).toThrow('문자를 제외한 숫자만 입력하세요.');
   });
 
-  test("예외 테스트: 입력값에 0이 포함된 경우", () => {
+  test('예외 테스트: 입력값에 0이 포함된 경우', () => {
     const randoms = [1, 3, 5];
-    const answers = ["102"];
+    const answers = ['102'];
 
     mockRandoms(randoms);
     mockQuestions(answers);
@@ -157,6 +157,6 @@ describe("숫자 야구 게임", () => {
     expect(() => {
       const app = new App();
       app.play();
-    }).toThrow("1~9 사이의 숫자만 입력하세요.");
+    }).toThrow('1~9 사이의 숫자만 입력하세요.');
   });
 });
