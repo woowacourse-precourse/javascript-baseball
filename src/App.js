6a2f10900a7d42617e62b00b0d6102b99e897a05
hafnium1923 <hafnium1923@gmail.com>
2022-11-07 22:16:06 +0900

    feat checkNum
    입력값 비교 기능

--- src/App.js
+++ src/App.js
@@ -5,35 +5,59 @@ class App {
   play() {
 
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
-    const CONTINUE =  1; 
-    while(CONTINUE === 1) {
-      CONTINUE = this.startGame();
-      if(CONTINUE != 0) {
-        MissionUtils.Console.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.', (CONTINUE) => {
-        this.CONTINUE = CONTINUE;
+    let continueGame = this.startGame();
+    if(continueGame != 0) {
+       MissionUtils.Console.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.', (reStart) => {
+        console.log('12312');
+        continueGame = reStart;
         MissionUtils.Console.close();
-      })
-    }
+    })
     }
   }
+  
   startGame() {
     const RAMDOM = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
+    console.log(RAMDOM);
+    let user;
     MissionUtils.Console.readLine('숫자를 입력해주세요', (user_num) => {
-      const FLAG = this.checkNum(RAMDOM,user_num)
-     
-      if(FLAG === -1) {
+      user = user_num;
+      let flag = this.checkNum(RAMDOM,user);
+      if(flag === -1) {
         return 0;
       }
-      if(FLAG) {
-        MissionUtils.Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
-        MissionUtils.Console.close();
+      else if(flag === 0){
+        return 1;
       }
+      else if(flag) {
+        MissionUtils.Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+        return 1;
+      }
+      MissionUtils.Console.close();
     })
-    return 1;
+    
   }
   checkNum(RANDOM,num) {
-
+    let bucketNum = [];
+    for(let i = 2; i >= 0; i--){
+      bucketNum[i] = num % 10;
+      num = parseInt(num /10); 
+    }
+    let strike = [];
+    let j = -1;
+    console.log(bucketNum);
+    for(let i = 0; i < bucketNum.length; i++){
+      if(bucketNum[i] === RANDOM[i]){
+        strike[++j] = bucketNum[i];
+      }
+    }
+    console.log(strike);
+    let ball = RANDOM.filter(x => bucketNum.includes(x)).filter(x => !strike.includes(x));
+    this.print(strike.length, ball.length);
+    if(strike.length === 3)
+      return 1;
+    return 0;
   }
+  
   
 }
 const app = new App();
