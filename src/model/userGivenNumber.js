7b6ec95cd7f1cdf80bfc508a02feb4595f48510c
sh981013s <sh981013s@gmail.com>
2022-11-07 19:02:31 +0900

    feat: 유저가 제시한 수 상태 관련 getter/setter 구현

--- src/model/userGivenNumber.js
+++ src/model/userGivenNumber.js
@@ -3,6 +3,22 @@ class UserGivenNumber {
   constructor() {
     this.number = null;
   }
+
+  /**
+   * 유저가 제시한 숫자를 return 한다.
+   * @return {string[]} [컴퓨터가 설정한 숫자를 담은 배열]
+   */
+  getState() {
+    return this.number;
+  }
+
+  /**
+   * 유저가가 설정한 숫자 상태를 변경한다.
+   * @param {string[]} newState [변경할 새로운 값]
+   */
+  setState(newState) {
+    this.number = newState;
+  }
 }
 
 module.exports = UserGivenNumber;
