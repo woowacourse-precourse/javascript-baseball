95939a9ed16165ae6949fc120af1cd5619dec4b8
yjh1008 <exesea12@gmail.com>
2022-11-08 02:00:19 +0900

    feat : - 생성자로 컴퓨터의 값과 유저의 값, strike 갯수와 ball 갯수를 저장.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const { Console, Random } = MissionUtils;
 class App {
+  constructor() {
+    this.enemy = [];
+    this.userInputArr = [];
+    this.strikes = 0;
+    this.balls = 0;
+  }
   play() {}
 }
 
+const app = new App();
+app.play();
 module.exports = App;
