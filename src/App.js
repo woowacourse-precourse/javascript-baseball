e72f9e668773902ab8c985d33668b6b3a9e1a691
Narcoker <jykim9868@gmail.com>
2022-11-07 15:28:18 +0900

    Feat: Add newGame()

--- src/App.js
+++ src/App.js
@@ -70,7 +70,7 @@ class App {
     MissionUtils.Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
     MissionUtils.Console.readLine("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.", (input) => {
       if (input === "1") { // 재시작
-        this.play();
+        this.newGame();
       }
       if (input === "2") { // 종료
         MissionUtils.Console.close();
@@ -89,12 +89,15 @@ class App {
     });
   }
 
-
-  play() {
-    this.printStart();
+  newGame() {
     let answer = this.makeAnswer();
     console.log(answer);
     this.guessAnswer(answer)
+  }
+
+  play() {
+    this.printStart();
+    this.newGame();
   }
 }
 
