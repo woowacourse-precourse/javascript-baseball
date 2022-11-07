7a0e4d141c4a471ad8cb311b4cd8ecc3463fbc72
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2022-11-08 02:43:02 +0900

    refactor(Render): 불필요한 함수 replayQnA제거

--- src/Render.js
+++ src/Render.js
@@ -1,50 +1,30 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 const { GAME } = require("./data/Constants");
-const { ERROR } = require("./data/Constants");
+
 class Render {
-  constructor({ errorResult, errorRetryResult }) {
-    this.errorResult = errorResult;
-    this.errorRetryResult = errorRetryResult;
-  }
+  constructor() {}
 
   startment() {
     MissionUtils.Console.print(GAME.START_MENTION);
   }
 
-  getUser() {
-    return new Promise((resolve, reject) => {
-      MissionUtils.Console.readLine(GAME.START_GETNUMBER, (number) => {
-        resolve(number);
-      });
-    });
-  }
-
   errorThrow(errorMessege) {
+    MissionUtils.Console.close();
     throw new Error(errorMessege);
   }
   errorRetryThrow(errorMessege) {
+    MissionUtils.Console.close();
     throw new Error(errorMessege);
   }
 
   result(ballCount, strikeCount) {
     if (strikeCount === 3) {
       MissionUtils.Console.print(GAME.GAME_THREE_STRIKE);
-      MissionUtils.Console.print(GAME.GAME_END_MENTION);
-    }
-
-    if (ballCount === 0 && strikeCount === 0) {
+    } else if (strikeCount === 0 && ballCount === 0) {
       MissionUtils.Console.print(GAME.GAME_NOTHING);
-    } else {
+    } else if (strikeCount !== 0 && ballCount !== 0) {
       MissionUtils.Console.print(`${ballCount}볼 ${strikeCount}스트라이크`);
     }
-  }
-
-  replayQnA() {
-    return new Promise((resolve, reject) => {
-      MissionUtils.Console.readLine(GAME.END_RETRY_MENTION, (number) => {
-        resolve(number);
-      });
-    });
   }
 
   end() {
