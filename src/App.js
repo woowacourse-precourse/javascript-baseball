832e6aa5240cc34defc95ebebe1d9fc11b379735
letsjo <hyunoh.jo@gmail.com>
2022-11-08 02:04:39 +0900

    feat(App): 기능7 재시작 또는 종료 메소드 추가

--- src/App.js
+++ src/App.js
@@ -28,23 +28,52 @@ class App {
     };
   }
 
+  setRandomDigit() {
+    const randomDigit = new Set();
+    while (randomDigit.size < INPUT_LENGTH) {
+      randomDigit.add(Random.pickNumberInRange(START_DIGIT, END_DIGIT));
+    }
+    return Array.from(randomDigit);
+  }
+
+  setUserInput(randomDigit) {
+    Console.readLine('숫자를 입력해주세요 : ', inputDigit => {
+      const userDigit = [...this.isDigitValidation(inputDigit)].map(Number);
+      const baseBallBoard = this.calcBaseBallDigit(userDigit, randomDigit);
+      this.isThreeStrike(baseBallBoard)
+        ? this.getRestartInput()
+        : this.setUserInput(randomDigit);
+    });
+  }
+
+  getRestartInput() {
+    Console.print('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.');
+    Console.readLine('\n', isRestart => {
+      if (isRestart === '1') return this.gameStart();
+      else if (isRestart === '2') return this.gameEnd();
+      else throw new Error('잘못된 값 입력됨');
+    });
+  }
+
   isThreeStrike(baseBallBoard) {
     const { strike, ball } = baseBallBoard;
     if (strike || ball) {
       Console.print(
         (ball ? `${ball}볼 ` : ``) + (strike ? `${strike}스트라이크` : ``),
       );
-    } else Console.print('낫싱');
+    } else {
+      Console.print('낫싱');
+    }
     return strike === INPUT_LENGTH;
   }
 
-  calcBaseBallDigit(inputDigit, randomDigit) {
+  calcBaseBallDigit(userDigit, randomDigit) {
     const { isStrike, isBall } = this.#collectCalculatorFn;
     const baseBallBoard = {
       strike: 0,
       ball: 0,
     };
-    inputDigit.forEach((digit, idx) => {
+    userDigit.forEach((digit, idx) => {
       if (isBall(randomDigit, digit, idx)) baseBallBoard.ball++;
       else if (isStrike(randomDigit, digit, idx)) baseBallBoard.strike++;
     });
@@ -63,28 +92,17 @@ class App {
     return inputDigit;
   }
 
-  setUserInput(randomDigit) {
-    Console.readLine('숫자를 입력해주세요 : ', inputDigit => {
-      const userDigit = [...this.isDigitValidation(inputDigit)].map(Number);
-      const baseBallBoard = this.calcBaseBallDigit(userDigit, randomDigit);
-      this.isThreeStrike(baseBallBoard, randomDigit);
-    });
-  }
-
-  setRandomDigit() {
-    const randomDigit = new Set();
-    while (randomDigit.size < INPUT_LENGTH) {
-      randomDigit.add(Random.pickNumberInRange(START_DIGIT, END_DIGIT));
-    }
-    return Array.from(randomDigit);
-  }
-
   showStartMessage() {
     Console.print('숫자 야구 게임을 시작합니다.');
   }
 
   gameStart() {
     this.setUserInput(this.setRandomDigit());
+  }
+
+  gameEnd() {
+    Console.print('게임 종료');
+    Console.close();
   }
 
   play() {
