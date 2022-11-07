4b5c791e9b4d1af7a42d18401ebff3a6674fdd43
sein <hyuntpdls@gmail.com>
2022-11-08 01:40:41 +0900

    feat: 사용자 입력 값에 대한 예외처리 구현

--- src/App.js
+++ src/App.js
@@ -27,10 +27,27 @@ class App {
 
   getUserNumber() {
     MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (answer) => {
+      this.exceptionAnwser(answer);
       this.answerNum = answer;
       MissionUtils.Console.print(`숫자를 입력해주세요 : ${this.answerNum}`);
     });
   }
+
+  exceptionAnwser(answer) {
+    const userNumber = String(answer).split('').map(Number);
+
+    if (userNumber.length !== 3) throw new Error('세자리 숫자만 입력해주세요.');
+
+    const checkDuplication = new Set(userNumber);
+    if (userNumber.length !== checkDuplication.size) throw new Error('중복된 숫자가 있습니다.');
+
+    for (let strNum = 0; strNum < userNumber.length; strNum++) {
+      if (Number(userNumber[strNum]) === 0) throw new Error('1~9 범위의 숫자만 입력해주세요.');
+    }
+
+    return answer;
+  }
+
 }
 
 module.exports = App;
