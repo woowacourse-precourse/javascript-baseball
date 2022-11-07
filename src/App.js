83d8af5609da4518b710c02aef7415f2495c1cd4
mochang2 <qjsrodksro@gmail.com>
2022-11-07 10:32:12 +0900

    feat: 프로그램 시작과 종료에 대한 메서드 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+const { Random, Console } = require('@woowacourse/mission-utils')
+
 class App {
+  /**
+   * @param {number=} from - 숫자를 선택할 시작 범위
+   * @param {number=} to - 숫자를 선택할 끝 범위
+   * @param {number=} ballCount - 야구 게임에서 정답으로 사용할 숫자의 개수
+   */
+  constructor(from = 1, to = 9, ballCount = 3) {
+    this.from = from
+    this.to = to
+    this.ballCount = ballCount
+    this.answer = []
+
+    Console.print('숫자 야구 게임을 시작합니다.')
+  }
+
   play() {}
+
+  close() {
+    Console.close()
+  }
 }
 
-module.exports = App;
+const app = new App()
+app.play()
+
+module.exports = App
