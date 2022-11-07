fb0d33959e33ed39149f44fb9dd21a27f9be35c2
RosaDamascena <hsj990604@gmail.com>
2022-11-07 14:49:47 +0900

    feat : 게임 플레이어에게 숫자를 입력받는 기능 구현

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ class App {
   play() {
     const random_num = MissionUtils.Random.pickUniqueNumbersInRange([1, 9, 3]);
 
-    MissionUtils.console.print("숫자 야구 게임을 시작합니다.");
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+
+    let user_num;
+    MissionUtils.Cosole.readLine("숫자를 입력해주세요 :", (number) => {
+      user_num = number;
+      console.log(user_num);
+    });
   }
 }
 
