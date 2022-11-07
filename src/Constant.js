3c47cc0fd3a11a4f8bd11a0d2985dbce3498da34
ilgon0110 <moses011010@gmail.com>
2022-11-07 20:54:41 +0900

    refactor: constants module 생성

---
+++ src/Constant.js
@@ -0,0 +1,10 @@
+const MESSAGE = {
+  START: '숫자 야구 게임을 시작합니다.',
+  USERINPUT: '숫자를 입력해주세요 : ',
+  ERROR: '잘못된 입력입니다.',
+  GAMEEND: '3개의 숫자를 모두 맞히셨습니다! 게임 종료',
+  ASKTORESTART: '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.',
+};
+const RESTART = '1';
+const EXIT = '2';
+module.exports = { MESSAGE, RESTART, EXIT };
