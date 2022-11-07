237f31289eccada1238bf64a944f7812efc2f97c
JSH99 <tjgus9966@naver.com>
2022-11-07 21:33:51 +0900

    refactor(restartOrQuitGame): 출력 문구 줄바꿈 추가

--- src/App.js
+++ src/App.js
@@ -99,7 +99,7 @@ const playGame = (computerNumArr) => {
 
 const restartOrQuitGame = () => {
   MissionUtils.Console.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.', (endOption) => {
-    MissionUtils.Console.print(`게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.${endOption}`);
+    MissionUtils.Console.print(`게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n${endOption}`);
 
     if (!isVaildEndOption(endOption)) {
       throw new Error('잘못된 값입니다. 1 또는 2를 입력해주세요.');
