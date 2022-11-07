300300f53018567327c57076ce893c0e1ad5d108
Hwang Juhee <julia0276@naver.com>
2022-11-08 01:42:21 +0900

    :sparkles: feat (initialGameSettings) : 게임 초기 세팅 함수를 구현한다

--- src/App.js
+++ src/App.js
@@ -1,5 +1,42 @@
+const { Console, Random } = require('@woowacourse/mission-utils');
+const { GAME_ANNOUNCEMENT_MESSAGE } = require('./constants.js');
+
+class Score {
+  constructor(ball, strike){
+    this.ball = ball;
+    this.strike = strike;
+  }
+  static makeScoreZero(){
+    return new Score(0, 0);
+  }
+};
+
+function initialGameSettings () {
+  Console.print(GAME_ANNOUNCEMENT_MESSAGE.GAME_START);
+  const answer = Random.pickUniqueNumbersInRange(1, 9, 3);
+  let userScore = Score.makeScoreZero();
+  return [answer,userScore];
+}
+
+function getUserInput() {
+}
+
+function checkInput () {
+}
+
+function compareNumber () {
+}
+
+function getBaseballGameResult () {
+}
+
+function checkRestart () {
+}
+
 class App {
-  play() {}
+  play() {
+    initialGameSettings();
+  }
 }
 
 module.exports = App;
---
+++ src/constants.js
@@ -0,0 +1,11 @@
+
+const GAME_ANNOUNCEMENT_MESSAGE = {
+  GAME_START : '숫자 야구 게임을 시작합니다.',
+  INPUT : '숫자를 입력해주세요.',
+  GAME_OVER : '3개의 숫자를 모두 맞히셨습니다! 게임 종료',
+  REPLAY : '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.',
+}
+
+module.exports = {
+  GAME_ANNOUNCEMENT_MESSAGE,
+};
