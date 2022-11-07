5c00eeae0932141493a5866f04372b320f00fd9e
김혜원 <dev.blair06@gmail.com>
2022-11-07 15:49:25 +0900

    feat:#blair06 사용자 입력 및 예외처리 - 사용자 표준입력 - 입력값 유효성검사 메소드 구현 - 에러 발생시 catch하여 프로그램 종료 안내 및 종료

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ const MissionUtils = require("@woowacourse/mission-utils");
 class App {
   constructor() {
     this.computerNumber = [];
+    this.userNumber = [];
   }
-
   createRandomNumber() {
     while (this.computerNumber.length < 3) {
       const number = MissionUtils.Random.pickNumberInRange(1, 9);
@@ -12,11 +12,35 @@ class App {
       }
     }
   }
+  checkValidate(value) {
+    if (value.length !== 3) throw "잘못된 입력값 입니다.";
+    if (value.includes("0")) throw "잘못된 입력값 입니다.";
+    if (isNaN(value)) throw "잘못된 입력값 입니다.";
 
+    const isDuplicated = value.some((elem) => {
+      return value.indexOf(elem) !== value.lastIndexOf(elem);
+    });
+    if (isDuplicated) throw "잘못된 입력값 입니다.";
+  }
+  userInput() {
+    MissionUtils.Console.readLine("숫자를 입력해주세요 :", (value) => {
+      this.userNumber = [...value];
+      try {
+        this.checkValidate(this.userNumber);
+      } catch (e) {
+        MissionUtils.Console.print(e);
+        MissionUtils.Console.close();
+        MissionUtils.Console.print("게임을 종료합니다.");
+      }
+    });
+  }
   play() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
     this.createRandomNumber();
+    this.userInput();
   }
 }
 
 module.exports = App;
+const app = new App();
+app.play();
