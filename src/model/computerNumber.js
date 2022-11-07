738709525b23285382a7c9e22b59d302b6f3fe63
sh981013s <sh981013s@gmail.com>
2022-11-07 18:53:28 +0900

    feat: 컴퓨터가 설정한 수 상태 관련 getter/setter 구현

--- src/model/computerNumber.js
+++ src/model/computerNumber.js
@@ -3,6 +3,22 @@ class ComputerNumber {
   constructor() {
     this.number = null;
   }
+
+  /**
+   * 컴퓨터가 설정한 숫자를 return 한다.
+   * @return {string[]} [컴퓨터가 설정한 숫자를 담은 배열]
+   */
+  getState() {
+    return this.number;
+  }
+
+  /**
+   * 컴퓨터가 설정한 숫자 상태를 변경한다.
+   * @param {string[]} newState [변경할 새로운 값]
+   */
+  setState(newState) {
+    this.number = newState;
+  }
 }
 
 module.exports = ComputerNumber;
