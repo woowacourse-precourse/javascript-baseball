28b2361c6eecf9a8b7afe768c06fa1e64e234265
soomin <qkrtnals970804@gmail.com>
2022-11-07 20:43:59 +0900

    feat: 컴퓨터의 랜덤 숫자 3글자 받기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+const { Console } = require("@woowacourse/mission-utils");
+const { Random } = require("@woowacourse/mission-utils");
 class App {
-  play() {}
+  gameStart(){
+    Console.print("숫자 야구 게임을 시작합니다.");
+    this.computerNumber = this.pickRandomNumber();
+  }
+
+  pickRandomNumber() {
+    let randomNumber = [];
+    while (randomNumber.length !== 3) {
+      const pickNumber = Random.pickNumberInRange(1,9);
+      if (!randomNumber.includes(pickNumber)) {
+        randomNumber.push(pickNumber);
+      }
+    }
+    return randomNumber;
+  }
+
+  play() {
+    this.gameStart();
+  }
 }
 
 module.exports = App;
