c42cd7294e66e3a4d1fc487dd39a5bf57902528a
hjy0951 <hjy0951@gmail.com>
2022-11-07 18:16:00 +0900

    feat: App 클래스 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
+const { Console } = require("@woowacourse/mission-utils");
+const Computer = require("./Computer");
+
+/*
+1. App
+ - Property
+  - Controller 클래스 인스턴스
+  - Computer 클래스 인스턴스
+ - Method
+  - 게임 시작 기능
+  - 게임 재시작 기능
+  - 게임 종료 기능
+*/
 class App {
-  play() {}
+  constructor() {
+    this._computer = new Computer();
+    /* new Controller */
+  }
+
+  play() {
+
+  }
+
+  restart() {
+
+  }
+
+  finish() {
+    Console.close();
+  }
 }
+
+Console.print('hello world');
+// Console.close();
 
 module.exports = App;
