a42bec7cc078fe37e5323db8e798f51eca6d812c
jinseo <ghkdwlstj99@gmail.com>
2022-11-07 20:24:56 +0900

    refactor: App class 로직 수정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+const GameProgress = require("./GameProgress");
+const UserNumber = require("./UserNumber");
+const CreateNumber = require("./CreateNumber");
+const { Console } = require("@woowacourse/mission-utils");
+
+
 class App {
-  play() {}
+  constructor(){
+    this.gameProgress = new GameProgress();
+    this.userNumber = new UserNumber();
+    this.createNumber = new CreateNumber();
+    
+
+  }
+  play() {
+    Console.print("숫자 야구 게임을 시작합니다.")
+    this.gameProgress.gameStart();
+  }
 }
+
 
 module.exports = App;
