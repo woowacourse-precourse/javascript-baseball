eb075214d20d13c01c221b001d0e2d5889f6a52a
chasj0326@naver.com <62418379+chasj0326@users.noreply.github.com>
2022-11-07 10:02:45 +0900

    fix: App.js - makeProblem 함수로 인해 테스트 코드 오류나는 부분 수정
    MissionUtils.Random.pickNumberInRange() 사용하는 함수로 수정

--- src/App.js
+++ src/App.js
@@ -10,13 +10,19 @@ function playGame(){ //문제 생성 - 게임 종료 옵션
   playOneRound(problem);
 }
 
-function makeProblem(NUMBER_LENGTH){//문제 생성 함수
-  const problemNumberList = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, NUMBER_LENGTH);
-  return problemNumberList;
+function makeProblem(NUMBER_LENGTH){
+  const problem = new Set([]);
+  while ([...problem].length < NUMBER_LENGTH){
+    problem.add(MissionUtils.Random.pickNumberInRange(1,9));
+  }
+  return [...problem];
 }
 
 function playOneRound(problem){//한 라운드 함수
   MissionUtils.Console.readLine("숫자를 입력해 주세요. : ", (input)=>{
+    if(input.length>3){
+      throw '길이 초과'
+    }
     const answer = input;
     const result = makeResult(answer, problem);
     const [ball_count, strike_count] = result;
@@ -24,9 +30,11 @@ function playOneRound(problem){//한 라운드 함수
     if(strike_count!==3){
       return playOneRound(problem);
     }
+    MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     quitOptions();
   })
 }
+
 
 function makeResult(answer, problem){//결과 계산 함수
   let ball_count = 0;
@@ -60,14 +68,13 @@ function showResult(ball_count, strike_count){//결과 출력 함수
 }
 
 function quitOptions(){
-  MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
   MissionUtils.Console.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
   MissionUtils.Console.readLine("",(input)=>{
-    switch(input){
-      case '1':
-        playGame();
-      case '2':
-        MissionUtils.Console.close();
+    if(input === '1'){
+      playGame();
+    }
+    else if(input === '2' ){
+      return;
     }
   })
 }
@@ -80,3 +87,5 @@ class App {
 }
 
 module.exports = App;
+// const app = new App();
+// app.play(); 
