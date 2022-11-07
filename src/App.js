93384bdeb045d5dc797b492c699adb2504b112df
ming0830 <minsun0830@naver.com>
2022-11-08 00:27:46 +0900

    feat : 사용자의 입력값을 받는 기능 구현

--- src/App.js
+++ src/App.js
@@ -5,9 +5,8 @@ class App{
     this.uniqueNumberList = [];
   }
 
-  play(){}
-    
-
+  
+  
   getRandumNumber(){
     const randomNumber = new Set();
     while(randomNumber.size !==3){
@@ -16,8 +15,28 @@ class App{
     const uniqueNumberList = [...randomNumber]
     return uniqueNumberList
   }
+  
+  userInput(){
+    MissionUtils.Console.readLine('숫자를 입력해주세요 : ',(userInputValue)=>{
+      const InputNumber = userInputValue.split('').map((v)=>parseInt(v))
+      this.userInput();
+    });
+  }
+  
+  
+  isVaildUserInput(userInput){
+    if(userInput.length === 0){
+      throw new Error("숫자를 입력하세요")
+    }
+    
+  }
+  play(){
+    this.getRandumNumber();
+    this.userInput();
+  }
+  
 }
 const app= new App();
-console.log(app.getRandumNumber())
+console.log(app.play())
 
 module.exports = App;
