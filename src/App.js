386ac49abd86a9f89bb50c1e32a4b4b65ef6f947
dianestar <dianestar@naver.com>
2022-11-07 23:46:18 +0900

    feat: 입력값 검증 및 예외 처리

--- src/App.js
+++ src/App.js
@@ -12,6 +12,18 @@ class App {
     return numbers;
   }
 
+  /* 입력값 검증 및 예외 처리 */
+  validateInput(input) {
+    if (input.length !== 3) {
+      throw "잘못된 값을 입력하였습니다. 3자리의 수만 입력할 수 있습니다.";
+    }
+    
+    const splited = input.split('');
+    if (splited[0] === splited[1] || splited[1] === splited[2] || splited[0] === splited[2]) {
+      throw "잘못된 값을 입력하였습니다. 3자리의 수는 각각 서로 다른 숫자여야 합니다."
+    }
+  }
+
   /* 게임 플레이 */
   play() {
     const numbers = this.pickRandomNumbers();
@@ -19,7 +31,11 @@ class App {
     // 게임 시작 문구 출력
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
     MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (answer) => {
-      // console.log(answer);
+      try {
+        this.validateInput(answer);
+      } catch(e) {
+        console.error(e);
+      }
     })
   }
 }
