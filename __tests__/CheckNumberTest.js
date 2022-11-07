7d21f64ec5d0500063a0b6c3a11aa9c4e7bcfc11
unknown <chn0202@gmail.com>
2022-11-07 19:52:57 +0900

    Test: CheckNumber메서드 테스트 구현
    1. 길이가 3이하 일때
    2. 숫자가 아닌게 들어올떄
    3. 1-9사이 숫자가 아닐떄
    thorw처리

---
+++ __tests__/CheckNumberTest.js
@@ -0,0 +1,21 @@
+const App = require("../src/App");
+
+describe("checkNumber() 테스트", () => {
+  test("예외-길이가 3이하 일때", () => {
+    const userNumber = ["1"];
+    const app = new App();
+
+    expect(() => app.checkNumber(userNumber)).toThrow();
+  });
+
+  test("예외-숫자가 아닌게 들어올때", () => {
+    const userNumber = ["1#2"];
+    const app = new App();
+    expect(() => app.checkNumber(userNumber)).toThrow();
+  });
+  test("예외-1-9사이가 아닐때", () => {
+    const userNumber = ["120"];
+    const app = new App();
+    expect(() => app.checkNumber(userNumber)).toThrow();
+  });
+});
