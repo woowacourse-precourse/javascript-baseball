a289947f9b8b28a0a37db936ed4fc08f3c586d86
금가경 <gageum0@gmail.com>
2022-11-07 20:11:35 +0900

    fix : 컴퓨터 랜덤 숫자가 게임 시작시 갱신되지 않는 문제 해결

--- src/App.js
+++ src/App.js
@@ -88,6 +88,7 @@ class App {
   resumeOrQuitGame(){
     MissionUtils.Console.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.', (answer) => {
       if (answer === '1') {
+        this.computerRandomNumbers = this.generateComputerRandomNumbers();
         this.getUserInput();
       }
       else if (answer === '2') {
