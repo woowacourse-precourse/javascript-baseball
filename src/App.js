72667c5fb5a661b75b2b2b65dbce4722df53dae5
nailen1 <nietsnie1@gmail.com>
2022-11-08 00:28:36 +0900

    style: 안내 메세지 글자 수정과 세미콜론 추가

--- src/App.js
+++ src/App.js
@@ -25,17 +25,17 @@ function generateAnswer() {
 function throwError(input) {
   let inputArr = Array.from(String(input));
   if (inputArr.length !== 3) {
-    throw new Error("3자리 숫자만 입력 가능합니다.")
+    throw new Error("3자리 숫자만 입력 가능합니다.");
   }
   if (input.includes(0)) {
-    throw new Error("0은 포함될 수 없습니다.")
+    throw new Error("0은 포함될 수 없습니다.");
   }
   let inputSet = new Set(inputArr);
   if (inputSet.size < 3) {
-    throw new Error("중복되지 않은 숫자 3개만 입력 가능합니다.")
+    throw new Error("중복되지 않은 숫자 3개만 입력 가능합니다.");
   }
   if (input.replace(/[0-9]/g, '').length !== 0) {
-    throw new Error("숫자만 입력 가능합니다.")
+    throw new Error("숫자만 입력 가능합니다.");
   }
 }
 
@@ -60,10 +60,10 @@ class Umpire {
 
 function winAndRestart() {
   printMessage('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
-  Console.readLine('게임을 새로 시작하시려면 1, 종료하려면 2를 입력하세요.\n', (oneOrtwo) => {
+  Console.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n', (oneOrtwo) => {
     if (Number(oneOrtwo) === 1) new App().play();
     else if (Number(oneOrtwo) === 2) Console.close();
-    else throw new Error('1 또는 2만 입력하세요.')
+    else throw new Error('1 또는 2만 입력하세요.');
   })
 }
 
