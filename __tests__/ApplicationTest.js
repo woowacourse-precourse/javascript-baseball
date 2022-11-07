b42e55e01ab36a4937d186a716c31696b574890f
mochang2 <qjsrodksro@gmail.com>
2022-11-07 13:58:38 +0900

    test: 정답 테스트 케이스 추가

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -47,6 +47,49 @@ describe('숫자 야구 게임', () => {
     })
   })
 
+  test('게임 종료 후 1, 2 이외에 다른 수 입력', () => {
+    const randoms = [1, 3, 5]
+    const answers = ['246', '135', '3', '3', '2']
+    const logSpy = getLogSpy()
+    const messages = ['낫싱', '3스트라이크', '게임 종료']
+
+    mockRandoms(randoms)
+    mockQuestions(answers)
+
+    const app = new App()
+    app.play()
+
+    messages.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output))
+    })
+  })
+
+  test('정답 맞춘 후 게임 종료', () => {
+    const randoms = [4, 7, 8]
+    const answers = ['123', '234', '345', '456', '567', '678', '478', '2']
+    const logSpy = getLogSpy()
+    const messages = [
+      '낫싱',
+      '낫싱',
+      '1볼',
+      '1스트라이크',
+      '1볼',
+      '2스트라이크',
+      '3스트라이크',
+      '게임 종료',
+    ]
+
+    mockRandoms(randoms)
+    mockQuestions(answers)
+
+    const app = new App()
+    app.play()
+
+    messages.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output))
+    })
+  })
+
   test('예외 테스트', () => {
     const randoms = [1, 3, 5]
     const answers = ['1234']
