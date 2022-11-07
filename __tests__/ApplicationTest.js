9736291358a08370ca81f951f16ceee14f44ac7b
coolIlive <galilee155@naver.com>
2022-11-08 00:15:09 +0900

    Style: 수정_ 테스트 파일의 함수식을 함수 선언으로
    JS 스타일 가이드 _ 함수파트

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -1,7 +1,7 @@
 const App = require('../src/App');
 const MissionUtils = require('@woowacourse/mission-utils');
 
-const mockQuestions = (answers) => {
+function mockQuestions(answers) {
   MissionUtils.Console.readLine = jest.fn();
   answers.reduce((acc, input) => {
     return acc.mockImplementationOnce((question, callback) => {
@@ -10,14 +10,14 @@ const mockQuestions = (answers) => {
   }, MissionUtils.Console.readLine);
 };
 
-const mockRandoms = (numbers) => {
+function mockRandoms(numbers) {
   MissionUtils.Random.pickNumberInRange = jest.fn();
   numbers.reduce((acc, number) => {
     return acc.mockReturnValueOnce(number);
   }, MissionUtils.Random.pickNumberInRange);
 };
 
-const getLogSpy = () => {
+function getLogSpy() {
   const logSpy = jest.spyOn(MissionUtils.Console, 'print');
   logSpy.mockClear();
   return logSpy;
@@ -209,5 +209,4 @@ describe('숫자 야구 게임', () => {
       app.play();
     }).toThrow();
   });
-
 });
