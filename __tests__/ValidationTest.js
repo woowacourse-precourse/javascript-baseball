b2ce5d8db22602bdb178fb9aafc2fb372dba3615
pakxe <pigkill40@naver.com>
2022-11-08 04:27:48 +0900

    test: 테스트 코드의 matcher 를 직관적인 것으로 변경

--- __tests__/ValidationTest.js
+++ __tests__/ValidationTest.js
@@ -9,49 +9,49 @@ const ERROR = require('../src/Constants/Error');
 
 describe('길이 검사', () => {
   test('case1', () => {
-    expect(validLength([1, 2, 3])).toEqual(true);
+    expect(validLength([1, 2, 3])).toBeTruthy();
   });
   test('case2', () => {
-    expect(validLength([1, 2, 3, 4])).toEqual(false);
+    expect(validLength([1, 2, 3, 4])).toBeFalsy();
   });
   test('case3', () => {
-    expect(validLength([1, 2])).toEqual(false);
+    expect(validLength([1, 2])).toBeFalsy();
   });
   test('case4', () => {
-    expect(validLength([])).toEqual(false);
+    expect(validLength([])).toBeFalsy();
   });
 });
 
 describe('범위 검사', () => {
   test('case1', () => {
-    expect(validRange([1, 2, 0])).toEqual(false);
+    expect(validRange([1, 2, 0])).toBeFalsy();
   });
   test('case2', () => {
-    expect(validRange([0, 3, 4])).toEqual(false);
+    expect(validRange([0, 3, 4])).toBeFalsy();
   });
   test('case3', () => {
-    expect(validRange([1, 2, 3])).toEqual(true);
+    expect(validRange([1, 2, 3])).toBeTruthy();
   });
   test('case4', () => {
-    expect(validRange([0, 0, 0])).toEqual(false);
+    expect(validRange([0, 0, 0])).toBeFalsy();
   });
 });
 
 describe('중복 검사', () => {
   test('case1', () => {
-    expect(duplication([1, 2, 3])).toEqual(false);
+    expect(duplication([1, 2, 3])).toBeFalsy();
   });
   test('case2', () => {
-    expect(duplication([1, 2, 2])).toEqual(true);
+    expect(duplication([1, 2, 2])).toBeTruthy();
   });
   test('case3', () => {
-    expect(duplication([1, 1, 1])).toEqual(true);
+    expect(duplication([1, 1, 1])).toBeTruthy();
   });
 });
 
 describe('유효값 검사', () => {
   test('case1', () => {
-    expect(isValidInput([1, 2, 3])).toEqual(true);
+    expect(isValidInput([1, 2, 3])).toBeTruthy();
   });
   test('case2', () => {
     expect(() => isValidInput([1, 2, 2])).toThrow(ERROR.DUPLICATION);
