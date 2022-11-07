be8ac6bf9b865a75beb58381d333b105e391f078
신선영 <sunyeong0412@naver.com>
2022-11-07 22:45:15 +0900

    feat: 정답이 될 세 개의 랜덤 수 배열 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  play() {
+    const answer = [];
+    while (answer.length < 3) {
+      const num = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!answer.includes(num)) {
+        answer.push(num);
+      }
+    }
+
+    MissionUtils.Console.print(answer);
+  }
 }
 
 module.exports = App;
+
+const app = new App();
+app.play();
