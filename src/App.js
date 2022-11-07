fa857881321911f625aab1c9d9c12bec054d649f
saetakki <lime.lemonade03@gmail.com>
2022-11-08 04:51:38 +0900

    Add score count function

--- src/App.js
+++ src/App.js
@@ -31,6 +31,58 @@ const parser = num => {
   return value;
 };
 
+const makeMap = list => {
+  const newMap = new Map(list.map((item, idx) => [idx, item]));
+  return newMap;
+};
+
+const testMap = makeMap([1, 5, 3]);
+
+const isBallOrStrike = (idx, value, whereToFind) => {
+  const IS_INCLUDED_AND_SAME_IDX = whereToFind.get(idx) === value;
+  const IS_INCLUDED_VALUE = [...whereToFind.values()].includes(value);
+  if (IS_INCLUDED_VALUE && IS_INCLUDED_AND_SAME_IDX) return '스트라이크';
+  else if (IS_INCLUDED_VALUE && !IS_INCLUDED_AND_SAME_IDX) return '볼';
+  else return '낫싱';
+};
+
+const countScore = userInput => {
+  const ballAndStrike = {
+    스트라이크: 0,
+    볼: 0,
+    낫싱: 0,
+  };
+  userInput.forEach((value, idx) => {
+    let response = isBallOrStrike(idx, value, testMap);
+    ballAndStrike[response]++;
+  });
+  return ballAndStrike;
+};
+
+console.log(countScore([1, 1, 2]));
+
+// console.log(isBall(x => [...testMap.values()].includes(x), 3));
+// console.log(
+//   isStrike(
+//     (candidateIdx, candidateValue) =>
+//       testMap.get(candidateIdx) === candidateValue,
+//     2,
+//     1,
+//   ),
+// );
+// const testMap = makeMap([1, 3, 2]);
+// const testGuess = [3, 3, 3];
+// const arr = [...testMap.values()];
+// let counter = [0, 0];
+
+// testGuess.forEach((item, idx) => {
+//   if (arr.includes(item)) {
+//     if (testMap.get(idx) === item) {
+//       counter[1]++;
+//     } else counter[0]++;
+//   }
+// });
+
 class App {
   play() {}
 }
