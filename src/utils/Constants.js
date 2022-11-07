acd6d396b0a51d1cfdf26e3c740f462ec0d41894
JuhyunLim <87933367+userJu@users.noreply.github.com>
2022-11-07 21:41:52 +0900

    Feat: Constants에 RETRY_VALUE와 END_VALUE 추가
    게임 재시작, 게임 종료 여부를 알 수 있는 상수를 정의함

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,6 +1,7 @@
 const START_GAME = "숫자 야구 게임을 시작합니다";
 const GAME_END = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
-const RETRY_OR_END = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요";
+
+const RETRY_OR_END = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
 const REQUIRE_NUMBER = "숫자를 입력해주세요 : ";
 
@@ -10,6 +11,8 @@ const STRIKE = "스트라이크";
 
 const START_NUMBER = 1;
 const END_NUMBER = 9;
+const RETRY_VALUE = "1";
+const END_VALUE = "2";
 
 module.exports = {
   START_GAME,
@@ -21,4 +24,6 @@ module.exports = {
   STRIKE,
   START_NUMBER,
   END_NUMBER,
+  RETRY_VALUE,
+  END_VALUE,
 };
