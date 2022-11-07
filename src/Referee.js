d83d32a1c9a840c6d76218bff96e6cc4b5a58269
kangju2000 <kangju2000@naver.com>
2022-11-08 02:53:30 +0900

    feat: Referee 클래스의 gameFinish 메서드 구현

--- src/Referee.js
+++ src/Referee.js
@@ -2,6 +2,9 @@ const MissionUtils = require('@woowacourse/mission-utils');
 const Computer = require('./Computer');
 const Player = require('./Player');
 
+const RESTART = '1';
+const GAME_OVER = '2';
+
 class Referee {
   constructor() {
     this.computer = new Computer(this);
@@ -13,6 +16,17 @@ class Referee {
   gameStart() {
     this.computer.setRandomValue();
     this.player.setValue();
+  }
+
+  gameFinish() {
+    MissionUtils.Console.readLine(
+      '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n',
+      (answer) => {
+        if (answer === RESTART) return this.gameStart();
+        if (answer === GAME_OVER) return MissionUtils.Console.print('숫자 야구 게임을 종료합니다.');
+        return this.gameFinish();
+      }
+    );
   }
 }
 
