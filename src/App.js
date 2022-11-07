cd0161efc52a15d6f351dcaf7551da86d5ef2bc6
HeoJiye <heoze1031@gmail.com>
2022-11-07 14:03:31 +0900

    feat(play): add 게임 시작 문구 출력 기능
    MissionUtils.Console.print() 사용해서 게임 시작 문구 "숫자 야구 게임을 시작합니다" 출력
    MissionUtils.Console.close() 사용해서 콘솔 사용 종료

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ const MissionUtils = require("@woowacourse/mission-utils");
 class App {
   play() {
     const numbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
-    
+
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    MissionUtils.Console.close();
   }
 }
 
