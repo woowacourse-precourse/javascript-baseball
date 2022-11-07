4525c8a216556dcb01d878bfa9bcb8dafbf9c77f
hyejinny97 <yhj970102@gmail.com>
2022-11-08 01:30:34 +0900

    feat: gameStart() /computerInput() 생성

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
+  constructor() {
+    this.computerInputNumbers = ''
+  }
+
+  computerInput() {
+    let computer = ''
+    while (computer.length < 3) {
+      let number = String(MissionUtils.Random.pickNumberInRange(1, 9))
+      if (!computer.includes(number)) {
+        computer += number
+      }
+    }
+    this.computerInputNumbers = computer
+  }
+
+  gameStart() {
+    this.computerInput()
+  }
+
   play() {
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.')
+    this.gameStart()
   }
 }
 
