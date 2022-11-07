81c2a5c12c0095b504632d24c441cd343e7f8162
Minju Park <project.mizzu@gmail.com>
2022-11-08 01:07:05 +0900

    Feat : 무작위 숫자 3자리 추출
    - 컴퓨터의 3개의 랜덤 넘버 추출 (1-9 범위) -> Random.pickNumberInRange(1, 9)
    - Answer.js 파일로 분리

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-const { MissionUtils, Console } = require('@woowacourse/mission-utils');
+const { Console } = require('@woowacourse/mission-utils');
+const computer = require('./Answer');
 
 class App {
   play() {
     this.print('숫자 야구 게임을 시작합니다.');
+    this.print(computer);
     this.close();
   }
 
