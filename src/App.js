bbdf005cb82e0b503e4a7e7e5974c472cfc789fa
ShimSungBo <shimdb96@gmail.com>
2022-11-07 16:51:26 +0900

    feat: 기능구현 완료.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-const COMPUTER_ANSWER = require("./makeComputerAnswer");
+const checkProgress = require("./checkProgress");
+const progress = require("./progress");
+const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
-  play() {}
+  play() {
+    let playing = false;
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    while (!playing) {
+      const PLAY_GAME = progress();
+      playing = PLAY_GAME;
+    }
+  }
 }
 
 const app = new App();
