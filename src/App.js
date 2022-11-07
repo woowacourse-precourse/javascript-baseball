6b8993583f4abf2a0a620067ae59d0934e1b8700
lsy20140 <lsy20140@naver.com>
2022-11-08 05:25:16 +0900

    feat: 컴퓨터와 유저 숫자 비교결과에 따라 메소드 호출하는 compareInput 생성

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ const { Random, Console } = require("@woowacourse/mission-utils");
 class App {
 
   constructor() {
-    this.computerInput = [];
-    this.userInput = [];
+    this.computerInputNums = [];
+    this.userInputNums = [];
   }
   
   computerInput(){
-    this.computerNumbers = Random.pickUniqueNumbersInRange(1, 9, 3);
+    this.computerInputNums = Random.pickUniqueNumbersInRange(1, 9, 3);
   }
 
   checkUserInput(userInput){
@@ -24,6 +24,22 @@ class App {
       this.compareInputNum(input);
       Console.close();
       })
+  }
+
+  compareInput(userInput){
+    this.userInputNums = userInput;
+    const subtractArr = this.computerInput.map((x,y) => x-userInput[y]);
+    const zeroCount = subtractArr.reduce((count, data) => data == 0 ? count + 1 : count, 0);
+    if(zeroCount == 3){
+      Console.print("3스트라이크");
+      Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+      //return this.restartGame();
+    }
+
+    //this.getResult();
+
+    return this.userInput();
+
   }
 
 
