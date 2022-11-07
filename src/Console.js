9c93a53b145708fac5f9095888fa432e4772825f
동동이 <drdragon777@naver.com>
2022-11-07 23:59:03 +0900

    feat - add Console 클래스 초기 버전

---
+++ src/Console.js
@@ -0,0 +1,22 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
+class Console {
+  static START = "숫자 야구 게임을 시작합니다.";
+  static INPUT = "숫자를 입력해주세요 : ";
+  static END = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+  static END_NOTICE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+  readLine(message, callback) {
+    MissionUtils.Console.readLine(message, callback);
+  }
+
+  print(message) {
+    MissionUtils.Console.print(message);
+  }
+
+  close() {
+    MissionUtils.Console.close();
+  }
+}
+
+module.exports = Console;
