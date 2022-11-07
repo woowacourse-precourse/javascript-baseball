e29fe8f5e67f7cfb2dbdb76f87822e562c59b32a
pakxe <pigkill40@naver.com>
2022-11-07 17:47:33 +0900

    test: 3자릿수가 아닌 4자릿수의 숫자야구 게임 플레이 테스트 코드 작성

---
+++ __tests__/ExtensionTest.js
@@ -0,0 +1,34 @@
+const App = require('../src/App');
+const { mockQuestions, mockRandoms, getLogSpy } = require('./ApplicationTest');
+
+const mockConstants = jest.requireMock('../src/constants');
+jest.mock('../src/constants');
+
+afterAll(() => {
+  jest.resetModules();
+});
+
+describe('3자릿수가 아닌 자릿수의 숫자야구 게임 해보기', () => {
+  test('4자릿수의 숫자 야구', () => {
+    const randoms = [1, 2, 3, 4];
+    const answers = ['1245', '1254', '1234', '2'];
+    const logSpy = getLogSpy();
+    const message = [
+      '1볼 2스트라이크',
+      '3스트라이크',
+      '4스트라이크',
+      '게임 종료',
+    ];
+
+    mockQuestions(answers);
+    mockRandoms(randoms);
+    mockConstants.ANSWER.LENGTH = 4;
+
+    const app = new App();
+    app.play();
+
+    message.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+});
