e56512f0f7d66039de7f659086bd03d4db6a37cc
letsjo <hyunoh.jo@gmail.com>
2022-11-08 00:12:05 +0900

    feat(App): 기능3 유저 숫자야구 입력 받는 메소드 추가

--- src/App.js
+++ src/App.js
@@ -6,6 +6,13 @@ const START_DIGIT = 1;
 const END_DIGIT = 9;
 
 class App {
+  setUserInput() {
+    Console.readLine('숫자를 입력해주세요 : ', inputDigit => {
+      const userDigit = [...inputDigit].map(Number);
+      Console.print(userDigit);
+    });
+  }
+
   setRandomDigit() {
     const randomDigit = new Set();
     while (randomDigit.size < INPUT_LENGTH) {
@@ -18,8 +25,14 @@ class App {
     Console.print('숫자 야구 게임을 시작합니다.');
   }
 
+  gameStart() {
+    this.setRandomDigit();
+    this.setUserInput();
+  }
+
   play() {
     this.showStartMessage();
+    this.gameStart();
   }
 }
 
