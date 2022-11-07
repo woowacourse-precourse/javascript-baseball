22448ee7a74d59767924051ff85eb8fdde7bc39e
Miniling <bsm3925@naver.com>
2022-11-08 01:30:58 +0900

    Modify: move this.user gameStart() -> userPlay(), sort Code

--- src/App.js
+++ src/App.js
@@ -1,30 +1,34 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
-  // 게임 종료 여부 확인(1: 시작, 2: 종료)
-  gameOver() {
-    MissionUtils.Console.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-    MissionUtils.Console.readLine("", (answer) => {
-      if (answer == '1') this.gameStart();
-      else if (answer == '2') {
-        MissionUtils.Console.print("게임 종료.");
-        MissionUtils.Console.close();
+  // 컴퓨터 랜덤으로 서로 다른 세 숫자 받기
+  computerSet() {
+    const computer = [];
+
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+        this.computer = computer;
       }
+    }
+  }
+
+  // 사용자 입력
+  userPlay() {
+    this.user = [];
+    MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (answer) => {
+      const user = answer.split('');
+      this.checkAnswer(user);
+      user.map(e => this.user.push(parseInt(e)));
+      this.playBaseball(this.user);
     });
   }
 
-  // 결과 출력
-  printResult(ball, strike) {
-    if (ball === 0 && strike === 0) MissionUtils.Console.print("낫싱");
-    else if (strike === 3) {
-      MissionUtils.Console.print("3스트라이크");
-      MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-      this.gameOver();
-    }
-    else {
-      MissionUtils.Console.print(`${ball}볼 ${strike}스트라이크`);
-    }
-    this.userPlay();
+  // 입력값 확인
+  checkAnswer(user) {
+    if (user.length !== 3) throw new Error("입력 숫자는 3개입니다!");
+    else if ([...new Set(user)].length !== 3) throw new Error("중복 입력입니다!");
   }
 
   // 입력 값과 컴퓨터 숫자 비교
@@ -42,33 +46,30 @@ class App {
     this.printResult(ball, strike);
   }
 
-  // 입력값 확인
-  checkAnswer(user) {
-    if (user.length !== 3) throw new Error("입력 개수는 3개입니다!");
-    else if ([...new Set(user)] !== 3) throw new Error("중복 입력입니다!");
-  }
-
-  // 사용자 입력
-  userPlay() {
-    MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (answer) => {
-      const user = answer.split('');
-      this.checkAnswer(user);
-      user.map(e => this.user.push(parseInt(e)));
-      this.playBaseball(this.user);
-    });
-  }
-
-  // 컴퓨터 랜덤으로 서로 다른 세 숫자 받기
-  computerSet() {
-    const computer = [];
-
-    while (computer.length < 3) {
-      const number = MissionUtils.Random.pickNumberInRange(1, 9);
-      if (!computer.includes(number)) {
-        computer.push(number);
-        this.computer = computer;
-      }
+  // 결과 출력
+  printResult(ball, strike) {
+    if (ball === 0 && strike === 0) MissionUtils.Console.print("낫싱");
+    else if (strike === 3) {
+      MissionUtils.Console.print("3스트라이크");
+      MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+      this.gameOver();
     }
+    else {
+      MissionUtils.Console.print(`${ball}볼 ${strike}스트라이크`);
+    }
+    this.userPlay();
+  }
+
+  // 게임 종료 여부 확인(1: 시작, 2: 종료)
+  gameOver() {
+    MissionUtils.Console.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    MissionUtils.Console.readLine("", (answer) => {
+      if (answer == '1') this.gameStart();
+      else if (answer == '2') {
+        MissionUtils.Console.print("게임 종료.");
+        MissionUtils.Console.close();
+      }
+    });
   }
 
   // 게임 시작
