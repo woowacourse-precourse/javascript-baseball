15998fe6c6797bfb7bb44fd1d55a89b36940758d
eunha <dmsgk2323@naver.com>
2022-11-07 10:16:06 +0900

    feat: 게임 재시작 질문 기능 추가

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ const MissionUtils = require("@woowacourse/mission-utils");
 let landomNum = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3).join("");
 let userAnswer;
 console.log(landomNum);
+let answer = false;
 function judgeSame() {
   // 숫자를 맞출 경우
   if (landomNum == userAnswer) {
     MissionUtils.Console.print("3스트라이크");
     MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-    MissionUtils.Console.close();
+    answer = true;
+    gameAgain();
   }
 }
 
@@ -43,11 +45,29 @@ let question = function () {
   MissionUtils.Console.readLine("숫자를 입력해주세요.", (answer) => {
     userAnswer = answer;
     // console.log(typeof userAnswer);
-    judgeSame();
     compareNums(landomNum, userAnswer);
-    question();
+    judgeSame();
+    if (answer === false) {
+      question();
+    }
   });
 };
+function gameAgain() {
+  MissionUtils.Console.readLine(
+    "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
+    (answer) => {
+      if (answer == 1) {
+        landomNum = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3).join(
+          ""
+        );
+        app.play();
+      }
+      if (answer == 2) {
+        MissionUtils.Console.close();
+      }
+    }
+  );
+}
 
 class App {
   play() {
@@ -56,7 +76,7 @@ class App {
   }
 }
 
-const app = new App();
-app.play();
+// const app = new App();
+// app.play();
 
-// module.exports = App;
+module.exports = App;
