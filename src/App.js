56572614d120dfabacedae5077445d3d9b9eb0c6
이석준 <sukjune96@gmail.com>
2022-11-07 17:32:51 +0900

    refactor: 출력메세지 상수화

--- src/App.js
+++ src/App.js
@@ -5,8 +5,14 @@ const getScore = require("./getScore");
 const showMessage = require("./showMessage");
 
 class App {
+  MESSAGES = {
+    START: "숫자 야구 게임을 시작합니다.",
+    INPUT: "숫자를 입력해주세요 : ",
+    END: "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
+    RESTART: "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n",
+  };
   play() {
-    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    MissionUtils.Console.print(this.MESSAGES.START);
     this.game();
   }
   game() {
@@ -14,7 +20,7 @@ class App {
     this.userInput(answer);
   }
   userInput(answer) {
-    MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (input) => {
+    MissionUtils.Console.readLine(this.MESSAGES.INPUT, (input) => {
       const inputArr = getUserExecption(input);
       const score = getScore(inputArr, answer);
       this.checkGame(score, answer);
@@ -23,7 +29,7 @@ class App {
   checkGame(score, answer) {
     if (score.strike === 3) {
       MissionUtils.Console.print("3스트라이크");
-      MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+      MissionUtils.Console.print(this.MESSAGES.END);
       this.restart();
     } else {
       showMessage(score);
@@ -31,18 +37,15 @@ class App {
     }
   }
   restart() {
-    MissionUtils.Console.readLine(
-      "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n",
-      (input) => {
-        if (parseInt(input) === 1) {
-          this.game();
-        } else if (parseInt(input) === 2) {
-          MissionUtils.Console.close();
-        } else {
-          throw new Error();
-        }
+    MissionUtils.Console.readLine(this.MESSAGES.RESTART, (input) => {
+      if (parseInt(input) === 1) {
+        this.game();
+      } else if (parseInt(input) === 2) {
+        MissionUtils.Console.close();
+      } else {
+        throw new Error();
       }
-    );
+    });
   }
 }
 const app = new App();
