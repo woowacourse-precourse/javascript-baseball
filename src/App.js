4b928814701c14c06f922d88d94388b615076dcd
jiho-bae <jiho@dgu.ac.kr>
2022-11-08 00:44:12 +0900

    Feat: getRandomsStr 메서드에서 3자리 숫자로 구성된 문자열 반환

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+
 class App {
-  play() {}
+  play() {
+    this.answer = this._getRandomsStr([1, 2, 3, 4, 5, 6, 7, 8, 9], 3);
+  }
+
+  _getRandomsStr(numbers, size) {
+    const pickNums = [];
+
+    while (pickNums.length < size) {
+      const pickNum = MissionUtils.Random.pickNumberInList(numbers);
+      numbers.splice(numbers.indexOf(pickNum), 1);
+      pickNums.push(pickNum);
+    }
+
+    return pickNums.join('');
+  }
 }
 
 module.exports = App;
