85ceab91b9e18138e034bb580151f3b354822539
imseoyoung <lsy783@sookmyung.ac.kr>
2022-11-08 03:58:43 +0900

    feat: 서로 다른 3자리 숫자 랜덤 생성 및 저장

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 
 class App {
+
   play() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    this.makeComputerNum();
   }
+
+  makeComputerNum() {
+    const random_num = [];
+    while(random_num.length < 3) {
+        const number = MissionUtils.Random.pickNumberInRange(1,9);
+        if(random_num.includes(number)) continue;
+        random_num.push(number);        
+    }
+  }
+
 }
 
 module.exports = App;
