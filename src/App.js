b988d1fcc9d719d27c4a7583fee3a02f486ff92f
jiho-bae <jiho@dgu.ac.kr>
2022-11-08 01:10:48 +0900

    Feat: validate 메서드로 입력숫자에 대한 유효성검사 후 힌트제공함수를 호출한다.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ const { Random, Console } = require('@woowacourse/mission-utils');
 class App {
   play() {
     this.answer = this._getRandomsStr([1, 2, 3, 4, 5, 6, 7, 8, 9], 3);
+    this.getUserInput(this.validate);
   }
 
   _getRandomsStr(numbers, size) {
@@ -21,9 +22,27 @@ class App {
     Console.print('숫자 야구 게임을 시작합니다.');
     Console.readLine('숫자를 입력해주세요 : ', cbFn);
   }
-}
 
-const a = new App();
-a.play();
+  isDuplicated(input) {
+    const numberSet = new Set([...input]);
+
+    return numberSet.size === 3;
+  }
+
+  evaluate(input) {}
+
+  validate(input) {
+    const regExp = new RegExp(/^[1-9]{1,3}$/);
+
+    try {
+      if (regExp.test(input) && !this.isDuplicated(input)) {
+        // 입력값이 3자리 숫자이고, 중복숫자가 없다면 다음 힌트제공 기능을 이용한다.
+        this.evaluate(input);
+      } else {
+        // 입력이 잘못되었다면 예외를 발생시키고 게임을 종료시킨다.
+      }
+    } catch (e) {}
+  }
+}
 
 module.exports = App;
