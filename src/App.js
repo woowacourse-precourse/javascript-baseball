9cfbf0c5734a2a31484efd5c0c43b27fb891e8d7
dianestar <dianestar@naver.com>
2022-11-07 14:42:05 +0900

    feat: 랜덤 숫자 추출

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  /* 랜덤 숫자 추출 */
+  pickRandomNumbers() {
+    const numbers = [];
+    while (numbers.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!numbers.includes(number)) numbers.push(number);
+    }
+
+    return numbers;
+  }
+
+  /* 게임 플레이 */
+  play() {
+    const numbers = this.pickRandomNumbers();
+    // console.log(numbers);
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
