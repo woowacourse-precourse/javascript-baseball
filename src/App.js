b6504c36b16bf22b978d906b4d0ea403ec63ed06
HWI <dlwognl99@naver.com>
2022-11-07 10:37:31 +0900

    feat: 유저의 숫자 입력 기능 구현

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ class App {
     gameStart();
     // 컴퓨터 랜덤 값 생성
     const computerNumber = RandomChoice();
+    // 사용자 값 입력
+    const userNumber = userInput();
   }
 }
 
@@ -23,5 +25,28 @@ const RandomChoice = () => {
   }
   return randomNumber;
 };
+
+// 유저 입력을 배열화
+function userInput() {
+  let userNumber;
+  MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (answer) => {
+    checkValidNumber(answer);
+    userNumber = answer.split("").map((number) => number);
+  });
+  return userNumber;
+}
+
+function checkValidNumber(answer) {
+  const checkDeplicated = [...new Set(answer)];
+  if (!answer.match(/[1-9]/)) {
+    throw new Error("1에서 9짜리의 숫자만 입력해주세요.");
+  }
+  if (answer.size !== 3) {
+    throw new Error("3자리 숫자만 입력해주세요.");
+  }
+  if (checkDeplicated.length !== 3) {
+    throw new Error("중복되지 않는 값을 입력해주세요.");
+  }
+}
 
 module.exports = App;
