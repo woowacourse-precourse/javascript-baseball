90177b221d0af362347a2985e96c8d79feec557a
wang-yurin <king_1994@naver.com>
2022-11-07 22:01:29 +0900

    Feat: 숫자 입력 기능 구현 중

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ class App {
   play() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
     const computerNumber = computerNumbers();
+    playerInputNumbers(computerNumber);
   }
 }
 
 // 플레이어 숫자 입력
 function playerInputNumbers() {
-  MissionUtils.Console.readLine("숫자를 입력해주세요: ", (answer) => {});
+  MissionUtils.Console.readLine("숫자를 입력해주세요: ", (answer) => {
+    const playerNumber = answer;
+  });
 }
 
 // 컴퓨터 숫자 생산
