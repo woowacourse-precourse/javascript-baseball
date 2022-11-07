265df048e0db84a712fbcc32b15409cdeb83b7aa
Yesong <to06109@naver.com>
2022-11-07 17:03:45 +0900

    feat: 랜덤값 추출

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+const { Random, Console } = require("@woowacourse/mission-utils");
+
 class App {
+  getRandomValue() {
+    return Random.pickNumberInRange(1, 9);
+  }
+
   play() {}
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
