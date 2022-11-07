cf14ec1e9ce80f1f095fb2111da5947c24d379a0
Sang-Mini <sangmin3062@gmail.com>
2022-11-08 06:34:40 +0900

    feat : Computer가 1 ~ 9까지 랜덤으로 중복없이 3개의 수를 생성하는 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+
 class App {
-  play() {}
+  play() { }
+
+  computerRandomNumber() {
+    let computerNumber = [];
+    while (computerNumber.length < 3) {
+      const NUMBER = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computerNumber.includes(NUMBER)) {
+        computerNumber.push(NUMBER);
+      }
+    }
+    return computerNumber;
+  };
+
 }
 
 module.exports = App;
