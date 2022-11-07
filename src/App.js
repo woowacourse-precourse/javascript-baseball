db15a7154dca20ebb6d2ce069b11e8eb58491c96
Hwang Seong Taek <hst007@naver.com>
2022-11-07 22:03:48 +0900

    feat: 상대방(컴퓨터)의 랜덤 숫자 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+const MissonUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  constructor() {
+    this.computerRandomNumbers = [];
+  }
+  play() {
+    this.createOpponentRandomNumber();
+  }
+
+  createOpponentRandomNumber() {
+    const computer = new Set();
+    while (computer.size < 3) {
+      const randomNumber = MissonUtils.Random.pickNumberInRange(1, 9);
+      computer.add(randomNumber);
+    }
+  
+    this.computerRandomNumbers = [...computer];
+  }
 }
 
 module.exports = App;
+
+const app = new App();
+app.play();
