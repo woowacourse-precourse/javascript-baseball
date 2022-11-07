7ec35b56ff5f95898b7da5ef53f2afc9de85cdba
cjm0611 <81799492+cjm0611@users.noreply.github.com>
2022-11-07 22:50:35 +0900

    test: 테스트 코드 추가 (게임 1회, 볼/스트라이크 확인, 재시작 9번)

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -59,4 +59,87 @@ describe("숫자 야구 게임", () => {
       app.play();
     }).toThrow();
   });
+
+  test("게임 1회 동작, 재시작X", () => {
+    const randoms = [7, 8, 9];
+    const answers = ["185", "273", "728", "987", "789", "2"];
+    const logSpy = getLogSpy();
+    const messages = [
+      "1스트라이크",
+      "1볼",
+      "1볼 1스트라이크",
+      "2볼 1스트라이크",
+      "3스트라이크",
+      "게임 종료",
+    ];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    const app = new App();
+    app.play();
+
+    messages.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
+  test("볼/스트라이크 가능한 경우들 확인, 재시작 1번", () => {
+    const randoms = [5, 2, 3, 1, 2, 7, 6, 8, 9];
+    const answers = ["789", "369", "652", "235", "567", "573", "523", "1", "723", "172", "127", "1", "689", "2"];
+    const logSpy = getLogSpy();
+    const messages = [
+      "낫싱",
+      "1볼",
+      "2볼",
+      "3볼",
+      "1스트라이크",
+      "2스트라이크",
+      "3스트라이크",
+      "1볼 1스트라이크",
+      "2볼 1스트라이크",
+      "3스트라이크",
+      "3스트라이크",
+      "게임 종료",
+    ];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    const app = new App();
+    app.play();
+
+    messages.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
+  test("게임 10회 (재시작 9회)", () => {
+    const randoms = [1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 3, 5, 2, 4, 6, 3, 5, 7, 4, 6, 8, 5, 7, 9, 9, 5, 1, 1, 5, 9];
+    const answers = ["123", "1", "456", "1", "789", "1", "135", "1", "246", "1", "357", "1", "468", "1", "579", "1", "951", "1", "159", "2"];
+    const logSpy = getLogSpy();
+    const messages = [
+      "3스트라이크",
+      "3스트라이크",
+      "3스트라이크",
+      "3스트라이크",
+      "3스트라이크",
+      "3스트라이크",
+      "3스트라이크",
+      "3스트라이크",
+      "3스트라이크",
+      "3스트라이크",
+      "게임 종료",
+    ];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    const app = new App();
+    app.play();
+
+    messages.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
 });
