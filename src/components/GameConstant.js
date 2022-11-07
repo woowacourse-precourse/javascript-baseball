e7b2489627d3d7d4a6797a623efed7d8409193f8
Inae Choi <inaechoi89@gmail.com>
2022-11-07 18:18:22 +0900

    feat: add list of fixed constant

---
+++ src/components/GameConstant.js
@@ -0,0 +1,19 @@
+const DIGIT = 3;
+const LIMIT_NUMBER_SIZE = 9;
+const MIN_NUMBER = 1;
+const MAX_NUMBER = 9;
+const START_MESSAGE = '숫자 야구 게임을 시작합니다.';
+const INPUT_ORDER_MESSAGE = '숫자를 입력해주세요 : ';
+const GAME_END_MESSAGE = '3개의 숫자를 모두 맞히셨습니다! 게임 종료';
+const BALL = '볼';
+const STRIKE = '스트라이크';
+
+exports.DIGIT = DIGIT;
+exports.LIMIT_NUMBER_SIZE = LIMIT_NUMBER_SIZE;
+exports.MIN_NUMBER = MIN_NUMBER;
+exports.MAX_NUMBER = MAX_NUMBER;
+exports.START_MESSAGE = START_MESSAGE;
+exports.INPUT_ORDER_MESSAGE = INPUT_ORDER_MESSAGE;
+exports.GAME_END_MESSAGE = GAME_END_MESSAGE;
+exports.BALL = BALL;
+exports.STRIKE = STRIKE;
