466c5d5c8e82cf2f6b7e25e90e6c3bae6f6da85e
random-olive <104327907+random-olive@users.noreply.github.com>
2022-11-07 18:40:12 +0900

    Fix: validation: number check

--- src/Validation.js
+++ src/Validation.js
@@ -8,14 +8,24 @@ const {
 } = require("./Constant");
 
 class Validation {
-  checkNumber(value) {
-    if (![1, 2, 3, 4, 5, 6, 7, 8, 9].includes(value)) {
-      throw INGAME_MESSAGE.ERROR;
-    }
+  checkAll(input) {
+    this.checkNumber(input);
+    this.checkLength(input);
+    this.checkRepeat(input);
   }
 
   checkLength(input) {
     if (input.length !== 3) {
+      throw INGAME_MESSAGE.ERROR;
+    }
+  }
+
+  checkNumber(input) {
+    if (
+      ![1, 2, 3, 4, 5, 6, 7, 8, 9].includes(input[0]) ||
+      ![1, 2, 3, 4, 5, 6, 7, 8, 9].includes(input[1]) ||
+      ![1, 2, 3, 4, 5, 6, 7, 8, 9].includes(input[2])
+    ) {
       throw INGAME_MESSAGE.ERROR;
     }
   }
