0a808a5595b828a343b9bc96874ad83ceb58e253
dustmddus <dustmddus5@gmail.com>
2022-11-07 22:19:43 +0900

    feat: 컴퓨터 랜덤 숫자 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+const { Console, Random } = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  start() {
+    Console.print("숫자 야구 게임을 시작합니다.");
+    const computerRandomNumber = Random.pickUniqueNumbersInRange(1, 9, 3).join(
+      ""
+    );
+  }
+
+  play() {
+    this.start();
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
