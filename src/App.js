e1fcd67ceeeeabd63ad8f34bfa4cdc5bc2dbfd40
yesolyo <ahnyelim77@gmail.com>
2022-11-07 22:05:02 +0900

    feat: 1~9사이 숫자 이용 서로 다른 3자리 수 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const COMPUTER_NUMBER = [];
+
 class App {
   play() {}
+
+  randomComputerNumber() {
+    while (COMPUTER_NUMBER.length < 3) {
+      const NUMBER = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!COMPUTER_NUMBER.includes(NUMBER)) {
+        COMPUTER_NUMBER.push(NUMBER);
+      }
+    }
+  }
 }
 
 module.exports = App;
