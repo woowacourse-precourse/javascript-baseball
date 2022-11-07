eb7101631456af81b12519b4e870c44ef7319267
lyssoi <114637463+lyssoi@users.noreply.github.com>
2022-11-07 20:08:17 +0900

    사용자에게 숫자 입력값받기
    사용자에게 숫자 입력값을 받고, 예외사항일경우 Error 메세지를 전송합니다.

--- src/App.js
+++ src/App.js
@@ -22,9 +22,26 @@ const createComputerNumber = () =>{
 }
 
 const inputUserNumber =() =>{
-  
+  MissionUtils.Console.readLine('숫자를 입력해주세요.:', (userNumber) => {
+    verifyUserNumber(userNumber);
   });
 }
+
+const verifyUserNumber = (userNumber) =>{
+  const userNumberArr = userNumber.split("")
+
+  if(userNumberArr.length !== 3){
+    throw "세개의 숫자가 아닙니다";
+  }
+
+  if(userNumber[0]==userNumber[1]||userNumber[1]==userNumber[2]||userNumber[0]==userNumber[2]){
+    throw "중복되는 숫자가 있습니다"
+  }
+  if(userNumber[0]==0||userNumber[1]==0||userNumber[2]==0){
+    throw "1~9사이의 수가 아닙니다"
+  }
+}
+
 
 const judgeNumber = () =>{}
 
