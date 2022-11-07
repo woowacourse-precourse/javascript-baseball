db938c28a2d67a1d08d802b8ebf3879b6cc74b01
chaeeun-seo <seochaeeun961126@gmail.com>
2022-11-08 04:42:02 +0900

    feat: create play that start game, make computer's random num and get user answer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
 class App {
-  play() {}
+  play() {
+    // 1. 게임 시작 문구 출력
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+
+    // 2. 컴퓨터의 랜덤 숫자 만들기
+    const computerRandomNumber = this.getComputerRandomNumber();
+
+    // 3. 숫자를 입력받는다. (반복되는 부분)
+    this.getUserAnswer(computerRandomNumber);
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
