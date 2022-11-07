e1092d453c88d99b16332741a6fe4087665a3d2d
Inae Choi <inaechoi89@gmail.com>
2022-11-07 18:10:15 +0900

    feat: set random number to answer

--- src/components/BaseballGame.js
+++ src/components/BaseballGame.js
@@ -2,6 +2,15 @@ const MissionUtils = require('@woowacourse/mission-utils');
 const gameConstant = require('./GameConstant');
 
 class BaseballGame {
+  constructor() {
+    this.answer = this.setAnswer();
+    this.startMessage = gameConstant.START_MESSAGE;
+  }
+
+  setAnswer() {
+    return MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3).join('');
+  }
+
   printStartMessage() {
     MissionUtils.Console.print(this.startMessage);
   }
