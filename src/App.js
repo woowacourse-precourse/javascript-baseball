c8ade1bac6b203abeb9386dc1a9b9c3560004c18
Miniling <bsm3925@naver.com>
2022-11-07 22:15:41 +0900

    Add: func_play

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  // 게임 시작
+  play() {
+    this.computer = [];
+    this.user = [];
+
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    this.computerSet();
+    this.userPlay();
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
