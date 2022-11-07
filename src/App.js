2d5c5634ccbd0863baf558ac7b0c970839f48b0e
Circlewee <pnj1118@naver.com>
2022-11-07 17:28:15 +0900

    feat(App): 유저의 입력값 처리 (#8)
    1. 적절한 입력값인지 검증
    2. 검증된 값을 저장

--- src/App.js
+++ src/App.js
@@ -2,11 +2,23 @@ const { Console } = require('@woowacourse/mission-utils');
 const computerUtils = require('./utils/computerUtils');
 
 class App {
-  answer = [];
+  answers = [];
+  userInputs = [];
 
   play() {
-    this.answer = computerUtils.getRandomNumber();
+    this.answers = computerUtils.getRandomNumber();
     Console.print('숫자 야구 게임을 시작합니다.');
+
+    const handleInput = (input) => {
+      computerUtils.validateInput(input);
+      this.userInputs = input.split('').map((input) => Number(input));
+    };
+
+    this.readLine(handleInput);
+  }
+
+  readLine(handleInputCallback) {
+    Console.readLine('숫자를 입력해주세요 : ', (input) => handleInputCallback(input));
   }
 }
 
