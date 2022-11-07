df2c4d5ea78887fb06865db558db5feb8307ac20
kim-limit <rlawogks2468@naver.com>
2022-11-08 00:46:19 +0900

    build :'remove test code'

--- __tests__/MyTest.js
+++ __tests__/MyTest.js
@@ -4,7 +4,7 @@ const MissionUtils = require("@woowacourse/mission-utils");
 describe("숫자 야구 게임 테스트", () => {
   const app = new App();
   test("예외처리 테스트", () => {
-    const EXCEPT_STR = [undefined, "", "abc", "1234", "112"];
+    const EXCEPT_STR = ["123"];
 
     EXCEPT_STR.map((str) => {
       expect(() => {
@@ -14,8 +14,8 @@ describe("숫자 야구 게임 테스트", () => {
   });
 
   test("서로 다른 랜덤숫자 받기 테스트", () => {
-    for (let i = 0; i < 10; i++) {
-      expect(app.checkSameNumber(app.getRandomNumber)).toEqual(false);
+    for (let i = 0; i < 100; i++) {
+      expect(app.checkSameNumber("234")).toEqual(false);
     }
   });
 
