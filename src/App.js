2a7a31957eacef911f5da884d257e97d11473a72
MJ <mj632900@gmail.com>
2022-11-08 02:22:38 +0900

    feat : 예외처리 추가

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 const MissionUtils = require("@woowacourse/mission-utils");
+
 const changeToArray = (number) => {
     return number
         .toString()
@@ -8,11 +9,14 @@ const changeToArray = (number) => {
 const printMassage = (message) => {
     return MissionUtils.Console.print(message);
 };
+
+let isFirst = true;
 class App {
     printStartMassage() {
-        MissionUtils.Console.print(`숫자 야구 게임을 시작합니다.`);
+        isFirst && MissionUtils.Console.print(`숫자 야구 게임을 시작합니다.`);
     }
     play() {
+        this.printStartMassage();
         let answer = this.createAnswer();
         // console.log(answer);
         this.getUserInput(answer);
@@ -66,37 +70,54 @@ class App {
     printEndMessage() {
         printMassage(`3스트라이크`);
         printMassage("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        return this.choosesNextStep();
     }
 
     choosesNextStep() {
         printMassage("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         MissionUtils.Console.readLine("", (number) => {
             if (number === "1") {
-                this.play();
+                isFirst = false;
+                return this.play();
             }
             if (number === "2") {
-                MissionUtils.Console.close();
+                return MissionUtils.Console.close();
             }
+
+            throw new Error("1이나 2를 입력해주세요");
         });
+    }
+
+    checkUserInput(input) {
+        if (input.includes(NaN)) {
+            throw new Error("숫자를 입력해주세요");
+        }
+        if (input.length !== 3) {
+            throw new Error("숫자 3개를 입력해주세요");
+        }
+        if (new Set(input).size !== 3) {
+            throw new Error("중복된 숫자가 있습니다.");
+        }
+        if (input.includes(0)) {
+            throw new Error("1~9 사이의 숫자만 입력해주세요");
+        }
     }
 
     getUserInput(answer) {
         MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (input) => {
             const INPUT_ARRAY = changeToArray(input);
+            this.checkUserInput(INPUT_ARRAY);
             if (this.isCorrect(INPUT_ARRAY, answer)) {
-                this.printEndMessage();
-                return this.choosesNextStep();
+                return this.printEndMessage();
             }
-
             this.inputResult(INPUT_ARRAY, answer);
-            this.getUserInput(answer);
+            return this.getUserInput(answer);
         });
     }
 }
 
 const app = new App();
 
-app.printStartMassage();
 app.play();
 
 module.exports = App;
