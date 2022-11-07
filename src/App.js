566c23991d31c34a676669f49afa8532f264e0e0
Minju Park <project.mizzu@gmail.com>
2022-11-08 02:56:26 +0900

    Feat : 사용자가 잘못된 값을 input에 작성했을 경우
    - 사용자가 3자리 숫자 외의 값을 입력했을 경우 throw, 종료

--- src/App.js
+++ src/App.js
@@ -16,6 +16,11 @@ class App {
       const USER_INPUT = input.split('').map((number) => number * 1);
       let countBall = 0;
       let countStrike = 0;
+
+      if (typeof USER_INPUT !== `number` && USER_INPUT.length !== 3) {
+        throw new Error('3자리 숫자만 입력해주세요!');
+      }
+
       for (let i = 0; i < COMPUTER.length; i += 1) {
         for (let j = 0; j < USER_INPUT.length; j += 1) {
           if (COMPUTER[i] === USER_INPUT[j]) {
