28e53361042e9eb8a44fe16be95cfc7b2019e60a
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2022-11-08 02:58:05 +0900

    refactor: 두 멘트를 한 멘트로 종합

--- src/data/Constants.js
+++ src/data/Constants.js
@@ -3,11 +3,11 @@ const GAME = Object.freeze({
   START_GETNUMBER: `숫자를 입력해주세요 :`,
 
   GAME_NOTHING: `낫싱`,
+  GAME_THREE_STRIKE: `3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료`,
 
-  GAME_END_MENTION: `3스트라이크 3개의 숫자를 모두 맞히셨습니다! 게임 종료`,
   END_RETRY_MENTION: `게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.`,
 
-  END_MENTION: `수고하셨습니다.`,
+  END_MENTION: `게임 종료`,
 });
 
 const ERROR = Object.freeze({
