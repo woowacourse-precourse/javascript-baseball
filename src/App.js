14958eae3fb15cb12aa73ddfb36af4422cc3e3ca
김희진 <choha941108@gmail.com>
2022-11-07 22:54:47 +0900

    fix(App): Convention 에 맞춤, eslint적용, pickNumberInRange 사용
    * Convention 에 맞추어 코드 포맷 변경
    * eslint(AirBnB 기준)에 맞추어 코드 컨벤션 변경
    * 문서에 맞추어, pickNumberInRange 를 사용하여 랜덤 숫자 생성 변경

--- src/App.js
+++ src/App.js
@@ -1,83 +1,92 @@
-const MissionUtils = require('@woowacourse/mission-utils');
+const MISSION_UTILS = require('@woowacourse/mission-utils');
+
+const TRUE = true;
+const RE_DO = '2';
+const DONE = '1';
 
 class App {
-
   play() {
-    MissionUtils.Console.print('숫자 게임을 시작 합니다');
-    while(true) {
+    MISSION_UTILS.Console.print('숫자 게임을 시작 합니다');
+    while (TRUE) {
       this.buildAnswer();
       let strike = 0;
-      let input;
-      while(strike != 3){
-          MissionUtils.Console.readLine(
-            '숫자를 입력해주세요 : ',
-            (_input) => {
-              input = _input;
-            }
-          );
-          MissionUtils.Console.close()
-          console.log("user input",input);
-          this.choiceAnswer(input);
-          console.log("list user", this.userSelect);
-          this.countStrike();
-          console.log("user, com", this.userSelect, this.answer);
-          console.log("strike", this.strike);
-          strike = this.strike;
-  
-          this.countBall();
-          console.log("ball", this.ball);
-  
-          this.printHint();
+      while (strike !== 3) {
+        let input;
+        MISSION_UTILS.Console.readLine(
+          '숫자를 입력해주세요 : ',
+          (_input) => {
+            input = _input;
+          },
+        );
+        MISSION_UTILS.Console.close();
+        this.choiceAnswer(input);
+        this.countStrike();
+        strike = this.strike;
+
+        this.countBall();
+
+        this.printHint();
       }
-      MissionUtils.Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
-      
+      MISSION_UTILS.Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+
       let select;
-      MissionUtils.Console.readLine(
+      MISSION_UTILS.Console.readLine(
         '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.',
         (start) => {
           select = start;
-        }
+        },
       );
-      MissionUtils.Console.close()
-      console.log("스타터",select)
-      if(select == "2") {
+      MISSION_UTILS.Console.close();
+      if (select === RE_DO) {
         break;
       }
-
+      if (select !== DONE) {
+        throw new Error('1 이나 2 중에 선택하지 않음');
+      }
     }
   }
+
   // build_answer
   buildAnswer() {
-    let answer;
-    do {
-      answer = MissionUtils.Random.pickUniqueNumbersInRange(0, 9, 3);
+    const ANSWER = [];
+
+    let randomNumber = 0;
+
+    for (let i = 0; i < 3; i += 1) {
+      do {
+        randomNumber = MISSION_UTILS.Random.pickNumberInRange(0, 9);
+      } while (ANSWER.includes(randomNumber));
+      ANSWER.push(randomNumber);
       // 첫 숫자가 0인경우 재생성
-    } while (answer[0] === 0);
-    this.answer = answer;
-    return answer;
+      if (i === 0 && randomNumber === 0) {
+        ANSWER.pop();
+        i -= 1;
+      }
+    }
+    this.answer = ANSWER;
+    return ANSWER;
   }
 
   choiceAnswer(userNums) {
     // 숫자이어야함
-    if (isNaN(userNums)) {
+    if (Number.isNaN(Number(userNums))) {
       throw new Error('userNums가 정수가 아님');
     }
     // 3자리어야함
-    if (userNums.length != 3) {
+    if (userNums.length !== 3) {
       throw new Error('userNums가 세자리가 아님');
     }
-    let userSelect = [...userNums].map(Number);
+    const USER_SELECT = [...userNums].map(Number);
 
     // 중복없어야함
-    const lenghtTest = [...new Set(userSelect)];
-    if (lenghtTest.length < 3) {
+    const LENGTH_TEST = [...new Set(USER_SELECT)];
+    if (LENGTH_TEST.length < 3) {
       throw new Error('중복 숫자가 존재합니다.');
     }
-    
-    this.userSelect;
-    this.userSelect = userSelect;
-    
-    return userSelect;
+
+    this.userSelect = USER_SELECT;
+
+    return USER_SELECT;
   }
 
   countStrike() {
@@ -95,8 +104,8 @@ class App {
   countBall() {
     let ball = 0;
     for (let i = 0; i < 3; i += 1) {
-      const index = this.userSelect.indexOf(this.answer[i]);
-      if (index !== -1 && index !== i) {
+      const INDEX = this.userSelect.indexOf(this.answer[i]);
+      if (INDEX !== -1 && INDEX !== i) {
         ball += 1;
       }
     }
@@ -105,20 +114,19 @@ class App {
   }
 
   printHint() {
-    const message = [];
+    const MESSAGE = [];
     if (this.ball >= 1) {
-      message.push(`${this.ball}볼`);
+      MESSAGE.push(`${this.ball}볼`);
     }
     if (this.strike >= 1) {
-      message.push(`${this.strike}스트라이크`);
+      MESSAGE.push(`${this.strike}스트라이크`);
     }
-    if (message.length === 0) {
-      message.push('낫싱');
+    if (MESSAGE.length === 0) {
+      MESSAGE.push('낫싱');
     }
 
-    MissionUtils.Console.print(message.join(' '));
+    MISSION_UTILS.Console.print(MESSAGE.join(' '));
   }
 }
-
 
 module.exports = App;
