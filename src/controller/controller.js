3acd6d1b3d465243caab87bc2ebb870dc3099e81
sh981013s <sh981013s@gmail.com>
2022-11-07 19:11:10 +0900

    feat: Controller constructor 구현

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -1,5 +1,14 @@
+const View = require("../view/view");
+const ComputerNumber = require("../model/computerNumber");
+const UserGivenNumber = require("../model/userGivenNumber");
+
 class Controller {
-  constructor() {}
+  constructor(isFirstGame) {
+    this.view = new View(this);
+    this.computerNumber = new ComputerNumber();
+    this.userGivenNumber = new UserGivenNumber();
+    this.isFirstGame = !!isFirstGame;
+  }
 }
 
 module.exports = Controller;
