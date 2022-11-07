ae0125c949fa524150ea87730a8444a98a7f1b03
yhnb3 <60080270+yhnb3@users.noreply.github.com>
2022-11-07 23:44:05 +0900

    Fix: checkRestartInputValidation fnc

--- src/libs/checkRestartInputValidation.js
+++ src/libs/checkRestartInputValidation.js
@@ -1,8 +1,8 @@
 const { GAME } = require("../constansts");
 
 const checkRestartInputValidation = (input) => {
-  if (!input === GAME.RESTART || !input === GAME.OVER)
-    throw new Error("1 또는 2를 입력해야합니다.");
+  if (input === GAME.RESTART || input === GAME.OVER) return;
+  throw new Error("1 또는 2를 입력해야합니다.");
 };
 
 module.exports = checkRestartInputValidation;
