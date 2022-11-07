98c99a690486544589be9e118f7520c7639b03c5
김세환 <sehwan@gimsehwan-ui-MacBookPro.local>
2022-11-07 23:53:07 +0900

    chore(Exception): MissionUtils.Console -> Console로 줄임

--- src/Exception.js
+++ src/Exception.js
@@ -1,4 +1,4 @@
-const MissionUtils = require("@woowacourse/mission-utils");
+const { Console } = require("@woowacourse/mission-utils");
 
 const THREE_DIGITS = 3;
 const FIRST_PLACE = 0;
@@ -9,22 +9,22 @@ const ZERO = "0";
 function exception(userNum) {
     // 길이가 3자리인지 확인, 3자리 전부 일의 자리 숫자인지 확인 가능.
     if (userNum.length !== THREE_DIGITS) {
-        MissionUtils.Console.close();
+        Console.close();
         throw "ERROR : 숫자의 길이가 올바르지 않습니다.";
     }
     // 서로 다른 수인지 확인
     if (userNum[FIRST_PLACE] === userNum[SECONED_PLACE] || userNum[SECONED_PLACE] === userNum[THIRD_PLACE] || userNum[FIRST_PLACE] === userNum[THIRD_PLACE]) {
-        MissionUtils.Console.close();
+        Console.close();
         throw "ERROR : 중복되지 않는 숫자를 입력해주세요.";
     }
     // 숫자가 아닐경우
     if (isNaN(userNum)) {
-        MissionUtils.Console.close();
+        Console.close();
         throw "ERROR : 숫자를 입력해주세요."
     }
     // 0을 넣었을 경우
     if (userNum.includes(ZERO)) {
-        MissionUtils.Console.close();
+        Console.close();
         throw "ERROR : 1 ~ 9 사이의 값을 입력해주세요.";
     }
 }
