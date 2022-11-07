44ae0c9e0259b7791bb371ac643681e269aa1b18
jmk5315 <31719854+jmk5315@users.noreply.github.com>
2022-11-08 03:44:34 +0900

    feat : 테스트 기능 4 구현

--- src/Game.js
+++ src/Game.js
@@ -1,13 +1,33 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
+
 class Game{
+
 
     getUserInput() {
         MissionUtils.Console.readLine("숫자를 입력해주세요 :", (numbers) => {
-            console.log(numbers)
+            this.checkValidationUserInput(numbers);
         });
     }
 
+    checkValidationUserInput(userNumber) {
+        if (isNaN(userNumber) === true) {
+          throw "숫자를 입력해주세요.";
+        }
+        if (userNumber.length !== 3) {
+          throw "3자리로 입력해주세요.";
+        }
+        if (new Set(userNumber).size !== 3) {
+          throw "서로 다른 값을 입력해주세요.";
+        }
+        if(userNumber.includes('0') === true){
+            throw "0은 입력하면 안됩니다.";
+        }
+    }
+
+    
+   
+    
     
 }
 
