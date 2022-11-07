c234912481445731dd5d853964c28cfe2758c148
sunny <huiseon37@gmail.com>
2022-11-07 23:57:14 +0900

    feat: 재시작, 종료 선택 함수 구현

--- src/App.js
+++ src/App.js
@@ -11,6 +11,15 @@ class User {
       if(isPlay){
         this.input();
       }
+      this.selectRePlay();
+    });
+  }
+  selectRePlay(){
+    MissionUtils.Console.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.', (selectReplay) => {
+      if(selectReplay === 1){
+        this.input();
+      }
+      //끝내기
     })
   }
 }
