2cda4abf6e1053c15eda085a84d699209dec8c5e
nailen1 <nietsnie1@gmail.com>
2022-11-07 21:54:07 +0900

    feat: 정답 숫자 생성 기능

--- src/App.js
+++ src/App.js
@@ -6,4 +6,13 @@ function printMessage(sentence) {
   return Console.print(sentence)
 }
 
+function generateAnswer() {
+  let numsArr = [];
+  while (numsArr.length < 3) {
+    let num = Random.pickNumberInRange(1, 9);
+    if (!numsArr.includes(num)) numsArr.push(num);
+  }
+  return numsArr.join('')
+}
+
 module.exports = App;
