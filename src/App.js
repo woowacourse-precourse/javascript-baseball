dc7fd526b13d7c7bcc1919baa60ff6f0a76dfcb1
xownsdl0 <xownsdl0@naver.com>
2022-11-07 21:46:40 +0900

    feat: 스트라이크 개수 확인하는 함수 추가

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ const isNumber = require("../src/IsNumber")
 class App {
   play() {
     const computer = this.computerRandomNumber();
-    this.getUserInput()
+    this.getUserInput(computer)
   }
 
   computerRandomNumber() {
@@ -19,13 +19,30 @@ class App {
     return computer;
   }
 
-  getUserInput() {
+  getUserInput(computer) {
     MissionUtils.Console.readLine(`숫자를 입력해주세요 : `, number => {
       if(isNumber(number)) {
-        MissionUtils.Console.print("숫자 입니다.")
+        this.checkStrike(computer, number)
       }
     });
   }
+
+  checkStrike(computer, user) {
+    let countStrike = 0;
+  
+    Array.from(user, Number).map((currentValue, index) => {
+      if (
+        computer.includes(currentValue) 
+        && computer.indexOf(currentValue) === index
+      ) {
+        countStrike += 1;
+      }
+  
+      return countStrike;
+    });
+  
+    return MissionUtils.Console.print(countStrike)
+  };
 }
 
 module.exports = App;
