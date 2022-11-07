08e02befabf2bc6239994ef4f2d49aeda741517e
letsjo <hyunoh.jo@gmail.com>
2022-11-07 22:57:41 +0900

    feat(App): 기능1 게임시작 알림 메소드 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+// 모듈 선언
+const { Console, Random } = require('@woowacourse/mission-utils');
+
 class App {
-  play() {}
+  showStartMessage() {
+    Console.print('숫자 야구 게임을 시작합니다.');
+  }
+
+  play() {
+    this.showStartMessage();
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
