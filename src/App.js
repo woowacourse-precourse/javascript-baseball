341281c7a205761f9d46f274b0c9bbe87bd1137f
lyssoi <114637463+lyssoi@users.noreply.github.com>
2022-11-07 17:21:17 +0900

    컴퓨터 숫자 생성하는 기능
    세자리의 중복되지 않은 랜덤 숫자 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
+const { MissionUtils } = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  play() {
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.")
+  }
 }
+
+const gamestart = () =>{
+
+}
+
+const createComputerNumber = () =>{
+  const computerNumber = [];
+  while (computerNumber.length<3){
+    const randomNumber = MissionUtils.Random.pickNumberInRange(1, 9);
+    if (!computerNumber.includes(randomNumber)){
+      computerNumber.push(randomNumber);
+    }
+  }
+  return computerNumber.join("")
+}
+
+const inputUserNumber =() =>{
+  
+  });
+}
+
+const judgeNumber = () =>{}
 
 module.exports = App;
