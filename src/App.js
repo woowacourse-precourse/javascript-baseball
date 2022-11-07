0478d71f8a8ffc5d67465d6602bc6903e376b60b
김혜원 <dev.blair06@gmail.com>
2022-11-07 14:53:07 +0900

    feat:#blair06 컴퓨터 난수 생성 - constructor 구현 - 게임시작 문구 출력 - 컴퓨터 난수 생성 메소드 구현 - 서로 다른 세자릿수 체크

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+const MissionUtils = require("@woowacourse/mission-utils");
 class App {
-  play() {}
+  constructor() {
+    this.computerNumber = [];
+  }
+
+  createRandomNumber() {
+    while (this.computerNumber.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!this.computerNumber.includes(number)) {
+        this.computerNumber.push(number);
+      }
+    }
+  }
+
+  play() {
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    this.createRandomNumber();
+  }
 }
 
 module.exports = App;
