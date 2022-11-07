cd7be16837909f291e84f8a812e939c44d587c12
Circlewee <pnj1118@naver.com>
2022-11-07 17:29:29 +0900

    test(App): 입력값 처리 테스트 (#8)

--- __tests__/GameTest.js
+++ __tests__/GameTest.js
@@ -4,20 +4,38 @@ const MissionUtils = require('@woowacourse/mission-utils');
 
 const getLogSpy = () => {
   const logSpy = jest.spyOn(MissionUtils.Console, 'print');
-  logSpy.mockClear();
+
   return logSpy;
 };
 
-describe('Game test', () => {
-  const app = new App();
+const mockReadline = (input) => {
+  MissionUtils.Console.readLine = jest.fn();
+  MissionUtils.Console.readLine.mockImplementationOnce((_, callback) => {
+    callback(input);
+  });
+};
 
+describe('Game test', () => {
   test('게임 시작 테스트', () => {
     const logSpy = getLogSpy();
     const INIT_MESSAGE = '숫자 야구 게임을 시작합니다.';
 
+    const app = new App();
     app.play();
 
-    expect(app.answer).toHaveLength(COMPUTER.ANSWER_LENGTH);
+    expect(app.answers).toHaveLength(COMPUTER.ANSWER_LENGTH);
     expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(INIT_MESSAGE));
+  });
+
+  test('입력값 처리 테스트', () => {
+    const input = '123';
+    const expectValue = [1, 2, 3];
+
+    mockReadline(input);
+
+    const app = new App();
+    app.play();
+
+    expect(app.userInputs).toEqual(expectValue);
   });
 });
