5e4d818cfa9a513778163836119954ab9d3c18ba
Steadystudy <sgmin206@naver.com>
2022-11-08 01:25:21 +0900

    feat: 3자리 수 만들기

--- src/App.js
+++ src/App.js
@@ -1,4 +1,22 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+
 class App {
+  #answer;
+
+  constructor() {
+    this.makeAnswer();
+  }
+
+  makeAnswer() {
+    this.#answer = [];
+    while (this.#answer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!this.#answer.includes(number)) {
+        this.#answer.push(number);
+      }
+    }
+  }
+
   play() {}
 }
 
