0f7d4a9bb43ba673ddcb3744b70a905a8082899a
huyntohoon <81351526+hyuntohoon@users.noreply.github.com>
2022-11-08 07:34:20 +0900

    feat : Game 시작 객체 생성 및 시작

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
-class App {
-  play() {}
-}
+const Game = require("./game");
 
+const game = new Game();
+class App {
+  play() {
+    game.play();
+  }
+}
+const app = new App();
+app.play();
 module.exports = App;
