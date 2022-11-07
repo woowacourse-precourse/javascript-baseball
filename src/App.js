e4913e2b9e3beb6b289f2ebae769d9651b648130
Steadystudy <sgmin206@naver.com>
2022-11-08 01:46:46 +0900

    feat: 받은 값 예외 처리하기

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 
+const GAME_START_COMMENT = '숫자 야구 게임을 시작합니다.';
+const INPUT_NUMBER = '숫자를 입력해주세요 : ';
+
+const ERROR_MESSAGE = {
+  TYPE: '숫자가 아닙니다',
+  LENGTH: '3자리 숫자여야 합니다',
+  GAME_END: '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.',
+};
 class App {
   #answer;
 
   constructor() {
+    MissionUtils.Console.print(GAME_START_COMMENT);
     this.makeAnswer();
   }
 
@@ -17,7 +26,16 @@ class App {
     }
   }
 
-  play() {}
+  isValidNumber(value) {
+    if (!Number(value)) throw new Error(ERROR_MESSAGE.TYPE);
+    if (value.length !== 3) throw new Error(ERROR_MESSAGE.LENGTH);
+  }
+
+  play() {
+    MissionUtils.Console.readLine(INPUT_NUMBER, (value) => {
+      this.isValidNumber(value);
+    });
+  }
 }
 
 module.exports = App;
