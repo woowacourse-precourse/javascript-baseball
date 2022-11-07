5c2298dc52b805e7bc1ef02c072fee7b58fb0514
JSH99 <tjgus9966@naver.com>
2022-11-07 21:27:21 +0900

    feat(inputNumbers): 숫자 입력 문구 및 숫자 출력

--- src/App.js
+++ src/App.js
@@ -30,6 +30,8 @@ const inputNumbers = () => {
   let userNum;
 
   MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (inputNum) => {
+    MissionUtils.Console.print(`숫자를 입력해주세요 : ${inputNum}`);
+
     if (!isVaildNumber(inputNum)) {
       throw new Error('잘못된 값입니다. 1부터 9까지 서로 다른 3자리 수를 입력하세요.');
     }
