8dc065cc80ccc9d68a592a8256f65ac3d5a4cd6c
pakxe <pigkill40@naver.com>
2022-11-08 04:56:14 +0900

    test: 테스트 케이스의 이름을 알아보기 쉽게 수정

--- __tests__/CheckAnswerTest.js
+++ __tests__/CheckAnswerTest.js
@@ -5,40 +5,40 @@ const {
 } = require('../src/ControlAnswer/CheckAnswer');
 
 describe('스트라이크 개수', () => {
-  test('case1', () => {
+  test('3스트라이크', () => {
     expect(strikeCount([1, 2, 3], [1, 2, 3])).toEqual(3);
   });
-  test('case2', () => {
+  test('2스트라이크', () => {
     expect(strikeCount([1, 2, 3], [1, 2, 4])).toEqual(2);
   });
-  test('case3', () => {
+  test('1스트라이크', () => {
     expect(strikeCount([4, 6, 7], [8, 6, 9])).toEqual(1);
   });
-  test('case4', () => {
+  test('0스트라이크', () => {
     expect(strikeCount([1, 2, 3], [4, 5, 6])).toEqual(0);
   });
 });
 
 describe('볼 개수', () => {
-  test('case1', () => {
+  test('3볼', () => {
     expect(ballCount([1, 2, 3], [3, 1, 2])).toEqual(3);
   });
-  test('case2', () => {
+  test('2볼', () => {
     expect(ballCount([1, 2, 3], [2, 1, 4])).toEqual(2);
   });
-  test('case3', () => {
+  test('1볼', () => {
     expect(ballCount([1, 2, 3], [3, 4, 5])).toEqual(1);
   });
-  test('case4', () => {
+  test('0볼', () => {
     expect(ballCount([1, 2, 3], [4, 5, 6])).toEqual(0);
   });
 });
 
 describe('낫싱 검사', () => {
-  test('case1', () => {
-    expect(isNothing(1, 2)).toEqual(false);
+  test('낫싱 아님', () => {
+    expect(isNothing(1, 2)).toBeFalsy();
   });
-  test('case2', () => {
-    expect(isNothing(0, 0)).toEqual(true);
+  test('낫싱임', () => {
+    expect(isNothing(0, 0)).toBeTruthy();
   });
 });
