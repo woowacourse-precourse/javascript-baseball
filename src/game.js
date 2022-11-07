e283258c26e5347dfdb3aa1c470fc7a55f3e66ab
huyntohoon <81351526+hyuntohoon@users.noreply.github.com>
2022-11-08 08:49:38 +0900

    fix : 잘못된 변수명 수정

--- src/game.js
+++ src/game.js
@@ -7,26 +7,34 @@ const {
 } = require("./constants");
 const { Console, Random } = require("@woowacourse/mission-utils");
 class Game {
+  print(message) {
+    return Console.print(message);
+  }
   play() {
     this.init();
+    this.getNumber(this.answerNumber);
   }
   init() {
     this.print(MESSAGE.START);
     this.answerNumber = this.createRandomNumber();
   }
   createRandomNumber() {
-    let ramdomNumberArr = [];
-    while (this.ramdomNumberArr.length < LENGTH) {
-      const N = Random.piickNumberInRange(MIN, MAX);
-      if (!ramdomNumberArr.includes(N)) {
-        ramdomNumberArr.push(N);
+    let randomNumberArr = [];
+    while (randomNumberArr.length < RANDOM_NUMBER.LENGTH) {
+      const N = Random.pickNumberInRange(
+        RANDOM_NUMBER.MIN,
+        RANDOM_NUMBER.MAX
+      );
+      if (!randomNumberArr.includes(N)) {
+        randomNumberArr.push(N);
       }
     }
-    return ramdomNumberArr;
+    return randomNumberArr;
   }
-  print(message) {
-    return Console.log(print(message));
+  getNumber(answer) {
+    //console.readLine();
   }
 }
-
+const game = new Game();
+game.play();
 module.exports = Game;
