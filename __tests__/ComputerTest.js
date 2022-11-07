2826b1da053b6a067ca242df1d7b5a5ebda403b9
woong <inwoong100@gmail.com>
2022-11-07 22:42:45 +0900

    test(computer): setRandomNumber 테스트 작성

--- __tests__/ComputerTest.js
+++ __tests__/ComputerTest.js
@@ -1,9 +1,19 @@
 const Computer = require('../src/Computer');
 
 describe('Computer class 테스트', () => {
+  let computer;
+
+  beforeEach(() => {
+    computer = new Computer();
+  });
+
   test('getRandomNumber 값 테스트', () => {
-    const computer = new Computer();
     const number = computer.getRandomNumber();
     expect(number).not.toEqual(0);
+  });
+
+  test('setRandomNumber 값 반환 테스트', () => {
+    computer.setRandomNumber();
+    expect(computer.selectedNumber.length).toBe(3);
   });
 });
