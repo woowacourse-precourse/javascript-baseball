2ad74fa6037d524a1f6115bbf49c7996f135e38c
hjy0951 <hjy0951@gmail.com>
2022-11-07 21:02:52 +0900

    feat: App 내부에 Controller 생성하여 기본 게임 로직 구현

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 const { Console } = require("@woowacourse/mission-utils");
 const Computer = require("./Computer");
+const Controller = require("./Controller");
 
 class App {
   constructor() {
     this._computer = new Computer();
-    /* new Controller */
+    this._controller = new Controller();
     this.MESSAGE = {
       START : "숫자 야구 게임을 시작합니다.",
       USER_ANSWER : "숫자를 입력해주세요 : ",
       GAME_OVER : "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
-      USER_SELECT : "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
+      USER_SELECT : "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n"
     }
   }
 
   play() {
-    Console.print(this.MESSAGE.START);
+    this.printMessage(this.MESSAGE.START);
     this.start();
   }
 
   start() {
+    console.log(this._computer.answer);
     this.getUserAnswer();
   }
 
@@ -26,11 +28,27 @@ class App {
     /** readline -> question으로 구현되어 있어 한번 input 받은 후 pause 
      *  따라서 callback으로 계속 실행될 수 있도록 구현해야 할 것 같다. 
      */
-    Console.readLine(this.MESSAGE.USER_ANSWER, function (input) {
-      /* 입력을 배열 형태로 쪼갬 */
-      /* Controller에서 유효성 검사 */
-      /* 유효하다면 Controller에서 정답 판별 */
-      /* 정답이라면 게임 재시작 or 게임 종료 여부를 유저에게 질문 */ 
+    Console.readLine(this.MESSAGE.USER_ANSWER, (input) => {
+      const numbers = this.splitInput(input);
+      if (this._controller.isValidInput(numbers) === false) {
+        throw "잘못된 입력입니다.";
+      };
+      const hint = this._controller.compareAnswer(numbers, this._computer.answer);
+      if (hint.length === 0) {
+        this.printMessage(this.MESSAGE.GAME_OVER);
+        this.getUserSelect();
+      } else {
+        this.printMessage(hint);
+        this.start();
+      }
+    });
+  }
+
+  getUserSelect() {
+    Console.readLine(this.MESSAGE.USER_SELECT, (input) => {
+      if (input === '1') this.restart();
+      else if (input === '2') this.finish();
+      else throw "잘못된 입력입니다.";
     });
   }
 
@@ -48,6 +66,12 @@ class App {
       return Number(number);
     });
   }
+
+  printMessage(message) {
+    Console.print(message);
+  }
 }
 
+const app = new App();
+app.play();
 module.exports = App;
