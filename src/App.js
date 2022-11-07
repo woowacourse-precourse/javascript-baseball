7b12e5e9e52c958e4f8e6fb97501268d87ace6ad
feb-dain <feb.dain@gmail.com>
2022-11-07 09:27:20 +0900

    feat: print result

--- src/App.js
+++ src/App.js
@@ -42,6 +42,28 @@ class App {
     return this.result(strike, ball);
   }
 
+  result(strike, ball) {
+    if (strike === 3) {
+      CONSOLE_UTIL.print(`${strike}스트라이크`);
+      CONSOLE_UTIL.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+      this.endGame();
+    }
+    if (strike === 0) {
+      if (ball === 0) CONSOLE_UTIL.print("낫싱");
+      else {
+        CONSOLE_UTIL.print(`${ball}볼`);
+      }
+      return this.acceptUserNumber();
+    }
+    if (strike !== 0 && strike !== 3) {
+      if (ball === 0) CONSOLE_UTIL.print(`${strike}스트라이크`);
+      else {
+        CONSOLE_UTIL.print(`${ball}볼 ${strike}스트라이크`);
+      }
+      return this.acceptUserNumber();
+    }
+  }
+
   play() {}
 }
 
