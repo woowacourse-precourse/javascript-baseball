17fbb6f92ef6a5ab01cdae56e4dce15fd5a352fe
Inae Choi <inaechoi89@gmail.com>
2022-11-07 22:08:55 +0900

    feat: add a function to validate number

--- src/components/HandlingException.js
+++ src/components/HandlingException.js
@@ -4,4 +4,10 @@ const checkDuplicate = (inputArray, digit) => {
   return [...new Set(inputArray)].length !== digit;
 };
 
+const checkValidNumber = inputArray => {
+  const inputString = inputArray.join('');
+  const regExp = /[^1-9]/;
+  return regExp.test(inputString);
+};
+
 module.exports = handleException;
