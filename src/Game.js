973c5a122bd01db116f42d8375a9e59c0389929a
spsp8426 <spsp8426@naver.com>
2022-11-07 13:58:34 +0900

    feat : 예외처리 구현

--- src/Game.js
+++ src/Game.js
@@ -1,6 +1,7 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 const { compareInputNum } = require("./countNum");
 const { createComputerNums } = require("./computerNum.js");
+const { checkuserNums, checkuserNums2 } = require("./checkError.js");
 const { Console } = require("@woowacourse/mission-utils");
 
 function gameStart() {
@@ -11,7 +12,7 @@ exports.gameStart = gameStart;
 
 function gameplay(computerNums) {
   Console.readLine("숫자를 입력해주세요 : ", (userNums) => {
-   /*예외 처리 구현 라인 */
+    if (!checkuserNums(userNums) || !checkuserNums2(userNums)) throw "잘못된 입력입니다.";
     Console.print(compareInputNum(computerNums, userNums));
     if (computerNums !== userNums) return gameplay(computerNums);
     Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
--- src/checkError.js
+++ src/checkError.js
@@ -8,3 +8,15 @@ function checkuserNums(input) {
   return true;
 }
 exports.checkuserNums = checkuserNums;
+
+function checkuserNums2(input) {
+  const inputArr = input.split("");
+  if (
+    inputArr[0] === inputArr[1] ||
+    inputArr[1] === inputArr[2] ||
+    inputArr[0] === inputArr[2]
+  )
+    return false;
+  return true;
+}
+exports.checkuserNums2 = checkuserNums2;
