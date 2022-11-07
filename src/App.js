624424d3907e8726b16b05c1ff8e415945b25d83
ho-ji <wkdd0205@gmail.com>
2022-11-07 21:25:46 +0900

    feat: Add startGame function
    컴퓨터의 수를 생성하고 게임을 시작하는 함수를 구현함

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ const MissionUtils = require("@woowacourse/mission-utils");
 class App {
   play() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    this.startGame();
+  }
+  startGame() {
+    const computerNumber = createComputerNumber();
   }
 }
 function createComputerNumber() {
