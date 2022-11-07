02dc1b07a64669a3b4ebf2fb9d14be5f39eccbec
hyukzz <ri_chard@naver.com>
2022-11-07 16:12:06 +0900

    feat: app.js 야구게임 구현

--- src/App.js
+++ src/App.js
@@ -1,38 +1,92 @@
-import useBall from "./utils/useBall";
-import useStrike from "./utils/useStrike";
-import isCpuInput from "./utils/isCpuInput";
-import restartGame from "./utils/restartGame";
+const { Console, Random } = require("@woowacourse/mission-utils");
+const { MESSAGE, RESULTGAME, RESULT } = require("./constants");
 
 class App {
-	constructor(app, input, contanier) {
-		this.app = app;
-		this.input = input;
-		this.contanier = contanier;
-		this.cpuInput = isCpuInput();
-	}
+	isDuplicated = str => {
+		return str.length !== new Set(str).size;
+	};
 
-	static play(cpuInput, userInput) {
-		if (cpuInput === userInput) return "성공";
+	inputValidation = str => {
+		if (
+			!Number.isInteger(Number(str)) ||
+			str.length !== 3 ||
+			str.includes(0) ||
+			this.isDuplicated(str)
+		) {
+			throw new Error(MESSAGE.ERROR);
+		}
+	};
 
-		const ball = useBall(cpuInput, userInput);
-		const strike = useStrike(cpuInput, userInput);
+	computerRandomInput = () => {
+		const computer = [];
+		while (computer.length < 3) {
+			const number = Random.pickNumberInRange(1, 9);
+			if (!computer.includes(number)) {
+				computer.push(number);
+			}
+		}
 
-		if (ball === 0 && strike === 0) return "낫싱";
+		return computer.join("");
+	};
+
+	printResult = (userInput, computerInput) => {
+		let strike = 0;
+		let ball = 0;
+		let nothing = true;
+
+		for (let i = 0; i < computerInput.length; i++) {
+			if (computerInput.includes(userInput[i])) {
+				nothing = false;
+				if (userInput[i] === computerInput[i]) {
+					strike++;
+				} else {
+					ball++;
+				}
+			}
+		}
+
+		if (nothing) {
+			return Console.print(RESULT.NOTHING);
+		}
 
 		let result = "";
 
-		if (ball) result += `${ball}}볼`;
-		if (strike) result += `${strike}스트라이크`;
+		if (ball) result += `${ball}${RESULT.BALL} `;
+		if (strike) result += `${strike}${RESULT.STRIKE}`;
 
-		return result;
-	}
+		Console.print(result);
+		strike === 3 && this.successResult();
+	};
 
-	restart(e) {
-		e.preventDefault();
-		restartGame(this.contanier);
-		this.cpuInput = isCpuInput();
-		this.input.value = "";
-	}
+	successResult = () => {
+		Console.print(MESSAGE.SUCCESS);
+
+		Console.readLine(MESSAGE.RESULTGAME, input => {
+			if (input === RESULTGAME.RESTART) {
+				this.play();
+			} else if (input === RESULTGAME.CLOSE) {
+				Console.close();
+			} else {
+				throw new Error(MESSAGE.ERROR);
+			}
+		});
+	};
+
+	userInputCompared = computerInput => {
+		Console.readLine(MESSAGE.INPUT_NUMBER, userInput => {
+			this.inputValidation(userInput);
+			this.printResult(userInput, computerInput);
+			this.userInputCompared(computerInput);
+		});
+	};
+
+	play = () => {
+		Console.print(MESSAGE.START);
+		this.userInputCompared(this.computerRandomInput());
+	};
 }
+
+const baseballGame = new App();
+baseballGame.play();
 
 module.exports = App;
