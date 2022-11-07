85b89d9139080a3fbeb1d0e0330665a209aaa260
c0c0pang <coc1789@naver.com>
2022-11-08 04:41:25 +0900

    fix: 파일 분리시 우테코 사이트에서 오류가 발생하여 파일 삭제

--- src/GameStart.js
+++
@@ -1,8 +0,0 @@
-const startMessage = require('./StartMessage');
-const selectGame = require('./SelectGame');
-const gameStart = (USER) => {
-    startMessage();
-    selectGame(USER);
-};
-
-module.exports = gameStart; 
--- src/StartMessage.js
+++
@@ -1,6 +0,0 @@
-const MissionUtils = require("@woowacourse/mission-utils");
-
-const startMessage = () => {
-  MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
-};
-module.exports = startMessage;
