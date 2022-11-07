628f2464dd4c4007442166a2cb86685c05e5a75c
MINJOO-KIM <gkminju2000@gmail.com>
2022-11-07 22:47:05 +0900

    feat: 컴퓨터의 서로 다른 3개의 수를 설정한다

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
   play() {}
 
   gameStart() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+  }
+
+  generateRandomNumber() {
+    const randomNumbers = new Set();
+
+    while (randomNumbers.size < 3) {
+      const pickedNumber = MissionUtils.Random.pickNumberInRange(1, 9);
+      randomNumbers.add(pickedNumber.toString());
+    }
+
+    return Array.from(randomNumbers).join("");
   }
 }
 
