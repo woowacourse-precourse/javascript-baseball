250511cc6fceb3f77c35cbd1e9104d902fda7a5e
cloudedpanther <rickeykoh@ajou.ac.kr>
2022-11-07 22:28:50 +0900

    feat(class): BallCount
    Add BallCount class

---
+++ src/BallCount.js
@@ -0,0 +1,16 @@
+class BallCount {
+  constructor(question, answer) {
+    this.strikes = null;
+    this.balls = null;
+  }
+
+  countStrikes(question, answer) {}
+
+  countBalls(question, answer, strikes) {}
+
+  toString() {}
+
+  isThreeStrikes() {}
+}
+
+module.exports = BallCount;
