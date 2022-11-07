a6ebadf8b67c826f13cc59838c5b1cdd52d58908
박승훈 <tmdgns1126@naver.com>
2022-11-07 23:21:25 +0900

    feat: [App] 게임 시작 문구 출력 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+const { Console } = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  play() {
+    Console.print("숫자 야구 게임을 시작합니다.");
+    this.startGame();
+  }
+
+  startGame() {}
 }
 
 module.exports = App;
