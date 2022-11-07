748f0da74a3e2e6d49fa48287e761db6e751ca37
HeoJiye <heoze1031@gmail.com>
2022-11-08 01:27:12 +0900

    test(describe): 예외 처리 테스트 추가
    4가지 예외처리를 테스트 생성 + 2가지 예제 테스트 추가

--- __tests__/MyTest.js
+++ __tests__/MyTest.js
@@ -36,16 +36,69 @@ const getLogSpy = () => {
 };
 
 describe("숫자 야구 게임 (MyTest)", () => {
-    test("게임 종료 후 재시작", () => {
-        const randoms = [1, 3, 5, 5, 8, 9];
-        const answers = ["246", "135", "1", "597", "589", "2"];
+    test("예외처리 1. 길이가 3이 아닌 값을 입력할 경우", () => {
+        const randoms = [1, 3, 5];
+        const answers = ["24"];
+    
+        mockRandoms(randoms);
+        mockQuestions(answers);
+    
+        expect(() => {
+            const app = new App();
+            app.play();
+          }).toThrow();
+      });
+    
+      test("예외 처리 2. 숫자가 아닌 값을 포함한 값을 입력할 경우", () => {
+        const randoms = [1, 3, 5];
+        const answers = ["1a4"];
+    
+        mockRandoms(randoms);
+        mockQuestions(answers);
+    
+        expect(() => {
+          const app = new App();
+          app.play();
+        }).toThrow();
+      });
+
+      test("예외 처리 3. 0이 포함된 수를 입력할 경우", () => {
+        const randoms = [1, 3, 5];
+        const answers = ["014"];
+    
+        mockRandoms(randoms);
+        mockQuestions(answers);
+    
+        expect(() => {
+          const app = new App();
+          app.play();
+        }).toThrow();
+      });
+
+      test("예외 처리 4. 중복된 값이 있는 수를 입력할 경우", () => {
+        const randoms = [1, 3, 5];
+        const answers = ["335"];
+    
+        mockRandoms(randoms);
+        mockQuestions(answers);
+    
+        expect(() => {
+          const app = new App();
+          app.play();
+        }).toThrow();
+      });
+
+      test("예제 1", () => {
+        const randoms = [1, 3, 5];
+        const answers = ["467", "389", "148", "513", "135", "2"];
         const logSpy = getLogSpy();
         const messages = [
           "낫싱",
+          "1볼",
+          "1스트라이크",
+          "3볼",
           "3스트라이크",
-          "1볼 1스트라이크",
-          "3스트라이크",
-          "게임 종료",
+          "게임 종료"
         ];
     
         mockRandoms(randoms);
@@ -58,18 +111,28 @@ describe("숫자 야구 게임 (MyTest)", () => {
           expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
         });
       });
-    
-      test("예외 테스트", () => {
-        const randoms = [1, 3, 5];
-        const answers = ["1234"];
+
+      test("예제 2", () => {
+        const randoms = [1, 3, 5, 2, 4, 6];
+        const answers = ["123", "135", "1", "513", "246", "2"];
+        const logSpy = getLogSpy();
+        const messages = [
+          "1스트라이크",
+          "3스트라이크",
+          "낫싱",
+          "3스트라이크",
+          "게임 종료"
+        ];
     
         mockRandoms(randoms);
         mockQuestions(answers);
     
-        expect(() => {
-          const app = new App();
-          app.play();
-        }).toThrow();
+        const app = new App();
+        app.play();
+    
+        messages.forEach((output) => {
+          expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+        });
       });
   });
   
