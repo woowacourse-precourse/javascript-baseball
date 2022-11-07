f65ab6080599011739ea279ef376e5d5a927e959
Wang HoEun <hoeunwang@gmail.com>
2022-11-07 10:49:58 +0900

    feat: user input 값 중복여부 확인 함수 생성

--- src/component/User.js
+++ src/component/User.js
@@ -2,4 +2,8 @@ class User {
   checkInputLength(userInput) {
     return userInput.length === 3;
   }
+  checkInputExist(userInput) {
+    const isExist = new Set([...userInput]).size !== 3;
+    return isExist === false;
+  }
 }
