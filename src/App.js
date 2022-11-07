5dd7aca32eb0c0d8c22da02ea8fe7c1a9b276a10
김아현 <dhflxhdxhd@gmail.com>
2022-11-07 21:54:24 +0900

    Feat : 재시작 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
+  isRestart(input) {
+    if (input == 1) {
+      this.startGame();
+    }
+  }
+  
   showCommand() {
     MissionUtils.Console.readLine(
       "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
