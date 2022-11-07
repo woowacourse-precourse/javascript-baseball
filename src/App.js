9329d8d3970a902e2bb1e5a5633978f65620c1d9
khj0426 <706shin1728@naver.com>
2022-11-07 20:36:48 +0900

    refactor:입력,스트라이크 볼 판별,출력 분리

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 const message = require('./MESSAGE');
+const UserInput = require('./UserInput');
 const StrikeBall = require('./StrikeBall');
 
 class App {
   constructor() {
-    this.strike = 0;
-    this.ball = 0;
-    this.RANGE = 3;
-    this.UserInputNumber = 'InitINPUT';
-  }
-
-  input() {
-    MissionUtils.Console.readLine('숫자를 입력하세요', (InputNumber) => {
-      MissionUtils.Console.print(InputNumber);
-      this.UserInputNumber = InputNumber;
-    });
+    this.MAXRANGE = 3;
   }
 
   CreateRandom() {
     const RandomArr = [];
-    while (RandomArr.length < 3) {
+    while (RandomArr.length < this.MAXRANGE) {
       const number = MissionUtils.Random.pickNumberInRange(1, 9);
       if (!RandomArr.includes(number)) {
         RandomArr.push(number);
@@ -28,44 +19,33 @@ class App {
     return RandomArr;
   }
 
-  CheckInputIsValid() {
-    const CheckSet = new Set();
-    const UserInputArray = [...this.UserInputNumber];
-    UserInputArray.forEach((EachChar) => {
-      CheckSet.add(EachChar);
-    });
-    if (CheckSet.size !== 3) {
-      return false;
-    }
-    return true;
-  }
-
   play() {
     this.Number = this.CreateRandom().join('');
-    MissionUtils.Console.print('숫자 야구 게임을 시작합니다!');
+    MissionUtils.Console.print(message.START);
     let strikeball = new StrikeBall();
+    const userinput = new UserInput();
     while (true) {
-      this.input();
-      if (this.CheckInputIsValid(this.UserInputNumber) === false) {
+      userinput.GetInput();
+      if (userinput.CheckInputIsValid(userinput.UserInputNumber) === false) {
         break;
       }
       strikeball = new StrikeBall();
       strikeball.InitStrikeBall();
-      strikeball.GetStrikeBall(this.UserInputNumber, this.Number);
+      strikeball.GetStrikeBall(userinput.UserInputNumber, this.Number);
       strikeball.PrintStrikeBall();
-      if (strikeball.Strike === 3) {
+      if (strikeball.Strike === message.THREESTRIKE) {
         MissionUtils.Console.print(message.SUCCESS);
         break;
       }
     }
-    if (strikeball.Strike === 3) {
+    if (strikeball.Strike === message.THREESTRIKE) {
       if (strikeball.IfStrike()) {
         this.play();
       } else {
         MissionUtils.Console.print(message.END);
         MissionUtils.Console.close();
       }
-    } else if (this.UserInputNumber !== 'InitINPUT') {
+    } else if (userinput.UserInputNumber !== message.INITINPUT) {
       throw message.INPUTERROR;
     }
   }
