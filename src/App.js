aa5c1f4d5be23d6bdd92af2396082ea43da95526
Choejeonghun <saa00123@naver.com>
2022-11-08 01:42:54 +0900

    refactor: 기능 3번 수정

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ class App {
   playBall() {
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
     this.computer = this.randomNum();
+    this.input();
   }
 
   randomNum() {
@@ -15,6 +16,24 @@ class App {
     }
     return randomNum;
   }
+
+  input() {
+    MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (inputNum) => {
+      this.checkInputNum(inputNum);
+    });
+  }
+
+  checkInputNum(inputNum) {
+    let duplicate =
+      inputNum[0] === inputNum[1] ||
+      inputNum[0] === inputNum[2] ||
+      inputNum[1] === inputNum[2];
+
+    if (Number.isNaN(inputNum) || inputNum.length !== 3 || duplicate) {
+      throw new Error('잘못된 입력값입니다.');
+    }
+  }
+
   play() {
     this.playBall();
   }
