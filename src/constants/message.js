3a6efe70eb82beeffa5394a07463e50674fbb7f4
jiho1996 <jhp9270@naver.com>
2022-11-07 16:12:06 +0900

    refactor : 게임 메세지 상수 저장

--- src/constants/message.js
+++ src/constants/message.js
@@ -1,7 +1,8 @@
 const GAME_MESSAGE = Object.freeze({
   NOTIFY_START_MESSAGE: "숫자 야구 게임을 시작합니다.",
-  ASK_INPUT_MESSAGE: "숫자를 입력하세요",
+  ASK_INPUT_MESSAGE: "숫자를 입력해주세요 : ",
   FINSH_GAME_MESSAGE: "숫자 야구 게임을 종료합니다.",
+  ASK_GAME_CONTINUE_OR_EXIT: "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
+  NOTIFY_INPUT_WRONG_NUMBER: "1또는 2만 입력해주세요.",
 });
-
 module.exports = GAME_MESSAGE;
