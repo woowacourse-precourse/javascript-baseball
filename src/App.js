41d1b40ff0322c456204dc373814e000208e3649
thewronghand <penfreak77@gmail.com>
2022-11-08 05:23:50 +0900

    refactor: 문서 내 indent 통일
    코드의 전체적인 indent depth를 통일시킨다.

--- src/App.js
+++ src/App.js
@@ -14,39 +14,41 @@ function App () {
     startGame();
     getUserAnswer();
   }
-    function matchNumber (num1, num2) {
-      let strike = 0;
-      let ball = 0;
-      const userInput = num1
-        .split('')
-        .map(char => Number(char));
-      const computerInput = num2
-        .split('')
-        .map(char => Number(char));
+  
+  function matchNumber (num1, num2) {
+    let strike = 0;
+    let ball = 0;
+    const userInput = num1
+      .split('')
+      .map(char => Number(char));
+    const computerInput = num2
+      .split('')
+      .map(char => Number(char));
 
-      userInput.forEach((element, index) => {
-        if (computerInput[index] === element) {
-          strike += 1;
-          return;
-        }
-        if (computerInput.includes(element)){
-            ball += 1;
-        }
-      });
-      let result = [ball, strike];
+    userInput.forEach((element, index) => {
+      if (computerInput[index] === element) {
+        strike += 1;
+        return;
+      }
+      if (computerInput.includes(element)){
+        ball += 1;
+      }
+    });
+    let result = [ball, strike];
 
-      return result;
-    }
+    return result;
+  }
     
-    function getScore (matchResult){
-      const [ball, strike] = matchResult;
-      const ANSWER_STRIKE_COUNT = 3;
+  function getScore (matchResult){
+    const [ball, strike] = matchResult;
+    const ANSWER_STRIKE_COUNT = 3;
 
-      if (strike === ANSWER_STRIKE_COUNT){
-        MissionUtils.Console.print (
-          "3스트라이크 \n 3개의 숫자를 모두 맞히셨습니다! 게임 종료"
-        );
-        MissionUtils.Console.readLine(`게임을 새로 시작하려면 ${GAME_START_KEY}, 종료하려면 ${GAME_EXIT_KEY}를 입력하세요.\n`, 
+    if (strike === ANSWER_STRIKE_COUNT){
+      MissionUtils.Console.print (
+        "3스트라이크 \n 3개의 숫자를 모두 맞히셨습니다! 게임 종료"
+      );
+      MissionUtils.Console.readLine(
+        `게임을 새로 시작하려면 ${GAME_START_KEY}, 종료하려면 ${GAME_EXIT_KEY}를 입력하세요.\n`, 
         (input) => {
           if(input === GAME_START_KEY){
             startGame();
@@ -59,37 +61,37 @@ function App () {
           } 
           throw '올바른 값을 입력하세요.';
         }
-        );
-        return;
-      }
-      if (strike === 0 && ball === 0){
-        MissionUtils.Console.print ("낫싱");
-        getUserAnswer();
-        return;
-      }
+      );
+      return;
+    }
 
-      MissionUtils.Console.print (`${ball}볼 ${strike}스트라이크`);
+    if (strike === 0 && ball === 0){
+      MissionUtils.Console.print ("낫싱");
       getUserAnswer();
+      return;
     }
 
-    function printNewGameInterface () {
-      MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
-    }
+    MissionUtils.Console.print (`${ball}볼 ${strike}스트라이크`);
+    getUserAnswer();
+  }
 
-    function startGame () {
-      this.randomNumber = getRandomNumber(MIN_RANDOM_NUIMBER,MAX_RANDOM_NUMBER);
-    }
+  function printNewGameInterface () {
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+  }
 
-    function getUserAnswer () {
-      MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (answer) => {
-        if(isValidInput(answer)){
-          getScore(matchNumber(answer, this.randomNumber));
-          return;
-        } throw '올바른 값을 입력하세요.';
-      })
-    }
+  function startGame () {
+    this.randomNumber = getRandomNumber(MIN_RANDOM_NUIMBER,MAX_RANDOM_NUMBER);
+  }
 
-
+  function getUserAnswer () {
+    MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (answer) => {
+      if(isValidInput(answer)){
+        getScore(matchNumber(answer, this.randomNumber));
+        return;
+      }
+      throw '올바른 값을 입력하세요.';
+    });
+  }
 }
 
 module.exports = App;
