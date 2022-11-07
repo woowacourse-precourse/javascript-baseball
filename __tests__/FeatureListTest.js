b12d2a945004dd8202af4857d6184917e4f4d567
DAIN KIM <dnkim419@gmail.com>
2022-11-08 05:42:05 +0900

    test(FeatureListTest): 기능 목록 테스트 파일 생성

---
+++ __tests__/FeatureListTest.js
@@ -0,0 +1,27 @@
+const App = require('../src/App');
+const Game = require('../src/Game');
+const MissionUtils = require('@woowacourse/mission-utils');
+
+const mockQuestions = (answers) => {
+  MissionUtils.Console.readLine = jest.fn();
+  answers.reduce((acc, input) => {
+    return acc.mockImplementationOnce((question, callback) => {
+      callback(input);
+    });
+  }, MissionUtils.Console.readLine);
+};
+
+const mockRandoms = (numbers) => {
+  MissionUtils.Random.pickNumberInRange = jest.fn();
+  numbers.reduce((acc, number) => {
+    return acc.mockReturnValueOnce(number);
+  }, MissionUtils.Random.pickNumberInRange);
+};
+
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(MissionUtils.Console, 'print');
+  logSpy.mockClear();
+  return logSpy;
+};
+
+describe('기능 목록 테스트', () => {});
