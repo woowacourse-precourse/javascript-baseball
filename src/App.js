c137891689a368e4c34fe27a3a7938a39fca9a88
chasj0326@naver.com <62418379+chasj0326@users.noreply.github.com>
2022-11-07 16:03:29 +0900

    refactor: App.js - 변수명 및 함수명 수정, 불필요한 부분 축소

--- src/App.js
+++ src/App.js
@@ -1,34 +1,28 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
-function startMessage(){//시작 문구 출력
-  MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
-}
-
-function playGame(){ //문제 생성 - 게임 종료 옵션
+function initBaseballGame(){
   const NUMBER_LENGTH = 3;
-  const problem = makeProblem(NUMBER_LENGTH);
-  playOneRound(problem);
+  const computerNumbers = makeComputerNumbers(NUMBER_LENGTH);
+  playOneRound(computerNumbers);
 }
 
-function makeProblem(NUMBER_LENGTH){
-  const problem = new Set([]);
-  while ([...problem].length < NUMBER_LENGTH){
-    problem.add(MissionUtils.Random.pickNumberInRange(1,9));
+function makeComputerNumbers(NUMBER_LENGTH){
+  const computerNumbers = new Set([]);
+  while ([...computerNumbers].length < NUMBER_LENGTH){
+    computerNumbers.add(MissionUtils.Random.pickNumberInRange(1,9));
   }
-  return [...problem];
+  return [...computerNumbers];
 }
 
-function playOneRound(problem){//한 라운드 함수
-  MissionUtils.Console.readLine("숫자를 입력해 주세요. : ", (input)=>{
-    if(input.length>3){
+function playOneRound(computerNumbers){
+  MissionUtils.Console.readLine("숫자를 입력해 주세요. : ", (userNumbers)=>{
+    if(userNumbers.length>3){
       throw '길이 초과'
     }
-    const answer = input;
-    const result = makeResult(answer, problem);
-    const [ball_count, strike_count] = result;
-    showResult(ball_count, strike_count);
-    if(strike_count!==3){
-      return playOneRound(problem);
+    const [ball, strike] = countBallAndStrike(userNumbers, computerNumbers);
+    showResult(ball, strike);
+    if(strike!==3){
+      return playOneRound(computerNumbers);
     }
     MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     quitOptions();
@@ -36,33 +30,27 @@ function playOneRound(problem){//한 라운드 함수
 }
 
 
-function makeResult(answer, problem){//결과 계산 함수
-  let ball_count = 0;
-  let strike_count = 0;
-  problem.forEach((problemNumber, idx)=>{
-    if([...answer].includes(String(problemNumber))){
-      ball_count += 1;
-    }
-    if(String(problemNumber)===answer[idx]){
-      strike_count += 1;
-    }
-  })
-  ball_count -= strike_count;
-  const result = [ball_count, strike_count];
-  return result;
+function countBallAndStrike(userNumbers, computerNumbers){//결과 계산 함수
+  const strike = computerNumbers.reduce((count,computerNumber,index)=>(
+    count+(String(computerNumber)===userNumbers[index])
+  ), 0)
+  const ball = computerNumbers.reduce((count, computerNumber)=>(
+    count+([...userNumbers].includes(String(computerNumber)))
+  ), 0) - strike;
+  return [ball, strike];
 }
 
-function showResult(ball_count, strike_count){//결과 출력 함수
-  if (ball_count+strike_count===0){
+function showResult(ball, strike){//결과 출력 함수
+  if (ball + strike ===0){
     MissionUtils.Console.print('낫싱');
     return;
   }
   let resultPrint = '';
-  if (ball_count){
-    resultPrint += `${ball_count}볼 `
+  if(ball){
+    resultPrint += `${ball}볼 `;
   }
-  if (strike_count){
-    resultPrint += `${strike_count}스트라이크`
+  if(strike){
+    resultPrint += `${strike}스트라이크`
   }
   MissionUtils.Console.print(resultPrint);
 }
@@ -71,18 +59,18 @@ function quitOptions(){
   MissionUtils.Console.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
   MissionUtils.Console.readLine("",(input)=>{
     if(input === '1'){
-      playGame();
+      initBaseballGame();
     }
-    else if(input === '2' ){
-      return;
+    else if(input === '2'){
+      MissionUtils.Console.close();
     }
   })
 }
 
 class App {
   play() {
-    startMessage();
-    playGame();
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    initBaseballGame();
   }
 }
 
