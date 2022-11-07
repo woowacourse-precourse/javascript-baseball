2b8899b0077c983a9cc1ade863da257e7750ef9e
HelloHailie <alsltjsals@gmail.com>
2022-11-08 01:01:22 +0900

    feat:MissionUtils 라이브러리를 이용해 컴퓨터가 3개의 랜덤값을 가져온다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 class App {
+  computerRandom() {
+    const computer = MissionUtils.Random.pickNumberInRange(1, 9, 3);
+    MissionUtils.Console.close();
+    return computer;
+  }
   play() {
     MissionUtils.Console.print("숫자 야구 게임 시작!");
   }
