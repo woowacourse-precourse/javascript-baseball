b58f7923ac4c52de6c1021a594426b4d20910e91
HeoJiye <heoze1031@gmail.com>
2022-11-07 22:25:44 +0900

    refactor(App): 숫자 입력 받기 함수 분리
    숫자를 입력받아 야구 게임의 기능을 수행하는 match 함수 추가
    기존 match() -> compare()로 함수명 변경
    
    다음 기능인 3strike가 나올 때까지 반복을 재귀함수로 구현하기 위해 입력 기능을 따로 함수로 분리함.

--- src/App.js
+++ src/App.js
@@ -5,20 +5,23 @@ class App {
     const computer_nums = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
 
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
-    
+
+    this.match(computer_nums);
+  }
+
+  match(computer_nums) {
     MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (answer) => {
       const player_nums = answer.split('').map(Number);
 
       try {
-        this.vaildation_player_nums(player_nums)
+        this.vaildation_player_nums(player_nums);
       } catch (e) {
         MissionUtils.Console.close();
         console.error(e);
         return; 
       }
 
-      this.match(computer_nums, player_nums);
-
+      this.compare(computer_nums, player_nums);
       MissionUtils.Console.close();
     });
   }
@@ -44,7 +47,7 @@ class App {
     }
   }
 
-  match(computer_nums, player_nums) {
+  compare(computer_nums, player_nums) {
     let strike = 0;
     let ball = 0;
 
@@ -53,11 +56,13 @@ class App {
       else if (computer_nums.indexOf(item) > -1) ball += 1;
     });
 
-    let result = '';
-    if(ball > 0) result += `${ball}볼 `;
-    if(strike > 0) result += `${strike}스트라이크`;
+    if(ball <= 0 && strike <= 0) {
+      MissionUtils.Console.print('낫싱');
+    }
 
-    if(ball <= 0 && strike <= 0) result = '낫싱';
+    let result = '';
+    if (ball > 0) result += `${ball}볼 `;
+    if (strike > 0) result += `${strike}스트라이크`;
 
     MissionUtils.Console.print(result);
   }
