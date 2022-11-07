b75587929a9274624a2e0563a967742ffcaee390
Miniling <bsm3925@naver.com>
2022-11-07 22:16:17 +0900

    Add: func_computerSet

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
+  // 컴퓨터 랜덤으로 서로 다른 세 숫자 받기
+  computerSet() {
+    const computer = [];
+
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+        this.computer = computer;
+      }
+    }
+  }
+
   // 게임 시작
   play() {
     this.computer = [];
