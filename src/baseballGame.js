ab0d28acd0f029eab14bcdf35d3176678f597bb1
Minhye Kang <alsgp9898@naver.com>
2022-11-07 20:46:06 +0900

    feat: 볼과 스트라이크 개수를 반환하는 함수 구현
    - 이전에 작성한 테스트 코드 성공
    - filter 고차함수를 사용하여 조건에 맞는 배열의 길이를 반환

--- src/baseballGame.js
+++ src/baseballGame.js
@@ -1,6 +1,13 @@
-const countStrikeNumbers = (computerNumbers, userInput) => {};
+const countStrikeNumbers = (computerNumbers, userInput) => {
+  return computerNumbers.filter((number, index) => number == userInput[index])
+  .length;
+};
 
-const countBallNumbers = (computerNumbers, userInput) => {};
+const countBallNumbers = (computerNumbers, userInput) => {
+  return computerNumbers.filter(
+    (number, index) => userInput.includes(number) && number != userInput[index]
+  ).length;
+};
 
 
 module.exports = { countStrikeNumbers, countBallNumbers };
