f84843d24887adefff8e572f2426ea7f304a6971
daal2quf <daal2quf@gmail.com>
2022-11-07 21:51:20 +0900

    test: 랜덤으로 만든 숫자들이 1~9 사이인지 확인하는 테스트

--- __tests__/NumberTest.js
+++ __tests__/NumberTest.js
@@ -1,13 +1,31 @@
 const App = require("../src/App.js");
-const MissionUtils = require("@woowacourse/mission-utils");
-const app = new App();
 
 
-describe("테스트", () => {
-  test(`test of number`, () => {
-    const correct_answer = app.createRandomNum().length
-    const ramdom_num_length = 3;
-    expect(correct_answer).toBe(ramdom_num_length);
+describe("랜덤 숫자 테스트", () => {
+  test("랜덤으로 만든 숫자의 길이가 3인지", () => {
+    const app = new App();
+    const computerMadeNum = app.createRandomNum().length;
+    const randomNumLength = 3;
+    expect(computerMadeNum).toBe(randomNumLength);
+  });
+
+  test("랜덤으로 만든 숫자의 타입이 숫자인지 확인", () => {
+    const app = new App();
+    const randomNums = !isNaN(app.createRandomNum());
+    const result = true;
+    expect(randomNums).toBe(result);
+  });
+
+  test("랜덤으로 만든 숫자의 값들이 1~9 사이의 숫자인지 확인", () => {
+    const app = new App;
+    const randomNums = app.createRandomNum();
+    let onlyNumberOfRandomNumLength = randomNums
+      .split('')
+      .map((e) => parseInt(e, 10))
+      .filter((el) => el >= 1 && el <= 9)
+      .length;
+    const randomNumsLength = 3;
+    expect(onlyNumberOfRandomNumLength).toBe(randomNumsLength);
   });
 })
 
