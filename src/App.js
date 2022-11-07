5f70bbce9823e72783864ca44badcc5850977274
Dae-une <ko301201@naver.com>
2022-11-07 16:07:41 +0900

    feat: 재시작 및 종료 추가

--- src/App.js
+++ src/App.js
@@ -74,14 +74,37 @@ class App {
       return result;
     };
 
+    const reStartOrEnd = () => {
+      Console.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. ");
+      Console.readLine("", (answer) => {
+        switch (answer) {
+          case "1":
+            startGame();
+            break;
+          case "2":
+            endGame();
+            break;
+          default:
+            throw "잘못입력하셨습니다.";
+        }
+      });
+    };
+
     const startGame = () => {
       targetNums = randomPick();
       enterNumber();
+    };
+
+    const endGame = () => {
+      Console.close();
     };
 
     Console.print("숫자야구 게임을 시작합니다.");
     startGame();
   }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
