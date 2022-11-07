14f820e51e7ad23c41e6fbbf23d73af4287478d3
wonseok <rhdqngkrh77@khu.ac.kr>
2022-11-08 06:05:36 +0900

    feat: 게임 재시작 여부 입력 기능 추가

--- src/App.js
+++ src/App.js
@@ -6,12 +6,7 @@ class App {
   }
   play() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
-    const answer = this.getThreeNum();
-    this.getUserNum();
-    var strike = this.countStrike(answer, this.userNumber);
-    var ball = this.countBall(answer, this.userNumber);
-    var result = this.printResult(strike, ball);
-    MissionUtils.Console.print(result);
+    this.playGame();
   }
 
   getThreeNum() {
@@ -67,6 +62,33 @@ class App {
     else if (strikes === 0) return `${balls}볼`;
     else if (balls === 0) return `${strikes}스트라이크`;
     else return `${balls}볼 ${strikes}스트라이크`;
+  }
+
+  restartGame() {
+    MissionUtils.Console.readLine(
+      "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
+      (answer) => {
+        if (answer === "1") this.playGame();
+        else if (answer === "2") MissionUtils.Console.close();
+        else throw "입력값이 잘못되었습니다.";
+      }
+    );
+  }
+
+  playGame() {
+    const answer = this.getThreeNum();
+    while (1) {
+      this.getUserNum();
+      var strike = this.countStrike(answer, this.userNumber);
+      var ball = this.countBall(answer, this.userNumber);
+      var result = this.printResult(strike, ball);
+      MissionUtils.Console.print(result);
+      if (result === "3스트라이크") {
+        MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        this.restartGame();
+        break;
+      }
+    }
   }
 }
 
