625f25d3a9232fbfef4f4b8be9aa89a8de6233b9
woong <inwoong100@gmail.com>
2022-11-07 21:17:19 +0900

    hotfix(Function): function의 validDuplicate 메소드 구현

--- src/Function.js
+++ src/Function.js
@@ -4,6 +4,12 @@ class Function {
   static validByRegex(input) {
     return !VALID_REGEX.test(input);
   }
+
+  static validDuplicate(input) {
+    const setInputLength = new Set(...input.toString().split('')).size;
+    if (input.length !== setInputLength) return false;
+    return true;
+  }
 }
 
 module.exports = Function;
