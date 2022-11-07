d035cdcceccb2498033b3db5741e5af1028eeacd
KUD <kimud6003@gmail.com>
2022-11-07 14:40:24 +0900

    ✨ Feature : APP 게임 실행 Class - gameStart : 사용자가 Input을 받고 게임을 플레이하는 메소드 - gameResult:  게임 종료를 알리고 재실행 유무 - gameRestart: 재시작 - gmaeDataReset: 게임 데이터 리셋

--- src/App.js
+++ src/App.js
@@ -1,11 +1,55 @@
+const { RESTART, MSG } = require('./utils/Constant');
+const { Console } = require('@woowacourse/mission-utils');
+const asyncLine = require('./utils/ReadLine');
 const User = require('./User');
+const Computer = require('./Computer');
+const ScoreManager = require('./ScoreManager');
+
 // 게임을 진행하는 클래스
 class App {
   constructor() {
     this.User = new User();
+    this.Computer = new Computer();
+    this.ScoreManager = new ScoreManager();
   }
 
-  play() {}
+  play() {
+    Console.print(MSG.START);
+    this.gameStart();
+  }
+
+  async gameStart() {
+    while (this.ScoreManager.isEndRound()) {
+      let targetNum = this.Computer.getComNums();
+      let userInput = this.User.getInputNums();
+
+      await this.User.setInputNums(MSG.INPUT, asyncLine);
+      this.ScoreManager.calScore(targetNum, userInput);
+      this.ScoreManager.getUserScoreStr();
+    }
+    this.gameResult();
+  }
+
+  async gameResult() {
+    Console.print(MSG.END);
+    let userSelect = await asyncLine(MSG.SELECT);
+    userSelect === RESTART ? this.gameRestart() : this.gameExit();
+  }
+
+  gameRestart() {
+    this.gameDataReset();
+    this.gameStart();
+  }
+
+  gameDataReset() {
+    this.User.resetUser();
+    this.Computer.resetComputer();
+    this.ScoreManager.resetScoreManager();
+  }
+
+  gameExit() {
+    Console.close();
+  }
 }
 
 module.exports = App;
