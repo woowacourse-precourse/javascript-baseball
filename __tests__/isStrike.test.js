4d2c7792543f9d3acff55e45cf8e519b036520af
jisu-kim <kss2460@naver.com>
2022-11-08 01:33:00 +0900

    test: isStrike 테스트 변경

--- __tests__/isStrike.test.js
+++ __tests__/isStrike.test.js
@@ -2,6 +2,6 @@ const isStrike = require("../src/isStrike");
 
 describe("스트라이크 횟수 체크", () => {
   test("값이 같은 자리에 있는지 확인", () => {
-    expect(isStrike([1, 2, 3], [2, 1, 3])).toBe(1);
+    expect(isStrike([1, 2, 3], [2, 1, 3])).toBe(`1스트라이크`);
   });
 });
