60e90306e8b7d2cb1398873588613bf78daf9ed3
100Gyeon <bjy9966@naver.com>
2022-11-08 04:00:28 +0900

    feat: 랜덤 정답 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+
+const MAX_ANSWER_COUNT = 3;
+
 class App {
-  play() {}
+  play() {
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+    const answer = this.setAnswer();
+  }
+
+  setAnswer() {
+    const randomList = [];
+    while (randomList.length < MAX_ANSWER_COUNT) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!randomList.includes(number)) {
+        randomList.push(number);
+      }
+    }
+    return randomList.join('');
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
