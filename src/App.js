5b7838e7fdf19f26df2db06f7273bd99aa7f2ec7
wusi-hub <jws9595@naver.com>
2022-11-08 07:07:02 +0900

    feat: 자릿수가 3자리인 컴퓨터의 수를 생성하는 함수 구현

--- src/App.js
+++ src/App.js
@@ -5,5 +5,15 @@ class App {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
   }
 }
+function createComputerNumber() {
+  const computer = [];
+  while (computer.length < 3) {
+    const number = MissionUtils.Random.pickNumberInRange(1, 9);
+    if (!computer.includes(number)) {
+      computer.push(number);
+    }
+  }
+  return computer;
+}
 
 module.exports = App;
