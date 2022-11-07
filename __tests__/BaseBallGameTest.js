96e3dcf964a74a6f526cc68d99eb31b89d0581b0
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2022-11-08 01:56:03 +0900

    test: 플레이어 입력 예외 처리에 대한 Test

--- __tests__/BaseBallGameTest.js
+++ __tests__/BaseBallGameTest.js
@@ -45,3 +45,51 @@ describe("구현 기능 목록 Test", () => {
     expect(logSpy).toHaveBeenCalledWith("낫싱");
   });
 });
+
+describe("게임 플러이어 입력 예외 처리 Test", () => {
+  const app = new App();
+
+  test("숫자가 아닌 문자가 입력으로 들어오는 경우 ", () => {
+    expect(() => app.isValidPlayerInput("이삼오")).toThrow(
+      "숫자로만 입력해주세요."
+    );
+    expect(() => app.isValidPlayerInput("/+*'~")).toThrow(
+      "숫자로만 입력해주세요."
+    );
+  });
+
+  test("3자리의 수가 아닌 입력이 들어오는 경우", () => {
+    expect(() => app.isValidPlayerInput("01")).toThrow(
+      "3자리의 수를 입력해주세요."
+    );
+    expect(() => app.isValidPlayerInput("5984")).toThrow(
+      "3자리의 수를 입력해주세요."
+    );
+  });
+
+  test("1부터 9까지의 범위를 벗어난 입력이 들어온 경우", () => {
+    expect(() => app.isValidPlayerInput("809")).toThrow(
+      "1부터 9까지의 수만 입력해주세요."
+    );
+    expect(() => app.isValidPlayerInput("800")).toThrow(
+      "1부터 9까지의 수만 입력해주세요."
+    );
+  });
+
+  test("중복된 숫자를 포함한 입력이 들어오는 경우", () => {
+    expect(() => app.isValidPlayerInput("772")).toThrow(
+      "서로 다른 3자리를 입력해주세요."
+    );
+    expect(() => app.isValidPlayerInput("131")).toThrow(
+      "서로 다른 3자리를 입력해주세요."
+    );
+  });
+
+  test("1(재시작) 또는 2(종료) 이외의 입력이 들어오는 경우", () => {
+    expect(() => app.isValidReplayNum("0")).toThrow("1 또는 2를 입력하세요.");
+    expect(() => app.isValidReplayNum("종료")).toThrow(
+      "1 또는 2를 입력하세요."
+    );
+    expect(() => app.isValidReplayNum("876")).toThrow("1 또는 2를 입력하세요.");
+  });
+});
