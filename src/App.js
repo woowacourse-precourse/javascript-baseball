5d654ee46706431e1f1bfc140e42af8eeb8b23b2
River <swimmingr@gmail.com>
2022-11-08 01:55:50 +0900

    기능목록 추가 및 기능목록 5번까지 테스트 통과

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ const MissionUtils = require("@woowacourse/mission-utils");
 
 const DEF_NUM =[];
 const ATK_NUM =[];
-let nothingCount = 0;
+let ballCount = 0
+let strikeCount = 0;
+
 function GameStart(){
   MissionUtils.Console.print("숫자 야구 게임을 시작합니다")
 }
 
 function MakeDef(){
 while (DEF_NUM.length < 3) {
-  const number = MissionUtils.Random.pickNumberInRange(1, 9);
+  const number = MissionUtils.Random.pickNumberInRange(1,9,3);
   if (!DEF_NUM.includes(number)) {
     DEF_NUM.push(number);
   }
@@ -17,61 +19,52 @@ while (DEF_NUM.length < 3) {
   return DEF_NUM;
 }
  function MakeAtk(){
-  let inputNum;
   MissionUtils.Console.readLine("숫자를 입력해주세요 : ",(answer)=>{
-    // inputNum = (`${answer}`+'').split('');
     for(let i=0;i<3;i++){
       ATK_NUM.push(parseInt(answer[i],10));
     }
-    console.log(ATK_NUM);
   });
+  findError();
   return ATK_NUM;
 }
 
 function getBall() {
-  let count = 0;
   for (let i = 0; i < 3; i++) {
       if (DEF_NUM.includes(ATK_NUM[i])) {
-        count++;
+        ballCount++;
       }
   }
-  return count;
+  return ballCount;
 }//ball 
 
 function getStrike() {
-  let count = 0;
   for (let i = 0; i < 3; i++) {
     if (ATK_NUM[i] === DEF_NUM[i]) {
-      count++;
+      strikeCount++;
     }
   }
-  return count;
+  return strikeCount;
 }//strike
 
-function getNothingBall(){
-  for (let i = 0; i < 3; i++) {
-    if (!DEF_NUM.includes(ATK_NUM[i])) {
-      nothingCount++;
-    }
-    return nothingCount;
-}
-function getNothingStrike() {
-  for (let i = 0; i < 3; i++) {
-    if (ATK_NUM[i] !== DEF_NUM[i]) {
-      nothingCount++
+function findError(){
+  for(let i=0;i<3;i++){
+    if(typeof ATK_NUM[i] !== 'number' || ATK_NUM.length !== 3){
+      throw Error("잘못된 입력");
+      MissionUtils.Console.close()
     }
   }
-  return nothingCount;
-}
 }
 function Gaming(){
-    let ballCount = getBall();
-    let strikeCount = getStrike();
-    if(nothingCount>2){
-      MissionUtils.Console.print("낫싱");
-      nothingCount = 0;
-    }
+    getStrike();
+    getBall();
+    
+  if(strikeCount===0 && ballCount===0){
+    MissionUtils.Console.print("낫싱");
+  }else{
     MissionUtils.Console.print(`${ballCount}볼 ${strikeCount}스트라이크`);
+  }
+  strikeCount=0;
+  ballCount=0;
 }
 function restartOrEnd(){
     MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
@@ -86,27 +79,25 @@ function restartOrEnd(){
 }
 
 class App {
-  prePlay(){
+  start(){
     GameStart();
-       // 사용자 숫자 입력
   }
-  play() {
-    this.prePlay();
+  MakeNumber(){
     MakeDef();
     MakeAtk();
-    Gaming();
-   
-   
+  }
+  play() {
+    while(strikeCount<3){
+    setTimeout(Gaming,0);
+    }
   }    
   finish(){
-    if(strikeCount===3){
       restartOrEnd();
-    }
-  }
-
-  
+  }  
 }
 const app = new App();
+app.start();
+app.MakeNumber();
 app.play();
 
 module.exports = App;
