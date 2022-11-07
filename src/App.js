9b47e94d7bbe856c99bec2109a5921c58fd780d1
yuna Kim <yuna000804@gmail.com>
2022-11-07 13:37:42 +0900

    feat : answer가 예외 사항인 경우

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ const MissionUtils = require("@woowacourse/mission-utils");
 const { Random, Console } = require("@woowacourse/mission-utils");
 
 class App {
-  play() {}
+  play() {
+    this.letComputerInput();
+    this.letUserInput();
+  }
   
   letComputerInput() {
     const computer = [];
@@ -17,12 +20,41 @@ class App {
   letUserInput() {
     Console.print("숫자 야구 게임을 시작합니다.");
     Console.readLine('숫자를 입력해주세요 : ', (answer) => {
-      //answer의 예외사항 체크
+      let errorCheck = new InputCheck(answer);
+      if(!errorCheck.isValid(answer)) {
+        throw new Error("올바르지 않은 입력값입니다.")
+      }
+      return answer;
     })
   }
-
 }
 
+class InputCheck {
+
+  isValid(answer) {
+    this.answer = answer;
+    let isValid =
+      this.isValidLength()
+      && this.isNotduplicated()
+      && this.isValidRange()
+    return isValid;
+  }
+
+  isValidLength() {
+    const LENGTH_LIMIT = 3;
+    return (this.answer.length === LENGTH_LIMIT);
+  }
+  
+  isNotduplicated() {
+    const arr = [...new Set(this.answer)].join('');
+    return this.answer === arr;
+  }
+
+  isValidRange() {
+    const range = Array(9).fill().map((v, i) => (i+1).toString());
+    return ([...new Set(this.answer)].every((num) => range.includes(num)));
+  } 
+  
+}
 
 module.exports = App;
-
