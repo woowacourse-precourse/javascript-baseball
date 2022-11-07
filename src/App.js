5a36c773f4cd79f1aa5fa7ee3d9e60b01aef753e
Doyoung.Kim <dyflower@nate.com>
2022-11-08 01:55:07 +0900

    feat : 입력값 점검

--- src/App.js
+++ src/App.js
@@ -14,4 +14,13 @@ function createAnswer() {
   }
   return answer;
 }
+
+function validNumber(inputNum) {
+  if (inputNum.includes(0)) throw new Error('0을 제외한 숫자를 입력해주세요.');
+  if (new Set(inputNum).size !== 3) throw new Error('중복되지 않게 입력해주세요.');
+  if (inputNum.map(Number).includes(NaN)) throw new Error('숫자만 입력해주세요.');
+  if (inputNum.length !== 3) throw new Error('3자리 숫자를 입력해주세요.');
+
+  return inputNum;
+}
 module.exports = App;
