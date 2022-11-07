303802e9c8b7a3d83ddd7068f60f8e0876303ee3
gabrielyoon7 <gabrielyoon7@gmail.com>
2022-11-07 14:43:28 +0900

    test: 테스트 케이스 추가

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -24,6 +24,7 @@ const getLogSpy = () => {
 };
 
 describe("숫자 야구 게임", () => {
+  // 원본
   test("게임 종료 후 재시작", () => {
     const randoms = [1, 3, 5, 5, 8, 9];
     const answers = ["246", "135", "1", "597", "589", "2"];
@@ -47,6 +48,74 @@ describe("숫자 야구 게임", () => {
     });
   });
 
+  test("볼볼볼", () => {
+    const randoms = [1, 2, 3];
+    const answers = ["981", "281", "231", "123", "2"];
+    const logSpy = getLogSpy();
+    const messages = [
+      "1볼",
+      "2볼",
+      "3볼",
+      "3스트라이크",
+      "게임 종료",
+    ];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    const app = new App();
+    app.play();
+
+    messages.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
+  test("스트라이크", () => {
+    const randoms = [1, 2, 3];
+    const answers = ["145", "125", "123", "2"];
+    const logSpy = getLogSpy();
+    const messages = [
+      "1스트라이크",
+      "2스트라이크",
+      "3스트라이크",
+      "게임 종료",
+    ];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    const app = new App();
+    app.play();
+
+    messages.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
+  test("혼합", () => {
+    const randoms = [1, 2, 3];
+    const answers = ["142", "321", "123", "2"];
+    const logSpy = getLogSpy();
+    const messages = [
+      "1볼 1스트라이크",
+      "2볼 1스트라이크",
+      "3스트라이크",
+      "게임 종료",
+    ];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    const app = new App();
+    app.play();
+
+    messages.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
+  // 원본
   test("예외 테스트", () => {
     const randoms = [1, 3, 5];
     const answers = ["1234"];
@@ -59,4 +128,46 @@ describe("숫자 야구 게임", () => {
       app.play();
     }).toThrow();
   });
+
+
+  test("예외 테스트 : 사용자가 입력한 숫자가 서로다른 숫자가 아닐 때", () => {
+    const randoms = [1, 3, 5];
+    const answers = ["133"];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow();
+  });
+
+  test("예외 테스트 : 공을 맞추라는 상황에서 3자리 미만의 숫자를 입력", () => {
+    const randoms = [1, 3, 5];
+    const answers = ["1"];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow();
+  });
+
+  test("예외 테스트 : 게임 재시작을 묻는 경우 1,2가 아닌 숫자를 입력할 때", () => {
+    const randoms = [1, 3, 5];
+    const answers = ["135", "3"];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow();
+  });
+
+
 });
