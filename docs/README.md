cdbd16c5a73595b265c398f772919c4114420126
Hchan2 <seongchan_@naver.com>
2022-11-07 21:39:07 +0900

    프로젝트 생성

---
+++ docs/README.md
@@ -0,0 +1,9 @@
+# 구현 기능 목록
+
+## 상수 정리
+* GAME_START: 게임 시작 메세지
+* RESTART: 게임 재시작 메세지
+
+## 컴퓨터 숫자
+* 1 ~ 9까지 랜덤의 숫자를 3개 생성
+* MissionUtils.Random.pickNumberInRange(1, 9) 사용
--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const GAME_START = '숫자 야구 게임을 시작합니다.';
+const RESTART = '숫자 야구 게임을 재시작합니다.';
+
 class App {
-  play() {}
+  play() {
+    MissionUtils.Console.print(GAME_START);
+    this.startGame();
+  }
 }
 
 module.exports = App;
