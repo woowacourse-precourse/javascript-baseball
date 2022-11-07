56ad30424c08acad7911673a9917e00e5c7ab11e
Monte <monte6198@gmail.com>
2022-11-08 01:37:22 +0900

    refactor: 사용자 입력 변수 수정

--- src/App.js
+++ src/App.js
@@ -9,9 +9,8 @@ class App {
   play() {
     this.gameStart();
     // MissionUtils.Console.print(`상대방의 숫자는 ${this.opponent.number} 입니다`);
-    const userInput = this.user.getInput();
-    // MissionUtils.Console.print(`나의 입력은 ${userInput} 입니다`);
-    this.user.checkValidation(userInput);
+    this.user.getInput();
+    this.user.checkValidation();
   }
   gameStart() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
@@ -36,20 +35,21 @@ class Opponent {
 }
 
 class User {
+  constructor() {
+    this.input = [];
+  }
   getInput() {
-    let userInput;
-    MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (userInputNumber) => {
-      userInput = userInputNumber.toString().split("");
+    MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (inputNumber) => {
+      this.input = inputNumber.toString().split("");
       MissionUtils.Console.close();
     });
-    return userInput;
   }
-  checkValidation(userInput) {
-    userInput.map((number) => {
+  checkValidation() {
+    this.input.map((number) => {
       if (isNaN(number)) throw new Error("오직 숫자만 입력이 가능합니다.");
     });
-    if (userInput.includes("0")) throw new Error("1부터 9 사이의 숫자만 입력 가능합니다.");
-    if (userInput.length !== 3) throw new Error("3자리 수만 입력이 가능합니다.");
+    if (this.input.includes("0")) throw new Error("1부터 9 사이의 숫자만 입력 가능합니다.");
+    if (this.input.length !== 3) throw new Error("3자리 수만 입력이 가능합니다.");
   }
 }
 
