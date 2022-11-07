b7bbad36b356e2dd6ea91bdc8193fbea2b1c0af5
BaeDongWoo <ehddn5410@naver.com>
2022-11-07 21:23:08 +0900

    기능 3) 입력받은 숫자가 올바른지 검사 올바르지않다면 throw로 에러 메세지 출력

--- src/App.js
+++ src/App.js
@@ -18,12 +18,22 @@ class App {
     MissionUtils.Console.print(computer);
     this.playerInput(computer);
   }
-
   playerInput(computer) {
-    MissionUtils.Console.readLine("숫자를 입력해주세요 :.", (input) => {
+    MissionUtils.Console.readLine("숫자를 입력해 주세요 :", (input) => {
+      if (this.playerNumberCheck(input)) {
+        
+      }
     });
   }
-
+  playerNumberCheck(input) {
+    if (input.length !== 3) {
+      throw new Error("잘못된 입력입니다.");
+    }
+    if (new Set(input).size !== 3) {
+      throw new Error("잘못된 입력입니다.");
+    }
+    return true;
+  }
 }
 
 const app = new App();
