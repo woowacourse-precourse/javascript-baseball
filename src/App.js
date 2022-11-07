a3f545e4e601e34684b9863fba6eddee06feffda
nalsae <nalsae0207@naver.com>
2022-11-07 20:21:06 +0900

    refactor: constantize values about error cases
    에러 케이스에 관련된 문자열의 상수화
    - #ERROR_CASES 클래스 필드를 정의하여 해결

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ const { Random, Console } = MissionUtils;
 
 class App {
   #RANDOM_NUM_LENGTH = 3;
+  #ERROR_CASES = {
+    reset: "failReset",
+    compare: "failCompare",
+  };
   #ERROR_MESSAGES = {
     failReset: "게임을 재시작하려면 1, 종료하려면 2를 입력해야 합니다.",
     failCompare: "서로 다른 1 ~ 9 사이의 숫자를 연속으로 3개 입력해야 합니다.",
@@ -90,12 +94,17 @@ class App {
       userInput.length !== 3 ||
       userInput.length !== new Set(userInput).size ||
       userInput.includes("0")
-    )
-      this.throwError("failCompare");
+    ) {
+      const { reset } = this.#ERROR_CASES;
+      this.throwError(reset);
+    }
   }
 
   findResetError(userInput) {
-    if (userInput !== "1" && userInput !== "2") this.throwError("failReset");
+    if (userInput !== "1" && userInput !== "2") {
+      const { compare } = this.#ERROR_CASES;
+      this.throwError(compare);
+    }
   }
 
   throwError(errorCase) {
