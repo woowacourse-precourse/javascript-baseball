6e3d04c8d17437c62cca66e1119afdf25a631338
su2333066 <su2333066@naver.com>
2022-11-08 04:31:49 +0900

    주석 제거

--- src/App.js
+++ src/App.js
@@ -11,16 +11,13 @@ class App {
     let strike = 0;
     let ball = 0;
     for (let number of this.selectComputerNumber) {
-      // 스트라이크
       if (
         this.inputNumber.includes(number) &&
         this.selectComputerNumber.indexOf(number) ===
           this.inputNumber.indexOf(number)
       ) {
         strike += 1;
-      }
-      // 볼
-      else if (
+      } else if (
         this.inputNumber.includes(number) &&
         this.selectComputerNumber.indexOf(number) !==
           this.inputNumber.indexOf(number)
@@ -28,41 +25,31 @@ class App {
         ball += 1;
       }
     }
-    // 스트라이크와 볼의 개수를 넘겨주면서 화면에 출력
+
     this.printHint(strike, ball);
   }
 
   printHint(strike, ball) {
-    // 볼이 없고, 스트라이크만 있을 경우
     if (ball === 0 && strike !== 0) {
-      // 3스트라이크가 아닐 경우
       if (strike !== 3) {
         MissionUtils.Console.print(`${strike}스트라이크`);
         this.inputNumber = [];
         this.inputNum();
-      }
-      // 3스트라이크일 경우
-      else {
+      } else {
         MissionUtils.Console.print(`${strike}스트라이크`);
         MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
         this.inputNumber = [];
         this.restartOrExit();
       }
-    }
-    // 스트라이크가 없고, 볼만 있을 경우
-    else if (strike === 0 && ball !== 0) {
+    } else if (strike === 0 && ball !== 0) {
       MissionUtils.Console.print(`${ball}볼`);
       this.inputNumber = [];
       this.inputNum();
-    }
-    // 아무것도 맞추지 못한 경우
-    else if (strike === 0 && ball === 0) {
+    } else if (strike === 0 && ball === 0) {
       MissionUtils.Console.print("낫싱");
       this.inputNumber = [];
       this.inputNum();
-    }
-    // 스트라이크와 볼의 개수가 1개이상일 경우
-    else {
+    } else {
       MissionUtils.Console.print(`${ball}볼 ${strike}스트라이크`);
       this.inputNumber = [];
       this.inputNum();
@@ -88,12 +75,8 @@ class App {
   }
 
   inputNum() {
-    // 사용자의 숫자 입력
     MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (number) => {
-      // 예외 처리
       this.inputException(number);
-
-      // 힌트 출력
       this.getHint();
     });
   }
@@ -121,7 +104,6 @@ class App {
   }
 
   selectRandomNum() {
-    // 컴퓨터의 1~9까지 서로다른 3자리의 숫자 선택
     while (this.selectComputerNumber.length < 3) {
       const number = MissionUtils.Random.pickNumberInRange(1, 9);
       if (!this.selectComputerNumber.includes(number)) {
@@ -132,7 +114,6 @@ class App {
 
   play() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
-
     this.selectRandomNum();
     this.inputNum();
   }
