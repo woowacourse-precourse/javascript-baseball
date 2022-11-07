3b370936bf3db817f3b5a928ce64aea18f0c4b55
jaehak26 <jack10829@naver.com>
2022-11-08 00:30:19 +0900

    사용자에게 입력값 받고 예외처리
    MissionUtils.Console.readLine으로 입력값을 받고 예외가 발생하면 재귀함수를 이용해 다시 실행한다.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
+// run `node index.js` in the terminal
+//@woowacourse/mission-utils
+
 const MissionUtils = require('@woowacourse/mission-utils');
 
+const GAME_START = 1;
+const GAME_END = 2;
+const ERROR = -1;
+
 class App {
-  play() {
-    this.gameStart();
+  #gameState;
+  #computerAnswer = [];
+  #userInput;
+
+  constructor() {
+    this.#gameState = GAME_START;
   }
 
-  gameStart() {
+  play() {
+    this.#computerAnswer = this.#gameStart();
+    console.log(this.#computerAnswer);
+
+    this.#inputFromUser();
+
+    //MissionUtils.Console.close();
+  }
+
+  //private 처리
+  #gameStart() {
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
 
     const computer = [];
@@ -16,12 +37,58 @@ class App {
         computer.push(number);
       }
     }
-    MissionUtils.Console.close();
-    console.log(computer);
+    return computer;
+  }
+
+  #inputFromUser() {
+    //출력 중간에 입력값이 보이는 에러가 있어 개행문자를 추가했습니다.
+    //ex) 숫자를 입력해345요
+    let input = 0;
+
+    MissionUtils.Console.readLine('숫자를 입력해주세요 : \n', (answer) => {
+      input = answer;
+      console.log(input);
+
+      //예외 발생 시 다시 입력 처리
+      if (this.#inputExcept(input) === ERROR) {
+        return this.#inputFromUser();
+      }
+
+      this.#userInput = this.#inputToArray(input)
+
+      
+
+      MissionUtils.Console.close();
+    });
+
+    return;
+  }
+
+  #
+
+  #inputToArray(input) {
+    return input.split('');
+  }
+
+  #inputExcept(input = '') {
+    if (input == '') {
+      MissionUtils.Console.print('입력값이 없습니다.');
+      return ERROR;
+    }
+
+    let numReg = /[0-9]/g;
+    if (input.match(numReg) == null) {
+      MissionUtils.Console.print('숫자가 아닙니다. ');
+      return ERROR;
+    }
+
+    if (input.length != 3) {
+      MissionUtils.Console.print('세자리 숫자가 아닙니다. ');
+      return ERROR;
+    }
+    return 0;
   }
 }
 
-const app = new App();
-app.play();
 
 module.exports = App;
