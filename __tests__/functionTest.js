09e101ee62d5bbcad746e93076c837fdaefb8189
chasj0326@naver.com <62418379+chasj0326@users.noreply.github.com>
2022-11-08 03:52:16 +0900

    test: tests/functionTest.js - 게임 구동 함수들 테스트 작성

--- __tests__/functionTest.js
+++ __tests__/functionTest.js
@@ -6,13 +6,53 @@ describe("기능 확인 테스트", () => {
   test("올바른 player 일 경우 true, 아니면 false 반환", () => {
     const player = ['123','1',' 1','a1','1234','112'];
     const result = player.map((str)=>app.isVaildPlayer(str));
+
     expect(result).toEqual([true,true,false,false,false,false]);
   });
 
   test("올바른 option 일 경우 true, 아니면 false 반환", () => {
     const option = ['1','11',' 1','2','22',' 2'];
     const result = option.map((str)=>app.isVaildOption(str));
+
     expect(result).toEqual([true,false,false,true,false,false]);
   });
 
+  test("computer : 숫자문자로 이루어진 길이3의 배열", () => {
+    const computer = app.makeComputer();
+    const computer_length = computer.length;
+    const numbers = ['1','2','3','4','5','6','7','8','9'];
+    let result;
+    computer.forEach((num)=>{
+      result = numbers.includes(num)
+    })
+
+    expect(computer_length).toEqual(3);
+    expect(result).toEqual(true);
+  })
+
+  test("스트라이크/볼 횟수 반환", () => {
+    const computers = [['1','4','5'],['3','6','7'],['7','3','1']]
+    const players = ['154','37','1']
+
+    let strikes = [];
+    players.forEach((player, idx)=>{
+      strikes.push(app.countStrike(player, computers[idx]))
+    })
+
+    let balls = [];
+    players.forEach((player, idx)=>{
+      balls.push(app.countBall(player, computers[idx], strikes[idx]))
+    })
+
+    expect(strikes).toEqual([1,1,0]);
+    expect(balls).toEqual([2,1,1]);
+  })
+
+  test("player가 숫자를 다 맞췄는지 반환", ()=>{
+    const result1 = app.playerWin(1);
+    const result2 = app.playerWin(3);
+    
+    expect(result1).toEqual(false);
+    expect(result2).toEqual(true);
+  })
 });
