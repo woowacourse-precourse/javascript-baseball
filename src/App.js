ad590c2464a1c41c2be7f49c34aeb849eb4d480a
leesoomin <soomin@leesoominui-MacBookPro.local>
2022-11-08 06:17:15 +0900

    feat: play 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 class App {
   play() {
-
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+    this.playGame();
   }
 
   // 사용자 입력 받기 1 예외처리
