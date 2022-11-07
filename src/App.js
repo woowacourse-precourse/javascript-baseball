e05c4f85ac72ccf38588f72260361fab9e8d7a4c
soomin <qkrtnals970804@gmail.com>
2022-11-07 23:02:40 +0900

    feat: 게임 재시작 또는 종료 기능 구현

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ const { Random } = require("@woowacourse/mission-utils");
 
 class App {
   play() {
+    Console.print("숫자 야구 게임을 시작합니다.");
     this.gameStart();
   }
 
   gameStart() {
-    Console.print("숫자 야구 게임을 시작합니다.");
     this.computerNumber = this.pickRandomNumber();
     this.input();
   }
@@ -64,15 +64,36 @@ class App {
   printPoint(strike, ball) {
     if (strike === 0 && ball === 0) {
       Console.print("낫싱");
+      this.input();
     } else if (strike === 3) {
       Console.print(`${strike}스트라이크`);
+      this.result();
     } else if (strike > 0 && ball === 0) {
       Console.print(`${strike}스트라이크`);
+      this.input();
     } else if (strike === 0 && ball > 0) {
       Console.print(`${ball}볼`);
+      this.input();
     } else {
       Console.print(`${ball}볼 ${strike}스트라이크`);
+      this.input();
     }
+  }
+
+  result() {
+    Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    Console.readLine(
+      "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
+      (restartNumber) => {
+        if (Number(restartNumber) === 1) {
+          this.gameStart();
+        } else if (Number(restartNumber) === 2) {
+          Console.close();
+        } else {
+          throw new Error("잘못된 입력 값입니다.");
+        }
+      }
+    );
   }
 }
 
