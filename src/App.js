aa573c621c53e76607e9e004d63c5dab71d761b0
subin <newdeas100@gmail.com>
2022-11-07 20:40:54 +0900

    feat(baseball): input과 target값을 비교해 strike ball 계산하는 handleData 기능추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
-
-function makeTarget(){
+function makeTarget() {
   const tempArr = [];
 
-  while(tempArr.length<3){
+  while (tempArr.length < 3) {
     const number = MissionUtils.Random.pickNumberInRange(1, 9);
-    if(!tempArr.includes(number)){
+    if (!tempArr.includes(number)) {
       tempArr.push(number);
     }
   }
@@ -14,33 +13,61 @@ function makeTarget(){
   return [...tempArr];
 }
 
-function readData(target){
+function readData(target) {
   let isAnswer = false;
 
-  while(!isAnswer){
-    MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (answer) =>{
-      isAnswer = handleData(answer, target);
+  while (!isAnswer) {
+    MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (input) => {
+      isAnswer = handleData(input, target);
     });
   }
 }
 
+function handleData(inputArr, targetArr) {
+  let [strike, ball] = [0,0];
+
+  inputArr.forEach((input,idx)=>{
+    const targetIdx = targetArr.findIndex(target => target === input);
+    if(targetIdx === idx){
+      strike += 1;
+    }
+    else if(targetIdx !== -1){
+      ball += 1;
+    }
+  })
+
+  return makeResult([strike,ball]);
+}
+
+function makeResult(countArr){
+  
+
+}
 
 
 
 class App {
   play() {
-    MissionUtils.Console.print('숫자 야구게임을 시작합니다.');
-    let isRepeat = true;
 
-    while(isRepeat){
-      const target = makeTarget();
-      readData(target);
+    try {
+      MissionUtils.Console.print('숫자 야구게임을 시작합니다.');
+      let isRepeat = true;
 
-
+      while (isRepeat) {
+        const target = makeTarget();
+        readData(target);
+        isRepeat =false;
+        MissionUtils.Console.close();
+      }
+    } catch (err) {
 
     }
+
+
+
   }
 }
+
 
 
 const app = new App();
