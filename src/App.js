7729cb6f448651943e681332ebd9d393abf8d8d9
Haram Baek <haram@Haramui-MacBookAir.local>
2022-11-07 22:00:33 +0900

    refactor: random number 생성 코드 변경(MissionUtils 사용)

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
-function createRandNum() {
-  let randNum = [];
+// function createRandNum() {
+//   let randNum = [];
 
-  for (let i = 0; i < 3; i++) {
-    let tempNum = MissionUtils.Random.pickNumberInList([1, 2, 3, 4, 5, 6, 7, 8, 9]);
-    if (!randNum.includes(tempNum)) {
-      randNum.push(tempNum);
-    } else {
-      i--;
-    }
-  }
+//   for (let i = 0; i < 3; i++) {
+//     let tempNum = MissionUtils.Random.pickNumberInList([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+//     if (!randNum.includes(tempNum)) {
+//       randNum.push(tempNum);
+//     } else {
+//       i--;
+//     }
+//   }
 
-  console.log(randNum, 'random number 생성');
-  MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+//   console.log(randNum, 'random number 생성');
+//   MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
 
-  return randNum
-}
+//   return randNum
+// }
 
 function checkNothing(randNum, inputNum) {
   let isNothing = true;
@@ -93,7 +93,8 @@ function inputUserAnswer(randNum) {
 class App {
   play() {
     // 1. 컴퓨터가 정답(1-9 사이의 서로 다른 수로 이루어진 3자리 숫자)을 생성한다.
-    const randNum = createRandNum();
+    const randNum = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
+    // console.log(randNum, "생성된 숫자");
     inputUserAnswer(randNum);
   }
 }
