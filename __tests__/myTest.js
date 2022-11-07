0281a5cc2c52a67ab216da5f73d7e0e05813d978
Ryong-E <think8867@gmail.com>
2022-11-07 17:48:11 +0900

    test: describe명 수정 테스트 코드 추가
    - 예외 테스트와 기능테스트로 나눔
    - validate가 잘 되는지 테스트 추가
    - 숫자 비교 기능 테스트시 값에 따른 출력이 잘 나오는지 테스트

--- __tests__/myTest.js
+++ __tests__/myTest.js
@@ -1,5 +1,7 @@
 const App = require('../src/App');
 const MissionUtils = require('@woowacourse/mission-utils');
+const getRandomNumbers = require('../src/utils/generate-random-number');
+const { restartEndGameAnswerValidator, guessAnswerValidate } = require('../src/utils/validation');
 
 const mockAnswer = (numbers) => {
   MissionUtils.Console.readLine = jest.fn();
@@ -17,16 +19,43 @@ const mockRandoms = (numbers) => {
   }, MissionUtils.Random.pickNumberInRange);
 };
 
-describe('숫자생성', () => {
-  test('랜덤수 생성', () => {
-    const app = new App();
-    const Numbers = app.getRandomNumbers();
-    const exceptNumbers = new Set(Numbers);
+describe('기능테스트', () => {
+  test('중복없는 3자리 숫자 생성', () => {
+    const exceptNumbers = new Set(getRandomNumbers());
     expect(exceptNumbers.size).toEqual(3);
   });
 
-  test('정답 입력 값 예외 테스트', () => {
-    const answers = ['102', '223', 'q23', '12', '1 2'];
+  test('숫자 비교 기능 테스트', () => {
+    const randoms = [4, 6, 7];
+    mockRandoms(randoms);
+    const app = new App();
+    app.play();
+    expect(app.outputResult('456')).toEqual('1볼 1스트라이크');
+    expect(app.outputResult('356')).toEqual('1볼');
+    expect(app.outputResult('459')).toEqual('1스트라이크');
+    expect(app.outputResult('123')).toEqual('낫싱');
+    expect(app.outputResult('467')).toEqual(
+      '3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료'
+    );
+  });
+});
+
+describe('예외 테스트', () => {
+  test('0이 들어간 입력시 에러', () => {
+    const randoms = [4, 6, 7];
+    const answers = ['102', '103'];
+    mockRandoms(randoms);
+    mockAnswer(answers);
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow();
+  });
+
+  test('1~9숫자가 아닐 때 에러', () => {
+    const randoms = [4, 6, 7];
+    const answers = ['10q'];
+    mockRandoms(randoms);
     mockAnswer(answers);
     expect(() => {
       const app = new App();
@@ -45,21 +74,16 @@ describe('숫자생성', () => {
     }).toThrow();
   });
 
-  test('재시작 입력 validate 예외 처리 테스트', () => {
+  test('restartEndGameAnswerValidator 예외 처리 테스트', () => {
     const numbers = ['12', '3', 'q', 1];
-    const app = new App();
     numbers.forEach((number) => {
-      expect(() => app.restartEndGameAnswerValidator(number)).toThrow();
+      expect(() => restartEndGameAnswerValidator(number)).toThrow();
     });
   });
-
-  test('숫자 비교 기능 테스트', () => {
-    const randoms = [4, 6, 7];
-    mockRandoms(randoms);
-    const app = new App();
-    app.play();
-    expect(app.outputResult('456')).toContain('볼');
-    expect(app.outputResult('123')).toContain('낫싱');
-    expect(app.outputResult('467')).toContain('3스트라이크');
+  test('guessAnswerValidate 예외 처리 테스트', () => {
+    const numbers = ['12', 'q23', '000', 1];
+    numbers.forEach((number) => {
+      expect(() => guessAnswerValidate(number)).toThrow();
+    });
   });
 });
