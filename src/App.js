f26ee1565ec1ac5032ba3bbcb35d809b2ddecb93
sein <hyuntpdls@gmail.com>
2022-11-08 01:35:58 +0900

    feat: 컴퓨터의 랜덤 숫자 생성

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 class App {
+  constructor() {
+    this.randomList = this.makeRandomNumber();
+  }
+  
   play() {
     this.gameStartMsg();
   }
 
   gameStartMsg() {
     console.log('숫자 야구 게임을 시작합니다.');
+  }
+
+  makeRandomNumber() {
+    const randomArr = [];
+    while (randomArr.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!randomArr.includes(number)) {
+        randomArr.push(number);
+      }
+    }
+    return randomArr;
   }
 }
 
