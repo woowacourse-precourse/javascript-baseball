5d20498b76b26ed799342b1c612d481ff4ce488a
saetakki <lime.lemonade03@gmail.com>
2022-11-08 01:21:18 +0900

    refactor: delete duplicated function

--- src/App.js
+++ src/App.js
@@ -2,33 +2,20 @@ const MissionUtils = require('@woowacourse/mission-utils');
 const Console = MissionUtils.Console;
 
 const getUserInput = (f, num) => {
-  if (f(num) === false) {
-    throw '유효한 입력값이 아닙니다!';
-  }
-  return num;
+  return f(num);
 };
 
-const isNumber = value => {
-  const regNumExp = /[0-9]/g;
-  if (regNumExp.test(value) === false) {
-    throw '숫자가 아닙니다!';
+const isValidNumber = value => {
+  const regValidNumExp = /^[1-9]{3}$/;
+  if (regValidNumExp.test(value) === false) {
+    throw '유효한 숫자가 아닙니다!';
   }
   return value;
 };
 
-const isValidNumber = value => {
-  const regValidNumExp = /^\d{3}$/
-  if (regValidNumExp.test(value) === false){
-    throw '유효한 숫자가 아닙니다!'
-  }
-  return 
-}
-
-
-
-
 class App {
   play() {}
 }
+
 
 module.exports = App;
