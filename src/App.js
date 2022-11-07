56691199cd4eed5071b933723c2c93c55cf92f6c
osdoonhyun <skw1996@naver.com>
2022-11-07 16:44:15 +0900

    Feat: App에서 게임 시작 시 Game START 문구 출력 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const Game = require("./Game");
+const Computer = require("./Computer");
+
 class App {
-  play() {}
+  constructor() {
+    this.computer = new Computer();
+  }
+
+  start() {
+    const computerNumber = this.computer.makeRandomNumber();
+  }
+
+  restart() {}
+
+  exit() {}
+
+  playBall() {
+    MissionUtils.Console.print(Game.MESSAGE.START);
+    this.start();
+  }
 }
+
+const numberbaseball = new App();
+numberbaseball.playBall();
 
 module.exports = App;
