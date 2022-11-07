2f2d7a05abc01a5134ae5833044249f80bb26997
shlee0820 <72565083+shlee0820@users.noreply.github.com>
2022-11-08 00:01:49 +0900

    chore: 코드 컨벤션을 위한 eslint, prettier 설치 및 설정

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    singleQuote: true, // 문자열을 사용 할 때에는 ' '를 사용
+    semi: true, // 코드 종료시 무조건 ;(세미콜론)
+    useTabs: true, // tab 대신에 스페이스를 사용
+    tabWidth : 2, // 들여쓰기 크기
+    trailingComma : 'all', // 객체나 배열의 마지막 값에도 , 를 붙임
+    printWidth : 80, // 한 줄의 최대 칸수
+    bracketSpacing: 'avoid' //화살표 함수가 하나의 매개변수를 받을 때 괄호 생략
+  }
