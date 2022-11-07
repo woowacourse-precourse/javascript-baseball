bdb78f00bb57238eddbaab2a7e8d7c27f66edff0
KINHYEONJI <hyeonji0718@gmail.com>
2022-11-07 22:12:13 +0900

    feat(App) : judge가 판단한 볼과 스트라이크 개수를 보여주는 함수 showBallState 구현

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 const MissionUtils = require("@woowacourse/mission-utils");
-const { SYS_MESSAGE } = require("./Constant.js");
+const { SYS_MESSAGE, BALL_STATE } = require("./Constant.js");
 
 class App {
   alertStart() {
     MissionUtils.Console.print(SYS_MESSAGE.START_MESSAGE);
+  }
+
+  showBallState(strikeCnt, ballCnt) {
+    if (strikeCnt === 3)
+      MissionUtils.Console.print(
+        strikeCnt + BALL_STATE.STRIKE + SYS_MESSAGE.FINISH_MESSAGE
+      );
+    if (strikeCnt > 0 && ballCnt > 0)
+      MissionUtils.Console.print(
+        ballCnt + BALL_STATE.BALL + strikeCnt + BALL_STATE.STRIKE
+      );
+    if (strikeCnt > 0 && ballCnt === 0)
+      MissionUtils.Console.print(strikeCnt + BALL_STATE.STRIKE);
+    if (strikeCnt === 0 && ballCnt > 0)
+      MissionUtils.Console.print(ballCnt + BALL_STATE.BALL);
+    if (strikeCnt === 0 && ballCnt === 0)
+      MissionUtils.Console.print(BALL_STATE.NOTHING);
   }
 }
 
