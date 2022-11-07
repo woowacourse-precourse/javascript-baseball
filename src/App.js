71fd18247f8c04a4bdff55276e0efde3f948b1b1
RosaDamascena <hsj990604@gmail.com>
2022-11-07 16:42:53 +0900

    feat : 볼 값을 구하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,37 +1,38 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
-function compare(computer_num, user_num) {
-  let i = 0;
-  let strike = 0;
-  let ball = 0;
-
-  //input 값 배열에 저장
-  const userNumArr = [];
-  userNumArr.push(user_num);
-
-  //스트라이크
-  for (i = 0; i < 3; i++) {
-    if (computer_num[i] == userNumArr[i]) {
-      strike += 1;
-    }
-  }
-  return strike;
-  
-}
-
 class App {
+  compare(computerNum, userNum) {
+    let i = 0;
+    let strike = 0;
+    let ball = 0;
+  
+    //input 값 배열에 저장
+    const userNumArr = [];
+    userNumArr.push(userNum);
+  
+    //스트라이크
+    for (i = 0; i < 3; i++) {
+      if (computerNum[i] == userNumArr[i]) {
+        strike += 1;
+      }
+      return strike;
+    }
+    
+    const intersect = computerNum.filter(x => userNumArr(x));
+    let countIntersect = intersect.length;
+    ball = countIntersect - strike;
+  }
+
   play() {
-    const computer_num = MissionUtils.Random.pickUniqueNumbersInRange([1, 9, 3]);
+    const computerNum = MissionUtils.Random.pickUniqueNumbersInRange([1, 9, 3]);
 
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
 
     let user_num;
     MissionUtils.Cosole.readLine("숫자를 입력해주세요 :", (number) => {
-      user_num = number;
-      console.print(user_num);
+      userNum = number;
+      console.print(userNum);
     });
-
-
   }
 }
 
