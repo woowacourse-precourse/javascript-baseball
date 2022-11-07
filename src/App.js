62023d9bc6aa14f09ad2701190c60aabb920d37a
junghwki <junghwki@gmail.com>
2022-11-08 02:05:09 +0900

    feat: 게임 메시지 상수화

--- src/App.js
+++ src/App.js
@@ -1,4 +1,14 @@
 const MissionUtils = require("@woowacourse/mission-utils");
+const NUMBER_LENGTH = 3;
+const RESTART_CODE = "1";
+const EXIT_CODE = "2";
+const START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+const INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+const BALL_STRING = "볼";
+const STRIKE_STRING = "스트라이크";
+const NOTHING_STRING = "낫싱";
+const CORRECT_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+const RESTART_MESSAGE = `게임을 새로 시작하려면 ${RESTART_CODE}, 종료하려면 ${EXIT_CODE}를 입력하세요.`;
 
 class App {
   play() {
