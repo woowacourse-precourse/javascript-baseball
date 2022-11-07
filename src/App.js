05d1788b074f3c5b882057cdb8ec868a107a211d
seoye0ng <101791501+seoye0ng@users.noreply.github.com>
2022-11-07 22:30:13 +0900

    feat : 게임시작 문구 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const { error, Console } = require("console");
+
 class App {
-  play() {}
+  play() {
+    startGame();
+  }
 }
 
 module.exports = App;
+
+function startGame() {
+  MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+}
