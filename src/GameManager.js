dcaa2ee29332f7eb0e6ddc503798b4fbbf75c157
jiho1996 <jhp9270@naver.com>
2022-11-07 18:09:35 +0900

    refactor : getResult함수에서 스트라이크, 볼 로직 분리

--- src/GameManager.js
+++ src/GameManager.js
@@ -8,44 +8,56 @@ class GameManager {
     const userScore = {
       strikeCount: 0,
       ballCount: 0,
-      nothingCount: 0,
     };
 
     userInput.forEach((userValue, idx) => {
       if (isStrike(userValue, idx)) userScore.strikeCount += 1;
       else if (computerInput.includes(userValue)) userScore.ballCount += 1;
-      else userScore.nothingCount += 1;
     });
     return userScore;
   }
 
-  getResult({ strikeCount, ballCount, nothingCount }) {
-    if (strikeCount === 3) {
+  getResult(strikeCount, ballCount) {
+    if (this.getJudgeStrikeOrBall().isAllStrike(strikeCount, ballCount)) {
       MissionUtils.Console.print(`${strikeCount}스트라이크`);
       MissionUtils.Console.print(`3개의 숫자를 모두 맞히셨습니다 ! 게임 종료`);
       return true;
     }
-    if (strikeCount && ballCount) {
+
+    if (this.getJudgeStrikeOrBall().isStrikeAndBall(strikeCount, ballCount)) {
       MissionUtils.Console.print(`${ballCount}볼 ${strikeCount}스트라이크`);
     }
-    if (strikeCount && !ballCount) {
+    if (this.getJudgeStrikeOrBall().isOnlyStrike(strikeCount, ballCount)) {
       MissionUtils.Console.print(`${strikeCount}스트라이크`);
     }
-    if (ballCount && !strikeCount) {
+    if (this.getJudgeStrikeOrBall().isOnlyBall(strikeCount, ballCount)) {
       MissionUtils.Console.print(`${ballCount}볼`);
     }
-    if (nothingCount === 3) {
+    if (this.getJudgeStrikeOrBall().isNotStrikeAndNotBall(strikeCount, ballCount)) {
       MissionUtils.Console.print(`낫싱`);
     }
     return false;
   }
 
-  start(numberFromUser, numberFromComputer) {
-    const { strikeCount, ballCount, nothingCount } = this.getStrikeBallCount(
-      numberFromUser,
-      numberFromComputer
-    );
-    let isGameEnd = this.getResult({ strikeCount, ballCount, nothingCount });
+  getJudgeStrikeOrBall() {
+    const isAllStrike = (strike, ball) => strike === 3 && ball === 0;
+    const isStrikeAndBall = (strike, ball) => strike && ball;
+    const isOnlyStrike = (strike, ball) => strike && !ball;
+    const isOnlyBall = (strike, ball) => ball && !strike;
+    const isNotStrikeAndNotBall = (strike, ball) => !ball && !strike;
+    return {
+      isAllStrike,
+      isStrikeAndBall,
+      isOnlyStrike,
+      isOnlyBall,
+      isNotStrikeAndNotBall,
+    };
+  }
+
+  apply(numberFromUser, numberFromComputer) {
+    const { strikeCount, ballCount } = this.getStrikeBallCount(numberFromUser, numberFromComputer);
+
+    let isGameEnd = this.getResult(strikeCount, ballCount);
     return isGameEnd;
   }
 }
