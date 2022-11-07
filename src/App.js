a374a6d794b43439cef6ddbe3d0e8377ff3b863d
Serin-Kim <serin9864@gmail.com>
2022-11-07 20:38:22 +0900

    refactor: handleSuccess 함수에서 askUserRestartOrExit 함수 분리
    사용자에게 입력을 받고 함수를 실행하는 로직을 함수로 분리함

--- src/App.js
+++ src/App.js
@@ -71,14 +71,17 @@ class App {
     MissionUtils.Console.close();
   };
 
-  handleSuccess = () => {
-    MissionUtils.Console.print(MESSAGE.SUCCESS);
-
+  askUserRestartOrExit = () => {
     MissionUtils.Console.readLine(MESSAGE.RESTART_OR_EXIT, (input) => {
       if (input === OPTION.RESTART) return this.play();
       if (input === OPTION.EXIT) return this.exit();
       if (input !== (OPTION.RESTART || OPTION.EXIT)) throw new Error(ERROR.INVALID_INPUT);
     });
+  };
+
+  handleSuccess = () => {
+    MissionUtils.Console.print(MESSAGE.SUCCESS);
+    return this.askUserRestartOrExit();
   };
 
   getResult = (ANSWER, input) => {
