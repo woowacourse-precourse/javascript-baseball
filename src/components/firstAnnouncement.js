816d39b269a9fd28dda054aa424860132656aa51
Ryan-Dia <cjfdnjs158@naver.com>
2022-11-07 18:48:11 +0900

    feat: 기능1  게임 시작시 멘트 출력 함수

---
+++ src/components/firstAnnouncement.js
@@ -0,0 +1,7 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
+const FirstAnnouncement = () => {
+  MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+};
+
+module.exports = FirstAnnouncement;
