d8283cb3d0d66006927eba4d320c48d93e7c701b
Jinu <jinu200131@gmail.com>
2022-11-07 17:39:38 +0900

    feat (User) : 사용자 입력 예외 검사 기능 추가

---
+++ src/User.js
@@ -0,0 +1,13 @@
+class User {
+    isInputValid(userGuess) {
+        if(isNaN(userGuess) || !Number.isInteger(Number(userGuess))) return false;
+    
+        if(userGuess.length !== 3) return false;
+    
+        if(new Set([...userGuess]).size !== 3) return false;
+    
+        if([...userGuess].includes('0')) return false;
+    
+        return true;
+    }
+}
