bc6652ef67c440c9f39297dcda490814ebda4e31
osdoonhyun <skw1996@naver.com>
2022-11-07 22:43:57 +0900

    Feat: App에서 사용자가 숫자 모두 맞출 시 게임 종료 문구 출력 후 게임 종료 기능 & 못 맞출 시 과정 반복하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -23,12 +23,22 @@ class App {
 
       //사용자가 입력한 숫자에 대한 결과 출력
       MissionUtils.Console.print(Game.ballCount(userNumber, computerNumber));
-    });
 
-    //컴퓨터가 선택한 숫자 3개 모두 맞출 시 게임 종료 문구 출력 후 게임 종료
+      //컴퓨터가 선택한 숫자 3개 모두 맞출 시 게임 종료 문구 출력 후 게임 종료
+      if (Game.ballCount(userNumber, computerNumber) === Game.MESSAGE.OUT) {
+        MissionUtils.Console.print(Game.MESSAGE.END);
+        MissionUtils.Console.print(Game.MESSAGE.SELECT);
+        return this.end();
+      }
+      if (Game.ballCount(userNumber, computerNumber) !== Game.MESSAGE.OUT) {
+        return this.start();
+      }
+    });
 
     //사용자 재시작/ 종료 선택
   }
+
+  end() {}
 
   restart() {}
 
