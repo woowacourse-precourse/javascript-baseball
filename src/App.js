2be726bbbdd509efeedccf71b9d1633689e60f65
Dae-une <ko301201@naver.com>
2022-11-07 16:05:49 +0900

    feat :  입력한 값 에러 처리

--- src/App.js
+++ src/App.js
@@ -19,6 +19,37 @@ class App {
       return result.join("");
     };
 
+    const enterNumber = () => {
+      Console.readLine("숫자를 입력해주세요 : ", (answer) => {
+        if (targetNums.toString() === answer) {
+          return (
+            Console.print("3스트라이크"), Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료"), reStartOrEnd()
+          );
+        }
+        if (!checkError(answer)) {
+          Console.print(checkNumbers(answer));
+          enterNumber();
+        }
+      });
+    };
+
+    const checkError = (input) => {
+      const regExp = /[^1-9]/g;
+      if (regExp.test(input)) {
+        throw "1 부터 9까지의 3자리 숫자만 입력해주세요.";
+      }
+      if (input.length !== 3) {
+        throw "3자리 숫자를 입력해주세요";
+      }
+      if (new Set(input).size !== 3) {
+        throw "중복되지 않게 입력해 주세요";
+      }
+      if (tries.includes(input)) {
+        throw "이미 시도한 값입니다.";
+      }
+      return false;
+    };
+
     const startGame = () => {
       targetNums = randomPick();
       enterNumber();
