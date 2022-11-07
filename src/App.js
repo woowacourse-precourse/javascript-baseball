40be573e3b4554ec712f8e0aa27d75fb17a8eecb
cjm0611 <81799492+cjm0611@users.noreply.github.com>
2022-11-08 01:11:53 +0900

    feat: 게임 중 사용자 입력 예외처리 (기능6-1)

--- src/App.js
+++ src/App.js
@@ -23,41 +23,36 @@ class App {
 
   numberBaseballGame(computer) {
     MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (user) => {
-      try{
-        this.invalidValueException(user);
+      this.throwExceptionForInvalidInput(user);
 
-        if(computer === user){
-          MissionUtils.Console.print('3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료')
-          this.selectRestartOrExitGame();
-        }
-        else{
-          this.showBallStrike(computer, user);
-          this.numberBaseballGame(computer);
-        }
-      } catch(e){
-        MissionUtils.Console.print(e);
-        MissionUtils.Console.close();
+      if(computer === user){
+        MissionUtils.Console.print('3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료')
+        this.selectRestartOrExitGame();
+      }
+      else{
+        this.showBallStrike(computer, user);
+        this.numberBaseballGame(computer);
       }
     });
   }
 
-  invalidValueException(user){
+  throwExceptionForInvalidInput(user){
     if(user === '' || user === ' ' || user.includes(' ')){
-      throw "예외 발생! 공백은 입력되지 않습니다.";
+      throw "입력에 공백은 포함될 수 없습니다.";
     }
     if(user.length !== 3){
-      throw "입력의 길이는 3이여야 합니다.";
+      throw "3자리의 수를 입력해주세요.";
     }
     if(isNaN(Number(user))){
-      throw "숫자가 아닌 입력이 있습니다.";
+      throw "숫자만 입력해주세요.";
     }
 
     [...user].forEach((num, numIndex)=>{
       if(isNaN(num)){
-        throw "음수 또는 소수점 입력은 불가합니다.";
+        throw "음수 또는 소수점은 입력할 수 없습니다.";
       }
-      if(num<1 || num>9){ //이 경우 num=0
-        throw "각 숫자는 1~9 사이입니다.";
+      if(!Number(num)){
+        throw "각 자리의 숫자는 1~9만 허용합니다.";
       }
       [...user].forEach((compareNum, compareIndex)=>{
         if(numIndex !== compareIndex && num === compareNum){
@@ -111,5 +106,8 @@ class App {
     });
   }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
