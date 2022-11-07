5d715fd1daeae60a7b921f4fe3245093e87c394c
Miniling <bsm3925@naver.com>
2022-11-08 01:26:54 +0900

    Add: func_gameStart

--- src/App.js
+++ src/App.js
@@ -72,13 +72,14 @@ class App {
   }
 
   // 게임 시작
-  play() {
+  gameStart() {
     this.computer = [];
-    this.user = [];
-
-    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
     this.computerSet();
     this.userPlay();
+  }
+  play() {
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    this.gameStart();
   }
 }
 
