f776e77cf1c2b23ed0e6b074b98d6fb7fc2e0d98
kim-limit <rlawogks2468@naver.com>
2022-11-07 15:25:32 +0900

    feat: 'add print result message func'

--- src/App.js
+++ src/App.js
@@ -21,10 +21,24 @@ function checkResult(computer, number) {
     }
   }
 
+  printMessage(strike, ball);
+
   if (strike === 3) {
     return true;
   }
   return false;
+}
+
+function printMessage(strike, ball) {
+  if (ball === 0 && strike === 0) {
+    MissionUtils.Console.print("낫싱");
+  } else if (ball === 0) {
+    MissionUtils.Console.print(`${strike}스트라이크`);
+  } else if (strike === 0) {
+    MissionUtils.Console.print(`${ball}볼`);
+  } else {
+    MissionUtils.Console.print(`${ball}볼 ${strike}스트라이크`);
+  }
 }
 
 function checkException(number) {
