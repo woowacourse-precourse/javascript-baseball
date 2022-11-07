7375a3cb22e73685c81c57a7ab133f7648d05c43
seunghoria <102285042+seunghoria@users.noreply.github.com>
2022-11-07 20:53:13 +0900

    test: 사용자 입력값 예외사항 파일 분리에 따른 테스트 코드 수정

--- __tests__/UserNumberTest.js
+++ __tests__/UserNumberTest.js
@@ -1,42 +1,37 @@
-const App = require("../src/App.js");
+const isValidUser = require("../src/modules/isValidUser");
 
 describe("사용자 입력값 유효성 검사 테스트", () => {
   test(`사용자가 0을 포함할 경우 예외발생`, () => {
-    const app = new App();
     const input = "078";
-    const result = () => app.isValidUser(input);
+    const result = () => isValidUser(input);
 
     expect(result).toThrow();
   });
 
   test(`사용자가 3개 숫자를 입력하지 않은 경우 예외발생`, () => {
-    const app = new App();
     const input = "0789";
-    const result = () => app.isValidUser(input);
+    const result = () => isValidUser(input);
 
     expect(result).toThrow();
   });
 
   test(`사용자가 중복되는 숫자를 입력할 경우 예외발생`, () => {
-    const app = new App();
     const input = "778";
-    const result = () => app.isValidUser(input);
+    const result = () => isValidUser(input);
 
     expect(result).toThrow();
   });
 
   test(`사용자가 숫자가 아닌 값을 입력할 경우 예외발생`, () => {
-    const app = new App();
     const input = "tw8";
-    const result = () => app.isValidUser(input);
+    const result = () => isValidUser(input);
 
     expect(result).toThrow();
   });
 
   test(`사용자가 공백을 입력할 경우 예외발생`, () => {
-    const app = new App();
     const input = " ";
-    const result = () => app.isValidUser(input);
+    const result = () => isValidUser(input);
 
     expect(result).toThrow();
   });
