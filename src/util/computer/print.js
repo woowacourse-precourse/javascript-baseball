f03caf3d84d26ee63ebc51184e12a41533cf0ef4
SNUPI <joohaem@daum.net>
2022-11-07 10:43:24 +0900

    fix: export error

--- src/util/computer/print.js
+++ src/util/computer/print.js
@@ -1,7 +1,9 @@
-export function getPrint(ball, strike) {
+function getPrint(ball, strike) {
   if (ball === 0 && strike === 0) return "낫싱";
   if (ball && strike === 0) return `${ball}볼`;
   if (ball === 0 && strike) return `${strike}스트라이크`;
   if (ball && strike) return `${ball}볼 ${strike}스트라이크`;
   return null;
 }
+
+module.exports = { getPrint };
