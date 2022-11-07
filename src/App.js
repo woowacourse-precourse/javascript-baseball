38d5cf2887c655955b42329a3901e953bed579a0
wonseok <rhdqngkrh77@khu.ac.kr>
2022-11-08 03:52:55 +0900

    feat: 랜덤 정답 생성 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
   play() {}
+  constructor() {}
+  play() {
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    const answer = this.getThreeNum();
+  }
+
+  getThreeNum() {
+    const computer = [];
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+    return computer;
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
