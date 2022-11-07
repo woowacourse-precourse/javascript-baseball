8940ea2b205bf0e4ebfdd313d1be9b2d38d452fd
Conchip <86344121+Conchip@users.noreply.github.com>
2022-11-08 05:06:59 +0900

    숫자 입력, throw문 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  play() {
+    const app = new App();
+    var input = app.setInput();
+    numberCheck(input);
+  }
+
+  setInput() {
+    var inputNumber;
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+    MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (inputNumber) => {
+      console.log(`입력한 숫자 : ${inputNumber}`);
+    });
+  }
+  
+}
+
+function numberCheck(input) {
+  if(typeof(input) !== "number")
+    throw '숫자를 입력해주세요';
+  if(input.length !== 3)
+    throw '숫자 3자리가 아닙니다.';
+  
 }
 
 module.exports = App;
