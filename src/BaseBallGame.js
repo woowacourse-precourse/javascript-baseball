b2e466bd6e3de494f8bbef02f4af5223bfb864bc
JaeyeonHong <39540473+HongJaeyeon@users.noreply.github.com>
2022-11-08 04:48:37 +0900

    feat: 결과를 출력하는 printResult 기능 추가

--- src/BaseBallGame.js
+++ src/BaseBallGame.js
@@ -72,13 +72,56 @@ class BaseBallGame{
     }
 
     compareUserWithAnswer = () => {
+        console.log(this.answer,this.userAnswer);
         for (let idx=0; idx<3; idx++){
             if (this.answer[idx] === this.userAnswer[idx]){
                 this.countResult("strike");
             }else if (this.answer.includes(this.userAnswer[idx])){
                 this.countResult("ball");
             }
-        };
+        }
+        this.printResult();
+    }
+
+    printCorrectMessage = () => {
+        MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        this.exitOrRestart();
+    }
+
+    exitOrRestart = () => {
+        MissionUtils.Console.readLine("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.", (input) => {
+            if (parseInt(input) === 1){
+                this.start();
+            }else if (parseInt(input) === 2){
+                return;
+            }else {
+                throw new Error("올바르지 않은 값이 입력됐습니다.");
+            }
+        });
+    }
+
+    printResult = () => {
+        let message = "";
+        if (this.score["strike"] === 3){
+            MissionUtils.Console.print(`${this.score["strike"]}스트라이크`);
+            this.printCorrectMessage();
+        }else {
+            if (!(this.score["strike"] + this.score["ball"])){
+                message = "낫싱";
+            }
+            
+            if (this.score["ball"] && this.score["strike"]){
+                message = `${this.score["ball"]}볼 `;
+            }else if(this.score["ball"] && !this.score["strike"]){
+                message = `${this.score["ball"]}볼`;
+            }
+            
+            if (this.score["strike"]){
+                message += `${this.score["strike"]}스트라이크`;
+            }
+            MissionUtils.Console.print(message);
+            this.enterUserInput();
+        }
     }
 }
 
