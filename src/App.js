294f48eff315322128ca588d7e0001f03da2adb4
jetty8013 <jetty8013@gmail.com>
2022-11-08 02:26:05 +0900

    fix: 재시작 기능 오류 수정

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ const game = require("./game")
 
 class App {
   play() {
-    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.")
+    // MissionUtils.Console.print("숫자 야구 게임을 시작합니다.")
     const computerNumber = game.computerNumber()
 
     while(true){
       let gameResult = game.battle(game.userInput(),computerNumber)
       if(game.message(gameResult) === "게임 종료") break;
       }
-    if(game.restart() === 1) this.play();
+    
+    let restart = game.restart();
+
+    if(restart === 1) return this.play();
+    else if(restart === 2) return MissionUtils.Console.close();
   }
 }
 
