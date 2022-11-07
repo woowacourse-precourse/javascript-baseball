c0a9fdb9a2ec3bc6bcd4ebd3b76871dd77c6e708
Wang HoEun <hoeunwang@gmail.com>
2022-11-07 10:57:51 +0900

    feat: user input 값 전체 체크 함수 생성

--- src/component/User.js
+++ src/component/User.js
@@ -1,17 +1,29 @@
 class User {
+  checkInput(userInput) {
+    return (
+      this.checkInputLength(userInput) &&
+      this.checkInputExist(userInput) &&
+      this.checkInputNum(userInput) &&
+      this.checkInputZero(userInput)
+    );
+  }
+
   checkInputLength(userInput) {
     return userInput.length === 3;
   }
+
   checkInputExist(userInput) {
     const isExist = new Set([...userInput]).size !== 3;
     return isExist === false;
   }
+
   checkInputNum(userInput) {
     const setInputToNum = Number(userInput);
     const isNumber = (num) => !Number.isNaN(num) && typeof num === "number";
 
     return isNumber(setInputToNum);
   }
+
   checkInputZero(userInput) {
     const isZeroExist = [...userInput].includes("0");
     return isZeroExist === false;
