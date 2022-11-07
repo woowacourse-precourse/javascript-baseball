fe407454f6d32b840134ca896a26b9d94a448909
Wang HoEun <hoeunwang@gmail.com>
2022-11-07 13:35:08 +0900

    feat: start 함수와 user,computer,play constructor 생성

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+const Computer = require("./component/Computer");
+const Play = require("./component/Play");
+const User = require("./component/User");
 const { MESSAGE } = require("./constant/message.constant");
 
 class App {
+  constructor() {
+    this.computer = new Computer();
+    this.user = new User();
+    this.game = new Play();
+  }
   play() {
     Console.print(MESSAGE.START);
     this.start();
+  }
+
+  start() {
+    const computerNum = this.computerNum.makeNumbers();
   }
 }
 
