1a7c86e80a97679c1c5989a8913935e6bcc5d7e7
ShimSungBo <shimdb96@gmail.com>
2022-11-07 23:45:59 +0900

    refactor(checkVaildData): 함수 내 예외처리 추가 1. isUniqueNumber 입력 값이 중복이 없는 3개인지 확인 2. userInput 값이 3개와 같지 않으면 예외처리

--- src/checkVaildData.js
+++ src/checkVaildData.js
@@ -1,15 +1,19 @@
-function checkVaildData(input) {
-  const vaildNumber = [...input].map((num) => {
-    if (isNaN(num)) throw new Error();
+const checkVaildData = (userInput) => {
+  const isUniqueNumber = new Set(userInput.split("")).size === 3;
+  const vaildNumber = [...userInput].map((num) => {
+    if (isNaN(num)) throw "잘못된 문자를 입력했습니다. 프로그램을 종료합니다.";
     return +num;
   });
-  const isUniqueNumber = new Set(input.split("")).size === 3;
+
+  if (userInput.length !== 3) {
+    throw "잘못된 문자를 입력했습니다. 프로그램을 종료합니다.";
+  }
 
   if (!vaildNumber || !isUniqueNumber || vaildNumber.includes(0)) {
-    throw new Error();
+    throw "잘못된 문자를 입력했습니다. 프로그램을 종료합니다.";
   }
 
   return vaildNumber;
-}
+};
 
 module.exports = checkVaildData;
