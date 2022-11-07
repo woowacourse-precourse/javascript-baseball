d53bb311b4a50df9bcbb9ee3304cd2d36813b053
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2022-11-08 01:54:26 +0900

    test: 컴퓨터 수 생성

--- __tests__/BaseBallGameTest.js
+++ __tests__/BaseBallGameTest.js
@@ -16,4 +16,13 @@ describe("구현 기능 목록 Test", () => {
     app.printGameMsg("안녕하세요.");
     expect(logSpy).toHaveBeenCalledWith("안녕하세요.");
   });
+
+  test("1부터 9까지 서로 다른 수로 이루어진 컴퓨터의 3자리 수 배열을 생성하는 경우 ", () => {
+    const computerNumArr = app.getComputerNum();
+    const set = new Set(computerNumArr);
+
+    expect(computerNumArr).not.toContain(0); // 1부터 9까지인지
+    expect(computerNumArr).toHaveLength(3); // 3자리인지
+    expect(Array.from(set)).toHaveLength(3); // 서로 다른 수인지
+  });
 });
