7bc033813dd247376c81f122475d96d92eb07cb1
Miniling <bsm3925@naver.com>
2022-11-08 01:25:32 +0900

    Add: func_printResult

--- src/App.js
+++ src/App.js
@@ -7,8 +7,17 @@ class App {
   }
 
   // 결과 출력
-  printResult() {
-
+  printResult(ball, strike) {
+    if (ball === 0 && strike === 0) MissionUtils.Console.print("낫싱");
+    else if (strike === 3) {
+      MissionUtils.Console.print("3스트라이크");
+      MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+      this.gameOver();
+    }
+    else {
+      MissionUtils.Console.print(`${ball}볼 ${strike}스트라이크`);
+    }
+    this.userPlay();
   }
 
   // 입력 값과 컴퓨터 숫자 비교
