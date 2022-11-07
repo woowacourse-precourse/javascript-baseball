3931155cb00e0e1659a2eb8ed29f1225205153e2
Ryong-E <think8867@gmail.com>
2022-11-07 11:19:37 +0900

    fix: scoreConversion함수 스트라이크나 볼이 없어도 볼 스트라이크 출력되는 버그 수정
    - ball과 strike 둘 다 있을때 ball,strike둘다 출력 조건문 추가
    - ball은 있는데 strike가 없을때 ball만 출력 조건문 추가
    - strike는 있는데 ball이 없을 때 strike만 출력 조건문 추가

--- src/App.js
+++ src/App.js
@@ -92,8 +92,15 @@ class App {
     if (strike === 3) {
       return '3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료';
     }
-
-    return `${ball}볼 ${strike}스트라이크`;
+    if (ball > 0 && strike > 0) {
+      return `${ball}볼 ${strike}스트라이크`;
+    }
+    if (ball > 0 && strike === 0) {
+      return `${ball}볼`;
+    }
+    if (strike > 0 && ball === 0) {
+      return `${strike}스트라이크`;
+    }
   }
 
   play() {
