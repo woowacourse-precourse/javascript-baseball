33b2f5b497e53aee4f04118797fba4598587f201
jisu-kim <kss2460@naver.com>
2022-11-08 01:32:12 +0900

    test: isBall 테스트 변경

--- __tests__/isBall.test.js
+++ __tests__/isBall.test.js
@@ -2,6 +2,6 @@ const isBall = require("../src/isBall");
 
 describe("볼 횟수 체크", () => {
   test("값이 있는지 확인", () => {
-    expect(isBall([1, 2, 3], [1, 3, 2])).toBe(2);
+    expect(isBall([1, 2, 3], [1, 3, 2])).toBe(`2볼`);
   });
 });
