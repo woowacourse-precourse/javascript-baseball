3eecfedc83d51049c56284c126ca4dd2b040ea0f
pakxe <pigkill40@naver.com>
2022-11-08 04:39:42 +0900

    test: 테스트 케이스의 이름을 알아보기 쉽게 수정

--- __tests__/ValidationTest.js
+++ __tests__/ValidationTest.js
@@ -8,61 +8,61 @@ const {
 const ERROR = require('../src/Constants/Error');
 
 describe('길이 검사', () => {
-  test('case1', () => {
+  test('길이가 맞는 123', () => {
     expect(validLength([1, 2, 3])).toBeTruthy();
   });
-  test('case2', () => {
+  test('길이 초과한 1234', () => {
     expect(validLength([1, 2, 3, 4])).toBeFalsy();
   });
-  test('case3', () => {
+  test('길이 미달인 12', () => {
     expect(validLength([1, 2])).toBeFalsy();
   });
-  test('case4', () => {
+  test('공백', () => {
     expect(validLength([])).toBeFalsy();
   });
 });
 
 describe('범위 검사', () => {
-  test('case1', () => {
+  test('범위 초과인 120', () => {
     expect(validRange([1, 2, 0])).toBeFalsy();
   });
-  test('case2', () => {
+  test('범위 초과인 034', () => {
     expect(validRange([0, 3, 4])).toBeFalsy();
   });
-  test('case3', () => {
+  test('범위 이내인 123', () => {
     expect(validRange([1, 2, 3])).toBeTruthy();
   });
-  test('case4', () => {
+  test('범위 초과인 000', () => {
     expect(validRange([0, 0, 0])).toBeFalsy();
   });
 });
 
 describe('중복 검사', () => {
-  test('case1', () => {
+  test('중복이 없는 123', () => {
     expect(duplication([1, 2, 3])).toBeFalsy();
   });
-  test('case2', () => {
+  test('중복이 있는 122', () => {
     expect(duplication([1, 2, 2])).toBeTruthy();
   });
-  test('case3', () => {
+  test('중복이 있는 111', () => {
     expect(duplication([1, 1, 1])).toBeTruthy();
   });
 });
 
-describe('유효값 검사', () => {
-  test('case1', () => {
+describe('모든 유효값 검사 함수를 통합한 isValidInput 테스트', () => {
+  test('올바른 인풋인 123', () => {
     expect(isValidInput([1, 2, 3])).toBeTruthy();
   });
-  test('case2', () => {
+  test('중복인 122', () => {
     expect(() => isValidInput([1, 2, 2])).toThrow(ERROR.DUPLICATION);
   });
-  test('case3', () => {
+  test('숫자가 아닌 a1', () => {
     expect(() => isValidInput(['a', 1])).toThrow(ERROR.NAN);
   });
-  test('case4', () => {
+  test('범위를 초과한 012', () => {
     expect(() => isValidInput([0, 1, 2])).toThrow(ERROR.RANGE);
   });
-  test('case5', () => {
+  test('길이를 초과한 5124', () => {
     expect(() => isValidInput([5, 1, 2, 4])).toThrow(ERROR.LENGTH);
   });
 });
