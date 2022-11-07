22fb4e5e401e0260ac3f93bc2d91e2347f9ff387
jinhobyeong <jhb_0921@naver.com>
2022-11-07 18:53:48 +0900

    feat : getStrikeAnBall 유효성 검사 함수 구현

--- src/App.js
+++ src/App.js
@@ -6,19 +6,36 @@ class App {
     Console.print("숫자 야구 게임을 시작합니다.");
   }
   play() {
-    this.makeRandomNum();
-    this.enterUserInput();
+    const computerValue = this.makeRandomNum();
+    this.enterUserInput(computerValue);
   }
 
-  enterUserInput() {
+  enterUserInput(computerValue) {
     Console.readLine("숫자를 입력해주세요 : ", (userInputValue) => {
       // 여기서 입력된 값을 비교하는 함수를 따로 만든다.
-      this.getStrikeAndBall(userInputValue);
+      this.getStrikeAndBall(computerValue, userInputValue);
     });
   }
 
-  getStrikeAndBall(userValue) {
-    const computerValue = this.makeRandomNum();
+  getStrikeAndBall(computerValue, userValue) {
+    if (userValue == computerValue) {
+      // 만약에 같은 수가 나오게 된다면 console.log 띄우고 재시작 여부 물어봄
+      Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    } else {
+      let strike = 0;
+      let ball = 0;
+
+      Array.from(userValue).forEach((value, index) => {
+        if (value === computerValue[index]) {
+          strike++;
+        } else if (computerValue.includes(value)) {
+          ball++;
+        }
+      });
+
+      Console.print(strike, "스트라이크 수");
+      Console.print(ball, "볼 수");
+    }
   }
 
   makeRandomNum() {
@@ -30,7 +47,7 @@ class App {
       }
     }
 
-    console.log(computerRandomNum.join("")); // ❌
+    console.log(computerRandomNum.join(""), "randomNum"); // ❌
 
     return computerRandomNum.join("");
   }
