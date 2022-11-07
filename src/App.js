abe0aa14529a3e05975b856cdaa19627ad1087b0
SoobinJ <soobin08011812@gmail.com>
2022-11-07 13:19:55 +0900

    feat : 사용자 값 입력/ 입력받은 값이 유효한지 확인 구현

--- src/App.js
+++ src/App.js
@@ -29,7 +29,23 @@ class App {
   userInput() {
     Console.readLine("숫자를 입력해주세요 : ", (inputNum) => {
       this.userNum = inputNum;
+      this.isValid(this.userNum);
     });
+  }
+
+  isValid(num) {
+    const inputNumArr = Array.from(num);
+    const inputNunSet = new Set(inputNumArr);
+
+    if (inputNumArr.length !== NUMBERLENGTH) {
+      throw "3자리가 아님";
+    }
+    if (inputNumArr.length !== inputNunSet.size) {
+      throw "중복 숫자 있음";
+    }
+    if (isNaN(num)) {
+      throw "숫자가 아님";
+    }
   }
 }
 const app = new App();
