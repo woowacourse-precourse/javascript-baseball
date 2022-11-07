0286eabe9158dfdb8e633dee735dccedd73161df
SorinKwon <ksl020700@gmail.com>
2022-11-07 20:20:30 +0900

    feat(App): changeUserNumberToArray 함수 추가
    새로운 함수 추가:
    - 사용자가 입력한 숫자를 배열로 바꾸는 기능
    - 컴퓨터 숫자와 비교하는 과정을 위한 목적

--- src/App.js
+++ src/App.js
@@ -2,34 +2,39 @@ const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
 
-    constructor(){
-      this.computer_number=0;
-    }
-    play() {
-        MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
-        this.selectComputerNumber();
-    }
-    startGame(){
-      this.getUserInputNumber();
-    }
+  constructor() {
+    this.computer_number = 0;
+    this.user_number=0;
+  }
+  play() {
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+    this.selectComputerNumber();
+    this.startGame();
+  }
+  startGame() {
+    this.getUserInputNumber();
+  }
 
-    selectComputerNumber() {
-      const random_number = [];
-      while (random_number.length < 3) {
-          const selected_number = MissionUtils.Random.pickNumberInRange(1, 9);
-          if (!random_number.includes(selected_number)) {
-              random_number.push(selected_number);
-          }
+  selectComputerNumber() {
+    const random_number = [];
+    while (random_number.length < 3) {
+      const selected_number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!random_number.includes(selected_number)) {
+        random_number.push(selected_number);
       }
-      this.computer_number = random_number;
+    }
+    this.computer_number = random_number;
   }
   getUserInputNumber() {
     MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (input_num) => {
-        console.log(input_num);
+      this.user_number=input_num;
+      this.changeUserNumberToArray();
     });
-}
-
-
+  }
+  changeUserNumberToArray(){
+    let user_num;
+    user_num = (this.user_number + '').split('');
+  }
 }
 const app = new App;
 app.play();
