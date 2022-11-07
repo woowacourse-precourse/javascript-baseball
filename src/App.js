dd9dd4d112910940809f5d39aeb49f5edbaa4f8e
yesolyo <ahnyelim77@gmail.com>
2022-11-07 15:13:43 +0900

    feat: 서로 다른 3자리의 수를 생성
    1~9 사이의 숫자를 이용하여 서로 다른 3자리의 수를 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const COMPUTER_NUMBER = [];
 class App {
   play() {}
+
+  randomComputerNumber() {
+    while (COMPUTER_NUMBER.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!COMPUTER_NUMBER.includes(number)) {
+        COMPUTER_NUMBER.push(number);
+      }
+    }
+  }
 }
 
 module.exports = App;
