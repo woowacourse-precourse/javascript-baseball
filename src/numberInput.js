3547bd15ce46678c18dbbd52a2ce521cb84e88f9
kimyou1102 <yoosk5485@naver.com>
2022-11-07 22:02:15 +0900

    feat: 사용자가 잘못된 값을 입력한 경우 예외를 발생시킨다.

--- src/numberInput.js
+++ src/numberInput.js
@@ -3,9 +3,21 @@ const MissionUtils = require("@woowacourse/mission-utils");
 const numberInput = () => {
   let inputNumber;
   MissionUtils.Console.readLine("숫자를 입력해주세요 :", (input) => {
+    validate(input);
     inputNumber = input;
   });
   return inputNumber;
-}
+};
+
+const validate = (num) => {
+  const set = new Set(num.split(""));
+  if (num.length > 3) {
+    throw new Error("잘못된 값을 입력하셨습니다 - 3이상 숫자 입력");
+  } else if (isNaN(Number(num))) {
+    throw new Error("잘못된 값을 입력하셨습니다 - 숫자가 아닌 값을 입력");
+  } else if (num.length !== [...set].length) {
+    throw new Error("잘못된 값을 입력하셨습니다 - 중복되는 숫자 입력");
+  }
+};
 
 module.exports = numberInput;
