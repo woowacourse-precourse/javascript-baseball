cf5ca8f0ee3d8d9ea4dcc1aa57a38cddaadff2b1
KINHYEONJI <hyeonji0718@gmail.com>
2022-11-08 18:20:13 +0900

    feat(App) : 전체적인 게임을 시작할 수 있는 함수 play 구현

--- src/App.js
+++ src/App.js
@@ -1,19 +1,52 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 const { SYS_MESSAGE, BALL_STATE } = require("./Constant.js");
+const Computer = require("./Computer.js");
+const Player = require("./Player.js");
+const Judge = require("./Judge.js");
 
 class App {
+  constructor() {
+    this.computer = null;
+    this.player = new Player();
+    this.judge = new Judge();
+    this.playCnt = 0;
+  }
+
+  play() {
+    if (this.playCnt === 0) this.alertStart();
+    this.playCnt++;
+
+    this.computer = new Computer();
+
+    let computerNum = this.computer.makeRandomNum();
+    let playerInput = null;
+
+    while (computerNum !== playerInput) {
+      playerInput = this.player.getNumber();
+      if (!this.judge.isPlayerInputValid(playerInput)) {
+        MissionUtils.Console.close();
+      }
+      if (this.judge.isPlayerInputValid(playerInput)) {
+        this.showBallState(
+          this.judge.findStrikeAndBallCnt(computerNum, playerInput)
+        );
+      }
+    }
+    this.askMoreGame();
+  }
+
   alertStart() {
     MissionUtils.Console.print(SYS_MESSAGE.START_MESSAGE);
   }
-  play() {}
+
   showBallState(strikeCnt, ballCnt) {
     if (strikeCnt === 3)
       MissionUtils.Console.print(
-        strikeCnt + BALL_STATE.STRIKE + SYS_MESSAGE.FINISH_MESSAGE
+        strikeCnt + BALL_STATE.STRIKE + "\n" + SYS_MESSAGE.FINISH_MESSAGE
       );
     if (strikeCnt > 0 && ballCnt > 0)
       MissionUtils.Console.print(
-        ballCnt + BALL_STATE.BALL + strikeCnt + BALL_STATE.STRIKE
+        ballCnt + BALL_STATE.BALL + " " + strikeCnt + BALL_STATE.STRIKE
       );
     if (strikeCnt > 0 && ballCnt === 0)
       MissionUtils.Console.print(strikeCnt + BALL_STATE.STRIKE);
@@ -22,6 +55,7 @@ class App {
     if (strikeCnt === 0 && ballCnt === 0)
       MissionUtils.Console.print(BALL_STATE.NOTHING);
   }
+
   askMoreGame() {
     MissionUtils.Console.readLine(
       "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. ",
@@ -37,3 +71,6 @@ class App {
 }
 
 module.exports = App;
+
+let app = new App();
+app.play();
