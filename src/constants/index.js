f148402a36d26c45adaa87bf0edd29ae1dddc0da
JISUN HAN <my__memoria@naver.com>
2022-11-08 15:04:00 +0900

    feat: 메세지 및 규칙 상수화

---
+++ src/constants/index.js
@@ -0,0 +1,30 @@
+const GAME_RULE = {
+  NUMBER_RANGE_START: 1,
+  NUMBER_RANGE_END: 9,
+  LENGTH: 3,
+  RESTART: "1",
+  END: "2",
+};
+
+const GAME_MESSAGE = {
+  START: "숫자 야구 게임을 시작합니다.",
+  INPUT: "숫자를 입력해주세요 : ",
+  STRIKE: "스트라이크",
+  BALL: "볼",
+  NOTHING: "낫싱",
+  CORRECT: `${LENGTH}개의 숫자를 모두 맞히셨습니다! 게임 종료`,
+  RESTART_OR_END: `게임을 새로 시작하려면 ${RESTART}, 종료하려면 ${END}를 입력하세요.\n`,
+};
+
+const ERROR_MESSAGE = {
+  INVALID_CHARACTER: `${RULE.NUMBER_RANGE_START}~${RULE.NUMBER_RANGE_END} 사이의 숫자를 입력해주세요.`,
+  DUPLICATE: "중복된 값이 포함되어 있습니다.",
+  INVALID_LENGTH: `${RULE.LENGTH}자리의 숫자를 입력해주세요.`,
+  INVALID_INPUT: "유효하지 않은 값을 입력하셨습니다. 게임 종료",
+};
+
+module.exports = {
+  GAME_RULE,
+  GAME_MESSAGE,
+  ERROR_MESSAGE,
+};
