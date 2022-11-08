3a6f07e8ba0561e7917e9da68b69d5b1b32b1349
yunjjeongjo <two6885@gmail.com>
2022-11-08 23:09:17 +0900

    feat: constants 추가
    출력 메시지 상수화를 위한 constants를 추가하였습니다.

---
+++ src/constants/constants.js
@@ -0,0 +1,15 @@
+const MESSAGE = {
+  START: "숫자 야구 게임을 시작합니다.",
+  INPUT: "숫자를 입력해주세요 : ",
+  SUCCESS: "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
+  RESTART_OR_TERMINATE: "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
+  STRIKE: "스트라이크",
+  BALL: "볼",
+  NOTHING: "낫싱",
+  RESTART: "1",
+  TERMINATE: "2",
+};
+
+
+
+module.exports = { MESSAGE, ANSWER_LENGTH, RANDOM };
