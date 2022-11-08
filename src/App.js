1319b7256d3515ea38f4aaa48883ddc2cc27c49b
Hyungeol94 <55428818+Hyungeol94@users.noreply.github.com>
2022-11-08 15:53:55 +0900

    feat: 결과출력 + 예외처리 + 게임 다시 시작 여부 입력받기

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
 class App {
+  print(values){
+     //입력한 결과 표시해주기
+     const MissionUtils = require("@woowacourse/mission-utils");
+     var pass = false;
+     var strike_sum, ball_sum; 
+     ball_sum = values[0];
+     strike_sum = values[1];
+
+     if (strike_sum==3){
+      MissionUtils.Console.print("3스트라이크");
+      //MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");  
+      pass = true;     
+      }
+
+    else if (strike_sum == 0 && ball_sum == 0){
+      MissionUtils.Console.print("낫싱");
+
+    }
+    else if (strike_sum ==0 && ball_sum != 0){
+      var sentence = ball_sum.toString()+"볼";
+      MissionUtils.Console.print(sentence);
+    
+    }
+    else if (strike_sum!= 0 && ball_sum != 0){
+      var sentence = ball_sum.toString()+"볼"+" "+strike_sum.toString()+"스트라이크";
+      MissionUtils.Console.print(sentence);
+      
+    }
+    else if (strike_sum!= 0 && ball_sum == 0){
+      var sentence = strike_sum.toString()+"스트라이크";
+      MissionUtils.Console.print(sentence);
+    }
+    return pass;
+
+  }
   calculate(Numbers, answer){ 
     var strike_sum = 0;
     var ball_sum = 0;
   
     for(var i =0;i<3;i++){
       //스트라이크 세기
-      var number = Numbers[i];
-      if (number == parseInt(answer[i])){
+      var number = parseInt(answer[i]);
+      if (number == Numbers[i]){
         strike_sum += 1;
         }
     
@@ -15,7 +50,7 @@ class App {
         if (i == j){
           continue;
         }
-        else if (number == parseInt(answer[j])){
+        else if (number == Numbers[j]){
           ball_sum += 1;
         }
       }
@@ -23,38 +58,57 @@ class App {
   
     return [ball_sum, strike_sum];
   }
-  
-  play() {
+
+  playoneround(){
+    //세 가지 숫자 만들기
     const MissionUtils = require("@woowacourse/mission-utils");
-    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
     var Numbers = [];
-    
-    //세 가지 함수 만들기
     for (var i =0;i<3;i++){
       var number = MissionUtils.Random.pickNumberInRange();
       Numbers.push(number);
     }
 
+   
     var pass = false;
     while (pass == false){
-     
-      var answer;
+      var answer  = 0;
       MissionUtils.Console.readLine("숫자를 입력해주세요: ", (x) =>{
-        answer = x;      
+        if (isNaN(parseInt(x))) throw 'not a number';
+        if (parseInt(x) <0) throw 'minus number';
+        //if (x.toString().split(",").join("").length!=3) throw 'length error';
+        answer = x.split(",");
       });
-      
+      answer = answer.join("");
+      if (answer.length != 3) throw 'length error'
       //계산하기
-      var strike_sum, ball_sum; 
       var values = this.calculate(Numbers, answer);
-      strike_sum = values[0];
-      ball_sum = values[1];
       
-      
-
-    }
+      //프린트하기
+      pass = this.print(values);
+      }
   }
-
   
+  
+  
+  play() {
+    const MissionUtils = require("@woowacourse/mission-utils");
+   // MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+   
+    var flag = false;
+    while(flag== false){
+      this.playoneround();
+      MissionUtils.Console.readLine("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. ", (x) =>{
+        if (x == 1){
+       //   App.play();
+       //   MissionUtils.Console.print("again");
+        }        
+        else if (x == 2){
+          flag = true;
+        }
+        else throw 'enter 1 or two'
+      });
+   }
+   MissionUtils.Console.print("게임 종료");
+  }
 }
-
 module.exports = App;
