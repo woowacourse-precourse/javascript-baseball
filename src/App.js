92ba26c1c917f0e3325b2150b4ce7bff731338f9
YoommY <isho48239@gmail.com>
2022-11-08 09:00:46 +0900

    Feat : 스트라이크, 볼, 낫싱 점수 처리

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-const MissionUtils = require("@woowacourse/mission-utils");
+const MissionUtils = require('@woowacourse/mission-utils');
 const constants = require('./constants');
 
 class App {
@@ -12,16 +12,37 @@ class App {
     function computerNumbers() {
       while (computer.length < 3) {
         let number = MissionUtils.Random.pickNumberInRange(1, 9);
-        if (!computer.includes(number)) { //중복되지 않는 숫자만 뽑기
+        if (!computer.includes(number)) {
+          //중복되지 않는 숫자만 뽑기
           computer.push(number);
         }
       }
     }
 
+    function numberInput() {
+      Console.readLine('숫자를 입력해주세요', (answer) => {
+        //숫자가 3자리수 이상인 경우 예외 처리
+        if (answer.length() > 3) throw '입력 값이 잘못됨';
 
+        //while문과 if문은 depth가 2 이상이므로 map을 활용
+        answer.map((number, i) => {
+          computer.includes(number)
+            ? computer.indexOf(number) === i
+              ? (strike += 1)
+              : (ball += 1)
+            : (nothing += 1);
+        });
+      });
+    }
+
+  
+    function gamePlay(){
+
+    }
+    
     computerNumbers();
-
+    numberInput();
   }
-  }
+}
 
 module.exports = App;
