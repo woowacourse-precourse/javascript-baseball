c86b5f5895d7bb7ec62bf53bc8cdab0b3e97a55a
Sanggil Han <tkdrlf95@naver.com>
2022-11-08 17:08:40 +0900

    feat: Implement counting balls and strikes
    사용자가 입력한 수에 따라 볼과 스트라이크의 개수가 몇 개인지 세는 기능을 구현하였음

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ const MissionUtils = require("@woowacourse/mission-utils");
 class App {
   play() {
     this.initializeNumber();
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+    let isGameOn = true;
+    let tempCountForTest = 1;
+    while (tempCountForTest < 3) {
+      this.readLineNumberOfPlayer();
+      isGameOn = false;
+      tempCountForTest += 1;
+    }
   }
 
   baseballNumber = [];
@@ -19,6 +27,52 @@ class App {
     if (!this.baseballNumber.includes(number)) {
       this.baseballNumber.push(number);
     }
+  }
+
+  readLineNumberOfPlayer() {
+    let numberOfBalls = 0;
+    let numberOfStrikes = 0;
+
+    MissionUtils.Console.readLine('숫자를 입력해주세요 :', (guessedBaseballNumber) => {
+      [numberOfBalls, numberOfStrikes] = this.checkingBallsAndStrikes(guessedBaseballNumber);
+    })
+    MissionUtils.Console.print(`${numberOfBalls}, ${numberOfStrikes}`);
+  }
+
+  checkingBallsAndStrikes(guessedBaseballNumber) {
+    const iteratorOfGuessedNumber = guessedBaseballNumber[Symbol.iterator]();
+    const iteratorOfBaseballNumber = this.baseballNumber[Symbol.iterator]();
+    
+    let charGuessedNumber = iteratorOfGuessedNumber.next();
+    let digitBaseballNumber = iteratorOfBaseballNumber.next();
+
+    let numberOfBalls = 0;
+    let numberOfStrikes = 0;
+
+    while (!charGuessedNumber.done && !digitBaseballNumber.done) {
+      const digitGuessedNumber = parseInt(charGuessedNumber.value, 10);
+      numberOfStrikes += this.return1IfSame(digitGuessedNumber, digitBaseballNumber.value);
+      numberOfBalls += this.return1IfIncludes(this.baseballNumber, digitGuessedNumber);
+      
+      charGuessedNumber = iteratorOfGuessedNumber.next();
+      digitBaseballNumber = iteratorOfBaseballNumber.next();
+    }
+    numberOfBalls -= numberOfStrikes;
+    return [numberOfBalls, numberOfStrikes];
+  }
+  
+  return1IfSame(comparitiveValueLeft, comparitiveValueRight) {
+    if (comparitiveValueLeft === comparitiveValueRight) {
+      return 1
+    }
+    return 0
+  }
+
+  return1IfIncludes(targetArray, targetValue) {
+    if (targetArray.includes(targetValue)) {
+      return 1
+    }
+    return 0
   }
 }
 
