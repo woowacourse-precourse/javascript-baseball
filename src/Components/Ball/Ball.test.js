72797a209212f32959048d6c0f275ec1b49c8f82
RyuJoonSik <tldtksahtxja@naver.com>
2022-11-08 14:54:16 +0900

    test: Ball.test.js에 compareTo가 1 스트라이크를 반환하는지 테스트 추가
    Ball이 스스로 다른 Ball의 값을 비교하여 특정 결과를 반환하는지 테스트할 수 있도록 compareTo 테스트 케이스를 추가했습니다.

--- src/Components/Ball/Ball.test.js
+++ src/Components/Ball/Ball.test.js
@@ -1,10 +1,17 @@
-const Ball = require('./Ball');
+const Ball = require("./Ball");
 
-describe('Ball', () => {
-  test('getNumber로 숫자 값을 반환', () => {
+describe("Ball", () => {
+  test("getNumber로 숫자 값을 반환", () => {
     const ballNumber = 425;
     const ball = new Ball(ballNumber);
 
     expect(ball.getNumber()).toBe(ballNumber);
+  });
+
+  test("나(공격수)의 Ball이 123이고 컴퓨터(수비수)의 Ball이 425이면 1스트라이크를 반환", () => {
+    const ballOfAttacker = new Ball(123);
+    const ballOfDefender = new Ball(425);
+
+    expect(ballOfDefender.compareTo(ballOfAttacker)).toBe("1스트라이크");
   });
 });
