29aadad022b6b1e36debdfdd0dc948cad4b4ba85
d-charlie-kim <dokkim@student.42seoul.kr>
2022-11-08 11:53:33 +0900

    Style: Airbnb 코드 컨벤션에 맞춰 quotes 정리

--- src/App.js
+++ src/App.js
@@ -51,10 +51,10 @@ class App {
       MissionUtils.Console.print(`${strike}스트라이크`);
     }
     if (strike == 3) {
-      MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+      MissionUtils.Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
     }
     if (ball == 0 && strike == 0) {
-      MissionUtils.Console.print("낫싱");
+      MissionUtils.Console.print('낫싱');
     }
   }
 
@@ -71,14 +71,14 @@ class App {
   static checkInputValid(input) {
     const numbers = /[^1-9]/;
     if (numbers.test(input)) {
-      throw "ERROR: Invalid input \n[ Valid Input : three digit number from 1 to 9 ]";
+      throw 'ERROR: Invalid input \n[ Valid Input : three digit number from 1 to 9 ]';
     }
     if (input.length != 3) {
-      throw "ERROR: Invalid length \n[ Valid Input : three digit number from 1 to 9 ]";
+      throw 'ERROR: Invalid length \n[ Valid Input : three digit number from 1 to 9 ]';
     }
     const dupleCheck = new Set(input);
     if (dupleCheck.size != 3) {
-      throw "ERROR: Duplicated numbers \n[ Valid Input : three digit number from 1 to 9 ]";
+      throw 'ERROR: Duplicated numbers \n[ Valid Input : three digit number from 1 to 9 ]';
     }
 
     return true;
@@ -114,9 +114,9 @@ class App {
   }
 
   endGame() {
-    MissionUtils.Console.readLine("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n", (input) => {
+    MissionUtils.Console.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n', (input) => {
       if (input != 1 && input != 2) {
-        throw "ERROR: Invalid input\n [ Valid Input : 1 or 2 ]";
+        throw 'ERROR: Invalid input\n [ Valid Input : 1 or 2 ]';
       }
 
       if (input == 1) {
@@ -134,7 +134,7 @@ class App {
   }
 
   play() {
-    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
     this.newGame();
   }
 }
