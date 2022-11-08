019abe7c2c90b1864b33cf4c851c68f43dba3d50
KINHYEONJI <hyeonji0718@gmail.com>
2022-11-08 20:40:34 +0900

    chore(Judge) : 콘솔 입력을 닫아줌

--- src/Player.js
+++ src/Player.js
@@ -6,6 +6,7 @@ class Player {
     let userInput = "";
     MissionUtils.Console.readLine(SYS_MESSAGE.INPUT_MESSAGE, (input) => {
       userInput = input;
+      MissionUtils.Console.close();
     });
     return userInput;
   }
