27d5e88720f1e50e377aed3aaf27ab5287f01a7f
김상두 <puki4416@gmail.com>
2022-11-08 17:17:09 +0900

    feat : App play 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+const { Console } = require("@woowacourse/mission-utils");
+const { START } = require("./Constant");
+const { makeRandomNumber } = require("./Make");
+const { selectNumQuery, selectNextQuery } = require("./Query");
+
 class App {
-  play() {}
+  play() {
+    Console.print(START);
+    selectNumQuery(makeRandomNumber(), selectNextQuery);
+  }
 }
 
 module.exports = App;
