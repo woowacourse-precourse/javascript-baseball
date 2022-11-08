b1375a2c44f0ddcce702439aaef6f71756f04c5b
userHWSEO <gmldnjseocnd@gmail.com>
2022-11-08 09:17:46 +0900

    feat: close를 이용한 게임 종료 기능 추가
    - close를 사용하여 게임 종료 시 콘솔을 닫도록 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
+  closeGame() {
+    MissionUtils.Console.print("게임 종료");
+    MissionUtils.Console.close();
+  }
   playGame() {
     MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (userNumber) => {
       function makeRandomNumber() {
@@ -33,7 +37,7 @@ class App {
         "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n",
         (answer) => {
           if (answer === "1") return this.playGame();
-          return MissionUtils.Console.print("게임 종료");
+          return this.closeGame();
         }
       );
     }
