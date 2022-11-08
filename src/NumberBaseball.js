a76ddf3d5b9a236c021990aa9e021b9bc575ca72
piporica <49173614+piporica@users.noreply.github.com>
2022-11-08 22:04:39 +0900

    feat : 콜백 이용하여 입력받은 수에 대한 결과 출력 구현:

--- src/NumberBaseball.js
+++ src/NumberBaseball.js
@@ -10,12 +10,13 @@ class NumberBaseball {
   gameSet() {
     const answer = this.makeAnswer();
     console.log(answer);
-    this.gamePlay();
+    this.gamePlay(answer);
   }
 
-  gamePlay() {
+  gamePlay(answer) {
     this.getUserInput((input) => {
-      console.log(input);
+      const scores = this.checkUserInput(input, answer);
+      this.printInputResult(scores.ball, scores.strike);
     });
   }
 
@@ -35,6 +36,31 @@ class NumberBaseball {
       const inputArray = [...input].map((x) => Number(x));
       callback(inputArray);
     });
+  }
+
+  checkUserInput(input, answer) {
+    const ballCount = input.filter(
+      (x, index) => answer.includes(x) && answer[index] !== x
+    ).length;
+    const strikeCount = input.filter((x, index) => answer[index] === x).length;
+    return { ball: ballCount, strike: strikeCount };
+  }
+
+  printInputResult(ballCount, strikeCount) {
+    let message = "";
+    if (ballCount === 0 && strikeCount === 0) {
+      message = MESSAGES.NOTHING;
+    } else if (strikeCount === 3) {
+      message = MESSAGES.CORRECT;
+    } else {
+      let ballMessage = ballCount !== 0 ? `${ballCount}볼` : "";
+      const strikeMessage = strikeCount !== 0 ? `${strikeCount}스트라이크` : "";
+      if (ballCount !== 0 && strikeCount !== 0) {
+        ballMessage = `${ballMessage} `;
+      }
+      message = `${ballMessage}${strikeMessage}`;
+    }
+    Console.print(message);
   }
 }
 module.exports = NumberBaseball;
