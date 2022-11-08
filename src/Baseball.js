0aa27c104989b19c65d50b16e116534d7fe959e9
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2022-11-08 21:04:23 +0900

    feat : 사용자 숫자셋 입력 구현

--- src/Baseball.js
+++ src/Baseball.js
@@ -6,6 +6,12 @@ class Baseball {
     Console.print("숫자 야구 게임을 시작합니다.");
     this.computerNumberSet = new Computer().pickRandomNumberSet();
   }
+
+  playGame() {
+    Console.readLine("숫자를 입력해주세요 : ", (userNumber) => {
+      userNumber = new User().getUserArray(userNumber);
+    });
+  }
 }
 
 module.exports = Baseball;
