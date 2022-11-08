48083b18ad9ff59521f55b6343eed7236c5dc615
pitang <wpeffect@gmail.com>
2022-11-08 20:06:46 +0900

    feat: 컴퓨터의 수 생성 함수

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 class App {
   play() {}
+  makeComputerNumber() {
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    let computerNumberSet = new Set();
+
+    while (computerNumberSet.size < 3) {
+      let randomNumber = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (randomNumber !== 0) computerNumberSet.add(randomNumber);
+    }
+    return [...computerNumberSet];
+  }
+
 }
 
 module.exports = App;
