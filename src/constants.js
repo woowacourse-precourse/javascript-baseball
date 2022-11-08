85848173ea8d92530ab96457c942e476d65a3c55
KES <aprilsmaple@gmail.com>
2022-11-08 19:40:14 +0900

    feat: constants error message created

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,13 @@
 const MESSAGES = {
   APP_START: "숫자 야구 게임을 시작합니다.",
+  GUESS_QUESTION: "숫자를 입력해주세요 : ",
 };
 
-const ERRORS = {};
+const ERRORS = {
+  LEGNTH_IS_NOT_THREE: "3자리 숫자만 입력이 가능합니다.",
+  IS_NOT_NUMBER: "숫자가 아닌 문자는 입력이 불가능합니다.",
+  HAS_ZERO: "0이 포함된 숫자는 입력이 불가능합니다.",
+  HAS_SAME_NUMBER: "중복되는 숫자는 입력이 불가능합니다.",
+};
 
 module.exports = { MESSAGES, ERRORS };
