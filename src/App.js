0f9da9e1d352c98b2e7e0a57b602de07f2aa7394
이여진 <jinspace3710@gmail.com>
2022-11-08 20:07:07 +0900

    feat: 사용자로부터 값 입력받는 기능

--- src/App.js
+++ src/App.js
@@ -16,6 +16,44 @@ const startGame = () => {
     if (!randomNumberArray.includes(randomNumber))
       randomNumberArray.push(randomNumber);
   }
+
+  guessNumber(randomNumberArray);
+};
+
+const guessNumber = randomNumberArray => {
+  Console.readLine('숫자를 입력해주세요 : ', userInputNumber => {
+    userInputNumberArray = convertNumberToNumberArray(userInputNumber);
+
+    if (isValidNumber(userInputNumber, userInputNumberArray)) {
+    } else {
+      throw new Error(
+        '1부터 9까지 서로 다른 수로 이루어진 3자리의 수를 입력해주세요.'
+      );
+    }
+  });
+};
+
+const convertNumberToNumberArray = number => {
+  let tempNumberArray = [];
+
+  while (number > 0) {
+    tempNumberArray.push(number % 10);
+    number = Math.floor(number / 10);
+  }
+
+  const numberArray = [...tempNumberArray.reverse()];
+
+  return numberArray;
+};
+
+const isValidNumber = (userInputNumber, userInputNumberArray) => {
+  if (isNaN(Number(userInputNumber))) return false;
+  if (userInputNumberArray.length !== 3) return false;
+  if (userInputNumberArray[0] === userInputNumberArray[1]) return false;
+  if (userInputNumberArray[0] === userInputNumberArray[2]) return false;
+  if (userInputNumberArray[1] === userInputNumberArray[2]) return false;
+
+  return true;
 };
 
 module.exports = App;
