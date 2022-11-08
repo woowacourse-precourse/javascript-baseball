e9c8142045045d9b36c839a96e180653e673e905
Hypark2018 <hayoungpark2018@gmail.com>
2022-11-08 13:51:50 +0900

    🖊 feat: add generateAnswer method on App.js
    generateAnswer method: return string of 3 length, which is made of not duplicated random numbers. (used MissionUtils libraryto generate random number)

--- src/App.js
+++ src/App.js
@@ -2,11 +2,21 @@ const MissionUtils = require("@woowacourse/mission-utils");
 class BaseBallGame {
   constructor() {
     this.start();
+    this.ANSWER = this.generateAnswer();
   }
 
   start() {
     const OPENING_MENT = "숫자 야구 게임을 시작합니다.";
     MissionUtils.Console.print(OPENING_MENT);
+  }
+
+  generateAnswer() {
+    const TEMPORARY_STORAGE = [];
+    while(TEMPORARY_STORAGE.length < 3) {
+      const TEMPORARY_NUMBER = MissionUtils.Random.pickNumberInRange(1, 9)
+      if (!TEMPORARY_STORAGE.includes(TEMPORARY_NUMBER)) TEMPORARY_STORAGE.push(TEMPORARY_NUMBER);
+    }
+    return TEMPORARY_STORAGE.join('');
   }
 }
 
