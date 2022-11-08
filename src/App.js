cf96aaebfee03b8c8dae2dec7f46773dd52231e3
Minyoung Lee <mminy62@gmail.com>
2022-11-08 16:16:32 +0900

    refactor: 기능 1,2 - async, await 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,32 +1,50 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
-let user = [];
-let computer = [];
 
 class App {
 
-  play() {
-    while (computer.length < 3) {
-      const number = MissionUtils.Random.pickNumberInRange(1, 9);
-      if (!computer.includes(number)) {
-        computer.push(number);
+  random(){
+    return new Promise(function(resolve, reject) {
+      let computer = [];
+      while (computer.length < 3) {
+        const number = MissionUtils.Random.pickNumberInRange(1, 9);
+        if (!computer.includes(number)) {
+          computer.push(number);
+        }
       }
-    }
-    //console.log(computer);
-    this.input();
+      resolve(computer);
+    });
   }
 
-  input(){
-    MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (answer) => {
-      user = answer.split('').map((el) => parseInt(el));
-      //console.log(user);
-      MissionUtils.Console.close();
+
+  input() {
+    return new Promise(function(resolve, reject) {
+      var items = [];
+      MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (answer) => {
+            items = answer.split('').map((el) => parseInt(el));
+            //console.log(user);
+            resolve(items);  
+            MissionUtils.Console.close();
+          });
     });
+    
+    
+  }
+
+
+  async play() {
+    let computer = await this.random(); 
+    let user = await this.input(); 
+    
   }
 
 }
 
+
+  
+
 const app = new App();
 app.play();
+
 
 module.exports = App;
