480ae4aa08ecd0ebe02e945b641810bf10600b62
OrangeChick <retww@naver.com>
2022-11-08 19:11:51 +0900

    feat: Implement a function for the computer to generate a 3-digit number
    - 컴퓨터가 3자리 수의 정답을 생성하는 기능을 구현하였습니다.

--- src/App.js
+++ src/App.js
@@ -11,6 +11,17 @@ class App {
   startGameMessage() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다");
   }
+
+  getComputerAnswer() {
+    this.computerNumber = [];
+
+    while (this.computerNumber.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9) + "";
+      if (!this.computerNumber.includes(number)) {
+        this.computerNumber.push(number);
+      }
+    }
+  }
 }
 
 const app = new App();
