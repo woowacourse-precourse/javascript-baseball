a402f1a9a61493bf148af2ad8f550b0fbf4ed02f
hayoun <fishingest@kookmin.ac.kr>
2022-11-08 12:42:23 +0900

    [FEAT]init()을 통한 랜덤 컴퓨터 숫자 생성

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 class App {
   
-
-  play() {
+  init(){
     console.log("숫자 야구 게임을 시작합니다")
+    const computer = [];
+    while (computer.length < 3) {
+    const number = MissionUtils.Random.pickNumberInRange(1, 9);
+    if (!computer.includes(number)) {
+    computer.push(number);
+    }
+    }
+    this.randomnum=computer.join("")
     
-   
+  }
+  
+  play() {
+    this.init();
+    var solve=false;
+    
+    console.log(this.randomnum)
+    
+
+    MissionUtils.Console.readLine('닉네임을 입력해주세요.', (answer) => {
+      console.log(`닉네임: ${answer}`);
+      
+    });
   }
 }
-// const app = new App();
-//       app.play();
-module.exports = App;
+const app = new App();
+      app.play();
+// module.exports = App;
