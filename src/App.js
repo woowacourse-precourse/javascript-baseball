66364e886839f0991e8620240958cce6d167e5ee
DESKTOP-8B9G08E\Cho young jae <rnlwlswk@naver.com>
2022-11-08 23:53:07 +0900

    유저넘버체크

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ const MissionUtils = require("@woowacourse/mission-utils");
 class App {
    play() {
     this.start();
+    this.randomNumber();
   }
   start(){
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
@@ -10,16 +11,38 @@ class App {
 
   randomNumber(){
     let computer = [];
-    for(let i = 0; i < 3; i++) {
-      const randomN = MissionUtils.pickNumberInRange(1, 9);
-      if (randomN[i] !== randomN[i-1]) {
+    for(let i = 0; computer.length < 3; i++) {
+      const randomN = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (computer[i-1] !== randomN) {
         computer.push(randomN[i]);
       }
     }
+    MissionUtils.Console.print(computer);
+    return computer;
   }
 
+  userNumber() {
+    let user = [];
+    MissionUtils.Console.readLine('숫자를 입력해주세요.', (answer) => {
+      let userN = answer.split("").map((element) => parseInt(element));
+      console.log(`: ${answer}`);
+      this.checkuser(userN);
+      this.compare(com, user);
+    });
+  }
+
+  checkuser(arr) {
+    if (arr.length != 3) {
+      throw '잘못된 값을 입력하여 게임을 종료합니다.'
+    }
+  }
 
   
+
+
+
+
+
 }
 
 
