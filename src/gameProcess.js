edd1be92e318ca8fd1f6d524f98c9b2bc87298d7
NYeonK <skdus728@gmail.com>
2022-11-08 17:46:47 +0900

    [ feat ] 입력 예외 처리

--- src/gameProcess.js
+++ src/gameProcess.js
@@ -11,7 +11,6 @@ function getComputerNumbers() {
 }
 
 function gameProcess() {
-    console.log(computerNumber)
     MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (answer) => {
         countResult(answer);
     });
@@ -19,7 +18,13 @@ function gameProcess() {
 
 function countResult(answer) {
     let ball = 0, strike = 0;
+
+    if (new Set(answer).size !== 3) throw new Error("서로 다른 3자리 수를 입력해주세요.");
+
     answer = answer.split('');
+    answer.map(element => {
+        if (isNaN(Number(element)) || element.includes('0')) throw new Error("1~9 사이의 숫자를 입력하세요.");
+    });
 
     for (let i = 0; i < 3; i++){
         if (Number(answer[i]) === computerNumber[i]) strike++;
@@ -38,18 +43,28 @@ function getHint(ball,strike) {
     : hintMessage = `낫싱`
     
     MissionUtils.Console.print(hintMessage);
-
+    
     return (strike !== 3) ? gameProcess() : askRetry();
 }
 
 function askRetry() {
     MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     MissionUtils.Console.readLine('', (answer) => {
-        computerNumber = getComputerNumbers()
-        return answer === "1" ? gameProcess() : MissionUtils.Console.close();
+        if (answer === "1") {
+            computerNumber = getComputerNumbers(); 
+            gameProcess();
+        }
+        else if (answer === "2") return MissionUtils.Console.close();
+        else throw new Error("1 아니면 2를 입력해주세요.");
     });
 }
 
-computerNumber = getComputerNumbers();
-MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
-gameProcess();
+function gameStart() {
+    computerNumber = getComputerNumbers();
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+    gameProcess();
+}
+
+module.exports = {
+    gameStart,
+} 
