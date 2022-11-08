4d9f4af36e2bea73590c86a2ae6e30803015c82f
sumin <csumin008255@gmail.com>
2022-11-08 14:03:19 +0900

    Feat: [기능1] 게임 시작 출력문 메소드 작성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  gameStartMessage() {
+    return MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+  }
+
+  play() {
+    this.gameStartMessage();
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
