63ea579d8cebde4377617d28664745f0fb501f17
고동현 <dhjk35@naver.com>
2022-11-08 18:41:15 +0900

    feat: 게임 시작 시 정답을 설정하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 const { Console } = require('@woowacourse/mission-utils');
 const { MESSAGES } = require('./constants');
+const { getRandomUniqueNumbers } = require('./utils');
 
 class App {
+  #answer;
+
   play() {
     this.#startGame();
+    this.#setAnswer();
   }
 
   #startGame() {
     Console.print(MESSAGES.START);
+  }
+
+  #setAnswer() {
+    this.#answer = getRandomUniqueNumbers(1, 9, 3).join('');
   }
 }
 
