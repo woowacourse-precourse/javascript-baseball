47590f58333d28869a42e5f4663dd6ece295959b
LGS <cj8928@gmail.com>
2022-11-08 23:21:35 +0900

    submit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  play() {
+    let random = [];
+    let number;
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    random = MissionUtils.Random.pickUniqueNumbersInRange(1,10,3);
+    number = random.join('');
+    while(true){
+      MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (answer)=>{
+        MissionUtils.Console.print(`${answer}`)
+      })
+      break;
+    }
+    
+    MissionUtils.Console.close()
+  }
 }
 
 module.exports = App;
