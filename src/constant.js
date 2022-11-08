23b75c82f3aedd9c430f193bcdc9cf48aa12b0c7
rovxxmjxx <rovxxmjxx@gmail.com>
2022-11-08 12:57:39 +0900

    feat: 상수 모듈 작성

---
+++ src/constant.js
@@ -0,0 +1,32 @@
+const MESSAGE = {
+  START: "숫자 야구 게임을 시작합니다.",
+  END: "숫자 야구 게임을 종료합니다.",
+  INPUT: "숫자를 입력해주세요 : ",
+  SUCCESS: "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
+  RESTART_OR_END: "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
+  ERROR: "잘못된 입력값입니다. 숫자 야구 게임을 종료합니다.",
+};
+
+const RESULT = {
+  BALL: "볼",
+  STRIKE: "스트라이크",
+  NOTHING: "낫싱",
+};
+
+const USER_CHOICE = {
+  RESTART: "1",
+  END: "2",
+};
+
+const NUMBER = {
+  TOTAL: 3,
+  START: 1,
+  END: 9,
+};
+
+module.exports = {
+  MESSAGE,
+  RESULT,
+  USER_CHOICE,
+  NUMBER,
+};
