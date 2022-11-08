8f0cb9c0de27f616bb4bc444ab8c8e574debb8f1
SeongOh Park <qkrtjddh1212@naver.com>
2022-11-08 13:16:14 +0900

    feat(start): create variable & start game
    Add MESSAGE variable & answer Set.
    Print gamestart message.

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -24,6 +24,15 @@ const getLogSpy = () => {
 };
 
 describe("숫자 야구 게임", () => {
+  test('feat#0, feat#1: 게임 시작 문구 출력', () => {
+    const logSpy = getLogSpy();
+    const app = new App();
+    app.play();
+    expect(logSpy).toHaveBeenCalledWith(
+      expect.stringContaining('숫자 야구 게임을 시작합니다.'),
+    );
+  });
+  
   test("게임 종료 후 재시작", () => {
     const randoms = [1, 3, 5, 5, 8, 9];
     const answers = ["246", "135", "1", "597", "589", "2"];
--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+const { Console } = require('@woowacourse/mission-utils');
+
+const MESSAGE = {
+  GAMESTART: '숫자 야구 게임을 시작합니다.',
+};
+
 class App {
-  play() {}
+  constructor() {
+    this.answer = new Set();
+  }
+
+  play() {
+    Console.print(MESSAGE.GAMESTART);
+    this.startGame();
+  }
+
+  startGame() {}
 }
 
 module.exports = App;
