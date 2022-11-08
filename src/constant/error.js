607c7e29c4daa0fa73b7c8ca206a994f58a64e79
shlee0820 <72565083+shlee0820@users.noreply.github.com>
2022-11-08 22:56:05 +0900

    refactor: 출력하는 문구 수정

--- src/constant/error.js
+++ src/constant/error.js
@@ -3,6 +3,7 @@ const ERROR_TEXT = {
   NOTTHREEDIGIT: '3자리의 수만 입력가능합니다.',
   NOTNUMBER: '숫자만 입력가능합니다.',
   INCLUDEZERO: '0은 입력할 수 없습니다. 1-9사이의 범위에서 입력해주세요.',
+  INCORRECT_INPUT: '올바르지 않은 입력입니다.',
 };
 
-module.exports = ERROR_TEXT;
+module.exports = { ERROR_TEXT };
--- src/constant/gameRule.js
+++ src/constant/gameRule.js
@@ -1,7 +1,7 @@
 const GAME_PROGRESS_TEXT = {
   START_TEXT: '숫자 야구 게임을 시작합니다.',
   REQUEST_INPUT: '숫자를 입력해주세요 : ',
-  RESTART_QUESTION: '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요\n',
+  RESTART_QUESTION: '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.',
 };
 
 const GAME_RESULT_TEXT = {
