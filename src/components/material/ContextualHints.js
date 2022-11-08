37114539872afd3891342496258e319e8b82b988
Ryan-Dia <cjfdnjs158@naver.com>
2022-11-09 03:40:11 +0900

    refactor: 함수명 변경 gameEnd -> endGame

--- src/components/material/ContextualHints.js
+++ src/components/material/ContextualHints.js
@@ -27,7 +27,7 @@ class ContextualHints {
     return isSame.filter((value) => Boolean(value)).length;
   }
 
-  gameEnd() {
+  endGame() {
     MissionUtils.Console.readLine(
       "3개의 숫자를 모두 맞히셨습니다. 게임 종료 \n" +
         "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
@@ -86,7 +86,7 @@ class ContextualHints {
     }
     if (this.NumOfSamePosition === this.THREE_STRIKE) {
       MissionUtils.Console.print("3스트라이크");
-      this.gameEnd();
+      this.endGame();
     }
   }
 }
