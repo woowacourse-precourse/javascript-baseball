73e49e9dee7eb9797e9d27c368d3318fd57cb27e
Jerry <110958704+Jerryjerry0102@users.noreply.github.com>
2022-11-08 23:28:49 +0900

    makeComputerNumbers

--- src/App.js
+++ src/App.js
@@ -5,6 +5,19 @@ class App {
 
   startGame() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    const computerNumbers = this.makeComputerNumbers();
+  }
+
+  makeComputerNumbers() {
+    let computerNumbers = new Array();
+    while (computerNumbers.length < 3) {
+      let number = MissionUtils.Random.pickNumberInRange(1, 9);
+
+      if (!computerNumbers.includes(number)) {
+        computerNumbers.push(number);
+      }
+    }
+    return computerNumbers;
   }
 }
 
