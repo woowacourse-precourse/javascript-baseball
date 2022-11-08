d2a6d8b6e761b4c9da8a334a6a61f748e29906a9
RyuJoonSik <tldtksahtxja@naver.com>
2022-11-08 16:06:48 +0900

    feat: Ball.js에 getBallOf 추가
    볼 반환 테스트 케이스를 통과할 수 있도록 코드를 추가했습니다.
    getStrikeOf: 볼을 구해 반환

--- src/Components/Ball/Ball.js
+++ src/Components/Ball/Ball.js
@@ -10,16 +10,33 @@ class Ball {
   }
 
   compareTo(otherBall) {
-    return this.getStrikeOf(otherBall);
+    return `${this.getStrikeOf(otherBall)} ${this.getBallOf(otherBall)}`.trim();
   }
 
   getStrikeOf(otherBall) {
-    const RESULT = this.number - otherBall.getNumber();
-    const COUNT = _.divideNumberToArray(RESULT).filter(
-      (number) => number === 0
+    const BALL_NUMBER_ARRAY = _.divideNumberToArray(this.number);
+    const OTHER_BALL_NUMBER_ARRAY = _.divideNumberToArray(
+      otherBall.getNumber()
+    );
+    const COUNT = BALL_NUMBER_ARRAY.filter(
+      (number, index) => number === OTHER_BALL_NUMBER_ARRAY[index]
     ).length;
 
-    return `${COUNT}스트라이크`;
+    return COUNT ? `${COUNT}스트라이크` : "";
+  }
+
+  getBallOf(otherBall) {
+    const BALL_NUMBER_ARRAY = _.divideNumberToArray(this.number);
+    const OTHER_BALL_NUMBER_ARRAY = _.divideNumberToArray(
+      otherBall.getNumber()
+    );
+    const COUNT = BALL_NUMBER_ARRAY.filter((number, index) => {
+      const SAME_NUMBER_INDEX = OTHER_BALL_NUMBER_ARRAY.indexOf(number);
+
+      return SAME_NUMBER_INDEX !== -1 && index !== SAME_NUMBER_INDEX;
+    }).length;
+
+    return COUNT ? `${COUNT}볼` : "";
   }
 }
 
