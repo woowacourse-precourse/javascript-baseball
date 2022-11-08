2325dd27890bc890e8dda4954cd21e53dbeb7017
OrangeVinyl <hap_hazard@naver.com>
2022-11-08 15:19:05 +0900

    fix: 예외처리 오류수정 및 입력값 수정

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-/* eslint-disable class-methods-use-this */
 const MissionUtils = require('@woowacourse/mission-utils');
 
 class App {
   constructor() {
-    this.computerNumber = 0;
+    this.computerNumber = null;
   }
 
   start() {
@@ -13,30 +12,26 @@ class App {
 
   getInputNumber() {
     MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (input) => {
-      this.validCheckInputNumber(String(input).split(''));
+      this.validCheckInputNumber(input);
     });
   }
 
   getRandomComputerNumber() {
-    const randomArray = [];
-
+    this.randomArray = [];
     while (this.randomArray.length < 3) {
       const randomNumber = MissionUtils.Random.pickNumberInRange(1, 9);
 
-      if (!randomArray.includes(randomNumber)) {
-        randomArray.push(randomNumber);
+      if (!this.randomArray.includes(randomNumber)) {
+        this.randomArray.push(randomNumber);
       }
     }
-    return randomArray.join('');
+    return this.randomArray.join('');
   }
 
   validCheckInputNumber(input) {
-    const setInput = new Set(input.split(''));
-
-    if (!/^[1-9]+$/g.test(input) || setInput.size !== 3 || input.length !== 3) {
-      throw Error('입력값이 잘못되었습니다.');
+    if (!String(input).match(/[1-9]/g) || new Set(input).size !== 3 || input.length !== 3) {
+      throw Error('정확한 값을 입력하세요.');
     } else { this.getInterimOutcome(input); }
-    return true;
   }
 
   getInterimOutcome(input) {
@@ -48,8 +43,8 @@ class App {
       MissionUtils.Console.print(`${BALL}볼`);
       this.getInputNumber();
     } else if (!BALL && STRIKE) {
-      MissionUtils.Console.print();
-      this.getInputNumber(`${STRIKE}스트라이크`);
+      MissionUtils.Console.print(`${STRIKE}스트라이크`);
+      this.getInputNumber();
     } else if (BALL && STRIKE) {
       MissionUtils.Console.print(`${BALL}볼 ${STRIKE}스트라이크`);
       this.getInputNumber();
@@ -61,12 +56,10 @@ class App {
 
   getBall(input) {
     let ball = 0;
-    input.forEach((num, idx) => {
-      if (
-        this.randomArray.includes(Number(num))
-        && this.randomArray.indexOf(Number(num)) !== idx
-      ) {
+    [].forEach.call(input, (num, idx) => {
+      if (num !== this.computerNumber[idx] && this.computerNumber.includes(num)) {
         ball += 1;
+        MissionUtils.Console.print(ball);
       }
     });
     return ball;
@@ -74,9 +67,10 @@ class App {
 
   getStrike(input) {
     let strike = 0;
-    input.forEach((num, idx) => {
-      if (Number(num) === this.randomArray[idx]) {
+    [].forEach.call(input, (num, idx) => {
+      if (num === this.computerNumber[idx]) {
         strike += 1;
+        MissionUtils.Console.print(strike);
       }
     });
     return strike;
@@ -105,10 +99,6 @@ class App {
     if (input === '2') {
       MissionUtils.Console.close();
     }
-
-    if (input !== '1' || '2') {
-      throw Error('입력값이 잘못되었습니다.');
-    }
   }
 
   play() {
@@ -116,5 +106,8 @@ class App {
     this.getInputNumber();
   }
 }
+
+const app = new App();
+MissionUtils.Console.print(app.play());
 
 module.exports = App;
