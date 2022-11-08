7042644a45b0fd17ae0754366b69be308119ce16
serin-B <bangserin980302@gmail.com>
2022-11-08 23:36:48 +0900

    feat: playGame 생성(구현 목록 6,7번)

--- src/App.js
+++ src/App.js
@@ -4,6 +4,22 @@ const e = require("cors");
 class App {
   play() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    this.playGame();
+  }
+  playGame() {
+    let gameState = 1;
+    while (gameState === 1) {
+      let pickedNumber = this.pickUniqueNumber();
+      this.getInputNumber(pickedNumber);
+      MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+      MissionUtils.Console.readLine(
+        "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
+        (answer) => {
+          this.restartOrNot(answer);
+          gameState = answer;
+        }
+      );
+    }
   }
   pickUniqueNumber() {
     return MissionUtils.Random.pickUniqueNumbersInRange(0, 9, 3).join("");
