d8d1b20b6822ad2797a97f31a83659190476e323
Vo9Ming <kimminj2017@gmail.com>
2022-11-08 18:24:21 +0900

    숫자 야구 게임의 시작을 알리는 startMessage()함수 호출하기

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 const MissionUtils = require("@woowacourse/mission-utils");
-class App {
-  play() {}
-}
 
+class App {
+  play() {
+    this.startMessage();
+  }
+
+  startMessage() {
+    return MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+  }
+}
+const app = new App();
+app.play();
 module.exports = App;
