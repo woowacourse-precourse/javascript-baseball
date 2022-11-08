8728c03b7d360a01b412fb1d32114c1ff61b4b3c
MINJOO-KIM <gkminju2000@gmail.com>
2022-11-08 22:08:15 +0900

    fix: 입력받은 수에 대한 유효성 코드를 수정한다

--- src/inputValidation.js
+++ src/inputValidation.js
@@ -1,20 +1,22 @@
-export const isInvalidLength = (value) => {
+const isInvalidLength = (value) => {
   if (value.length !== 3) {
     throw new Error("입력한 수가 3자리가 아닙니다.");
   }
 };
-export const isDuplicated = (value) => {
+const isDuplicated = (value) => {
   if (new Set(value).size !== value.length) {
     throw new Error("중복된 수를 입력했습니다.");
   }
 };
-export const isNaN = (value) => {
+const isNaN = (value) => {
   if (Number.isNaN(Number(value))) {
     throw new Error("숫자만 입력해주세요. ");
   }
 };
-export const includeSpace = (value) => {
+const includeSpace = (value) => {
   if (!!value.match(/ /gi)) {
     throw new Error("공백을 제외해주세요.");
   }
 };
+
+module.exports = { isInvalidLength, isDuplicated, isNaN, includeSpace };
