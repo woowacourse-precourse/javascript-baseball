5e9c9407813c3590baeb57b9bc57793d549af117
elasjay <elasjay@LAPTOP-5ETGJV08.localdomain>
2022-11-08 11:15:05 +0900

    feat(): constructor method
    Added constructor to :
    - declare the class's variable: this._computer , this._player
    
    Breaks .this_computer and .this_player, which was declared in .setting move to .constructor

--- docs/README.md
+++ docs/README.md
@@ -3,8 +3,22 @@
 feat($App): setting method
 
 Added new method to $App:
-- declare the class's variable: `COMPUTER`, `PLAYER`
-- store the COMPUTER value using `MissionUtils.Random.pickNumberInRange`
+- declare the class's variable: COMPUTER, PLAYER
+- store the COMPUTER value using MissionUtils.Random.pickNumberInRange
 
 Breaks $App.setting, which was added
 
+fix($MissionUtils): import MissionUtils
+
+Changed the way to import MissionUtils:
+- change the importing method "ES Modules" to "Common JS"
+- const MissionUtils = require("@woowacourse/mission-utils");
+
+Breaks ES Module's Importing Method to Common JS's Importing Method
+
+feat($App): constructor method
+
+Added constructor to $App:
+- declare the class's variable: this._computer , this._player
+
+Breaks $App.this_computer and $App.this_player, which was declared in $App.setting move to $App.constructor
--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
-import * as MissionUtils from "@woowacourse/mission-utils";
+const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
+  constructor(){
+    this._computer = [];
+    this._player = [];
+  }
+
   setting(){
-    const COMPUTER = [];
-    const PLAYER = [];
-    while(COMPUTER.length < 3){
+    while(this._computer.length < 3){
         const NUMBER = MissionUtils.Random.pickNumberInRange(1, 9);
-        if(!COMPUTER.includes(NUMBER))
-            COMPUTER.push(NUMBER);
+        if(!this._computer.includes(NUMBER))
+            this._computer.push(NUMBER);
     }
   }
+
+  play() {
+    while(true){
+        this.setting();
+        console.log("숫자 야구 게임을 시작합니다.");
+        this.input();
+        let success = 0;
+        do{
+            success = this.check();
+        }while(success == 0);
+        if(this.end() == 2){
+            break;
+        }
+    }
+  }
+
 }
 
 module.exports = App;
