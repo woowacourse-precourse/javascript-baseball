59adbc1724af972d2fcba5d93a2c06bd1b4f4c14
JuhyunLim <87933367+userJu@users.noreply.github.com>
2022-11-08 22:03:10 +0900

    Refactor: 필요없는 require문 삭제

--- src/utils/MissionUtils.js
+++ src/utils/MissionUtils.js
@@ -1,23 +1,4 @@
 const MissionUtils = require("@woowacourse/mission-utils");
-const {
-  makeNumberToArray,
-  useNumberToArray,
-} = require("../hooks/useNumberToArray.js");
-const {
-  NOTTHING,
-  STRIKE,
-  BALL,
-  GAME_END,
-  REQUIRE_NUMBER,
-  RETRY_OR_END,
-  RETRY_VALUE,
-  END_VALUE,
-  START_NUMBER,
-  END_NUMBER,
-  MAX_CORRECT,
-  MIN_CORRECT,
-} = require("./Constants.js");
-const { NUMBER_ONE_TO_NINE } = require("./RegExpress.js");
 
 const setUserInput = (inputMessage, treatUserValueFunction) => {
   MissionUtils.Console.readLine(inputMessage, (userInput) => {
@@ -34,7 +15,6 @@ const deInitializationGame = () => {
 };
 
 module.exports = {
-  getUserNumber,
   setUserInput,
   printMessage,
   deInitializationGame,
