45314f22ebff7997c434ca851667f788c69989e8
cheonyeji <ssaltuck@gmail.com>
2022-11-08 20:50:57 +0900

    feat: 유저 입력 숫자 유효성 검사

--- src/App.js
+++ src/App.js
@@ -18,6 +18,17 @@ class App {
 
     return num;
   }
+
+  checkNum(userNum) {
+    const regex = /[^1-9]/;
+    if (regex.test(userNum)) {
+      throw new Error("숫자만 입력 바랍니다.");
+    } else if (userNum.length !== 3) {
+      throw new Error("3자리의 숫자를 입력 바랍니다.");
+    } else if ([...new Set(str)].length !== 3) {
+      throw new Error("숫자의 중복은 허용되지 않습니다. 바른 입력 바랍니다.");
+    }
+  }
 }
 
 module.exports = App;
