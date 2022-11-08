ff1caddb128a8c003248a17dfd745bcf6d891dba
gogh21 <gogh.phy@gmail.com>
2022-11-08 21:29:31 +0900

    feat: initialize variables and import external library

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  play() {
+    const computer = [];
+    let strike = 0;
+    let ball = 0;
+    let nothing = 0;
+    let userNum = '';
+  }
 }
 
 module.exports = App;
