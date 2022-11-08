b217942ac95046ba4c0fe3300f6f9a7077ffdd44
jiseung-kang <ill0o0lli@ajou.ac.kr>
2022-11-08 11:11:26 +0900

    fix(App): 잘못된 반복 코드 제거
    App.js를 실행했을 때 무한 루프 발견
    - playGame()을 이미 재귀적으로 실행하고 있으므로, play() 내에서 구현된 while()문 제거
    - strike결과에 따라 endGame()이나 playGame()을 실행하도록 구현
    
    Console.close()를 게임을 완전히 종료할 때 단한번 실행하는 것으로 수정

--- src/App.js
+++ src/App.js
@@ -9,20 +9,18 @@ class App {
   }
 
   play() {
-    this.resetGame();
     this.startGame();
-    while (this.strike !== LENGTH) this.playGame();
-    this.endGame();
+    this.playGame();
   }
 
   startGame() {
+    this.resetGame();
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
     this.setComputer();
   }
 
   playGame() {
     MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (answer) => {
-      MissionUtils.Console.close();
       this.user = answer;
 
       if (!this.isValidAnswer) {
@@ -38,6 +36,7 @@ class App {
         MissionUtils.Console.print(
           `${LENGTH}개의 숫자를 모두 맞히셨습니다! 게임 종료`
         );
+        this.endGame();
       } else {
         this.playGame();
       }
@@ -87,10 +86,10 @@ class App {
     MissionUtils.Console.readLine(
       '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.',
       (answer) => {
-        MissionUtils.Console.close();
         if (+answer === 1) this.play();
         else if (+answer === 2) {
-          MissionUtils.Console.print('게임 종료');
+          MissionUtils.Console.close();
+          return MissionUtils.Console.print('게임 종료');
         } else {
           throw new TypeError('1 또는 2만 입력해주세요');
         }
