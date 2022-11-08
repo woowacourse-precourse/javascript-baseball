9984a651ce22875a77d95cb26dd3fa06d19a3588
yunjjeongjo <two6885@gmail.com>
2022-11-08 23:07:04 +0900

    feat: 낫싱, 3스트라이크 결과 출력 구현
    printResultMessage 내에서 낫싱 이거나 3스트라이크일 때에 각각의 결과를 출력하게 변경했습니다.

--- src/utils/printResultMessage.js
+++ src/utils/printResultMessage.js
@@ -2,7 +2,13 @@ const { Console } = require("@woowacourse/mission-utils");
 const { MESSAGE } = require("../constants/constants");
 
 function printResultMessage(ball, strike) {
+  if (strike === 3) {
+    Console.print(`${strike}${MESSAGE.STRIKE}`);
+  } else if (ball === 0 && strike === 0) {
+    Console.print(MESSAGE.NOTHING);
+  } else {
     Console.print(`${ball}${MESSAGE.BALL} ${strike}${MESSAGE.STRIKE}`);
+  }
 }
 
 module.exports = printResultMessage;
