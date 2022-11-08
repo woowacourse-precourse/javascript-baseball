afda17b1a6540f0679fef206d9eaf90b93a851cb
Doyoung.Kim <dyflower@nate.com>
2022-11-08 16:29:32 +0900

    feat : 결과에 따라 재입력, 종료 실행 기능

--- src/App.js
+++ src/App.js
@@ -75,4 +75,7 @@ function scoreCheck(answercheck, answer) {
   }
 }
 
+function restartGame() {
+  MissionUtils.Console.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n', (input) => gameStartState(input));
+}
 module.exports = App;
