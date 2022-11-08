a72d649ffe12b594792a1408b6f408a3959a716e
kimeodml <kimeodml@gmail.com>
2022-11-08 21:16:08 +0900

    feat: gameProcess() 함수를 추가해 첫 문구 반복을 제거

--- src/App.js
+++ src/App.js
@@ -5,8 +5,12 @@ const ONLY_NUMBER = /^[1-9]+$/;
 class App {
   play() {
     Console.print('숫자 야구 게임을 시작합니다.');
-    this.computer = [];
+    this.gameProcess();
+  }
+
+  gameProcess() {
     this.input = [];
+    this.computer = [];
     this.computerRandomNumber();
     this.inputNumber();
   }
@@ -64,7 +68,7 @@ class App {
     if (strike !== 3) this.inputNumber();
     else this.restartGame();
   }
-  
+
   restartGame() {
     Console.print('3개의 숫자를 모두 맞히셨습니다! 게임종료');
     Console.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.', (input) => {
@@ -82,9 +86,5 @@ class App {
   }
 }
 
-
-
-
-}
 
 module.exports = App;
