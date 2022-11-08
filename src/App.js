80e854af699545adf5b3c452d6fd74607836ac11
soohyun_kim5 <93cherry@naver.com>
2022-11-08 19:14:51 +0900

    컴퓨터 랜덤 숫자 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 class App {
-  play() {}
+
+  play() {
+    let computerNumber = new Set();
+    while (computerNumber.size < 3) {
+      computerNumber.add(MissionUtils.Random.pickNumberInRange(1, 9))
+    }
+    computerNumber = Array.from(computerNumber)
+    console.log(computerNumber)
+  }
 }
 
 module.exports = App;
