f18122dce3786372097ced3a3a8467e3c98403d0
Wooseong Kim <kimwooseong8313@gmail.com>
2022-11-08 19:45:09 +0900

    modify: 컴퓨터 랜덤 숫자 생성

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
-  printGameStart() {
-    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
-  }
-
   createComputerNumber() {
-    const computerNumber = [];
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
 
-    while (computerNumber.length < 3) {
-      const number = MissionUtils.Random.pickNumberInRange(1, 9);
-      if (!computerNumber.includes(number)) {
-        computerNumber.push(number);
+    let computerNumber = new Set();
+
+    while (computerNumber.size !== 3) {
+      let number = MissionUtils.Random.pickNumberInRange(1, 9);
+
+      if (number !== 0) {
+        computerNumber.add(number);
       }
     }
 
-    return computerNumber;
+    return [...computerNumber];
   }
 
   checkBallStrike(computerNumber, userInput) {
