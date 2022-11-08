42abb53719bbbe8cca31e54ad55a3f5f545abbe0
shlee0820 <72565083+shlee0820@users.noreply.github.com>
2022-11-08 19:25:18 +0900

    feat: 컴퓨터가 생성한 난수 예외 처리 추가

--- src/utils/checkInvalidRandomNumber.js
+++ src/utils/checkInvalidRandomNumber.js
@@ -1,5 +1,36 @@
-const checkInvalidRandomNumber = () => {
-  const isInValid = false;
+// 난수의 조건
+// 2-1. 1-9까지의 범위이다.
+// 2-2. 서로 중복되지 않는다.
+// 2-3. 3자리이다.
+// 2-4. 숫자이다.
+
+const isDuplicatedNumber = (userInputNumber) => {
+  const newSet = new Set(userInputNumber);
+  return newSet.size !== userInputNumber.length;
+};
+
+const isNotThreeDigit = (userInputNumber) => userInputNumber.length !== 3;
+
+const isNotNumber = (userInputNumber) => {
+  let hasNan = false;
+  userInputNumber.split('').forEach((x) => {
+    if (Number.isNaN(+x)) hasNan = true;
+  });
+  return hasNan;
+};
+
+const checkInvalidRandomNumber = (input) => {
+  const userInputNumber = input.join('');
+  let isInValid = false;
+  if (isDuplicatedNumber(userInputNumber)) {
+    isInValid = true;
+  }
+  if (isNotThreeDigit(userInputNumber)) {
+    isInValid = true;
+  }
+  if (isNotNumber(userInputNumber)) {
+    isInValid = true;
+  }
   return isInValid;
 };
 
