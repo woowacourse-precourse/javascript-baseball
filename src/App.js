cbe737bd32a115b254275467565965d2f4f18cf9
turtle601 <turtle601@naver.com>
2022-11-08 14:41:48 +0900

    refactor(app) doNext, enter 함수 private 함수 선언

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-const { Console } = require('@woowacourse/mission-utils');
+const { Console } = require("@woowacourse/mission-utils");
 
-const BaseBall = require('./BaseBall');
+const BaseBall = require("./BaseBall");
 
-const RandomNumber = require('./RandomNumber');
+const RandomNumber = require("./RandomNumber");
 
-const Exception = require('./exception');
-const BaseBallException = require('./exception/BaseBall');
-const NextException = require('./exception/Next');
+const Exception = require("./exception");
+const BaseBallException = require("./exception/BaseBall");
+const NextException = require("./exception/Next");
 
-const { COMMAND } = require('./utils/constants');
+const { COMMAND } = require("./utils/constants");
 
 class App {
   #exception;
@@ -29,29 +29,29 @@ class App {
     Console.close();
   }
 
-  enter(random) {
+  play() {
+    this.print(COMMAND.START_MESSAGE);
+    this.#enter(RandomNumber.makeNew());
+  }
+
+  #enter(random) {
     Console.readLine(COMMAND.QUESTION, (input) => {
       this.#exception.checkErrorFor(new BaseBallException(input));
       this.print(this.#baseball.getResultToString(random, input));
 
-      if (this.#baseball.isStrikeOut(random, input)) this.doNext();
-      this.enter(random);
+      if (this.#baseball.isStrikeOut(random, input)) this.#doNext();
+      this.#enter(random);
     });
   }
 
-  doNext() {
+  #doNext() {
     Console.readLine(`${COMMAND.NEXT_QUESTION}\n`, (input) => {
       this.print(COMMAND.STRIKEOUT);
       this.#exception.checkErrorFor(new NextException(input));
 
-      if (input === COMMAND.RESTART) this.enter(RandomNumber.makeNew());
+      if (input === COMMAND.RESTART) this.#enter(RandomNumber.makeNew());
       this.end();
     });
-  }
-
-  play() {
-    this.print(COMMAND.START_MESSAGE);
-    this.enter(RandomNumber.makeNew());
   }
 }
 
