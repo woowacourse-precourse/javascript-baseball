859936908658cbbd35d7b74bf9b85566d36ec30c
cruelladevil <dev.timetravel@gmail.com>
2022-11-08 18:46:28 +0900

    refactor: change error type

--- src/Computer.js
+++ src/Computer.js
@@ -1,4 +1,5 @@
 const { Random } = require('@woowacourse/mission-utils');
+const ValidationError = require('./ValidationError');
 
 class Computer {
   static createUniqueNumbers(start, end, count) {
@@ -20,19 +21,19 @@ class Computer {
       !Number.isInteger(end) ||
       !Number.isInteger(count)
     ) {
-      throw new Error('인수는 정수여야 합니다.');
+      throw new ValidationError('인수는 정수여야 합니다.');
     }
 
     if (start > end) {
-      throw new Error('start가 end보다 커서는 안됩니다.');
+      throw new ValidationError('start가 end보다 커서는 안됩니다.');
     }
 
     if (count < 0) {
-      throw new Error('count는 0보다 작으면 안됩니다.');
+      throw new ValidationError('count는 0보다 작으면 안됩니다.');
     }
 
     if (count > end - start + 1) {
-      throw new Error(
+      throw new ValidationError(
         `count: ${count}가 input 범위 (end - start + 1): ${end - start + 1}보다 커서는 안됩니다.`
       );
     }
