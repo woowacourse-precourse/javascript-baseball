50d1e180c4a1bbed44325326b8c21d7de5345c2c
seungki <sysh9498@gmail.com>
2022-11-08 18:55:04 +0900

    feat : 게임 시작 메세지 출력

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
-import * as MissionUtils from '@woowacourse/mission-utils';
+const MissionUtils = require('@woowacourse/mission-utils');
+const { Console } = MissionUtils;
 class App {
   play() {}
+  printStartMessage() {
+    const START_MESSAGE = '숫자 야구 게임을 시작합니다.';
+    Console.print(START_MESSAGE);
+  }
 }
-
 module.exports = App;
