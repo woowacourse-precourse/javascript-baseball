6151656c1a6ee704afd4f9082d8231d6571d95b6
SALT0601 <thfxm0601@gmail.com>
2022-11-08 20:14:33 +0900

    add: 사용자가 숫자 3자리 입력

--- src/App.js
+++ src/App.js
@@ -9,7 +9,24 @@ class App {
        }
      }    
   }
-  
+  inputNumber() {
+    Console.readLine("숫자를 입력해주세요 : ", (ans) => {
+      if (ans.length === 3 && !isNaN(Number(ans))) {
+        MissionUtils.Console.print("중복되지 않은 숫자 3개를 입력해주세요.");
+      }
+      const playerNumber = [];
+      let res = {};
+
+        playerNumber.forEach((num, i) => {
+        if (computer[i] === num) {
+          res.strike = res.strike + 1 || 1;
+        }
+        if (computer[i] !== num && computer.includes(num)) {
+          res.ball = res.ball + 1 || 1;
+        }
+      });
+    });
+  }
   play() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
     randomNumber();
