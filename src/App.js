1c2b2b2ce7b49889e91bdd8fcc958e3803cdfd66
Hannah Ahn <ahnlook90@gmail.com>
2022-11-08 19:07:56 +0900

    refactor: sequence function fix: not working function

--- src/App.js
+++ src/App.js
@@ -1,34 +1,24 @@
-// import * as MissionUtils from "@woowacourse/mission-utils"
 const MissionUtils = require('@woowacourse/mission-utils')
 const { Console, Random } = MissionUtils
 
-
 class App {
-  initGame () {
-    const randomNumber = Random.pickUniqueNumbersInRange(1, 9, 3)
-    const rightnumber = randomNumber.join('')
-    Console.print(announcement('start'))
-    palyBaseballGame(rightnumber)
+  gameOver () {
+    Console.readLine('숫자를 게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n', (answer) => {
+      const userAnswer = answer.trim()
+      switch (userAnswer) {
+        case '1': return this.initGame()
+        case '2': return Console.close()
+        default: this.gameOver()
+      }
+    })
   }
 
-  playHandler = {
-    start : '숫자 야구 게임을 시작합니다.',
-    requestAndAnswer :
-      Console.readLine('숫자를 입력해주세요 : ',
-        (answer) => {
-          return `${answer}`
-        }),
-    requestRestart :
-      Console.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n',
-        (answer) => {
-          selectEndOption(answer)
-        })
-    }
-  
-  initCount () {
-    return [0, 0]
+  checkOver (strike) {
+    if (strike === 3) {
+      return true
+     } false
   }
-  
+
   feedbackMessage (strike, ball) {
     if (strike === 3) {
       Console.print(`${strike}스트라이크 3개의 숫자를 모두 맞히셨습니다! 게임 종료`)
@@ -56,55 +46,68 @@ class App {
     }
   }
 
-  selectEndOption(option) {
-    switch (option) {
-      case '1': return initGame() // 게임 재실행 함수
-      case '2': return Console.close() // 게임 종료 
-      default: 
-        Console.print(`올바른 입력을 해주세요`)
-        announcement.requestRestart() // 다시 물어보기
-    }
+  initCount () {
+    return [0, 0]
   }
 
-  exception (value) {
-    switch (value) {
-      case isNaN(Number(value)): return true
-      case value < 1: return true
-      case value.length > 3: return true
-      default: false
-    }
-  }
-  
-  // 사용자에게 number 입력 받아 실행하는 함수
-  palyBaseballGame (rightNumber) {
-    let inputNumber = this.playHandler.requestAndAnswer()
-    
-    if (exception(inputNumber)) {
-      throw new Error('값이 잘못되었습니다.\n 어플리케이션을 종료합니다.')
-    }
-  
+  checkCount (userAnswer, rightNumber) {
+    console.log(userAnswer, rightNumber)
     let [strike, ball] = this.initCount()
   
     for (let i = 0; i < 3; i++) {
-      if (rightNumber.includes(inputNumber[i])) {
+      if (rightNumber.includes(userAnswer[i])) {
         ball += 1
       }
-      if (inputNumber[i] === rightNumber[i]) {
+      if (userAnswer[i] === rightNumber[i]) {
         strike += 1
         ball -= 1
       }
     }
-  
-    Console.print(this.feedbackMessage(strike, ball))
-    if (start === 3) {
-      this.playHandler.requestRestart()
+
+    this.feedbackMessage(strike, ball)
+
+    let gameOverStatus = this.checkOver(strike)
+    if (!gameOverStatus) {
+      this.getUserInput(rightNumber)
     }
 
-    palyBaseballGame()
+    if (gameOverStatus) {
+      this.gameOver()
+    }
+
   }
 
-  play() {
-    this.initGame()
+  exception (userNumber) {
+    switch (userNumber) {
+      case !userNumber: return true
+      case isNaN(Number(userNumber)): return true
+      case userNumber.length !== 3: return true
+      case userNumber < 0 : return true
+      default : return false
+    }
+  }
+
+  getUserInput (rightNumber) {
+    Console.readLine('숫자를 입력해주세요 : ', (answer) => {
+      const userAnswer = answer.trim()
+      const error = this.exception(userAnswer)
+      if (error) {
+        throw new Error('잘못된 입력으로 종료되었습니다.')
+      } else {
+      this.checkCount(userAnswer,rightNumber)
+      }
+    })
+  }
+
+  initGame () {
+    const randomNumber = Random.pickUniqueNumbersInRange(1, 9, 3)
+    const rightNumber = randomNumber.join('')
+    Console.print('숫자 야구 게임을 시작합니다.')
+    this.getUserInput(rightNumber)
+  }
+
+  play () {
+   this.initGame ()
   }
 }
 
