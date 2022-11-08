561971cfcf55ed514df18e75363f2662296665c5
RyuJoonSik <tldtksahtxja@naver.com>
2022-11-08 21:18:48 +0900

    test: AutomaticBallGenerator.test.js의 execute 테스트 케이스 수정
    "execute로 3개의 숫자를 생성"에서 "execute로 숫자를 생성"로 수정했습니다.

--- src/Components/AutomaticBallGenerator/AutomaticBallGenerator.test.js
+++ src/Components/AutomaticBallGenerator/AutomaticBallGenerator.test.js
@@ -1,10 +1,10 @@
 const AutomaticBallGenerator = require("./AutomaticBallGenerator");
 
 describe("AutomaticBallGenerator", () => {
-  test("execute로 3개의 숫자를 생성", () => {
+  test("execute로 숫자를 생성", () => {
     const BALL_GENERATOR = new AutomaticBallGenerator();
-    const NUMBER_ARRAY = BALL_GENERATOR.execute();
+    const BALL = BALL_GENERATOR.execute();
 
-    expect(NUMBER_ARRAY).toHaveLength(3);
+    expect(typeof BALL.getNumber()).toBe("number");
   });
 });
