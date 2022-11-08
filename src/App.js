28c2c410b81af02f482f4e470366b1a1fb1a66d6
KINHYEONJI <hyeonji0718@gmail.com>
2022-11-08 13:59:45 +0900

    feat(App) : 재시작 여부를 묻는 함수 askMoreGame 구현

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App {
   alertStart() {
     MissionUtils.Console.print(SYS_MESSAGE.START_MESSAGE);
   }
-
+  play() {}
   showBallState(strikeCnt, ballCnt) {
     if (strikeCnt === 3)
       MissionUtils.Console.print(
@@ -21,6 +21,18 @@ class App {
       MissionUtils.Console.print(ballCnt + BALL_STATE.BALL);
     if (strikeCnt === 0 && ballCnt === 0)
       MissionUtils.Console.print(BALL_STATE.NOTHING);
+  }
+  askMoreGame() {
+    MissionUtils.Console.readLine(
+      "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. ",
+      (userInputAboutRestart) => {
+        if (userInputAboutRestart === "1") MissionUtils.Console.close();
+        return this.play();
+        if (userInputAboutRestart === "2") MissionUtils.Console.close();
+        MissionUtils.Console.print("종료");
+        MissionUtils.Console.close();
+      }
+    );
   }
 }
 
