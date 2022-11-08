07a41803f37d80692f5aade85113957d41ed720d
SeongOh Park <qkrtjddh1212@naver.com>
2022-11-08 21:44:35 +0900

    feat(result): calculate result and print out
    Breaks answer is not Set anymore, instead Array for using includes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,11 @@ const MESSAGE = {
 
 class App {
   constructor() {
-    this.answer = new Set();
+    this.answer = [];
+    this.result = {
+      ball: 0,
+      strike: 0,
+    };
   }
 
   play() {
@@ -16,16 +20,44 @@ class App {
   }
 
   pickRandomNumber() {
-    while (this.answer.size < 3) {
+    while (this.answer.length < 3) {
       const number = Random.pickNumberInRange(1, 9);
-      if (!this.answer.has(number)) {
-        this.answer.add(number);
+      if (!this.answer.includes(number)) {
+        this.answer.push(number);
       }
     }
   }
 
   playInning() {
     const userAnswer = this.getUserInput();
+    this.getResult(userAnswer);
+    this.printResult();
+  }
+
+  printResult() {
+    if (!this.result.ball) {
+      Console.print(`${this.result.strike}스트라이크`);
+    } else if (!this.result.strike) {
+      Console.print(`${this.result.ball}볼`);
+    } else {
+      Console.print(`${this.result.ball}볼 ${this.result.strike}스트라이크`);
+    }
+  }
+
+  getResult(userAnswer) {
+    [...userAnswer].forEach((userNum, idx) => {
+      this.countBallAndStrike(userNum, idx);
+    });
+  }
+
+  countBallAndStrike(userNum, idx) {
+    if (this.answer.includes(parseInt(userNum, 10))) {
+      if (this.answer.indexOf(parseInt(userNum, 10)) === idx) {
+        this.result.strike += 1;
+      } else {
+        this.result.ball += 1;
+      }
+    }
   }
 
   getUserInput() {
