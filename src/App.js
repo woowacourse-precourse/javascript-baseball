16a83d987ff710c4e7745a81bcd120bacbadf735
Minyoung Lee <mminy62@gmail.com>
2022-11-08 23:25:28 +0900

    refactor: debug용 console.log 삭제

--- src/App.js
+++ src/App.js
@@ -12,13 +12,13 @@ class App {
           computer.push(number);
         }
       }
-      console.log("computer: " + computer); 
+      //console.log("computer: " + computer); 
       resolve(computer);
     });
   }
 
   inputCheck(userArray){
-    console.log("check: " + userArray);
+    //console.log("check: " + userArray);
     if(userArray.length > 3){ //1) 입력 값은 3자리
       throw "input length > 3";
     }
@@ -37,7 +37,7 @@ class App {
         if (isDuplicate){
           throw "is duplicate";
         }
-        
+
       }
 
     });
@@ -54,7 +54,7 @@ class App {
         MissionUtils.Console.close();
       });
 
-    
+
     });
     
   }
@@ -68,14 +68,20 @@ class App {
     return [ballNum, strikeNum];
   }
 
-  print(ball, strike){
+  async print(ball, strike){
+    
+    let isSuccess= [0,0]; // 0번째 -> 정답 유무, 1번째 -> restart 유무
+
     if (ball + strike === 0){
       console.log("낫싱");
     }
     else if (strike === 3){
-      let string = "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
       console.log(strike + "스트라이크");
-      console.log(string);
+      console.log("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+      
+      isSuccess[0] = 1;
+      isSuccess[1] = await this.restart(); // input 값 체크하는 용도
+      
     }
     else if (ball !== 0 && strike !== 0){
       console.log(ball + "볼 " + strike + "스트라이크");
@@ -87,35 +93,77 @@ class App {
       console.log(strike + "스트라이크");
     }
 
+    return isSuccess;
+
+  }
+
+  restart(){
+    let restartValue;
+    console.log("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+
+
+    return new Promise(function(resolve, reject) {
+      MissionUtils.Console.readLine('', (answer) => {
+        console.log(`${answer}`);
+        if(answer === 1){ //restart 
+          restartValue = 1;
+        }
+        else if(answer === 2){
+          restartValue = 0;
+        }
+        resolve(restartValue);
+        
+        MissionUtils.Console.close();
+      });
+
+    
+    });
   }
 
 
   async play() {
-    let computer = await this.random(); 
-    let user = await this.input();
-    try {
-      this.inputCheck(user);
-    } catch (e) {
-      console.error(e);
-      //break;
-    }
-    //  let bNum, sNum;
-    //  [bNum, sNum] = this.compare(user, computer);
 
-    //  this.print(bNum, sNum);
-
+    console.log("숫자 야구 게임을 시작합니다.");
     
+    let computer = await this.random();
+    let isSuccess;
+    let flag;
 
+    while(true){
+      
+      let user = await this.input();
+      
+      try {
+        this.inputCheck(user);
+        let bNum, sNum;
+        [bNum, sNum] = this.compare(user, computer);
+
+        isSuccess = this.print(bNum, sNum);
+
+      }
+      catch(e){//input error
+        //console.error(e);
+        break;
+      }
+      //console.log(isSuccess);
+
+      if(isSuccess[0] === 1){ //success the game
+        break;
+      }
+      
+      if(isSuccess[1] === 1){ //restart 
+        break;
+      }
+
+    }
     
   }
 
 }
 
-
-  
-
 const app = new App();
 app.play();
+
 
 
 
