8f21a163f0f2ce1e937599b723d4a3ea8933d921
Jongco <narira0922@gmail.com>
2022-11-08 20:48:39 +0900

    feat: computer array 생성 및 랜덤 숫자 3개 넣기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+const { Console, Random } = require("@woowacourse/mission-utils");
 class App {
-  play() {}
+  play() {
+    const computer = [];
+
+    while (computer.length < 3) {
+      const number = Random.pickNumberInRange(1, 9);
+      if (!computer.includes(String(number))) {
+        computer.push(String(number));
+      }
+    }
+    console.log(computer);
+  }
 }
+
+const app = new App();
+
+app.play();
 
 module.exports = App;
