08047b8c1f08ddaa43d63afa0625fea0b4bfa56b
cocoa <jwv0323@kau.kr>
2022-11-08 20:09:28 +0900

    feat: eslint, prettier 적용

--- src/components/baseballGame.js
+++ src/components/baseballGame.js
@@ -1,125 +1,126 @@
-const { Console, Random } = require("@woowacourse/mission-utils");
-const { NUMBER, MESSAGE, GAME , ERROR} = require('../constant/baseballGame');
+const { Console, Random } = require('@woowacourse/mission-utils');
+const { NUMBER, MESSAGE, GAME, ERROR } = require('../constant/baseballGame');
 
-class BaseballGame{
+class BaseballGame {
+  start() {
+    const answer = BaseballGame.createAnswer();
 
-    start() {
-        const answer = this.createAnswer();
+    BaseballGame.startMessage();
+    this.inputNumber(answer);
+  }
 
-        this.startMessage();
-        this.inputNumber(answer);
+  progress(answer, strike) {
+    if (strike === 3) {
+      Console.print(MESSAGE.END);
+      this.inputRestartOrEnd();
     }
 
-    progress(answer, strike) {
-        if(strike === 3){
-            Console.print(MESSAGE.END);
-            this.inputRestartOrEnd();
-        }
-    
-        this.inputNumber(answer);
+    this.inputNumber(answer);
+  }
+
+  static startMessage() {
+    Console.print(MESSAGE.START);
+  }
+
+  resultMessage(answer, number) {
+    const ball = BaseballGame.getBall(answer, number);
+    const strike = BaseballGame.getStrike(answer, number);
+
+    let message = '';
+
+    if (ball) {
+      message += `${ball}${GAME.BALL} `;
     }
 
-    startMessage() {
-        Console.print(MESSAGE.START);
+    if (strike) {
+      message += `${strike}${GAME.STRIKE}`;
     }
 
-    resultMessage(answer, number) {
-        const ball = this.getBall(answer, number);
-        const strike = this.getStrike(answer, number);
-
-        let message = ``;
-
-        if(ball){ 
-            message += `${ball}${GAME.BALL} `; 
-        }
-
-        if(strike){ 
-            message += `${strike}${GAME.STRIKE}`; 
-        }
-
-        if(!ball && !strike){ 
-            message += `${GAME.NOTHING}`; 
-        }
-
-        Console.print(message);
-        this.progress(answer, strike);
+    if (!ball && !strike) {
+      message += `${GAME.NOTHING}`;
     }
 
-    getBall(answer, number){
-        let ball = 0;
-        
-        [...number].forEach((number, index) => {
-            if(number !== answer[index] && answer.includes(number)){
-                ball++;
-            }
-        });
+    Console.print(message);
+    this.progress(answer, strike);
+  }
 
-        return ball;
+  static getBall(answer, number) {
+    let ball = 0;
+
+    [...number].forEach((digit, index) => {
+      if (digit !== answer[index] && answer.includes(digit)) {
+        ball += 1;
+      }
+    });
+
+    return ball;
+  }
+
+  static getStrike(answer, number) {
+    let strike = 0;
+
+    [...number].forEach((digit, index) => {
+      if (digit === answer[index]) {
+        strike += 1;
+      }
+    });
+
+    return strike;
+  }
+
+  static createAnswer() {
+    const answer = [];
+
+    while (answer.length < NUMBER.DIGIT) {
+      const number = Random.pickNumberInRange(
+        NUMBER.MINIMUM_RANGE,
+        NUMBER.MAXIMUM_RANGE
+      );
+
+      if (!answer.includes(number)) {
+        answer.push(number);
+      }
     }
 
-    getStrike(answer, number){
-        let strike = 0;
-        
-        [...number].forEach((number, index) => {
-            if(number === answer[index]) {
-                strike++;
-            }
-        });
+    return answer.join('');
+  }
 
-        return strike;
+  inputNumber(answer) {
+    Console.readLine(MESSAGE.INPUT, (number) => {
+      BaseballGame.validateInputNumber(number);
+      this.resultMessage(answer, number);
+    });
+  }
+
+  static validateInputNumber(number) {
+    if (!Number(number)) {
+      throw new Error(ERROR.NUMBER);
     }
-
-    createAnswer() {
-        const answer = [];
-
-        while (answer.length < NUMBER.DIGIT) {
-            const number = Random.pickNumberInRange(NUMBER.MINIMUM_RANGE, NUMBER.MAXIMUM_RANGE);
-
-            if (!answer.includes(number)) {
-                answer.push(number);
-            }
-        }
-
-        return answer.join('');
+    if (number.length !== NUMBER.DIGIT) {
+      throw new Error(ERROR.LENGTH);
     }
-
-    inputNumber(answer) {
-        Console.readLine(MESSAGE.INPUT, (number) => {
-            this.validateInputNumber(number);
-            this.resultMessage(answer, number);
-        });
+    if ([...new Set(number)].length !== NUMBER.DIGIT) {
+      throw new Error(ERROR.OVERLAP);
     }
+  }
 
-    validateInputNumber(number) {
-        if (!Number(number)) {
-            throw new Error(ERROR.NUMBER);
-        }
-        if (number.length !== NUMBER.DIGIT) {
-            throw new Error(ERROR.LENGTH);
-        }
-        if ([...new Set(number)].length !== NUMBER.DIGIT){
-            throw new Error(ERROR.OVERLAP);
-        }
-    }
+  inputRestartOrEnd() {
+    Console.readLine(MESSAGE.OPTION, (number) => {
+      BaseballGame.validateInputRestartOrEnd(number);
+      if (number === '1') {
+        this.start();
+      }
+      if (number === '2') {
+        Console.close();
+      }
+    });
+  }
 
-    inputRestartOrEnd() {
-        Console.readLine(MESSAGE.OPTION, (number) => {
-            this.validateInputRestartOrEnd(number)
-            if(number === '1'){
-                this.start();
-            }
-            if(number === '2'){
-                Console.close();
-            }
-        });
+  static validateInputRestartOrEnd(number) {
+    if (number !== '1' && number !== '2') {
+      throw new Error(ERROR.OPTION);
     }
-    
-    validateInputRestartOrEnd(number) {
-        if(number !== '1' && number !== '2'){
-            throw new Error(ERROR.OPTION);
-        }
-    }
-
+  }
 }
 
 module.exports = BaseballGame;
--- src/constant/baseballGame.js
+++ src/constant/baseballGame.js
@@ -1,32 +1,32 @@
 const NUMBER = {
-    DIGIT: 3,
-    MINIMUM_RANGE: 1,
-    MAXIMUM_RANGE: 9
+  DIGIT: 3,
+  MINIMUM_RANGE: 1,
+  MAXIMUM_RANGE: 9,
 };
 
 const MESSAGE = {
-    START: '숫자 야구 게임을 시작합니다.',
-    INPUT: '숫자를 입력해주세요 : ',
-    END: '3개의 숫자를 모두 맞히셨습니다! 게임 종료',
-    OPTION: '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n',
+  START: '숫자 야구 게임을 시작합니다.',
+  INPUT: '숫자를 입력해주세요 : ',
+  END: '3개의 숫자를 모두 맞히셨습니다! 게임 종료',
+  OPTION: '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n',
 };
 
 const GAME = {
-    BALL: '볼',
-    STRIKE: '스트라이크',
-    NOTHING: '낫싱'
-}
+  BALL: '볼',
+  STRIKE: '스트라이크',
+  NOTHING: '낫싱',
+};
 
 const ERROR = {
-    OPTION: '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.',
-    NUMBER: '숫자를 입력하세요.',
-    LENGTH: '세 자릿수를 입력하세요.',
-    OVERLAP: '중복되지 않은 수를 입력하세요.'
-}
+  OPTION: '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.',
+  NUMBER: '숫자를 입력하세요.',
+  LENGTH: '세 자릿수를 입력하세요.',
+  OVERLAP: '중복되지 않은 수를 입력하세요.',
+};
 
 module.exports = {
-    NUMBER,
-    MESSAGE,
-    GAME,
-    ERROR
-}
+  NUMBER,
+  MESSAGE,
+  GAME,
+  ERROR,
+};
