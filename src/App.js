70efe4b5aa91cf78e50350a82a5d5ae2c4f5edce
gogh21 <gogh.phy@gmail.com>
2022-11-08 21:31:10 +0900

    feat: generate computer numbers

--- src/App.js
+++ src/App.js
@@ -7,6 +7,16 @@ class App {
     let ball = 0;
     let nothing = 0;
     let userNum = '';
+
+    // 컴퓨터가 랜덤한 숫자 3개를 만드는 함수
+    function createComputerNumber() {
+      while (computer.length < 3) {
+        let number = MissionUtils.Random.pickNumberInRange(1, 9);
+        if (!computer.includes(number)) {
+          computer.push(number);
+        }
+      }
+    }
   }
 }
 
