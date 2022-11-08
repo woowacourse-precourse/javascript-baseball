0eaf38491e8c4ed714a035e087548f4be5ae5df9
cheonyeji <ssaltuck@gmail.com>
2022-11-08 21:23:50 +0900

    feat: 유저로부터 숫자 입력받아 숫자야구 실행하도록 코드 업데이트

--- src/App.js
+++ src/App.js
@@ -33,9 +33,22 @@ class App {
   inputNum() {
     MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (inputNumber) => {
       this.checkNum(inputNumber);
+
       // 숫자 야구 실행 후 결과 출력
+      var result = this.checkBaseball(this.computerNum, inputNumber);
+      var printResult = this.printBaseballResult(result);
+
+      MissionUtils.Console.print(printResult);
+
       // 만약 스트라이크인 경우 재시작/종료 여부 입력받기
+      if (result[1] === 3) {
+        MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        return this.restartOrExit();
+      }
       // 아니면 숫자 재입력
+      else {
+        this.inputNum();
+      }
     });
   }
 
