dfdf4bafb75af4f48639b2af753fad253cf3f2b4
Y2IN <dpdls0228@daum.net>
2022-11-08 13:39:56 +0900

    refactor : 안 써도 되는 조건문 삭제

--- src/baseball/ValidUserInput.js
+++ src/baseball/ValidUserInput.js
@@ -1,21 +1,14 @@
 class ValidUserNumbers {
-  static isThreeNumberInRange(userInput) {
-    const NUMBER_IN_RANGE = /^[1-9]+$/;
-    const ERROR_MESSAGE =
-      "1부터 9까지 서로 다른 수로 이루어진 3자리 숫자를 입력해주세요.";
-    const USER_NUMBERS = [];
+  static isValidUserInput(userInput) {
+    const isNumberElement = (element) => element >= "1" && element <= "9";
+    const userNumbers = [];
+    userInput.split("").forEach((element) => {
+      !userNumbers.includes(element) ? userNumbers.push(element) : "";
+    });
 
-    userInput
-      .split("")
-      .forEach((element) =>
-        !USER_NUMBERS.includes(element) ? USER_NUMBERS.push(element) : ""
-      );
-    if (USER_NUMBERS.length !== 3) throw new Error(ERROR_MESSAGE);
-
-    if (userInput.length !== 3 || !NUMBER_IN_RANGE.test(userInput)) {
-      throw new Error(ERROR_MESSAGE);
-    }
+    return userNumbers.length === 3 && userNumbers.every(isNumberElement);
   }
+
   static isvalidRestart(restartNumber) {
     restartNumber = Number(restartNumber);
 
@@ -23,4 +16,5 @@ class ValidUserNumbers {
       throw new Error("1 또는 2만 입력해주세요.");
   }
 }
+
 module.exports = ValidUserNumbers;
