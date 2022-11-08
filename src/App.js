f24460c77b9de840dd112fc964b596786d7fa4f5
Haram Baek <haram@Haramui-MacBookAir.local>
2022-11-08 13:21:48 +0900

    refactor: constructor 변수명 변경

--- src/App.js
+++ src/App.js
@@ -1,157 +1,107 @@
-const MissionUtils = require("@woowacourse/mission-utils");
-
-// function createRandNum() {
-//   let randNum = [];
-
-//   for (let i = 0; i < 3; i++) {
-//     let tempNum = MissionUtils.Random.pickNumberInList([1, 2, 3, 4, 5, 6, 7, 8, 9]);
-//     if (!randNum.includes(tempNum)) {
-//       randNum.push(tempNum);
-//     } else {
-//       i--;
-//     }
-//   }
-
-//   console.log(randNum, 'random number 생성');
-//   MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
-
-//   return randNum
-// }
-
-// function inputUserAnswer(randNum) {
-//   let strike = 0;
-//   let ball = 0;
-//   let inputNum = [];
-
-//   MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (answer) => {
-//     for (a of answer) {
-//       inputNum.push(parseInt(a))
-//     }
-//     console.log(inputNum, '입력된 숫자');
-
-//     // 낫싱인지 확인
-//     if (checkNothing(randNum, inputNum)) {
-//       MissionUtils.Console.print('낫싱');
-//     } else {
-//       // 스트라이크 갯수 확인
-//       strike = checkStrike(randNum, inputNum);
-//       // 볼 갯수 확인
-//       ballList = checkBall(randNum, inputNum);
-//     }
-//     if (strike === 3) {
-//       MissionUtils.Console.print('3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료\n')
-//       MissionUtils.Console.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.', (opinion) => {
-//         if (opinion === 1) {
-//           inputUserAnswer();
-//         } else {
-//           MissionUtils.Console.close()
-//         }
-//       })
-//     } else {
-//       MissionUtils.Console.print(`${ball}볼 ${strike}스트라이크`);
-//       inputUserAnswer();
-//     }
-//   })
-// }
+const { Console, Random } = require("@woowacourse/mission-utils");
 
 class App {
   constructor() {
-    this.randNum = [];
-    this.inputNum = [];
-    this.isNothing = true;
-    this.ball = 0;
-    this.strike = 0;
+    this.RAND_NUM = [];
+    this.INPUT_NUM = [];
+    this.IS_NOTHING = true;
+    this.COUNT_BALL = 0;
+    this.COUNT_STRIKE = 0;
+  }
+
+  init() {
+    this.RAND_NUM = [];
+    this.INPUT_NUM = [];
+    this.IS_NOTHING = true;
+    this.COUNT_BALL = 0;
+    this.COUNT_STRIKE = 0;
   }
 
   play() {
-    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+    Console.print('숫자 야구 게임을 시작합니다.');
 
-    // 1. 컴퓨터가 정답(1-9 사이의 서로 다른 수로 이루어진 3자리 숫자)을 생성한다.
-    while (this.randNum.length < 3) {
-      const number = MissionUtils.Random.pickNumberInRange(1, 9);
-      if (!this.randNum.includes(number)) {
-        this.randNum.push(number);
-      }
-    }
+    // 1. random number(컴퓨터 숫자) 생성
+    this.createRandomNumber();
 
     this.playGame();
   }
 
-  playGame() {
-    this.init();
-
-    //2. 사용자가 숫자를 입력한다. 
-    MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (answer) => {
-      const ANSWER = Array.from(answer);
-      ANSWER.map((a, idx) => {
-        this.inputNum[idx] = parseInt(a);
-      })
-
-      console.log(this.randNum, this.inputNum, "랜덤 숫자, 입력 숫자")
-      this.validateInput(this.inputNum);
-
-      //2. randNum과 비교하여 결과를 확인한다.
-      this.checkResult();
-    });
-  }
-
-  init() {
-    this.isNothing = true;
-    this.ball = 0;
-    this.strike = 0;
-  }
-
-  checkResult() {
-    // console.log(this.inputNum, this.randNum, '입력한 숫자, 랜덤 숫자');
-    if (this.inputNum !== []) {
-      this.inputNum.map((num, idx) => {
-        if (this.randNum.includes(num)) {
-          this.isNothing = false
-          if (num !== this.randNum[idx]) {
-            this.ball++;
-          } else {
-            this.strike++;
-          }
-        }
-      })
-      this.printMessages();
-    }
-  }
-
-  printMessages() {
-    if (this.isNothing) {
-      // 정답을 맞춘 경우가 아니면 사용자에게 입력만 다시 받기
-      MissionUtils.Console.print('낫싱');
-      this.playGame();
-    } else {
-      // 3. 정답을 맞춘 경우 게임 시작
-      if (this.strike === 3) {
-        // MissionUtils.Console.print('3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료\n')
-        console.log('3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료\n');
-        MissionUtils.Console.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.', (opinion) => {
-          if (opinion === 1) {
-            this.play();
-          } else {
-            MissionUtils.Console.close()
-          }
-        })
-      } else {
-        // 정답을 맞춘 경우가 아니면 사용자에게 입력만 다시 받기
-        MissionUtils.Console.print(`${this.ball}볼 ${this.strike}스트라이크`);
-        this.playGame();
+  createRandomNumber() {
+    while (this.RAND_NUM.length < 3) {
+      const number = Random.pickNumberInRange(1, 9);
+      if (!this.RAND_NUM.includes(number)) {
+        this.RAND_NUM.push(number);
       }
     }
   }
 
-  validateInput(input) {
-    if (input.length === 0) {
+  playGame() {
+    //2. 사용자가 숫자를 입력한다. 
+    Console.readLine('숫자를 입력해주세요 : ', (answer) => {
+      const ANSWER = Array.from(answer);
+      ANSWER.map((a, idx) => {
+        this.INPUT_NUM[idx] = parseInt(a);
+      })
+
+      // 4. 사용자가 입력한 입력에 대한 validation을 수행한다.
+      this.validateInput(this.INPUT_NUM);
+
+      //2. randNum과 비교하여 결과를 확인한다.
+      this.checkResult(this.INPUT_NUM, this.RAND_NUM);
+    });
+  }
+
+  checkResult(INPUT_NUM, RAND_NUM) {
+    console.log(INPUT_NUM, RAND_NUM, '입력한 숫자, 랜덤 숫자');
+    INPUT_NUM.map((num, idx) => {
+      if (RAND_NUM.includes(num)) {
+        this.IS_NOTHING = false
+        if (num !== RAND_NUM[idx]) {
+          this.COUNT_BALL++;
+        } else {
+          this.COUNT_STRIKE++;
+        }
+      }
+    })
+    this.printMessages();
+  }
+
+  printMessages() {
+    if (this.IS_NOTHING) {
+      // 정답을 맞춘 경우가 아니면 사용자에게 입력만 다시 받기
+      Console.print('낫싱');
+      this.init();
+      this.play();
+    } else {
+      // 3. 정답을 맞춘 경우 게임 시작
+      if (this.COUNT_STRIKE === 3) {
+        Console.print('3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료\n')
+        Console.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.', (opinion) => {
+          if (opinion === 1) {
+            this.init();
+            this.play();
+          } else {
+            Console.close()
+          }
+        })
+      } else {
+        // 정답을 맞춘 경우가 아니면 사용자에게 입력만 다시 받기
+        Console.print(`${this.COUNT_BALL}볼 ${this.COUNT_STRIKE}스트라이크`);
+        this.init();
+        this.play();
+      }
+    }
+  }
+
+  validateInput(INPUT_NUM) {
+    if (INPUT_NUM.length === 0) {
       throw '정답을 입력해 주세요!';
     }
-    if (input.length > 3) {
+    if (INPUT_NUM.length > 3) {
       throw '3자리의 수를 입력해 주세요!';
     }
-    for (let i = 0; i < this.inputNum.length; i++) {
-      if (input[i] == input[i + 1]) {
+    for (let i = 0; i < INPUT_NUM.length; i++) {
+      if (INPUT_NUM[i] == INPUT_NUM[i + 1]) {
         throw '서로 다른 수를 입력해 주세요!';
       }
     }
