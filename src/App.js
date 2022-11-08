8a9fe2c17ce4580d9fb4b269eac25247b16b41a9
jinyong <zhaqh146@naver.com>
2022-11-08 21:54:52 +0900

    feat: 사용자가 입력한 3자리 숫자의 오류 확인 함수 구현

--- src/App.js
+++ src/App.js
@@ -6,13 +6,36 @@ const makeNumber = () => {
 };
 
 const userInput = () => {
-  MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (input) => {})
+  MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (input) => {
+    checkNumber(input);
+  })
+};
+
+const checkNumber = (target) => {
+  const isNumber = /[^1-9]/g;
+  const targetSet = new Set(target);
+  
+  if (target === undefined) {
+    throw new Error('숫자를 입력해야 합니다.')
+  };
+
+  if (target.length !== 3) {
+    throw new Error(`3자리 숫자를 입력해야 합니다.`);
+  };
+  
+  if (isNumber.test(target)) {
+    throw new Error('3개의 숫자가 모두 1 ~ 9 사이의 숫자로 이루어져야 합니다.');
+  };
+  
+  if (targetSet.size !== 3) {
+    throw new Error('3개의 숫자를 모두 다르게 입력해야 합니다.');
+  };
 };
 
 class App {
   play() {
     let answer = makeNumber();
-
+    userInput();
   };
 };
 
