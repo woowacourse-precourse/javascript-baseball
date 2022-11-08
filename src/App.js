3b6ccf64ed0d2c48f11b08c588e9ea08fb977608
shlee0820 <72565083+shlee0820@users.noreply.github.com>
2022-11-08 11:29:37 +0900

    feat: App 객체 생성 시, App 클래스가 가진 변수들을 초기화하는 함수 추가 (initGame)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+
 class App {
-  play() {}
+  initGame() {
+    this.userInputNumber = [];
+    this.computerRandomNumber = [];
+    this.strikeScore = 0;
+    this.ballScore = 0;
+    this.compareResultText = '';
+  }
+
+  startGame() {
+    this.initGame();
+  }
+
+  play() {
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+    this.startGame();
+  }
 }
 
 module.exports = App;
