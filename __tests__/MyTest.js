e60ee457f9c6bed121de4e5e473dee471aa1b585
DongjoonShin <ehdwns1516@ajou.ac.kr>
2022-11-08 16:26:55 +0900

    test: 테스트 코드 리팩토링

--- __tests__/MyTest.js
+++ __tests__/MyTest.js
@@ -1,4 +1,5 @@
 const App = require('../src/App');
+const Game = require('../src/Game');
 const MissionUtils = require('@woowacourse/mission-utils');
 
 const mockQuestions = (answers) => {
@@ -17,35 +18,42 @@ const mockRandoms = (numbers) => {
   }, MissionUtils.Random.pickNumberInRange);
 };
 
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(MissionUtils.Console, 'print');
+  logSpy.mockClear();
+  return logSpy;
+};
+
 describe('숫자 야구 게임 My Test', () => {
   test('3자리 숫자 랜덤 숫자 생성', () => {
     const app = new App();
-    const rand_num1 = app.makeRandomNumber(3);
-    const rand_num2 = app.makeRandomNumber(3);
-    let result = rand_num1 === rand_num2;
+    const randNum1 = app.makeRandomNumber(3);
+    const randNum2 = app.makeRandomNumber(3);
+    let result = randNum1 === randNum2;
 
     if (result) {
-      const rand_num3 = app.makeRandomNumber(3);
-      const rand_num4 = app.makeRandomNumber(3);
-      result = rand_num3 === rand_num4;
+      const randNum3 = app.makeRandomNumber(3);
+      const randNum4 = app.makeRandomNumber(3);
+      result = randNum3 === randNum4;
 
-      expect(rand_num3).toHaveLength(3);
-      expect(rand_num4).toHaveLength(3);
+      expect(randNum3).toHaveLength(3);
+      expect(randNum4).toHaveLength(3);
     }
 
-    expect(rand_num1).toHaveLength(3);
-    expect(rand_num2).toHaveLength(3);
+    expect(randNum1).toHaveLength(3);
+    expect(randNum2).toHaveLength(3);
     expect(result).toBeFalsy();
   });
 
   test('상대방의 숫자와 사용자가 입력한 숫자 비교', () => {
-    const app = new App();
     const computer = '369';
-    const user_inputs = ['000', '146', '321', '398', '369'];
+    const game = new Game(computer);
+    const userInputs = ['000', '146', '321', '398', '369'];
     const result = [];
 
-    user_inputs.forEach((input) => {
-      result.push(app.compareNumbers(computer, input));
+    userInputs.forEach((input) => {
+      game.setUserNumber(input);
+      result.push(game.compareNumbers());
     });
 
     expect(result).toEqual([
@@ -58,35 +66,41 @@ describe('숫자 야구 게임 My Test', () => {
   });
 
   test('숫자 비교 결과를 출력', () => {
-    const app = new App();
-    const compare_results = [
+    const game = new Game();
+    const result = [];
+    const logSpy = getLogSpy();
+    const messages = [
+      '낫싱',
+      '1볼',
+      '1스트라이크',
+      '1볼 1스트라이크',
+      '3스트라이크',
+    ];
+    const compareResults = [
       [0, 0],
       [1, 0],
       [0, 1],
       [1, 1],
       [0, 3],
     ];
-    const result = [];
 
-    compare_results.forEach((compare_result) => {
-      result.push(app.printCompareResult(compare_result));
+    compareResults.forEach((compareResult) => {
+      game.setBall(compareResult[0]);
+      game.setStrike(compareResult[1]);
+      result.push(game.printCompareResult());
     });
 
-    expect(result).toEqual([
-      '낫싱',
-      '1볼',
-      '1스트라이크',
-      '1볼 1스트라이크',
-      '3스트라이크',
-    ]);
+    messages.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
   });
 
   test('사용자의 잘못된 입력시 예외 발생 후 애플리케이션 종료', () => {
     const computer = [3, 6, 9];
-    const user_inputs = ['324', '23'];
+    const userInputs = ['324', '23'];
 
     mockRandoms(computer);
-    mockQuestions(user_inputs);
+    mockQuestions(userInputs);
 
     expect(() => {
       const app = new App();
@@ -96,7 +110,7 @@ describe('숫자 야구 게임 My Test', () => {
 
   test('게임을 종료하고 게임 재시작 & 애플리이션 완전 종료', () => {
     const randoms = [3, 6, 9, 5, 8, 9];
-    const answers = ['000', '146', '321', '398', '369', '1', '597', '589', '2'];
+    const answers = ['458', '146', '321', '398', '369', '1', '597', '589', '2'];
     const logSpy = getLogSpy();
     const messages = [
       '낫싱',
