1f3e0af9df487c5b088b772f7ea08e420bb70466
Suhyeon Lee <suhyeonlee.dev@gmail.com>
2022-11-08 16:51:40 +0900

    refactor: Computer 클래스의 함수들을 Game 클래스로 이동

--- src/Game.js
+++ src/Game.js
@@ -1,30 +1,82 @@
-const Computer = require('./Computer');
-const { Console } = require('@woowacourse/mission-utils');
+const { Console, Random } = require('@woowacourse/mission-utils');
 const { ANSWER, OPTION } = require('./constants/constants');
 
 class Game {
-  constructor() {
-    this.computer = new Computer();
+  makeAnswer() {
+    const randomNumList = [];
+    while (randomNumList.length < ANSWER.LENGTH) {
+      const number = Random.pickNumberInRange(ANSWER.MIN, ANSWER.MAX);
+      if (!randomNumList.includes(number)) {
+        randomNumList.push(number);
+      }
+    }
+    return randomNumList.join('');
   }
 
-  start() {
-    const answer = this.computer.makeAnswer();
-    progress(answer);
+  validateInput(userInput) {
+    return (
+      userInput.length === ANSWER.LENGTH &&
+      Boolean(userInput.match(/^[1-9]+$/)) &&
+      new Set(userInput.split('')).size === ANSWER.LENGTH
+    );
   }
 
-  async progress(answer) {
-    while (true) {
-      const userNumber = await this.computer.getUserNumber();
-      const result = this.computer.getResult(answer, userNumber);
-      this.computer.printResult(result);
+  getResult(answer, userNumber) {
+    const userNumberArr = [...userNumber];
+    const result = userNumberArr.reduce(
+      ({ strikeCnt, ballCnt }, curNum, curIndex) => {
+        const index = answer.indexOf(curNum);
+        if (index === -1) return { strikeCnt, ballCnt };
+        else if (index === curIndex)
+          return { strikeCnt: strikeCnt + 1, ballCnt };
+        else return { strikeCnt, ballCnt: ballCnt + 1 };
+      },
+      {
+        strikeCnt: 0,
+        ballCnt: 0,
+      }
+    );
+    return result;
+  }
+
+  printResult(result) {
+    const { strikeCnt, ballCnt } = result;
+    let message = '';
+    if (strikeCnt === 0 && ballCnt === 0) {
+      Console.print('낫싱');
+      return;
+    }
+    if (ballCnt) {
+      message += `${ballCnt}볼 `;
+    }
+    if (strikeCnt) {
+      message += `${strikeCnt}스트라이크`;
+    }
+    Console.print(message);
+  }
+
+  progress(answer) {
+    Console.readLine('숫자를 입력해주세요 : ', (userNumber) => {
+      if (!this.validateInput(userNumber)) {
+        throw new Error('유효하지 않은 값이 입력되어 게임이 종료됩니다.');
+      }
+      const result = this.getResult(answer, userNumber);
+      this.printResult(result);
+
       if (result.strikeCnt === ANSWER.LENGTH) {
         Console.print(
           `${ANSWER.LENGTH}개의 숫자를 모두 맞히셨습니다! 게임 종료`
         );
-        break;
+        this.askRestart();
+      } else {
+        this.progress(answer);
       }
-    }
-    this.askRestart();
+    });
+  }
+
+  start() {
+    const answer = this.makeAnswer();
+    this.progress(answer);
   }
 
   askRestart() {
