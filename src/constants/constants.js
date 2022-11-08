bed09b227484a4e4e51da2e8bf99ff3f364b4345
Y2IN <dpdls0228@daum.net>
2022-11-08 22:19:38 +0900

    refactor : 문구 수정

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,18 +1,16 @@
 const GAME_MESSAGE = {
-  START_MESSAGE: "숫자 야구 게임을 시작합니다.",
-  ENTER_NUMBER: "숫자를 입력해주세요 : ",
-  FINISH_MESSAGE: "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
-  GAME_RESTART: "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n",
-  GAME_END: "게임 종료",
+  GAME_START: "숫자 야구 게임을 시작합니다.",
+  GAME_INPUT_NUMBERS: "숫자를 입력해주세요 : ",
+  GAME_CLEAR: "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
+  GAME_RESTART: "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n"
 };
 
 const ERROR_MESSAGE = {
-  ERROR_RESTART_MESSAGE: "잘못된 값을 입력하셨습니다. 1 또는 2를 입력해주세요.",
-  ERROR_USER_INPUT:
-    "1부터 9까지 서로 다른 수로 이루어진 3자리 숫자를 입력해주세요.",
+  ERROR_USERINPUT_RESTART: "잘못된 값을 입력하셨습니다. 1또는 2를 입력해주세요.",
+  ERROR_USERINPUT_NOT_VALID: "세 자리 수를 1부터 9까지 중복되지 않도록 입력해주세요!",
 };
 
 module.exports = {
   GAME_MESSAGE,
   ERROR_MESSAGE,
-};
+}
