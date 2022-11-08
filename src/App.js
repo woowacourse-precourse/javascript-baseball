19c0a0e3ffe48bb6fd71a912318379184e2233fb
Sanggil Han <tkdrlf95@naver.com>
2022-11-08 15:04:04 +0900

    feat: Implement random selection of 3 numbers
    랜덤으로 숫자 3개를 겹치지 않게 고르는 기능을 구현하였음
    문제에서 제공하는 MissionUtils 모듈을 사용하였고 해당 모듈의 Random 피쳐의 pickNumberInRange 함수를 사용하였음

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+// FIXME: 비표준 모듈 시스템을 사용하고 있음
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  play() {
+    this.initializeNumber();
+  }
+
+  baseballNumber = [];
+  initializeNumber() {
+    this.baseballNumber = [];
+    while (this.baseballNumber.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      this.pushNumberIntoBaseballNumber(number);
+    }
+  }
+
+  pushNumberIntoBaseballNumber(number) {
+    if (!this.baseballNumber.includes(number)) {
+      this.baseballNumber.push(number);
+    }
+  }
 }
 
 module.exports = App;
