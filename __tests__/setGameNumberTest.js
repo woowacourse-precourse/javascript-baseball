4f8115ac8e2f5a7def4b3ab6b763f2cf0735e2bb
hahagarden <88613455+hahagarden@users.noreply.github.com>
2022-11-08 16:03:26 +0900

    feat: 1-2. 1자리 랜덤 숫자가 이미 존재하는지 판별 함수 및 테스트

--- __tests__/setGameNumberTest.js
+++ __tests__/setGameNumberTest.js
@@ -1,7 +1,16 @@
-const { generateRandomDigit } = require("../src/App");
+const { generateRandomDigit, isExist } = require("../src/App");
 
 describe("1자리 랜덤 숫자 발생", () => {
   test("1자리 랜덤 숫자 발생", () => {
     expect(generateRandomDigit()).toBeTruthy();
+  });
+});
+
+describe("1자리 랜덤 숫자가 이미 존재하는지 판별", () => {
+  test("case1", () => {
+    expect(isExist([1, 2, 3], 1)).toBe(true);
+  });
+  test("case2", () => {
+    expect(isExist([1, 2, 3], 4)).toBe(false);
   });
 });
--- src/App.js
+++ src/App.js
@@ -4,9 +4,14 @@ function generateRandomDigit() {
   return MissionUtils.Random.pickNumberInRange(1, 9);
 }
 
+function isExist(gameNumber, digit) {
+  if (gameNumber.includes(digit)) return true;
+  return false;
+}
+
 class App {
   play() {}
 }
 
 module.exports = App;
-module.exports = { generateRandomDigit };
+module.exports = { generateRandomDigit, isExist };
