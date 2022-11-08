14ae8ebfdf26ebb04054920f6529786086d6510e
myeongheonhong <ruddyh7076@gmail.com>
2022-11-08 18:27:16 +0900

    [ Feat ] : 숫자야구게임 관련 기능함수 구현 완료.
    - quit() : 게임 종료함수.
    - getComputerNum : this.computer변수에 중복되지 않는 3개의 숫자를 무작위로 넣는함수.
    - checkResult : 인덱스와 숫자가 동시에 일치할 때 스트라이크 증가, 숫자가 포함만 할 경우 볼 증가, 두 상황모두 없을 경우 out을 true로 변경.
    - printEndMessage : strike가 3일 경우 게임종료 메세지와 다시하기를 선택할 수 있는 메세지 출력과 입력을 대기.(1번의 경우 재시작 2번의 경우 종료)
    - printResult : 증가된 볼,스트라이크,아웃의 결과를 메세지로 출력
    - resetCount : 재입력을 위해 볼,스트라이크,아웃의 값을 초기화
    - resetAll : 재시작을 위한 모든 멤버변수 초기화

--- src/App.js
+++ src/App.js
@@ -16,9 +16,21 @@ class App {
         this.tryCheckingAnswer();
     }
 
-    //게임의 시작메세지를 출력하는 함수.
-    printStartMessage() {
-        MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+    //숫자야구게임 종료함수.
+    quit() {
+        MissionUtils.Console.close();
+    }
+
+    // 컴퓨터가 랜덤으로 숫자를 생성하는 함수.
+    getComputerNum() {
+        const computer = [];
+        while (computer.length < 3) {
+            const number = MissionUtils.Random.pickNumberInRange(1, 9);
+            if (!computer.includes(number)) {
+                computer.push(number);
+            }
+        }
+        return computer;
     }
 
     //사용자의 숫자 입력을 받는 함수.
@@ -26,7 +38,81 @@ class App {
         MissionUtils.Console.readLine('숫자 3개를 입력하세요 : ', (answer) => {
             this.checkException(answer);
             this.user = answer.split('');
+            this.checkResult();
+            if (this.gameOver) this.printEndMessage();
+            this.printResult();
+            this.resetCount();
+            this.tryCheckingAnswer();
         });
+    }
+
+    //게임의 시작메세지를 출력하는 함수.
+    printStartMessage() {
+        MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+    }
+
+    //strike,ball,out의 결과를 출력하는 함수.
+    printResult() {
+        if (this.gameOver) {
+            MissionUtils.Console.print('3스트라이크');
+            MissionUtils.Console.print(
+                '3개의 숫자를 모두 맞히셨습니다! 게임 종료'
+            );
+        } else {
+            this.out
+                ? MissionUtils.Console.print('낫싱')
+                : MissionUtils.Console.print(
+                      this.ball + '볼 ' + this.strike + '스트라이크'
+                  );
+        }
+    }
+
+    printEndMessage() {
+        MissionUtils.Console.print(
+            '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.'
+        );
+        MissionUtils.Console.readLine('', (answer) => {
+            let isNumber = /^[1-2]+$/;
+            if (!isNumber.test(answer)) {
+                MissionUtils.Console.print('1번과 2번만 눌러주세요!');
+            } else {
+                if (Number(answer) === 1) {
+                    this.resetAll();
+                    this.play();
+                } else {
+                    this.quit();
+                }
+            }
+        });
+    }
+
+    resetCount() {
+        [this.ball, this.strike, this.out] = [0, 0, false];
+    }
+    resetAll() {
+        this.computer = this.getComputerNum();
+        this.user = [];
+        this.gameOver = false;
+        this.ball = 0;
+        this.strike = 0;
+        this.out = false;
+    }
+
+    //BALL,STRIKE,OUT을 체크하는 함수.
+    checkResult() {
+        for (let i = 0; i < this.user.length; i++) {
+            if (Number(this.user[i]) === Number(this.computer[i])) {
+                this.strike++;
+            } else if (this.computer.includes(Number(this.user[i]))) {
+                this.ball++;
+            }
+        }
+
+        if (this.strike === 3) this.gameOver = true;
+
+        this.strike === 0 && this.ball === 0
+            ? (this.out = true)
+            : (this.out = false);
     }
 
     checkException(answer) {
