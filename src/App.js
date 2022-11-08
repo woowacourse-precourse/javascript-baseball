c449e6229e118572ec1e07a704c2910c255f548f
minh0518 <rubics@naver.com>
2022-11-08 19:25:51 +0900

    feat : 숫자 입력 구현

--- src/App.js
+++ src/App.js
@@ -2,19 +2,11 @@ const MissionUtils = require('@woowacourse/mission-utils')
 
 class App {
   play() {
-
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다')
 
-    
-
     MissionUtils.Console.readLine('숫자를 입력해 주세요', (num) => {
-      MissionUtils.Console.print(num.split('').map(Number))
-      
-      MissionUtils.Console.print(MissionUtils.Random.pickNumberInRange(1,9,3))
-      MissionUtils.Console.print(MissionUtils.Random.pickNumberInRange(1,9,3))
-      MissionUtils.Console.print(MissionUtils.Random.pickNumberInRange(1,9,3))
-      
-      
+      const playerNum = num.split('').map(Number) //내가 입력
+      const comNum = MissionUtils.Random.pickNumberInRange(1, 9, 3) //컴퓨터가 입력
     })
   }
 }
