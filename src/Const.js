5c3a0d26e6e51c6d4eb326f91b087dcf885007db
Jinu <jinu200131@gmail.com>
2022-11-08 16:54:37 +0900

    fix (Const) : 객체화 및 모듈 export 추가

--- src/Const.js
+++ src/Const.js
@@ -1,17 +1,29 @@
-const NUMBER_MAX_LENGTH = 3;
-const NUMBER_RESTART_TRUE = 1;
-const NUMBER_RESTART_FALSE = 2;
+const NUMBER = {
+    MAX_LENGTH : 3,
+    RANGE_LEFT : 1,
+    RANGE_RIGHT : 9,
+};
 
-const STRING_ZERO = '0';
-const STRING_EMPTY = '';
+const STRING = {
+    RESTART_TRUE : "1",
+    RESTART_FALSE : "2",
+    ZERO : '0',
+    EMPTY : '',
+};
 
-const RESULT_NOTHING = "낫싱";
-const RESULT_STRIKE = "스트라이크";
-const RESULT_BALL = "볼";
+const RESULT = {
+    NOTHING : "낫싱",
+    STRIKE : "스트라이크",
+    BALL : "볼",
+};
 
-const ALERT_START = "숫자 야구 게임을 시작합니다.";
-const ALERT_INPUT_NUMBER = "숫자를 입력해주세요 : ";
-const ALERT_INVALIED_INPUT = "올바른 입력이 아닙니다. 프로그램을 종료합니다.";
-const ALERT_CORRECT_ANSWER = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
-const ALERT_EXIT_GAME = "게임을 종료합니다.";
-const ALERT_RESTART_GAME = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+const ALERT = {
+    START : "숫자 야구 게임을 시작합니다.",
+    INPUT_NUMBER : "숫자를 입력해주세요 : ",
+    INVALID_INPUT : "올바른 입력이 아닙니다. 프로그램을 종료합니다.",
+    CORRECT_ANSWER : "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
+    EXIT_GAME : "게임을 종료합니다.",
+    RESTART_GAME : "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
+};
+
+module.exports = { NUMBER, STRING, RESULT, ALERT };
