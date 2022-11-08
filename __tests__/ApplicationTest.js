9a71a56b8955f39b26296f0aa0b2bed5b2e26a35
kimseo-0 <42965120+kimseo-0@users.noreply.github.com>
2022-11-08 23:22:32 +0900

    test: 사용자 커맨드 입력값 예외처리 테스트

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -23,9 +23,9 @@ const getLogSpy = () => {
   return logSpy;
 };
 
-describe("숫자 야구 게임", () => {
+describe("숫자 야구 게임 테스트", () => {
   test("게임 종료 후 재시작", () => {
-    const randoms = [1, 3, 5, 5, 8, 9];
+    const randoms = [[1, 3, 5], [5, 8, 9]];
     const answers = ["246", "135", "1", "597", "589", "2"];
     const logSpy = getLogSpy();
     const messages = [
@@ -89,3 +89,17 @@ describe("2 사용자에게 3자리 수 입력 받기 예외 처리 테스트", 
     }).toThrow();
   });
 })
+describe("6 게임 성공시 사용자 커맨드 받기 예외 처리 테스트", () => {
+  test("잘못된 입력값", () => {
+    const randoms = [[1, 3, 5], [5, 8, 9]];
+    const answers = ["246", "135", "3"];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow();
+  });
+});
