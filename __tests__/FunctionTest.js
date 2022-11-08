2f4a3de4b313bd004926e521a75f6ab240c51f50
김세환 <sehwan@gimsehwan-ui-MacBookPro.local>
2022-11-08 13:49:17 +0900

    feat(FunctionTest): resultMessage 함수 테스트 기능 구현

--- __tests__/FunctionTest.js
+++ __tests__/FunctionTest.js
@@ -18,5 +18,21 @@ describe("Game.js 기능 테스트", () => {
 
         expect(test_result).toStrictEqual([1, 2]);
     });
+
+    test("resultMessage 결과값 테스트", () => {
+        const strike = 1;
+        const ball = 2;
+        const test_result = game.resultMessage(strike, ball);
+
+        expect(test_result).toStrictEqual(Console.print("2볼 1스트라이크"));
+    });
+
+    test("resultMessage 결과값 테스트", () => {
+        const strike = 0;
+        const ball = 0;
+        const test_result = game.resultMessage(strike, ball);
+
+        expect(test_result).toStrictEqual(Console.print("낫싱"));
+    });
     
 })
