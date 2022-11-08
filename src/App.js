6c17069702b1ab8a1af3020bb91addf2759306e1
KPGY <yuchw8@korea.ac.kr>
2022-11-08 23:09:17 +0900

    정답을 맞췄을때 게임의 재시작여부를 묻고 재시작시 입력값 예외처리를 포함한 함수

--- src/App.js
+++ src/App.js
@@ -52,4 +52,18 @@ checkresult(strike, ball) {
     MissionUtils.Console.print(`${ball}볼 ${strike}스트라이크`);
   }
 }
+
+restartgame() {
+  MissionUtils.Console.print("3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+  MissionUtils.Console.readLine("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.", (answer) => {
+    if (answer === "1") {
+        this.play();
+    } else if (answer === "2") {
+        MissionUtils.Console.close();
+    } else {
+        throw new Error('입력값이 틀렸습니다');
+    }
+  })
+}
+}
 module.exports = App;
