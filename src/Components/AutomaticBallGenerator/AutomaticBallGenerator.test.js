91cb02ba6ff09cb705b78e82e88892017b300f14
RyuJoonSik <tldtksahtxja@naver.com>
2022-11-08 20:42:24 +0900

    test: AutomaticBallGenerator.test.js 컨텍스트 제목 수정

--- src/Components/AutomaticBallGenerator/AutomaticBallGenerator.test.js
+++ src/Components/AutomaticBallGenerator/AutomaticBallGenerator.test.js
@@ -1,6 +1,6 @@
 const AutomaticBallGenerator = require("./AutomaticBallGenerator");
 
-describe("Ball", () => {
+describe("AutomaticBallGenerator", () => {
   test("execute로 3개의 숫자를 생성", () => {
     const BALL_GENERATOR = new AutomaticBallGenerator();
     const NUMBER_ARRAY = BALL_GENERATOR.execute();
