942ab4c53908f55e3adfdb37819be26cb9b6bf30
JaeyeonHong <39540473+HongJaeyeon@users.noreply.github.com>
2022-11-08 16:21:12 +0900

    test: testCase 추가

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -47,9 +47,9 @@ describe("숫자 야구 게임", () => {
     });
   });
 
-  test("예외 테스트", () => {
-    const randoms = [1, 3, 5];
-    const answers = ["1234"];
+  test("중복된 숫자 3개가 입력으로 들어온 경우", () => {
+    const randoms = [4, 5, 6];
+    const answers = ["133"];
 
     mockRandoms(randoms);
     mockQuestions(answers);
@@ -58,5 +58,88 @@ describe("숫자 야구 게임", () => {
       const app = new App();
       app.play();
     }).toThrow();
+  })
+
+  test("3개가 넘는 숫자가 입력으로 들어온 경우", () => {
+    const randoms = [4, 5, 6];
+    const answers = ["1753"];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow();
+  })
+
+  test("3개가 넘는 중복된 숫자가 입력으로 들어온 경우", () => {
+    const randoms = [4, 5, 6];
+    const answers = ["17531753"];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow();
+  })
+
+  test("3볼, 3스트라이크 출력 후 게임 종료 되는 경우", () => {
+    const randoms = [4, 5, 6];
+    const answers = ["564", "456", "2"];
+    const logSpy = getLogSpy();
+    const messages = [
+      "3볼",
+      "3스트라이크",
+      "게임 종료"
+    ];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    const app = new App();
+    app.play();
+
+    messages.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
   });
+
+  test("3스트라이크 출력 후 게임 다시 시작, 1스트라이크 1볼, 낫싱, 3스트라이크, 게임 종료되는 경우", () => {
+    const randoms = [7, 2, 4, 5, 6, 8];
+    const answers = ["724", "1", "589", "214", "568", "2"];
+    const logSpy = getLogSpy();
+    const messages = [
+      "3스트라이크",
+      "1볼 1스트라이크",
+      "낫싱",
+      "3스트라이크",
+      "게임 종료"
+    ];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    const app = new App();
+    app.play();
+
+    messages.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
+  test("게임 종료 후 재시작과 종료 입력 시 예외처리", () => {
+    const randoms = [4, 5, 6];
+    const answers = ["564", "456", "7"];
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow();
+  });
+
 });
