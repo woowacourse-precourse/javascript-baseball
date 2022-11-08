5746b02a4112ca57e343d75d44c83db44abf8fb6
Buyoung Chung <sunarol@naver.com>
2022-11-08 17:10:38 +0900

    test: Add exception test error message

--- __tests__/MyTest.js
+++ __tests__/MyTest.js
@@ -43,7 +43,7 @@ describe("기능 테스트", () => {
     expect(logSpy).toHaveBeenCalledWith(expect.stringContaining("숫자 야구 게임을 시작합니다."));
   });
 
-  test("컴퓨터 난수 배열 테스트", () => {
+  test("컴퓨터 난수 배열 중복 확인 테스트", () => {
     baseballGame.computer.makeRandomNumberArray();
     const testArray = baseballGame.computer.computerRandomNumberArray;
 
@@ -77,6 +77,6 @@ describe("기능 테스트", () => {
     expect(() => {
       const app = new App();
       app.play();
-    }).toThrow();
+    }).toThrow("중복이 없는 숫자를 입력해주세요.");
   });
 });
