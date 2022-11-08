33c872fb54a18ed6c61536b041901f6979c9db1f
Kim jinwoo <49385170+Sunny-jinn@users.noreply.github.com>
2022-11-08 23:12:50 +0900

    test(ApplicationTest): test new case

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -47,6 +47,32 @@ describe("숫자 야구 게임", () => {
     });
   });
 
+  test("게임 종료 후 재시작2", () => {
+    const randoms = [9, 7, 5, 3, 1, 4];
+    const answers = ["123", "957", "975", "1", "319", "345", "714", "314", "2"];
+    const logSpy = getLogSpy();
+    const messages = [
+      "낫싱",
+      "2볼 1스트라이크",
+      "3스트라이크",
+      "2스트라이크",
+      "1볼 1스트라이크",
+      "2스트라이크",
+      "3스트라이크",
+      "게임 종료"
+    ];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    const app = new App();
+    app.play();
+
+    messages.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
   test("예외 테스트", () => {
     const randoms = [1, 3, 5];
     const answers = ["1234"];
