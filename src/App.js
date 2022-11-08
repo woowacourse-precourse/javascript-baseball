6dfb3908771faf8b350283d3e26028e9d76bbd4d
Doyoung.Kim <dyflower@nate.com>
2022-11-08 16:26:25 +0900

    feat : 사용자의 입력값 받는 기능

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ const MissionUtils = require('@woowacourse/mission-utils');
 class App {
   play() {
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
-    let Answer = createAnswer();
-    inputNumber(Answer);
+    let answer = createAnswer();
+    inputNumber(answer);
   }
 }
 
@@ -19,18 +19,23 @@ function createAnswer() {
   return answer;
 }
 
-// function inputNumber() {
-//   MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (inputNum) => {
-//     const validInput = validNumber(inputNum);
-//   });
-// }
+function inputNumber(answer) {
+  MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (inputNum) => {
+    const inputNum = inputNum.split('').map(Number);
+    const validInput = validNumber(inputNum);
+    const answercheck = answerCheck(validInput, answer);
+    MissionUtils.Console.print(answercheck);
+    scoreCheck(answercheck, answer);
+  });
+}
 
 function validNumber(inputNum) {
-  if (inputNum.includes(0)) throw new Error('0을 제외한 숫자를 입력해주세요.');
+  if (inputNum.includes('0')) throw new Error('0을 제외한 숫자를 입력해주세요.');
   if (new Set(inputNum).size !== 3) throw new Error('중복되지 않게 입력해주세요.');
   if (inputNum.map(Number).includes(NaN)) throw new Error('숫자만 입력해주세요.');
   if (inputNum.length !== 3) throw new Error('3자리 숫자를 입력해주세요.');
 
   return inputNum;
 }
+
 module.exports = App;
