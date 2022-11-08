447efc563e6b476ef2c8b98b6363da1b04d89ab3
Sangmin Lee <gink.ss00@gmail.com>
2022-11-08 13:56:13 +0900

    feat: 주요 상수들 설정하기
    게임을 진행하기 위한 상수들을 미리 설정하였다.
    - 게임 진행을 위한 메세지 객체
    - 결과 출력을 위한 힌트 단어 객체
    - 게임 재시작 및 종료를 위한 모드 객체

--- src/App.js
+++ src/App.js
@@ -1,3 +1,22 @@
+const HINT_WORD = {
+  BALL: '볼',
+  STRIKE: '스트라이크',
+  NOTHING: '낫싱',
+};
+
+const MODE_NUMBER = {
+  RESTART: 1,
+  END: 2,
+};
+
+const MESSAGE = {
+  START: '숫자 야구 게임을 시작합니다.',
+  INPUT_NUMBER: '숫자를 입력해주세요 : ',
+  CORRECT: '3개의 숫자를 모두 맞히셨습니다! 게임 종료',
+  ERROR: '잘못된 값이 입력되었습니다. 게임을 종료합니다.',
+  END: '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.',
+};
+
 class App {
   play() {}
 }
