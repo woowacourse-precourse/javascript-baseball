d01fb554a30b05703cac22c2874f38caf64cd19b
semnil5202 <semnil5202@gmail.com>
2022-11-08 20:58:25 +0900

    refactor: undefined를 리턴 값으로 갖는 readLine 콜백 함수 수정
    이전 :
    continueOrEnd 함수의 리턴 값인 undefined를 리턴 값으로 반환한다.
    
    이후 :
    리턴 값을 반환하지 않는다.
    
    readLine의 콜백함수는 반환값이 필요 없기에 위와 같이 수정한다.

--- src/App.js
+++ src/App.js
@@ -61,7 +61,9 @@ class App {
   isContinueGame() {
     MissionUtils.Console.readLine(
       "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. \n",
-      (input) => this.continueOrEnd(Number(input))
+      (input) => {
+        this.continueOrEnd(Number(input));
+      }
     );
   }
 
