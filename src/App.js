1acbc3f251fc00fdbcbc3347beed3d7a4a5788e5
Parksolmi <7548ck@naver.com>
2022-11-08 09:23:35 +0900

    docs: 기능 목록.md 작성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
-class App {
-  play() {}
+
+
+function random_three_numbers () {
+
 }
 
+class App {
+
+  constructor () {
+
+  }
+
+  start_message () {
+    const str = "숫자 야구 게임을 시작합니다.";
+    return str;
+  }
+
+  play() {
+    console.log(start_message());
+
+
+
+  }
+}
+
+const app = new App();
+app.play();
+
 module.exports = App;
+
