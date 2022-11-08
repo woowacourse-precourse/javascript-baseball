69c3a9ead2e4cb71504eea970a49d8aba8638095
devMingu <rooster100@naver.com>
2022-11-08 17:53:26 +0900

    feat: 기능 목록 테스트

--- __tests__/StringTest.js
+++ __tests__/StringTest.js
@@ -1,37 +1,117 @@
-describe("문자열 테스트", () => {
-  test("split 메서드로 주어진 값을 구분", () => {
-    const input = "1,2";
-    const result = input.split(",");
+const App = require("../src/App.js");
+const MissionUtils = require("@woowacourse/mission-utils");
 
-    expect(result).toContain("2", "1");
-    expect(result).toContainEqual("1", "2");
+describe("기능 구현 테스트", () => {
+
+  test("숫자 야구 게임 예외사항이 없을때.", () => {
+
+    const detectError = (user) => {
+      user = removeRepeated(user);
+  
+      if (detectStringError(user)) {
+        if (user.length !== 3) {
+          return -1;
+        }
+      }
+  
+      return 1;
+    }
+  
+    const detectStringError = (user) => {
+      user = [...user];
+      user.forEach((element) => {
+        if (!(element >= "1" && element <= "9")) {
+          return -1;
+        }
+      });
+  
+      return 1;
+    }
+  
+    const removeRepeated = (user) =>{
+      const set = new Set([...user]);
+      user = [...set];
+      return user;
+    }
+
+    const result = detectError('123');
+    expect(result).toEqual(1);
   });
 
-  test("split 메서드로 구분자가 포함되지 않은 경우 값을 그대로 반환", () => {
-    const input = "1";
-    const result = input.split(",");
+  test("숫자 야구 게임 예외사항이 존재할때.", () => {
 
-    expect(result).toContain("1");
+    const detectError = (user) => {
+      user = removeRepeated(user);
+  
+      if (detectStringError(user)) {
+        if (user.length !== 3) {
+          return -1;
+        }
+      }
+  
+      return 1;
+    }
+  
+    const detectStringError = (user) => {
+      user = [...user];
+      user.forEach((element) => {
+        if (!(element >= "1" && element <= "9")) {
+          return -1;
+        }
+      });
+  
+      return 1;
+    }
+  
+    const removeRepeated = (user) =>{
+      const set = new Set([...user]);
+      user = [...set];
+      return user;
+    }
+
+    const result = detectError('1234');
+    expect(result).toEqual(-1);
   });
 
-  test("substring 메서드로 특정 구간 값을 반환", () => {
-    const input = "(1,2)";
-    const result = input.substring(1, 4);
+  test("스트라이크/ 볼 개수 찾아내기", () => {
 
-    expect(result).toEqual("1,2");
+    const getStrikeAndBall = (user, computer) => {
+      user = [...user];
+      computer = [...computer];
+  
+      let idx = 0;
+      let strike = 0;
+      let ball = 0;
+      user.forEach((element) => {
+        if (computer.includes(element) && element === computer[idx]) { 
+          strike += 1 
+        } else if (computer.includes(element) && element !== computer[idx]) { 
+          ball += 1 
+        }
+        idx += 1;
+      });
+  
+      return [strike, ball];
+    }
+
+    const nothing = getStrikeAndBall('123', '456');
+    expect(nothing).toEqual([0,0]);
+    let result = [];
+    // result = getStrikeAndBall('123', '123');
+    // expect(result).toEqual([3,0]);
+
+    // result = getStrikeAndBall('123', '312');
+    // expect(result).toEqual([0,3]);
+    
+    // result = getStrikeAndBall('123', '124');
+    // expect(result).toEqual([2,0]);
+
+    result = getStrikeAndBall('123', '819');
+    expect(result).toEqual([0,1]);
+
   });
 
-  test("repeat 메서드로 문자열을 여러번 반복", () => {
-    const input = "abc";
-    const result = input.repeat(3);
 
-    expect(result).toEqual("abcabcabc");
-  });
 
-  test("repeat 메서드에 음수 값을 넣었을 때 예외 발생", () => {
-    const input = "abc";
-    const result = () => input.repeat(-1);
 
-    expect(result).toThrow(RangeError);
-  });
 });
