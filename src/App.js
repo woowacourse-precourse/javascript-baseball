698e48452de52ecd46664607b872638cfdd60cd9
hahagarden <88613455+hahagarden@users.noreply.github.com>
2022-11-08 23:57:02 +0900

    feat: 3. 1번과 2번을 실행하여 플레이 메소드

--- src/App.js
+++ src/App.js
@@ -103,8 +103,15 @@ class App {
     });
   }
 
-  play() {}
+  play() {
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    const gameNumber = this.setGameNumber();
+    this.guessGameNumber(gameNumber);
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = {
   generateRandomDigit,
