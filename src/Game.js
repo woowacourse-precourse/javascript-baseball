b28c0637b516e60f6b000dce412f4df7f96bb010
osdoonhyun <skw1996@naver.com>
2022-11-08 21:01:41 +0900

    Refactor: Game에 있던 사용자 입력한 숫자 결과 갯수 세는 ballCount 관련 메서드 Referee.js로 이동

--- src/Game.js
+++ src/Game.js
@@ -1,67 +1,25 @@
-class Game {
-  static MESSAGE = {
-    START: "숫자 야구 게임을 시작합니다.",
-    INPUT: "숫자를 입력해주세요 : ",
-    END: "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
-    SELECT: "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
-    ERROR: "입력 형식이 잘못되었습니다. 서로 다른 3개의 숫자를 입력하세요.",
-    OUT: "3스트라이크",
-  };
+const MESSAGE = {
+  START: "숫자 야구 게임을 시작합니다.",
+  INPUT: "숫자를 입력해주세요 : ",
+  END: "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
+  SELECT: "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n",
+  ERROR: "입력 형식이 잘못되었습니다. 서로 다른 3개의 숫자를 입력하세요.",
+  END_ERROR: "입력 형식이 잘못되었습니다.",
+  OUT: "3스트라이크",
+};
 
-  static BALLCOUNT_HINT = {
-    STRIKE: "스트라이크",
-    BALL: "볼",
-    NOTHING: "낫싱",
-  };
+const BALLCOUNT_HINT = {
+  STRIKE: "스트라이크",
+  BALL: "볼",
+  NOTHING: "낫싱",
+};
 
-  static NUMBER = {
-    START: 1,
-    END: 9,
-    LENGTH: 3,
-    RESTART: 1,
-    EXIT: 2,
-    FAIL: 0,
-  };
+const NUMBER = {
+  START: 1,
+  END: 9,
+  LENGTH: 3,
+  RESTART: 1,
+  EXIT: 2,
+};
 
-  countBall(userNum, computerNum, length) {
-    let count = 0;
-    for (let i = 0; i < length; i++) {
-      if (userNum[i] === computerNum[i]) count++;
-    }
-
-    if (count > 0) return count;
-  }
-  countStrike(userNum, computerNum) {
-    let count = 0;
-    userNum.map((number, index) => {
-      if (computerNum[index] !== number && computerNum.includes(number))
-        count++;
-    });
-
-    if (count > 0) return count;
-  }
-
-  ballCount(userNum, computerNum) {
-    let hint = this.BALLCOUNT_HINT.NOTHING;
-    const countBall = this.countBall(userNum, computerNum, NUMBER.LENGTH);
-    const countStrike = this.countStrike(userNum, computerNum);
-
-    if (countBall && countStrike) {
-      hint = `${countBall}${this.BALLCOUNT_HINT.BALL} ${countStrike}${this.BALLCOUNT_HINT.STRIKE}`;
-      return hint;
-    }
-
-    if (countBall) {
-      hint = `${countBall}${this.BALLCOUNT_HINT.BALL}`;
-      return hint;
-    }
-    if (countStrike) {
-      hint = `${countStrike}${this.BALLCOUNT_HINT.BALL}`;
-      return hint;
-    }
-
-    return hint;
-  }
-}
-
-module.exports = Game;
+module.exports = { MESSAGE, BALLCOUNT_HINT, NUMBER };
