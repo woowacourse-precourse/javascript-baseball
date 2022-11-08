e33497bbfd5ed4fa16747d07ab939517f8a540d0
pitang <wpeffect@gmail.com>
2022-11-08 20:07:52 +0900

    feat: 개수에 대한 결과메세지 출력 함수

--- src/App.js
+++ src/App.js
@@ -11,6 +11,13 @@ class App {
     return [...computerNumberSet];
   }
 
+  resultMessage(checkCount) {
+    if (checkCount[0] == 0 && checkCount[1] == 0) return "낫싱";
+    else if (checkCount[0] == 0) return `${checkCount[1]}스트라이크`;
+    else if (checkCount[1] == 0) return `${checkCount[0]}볼`;
+    else return `${checkCount[0]}볼 ${checkCount[1]}스트라이크`;
+  }
+
   checkResult(userNumber, computerNumber) {
     let checkCount = [0, 0];
     
