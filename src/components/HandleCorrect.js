c058f5cc2312c44baa24cfff7ab34fd3849465c9
Inae Choi <inaechoi89@gmail.com>
2022-11-08 15:10:56 +0900

    feat: add functions to print when it is correct

--- src/components/HandleCorrect.js
+++ src/components/HandleCorrect.js
@@ -1,3 +1,13 @@
-const handleCorrectAnswer = () => {};
+const MissionUtils = require('@woowacourse/mission-utils');
+const gameConst = require('./GameConstant');
+
+const printAnswerMessage = () => {
+  MissionUtils.Console.print(gameConst.CORRECT_ANSWER_MESSAGE);
+  MissionUtils.Console.print(gameConst.GAME_END_MESSAGE);
+};
+
+const handleCorrectAnswer = () => {
+  printAnswerMessage();
+};
 
 module.exports = handleCorrectAnswer;
