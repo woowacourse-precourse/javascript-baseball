ae024461c46f4966692d6156b086ed44e22f8a47
leeharyung <lhr4884@naver.com>
2022-11-08 22:11:16 +0900

    feat: check user guess input

--- src/utils/checkInput.js
+++ src/utils/checkInput.js
@@ -13,3 +13,13 @@ function checkDuplicate(input) {
   const inputArray = [...input];
   return new Set(inputArray).size === inputArray.length;
 }
+
+function checkGuessInput(input) {
+  if (!checkLength(input, 3)) throw new Error(ERROR.INVALID_LENGTH);
+  if (!checkRange(input, 1, 9)) throw new Error(ERROR.INVALID_RANGE);
+  if (!checkDuplicate(input)) throw new Error(ERROR.INVALID_DUPLICATE);
+}
+
+module.exports = {
+  checkGuessInput,
+};
