341812fcd773c5358e6d68370c1b45fc53bd0c2a
sunub <bsc5672@gmail.com>
2022-11-08 23:05:00 +0900

    test : 비동기 함수를 대상으로 재시작이 가능한가
    미션에서 주어진 방식 대로 비동기 함수를 실행할 경우 테스트케이스를 통과하지 못한다. 그래서 비동기 함수가 가능하게끔 테스트케이스를 추가했다.

---
+++ src/AsyncApp/test/app.test.js
@@ -0,0 +1,46 @@
+const App = require("./src/app.js");
+const MissionUtils = require("@woowacourse/mission-utils");
+
+const mockQuestions = (answers) => {
+  MissionUtils.Console.readLine = jest.fn();
+  answers.reduce((acc, input) => {
+    return acc.mockImplementationOnce((question, callback) => {
+      callback(input);
+    });
+  }, MissionUtils.Console.readLine);
+};
+
+const mockRandoms = (numbers) => {
+  MissionUtils.Random.pickNumberInRange = jest.fn();
+  numbers.reduce((acc, number) => {
+    return acc.mockReturnValueOnce(number);
+  }, MissionUtils.Random.pickNumberInRange);
+};
+
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(MissionUtils.Console, "print");
+  logSpy.mockClear();
+  return logSpy;
+};
+
+describe("숫자 야구 게임", () => {
+  test("게임 종료 후 재시작", async () => {
+    const randoms = [1, 3, 5, 5, 8, 9];
+    const answers = ["246", "135", "1", "597", "589", "2"];
+    const logSpy = getLogSpy();
+    const messages = ["낫싱", "3스트라이크", "3스트라이크", "게임 종료", "1볼 1스트라이크"];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    try {
+      const app = new App();
+      await app.play();
+      messages.forEach((output) => {
+        expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  });
+});
