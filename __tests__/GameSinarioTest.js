78b67626b3cb5a8425212cae0745dc9f9ffc6157
KimDongYoung <ehddud1006@pusan.ac.kr>
2022-11-08 17:03:43 +0900

    test: 사용자의 게임숫자 입력에 대한 유효성 검증

--- __tests__/GameSinarioTest.js
+++ __tests__/GameSinarioTest.js
@@ -1,9 +1,9 @@
-const App = require("../src/App");
-const MissionUtils = require("@woowacourse/mission-utils");
-const generateRandomNumberArray = require("../src/utils/game/generateRandomNumber");
+const App = require('../src/App');
+const MissionUtils = require('@woowacourse/mission-utils');
+const generateRandomNumberArray = require('../src/utils/game/generateRandomNumber');
 
 const getLogSpy = () => {
-  const logSpy = jest.spyOn(MissionUtils.Console, "print");
+  const logSpy = jest.spyOn(MissionUtils.Console, 'print');
   logSpy.mockClear();
   return logSpy;
 };
@@ -17,15 +17,22 @@ const mockQuestions = (answers) => {
   }, MissionUtils.Console.readLine);
 };
 
-describe("숫자 야구 게임 시나리오", () => {
-  test("게임 시작 시 게임시작 문구 출력", () => {
+const mockRandoms = (numbers) => {
+  MissionUtils.Random.pickNumberInRange = jest.fn();
+  numbers.reduce((acc, number) => {
+    return acc.mockReturnValueOnce(number);
+  }, MissionUtils.Random.pickNumberInRange);
+};
+
+describe('숫자 야구 게임 시나리오', () => {
+  test('게임 시작 시 게임시작 문구 출력', () => {
     const logSpy = getLogSpy();
     const app = new App();
     app.play();
-    expect(logSpy).toHaveBeenCalledWith("숫자 야구 게임을 시작합니다.");
+    expect(logSpy).toHaveBeenCalledWith('숫자 야구 게임을 시작합니다.');
   });
 
-  test("컴퓨터가 랜덤 숫자를 생성", () => {
+  test('컴퓨터가 랜덤 숫자를 생성', () => {
     for (let i = 0; i < 100; i += 1) {
       const randomNumberArray = generateRandomNumberArray();
       expect(randomNumberArray.length).toBe(3);
@@ -37,12 +44,26 @@ describe("숫자 야구 게임 시나리오", () => {
     }
   });
 
-  test("사용자로 부터 1 부터 9 까지 서로 다른 세자리 수를 입력을 받는다.", () => {
-    const userInput = ["123"];
+  test('사용자로 부터 1 부터 9 까지 서로 다른 세자리 수를 입력을 받는다.', () => {
+    const userInput = ['123'];
     mockQuestions(userInput);
 
     const app = new App();
     app.play();
-    expect(app.numberEnteredByUser()).toBe("123");
+    expect(app.numberEnteredByUser()).toBe('123');
+  });
+
+  const pickedNumberArrayByComputer = [1, 2, 4];
+  const wrongNumberEnteredByUser = [['1234'], ['333'], ['102'], ['3.14'], ['-5'], ['😇']];
+  wrongNumberEnteredByUser.forEach((errorInput, index) => {
+    test(`예외 테스트${index + 1}`, () => {
+      mockRandoms(pickedNumberArrayByComputer);
+      mockQuestions(errorInput);
+
+      expect(() => {
+        const app = new App();
+        app.play();
+      }).toThrow();
+    });
   });
 });
--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,10 @@
-const { Console } = require("@woowacourse/mission-utils");
-const generateRandomNumberArray = require("./utils/game/generateRandomNumber.js");
+const { Console } = require('@woowacourse/mission-utils');
+const { isValidNumbers } = require('./utils/validate/validate.js');
+const generateRandomNumberArray = require('./utils/game/generateRandomNumber.js');
 
 class Game {
   constructor() {
-    Console.print("숫자 야구 게임을 시작합니다.");
+    Console.print('숫자 야구 게임을 시작합니다.');
     this.generateNumberArrayByComputer();
   }
 
@@ -12,11 +13,12 @@ class Game {
   }
 
   start() {
-    Console.readLine("숫자를 입력해주세요 : ", this.playTurn);
+    Console.readLine('숫자를 입력해주세요 : ', this.playTurn);
   }
 
   playTurn = (numberEnteredByUser) => {
     this.numberEnteredByUser = numberEnteredByUser;
+    isValidNumbers(this.numberEnteredByUser);
   };
 }
 
