02c7d1995ad80d244801876d14eb85d855d171a6
Saans <92422813+saans808@users.noreply.github.com>
2022-11-08 14:15:04 +0900

    feat: 서로 다른 세 자리의 랜덤 숫자 생성

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ const Random = MissionUtils.Random;
 class App {
   play() {
     Console.print('숫자 야구 게임을 시작합니다.');
+        
+    function randomNum() {
+      let randomNum = Random.pickUniqueNumbersInRange(1, 10, 3);
+    }
+
   }
 }
 
