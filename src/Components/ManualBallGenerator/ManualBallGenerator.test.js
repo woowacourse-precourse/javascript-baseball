372efa7b38439f8fb94d4a5450b06ce465b23708
RyuJoonSik <tldtksahtxja@naver.com>
2022-11-08 18:37:35 +0900

    test: ManualBallGenerator.test.js에 입력이 숫자가 아니면 throw로 예외 발생

--- src/Components/ManualBallGenerator/ManualBallGenerator.test.js
+++ src/Components/ManualBallGenerator/ManualBallGenerator.test.js
@@ -1,10 +1,23 @@
 const ManualBallGenerator = require("./ManualBallGenerator");
+const mockFunction = require("../../mockFunction");
 
 describe("Ball", () => {
-  test("execute로 숫자 생성", () => {
-    const BALL_GENERATOR = new ManualBallGenerator();
-    const NUMBER_ARRAY = BALL_GENERATOR.execute();
+  test("execute로 숫자 생성", async () => {
+    const NUMBER_ARRAY = ["246"];
+    mockFunction.mockQuestions(NUMBER_ARRAY);
 
-    expect(typeof NUMBER_ARRAY[0]).tobe("number");
+    const BALL_GENERATOR = new ManualBallGenerator();
+    const NUMBER = await BALL_GENERATOR.execute();
+
+    expect(typeof NUMBER).toBe("number");
+  });
+
+  test("숫자가 아니면 throw로 예외 발생", async () => {
+    const STRING_ARRAY = ["hello"];
+    mockFunction.mockQuestions(STRING_ARRAY);
+
+    const BALL_GENERATOR = new ManualBallGenerator();
+
+    expect(async () => await BALL_GENERATOR.execute()).toThrow();
   });
 });
