8d5bd172e80c7c0786fb7e6e489fa16bd4375159
GuHyeonWoo <ghw9174@gmail.com>
2022-11-08 20:59:13 +0900

    style (askingRestart): 문구 수정
    before: '숫자를 입력해주세요 :'
    after : '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.'
    
    정해진 문구가 있기 때문에 그에 맞게 수정

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App {
     let answer_of_restart;
 
     Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
-    Console.readLine('숫자를 입력해주세요 : ', (answer) => { try { this.answerException(answer) } catch (e) { this.endGame(e) }; });
+    Console.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n', (answer) => { try { this.answerException(answer) } catch (e) { this.endGame(e) }; });
   }
 
   printResult(result_text) {
