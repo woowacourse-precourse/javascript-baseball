6afaa2bbcaa135bfb0de1608c1f5dae44a8be422
hayeong <fibonacci2246@gmail.com>
2022-11-08 20:57:20 +0900

    feat: getPlayerInput method

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import * as MissionUtils from "@woowacourse/mission-utils";
 
 class App {
-  #answer;
+  #playerInput;
+
   constructor() {
     this.#missionUtils = new MissionUtils();
+    this.#answer = new Set();
   }
 
   #printStartMessage() {
-    this.#missionUtils.print("숫자 야구 게임을 시작합니다.");
+    this.#missionUtils.Console.print("숫자 야구 게임을 시작합니다.");
   }
 
   #makeAnswer() {
-    const firstNumber = this.#missionUtils.getRandomNumber(1, 9);
-    const secondNumber = this.#missionUtils.getRandomNumber(1, 9);
-    const thirdNumber = this.#missionUtils.getRandomNumber(1, 9);
+    const randomNumber = this.#missionUtils.Random.getRandomNumber(1, 9);
+    this.#answer.add(randomNumber);
+    if (this.#answer.size < 3) {
+      this.#makeAnswer();
+    } else {
+      return;
+    }
+  }
 
-    this.#answer = `${firstNumber}${secondNumber}${thirdNumber}`;
+  #validatePlayerInput() {}
+
+  #getPlayerInput() {
+    const playerInput = this.#missionUtils.Console.getInput(
+      "숫자를 입력해주세요: ",
+      this.#validatePlayerInput
+    );
+    this.#playerInput = playerInput;
   }
 
   play() {
     this.#printStartMessage();
     this.#makeAnswer();
+    this.#getPlayerInput();
   }
 }
 
