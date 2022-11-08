a0d871d6d8cdbf9d694dbc048e83707caa7dcbd3
JiYoon Lee <wldbseja@daum.net>
2022-11-08 22:28:20 +0900

    [✔]컴퓨터 랜덤 숫자 생성 기능
    -[1]1부터 9까지의 랜덤 숫자를 생성
    -[2]중복이 있으면 넣지 않기

--- src/App.js
+++ src/App.js
@@ -1,4 +1,17 @@
+const { Random } = require('@woowacourse/mission-utils');
+
 class App {
+  generateRandomNumbers() {
+    this.answerArray = [];
+    while (this.answerArray.length < 3) {
+      const inputString = Random.pickNumberInRange(1, 9);
+
+      if (!this.answerArray.includes(inputString)) {
+        this.answerArray.push(inputString);
+      }
+    }
+  }
+
   play() {}
 }
 
