b12a8777597848abc8917a6ae6bd0c14922d8872
jinseo <ghkdwlstj99@gmail.com>
2022-11-08 17:27:29 +0900

    refactor: CreateNumber 인덴트 수정

--- src/CreateNumber.js
+++ src/CreateNumber.js
@@ -4,13 +4,14 @@ class CreateNumber {
   constructor(){
     this.pickNumber = [];
   }
- pickedComputerNumber() {
+  
+  pickedComputerNumber() {
     while (this.pickNumber.length < 3) {
       let computerNumber = Random.pickNumberInRange(1, 9);
       if (!this.pickNumber.includes(computerNumber)){
         this.pickNumber.push(computerNumber);
+      }
     }
-  }
   }
 }
 
