ed4c2efa36ef95d7593218e678977a01d3882262
wldnd2 <jiwoong000628@gmail.com>
2022-11-08 19:24:05 +0900

    feat: user input 구현

--- src/App.js
+++ src/App.js
@@ -13,6 +13,16 @@ class App {
     Console.print("숫자 야구 게임을 시작합니다.");
     // 기능 1번 구현
     this.computer_random_number = Random.pickUniqueNumbersInRange(1, 9, 3);
+    // 기능 2번 구현
+    this.user_input();
+  }
+  user_input() {
+    Console.readLine("숫자를 입력해주세요 : ", (user_input) => {
+      // 사용자의 입력 체크후 return 받은 값을 이용
+      const user_number = this.check_user_input(user_input);
+      Console.print(user_number);
+      Console.print(this.computer_random_number);
+    });
   }
 }
 
