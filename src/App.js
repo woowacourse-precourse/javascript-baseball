a0d93e78884aa24a1efdc7075b2692f398259f49
cofla <cofla159@naver.com>
2022-11-08 23:23:28 +0900

    feat: startGame함수 구현

--- src/App.js
+++ src/App.js
@@ -5,8 +5,30 @@ class App {
     const answer = MissionUtils.Random.pickNumberInRange(111, 999);
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
     let isGamePlaying = true;
+    while (isGamePlaying) {
+      startGame();
+      isGamePlaying = false;
+    }
   }
 }
+async function startGame() {
+  try {
+    const userNumber = await getUserNumber();
+  } catch (error) {
+    MissionUtils.Console.print(
+      `잘못된 입력입니다. 게임을 종료합니다.: ${error}`
+    );
+    restartOrExit(2);
+  }
+}
+function getUserNumber() {
+  return new Promise((resolve, reject) => {
+    MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (input) => {
+      resolve(input);
+    });
+  });
+}
+
 const app = new App();
 app.play();
 
