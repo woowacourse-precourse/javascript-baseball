957e4bb85dc629569b2998f64c5d6cf3de457479
yuna Kim <yuna000804@gmail.com>
2022-11-08 13:19:32 +0900

    feat : 사용자 입력값 제출

--- src/App.js
+++ src/App.js
@@ -22,11 +22,23 @@ class gameStart {
   start() {
     let computer = computerRandom();
     this.computer = this.handleComputer(computer);
+    Console.print("숫자 야구 게임을 시작합니다.");
+    this.user();
   }
 
+  user() {
+    Console.readLine('숫자를 입력해주세요 : ', (answer) => {
+      let newCountScore = this.countScore(answer);
+      newCountScore;
+    })
+  }
+  
   handleComputer(computer) {
     return (computer.join(''))
   }
+
+  
+
 
 }
 
