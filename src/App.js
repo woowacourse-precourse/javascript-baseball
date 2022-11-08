49c890f0df9e0867f62547cc2c7cc8c6ad0b01a7
letsjo <hyunoh.jo@gmail.com>
2022-11-08 19:13:09 +0900

    refactor(App): 메소드 private 처리 테스트에 필요한 메소드는 제외

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // 클래스 모듈 선언
-const { Console, Random } = require('@woowacourse/mission-utils');
+const { Console } = require('@woowacourse/mission-utils');
 const Computer = require('./Computer');
 // 상수 선언
 const INPUT_LENGTH = 3;
@@ -25,22 +25,13 @@ class App {
     Console.readLine('숫자를 입력해주세요 : ', inputDigit => {
       const userDigit = [...this.isDigitValidation(inputDigit)].map(Number);
       const baseBallBoard = this.#computer.calcBaseBallDigit(userDigit);
-      this.isThreeStrike(baseBallBoard)
+      this.#isThreeStrike(baseBallBoard)
         ? this.getRestartInput()
         : this.#setUserInput();
     });
   }
 
-  getRestartInput() {
-    Console.print('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.');
-    Console.readLine('', isRestart => {
-      if (isRestart === '1') return this.gameStart();
-      else if (isRestart === '2') return this.gameEnd();
-      else throw new Error('잘못된 값 입력됨');
-    });
-  }
-
-  isThreeStrike(baseBallBoard) {
+  #isThreeStrike(baseBallBoard) {
     const { strike, ball } = baseBallBoard;
     if (strike || ball) {
       Console.print(
@@ -48,6 +39,21 @@ class App {
       );
     } else Console.print('낫싱');
     return strike === INPUT_LENGTH;
+  }
+
+  #showStartMessage() {
+    Console.print('숫자 야구 게임을 시작합니다.');
+  }
+
+  #gameStart() {
+    this.#computer = new Computer();
+    this.#setUserInput();
+  }
+
+  #gameEnd() {
+    this.#computer = null;
+    Console.print('게임 종료');
+    Console.close();
   }
 
   isDigitValidation(inputDigit) {
@@ -61,24 +67,19 @@ class App {
       throw new Error('잘못된 값 입력됨');
     return inputDigit;
   }
-
-  showStartMessage() {
-    Console.print('숫자 야구 게임을 시작합니다.');
-  }
-
-  gameStart() {
-    this.#computer = new Computer();
-    this.#setUserInput();
-  }
-
-  gameEnd() {
-    Console.print('게임 종료');
-    Console.close();
+  
+  getRestartInput() {
+    Console.print('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.');
+    Console.readLine('', isRestart => {
+      if (isRestart === '1') return this.#gameStart();
+      else if (isRestart === '2') return this.#gameEnd();
+      else throw new Error('잘못된 값 입력됨');
+    });
   }
 
   play() {
-    this.showStartMessage();
-    this.gameStart();
+    this.#showStartMessage();
+    this.#gameStart();
   }
 }
 
