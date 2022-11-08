9b89793af941f985a2b668ad5a485d6f49ac02bc
lineJeong <line.jeong32@gmail.com>
2022-11-08 18:41:43 +0900

    FEAT: 사용자가 입력 받은 수를 받는 기능 구현

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ class App {
 
   play() {
     Console.print("숫자 야구 게임을 시작합니다.");
+    this.getUserInput();
   }
 
   createRandomNums() {
@@ -16,6 +17,12 @@ class App {
       if (!randomNums.includes(randomNum)) randomNums.push(randomNum);
     }
     return randomNums;
+  }
+
+  getUserInput() {
+    Console.readLine("숫자를 입력해주세요 : ", (input) => {
+      parseInt(input);
+    });
   }
 }
 
