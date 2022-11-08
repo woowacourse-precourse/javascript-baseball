113c54be19b4fa98fdedc37dfd879a037ee1077b
haesa <xdhaesa@gmail.com>
2022-11-08 15:26:44 +0900

    feat(App.js): 볼 구하기

--- src/App.js
+++ src/App.js
@@ -50,6 +50,14 @@ class App {
     );
   }
 
+  setBall(numbers) {
+    this.ball =
+      numbers.reduce(
+        (count, number) => (this.baseballNumber.includes(number) ? count + 1 : count),
+        0
+      ) - this.strike;
+  }
+
   play() {
     Console.print('숫자 야구 게임을 시작합니다.');
     this.pickNumber();
