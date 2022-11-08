bba2d6b6643104800cd1e7ed7982233b8aac9e0d
energy1-914 <dlawldnjs5@daum.net>
2022-11-08 21:16:57 +0900

    fix: - pickUniqueNumbersInRange 메소드 pickNumberInRange 메소드로 변경 - 컴퓨터의 3자리랜덤숫자생성하는 메소드 구현 - 게임 재실행 메소드명 변경(isGameRestart) - countMethod 안의 변수명 변경

--- src/App.js
+++ src/App.js
@@ -2,15 +2,23 @@ const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
   constructor() {
-    this.computerNumber = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
+    this.computerNumberArray = this.computerRandomNumber();
     this.userInputNumber;
     this.countStrike = 0;
     this.countBall = 0;
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
   }
 
+  computerRandomNumber() {
+    const computerNumberSet = new Set();
+    while (computerNumberSet.size < 3) {
+      computerNumberSet.add(MissionUtils.Random.pickNumberInRange(1,9));
+    }
+    const computerNumberArray = [...computerNumberSet];
+    return computerNumberArray;
+  }
+
   play() {
-    //console.log(this.computerNumber);
     MissionUtils.Console.readLine('숫자를 입력해주세요 :', (userInputNumber) => {
       this.userInputNumber = userInputNumber;
       this.isError();
@@ -19,24 +27,24 @@ class App {
       if (this.countStrike < 3) {
         this.countStrike = 0;
         this.countBall = 0;
-        this.play();
+        this.play(); 
       }
       else if (this.countStrike === 3) {
         MissionUtils.Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
-        this.gameRestart();
+        this.isGameRestart();
       }
     });
   }
 
   countMethod() {
     const userNumberArray = this.userInputNumber.split('');  
-    const computerNumberArray = this.computerNumber.map(num=>num.toString());
+    const computerStringArray = this.computerNumberArray.map(num=>num.toString());
     
     for ( let i=0; i<3; i++ ) {
-      if ( userNumberArray[i] === computerNumberArray[i] ) {
+      if ( userNumberArray[i] === computerStringArray[i] ) {
         this.countStrike += 1;
       }
-      else if (userNumberArray.includes(computerNumberArray[i])) {
+      else if (userNumberArray.includes(computerStringArray[i])) {
         this.countBall += 1;
       }
     }
@@ -57,12 +65,12 @@ class App {
     }
  }
 
- gameRestart() {
+ isGameRestart() {
   MissionUtils.Console.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. \n', (userResponse)=>{
     if (userResponse === '1') {
       this.countStrike = 0;
       this.countBall = 0;
-      this.computerNumber = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
+      this.computerNumberArray = this.computerRandomNumber();
       this.play();
     }
     else if (userResponse === '2') {
