719fca94c9b7664ab746ccf988bf868087b9dd3b
saram <yujun1018@naver.com>
2022-11-08 13:03:15 +0900

    test: 1에서 9까지 서로 다른 임의의 수 3개를 만드는 기능 테스트 코드 추가

--- __tests__/MyTest.js
+++ __tests__/MyTest.js
@@ -17,4 +17,24 @@ describe('기능 목록 테스트', () => {
 
     expect(logSpy).toHaveBeenCalledWith(expect.stringMatching(PHRASE.START));
   });
+
+  test('1에서 9까지 서로 다른 임의의 수 3개를 만드는 기능', () => {
+    const app = new App();
+    const numberList = app.answerNumberList;
+
+    app.createNumberList();
+
+    // 세 자리 수
+    expect(numberList.length).toEqual(GAME.NUMBER_COUNT);
+
+    numberList.forEach((number, idx) => {
+      // 숫자
+      expect(typeof number).toEqual('number');
+
+      // 서로 다른 수
+      const newList = [...numberList];
+      newList.splice(idx, 1);
+      expect(newList).toEqual(expect.not.arrayContaining([number]));
+    });
+  });
 });
