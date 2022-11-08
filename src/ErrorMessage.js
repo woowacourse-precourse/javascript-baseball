0736cd01fe6d9800be74268d7957b1636d5fea12
kaori-killer <sksbthwjd@daum.net>
2022-11-08 19:59:59 +0900

    style: 오타 수정

--- src/ErrorMessage.js
+++ src/ErrorMessage.js
@@ -1,5 +1,5 @@
 module.exports = Object.freeze({
-    INCLUDE_ZERO_ERROR: "1에서 9까지의 수만 입력해주세요.",
+    INCLUDE_ZERO_ERROR: "1에서 9까지의 수를 입력해주세요.",
     NOT_A_POSITIVE_NUMBER_ERROR: "양의 정수를 입력해주세요.", 
     OVERLAP_ERROR: "서로 다른 수를 입력해주세요.",
     SIZE_ERROR: "3자리 수를 입력해주세요.",
