85159f247daaa1df4b4518fb682184691788546f
ryuna <anottrx@gmail.com>
2022-11-08 15:45:43 +0900

    refactor: indent 조건에 맞게 리팩터링

--- src/App.js
+++ src/App.js
@@ -1,147 +1,113 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 
-/**
- * 컴퓨터가 숫자 3개를 고른다
- * @returns {number[]} 컴퓨터가 고른 숫자
- */
-const getRandomNumber = () => {
-  const computer = [];
-  while (computer.length < 3) {
-    const number = MissionUtils.Random.pickNumberInRange(1, 9);
-    if (!computer.includes(number)) computer.push(number);
-  }
-  return computer;
-};
-
-/**
- * 사용자가 숫자 3개를 입력한다
- * @returns {number[]} 사용자가 입력한 숫자
- */
-const guessUserNumber = () => {
-  let guessNumber;
-  MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (x) => {
-    guessNumber = x;
-  });
-  MissionUtils.Console.close();
-  return guessNumber.split('');
-};
-
-/**
- * strike 개수를 구한다
- * @param {number[]} computerNumber - 컴퓨터가 고른 숫자
- * @param {string[]} guessNumber - 사용자가 예상한 숫자
- * @returns {number} 스트라이크 개수
- */
-const countStrike = (computerNumber, guessNumber) => {
-  const strike = computerNumber.filter(
-    (num, index) => num.toString() === guessNumber[index],
-  ).length;
-  // let strike = 0;
-  // computerNumber.forEach((num, index) => {
-  // if (num.toString() === guessNumber[index]) strike += 1;
-  // });
-  return strike;
-};
-
-/**
- * ball 개수를 구한다
- * @param {number[]} computerNumber - 컴퓨터가 고른 숫자
- * @param {string[]} guessNumber - 사용자가 예상한 숫자
- * @returns {number} 볼 개수
- */
-const countBall = (computerNumber, guessNumber) => {
-  let ball = 0;
-  computerNumber.forEach((computerNum, index) => {
-    const num = computerNum.toString();
-    if (guessNumber.includes(num) && guessNumber[index] !== num) {
-      ball += 1;
-    }
-  });
-  return ball;
-};
-
-/**
- * ball, strike 개수로 결과를 출력할 수 있게 한다
- * @param {number} strike - 스트라이크 개수
- * @param {number} ball - 볼 개수
- * @returns {string} 출력할 결과
- */
-const getResult = (strike, ball) => {
-  if (strike > 0 && ball > 0) return `${ball}볼 ${strike}스트라이크`;
-  if (strike > 0) return `${strike}스트라이크`;
-  if (ball > 0) return `${ball}볼`;
-  return '낫싱';
-};
-
-/**
- * 게임을 계속할지 그만둘지 선택한다
- * @returns {string} 계속할지 그만둘지 입력한 결과
- */
-const continueOrFinish = () => {
-  let input;
-  MissionUtils.Console.readLine(
-    '3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.',
-    (x) => {
-      input = x;
-    },
-  );
-  MissionUtils.Console.close();
-  return input;
-};
-
-/**
- * 사용자의 입력이 잘못되었는지 판단한다
- * @param {string[]} guessNumber - 사용자가 예상한 숫자
- * @returns {Error} 잘못될 경우 에러
- */
-const inputException = (guessNumber) => {
-  if (guessNumber.length !== 3) {
-    throw new Error('예외');
-  }
-  if (guessNumber.filter((num) => Number.isNaN(num)).length > 0) {
-    throw new Error('예외');
-  }
-};
-
 class App {
+  constructor() {
+    this.isPlaying = true;
+    this.computerNumber = [];
+  }
   // Expected 'this' to be used by class method 'play'.
 
-  playGame() {
-    let computerNumber = getRandomNumber();
-    const IS_PLAYING = true;
+  getRandomNumber() {
+    while (this.computerNumber.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!this.computerNumber.includes(number)) this.computerNumber.push(number);
+    }
+  }
 
-    while (IS_PLAYING) {
-      const guessNumber = guessUserNumber();
-      try {
-        inputException(guessNumber);
+  guessUserNumber() {
+    let guessNumber;
+    MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (x) => {
+      guessNumber = x;
+    });
+    MissionUtils.Console.close();
+    return guessNumber.split('');
+  }
 
-        const strike = countStrike(computerNumber, guessNumber);
-        const ball = countBall(computerNumber, guessNumber);
-        MissionUtils.Console.print(getResult(strike, ball));
+  countStrike(guessNumber) {
+    const strike = this.computerNumber.filter(
+      (num, index) => num.toString() === guessNumber[index],
+    ).length;
+    return strike;
+  }
 
-        if (strike === 3) {
-          const input = continueOrFinish();
+  continueOrFinish() {
+    let input;
+    MissionUtils.Console.readLine(
+      '3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.',
+      (x) => {
+        input = x;
+      },
+    );
+    MissionUtils.Console.close();
+    return input;
+  }
 
-          if (input.toString() === '2') {
-            MissionUtils.Console.print('게임 종료');
-            break;
-          } else {
-            computerNumber = getRandomNumber();
-          }
-        }
-        // inputException(this.guessNumber);
-      } catch (e) {
-        // IS_PLAYING = false;
-        // console.log('여기');
-        // MissionUtils.Console.print(e);
-        // break;
+  countBall(guessNumber) {
+    let ball = 0;
+    this.computerNumber.forEach((computerNum, index) => {
+      const num = computerNum.toString();
+      if (guessNumber.includes(num) && guessNumber[index] !== num) {
+        ball += 1;
       }
+    });
+    return ball;
+  }
+
+  getResult(strike, ball) {
+    if (strike > 0 && ball > 0) return `${ball}볼 ${strike}스트라이크`;
+    if (strike > 0) return `${strike}스트라이크`;
+    if (ball > 0) return `${ball}볼`;
+    return '낫싱';
+  }
+
+  inputException(guessNumber) {
+    if (guessNumber.length !== 3) {
+      throw new Error('예외');
+    }
+    if (guessNumber.filter((num) => Number.isNaN(num)).length > 0) {
+      throw new Error('예외');
+    }
+  }
+
+  chooseContinueFinish() {
+    const input = this.continueOrFinish();
+
+    if (input.toString() === '2') {
+      MissionUtils.Console.print('게임 종료');
+      this.isPlaying = false;
+    } else {
+      this.computerNumber = [];
+      this.getRandomNumber();
+    }
+  }
+
+  playGame() {
+    // const IS_PLAYING = true;
+
+    const guessNumber = this.guessUserNumber();
+    try {
+      this.inputException(guessNumber);
+
+      const strike = this.countStrike(guessNumber);
+      const ball = this.countBall(guessNumber);
+      MissionUtils.Console.print(this.getResult(strike, ball));
+
+      if (strike === 3) {
+        this.chooseContinueFinish();
+      }
+      // inputException(this.guessNumber);
+    } catch (e) {
+      // IS_PLAYING = false;
+      // break;
     }
   }
 
   play() {
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
-    this.playGame();
+    this.getRandomNumber();
+    while (this.isPlaying) {
+      this.playGame();
+    }
   }
 }
 
