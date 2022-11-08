8060aaf6979a5da4b0357541d02a41701b0e4613
rovxxmjxx <rovxxmjxx@gmail.com>
2022-11-08 13:17:47 +0900

    feat: App 클래스 생성 및 변수 초기화

--- src/App.js
+++ src/App.js
@@ -1,4 +1,14 @@
+const { Console } = require("@woowacourse/mission-utils");
+const { MESSAGE, RESULT, USER_CHOICE } = require("./constants");
+const { Input } = require("./utils");
+
 class App {
+  constructor() {
+    this.user = new Input();
+    this.computer = new Input();
+    this.result = {};
+  }
+
   play() {}
 }
 
