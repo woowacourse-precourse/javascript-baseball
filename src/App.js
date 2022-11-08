9742e7702a9c72e11cfbe132bc10d02a68fa2630
Last-SilverLight-End <68593892+Last-SilverLight-End@users.noreply.github.com>
2022-11-08 22:07:18 +0900

    modify throw

--- src/App.js
+++ src/App.js
@@ -4,34 +4,37 @@ class App {
 
   throwError(input) {
 
+    if (input.replace( /[0-9]/g, '' ).length != 0) {
+      throw new Error("숫자만 입력 가능 합니다.")
+    }
 
-    if (input.length !== 3) {
-      throw new Error("3자리 숫자만 입력 가능합니다.")
+    if (input.length != 3) {
+      throw new Error("3자리 숫자로 이루어져야 합니다.")
     }
 
     if (input.includes(0)) {
-      throw new Error("0은 포함될 수 없습니다.")
+      throw new Error("1-9 사이의 숫자를 입력 하셔야 합니다.")
     }
 
-    let inputSet = new Set(input);
-
-    if (inputSet.size < 3) {
-      throw new Error("중복되지 않은 숫자 3개만 입력 가능합니다.")
+    if (input.size < 3) {
+      throw new Error("중복되지 않은 숫자 3자리만 가능합니다.")
     }
 
-    if (input.replace(/[0-9]/g, '').length !== 0) {
-      throw new Error("숫자만 입력 가능합니다.")
-    }
+
+
   }
 
   computerMakeNum() {
     const computer = [];
+
     while (computer.length < 3) {
       const number = MissionUtils.Random.pickNumberInRange(1, 9);
+
       if (!computer.includes(number)) {
         computer.push(number);
       }
     }
+
     return computer.join('');
   }
 
@@ -40,6 +43,7 @@ class App {
     MissionUtils.Console.readLine('게임을 새로 시작하시려면 1, 종료하려면 2를 입력하세요.', (order) => {
 
       if (Number(order) == 1) new App().play();
+
       else if (Number(order) == 2)  MissionUtils.Console.close();
 
       else throw new Error('1 또는 2만 입력하세요.')
