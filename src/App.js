aee50b51f2bfcdc0e5b6cb4d32661c5d32c71f94
cyberjam <jamsince1994@naver.com>
2022-11-08 10:39:12 +0900

    refactor: change the functions to method

--- src/App.js
+++ src/App.js
@@ -2,21 +2,6 @@
 /* eslint-disable operator-linebreak */
 const MissionUtils = require('@woowacourse/mission-utils');
 
-const isSameNumber = (input) => {
-  const numberArray = input.split('').map((number) => parseInt(number, 10));
-  const numberSet = new Set(numberArray);
-  // console.log(`arr ${numberArray.length} set ${numberSet.size}`);
-  return numberArray.length !== numberSet.size;
-};
-
-const isOutRangeNumber = (input) => {
-  const numberArray = input.split('').map((number) => parseInt(number, 10));
-  const checkedNumberArray = numberArray.filter(
-    (number) => number > 0 && number < 10,
-  );
-  return numberArray.length !== checkedNumberArray.length;
-};
-
 class App {
   constructor() {
     this.computerNumberArray = MissionUtils.Random.pickUniqueNumbersInRange(
@@ -24,26 +9,60 @@ class App {
       9,
       3,
     );
+    this.userNumberInput = '';
+    this.userNumberArray = [];
+    this.userNumberSet = new Set();
+  }
+
+  // get userNumberArray() {
+  //   return this.serNumberArray;
+  // }
+
+  // set userNumberArray(array) {
+  //   this.userNumberArray = array;
+  // }
+
+  isSameNumber() {
+    this.userNumberArray = this.userNumberInput
+      .split('')
+      .map((number) => parseInt(number, 10));
+    this.userNumberSet = new Set(this.userNumberArray);
+    // console.log(
+    //   `arr ${this.userNumberArray.length} set ${this.userNumberSet.size}`,
+    // );
+    return this.userNumberArray.length !== this.userNumberSet.size;
+  }
+
+  isOutRangeNumber() {
+    const checkedNumberArray = this.userNumberArray.filter(
+      (number) => number > 0 && number < 10,
+    );
+    return this.userNumberArray.length !== checkedNumberArray.length;
+  }
+
+  checkInvalidInput() {
+    if (
+      !this.userNumberInput ||
+      this.userNumberInput.length !== 3 ||
+      isNaN(this.userNumberInput, 10) ||
+      this.isSameNumber() ||
+      this.isOutRangeNumber()
+    ) {
+      throw new Error('3자리 숫자를 입력하세요');
+    }
   }
 
   play() {
     MissionUtils.Console.print(this.computerNumberArray);
     MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (input) => {
       // console.log(`입력받은 숫자 : ${input}`);
-      if (
-        !input ||
-        input.length !== 3 ||
-        isNaN(input, 10) ||
-        isSameNumber(input) ||
-        isOutRangeNumber(input)
-      ) {
-        throw new Error('3자리 숫자를 입력하세요');
-      }
+      this.userNumberInput = input;
+      this.checkInvalidInput();
     });
   }
 }
 
-const t = new App();
-t.play();
+const app = new App();
+app.play();
 
 module.exports = App;
