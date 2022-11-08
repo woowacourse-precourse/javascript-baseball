682b0ba49a5d5d6707aadbc5a70b0febea840df2
ywoo12321 <ywoo12321@gmail.com>
2022-11-08 18:33:31 +0900

    refactor: restart함수 invalidTest 추가 1 또는 2 이외의 문자 입력시 오류 출력 후 프로그램 종료

--- src/BaseballGame.js
+++ src/BaseballGame.js
@@ -84,9 +84,10 @@ class BaseballGame {
     Console.readLine(
       "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
       (number) => {
+        InvalidTest.isOneOrTwo(number);
         if (Number(number) === 1) {
           this.gameStart();
-        } else {
+        } else if (Number(number) === 2) {
           Console.close();
         }
       }
