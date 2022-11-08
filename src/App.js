4e5a9ca0f7c240f6460eaca9ce113c4559c83799
GuHyeonWoo <ghw9174@gmail.com>
2022-11-08 16:39:15 +0900

    feat (openingOutput): 시작 문구 출력 기능 구현
    '숫자 야구 게임을 시작합니다.'라는 문구를 게임 시작 시 출력
    
    docs/README.md의 구현 할 기능 목록 #1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const Console = MissionUtils.Console;
+
 class App {
-  play() {}
+  openingOutput() {
+    Console.print('숫자 야구 게임을 시작합니다.');
+    Console.close();
+  }
+
+  play() {
+    openingOutput();
+  }
 }
 
 module.exports = App;
