f1f17345baff537090276b5a0257d068574a9b0a
serin-B <bangserin980302@gmail.com>
2022-11-08 23:30:39 +0900

    feat: 이용자가 맞춰야하는 3자리 숫자 생성 (구현목록 2번)

--- src/App.js
+++ src/App.js
@@ -5,5 +5,8 @@ class App {
   play() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
   }
+  pickUniqueNumber() {
+    return MissionUtils.Random.pickUniqueNumbersInRange(0, 9, 3).join("");
+  }
 }
 module.exports = App;
