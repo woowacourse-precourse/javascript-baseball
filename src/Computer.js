dea7d29be271a2ce4b73effaf088dbc7f01a042c
letsjo <hyunoh.jo@gmail.com>
2022-11-08 14:56:19 +0900

    refactor(Computer): Class Computer 추가 및 객체화 작업
    랜덤숫자 생성 및 관리메소드를 따로 객체화 시켜 중요 데이터 캡슐화처리

---
+++ src/Computer.js
@@ -0,0 +1,49 @@
+// 모듈 선언
+const { Random } = require('@woowacourse/mission-utils');
+// 상수 선언
+const INPUT_LENGTH = 3;
+const START_DIGIT = 1;
+const END_DIGIT = 9;
+
+class Computer {
+  #baseBallDigit;
+
+  constructor() {
+    this.#baseBallDigit = this.#setRandomDigit();
+  }
+
+  #setRandomDigit() {
+    const randomDigit = new Set();
+    while (randomDigit.size < INPUT_LENGTH)
+      randomDigit.add(Random.pickNumberInRange(START_DIGIT, END_DIGIT));
+    return Array.from(randomDigit);
+  }
+
+  #isBall({ randomDigit, digit, idx }) {
+    return randomDigit.includes(digit) && randomDigit[idx] !== digit;
+  }
+
+  #isStrike({ randomDigit, digit, idx }) {
+    return randomDigit.includes(digit) && randomDigit[idx] === digit;
+  }
+
+  getRandomDigit() {
+    return this.#baseBallDigit;
+  }
+
+  calcBaseBallDigit(userDigit) {
+    const randomDigit = this.#baseBallDigit;
+    const baseBallBoard = {
+      strike: 0,
+      ball: 0,
+    };
+    userDigit.forEach((digit, idx) => {
+      if (this.#isBall({ randomDigit, digit, idx })) baseBallBoard.ball++;
+      else if (this.#isStrike({ randomDigit, digit, idx }))
+        baseBallBoard.strike++;
+    });
+    return baseBallBoard;
+  }
+}
+
+module.exports = Computer;
