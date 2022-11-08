348382094282a5c436a3ad446890fb9a312172ce
RooT_Kim <bdd3452@gmail.com>
2022-11-08 16:53:33 +0900

    기능: 입력값 유효성 검증 메소드 구현 사용자 입력값이 유효한 값인지 검증하는 메소드 validateAnswer 구현

--- src/App.js
+++ src/App.js
@@ -12,7 +12,11 @@ class App {
 
 	inputAnswer() {}
 
-	validateAnswer() {}
+	validateAnswer(answer) {
+		if (answer.length !== 3) throw new Error('세자리 수가 아닙니다.');
+		if ([...new Set(answer)].length !== 3) throw new Error('같은 숫자를 입력하셨습니다.');
+		if (/[^1-9]/g.test(answer.join(''))) throw new Error('1~9 이외의 숫자 혹은 문자를 입력하셨습니다.');
+	}
 
 	printResult() {}
 
