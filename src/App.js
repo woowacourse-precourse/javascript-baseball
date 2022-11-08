4aa643d2342ee32f61b4a4552d35425e7e61c555
jjjjjeongwon <jjjjjwon21@gmail.com>
2022-11-08 21:13:44 +0900

    feat: [기능 2] 랜덤 수 생성 및 저장

--- src/App.js
+++ src/App.js
@@ -9,6 +9,18 @@ class App {
   printStartGame() {
     Console.print("숫자 야구 게임을 시작합니다.");
   }
+
+  createRandomNumber() {
+    const randomNumberList = [];
+
+    while (randomNumberList.length < 3) {
+      const collectRandomNumber = Random.pickNumberInRange(1, 9);
+      !randomNumberList.includes(collectRandomNumber) &&
+        randomNumberList.push(collectRandomNumber);
+    }
+
+    return randomNumberList;
+  }
 }
 
 const app = new App();
