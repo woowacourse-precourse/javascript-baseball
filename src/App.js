f276a98e5d563ed3ecbb41025e298673a590c0cf
hayoun <fishingest@kookmin.ac.kr>
2022-11-08 20:10:45 +0900

    [FEAT] 스트라이크, 볼 출력, 게임 재시작 구현

--- src/App.js
+++ src/App.js
@@ -14,13 +14,15 @@ class App {
     
   }
   
-    
+  
 
     
   
   inference(){
     MissionUtils.Console.readLine('숫자를 입력해주세요 :', (answer) => {
-    
+      this.ballcount=0;
+      this.strikecount=0;
+      
     try{
     const set=new Set(answer.split(""))
     if(set.size!=3){
@@ -36,18 +38,40 @@ class App {
   }
     
     if(answer===this.randomnum){
+      MissionUtils.Console.print(`0볼 3스트라이크`)
       MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료")
-      MissionUtils.Console.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
-      var answer=0;
-      MissionUtils.Console.readLine('숫자를 입력해주세요 :', answer)
+      // MissionUtils.Console.close();
+      
+      this.restart()
       
     }
     else{
     this.ballcount=0;
     this.strikecount=0;
-    this.setcount(answer)
+    
+    for(var i=0;i<3;i++){
+      if(this.computer[i]==answer[i]){
+        this.strikecount++;
+      }
+      else if(answer.includes(this.computer[i])){
+        this.ballcount++;
+      }
+    }
+    if(this.ballcount>0&&this.strikecount>0){
+      MissionUtils.Console.print(`${this.ballcount}볼 ${this.strikecount}스트라이크`)
+    }
+    else if(this.ballcount>0&&this.strikecount==0){
+      MissionUtils.Console.print(`${this.ballcount}볼`)
+    }
+    else if(this.ballcount==0&&this.strikecount>0){
+      MissionUtils.Console.print(`${this.strikecount}스트라이크`)
+    }
+    else{
+      MissionUtils.Console.print(`낫싱`)
+    }
+    this.inference();
   }
-    MissionUtils.Console.close();
+    
   
     
   })
@@ -61,8 +85,18 @@ class App {
 
     this.inference();
   }
+  restart(){
+    MissionUtils.Console.readLine("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.", (answer) => {
+      if(answer=='1'){
+        this.play();
+      }
+      else{
+        MissionUtils.Console.close();
+      }
+    })
+  }
 
 }
-const app = new App();
-app.play();
-// module.exports = App;
+// const app = new App();
+// app.play();
+module.exports = App;
