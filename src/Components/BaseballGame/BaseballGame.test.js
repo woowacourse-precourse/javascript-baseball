dcbb1084b28a6ac32ba26919e190fa833bc83796
RyuJoonSik <tldtksahtxja@naver.com>
2022-11-08 23:00:27 +0900

    test: BaseballGame.test.js에 통합 테스트 추가

--- src/Components/BaseballGame/BaseballGame.test.js
+++ src/Components/BaseballGame/BaseballGame.test.js
@@ -1,9 +1,31 @@
 const BaseballGame = require("./BaseballGame");
 
-describe("BaseballGame", () => {
-  test("addPlayer 메서드를 가진다.", () => {
-    const baseballGame = new BaseballGame();
+const { getLogSpy, mockRandoms, mockQuestions } = require("../../mockFunction");
 
-    expect(baseballGame).toHaveProperty("addPlayer");
+describe("BaseballGame", () => {
+  test("게임 종료 후 재시작", () => {
+    const randoms = [
+      [1, 3, 5],
+      [5, 8, 9],
+    ];
+    const answers = ["246", "135", "1", "597", "589", "2"];
+    const logSpy = getLogSpy();
+    const messages = [
+      "낫싱",
+      "3스트라이크",
+      "1볼 1스트라이크",
+      "3스트라이크",
+      "게임 종료",
+    ];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    const BASEBALL_GAME = new BaseballGame();
+    BASEBALL_GAME.start();
+
+    messages.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
   });
 });
