faa2950d2932ad118acf2b2339e99bbd954664af
박겸영 <yong_drg@naver.com>
2022-11-08 22:33:18 +0900

    feat: 사용자에게 3자리 수 입력 받고 야구 결과 출력하는 함수
    - 사용자에게 3자리 수를 입력 받는다(Console.readLine).
    - 야구 결과(스트라이크, 볼, 낫싱)반환하는 함수를 호출하여 결과를 출력한다(Console.print).
    - 맞혀야 하는 값(target)과 사용자 입력값(input)이 같을 때까지 위 과정을 반복한다. 같으면 함수 실행을 종료한다.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,23 @@
+const { Console } = require('@woowacourse/mission-utils');
+const Message = require('./assets/Message');
+const { getBaseballResultMessage } = require('./utils/getBaseballResultMessage');
+
+const playEachRound = (target, input) => {
+  const isThreeStrikes = target === input;
+
+  if (isThreeStrikes) {
+    Console.print(Message.getCorrect());
+    return;
+  }
+
+  Console.readLine(Message.INPUT, (answer) => {
+    input = answer;
+
+    Console.print(getBaseballResultMessage(target, input));
+    playEachRound(target, input);
+  });
+};
+
 class App {
   play() {}
 }
