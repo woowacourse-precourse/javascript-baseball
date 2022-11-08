7526345287683f8ecd8d3e77227eac186ae48997
jiyun1006 <jiyun4251@gmail.com>
2022-11-08 18:54:51 +0900

    feat : 숫자 입력 기능 구현
    MissionUtils를 이용해 숫자를 입력 받는 기능 구현.

--- src/App.js
+++ src/App.js
@@ -9,5 +9,10 @@ const startNumberBaseball = () => {
   MissionUtils.Console.print("숫자 야구를 게임을 시작합니다.");
 };
 
+const inputNumber = (answerNumber) => {
+  MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (number) => {
+    
+  });
+};
 
 module.exports = App;
