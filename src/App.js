c566a0fc2c8bf7a77c866f7ebea756efced7c298
jaesukpark77 <jaesukpark77@naver.com>
2022-11-08 10:17:14 +0900

    feat: 컴퓨터가 만든 랜덤 3자리 수 생성

--- src/App.js
+++ src/App.js
@@ -2,9 +2,24 @@ const MissionUtils = require("@woowacourse/mission-utils");
 
 
 class App {
-  play() {
+  constructor() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+  };
+  play() {
+    let randomNumber = [];
+    randomNumber = createRandomNumber();
   }
+}
+
+function createRandomNumber() {
+  let randomNumber = [];
+  let tempNumber;
+
+  while (randomNumber.length < 3) {
+    tempNumber = MissionUtils.Random.pickNumberInRange(1,9);
+    randomNumber.push(tempNumber);
+  }
+  return randomNumber;
 }
 
 module.exports = App;
