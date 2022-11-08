ec298c74a7bb546aee7a78cfaeec76ad0a0df774
seunghoria <102285042+seunghoria@users.noreply.github.com>
2022-11-08 12:26:21 +0900

    test: Referee 클래스 분리에 따른 컴퓨터 랜덤 숫자, 카운트 테스트 코드 수정

--- __tests__/ComputerNumberTest.js
+++ __tests__/ComputerNumberTest.js
@@ -1,16 +1,18 @@
-const getComputerNumber = require("../src/modules/getComputerNumber");
+const Referee = require("../src/Referee");
 const GAME_NUMBER_LENGTH = 3;
 
 describe("컴퓨터 입력값 유효성 검사 테스트", () => {
   test(`길이가 GAME_NUMBER_LENGTH인지 확인`, () => {
-    const computerNumber = getComputerNumber();
+    const referee = new Referee();
+    const computerNumber = referee.getComputerNumber();
     const computerLength = computerNumber.length;
 
     expect(computerLength).toBe(GAME_NUMBER_LENGTH);
   });
 
   test(`중복 숫자가 있는지 확인`, () => {
-    const computerNumber = getComputerNumber();
+    const referee = new Referee();
+    const computerNumber = referee.getComputerNumber();
     const criteriaComputer = new Set(computerNumber);
 
     expect(computerNumber.length).toBe(criteriaComputer.size);
--- __tests__/CountInputTest.js
+++ __tests__/CountInputTest.js
@@ -1,32 +1,35 @@
-const countInput = require("../src/modules/countInput");
+const Referee = require("../src/Referee");
 
 describe("스트라이크와 볼 갯수 카운트 기능 테스트", () => {
   test(`countInput 볼 카운트 확인`, () => {
+    const referee = new Referee();
     const computerInput = [7, 8, 9];
     const userInput = [9, 7, 8];
     const result = [3, 0];
 
-    const methodResult = countInput(computerInput, userInput);
+    const methodResult = referee.countInput(computerInput, userInput);
 
     expect(methodResult).toEqual(result);
   });
 
   test(`countInput 스트라이크 카운트 확인`, () => {
+    const referee = new Referee();
     const computerInput = [7, 8, 9];
     const userInput = [7, 1, 2];
     const result = [0, 1];
 
-    const methodResult = countInput(computerInput, userInput);
+    const methodResult = referee.countInput(computerInput, userInput);
 
     expect(methodResult).toEqual(result);
   });
 
   test(`countInput 스트라이크,볼 카운트 확인`, () => {
+    const referee = new Referee();
     const computerInput = [7, 8, 9];
     const userInput = [7, 2, 8];
     const result = [1, 1];
 
-    const methodResult = countInput(computerInput, userInput);
+    const methodResult = referee.countInput(computerInput, userInput);
 
     expect(methodResult).toEqual(result);
   });
