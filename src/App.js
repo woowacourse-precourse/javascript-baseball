508e9ad76f660dcbcddb174b1edce720bfeb1a4e
Yoonsik-Shin <sys25800994@gmail.com>
2022-11-08 23:45:30 +0900

    feat : 게임시작 멘트

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+    play() {
+        // 1. 게임시작 멘트
+        MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+        const computerDefinedArr = this.computerDefine();
+        this.userInputValue(computerDefinedArr, this.resetResult());
+    }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
