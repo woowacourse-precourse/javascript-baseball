ea99afafa7507ce4acd74434c2118346c9263c7c
Bori-github <qhflrnfl4324@gmail.com>
2022-11-08 23:07:36 +0900

    feat: convert a player's input into a numeric array

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ const MissionUtils = require('@woowacourse/mission-utils');
 
 class App {
   getRandomNumbers() {
-    return MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3).join('');
+    return MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
+  }
+
+  convertToNumberArray(numbers) {
+    return numbers.split('').map(Number);
   }
 
   play() {
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
     const computer = this.getRandomNumbers();
 
-    MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (answer) =>
-      console.log(answer)
-    );
+    MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (answer) => {
+      const player = this.convertToNumberArray(answer);
+    });
   }
 }
 
