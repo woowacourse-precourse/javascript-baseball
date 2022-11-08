5f93251b90d85a1405ec88f88d4648fbf3a72973
SorinKwon <ksl020700@gmail.com>
2022-11-08 11:53:01 +0900

    style(App): 함수 선언식 -> 함수 표현식 변경
    - 파일 외부에서 함수 호출에 용이하기 위한 목적
    - 코드 컨벤션 반영
    - 오류 발생(play함수 실행 오류)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 const MissionUtils = require("@woowacourse/mission-utils");
+const Validation = require("./Validation");
+
 class App {
   play(){}
   constructor() {
@@ -6,83 +8,84 @@ class App {
     this.user_number=0;
   }
   play() {
-    this.selectComputerNumber();
-    this.getUserInputNumber();
-  }
-  
-  selectComputerNumber() {
-    const random_number = [];
-    while (random_number.length < 3) {
-      const selected_number = MissionUtils.Random.pickNumberInRange(1, 9);
-      if (!random_number.includes(selected_number)) {
-        random_number.push(selected_number);
-      }
-    }
-    this.changeComputerNumberToArray(random_number);
-  }
-  getUserInputNumber() {
-    MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (input_num) => {
-      this.changeUserNumberToArray(input_num);
-    });
-  }
-  changeComputerNumberToArray(random_number){
-    this.computer_number = (random_number + '').split(',');
-  }
-  changeUserNumberToArray(input_num){
-    this.user_number = (input_num + '').split('');
-    this.findEqualNumber();
-  }
-
-  findEqualNumber(){
-    let strike=0;
-    let ball=0;
-    this.user_number.forEach((number,index)=>{
-      if(this.computer_number[index]===number){
-        strike+=1;
-      }
-      if(this.computer_number[index]!==number&&this.computer_number.includes(number)){
-        ball+=1;
-      }
-    })
-    this.resultGame(strike,ball);
-  }
-  resultGame(strike,ball){
-    if(strike==3){
-      this.endGame();
-    }
-    if(strike==0&&ball!=0){
-      this.resultBall(ball);
-    }
-    if(strike==0&&ball==0){
-      this.resultNothing();
-    }
-    if(strike!=0&&ball!=0){
-      this.resultStrikeAndBall(strike,ball);
-    }
-  }
-  endGame(){
-    MissionUtils.Console.print('3스트라이크\n');
-    MissionUtils.Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료\n');
-    MissionUtils.Console.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n', (number) => {
-      if(number==1){
-        this.play();
-      }
-      if(number==2){
-        MissionUtils.Console.close();
-      }
-    });
-  }
-  resultBall(ball){
-      MissionUtils.Console.print(`${ball}볼`);
-      this.getUserInputNumber();
-  }
-  resultNothing(){
-    MissionUtils.Console.print('낫싱');
-    this.getUserInputNumber();
-  }
-  resultStrikeAndBall(strike,ball){
-    MissionUtils.Console.print(`${ball}볼 ${strike}스트라이크`);
-    this.getUserInputNumber();
+    select_computer_num();
+    user_input_number();
   }
 }
+const select_computer_num = function selectComputerNumber() {
+  const random_number = [];
+  while (random_number.length < 3) {
+    const selected_number = MissionUtils.Random.pickNumberInRange(1, 9);
+    if (!random_number.includes(selected_number)) {
+      random_number.push(selected_number);
+    }
+  }
+  computer_to_array(random_number);
+}
+const user_input_number= function getUserInputNumber() {
+  MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (input_num) => {
+    user_to_array(input_num);
+  });
+}
+const computer_to_array=function changeComputerNumberToArray(random_number){
+  App.computer_number = (random_number + '').split(',');
+}
+const user_to_array=function changeUserNumberToArray(input_num){
+  App.user_number = (input_num + '').split('');
+  find_equal_num();
+}
+
+const find_equal_num= function findEqualNumber(){
+  let strike=0;
+  let ball=0;
+  App.user_number.forEach((number,index)=>{
+    if(App.computer_number[index]===number){
+      strike+=1;
+    }
+    if(App.computer_number[index]!==number&&App.computer_number.includes(number)){
+      ball+=1;
+    }
+  })
+  game_result(strike,ball);
+}
+const game_result=function resultGame(strike,ball){
+  if(strike==3){
+    game_end();
+  }
+  if(strike==0&&ball!=0){
+    ball_score(ball);
+  }
+  if(strike==0&&ball==0){
+    nothing();
+  }
+  if(strike!=0&&ball!=0){
+    stirke_and_ball(strike,ball);
+  }
+}
+const game_end= function endGame(){
+  MissionUtils.Console.print('3스트라이크\n');
+  MissionUtils.Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료\n');
+  MissionUtils.Console.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n', (number) => {
+    if(number==1){
+      App.play();
+    }
+    if(number==2){
+      MissionUtils.Console.close();
+    }
+  });
+}
+const ball_score = function resultBall(ball){
+    MissionUtils.Console.print(`${ball}볼`);
+    user_input_number();
+}
+const nothing = function resultNothing(){
+  MissionUtils.Console.print('낫싱');
+  user_input_number();
+}
+const stirke_and_ball=function resultStrikeAndBall(strike,ball){
+  MissionUtils.Console.print(`${ball}볼 ${strike}스트라이크`);
+  user_input_number();
+}
+const app=new App;
+app.play();
 module.exports = App;
