0f89f4b6a0cacca3d769e0b324ee4456efe373aa
jjo-mi <whdal2636@naver.com>
2022-11-08 13:57:19 +0900

    feat: 2. 컴퓨터 랜덤 숫자 세자리 추출

--- src/App.js
+++ src/App.js
@@ -3,12 +3,28 @@ const MissionUtils = require("@woowacourse/mission-utils");
 class App {
   play() {
     this.printStart();
+    var computerArr = this.computerRandomNumbers();
   }
 
   /** 1. 게임시작 안내 문구 출력*/
   printStart() {
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
   }
+
+  /** 2. 컴퓨터 랜덤 숫자 세자리 추출 */
+  computerRandomNumbers() {
+    var computerArr = [];
+    while (computerArr.length < 3) {
+    var number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computerArr.includes(number)) {
+        computerArr.push(number);
+      }
+    }
+    return computerArr;
+  }
+
+
+
 }
 
 const app = new App();
