cc7d0c0fd03dc09b3698500b6ba5d094d308563b
cho seonghoon <chsida655@gmail.com>
2022-11-08 18:43:11 +0900

    feat: 에러메세지 분리

---
+++ src/Error.js
@@ -0,0 +1,13 @@
+const DUPLICATE_NUMBER = "중복된 숫자 입력했습니다.";
+const IS_NOT_NUM = "숫자를 입력해주세요.";
+const IS_NOT_1_2 = "1 또는 2만 입력해주세요.";
+const IS_NOT_3_LENGTH = "3자리만 입력해주세요.";
+const IS_NOT_1_LENGTH = "1자리만 입력해주세요.";
+
+module.exports = {
+  DUPLICATE_NUMBER,
+  IS_NOT_NUM,
+  IS_NOT_3_LENGTH,
+  IS_NOT_1_LENGTH,
+  IS_NOT_1_2,
+};
