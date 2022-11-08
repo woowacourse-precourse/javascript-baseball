25e6f20f1d5fa54aab534f0647669a0c444f62b8
Minhye Kang <alsgp9898@naver.com>
2022-11-08 15:06:16 +0900

    test: 볼, 스트라이크 개수와 맞는 결과 메세지를 출력하는지 검사하는 테스트 코드 작성

--- src/test/baseballGame.test.js
+++ src/test/baseballGame.test.js
@@ -1,25 +1,46 @@
-const { countStrikeNumbers, countBallNumbers } = require("../baseballGame.js");
+const {
+  countStrikeNumbers,
+  countBallNumbers,
+  printGameResult,
+} = require("../baseballGame.js");
+const MissionUtils = require("@woowacourse/mission-utils");
+
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(MissionUtils.Console, "print");
+  logSpy.mockClear();
+  return logSpy;
+};
 
 describe("컴퓨터 숫자와 사용자 숫자 비교 결과", () => {
   test("스트라이크 개수가 맞는지 검사", () => {
-    const computerNumber = [1, 2, 3];
-    const userInput = ["123", "124", "145", "456"];
-    const strikeNumber = [3, 2, 1, 0];
+    const computer = [1, 2, 3];
+    const user = ["123", "124", "145", "456"];
+    const strike = [3, 2, 1, 0];
 
-    strikeNumber.forEach((strikeNumber, index) => {
-      expect(countStrikeNumbers(computerNumber, userInput[index]))
-        .toBe(strikeNumber);
+    strike.forEach((strike, index) => {
+      expect(countStrikeNumbers(computer, user[index])).toBe(strike);
     });
   });
 
   test("볼 개수가 맞는지 검사", () => {
-    const computerNumber = [1, 2, 3];
-    const userInput = ["312", "321", "134", "145"];
-    const ballNumber = [3, 2, 1, 0];
+    const computer = [1, 2, 3];
+    const user = ["312", "321", "134", "145"];
+    const ball = [3, 2, 1, 0];
 
-    ballNumber.forEach((ballNumber, index) => {
-      expect(countBallNumbers(computerNumber, userInput[index]))
-        .toBe(ballNumber);
+    ball.forEach((ball, index) => {
+      expect(countBallNumbers(computer, user[index])).toBe(ball);
+    });
+  });
+
+  test("콘솔에 알맞은 문자열을 출력하는지 검사", () => {
+    const logSpy = getLogSpy();
+    const ball = [3, 2, 0, 0];
+    const strike = [0, 1, 3, 0];
+    const messages = ["3볼", "2볼 1스트라이크", "3스트라이크", "낫싱"];
+
+    messages.forEach((output, index) => {
+      printGameResult(strike[index], ball[index]);
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
     });
   });
 });
