19bf32f81aafe2a05beb74a8239232d185b0bfc5
jlee0505 <hj960322@gmail.com>
2022-11-08 10:29:16 +0900

    refactor: 정규식 상수화

--- src/constants.js
+++ src/constants.js
@@ -4,6 +4,8 @@ const MESSAGES = Object.freeze({
     INVALID_LENGTH: "3자리 숫자를 입력해주세요!",
     NOT_A_NUMBER: "숫자만 입력해주세요!",
     REQUIRE_USER_INPUT: (num) => `${num}자리 숫자를 입력해주세요: `,
+    ASK_RESTART: '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n',
+    FORMAT_ERROR_CHOICE: '입력값은 반드시 1 또는 2여야 합니다.',
     END: (num) => `${num}개의 숫자를 모두 맞히셨습니다! 게임 종료`,
   });
   
@@ -18,5 +20,16 @@ const MESSAGES = Object.freeze({
     BALL_MAX: 2,
     GAME_MAX: 3,
   });
+
+  const CHOICE = Object.freeze({
+    PLAY_AGAIN: '1',
+    EXIT: '2',
+  });
   
-  module.exports = { MESSAGES, UNITS, NUMBERS };
+  const REGEX = Object.freeze({
+    GUESS: /^[1-9]{3}$/,
+    CHOICE: /^[12]$/,
+    SPACE: / /gi,
+  });
+
+  module.exports = { MESSAGES, UNITS, NUMBERS, CHOICE, REGEX };
