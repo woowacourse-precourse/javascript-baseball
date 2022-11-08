24122b67272926014bae18c89aadaad6989a660f
kimeodml <kimeodml@gmail.com>
2022-11-08 22:36:54 +0900

    feat: 컴퓨터가 랜덤 숫자를 추출하는 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+const { Console, Random } = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  play() {
+    Console.print('숫자 야구 게임을 시작합니다.');
+    this.computer = [];
+    this.computerRandomNumber();
+  }
+
+  computerRandomNumber() {
+    while (this.computer.length < 3) {
+      const number = Random.pickNumberInRange(1, 9);
+      if (!this.computer.includes(number)) {
+        this.computer.push(number);
+      }
+    }
+  }
+
 }
 
 module.exports = App;
