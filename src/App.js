000d12db42c1c0fe257af31d86b7bc607490bc15
jlee0505 <hj960322@gmail.com>
2022-11-08 09:58:46 +0900

    feat: 게임초기화(랜덤넘버 생성) 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+const { Console, Random } = require('@woowacourse/mission-utils');
+
 class App {
-  play() {}
+	computerNumArr = [];
+
+	play() {
+		Console.print(MESSAGES.START);
+		this.initGame();
+		// TODO: this.playGame();
+	}
+
+	initGame() {
+		while (this.computerNumArr.length < 3) {
+			let number = Random.pickNumberInRange(1, 9);
+			if (!this.computerNumArr.includes(number))
+				this.computerNumArr.push(number);
+		}
+		Console.print(this.computerNumArr);
+	}
 }
 
 module.exports = App;
