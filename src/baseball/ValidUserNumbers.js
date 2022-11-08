862864ff4df75468399cdbeee2a65221051c9715
Y2IN <dpdls0228@daum.net>
2022-11-08 21:52:12 +0900

    refactor : 가독성을 위해 행 추가

--- src/baseball/ValidUserNumbers.js
+++ src/baseball/ValidUserNumbers.js
@@ -4,13 +4,14 @@ class ValidUserNumbers {
   isValidUserInput(userInput) {
     const isNumberElement = (element) => element >= "1" && element <= "9";
     const userNumbers = [];
-    userInput.split("").forEach((element) => {
+    userInput.split('').
+    forEach(element => {
       !userNumbers.includes(element)
         ? userNumbers.push(element)
         : userNumbers.push("NO");
     });
 
-    return userNumbers.length === 3 && userNumbers.every(isNumberElement);
+    return (userNumbers.length === 3 && (userNumbers).every(isNumberElement));
   }
 }
 
