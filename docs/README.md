df62a0e9bd9bd771a14158574440801dfd0e0368
elasjay <elasjay@LAPTOP-5ETGJV08.localdomain>
2022-11-08 09:25:42 +0900

    feat(): setting method
    Added new method to :
    - declare the class's variable: ,
    - store the COMPUTER value using
    
    Breaks .setting, which was added

---
+++ docs/README.md
@@ -0,0 +1,10 @@
+## ⚾숫자야구 - Commit List
+---
+feat($App): setting method
+
+Added new method to $App:
+- declare the class's variable: `COMPUTER`, `PLAYER`
+- store the COMPUTER value using `MissionUtils.Random.pickNumberInRange`
+
+Breaks $App.setting, which was added
+
--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import * as MissionUtils from "@woowacourse/mission-utils";
+
 class App {
-  play() {}
+  setting(){
+    const COMPUTER = [];
+    const PLAYER = [];
+    while(COMPUTER.length < 3){
+        const NUMBER = MissionUtils.Random.pickNumberInRange(1, 9);
+        if(!COMPUTER.includes(NUMBER))
+            COMPUTER.push(NUMBER);
+    }
+  }
 }
 
 module.exports = App;
