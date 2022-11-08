f5a21f4c1b8ab8e0a33d4edca20e4602354f4bb9
Sangmin Lee <gink.ss00@gmail.com>
2022-11-08 21:06:26 +0900

    feat: 게임 진행 테스트 - 컴퓨터의 숫자와 플레이어가 입력한 숫자 비교 테스트 구현
    GameManager 클래스의 compare() 메소드 테스트와 같다.
    
    - 비교를 하여 같은 자리에 같은 숫자가 있으면 strike, 다른 자리에 같은 숫자가 있으면 ball 카운트가 올라간다.

--- __tests__/GameTest.js
+++ __tests__/GameTest.js
@@ -14,4 +14,28 @@ describe('게임 진행 테스트', () => {
       expect(answer.join('')).toMatch(/[1-9]/);
     });
   });
+
+  test('컴퓨터의 숫자와 플레이어가 입력한 숫자 비교 테스트', () => {
+    const gameManager = new GameManager();
+    gameManager.answer = [1, 4, 7];
+    const answers = [
+      gameManager.compare(['2', '5', '8']),
+      gameManager.compare(['1', '4', '9']),
+      gameManager.compare(['4', '1', '7']),
+      gameManager.compare(['1', '4', '7']),
+      gameManager.compare(['4', '7', '1']),
+    ];
+
+    const results = [
+      { ballCount: 0, strikeCount: 0 },
+      { ballCount: 0, strikeCount: 2 },
+      { ballCount: 2, strikeCount: 1 },
+      { ballCount: 0, strikeCount: 3 },
+      { ballCount: 3, strikeCount: 0 },
+    ];
+
+    answers.forEach((answer, index) => {
+      expect(answer).toEqual(results[index]);
+    });
+  });
 });
