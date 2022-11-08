2790d9f4c7d9211a816ed254d3cfa78b32138240
MiJung Lee <mijunglee@MiJungs-MacBook-Air.local>
2022-11-08 17:11:04 +0900

    add function that creates random 3 digit numbers

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+
 class App {
-  play() {}
+  createNumber() {
+    let three_digit = []
+    let number = [1,2,3,4,5,6,7,8,9]
+    for(let i =0; i <3; i++){
+      let random_num = number.splice(Math.floor(Math.random()*(9-i)),1)[0]
+      three_digit .push(random_num)
+    }
+    return three_digit.join("")
+  }
+
+  play(){
+    const NUM = this.createNumber()
+    console.log(NUM)
+  
+}
 }
 
 module.exports = App;
+
+
+const app = new App();
+app.play()
