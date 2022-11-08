69e63399ac85db19e3e5c9038cdb656f758a6f8d
JiYoon Lee <wldbseja@daum.net>
2022-11-08 23:05:30 +0900

    [✔]게임 시작 기능

--- src/App.js
+++ src/App.js
@@ -127,7 +127,14 @@ class App {
     this.processNextStep(numberStrike);
   }
 
-  play() {}
+  play() {
+    if (!this.answerArray) {
+      Console.print('⚾️숫자 야구 게임을 시작합니다.⚾️');
+    }
+
+    this.generateRandomNumbers();
+    this.readUserInput();
+  }
 }
 
 module.exports = App;
