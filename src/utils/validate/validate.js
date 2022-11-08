d03fe52dc8ff11991a9082652775ecaa07345be2
KimDongYoung <ehddud1006@pusan.ac.kr>
2022-11-08 16:50:31 +0900

    feat: 중복된 숫자가 있는지 체크하는 함수

--- src/utils/validate/validate.js
+++ src/utils/validate/validate.js
@@ -1,3 +1,8 @@
 const isWrongLength = (num) => {
   if (num.length !== 3) throw new Error("3자리의 숫자를 입력해주세요");
 };
+
+const hasDuplicatedNumbers = (num) => {
+  if (new Set(num).size !== num.length)
+    throw new Error("중복된 숫자가 없이 입력해주세요.");
+};
