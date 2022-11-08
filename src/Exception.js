4bc3cacc575dbf3b988cea71c9db55a8a33a6065
1 <wrasf175@naver.com>
2022-11-08 15:31:45 +0900

    feat: 입력값을 받아 예외시 에러를 출력하는 기능 구현, 예외사항 추가

---
+++ src/Exception.js
@@ -0,0 +1,29 @@
+const { WRONG_INPUT_ALERT } = require("./lib/constants");
+
+class Exception {
+  static answer(answer) {
+    if (isNaN(parseInt(answer))) {
+      throw WRONG_INPUT_ALERT.NOT_NUMBER;
+    }
+
+    if (answer.length !== 3) {
+      throw WRONG_INPUT_ALERT.NOT_THREE_DIGITS;
+    }
+
+    if (answer.split("").includes("0")) {
+      throw WRONG_INPUT_ALERT.INCLUDES_ZERO;
+    }
+
+    if (answer.length !== new Set(answer).size) {
+      throw WRONG_INPUT_ALERT.NOT_UNIQUE_NUMBER;
+    }
+
+    if(answer.length < 0){
+      throw WRONG_INPUT_ALERT.NOT_POSITIVE;
+    }
+
+    return answer;
+  }
+}
+
+module.exports = Exception;
--- src/User.js
+++ src/User.js
@@ -1,9 +1,9 @@
 const MissionUtils = require("@woowacourse/mission-utils");
-
+const Exception = require("./Exception");
 class User {
   readAnswer(query, callback) {
     MissionUtils.Console.readLine(query, (answer) => {
-      callback(answer);
+      callback(Exception.answer(answer));
     });
   }
 }
