20d4088630bb1b22cb9eb5e4d89f4f108b2362a7
RyuJoonSik <tldtksahtxja@naver.com>
2022-11-08 22:35:48 +0900

    refactor: ManualBallGenerator.test.js의 중복되는 코드 제거

--- src/Components/ManualBallGenerator/ManualBallGenerator.test.js
+++ src/Components/ManualBallGenerator/ManualBallGenerator.test.js
@@ -1,11 +1,11 @@
 const ManualBallGenerator = require("./ManualBallGenerator");
-const mockFunction = require("../../mockFunction");
+const { mockQuestions } = require("../../mockFunction");
 
 describe("ManualBallGenerator", () => {
   test("execute로 숫자 생성", async () => {
     const INPUT_NUMBER = 246;
     const STRING_ARRAY = [String(INPUT_NUMBER)];
-    mockFunction.mockQuestions(STRING_ARRAY);
+    mockQuestions(STRING_ARRAY);
     const BALL = await new ManualBallGenerator().execute();
 
     expect(BALL.getNumber()).toBe(INPUT_NUMBER);
@@ -13,21 +13,21 @@ describe("ManualBallGenerator", () => {
 
   test("숫자가 아니면 throw로 예외 발생", async () => {
     const STRING_ARRAY = ["hello"];
-    mockFunction.mockQuestions(STRING_ARRAY);
+    mockQuestions(STRING_ARRAY);
 
     await expect(new ManualBallGenerator().execute()).rejects.toThrow();
   });
 
   test("3자리 숫자가 아니면 throw로 예외 발생", async () => {
     const STRING_ARRAY = ["2464"];
-    mockFunction.mockQuestions(STRING_ARRAY);
+    mockQuestions(STRING_ARRAY);
 
     await expect(new ManualBallGenerator().execute()).rejects.toThrow();
   });
 
   test("0을 포함하면 throw로 예외 발생", async () => {
     const NUMBER_ARRAY = ["024"];
-    mockFunction.mockQuestions(NUMBER_ARRAY);
+    mockQuestions(NUMBER_ARRAY);
 
     await expect(new ManualBallGenerator().execute()).rejects.toThrow();
   });
@@ -35,7 +35,7 @@ describe("ManualBallGenerator", () => {
   test("숫자 앞 뒤의 공백 제거", async () => {
     const INPUT_NUMBER = 246;
     const STRING_ARRAY = ["   " + String(INPUT_NUMBER) + "    "];
-    mockFunction.mockQuestions(STRING_ARRAY);
+    mockQuestions(STRING_ARRAY);
     const BALL = await new ManualBallGenerator().execute();
 
     expect(BALL.getNumber()).toBe(INPUT_NUMBER);
@@ -44,7 +44,7 @@ describe("ManualBallGenerator", () => {
   test("숫자 사이에 공백이 있으면 throw로 예외 발생", async () => {
     const INPUT_NUMBER = 246;
     const STRING_ARRAY = ["123 " + String(INPUT_NUMBER) + " 123"];
-    mockFunction.mockQuestions(STRING_ARRAY);
+    mockQuestions(STRING_ARRAY);
 
     await expect(new ManualBallGenerator().execute()).rejects.toThrow();
   });
