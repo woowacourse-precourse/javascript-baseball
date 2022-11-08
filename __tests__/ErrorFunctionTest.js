94ab013abb46f7799c7a54a728177210bd559d4f
김상두 <puki4416@gmail.com>
2022-11-08 17:19:05 +0900

    test : checkOneOrTwo 함수 테스트

--- __tests__/ErrorFunctionTest.js
+++ __tests__/ErrorFunctionTest.js
@@ -1,4 +1,4 @@
-const { checkThreeDifferentNumbers } = require("../src/Error");
+const { checkOneOrTwo, checkThreeDifferentNumbers } = require("../src/Error");
 
 describe("checkThreeDifferentNumbers 함수 테스트 ", () => {
   test("정상적인 입력이 들어오는 경우", () => {
@@ -48,5 +48,24 @@ describe("checkThreeDifferentNumbers 함수 테스트 ", () => {
     }).toThrow(
       new Error("1에서 9 사이의 서로 다른 숫자 3개를 입력해야 합니다")
     );
+  });
+});
+
+describe("checkOneOrTwo 함수 테스트", () => {
+  test("1이 입력되는 경우", () => {
+    const input = "1";
+    expect(checkOneOrTwo(input)).toEqual(true);
+  });
+
+  test("2가 입력되는 경우", () => {
+    const input = "2";
+    expect(checkOneOrTwo(input)).toEqual(true);
+  });
+
+  test("1,2 이외의 값이 입력되는 경우", () => {
+    const input = "asdfe13d93";
+    expect(() => {
+      checkOneOrTwo(input);
+    }).toThrow(new Error("1 또는 2 만 입력해야합니다"));
   });
 });
