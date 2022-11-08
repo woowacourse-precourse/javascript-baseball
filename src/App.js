5e901be0a64898278654d94934014ba135286a33
myeongheonhong <ruddyh7076@gmail.com>
2022-11-08 18:11:36 +0900

    [ Feat ] :  printStartMessage & tryCheckingAnswer & checkException 함수 구현.
    - print StartMessage : MissionUtil모듈을 이용하여 숫자야구 게임의 시작을 알리는 메세지 출력함수.
    - tryCheckingAnswer : 컴퓨터의 숫자를 알아내기 위해 사용자의 입력을 받는 함수.
    - checkException : 입력받은 answer가 다음과 같은 예외 상황일 경우 에러를 throw하는 함수.
    
    (예외 상황)
    1. 1-9숫자 외에 문자를 입력받았을 경우.
    2. 3개 이하의 숫자를 입력받았을 경우.
    3. 중복된 숫자를 입력받았을 경우.

--- src/App.js
+++ src/App.js
@@ -10,7 +10,40 @@ class App {
         this.out = false;
     }
 
-    play() {}
+    //숫자야구게임 시작함수.
+    play() {
+        this.printStartMessage();
+        this.tryCheckingAnswer();
+    }
+
+    //게임의 시작메세지를 출력하는 함수.
+    printStartMessage() {
+        MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+    }
+
+    //사용자의 숫자 입력을 받는 함수.
+    tryCheckingAnswer() {
+        MissionUtils.Console.readLine('숫자 3개를 입력하세요 : ', (answer) => {
+            this.checkException(answer);
+            this.user = answer.split('');
+        });
+    }
+
+    checkException(answer) {
+        let isNumber = /^[1-9]+$/;
+        let arrToAnswer = answer.split('');
+        let checkInputDuplicate = [...new Set(arrToAnswer)];
+
+        if (!isNumber.test(answer)) {
+            throw '[error] : 1-9까지의 숫자만 입력 가능합니다.';
+        }
+        if (checkInputDuplicate.length > 3) {
+            throw '[error] : 3개 이하의 숫자를 입력해주세요!!';
+        } else if (checkInputDuplicate.length < 3 && arrToAnswer.length === 3) {
+            throw '[error] : 중복된 숫자를 입력할 수 없습니다.';
+        }
+        return true;
+    }
 }
 
 module.exports = App;
