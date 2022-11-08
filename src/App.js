c7f0e89e77193f8130c62d9864c2712bc6ce1141
연 <mogoo22@naver.com>
2022-11-08 18:13:49 +0900

    feat: 게임 시작 문구를 출력한다.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 const { Random, Console } = require('@woowacourse/mission-utils');
 
 const MESSAGE = {
+  PLAY: '숫자 야구 게임을 시작합니다.',
   QUIT: '게임 종료',
   WIN: '3개의 숫자를 모두 맞히셨습니다! 게임 종료',
   REPLAY_OR_QUIT: '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.',
@@ -28,6 +29,7 @@ class App {
 
   play(type) {
     if (type !== 'WRONG_ANSWER') {
+      Console.print(MESSAGE.PLAY);
       this.#answer = App.#pickRandomNumbers(3);
     }
     this.#hitBalls();
