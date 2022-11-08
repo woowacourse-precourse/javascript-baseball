bf32d375fb47273872c1450d0d42fbb98ca0f10e
Kim jinwoo <49385170+Sunny-jinn@users.noreply.github.com>
2022-11-08 23:03:32 +0900

    feat(App): set constructor

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+
+const MAX_LENGTH = 3;
+
 class App {
-  play() {}
+  constructor() {
+    this.userNumber = [];
+    this.computerNumber = [];
+    this.countStrike = 0;
+    this.countBall = 0;
+    this.gameover = false;
+  }
+
+  play() { }
+
 }
 
 module.exports = App;
