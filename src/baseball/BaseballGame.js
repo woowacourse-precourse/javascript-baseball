af8497b044aa0614d7d9d293a44d51bf5c43c534
Y2IN <dpdls0228@daum.net>
2022-11-08 12:32:36 +0900

    refactor : 메세지 변경 필요 없는 변수 지움

--- src/baseball/BaseballGame.js
+++ src/baseball/BaseballGame.js
@@ -1,6 +1,7 @@
 const { Console } = require("@woowacourse/mission-utils");
 const ComputerNumbers = require("./ComputerNumbers");
 const ValidUserNumbers = require("./ValidUserInput");
+const { GAME_MESSAGE, WHAT, ERROR_MESSAGE } = require("../constants/constants");
 
 class BaseballGame {
   constructor() {
@@ -11,75 +12,76 @@ class BaseballGame {
     this.FirstGame = FirstGame;
     this.computerNumbers = ComputerNumbers.randomSelectComputerNumbers();
   }
+
   playGame = () => {
     if (this.FirstGame) {
-      Console.print("숫자 야구 게임을 시작합니다.");
+      Console.print(GAME_MESSAGE.START_MESSAGE);
     }
-    Console.readline("숫자를 입력해주세요 : ", this.playing);
+    Console.readLine(GAME_MESSAGE.ENTER_NUMBER, this.playing);
   };
 
   playing = (userInput) => {
-    ValidUserNumbers.isThreeNumberInRange(userInput);
-
-    let [strike, ball, nothing] = this.StrikeCount(
-      userInput,
-      this.computerNumbers
-    );
-    this.printResultCount(strike, ball, nothing);
-    // console.log(typeof strike);
-    if (strike == 3) {
-      Console.readline(
-        "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n",
-        this.RestartEndDistinction
-      );
+    const validUserInput = ValidUserNumbers.isValidUserInput(userInput);
+    if (validUserInput === false) {
+      this.throwError(ERROR_MESSAGE.ERROR_USER_INPUT);
+    }
+    const { strike, ball } = this.StrikeCount(userInput, this.computerNumbers);
+    this.printResult(strike, ball);
+    if (strike === 3) {
+      Console.readLine(GAME_MESSAGE.GAME_RESTART, this.isvalidRestart);
       return;
     }
     this.playGame();
   };
 
   StrikeCount = (userInput, computerNumbers) => {
+    const computerNumbersArr = [...computerNumbers];
+    const userInputArr = [...userInput];
     let strike = 0;
     let ball = 0;
-    let nothing = 0;
-    computerNumbers.forEach((number, idx) => {
-      if (!computerNumbers.includes(Number(userInput[idx]))) {
-        nothing++;
-        return;
-      }
-      if (number === Number(userInput[idx])) {
-        strike++;
-      }
-      ball++;
+    console.log(computerNumbersArr);
+    console.log(userInputArr);
+    computerNumbersArr.forEach((number, index) => {
+      number === Number(userInputArr[index])
+        ? strike++
+        : Number(userInputArr.includes(number))
+        ? ball++
+        : 0;
     });
-    return [strike, ball, nothing];
+    return { strike, ball };
   };
 
-  printResultCount = (strike, ball, nothing) => {
-    if (nothing == 3) {
-      Console.print("낫싱");
+  printResult = (strike, ball) => {
+    if (strike === 0 && ball === 0) {
+      return Console.print(WHAT.NOTHING);
     } else if (ball > 0 && strike === 0) {
-      Console.print(`${ball}볼`);
-    } else if (ball == 0 && strike > 0) {
-      Console.print(`${strike}strike`);
+      return Console.print(`${ball}볼`);
+    } else if (ball === 0 && strike > 0) {
+      return Console.print(`${strike}스트라이크`);
     }
     Console.print(`${ball}볼 ${strike}스트라이크`);
 
-    if (strike === 3)
-      Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    if (strike === 3) {
+      Console.print(GAME_MESSAGE.FINISH_MESSAGE);
+    }
   };
 
   isvalidRestart = (OneOrTwo) => {
     OneOrTwo = Number(OneOrTwo);
-    ValidUserInput.isvalidRestart(OneOrTwo);
+    ValidUserNumbers.isvalidRestart(OneOrTwo);
 
     if (OneOrTwo == 1) {
       this.GameStart(false);
       this.playGame();
     } else if (OneOrTwo == 2) {
-      Console.print("게임 종료");
+      Console.print("GAME_MESSAGE.GAME_END");
       Console.close();
     }
   };
+
+  throwError(messages) {
+    throw new Error(messages);
+  }
 }
 
 module.exports = BaseballGame;
