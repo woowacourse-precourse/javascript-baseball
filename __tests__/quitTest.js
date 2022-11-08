f29cabd448751c5c51959c77e3b317ca27b2d354
sojinjang <jsojin6119@gmail.com>
2022-11-08 23:47:46 +0900

    :white_check_mark: test: 게임 종료 기능 테스트

---
+++ __tests__/quitTest.js
@@ -0,0 +1,43 @@
+const App = require("../src/App");
+const MissionUtils = require("@woowacourse/mission-utils");
+
+const mockQuestions = (answers) => {
+  MissionUtils.Console.readLine = jest.fn();
+  answers.reduce((acc, input) => {
+    return acc.mockImplementationOnce((question, callback) => {
+      callback(input);
+    });
+  }, MissionUtils.Console.readLine);
+};
+
+const mockRandoms = (numbers) => {
+  MissionUtils.Random.pickNumberInRange = jest.fn();
+  numbers.reduce((acc, number) => {
+    return acc.mockReturnValueOnce(number);
+  }, MissionUtils.Random.pickNumberInRange);
+};
+
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(MissionUtils.Console, "print");
+  logSpy.mockClear();
+  return logSpy;
+};
+
+describe("게임 종료 혹은 재시작 테스트", () => {
+  test("게임 종료 테스트", () => {
+    const randoms = [1, 2, 3];
+    const answers = ["123", "2"];
+    const logSpy = getLogSpy();
+    const messages = ["3스트라이크", "게임 종료"];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    const app = new App();
+    app.play();
+
+    messages.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+});
