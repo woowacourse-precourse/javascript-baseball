daf783aa8091feb203e8bf89820dbacfaf1e0837
HAEROOL <71335694+HAEROOL@users.noreply.github.com>
2022-11-08 16:29:21 +0900

    feat: add function to validate user input

--- src/User.js
+++ src/User.js
@@ -1,4 +1,5 @@
 const { Console } = require("@woowacourse/mission-utils");
+const userInputRegEx = /^[1-9]{3}$/;
 class User {
   constructor() {
     this.userInput = "";
@@ -9,4 +10,16 @@ class User {
       Console.close();
     });
   };
+  validateInput = (input) => {
+    if (!userInputRegEx.test(input)) {
+      throw new Error("3자리의 중복되지 않는 숫자로 입력해주세요");
+    }
+    if (new Set(input.split("")).size > 3) {
+      throw new Error("3자리의 중복되지 않는 숫자로 입력해주세요");
+    }
+    return true;
+  };
 }
+module.exports = {
+  User,
+};
