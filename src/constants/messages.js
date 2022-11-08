ef849e790fd812e67e14b8f101f2a01eca529946
NaveOWO <pianowell96@gmail.com>
2022-11-08 18:06:21 +0900

    [ feat ] 상수 추가

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -1,11 +1,13 @@
 const GAME_MESSAGE = {
   GMAE_START: "숫자 야구 게임을 시작합니다.",
   INPUT_TEXT: "숫자를 입력해주세요 : ",
-  ANSWER: "3스트라이크",
   GAME_OVER: "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
   RETRY_TEXT: "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
   MAX_LENGTH: 3,
   NO_MESSAGE: "",
+  ANSWER: "3스트라이크",
+  STRIKE: "스트라이크",
+  BALL: "볼",
 };
 
 const ERROR_MESSAGE = {
