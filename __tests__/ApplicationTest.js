a90d38a5213f04a61c28f3e5768e075787672fa8
d-charlie-kim <dokkim@student.42seoul.kr>
2022-11-08 09:55:56 +0900

    Test: 정상 종료 테스트 케이스 추가

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -47,6 +47,32 @@ describe("숫자 야구 게임", () => {
     });
   });
 
+  test("게임 종료 후 재시작 후 재시작", () => {
+    const randoms = [1, 3, 5, 5, 8, 9, 2, 4, 6];
+    const answers = ["246", "135", "1", "597", "589", "1", "123", "456", "246", "2"];
+    const logSpy = getLogSpy();
+    const messages = [
+      "낫싱",
+      "3스트라이크",
+      "1볼 1스트라이크",
+      "3스트라이크",
+      "1볼",
+      "1볼 1스트라이크",
+      "3스트라이크",
+      "게임 종료",
+    ];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    const app = new App();
+    app.play();
+
+    messages.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
   test("예외 테스트", () => {
     const answers = ["1234"];
     
