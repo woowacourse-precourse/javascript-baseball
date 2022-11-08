566bb5c1c32d6acb893203167a4c5fc515736544
GuHyeonWoo <ghw9174@gmail.com>
2022-11-08 17:57:33 +0900

    feat (startGame): 숫자 입력 기능 구현
    Console.readLine()을 통해 값을 입력 받음
    
    docs/README.md의 구현 할 기능 목록 #3

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ const Console = MissionUtils.Console;
 const Random = MissionUtils.Random;
 
 class App {
+  inputNumber(answer) {
+    // 입력한 값에 대한 처리 필요
+  }
+
+  startGame(CORRECT_LIST) {
+    Console.readLine('숫자를 입력해주세요 : ', (answer) => {this.inputNumber(answer)});
+  }
+
   createRandomValue() {
     const TEMP_LIST = Random.pickUniqueNumbersInRange(1, 9, 3)
     return TEMP_LIST;
@@ -16,6 +24,7 @@ class App {
   play() {
     this.openingOutput();
     const CORRECT_LIST = this.createRandomValue();
+    this.startGame(CORRECT_LIST);
   }
 }
 
