30c7f310d43c683918f5dbe2caecf5385849c4f9
ksone02 <eksovus@gmail.com>
2022-11-08 19:27:19 +0900

    feat: 다시시작 예외처리 함수 구현

--- src/utils.js
+++ src/utils.js
@@ -12,10 +12,18 @@ function checkNumberRange(value) {
   return /^[1-9]*$/g.test(value);
 }
 
-function checkException(value, length) {
+function checkRestart(value) {
+  return ['1', '2'].includes(value);
+}
+
+function checkRestartException(value) {
+  if (!checkRestart(value)) throw ERROR.RESTART_RANGE;
+}
+
+function checkBaseBallException(value, length) {
   if (!checkLength(value, length)) throw ERROR.LENGTH;
   if (!checkDifferentAll(value, length)) throw ERROR.NOTUNIQUE;
   if (!checkNumberRange(value)) throw ERROR.NUMBER_RANGE;
 }
 
-module.exports = checkException;
+module.exports = { checkBaseBallException, checkRestartException };
