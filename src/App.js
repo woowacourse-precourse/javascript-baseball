76f5f993da92abbc426565f8fd7e2329c9927acf
shlee0820 <72565083+shlee0820@users.noreply.github.com>
2022-11-08 12:30:11 +0900

    feat: 볼/스트라이크의 개수에 따른 출력 결과를 만드는 함수 추가 (getCompareResultText)

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ class App {
       if (checkValidUserInput(this.userInputNumber)) {
         this.calculateResult();
       }
+      this.getCompareResultText();
     });
   }
 
@@ -37,6 +38,21 @@ class App {
         this.ballScore += 1;
       }
     });
+  }
+
+  getCompareResultText() {
+    if (this.strikeScore === 0 && this.ballScore === 0) {
+      this.compareResultText = '낫싱';
+    } else if (this.ballScore === 0) {
+      this.compareResultText = `${this.strikeScore}스트라이크`;
+      if (this.strikeScore === 3) {
+        this.isThreeStrike = true;
+      }
+    } else if (this.strikeScore === 0) {
+      this.compareResultText = `${this.ballScore}볼`;
+    } else if (this.strikeScore > 0 && this.ballScore > 0) {
+      this.compareResultText = `${this.ballScore}볼 ${this.strikeScore}스트라이크`;
+    }
   }
 
   startGame() {
