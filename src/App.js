0f417be29aa84e659c4a85d04f820529ba125848
sebin kwon <sbkwon98@gmail.com>
2022-11-08 21:48:09 +0900

    feat : 숫자 입력 기능

--- src/App.js
+++ src/App.js
@@ -10,4 +10,10 @@ const startBaseballNumber = () => {
   MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
 };
 
+const inputNumber = (answerNumber) => {
+  MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (number) => {
+
+  });
+};
+
 module.exports = App;
