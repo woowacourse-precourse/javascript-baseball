89558548e47deef6af6ed1effeccfd021f6f6b20
KPGY <yuchw8@korea.ac.kr>
2022-11-08 23:07:47 +0900

    스트라이크와 볼의 갯수를 가져와 힌트를 주는 함수 구현

--- src/App.js
+++ src/App.js
@@ -36,4 +36,20 @@ countball(inputArr, computernum) {
   }
   return ball;
 }
+
+checkresult(strike, ball) {
+  if (strike === 0 && ball === 0) {
+    MissionUtils.Console.print('낫싱');
+  }
+  else if (strike === 0) {
+    MissionUtils.Console.print(`${ball}볼`);
+  } 
+  else if (ball === 0) {
+    MissionUtils.Console.print(`${strike}스트라이크`);
+  }
+  else
+  {
+    MissionUtils.Console.print(`${ball}볼 ${strike}스트라이크`);
+  }
+}
 module.exports = App;
