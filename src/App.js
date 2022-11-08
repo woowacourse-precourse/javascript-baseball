10da508ba4cf3670fea864de80d71905ac24a8f4
SorinKwon <ksl020700@gmail.com>
2022-11-08 23:13:13 +0900

    style(App): 소스 코드 정렬

--- src/App.js
+++ src/App.js
@@ -2,20 +2,38 @@ const MissionUtils = require("@woowacourse/mission-utils");
 const Validation = require("./Validation");
 
 class App {
-  play(){}
+  play() {}
   constructor() {
     this.computer_number = 0;
-    this.user_number=0;
+    this.user_number = 0;
   }
   play() {
-    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
     this.startGame();
   }
 
-  startGame(){
+  startGame() {
     this.selectComputerNumber();
     this.getUserInputNumber();
   }
+
+  endGame() {
+    MissionUtils.Console.print("3스트라이크\n");
+    MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n");
+    MissionUtils.Console.readLine(
+      "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n",
+      (number) => {
+        Validation.endgame_choice_valid(number);
+        if (number == 1) {
+          this.startGame();
+        }
+        if (number == 2) {
+          MissionUtils.Console.close();
+        }
+      }
+    );
+  }
+
   selectComputerNumber() {
     let random_number = [];
     while (random_number.length < 3) {
@@ -27,74 +45,69 @@ class App {
     this.changeComputerNumberToArray(random_number);
     return random_number;
   }
+
   getUserInputNumber() {
-    MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (input_num) => {
+    MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (input_num) => {
       Validation.number_input_valid(input_num);
       this.changeUserNumberToArray(input_num);
     });
   }
-  changeComputerNumberToArray(random_number){
-    App.computer_number = (random_number + '').split(',');
+
+  changeComputerNumberToArray(random_number) {
+    App.computer_number = (random_number + "").split(",");
     return App.computer_number;
   }
-  changeUserNumberToArray(input_num){
-    App.user_number = (input_num + '').split('');
-    this.findEqualNumber();
+
+  changeUserNumberToArray(input_num) {
+    App.user_number = (input_num + "").split("");
+    this.findEqualIndexNumber();
     return App.user_number;
   }
-  
-  findEqualNumber(){
-    let strike=0;
-    let ball=0;
-    App.user_number.forEach((number,index)=>{
-      if(App.computer_number[index]===number){
-        strike+=1;
+
+  findEqualIndexNumber() {
+    let strike = 0;
+    let ball = 0;
+    App.user_number.forEach((number, index) => {
+      if (App.computer_number[index] === number) {
+        strike += 1;
       }
-      if(App.computer_number[index]!==number&&App.computer_number.includes(number)){
-        ball+=1;
-      }
-    })
-    this.resultGame(strike,ball);
-  }
-  resultGame(strike,ball){
-    if(strike==3){
-      this.endGame();
-    }
-    if(strike==0&&ball!=0){
-      this.resultBall(ball);
-    }
-    if(strike==0&&ball==0){
-      this.resultNothing();
-    }
-    if(strike!=0&&ball!=0){
-      this.resultStrikeAndBall(strike,ball);
-    }
-  }
-  endGame(){
-    MissionUtils.Console.print('3스트라이크\n');
-    MissionUtils.Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료\n');
-    MissionUtils.Console.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n', (number) => {
-      Validation.endgame_choice_valid(number);
-      if(number==1){
-        this.startGame();
-      }
-      if(number==2){
-        MissionUtils.Console.close();
+      if (
+        App.computer_number[index] !== number &&
+        App.computer_number.includes(number)
+      ) {
+        ball += 1;
       }
     });
+    this.resultGame(strike, ball);
   }
-  resultBall(ball){
-      MissionUtils.Console.print(`${ball}볼`);
-      this.getUserInputNumber();
+
+  resultGame(strike, ball) {
+    if (strike == 3) {
+      this.endGame();
+    }
+    if (strike == 0 && ball != 0) {
+      this.resultBall(ball);
+    }
+    if (strike == 0 && ball == 0) {
+      this.resultNothing();
+    }
+    if (strike != 0 && ball != 0) {
+      this.resultStrikeAndBall(strike, ball);
+    }
   }
-  resultNothing(){
-    MissionUtils.Console.print('낫싱');
+
+  resultBall(ball) {
+    MissionUtils.Console.print(`${ball}볼`);
     this.getUserInputNumber();
   }
-  resultStrikeAndBall(strike,ball){
+  resultNothing() {
+    MissionUtils.Console.print("낫싱");
+    this.getUserInputNumber();
+  }
+  resultStrikeAndBall(strike, ball) {
     MissionUtils.Console.print(`${ball}볼 ${strike}스트라이크`);
     this.getUserInputNumber();
   }
 }
 
-module.exports=App;
+module.exports = App;
