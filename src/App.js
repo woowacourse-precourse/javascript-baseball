b02785cfdaacd2f4f0d1eda589110b2b51a33fa0
MyungAe <ladiescheong@naver.com>
2022-11-08 22:17:54 +0900

    feat(App): 주요 기능 App 클래스로 편입, 클래스 프로퍼티 대신 콜백 사용
    주요 변경점
    - System, User, Utility 클래스 폐기, 주요 기능 App 클래스 메소드로 편입
    - 클래스 프로퍼티를 이용해 값을 전달하던 방식 대신 콜백과 리턴 값을 통해 전달

--- src/App.js
+++ src/App.js
@@ -1,46 +1,95 @@
-const System = require("./System");
-const User = require("./User");
-const { Console } = require("./Utilitys");
+const MissionUtils = require("@woowacourse/mission-utils");
 class App {
-  async play() {
-    const NumberBaseBallSystem = new System();
-    const PlayingUser = new User();
+  play() {
+    this.getStarted();
+    this.playGame();
+  }
 
-    NumberBaseBallSystem.getStarted();
+  getStarted() {
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+  }
 
-    let replay = 1;
-    let reGuess = true;
-    do {
-      NumberBaseBallSystem.createAnswerNumber();
-      // console.log(NumberBaseBallSystem.getNumber);
-      do {
-        const guessNumber = await PlayingUser.enterGuessNumber();
-        const scoreboard = NumberBaseBallSystem.isStrike(guessNumber);
-        NumberBaseBallSystem.notifyGuessResult(scoreboard);
-        if (scoreboard.strike === 3) reGuess = false;
-        else reGuess = true;
-      } while (reGuess);
-      replay = await PlayingUser.isReplay();
-    } while (replay !== "2");
-    Console.closeConsole();
+  playGame() {
+    const correctNumber = this.createAnswerNumber();
+    console.log(correctNumber);
+    this.enterGuessNumber(correctNumber);
+  }
+
+  createAnswerNumber() {
+    let numberArr = [];
+    while (numberArr.length < 3) {
+      let number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!numberArr.includes(number)) numberArr.push(number);
+    }
+    return numberArr.join("");
+  }
+
+  enterGuessNumber(correctNumber) {
+    MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (guessNumber) => {
+      if (guessNumber.length !== 3) {
+        throw "input value length isn't correct!";
+      }
+      const guessNumberArray = guessNumber
+        .split("")
+        .map((value) => parseInt(value));
+      const correctNumberArray = correctNumber
+        .split("")
+        .map((value) => parseInt(value));
+      const scoreboard = { strike: 0, ball: 0 };
+
+      guessNumberArray.forEach((number, index) => {
+        if (correctNumberArray.includes(number)) {
+          if (correctNumberArray[index] === number) {
+            scoreboard.strike++;
+          } else {
+            scoreboard.ball++;
+          }
+        }
+      });
+
+      this.notifyGuessResult(scoreboard);
+
+      if (scoreboard.strike === 3) {
+        this.isReplay();
+        return;
+      }
+
+      this.enterGuessNumber(correctNumber);
+    });
+  }
+
+  notifyGuessResult(scoreboard) {
+    if (!scoreboard.strike && !scoreboard.ball) {
+      MissionUtils.Console.print("낫싱");
+    }
+
+    if (!scoreboard.strike && scoreboard.ball) {
+      MissionUtils.Console.print(`${scoreboard.ball}볼 `);
+    }
+
+    if (scoreboard.strike && !scoreboard.ball) {
+      MissionUtils.Console.print(`${scoreboard.strike}스트라이크`);
+      if (scoreboard.strike === 3) {
+        MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+      }
+    }
+
+    if (scoreboard.strike && scoreboard.ball) {
+      MissionUtils.Console.print(
+        `${scoreboard.ball}볼 ${scoreboard.strike}스트라이크`
+      );
+    }
+  }
+
+  isReplay() {
+    MissionUtils.Console.print(
+      "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
+    );
+    MissionUtils.Console.readLine("", (answer) => {
+      if (answer === "1") this.playGame();
+      if (answer === "2") MissionUtils.Console.close();
+    });
   }
 }
-
-// App.prototype.play();
-
-// const readLine = (question) => {
-//   return new Promise((resolve) => {
-//     MissionUtils.Console.readLine(question, (answer) => {
-//       resolve(answer);
-//     });
-//   });
-// };
-
-// const a = async () => {
-//   const guessNumber = await readLine("숫자를 입력해주세요 : ");
-//   console.log(guessNumber);
-// };
-
-// a();
 
 module.exports = App;
