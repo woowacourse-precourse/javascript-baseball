cf614156c1be5846df2a1326debae0b818b83f3d
lego37yoon <lego37yoon@outlook.com>
2022-11-08 18:29:03 +0900

    feat: init values to start new game
    Add random values to start new game.
    - Use array to save random values
    - Use `const` to protect array (not values inside array)
    - Use `push()` method to init values
    - Use Random.pickNumberInRange() method in MissionUtils, to push random values into array
    
    Remove unused notes
    - Remove note to run MISSION_UTILS.Console.close() in App.play()
    - Run Console.close() only in questionToContinueGame() function to prevent standard input not working

--- src/App.js
+++ src/App.js
@@ -4,11 +4,16 @@ class App {
   async play() {
     MISSION_UTILS.Console.print('숫자 야구 게임을 시작합니다.');
     newGamePlay();
-    //MISSION_UTILS.Console.close();
   }
 }
 
 async function newGamePlay() {
+  const COMPETITOR = [];
+  
+  while(COMPETITOR.length < 3) {
+    COMPETITOR.push(MISSION_UTILS.Random.pickNumberInRange(1, 9));
+  }
+
   questionToContinueGame();
 }
 
