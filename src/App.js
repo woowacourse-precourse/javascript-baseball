92e3f20ca794c3683ccf576a7138ff576aa5b86f
foundaway98 <foundaway98@naver.com>
2022-11-08 14:41:16 +0900

    <feat> : make getComputerNumber function
    make getComputerNumber function
    - use MissionUtils to get random number
    - it returns 3 random number array

--- src/App.js
+++ src/App.js
@@ -1,4 +1,16 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
+  getComputerNumber() {
+    const COMPUTER = [];
+    while (COMPUTER.length < 3) {
+      const NUMBER = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!COMPUTER.includes(NUMBER)) {
+        COMPUTER.push(NUMBER);
+      }
+    }
+    return COMPUTER;
+  }
   play() {}
 }
 
