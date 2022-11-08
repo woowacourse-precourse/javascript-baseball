41a5c75988e977a11f1c04b4ebd165dc3a70eac4
jjjjjeongwon <jjjjjwon21@gmail.com>
2022-11-08 21:16:55 +0900

    feat: [기능 3] 입력 문구 출력

--- src/App.js
+++ src/App.js
@@ -21,6 +21,13 @@ class App {
 
     return randomNumberList;
   }
+
+  requireInputRandomNumber(randomNumber) {
+    Console.print("숫자를 입력해주세요 : ");
+    Console.readLine(" ", (answer) => {
+      this.isRandomInputErrorCase(answer);
+    });
+  }
 }
 
 const app = new App();
