8c3c7ce47904b2e13e38ac8c3f296ad28d42144a
RooT_Kim <bdd3452@gmail.com>
2022-11-08 16:50:58 +0900

    기능: App.js 메소드 선언 구현할 기능 목록에 포함되어있는 메소드 선언

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+const MissionUtils = require('@woowacourse/mission-utils');
 class App {
-  play() {}
+	play() {
+		this.runGame();
+	}
+
+	runGame() {}
+
+	process() {}
+
+	getRandomNumber() {}
+
+	inputAnswer() {}
+
+	validateAnswer() {}
+
+	printResult() {}
+
+	getResult() {}
+
+	isNotThreeStrike() {}
+
+	validateOption() {}
 }
 
 module.exports = App;
