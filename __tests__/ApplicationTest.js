f9d707419391cfff9d61e4f2f79d16c1e9ed3754
leeharyung <lhr4884@naver.com>
2022-11-08 22:20:23 +0900

    test: get strike and ball number

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -1,5 +1,7 @@
 const App = require("../src/App");
 const MissionUtils = require("@woowacourse/mission-utils");
+const { getRandomNumber } = require("../src/utils/getRandomNumber");
+const { checkInput } = require("../src/utils/checkInput");
 
 const mockQuestions = (answers) => {
   MissionUtils.Console.readLine = jest.fn();
@@ -44,6 +46,22 @@ describe("숫자 야구 게임", () => {
 
     messages.forEach((output) => {
       expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
+  test("strike & ball 수 구하기", () => {
+    const app = new App();
+    const input = [1, 2, 3];
+    const answers = ["134", "245", "769", "123", "127"];
+    const result = {
+      strike: [1, 0, 0, 3, 2],
+      ball: [1, 1, 0, 0, 0],
+    };
+
+    answers.forEach((answer, index) => {
+      const { strike, ball } = app.getStrikeAndBallNumber(input, answer);
+      expect(strike).toBe(result.strike[index]);
+      expect(ball).toBe(result.ball[index]);
     });
   });
 
