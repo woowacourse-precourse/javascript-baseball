72ef27fb140b83d153e5810db67011c3fc93890e
RyuJoonSik <tldtksahtxja@naver.com>
2022-11-08 16:38:17 +0900

    test: AutomaticBallGenerator.test.js의 execute 테스트 수정
    "execute로 숫자를 생성"에서 "execute로 3개의 숫자를 생성"로 케이스를 수정했습니다.

--- src/Components/AutomaticBallGenerator/AutomaticBallGenerator.js
+++ src/Components/AutomaticBallGenerator/AutomaticBallGenerator.js
@@ -1,8 +1,18 @@
 const { Random } = require("@woowacourse/mission-utils");
 
 class AutomaticBallGenerator {
+  startNumber = 1;
+  endNumber = 9;
+  maxNumberCount = 3;
+
   execute() {
-    return Random.pickNumberInRange(1, 10);
+    const { startNumber, endNumber, maxNumberCount } = this;
+
+    return Random.pickUniqueNumbersInRange(
+      startNumber,
+      endNumber,
+      maxNumberCount
+    );
   }
 }
 
--- src/Components/AutomaticBallGenerator/AutomaticBallGenerator.test.js
+++ src/Components/AutomaticBallGenerator/AutomaticBallGenerator.test.js
@@ -1,10 +1,10 @@
 const AutomaticBallGenerator = require("./AutomaticBallGenerator");
 
 describe("Ball", () => {
-  test("execute로 숫자를 생성", () => {
+  test("execute로 3개의 숫자를 생성", () => {
     const BALL_GENERATOR = new AutomaticBallGenerator();
-    const NUMBER = BALL_GENERATOR.execute();
+    const NUMBER_ARRAY = BALL_GENERATOR.execute();
 
-    expect(typeof NUMBER).toBe("number");
+    expect(NUMBER_ARRAY).toHaveLength(3);
   });
 });
