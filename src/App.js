be0cb68eff0eaacd2b02e71387b1cf6c29e14315
Ji Seungha <jish0210@naver.com>
2022-11-08 16:23:38 +0900

    Feature 4 : Game Shutdown and Restart implemented

--- src/App.js
+++ src/App.js
@@ -84,6 +84,20 @@ function resultCheck(strike, ball){
   }
 }
 
+function endsGame(){
+  MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+  MissionUtils.Console.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. : ', command => {
+    if (command === '1') {
+      const app = new App();
+      app.startGame();
+    } 
+    else if (command === '2') {
+      MissionUtils.Console.close();
+    }
+    else throw new Error();
+  },);
+}
+
 class App {
   constructor() {
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
