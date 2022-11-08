96decb4c9e4e2604a2f267b7a75ec5e6dd4361cf
ShimSungBo <shimdb96@gmail.com>
2022-11-08 11:15:40 +0900

    refactor: play메서드 로직 수정 1. 반복문을 없애고 progress 함수 안 콜백으로 로직 변경 2. 컴퓨터의 랜덤 한 숫자를 progress의 매개변수로 전달

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-const checkProgress = require("./checkProgress");
 const progress = require("./progress");
+const computerAnswer = require("./makeComputerAnswer");
 const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
   play() {
-    let playing = false;
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
-    while (!playing) {
-      const PLAY_GAME = progress();
-      playing = PLAY_GAME;
-    }
+    const PLAY_GAME = progress(computerAnswer());
   }
 }
 
