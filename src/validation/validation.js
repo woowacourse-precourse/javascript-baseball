20a007ea87915ffd6558b765a7503e4fe0ef0cc3
kyeongwxx <kyeongwxx@gmail.com>
2022-11-08 12:53:38 +0900

    fix(validate.userInput()): 사용자 입력값 유효성 검사 함수 수정
    Before: input.match(/[1-9]/g)
    After: input.match(/[^[1-9]/g)
    
    추가된 테스트 코드 실행 시 발생하는 에러 해결

--- src/validation/validation.js
+++ src/validation/validation.js
@@ -1,14 +1,14 @@
 const { ERROR, RESTART } = require("../constants/constants.js");
 
 const validateLength = (input) => input.length === 3;
-const validateNumber = (input) => input.match(/[1-9]/g);
+const validateNumber = (input) => input.match(/[^1-9]/g);
 const validateUnique = (input) => new Set(input.split("")).size === 3;
 
 const validate = {
   userInput: (input) => {
     if (
       !validateLength(input) ||
-      !validateNumber(input) ||
+      validateNumber(input) ||
       !validateUnique(input)
     )
       throw new Error(ERROR.INVALID_USER_INPUT);
