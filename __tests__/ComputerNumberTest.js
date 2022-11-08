4c3dd961e2605c9267d7c20a023a16778f601939
seunghoria <102285042+seunghoria@users.noreply.github.com>
2022-11-08 14:19:30 +0900

    refactor: 상수 적용에 따른 테스트 코드 수정

--- __tests__/ComputerNumberTest.js
+++ __tests__/ComputerNumberTest.js
@@ -1,5 +1,5 @@
 const Referee = require("../src/Referee");
-const GAME_NUMBER_LENGTH = 3;
+const { GAME_LENGTH } = require("../src/constants/constants");
 
 describe("컴퓨터 입력값 유효성 검사 테스트", () => {
   test(`길이가 GAME_NUMBER_LENGTH인지 확인`, () => {
@@ -7,7 +7,7 @@ describe("컴퓨터 입력값 유효성 검사 테스트", () => {
     const computerNumber = referee.getComputerNumber();
     const computerLength = computerNumber.length;
 
-    expect(computerLength).toBe(GAME_NUMBER_LENGTH);
+    expect(computerLength).toBe(GAME_LENGTH);
   });
 
   test(`중복 숫자가 있는지 확인`, () => {
