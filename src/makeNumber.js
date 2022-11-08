73ed43719022e283883d72fe4899b704056952e5
jaesukpark77 <jaesukpark77@naver.com>
2022-11-08 12:42:06 +0900

    Remove unneeded file

--- src/makeNumber.js
+++
@@ -1,8 +0,0 @@
-const MissionUtils = require("@woowacourse/mission-utils");
-
-class makeNumber {
-  setAnswerNumber() {
-    const numbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3)
-    return numbers.join('')
-  }
-}
