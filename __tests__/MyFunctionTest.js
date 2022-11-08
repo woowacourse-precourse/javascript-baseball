fbb57ecd20cadb4710c08c4d2ac4ab43b2e59196
JangHoonSeok <orirori512@naver.com>
2022-11-08 20:41:25 +0900

    🧪 test: compareTwoNumbers, checkEnd 테스트 케이스 추가

--- __tests__/MyFunctionTest.js
+++ __tests__/MyFunctionTest.js
@@ -18,4 +18,38 @@ describe("기능 구현 테스트", () => {
 
     expect(isTruth).toBeTruthy();
   });
+
+  test("compareTwoNumbers함수 테스트 - 스트라이크, 볼 체크", () => {
+    const app = new App();
+    const answer = [1, 3, 5];
+    const number = [3, 2, 1];
+    const testNumberArray = app.compareTwoNumbers(answer, number);
+
+    expect(testNumberArray[0]).toEqual(0);
+    expect(testNumberArray[1]).toEqual(2);
+  });
+
+  test("checkEnd함수 테스트 - 3스트라이크 일 때", () => {
+    const app = new App();
+    const hint = [3, 0];
+    const test = app.checkEnd(hint);
+
+    expect(test).toBeTruthy();
+  });
+
+  test("checkEnd함수 테스트 - 3스트라이크 일 때", () => {
+    const app = new App();
+    const hint = [3, 0];
+    const test = app.checkEnd(hint);
+
+    expect(test).toBeTruthy();
+  });
+
+  test("checkEnd함수 테스트 - 3스트라이크가 아닐 때", () => {
+    const app = new App();
+    const hint = [2, 0];
+    const test = app.checkEnd(hint);
+
+    expect(test).toBeFalsy();
+  });
 });
