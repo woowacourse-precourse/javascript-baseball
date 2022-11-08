05a3b62c128aca496ef5ca47929c5a99a4fe96f3
HWI <dlwognl99@naver.com>
2022-11-08 13:31:33 +0900

    refactor: 게임 결과 출력 함수 클래스에서 분리 refactor: 불필요한 콘솔 로그 삭제

--- src/App.js
+++ src/App.js
@@ -18,24 +18,23 @@ class App {
       validation.checkUserInput(value);
       const resultCount = getCount(userInputValue, this.computerRandomValue);
       getResult(resultCount);
-      console.log(userInputValue);
       if (resultCount.strike !== 3) {
         return this.userInput();
       }
-      this.gameClear();
+      gameClear();
     });
   }
-  gameClear() {
-    const validation = new Validation();
-    MissionUtils.Console.readLine(
-      "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
-      (value) => {
-        validation.checkUserEndInput(value);
-        console.log(value);
-        askRestart(value);
-      }
-    );
-  }
+}
+
+function gameClear() {
+  const validation = new Validation();
+  MissionUtils.Console.readLine(
+    "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
+    (value) => {
+      validation.checkUserEndInput(value);
+      askRestart(value);
+    }
+  );
 }
 
 function askRestart(value) {
