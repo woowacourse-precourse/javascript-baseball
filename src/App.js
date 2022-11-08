73f1878f14add7e476009bd8f7929032d452e130
연 <mogoo22@naver.com>
2022-11-08 17:42:26 +0900

    feat: 컴퓨터는 1에서 9까지 서로 다른 임의의 수 3개를 선택한다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+const { Random, Console } = require('@woowacourse/mission-utils');
 class App {
-  play() {}
+  #answer = [];
+
+  play() {
+    this.#answer = App.#pickRandomNumbers(3);
+  }
+
+  static #pickRandomNumbers(count) {
+    const result = new Set();
+    while (result.size !== count) {
+      const random = Random.pickNumberInRange(1, 9);
+      result.add(random);
+    }
+    return Array.from(result);
+  }
 }
 
 module.exports = App;
