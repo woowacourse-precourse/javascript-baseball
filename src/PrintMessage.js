cc91ba0e616858d5212db9fe4c43e7536ebe93dd
h2002y <h2002y@naver.com>
2022-11-08 11:15:49 +0900

    refactor: Refactor PrintMessage.js

--- src/PrintMessage.js
+++ src/PrintMessage.js
@@ -1,52 +1,48 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
+const GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+const GAME_WIN_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+
 function printGameStart() {
-    const GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
     MissionUtils.Console.print(GAME_START_MESSAGE);
 }
 
 function printBallAndStrike(ball, strike){
+    const NUM_OF_BALL = `${ball}볼`;
+    const NUM_OF_STRIKE = `${strike}스트라이크`;
+    const NOTHINGCORRECT = "낫싱";
+
     if(ball > 0 && strike > 0){
-        printBoth(ball, strike);
+        printBoth(NUM_OF_BALL, NUM_OF_STRIKE);
     }
     else if(ball > 0 && strike === 0){
-        printNumOfBall(ball);
+        printNumOfBall(NUM_OF_BALL);
     }
     else if(strike > 0 && ball === 0){
-        printNumOfStrike(strike);
+        printNumOfStrike(NUM_OF_STRIKE);
     }
     else if(ball === 0 && strike === 0){
-        printNothing();
+        printNothing(NOTHINGCORRECT);
     }
 }
 
-function printBoth(numOfBall, numOfStrike) {
-    const NUM_OF_BALL = `${numOfBall}볼`;
-    const NUM_OF_STRIKE = `${numOfStrike}스트라이크`;
+function printBoth(NUM_OF_BALL, NUM_OF_STRIKE) {
     MissionUtils.Console.print(`${NUM_OF_BALL} ${NUM_OF_STRIKE}`);
 }
 
-function printNumOfStrike(numOfStrike) {
-    if(numOfStrike > 0) {
-        const NUM_OF_STRIKE_MESSAGE = `${numOfStrike}스트라이크`;
-        MissionUtils.Console.print(NUM_OF_STRIKE_MESSAGE);
-    }
+function printNumOfStrike(NUM_OF_STRIKE) {
+    MissionUtils.Console.print(NUM_OF_STRIKE);
 }
 
-function printNumOfBall(numOfBall) {
-    if(numOfBall > 0) {
-        const NUM_OF_BALL_MESSAGE = `${numOfBall}볼`;
-        MissionUtils.Console.print(NUM_OF_BALL_MESSAGE);
-    }
+function printNumOfBall(NUM_OF_BALL) {
+    MissionUtils.Console.print(NUM_OF_BALL);
 }
 
-function printNothing(){
-    const NOTHINGCORRECT = "낫싱";
+function printNothing(NOTHINGCORRECT){
     MissionUtils.Console.print(NOTHINGCORRECT);
 }
 
 function printGameWin() {
-    const GAME_WIN_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     MissionUtils.Console.print(GAME_WIN_MESSAGE);
 }
 
