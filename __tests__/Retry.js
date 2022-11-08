1b427c4ca5f92e9c9c87884e30b9b0904d6306e6
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2022-11-08 15:31:49 +0900

    test: 입력값의 유효성 체크 테스트

--- __tests__/Retry.js
+++ __tests__/Retry.js
@@ -11,3 +11,26 @@ describe("Computer에서 랜덤숫자 배열 추출", () => {
     expect(checkDuplicates).toBe(3);
   });
 });
+
+describe("UserInput과 관련된 테스트", () => {
+  const checkInputValid = new CheckInputValid();
+  test("유효성 체크: 1~9 사이의 숫자인지", () => {
+    expect(checkInputValid.checkUserInput(["1", "2", "3"])).toBe(
+      ERROR.USER_INPUT_PASS
+    );
+    expect(checkInputValid.checkUserInput(["1", "0", "2"])).toBe(
+      ERROR.USER_INPUT_RANGE
+    );
+  });
+  test("유효성체크: 입력값에 중복이 있는지", () => {
+    expect(checkInputValid.checkUserInput(["1", "1", "1"])).toBe(
+      ERROR.USER_INPUT_DUPLICATES
+    );
+  });
+
+  test("유효성체크: 입력값의 길이가 3인지", () => {
+    expect(checkInputValid.checkUserInput(["1", "2", "3", "4"])).toBe(
+      ERROR.USER_INPUT_LENGTH
+    );
+  });
+});
