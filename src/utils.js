c75b6e7c441bf84bea5d8b0c849e24126dbcf86b
dldnjswns31 <dldnjswns32@gmail.com>
2022-11-08 18:15:02 +0900

    feat: 사용자 입력 번호 유효성 검사 함수 추가

---
+++ src/utils.js
@@ -0,0 +1,18 @@
+const checkLength = (num) => {
+  return num.length === 3;
+};
+
+const checkZeroExist = (num) => {
+  return num.includes(0);
+};
+
+const checkDuplicate = (num) => {
+  const numberSet = new Set([...number]);
+  return numberSet.size === num.length;
+};
+
+module.exports({
+  checkLength,
+  checkZeroExist,
+  checkDuplicate,
+});
