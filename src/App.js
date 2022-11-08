7ddb8ac8fcfd157bed7d71c9437a0009b64ae503
HaroldBKim <sbookim2@gmail.com>
2022-11-08 17:08:42 +0900

    feat: 컴퓨터의 랜덤 숫자 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+const MissionUtils = require("@woowacourse/mission-utils");
 class App {
-  play() {}
+  play() { }
+
+  generateComputerNumber() {
+    const computerNumber = [];
+    while (computerNumber.length < 3) {
+      const randomNumber = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computerNumber.includes(randomNumber)) {
+        computerNumber.push(randomNumber);
+      }
+    }
+    return computerNumber;
+  }
 }
 
 module.exports = App;
