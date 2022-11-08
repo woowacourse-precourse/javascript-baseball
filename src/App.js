4a596714aa9862cc5d2bda369042281f4b0e9216
윤동주 <yoondj98@naver.com>
2022-11-08 22:27:54 +0900

    feat: 컴퓨터의 랜덤값 생성 및 리턴하는 기능 구현 - createRandomComNum 함수

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
   play() {}
+
+  createRandomComNum() {
+    const randomNums = [];
+    while (randomNums.length < 3) {
+      const randomNum = MissionUtils.Random.pickNumberInRange(1, 10);
+      if (!randomNums.includes(randomNum)) {
+        randomNums.push(randomNum);
+      }
+    }
+    return randomNums;
+  }
+  
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
