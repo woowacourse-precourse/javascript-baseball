8307ff657177f15d37877d2252bbd01f96bd0824
Yoonsik-Shin <sys25800994@gmail.com>
2022-11-08 23:46:13 +0900

    feat : 컴퓨터 숫자값 3개 정하기, 결과값 저장

--- src/App.js
+++ src/App.js
@@ -7,6 +7,26 @@ class App {
         const computerDefinedArr = this.computerDefine();
         this.userInputValue(computerDefinedArr, this.resetResult());
     }
+    // 2. 컴퓨터 숫자값 3개 정하기
+    computerDefine() {
+        const computerDefinedArr = [];
+        while (computerDefinedArr.length < 3) {
+            const number = MissionUtils.Random.pickNumberInRange(1, 9);
+            if (!computerDefinedArr.includes(number)) {
+                computerDefinedArr.push(number);
+            }
+        }
+        return computerDefinedArr;
+    }
+
+    // 2-1. 결과값 저장
+    resetResult() {
+        const result = {
+            strike: 0,
+            ball: 0,
+        };
+        return result;
+    }
 }
 
 const app = new App();
