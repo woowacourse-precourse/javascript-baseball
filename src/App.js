09cc815a66f0917aa02f6923705a28ef9215e479
leesoomin <soomin@leesoominui-MacBookPro.local>
2022-11-08 21:57:36 +0900

    fix: 예외처리 함수 세분화 하기

--- src/App.js
+++ src/App.js
@@ -8,25 +8,65 @@ class App {
   // 사용자 입력 받기 1 예외처리
   checkExceptionOne(num){
     if(isNaN(num)){
-      throw new Error("숫자를 입력하세요.");
-    }else if(num.charAt(0) === 0 || num.charAt(1) === 0 || num.charAt(2) === 0){
-      throw new Error("1-9 사이의 숫자를 3번 입력하세요.");
-    } else if(num.charAt(0) === " " || num.charAt(1) === " " || num.charAt(2) === " "){
+      throw  "숫자를 입력하세요.";
+    }else if(this.checkSpace(num)){
       throw new Error("공백을 포함하지 않는 숫자를 입력하세요.");
+    }else if(this.checkZero(num)){
+      throw  "1-9 사이의 숫자를 3번 입력하세요.";
     } else if(num.length !== 3){
-      throw new Error("세자리 숫자를 입력하세요.");
-    } else if(num.charAt(0) === num.charAt(1)|| num.charAt(0) === num.charAt(2) || num.charAt(1) === num.charAt(2)){
-      throw new Error("중복되지 않는 숫자를 입력하세요.");
+      throw "세자리 숫자를 입력하세요.";
+    } else if(this.checkOverlap(num)){
+      throw "중복되지 않는 숫자를 입력하세요.";
     } else {
       return 1;
     }
   }
+
+  // 입력 받은 숫자에 0 이 있는지 체크
+  checkZero(num){
+    if(num.charAt(0) === 0){
+      return true;
+    } else if(num.charAt(1) === 0){
+      return true;
+    } else if(num.charAt(2) === 0){
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+   // 입력 받은 숫자에 공백이 있는지 체크
+  checkSpace(num){
+    if(num.charAt(0) === " "){
+      return true;
+    } else if(num.charAt(1) === " "){
+      return true;
+    } else if(num.charAt(2) === " "){
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+   // 입력 받은 숫자에 중복s이 있는지 체크
+  checkOverlap(num){
+    if(num.charAt(0)===num.charAt(1)){
+      return true;
+    } else if(num.charAt(0)===num.charAt(2)){
+      return true;
+    } else if(num.charAt(1)===num.charAt(2)){
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   // 사용자 입력 받기 2 예외처리
   checkExceptionTwo(input){
     if(input === "1" || input === "2"){
       return 1;
     } else {
-      throw new Error("1 또는 2를 입력하세요.");
+      throw '1 또는 2를 입력하세요.';
     }
   }
 
@@ -71,6 +111,7 @@ class App {
       }
     }
     this.printResult(strike, ball);
+
     if(strike === 3){
       return true;
     } else {
@@ -80,12 +121,12 @@ class App {
 
   // 사용자 input 받기
   getUserNum(computer){
-    let user;
+    let user = 0;
     do{
       MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (input_number)=> {
+        this.checkExceptionOne(input_number);
         user = input_number;
       });
-      this.checkExceptionOne(user);
     } while(!this.compareNum(computer, user));
   }
 
@@ -97,9 +138,9 @@ class App {
       this.getUserNum(com_num);
       MissionUtils.Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
       MissionUtils.Console.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.', (answer)=>{
+        this.checkExceptionTwo(answer);
         game_status = answer;
       });
-      this.checkExceptionTwo(game_status);
     }
   }
 
