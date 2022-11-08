5a21d9fc3f78223a414e48545418dc11ca8fd5ab
Y2IN <dpdls0228@daum.net>
2022-11-08 17:34:18 +0900

    refactor : if문 3항연산자로 변경

--- src/baseball/BaseballGame.js
+++ src/baseball/BaseballGame.js
@@ -48,11 +48,11 @@ class BaseballGame {
   printResult = (strike, ball) => {
     if (strike === 0 && ball === 0) {
       return Console.print("낫싱");
-    } else if (ball > 0 && strike === 0) {
-      Console.print(`${ball}볼`);
-    } else if (ball === 0 && strike > 0) {
-      Console.print(`${strike}스트라이크`);
-    } else Console.print(`${ball}볼 ${strike}스트라이크`);
+    }
+    const strikePrint = strike > 0 ? `${strike}스트라이크` : "";
+    ball === 0
+      ? Console.print(`${strikePrint}`)
+      : Console.print(`${ball}볼 ${strikePrint}`);
   };
 
   isValidRestart = (OneOrTwo) => {
