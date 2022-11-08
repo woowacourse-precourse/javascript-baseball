4f8c21c1679776bd995b5be2249c8da1797bcb2a
yunjjeongjo <two6885@gmail.com>
2022-11-08 23:03:41 +0900

    feat: hasDuplicateNumber 함수 추가
    validateInputValue내에서 userInput의 중복 숫자 존재 여부를 판별하는 함수를 추가했습니다.

--- src/utils/validateInputValue.js
+++ src/utils/validateInputValue.js
@@ -1,11 +1,19 @@
 const { ANSWER_LENGTH } = require("../constants/constants");
 
 function validateInputValue(userInput) {
+  function hasDuplicateNumber(number) {
+    const numberSet = new Set(number.split(""));
+    return numberSet.size !== number.length ? true : false;
+  }
+
 
   if (isNaN(Number(userInput))) {
     return false;
   }
   if (userInput.length !== ANSWER_LENGTH) {
+    return false;
+  }
+  if (hasDuplicateNumber(userInput)) {
     return false;
   }
   return true;
