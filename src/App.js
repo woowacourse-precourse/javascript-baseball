637c6bda413d60ba6aba6deabba50b46f06a0960
wldnd2 <jiwoong000628@gmail.com>
2022-11-08 18:52:08 +0900

    feat: 초기 세팅

--- src/App.js
+++ src/App.js
@@ -1,4 +1,14 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const Random = MissionUtils.Random;
+const Console = MissionUtils.Console;
+
 class App {
+  constructor() {
+    this.computer_random_number = [];
+    this.strike = 0;
+    this.ball = 0;
+    this.nothing = "낫싱";
+  }
   play() {}
 }
 
