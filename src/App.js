325195cecef684dab84e0fc3c564ddcc205a9ad7
cofla <cofla159@naver.com>
2022-11-08 23:58:46 +0900

    feat

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import MissionUtils from "@woowacourse/mission-utils";
 
 class App {
   play() {
-    const answer = MissionUtils.Random.pickNumberInRange(111, 999);
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
     let isGamePlaying = true;
     while (isGamePlaying) {
@@ -12,25 +11,65 @@ class App {
   }
 }
 async function startGame() {
+  const answer = MissionUtils.Random.pickNumberInRange(111, 999).toString();
   try {
-    const userNumber = await getUserNumber();
+    const userNumber = await getInput();
+    const result = getResult(userNumber, answer);
+    if (result === "3스크라이크") {
+      MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
   } catch (error) {
-    MissionUtils.Console.print(
-      `잘못된 입력입니다. 게임을 종료합니다.: ${error}`
-    );
+    MissionUtils.Console.print(`잘못된 입력입니다. 게임을 종료합니다.`);
     restartOrExit(2);
   }
 }
-function getUserNumber() {
+function getInput() {
   return new Promise((resolve, reject) => {
     MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (input) => {
+      if (isNaN(Number(input))) throw new Error("잘못된 입력입니다.");
       resolve(input);
     });
   });
 }
+function getRestartOrExit() {
+  return new Promise((resolve, reject) => {
+    MissionUtils.Console.readLine(
+      "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. ",
+      (input) => {
+        resolve(input);
+      }
+    );
+  });
+}
 function restartOrExit(input) {
-  if (input == 1) return true;
-  else return false;
+  if (input === 1) {
+    return true;
+  }
+  return false;
+}
+function getResult(inputNumber, answer) {
+  const strike = countStrike(inputNumber, answer);
+  const ball = countBall();
+  const nothing = 3 - strike - ball;
+  if (nothing === 3) return "낫싱";
+  if (ball === 0) return `${strike}스크라이크`;
+  if (strike === 0) return `${ball}볼`;
+  return `${ball}볼 ${strike}스크라이크`;
+}
+function countStrike(inputNumber, answer) {
+  let count = 0;
+  for (let i = 0; i < 3; i++) {
+    if (answer[i] === inputNumber[i]) count++;
+  }
+  return count;
+}
+function countBall(inputNumber, answer) {
+  let count = 0;
+  for (let i = 0; i < 3; i++) {
+    if (answer.includes(inputNumber[i]) && answer[i] !== inputNumber[i])
+      count++;
+  }
+  return count;
 }
 
 const app = new App();
