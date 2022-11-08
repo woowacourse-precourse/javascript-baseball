ca581f5bb4ce5929bd642553ea06269a2ba6e7d5
Sangmin Lee <gink.ss00@gmail.com>
2022-11-08 21:12:43 +0900

    feat: 메세지 출력 테스트 - 컴퓨터와 플레이어 수 비교 결과 메세지 출력 테스트 구현
    Printer 클래스의 showResult() 메소드 테스트와 같다.
    
    - 계산을 통해 얻은 볼과 스트라이크 카운트에 맞춰 결과 메세지를 출력한다.
    - 볼과 스트라이크 아무 것도 없다면 '낫싱'이 출력된다.

--- __tests__/PrintTest.js
+++ __tests__/PrintTest.js
@@ -16,4 +16,32 @@ describe('메세지 출력 테스트', () => {
 
     expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(startMessage));
   });
+
+  test('컴퓨터의 숫자와 플레이어 숫자의 비교 결과 메세지 출력 테스트', () => {
+    const printer = new Printer();
+    const answers = [
+      { ballCount: 0, strikeCount: 0 },
+      { ballCount: 1, strikeCount: 0 },
+      { ballCount: 0, strikeCount: 2 },
+      { ballCount: 2, strikeCount: 1 },
+      { ballCount: 1, strikeCount: 1 },
+      { ballCount: 3, strikeCount: 0 },
+      { ballCount: 0, strikeCount: 3 },
+    ];
+    const messages = [
+      '낫싱',
+      '1볼',
+      '2스트라이크',
+      '2볼 1스트라이크',
+      '1볼 1스트라이크',
+      '3볼',
+      '3스트라이크',
+    ];
+    const logSpy = getLogSpy();
+
+    messages.forEach((message, index) => {
+      printer.showResult(answers[index].ballCount, answers[index].strikeCount);
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(message));
+    });
+  });
 });
