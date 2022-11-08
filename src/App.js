b494dc759957a11c9ca9e77961d0f42757b88ce6
terry <kdjtiger@naver.com>
2022-11-08 18:41:07 +0900

    feat: 컴퓨터 숫자 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  getComputerNumber () {
+    const computer = [];
+    while (computer.length <= 2) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+    return computer;
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
