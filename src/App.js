c630dd3002a899d086d97743f7da5407b6ae75f8
rickyshu <hoinleekk@naver.com>
2022-11-08 15:44:50 +0900

    feat: 시작 문구 출력하기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
-class App {
-  play() {}
-}
+const { Console } = require("@woowacourse/mission-utils");
+const { MESSAGE } = require("./constant/constants");
+const makeRandomNumber = require("./utils/randomNumber");
 
+class App {
+  play() {
+    //시작 메세지 출력하기!
+    Console.print(MESSAGE.START);
+  }
+}
+const app = new App();
+app.play();
+app.gameSetter();
 module.exports = App;
