b3915358b892fa56f7212438e9335caefe796acf
s0ojin <onepst@hanyang.ac.kr>
2022-11-08 13:31:03 +0900

    fix : missionUtils 적용

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+const MissionUtils = require('@woowacourse/mission-utils');
 class App {
   constructor() {
     this.printGreeting();
@@ -5,13 +6,13 @@ class App {
   }
 
   printGreeting() {
-    Console.print('숫자 야구 게임을 시작합니다.');
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
   }
 
   makeRandomNumber() {
     let randomNumSet = new Set();
     while (3) {
-      randomNumSet.add(Random.pickNumbersInRange(1, 9));
+      randomNumSet.add(MissionUtils.Random.pickNumberInRange(1, 9));
     }
     return Array.from(randomNumSet);
   }
