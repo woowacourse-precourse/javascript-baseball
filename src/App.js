452670d50d2838005978906b699a7b88df9caa82
jisu-kim <kss2460@naver.com>
2022-11-08 20:23:37 +0900

    feat: initNumber 함수 이식

--- src/App.js
+++ src/App.js
@@ -13,6 +13,17 @@ class App {
   showPrint(messages) {
     MissionUtils.Console.print(messages);
   }
+
+  initNumber() {
+    const computer = [];
+    while (computer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
+    return computer;
+  }
 }
 
 const app = new App();
