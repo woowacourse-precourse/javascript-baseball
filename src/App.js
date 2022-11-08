db663718964c5ed44dab6423b394eff6f60c5101
myung-j-p <jmpack97@gmail.com>
2022-11-08 20:22:43 +0900

    Fix: 비동기 기능의 동기적 처리를 위한 함수 구조변경, 세분화

--- src/App.js
+++ src/App.js
@@ -2,47 +2,56 @@ const MissionUtils = require("@woowacourse/mission-utils");
 
 
 class App {
+  constructor(){}
+
   play() {
-    console.log('숫자 야구 게임을 시작합니다.')
+    console.log('숫자 야구 게임을 시작합니다.');
+    return this.startGame();
   }
 
   startGame() {
-    // 1.컴퓨터 리스트 생성
+    this.computerNumber = [];
+    this.makeComputerNumber ()
+    return this.getUserNumber()
+  }
+
+  getUserNumber () {
+    MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (userNumber) => {
+      return this.inputChecker(userNumber)
+    });
+  }
+
+  inputChecker (userNumber) {
+    if (userNumber.length !== 3){
+      throw '3자리 수를 입력하세요.'
+    }
+    else if (this.duplicationCheck(userNumber) == false){
+      throw '서로 중복되지 않는 수를 입력하세요'
+    }
+    return this.readyResultCheck(userNumber)
+  }
+
+  makeComputerNumber () {
     const computerLs = new Computer();
-    const computer = computerLs.makeComNumLs();
-    // 2.유저 리스트 입력
-    const userLs = new User();
-    const user = userLs.getUserLs();
+    let makeComputerNumber = computerLs.makeComNumLs();
+    makeComputerNumber = makeComputerNumber.join('');
 
-    this.checkGameResult(computer, user);
+    return this.computerNumber.push(makeComputerNumber)
   }
 
-  checkGameResult(computerNum, userNum) {
-    let strike = this.countStrike(computerNum, userNum);
-    let ball = this.countBall(computerNum, userNum, strike[1]);
 
-    if (strike == 3) {
-      console.log('3개의 숫자를 모두 맞히셨습니다! 게임종료');
-      return this.chooseReStart();
-    }
-    
-    if (strike == 0 && ball == 0){
-      console.log('낫싱');
-      return this.startGame()
-    }
 
-    else {
-      console.log(scoreSpeaker(ball, strike))
-      return this.startGame()
-    }
-
+  readyResultCheck (userNumber) {
+    const computerNumber = this.computerNumber[0]
+    console.log('컴퓨터 넘버 : ', computerNumber)
+    return this.checkStrikeResult(computerNumber, userNumber)
   }
 
-  scoreSpeaker(ball, strike) {
-    const announcement = []
-    if (ball > 0) announcement.push(ball + ' 볼');
-    if (strike > 0) announcement.push(strike + ' 스트라이크');
-    return announcement.join(' ')
+
+
+  checkStrikeResult(computerNum, userNum) {
+    let strikeResultAndIndex = this.countStrike(computerNum, userNum);
+    return this.checkBallResult(computerNum, userNum, strikeResultAndIndex)
   }
 
   countStrike(computerNum, userNum) {
@@ -57,8 +66,24 @@ class App {
     return [strike, strikeIndex]
   }
 
-  exceptStrikedNumbers(computerNum, userNum, strikeIndex) {
+  checkBallResult(computerNum, userNum, strikeResultAndIndex) {
+    const strikeResult = strikeResultAndIndex[0];
+    const strikeIndex = strikeResultAndIndex[1];
 
+    const ballResult = this.countBall(computerNum, userNum, strikeIndex);
+
+    return this.scoreChecker([strikeResult, ballResult], computerNum)
+  }
+
+  countBall(computerNum, userNum, strikeIndex) {
+    const listOfComAndUser = this.exceptStrikedNumbers(computerNum, userNum, strikeIndex);
+    const comLs = listOfComAndUser[0];
+    const userLs = listOfComAndUser[1];
+
+    return comLs.filter(item => userLs.includes(item)).length
+  }
+
+  exceptStrikedNumbers(computerNum, userNum, strikeIndex) {
     let comLs = [...computerNum];
     let userLs = [...userNum];
 
@@ -72,43 +97,44 @@ class App {
     return [comLs, userLs]
   }
 
-  countBall(computerNum, userNum, strikeIndex) {
-    const listOfComAndUser = this.exceptStrikedNumbers(computerNum, userNum, strikeIndex);
-    const comLs = listOfComAndUser[0];
-    const userLs = listOfComAndUser[1];
 
-    return comLs.filter(item => userLs.includes(item)).length
+  scoreChecker(results, computerNum) {
+    const strikeResult = results[0];
+    const ballResult = results[1];
+
+
+    if (strikeResult == 3) {
+      console.log('3개의 숫자를 모두 맞히셨습니다! 게임종료');
+      return this.chooseReStart();
+    }
+    
+    if (strikeResult == 0 && ballResult == 0){
+      console.log('낫싱');
+      return this.getUserNumber(computerNum)
+    }
+
+    else {
+      return this.scoreSpeaker(ballResult, strikeResult, computerNum)
+      }
+      
+  }
+
+  scoreSpeaker(ballResult, strike, computerNum) {
+    const announcement = []
+    if (ballResult > 0) announcement.push(ballResult + ' 볼');
+    if (strike > 0) announcement.push(strike + ' 스트라이크');
+    console.log(announcement.join(' '));
+    setTimeout(() => {return this.getUserNumber(computerNum);})
   }
 
   chooseReStart() {
-    MissionUtils.Console.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.', (answer) => {
+    MissionUtils.Console.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.', (userNumber) => {
       MissionUtils.Console.close();
-      if (answer == 1) return this.startGame()
-      else if (answer == 2) return 
-      else if (answer !== 1 || answer !== 2) throw '1이나 2를 입력하세요.'
+      if (userNumber == 1) return this.startGame()
+      else if (userNumber == 2) return 
+      else if (userNumber !== 1 || userNumber !== 2) throw '1이나 2를 입력하세요.'
       else '1이나 2를 입력하세요.'
     })
-  }
-}
-
-class Computer extends App {
-  makeComNumLs() {
-    return String(MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3));
-  }
-}
-
-class User extends App {
-  getUserLs () {
-    MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (answer) => {
-      if (answer.length !== 3){
-        throw '3자리 수를 입력하세요.'
-      }
-      else if (this.duplicationCheck(answer) == false){
-        throw '서로 중복되지 않는 수를 입력하세요'
-      }
-      MissionUtils.Console.close();
-      return answer
-    });
   }
 
   duplicationCheck (numForCheck){
@@ -118,6 +144,11 @@ class User extends App {
   }
 }
 
+class Computer extends App {
+  makeComNumLs() {
+    return MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
+  }
+}
+
 module.exports = App;
-module.exports = Computer;
-module.exports = User;
+
