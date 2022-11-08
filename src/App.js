af4e9183d60a205b187cb3f8116a82709a5156e5
runkey <csi9509@naver.com>
2022-11-08 20:29:14 +0900

    Feat: 숫자 랜덤 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+const { Console, Random } = require("@woowacourse/mission-utils"); // https://github.com/woowacourse-projects/javascript-mission-utils
+let com = [];
 class App {
-  play() {}
+  play() {
+    Console.print("숫자 야구 게임을 시작합니다.");
+    com = [];
+    randNum();
+    // guessNum();
+  }
 }
+
+function randNum() {
+  while (com.length < 3) { // com 이란 변수에 3개의 숫자를 넣을 때 까지 반복
+    const randNumPicked = Random.pickNumberInRange(1, 9); // 1 ~ 9 사이의 랜덤숫자
+    if (!com.includes(randNumPicked)) { // 같은 숫자 포함 안 되게 하기
+      com.push(parseInt(randNumPicked, 10)); // com 배열에 숫자 넣기
+    }
+  }
+}
+
 
 module.exports = App;
