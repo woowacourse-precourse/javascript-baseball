37cb9275e7a93b0fc60200c00e8097d90467630c
dudgp128 <20181048@sungshin.ac.kr>
2022-11-08 16:21:35 +0900

    STYLE : 비구조화 할당 형태으로 라이브러리 가져오기

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-const MissionUtils = require("@woowacourse/mission-utils");
+const { Console, Random } = require("@woowacourse/mission-utils/");
 class App {
   constructor(gameResult) {
     this.gameResult = gameResult;
@@ -12,7 +12,7 @@ class App {
   setComputerNum() {
     const NumSet = new Set();
     while (NumSet.size < 3) {
-      NumSet.add(MissionUtils.Random.pickNumberInRange(1, 9));
+      NumSet.add(Random.pickNumberInRange(1, 9));
     }
 
     return Number([...NumSet].join(""));
@@ -20,8 +20,8 @@ class App {
 
   gameStart(computer) {
     if (this.gameResult == undefined)
-      MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
-    MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (input) => {
+      Console.print("숫자 야구 게임을 시작합니다.");
+    Console.readLine("숫자를 입력해주세요 : ", (input) => {
       if (!this.isValidInput(input)) {
         throw "유효하지 않은 숫자입니다.";
       }
@@ -85,10 +85,10 @@ class App {
 
     switch (ball) {
       case null:
-        MissionUtils.Console.print(strike ?? "낫싱");
+        Console.print(strike ?? "낫싱");
         break;
       default:
-        MissionUtils.Console.print(ball + (strike ? " " + strike : ""));
+        Console.print(ball + (strike ? " " + strike : ""));
     }
   }
 
@@ -97,13 +97,13 @@ class App {
       "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n" +
       "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n";
 
-    MissionUtils.Console.readLine(userQuestion, (input) => {
+    Console.readLine(userQuestion, (input) => {
       switch (input) {
         case "1":
           return this.play();
         case "2":
-          MissionUtils.Console.print("게임 종료");
-          MissionUtils.Console.close();
+          Console.print("게임 종료");
+          Console.close();
           break;
         default:
           throw "유효하지 않은 입력값입니다.";
