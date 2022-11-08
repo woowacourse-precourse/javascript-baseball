842025b3c46f76673f398df8a7c38cc057d1dbf0
lineJeong <line.jeong32@gmail.com>
2022-11-08 19:42:58 +0900

    FEAT: 사용자가 입력한 수에 대한 유효성을 검사하는 기능

--- src/App.js
+++ src/App.js
@@ -21,8 +21,18 @@ class App {
 
   getUserInput() {
     Console.readLine("숫자를 입력해주세요 : ", (input) => {
-      parseInt(input);
+      this.checkUserInput(input);
     });
+  }
+
+  checkUserInput(input) {
+    if (isNaN(parseInt(input))) throw Error("숫자를 입력해주세요.");
+    if (input.length !== 3) throw Error("3자릿수의 숫자를 입력해주세요.");
+    if (new Set(input).size < 3) throw Error("중복된 숫자는 제외해주세요.");
+    if (!/^[1-9]{3}$/.test(input))
+      throw Error("1~9 사이의 숫자로 구성된 3자릿수의 숫자를 입력해주세요.");
+
+    return input.split("").map((it) => parseInt(it));
   }
 }
 
