4cde17354ddfa1c9ab546879696a8d2966dd94b5
rayark1 <max980203@snu.ac.kr>
2022-11-08 16:25:56 +0900

    refactor(src/BaseballCounter.js): 단항 연산자 제거, 큰 따옴표 제거

--- src/BaseballCounter.js
+++ src/BaseballCounter.js
@@ -1,12 +1,12 @@
 class BaseBallCounter {
   static calculateResult(userInput, answer) {
-    const inputArray = userInput.split("").map(Number);
+    const inputArray = userInput.split('').map(Number);
     const answerArray = answer;
     const ball = this.countBall(inputArray, answerArray);
     const strike = this.countStrike(inputArray, answerArray);
 
     if (ball === 0 && strike === 0) {
-      return "낫싱";
+      return '낫싱';
     }
     if (ball === 0) {
       return `${strike}스트라이크`;
@@ -18,21 +18,21 @@ class BaseBallCounter {
     return `${ball}볼 ${strike}스트라이크 `;
   }
 
-  static countBall(userInput, answer) {
+  static countBall(userInputs, answer) {
     let ball = 0;
-    userInput.forEach((userInput, index) => {
+    userInputs.forEach((userInput, index) => {
       if (answer.includes(userInput) && userInput !== answer[index]) {
-        ball++;
+        ball += 1;
       }
     });
     return ball;
   }
 
-  static countStrike(userInput, answer) {
+  static countStrike(userInputs, answer) {
     let strike = 0;
-    userInput.forEach((userInput, index) => {
+    userInputs.forEach((userInput, index) => {
       if (userInput === answer[index]) {
-        strike++;
+        strike += 1;
       }
     });
     return strike;
