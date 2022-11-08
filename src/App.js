263c3dcd1dc9db5f4348053bc6b54956efd5cf2c
Ji Seungha <jish0210@naver.com>
2022-11-08 15:23:13 +0900

    Online TestCase Test

--- src/App.js
+++ src/App.js
@@ -11,26 +11,25 @@ function generateRandomNumbers( ){
   return numberArray;
 }
 
-function validationCheck(answer){
-  const digitsOnly = string => [...string].every(c => '123456789'.includes(c))
-  if(answer.length != 3){
-    throw new Error("Invalid length (Should be 3) : " + answer.length);
-  }
-  else if(!digitsOnly(answer)){
-    throw new Error("Invalid input (Should be a number from 1 to 9) : " + answer);
-  }
-  else if(answer[0] == answer[1] || answer[1] == answer[2] || answer[2] == answer[0]){
-   throw new Error("Invalid input (Should be 3 different numbers) : " + answer); 
-  }
-  else{
-    answerToIntArr = [0,0,0];
-    for(var i  = 0; i < 3; i++){
-      answerToIntArr[i] = Number(answer[i]);
-    }
-    return answerToIntArr;
-  }  
+function answerToIntArr(inputs){
+  return [Number(inputs[0]),Number(inputs[1]),Number(inputs[2])];
 }
 
+function validationCheck(answer){
+  const digitsOnly = string => [...string].every(c => '123456789'.includes(c))
+  if(answer.length != 3){ //Length should be 3
+    throw new Error();
+  }
+  else if(!digitsOnly(answer)){ //Input should be valid number
+    throw new Error();
+  }
+  else if(answer[0] == answer[1] || answer[1] == answer[2] || answer[2] == answer[0]){ //Input should be 3 different numbers
+    throw new Error();
+  }
+  else{
+    return answerToIntArr(answer);
+  }  
+}
 
 function countStrike(guess, computer){
   var strike = 0;
@@ -54,34 +53,44 @@ function countBall(guess, computer){
   return ball;
 }
 
-function printResult(strike, ball){
-  if(strike == 0 && ball == 0){
+function resultCheck(strike, ball){
+  if(strike == 3){
+    MissionUtils.Console.print("3스트라이크");
+    return true;
+  }
+  else if(strike == 0 && ball == 0){
     MissionUtils.Console.print("낫싱");
+    return false;
   }
   else if (strike == 0){
     MissionUtils.Console.print("" + ball + "볼");
+    return false;
   }
   else if (ball == 0){
     MissionUtils.Console.print("" + strike + "스트라이크");
+    return false;
   }
   else{
     MissionUtils.Console.print("" + ball + "볼 " + strike + "스트라이크");
+    return false;
   }
 }
 
 function endsGame(){
+  MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+  MissionUtils.Console.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
   MissionUtils.Console.readLine('', (answer) => {
     if(answer[0] == '1' || answer[0] == 1){
+      startGame();
       return false;
     }
     else if(answer[0] == '2' || answer[0] == 2){
       return true;
     }
     else{
-      return true;
+      throw new Error();
     }
   });
-  
 }
 
 function startGame(){
@@ -94,23 +103,21 @@ function startGame(){
     });
     var strike = countStrike(guess, computer);
     var ball = countBall(guess, computer);
-    printResult(strike, ball);
-    if(strike == 3){
-      MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-      MissionUtils.Console.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");      
+    if(resultCheck(strike, ball)){
       isGameEnd = true;
     }
   }
+  endsGame();
   return 1;
 }
 
 class App {
-  play(){
+  constructor(){
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+  }
+
+  play(){
     startGame();
-    if(!endsGame()){
-      startGame();
-    }
   }
 }
 
