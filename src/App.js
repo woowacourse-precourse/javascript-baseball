ad0917db794c2c0fa53a103c64471f8c87c63354
aram <snsdl0905@naver.com>
2022-11-08 13:46:09 +0900

    feat: ball, strike의 결과를 알려주는 기능

--- src/App.js
+++ src/App.js
@@ -38,6 +38,18 @@ class App {
         continue;
       }
     }
+
+    let message = this.print_message(strike, ball);
+    MissionUtils.Console.print(message);
+  }
+
+  print_message(strike, ball) {
+    let save_message = "";
+    if (ball > 0) save_message += `${ball}볼 `;
+    if (strike > 0) save_message += `${strike}스트라이크`;
+    if (ball === 0 && strike === 0) save_message = '낫싱';
+    if (strike === 3) save_message = '3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료';
+    return save_message;
   }
 }
 
