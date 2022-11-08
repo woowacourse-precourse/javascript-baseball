ec6797f3d5d68e86199974059af53f2ed4ccf9c4
myeongheonhong <ruddyh7076@gmail.com>
2022-11-08 18:02:06 +0900

    [ Feat ] : 숫자야구에 필요한 생성자 멤버변수 선언.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 
 class App {
+    constructor() {
+        this.computer = this.getComputerNum();
+        this.user = [];
+        this.gameOver = false;
+        this.ball = 0;
+        this.strike = 0;
+        this.out = false;
+    }
+
     play() {}
 }
 
