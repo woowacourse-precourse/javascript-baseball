caff27b94aba2f8e9e3b3c80b943754b6c3aae03
Doyoung.Kim <dyflower@nate.com>
2022-11-08 16:28:46 +0900

    feat : 비교한 결과를 출력하는 기능

--- src/App.js
+++ src/App.js
@@ -66,4 +66,13 @@ function answerCheck(inputNum, answer) {
   if (true) return `${ball}볼 ${strike}스트라이크`;
 }
 
+function scoreCheck(answercheck, answer) {
+  if (answercheck === '3스트라이크') {
+    MissionUtils.Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+    restartGame();
+  } else {
+    inputNumber(answer);
+  }
+}
+
 module.exports = App;
