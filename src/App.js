5ba04ce3c4124e4ac536642ef916a27b4cbc052b
Hypark2018 <hayoungpark2018@gmail.com>
2022-11-08 13:43:15 +0900

    🖊 feat: add start method & MissionUtils library on App.js
    start method: print opening ment of baseball-game.
    
    MissionUtils library: provided library to use in implement of second-week mission.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
-class App {
-  play() {}
+const MissionUtils = require("@woowacourse/mission-utils");
+class BaseBallGame {
+  constructor() {
+    this.start();
+  }
+
+  start() {
+    const OPENING_MENT = "숫자 야구 게임을 시작합니다.";
+    MissionUtils.Console.print(OPENING_MENT);
+  }
 }
 
-module.exports = App;
+const game = new BaseBallGame;
+game.start();
+
+module.exports = BaseBallGame;
