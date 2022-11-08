700e246cbe97f6d6d72e9ef40080dc22ad50d66b
JuhyunLim <87933367+userJu@users.noreply.github.com>
2022-11-08 21:00:40 +0900

    Refactor: pickNumberRange 함수명 변경

--- src/utils/MissionUtils.js
+++ src/utils/MissionUtils.js
@@ -16,13 +16,13 @@ const { NUMBER_ONE_TO_NINE } = require("./RegExpress.js");
 const createComputerNumber = (start, end) => {
   const computer = [];
   while (computer.length < 3) {
-    const number = pickNumberRange(start, end);
+    const number = pickNumberBetweenRange(start, end);
     const isSelected = isSelectedNumber(number, computer);
   }
   return computer;
 };
 
-const pickNumberRange = (start, end) => {
+const pickNumberBetweenRange = (start, end) => {
   const number = MissionUtils.Random.pickNumberInRange(start, end);
   return number;
 };
