47acba10ac760b6e86c3cab15681aeb691a1d2ae
100Gyeon <bjy9966@naver.com>
2022-11-08 17:28:17 +0900

    refactor: constructor 사용하는 방식으로 리팩토링

--- src/App.js
+++ src/App.js
@@ -1,45 +1,50 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 
-const MAX_ANSWER_COUNT = 3;
+const ANSWER_LENGTH = 3;
+const START_NUMBER = 1;
+const END_NUMBER = 9;
 
 class App {
-  play() {
-    const answer = this.setAnswer();
-    this.startGame(answer);
+  constructor() {
+    this.answerList = [];
+    this.inputList = [];
   }
 
-  setAnswer() {
+  play() {
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+  }
+
+  setAnswerList() {
     const randomList = [];
-    while (randomList.length < MAX_ANSWER_COUNT) {
-      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+    while (randomList.length < ANSWER_LENGTH) {
+      const number = MissionUtils.Random.pickNumberInRange(START_NUMBER, END_NUMBER);
       if (!randomList.includes(number)) {
         randomList.push(number);
       }
     }
-    return randomList.join('');
+    return randomList;
   }
 
   getPlayerInputList() {
-    let inputList = [];
     MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (input) => {
-      inputList = [...String(input)];
+      this.inputList = input.trim().split('');
     });
-    return inputList;
+    MissionUtils.Console.close();
+
+    if (!this.isValid(this.inputList)) {
+      throw new Error('유효하지 않은 입력입니다.');
+    }
+
+    this.inputList = this.inputList.map(Number);
   }
 
   setIsValid(inputList) {
-    const inputListLength = inputList.length;
     const firstCondition = /^[1-9]+$/.test(inputList.join(''));
-    const secondCondition = inputListLength !== new Set(inputList).size;
-    const thirdCondition = inputListLength === MAX_ANSWER_COUNT;
-    return firstCondition && secondCondition && thirdCondition;
-  }
+    const secondCondition = ANSWER_LENGTH === new Set(inputList).size;
+    const thirdCondition = ANSWER_LENGTH === inputList.length;
 
-  startGame(answer) {
-    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
-    const inputList = this.getPlayerInputList();
-    const isValid = this.setIsValid(inputList);
-    if (!isValid) throw new Error('유효하지 않은 입력입니다.');
+    if (firstCondition && secondCondition && thirdCondition) return true;
+    return false;
   }
 }
 
