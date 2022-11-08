bca7f2da01e8cd3319cd1b2506abc5e90bb09e84
tkdrb12 <tkdrb15@gmail.com>
2022-11-08 20:59:48 +0900

    테스트 완료

--- src/App.js
+++ src/App.js
@@ -9,24 +9,55 @@ class App {
   play() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
     this.random_numbers = CreateRandomNum();
-    MissionUtils.Console.print(this.random_numbers);
     this.start();
-
   }
 
   start() {
-    MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (answer) => {
-      Except(answer);
-      // this.input_numbers = answer;
-      // MissionUtils.Console.print(this.input_numbers);
+    MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (input) => {
+      this.input_numbers = input;
+      Except(this.input_numbers);
 
-    this.start();
+      let strike = CalStrike(this.random_numbers, this.input_numbers);
+      let ball = CalBall(this.random_numbers, this.input_numbers);
+      PrintScore(strike, ball);
+      
+      if(strike == 3){
+        MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        this.restart();
+      }
+      else
+        this.start();
+    });
+  }
+  
+  restart(){
+    MissionUtils.Console.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    MissionUtils.Console.readLine("", (input) => {
+      if(input === "1")
+        this.play();
+      else if(input === "2"){
+        MissionUtils.Console.print("게임을 종료합니다.");
+        MissionUtils.Console.close();
+      }
+      else 
+        throw "1또는 2값을 입력해주세요"
     });
   }
 }
 
+//볼, 스트라이크 갯수 출력 함수
+function PrintScore(strike, ball){
+  if(strike == 0 && ball == 0)
+    MissionUtils.Console.print('낫싱');
+  else if(strike == 0)
+    MissionUtils.Console.print(ball.toString() + "볼");
+  else if(ball == 0)
+    MissionUtils.Console.print(strike.toString() + "스트라이크");
+  else
+    MissionUtils.Console.print(ball.toString() + "볼" + " "+ strike.toString() + "스트라이크");
+}
 
-
+//1~9까지의 세개의 서로 다른수가 아닐시 예외처리
 function Except(input){
 
   for(let i=0; i<input.length; i++){
@@ -44,7 +75,7 @@ function Except(input){
     throw "서로 다른 숫자를 입력해주세요";
 }
 
-
+//숫자야구 게임 스트라이크 판정 함수, 두개의 문자열을 받아 스트라이크 갯수를 반환 
 function CalStrike(str1, str2){
   let strike = 0;
   for(let index=0; index<3; index++){
@@ -54,18 +85,17 @@ function CalStrike(str1, str2){
   return strike;
 }
 
+//숫자야구 게임 볼 판정 함수, 두개의 문자열을 받아 볼 갯수를 반환 
 function CalBall(str1, str2){
   let ball = 0;
   for(let index=0; index<3; index++){
-    if(str2.includes(str1[index]))
-    ball = ball + 1;
+    if(str2.includes(str1[index]) && str1[index] != str2[index]) 
+      ball = ball + 1;
   }
   return ball;
 }
 
 //숫자야구 게임 랜덤 숫자 생성 함수
-//input : None
-//output : 중복되지 않는 3개의 숫자를 문자열 형태로 반환
 function CreateRandomNum(){
   let randstr = "";
   while(randstr.length < 3){
@@ -77,9 +107,7 @@ function CreateRandomNum(){
   return randstr;
 }
 
-
-
 const app = new App();
 app.play();
 
-// module.exports = App;
+module.exports = App;
