0fe9ab6fa125dd3a573dfc8ff324fdc25aee07ff
rovxxmjxx <rovxxmjxx@gmail.com>
2022-11-08 13:07:11 +0900

    feat: Error 모듈 내 유효성 검사 함수 작성

--- src/utils/error.js
+++ src/utils/error.js
@@ -11,6 +11,22 @@ class Error {
     return string.indexOf(element) !== string.lastIndexOf(element);
   }
 
+  static validate(input) {
+    let string = input;
+    if (Array.isArray(input)) string = input.join("");
+
+    const regex = new RegExp(
+      `^[${NUMBER.START}-${NUMBER.END}]{${NUMBER.TOTAL}}$`
+    );
+
+    const match = regex.test(string);
+
+    if (!match) Error.throw(MESSAGE.ERROR);
+
+    string.split("").forEach((element) => {
+      if (this.isDuplicated(input, element)) Error.throw(MESSAGE.ERROR);
+    });
+  }
 }
 
 module.exports = Error;
