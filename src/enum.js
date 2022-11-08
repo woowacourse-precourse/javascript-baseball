ba7b3ed3c9769ef69b11001fda94c921b5730448
yeong bin <2976622@naver.com>
2022-11-08 15:16:10 +0900

    refactor: GAME_STATE상수 enum.js로 이동

--- src/enum.js
+++ src/enum.js
@@ -1,9 +1,9 @@
-/** @enum */
+//@ts-check
+/** @enum {Symbol} */
 const GAME_STATE = Object.freeze({
+  START: Symbol('start game'),
   ING: Symbol('playing game'),
-  WIN: Symbol('win game'),
   END: Symbol('end game'),
 });
-
 
 module.exports = { GAME_STATE };
