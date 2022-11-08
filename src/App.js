04bda7a30583fb13e6ee38654915af07ef3a1912
Minju Park <project.mizzu@gmail.com>
2022-11-08 10:05:29 +0900

    Feat : readline 반복과 게임종료
    - 컴퓨터와 사용자의 숫자가 모두 일치할 때 까지 readline 반복
    - 숫자가 모두 일치할 경우 게임 종료
    - 모든 과정이 종료된 후 게임 재시작과 완전 종료 안내 메시지 출력

--- src/App.js
+++ src/App.js
@@ -14,37 +14,67 @@ class App {
   readLine() {
     Console.readLine(`숫자를 입력해주세요 : `, (input) => {
       const USER_INPUT = input.split('').map((number) => number * 1);
-      let countBall = 0;
-      let countStrike = 0;
+      this.check(USER_INPUT);
+    });
+  }
 
-      if (typeof USER_INPUT !== `number` && USER_INPUT.length !== 3) {
-        throw new Error('3자리 숫자만 입력해주세요!');
-      }
+  check(USER_INPUT) {
+    let countBall = 0;
+    let countStrike = 0;
+    if (typeof USER_INPUT !== `number` && USER_INPUT.length !== 3) {
+      throw new Error('3자리 숫자만 입력해주세요!');
+    }
 
-      for (let i = 0; i < COMPUTER.length; i += 1) {
-        for (let j = 0; j < USER_INPUT.length; j += 1) {
-          if (COMPUTER[i] === USER_INPUT[j]) {
-            countBall += 1;
-          }
+    for (let i = 0; i < COMPUTER.length; i += 1) {
+      for (let j = 0; j < USER_INPUT.length; j += 1) {
+        if (COMPUTER[i] === USER_INPUT[j]) {
+          countBall += 1;
         }
       }
-      COMPUTER.forEach((number, index) => {
-        if (number === USER_INPUT[index]) {
-          countStrike += 1;
-        }
-      });
+    }
+    COMPUTER.forEach((number, index) => {
+      if (number === USER_INPUT[index]) {
+        countStrike += 1;
+      }
+    });
 
-      if (countBall === 0 && countStrike === 0) {
-        this.print(`${COMPUTER} 낫싱`);
+    if (countBall !== 0 && countStrike === 3) {
+      this.result(countBall, countStrike);
+      this.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+      this.replay();
+    } else {
+      this.result(countBall, countStrike);
+      this.readLine();
+    }
+  }
+
+  result(countBall, countStrike) {
+    if (countBall === 0 && countStrike === 0) {
+      this.print(`${COMPUTER} 낫싱`);
+    }
+    if (countBall === 0 && countStrike !== 0) {
+      this.print(`${COMPUTER} ${countStrike}스트라이크`);
+    }
+    if (countBall !== 0 && countStrike === 0) {
+      this.print(`${COMPUTER} ${countBall}볼`);
+    }
+    if (countBall !== 0 && countStrike !== 0) {
+      this.print(`${COMPUTER} ${countBall}볼 ${countStrike}스트라이크`);
+    }
+    if (countBall !== 0 && countStrike === 3) {
+      this.print(`${countStrike}스트라이크`);
+    }
+  }
+
+  replay() {
+    this.print('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요');
+    Console.readLine('', (input) => {
+      if (input === '1') {
+        this.readLine();
       }
-      if (countBall === 0 && countStrike !== 0) {
-        this.print(`${COMPUTER} ${countStrike}스트라이크`);
-      }
-      if (countBall !== 0 && countStrike === 0) {
-        this.print(`${COMPUTER} ${countBall}볼`);
-      }
-      if (countBall !== 0 && countStrike !== 0) {
-        this.print(`${COMPUTER} ${countBall}볼 ${countStrike}스트라이크`);
+      if (input === '2') {
+        this.print('게임을 종료합니다.');
+        this.close();
       }
     });
   }
