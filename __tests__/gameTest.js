c0889c4029ce7b627e511e97af92ebe14df4473e
huyntohoon <81351526+hyuntohoon@users.noreply.github.com>
2022-11-08 14:40:40 +0900

    test : 랜덤 정답 숫자 확인

--- __tests__/gameTest.js
+++ __tests__/gameTest.js
@@ -1,3 +1,4 @@
+const { RANDOM_NUMBER } = require("../src/constants");
 const Game = require("../src/game");
 
 describe("기능 목록 테스트", () => {
@@ -6,5 +7,13 @@ describe("기능 목록 테스트", () => {
     const game = new Game();
     game.init();
     expect(log).toHaveBeenCalledWith("숫자 야구 게임을 시작합니다.");
+  });
+  test("서로 다른 세자리 수 생성 및 1 ~ 9 숫자 확인", () => {
+    const game = new Game();
+    const randomNumber = game.createRandomNumber();
+    expect(randomNumber.length).toEqual(3);
+    randomNumber.forEach((number) => {
+      expect(RANDOM_NUMBER.RANGE.test(number)).toBe(true);
+    });
   });
 });
