056632547c419937a3334f898aa6edc6e2068557
Yebin Kim <kyb4312@gmail.com>
2022-11-08 21:56:49 +0900

    style: apply the Airbnb style guide to App.js file

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-const { Console, Random } = require("@woowacourse/mission-utils");
+const { Console, Random } = require('@woowacourse/mission-utils');
 
 class App {
   constructor() {
-    this.NEW_GAME = "1";
+    this.NEW_GAME = '1';
     this.DIGITS_LENGTH = 3;
     this.computerDigits = [];
     this.score = { strikes: 0, balls: 0 };
   }
 
   play() {
-    Console.print("숫자 야구 게임을 시작합니다.");
+    Console.print('숫자 야구 게임을 시작합니다.');
     this.startNewGame();
   }
 
@@ -26,31 +26,33 @@ class App {
         randomDigits.push(newDigit);
       }
     }
+
     this.computerDigits = randomDigits;
   }
 
   playTurn() {
-    Console.readLine("숫자를 입력해주세요 : ", (userInput) => {
+    Console.readLine('숫자를 입력해주세요 : ', (userInput) => {
       let userDigits;
       if (this.checkUserDigitsValidity(userInput)) {
-        userDigits = [...userInput].map(Number);
+        userDigits = Array.from(userInput, Number);
       }
+
       return this.referee(userDigits);
     });
   }
 
   checkUserDigitsValidity(userInput) {
     if (userInput != parseInt(userInput, 10)) {
-      return App.throwError("The input value must contain only numbers");
+      return App.throwError('The input value must contain only numbers');
     }
     if (userInput.length !== this.DIGITS_LENGTH) {
-      return App.throwError("The input value must be three digits");
+      return App.throwError('The input value must be three digits');
     }
-    if (userInput.includes("0")) {
-      return App.throwError("The input value must not contain zero");
+    if (userInput.includes('0')) {
+      return App.throwError('The input value must not contain zero');
     }
     if (new Set(userInput).size !== this.DIGITS_LENGTH) {
-      return App.throwError("Input value must not contain duplicate numbers");
+      return App.throwError('Input value must not contain duplicate numbers');
     }
     return true;
   }
@@ -68,31 +70,33 @@ class App {
   countScore(userDigits) {
     let strikes = 0;
     let balls = 0;
-    for (let i = 0; i < this.DIGITS_LENGTH; i++) {
+    for (let i = 0; i < this.DIGITS_LENGTH; i += 1) {
       if (this.computerDigits[i] === userDigits[i]) {
         strikes += 1;
       } else if (this.computerDigits.includes(userDigits[i])) {
         balls += 1;
       }
     }
+
     this.score.strikes = strikes;
     this.score.balls = balls;
   }
 
   printScore() {
-    let scoreSentence = "";
+    let scoreSentence = '';
     if (this.score.balls) {
       scoreSentence += `${this.score.balls}볼`;
     }
     if (this.score.strikes) {
       if (scoreSentence) {
-        scoreSentence += " ";
+        scoreSentence += ' ';
       }
       scoreSentence += `${this.score.strikes}스트라이크`;
     }
     if (!scoreSentence) {
-      scoreSentence = "낫싱";
+      scoreSentence = '낫싱';
     }
+
     Console.print(scoreSentence);
   }
 
@@ -103,6 +107,7 @@ class App {
       );
       return true;
     }
+
     return false;
   }
 
@@ -119,7 +124,7 @@ class App {
   }
 
   static finishGame() {
-    Console.print("숫자 야구 게임이 종료되었습니다.");
+    Console.print('숫자 야구 게임이 종료되었습니다.');
     return Console.close();
   }
 }
