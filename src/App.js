513e9c0a3a97bc804132fbd0365a590902f87d02
wlthdud <thdudzi@naver.com>
2022-11-08 17:04:16 +0900

    feat: constructor로 변수 선언

--- src/App.js
+++ src/App.js
@@ -1,41 +1,41 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
 class App{
-
-
+  constructor(){ //변수 선언
+    this.comNum = [];
+    this.strike = 0;
+    this.ball = 0;
+    this.nothing = 0;
+  }
+  
   comRandomNumber(){ //컴퓨터 랜덤 숫자 정하기 함수
-    const comNum = [];
-    while (comNum.length < 3) {
+    
+    while (this.comNum.length < 3) {
       const number = MissionUtils.Random.pickNumberInRange(1, 9);
-      if (!comNum.includes(number)) {
-        comNum.push(number);
+      if (!this.comNum.includes(number)) {
+        this.comNum.push(number);
       }
     }
-    MissionUtils.Console.print(comNum);
+    MissionUtils.Console.print(this.comNum);
   }
 
   gameStartNotice(){ //게임 시작 알림
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
   }
 
-
   getGameResult(){ //스트라이크, 볼 계산
     MissionUtils.Console.readLine('숫자를 입력해주세요', (number) => {
-      let strike = 0;
-      let ball = 0;
-      let nothing = 0;
-
       let usrNum = [...number];
 
       for(let i = 0; i < usrNum.length; i++){
-        if(comNum.includes(u[i])){
-          if(usrNum[i] == comNum[i]){
-            strike ++;
+        if(this.comNum.includes(usrNum[i])){
+          if(usrNum[i] == this.comNum[i]){
+            this.strike ++;
           } else {
-            ball ++;
+            this.ball ++;
           }
         } else {
-          nothing++;
+          this.nothing++;
         }
       }
     });
@@ -43,20 +43,19 @@ class App{
   }
 
   printGameResult(){ //게임 결과 출력
-    if(strike == 3){
+    if(this.strike == 3){
       MissionUtils.Console.print('3스트라이크');
 
-    } else if(strike > 0 && strike < 3 && ball ==0){
-      MissionUtils.Console.print('%d스트라이크', strike);
-    } else if(ball > 0 && ball <= 3 && strike == 0){
-      MissionUtils.Console.print('%d볼', ball);
-    } else if(strike == 2 && ball == 1){
-      MissionUtils.Console.print('%d스트라이크 %d볼', strike, ball);
-    } else if(nothing == 3){
+    } else if(this.strike > 0 && this.strike < 3 && this.ball ==0){
+      MissionUtils.Console.print('%d스트라이크', this.strike);
+    } else if(this.ball > 0 && this.ball <= 3 && this.strike == 0){
+      MissionUtils.Console.print('%d볼', this.ball);
+    } else if(this.strike == 2 && this.ball == 1){
+      MissionUtils.Console.print('%d스트라이크 %d볼', this.strike, this.ball);
+    } else if(this.nothing == 3){
       MissionUtils.Console.print('낫싱');
     }
   }
-
 
   gameStart(){ //게임 시작
     this.getGameResult();
