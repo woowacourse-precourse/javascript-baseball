2162ead6be44f735fc2c2f80d14d32def8abba1e
khkh0109 <ist200120@gmail.com>
2022-11-08 20:39:26 +0900

    style: 줄바꿈 지우기

--- src/App.js
+++ src/App.js
@@ -101,7 +101,6 @@ function getResult(result) {
   } 
 }
 
-
 function selectPlayGame() {
   MissionUtils.Console.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.' + '\n', (answer) => {
     if(answer == 1) {
