b76b49ee0f64fdf8c5c4cca630078e7323d8d0b0
yuna Kim <yuna000804@gmail.com>
2022-11-08 13:15:29 +0900

    feat : 컴퓨터 랜덤 값 생성

--- src/App.js
+++ src/App.js
@@ -7,4 +7,16 @@ class App {
   }
 }
 
+const computerRandom = () => {
+  const computer = [];
+  while (computer.length < 3) {
+    const number = Random.pickNumberInRange(1, 9);
+    if (!computer.includes(number)) {
+      computer.push(number);
+    }
+  }
+  return computer;
+}
+
+
 module.exports = App;
