8cc08a92232152ac70340d222232b9b755377337
chee9835 <huisu121@gmail.com>
2022-11-08 20:50:11 +0900

    feat: 테스트케이스 추가

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -47,16 +47,85 @@ describe("숫자 야구 게임", () => {
     });
   });
 
-  test("예외 테스트", () => {
-    const randoms = [1, 3, 5];
-    const answers = ["1234"];
+  test("재시작(1)/종료(2) 외 다른 숫자 입력", () => {
+    const randoms = [1, 3, 5, 5, 8, 9];
+    const answers = ["135", "3", "2"];
+    const logSpy = getLogSpy();
+    const messages = ["3스트라이크", "1 또는 2를 입력해주세요.", "게임 종료"];
 
     mockRandoms(randoms);
     mockQuestions(answers);
 
+    const app = new App();
+    app.play();
+
+    messages.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
+  test("컴퓨터가 1에서 9까지 서로 다른 임의의 수 3개 선택", () => {
+    const randoms = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+    const app = new App();
+    app.play();
+
     expect(() => {
-      const app = new App();
-      app.play();
-    }).toThrow();
+      setAnswer(randoms).toHaveLength(3);
+    });
+  });
+
+  describe("input에 올바르지 않은 값 입력", () => {
+    test("예외 테스트1 - 길이 3자리 이상", () => {
+      const randoms = [1, 3, 5];
+      const answers = ["1234"];
+
+      mockRandoms(randoms);
+      mockQuestions(answers);
+
+      expect(() => {
+        const app = new App();
+        app.play();
+      }).toThrow();
+    });
+
+    test("예외 테스트2 - 중복된 숫자", () => {
+      const randoms = [1, 3, 5];
+      const answers = ["117"];
+
+      mockRandoms(randoms);
+      mockQuestions(answers);
+
+      expect(() => {
+        const app = new App();
+        app.play();
+      }).toThrow();
+    });
+
+    test("예외 테스트3 - 한글", () => {
+      const randoms = [1, 3, 5];
+      const answers = ["ㄴㅇㄱ"];
+
+      mockRandoms(randoms);
+      mockQuestions(answers);
+
+      expect(() => {
+        const app = new App();
+        app.play();
+      }).toThrow();
+    });
+
+    test("예외 테스트4 - 영어소문자", () => {
+      const randoms = [1, 3, 5];
+      const answers = ["o12"];
+
+      mockRandoms(randoms);
+      mockQuestions(answers);
+
+      expect(() => {
+        const app = new App();
+        app.play();
+      }).toThrow();
+    });
   });
 });
