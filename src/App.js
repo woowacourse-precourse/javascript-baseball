987c984a1192d10662cec96c4ca7c4f0039370e5
Moyujin <scab12341@gmail.com>
2022-11-08 18:51:45 +0900

    feat: 게임 시작 문구 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
+const { Console, Random } = require('@woowacourse/mission-utils');
+
+const MSG = {
+  initMsg: '숫자 야구 게임을 시작합니다.',
+  inputNumber: '숫자를 입력해주세요 : ',
+  endMsg: '3개의 숫자를 모두 맞히셨습니다! 게임 종료',
+  requestMsg: '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요',
+  invalidNumber: '잘못된 값을 입력하셨습니다',
+  strike: '스트라이크',
+  ball: '볼',
+  nothing: '낫싱',
+};
+
 class App {
-  play() {}
+  #randomNumber;
+  #inputNumber;
+
+  play() {
+    Console.print(Msg.initMsg);
+    this.#randomNumber = this.makeRandomNumber();
+  }
+
+  makeRandomNumber() {
+    const computer = [];
+
+    while (computer.length < 3) {
+      const number = Random.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) computer.push(number);
+    }
+    return computer.join('');
+  }
 }
 
 module.exports = App;
