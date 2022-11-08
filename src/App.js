163cf076fdedb0658c815a8f141e73d980f487e9
kaori-killer <sksbthwjd@daum.net>
2022-11-08 18:17:36 +0900

    feat: [숫자 생성] 컴퓨터의 숫자를 생성하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -3,11 +3,23 @@ const MISSIONUTILS_IO = require("@woowacourse/mission-utils");
 class Game {
   constructor(){
     this.utilsIo = MISSIONUTILS_IO.Console;
-    
+    this.utilsRandom = MISSIONUTILS_IO.Random;
+
     this.userNumberArray = [];
+    this.compareNumberArray = [];
   }
+
   inputUserNumber(text, callback) {
     this.utilsIo.readLine(text, callback.bind(this));
+  }
+  
+  makeComputerNumer(){
+    const computer = [];
+    while (computer.length < 3) {
+      const number = this.utilsRandom.pickNumberInRange(1, 9);
+      if (!computer.includes(number)) computer.push(number);
+    }
+    this.compareNumberArray = computer;
   }
 }
 
