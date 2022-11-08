7583de34995e50f81f46b020620e09d6b608d463
RyuJoonSik <tldtksahtxja@naver.com>
2022-11-08 15:33:55 +0900

    feat: Ball.js에 compareTo, getStrikeOf 추가
    스트라이크 반환 테스트 케이스를 통과할 수 있도록 코드를 추가했습니다.
    compareTo: 최종적으로 스트라이크, 볼, 낫싱을 구한 결과를 반환
    getStrikeOf: 스트라이크만을 구해 반환

--- src/Components/Ball/Ball.js
+++ src/Components/Ball/Ball.js
@@ -1,11 +1,26 @@
-class Ball {
-    constructor(number) {
-        this.number = number;
-    }
+const _ = require("../../function");
 
-    getNumber() {
-        return this.number;
-    }
+class Ball {
+  constructor(number) {
+    this.number = number;
+  }
+
+  getNumber() {
+    return this.number;
+  }
+
+  compareTo(otherBall) {
+    return this.getStrikeOf(otherBall);
+  }
+
+  getStrikeOf(otherBall) {
+    const RESULT = this.number - otherBall.getNumber();
+    const COUNT = _.divideNumberToArray(RESULT).filter(
+      (number) => number === 0
+    ).length;
+
+    return `${COUNT}스트라이크`;
+  }
 }
 
 module.exports = Ball;
