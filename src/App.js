0d77eaa784a5367b4dbde7f62d8652a209c0a6e3
KangYunJi <dbswl701@ajou.ac.kr>
2022-11-08 16:39:13 +0900

    feat: 시작 문구 출력 및 컴퓨터 숫자 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+
 class App {
-  play() {}
+  // play() {}
+  constructor(){ // 일단 배열 초기화
+    this.computerArr=[];
+    this.userArr=[];
+  }
+
+  play() { // main
+    // 시작 메시지 출력
+    const computer = new Set(); // set은 중복값 허용x
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+    while (computer.size < 3) {
+      computer.add(MissionUtils.Random.pickNumberInRange(1, 9));
+    }
+    this.computerArr = [...computer]; // 배열로 바꿔야 출력 가능
+    console.log(`computer : ${this.computerArr}`);
+
+  }
+
+
+
+
 }
 
 module.exports = App;
