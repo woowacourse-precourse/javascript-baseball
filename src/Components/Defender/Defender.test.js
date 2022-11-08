28924726596d8df0d4f4b6169b953897249f955e
RyuJoonSik <tldtksahtxja@naver.com>
2022-11-08 20:47:15 +0900

    test: Defender.test.js에 reportAbout 메서드 테스트 케이스 추가

---
+++ src/Components/Defender/Defender.test.js
@@ -0,0 +1,32 @@
+const { Console, Random } = require("@woowacourse/mission-utils");
+
+const AutomaticBallGenerator = require("../AutomaticBallGenerator/AutomaticBallGenerator");
+const Ball = require("../Ball/Ball");
+const Defender = require("./Defender");
+
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(Console, "print");
+  logSpy.mockClear();
+  return logSpy;
+};
+
+const mockRandoms = (numbers) => {
+  Random.pickUniqueNumbersInRange = jest.fn();
+  numbers.reduce((acc, number) => {
+    return acc.mockReturnValueOnce(number);
+  }, Random.pickUniqueNumbersInRange);
+};
+
+describe("Defender", () => {
+  test("나(공격수)의 Ball이 425이고 컴퓨터(수비수)의 Ball이 425이면 3스트라이크를 출력", () => {
+    const BALL_GENERATOR = new AutomaticBallGenerator();
+    jest.spyOn(BALL_GENERATOR, "execute").mockReturnValue([4, 2, 5]);
+
+    const DEFENDER = new Defender(BALL_GENERATOR);
+    const BALL_OF_ATTACKER = new Ball(425);
+    const LOG_SPY = getLogSpy();
+    DEFENDER.reportAbout(BALL_OF_ATTACKER);
+
+    expect(LOG_SPY).toHaveBeenCalledWith("3스트라이크");
+  });
+});
