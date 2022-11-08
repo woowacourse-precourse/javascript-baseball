b35cb1341a59dbe523487b0c170291c0687c8d12
Hchan2 <seongchan_@naver.com>
2022-11-08 20:38:17 +0900

    랜덤 숫자, 게임 결과, 유저 입력 함수 분리

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 const { GAME_START, RESTART } = require('./constant/constant');
 const validation = require('./user/validation');
+const { ballCheck, strikeCheck, compareNumber } = require('./game/bowlingResult');
+const numberRandom = require('./computer/randomNumber');
+const userInputNumber = require('./user/userInputNumber');
 
 class App {
   // 게임 재시작 입력 오류
@@ -23,39 +26,11 @@ class App {
     });
   }
 
-  // 볼, 스트라이크 출력
-  compareNumber(strike, ball) {
-    if (strike === 0 && ball === 0) return '낫싱';
-    if (strike === 0 && ball > 0) return `${ball}볼`;
-    if (strike > 0 && ball === 0) return `${strike}스트라이크`;
-    return `${ball}볼 ${strike}스트라이크`;
-  }
-
-  // 볼 체크
-  ballCheck(user, random) {
-    let ball = 0;
-    for (let i = 0; i < 3; i++) {
-      if (random[i] !== user[i] && random.includes(user[i])) ball++;
-    }
-
-    return ball;
-  }
-
-  // 스트라이크 체크
-  strikeCheck(user, random) {
-    let strike = 0;
-    for (let i = 0; i < 3; i++) {
-      if (user[i] === random[i]) strike++;
-    }
-
-    return strike;
-  }
-
   // 게임 결과
   getResult(userNumber, randomNumber) {
-    const strike = this.strikeCheck(userNumber, randomNumber);
-    const ball = this.ballCheck(userNumber, randomNumber)
-    const resultMessage = this.compareNumber(strike, ball);
+    const strike = strikeCheck(userNumber, randomNumber);
+    const ball = ballCheck(userNumber, randomNumber)
+    const resultMessage = compareNumber(strike, ball);
 
     MissionUtils.Console.print(resultMessage);
     if (strike !== 3) {
@@ -65,40 +40,17 @@ class App {
       this.restartGame();
     }
   }
-
-  // 사용자 입력
-  userInputNumber() {
-    let userAnswer = "";
-    MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (input) => {
-      validation(input);
-      userAnswer = input;
-    });
-
-    return userAnswer;
-  }
   
   // 사용자 입력 숫자 생성
   generateNumber(randomNumber) {
-    const userNumber = this.userInputNumber();
+    const userNumber = userInputNumber();
 
     this.getResult(userNumber, randomNumber);
   }
 
-  // 랜덤 숫자 발생
-  numberRandom() {
-    const computer = [];
-    while (computer.length < 3) {
-        const number = MissionUtils.Random.pickNumberInRange(1, 9);
-        if (!computer.includes(number)) {
-            computer.push(number);
-        }
-    }
-    return computer.join("");
-  }
-
   // 게임 실행 (랜덤 숫자 생성)
   startGame() {
-    const randomNumber = this.numberRandom();
+    const randomNumber = numberRandom();
 
     this.generateNumber(randomNumber);
   }
