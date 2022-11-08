3082f4e19907e6cfe08601053d1146eb0f969e64
cruelladevil <dev.timetravel@gmail.com>
2022-11-08 18:04:32 +0900

    refactor: validate arguments for createUniqueNumbers

--- src/Computer.js
+++ src/Computer.js
@@ -2,6 +2,8 @@ const { Random } = require('@woowacourse/mission-utils');
 
 class Computer {
   static createUniqueNumbers(start, end, count) {
+    Computer.#validateUniqueNumbers(start, end, count);
+
     const numberSet = new Set();
 
     while (numberSet.size !== count) {
@@ -10,6 +12,30 @@ class Computer {
     }
 
     return [...numberSet];
+  }
+
+  static #validateUniqueNumbers(start, end, count) {
+    if (
+      !Number.isInteger(start) ||
+      !Number.isInteger(end) ||
+      !Number.isInteger(count)
+    ) {
+      throw new Error('인수는 정수여야 합니다.');
+    }
+
+    if (start > end) {
+      throw new Error('start가 end보다 커서는 안됩니다.');
+    }
+
+    if (count < 0) {
+      throw new Error('count는 0보다 작으면 안됩니다.');
+    }
+
+    if (count > end - start + 1) {
+      throw new Error(
+        `count: ${count}가 input 범위 (end - start + 1): ${end - start + 1}보다 커서는 안됩니다.`
+      );
+    }
   }
 }
 
