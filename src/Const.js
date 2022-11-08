f2a7c4dead75ff0c32a181c34f4392d9241394f3
Jinu <jinu200131@gmail.com>
2022-11-08 16:28:34 +0900

    feat (Const) : 상수 파일 추가

---
+++ src/Const.js
@@ -0,0 +1,17 @@
+const NUMBER_MAX_LENGTH = 3;
+const NUMBER_RESTART_TRUE = 1;
+const NUMBER_RESTART_FALSE = 2;
+
+const STRING_ZERO = '0';
+const STRING_EMPTY = '';
+
+const RESULT_NOTHING = "낫싱";
+const RESULT_STRIKE = "스트라이크";
+const RESULT_BALL = "볼";
+
+const ALERT_START = "숫자 야구 게임을 시작합니다.";
+const ALERT_INPUT_NUMBER = "숫자를 입력해주세요 : ";
+const ALERT_INVALIED_INPUT = "올바른 입력이 아닙니다. 프로그램을 종료합니다.";
+const ALERT_CORRECT_ANSWER = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+const ALERT_EXIT_GAME = "게임을 종료합니다.";
+const ALERT_RESTART_GAME = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
