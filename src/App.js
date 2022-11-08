1ae31f4fb1976a5e3207e13cc17245cd6a303a81
하지훈 <jnh03336@gmail.com>
2022-11-08 10:37:02 +0900

    feat(overall logic): overall logic for baseball game play

--- src/App.js
+++ src/App.js
@@ -1,5 +1,53 @@
+const { Console } = require("@woowacourse/mission-utils");
+const getUserInputs = require("./lib/getUserInput");
 class App {
-  play() {}
+  constructor() {}
+
+  selectGameNumbers() {}
+
+  isValidGameInput() {}
+
+  parseGameInput() {}
+
+  getGameResult() {}
+
+  getNumOfSameIndexSameNumber() {}
+
+  getNumOfSameNumber() {}
+
+  printGameResult() {}
+
+  isValidGameOverInput() {}
+
+  async play() {
+    Console.print("숫자 야구 게임을 시작합니다.");
+    let gameNumbers = selectGameNumbers();
+    while (true) {
+      const userInput = await getUserInputs("숫자를 입력해주세요 : ");
+      if (!isValidGameInput(userInput)) {
+        throw new Error("잘못된 입력값입니다.");
+      }
+      const parsedInput = parseGameInput(userInput);
+      const gameResult = getGameResult(gameNumbers, parsedInput);
+      printGameResult(gameResult);
+      if (gameResult.strike !== 3) continue;
+
+      Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+      const userGameOverSelection = await getUserInputs(
+        "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
+      );
+      if (!isValidGameInput(userGameOverSelection)) {
+        throw new Error("잘못된 입력값입니다.");
+      }
+      if (userGameOverSelection === "1") {
+        gameNumbers = selectGameNumbers();
+        continue;
+      }
+      if (userGameOverSelection === "2") {
+        break;
+      }
+    }
+  }
 }
 
 module.exports = App;
