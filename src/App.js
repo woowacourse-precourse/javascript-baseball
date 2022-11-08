9255f9973544e9348b7d85003644ed7662a33e0a
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2022-11-08 20:32:58 +0900

    feat: add function 4

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ class App {
   play() {
     let randombaseball = createRandomNumber();
     start();
-    getNumber();
+    getNumber(randombaseball);
   }
 }
 module.exports = App;
@@ -22,18 +22,41 @@ const createRandomNumber = () => {
 
 const start = () => {
   MissionUtils.Console.print("숫자 야구를 게임을 시작합니다.");
-}
+};
 
-const getNumber = () => {
+const getNumber = (randombaseball) => {
   MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (number) => {
     checkNumberError(number);
+    let result = computeBallStrike(randombaseball, number);
+    computeResult(result);
   });
-}
+};
 
 const checkNumberError = (number) => {
-  if (number.length !== 3) throw new Error ("3자리 수를 입력해주세요.");
-  let checkNumber = number.split('').map(Number);
+  if (number.length !== 3) throw new Error("3자리 수를 입력해주세요.");
+  let checkNumber = number.split("").map(Number);
   const set = new Set(checkNumber);
   const newArr = [...set];
-  if(newArr.length !==3 ) throw new Error("중복되지 않는 수들을 입력해주세요.");
-}
+  if (newArr.length !== 3)
+    throw new Error("중복되지 않는 수들을 입력해주세요.");
+};
+
+const computeBallStrike = (randombaseball, number) => {
+  let ball = 0;
+  let strike = 0;
+
+  let userNumber = number.split('').map(Number);
+
+  for (let i = 0; i < 3; i++) {
+    if (randombaseball.includes(userNumber[i])) {
+      if (randombaseball.indexOf(userNumber[i]) == i) {
+        strike++;
+        continue;
+      }
+      ball++;
+    }
+  }
+
+  const result = [ball, strike]
+  return result;
+};
