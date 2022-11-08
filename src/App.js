203403508687fb36547bcaa31d8d0c42eaf58eb1
hayeong <fibonacci2246@gmail.com>
2022-11-08 20:36:39 +0900

    feat: printStartMessage method

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import * as MissionUtils from "@woowacourse/mission-utils";
+
 class App {
-  play() {}
+  constructor() {
+    this.#missionUtils = new MissionUtils();
+  }
+
+  #printStartMessage() {
+    this.#missionUtils.print("숫자 야구 게임을 시작합니다.");
+  }
+
+  play() {
+    this.#printStartMessage();
+  }
 }
 
 module.exports = App;
