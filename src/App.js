a90b1c58401b77e3a79f79b6acc3ca3ff589b1b3
HWI <dlwognl99@naver.com>
2022-11-08 14:13:42 +0900

    refactor: 스트라이크, 볼 카운팅 관련 클래스 생성

--- src/App.js
+++ src/App.js
@@ -11,11 +11,16 @@ class App {
   userInput() {
     let userInputValue;
     const validation = new Validation();
+    const count = new ResultCounting();
+
     MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (value) => {
       userInputValue = value.split("").map((number) => +number);
       validation.checkUserInput(value);
-      const resultCount = getCount(userInputValue, this.computerRandomValue);
-      getResult(resultCount.strike, resultCount.ball);
+      const resultCount = count.getCount(
+        userInputValue,
+        this.computerRandomValue
+      );
+      count.getResult(resultCount.strike, resultCount.ball);
       this.checkGameClear(resultCount.strike);
     });
   }
@@ -39,6 +44,29 @@ class Validation {
   checkUserEndInput(value) {
     if (![1, 2].includes(+value))
       throw new Error("1과 2만 입력할 수 있습니다.");
+  }
+}
+
+// 스트라이크, 볼 카운팅 관련 클래스
+class ResultCounting {
+  getCount(userInputValue, computerRandomValue) {
+    const result = {
+      strike: 0,
+      ball: 0,
+    };
+    userInputValue.forEach((userElement, index) => {
+      const computerElement = computerRandomValue[index];
+      if (computerElement === userElement) return result.strike++;
+      if (computerRandomValue.includes(userElement)) return result.ball++;
+    });
+    return result;
+  }
+  getResult(strike, ball) {
+    let resultMessage = "";
+    if (ball) resultMessage += `${ball}볼`;
+    if (strike) resultMessage += ` ${strike}스트라이크`;
+    if (resultMessage === "") resultMessage = "낫싱";
+    MissionUtils.Console.print(resultMessage);
   }
 }
 
@@ -73,28 +101,6 @@ function addNumber(randomSet) {
   const randomNumber = MissionUtils.Random.pickNumberInRange(1, 9);
   randomSet.add(randomNumber);
   if (randomSet.size < 3) addNumber(randomSet);
-}
-
-function getCount(userInputValue, computerRandomValue) {
-  const result = {
-    strike: 0,
-    ball: 0,
-  };
-  userInputValue.forEach((userElement, index) => {
-    const computerElement = computerRandomValue[index];
-    if (computerElement === userElement) return result.strike++;
-    if (computerRandomValue.includes(userElement)) return result.ball++;
-  });
-  return result;
-}
-
-function getResult(strike, ball) {
-  let resultMessage = "";
-  if (ball) resultMessage += `${ball}볼`;
-  if (strike) resultMessage += ` ${strike}스트라이크`;
-  if (resultMessage === "") resultMessage = "낫싱";
-
-  MissionUtils.Console.print(resultMessage);
 }
 
 const app = new App();
