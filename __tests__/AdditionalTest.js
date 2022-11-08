69908f4f6ed38a5ca47c68ed94beaf36be00d494
soohyun_kim5 <93cherry@naver.com>
2022-11-08 20:39:22 +0900

    test: 게임 재시작

--- __tests__/AdditionalTest.js
+++ __tests__/AdditionalTest.js
@@ -105,6 +105,22 @@ describe("숫자 야구 게임", () => {
             expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
         });
     });
+    test("2볼 1스트라이크", () => {
+        const randoms = [1, 3, 6];
+        const answers = ["163"];
+        const logSpy = getLogSpy();
+        const messages = ["2볼 1스트라이크"];
+
+        mockRandoms(randoms);
+        mockQuestions(answers);
+
+        const app = new App();
+        app.play();
+
+        messages.forEach((output) => {
+            expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+        });
+    });
     test("게임 종료", () => {
         const randoms = [5, 8, 9];
         const answers = ["589", "2"];
@@ -121,7 +137,25 @@ describe("숫자 야구 게임", () => {
             expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
         });
     });
+    test("게임 종료 후 재시작", () => {
+        const randoms = [1, 3, 5, 5, 8, 9];
+        const answers = ["135", "1", "597"];
+        const logSpy = getLogSpy();
+        const messages = [
+            "3스트라이크",
+            "1볼 1스트라이크",
+        ];
 
+        mockRandoms(randoms);
+        mockQuestions(answers);
+
+        const app = new App();
+        app.play();
+
+        messages.forEach((output) => {
+            expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+        });
+    });
 
 
 });
