09292913c4c00c311f56aa62d97e20662e6c5fb9
HamZik <b1y9mn@gmail.com>
2022-11-08 11:15:42 +0900

    refactor: computerPicksNumber 함수 변경
    기존의 방식과는 달리 한 자리 수를 랜덤으로 뽑아서 모두 다른 세 자리 수를 뽑도록 computerPicksNumber 함수를 변경하였습니다.

--- src/App.js
+++ src/App.js
@@ -5,10 +5,23 @@ class App {
     Console.print('숫자 야구 시작합니다.');
   };
 
-  #startGame() {
+  #computerPicksNumber() {
+    const computerPickArr = [];
+    while (computerPickArr.length < 3) {
+      const number = Random.pickNumberInRange(1, 9);
+      if (!computerPickArr.includes(number)) {
+        computerPickArr.push(number);
+      }
+    }
+    const computerPick = computerPickArr.join('');
+    console.log(computerPick);
+    return computerPick;
+  }
 
+  #startGame() {
+    const computerPick = this.#computerPicksNumber();
   };
-  
+
   play() {
     this.#sayStart();
     this.#startGame();
