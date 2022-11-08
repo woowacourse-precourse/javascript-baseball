d9b9bae7303aac4676ea1268790d593c17827023
Yesong <to06109@naver.com>
2022-11-08 18:02:58 +0900

    test(userInputTest): 사용자 입력 예외처리 테스트 케이스 추가

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -61,20 +61,26 @@ describe('숫자 야구 게임', () => {
     }).toThrow();
   });
 
-  test('입력 1-9 사이의 정수 예외 테스트', () => {
+  test('사용자 입력 1-9 사이의 정수 예외 테스트(문자 포함)', () => {
     const input = 'a34';
 
     expect(() => handleGameException(input)).toThrow();
   });
 
-  test('입력 1-9 사이의 정수 예외 테스트2', () => {
+  test('사용자 입력 1-9 사이의 정수 예외 테스트(범위 벗어난 숫자)', () => {
     const input = '904';
 
     expect(() => handleGameException(input)).toThrow();
   });
 
-  test('입력 서로 다른 수 예외 테스트', () => {
+  test('사용자 입력 서로 다른 수 예외 테스트', () => {
     const input = '994';
+
+    expect(() => handleGameException(input)).toThrow();
+  });
+
+  test('사용자 입력 길이 예외 테스트', () => {
+    const input = '12';
 
     expect(() => handleGameException(input)).toThrow();
   });
