45321d719a5449435887fb92e11c5247575e963f
Jeong <jeong@HwiJeongs-MacBook-Pro.local>
2022-11-08 23:17:32 +0900

    feature(update): try.. catch logic

--- src/App.js
+++ src/App.js
@@ -23,35 +23,35 @@ class App {
 
   gameStart(gameNumber) {
     MissionUtils.Console.readLine("3자리 숫자를 입력해주세요: ", (answer) => {
-      console.log(`입력값: ${answer}`);
-      if (answer.length !== 3) {
-        throw new Error("입력값은 반드시 3자리 숫자여야 합니다.");
-      }
-
-      if (Number(gameNumber) === Number(answer)) {
-        this.result = "3스트라이크";
-        if (this.result === "3스트라이크") {
-          MissionUtils.Console.print(this.result);
-          MissionUtils.Console.print(
-            "3개의 숫자를 모두 맞히셨습니다! 게임 종료"
-          );
-          MissionUtils.Console.print(
-            "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
-          );
-          return MissionUtils.Console.readLine(
-            "게임 재개 여부: ",
-            (gameResumeChoice) => {
-              if (Number(gameResumeChoice) === 1) {
-                this.play();
-              } else if (Number(gameResumeChoice) === 2) {
-                MissionUtils.Console.print("게임 종료");
-              }
-            }
-          );
-        }
-      }
-
       try {
+        if (answer.length !== 3) {
+          throw new Error("입력값은 반드시 3자리 숫자여야 합니다.");
+        }
+
+        if (Number(gameNumber) === Number(answer)) {
+          this.result = "3스트라이크";
+          if (this.result === "3스트라이크") {
+            MissionUtils.Console.print(this.result);
+            MissionUtils.Console.print(
+              "3개의 숫자를 모두 맞히셨습니다! 게임 종료"
+            );
+            MissionUtils.Console.print(
+              "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
+            );
+            return MissionUtils.Console.readLine(
+              "게임 재개 여부: ",
+              (gameResumeChoice) => {
+                if (Number(gameResumeChoice) === 1) {
+                  this.play();
+                } else if (Number(gameResumeChoice) === 2) {
+                  MissionUtils.Console.print("게임 종료");
+                }
+              }
+            );
+          }
+        }
+
+        let result = this.reulst;
         let strikeCount = 0;
         let ballCount = 0;
 
@@ -71,29 +71,31 @@ class App {
           ) {
             ballCount = ballCount + 1;
           } else if (!numberArr.includes(answerArr[i])) {
-            this.result = "낫싱";
+            result = "낫싱";
           }
         }
 
-        if (this.result === "0볼 0스트라이크") {
-          this.result = "낫싱";
+        if (result === "0볼 0스트라이크") {
+          result = "낫싱";
         } else if ((strikeCount > 0) & (ballCount === 0)) {
-          this.result = `${strikeCount}스트라이크`;
+          result = `${strikeCount}스트라이크`;
         } else if ((strikeCount == 0) & (ballCount > 0)) {
-          this.result = `${ballCount}볼`;
+          result = `${ballCount}볼`;
         } else if ((strikeCount > 0) & (ballCount > 0)) {
-          this.result = `${ballCount}볼 ${strikeCount}스트라이크`;
+          result = `${ballCount}볼 ${strikeCount}스트라이크`;
         }
-        MissionUtils.Console.print(`${this.result}`);
+        MissionUtils.Console.print(`${result}`);
 
-        if (this.result !== "3스트라이크") {
+        if (result !== "3스트라이크") {
           this.gameStart(gameNumber);
         }
       } catch (e) {
-        MissionUtils.Console.print(e);
+        console.error(e);
       }
     });
   }
 }
 
 module.exports = App;
+const app = new App();
+app.play();
