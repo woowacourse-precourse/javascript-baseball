9d243f0b5800426a851e2180e14fb37c3826dc72
park junwoo <always1997@kakao.com>
2022-11-08 09:30:58 +0900

    feat: 사용자 입력 유효검사 inputValidation 구현

--- src/App.js
+++ src/App.js
@@ -1,6 +1,37 @@
-import * as MissionUtils from '@woowacourse/mission-utils';
+const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
+
+  checkRange(input) {
+    input.map((num)=> {
+      if(num > 9 || num<1) {
+        return false;  
+      }
+    })
+    return true;
+  }
+  checkDupulicate(input) {
+    const set = new Set(input);
+    if(set.size !== input.length) return true;
+    return false;
+  }
+
+  inputValidation(input) {
+    if(input.length !== 3)  {
+      throw new("입력은 세자리 숫자로 해주세요.");
+    }
+    if(input.some((num)=> isNaN(num))) {
+      throw new("숫자만 입력 가능합니다.");
+    }
+    if(checkRange(input)) {
+      throw new("1~9사이의 숫자를 입력해주세요.")
+    }
+    if(checkDupulicate(input)) {
+      throw new("서로 다른숫자로 입력해주세요.")
+    }
+    return true;
+  }
+  
   play() {
     const computer = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
     let userAnswer;
@@ -9,10 +40,9 @@ class App {
       userAnswer = answer.split('').map((num)=>{return Number(num)});
     });
   }
-
 }
 
-
-
+// const app = new App();
+// app.play();
 
 module.exports = App;
