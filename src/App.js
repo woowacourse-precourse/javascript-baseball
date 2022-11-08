73b9dcd66cfb3f0174284d2ff530257041741bda
kaori-killer <sksbthwjd@daum.net>
2022-11-08 18:15:44 +0900

    feat: [숫자 생성] 유저의 숫자를 입력받는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
-class App {
-  play() {}
+const MISSIONUTILS_IO = require("@woowacourse/mission-utils");
+
+class Game {
+  constructor(){
+    this.utilsIo = MISSIONUTILS_IO.Console;
+    
+    this.userNumberArray = [];
+  }
+  inputUserNumber(text, callback) {
+    this.utilsIo.readLine(text, callback.bind(this));
+  }
 }
 
+class App {
+  constructor() {
+    this.game = new Game();
+  }
+  play() {
+  }
+}
+
+const app = new App();
+app.play();
+
 module.exports = App;
+
