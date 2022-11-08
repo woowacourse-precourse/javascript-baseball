43ee6b86d70203f28e24612f02f90607c0df3502
jincde <asdzxc@kakao.com>
2022-11-08 21:20:27 +0900

    chore : npm 설치 및 테스트

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+const GET_ANSWER = require("./")
+
 class App {
-  play() {}
+  play() { }
 }
 
 module.exports = App;
+
+const getAnswer = require("./createAnswer");
+
+class App {
+  play() { }
+  play() {
+    const answer = getAnswer();
+  }
+}
+
+const app = new App();
+app.play();
