500ef803e2be748424e225981de594bda701b0f5
Jungkyu Yang <67031524+jungkyuYang@users.noreply.github.com>
2022-11-08 22:59:39 +0900

    test: add countBall(), countStrike(), createComputerNumber() 테스트 케이스

--- __tests__/FeatureTest.js
+++ __tests__/FeatureTest.js
@@ -24,7 +24,7 @@ const getLogSpy = () => {
 };
 
 describe("숫자 야구 게임", () => {
-  test("게임 시작 메세지 테스트", () => {
+  test("첫 게임에만 시작 메세지 출력", () => {
     const logSpy = getLogSpy();
 
     const app = new App();
@@ -32,7 +32,7 @@ describe("숫자 야구 게임", () => {
     expect(logSpy).toHaveBeenCalledWith("숫자 야구 게임을 시작합니다.");
   });
 
-  test("입력값 예외 테스트 - 숫자", () => {
+  test("문자열 입력했을 때 예외처리", () => {
     const randoms = [1, 3, 5];
     const answers = ["5sd"];
 
@@ -45,7 +45,20 @@ describe("숫자 야구 게임", () => {
     }).toThrow("숫자를 입력해주세요.");
   });
 
-  test("입력값 유효성 검사 테스트 - 길이", () => {
+  test("숫자 '0'입력했을 떄 예외처리", () => {
+    const randoms = [1, 3, 5];
+    const answers = ["0"];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow("숫자를 입력해주세요.");
+  });
+
+  test("길이가 3이 아닐 떄 예외처리", () => {
     const randoms = [1, 3, 5];
     const answers = ["123456"];
 
@@ -58,7 +71,7 @@ describe("숫자 야구 게임", () => {
     }).toThrow("3자리로 입력해주세요.");
   });
 
-  test("입력값 유효성 검사 테스트 - 중복", () => {
+  test("중복값이 있을 때 예외처리", () => {
     const randoms = [1, 3, 5];
     const answers = ["333"];
 
@@ -71,11 +84,46 @@ describe("숫자 야구 게임", () => {
     }).toThrow("서로 다른 값을 입력해주세요.");
   });
 
-  test("게임 종료 테스트", () => {
+  test.each([
+    [[1, 2, 3], [1, 2, 3], 0],
+    [[1, 2, 3], [1, 4, 2], 1],
+    [[1, 2, 3], [1, 3, 2], 2],
+    [[1, 2, 3], [3, 1, 2], 3],
+  ])("ball카운터(%s, %s) return %s ", (user, computer, expected) => {
+    const app = new App();
+    expect(app.countBall(user, computer)).toEqual(expected);
+  });
+
+  test.each([
+    [[1, 2, 3], [4, 5, 6], 0],
+    [[1, 2, 3], [1, 5, 6], 1],
+    [[1, 2, 3], [4, 2, 3], 2],
+    [[1, 2, 3], [1, 2, 3], 3],
+  ])("strike카운터(%s, %s) return %s ", (user, computer, expected) => {
+    const app = new App();
+    expect(app.countStrike(user, computer)).toEqual(expected);
+  });
+
+  test("1에서 9까지 서로 다른 임의의 수 3개를 생성", () => {
+    const randoms = [3, 3, 6, 6, 9, 9];
+    mockRandoms(randoms);
+
+    const app = new App();
+    const randomNumbers = app.createComputerNumber();
+    const randomNumbersSet = new Set(randomNumbers);
+
+    expect(Array.isArray(randomNumbers)).toEqual(true);
+    expect(randomNumbers.length).toEqual(3);
+    expect(randomNumbers.length === randomNumbersSet.size).toEqual(true);
+    expect(randomNumbers).toEqual([3, 6, 9]);
+  });
+
+  test("게임 종료 확인", () => {
     const randoms = [1, 3, 5, 5, 8, 9];
     const answers = ["246", "135", "1", "597", "589", "2"];
     const logSpy = getLogSpy();
     const messages = [
+      "숫자 야구 게임을 시작합니다.",
       "낫싱",
       "3스트라이크",
       "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
@@ -96,9 +144,9 @@ describe("숫자 야구 게임", () => {
     });
   });
 
-  test("게임종료 입력값 유효성 검사 테스트", () => {
+  test("1 또는 2 외의 문자열 입력 시 예외처리", () => {
     const randoms = [1, 3, 5, 5, 8, 9];
-    const answers = ["246", "135", "3"];
+    const answers = ["135", "3"];
 
     mockRandoms(randoms);
     mockQuestions(answers);
