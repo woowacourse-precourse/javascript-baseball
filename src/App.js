3f11f78565e1913f774113a3c0788080d0b54642
이여진 <jinspace3710@gmail.com>
2022-11-08 20:05:11 +0900

    feat: 서로 다른 임의의 수 3개 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+const { Console, Random } = MissionUtils;
+
 class App {
-  play() {}
+  play() {
+    Console.print('숫자 야구 게임을 시작합니다.');
+    startGame();
+  }
 }
+
+const startGame = () => {
+  let randomNumberArray = [];
+
+  for (let i = 0; i < 3; i++) {
+    let randomNumber = Random.pickNumberInRange(1, 9);
+    if (!randomNumberArray.includes(randomNumber))
+      randomNumberArray.push(randomNumber);
+  }
+};
 
 module.exports = App;
