27a6beb7a92a2e5091af4c74fea89d324fca6ebb
catca <kdg96g@gmail.com>
2022-11-08 19:48:50 +0900

    feat: 스트라이크, 볼 판별하는 함수 테스트

--- __tests__/BaseballTest.js
+++ __tests__/BaseballTest.js
@@ -14,29 +14,57 @@ describe("gameTool 테스트", () => {
 
     expect(set.size).toBe(3);
   });
+
   test("isValidateNumber, 입력받은 값이 겹치지 않는 3자리 수인 경우 true", () => {
     const wrongNumbers = ['123', '813', '352'];
     wrongNumbers.forEach(wrongNumber => {
       expect(gameTool.isValidateNumber(wrongNumber)).toBe(true);
     });
   });
+
   test("isValidateNumber, 입력받은 값이 1-9 사이의 숫자가 아닌 경우 false", () => {
     const wrongNumbers = ['240', '2.3', 's56'];
     wrongNumbers.forEach(wrongNumber => {
       expect(gameTool.isValidateNumber(wrongNumber)).toBe(false);
     });
   });
+
   test("isValidateNumber, 입력받은 값이 3자리가 아닌 경우 false", () => {
     const wrongNumbers = ['1234', '84213', '35'];
     wrongNumbers.forEach(wrongNumber => {
       expect(gameTool.isValidateNumber(wrongNumber)).toBe(false);
     });
   });
+
   test("isValidateNumber, 입력받은 값이 중복되는 값이 있는 경우 false", () => {
     const wrongNumbers = ['122', '888', '339'];
     wrongNumbers.forEach(wrongNumber => {
       expect(gameTool.isValidateNumber(wrongNumber)).toBe(false);
     });
+  });
+
+  test("checkBaseballCount, 스트라이크, 볼 판별", () => {
+    const tests = [
+      {
+        answer: '734',
+        input: '137',
+        result: {strike: 1, ball: 1},
+      },
+      {
+        answer: '849',
+        input: '849',
+        result: {strike: 3, ball: 0},
+      },
+      {
+        answer: '276',
+        input: '627',
+        result: {strike: 0, ball: 3},
+      },
+    ]
+    
+    tests.forEach(test => {
+      expect(gameTool.checkBaseballCount(test.answer, test.input)).toStrictEqual(test.result);
+    })
   });
 });
 
