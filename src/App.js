b2da6a069356b651e3edd152946e35e12bcd1654
Wooseong Kim <kimwooseong8313@gmail.com>
2022-11-08 17:44:36 +0900

    feat: 컴퓨터 랜덤 숫자 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  printGameStart() {
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+  }
+
+  createComputerNumber() {
+    const computerNumber = [];
+
+    while (computerNumber.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computerNumber.includes(number)) {
+        computerNumber.push(number);
+      }
+    }
+
+    return computerNumber.join("");
+  }
 }
 
 module.exports = App;
