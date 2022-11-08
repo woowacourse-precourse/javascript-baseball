1fb7800d28a34450afd7b4387f34a25a96b273ff
rickyshu <hoinleekk@naver.com>
2022-11-08 18:04:38 +0900

    player가 입력한 값이 유효한 값인지 판별하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 const { Console } = require("@woowacourse/mission-utils");
 const { MESSAGE } = require("./constant/constants");
 const makeRandomNumber = require("./utils/randomNumber");
+const { validNumber } = require("./utils/checkInputNumber");
 
 class App {
   play() {
@@ -11,6 +12,19 @@ class App {
   //램덤 숫자(답) 미리 구하기!
   gameSetter() {
     const anwer = makeRandomNumber.createRandomNumber();
+    this.startGame(anwer);
+  }
+  startGame() {
+    Console.readLine(MESSAGE.INPUT, (input) => {
+      //사용자 입력한 값의 유효 여부를 먼저 확인한다
+      if (!validNumber(input)) this.inputException();
+      else {
+      }
+    });
+  }
+
+  inputException() {
+    throw new Error(MESSAGE.WRONG_INPUT);
   }
 }
 const app = new App();
