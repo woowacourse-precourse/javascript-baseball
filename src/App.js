e8e3a041d52f83234bff3285d6ce8c86e0210545
JeongSabibi <jsbb528@naver.com>
2022-11-08 17:52:15 +0900

    feat: 서로 다른 숫자 3개 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  play() {
+    const computer = makeRandomNumber(MissionUtils);
+  }
+}
+
+function makeRandomNumber() {
+  var computer = [];
+  while(computer.length < 3) {
+    var number = MissionUtils.Random.pickNumberInRange(1, 9);
+    if(computer.includes(number) ? false : true) {
+      computer.push(number);
+    }
+  }
+  return computer.join('');
 }
 
 module.exports = App;
