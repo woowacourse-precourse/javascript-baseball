4c21fbad9337656fc498b953d1e09f460ae84943
Lim_JiSeon <dpf226@naver.com>
2022-11-08 22:05:25 +0900

    feat(Functions.js) : add functions - findInputNumExcept - printBallStrike - playGame

--- src/components/Functions.js
+++ src/components/Functions.js
@@ -46,10 +46,55 @@ function returnResult(ball, strike) {
     else return Messages.NOTHING
 }
 
+function findInputNumExcept(inputNum) {
+    let inputString = inputNum.toString();
+    if (inputString.indexOf(0) != -1) return Messages.ERROR.ERROR_RANGE;
+    if (inputString.length != 3) return Messages.ERROR.ERROR_NUMBER_COUNT;
+    if (Set(inputString).length != 3) return Messages.ERROR.ERROR_OVERLAP;
+    return false;
+}
+
 function printBallStrike(computerNum) {
+    let strike = 0;
+    let ball = 0;
     Console.readLine(Messages.INPUT_NUM, (inputNum) => {
-        Console.print(returnResult(countBall(inputNum,computerNum),countStrike(inputNum,computerNum)))
+        const error = findInputNumExcept(inputNum);
+        if (error) {
+            throw new Error(error);
+        }
+        strike = countStrike(inputNum,computerNum);
+        ball = countBall(inputNum,computerNum);
+        Console.print(returnResult(ball,strike));
     });
+    Console.close();
+    return strike;
+}
+
+function selectRestart() {
+    Console.readLine(Messages.RESTART_OR_CLOSE, (inputCommand) => {
+        const playCommand = replayGame(inputCommand);
+        if (inputCommand != START_GAME || inputCommand != END_GAME) {
+            throw new Error(Messages.ERROR.ERROR_VALUE);
+        }
+        Console.close();
+        return playCommand;
+    });
+}
+
+function playGame() {
+    let playing = true;
+    Console.print(Messages.START);
+
+    while (playing) {
+        let computerNum = selectComputer();
+        let playRound = true;
+        while (playRound) {
+            let countStrike = printBallStrike(computerNum);
+            playRound = endGame(countStrike);
+        }
+        Console.print(Messages.SUCCESS);
+        playing = selectRestart();
+    }
 }
 
 module.exports = Functions;
