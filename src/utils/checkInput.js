3f5c8698aaf15d6ba5a345fbe4f61c11a127cc8d
leeharyung <lhr4884@naver.com>
2022-11-08 22:10:18 +0900

    feat: check user input range

--- src/utils/checkInput.js
+++ src/utils/checkInput.js
@@ -3,3 +3,8 @@ const { RESTART_INPUT, ERROR } = require("../constants/constants");
 function checkLength(input, length) {
   return input.length === length;
 }
+
+function checkRange(input, min, max) {
+  const inputArray = [...input];
+  return inputArray.every((number) => number >= min && number <= max);
+}
