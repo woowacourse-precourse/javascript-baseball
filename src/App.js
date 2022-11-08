1a8e7b39306b90b9855b6c2265fe2164a4227d8f
gogh21 <gogh.phy@gmail.com>
2022-11-08 21:34:52 +0900

    feat: check whether game result is strike, ball or nothing

--- src/App.js
+++ src/App.js
@@ -49,6 +49,26 @@ class App {
         }
       }
     }
+
+    // 게임 결과를 체크하는 함수
+    function checkGameResult() {
+      getUserInput();
+      compareNumber();
+      // 정답일 경우
+      if (strike === 3) {
+        MissionUtils.Console.print("3스트라이크");
+        return;
+      } else if (nothing === 3) {
+        MissionUtils.Console.print("낫싱");
+      } else if (ball > 0 && strike > 0) {
+        MissionUtils.Console.print(`${ball}볼 ${strike}스트라이크`);
+      } else if (ball > 0 && strike === 0){
+        MissionUtils.Console.print(`${ball}볼`);
+      } else if (ball === 0 && strike > 0){
+        MissionUtils.Console.print(`${strike}스트라이크`);
+      }
+      return checkGameResult();
+    }
   }
 }
 
