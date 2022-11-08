9530fd0e5ebc87edc7b3c64ab62a97c36e3c0837
SorinKwon <ksl020700@gmail.com>
2022-11-08 16:52:49 +0900

    test: 컴퓨터 랜덤 숫자 중복 확인 테스트 코드 추가
    테스트 코드 추가:
    - 컴퓨터 랜덤 숫자가 중복되지 않는 숫자인지 테스트

--- __tests__/Test.js
+++ __tests__/Test.js
@@ -1,9 +1,24 @@
 const App = require("../src/App");
 
+const duplicate_number = (n)=>{
+    if(n[0]==n[1]) return 0;
+    if(n[0]==n[2]) return 0;
+    if(n[1]==n[2]) return 0;
+    return 1;
+}
+
 describe("함수 기능 테스트", () => {
     test("selectComputerNumber() 함수로 컴퓨터 숫자 3개를 반환", () => {
         const app =new App();
         const result = app.selectComputerNumber().length;
         expect(result).toEqual(3);
+    });
+    test("selectComputerNumber() 함수로 컴퓨터 숫자 서로 다른 숫자 반환", () => {
+        const app =new App();
+        let result = app.selectComputerNumber();
+        if(duplicate_number(result)){
+            result=0;
+        }
+        expect(result).toEqual(0);
     });
 });
