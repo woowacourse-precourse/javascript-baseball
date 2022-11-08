ab72a64a1ba4adc6e2694521b3e8ff92d69801f3
KPGY <yuchw8@korea.ac.kr>
2022-11-08 23:05:05 +0900

    게임을 시작할 play 함수 및 컴퓨터의 랜덤숫자를 생성하는 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,4 +1,19 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 class App {
+  play() {
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    this.Randomnum();
+  }
+
+  Randomnum() {
+    var computernum =[];
+    while (computernum.length < 3) {
+      let num = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computernum.includes(num)) {
+        computernum.push(num);
+      }
+    }
+    this.Userinput(computernum);
+  }
 }
 module.exports = App;
