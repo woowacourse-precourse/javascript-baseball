bb8b3f031f3adf97aa83205c6282ed4e685af714
HWI <dlwognl99@naver.com>
2022-11-08 12:13:48 +0900

    refactor: 예외처리 클래스 생성 refactor: 변수 명 변경, 불필요한 코드 삭제

--- src/App.js
+++ src/App.js
@@ -6,19 +6,19 @@ class App {
   }
   play() {
     // 컴퓨터 랜덤 값 생성
-    this.computerNumber = RandomChoice();
+    this.computerRandomValue = RandomChoice();
     // 사용자 값 입력
     this.userInput();
-    console.log(this.computerNumber);
   }
   userInput() {
-    let userNumber;
-    MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (answer) => {
-      userNumber = answer.split("").map((number) => +number);
-      checkValidNumber(answer);
-      const resultCount = getCount(userNumber, this.computerNumber);
+    let userInputValue;
+    const validation = new Validation();
+    MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (value) => {
+      userInputValue = value.split("").map((number) => +number);
+      validation.checkUserInput(value);
+      const resultCount = getCount(userInputValue, this.computerRandomValue);
       getResult(resultCount);
-      console.log(userNumber);
+      console.log(userInputValue);
       if (resultCount.strike !== 3) {
         return this.userInput();
       }
@@ -26,15 +26,16 @@ class App {
     });
   }
   gameClear() {
+    const validation = new Validation();
     MissionUtils.Console.readLine(
       "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
-      (answer) => {
-        checkValidation(answer);
-        console.log(answer);
-        if (+answer === 1) {
+      (value) => {
+        validation.checkUserEndInput(value);
+        console.log(value);
+        if (+value === 1) {
           this.play();
         }
-        if (+answer === 2) {
+        if (+value === 2) {
           MissionUtils.Console.print("게임 종료");
           MissionUtils.Console.close();
         }
@@ -42,21 +43,47 @@ class App {
     );
   }
 }
-
-function checkValidation(answer) {
-  if (![1, 2].includes(+answer)) throw new Error("1과 2만 입력할 수 있습니다.");
+// 예외처리 클래스
+class Validation {
+  checkUserInput(value) {
+    const checkDeplicated = [...new Set(value)];
+    if (value.length !== 3) throw new Error("3글자여야 합니다.");
+    if (!value.match(/[1-9]/)) {
+      throw new Error("1에서 9짜리의 숫자만 입력해주세요.");
+    }
+    if (checkDeplicated.length !== 3) {
+      throw new Error("중복되지 않는 값을 입력해주세요.");
+    }
+  }
+  checkUserEndInput(value) {
+    if (![1, 2].includes(+value))
+      throw new Error("1과 2만 입력할 수 있습니다.");
+  }
 }
-function getCount(userNumber, computerNumber) {
+
+function RandomChoice() {
+  let randomSet = new Set();
+  addNumber(randomSet);
+  return [...randomSet];
+}
+
+function addNumber(randomSet) {
+  const randomNumber = MissionUtils.Random.pickNumberInRange(1, 9);
+  randomSet.add(randomNumber);
+  if (randomSet.size < 3) addNumber(randomSet);
+}
+
+function getCount(userInputValue, computerRandomValue) {
   const result = {
     strike: 0,
     ball: 0,
   };
-  userNumber.forEach((userElement, index) => {
-    const computerElement = computerNumber[index];
-    if (userElement === computerElement) {
+  userInputValue.forEach((userElement, index) => {
+    const computerElement = computerRandomValue[index];
+    if (computerElement === userElement) {
       return result.strike++;
     }
-    if (computerNumber.includes(userElement)) {
+    if (computerRandomValue.includes(userElement)) {
       return result.ball++;
     }
   });
@@ -65,43 +92,17 @@ function getCount(userNumber, computerNumber) {
 
 function getResult(result) {
   let resultMessage = "";
+
+  if (resultMessage === "") {
+    resultMessage = "낫싱";
+  }
   if (result.ball) {
     resultMessage += `${result.ball}볼`;
   }
   if (result.strike) {
     resultMessage += ` ${result.strike}스트라이크`;
   }
-  if (resultMessage === "") {
-    resultMessage = "낫싱";
-  }
   MissionUtils.Console.print(resultMessage);
 }
-
-function RandomChoice() {
-  let randomNumber = new Set();
-  addNumber(randomNumber);
-  randomNumber = [...randomNumber];
-  return randomNumber;
-}
-
-function addNumber(randomNumber) {
-  const number = MissionUtils.Random.pickNumberInRange(1, 9);
-  randomNumber.add(number);
-  if (randomNumber.size < 3) addNumber(randomNumber);
-}
-
-// 유저 입력 예외처리
-function checkValidNumber(answer) {
-  const checkDeplicated = [...new Set(answer)];
-  if (answer.length !== 3) throw new Error("3글자여야 합니다.");
-  if (!answer.match(/[1-9]/)) {
-    throw new Error("1에서 9짜리의 숫자만 입력해주세요.");
-  }
-  if (checkDeplicated.length !== 3) {
-    throw new Error("중복되지 않는 값을 입력해주세요.");
-  }
-}
-const app = new App();
-app.play();
 
 module.exports = App;
