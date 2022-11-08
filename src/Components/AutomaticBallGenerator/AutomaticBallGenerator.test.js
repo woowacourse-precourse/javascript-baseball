1f5d898867edd90ccf14176e07b1cb5bad9dde72
RyuJoonSik <tldtksahtxja@naver.com>
2022-11-09 01:27:42 +0900

    test: AutomaticBallGenerator.test.js에 중복 테스트 추가

--- src/Components/AutomaticBallGenerator/AutomaticBallGenerator.test.js
+++ src/Components/AutomaticBallGenerator/AutomaticBallGenerator.test.js
@@ -3,7 +3,15 @@ const { mockRandoms } = require("../../mockFunction");
 
 describe("AutomaticBallGenerator", () => {
   test("execute로 숫자를 생성", async () => {
-    mockRandoms([[1, 2, 3]]);
+    mockRandoms([1, 2, 3]);
+    const BALL_GENERATOR = new AutomaticBallGenerator();
+    const BALL = await BALL_GENERATOR.execute();
+
+    expect(BALL.getNumber()).toBe(123);
+  });
+
+  test("중복이 있으면 다시 생성", async () => {
+    mockRandoms([1, 1, 1, 1, 2, 3]);
     const BALL_GENERATOR = new AutomaticBallGenerator();
     const BALL = await BALL_GENERATOR.execute();
 
