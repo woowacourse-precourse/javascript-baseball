717672464798bb3a43b4ae86b16a695801e8f276
cheonyeji <ssaltuck@gmail.com>
2022-11-08 20:38:50 +0900

    feat: 랜덤 숫자 생성

--- src/App.js
+++ src/App.js
@@ -5,6 +5,19 @@ class App {
   startGame() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
   }
+
+  generateNum() {
+    var num = [];
+
+    while (num.length < 3) {
+      var randomNum = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (num.includes(randomNum) == false) {
+        num.push(randomNum);
+      }
+    }
+
+    return num;
+  }
 }
 
 module.exports = App;
