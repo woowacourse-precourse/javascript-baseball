0d5669275bd3ad217becbc574730331cb98d2ec7
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2022-11-08 21:09:19 +0900

    feat : 스트라이크, 볼 개수 계산 구현

--- src/Baseball.js
+++ src/Baseball.js
@@ -6,11 +6,27 @@ class Baseball {
     Console.print("숫자 야구 게임을 시작합니다.");
     this.computerNumberSet = new Computer().pickRandomNumberSet();
   }
+  getScore(computerNumberSet, userNumberSet) {
+    let strike = 0;
+    let ball = 0;
+
+    userNumberSet.forEach((number, idx) => {
+      if (number === computerNumberSet[idx]) strike++;
+      else if (
+        number !== computerNumberSet[idx] &&
+        computerNumberSet.includes(number)
+      )
+        ball++;
+    });
+    return [strike, ball];
+  }
 
   playGame() {
     Console.readLine("숫자를 입력해주세요 : ", (userNumber) => {
       exception(userNumber);
       userNumber = new User().getUserArray(userNumber);
+
+      const [strike, ball] = this.getScore(this.computerNumberSet, userNumber);
     });
   }
 }
