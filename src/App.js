65203db566ddc0fbcd61751d5b962527fc25db67
f1rstf1y9 <f1rstf1y9@naver.com>
2022-11-08 19:52:39 +0900

    feat: 게임 시작 후 최초 라운드에 시작 문구 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+const { Console, Random } = require("@woowacourse/mission-utils/");
+
 class App {
-  play() {}
+  
+  constructor() {
+    this.isPlayingNow = false;
+  }
+
+  roundStart() {
+    if (!this.isPlayingNow) {
+      this.isPlayingNow = true;
+      Console.print("숫자 야구 게임을 시작합니다.");
+    }
+  }
+  
+  play() {
+    this.roundStart();
+  }
+
 }
 
 module.exports = App;
