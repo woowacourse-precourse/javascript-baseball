13ba64901650976d4f285ca26f064b56f42e7093
Ji Seungha <jish0210@naver.com>
2022-11-08 16:17:53 +0900

    Feature1 : Generate Random Numbers implemented

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
+function generateRandomNumbers(){
+  const numberArray= [];
+  while (numberArray.length < 3) {
+    const number = MissionUtils.Random.pickNumberInRange(1, 9);
+    if (!numberArray.includes(number)) {
+      numberArray.push(number);
+    }
+  }
+  return numberArray;
+}
+
 class App {
-  play() {}
+  constructor() {
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+  }
+  play() {
+    this.startGame();
+  }
+  startGame() {
+    var computer = generateRandomNumbers();
+  }
 }
 
 module.exports = App;
