c15f957fafb9a02b03091f6a995abb0af8275dff
woo7i <or2447221@gmail.com>
2022-11-08 22:28:17 +0900

    로직 오류 수정 및 테스트 통과

--- src/App.js
+++ src/App.js
@@ -2,22 +2,24 @@ const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
   constructor(){
-    this.computerInputNum = []
+    this.computerInputNum = [] // 초기화 영역
   };
 
   play() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
-    this.computerInputRandomNum();
+    this.computerInputRandomNum()
     this.userInputRandomNum();
   };
 
   computerInputRandomNum(){
     this.computerInputNum = []
-    let comNum = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3)
-    if(this.computerInputNum.length = 3){
-     this.computerInputNum.push(comNum);
-    };
-  };
+    while (this.computerInputNum.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!this.computerInputNum.includes(number)) {
+        this.computerInputNum.push(number);
+      }
+    }
+  }
 
   userInputRandomNum(){
     MissionUtils.Console.readLine('숫자를 입력 해주세요 : ',(answer) => {
@@ -27,7 +29,7 @@ class App {
   };
 
   checkError(answer){
-    const answerCheck = answer.toString().split('').map(Number)
+    const answerCheck = answer.split('').map(Number)
     if(answerCheck.length !== 3) throw '숫자의 길이는 3자리 이하이여야 합니다.'
     if(answerCheck.includes(('0'))) throw '0을 제외한 숫자를 입력하세요.'
     if(answerCheck.includes(NaN)) throw '숫자가 아닙니다.'
@@ -40,7 +42,7 @@ class App {
   };
 
   collectAnswer(answer){
-    let userAnswer = answer.toString().split('').map(Number)
+    let userAnswer = answer.split('').map(Number)
     let checkStrike = 0
     let checkBall = 0
 
@@ -48,16 +50,20 @@ class App {
       if(userAnswer[i] === this.computerInputNum[i]) checkStrike +=1;
       else if(this.computerInputNum.includes(userAnswer[i])) checkBall +=1;
     };
+    this.getAnswer(checkStrike,checkBall)
   };
 
-  getAnswer(){
-    if (checkStrike === 0 && checkBall === 0) MissionUtils.Console.print("낫싱");
-    else MissionUtils.Console.print(`${checkBall}:볼 ${checkStrike}:스트라이크`);
+  getAnswer(checkStrike,checkBall){
+    if(checkBall === 0 && checkStrike) MissionUtils.Console.print(`${checkStrike}스트라이크`);
+    else if(checkStrike === 0 && checkBall) MissionUtils.Console.print(`${checkBall}볼`);
+    else if (checkStrike === 0 && checkBall === 0) MissionUtils.Console.print("낫싱");
+    else MissionUtils.Console.print(`${checkBall}볼 ${checkStrike}스트라이크`);
 
     if(checkStrike === 3) {
       MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
       this.endGame()
-    };
+    }
+    else this.userInputRandomNum()
   };
 
   endGame(){
@@ -67,13 +73,12 @@ class App {
   };
 
   resetGame(value){
-    if(value = "1") app.play()
-    else if(value = "2") MissionUtils.Console.close();
+    if(value === "1") app.play()
+    else if(value === "2") MissionUtils.Console.close();
   }
 }
 
 const app = new App();
-app.play();
-console.log(app)
+app.play()
 
 module.exports = App;
