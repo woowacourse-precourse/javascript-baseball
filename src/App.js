7edceb617e914ae61b78c20758319169702c7354
jw <always1997@kakao.com>
2022-11-08 23:19:54 +0900

    refactor: 실행을 위해 리팩토링

--- src/App.js
+++ src/App.js
@@ -17,16 +17,17 @@ class App {
   }
 
   inputValidation(input) {
-    if(input.length !== 3)  {
+    let testInput = input.split('').map((num)=> Number(num));
+    if(testInput.length !== 3)  {
       throw new("입력은 세자리 숫자로 해주세요.");
     }
-    if(input.some((num)=> isNaN(num))) {
+    if(testInput.some((num)=> isNaN(num))) {
       throw new("숫자만 입력 가능합니다.");
     }
-    if(checkRangeOver(input)) {
+    if(this.checkRangeOver(testInput)) {
       throw new("1~9사이의 숫자를 입력해주세요.")
     }
-    if(checkDupulicate(input)) {
+    if(this.checkDupulicate(testInput)) {
       throw new("서로 다른숫자로 입력해주세요.")
     }
     return true;
@@ -35,7 +36,7 @@ class App {
   getStrikeAndBall(comNums,userNums) {
     let strikeCounter = 0;
     let ballCounter = this.getBallCounter(comNums,userNums);
-    for(let i =0; i<userNums; i++) {
+    for(let i =0; i<userNums.length; i++) {
       if(userNums[i] === comNums[i]) {
         strikeCounter++;
       }
@@ -44,7 +45,7 @@ class App {
   }
   getBallCounter(comNums,userNums) {
     let ballCounter = 0;
-    for(let i =0; i<userNums; i++) {
+    for(let i =0; i<userNums.length; i++) {
       if(comNums.includes(userNums[i])) {
         ballCounter++;
       }
@@ -52,60 +53,54 @@ class App {
     return ballCounter;
   }
   restartOrEnd() {
-    let userCommand;
-    Console.readLine("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n",(input)=> {
-      userCommand = input;
+    Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    Console.readLine("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",(input)=> {
+      if (input !== '1' && input !== '2') {
+        throw new Error("1또는 2를 선택해야합니다.");
+      }
+      if(input === '1') {
+        return this.play();
+      }
+      Console.print("게임이 종료되었습니다.");
+      Console.close();
     });  
-    switch(userCommand) {
-      case '1' : this.play(); break;
-      case '2' : Console.close();
-      default : throw new Error("잘못된 입력입니다.");
-    }
   }
-  gameOver() {
-    Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n");
-    this.restartOrEnd();
-    
-  }
-  guessGame(strikeCounter,ballCounter,comNums) {
-    if(strikeCounter === 3) {
-      return gameOver();
-    }
+  
+  guessGame(comNums,userNums) {
+    console.log(comNums,userNums,"답지 컴터,유저 정보")
+    const [strikeCounter,ballCounter] = this.getStrikeAndBall(comNums,userNums);
     Console.print(this.resultMessage(strikeCounter,ballCounter));
-    return this.userInput(comNums);
+    if(strikeCounter !== 3) {
+      return this.userInput(comNums);
+    }
+    restartOrEnd();
   }
 
   resultMessage(strikeCounter,ballCounter) {
     let message = '';
-    
     if(ballCounter) {
-      message.concat(`${ballCounter}볼 `);
+      message+=`${ballCounter}볼 `;
     }
     if(strikeCounter) {
-      message.concat(`${strikeCounter}스트라이크 `);
+      message+=`${strikeCounter}스트라이크 `;
     }
     return (!ballCounter && !strikeCounter) ? "낫싱" : message;
   }
   userInput(comNums) {
-    let userNums;
-    Console.readLine("숫자를 입력해주세요 : ",(answer)=> {
-      userNums = answer.split('').map((num)=>{return Number(num)});
+    Console.readLine("숫자를 입력해주세요 : ",(input)=> {
+      this.inputValidation(input);
+      this.guessGame(comNums,input.split('').map((num)=> Number(num)));
     });
-    const [strikeCounter,ballCounter] = this.getStrikeAndBall(comNums,userNums);
-    Console.print(this.resultMessage(strikeCounter,ballCounter));
   }
-  startBaseBallGame() {
+
+  play() {
     const comNums = Random.pickUniqueNumbersInRange(1, 9, 3);
     Console.print("숫자 야구 게임을 시작합니다.");
-    userInput(comNums);
-  }
-  
-  play() {
-    this.startBaseBallGame();
+    this.userInput(comNums);
   }
 }
 
-// const app = new App();
-// app.play();
+const app = new App();
+app.play();
 
 module.exports = App;
