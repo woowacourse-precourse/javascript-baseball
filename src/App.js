c355112527d04e6524c7daad13dacf7e0d9adc17
nara04040 <nara04040@naver.com>
2022-11-08 13:47:58 +0900

    feat : 컴퓨터 랜덤 숫자를 만드는 기능 구현

--- src/App.js
+++ src/App.js
@@ -2,8 +2,23 @@ const MissionUtils = require("@woowacourse/mission-utils");
 class App {
   constructor() {}
 
+  getComputerNumber() {
+    const computerNumber = [];
+    while (computerNumber.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computerNumber.includes(number)) {
+        computerNumber.push(number);
+      }
+    }
+    MissionUtils.Console.print(computerNumber);
+    return computerNumber;
+  }
+  gameStart() {
+    this.getComputerNumber();
+  }
   play() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    this.gameStart();
   }
 }
 const app = new App();
