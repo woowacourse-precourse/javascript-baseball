4a2581cfb47b5bf897e6922488b0ff3cf13515a4
aram <snsdl0905@naver.com>
2022-11-08 13:32:57 +0900

    feat: 컴퓨터의 랜덤 수를 생성하는 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 class App {
   play() {}
+
+  get_computer_number() {
+    let computer_number = [];
+    while (computer_number.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computer_number.includes(number)) computer_number.push(number);
+         
+    }
+    return computer_number;
+  }
+
 }
 
 module.exports = App;
