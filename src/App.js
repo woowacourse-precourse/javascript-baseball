5f6bff187ff79112d3080bbd468fe8a179a1ee90
hayeong <fibonacci2246@gmail.com>
2022-11-08 20:37:30 +0900

    feat: makeAnswer method

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import * as MissionUtils from "@woowacourse/mission-utils";
 
 class App {
+  #answer;
   constructor() {
     this.#missionUtils = new MissionUtils();
   }
@@ -9,8 +10,17 @@ class App {
     this.#missionUtils.print("숫자 야구 게임을 시작합니다.");
   }
 
+  #makeAnswer() {
+    const firstNumber = this.#missionUtils.getRandomNumber(1, 9);
+    const secondNumber = this.#missionUtils.getRandomNumber(1, 9);
+    const thirdNumber = this.#missionUtils.getRandomNumber(1, 9);
+
+    this.#answer = `${firstNumber}${secondNumber}${thirdNumber}`;
+  }
+
   play() {
     this.#printStartMessage();
+    this.#makeAnswer();
   }
 }
 
