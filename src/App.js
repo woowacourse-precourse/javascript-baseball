1dc5ec5eeb9c00bacc6bcfe5bb128cce2168065b
yeonkyoung <keep.ykk@gmail.com>
2022-11-08 23:10:15 +0900

    feat: 게임 시작 문구 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
+let user;
+let computer;
+let isEndGame = true;
+let result = true;
+const game_start = "숫자 야구 게임을 시작합니다.";
+
 class App {
-  play() {}
+  play() {
+    MissionUtils.Console.print(game_start);
+  }
 }
 
 module.exports = App;
