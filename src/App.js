9295bd29ead1f0be3a1d5b90fa98b0caef329c65
Wooseong Kim <kimwooseong8313@gmail.com>
2022-11-08 18:15:28 +0900

    feat: 사용자의 입력 숫자 판정

--- src/App.js
+++ src/App.js
@@ -17,6 +17,26 @@ class App {
 
     return computerNumber.join("");
   }
+
+  checkBallStrike(nums, answer) {
+    let ballStrikeCount = [0, 0];
+
+    if (answer.length !== 3) throw new Error("잘못된 입력입니다.");
+
+    for (let i = 0; i < 3; i++) {
+      let checkNums = Number(nums[i]);
+
+      if (!(1 <= checkNums && checkNums <= 9)) {
+        throw new Error("잘못된 입력입니다.");
+      } else if (nums[i] === checkNums) {
+        ballStrikeCount[1]++;
+      } else if (nums.indexOf(checkNums) !== -1) {
+        ballStrikeCount[0]++;
+      }
+    }
+
+    return ballStrikeCount;
+  }
 }
 
 module.exports = App;
