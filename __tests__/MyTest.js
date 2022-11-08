c08ad4b02d9abac18857dd65b3b10b5f53dee451
yeongi <cbg5255@gmail.com>
2022-11-08 15:33:17 +0900

    test (myTest) 결과 출력 멘트 테스트 작성

--- __tests__/MyTest.js
+++ __tests__/MyTest.js
@@ -1,6 +1,32 @@
 const App = require("../src/App");
+const MissionUtils = require("@woowacourse/mission-utils");
 
 const app = new App();
+
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(MissionUtils.Console, "print");
+  logSpy.mockClear();
+  return logSpy;
+};
+
+describe("출력 테스트", () => {
+  test("결과 출력 테스트", () => {
+    const result = [
+      { ball: 0, strike: 0 },
+      { ball: 3, strike: 0 },
+      { ball: 1, strike: 1 },
+      { ball: 0, strike: 3 },
+    ];
+    const logSpy = getLogSpy();
+    const messages = ["낫싱", "3볼", "1볼 1스트라이크", "3스트라이크"];
+
+    result.forEach((e) => app.resultPrint(e));
+
+    messages.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+});
 
 describe("숫자 비교 테스트", () => {
   const input = [
@@ -51,6 +77,4 @@ describe("예외처리 테스트", () => {
       app.answerChecker(input);
     }).toThrow();
   });
-
-  test("예외처리 테스트", () => {});
 });
