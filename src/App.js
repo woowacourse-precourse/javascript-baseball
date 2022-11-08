b6514350414c99493f6561eb1083473947e2bbe4
kaori-killer <sksbthwjd@daum.net>
2022-11-08 18:18:56 +0900

    feat: [게임 시작] 시작 메시지를 출력

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 const MISSIONUTILS_IO = require("@woowacourse/mission-utils");
 
+const TEXTS = Object.freeze({
+  START_TEXT: "숫자 야구 게임을 시작합니다.",
+  INPUT_TEXT: "숫자를 입력해주세요 : ",
+  END_TEXT: "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
+  RESELECT_TEXT: "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n"
+});
+
 class Game {
   constructor(){
     this.utilsIo = MISSIONUTILS_IO.Console;
     this.utilsRandom = MISSIONUTILS_IO.Random;
-
+    this.utilsIo.print(TEXTS.START_TEXT);
+    
     this.userNumberArray = [];
     this.compareNumberArray = [];
   }
