8ded2b339fa7d0b8ab02f6f737dbf30b58dcb7c8
ksg2388 <ksg2388@naver.com>
2022-11-08 18:13:13 +0900

    test(BaseballFunction): 테스트 케이스 추가

--- __tests__/BaseballFunctionTest.js
+++ __tests__/BaseballFunctionTest.js
@@ -3,16 +3,18 @@ const { getStrike, getBall } = require('../src/libs/BaseballFunction');
 describe('숫자야구 기능 테스트', () => {
   test('스트라이크 개수 확인', () => {
     const computer = [1, 2, 3];
-    const user = [1, 3, 5];
 
-    expect(getStrike(computer, user)).toEqual(1);
+    expect(getStrike(computer, [1, 3, 5])).toEqual(1);
+    expect(getStrike(computer, [3, 6, 7])).toEqual(0);
+    expect(getStrike(computer, [1, 2, 3])).toEqual(3);
   });
 
   test('볼 개수 확인', () => {
     const computer = [1, 2, 3];
-    const user = [1, 3, 5];
 
-    expect(getBall(computer, user)).toEqual(1);
+    expect(getBall(computer, [1, 3, 5])).toEqual(1);
+    expect(getBall(computer, [2, 3, 1])).toEqual(3);
+    expect(getBall(computer, [1, 2, 3])).toEqual(0);
   });
 
   test('스트라이크, 볼 개수 확인', () => {
