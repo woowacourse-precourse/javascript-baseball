ba60ac16fa4000da3e3e6d24a365652ced470013
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2022-11-08 19:18:53 +0900

    feat: add function 1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
-class App {
-  play() {}
-}
+const MissionUtils = require("@woowacourse/mission-utils");
 
+class App {
+  play() {
+    let randombaseball = computeRandomNumber();
+    console.log(randombaseball);
+    start();
+  }
+}
 module.exports = App;
+
+const computeRandomNumber = () => {
+  let number = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
+  return number;
+};
+
+const start = () => {
+  MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+};
+
+const app = new App();
+app.play();
