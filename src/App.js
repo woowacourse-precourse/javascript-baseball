fe47f7318c0ffdc4dad95884b2d9372d5b58480e
yuna Kim <yuna000804@gmail.com>
2022-11-08 13:22:21 +0900

    feat : 3스트라이크인 경우 판단 후 안내문구 출력

--- src/App.js
+++ src/App.js
@@ -44,6 +44,19 @@ class gameStart {
     }
   }
 
+  isThreeStrike(computer, user) {
+    if(computer === user) {
+      Console.print("3스트라이크");
+      Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+      Console.readLine("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n", (answer) => {
+        let notice = this.endOrRestart(answer);
+        notice;
+      });
+      return true;
+    }
+    return false;
+  }
+
 }
 
 class InputCheck {
