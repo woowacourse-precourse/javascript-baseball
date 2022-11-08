e871b186f1e22eab2ec05015f93966c20729e183
Hanav52 <wnsrl8329@gmail.com>
2022-11-08 23:07:21 +0900

    랜덤으로 숫자를 추출한다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
   play() {}
+  // 랜덤 숫자 추출
+  randomPickNumbers() {
+    const numbers = [];
+    while (numbers.length < 3) {
+      const number = MissionUtils.Random.randomPickNumbers(1, 9);
+      if (!numbers.includes(number)) numbers.push(number);
+    }
+
+    return numbers;
+  }
+
+  // 게임
+  play() {
+    const numbers = this.randomPickNumbers();
+  }
 }
+
+module.exports = App;
+const app = new App();
+app.play();
 
 module.exports = App;
