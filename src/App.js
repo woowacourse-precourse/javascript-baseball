5ebc105dd0c3c7749fdb2854b4ccba51459b8eda
recomplete <wantop@jbnu.ac.kr>
2022-11-08 19:51:05 +0900

    feat: 1~9까지의 서로 다른 임의의 수 3개 선택

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+const { Console } = require("@woowacourse/mission-utils");
+const { Random } = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  constructor() {
+    Console.print("숫자 야구 게임을 시작합니다.");
+  }
+
+  play() {
+    const randomNumbers = this.generateRandomNumbers();
+  }
+
+  generateRandomNumbers() {
+    const randomNumbers = [];
+
+    while (randomNumbers.length < 3) {
+      const randomNumber = Random.pickNumberInRange(1, 9);
+
+      if (!randomNumbers.includes(randomNumber)) {
+        randomNumbers.push(randomNumber);
+      }
+    }
+    return randomNumbers.join("");
+  }
 }
+const app = new App();
+app.play();
 
 module.exports = App;
