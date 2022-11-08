26aa2b9cde597dc907dc5ed90352d182a3a3031f
f1rstf1y9 <f1rstf1y9@naver.com>
2022-11-08 19:53:59 +0900

    feat: 최초 라운드에 서로 다른 임의의 수 3개를 선택하는 기능

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 const { Console, Random } = require("@woowacourse/mission-utils/");
+const NUMBER_LENGTH = 3;
 
 class App {
   
   constructor() {
     this.isPlayingNow = false;
+    this.computerNumberList = [];
+  }
+
+  getComputerNumberList() {
+    const pickedRandomNumberSet = new Set();
+    while(pickedRandomNumberSet.size < NUMBER_LENGTH) {
+      pickedRandomNumberSet.add(Random.pickNumberInRange(1, 9));
+    }
+    this.computerNumberList = [...pickedRandomNumberSet];
   }
 
   roundStart() {
     if (!this.isPlayingNow) {
       this.isPlayingNow = true;
       Console.print("숫자 야구 게임을 시작합니다.");
+      this.getComputerNumberList();
     }
   }
   
