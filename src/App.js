bb7fff4c551380a6d6c415f052452cdd4deecef4
soohyun <bmy1320@gmail.com>
2022-11-08 16:06:01 +0900

    refactor: createResult 메서드 리팩터링
    1. result 초깃값 낫싱
    2. if 문 할당 방식 변경

--- src/App.js
+++ src/App.js
@@ -23,12 +23,11 @@ class App {
 
   createResult() {
     const [ball, strike] = this.getCnt();
-    let result = "";
+    let result = "낫싱";
 
-    if (ball === 0 && strike === 0) result = "낫싱";
-    if (ball > 0) result += `${ball}볼`;
-    if (ball > 0 && strike > 0) result += " ";
-    if (strike > 0) result += `${strike}스트라이크`;
+    if (ball > 0 && strike > 0) result = `${ball}볼 ${strike}스트라이크`;
+    else if (ball > 0) result = `${ball}볼`;
+    else if (strike > 0) result = `${strike}스트라이크`;
 
     Console.print(result);
     if (result === MESSAGE.CLEAR) {
