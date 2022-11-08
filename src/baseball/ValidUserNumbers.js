a1064181f1ff321019598693da18a60e9d6798e8
Y2IN <dpdls0228@daum.net>
2022-11-08 22:08:21 +0900

    refactor : 괄호 추가

--- src/baseball/ValidUserNumbers.js
+++ src/baseball/ValidUserNumbers.js
@@ -2,16 +2,16 @@ const { GAME_MESSAGE, ERROR_MESSAGE } = require("../constants/constants");
 
 class ValidUserNumbers {
   isValidUserInput(userInput) {
-    const isNumberElement = (element) => element >= "1" && element <= "9";
+    const isNumberElement = (element) => (element >= "1" && element <= "9");
     const userNumbers = [];
-    userInput.split('').
-    forEach(element => {
+    userInput
+      .split('')
+      .forEach(element => {
       !userNumbers.includes(element)
         ? userNumbers.push(element)
         : userNumbers.push("NO");
     });
-
-    return (userNumbers.length === 3 && (userNumbers).every(isNumberElement));
+  return (userNumbers.length === 3 && (userNumbers).every(isNumberElement));
   }
 }
 
