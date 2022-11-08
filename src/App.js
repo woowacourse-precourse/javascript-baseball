505aee729ec8c31e7986a27aea953fb8a1d04e07
leesoyuun <leesoyun4206@naver.com>
2022-11-08 21:33:56 +0900

    Failed to catch errors

--- src/App.js
+++ src/App.js
@@ -5,29 +5,29 @@ class App {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
     startgame();
   }
-
-  startgame(){
-    const RandomNumber = MakeNumber();
-    InputNum(answer) = RandomNumber;
+}
+const startgame = () =>{
+    const answer = MakeNumber();
+    InputNum(answer);
 
     return;
   }
 
   //Make random number
-  MakeNumber(){
+const MakeNumber = () => {
     const RandomNumber = [];
     while(RandomNumber.length < 3){
       const numbers = MissionUtils.Random.pickNumberInRange(1,9);
       
       //Check for duplicates
-      if(!random.includes(numbers)){
+      if(!RandomNumber.includes(numbers)){
         RandomNumber.push(numbers);
       }
     }
     return RandomNumber.join("");
   }
 
-  InputNum(answer){
+const InputNum = (answer)=>{
     MissionUtils.Console.readLine("숫자를 입력해주세요 :", (number) => {
       const isValidInput = checkExcept(number);
       if (isValidInput == true) {
@@ -39,7 +39,7 @@ class App {
     return;
   }
 
-  checkExcept(number){
+const checkExcept = (number) => {
     const numArr = number.split("");
     const numSet = new Set(numArr);
 
@@ -53,30 +53,32 @@ class App {
 
   }
 
-  CheckAnswer(answer,number){
+const CheckAnswer = (answer,number) =>{
     const strike = 0;
     const ball = 0;
     for(let i = 0 ; i < 3 ; i++){
-      if(answer[i] === number[i])
-        strike++;
-      if(answer[i] == answer.includes(number[i]))
-        ball++;
+      if(answer[i] == number[i]){
+        strike +=1;
+      }
+      if (answer[i] != number[i] && answer.includes(number[i])){
+        ball +=1;
+      }
     }
     Result(answer,strike,ball);
     return;
   }
 
-  Result(answer,strike,ball){
+const Result = (answer,strike,ball) =>{
     if(strike == 0 && ball == 0){
       MissionUtils.Console.print("낫싱");
       InputNum(answer);
       return;
     }
-    else if(strike == 3){
+    if(strike == 3){
       End();
       return;
     }
-    const checkStrike = strike == 0 ? "":`${strike}스트라이크`;
+    const checkStrike = strike == 0 ? "": `${strike}스트라이크`;
     const checkBall = ball == 0 ? "" : `${ball}볼`;
 
     MissionUtils.Console.print(checkBall + checkStrike);
@@ -85,7 +87,7 @@ class App {
     return;
   }
 
-  End(){
+const End = () =>{
     MissionUtils.Console.print("3스트라이크");
     MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
 
@@ -99,6 +101,6 @@ class App {
     });
     return;
   }
-}
+
 module.exports = App;
 
