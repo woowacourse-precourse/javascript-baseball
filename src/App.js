8878565ab4faaeab09aac3f91cc70f1a332cb444
KUD <kimud6003@gmail.com>
2022-11-08 23:13:17 +0900

    🔥 HotFix: Game Class 생성후 변경 - App은 실행만 할수있도록 변경

--- src/App.js
+++ src/App.js
@@ -1,54 +1,8 @@
-const { RESTART, MSG } = require('./utils/Constant');
-const { Console } = require('@woowacourse/mission-utils');
-const asyncLine = require('./utils/ReadLine');
-const User = require('./User');
-const Computer = require('./Computer');
-const ScoreManager = require('./ScoreManager');
-
-// 게임을 진행하는 클래스
+const Game = require('./Game');
 class App {
-  constructor() {
-    this.User = new User();
-    this.Computer = new Computer();
-    this.ScoreManager = new ScoreManager();
-  }
-
   play() {
-    Console.print(MSG.START);
-    this.gameStart();
-  }
-
-  async gameStart() {
-    while (this.ScoreManager.isEndRound()) {
-      let targetNum = this.Computer.getComNums();
-      let userInput = this.User.getInputNums();
-
-      await this.User.setInputNums(MSG.INPUT, asyncLine);
-      this.ScoreManager.calScore(targetNum, userInput);
-      this.ScoreManager.getUserScoreStr();
-    }
-    this.gameResult();
-  }
-
-  async gameResult() {
-    Console.print(MSG.END);
-    let userSelect = await asyncLine(MSG.SELECT);
-    userSelect === RESTART ? this.gameRestart() : this.gameExit();
-  }
-
-  gameRestart() {
-    this.gameDataReset();
-    this.gameStart();
-  }
-
-  gameDataReset() {
-    this.User.resetUser();
-    this.Computer.resetComputer();
-    this.ScoreManager.resetScoreManager();
-  }
-
-  gameExit() {
-    Console.close();
+    let game = new Game();
+    game.play();
   }
 }
 
