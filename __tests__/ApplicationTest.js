ccfcea497f4a5341947609647dcabc5b59fc321c
Chanwoong1 <jcw7468@gmail.com>
2022-11-08 14:57:42 +0900

    test: 예외 테스트 변경

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -100,7 +100,7 @@ describe("숫자 야구 게임", () => {
 
   test("예외 테스트", () => {
     const randoms = [1, 3, 5];
-    const answers = ["1234", "122", "abc", "1 2 3", "12", "120"];
+    const answers = ["1234", "122", "abc", "1 2 3", "120"];
 
     mockRandoms(randoms);
     mockQuestions(answers);
