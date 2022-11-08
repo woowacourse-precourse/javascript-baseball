a642750909492faa1bf84547b0676c625b80dbc0
Lena <lny970116@gmail.com>
2022-11-08 22:30:34 +0900

    feat: 플레이어가 숫자를 반복해서 입력하게 한다.

--- src/App.js
+++ src/App.js
@@ -5,8 +5,22 @@ class App {
   randomArray() {
     return Random.pickUniqueNumbersInRange(1, 9, 3);
   }
+
+  recursiveInput() {
+    Console.readLine(MESSAGE.INPUT, inputNum => {
+      this.restart();
+    });
+  }
+
+  restart() {
+    this.computerNumber = this.randomArray();
+    this.recursiveInput();
+  }
+
   play() {
     Console.print(MESSAGE.START);
+    this.computerNumber = this.randomArray();
+    this.recursiveInput();
   }
 }
 
