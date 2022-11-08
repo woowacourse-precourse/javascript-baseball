e53444d2e4829f8b967c87d789e0f66a1776e46c
Yesong <to06109@naver.com>
2022-11-08 14:05:00 +0900

    feat(Message): 재시작/종료 번호, 게임 종료 메세지 추가

--- src/Message.js
+++ src/Message.js
@@ -3,6 +3,7 @@ const Message = {
   input: "숫자를 입력해주세요 : ",
   correct: "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
   restart: "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n",
+  gameover: "게임 종료",
   error: "잘못된 값을 입력했습니다. 프로그램을 종료합니다.",
 
   strike: "스트라이크",
@@ -10,6 +11,9 @@ const Message = {
   nothing: "낫싱",
 
   strike_3: "3스트라이크",
+
+  restartNum: "1",
+  gameoverNum: "2",
 };
 
 module.exports = Message;
