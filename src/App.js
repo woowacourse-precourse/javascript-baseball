1ef3c679087a59efc4a1551217f8ca80a966bdd7
Leejunyoung <39412219+moko0428@users.noreply.github.com>
2022-11-08 19:59:52 +0900

    feat: create computer unique numbers

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-const { Console } = require("@woowacourse/mission-utils");
+const { Console, Random } = require("@woowacourse/mission-utils");
 
 class App {
   play() {
     Console.print("숫자 야구 게임을 시작합니다.");
+    const computer = this.createComputerNumber(1, 9, 3);
+    console.log(computer);
+  }
+  createComputerNumber(start, end, count) {
+    const numbers = [];
+
+    while (numbers < count) {
+      numbers.push(Random.pickUniqueNumbersInRange(start, end, count));
+    }
+    return numbers[0];
   }
 }
 
