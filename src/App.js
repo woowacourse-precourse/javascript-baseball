60e2145f07659e36a33f4334c662ec434b0433f4
jiwon903 <50747295+jiwon903@users.noreply.github.com>
2022-11-08 23:54:44 +0900

    최종 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,7 +1,64 @@
+const MissionUtils = require("@woowacourse/mission-utils");
 class App {
-  play() {}
+  play() {
+    
+    var randomNum = makeRandomNum();
+    startGame(randomNum);
+    
+  
+    
+  }
 }
 
+function makeRandomNum(){
+  //컴퓨터가 랜덤한 3자리 수(1~9)를 생성
+  const randomSoleNum= MissionUtils.Random.pickUniqueNumbersInRange(1, 10, 3);
+  var randomNum=(randomSoleNum[0]*100)+(randomSoleNum[1]*10)+(randomSoleNum[2]);
+  return randomNum;
+}
+
+function startGame(randomNum){
+  
+  //게임 시작문구 출력
+  MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+  //사용자에게 숫자 입력받기
+  var userNumber;
+
+  console.log(randomNum);
+
+  MissionUtils.Console.readLine('숫자를 입력해주세요 : ',(answer)=>{
+    userNumber = Number(answer);
+    var answer = gameResult(randomNum,userNumber);
+    var finalAnswer = printGameResult(answer);
+    console.log(finalAnswer);
+
+
+    if(finalAnswer=="3개의 숫자를 모두 맞히셨습니다! 게임 종료"){
+      gameRestart();
+    }
+    else{
+      getUserInput();
+    }
+
+    // MissionUtils.Console.close();
+  });
+
+}
+
+function getUserInput(){
+  MissionUtils.Console.readLine('숫자를 입력해주세요 : ',(answer)=>{
+    userNumber = Number(answer);
+    var answer = gameResult(randomNum,userNumber);
+    var finalAnswer = printGameResult(answer);
+    console.log(finalAnswer);
+    if(finalAnswer=="3개의 숫자를 모두 맞히셨습니다! 게임 종료"){
+      gameRestart();
+    }
+    else{
+      getUserInput();
+    }
+  })
+}
 
 function checkStrike(randomNum,userNumber) {
   var strike=0;
@@ -78,8 +135,25 @@ function printGameResult(answer){
       return answer;
   }
   else{
-      answer="틀렸어요" //+다시 사용자의 입력을 받아야함
-      return answer;
+      return answer; //+다시 사용자의 입력을 받아야함
+      
   }
 }
 
+function gameRestart(){
+  var newanswer=0;
+  MissionUtils.Console.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n',(answer)=>{
+    newanswer = answer;
+    if(newanswer==2){
+      MissionUtils.Console.close();
+    }
+    else{
+      startGame();
+    }
+  })
+}
+
+
+//실행테스트
+const app = new App();
+app.play();
