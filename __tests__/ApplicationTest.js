c0ca79aea38a762ef35a8b6d0f94633c593a4a46
Hypark2018 <hayoungpark2018@gmail.com>
2022-11-08 22:03:26 +0900

    🧪 test: used destructuring assignment of using MissionUtils library to increase readability.

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -1,24 +1,24 @@
 const App = require("../src/App");
-const MissionUtils = require("@woowacourse/mission-utils");
+const { Console, Random } = require('@woowacourse/mission-utils');
 
 const mockQuestions = (answers) => {
-  MissionUtils.Console.readLine = jest.fn();
+  Console.readLine = jest.fn();
   answers.reduce((acc, input) => {
     return acc.mockImplementationOnce((question, callback) => {
       callback(input);
     });
-  }, MissionUtils.Console.readLine);
+  }, Console.readLine);
 };
 
 const mockRandoms = (numbers) => {
-  MissionUtils.Random.pickNumberInRange = jest.fn();
+  Random.pickNumberInRange = jest.fn();
   numbers.reduce((acc, number) => {
     return acc.mockReturnValueOnce(number);
-  }, MissionUtils.Random.pickNumberInRange);
+  }, Random.pickNumberInRange);
 };
 
 const getLogSpy = () => {
-  const logSpy = jest.spyOn(MissionUtils.Console, "print");
+  const logSpy = jest.spyOn(Console, "print");
   logSpy.mockClear();
   return logSpy;
 };
