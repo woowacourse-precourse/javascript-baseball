678706c5ac00e412e2b1b140c6b9dd15b0737bb4
wldnd2 <jiwoong000628@gmail.com>
2022-11-08 18:53:35 +0900

    feat: 컴퓨터 랜덤 숫자 생성

--- src/App.js
+++ src/App.js
@@ -9,7 +9,11 @@ class App {
     this.ball = 0;
     this.nothing = "낫싱";
   }
-  play() {}
+  play() {
+    Console.print("숫자 야구 게임을 시작합니다.");
+    // 기능 1번 구현
+    this.computer_random_number = Random.pickUniqueNumbersInRange(1, 9, 3);
+  }
 }
 
 module.exports = App;
