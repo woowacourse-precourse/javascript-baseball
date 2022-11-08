5c5c18d1d6faa0eb267b6b3d7ddc623a327fbb44
f1rstf1y9 <f1rstf1y9@naver.com>
2022-11-08 19:56:14 +0900

    feat: 사용자로부터 서로 다른 3개의 숫자를 입력받는 기능

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ class App {
   constructor() {
     this.isPlayingNow = false;
     this.computerNumberList = [];
+    this.playerNumberList = [];
   }
 
   getComputerNumberList() {
@@ -16,12 +17,19 @@ class App {
     this.computerNumberList = [...pickedRandomNumberSet];
   }
 
+  getPlayerNumberList() {
+    Console.readLine("숫자를 입력해주세요 : ", (answer) => {
+      this.playerNumberList = answer;
+    });
+  }
+
   roundStart() {
     if (!this.isPlayingNow) {
       this.isPlayingNow = true;
       Console.print("숫자 야구 게임을 시작합니다.");
       this.getComputerNumberList();
     }
+    this.getPlayerNumberList();
   }
   
   play() {
