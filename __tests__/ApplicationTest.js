f718b99daa7753c5cf195bb2d6dff266757022cb
Yoonkyoung Kim <100656920+Yoonkyoungme@users.noreply.github.com>
2022-11-08 18:26:37 +0900

    feat: 테스트 코드 추가

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -59,4 +59,121 @@ describe("숫자 야구 게임", () => {
       app.play();
     }).toThrow();
   });
+
+  test("예외 테스트: 입력 값이 3자리가 아닐 때", () => {
+    const randoms = [1, 3, 5];
+    const answers = ["12"];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow();
+  });
+
+  test("예외 테스트: 중복 입력", () => {
+    const randoms = [1, 3, 5];
+    const answers = ["177"];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow();
+  });
+
+  test("예외 테스트: 입력 값이 1 ~ 9 범위에 해당하는 수가 아닐 때", () => {
+    const randoms = [1, 3, 5];
+    const answers = ["107"];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow();
+  });
+
+  test("예외 테스트: 입력 값이 숫자가 아닐 때", () => {
+    const randoms = [1, 3, 5];
+    const answers = ["1a7"];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow();
+  });
+
+  test("게임 종료 후 잘못된 옵션 선택", () => {
+    const randoms = [3, 6, 9];
+    const answers = ["324", "369", "4"];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow();
+  });
+
+  test("게임 종료 후 잘못된 옵션 선택", () => {
+    const randoms = [3, 6, 9];
+    const answers = ["324", "369", "a"];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow();
+  });
+
+  test("사용자의 잘못된 입력시 예외 발생 후 애플리케이션 종료", () => {
+    const computer = [2, 3, 4];
+    const userInputs = ["875", "9a"];
+
+    mockRandoms(computer);
+    mockQuestions(userInputs);
+
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow();
+  });
+
+  test("게임을 종료하고 게임 재시작 & 애플리이션 완전 종료", () => {
+    const randoms = [3, 6, 9, 5, 8, 9];
+    const answers = ["458", "146", "321", "398", "369", "1", "597", "589", "2"];
+    const logSpy = getLogSpy();
+    const messages = [
+      "낫싱",
+      "1볼",
+      "1스트라이크",
+      "1볼 1스트라이크",
+      "3스트라이크",
+      "1볼 1스트라이크",
+      "3스트라이크",
+      "게임 종료",
+    ];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    const app = new App();
+    app.play();
+
+    messages.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
 });
