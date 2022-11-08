cbbb8a801cb8eb0ad351952527dcc625c3bbc040
soohyun_kim5 <93cherry@naver.com>
2022-11-08 19:37:52 +0900

    사용자 숫자 입력 받기

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 class App {
 
-  play() {
-    let computerNumber = new Set();
-    while (computerNumber.size < 3) {
-      computerNumber.add(MissionUtils.Random.pickNumberInRange(1, 9))
-    }
-    computerNumber = Array.from(computerNumber)
-    console.log(computerNumber)
+    play() {
+        let computerNumber = new Set();
+        while (computerNumber.size < 3) {
+            computerNumber.add(MissionUtils.Random.pickNumberInRange(1, 9))
+        }
+        computerNumber = Array.from(computerNumber)
+        console.log(computerNumber)
 
-    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.")
-  }
+        MissionUtils.Console.print("숫자 야구 게임을 시작합니다.")
+    }
+
+    read(computerNumber) {
+        let userNumber;
+        MissionUtils.Console.readLine("숫자를 입력해주세요.", (number) => {
+            userNumber = Array.from(number)
+        })
+    }
+
 }
 
 module.exports = App;
