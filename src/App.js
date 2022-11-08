772b8dbdceafc9a5402fe63d93f8d8dc0ab81344
JeongSabibi <jsbb528@naver.com>
2022-11-08 18:02:30 +0900

    feat: 게임 재시작 여부 확인

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App {
 
 function gameStart(computer) {
   MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (user) => {
+    checkUserNumber(user);
 
     var strike = strikeCount(computer, user);
     var ball = ballCount(computer, user);
@@ -26,6 +27,39 @@ function gameStart(computer) {
 
     gameStart(computer);
   });
+}
+
+function repeat() {
+  MissionUtils.Console.readLine("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n", (number) => {
+    if(number == 1) {
+      const computer = makeRandomNumber();
+      return gameStart(computer);
+    }
+    else if(number == 2) {
+      return MissionUtils.Console.close();
+    }
+    else {
+      throw new Error("1 또는 2를 입력하세요.");
+    }
+  });
+}
+
+function checkUserNumber(user) {
+  var userString = String(user);
+  var userArr = [...userString];
+  if(userString.length !== 3) {
+    throw new Error("3자리 숫자를 입력하세요.");
+  }
+  if(isNaN(user)) {
+    throw new Error("숫자를 입력하세요.");
+  }
+  if(userString.includes("0")) {
+    throw new Error("1~9 사이의 숫자를 입력하세요.");
+  }
+  var isDub = userArr.some(x => userArr.indexOf(x) !== userArr.lastIndexOf(x));
+  if(isDub) {
+    throw new Error("서로 다른 숫자를 입력하세요.");
+  }
 }
 
 function makeRandomNumber() {
@@ -64,3 +98,4 @@ function ballCount(computer, user) {
 }
 
 module.exports = App;
+
