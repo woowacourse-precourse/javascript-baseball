217380f27c74c9df7a2ff1b2acb0b23693db2d33
cofla <cofla159@naver.com>
2022-11-08 22:42:28 +0900

    feat: play 함수 구현
    play 함수의 기본 기능을 구현함.
    - MissionUtils 라이브러리로 임의의 수 answer에 할당
    - MissionUtils 라이브러리로 시작 문구 출력
    - isGamePlaying 변수 정의 및 할당

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
-class App {
-  play() {}
-}
+import MissionUtils from "@woowacourse/mission-utils";
 
-module.exports = App;
+class App {
+  play() {
+    const answer = MissionUtils.Random.pickNumberInRange(111, 999);
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    let isGamePlaying = true;
+  }
+}
+const app = new App();
+app.play();
+
+export default App;
