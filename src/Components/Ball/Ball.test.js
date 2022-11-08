161a21de419f4b396e48c49b877548a51f05534c
RyuJoonSik <tldtksahtxja@naver.com>
2022-11-08 15:01:38 +0900

    style: Ball.test.js의 const 변수명 SNAKE_CASE로 수정
    제안된 네이밍 컨벤션에 맞추어 SNAKE_CASE로 수정했습니다.

--- src/Components/Ball/Ball.test.js
+++ src/Components/Ball/Ball.test.js
@@ -2,16 +2,16 @@ const Ball = require("./Ball");
 
 describe("Ball", () => {
   test("getNumber로 숫자 값을 반환", () => {
-    const ballNumber = 425;
-    const ball = new Ball(ballNumber);
+    const BALL_NUMBER = 425;
+    const BALL = new Ball(ballNumber);
 
-    expect(ball.getNumber()).toBe(ballNumber);
+    expect(BALL.getNumber()).toBe(ballNumber);
   });
 
   test("나(공격수)의 Ball이 123이고 컴퓨터(수비수)의 Ball이 425이면 1스트라이크를 반환", () => {
-    const ballOfAttacker = new Ball(123);
-    const ballOfDefender = new Ball(425);
+    const BALL_OF_ATTACKER = new Ball(123);
+    const BALL_OF_DEFENDER = new Ball(425);
 
-    expect(ballOfDefender.compareTo(ballOfAttacker)).toBe("1스트라이크");
+    expect(BALL_OF_DEFENDER.compareTo(BALL_OF_ATTACKER)).toBe("1스트라이크");
   });
 });
