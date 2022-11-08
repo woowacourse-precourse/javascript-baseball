18e56cbbac958cbe629fd3f5fc0106f6c4954277
doka <posmane5@gmail.com>
2022-11-08 13:16:23 +0900

    matchNumber 함수의 볼 문자열뒤 공백 이슈 및 잘못된 주석 수정

---
+++ __tests__/matchNumber.test.js
@@ -0,0 +1,33 @@
+//기능1. matchNumber함수 검증 : 숫자, 숫자의 위치 일치여부 가려내는 기능 검증
+
+const fn = require("../src/App");
+
+
+
+test("기본 테스트1", () => {
+  expect(fn.matchNumber(["1", "2", "3"], ["8", "1", "3"])).toEqual(
+    "1볼 1스트라이크"
+  );
+});
+
+test("기본 테스트2", () => {
+  expect(fn.matchNumber(["5", "2", "1"], ["5", "1", "2"])).toEqual(
+    "2볼 1스트라이크"
+  );
+});
+
+test("0볼 검증", () => {
+  expect(fn.matchNumber(["1", "2", "3"], ["1", "2", "3"])).toEqual(
+    "3스트라이크"
+  );
+});
+
+test("낫싱 검증", () => {
+  expect(fn.matchNumber(["1", "2", "3"], ["4", "5", "6"])).toEqual(
+    "낫싱"
+  );
+});
+
+test("0스트라이크 검증", () => {
+  expect(fn.matchNumber(["3", "1", "5"], ["1", "2", "3"])).toEqual("2볼");
+}); 
--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+ 
 class App {
   play() {}
 }
 
 /**
- * 숫자, 숫자의 위치 일치여부 가려내는 기능
- * @param {Array} userNums
- * @param {Array} computerNums
+ * 기능2. 임의의 수 3개 구현
+ */
+
+/**
+ * 기능1. 숫자, 숫자의 위치 일치여부 가려내는 기능
+ * @param {string} userNums 유저의 숫자
+ * @param {string} computerNums 맞출 임의의 숫자
+ * @returns {string}결과값에 따른 리턴값 예) 2스트라이크 1볼, 낫싱, 2볼, 3스트라이크
  */
 function matchNumber(userNums, computerNums) {
   let userNumsList = [...userNums];
@@ -39,8 +46,7 @@ function matchNumber(userNums, computerNums) {
       matchResult.strike ? matchResult.strike + "스트라이크" : ""
     }`;
   }
-}
-
+} 
 // Test를 위한 exports 코드
 module.exports = {
   matchNumber,
