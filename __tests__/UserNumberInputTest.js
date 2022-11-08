e541e562052a08c3fda093a1e2fbd816f1743283
Dohyeon Lee <dlehgus1110@naver.com>
2022-11-08 16:49:28 +0900

    test: 사용자 숫자 유효성 검사 메서드 테스트 케이스 수정 및 추가
    ValidationCheck.js 파일로 분리된 유틸 함수에 대한 테스트 케이스 수정
    추가한 case: 유효한 input에 대한 test

--- __tests__/UserNumberInputTest.js
+++ __tests__/UserNumberInputTest.js
@@ -1,39 +1,42 @@
-const App = require("../src/App.js");
+const isValidUserNumberInput = require("../src/ValidationCheck.js");
 const { Console } = require("@woowacourse/mission-utils");
 
 describe("사용자 숫자 유효성 검사 메서드 테스트", () => {
   afterEach(() => {
     Console.close();
   });
-  test(`hasOnlyNumber메서드를 'abc' 인자로 호출하면 false를 return한다.`, () => {
-    const app = new App();
+  test(`isValidUserNumberInput메서드를 'abc' 인자로 호출하면 output을 return한다.`, () => {
     const input = "abc";
-    const result = app.hasOnlyNumber(input);
-
-    expect(result).toBe(false);
+    const result = isValidUserNumberInput(input);
+    const output = { isValid: false, errorType: "INVALID_INPUT_TYPE" };
+    expect(result).toEqual(output);
   });
 
-  test(`hasValidLength메서드를 '1234' 인자로 호출하면 false를 return한다.`, () => {
-    const app = new App();
+  test(`isValidUserNumberInput메서드를 '1234' 인자로 호출하면 output을 return한다.`, () => {
     const input = "1234";
-    const result = app.hasValidLength(input);
-
-    expect(result).toBe(false);
+    const result = isValidUserNumberInput(input);
+    const output = { isValid: false, errorType: "INVALID_INPUT_LENGTH" };
+    expect(result).toEqual(output);
   });
 
-  test(`hasOnlyUniqueNumber메서드를 '112' 인자로 호출하면 false를 return한다.`, () => {
-    const app = new App();
+  test(`isValidUserNumberInput메서드를 '112' 인자로 호출하면 output을 return한다.`, () => {
     const input = "112";
-    const result = app.hasOnlyUniqueNumber(input);
-
-    expect(result).toBe(false);
+    const result = isValidUserNumberInput(input);
+    const output = { isValid: false, errorType: "DUPLICATED_NUMBER" };
+    expect(result).toEqual(output);
   });
 
-  test(`hasOnlyValidRangeNumber메서드를 '012' 인자로 호출하면 false를 return한다.`, () => {
-    const app = new App();
+  test(`isValidUserNumberInput메서드를 '012' 인자로 호출하면 output을 return한다.`, () => {
     const input = "012";
-    const result = app.hasOnlyValidRangeNumber(input);
+    const result = isValidUserNumberInput(input);
+    const output = { isValid: false, errorType: "INVALID_INPUT_RANGE" };
+    expect(result).toEqual(output);
+  });
 
-    expect(result).toBe(false);
+  test(`isValidUserNumberInput메서드를 '123' 인자로 호출하면 output을 return한다.`, () => {
+    const input = "123";
+    const result = isValidUserNumberInput(input);
+    const output = { isValid: true };
+    expect(result).toEqual(output);
   });
 });
