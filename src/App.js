a46dfa29d12643bca578d2df82d0788284a720ea
tkdrb12 <tkdrb15@gmail.com>
2022-11-08 15:53:27 +0900

    숫자 생성 함수

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  constructor() {
+    this.random_numbers = ''
+    this.input_numbers = ''
+  }
+
+  play() {
+    this.random_numbers = CreateRandomNum();
+    MissionUtils.Console.print(this.random_numbers);
+  }
 }
 
-module.exports = App;
+
+function CreateRandomNum(){
+  let randstr = "";
+  while(randstr.length < 3){
+    let randnum_each = MissionUtils.Random.pickNumberInRange(1, 9);
+    randnum_each = randnum_each.toString();
+    if(!randstr.includes(randnum_each))
+      randstr = randstr + randnum_each;
+  }
+  return randstr;
+}
+
+
+
+
+
+
+const app = new App();
+app.play();
+
+// module.exports = App;
