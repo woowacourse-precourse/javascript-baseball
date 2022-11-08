927650da158cd8dca608051b6d329f37a5703f1c
KES <aprilsmaple@gmail.com>
2022-11-08 19:31:11 +0900

    feat:constants file created

--- src/App.js
+++ src/App.js
@@ -1,123 +1,30 @@
 const MissionUtils = require("@woowacourse/mission-utils");
+const { MESSAGES, ERRORS } = require("./constants.js");
 const Console = MissionUtils.Console;
+const Random = MissionUtils.Random;
 
 class App {
   constructor() {
     this.answer = {};
-    this.guess = null;
   }
 
   play() {
-    Console.print("숫자 야구 게임을 시작합니다.");
-    this.selectAnswerNumber();
-    this.getUserGuess();
+    Console.print(MESSAGES.APP_START);
+    game();
   }
 
-  selectAnswerNumber() {
-    const oneToNine = new Array(9).fill().map((_, idx) => idx + 1);
-    this.answer = this.shuffle(oneToNine)
-      .slice(0, 3)
-      .reduce((acc, cur, idx) => {
-        acc[cur] = idx + 1;
-        return acc;
-      }, {});
+  game() {
+    this.answer = this.setAnswer();
   }
 
-  shuffle(array) {
-    return array.sort(() => Math.random() - 0.5).slice(0, 3);
-  }
-
-  getUserGuess() {
-    Console.readLine("숫자를 입력해주세요 : ", (guess) => {
-      try {
-        this.checkGuessVaildation(guess);
-        this.compareAndDisplay(guess);
-      } catch (err) {
-        console.log(err.message);
-        Console.close();
-      }
-    });
-  }
-
-  checkGuessVaildation(guess) {
-    if (!this.isDigits(guess)) {
-      throw new Error("숫자가 아닌 문자는 입력이 불가능합니다.");
-    } else if (guess.length !== 3 || this.hasSameNumber(guess)) {
-      throw new Error(
-        "1부터 9까지 서로 다른 수로 이루어진 3자리 숫자만 입력이 가능합니다."
-      );
-    } else if (this.hasZero(guess)) {
-      throw new Error("0이 포함된 숫자는 입력이 불가능합니다.");
-    }
-  }
-
-  isDigits(number) {
-    return /^\d+$/.test(number);
-  }
-
-  hasZero(number) {
-    return number.includes("0");
-  }
-
-  hasSameNumber(number) {
-    for (let index = 0; index < 3; index++) {
-      if (number.indexOf(number[index]) !== number.lastIndexOf(number[index])) {
-        return true;
-      }
-    }
-    return false;
-  }
-
-  compareAndDisplay(guess) {
-    let result = this.compareGuessAndAnswer(guess);
-    this.displayResult(result);
-    if (this.isAnswer(result.strike)) {
-      console.log("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-      this.replayOrEnd();
-    } else {
-      this.getUserGuess();
-    }
-  }
-
-  compareGuessAndAnswer(guess) {
-    let ball = 0;
-    let strike = 0;
-
-    guess.split("").forEach((digit, index) => {
-      if (this.answer[digit])
-        this.answer[digit] === index + 1 ? strike++ : ball++;
-    });
-    return { ball: ball, strike: strike };
-  }
-
-  displayResult({ ball, strike }) {
-    const displayed = [];
-    if (ball) displayed.push(`${ball}볼`);
-    if (strike) displayed.push(`${strike}스트라이크`);
-    if (!ball & !strike) displayed.push("낫싱");
-    console.log(displayed.join(" "));
-  }
-
-  isAnswer(strike) {
-    return strike == 3;
-  }
-
-  replayOrEnd() {
-    Console.readLine(
-      "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n",
-      (reply) => {
-        if (reply === "1") {
-          this.play();
-        } else {
-          if (reply !== "2") console.log("옳지 않은 값을 입력하셨습니다.");
-          console.log("프로그램이 종료됩니다.");
-          Console.close();
-        }
-      }
-    );
+  setAnswer() {
+    return Random.pickUniqueNumbersInRange(1, 9, 3).reduce((acc, cur, idx) => {
+      acc[cur] = idx + 1;
+      return acc;
+    }, {});
   }
 }
 
+module.exports = App;
 const app = new App();
 app.play();
-module.exports = App;
---
+++ src/constants.js
@@ -0,0 +1,7 @@
+const MESSAGES = {
+  APP_START: "숫자 야구 게임을 시작합니다.",
+};
+
+const ERRORS = {};
+
+module.exports = { MESSAGES, ERRORS };
