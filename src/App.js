d9d49df6ae968e7b18eb27bdf798483422c0654b
cmun2 <cmun2@illinois.edu>
2022-11-08 18:50:52 +0900

    feat: 컴퓨터 랜덤숫자 생성

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-const { Console } = require("@woowacourse/mission-utils");
-const {} = require("./MissionUtil.js");
+const MissionUtils = require("@woowacourse/mission-utils");
 class App {
-  play() {}
+  play() {
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    this.startGame();
+  }
+
+  startGame() {
+    this.computerData();
+    this.userData();
+  }
+
+  computerData() {
+    const computerNum = [];
+    while (computerNum.length < 3) {
+      const num = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!computerNum.includes(num)) {
+        computerNum.push(num);
+      }
+    }
+    return computerNum;
+  }
 }
 
 module.exports = App;
