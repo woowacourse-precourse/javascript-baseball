1cc7d69e1457d299396da0afe943ed61d2f7863f
jisu-kim <kss2460@naver.com>
2022-11-08 20:24:22 +0900

    feat: compareNumber 로직 이식

--- src/App.js
+++ src/App.js
@@ -24,6 +24,91 @@ class App {
     }
     return computer;
   }
+
+  compareNumber() {
+    const initPrintNumber = this.computerNumber.join("");
+    console.log("시작 값", initPrintNumber);
+    MissionUtils.Console.readLine("번호를 입력하세요", (userPrintNumber) => {
+      const userNumber = userPrintNumber.split("").map((number) => +number);
+      const strikeScore = this.isStrike(this.computerNumber, userNumber);
+      const ballScore = this.isBall(this.computerNumber, userNumber);
+      const checkNothing = this.isNothing(this.computerNumber, userNumber);
+      let saveString = `${ballScore} ${strikeScore}`;
+      this.isValidate(userNumber);
+      if (checkNothing === "낫싱") {
+        MissionUtils.Console.print(`낫싱`);
+        this.compareNumber();
+      } else if (
+        ballScore === null &&
+        checkNothing == false &&
+        strikeScore === "3스트라이크"
+      ) {
+        MissionUtils.Console.print(`3스트라이크`);
+        MissionUtils.Console.print(`3개의 숫자를 모두 맞히셨습니다!`);
+        this.isContinue();
+      } else if (strikeScore !== null && ballScore !== null) {
+        MissionUtils.Console.print(saveString);
+        this.compareNumber();
+      } else if (strikeScore !== null && ballScore === null) {
+        MissionUtils.Console.print(strikeScore);
+        this.compareNumber();
+      } else if (strikeScore === null && ballScore !== null) {
+        MissionUtils.Console.print(ballScore);
+        this.compareNumber();
+      }
+    });
+  }
+  isContinue() {
+    MissionUtils.Console.readLine(
+      "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
+      (userNumber) => {
+        if (userNumber == 1) {
+          this.play();
+        }
+        if (userNumber == 2) {
+          this.showPrint("게임 종료");
+          MissionUtils.Console.close();
+        }
+      }
+    );
+  }
+  isBall(initNum, userNum) {
+    let ball = 0;
+
+    initNum.forEach((number, index) => {
+      userNum.indexOf(number) !== -1 && userNum.indexOf(number) !== index
+        ? ball++
+        : null;
+    });
+
+    if (ball === 0) {
+      return null;
+    } else {
+      return `${ball}볼`;
+    }
+  }
+  isStrike(initNum, userNum) {
+    let strike = 0;
+    for (let i = 0; i < 3; i++) {
+      initNum[i] === userNum[i] ? strike++ : null;
+    }
+
+    if (strike === 0) {
+      return null;
+    } else {
+      return `${strike}스트라이크`;
+    }
+  }
+  isNothing(initNum, userNum) {
+    let answer = true;
+
+    initNum.forEach((number, index) => {
+      userNum.indexOf(number) !== -1 ? (answer = false) : null;
+    });
+
+    if (answer === true) return "낫싱";
+    return answer;
+  }
 }
 
 const app = new App();
