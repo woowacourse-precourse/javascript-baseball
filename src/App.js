f4219c4d3959b5d28abe379fed88595a26661a93
leeharyung <lhr4884@naver.com>
2022-11-08 22:08:19 +0900

    feat: find strikes and ball using inputs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+const { Console } = require("@woowacourse/mission-utils");
+const { MESSAGE } = require("./constants/constants");
+
 class App {
-  play() {}
+  play() {
+    Console.print(MESSAGE.START);
+  }
+
+  getStrikeAndBallNumber(computerNumber, userInput) {
+    const count = {
+      strike: 0,
+      ball: 0,
+    };
+    computerNumber.forEach((computer, index) => {
+      if (`${computer}` === userInput[index]) {
+        count.strike += 1;
+      } else if (userInput.includes(computer)) {
+        count.ball += 1;
+      }
+    });
+    return count;
+  }
 }
 
+const app = new App();
+app.play();
 module.exports = App;
