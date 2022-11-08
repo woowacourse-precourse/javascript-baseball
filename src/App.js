0a5c8701a18048f506d0ab084361f2f7d5972c45
greeneeya <suzythetravelerr@gmail.com>
2022-11-08 18:41:44 +0900

    컴퓨터수 랜덤 숫자 받아오기

--- src/App.js
+++ src/App.js
@@ -1,3 +1,15 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+
+const computer = [];
+while (computer.length < 3) {
+  const number = MissionUtils.Random.pickNumberInRange(1, 9);
+  if (!computer.includes(number)) {
+    computer.push(number);
+  }
+}
+
+console.log(computer);
+
 class App {
   play() {}
 }
