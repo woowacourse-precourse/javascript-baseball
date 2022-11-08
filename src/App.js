96bfb9bd638c0565b2067490d2495a2ad6b93d30
Parksolmi <7548ck@naver.com>
2022-11-08 10:07:43 +0900

    feat: 랜덤 세자리 수 생성

--- src/App.js
+++ src/App.js
@@ -6,26 +6,38 @@ class App {
 
   }
 
-  start_message () {
+  print_start_message () {
     const str = "숫자 야구 게임을 시작합니다.";
     return str;
   }
 
   random_three_numbers() {
-    
+
+    let three_numbers = [];
+    let array_size = 3;
+    for(let i=0; i<array_size; i++) {
+      const number = MissionUtils.Random.pickNumberInRange(1,9);
+      three_numbers.push(number);
+    }
+
+    //확인용 (지우기)
+    MissionUtils.Console.print(three_numbers);
+
+
+    // return three_numbers;
   }
 
   play() {
 
-    MissionUtils.Console.print(this.start_message());
+    MissionUtils.Console.print(this.print_start_message());
+    this.random_three_numbers();
+
+
 
 
 
   }
 }
-
-const app = new App();
-app.play();
 
 module.exports = App;
 
