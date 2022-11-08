004abcc3c6b9aa9157516c5af4c159d8cf04f54a
윤동주 <yoondj98@naver.com>
2022-11-08 22:33:32 +0900

    feat: 입력받은 숫자에 대한 볼, 스트라이크 개수 출력 기능 구현 - printBallStrikeCount 함수

--- src/App.js
+++ src/App.js
@@ -45,6 +45,19 @@ class App {
     this.printBallStrikeCount(ball, strike);
     this.tryToAnswer(randomNums);
   }
+
+  printBallStrikeCount(ball, strike) {
+    if (strike == 3) {
+      MissionUtils.Console.print("3스트라이크\n");
+      MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n");
+      this.newGame();
+      return;
+    }
+    if (ball == 0 && strike == 0) MissionUtils.Console.print("낫싱\n");
+    else if (strike == 0) MissionUtils.Console.print(ball + "볼\n");
+    else if (ball == 0) MissionUtils.Console.print(strike + "스트라이크\n");
+    else MissionUtils.Console.print(ball + "볼 " + strike + "스트라이크\n");
+  }
   
 }
 
