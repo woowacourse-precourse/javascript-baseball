ababca134f49af73f63ec6a0be5f2e413937d338
kimseo-0 <42965120+kimseo-0@users.noreply.github.com>
2022-11-08 21:34:54 +0900

    feat: [7] 게임 시작

--- src/App.js
+++ src/App.js
@@ -3,7 +3,16 @@ const MissionUtils = require("@woowacourse/mission-utils");
 class App {
   play() {
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+    startGame();
   }
+}
+
+let gameAnswerNumList;
+
+const startGame = () => {
+  gameAnswerNumList = createAnswerNumList();
+  console.log(gameAnswerNumList)
+  inputUserNum();
 }
 
 
