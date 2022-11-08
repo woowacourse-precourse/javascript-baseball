c8c8ca2545b7ec76ef93adce9778ffe5877b8b07
terry <kdjtiger@naver.com>
2022-11-08 18:43:47 +0900

    feat: 유저 값 받고 실행 후 오류 제어

--- src/App.js
+++ src/App.js
@@ -11,8 +11,40 @@ class App {
     }
     return computer;
   }
-}
+  
+  getUserInputAndPlay(computerNumber) {
+    MissionUtils.Console.readLine('숫자를 입력해주세요: ', answer => {
+      let answerSet = new Set(answer);
+      let answerSetCopy = [...answerSet];
+      if (answerSetCopy.length != 3) {
+        throw new Error("입력 값이 세글자가 아니라서 게임을 종료합니다");
+      }
+      if (answer.includes('0')){
+        throw new Error("숫자 0 이 포함되어 게임을 종료합니다")
+      }
+      try {
+        let answerArray = answer.split('');
+        answerArray = answerArray.map(number => {
+          number = parseInt(number);
+          if (isNaN(number)) {
+            throw error;
+          }
+          return number;
+        });
 
+        if (!this.compareNumbers(computerNumber, answerArray)) {
+          return this.getUserInputAndPlay(computerNumber);
+        } else {
+          console.log("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+          return this.replayGame();
+        }
+      }
+      catch (error) {
+        throw new Error("입력 값을 숫자로 변환하는 과정에서 오류 발생하여 게임을 종료합니다");
+      }
+    });  
+  }
+}
 const app = new App();
 app.play();
 
