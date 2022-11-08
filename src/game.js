21a736b21e5c641b40c1c4a8840db8d7dccf0c1a
hvvany <kjunhwan1998@gmail.com>
2022-11-08 22:28:11 +0900

    feat: strike 구현

--- src/game.js
+++ src/game.js
@@ -13,4 +13,26 @@ function addNumber(numbers) {
   if(numbers.size < 3) addNumber(numbers);
 }
 
+class GetResult {
+  constructor(input, answer) {
+    this.result = {
+      strike: 0,
+      ball: 0,
+    }
+    this.input = input;
+    this.answer = answer;
+    this.result.strike = this.getStrike(this.input, this.answer);
+  }
+  getStrike(input, answer) {
+    console.log(input);
+    let strike = 0;
+    input.forEach((inputNumber, idx) => {
+      const answerNumber = answer[idx];
+      if(inputNumber === answerNumber) strike++;
+    });
+    return strike;
+  }
+}
+
 exports.getAnswer = getAnswer;
+exports.GetResult = GetResult;
