789ff93db05ae1ebd25ef0a71d70cbe3ebcf28d6
KimDongYoung <ehddud1006@pusan.ac.kr>
2022-11-08 16:51:05 +0900

    feat: 0이 포함되어있는지 체크하는 함수

--- src/utils/validate/validate.js
+++ src/utils/validate/validate.js
@@ -6,3 +6,10 @@ const hasDuplicatedNumbers = (num) => {
   if (new Set(num).size !== num.length)
     throw new Error("중복된 숫자가 없이 입력해주세요.");
 };
+
+const hasZero = (num) => {
+  if (num.indexOf("0") !== -1)
+    throw new Error(
+      "0을 입력하셨습니다. 1부터 9까지의 숫자범위에서 입력해주세요."
+    );
+};
