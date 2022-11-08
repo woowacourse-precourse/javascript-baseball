c057294bb27e09ce44be04622beb36a01878e9d3
kangju2000 <kangju2000@naver.com>
2022-11-08 22:01:57 +0900

    feat: gameSetting util 파일 생성

---
+++ src/constants/gameSetting.js
@@ -0,0 +1,19 @@
+const MIN_NUMBER = 1;
+const MAX_NUMBER = 9;
+const VALUE_SIZE = 3;
+const RESTART = '1';
+const GAME_OVER = '2';
+const NOTHING = '낫싱';
+const BALL = '볼';
+const STRIKE = '스트라이크';
+
+module.exports = {
+  MIN_NUMBER,
+  MAX_NUMBER,
+  VALUE_SIZE,
+  RESTART,
+  GAME_OVER,
+  NOTHING,
+  BALL,
+  STRIKE,
+};
