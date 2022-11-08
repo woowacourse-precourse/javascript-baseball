8b755792b6bfb9d3fcdce0f4f3f325943b47ed14
piporica <49173614+piporica@users.noreply.github.com>
2022-11-08 21:19:10 +0900

    feat: 게임시작메시지/답안 생성
    전부 지우고 다시 작성하였다

--- src/NumberBaseball.js
+++ src/NumberBaseball.js
@@ -4,6 +4,23 @@ const { MESSAGES } = require("./lib/Constants");
 class NumberBaseball {
   gameStart() {
     Console.print(MESSAGES.START_GAME);
+    this.gamePlay();
+  }
+
+  gamePlay() {
+    const answer = this.makeAnswer();
+    console.log(answer);
+  }
+
+  makeAnswer() {
+    const answer = [];
+    while (answer.length !== 3) {
+      const nextNum = Random.pickNumberInRange(1, 9);
+      if (!answer.includes(nextNum)) {
+        answer.push(nextNum);
+      }
+    }
+    return answer;
   }
 }
 module.exports = NumberBaseball;
