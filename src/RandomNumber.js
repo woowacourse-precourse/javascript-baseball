f43a3dafe927b69bb3ab78971084eb2506f02d99
y00eunji <aine0227@naver.com>
2022-11-08 19:43:34 +0900

    feat(RandomNumber): Add generating RandomNumber Class

---
+++ src/RandomNumber.js
@@ -0,0 +1,33 @@
+const { Random } = require("@woowacourse/mission-utils");
+const RANDOM_NUMBER = require("./../constant/constant");
+
+class RandomNumber {
+  constructor() {
+    this.numbers = [];
+  }
+
+  isDuplicatedNumber(number) {
+    if (!this.numbers.includes(number)) {
+      this.numbers.push(number);
+    }
+  }
+
+  getRandomNumber() {
+    const number = Random.pickNumberInRange(
+      RANDOM_NUMBER.MIN_NUMBER,
+      RANDOM_NUMBER.MAX_NUMBER
+    );
+
+    this.isDuplicatedNumber(number);
+  }
+
+  generateAnswerNumbers() {
+    while (this.numbers.length < RANDOM_NUMBER.MAX_SIZE) {
+      this.getRandomNumber();
+    }
+
+    return this.numbers;
+  }
+}
+
+module.exports = RandomNumber;
