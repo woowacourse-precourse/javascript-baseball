f24f37f95939b77de0d3f8eac56714d91318a3a5
hyeongdong <khd941018@gmail.com>
2022-11-08 15:55:13 +0900

    feat : 입력값이 유의한지 판별하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -17,6 +17,24 @@ class App {
     return deduplicateRandomNumbers;
   }
 
+  checkValid(userInput) {
+    if (Number.isNaN(userInput) || !Number.isInteger(Number(userInput))) {
+      throw new Error("정수 숫자를 입력하셔야 합니다.");
+    }
+
+    if (!(userInput.length === 3)) {
+      throw new Error("숫자 3개만 입력하셔야 합니다.");
+    }
+
+    if (userInput.includes("0") || Number(userInput) < 0) {
+      throw new Error("1~9사이의 숫자 3개를 입력하셔야 합니다.");
+    }
+
+    if (new Set(userInput).size !== 3) {
+      throw new Error("각각 다른 숫자를 입력하셔야 합니다.");
+    }
+  }
+
   play() {}
 }
 
