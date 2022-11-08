d35e0e37ec5c924882f1d2d0c5b20e8b253955fb
MiJung Lee <mijunglee@MiJungs-MacBook-Air.local>
2022-11-08 22:22:22 +0900

    simplify MissionUtils.Console to Console

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
+const { Console } = MissionUtils;
+
 class App {
   play() {
-    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.")
+    Console.print("숫자 야구 게임을 시작합니다.")
     return this.input();
   }
 
@@ -19,12 +21,12 @@ class App {
   
   input() {
     const computerNumber = this.createNumber()
-    MissionUtils.Console.readLine('숫자를 입력해주세요 : ',  (userInput) => {
+    Console.readLine('숫자를 입력해주세요 : ',  (userInput) => {
       return this.validateInput(computerNumber, userInput)
-      })
+    })
   }
 
-  validateInput(computerNumber, userInput){
+  validateInput(computerNumber, userInput) {
     let userInputList = userInput.split("").map(Number)
 
     if(isNaN(userInput)==true ) throw "올바른 숫자를 입력해주세요."
@@ -34,7 +36,7 @@ class App {
     return this.compareNumber(computerNumber, userInputList)
   }
 
-  compareNumber(computerNumber, userInputList){
+  compareNumber(computerNumber, userInputList) {
     let result = [0,0] 
     let message = ""
     for (let i =0 ; i<3 ; i++){
@@ -42,34 +44,34 @@ class App {
       else if (computerNumber.includes(String(userInputList[i]))) result[0] += 1
     }
     if (result[1]===3) {
-      MissionUtils.Console.print("3스트라이크");
-      MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+      Console.print("3스트라이크");
+      Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
       return this.endOption()
     } else {
       if (result[0]==0 && result[1]==0) message += "낫싱";
       if (result[0]>0) message += result[0] + "볼" + " "
       if (result[1]>0) message += result[1] + "스트라이크" 
 
-      MissionUtils.Console.print(message);
-      MissionUtils.Console.readLine('숫자를 입력해주세요 : ',  (userInput) => {
+      Console.print(message);
+      Console.readLine('숫자를 입력해주세요 : ',  (userInput) => {
         return this.validateInput(computerNumber, userInput)
-        })
+      })
     }
   }
 
-  endOption (){
-    MissionUtils.Console.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-    MissionUtils.Console.readLine('',  (userInput) => {
+  endOption() {
+    Console.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    Console.readLine('',  (userInput) => {
       if (userInput == "1") return this.input();
       else if (userInput == "2") {
-        MissionUtils.Console.print("게임 종료");
-        MissionUtils.Console.close();
+        Console.print("게임 종료");
+        Console.close();
       } 
       else throw new Error("올바른 값을 입력해주세요.")
-      })
-    }
- 
+    })
+  }
 }
+
 const app = new App();
 app.play()
 
