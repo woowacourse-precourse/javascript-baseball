98c46787d721a9f15afe8e9f8f8c34ffc090e9c0
Jaehyeon Park <sawogus29@gmail.com>
2022-11-08 14:06:20 +0900

    style: modify App.isValidPlayInput to meet airbnb convention
    - Array.from(input) -> [...input]

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App {
   static isValidPlayInput(input) {
     return (
       input.length === 3
-      && Array.from(input).every((x) => Number(x) >= 1 && Number(x) <= 9)
+      && [...input].every((x) => Number(x) >= 1 && Number(x) <= 9)
     );
   }
 
