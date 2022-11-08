ac5af3f1bed82ebc7e52c526ecc0fa7c44acf217
HaroldBKim <sbookim2@gmail.com>
2022-11-08 19:13:23 +0900

    feat: 게임 시작

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 class App {
-  play() { }
+  static answer;
+  static hintString;
+
+  play() {
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+    this.answer = this.generateComputerNumber();
+    this.startGame();
+  }
 
   generateComputerNumber() {
     const computerNumber = [];
