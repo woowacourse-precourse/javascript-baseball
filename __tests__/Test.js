c1198556d28b04ff15ff185d491d2f583e6c67c7
SorinKwon <ksl020700@gmail.com>
2022-11-08 23:27:01 +0900

    refactor(Test): 변수명 변경, 공백 추가, 제거

--- __tests__/Test.js
+++ __tests__/Test.js
@@ -1,6 +1,6 @@
 const App = require("../src/App");
 
-const duplicate_number = (n)=>{
+const find_duplicate_number = (n)=>{
     if(n[0]==n[1]) return 0;
     if(n[0]==n[2]) return 0;
     if(n[1]==n[2]) return 0;
@@ -13,19 +13,22 @@ describe("함수 기능 테스트", () => {
         const result = app.selectComputerNumber().length;
         expect(result).toEqual(3);
     });
+
     test("selectComputerNumber() 함수로 컴퓨터 숫자 서로 다른 숫자 반환", () => {
         const app =new App();
         let result = app.selectComputerNumber();
-        if(duplicate_number(result)){
+        if(find_duplicate_number(result)){
             result=0;
         }
         expect(result).toEqual(0);
     });
+    
     test("changeUserNumberToArray() 배열 타입 반환", () => {
         const app =new App();
         let result = app.changeUserNumberToArray("123");
         expect(result).toEqual(['1','2','3']);
     });
+
     test("changeComputerNumberToArray() 배열 타입 반환", () => {
         const app =new App();
         let result = app.changeComputerNumberToArray("1,2,3");
