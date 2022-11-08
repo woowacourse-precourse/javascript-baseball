fd0c85467f221432345a2382e63090d841bb484b
Y2IN <dpdls0228@daum.net>
2022-11-08 20:35:20 +0900

    chore : 불필요한 코드 제거

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
 const BaseballGame = require("./baseball/BaseballGame");
-const { console } = require("@woowacourse/mission-utils");
-const ComputerNumbers = require("./baseball/ComputerNumbers");
-const ValidUserNumbers = require("./baseball/ValidUserInput");
 class App {
   constructor() {
     this.baseballGame = new BaseballGame();
