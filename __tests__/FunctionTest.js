917e5d7d8b6bdd0d1c9b6753723e160c6acb12a4
ghenmaru <rbgks1937@naver.com>
2022-11-08 18:09:23 +0900

    test: setting 테스트 코드 작성

--- __tests__/FunctionTest.js
+++ __tests__/FunctionTest.js
@@ -1,24 +1,25 @@
 const App = require("../src/App");
 const MissionUtils = require("@woowacourse/mission-utils");
-
-
-const getInputIntArray = (input) => {
-    const userInputArray = [...input];
-    const newArray = [];
-    for (let i = 0; i < userInputArray.length; i++){
-      newArray.push(parseInt(userInputArray[i]));
-    }
-    return newArray;
-};
+const setting = require("../src/Setting");
 
 
 describe("숫자야구 함수 테스트", () => {
     test("숫자문자열을 int배열로 만들어 반환", () => {
       const input = "123";
-      const result = getInputIntArray(input);
+      const result = setting.getInputIntArray(input);
   
       expect(result).toEqual([1, 2, 3]);
     });
 
-    
+    test("input길이가 3 아니면 false 반환", () => {
+        const input = "12345";
+        const result = setting.checkInput(input);
+        expect(result).toEqual(false);
+      });  
+      
+    test("input이 숫자로 된 문자열이 아니면 false 반환", () => {
+        const input = "abc";
+        const result = setting.checkInput(input);
+        expect(result).toEqual(false);
+      });               
 });
