62a9593f1743cae54815ad48e9dbfd2798b165d4
yeong bin <2976622@naver.com>
2022-11-08 17:07:24 +0900

    feat(Referee.js): judge메서드 기능 구현

--- src/model/Referee.js
+++ src/model/Referee.js
@@ -5,17 +5,30 @@ class Referee {
   constructor() {
 		this._game;
   }
+	_isBall(prevIdx, curIdx) {
+		if (curIdx >= 0 && curIdx !== prevIdx)
+			return 1;
+		return 0;
+	}
+	_isStrike(prevIdx, curIdx) {
+		if (curIdx >= 0 && curIdx === prevIdx)
+			return 1;
+		return 0;
+	}
 	
 	/** @param {Game} */
   chargeGame(game) {
 		this._game = game;
   }
-	
 	/**
 	 * @param {Array<number>} balls 
 	 * @returns {Judgement}
 	 */
 	judge(balls) {
+		return new Judgement(...this._game
+			.indexOfBalls(balls)
+			.reduce((acc, cur, index) => 
+				[acc[0] + this._isBall(index, cur), acc[1] + this._isStrike(index, cur)], [0, 0]));
 	}
 }
 
