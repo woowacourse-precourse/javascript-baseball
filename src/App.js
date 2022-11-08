0aa3fa31a9d1239655107adc464d75d06f6dd746
jinyong <zhaqh146@naver.com>
2022-11-08 21:52:35 +0900

    refactor: 게임에 사용할 3자리 숫자 생성 함수 App.js에 통합

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+
+const makeNumber = () => {
+  const result = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
+  return result;
+};
+
 class App {
-  play() {}
-}
+  play() {
+    let answer = makeNumber();
+  };
+};
+
+const app = new App()
+app.play()
 
 module.exports = App;
--- src/makeNumber.js
+++
@@ -1,7 +0,0 @@
-const makeNumber = () => {
-  const MissonUtils = require('@woowacourse/mission-utils');
-  const result = MissonUtils.Random.pickUniqueNumbersInRange(1, 10, 3);
-  return result;
-}
-
-module.exports = makeNumber;
