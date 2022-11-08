466bf936f6a2dc7cf9d45c4f8acc437413409206
seungki <sysh9498@gmail.com>
2022-11-08 18:56:11 +0900

    feat : 맞춰야 할 정답 랜덤으로 생성

--- src/App.js
+++ src/App.js
@@ -2,9 +2,21 @@ const MissionUtils = require('@woowacourse/mission-utils');
 const { Console } = MissionUtils;
 class App {
   play() {}
+
   printStartMessage() {
     const START_MESSAGE = '숫자 야구 게임을 시작합니다.';
     Console.print(START_MESSAGE);
+  }
+
+  getAnswerNumber() {
+    const answer = [];
+    while (answer.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!answer.includes(number)) {
+        answer.push(number);
+      }
+    }
+    return answer;
   }
 }
 module.exports = App;
