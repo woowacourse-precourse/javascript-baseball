9ec7fc4290bbb393bb4ef9cb995d9b21b1ec1936
kimeodml <kimeodml@gmail.com>
2022-11-08 21:08:07 +0900

    feat: 입력값이 올바른지 확인하는 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 const { Console, Random } = require("@woowacourse/mission-utils");
+const ONLY_NUMBER = /^[1-9]+$/;
 
 
 class App {
@@ -22,8 +23,22 @@ class App {
   inputNumber() {
     Console.readLine('숫자를 입력하세요 : ', (input) => {
       this.input = input.split('').map((i) => Number(i));
+      this.checkValidation(this.input);
     })
   }
+
+  checkValidation (input) {
+    if (input.length !== 3) {
+      throw '세자리 숫자를 입력해 주세요.';
+    }
+    if (new Set(input).size !== 3) {
+      throw '중복되지 않게 숫자를 입력해 주세요';
+    }
+    if (!ONLY_NUMBER.test(input.join(''))) {
+      throw '숫자 외의 값을 입력하셨습니다.';
+    }
+  }
+
 
 }
 
