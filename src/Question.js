a26f1a893f3d5532db83580bed7f4a636c54f826
cloudedpanther <rickeykoh@ajou.ac.kr>
2022-11-08 13:14:19 +0900

    fix(bug fix): create of Question
    Modify logic of create method of Question class
    - Use MissionUtils.Random.pickNumberInRange instead of MissionUtils.Random.pickUniqueNumbersInRange

--- src/Question.js
+++ src/Question.js
@@ -2,7 +2,20 @@ const MissionUtils = require("@woowacourse/mission-utils");
 
 class Question {
   static create() {
-    return MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
+    const question = [];
+
+    for (let index = 0; index < 3; index++) {
+      while (true) {
+        const nextNumber = MissionUtils.Random.pickNumberInRange(1, 9);
+
+        if (question.includes(nextNumber)) continue;
+
+        question.push(nextNumber);
+        break;
+      }
+    }
+
+    return question;
   }
 }
 
