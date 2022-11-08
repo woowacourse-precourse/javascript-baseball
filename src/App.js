6fb2288e5bea78169be6f52d16112d355305732b
lineJeong <line.jeong32@gmail.com>
2022-11-08 18:27:23 +0900

    FEAT: 사용자가 맞춰야 하는 3자리 임의의 수를 생성하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+const { Random, Console } = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  constructor() {
+    this.randomNums = this.createRandomNums();
+  }
+
+  play() {
+    Console.print("숫자 야구 게임을 시작합니다.");
+  }
+
+  createRandomNums() {
+    const randomNums = [];
+    while (randomNums.length < 3) {
+      let randomNum = Random.pickNumberInRange(1, 9);
+      if (!randomNums.includes(randomNum)) randomNums.push(randomNum);
+    }
+    return randomNums;
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
