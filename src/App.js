28dd26b53c939faceb8a107c505dffa7926bedb9
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2022-11-08 19:55:00 +0900

    feat: add function 2,3

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ class App {
   play() {
     let randombaseball = createRandomNumber();
     start();
+    getNumber();
   }
 }
 module.exports = App;
@@ -23,5 +24,16 @@ const start = () => {
   MissionUtils.Console.print("숫자 야구를 게임을 시작합니다.");
 }
 
-const app = new App();
-app.play();
+const getNumber = () => {
+  MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (number) => {
+    checkNumberError(number);
+  });
+}
+
+const checkNumberError = (number) => {
+  if (number.length !== 3) throw new Error ("3자리 수를 입력해주세요.");
+  let checkNumber = number.split('').map(Number);
+  const set = new Set(checkNumber);
+  const newArr = [...set];
+  if(newArr.length !==3 ) throw new Error("중복되지 않는 수들을 입력해주세요.");
+}
