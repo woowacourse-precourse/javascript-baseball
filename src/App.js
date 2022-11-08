03390553cb864a96d6420671ac01907ce28453b2
shlee0820 <72565083+shlee0820@users.noreply.github.com>
2022-11-08 18:04:48 +0900

    refactor: 함수 분리에 따른 App.js 구조 리팩토링

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 const checkValidUserInput = require('./utils/checkValidUserInput');
 const generateRandomNumber = require('./game/generateRandomNumber');
+const calculateScore = require('./game/calculateScore');
+const getCompareResultText = require('./game/getCompareResultText');
 
 class App {
   startGame() {
@@ -12,53 +14,29 @@ class App {
   initGame() {
     this.userInputNumber = [];
     this.computerRandomNumber = [];
-    this.strikeScore = 0;
-    this.ballScore = 0;
+    this.strike = 0;
+    this.ball = 0;
     this.compareResultText = '';
-    this.isThreeStrike = false;
   }
 
   getUserInput() {
     MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (userInput) => {
       this.userInputNumber = [...String(userInput).split('').map((x) => +x)];
       if (checkValidUserInput(this.userInputNumber)) {
-        this.calculateResult();
+        [this.strike, this.ball] = calculateScore(this.userInputNumber, this.computerRandomNumber);
+        this.compareResultText = getCompareResultText(this.strike, this.ball);
+        MissionUtils.Console.print(this.compareResultText);
       }
-      this.getCompareResultText();
-      MissionUtils.Console.print(this.compareResultText);
-      if (!this.isThreeStrike) {
-        return this.getUserInput();
-      }
-      if (this.isThreeStrike) {
-        return this.askRestart();
-      }
+      this.checkThreeStrike();
     });
   }
 
-  calculateResult() {
-    this.strikeScore = 0;
-    this.ballScore = 0;
-    this.userInputNumber.forEach((digit, digitIndex) => {
-      if (digit === this.computerRandomNumber[digitIndex]) {
-        this.strikeScore += 1;
-      } else if (this.computerRandomNumber.includes(digit)) {
-        this.ballScore += 1;
-      }
-    });
-  }
-
-  getCompareResultText() {
-    if (this.strikeScore === 0 && this.ballScore === 0) {
-      this.compareResultText = '낫싱';
-    } else if (this.ballScore === 0) {
-      this.compareResultText = `${this.strikeScore}스트라이크`;
-      if (this.strikeScore === 3) {
-        this.isThreeStrike = true;
-      }
-    } else if (this.strikeScore === 0) {
-      this.compareResultText = `${this.ballScore}볼`;
-    } else if (this.strikeScore > 0 && this.ballScore > 0) {
-      this.compareResultText = `${this.ballScore}볼 ${this.strikeScore}스트라이크`;
+  checkThreeStrike() {
+    if (this.strike !== 3) {
+      this.getUserInput();
+    }
+    if (this.strike === 3) {
+      this.askRestart();
     }
   }
 
