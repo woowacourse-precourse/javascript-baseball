0395822f0c1214ff781793076ca55597d324d8cd
ReturnReason <reasonz@daum.net>
2022-11-08 16:26:14 +0900

    refactor: 함수명 수정, getPlayerInput 함수 리팩토링

--- src/BaseballGame.js
+++ src/BaseballGame.js
@@ -34,51 +34,46 @@ class BaseballGame {
 
   initRandomNumbers() {
     const pickedNumbers = [];
-
     while (pickedNumbers.length < MAX_LENGTH) {
       const number = Random.pickNumberInRange(BEGIN_NUM, END_NUM);
       if (!pickedNumbers.includes(number)) {
         pickedNumbers.push(number);
       }
     }
-
     return [...pickedNumbers];
   }
 
   getPlayerInput() {
-    Console.readLine(`${INPUT_MESSAGE}`, this.checkPlayerInput());
-  }
-
-  checkPlayerInput() {
     const playerInput = (answer) => {
-      const isThreeStrike = this.gameHint.checkPlayerInput(this.randomNumbers, answer);
+      const isThreeStrike = this.checkThreeStrike(answer);
       if (isThreeStrike) {
         return this.gameOver();
       }
-      this.getPlayerInput();
+      return this.getPlayerInput();
     };
 
-    return playerInput;
+    Console.readLine(`${INPUT_MESSAGE}`, playerInput);
+  }
+
+  checkThreeStrike(playerInput) {
+    return this.gameHint.checkPlayerInput(this.randomNumbers, playerInput);
   }
 
   gameOver() {
     printEndMessage();
-    this.getGameOverInput();
+    this.getRestartAnswer();
   }
 
-  getGameOverInput() {
+  getRestartAnswer() {
     const playerAnswer = (answer) => {
-      const convertNumberInput = Number(answer);
-      if (convertNumberInput === 1) {
+      if (answer === '1') {
         return this.restartGame();
       }
-      if (convertNumberInput === 2) {
+      if (answer === '2') {
         return Console.close();
       }
-
       throw new Error('잘못된 입력');
     };
-
     Console.readLine(`${END_MESSAGE}\n`, playerAnswer);
   }
 }
