549bd63ccfea465b0494e67514b9fa688eb31a8a
woo-jk <201702042@o.cnu.ac.kr>
2022-11-08 19:40:37 +0900

    fix: throw error 전 console.close 제거 (예제 테스트 오류 원인 찾기 위해)

--- src/ThrowError.js
+++ src/ThrowError.js
@@ -1,8 +1,6 @@
-const MissionUtils = require("@woowacourse/mission-utils");
 const { WRONG_INPUT_ERROR_MESSAGE } = require("./constants/messages");
 
 const throwError = () => {
-  MissionUtils.Console.close();
   throw new Error(WRONG_INPUT_ERROR_MESSAGE);
 };
 
