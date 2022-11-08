12e576d3224450a1eb019edf7ff4aec180de25f4
cloudedpanther <rickeykoh@ajou.ac.kr>
2022-11-08 13:40:33 +0900

    feat(method): play
    Add play method to App class

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+const Game = require("../src/Game");
+const Input = require("../src/Input");
+const { Output, WELCOME_MESSAGE } = require("../src/Output");
+
 class App {
-  play() {}
+  play() {
+    Output.printToUser(WELCOME_MESSAGE);
+
+    while (true) {
+      const game = new Game();
+      game.run();
+
+      const userWantsReplay = Input.getReplayRequest();
+      if (!userWantsReplay) break;
+    }
+  }
 }
 
 module.exports = App;
