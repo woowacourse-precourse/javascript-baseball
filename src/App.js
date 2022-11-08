8da71488f78acaf50682c61229831309c50f4395
cmun2 <cmun2@illinois.edu>
2022-11-08 20:12:53 +0900

    feat: 사용자 입력 검증 및 컴퓨터 숫자와 유저 입력 숫자 비교

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 class App {
+  constructor() {
+    this.COMPUTER_NUM = [];
+    this.USER_NUM = [];
+  }
+
   play() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
     this.startGame();
@@ -20,6 +25,42 @@ class App {
     }
     return COMPUTER_NUM;
   }
+
+  userData() {
+    MissionUtils.Console.readLine("숫자를 입력해주세요: ", (USER_NUM) => {
+      this.checkIsValid(USER_NUM);
+      this.checkIsRight();
+    });
+  }
+
+  checkIsValid(USER_NUM) {
+    if (USER_NUM === "") {
+      throw new Error("입력값이 없습니다. 숫자를 입력해주세요");
+    }
+    if (USER_NUM.length !== 3) {
+      throw new Error("숫자 3개를 입력해주세요.");
+    }
+    if ([...new Set(USER_NUM.split(""))].length !== 3) {
+      throw new Error("중복된 숫자가 존재합니다.");
+    }
+  }
+
+  checkIsRight() {
+    let STRIKE = 0;
+    let BALL = 0;
+
+    this.USER_NUM.map((num, idx) => {
+      if (this.COMPUTER_NUM[idx] === num) {
+        STRIKE++;
+      } else {
+        this.COMPUTER_NUM.includes(num) ? BALL++ : BALL;
+      }
+    });
+
+    this.showResult(STRIKE, BALL);
+  }
+
+  showResult() {}
 }
 
 module.exports = App;
