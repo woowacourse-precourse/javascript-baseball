100bad6b0ea8bc93d672fcf26d275138e2648f11
jinyong <zhaqh146@naver.com>
2022-11-08 22:00:39 +0900

    feat: 게임 시작 함수 구현

--- src/App.js
+++ src/App.js
@@ -95,8 +95,13 @@ const askRestart = () => {
   })
 };
 
+const gameStart = () => {
+  MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+};
+
 class App {
   play() {
+    gameStart();
     let answer = makeNumber();
     userInput(answer);
   };
