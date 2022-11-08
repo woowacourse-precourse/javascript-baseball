c4d4ccf5881117e9793f306396659cd1fb8cac39
kyw0716 <kyw0716@naver.com>
2022-11-08 16:34:10 +0900

    refactor: 입력값 처리 함수 분리 (게임 중, 게임 후)

--- src/BaseballGame.js
+++ src/BaseballGame.js
@@ -1,4 +1,4 @@
-const MissionUtils = require("@woowacourse/mission-utils");
+const { Console } = require("@woowacourse/mission-utils");
 const getIsInputValueValid = require("./ValidCheck");
 const getHint = require("./Hint");
 const Computer = require("./Computer");
@@ -11,7 +11,7 @@ class BaseballGame {
 
   setAndPrintHint(correctNumber, inputNumber) {
     this.hint = getHint(correctNumber, inputNumber);
-    MissionUtils.Console.print(this.hint);
+    Console.print(this.hint);
   }
 
   startGame() {
@@ -19,34 +19,47 @@ class BaseballGame {
   }
 
   getUserInput() {
-    MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (input) => {
-      if (!getIsInputValueValid(input)) {
-        throw Error("입력값이 유효하지 않습니다.");
-      }
-
-      this.setAndPrintHint(this.computer.correctNumber, input);
-
-      if (this.hint !== "3스트라이크") this.getUserInput();
-      else this.recommendRestart();
+    Console.readLine("숫자를 입력해주세요 : ", (input) => {
+      this.handleInputDuringGame(input);
     });
   }
 
   recommendRestart() {
-    const expectInputValues = ["1", "2"];
-    MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-    MissionUtils.Console.readLine(
+    Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+
+    Console.readLine(
       "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n",
       (input) => {
-        if (!expectInputValues.includes(input))
-          throw Error("입력값이 유효하지 않습니다.");
-        if (input === "1") {
-          this.restartGame();
-        }
-        if (input === "2") {
-          MissionUtils.Console.close();
-        }
+        this.handleInputAfterEndGame(input);
       }
     );
+  }
+
+  handleInputDuringGame(input) {
+    if (!getIsInputValueValid(input)) {
+      throw Error("입력값이 유효하지 않습니다.");
+    }
+
+    this.setAndPrintHint(this.computer.correctNumber, input);
+
+    if (this.hint !== "3스트라이크") this.getUserInput();
+    else this.recommendRestart();
+  }
+
+  handleInputAfterEndGame(input) {
+    const expectInputValues = ["1", "2"];
+
+    if (!expectInputValues.includes(input))
+      throw Error("입력값이 유효하지 않습니다.");
+
+    switch (input) {
+      case "1":
+        this.restartGame();
+        break;
+      case "2":
+        Console.close();
+        break;
+    }
   }
 
   resetGameValue() {
