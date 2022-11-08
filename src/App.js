22a1452dc9659a94db59e2157db58d971f9a63ab
s0ojin <onepst@hanyang.ac.kr>
2022-11-08 20:49:59 +0900

    feat : 게임 재시작을 결정하는 wantRestart 함수 구현

--- src/App.js
+++ src/App.js
@@ -11,6 +11,10 @@ class App {
 
   printGreeting() {
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+    this.setGame();
+  }
+
+  setGame() {
     this.makeRandomNumber();
   }
 
@@ -42,15 +46,12 @@ class App {
     if (strike === 0 && ball === 0) {
       MissionUtils.Console.print('낫싱');
     }
-
     if (strike > 0 && ball === 0) {
       MissionUtils.Console.print(`${strike}스트라이크`);
     }
-
     if (ball > 0 && strike === 0) {
       MissionUtils.Console.print(`${ball}볼`);
     }
-
     if (ball > 0 && strike > 0) {
       MissionUtils.Console.print(`${ball}볼 ${strike}스트라이크`);
     }
@@ -60,10 +61,22 @@ class App {
 
   isCorrect(strike) {
     if (strike === 3) {
-      console.log('정답!');
+      MissionUtils.Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+      this.wantRestart();
     } else {
       this.getUserNumber();
     }
+  }
+
+  wantRestart() {
+    MissionUtils.Console.readLine(
+      '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n',
+      answer => {
+        if (this.InputValidation.isValidRestartInput(answer)) {
+          answer === '1' ? this.setGame() : '종료';
+        }
+      },
+    );
   }
 }
 
