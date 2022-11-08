f7efde07f61deeebc04d92707d81b2cd21af41b2
Jaehyeon Park <sawogus29@gmail.com>
2022-11-08 18:09:08 +0900

    style: fix message typos

--- src/App.js
+++ src/App.js
@@ -50,9 +50,9 @@ class App {
 
   play() {
     const GREETING_MESSAGE = '숫자 야구 게임을 시작합니다';
-    const PLAY_QUESTION = '숫자를 입력해주세요 :';
+    const PLAY_QUESTION = '숫자를 입력해주세요 : ';
     const COMPLETE_MESSAGE = '3개의 숫자를 모두 맞히셨습니다! 게임 종료';
-    const MENU_QUESTION = '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요';
+    const MENU_QUESTION = '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n';
     let answer = App.pickThreeDigits();
 
     const turn = function playOneTurn(input) {
