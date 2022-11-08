d04c96e9b1e3cd4ce6229cc3c38f62b379fbbbec
jaesukpark77 <jaesukpark77@naver.com>
2022-11-08 12:47:45 +0900

    feat: 사용자가 잘못된 값을 입력한 경우 예외 처리

--- src/App.js
+++ src/App.js
@@ -1,39 +1,64 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
-
 class App {
   constructor() {
-    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
   };
+
   play() {
     let randomNumber = [];
-    randomNumber = createRandomNumber();
+    let userInput;
+
+    randomNumber = createRandom();
+
+    while (true) {
+      let strike = 0;
+      let ball = 0;
+
+      let inputresult = checkUserInput();
+      if (inputresult[0] === false)
+        throw new Error();
+      else if (inputresult[0] === true)
+        userInput = inputresult[1];
+
+      let strikeAndBall = baseballGame(randomNumber, userInput);
+      strike = strikeAndBall[0];
+      ball = strikeAndBall[1];
+
+      let result = check3Strike(strike, ball);
+      if (result[0] === 'break')
+        break;
+      else if (result[0] === 'restart')
+        randomNumber = result[1];
+    }
   }
 }
 
-function createRandomNumber() {
+function createRandom() {
   let randomNumber = [];
-  let tempNumber;
+  let tempForRandom;
 
   while (randomNumber.length < 3) {
-    tempNumber = MissionUtils.Random.pickNumberInRange(1,9);
-    randomNumber.push(tempNumber);
-  }
+    tempForRandom = MissionUtils.Random.pickNumberInRange(1,9);
+    randomNumber.push(tempForRandom);
+  } 
   return randomNumber;
 }
 
-function checkUserInput(userInput) {
+function checkUserInput() {
   let userInput;
+  let verification;
+  let str = '숫자를 입력해주세요 : ';
 
-  userInput = input('숫자를 입력해주세요 : ');
-  if (userInput.length !== 3) {
-    throw new Error();
+  userInput = input(str);
+  verification = checklength(str, userInput);
+
+  if (verification === false) {
+    return [false, undefined];
   }
-
   let userInputStr = userInput.split("");
   userInput = userInputStr.map((val) => Number(val));
-
-  return userInput;
+  return [true, userInput];
 }
 
 function input(text) {
@@ -42,11 +67,22 @@ function input(text) {
     MissionUtils.Console.print(`${text}${answer}`);
     result = answer;
   });
-
   return result;
 }
 
-function Game(randomNumber, userInput) {
+function checklength(text, input) {
+  if (text === '숫자를 입력해주세요 : ') {
+    if (input.length === 3)
+      return true;
+  }
+  else if (text === '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n') {
+    if (input.length === 1)
+      return true;
+  }
+  return false;
+}
+
+function baseballGame(randomNumber, userInput) {
   let strike = 0;
   let ball = 0;
 
@@ -58,42 +94,54 @@ function Game(randomNumber, userInput) {
     else if (randomNumber.indexOf(val) !== userInput.indexOf(val) && randomNumber.indexOf(val) !== -1)
       ball += 1;
   }
-
   return [strike, ball];
 }
 
-
-function checkStrike(strike, ball) {
+function check3Strike(strike, ball) {
   if (strike === 3) {
     MissionUtils.Console.print(`${strike}스트라이크`);
     MissionUtils.Console.print(`3개의 숫자를 모두 맞히셨습니다! 게임 종료`);
-    userInput = input('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n');
-    userInput = Number(userInput);
-    if (userInput === 1) {
-      let randomNumber = [];
-      randomNumber = createRandom();
-      return randomNumber;
-    }
-    else if (userInput === 2) {
-      MissionUtils.Console.print('게임 종료');
-      return 'break';
-    }
+    let result = success();
+    console.log(result)
+    return result;
   }
   else if (strike > 0 && ball > 0) {
     MissionUtils.Console.print(`${ball}볼 ${strike}스트라이크`);
-    return 'not a 3 strike';
+    return ['not a 3 strike'];
   }
   else if (strike > 0 && ball <= 0) {
     MissionUtils.Console.print(`${strike}스트라이크`);
-    return 'not a 3 strike';
+    return ['not a 3 strike'];
   }
   else if (strike <= 0 && ball > 0) {
     MissionUtils.Console.print(`${ball}볼`);
-    return 'not a 3 strike';
+    return ['not a 3 strike'];
   }
   else {
     MissionUtils.Console.print(`낫싱`);
-    return 'not a 3 strike';
+    return ['not a 3 strike'];
+  }
+}
+
+function success() {
+  let userInput;
+  let verification;
+  let str = '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n';
+
+  userInput = input(str);
+  verification = checklength(str, userInput);
+
+  if (verification === true) {
+      userInput = Number(userInput);
+      if (userInput === 1) {
+          let randomNumber = [];
+          randomNumber = createRandom();
+          return ['restart', randomNumber];
+      }
+      else if (userInput === 2) {
+          MissionUtils.Console.print('게임 종료');
+          return ['break'];
+      }
   }
 }
 
