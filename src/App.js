7f6f153e493cfc50e3f5c7847dc2afe135d4dd56
100Gyeon <bjy9966@naver.com>
2022-11-08 21:59:28 +0900

    refactor: constants 분리

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 const MissionUtils = require('@woowacourse/mission-utils');
-
-const ANSWER_LENGTH = 3;
-const START_NUMBER = 1;
-const END_NUMBER = 9;
-const YES = 1;
-const NO = 2;
+const { ANSWER_LENGTH, START_NUMBER, END_NUMBER, YES, NO } = require('./constants');
 
 class App {
   constructor() {
