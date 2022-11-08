2778d5be0a25f5edcf2e1fdacdeb317250e19afb
SALT0601 <thfxm0601@gmail.com>
2022-11-08 20:30:14 +0900

    refactor: 중복 숫자와, 숫자가 3자리 이상일 경우 예외처리 추가

--- src/App.js
+++ src/App.js
@@ -9,9 +9,23 @@ class App {
        }
      }    
   }
+  isdupli(answer) {
+    let cnt=0;
+    for(let i=1;i<3;i++){
+      if(answer[i]===answer[i-1]){
+        cnt++;
+      }
+    }
+    if(cnt>0) return true;
+    return false;
+  }
   inputNumber() {
     Console.readLine("숫자를 입력해주세요 : ", (ans) => {
       if (ans.length === 3 && !isNaN(Number(ans))) {
+        MissionUtils.Console.print("숫자만 3개를 입력해주세요.");
+      }else if(ans.length>3){
+        MissionUtils.Console.print("숫자를 3개만 입력해주세요.");
+      }else if(isdupli(ans)){
         MissionUtils.Console.print("중복되지 않은 숫자 3개를 입력해주세요.");
       }
       const playerNumber = [];
@@ -25,11 +39,30 @@ class App {
           res.ball = res.ball + 1 || 1;
         }
       });
+      resultGame();
     });
+  }
+
+  resultGame() {
+    let resultValue = "";
+    if (res.ball > 0) {
+      resultValue += `${res.ball}볼 `;
+    }else if(res.strike > 0) {
+      resultValue += `${res.strike}스트라이크 `;
+    }else if(!res.ball && !res.strike) {
+      resultValue = "낫싱";
+    }
+
+    MissionUtils.Console.print(resultValue);
+    if (res.strike === 3) {
+      MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }else {
+      inputNumber();
+    } 
   }
   play() {
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
-    randomNumber();
+    this.randomNumber();
   }
 }
 const app = new App();
