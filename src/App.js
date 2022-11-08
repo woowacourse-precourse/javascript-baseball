0660a3c9f08410f9269fbf323713f05731d6ca8e
Kimyewon <yewon6282@gmail.com>
2022-11-08 17:09:24 +0900

    feat : 컴퓨터의 랜덤 3자리 숫자

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
+  constructor() {
+    this.answer = this.getComputerNum();
+  }
+
   play() {}
+
+  getComputerNum() {
+    const randomNum = [];
+    while (randomNum.length < 3) {
+      const number = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (!randomNum.includes(number)) randomNum.push(number);
+    }
+    return randomNum;
+  }
 }
 
 module.exports = App;
