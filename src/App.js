9ac9c57b5b0b76b8312a064e8d96d5cf1f7a0383
kimeodml <kimeodml@gmail.com>
2022-11-08 22:43:45 +0900

    feat: 터미널 실행을 위한 코드 추가

--- src/App.js
+++ src/App.js
@@ -4,11 +4,16 @@ const ONLY_NUMBER = /^[1-9]+$/;
 class App {
   play() {
     Console.print('숫자 야구 게임을 시작합니다.');
-    this.computer = [];
+    this.gameProcess();
+  }
+
+  gameProcess() {
     this.input = [];
+    this.computer = [];
     this.computerRandomNumber();
     this.inputNumber();
   }
+
 
   computerRandomNumber() {
     while (this.computer.length < 3) {
@@ -68,7 +73,7 @@ class App {
     Console.print('3개의 숫자를 모두 맞히셨습니다! 게임종료');
     Console.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.', (input) => {
       if (input === '1') {
-        return this.play();
+        return this.gameProcess();
       }
       else if (input === '2') {
         Console.print('게임 종료');
