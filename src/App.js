b2577c30755ce3b77e33b832903cbd86775047fd
1wooseok <74036731+1wooseok@users.noreply.github.com>
2022-11-08 21:24:30 +0900

    feat: 랜덤 숫자 생성 함수

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+const { Console, Random } = require('@woowacourse/mission-utils');
+
 class App {
-  play() {}
+  play() { }
+
+  createComputerNumber() {
+    const computerNumber = new Set();
+
+    while (computerNumber.size < 3) {
+      computerNumber.add(Random.pickNumberInRange(1, 9));
+    }
+
+    return [...computerNumber];
+  }
 }
 
 module.exports = App;
