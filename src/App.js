ba5017109372252bca14839329647f5a59bc722c
tkdrb12 <tkdrb15@gmail.com>
2022-11-08 16:16:50 +0900

    strike, ball점수  계산  함수 수정

--- src/App.js
+++ src/App.js
@@ -7,12 +7,50 @@ class App {
   }
 
   play() {
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
     this.random_numbers = CreateRandomNum();
     MissionUtils.Console.print(this.random_numbers);
+    this.input_numbers = InputNumber();
+    MissionUtils.Console.print(this.input_numbers);
   }
 }
 
+//숫자 입력 함수
+//
+// function InputNumber(){
+//   let input_numbers;
+  
+//   MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (input_numbers) => {
+//     console.log(`숫자: ${input_numbers}`);
+//   });
 
+// //throw new Error("숫자를 입력하세요");
+
+//   return input_numbers;
+// }
+
+
+function CalStrike(str1, str2){
+  let strike = 0;
+  for(let index=0; index<3; index++){
+    if(str1[index] == str2[index])
+    strike = strike + 1;
+  }
+  return strike;
+}
+
+function CalBall(str1, str2){
+  let ball = 0;
+  for(let index=0; index<3; index++){
+    if(str2.includes(str1[index]))
+    ball = ball + 1;
+  }
+  return ball;
+}
+
+//숫자야구 게임 랜덤 숫자 생성 함수
+//input : None
+//output : 중복되지 않는 3개의 숫자를 문자열 형태로 반환
 function CreateRandomNum(){
   let randstr = "";
   while(randstr.length < 3){
