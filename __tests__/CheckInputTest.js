6b14ceae46e5e1a463cb5166e77e2d0589b936b0
shlee0820 <72565083+shlee0820@users.noreply.github.com>
2022-11-08 20:16:25 +0900

    test: 사용자 입력 예외처리 테스트 케이스 추가

--- __tests__/CheckInputTest.js
+++ __tests__/CheckInputTest.js
@@ -1,5 +1,6 @@
 /* eslint-disable */
 const checkInvalidRandomNumber = require('../src/utils/checkInvalidRandomNumber');
+const checkValidUserInput = require('../src/utils/checkValidUserInput');
 
 describe('난수 예외 처리', () => {
     test('중복되는 수가 있는 경우', () => {
@@ -22,3 +23,25 @@ describe('난수 예외 처리', () => {
       });
   });
   
+describe('사용자 입력값 예외 처리', () => {
+    test('중복되는 수가 있는 경우', () => {
+      const input = [1, 2, 2];
+  
+      expect(() => checkValidUserInput(input)).toThrow('error1');
+    });
+    test('3자리가 아닌 경우', () => {
+      const input = [1, 2, 6, 3];
+  
+      expect(() => checkValidUserInput(input)).toThrow('error2');
+    });
+    test('문자가 섞여있는 경우', () => {
+        const input = [1, 2, 'e'];
+    
+        expect(() => checkValidUserInput(input)).toThrow('error3');
+      });
+    test('0이 들어가 있는 경우', () => {
+        const input = [1, 2, 0];
+    
+        expect(() => checkValidUserInput(input)).toThrow('error4');
+      });
+});
