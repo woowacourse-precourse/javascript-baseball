3916f67898f4352608fcbe3610f3535f4c56a904
RyuJoonSik <tldtksahtxja@naver.com>
2022-11-08 22:34:28 +0900

    test: AutomaticBallGenerator.test.js의 execute 메서드 테스트 추가

--- src/Components/AutomaticBallGenerator/AutomaticBallGenerator.test.js
+++ src/Components/AutomaticBallGenerator/AutomaticBallGenerator.test.js
@@ -1,9 +1,9 @@
 const AutomaticBallGenerator = require("./AutomaticBallGenerator");
 
 describe("AutomaticBallGenerator", () => {
-  test("execute로 숫자를 생성", () => {
+  test("execute로 숫자를 생성", async () => {
     const BALL_GENERATOR = new AutomaticBallGenerator();
-    const BALL = BALL_GENERATOR.execute();
+    const BALL = await BALL_GENERATOR.execute();
 
     expect(typeof BALL.getNumber()).toBe("number");
   });
