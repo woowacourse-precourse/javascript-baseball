2b2562d4e6ef52aef8351bbafad340e72ee8063d
Lim_JiSeon <dpf226@naver.com>
2022-11-08 20:27:50 +0900

    feat(Messages.js, Functions.js) : update and fix file add functions - countStrike - countBall - returnResult - printBallStrike
    add message
    - nothing

--- src/components/Functions.js
+++ src/components/Functions.js
@@ -19,8 +19,37 @@ function selectComputer() {
     return Random.pickUniqueNumbersInRange(1, 9, 3);
 }
 
-function inputUserNum() {
+function countStrike(userNum, computerNum) {
+    let strike = 0;
+    for (let index = 0; i < 3; i++) {
+        if (userNum[index] == computerNum[index]) strike++;
+    }
+    return strike;
+}
 
+function countBall(userNum, computerNum) {
+    let ball = 0;
+    for (let userIdx = 0; userIdx < 3; userIdx++) {
+        for (let computerIdx = userIdx+1; computerIdx < 3; computerIdx++)
+            if (userNum[userIdx] == computerNum[computerIdx]) ball++;
+    }
+    return ball;
+}
+
+function returnResult(ball, strike) {
+    if (ball && strike) 
+        return ball + Messages.BALL + " " + strike + Messages.STRIKE;
+    else if (ball)
+        return ball + Messages.BALL;
+    else if (strike)
+        return strike + Messages.STRIKE;
+    else return Messages.NOTHING
+}
+
+function printBallStrike(computerNum) {
+    Console.readLine(Messages.INPUT_NUM, (inputNum) => {
+        Console.print(returnResult(countBall(inputNum,computerNum),countStrike(inputNum,computerNum)))
+    });
 }
 
 module.exports = Functions;
--- src/components/Messages.js
+++ src/components/Messages.js
@@ -3,6 +3,7 @@ const Messages = Object.freeze({
     INPUT_NUM : "숫자를 입력해주세요 : ",
     BALL : "볼",
     STRIKE : "스트라이크",
+    NOTHING : "낫싱",
     SUCCESS : "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
     RESTART_OR_CLOSE : "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
 
