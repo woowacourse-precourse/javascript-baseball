7274d448576c4b80c335837eaa7a0a19cf4ab53d
jisu-kim <kss2460@naver.com>
2022-11-08 20:22:30 +0900

    chore: 폴더 정리

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-const showPrint = require("../src/showPrint");
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
+  constructor() {
+    this.computerNumber = [];
+    this.userNumber = [];
+  }
   play() {
-    showPrint("숫자 야구 게임을 시작합니다.");
+    this.computerNumber = this.initNumber();
+    this.showPrint("숫자 야구 게임을 시작합니다.");
+    this.compareNumber();
   }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
