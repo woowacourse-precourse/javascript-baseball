edb6c6b522ffcece3da99a79cfc7547bdc1e5d23
lazypalegg <109095071+lazypalegg@users.noreply.github.com>
2022-11-08 18:53:25 +0900

    feat: generate answer function

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
 class App {
-  play() {}
+
+  constructor() {
+    this.answer = [];
+  }
+
+  play() {
+  }
+
+  generateAnswer() {
+    const MissionUtils = require("@woowacourse/mission-utils");
+    while (this.answer.length < 3) {
+      let randomNum = MissionUtils.Random.pickNumberInRange(1, 9);
+      if (this.answer.indexOf(randomNum) === -1) this.answer.push(randomNum);
+    }
+  }
+
+  
 }
+
+const app = new App;
+app.play();
 
 module.exports = App;
