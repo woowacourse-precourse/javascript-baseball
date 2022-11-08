066d1404432264ea1d2d8c605ed8ec0d08802e2a
김지수 <jisudev00@gmail.com>
2022-11-08 09:44:49 +0900

    Feat: 게임 시작 문구 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+const { Console } = require("@woowacourse/mission-utils");
+const MESSAGE = require("./assets/Message");
+
 class App {
-  play() {}
+  play() {
+    Console.print(MESSAGE.START);
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
