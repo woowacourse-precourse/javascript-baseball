86d704954defd1d8b6024f68a241fd834d3081f5
lego37yoon <lego37yoon@outlook.com>
2022-11-08 18:23:48 +0900

    feat: add function to continue game
    Add function to continue game
    - Run game with newGamePlay() function
    - Check if user wants to continue game with questionToContinueGame() function
    - Close standard input with Console.close() method in MissionUtils
    - Throw error when wrong value inside

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 const MISSION_UTILS = require('@woowacourse/mission-utils');
 
 class App {
-  play() {}
+  async play() {
+    MISSION_UTILS.Console.print('숫자 야구 게임을 시작합니다.');
+    newGamePlay();
+    //MISSION_UTILS.Console.close();
+  }
+}
+
+async function newGamePlay() {
+  questionToContinueGame();
+}
+
+async function questionToContinueGame() {
+  
+  MISSION_UTILS.Console.readLine('게임을 새로 시작하시려면 1, 종료하려면 2를 입력하세요.', (ans) => {
+    if (Number(ans) === 1) {
+      console.log('Trying to run new game');
+      newGamePlay();
+    } else if (Number(ans) === 2) {
+      MISSION_UTILS.Console.close();
+    } else {
+      throw new Error('잘못된 값을 입력하셨습니다. 애플리케이션을 종료합니다.');
+    }
+  });
 }
 
 module.exports = App;
