b2976487f0a31b10628755583b48d2de44ed42ef
KangYunJi <dbswl701@ajou.ac.kr>
2022-11-08 22:41:40 +0900

    comment: 주석 정리

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 const MissionUtils = require('@woowacourse/mission-utils');
-// 에러 계속 뜨는데 입력 한번만 받도록 해놔서 그런듯
+
 class App {
-  // play() {}
-  init(){ // 일단 배열 초기화
+
+  init(){
     this.computerArr=[];
     this.userArr=[];
   }
 
   makeUserInputNum(){
-    // 게임이 끝날 때 까지 사용자 입력을 계속 받아야 한다. => 재귀
-    MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (input) => { // readline 출력 안됨
+    MissionUtils.Console.readLine('숫자를 입력해주세요 : ', (input) => {
       this.userArr = input.split('').map(Number);
-      console.log(`userInput: ${this.userArr}`);
+      // console.log(`userInput: ${this.userArr}`);
       this.printResult();
     });
   }
@@ -33,6 +32,30 @@ class App {
     this.makeUserInputNum(); // 사용자 재입력
   }
 
+  // 유효성 검사
+  isValid(userInput){
+    const NOT_THREE_NUM_ERROR = "입력은 3개까지 허용입니다."
+    const NOT_DIGIT_ERROR = "숫자만 입력해야 합니다."
+    const DUPLICATE_ERROR = "중복된 입력입니다."
+    const NOT_ALLOW_ZERO_ERROR = "1~9사이의 숫자만 입력하세요"
+
+    // 입력값 3자리인지 확인
+    if(userInput.length !== 3) throw NOT_THREE_NUM_ERROR;
+
+    // 숫자인지 확인
+    if(!isNaN(userInput)) throw NOT_DIGIT_ERROR;
+
+    // 중복 없는지 확인
+    const array = [...userInput];
+    const arraySet = new Set(array); 
+    if(arraySet.size!==array.length) throw DUPLICATE_ERROR;
+
+    // 1~9사이의 숫자인지 확인
+    if(userInput.includes(0)) throw NOT_ALLOW_ZERO_ERROR;
+
+    return true;
+  }
+
   makeComputerInputNum(){
     const randomArr = [];
     while (randomArr.length < 3) {
@@ -41,7 +64,37 @@ class App {
     }
 
     this.computerArr = [...randomArr];
-    console.log(`computerInput : ${this.computerArr}`);
+    // console.log(`computerInput : ${this.computerArr}`);
+  }
+
+  // 결과 확인
+  playing(){
+    const same = this.computerArr.filter(x=>this.userArr.includes(x));
+    let nothing = -1;
+    let size = same.length;
+    let strike = 0;
+    let ball = 0;
+
+    if(same.length === 0) nothing++;
+    while(size--){
+      if(this.computerArr.indexOf(same[size]) === this.userArr.indexOf(same[size])) strike++;
+    }
+    ball = same.length - strike;
+
+    return [strike, ball, nothing];
+  }
+
+  isRepeat(){
+    const INPUT_ERROR = "잘못된 입력입니다.(1, 2 이외의 입력)"
+    MissionUtils.Console.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.', (input) => {
+      // console.log(`${input}`);
+      if(input === '1') this.playBaseball();
+      else if(input === '2'){
+        MissionUtils.Console.print('게임 종료');
+        MissionUtils.Console.close();
+      }
+      else throw INPUT_ERROR;
+    });
   }
 
   playBaseball(){
@@ -56,60 +109,6 @@ class App {
     this.playBaseball();
   }
 
-  // 유효성 검사
-  isValid(userInput){
-    const NOT_THREE_NUM_ERROR = "입력은 3개까지 허용입니다."
-    const NOT_DIGIT_ERROR = "숫자만 입력해야 합니다."
-    const DUPLICATE_ERROR = "중복된 입력입니다."
-
-    // 입력값 3자리인지 확인
-    if(userInput.length !== 3) throw NOT_THREE_NUM_ERROR;
-
-    // 숫자인지 확인
-    if(!isNaN(userInput)) throw NOT_DIGIT_ERROR;
-
-    // 중복 없는지 확인
-    const array = [...userInput];
-    const arraySet = new Set(array); 
-    if(arraySet.size!==array.length) throw DUPLICATE_ERROR;
-
-    return true;
-  }
-
-  // 결과 확인
-  playing(){
-    const same = this.computerArr.filter(x=>this.userArr.includes(x));
-    let nothing = -1;
-    let size = same.length;
-    let strike = 0;
-    let ball = 0;
-
-    console.log(`same : ${same}, length : ${same.length}`);
-
-    if(same.length === 0) nothing++; // 아무것도 겹치는거 없음
-    while(size--){
-      if(this.computerArr.indexOf(same[size]) === this.userArr.indexOf(same[size])) strike++;
-    }
-    ball = same.length - strike;
-
-    console.log(`ball : ${ball}, strike : ${strike}, nothing : ${nothing}`)
-    console.log(`same` + this.computerArr.filter(x=>this.userArr.includes(x)));
-
-    return [strike, ball, nothing];
-  }
-
-  isRepeat(){
-    const INPUT_ERROR = "잘못된 입력입니다.(1, 2 이외의 입력)"
-    MissionUtils.Console.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.', (input) => {
-      console.log(`재시작??? : ${input}`);
-      if(input === '1') this.playBaseball();
-      else if(input === '2'){
-        MissionUtils.Console.print('게임 종료');
-        MissionUtils.Console.close();
-      }
-      else throw INPUT_ERROR;
-    });
-  }
 }
 
 module.exports = App;
