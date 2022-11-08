1229fe16149f6dcb00c9cf99e50d3d85ff4230ed
Hanav52 <wnsrl8329@gmail.com>
2022-11-08 18:17:59 +0900

    코드 정리

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
-
   constructor() {
     this.numbers = [];
   }
@@ -19,9 +18,13 @@ class App {
       throw "잘못된 값을 입력하셨습니다. 3자리의 수만 입력할 수 있습니다.";
     }
 
-    const splited = input.split('');
-    if (splited[0] === splited[1] || splited[1] === splited[2] || splited[0] === splited[2]) {
-      throw "잘못된 값을 입력하셨습니다. 3자리의 수는 각각 서로 다른 숫자여야 합니다."
+    const splited = input.split("");
+    if (
+      splited[0] === splited[1] ||
+      splited[1] === splited[2] ||
+      splited[0] === splited[2]
+    ) {
+      throw "잘못된 값을 입력하셨습니다. 3자리의 수는 각각 서로 다른 숫자여야 합니다.";
     }
   }
 
@@ -30,17 +33,21 @@ class App {
     this.validateInput(input);
     const [strike, ball] = this.checkStrikeBall(input);
 
-    if (strike === 3) { // 게임 종료
+    if (strike === 3) {
+      // 게임 종료
       MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-      MissionUtils.Console.readLine("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.", (flag) => {
-        this.exitOrRestart(flag);
-      });
-    }
-    else { // 결과(힌트) 출력
+      MissionUtils.Console.readLine(
+        "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
+        (flag) => {
+          this.exitOrRestart(flag);
+        }
+      );
+    } else {
+      // 결과(힌트) 출력
       if (strike === 0 && ball === 0) MissionUtils.Console.print("읎다");
       else MissionUtils.Console.print(ball + "볼 " + strike + "스트라이크");
 
-        // 숫자를 모두 맞힐 때까지 반복
+      // 숫자를 모두 맞힐 때까지 반복
       MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (input) => {
         this.processInput(input);
       });
@@ -52,8 +59,8 @@ class App {
     let strike = 0;
     let ball = 0;
 
-    const splited = input.split('');
-    for (let i=0; i<splited.length; i++) {
+    const splited = input.split("");
+    for (let i = 0; i < splited.length; i++) {
       const splitedNum = Number(splited[i]);
       if (splitedNum === this.numbers[i]) strike++;
       else if (this.numbers.includes(splitedNum)) ball++;
@@ -66,13 +73,13 @@ class App {
   exitOrRestart(flag) {
     if (flag == 1) this.play();
     else if (flag == 2) return;
-    else throw "잘못된 값을 입력하였습니다. 1 또는 2만 입력할 수 있습니다."
-  }  
+    else throw "잘못된 값을 입력하였습니다. 1 또는 2만 입력할 수 있습니다.";
+  }
 
   // 게임
   play() {
     this.pickRandomNumbers();
-    
+
     // 게임 시작 문구 출력
     MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
     MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (input) => {
