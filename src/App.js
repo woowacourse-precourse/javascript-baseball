7ed0967b590bc9c0da32f2102a549a85b9bfb8e1
Sallyvely <tpdus4159@naver.com>
2022-11-08 10:06:18 +0900

    기능 구현 : 볼인지 스트라이크인지 판단하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -18,6 +18,25 @@ class App {
       })
         return userInput;
     }
+    strike(computerRandomNum, userInputNum){
+      let strikeScore=0;
+      for(let index=0;index<3;index++){
+        if(String(computerRandomNum).charAt(index)===String(userInputNum).charAt(index)){
+          strikeScore+=1;  
+        }
+      }  
+      return strikeScore;
+    }
+    /*볼인지 점수 계산하는 기능*/
+    ball(computerRandomNum, userInputNum){
+      let ballScore=0;
+      for(let index=0; index<3;index++){
+        if((computerRandomNum).includes(String(userInputNum).charAt(index))&&!(String(computerRandomNum).charAt(index)===String(userInputNum).charAt(index))){
+          ballScore+=1;
+        }
+      }
+      return ballScore;
+    }
 }
 
 module.exports = App;
