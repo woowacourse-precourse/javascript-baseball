8f9a02f8f720b49eccfaac94a8ddde713d9f12d0
yunjjeongjo <two6885@gmail.com>
2022-11-08 23:14:05 +0900

    feat: 게임 시작 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+const { Console } = require("@woowacourse/mission-utils");
+const { MESSAGE } = require("./constants/constants");
 class App {
-  play() {}
-}
+  play() {
+    Console.print(MESSAGE.START);
+    this.gamePrepare();
+  }
 
+  gamePrepare() {
+    const answer = createRandomNumber();
+  }
+
+
+}
+const app = new App();
+app.play();
 module.exports = App;
