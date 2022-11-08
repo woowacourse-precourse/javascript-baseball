fbfd863df7c92e4658662897941c7b47ae84257a
heejorika <y20001206@gmail.com>
2022-11-08 20:13:43 +0900

    ㅜ

--- src/App.js
+++ src/App.js
@@ -4,36 +4,40 @@
 
 // module.exports = App;
 
-const MU = require("@woowacourse/mission-utils");
-//const Console = require("@woowacourse/mission-utils/src/console");
-//const Random = require("@woowacourse/mission-utils/src/random");
+const { Console, Random } = require('@woowacourse/mission-utils');
 
 function gameSet(){
   let game_flag = true;  // 게임을 재시작하거나 종료하기 위한 flag
-  MU.Console.print("숫자 야구 게임을 시작합니다.");
+  Console.print("숫자 야구 게임을 시작합니다.");
 
   while(game_flag){
     gameStart();
     let input_flag = true;
 
     while(input_flag){
-      MU.Console.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요");
-      const input = MU.Console.readLine();  // 입력받기
+      Console.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요");
+      const input = Console.readLine();  // 입력받기
       if (Number(input)==1){
         input_flag=false;
       }else if(Number(input)==2){
         input_flag=false;
         game_flag =false;
-        MU.Console.close();
+        Console.close();
       }else{
-        MU.Console.print("1 또는 2를 입력하세요.");
+        Console.print("1 또는 2를 입력하세요.");
       }
     }
   }
 }
 
+// 숫자와 자리 둘다 맞는지
+function isStrike(number){
+  
+}
 
+function isBall(number){
 
+}
 
 function setRandomNumber(){
   const computer = [];
