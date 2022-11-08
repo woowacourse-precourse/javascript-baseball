661c4886a6da7f683fcf4dcfd9e2548be03c2b19
SALT0601 <thfxm0601@gmail.com>
2022-11-08 19:14:25 +0900

    add: 1~9까지의 랜덤 숫자 생성
    MissionUtils을 이용해서 컴퓨터가 1~9까지의 숫자중 3개를 랜덤하게 생성하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+const MissionUtils = require("@woowacourse/mission-utils");
 class App {
-  play() {}
+  randomNumber(){
+    const computer = [];
+    while (computer.length < 3) {
+    const number = MissionUtils.Random.pickNumberInRange(1, 9);
+    if (!computer.includes(number)) {
+     computer.push(number);
+       }
+     }    
+  }
+  play() {
+    randomNumber();
+  }
 }
-
+const app = new App();
+app.play();
 module.exports = App;
