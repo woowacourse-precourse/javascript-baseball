10b8e5f6f10fd08d253c50cbcd64821f647a9d30
yeonkyoung <keep.ykk@gmail.com>
2022-11-08 23:11:37 +0900

    feat: 컴퓨터는 1에서 9까지의 서로 다른 임의의 수를 3개 선택 함수

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ let isEndGame = true;
 let result = true;
 const game_start = "숫자 야구 게임을 시작합니다.";
 
+function computerPickNumbers() {
+  computer = MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3);
+}
+
 class App {
   play() {
     MissionUtils.Console.print(game_start);
