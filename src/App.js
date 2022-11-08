283caab8e28832267dfd3e3733fa926d13f16d84
yeeun426 <thsudkcla7@naver.com>
2022-11-08 23:54:44 +0900

    feat 야구게임 - 유저가 숫자 입력

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ const MissionUtils = require("@woowacourse/mission-utils");
 class App {
   // 기능 목록
   play() {
-    Console.print("숫자 야구 게임을 시작합니다.");
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
   }
   // 컴퓨터가 랜덤으로 3개의 숫자 생성
   makeRandomNum() {
@@ -14,7 +14,20 @@ class App {
     }
     this.baseballNum = [...baseballNums].join("");
   }
+  
   // 유저가 숫자 입력
+  userInputNum(){
+    MissionUtils.Console.readline("숫자를 입력해주세요. : ", (userNum) => {
+        this.stirup(userNum);
+    })
+  }
+
+  // 입력 숫자 유효한 값인지 확인
+  checkValidNum(userNum) {
+    if(this.isValidNum(userNum)) {
+      this.gradeInput(userNum);
+    }
+  }
   // 스트라이크(같은 자리, 같은 숫자) 제거
   // 볼 (다른 자리, 같은 숫자)제거
   play() {
@@ -22,6 +35,6 @@ class App {
   }
   
 }
-// module.exports = App;
-const test = new App();
-console.log(App.play());
+module.exports = App;
+// const test = new App();
+// console.log(App.play());
