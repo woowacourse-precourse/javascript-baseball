b3cea65229e872ea75557c12dba8628e59105e3c
윤동주 <yoondj98@naver.com>
2022-11-08 22:30:01 +0900

    feat: 입력받은 숫자가 유효한 값인지 여부 확인하는 기능 구현 - checkValidation 함수

--- src/App.js
+++ src/App.js
@@ -20,6 +20,20 @@ class App {
       this.getBallStrikeCount(randomNums, answer);
     });
   }
+
+  checkValidation(answer) {
+    if (answer.length != 3)
+      throw new Error("입력하신 숫자가 3자리가 아닙니다.");
+    else if (
+      answer[0] == answer[1] ||
+      answer[0] == answer[2] ||
+      answer[1] == answer[2]
+    ) {
+      throw new Error("입력하신 숫자에 똑같은 숫자가 존재합니다.");
+    } else if (isNaN(answer) || answer.includes(0)) {
+      throw new Error("입력하신 글자에 1~9가 아닌 글자가 포함되어 있습니다.");
+    }
+  }
   
 }
 
