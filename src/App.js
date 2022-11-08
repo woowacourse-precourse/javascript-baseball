5834f307041912b422c5afaab77c25fa216167ac
subin <newdeas100@gmail.com>
2022-11-08 17:17:24 +0900

    feat(baseball) util 모듈화 및 class 메소드로 변경

--- src/App.js
+++ src/App.js
@@ -1,111 +1,70 @@
 const MissionUtils = require("@woowacourse/mission-utils");
-const [Console, Random] = [MissionUtils.Console, MissionUtils.Random];
-
-function makeTarget() {
-  const tempArr = [];
-
-  while (tempArr.length < 3) {
-    const number = Random.pickNumberInRange(1, 9);
-    if (!tempArr.includes(number)) {
-      tempArr.push(number);
-    }
-  }
-
-  return [...tempArr];
-}//random한 3개의 숫자를 배열형태로 return
-
-
-function handleData(inputArr, targetArr) {
-  let [strike, ball] = [0, 0];
-
-  inputArr.forEach((input, idx) => {
-    const targetIdx = targetArr.findIndex(target => target === input);
-    if (targetIdx === idx) {
-      strike += 1;
-    }
-    else if (targetIdx !== -1) {
-      ball += 1;
-    }
-  })
-
-  return printResult([strike, ball]);
-}//target과 input의 ball, strike를 return함.
-
-function printResult(countArr) {
-  const [strike, ball] = countArr;
-
-  if (strike === 0 && ball === 0) {
-    Console.print("낫싱");
-    return false;
-  }
-  else if (ball !== 0 && strike === 0) {
-    Console.print(`${ball}볼`);
-    return false;
-  }
-  else if (ball === 0 && strike !== 0) {
-    Console.print(`${strike}스트라이크`);
-    if (strike === 3) return true;
-    else return false;
-  }
-  else if (ball !== 0 && strike !== 0) {
-    Console.print(`${ball}볼 ${strike}스트라이크`);
-    return false;
-  }
-}//ball, strike count에 따라 결과 출력
-
-function isValidInput(input) {
-  const regex = /^[0-9]+$/;
-  if (!regex.test(input) || input.length !== 3) {
-    throw new Error("유효한 입력값이 아닙니다.");
-  }
-
-  const array = [...input];
-  const arraySet = new Set(array);
-  if (array.length !== arraySet.size) {
-    throw new Error("입력에 중복값이 포함되어 있습니다.");
-  }
-}
-
-
-
-function playGame() {
-  const targetArr = makeTarget();
-  readData(targetArr);
-}//게임로직
-
-function readData(targetArr) {
-  let isAnswer = false;
-
-  Console.readLine("숫자를 입력해주세요 : ", (input) => {
-    isValidInput(input);
-    const inputArr = [...input].map(Number);
-    isAnswer = handleData(inputArr, targetArr);
-    if (!isAnswer) readData(targetArr);
-    else {
-      Console.print("3개의 숫자를 모두 맞히셨습니다! 게임종료");
-      isRepeatGame();
-    }
-  });
-}//입력을 받아서 로직을 처리하는 부분
-
-function isRepeatGame() {
-  Console.readLine("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요 : ", (input) => {
-    if (input === "1") playGame();
-    else if (input === "2") {
-      Console.print("게임 종료");
-      Console.close();
-    }
-    else {
-      throw new Error("1,2 이외의 숫자가 입력되었습니다.");
-    }
-  });
-}
-
+const Console = MissionUtils.Console;
+const {makeTarget, handleData, isValidInput,printResult} = require("./util");
 
 class App {
+
+  #targetArr;
+
+  constructor(){
+   this.#targetArr = [];
+   this.countArr = []; // [strike,ball] 형태
+  }
+
+  initData(){
+    this.#targetArr = [];
+    this.countArr = [];
+  }
+
+  setCountArr(array){
+    this.countArr = array;
+  }
+  
+  getCountArr(){
+    return [...this.countArr];
+  }
+
+  readData(targetArr){
+    let isAnswer = false;
+
+    Console.readLine("숫자를 입력해주세요 : ", (input) => {
+      isValidInput(input);
+      const inputArr = [...input].map(Number);
+      const handleResult = handleData(inputArr, targetArr);
+      this.setCountArr(handleResult);
+
+      isAnswer = printResult(this.getCountArr());
+      if (!isAnswer) this.readData(targetArr);
+      else {
+        Console.print("3개의 숫자를 모두 맞히셨습니다! 게임종료");
+        this.isRepeatGame();
+      }
+    });
+  }
+
+  isRepeatGame(){
+    Console.readLine("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요 : ", (input) => {
+      if (input === "1") this.playGame();
+      else if (input === "2") {
+        Console.print("게임 종료");
+        Console.close();
+      }
+      else {
+        throw new Error("1,2 이외의 숫자가 입력되었습니다.");
+      }
+    });
+  }
+
+  playGame(){
+    this.initData();
+    this.#targetArr = makeTarget();
+    console.log(this.#targetArr);
+    this.readData(this.#targetArr);
+  }
+
   play() {
     Console.print('숫자 야구게임을 시작합니다.');
-    playGame();
+    this.playGame();
   }
 }
 
