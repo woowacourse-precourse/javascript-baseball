9a03721290e203ec51ee21b6a341fbcfe14702a5
wnw <fkaus65@naver.com>
2022-11-08 15:04:40 +0900

    refactor(error message): change TYPE_ERROR message
    Before:
    
    TYPE_ERROR: ,
    
    After:
    
    TYPE_ERROR:

--- src/constants/error message.js
+++ src/constants/error message.js
@@ -2,7 +2,7 @@ const { REPLAY_NUMBER, GAME_NUMBER, PICK_NUMBER } = require('./game numbers');
 
 const ERROR_MESSAGE = Object.freeze({
   LENGTH_ERROR: `${GAME_NUMBER.CORRECT}개 숫자를 입력하세요.`,
-  TYPE_ERROR: `${GAME_NUMBER.INIT} ~ ${PICK_NUMBER.MAX} 사이의 숫자만 입력해주세요.`,
+  TYPE_ERROR: `${PICK_NUMBER.MIN} ~ ${PICK_NUMBER.MAX} 사이의 숫자만 입력해주세요.`,
   DUP_ERROR: `중복 없이 ${GAME_NUMBER.CORRECT}개 숫자를 입력해주세요,`,
   INPUT_ERROR: '게임 종료',
   INVALID_NUM_ERROR: `숫자로 ${REPLAY_NUMBER.KEEP_PLAY} 혹은 ${REPLAY_NUMBER.EXIT}를 입력해주세요.`,
