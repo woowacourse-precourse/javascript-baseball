7014a07ebacbc68215a3264b9c7a36ceaa9c24f6
Vo9Ming <kimminj2017@gmail.com>
2022-11-08 18:27:12 +0900

    상대방이 될 컴퓨터에 담을 세 수를 중복되지않게 뽑아내는 함수를 생성한다.

--- src/App.js
+++ src/App.js
@@ -3,10 +3,23 @@ const MissionUtils = require("@woowacourse/mission-utils");
 class App {
   play() {
     this.startMessage();
+    this.computerExtrackNumber();
   }
 
   startMessage() {
     return MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+  }
+
+  computerExtrackNumber() {
+    const COMPUTER_NUMBER = [];
+    for (var int = 0; COMPUTER_NUMBER.length < 3; int++) {
+      const computerNumber = MissionUtils.Random.pickNumberInRange(1, 9);
+      MissionUtils.Console.close();
+      if (COMPUTER_NUMBER[int - 1] !== computerNumber) {
+        COMPUTER_NUMBER.push(computerNumber);
+      }
+    }
+    return COMPUTER_NUMBER;
   }
 }
 const app = new App();
