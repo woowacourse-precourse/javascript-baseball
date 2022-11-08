2fdc8aade8584bcb9084b82d0e9cb78672335391
10_2pang <57481378+yedol1@users.noreply.github.com>
2022-11-08 18:18:58 +0900

    feat: 컴퓨터가 설정한 랜덤 숫자 3개를 생성하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -4,5 +4,17 @@ class App {
         MissionUtils.Console.print(MESSAGE.START_GAME);
         return this.getComputerNumbers();
     }
+    getComputerNumbers() {
+        const GET_NUMBERS = [];
+        GET_NUMBERS.push(MissionUtils.Random.pickNumberInRange(1, 9));
+        for (let i = 0; i < 2; i++) {
+            const SPACE = MissionUtils.Random.pickNumberInRange(1, 9);
+            if (GET_NUMBERS.indexOf(SPACE) === -1) {
+                GET_NUMBERS.push(SPACE);
+            }
+        }
+
+        return this.playTheGame(GET_NUMBERS.join(""));
+    }
 }
 module.exports = App;
