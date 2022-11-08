8cd708525fd26b22bb8a9e0e2a47135e74174cb4
Heeseung <gmltmd0816@naver.com>
2022-11-08 23:07:32 +0900

    feat: 상대방 임의의 수 3개 선택 함수 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
+const MissionUtils = require('@woowacourse/mission-utils');
+
 class App {
-  play() {}
+  constructor() {
+    this.computerRandomNumbers = this.generateComputerRandomNumbers();
+  }
+
+  play() {
+
+  }
+
+  printStartMessage() {
+    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+  }
+
+  createRandomNumber() {
+    const randomNumbers = [];
+    while (randomNumbers.length < 3) {
+      const randomNumber = MissionUtils.Random.pickNumberInRange(1, 10);
+      if (!randomNumbers.includes(randomNumber)) {
+        randomNumbers.push(randomNumber);
+      }
+    }
+    return randomNumbers;
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
