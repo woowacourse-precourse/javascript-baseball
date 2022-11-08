1aeeb0903c5ff3b7e118b811b01aedd3c04313c7
RyuJoonSik <tldtksahtxja@naver.com>
2022-11-08 21:21:24 +0900

    test: Defender.test.js 테스트 케이스 추가

--- src/Components/Defender/Defender.test.js
+++ src/Components/Defender/Defender.test.js
@@ -20,7 +20,7 @@ const mockRandoms = (numbers) => {
 describe("Defender", () => {
   test("나(공격수)의 Ball이 425이고 컴퓨터(수비수)의 Ball이 425이면 3스트라이크를 출력", () => {
     const BALL_GENERATOR = new AutomaticBallGenerator();
-    jest.spyOn(BALL_GENERATOR, "execute").mockReturnValue([4, 2, 5]);
+    jest.spyOn(BALL_GENERATOR, "execute").mockReturnValue(new Ball(425));
 
     const DEFENDER = new Defender(BALL_GENERATOR);
     const BALL_OF_ATTACKER = new Ball(425);
@@ -28,5 +28,27 @@ describe("Defender", () => {
     DEFENDER.reportAbout(BALL_OF_ATTACKER);
 
     expect(LOG_SPY).toHaveBeenCalledWith("3스트라이크");
+  });
+
+  test("나(공격수)의 Ball이 456이고 컴퓨터(수비수)의 Ball이 425이면 1볼 1스트라이크를 출력", () => {
+    const BALL_GENERATOR = new AutomaticBallGenerator();
+    jest.spyOn(BALL_GENERATOR, "execute").mockReturnValue(new Ball(456));
+
+    const DEFENDER = new Defender(BALL_GENERATOR);
+    const BALL_OF_ATTACKER = new Ball(425);
+    const LOG_SPY = getLogSpy();
+    DEFENDER.reportAbout(BALL_OF_ATTACKER);
+
+    expect(LOG_SPY).toHaveBeenCalledWith("1볼 1스트라이크");
+  });
+
+  test("게임이 끝났으면 true 반환", () => {
+    const BALL_GENERATOR = new AutomaticBallGenerator();
+    jest.spyOn(BALL_GENERATOR, "execute").mockReturnValue(new Ball(425));
+
+    const DEFENDER = new Defender(BALL_GENERATOR);
+    const BALL_OF_ATTACKER = new Ball(425);
+
+    expect(DEFENDER.isGameEnd(BALL_OF_ATTACKER)).toBe(true);
   });
 });
