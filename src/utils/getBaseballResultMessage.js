b3465f90db29622f2563a2a9df7328474d9d7f29
박겸영 <yong_drg@naver.com>
2022-11-08 22:27:32 +0900

    feat: 스트라이크, 볼, 낫싱 출력값 반환하는 함수
    - 스트라이크 개수와 볼 개수가 모두 0일 경우 '낫싱' 반환
    - n이 1 이상일 때 볼 n개, 스트라이크 n개일 경우 'n볼 n스트라이크' 반환
    - 스트라이크 n개, 볼 0개일 경우 'n스트라이크' 반환
    - 볼 n개, 스트라이크 0개일 경우 'n볼' 반환

--- src/utils/getBaseballResultMessage.js
+++ src/utils/getBaseballResultMessage.js
@@ -1,3 +1,5 @@
+const Message = require('../assets/Message');
+
 const getStrikeAndBallCount = (target, input) => {
   let [strikeCount, ballCount] = [0, 0];
 
@@ -14,3 +16,19 @@ const getStrikeAndBallCount = (target, input) => {
 
   return [strikeCount, ballCount];
 };
+
+const getBaseballResultMessage = (target, input) => {
+  const [strikeCount, ballCount] = getStrikeAndBallCount(target, input);
+  const { BALL, STRIKE, NOTHING } = Message;
+
+  const isNothing = !strikeCount && !ballCount;
+  if (isNothing) return NOTHING;
+
+  const [ballMessage, strikeMessage] = [`${ballCount}${BALL}`, `${strikeCount}${STRIKE}`];
+
+  if (ballCount && strikeCount) return `${ballMessage} ${strikeMessage}`;
+  if (ballCount) return ballMessage;
+  if (strikeCount) return strikeMessage;
+};
+
+module.exports = { getBaseballResultMessage };
