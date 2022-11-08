46aad71ae383aec33c6ac553ecdaa7f3bd62ac7d
Y2IN <dpdls0228@daum.net>
2022-11-08 22:09:43 +0900

    refactor : 가독성 위해 false 추가

--- src/baseball/computerNumbers.js
+++ src/baseball/computerNumbers.js
@@ -5,7 +5,7 @@ class ComputerNumbers {
     const randomNumbers = [];
     while (randomNumbers.length < 3) {
       const number = Random.pickNumberInRange(1, 9);
-      if (!randomNumbers.includes(number)) {
+      if (randomNumbers.includes(number) === false) {
         randomNumbers.push(number);
       }
     }
