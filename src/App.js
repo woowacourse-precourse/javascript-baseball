7750db62b00f96acff039f250d509df1b7f12c2c
osdoonhyun <skw1996@naver.com>
2022-11-08 20:41:18 +0900

    Feat: App에서 재시작/ 종료하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,60 +1,70 @@
 const MissionUtils = require("@woowacourse/mission-utils");
-const Game = require("./Game");
+const { MESSAGE, NUMBER } = require("./Game");
 const Computer = require("./Computer");
 const User = require("./User");
+const Referee = require("./Referee");
 
 class App {
   constructor() {
     this.computer = new Computer();
     this.user = new User();
+    this.referee = new Referee();
+  }
+
+  playBall() {
+    MissionUtils.Console.print(MESSAGE.START);
+    this.play();
+  }
+
+  play() {
+    const computerNumber = this.computer.makeRandomNumber();
+    this.start(computerNumber);
   }
 
   start(computerNumber) {
-    //사용자 숫자 입력
-    MissionUtils.Console.readLine(Game.MESSAGE.INPUT, (userInput) => {
+    console.log("정답", computerNumber);
+    MissionUtils.Console.readLine(MESSAGE.INPUT, (userInput) => {
       //입력이 형식에 맞는지 유효성 검사
       const userNumber = this.user.makeUserNumber(userInput);
       const userInputValidation = this.user.validateInput(userNumber);
-      if (userInputValidation === Game.NUMBER.FAIL) {
-        throw new Error(Game.MESSAGE.ERROR);
+
+      if (userInputValidation === false) {
+        throw new Error(MESSAGE.ERROR);
       }
 
       //사용자가 입력한 숫자에 대한 결과 출력
-      MissionUtils.Console.print(Game.ballCount(userNumber, computerNumber));
+      MissionUtils.Console.print(
+        this.referee.ballCount(userNumber, computerNumber)
+      );
 
       //컴퓨터가 선택한 숫자 3개 모두 맞출 시 게임 종료 문구 출력 후 게임 종료
-      if (Game.ballCount(userNumber, computerNumber) === Game.MESSAGE.OUT) {
-        return this.end();
-      }
-      if (Game.ballCount(userNumber, computerNumber) !== Game.MESSAGE.OUT) {
-        return this.start();
-      }
-    });
-
-    //사용자 재시작/ 종료 선택
-  }
-
-  end() {
-    MissionUtils.Console.print(Game.MESSAGE.END);
-    MissionUtils.Console.readLine(Game.MESSAGE.SELECT, (userInput) => {
-      if (userInput === Game.NUMBER.RESTART) return this.restart();
-      if (userInput === Game.NUMBER.EXIT) return this.exit();
+      if (this.referee.ballCount(userNumber, computerNumber) === MESSAGE.OUT)
+        this.end();
+      if (this.referee.ballCount(userNumber, computerNumber) !== MESSAGE.OUT)
+        this.start(computerNumber);
     });
   }
 
   restart() {
-    this.playBall();
+    this.play();
   }
 
   exit() {
     MissionUtils.Console.close();
   }
+  //사용자 재시작/ 종료 선택
+  end() {
+    MissionUtils.Console.print(MESSAGE.END);
+    MissionUtils.Console.readLine(MESSAGE.SELECT, (userInput) => {
+      const userNumber = Number(userInput);
 
-  playBall() {
-    MissionUtils.Console.print(Game.MESSAGE.START);
-    const computerNumber = this.computer.makeRandomNumber();
-    this.start(computerNumber);
+      if (userNumber === NUMBER.RESTART) this.restart();
+      if (userNumber === NUMBER.EXIT) this.exit();
+    });
   }
+  // throwError(errorMessage) {
+  // throw new Error(errorMessage);
+  // }
 }
 
 const numberbaseball = new App();
