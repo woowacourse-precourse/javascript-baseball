0f687cb1c58ae0172cdadc3ce80389456b0e2fc8
soohyun_kim5 <93cherry@naver.com>
2022-11-08 20:16:51 +0900

    test: 예외처리 1 길이 테스트

---
+++ __tests__/AdditionalTest.js
@@ -0,0 +1,40 @@
+const App = require("../src/App");
+const MissionUtils = require("@woowacourse/mission-utils");
+
+const mockQuestions = (answers) => {
+    MissionUtils.Console.readLine = jest.fn();
+    answers.reduce((acc, input) => {
+        return acc.mockImplementationOnce((question, callback) => {
+            callback(input);
+        });
+    }, MissionUtils.Console.readLine);
+};
+
+const mockRandoms = (numbers) => {
+    MissionUtils.Random.pickNumberInRange = jest.fn();
+    numbers.reduce((acc, number) => {
+        return acc.mockReturnValueOnce(number);
+    }, MissionUtils.Random.pickNumberInRange);
+};
+
+const getLogSpy = () => {
+    const logSpy = jest.spyOn(MissionUtils.Console, "print");
+    logSpy.mockClear();
+    return logSpy;
+};
+
+describe("숫자 야구 게임", () => {
+        test("예외처리 1 길이", () => {
+        const randoms = [1, 3, 5];
+        const answers = ["1"];
+
+        mockRandoms(randoms);
+        mockQuestions(answers);
+
+        expect(() => {
+            const app = new App();
+            app.play();
+        }).toThrow();
+    });
+
+});
