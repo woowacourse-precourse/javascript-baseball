e17993db7f59416a4a92017283b9cbb4d2b5b5ce
1wooseok <74036731+1wooseok@users.noreply.github.com>
2022-11-08 21:29:52 +0900

    feat: 생성자 추가 (시작 문구 출력)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 const { Console, Random } = require('@woowacourse/mission-utils');
 
 class App {
+  constructor() {
+    Console.print('숫자 야구 게임을 시작합니다.');
+  }
+
   play() { }
 
   createComputerNumber() {
