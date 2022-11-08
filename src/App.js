6ccf2edb0268f4ace37472bf17d08bd0b2d87398
Bori-github <qhflrnfl4324@gmail.com>
2022-11-08 23:05:39 +0900

    feat: get 3 random numbers

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 const MissionUtils = require('@woowacourse/mission-utils');
 
 class App {
+  getRandomNumbers() {
+    return MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3).join('');
+  }
+
   play() {
     MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+    const computer = this.getRandomNumbers();
   }
 }
 
