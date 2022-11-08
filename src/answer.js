944c036d0c5589a7d9f94f01f1b157d46a71cfc7
seoyeon <wjstjdus96@naver.com>
2022-11-08 13:51:15 +0900

    fix: 사용자 입력값 중복 검사 추가

--- src/answer.js
+++ src/answer.js
@@ -13,6 +13,9 @@ exports.inputAnswer = function inputAnswer() {
 
 exports.isRightAnswer = function isRightAnswer(answer) {
   let answerArr = [...answer].map((item) => Number(item));
+  let answerSet = new Set(answerArr);
 
   if (answerArr.length != 3) throw "잘못된 입력입니다";
+  if ([...answerSet].length != answerArr.length) throw "중복된 숫자가 있습니다";
+  if (answerArr.includes(0)) throw "1-9까지의 숫자만 입력 가능합니다";
 };
