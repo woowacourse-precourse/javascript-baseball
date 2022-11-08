f9a5ac6e0b2a0c118dc57b244179118c82ee2dc9
lineJeong <line.jeong32@gmail.com>
2022-11-08 23:00:55 +0900

    TEST: 사용자가 입력한 값의 유효성에 대한 유닛테스트 작성

--- __tests__/MyTest.js
+++ __tests__/MyTest.js
@@ -19,4 +19,30 @@ describe("구현한 기능 유닛 테스트", () => {
       expect(isUnique).toEqual(true);
     });
   });
+
+  describe("사용자가 입력한 값에 대한 유효성 테스트", () => {
+    test("입력값에 숫자가 아닌 값이 포함되면 예외 처리한다.", () => {
+      const result = () => app.checkUserInput("12b");
+
+      expect(result).toThrow();
+    });
+
+    test("입력값이 3자릿수가 아니면 예외 처리한다", () => {
+      const result = () => app.checkUserInput("1234");
+
+      expect(result).toThrow();
+    });
+
+    test("입력값에 중복된 숫자가 있으면 예외처리한다.", () => {
+      const result = () => app.checkUserInput("232");
+
+      expect(result).toThrow();
+    });
+
+    test("입력값에 0이 포함되면 예외처리한다.", () => {
+      const result = () => app.checkUserInput("230");
+
+      expect(result).toThrow();
+    });
+  });
 });
