a80ce68274931ad0ccec5d6c64fce3459e3ed077
loco9939 <kls9939@naver.com>
2022-11-08 14:03:32 +0900

    docs: test case

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -25,10 +25,16 @@ const getLogSpy = () => {
 
 describe.only("숫자 야구 게임", () => {
   test("게임 종료 후 재시작", () => {
-    const randoms = [1, 2, 3];
-    const answers = ["246", "123", "1"];
+    const randoms = [1, 3, 5, 5, 8, 9];
+    const answers = ["246", "135", "1", "597", "589", "2"];
     const logSpy = getLogSpy();
-    const messages = ["낫싱", "3스트라이크", "게임 종료"];
+    const messages = [
+      "낫싱",
+      "3스트라이크",
+      "1볼 1스트라이크",
+      "3스트라이크",
+      "게임 종료",
+    ];
 
     mockRandoms(randoms);
     mockQuestions(answers);
