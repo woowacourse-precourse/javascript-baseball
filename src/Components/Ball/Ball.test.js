dcd8668ca18f80be228737555867b0c32754422d
RyuJoonSik <tldtksahtxja@naver.com>
2022-11-08 16:08:49 +0900

    test: Ball.test.js에 compareTo가 1볼 1스트라이크를 반환하는지 테스트 추가

--- src/Components/Ball/Ball.test.js
+++ src/Components/Ball/Ball.test.js
@@ -3,9 +3,9 @@ const Ball = require("./Ball");
 describe("Ball", () => {
   test("getNumber로 숫자 값을 반환", () => {
     const BALL_NUMBER = 425;
-    const BALL = new Ball(ballNumber);
+    const BALL = new Ball(BALL_NUMBER);
 
-    expect(BALL.getNumber()).toBe(ballNumber);
+    expect(BALL.getNumber()).toBe(BALL_NUMBER);
   });
 
   test("나(공격수)의 Ball이 123이고 컴퓨터(수비수)의 Ball이 425이면 1스트라이크를 반환", () => {
@@ -13,5 +13,14 @@ describe("Ball", () => {
     const BALL_OF_DEFENDER = new Ball(425);
 
     expect(BALL_OF_DEFENDER.compareTo(BALL_OF_ATTACKER)).toBe("1스트라이크");
+  });
+
+  test("나(공격수)의 Ball이 456이고 컴퓨터(수비수)의 Ball이 425이면 1볼 1스트라이크를 반환", () => {
+    const BALL_OF_ATTACKER = new Ball(456);
+    const BALL_OF_DEFENDER = new Ball(425);
+
+    expect(BALL_OF_DEFENDER.compareTo(BALL_OF_ATTACKER)).toBe(
+      "1볼 1스트라이크"
+    );
   });
 });
