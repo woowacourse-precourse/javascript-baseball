5d105c90cbd64cf0491d4dba04f1be36721fe5d4
Sangjun-man <molung@icloud.com>
2022-11-08 17:43:24 +0900

    feat : 앱 전체 기능 구성하기
    숫자야구게임을 구현했습니다.
    - 주제에 따라 나누어진 기능을 구성하여 숫자야구게임을 구현했습니다.
    - App.js의 play, playing, end 메서드를 통해 숫자야구게임을 작동시킵니다.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,35 @@
 const { getInputValue, print, consoleClose } = require("./InputOutput");
 const { getThisTurnResult } = require("./Check");
+const {
+  convertThreeNumStringToArray,
+} = require("./utils/convertThreeNumStringToArray");
 const { getRandomThreeNumbers } = require("./Random");
 
 class App {
-  gameState;
-  randomNumber;
-  constructor() {
-    this.play();
-  }
+  #gameState;
+  #randomNumber;
   async play() {
-    this.randomNumber = getRandomThreeNumbers();
-    this.gameState = true;
+    this.#randomNumber = getRandomThreeNumbers();
+    this.#gameState = true;
     print("숫자 야구 게임을 시작합니다.");
-    while (this.gameState) {
-      this.playing();
+    while (this.#gameState) {
+      await this.playing();
     }
     return this.end();
   }
-  playing() {
-    print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-    this.gameState = false;
+  async playing() {
+    const userInput = await getInputValue("숫자를 입력해주세요 : ");
+    const userAnswer = getThisTurnResult({
+      input: convertThreeNumStringToArray(userInput),
+      random: this.#randomNumber,
+    });
+    print(userAnswer);
+    if (userAnswer.strike === 3) {
+      this.#gameState = false;
+    }
   }
   async end() {
+    print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     let gameRestartCheck = Number(
       await getInputValue(
         "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
@@ -42,4 +50,5 @@ class App {
 }
 
 const app = new App();
+app.play();
 module.exports = App;
