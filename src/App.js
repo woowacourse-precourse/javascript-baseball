d63fe84ce103e26be1bb087d9bd0c64fb582d3cd
lineJeong <line.jeong32@gmail.com>
2022-11-08 21:38:24 +0900

    FEAT: 사용자가 입력한 수에 대한 결과값을 도출하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 const { Random, Console } = require("@woowacourse/mission-utils");
 
 class App {
-  constructor() {
-    this.randomNums = this.createRandomNums();
-  }
-
   play() {
     Console.print("숫자 야구 게임을 시작합니다.");
-    this.getUserInput();
+    this.startGame();
   }
 
-  createRandomNums() {
+  startGame() {
+    this.initRandomNums();
+    this.getResultOfUserInput();
+  }
+
+  initRandomNums() {
+    this.randomNums = this.getRandomNums();
+  }
+
+  getRandomNums() {
     const randomNums = [];
     while (randomNums.length < 3) {
       let randomNum = Random.pickNumberInRange(1, 9);
@@ -19,9 +24,9 @@ class App {
     return randomNums;
   }
 
-  getUserInput() {
+  getResultOfUserInput() {
     Console.readLine("숫자를 입력해주세요 : ", (input) => {
-      this.checkUserInput(input);
+      this.compareNums(this.checkUserInput(input));
     });
   }
 
@@ -33,6 +38,42 @@ class App {
       throw Error("1~9 사이의 숫자로 구성된 3자릿수의 숫자를 입력해주세요.");
 
     return input.split("").map((it) => parseInt(it));
+  }
+
+  compareNums(userNums) {
+    const result = {
+      strike: 0,
+      ball: 0,
+    };
+
+    for (let i = 0; i < 3; i++) {
+      if (this.isStrike(this.randomNums, userNums[i], i)) {
+        result.strike += 1;
+      } else if (this.isBall(this.randomNums, userNums[i], i)) {
+        result.ball += 1;
+      }
+    }
+
+    const resultMessage = this.getResultMessage(result);
+    Console.print(resultMessage);
+  }
+
+  isStrike(answer, number, idx) {
+    return answer[idx] === number;
+  }
+
+  isBall(answer, number, idx) {
+    return answer[idx] !== number && answer.includes(number);
+  }
+
+  getResultMessage({ ball, strike }) {
+    let resultMessage = "낫싱";
+
+    if (ball > 0 && strike === 0) resultMessage = `${ball}볼`;
+    if (ball === 0 && strike > 0) resultMessage = `${strike}스트라이크`;
+    if (ball > 0 && strike > 0) resultMessage = `${ball}볼 ${strike}스트라이크`;
+
+    return resultMessage;
   }
 }
 
