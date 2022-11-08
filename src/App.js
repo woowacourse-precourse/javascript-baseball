874bfefce41c08b22546950261aef8038ebf6878
woo-jk <201702042@o.cnu.ac.kr>
2022-11-08 20:01:33 +0900

    fix: constants 폹더 의존 제거(예제 테스트 오류 원인 찾기 위해)

--- src/App.js
+++ src/App.js
@@ -3,22 +3,22 @@ const MissionUtils = require("@woowacourse/mission-utils");
 // const getStrikeAndBall = require("./StrikeAndBall");
 // const getStrikeAndBallText = require("./StrikeAndBallText");
 // const throwError = require("./ThrowError");
-const { NUMBER_LENGTH, END_INPUT, RESTART_INPUT, MIN_NUMBER, MAX_NUMBER } = require("./constants/constantValues");
-const {
-  START_MESSAGE,
-  INPUT_NUMBER_MESSAGE,
-  END_MESSAGE,
-  INPUT_RESTART_OR_END_MESSAGE,
-} = require("./constants/messages");
+// const { 3, "2", "1", 1, 9 } = require("./constants/constantValues");
+// const {
+//   숫자 야구 게임을 시작합니다.,
+//   "숫자를 입력해주세요 : ",
+//   END_MESSAGE,
+//   "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
+// } = require("./constants/messages");
 
 class App {
   constructor() {
-    MissionUtils.Console.print(START_MESSAGE);
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
     this.threeRandomNumbers = this.getThreeRandomNumbers();
   }
 
   play() {
-    MissionUtils.Console.readLine(INPUT_NUMBER_MESSAGE, (input) => {
+    MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (input) => {
       if (!this.validateInput(input)) {
         this.throwError();
       }
@@ -26,7 +26,7 @@ class App {
       const [strikeCount, ballCount] = this.getStrikeAndBall(this.threeRandomNumbers, input);
       MissionUtils.Console.print(this.getStrikeAndBallText(strikeCount, ballCount));
 
-      if (strikeCount === NUMBER_LENGTH) {
+      if (strikeCount === 3) {
         this.end();
         return;
       }
@@ -36,17 +36,17 @@ class App {
   }
 
   end() {
-    MissionUtils.Console.print(END_MESSAGE);
-    MissionUtils.Console.readLine(INPUT_RESTART_OR_END_MESSAGE, (input) => {
-      if (input !== RESTART_INPUT && input !== END_INPUT) {
+    MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    MissionUtils.Console.readLine("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.", (input) => {
+      if (input !== "1" && input !== "2") {
         this.throwError();
       }
-      if (input === RESTART_INPUT) {
+      if (input === "1") {
         this.threeRandomNumbers = this.getThreeRandomNumbers();
         this.play();
         return;
       }
-      if (input === END_INPUT) {
+      if (input === "2") {
         MissionUtils.Console.close();
         return;
       }
@@ -55,8 +55,8 @@ class App {
 
   getThreeRandomNumbers() {
     const threeRandomNumber = new Set();
-    while (threeRandomNumber.size < NUMBER_LENGTH) {
-      const newNumber = MissionUtils.Random.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
+    while (threeRandomNumber.size < 3) {
+      const newNumber = MissionUtils.Random.pickNumberInRange(1, 9);
       threeRandomNumber.add(newNumber);
     }
 
@@ -90,11 +90,11 @@ class App {
   }
 
   validateInputLength(input) {
-    return input.length !== NUMBER_LENGTH;
+    return input.length !== 3;
   }
 
   validateInputDuplication(input) {
-    return new Set(input.split("")).size !== NUMBER_LENGTH;
+    return new Set(input.split("")).size !== 3;
   }
 
   validateInputIsNaN(input) {
@@ -120,7 +120,7 @@ class App {
     return output.trim();
   }
   throwError() {
-    throw new Error(WRONG_INPUT_ERROR_MESSAGE);
+    throw new Error("WRONG_INPUT_ERROR");
   }
 }
 
