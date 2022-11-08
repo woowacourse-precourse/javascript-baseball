1b31fd1af625e1ba19b05000276c1f77078d7a31
lego37yoon <lego37yoon@outlook.com>
2022-11-08 19:12:32 +0900

    feat: add function to check input value
    Add function to check input value in game
    - Get user input value with checkPass() function, with value from computer
    - Check if user input is similar or same as computer's value with checkResult() function
    - Convert value to array because it is much easier to compare values

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
 const MISSION_UTILS = require('@woowacourse/mission-utils');
 
 class App {
-  async play() {
+  play() {
     MISSION_UTILS.Console.print('숫자 야구 게임을 시작합니다.');
     newGamePlay();
   }
 }
 
-async function newGamePlay() {
+function newGamePlay() {
   const COMPETITOR = [];
   
   while(COMPETITOR.length < 3) {
     COMPETITOR.push(MISSION_UTILS.Random.pickNumberInRange(1, 9));
   }
 
-  questionToContinueGame();
+  checkPass(COMPETITOR);
+
 }
 
-async function questionToContinueGame() {
+function questionToContinueGame() {
   
   MISSION_UTILS.Console.readLine('게임을 새로 시작하시려면 1, 종료하려면 2를 입력하세요.', (ans) => {
     if (Number(ans) === 1) {
-      console.log('Trying to run new game');
       newGamePlay();
     } else if (Number(ans) === 2) {
       MISSION_UTILS.Console.close();
@@ -29,6 +29,47 @@ async function questionToContinueGame() {
       throw new Error('잘못된 값을 입력하셨습니다. 애플리케이션을 종료합니다.');
     }
   });
+}
+
+function checkPass(COMPETITOR) {
+  MISSION_UTILS.Console.readLine('숫자를 입력해주세요 : \n', async function(answer) {
+    const answerArray = Array.from(String(answer), (num) => Number(num));
+    if (!checkResult(COMPETITOR, answerArray)) {
+      checkPass(COMPETITOR)
+    } else {
+      questionToContinueGame();
+    }
+  });
+}
+
+function checkResult(COMPETITOR, answer) {
+  let ball = 0;
+  let strike = 0;
+  
+  for (let i = 0; i < 3; i++) {
+    if (COMPETITOR[i] === answer[i]) {
+      strike++;
+    } else if (COMPETITOR.includes(answer[i])) {
+      ball++;
+    }
+  }
+
+  if (ball > 0 && strike > 0) {
+    MISSION_UTILS.Console.print(`${ball}볼 ${strike}스트라이크`);
+  } else if (ball > 0) {
+    MISSION_UTILS.Console.print(`${ball}볼`);
+  } else if (strike > 0) {
+    MISSION_UTILS.Console.print(`${strike}스트라이크`);
+  } else {
+    MISSION_UTILS.Console.print('낫싱');
+  }
+
+  if (strike === 3) {
+    MISSION_UTILS.Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
+    return true;
+  }
+
+  return false;
 }
 
 module.exports = App;
