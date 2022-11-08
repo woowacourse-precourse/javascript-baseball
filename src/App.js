19e4f9972c6341e2584894b31ac8e430863fa36d
DESKTOP-8B9G08E\Cho young jae <rnlwlswk@naver.com>
2022-11-08 23:29:15 +0900

    computer에 랜덤넘버받기

--- src/App.js
+++ src/App.js
@@ -2,9 +2,24 @@ const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
    play() {
-    
+    this.start();
+  }
+  start(){
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
   }
 
+  randomNumber(){
+    let computer = [];
+    for(let i = 0; i < 3; i++) {
+      const randomN = MissionUtils.pickNumberInRange(1, 9);
+      if (randomN[i] !== randomN[i-1]) {
+        computer.push(randomN[i]);
+      }
+    }
+  }
+
+
+  
 }
 
 
