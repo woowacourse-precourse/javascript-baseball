b444ed60f48e95421b87ee560ec9b7a7ad304d18
Monte <monte6198@gmail.com>
2022-11-08 14:06:08 +0900

    feat: 예외사항 테스트 케이스 추가

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -28,13 +28,43 @@ describe("숫자 야구 게임", () => {
     const randoms = [1, 3, 5, 5, 8, 9];
     const answers = ["246", "135", "1", "597", "589", "2"];
     const logSpy = getLogSpy();
-    const messages = [
-      "낫싱",
-      "3스트라이크",
-      "1볼 1스트라이크",
-      "3스트라이크",
-      "게임 종료",
-    ];
+    const messages = ["낫싱", "3스트라이크", "1볼 1스트라이크", "3스트라이크", "게임 종료"];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    const app = new App();
+    app.play();
+
+    messages.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
+  test("(NEW) 재시작 없이 게임 종료", () => {
+    // 새로운 테스트 케이스
+    const randoms = [1, 3, 5, 5, 8, 9];
+    const answers = ["246", "137", "135", "2"];
+    const logSpy = getLogSpy();
+    const messages = ["낫싱", "2스트라이크", "3스트라이크", "게임 종료"];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    const app = new App();
+    app.play();
+
+    messages.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
+  test("(NEW) 게임 종료 후 재시작 여러 번 + 기타 입력 추가", () => {
+    // 새로운 테스트 케이스
+    const randoms = [1, 3, 5, 5, 8, 9, 9, 8, 7];
+    const answers = ["246", "137", "135", "1", "597", "589", "1", "987", "2"];
+    const logSpy = getLogSpy();
+    const messages = ["낫싱", "2스트라이크", "3스트라이크", "1볼 1스트라이크", "3스트라이크", "3스트라이크", "게임 종료"];
 
     mockRandoms(randoms);
     mockQuestions(answers);
@@ -48,8 +78,51 @@ describe("숫자 야구 게임", () => {
   });
 
   test("예외 테스트", () => {
+    // 사용자 입력이 3자리 수가 아닌 경우
     const randoms = [1, 3, 5];
     const answers = ["1234"];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow();
+  });
+
+  test("(NEW) 예외 테스트 - 숫자 이외의 입력", () => {
+    // 사용자 입력에 숫자가 아닌 것이 포함된 경우
+    const randoms = [1, 3, 5];
+    const answers = ["1?4"];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow();
+  });
+
+  test("(NEW) 예외 테스트 - 숫자 이외의 입력", () => {
+    // 사용자 입력에 숫자가 아닌 것이 포함된 경우
+    const randoms = [1, 3, 5];
+    const answers = ["1a4"];
+
+    mockRandoms(randoms);
+    mockQuestions(answers);
+
+    expect(() => {
+      const app = new App();
+      app.play();
+    }).toThrow();
+  });
+
+  test("(NEW) 예외 테스트 - 0을 포함한 입력", () => {
+    // 사용자 입력에 0이 포함된 경우
+    const randoms = [1, 3, 5];
+    const answers = ["104"];
 
     mockRandoms(randoms);
     mockQuestions(answers);
