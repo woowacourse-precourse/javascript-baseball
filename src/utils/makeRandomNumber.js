b4d78ac1fbee3e70cf87301c18803a8ab42cc4f6
dladncks1217 <dlaxodud1217@gmail.com>
2022-11-09 11:14:19 +0900

    fix: 2, 3번째 값에서 0이 나올 수 있는 경우 제거

--- src/utils/makeRandomNumber.js
+++ src/utils/makeRandomNumber.js
@@ -4,15 +4,14 @@ const { ANSWER_LENGTH } = require('../constants/constants');
 const makeRandomNumber = () => {
   const answer = [];
 
-  answer.push(Random.pickNumberInRange(1, 9)); // 첫 자릿수에 0이 나오지 않도록
   while (answer.length < ANSWER_LENGTH) {
-    const tempNumber = Random.pickNumberInRange(0, 9);
-    if (answer.indexOf(tempNumber) === -1) {
+    const tempNumber = Random.pickNumberInRange(1, 9);
+    if (!answer.includes(tempNumber)) {
       answer.push(tempNumber);
     }
   }
 
-  return answer.join(''); // 3자리 수를 만들어 number형태로 리턴
+  return answer.join(''); // 3자리 수를 만들어 string 형태로 리턴
 };
 
 module.exports = makeRandomNumber;
