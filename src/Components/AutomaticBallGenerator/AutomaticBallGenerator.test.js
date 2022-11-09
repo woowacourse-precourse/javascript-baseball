f9f7d7d116ce68b8eff122324da5b3a6934c162d
RyuJoonSik <tldtksahtxja@naver.com>
2022-11-09 13:19:55 +0900

    feat: 비동기 예약어 제거

--- src/Components/AutomaticBallGenerator/AutomaticBallGenerator.test.js
+++ src/Components/AutomaticBallGenerator/AutomaticBallGenerator.test.js
@@ -2,18 +2,18 @@ const AutomaticBallGenerator = require("./AutomaticBallGenerator");
 const { mockRandoms } = require("../../mockFunction");
 
 describe("AutomaticBallGenerator", () => {
-  test("execute로 숫자를 생성", async () => {
+  test("execute로 숫자를 생성", () => {
     mockRandoms([1, 2, 3]);
     const BALL_GENERATOR = new AutomaticBallGenerator();
-    const BALL = await BALL_GENERATOR.execute();
+    const BALL = BALL_GENERATOR.execute();
 
     expect(BALL.getNumber()).toBe(123);
   });
 
-  test("중복이 있으면 다시 생성", async () => {
+  test("중복이 있으면 다시 생성", () => {
     mockRandoms([1, 1, 1, 1, 2, 3]);
     const BALL_GENERATOR = new AutomaticBallGenerator();
-    const BALL = await BALL_GENERATOR.execute();
+    const BALL = BALL_GENERATOR.execute();
 
     expect(BALL.getNumber()).toBe(123);
   });
