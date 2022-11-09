ff0abe272d98a141c9c933b16255351cd57ba13d
RyuJoonSik <tldtksahtxja@naver.com>
2022-11-09 13:20:56 +0900

    test: 게임 1회 실행 테스트로 변경

--- src/Components/BaseballGame/BaseballGame.test.js
+++ src/Components/BaseballGame/BaseballGame.test.js
@@ -1,26 +1,33 @@
-const BaseballGame = require("./BaseballGame");
-
 const { getLogSpy, mockRandoms, mockQuestions } = require("../../mockFunction");
+const BaseballGame = require("./BaseballGame");
 const Attacker = require("../Attacker/Attacker");
 const AutomaticBallGenerator = require("../AutomaticBallGenerator/AutomaticBallGenerator");
 const Defender = require("../Defender/Defender");
 const ManualBallGenerator = require("../ManualBallGenerator/ManualBallGenerator");
 
 describe("BaseballGame", () => {
-  test("게임 종료 후 재시작", async () => {
-    const randoms = [
-      [1, 3, 5],
-      [5, 8, 9],
-    ];
-    const answers = ["246", "135", "1", "597", "589", "2"];
-    const logSpy = getLogSpy();
-    const messages = [
-      "낫싱",
-      "3스트라이크",
-      "1볼 1스트라이크",
-      "3스트라이크",
-      "게임 종료",
-    ];
+  test("게임 시작", async () => {
+    const RANDOM_ARRAY = [1, 3, 5];
+    const ANSWER_ARRAY = ["246", "135"];
+    const LOG_SPY = getLogSpy();
+    const MESSAGE_ARRAY = ["낫싱", "3스트라이크"];
+
+    mockRandoms(RANDOM_ARRAY);
+    mockQuestions(ANSWER_ARRAY);
+
+    const ATTACKER = new Attacker(new ManualBallGenerator());
+    const DEFENDER = new Defender(new AutomaticBallGenerator());
+    const BASEBALL_GAME = new BaseballGame(ATTACKER, DEFENDER);
+    BASEBALL_GAME.start();
+
+    MESSAGE_ARRAY.forEach((output) => {
+      expect(LOG_SPY).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
+  test("예외 테스트", async () => {
+    const randoms = [1, 3, 5];
+    const answers = ["1234"];
 
     mockRandoms(randoms);
     mockQuestions(answers);
@@ -28,10 +35,7 @@ describe("BaseballGame", () => {
     const ATTACKER = new Attacker(new ManualBallGenerator());
     const DEFENDER = new Defender(new AutomaticBallGenerator());
     const BASEBALL_GAME = new BaseballGame(ATTACKER, DEFENDER);
-    await BASEBALL_GAME.start();
 
-    messages.forEach((output) => {
-      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
-    });
+    expect(BASEBALL_GAME.start).toThrow();
   });
 });
