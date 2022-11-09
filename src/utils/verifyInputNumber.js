9bd4f1279bf567ed917b4823a7f1880cec5ae281
dladncks1217 <dlaxodud1217@gmail.com>
2022-11-09 11:29:15 +0900

    refactor: 숫자 중복 확인 방식 리팩토링(set 도입)

--- src/utils/verifyInputNumber.js
+++ src/utils/verifyInputNumber.js
@@ -3,8 +3,7 @@ const { ANSWER_LENGTH } = require('../constants/constants');
 const verifyInputNumber = (input) => {
   if (isNaN(+input)) return false; // 숫자 아닌 수 입력 시
   if (input.length !== ANSWER_LENGTH) return false; // 입력값이 3자리수가 아닌 경우
-  if (input[0] === input[1] || input[1] === input[2] || input[0] === input[2])
-    return false; // 중복된 숫자가 있을 경우
+  if (new Set(input).size !== input.length) return false; // 중복된 숫자가 있을 경우
 
   return true; // 위 3가지 조건 해당사항 없을 시 검증완료
 };
