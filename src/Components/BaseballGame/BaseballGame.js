5e3ee1cdc72d6c270ead1d49765db0631a1dc167
RyuJoonSik <tldtksahtxja@naver.com>
2022-11-09 16:10:42 +0900

    feat: BaseballGame.js에 select, afterGameEndCallback 메서드 추가
    3스트라이크이면 선택지를 제안할 수 있도록 메서드를 추가했습니다.

--- src/Components/BaseballGame/BaseballGame.js
+++ src/Components/BaseballGame/BaseballGame.js
@@ -1,51 +1,35 @@
 const { Console } = require("@woowacourse/mission-utils");
-const Attacker = require("../Attacker/Attacker");
-const AutomaticBallGenerator = require("../AutomaticBallGenerator/AutomaticBallGenerator");
-const Defender = require("../Defender/Defender");
-const ManualBallGenerator = require("../ManualBallGenerator/ManualBallGenerator");
 
 class BaseballGame {
   constructor(attacker, defender) {
     this.attacker = attacker;
     this.defender = defender;
+    this.isInstalled = true;
   }
 
   start() {
+    if (this.isInstalled) Console.print("숫자 야구 게임을 시작합니다.");
+    this.isInstalled = false;
+
     const { attacker, defender } = this;
-    attacker.throwTo(defender);
-    // let select = null;
-
-    // do {
-    // await this.defender.ready();
-    // await this.attacker.throwTo(this.defender);
-
-    // return this.ask();
-    // } while (select === "1");
-
-    // if (select === "2") {
-    //   Console.print("게임 종료");
-    //   return;
-    // }
+    defender.ready();
+    attacker.throwTo(defender, this.afterGameEndCallback.bind(this));
   }
 
-  // async ask() {
-  //   return await new Promise((resolve) => {
-  //     const callback = (input) => {
-  //       Console.close();
-  //       resolve(input);
-  //     };
+  afterGameEndCallback() {
+    Console.readLine(
+      "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n",
+      this.select.bind(this)
+    );
+  }
 
-  //     Console.readLine(
-  //       "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n",
-  //       callback
-  //     );
-  //   });
-  // }
+  select(input) {
+    if (input === "1") return this.start();
+
+    if (input === "2") {
+      Console.print("게임 종료");
+    }
+  }
 }
 
-const ATTACKER = new Attacker(new ManualBallGenerator());
-const DEFENDER = new Defender(new AutomaticBallGenerator());
-const BASEBALL_GAME = new BaseballGame(ATTACKER, DEFENDER);
-
-BASEBALL_GAME.start();
 module.exports = BaseballGame;
