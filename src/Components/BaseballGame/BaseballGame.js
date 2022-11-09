9005f613d9a424ad69d8488fa099cf0363862079
RyuJoonSik <tldtksahtxja@naver.com>
2022-11-09 13:20:15 +0900

    feat: 비동기 로직 제거

--- src/Components/BaseballGame/BaseballGame.js
+++ src/Components/BaseballGame/BaseballGame.js
@@ -1,4 +1,8 @@
 const { Console } = require("@woowacourse/mission-utils");
+const Attacker = require("../Attacker/Attacker");
+const AutomaticBallGenerator = require("../AutomaticBallGenerator/AutomaticBallGenerator");
+const Defender = require("../Defender/Defender");
+const ManualBallGenerator = require("../ManualBallGenerator/ManualBallGenerator");
 
 class BaseballGame {
   constructor(attacker, defender) {
@@ -6,29 +10,42 @@ class BaseballGame {
     this.defender = defender;
   }
 
-  async start() {
-    await this.defender.ready();
-    await this.attacker.throwTo(this.defender);
+  start() {
+    const { attacker, defender } = this;
+    attacker.throwTo(defender);
+    // let select = null;
 
-    const INPUT = await new Promise((resolve) => {
-      const callback = (input) => {
-        Console.close();
-        resolve(input);
-      };
+    // do {
+    // await this.defender.ready();
+    // await this.attacker.throwTo(this.defender);
 
-      Console.readLine(
-        `
-        3개의 숫자를 모두 맞히셨습니다! 게임 종료
-        게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.
-      `,
-        callback
-      );
-    });
+    // return this.ask();
+    // } while (select === "1");
 
-    if (INPUT === "1") await this.start();
-
-    return;
+    // if (select === "2") {
+    //   Console.print("게임 종료");
+    //   return;
+    // }
   }
+
+  // async ask() {
+  //   return await new Promise((resolve) => {
+  //     const callback = (input) => {
+  //       Console.close();
+  //       resolve(input);
+  //     };
+
+  //     Console.readLine(
+  //       "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n",
+  //       callback
+  //     );
+  //   });
+  // }
 }
 
+const ATTACKER = new Attacker(new ManualBallGenerator());
+const DEFENDER = new Defender(new AutomaticBallGenerator());
+const BASEBALL_GAME = new BaseballGame(ATTACKER, DEFENDER);
+
+BASEBALL_GAME.start();
 module.exports = BaseballGame;
