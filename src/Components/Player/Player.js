a9204a86bbb94c132a77caa9c449f01d0a80b0fd
RyuJoonSik <tldtksahtxja@naver.com>
2022-11-09 13:22:31 +0900

    chore: 불필요한 파일 삭제

--- src/Components/Player/Player.js
+++
@@ -1,5 +0,0 @@
-class Player {
-  enter() {}
-}
-
-module.exports = Player;
--- src/Components/Player/Player.test.js
+++
@@ -1,9 +0,0 @@
-const Player = require("./Player");
-
-describe("Player", () => {
-  test("enter 메서드를 가진다.", () => {
-    const player = new Player();
-
-    expect(player).toHaveProperty("enter");
-  });
-});
