91cb12ff94b94f61f637ec1a8be0112a95160aa8
kangju2000 <kangju2000@naver.com>
2022-11-10 12:42:09 +0900

    refactor: isAvailableValue util 함수 수정

--- src/utils/isAvailableValue.js
+++ src/utils/isAvailableValue.js
@@ -1,8 +1,10 @@
 function isAvailableValue(value) {
   const stringValue = value + '';
+
+  if (stringValue.length !== 3) return false;
   const uniqueValue = [...new Set(stringValue)].join('');
 
-  return stringValue.length === 3 && /^[1-9]{3}$/.test(uniqueValue);
+  return /^[1-9]{3}$/.test(uniqueValue);
 }
 
 module.exports = isAvailableValue;
