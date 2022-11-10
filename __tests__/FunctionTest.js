7bd728a8bdebed13cd335eed6e368e0739ffe4b3
jjjjjeongwon <jjjjjwon21@gmail.com>
2022-11-10 23:52:57 +0900

    style: 에어비앤비 컨벤션에 맞춰 코드 스타일 수정

--- __tests__/FunctionTest.js
+++ __tests__/FunctionTest.js
@@ -1,5 +1,5 @@
-const App = require("../src/App");
-const MissionUtils = require("@woowacourse/mission-utils");
+const App = require('../src/App');
+const MissionUtils = require('@woowacourse/mission-utils');
 
 const mockQuestions = (answers) => {
   MissionUtils.Console.readLine = jest.fn();
@@ -18,24 +18,24 @@ const mockRandoms = (numbers) => {
 };
 
 const getLogSpy = () => {
-  const logSpy = jest.spyOn(MissionUtils.Console, "print");
+  const logSpy = jest.spyOn(MissionUtils.Console, 'print');
   logSpy.mockClear();
   return logSpy;
 };
 
-describe("기능 테스트", () => {
-  test("[기능 1] 게임 시작 문구 출력", () => {
+describe('기능 테스트', () => {
+  test('[기능 1] 게임 시작 문구 출력', () => {
     const logSpy = getLogSpy();
 
     const app = new App();
     app.printStartGame();
 
     expect(logSpy).toHaveBeenCalledWith(
-      expect.stringContaining("숫자 야구 게임을 시작합니다.")
+      expect.stringContaining('숫자 야구 게임을 시작합니다.'),
     );
   });
-  
-  test("[기능 2] 랜덤 수 생성 및 저장", () => {
+
+  test('[기능 2] 랜덤 수 생성 및 저장', () => {
     const randoms = [1, 5, 5, 5, 8, 9];
 
     const app = new App();
@@ -45,8 +45,8 @@ describe("기능 테스트", () => {
     expect(app.createRandomNumber()).toStrictEqual([1, 5, 8]);
   });
 
-  test("[기능 4] 게임 예상 숫자 입력 받은 후 예외사항 판단", () => {
-    const answers = ["509"];
+  test('[기능 4] 게임 예상 숫자 입력 받은 후 예외사항 판단', () => {
+    const answers = ['509'];
 
     mockQuestions(answers);
 
@@ -56,19 +56,19 @@ describe("기능 테스트", () => {
     }).toThrow();
   });
 
-  test("[기능 5] 입력 값이 랜덤수와 일치하는지 확인", () => {
+  test('[기능 5] 입력 값이 랜덤수와 일치하는지 확인', () => {
     const randoms = [1, 3, 2];
-    const answer = "132";
+    const answer = '132';
 
     const app = new App();
     expect(app.isCorrectNumber(randoms, answer)).toBeTruthy();
   });
 
-  test("[기능 6] 입력값에 따른 결과 값 출력", () => {
+  test('[기능 6] 입력값에 따른 결과 값 출력', () => {
     const randoms = [1, 3, 2];
-    const answers = ["134", "123"];
+    const answers = ['134', '123'];
     const logSpy = getLogSpy();
-    const messages = ["2스트라이크", "2볼 1스트라이크"];
+    const messages = ['2스트라이크', '2볼 1스트라이크'];
 
     mockRandoms(randoms);
     mockQuestions(answers);
@@ -81,25 +81,25 @@ describe("기능 테스트", () => {
     });
   });
 
-  test("[기능 7] 종료, 재시작 선택 입력받은 후 예외사항 판단", () => {
-    const input = "0";
+  test('[기능 7] 종료, 재시작 선택 입력받은 후 예외사항 판단', () => {
+    const input = '0';
 
     const app = new App();
 
     expect(() => {
       app.checkInputRestartExit(input);
-    }).toThrow("잘못된 값 입력");
+    }).toThrow('잘못입력함. 종료');
   });
 
-  test("[기능 8] 1 or 2 입력 시 결과에 따라 돌아가거나, 종료", () => {
+  test('[기능 8] 1 or 2 입력 시 결과에 따라 돌아가거나, 종료', () => {
     const randoms = [1, 3, 2, 5, 6, 7];
-    const answers = ["132", "1", "567", "2"];
+    const answers = ['132', '1', '567', '2'];
     const logSpy = getLogSpy();
     const messages = [
-      "3스트라이크",
-      "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
-      "3스트라이크",
-      "게임 종료",
+      '3스트라이크',
+      '3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.',
+      '3스트라이크',
+      '게임 종료',
     ];
 
     mockRandoms(randoms);
--- src/App.js
+++ src/App.js
@@ -3,41 +3,39 @@ const { Console, Random } = MissionUtils;
 const NUMBER = require('../constants/gameSetting');
 const MESSAGE = require('../constants/gameMessages');
 
-
 class App {
-  play() {
+  play () {
     this.printStartGame();
     this.handleInputAnswer(this.createRandomNumber());
   }
 
-  printStartGame() {
+  printStartGame () {
     Console.print(MESSAGE.GAME.START);
   }
 
-  createRandomNumber() {
+  createRandomNumber () {
     const randomNumberList = [];
 
     while (randomNumberList.length < NUMBER.RANDOM_LENGTH) {
       const collectRandomNumber = Random.pickNumberInRange(
         NUMBER.FIRST,
-        NUMBER.LAST
+        NUMBER.LAST,
       );
-      !randomNumberList.includes(collectRandomNumber) &&
+      if (!randomNumberList.includes(collectRandomNumber)) {
         randomNumberList.push(collectRandomNumber);
+      }
     }
 
     return randomNumberList;
   }
 
-  handleInputAnswer(randomNumber) {
+  handleInputAnswer (randomNumber) {
     Console.readLine(MESSAGE.GAME.INPUT, (answer) => {
       this.isRandomInputErrorCase(answer);
 
       if (this.isCorrectNumber(randomNumber, answer)) {
         Console.print(MESSAGE.GAME.SUCCESS);
-        Console.print(
-          MESSAGE.GAME.FINISH_OPTION
-        );
+        Console.print(MESSAGE.GAME.FINISH_OPTION);
         Console.readLine('', (input) => {
           if (this.checkInputRestartExit(input)) {
             this.handleInputAnswer(this.createRandomNumber());
@@ -53,7 +51,7 @@ class App {
     });
   }
 
-  isRandomInputErrorCase(answer) {
+  isRandomInputErrorCase (answer) {
     const exceptionInput = answer;
 
     const inputList = exceptionInput?.split('');
@@ -63,20 +61,20 @@ class App {
     if (
       exceptionInput?.split('').map(Number)
         .includes(NUMBER.EXCEPT)
-      || exceptionInput?.split('').includes('-')
-      || isNaN(exceptionInput)
-      || exceptionInput?.toString().length !== NUMBER.RANDOM_LENGTH
-      || isSame
+        || exceptionInput?.split('').includes('-')
+        || isNaN(exceptionInput)
+        || exceptionInput?.toString().length !== NUMBER.RANDOM_LENGTH
+        || isSame
     ) {
       throw new Error(MESSAGE.GAME.ERROR);
     }
   }
 
-  isCorrectNumber(randomNumber, answer) {
+  isCorrectNumber (randomNumber, answer) {
     return randomNumber?.join('') === answer;
   }
 
-  resultBaseballRule(randomNumber, answer) {
+  resultBaseballRule (randomNumber, answer) {
     const random = randomNumber;
     const input = answer.split('').map(Number);
 
@@ -84,17 +82,15 @@ class App {
     let ballCount = 0;
     for (let idx = 0; idx < random?.length; idx++) {
       if (random.includes(input[idx]) && random[idx] === input[idx]) strikeCount += 1;
-
       if (random.includes(input[idx]) && random[idx] !== input[idx]) ballCount += 1;
     }
 
-    const resultBaseball =
-      (ballCount ? `${ballCount}${MESSAGE.GAME.BALL} ` : '') +
-      (strikeCount ? `${strikeCount}${MESSAGE.GAME.STRIKE}` : '');
+    const resultBaseball = (ballCount ? `${ballCount}${MESSAGE.GAME.BALL} ` : '')
+    + (strikeCount ? `${strikeCount}${MESSAGE.GAME.STRIKE}` : '');
     return resultBaseball ? resultBaseball : MESSAGE.GAME.NOTHING;
   }
 
-  checkInputRestartExit(input) {
+  checkInputRestartExit (input) {
     if (input === NUMBER.RESTART) return true;
     if (input === NUMBER.EXIT) return false;
     throw new Error(MESSAGE.GAME.ERROR);
