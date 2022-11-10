3871b0074a11dc5d606685fdd6fa3e5051491063
jjjjjeongwon <jjjjjwon21@gmail.com>
2022-11-11 00:01:35 +0900

    refactor: [기능 4] 입력 숫자가 3자리인지 확인하는 코드를 isThreeNumber로 묶어서 수정

--- src/App.js
+++ src/App.js
@@ -57,13 +57,14 @@ class App {
     const inputList = exceptionInput?.split('');
     const setCollection = new Set(inputList);
     const isSame = setCollection.size !== inputList?.length;
+    const isThreeNumber = exceptionInput?.toString().length !== NUMBER.RANDOM_LENGTH;
 
     if (
       exceptionInput?.split('').map(Number)
         .includes(NUMBER.EXCEPT)
         || exceptionInput?.split('').includes('-')
         || isNaN(exceptionInput)
-        || exceptionInput?.toString().length !== NUMBER.RANDOM_LENGTH
+        || isThreeNumber
         || isSame
     ) {
       throw new Error(MESSAGE.GAME.ERROR);
