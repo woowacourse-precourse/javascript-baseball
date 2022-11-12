ce73eeebeb87f09a8960d2a6dfb91f2f76e28f5e
nerdchanii <nerdchanii@gmail.com>
2022-11-13 01:31:48 +0900

    refactor: 멤버변수로 등록된 스태틱 클래스 삭제

--- src/App.js
+++ src/App.js
@@ -2,12 +2,11 @@ const Game = require('./Game');
 const Io = require('./Io');
 class App {
   constructor () {
-    this.Game = new Game();
-    this.Io = Io;
+    this.game = new Game();
   }
 
   play () {
-    this.Io.output('숫자 야구 게임을 시작합니다.');
+    Io.output('숫자 야구 게임을 시작합니다.');
     this.Game.playCommand();
   }
 }
