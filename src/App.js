ccd47af24fedaab69ae58f6c457d4788c5458aa3
An Sol <apdlvmf486@gmail.com>
2022-11-12 10:16:55 +0900

    refactor: 메세지를 출력하는 기능을 view로 분리
    view.js를 생성하여 mvc 패턴의 view를 담당하도록 리팩토링

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-const GameStartOrRestart = require('./gameSource/controller');
+const GameStartOrRestart = require("./gameSource/controller");
 const MissionUtils = require("@woowacourse/mission-utils");
 
 class App {
   play() {
-    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
     GameStartOrRestart.launchNewGame();
   }
 }
+const app = new App();
+app.play();
 
 module.exports = App;
