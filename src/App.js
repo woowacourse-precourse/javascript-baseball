525eb67b3c8ce5ffae6dcdbe76ccef84f4addd54
hyunjoebrother <hyunjoe31@gmail.com>
2022-11-14 17:11:35 +0900

    feat: MissionUtils 라이브러리 내 API 사용하여 게임 시작 문구 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+
 class App {
-  play() {}
+  showStartText() {
+    Console.print("숫자 야구 게임을 시작합니다.");
+  }
+
+  play() {
+    this.showStartText();
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
 // module : 프로그램을 구성하는 구성 요소, 관련 data와 함수를 하나로 묶은 단위
