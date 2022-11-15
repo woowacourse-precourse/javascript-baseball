58bbd3ca451325c0ff5c99a378f5f79380a195dc
froggy1014 <clcl6084@gmail.com>
2022-11-16 02:45:13 +0900

    refactor: esLint, prettier 적용

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 const { Console } = require('@woowacourse/mission-utils');
 const GameUtil = require('./GameUtil');
 
-class App extends GameUtil{
+class App extends GameUtil {
   constructor() {
     super();
     this.init();
@@ -22,32 +22,35 @@ class App extends GameUtil{
     const result = super.validateInput(playerInput);
     if (result === 'PASS') return this.printHint(playerInput);
 
-    throw new Error(result);  
+    throw new Error(result);
   }
 
   printHint(playerInput) {
     const ball = super.countBall(this.randoms, playerInput);
     const strike = super.countStrike(this.randoms, playerInput);
-    
+
     if (strike === 3) return this.correctAnswer();
-    
+
     Console.print(super.showHint(ball, strike));
-    this.play();
+    return this.play();
   }
-  
-  correctAnswer() { 
+
+  correctAnswer() {
     Console.print('3스트라이크');
     Console.print('3개의 숫자를 모두 맞히셨습니다! 게임 종료');
-    Console.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n', (decision) => {
-      switch (Number(decision)) {
+    Console.readLine(
+      '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n',
+      (decision) => {
+        switch (Number(decision)) {
         case 1:
           return this.restart();
         case 2:
           return this.terminate();
         default:
           return this.correctAnswer();
-      };
-    });
+        }
+      },
+    );
   }
 
   restart() {
@@ -55,7 +58,7 @@ class App extends GameUtil{
     this.play();
   }
 
-  terminate() {
+  static terminate() {
     Console.print('게임 종료');
     Console.close();
   }
