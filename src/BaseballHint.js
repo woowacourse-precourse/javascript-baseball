fc356ac1c41e22bd9a6d8b666ca02b2c11546d65
cruelladevil <dev.timetravel@gmail.com>
2022-11-17 03:26:06 +0900

    feat(BaseballHint): add validation

--- src/BaseballHint.js
+++ src/BaseballHint.js
@@ -8,7 +8,23 @@ class BaseballHint {
     this.#ball = ball;
   }
 
-  validate(strike, ball) {}
+  validate(strike, ball) {
+    if (!this.#isNumber(strike) || !this.#isNumber(ball)) {
+      throw new Error('스트라이크와 볼 갯수는 숫자여야 합니다.');
+    }
+
+    if (!this.#isInRange(strike, 0, 3) || !this.#isInRange(ball, 0, 3)) {
+      throw new Error('스트라이크와 볼은 최소 0개부터 최대 3개입니다.');
+    }
+  }
+
+  #isNumber(value) {
+    return typeof value === 'number';
+  }
+
+  #isInRange(value, min, max) {
+    return value >= min && value <= max;
+  }
 
   toString() {
     if (this.#strike === 0 && this.#ball === 0) return '낫싱';
