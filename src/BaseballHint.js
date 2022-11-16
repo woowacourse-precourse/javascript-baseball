eef7713c06b44b537e34f0413543cfe0c9d4f578
cruelladevil <dev.timetravel@gmail.com>
2022-11-17 02:53:45 +0900

    feat(BaseballHint): toString

--- src/BaseballHint.js
+++ src/BaseballHint.js
@@ -9,6 +9,15 @@ class BaseballHint {
   }
 
   validate(strike, ball) {}
+
+  toString() {
+    if (this.#strike === 0 && this.#ball === 0) return '낫싱';
+
+    const ballString = this.#ball > 0 ? `${this.#ball}볼` : '';
+    const strikeString = this.#strike > 0 ? `${this.#strike}스트라이크` : '';
+
+    return `${ballString} ${strikeString}`.trim();
+  }
 }
 
 module.exports = BaseballHint;
