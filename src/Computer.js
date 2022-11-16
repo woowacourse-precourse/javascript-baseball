f43ee40521a5ea6333bb098fce043b5080a164a5
cruelladevil <dev.timetravel@gmail.com>
2022-11-17 02:48:08 +0900

    refactor(Computer): pickRandomBaseball, giveHint

--- src/Computer.js
+++ src/Computer.js
@@ -1,42 +1,29 @@
 const { Random } = require('@woowacourse/mission-utils');
-const ValidationError = require('./ValidationError');
+const Baseball = require('./Baseball');
 
 class Computer {
-  static createUniqueNumbers(start, end, count) {
-    Computer.#validateUniqueNumbers(start, end, count);
+  #baseball;
 
+  pickRandomBaseball() {
+    const numbers = this.#pickRandomNumbers();
+    this.#baseball = new Baseball(numbers);
+  }
+
+  #pickRandomNumbers() {
     const numberSet = new Set();
 
-    while (numberSet.size !== count) {
-      const number = Random.pickNumberInRange(start, end);
+    while (numberSet.size < 3) {
+      const number = Random.pickNumberInRange(1, 9);
       numberSet.add(number);
     }
 
     return [...numberSet];
   }
 
-  static #validateUniqueNumbers(start, end, count) {
-    if (
-      !Number.isInteger(start) ||
-      !Number.isInteger(end) ||
-      !Number.isInteger(count)
-    ) {
-      throw new ValidationError('인수는 정수여야 합니다.');
-    }
+  giveHint(numbers) {
+    this.#baseball.validate(numbers);
 
-    if (start > end) {
-      throw new ValidationError('start가 end보다 커서는 안됩니다.');
-    }
-
-    if (count < 0) {
-      throw new ValidationError('count는 0보다 작으면 안됩니다.');
-    }
-
-    if (count > end - start + 1) {
-      throw new ValidationError(
-        `count: ${count}가 input 범위 (end - start + 1): ${end - start + 1}보다 커서는 안됩니다.`
-      );
-    }
+    return this.#baseball.createHint(numbers);
   }
 }
 
