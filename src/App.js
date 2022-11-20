4c3660442930bacdadec33d0e6ba43b8a4f75330
Yena Lee <lmonari71@gmail.com>
2022-11-21 00:02:06 +0900

    feat: user input validation + 결과 판별

--- src/App.js
+++ src/App.js
@@ -37,6 +37,32 @@ class App {
     }
     return [result, err];
   }
+  userInputValue(input) {
+    const [isValidate, err] = this.userInputValidation(input);
+    if (!isValidate) throw new Error(err);
+    const [strikeCount, ballCount] = this.getWholeCount(input);
+    if (!strikeCount && !ballCount) MissionUtils.Console.print("낫싱");
+    else {
+      if (ballCount && !strikeCount) {
+        MissionUtils.Console.print(`${ballCount}볼`);
+      } else if (ballCount && strikeCount) {
+        MissionUtils.Console.print(`${ballCount}볼 ${strikeCount}스트라이크`);
+      } else if (!ballCount && strikeCount) {
+        MissionUtils.Console.print(`${strikeCount}스트라이크`);
+      }
+    }
+    if (strikeCount === 3) {
+      MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+      MissionUtils.Console.readLine(
+        "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
+        (input) => this.userResult(input)
+      );
+    } else {
+      MissionUtils.Console.readLine("숫자를 입력해주세요: ", (input) =>
+        this.userInputValue(input)
+      );
+    }
+  }
 }
 const app = new App();
 app.play();
