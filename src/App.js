57026317726bc44b9f981e2f4615bdd389925521
Yena Lee <lmonari71@gmail.com>
2022-11-21 00:00:51 +0900

    feat: user input 받기

--- src/App.js
+++ src/App.js
@@ -20,6 +20,23 @@ class App {
     }
     return arr;
   }
+  userInputValidation(input) {
+    const userInputReg = /[1-9]{3}/g;
+    let err = "";
+    let result = true;
+    const checkarr = new Set([...input]);
+    if (input.length !== 3) {
+      err = "3글자를 입력해주세요";
+      result = false;
+    } else if (!userInputReg.test(input)) {
+      err = "1-9 사이의 값을 입력해주세요.";
+      result = false;
+    } else if (checkarr.size !== 3) {
+      err = "서로 다른 값을 입력해주세요.";
+      result = false;
+    }
+    return [result, err];
+  }
 }
 const app = new App();
 app.play();
