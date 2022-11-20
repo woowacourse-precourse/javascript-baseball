461663d55556d7ca4f39423ca3fb7d3e1fe32854
Yena Lee <lmonari71@gmail.com>
2022-11-20 23:54:58 +0900

    feat: 게임 시작 문구 출력

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import * as MissionUtils from "@woowacourse/mission-utils";
-
+const MissionUtils = require("@woowacourse/mission-utils");
 class App {
   constructor() {
     this.computerValue = [];
   }
   play() {
     this.computerValue = this.renderComputerValue();
-    this.gameStart();
+    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
+    MissionUtils.Console.readLine("숫자를 입력해주세요: ", (input) =>
+      this.userInputValue(input)
+    );
   }
   renderComputerValue() {
     let arr = [];
