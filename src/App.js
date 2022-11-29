b56860776cb1bf4deee356aaa14938f62f7cf129
violetwhenisawu <anashape710@gmail.com>
2022-11-29 19:54:37 +0900

    refactor: mvc 구조에 맞춰 파일 이동

--- src/App.js
+++ src/App.js
@@ -1,59 +1,14 @@
-const GAME_RESULT = require('./Baseball/gameResult');
-const GET_COMPUTER_NUM = require('./Baseball/computerNum');
-const INPUT_CHECK = require('./Baseball/inputCheck');
-
-const InputView = require('../src/view/InputView');
-const OutputView = require('../src/view/OutputView');
+const Controller = require('../src/controller/Controller');
 
 class App {
+  #controller;
+
   constructor() {
-    this.userInputNum = '';
-    this.COMPUTER_NUM;
+    this.#controller = new Controller();
   }
 
   play() {
-    OutputView.printGameStart();
-    this.startGame();
-  }
-
-  startGame() {
-    this.COMPUTER_NUM = GET_COMPUTER_NUM.getComputerRandomNum();
-    this.inputGameNum();
-  }
-
-  inputGameNum() {
-    InputView.readInputNum(this.getInputNum.bind(this));
-  }
-
-  getInputNum(gameNum) {
-    this.userInputNum = gameNum;
-    INPUT_CHECK.checkInputValidation(this.userInputNum);
-
-    this.gameResult();
-    this.inputGameNum();
-  }
-
-  gameResult() {
-    const getGameHint = GAME_RESULT.gameCounter(
-      this.userInputNum.split('').map(Number),
-      this.COMPUTER_NUM
-    );
-    OutputView.printGameHint(getGameHint);
-    if (getGameHint.strike === 3) this.inputGameOver();
-  }
-
-  inputGameOver() {
-    InputView.readRetry(this.getGameOverSelect.bind(this));
-  }
-
-  getGameOverSelect(userSelect) {
-    INPUT_CHECK.checkUserSelect(userSelect);
-    this.runSelectResult(userSelect);
-  }
-
-  runSelectResult(userSelect) {
-    if (userSelect === '1') this.startGame();
-    if (userSelect === '2') OutputView.printGameEnd();
+    this.#controller.start();
   }
 }
 
--- src/model/baseballGame.js
+++
@@ -1,3 +0,0 @@
-class baseballGame {}
-
-module.exports = baseballGame;
