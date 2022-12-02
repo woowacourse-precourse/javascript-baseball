adb08a30a89ada5e8c271cdd54005d66edc86df7
ming0830 <minsun0830@naver.com>
2022-12-02 23:49:46 +0900

    refactor : 불필요한 코드 제거

--- src/Model/BaseballMaker.js
+++ src/Model/BaseballMaker.js
@@ -2,16 +2,14 @@ const MissionUtils = require("@woowacourse/mission-utils");
 
 const BaseballMaker = {
   getRandomNumber() {
-    const MAX_RANGE = 9;
     const MIN_RANGE = 1;
+    const MAX_RANGE = 9;
     const randomNumber = new Set();
     while (randomNumber.size !== 3) {
       randomNumber.add(
         MissionUtils.Random.pickNumberInRange(MIN_RANGE, MAX_RANGE)
       );}
-    const uniqueNumberList = [...randomNumber];
-    this.uniqueNumberList = uniqueNumberList;
-    return uniqueNumberList;
+    return [...randomNumber]
   }};
 
 module.exports = BaseballMaker;
