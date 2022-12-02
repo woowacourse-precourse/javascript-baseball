2a40f00b1fe4a542f84ed00ed8148b9bba370bdb
ming0830 <minsun0830@naver.com>
2022-12-02 23:54:18 +0900

    refactor : MVC패턴 적용으로 파일 정리, controller로 연결을 위함

--- src/App.js
+++ src/App.js
@@ -1,99 +1,15 @@
-const MissionUtils = require("@woowacourse/mission-utils");
-const IsVaildNumber = require("../src/IsVaildNumber");
-
-class App{
-  constructor(){
-    this.uniqueNumberList =[];
-  }
-
-  
-  
-  getRandomNumber(){
-    const Max_RANGE = 9;
-    const MIN_RANGE = 1
-    const randomNumber = new Set();
-    while(randomNumber.size !==3){
-    randomNumber.add(MissionUtils.Random.pickNumberInRange(MIN_RANGE,Max_RANGE));
-    }
-    const uniqueNumberList = [...randomNumber]
-    this.uniqueNumberList = uniqueNumberList;
-    return uniqueNumberList
+const Controller = require('../src/Controller/Controller');
+class App {
+  constructor() {
+    this.controller = new Controller();
   }
   
-  
-  userInput(){
-    MissionUtils.Console.readLine('숫자를 입력해주세요 : ',(userInputValue)=>{
-    IsVaildNumber.isVaildUserInput(userInputValue);
-    const InputNumber = userInputValue.split('').map((v)=>parseInt(v));
-    this.getHintCount(InputNumber);
-    });
+  play() {
+    this.controller.Hello();
   }
-
-
-  getHintCount(userInput){
-    let COUNT_STRIKE = 0;
-    let COUNT_DUPLICATION= 0;
-    
-    if(this.uniqueNumberList === userInput) COUNT_STRIKE = 3;
-    
-    for(let index = 0; index<this.uniqueNumberList.length;index++){
-    if(this.uniqueNumberList[index] === userInput[index]) COUNT_STRIKE++
-    if(userInput.includes(this.uniqueNumberList[index])) COUNT_DUPLICATION++
-    }
-    const COUNT_BALL = COUNT_DUPLICATION - COUNT_STRIKE ;
-    this.printResult(COUNT_STRIKE,COUNT_BALL);
-  }
-
-  printResult(countStrike,countBall){
-  const printMessage = MissionUtils.Console.print;
-
-  if(countStrike===3)
-  printMessage('3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료');
-  if(countStrike ===0 && countBall ===0)
-  printMessage('낫싱');
-  if(countStrike > 0 && countBall >0)
-  printMessage(`${countBall}볼 ${countStrike}스트라이크`);
-  if((countStrike > 0 && countBall ===0) && countStrike !==3)
-  printMessage(`${countStrike}스트라이크`);
-  if(countStrike === 0 && countBall >0)
-  printMessage(`${countBall}볼`);
-
-  this.isSuccessGame(countStrike)
-  }
-
-  isSuccessGame(strike){
-    if(strike ===3){
-    MissionUtils.Console.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n',(optionNumber)=>{
-    IsVaildNumber.IsVaildOptionNumber(optionNumber);
-    this.reStart(optionNumber);
-    this.gameExit(optionNumber);
-    });
-    } else this.userInput();    
-  }
-
-  reStart(reStartNumber){
-    const RESTART_NUMBER = '1';
-    if(reStartNumber === RESTART_NUMBER) this.startGame();
-  }
-
-  gameExit(exitNumber){
-    const EXIT_NUMBER = '2';
-    if(exitNumber === EXIT_NUMBER){
-    MissionUtils.Console.print('게임 종료');
-    MissionUtils.Console.close();
-    }
-  }
-  
-  startGame(){
-    this.getRandomNumber();
-    this.userInput(); 
-  }
-
-  play(){
-    MissionUtils.Console.print('숫자 야구 게임을 시작합니다.');
-    this.startGame();
-  }
-  
 }
+
+const app = new App();
+app.play()
 
 module.exports = App;
