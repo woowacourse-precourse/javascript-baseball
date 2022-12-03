8d44ae2a1ddd6681ffef87568dfa0930f4e5673e
ming0830 <minsun0830@naver.com>
2022-12-04 00:03:55 +0900

    fix : 정규표현식 수정

--- src/util/IsValidNumber.js
+++ src/util/IsValidNumber.js
@@ -7,12 +7,12 @@ class IsvalidNumber{
     if(uniqueUserNum.size !== number.length){
       throw new Error("중복되지 않는 숫자를 입력하세요.");
     }
-    const numPattern = /[^0-9]/g;
+    const numPattern = /[0-9]$/g;
     if(!numPattern.test(number)) throw new Error("숫자만 입력하세요.");
   }
 
   isValidOptionNumber(optionNum){
-    const optionPattern = /[^1-2]/g;
+    const optionPattern = /[12]$/g;
     if(!optionPattern.test(optionNum)) throw new Error("1 또는 2 만 입력 가능합니다.");
   }
 }
