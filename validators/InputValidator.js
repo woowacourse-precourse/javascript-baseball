87f8b31f179325b49e54588acefc69a071cd9ace
jjjjjeongwon <jjjjjwon21@gmail.com>
2022-12-13 22:19:53 +0900

    refactor(InputValidator): 중복 에러메시지 제거

--- validators/InputValidator.js
+++ validators/InputValidator.js
@@ -2,41 +2,41 @@ const MESSAGE = require('../constants/gameMessages');
 const NUMBER = require('../constants/gameSetting');
 
 class InputValidator {
+  static checkExceptNumber(answer) {
+    return answer?.split('').map(Number)
+      .includes(NUMBER.EXCEPT);
+  }
 
-    static checkExceptNumber (answer) {
-        if(answer?.split('').map(Number)
-        .includes(NUMBER.EXCEPT)) throw new Error(MESSAGE.GAME.ERROR);
-        
+  static checkNumber(answer) {
+    return isNaN(answer);
+  }
+
+  static checkThreeNumber(answer) {
+    return answer?.toString().length !== NUMBER.RANDOM_LENGTH;
+  }
+
+  static checkDuplication(answer) {
+    const inputList = answer?.split('');
+    const setCollection = new Set(inputList);
+    return setCollection.size !== inputList?.length;
+  }
+
+  static checkBaseballNumber(number) {
+    if (
+      this.checkExceptNumber(number)
+      || this.checkNumber(number)
+      || this.checkThreeNumber(number)
+      || this.checkDuplication(number)
+    ) {
+      throw new Error(MESSAGE.GAME.ERROR);
     }
+  }
 
-    static checkNumber (answer) {
-        if(isNaN(answer)) throw new Error(MESSAGE.GAME.ERROR);
-    }
-
-    static checkThreeNumber (answer) {
-        if(answer?.toString().length !== NUMBER.RANDOM_LENGTH) throw new Error(MESSAGE.GAME.ERROR);
-        
-    }
-
-    static checkDuplication (answer) {
-        const inputList = answer?.split('');
-        const setCollection = new Set(inputList);
-        if(setCollection.size !== inputList?.length) throw new Error(MESSAGE.GAME.ERROR);
-    }
-
-    static checkBaseballNumber (number) {
-        this.checkExceptNumber(number);
-        this.checkNumber(number);
-        this.checkThreeNumber(number);
-        this.checkDuplication(number);
-    }
-
-    static checkInputRestartExit (input) {
-        if (input === NUMBER.RESTART) return true;
-        if (input === NUMBER.EXIT) return false;
-        throw new Error(MESSAGE.GAME.ERROR);
-    }
-
+  static checkInputRestartExit(input) {
+    if (input === NUMBER.RESTART) return true;
+    if (input === NUMBER.EXIT) return false;
+    throw new Error(MESSAGE.GAME.ERROR);
+  }
 }
 
 module.exports = InputValidator;
