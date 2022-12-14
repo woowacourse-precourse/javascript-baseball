848155103257613d177e2c6dd0957b2762907173
박정규 <srk99279@naver.com>
2022-12-14 20:18:44 +0900

    remove : make empty for practice

--- src/App.js
+++ src/App.js
@@ -1,13 +1,5 @@
-const Controller = require("./controller/Controller");
-
 class App {
-  play() {
-    const controller = new Controller();
-    controller.startGame();
-  }
+  play() {}
 }
-
-const app = new App();
-app.play();
 
 module.exports = App;
